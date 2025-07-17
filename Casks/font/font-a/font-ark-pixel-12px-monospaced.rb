33f50331962452b263ed5285d258c4bc9967cb46
TakWolf <takwolf@foxmail.com>
2025-07-17 17:25:25 +0800

    font-ark-pixel-12px-monospaced 2025.07.17
    font-ark-pixel-12px-monospaced: remove no_autobump

--- Casks/font/font-a/font-ark-pixel-12px-monospaced.rb
+++ Casks/font/font-a/font-ark-pixel-12px-monospaced.rb
@@ -1,12 +1,10 @@
 cask "font-ark-pixel-12px-monospaced" do
-  version "2025.03.14"
-  sha256 "d8d2da69d5a6642c1d84e24325f3694308b86f0be5ea6a81be3cd73b8c44706e"
+  version "2025.07.17"
+  sha256 "81c3f269fe130cd0ae436dc6357ac77e1b209afae17bcc7a5183bb0f3efbbd60"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 12px Monospaced"
   homepage "https://github.com/TakWolf/ark-pixel-font"
-
-  no_autobump! because: :requires_manual_review
 
   font "ark-pixel-12px-monospaced-ja.otf"
   font "ark-pixel-12px-monospaced-ko.otf"
