ddd4216ea144e798d7309e0b351d1ad2132a86cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-19 12:55:33 +0000

    font-iosevka-curly-slab 33.2.7

--- Casks/font/font-i/font-iosevka-curly-slab.rb
+++ Casks/font/font-i/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "33.2.6"
-  sha256 "67e20e3f6c449d7fe1dc05f06cdd70f2665a2a4ad4905006b71e2ffa8b52103e"
+  version "33.2.7"
+  sha256 "c6dfcef8c0ca413fd0318c81fd62b08cac47cd1a3d7ee8dbf536f1a9cec66843"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/SuperTTC-IosevkaCurlySlab-#{version}.zip"
   name "Iosevka Curly Slab"
