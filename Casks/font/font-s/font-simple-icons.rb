e6d00c13800ae9eafdc3282cc81099411b629a29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-20 04:09:44 +0000

    font-simple-icons 15.7.0

--- Casks/font/font-s/font-simple-icons.rb
+++ Casks/font/font-s/font-simple-icons.rb
@@ -1,6 +1,6 @@
 cask "font-simple-icons" do
-  version "15.6.0"
-  sha256 "36a2a2249125e3768e3a1f1bfd0b7c3adfea69be50449e00f0dd28312c42125c"
+  version "15.7.0"
+  sha256 "591fc2f6a8b9b5a6a1e472c7c7e1c3c8520a1b1747945eafbc26acb3c1983b10"
 
   url "https://github.com/simple-icons/simple-icons-font/releases/download/#{version}/simple-icons-font-#{version}.zip",
       verified: "github.com/simple-icons/simple-icons-font/"
