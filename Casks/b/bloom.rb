6705e93eccde91d76a5607dda5c0a6d2796d918f
INCHMAN1900 <asiafu1900@gmail.com>
2025-07-20 14:44:04 +0800

    fix: cask for Bloom

--- Casks/b/bloom.rb
+++ Casks/b/bloom.rb
@@ -2,16 +2,16 @@ cask "bloom" do
   version "1.3.1"
   sha256 "50a6f99880fa995393d0b50f178bce0c4e63348181eb690020cf1198bb6fd44f"
 
-  url "https://github.com/INCHMAN1900/bloom-release/releases/download/v#{version}/Bloom-v#{version}.zip"
+  url "https://bloomapp.club/downloads/bloom/Bloom%20-%20v#{version}.zip"
   name "Bloom"
-  desc "Refined file manager"
+  desc "File manager"
   homepage "https://bloomapp.club/"
 
   depends_on macos: ">= :sonoma"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://bloomapp.club/downloads/bloom/appcast.xml"
+    strategy :sparkle, &:short_version
   end
 
   app "Bloom.app"
