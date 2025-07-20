02f0e99164bb787d593a109403c6bdf3bad03453
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-20 21:44:11 +0000

    font-ark-pixel-10px-monospaced 2025.07.21

--- Casks/font/font-a/font-ark-pixel-10px-monospaced.rb
+++ Casks/font/font-a/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-monospaced" do
-  version "2025.07.17"
-  sha256 "237799b4c87fb562b2901b5806d99400746fbec223f805f28f3ceab813f91191"
+  version "2025.07.21"
+  sha256 "421c0f9abf93e11c92c084593889714452a6008c42e2d697cfd379d6c8f1c206"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 10px Monospaced"
