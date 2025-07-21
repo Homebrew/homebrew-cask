a116abe400f7c9838a66c674ea68925362ec0dae
moljac <mcvjetko@holisticware.net>
2025-07-21 15:24:45 +0200

    Update microsoft-openjdk@11.rb
    case sensitivity fix

--- Casks/m/microsoft-openjdk@11.rb
+++ Casks/m/microsoft-openjdk@11.rb
@@ -5,7 +5,7 @@ cask "microsoft-openjdk@11" do
   sha256 arm:   "989c81816807fda0432f0b6928d55bc55268b71f25fc983a9ee89b4af10864b2",
          intel: "80f0f0db4d56fd38fc374a3f89ba6f646431e33d37f7e892ffc283e94236e034"
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-#{arch}.pkg",
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macos-#{arch}.pkg",
       verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
@@ -13,12 +13,12 @@ cask "microsoft-openjdk@11" do
 
   livecheck do
     url "https://docs.microsoft.com/java/openjdk/download"
-    regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macOS[._-]#{arch}\.pkg}i)
+    regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macos[._-]#{arch}\.pkg}i)
   end
 
   no_autobump! because: :requires_manual_review
 
-  pkg "microsoft-jdk-#{version}-macOS-#{arch}.pkg"
+  pkg "microsoft-jdk-#{version}-macos-#{arch}.pkg"
 
   uninstall pkgutil: "com.microsoft.#{version.major}.jdk"
 
