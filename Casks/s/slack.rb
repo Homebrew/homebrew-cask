6518cde6bc2cec9484a04923b9b23492c6997059
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-22 01:21:41 +0000

    slack 4.45.64

--- Casks/s/slack.rb
+++ Casks/s/slack.rb
@@ -14,9 +14,9 @@ cask "slack" do
     end
   end
   on_big_sur :or_newer do
-    version "4.45.60"
-    sha256 arm:   "6c78bfdf183cf8ed5b1aa8178190ecb507cbef956e780e1292f7e325c9b0747e",
-           intel: "6d7b74e86bde7afa9b65dfb791efa7daa2195d1f73b814baa813b644b5a1db9b"
+    version "4.45.64"
+    sha256 arm:   "728a7deec2134a31591baefa20f69c9432c400cd626180ccb55f2d7f42cbcc8c",
+           intel: "ed2cd254cfb682d9ac5709a8654977590708f6c8b8ea7b2d6c65065e5f98cacf"
 
     url "https://downloads.slack-edge.com/desktop-releases/mac/#{arch}/#{version}/Slack-#{version}-macOS.dmg",
         verified: "downloads.slack-edge.com/"
