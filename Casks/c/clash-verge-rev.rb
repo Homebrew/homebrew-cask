f8047321603c3e0494f00ad4b0b2ba1579e765af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-22 03:55:36 +0000

    clash-verge-rev 2.3.2

--- Casks/c/clash-verge-rev.rb
+++ Casks/c/clash-verge-rev.rb
@@ -1,9 +1,9 @@
 cask "clash-verge-rev" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.3.1"
-  sha256 arm:   "2a0342b819408f182befcaa45123b443bde52d6da51ee5e69661a0da3809d1b4",
-         intel: "d644e9bbab2d83c4e72371ceb1c6c02a61338c0821f75d184401b18acb951e9d"
+  version "2.3.2"
+  sha256 arm:   "5a11e6a3b3a4dc2aa0fb68b153af735537c2c09a2f73958c0358db4ad2b0d7a8",
+         intel: "1ffd35719717bd2e797e2bf4aff8149d8647fc5330dc17deac83e084e9d25fc7"
 
   url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg",
       verified: "github.com/clash-verge-rev/clash-verge-rev/"
