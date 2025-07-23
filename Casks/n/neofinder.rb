50c0a4571d560bc9248d0c57ac291f7b63edf179
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-23 10:01:54 +0000

    neofinder 8.9.1

--- Casks/n/neofinder.rb
+++ Casks/n/neofinder.rb
@@ -1,6 +1,6 @@
 cask "neofinder" do
-  version "8.9"
-  sha256 "9bc6c20b77db6cf63d35e9aecdd9d35e1114bdb39a957d4e245df4d556c359ba"
+  version "8.9.1"
+  sha256 "1f7c7e7efac83aacf7cc83018e4513b07f92f8f66d5cae6bed53b93636202482"
 
   url "https://www.wfs-apps.de/updates/neofinder-mac.#{version}.zip",
       verified: "wfs-apps.de/"
