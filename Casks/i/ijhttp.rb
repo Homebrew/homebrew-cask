8e7596c8b8b411c8750f5d2616d6d8ee1b4ab8f8
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-07-23 16:09:01 +0200

    ijhttp: enable autobumps again, and simplify `#{version}` in `url`

--- Casks/i/ijhttp.rb
+++ Casks/i/ijhttp.rb
@@ -2,7 +2,7 @@ cask "ijhttp" do
   version "243.24978.46"
   sha256 "2fdbbf634a43fce0f6236597ea6815e6b88ff32ec893eeb355c33f59926ceeb1"
 
-  url "https://download.jetbrains.com/resources/intellij/http-client/#{version.major_minor_patch}/intellij-http-client.zip"
+  url "https://download.jetbrains.com/resources/intellij/http-client/#{version}/intellij-http-client.zip"
   name "IntelliJ HTTP Client CLI"
   desc "HTTP client from JetBrains IDEs available as a standalone CLI tool"
   homepage "https://www.jetbrains.com/ijhttp/download"
@@ -11,8 +11,6 @@ cask "ijhttp" do
     url "https://jb.gg/ijhttp/latest"
     strategy :header_match
   end
-
-  no_autobump! because: :requires_manual_review
 
   binary "ijhttp/ijhttp"
 
