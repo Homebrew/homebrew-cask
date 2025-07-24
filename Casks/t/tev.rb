0315bae5bd6afbe5117ac2b83dbeda5461920b95
bevanjkay <email@bevankay.me>
2025-07-24 14:24:29 +1000

    tev 2.3

--- Casks/t/tev.rb
+++ Casks/t/tev.rb
@@ -1,9 +1,9 @@
 cask "tev" do
   arch intel: "-intel"
 
-  version "2.2"
-  sha256 arm:   "2068cb3d5516e325d459bfa1a6816d7a9245f736be7f23eaf01bacdb8d707b6d",
-         intel: "5ad9a44afd6e8d1dd89db347f44e6425aa86fbab036aa0a2bfb51535c7d11228"
+  version "2.3"
+  sha256 arm:   "891a943d0d5914977582da57bf4da338a92c612e3b3f4d5b71f00bdfb84222fa",
+         intel: "7ff901843c72314bc993f34b9d6745c63db00d396381bacee391512ec7772710"
 
   url "https://github.com/Tom94/tev/releases/download/v#{version}/tev#{arch}.dmg"
   name "tev"
@@ -15,7 +15,7 @@ cask "tev" do
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  no_autobump! because: :requires_manual_review
+  disable! date: "2026-09-01", because: :unsigned
 
   depends_on macos: ">= :catalina"
 
