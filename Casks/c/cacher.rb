6890630315569894502014c9518b6eb27d8200e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-24 21:54:16 +0000

    cacher 2.47.7

--- Casks/c/cacher.rb
+++ Casks/c/cacher.rb
@@ -1,6 +1,6 @@
 cask "cacher" do
-  version "2.47.6"
-  sha256 "1976a9ffbe92154ae34a714475852ea307d9b0a8c8aae4b0828968ed89bfd1c7"
+  version "2.47.7"
+  sha256 "8d649dae3468716a10752462650685a3dff17957195ebe402743fc955cc5275b"
 
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-universal-mac.zip",
       verified: "cacher-download.nyc3.digitaloceanspaces.com/"
