31bcc40cc06abbc795cafdeb01be8b2cfabfe491
Bevan Kay <email@bevankay.me>
2025-07-24 12:00:58 +1000

    chromedriver: deprecate intel version

--- Casks/c/chromedriver.rb
+++ Casks/c/chromedriver.rb
@@ -5,6 +5,10 @@ cask "chromedriver" do
   sha256 arm:   "c64e1ad395a310c8db4926b335618ef32a03acdc6c69a29d61f0146486b6dc13",
          intel: "26fc85d280f5563a08e736e99fcdbd98f817ac80d6c032c4afdf89f32257ce75"
 
+  on_intel do
+    disable! date: "2026-09-01", because: :unsigned
+  end
+
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
   name "ChromeDriver"
