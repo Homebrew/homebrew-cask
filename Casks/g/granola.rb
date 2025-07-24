ec72d5d5a0ff88c0b2fbc4def382c9a3d22b2c94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-24 09:55:13 +0000

    granola 6.119.0

--- Casks/g/granola.rb
+++ Casks/g/granola.rb
@@ -1,6 +1,6 @@
 cask "granola" do
-  version "6.107.0"
-  sha256 "0728031f58228436de0da6152d5d913a45f3289a4f1a266f0f1e3a6962c366e9"
+  version "6.119.0"
+  sha256 "ed0b154cedb843e005b9aa814fac3e82b3d364fbf121b2e3f8001ff54b2ecb7f"
 
   url "https://dr2v7l5emb758.cloudfront.net/#{version}/Granola-#{version}-mac-universal.dmg",
       verified: "dr2v7l5emb758.cloudfront.net/"
