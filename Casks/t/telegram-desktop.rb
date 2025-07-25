862ca8ff30bdc75be0d1c8296fe159a9d82504fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-25 07:12:33 +0000

    telegram-desktop 5.16.5

--- Casks/t/telegram-desktop.rb
+++ Casks/t/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "5.16.4"
-  sha256 "2a4e94cd05e05ab6ea55ebecbf773904050522044fb0bedaf1b4c13eca01b556"
+  version "5.16.5"
+  sha256 "eae1dd40fd2fd2b3d57aeec51d72e9dc768011ab166dabf7de4951a627ee29b2"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
       verified: "updates.tdesktop.com/tmac/"
