ac50035ad27759c92eed057ae68bd0006da047d3
botantony <antonsm21@gmail.com>
2025-07-25 13:50:48 +0200

    hummingbird 2.0.0

--- Casks/h/hummingbird.rb
+++ Casks/h/hummingbird.rb
@@ -1,9 +1,9 @@
 cask "hummingbird" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.3.0"
-  sha256 arm:   "57201d496ffa5f3e259903ad30e0760e481573813aafee934ace63e6895aa2de",
-         intel: "277945f88a18799320d027ad6017f70c7c976bdcbcb542151df5284f24bba1d4"
+  version "2.0.0"
+  sha256 arm:   "ded04a6b3909a5f001825851cf4941489fc3b780b382e6bac4bb9f18596b3b30",
+         intel: "43ce210c9e5b62bc0c2f147b4631a7d021c4bd8eb2f4cd49d937b0a887513122"
 
   url "https://eddie.website/repository/hummingbird/#{version}/hummingbird-macos-#{arch}-notarized-#{version}.zip",
       verified: "eddie.website/repository/hummingbird/"
@@ -15,8 +15,6 @@ cask "hummingbird" do
     url "https://airvpn.org/macos/hummingbird/"
     regex(/href=.*?hummingbird[._-]macos[._-]#{arch}[._-]notarized[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   depends_on macos: ">= :high_sierra"
 
