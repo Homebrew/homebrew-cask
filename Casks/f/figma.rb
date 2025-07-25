1f91f82570aef7c08558be4f5b866fca246795e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-25 00:53:28 +0000

    figma 125.6.5

--- Casks/f/figma.rb
+++ Casks/f/figma.rb
@@ -1,9 +1,9 @@
 cask "figma" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "125.5.6"
-  sha256 arm:   "d8b869dee0105ac5c42d2fb3b2f00397be5e2192a3c98d7dca9f6aebd6ddab12",
-         intel: "e2ad066c156ececd0b59bdc8f6ab5d369ac3ce50016887cb229cbcaff5de7c4b"
+  version "125.6.5"
+  sha256 arm:   "bbd7cd7cd9e37bbfa2d667913aca36144e927aea8083c5bb65d331c651aa08f3",
+         intel: "033100c997171f4ddd44c387279affffab51558827f1b7947039ca7e2eba1f53"
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
