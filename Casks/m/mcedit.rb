b667d76a9d60f670343cd8b86188a25e97684848
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-26 19:38:06 -0400

    mcedit: disable

--- Casks/m/mcedit.rb
+++ Casks/m/mcedit.rb
@@ -11,6 +11,7 @@ cask "mcedit" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-27", because: :unmaintained
+  disable! date: "2025-07-27", because: :unmaintained
 
   app "mcedit.app"
 
