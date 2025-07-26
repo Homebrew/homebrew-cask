b8f89e569253732a0bee90c7ef60ec144a0dd3ed
bevanjkay <email@bevankay.me>
2025-07-26 10:57:55 +1000

    aptible 0.24.8

--- Casks/a/aptible.rb
+++ Casks/a/aptible.rb
@@ -1,6 +1,6 @@
 cask "aptible" do
-  version "0.24.7,20250618130710,gh-56"
-  sha256 "eb5469f2381d0af8cdd5bd15a442e078003ad670c37ddf69d534da7d4c29cdd9"
+  version "0.24.8,20250725144154,gh-58"
+  sha256 "66a01ed123c22f2be69b3f1d3f7315aa12804ca0655106423adbe7bb6ad08203"
 
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.10.15.7-1.pkg",
       verified: "omnibus-aptible-toolbelt.s3.amazonaws.com/"
@@ -15,6 +15,8 @@ cask "aptible" do
       page.scan(regex).map { |match| "#{match[1]},#{match[2]},#{match[0]}" }
     end
   end
+
+  disable! date: "2026-09-01", because: :unsigned
 
   depends_on formula: "libfido2"
 
