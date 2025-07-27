dc1362ac39b9a72a38936ca4172cc01655dee82b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 13:17:08 +0000

    zen-privacy 0.11.2

--- Casks/z/zen-privacy.rb
+++ Casks/z/zen-privacy.rb
@@ -1,9 +1,9 @@
 cask "zen-privacy" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.11.1"
-  sha256 arm:   "3967b6df5e2c8e7af8f26a418c5d14d0554320b925009089e13a42f66f59ef8c",
-         intel: "a109bd0ed5eeed7bd8f76c3453f051e17200e002b5cbd5f2b87ac5b6036ec0ad"
+  version "0.11.2"
+  sha256 arm:   "8fb442a2a0db88400c898ba1d2e0829dc4a5064b53fde0110b93eae9b9d4a8e5",
+         intel: "faed1550012e525698c9b892844e15da51d917a6935378ed7ca055bbc53ca05b"
 
   url "https://github.com/ZenPrivacy/zen-desktop/releases/download/v#{version}/Zen_darwin_#{arch}_noselfupdate.tar.gz",
       verified: "github.com/ZenPrivacy/zen-desktop/"
