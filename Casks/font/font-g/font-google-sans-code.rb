dba776f3cf4cb6b0fb644c4a1b0d18aa167658e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 00:18:38 +0000

    font-google-sans-code: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-g/font-google-sans-code.rb
+++ Casks/font/font-g/font-google-sans-code.rb
@@ -3,10 +3,11 @@ cask "font-google-sans-code" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/googlesanscode"
   name "Google Sans Code"
-  homepage "https://github.com/googlefonts/googlesans-code"
+  homepage "https://fonts.google.com/specimen/Google+Sans+Code"
 
   font "GoogleSansCode-Italic[wght].ttf"
   font "GoogleSansCode[wght].ttf"
