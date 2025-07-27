c006c28466711b9c95040f0a058398022c2c0a8d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-27 19:58:17 -0400

    panda: disable

--- Casks/p/panda.rb
+++ Casks/p/panda.rb
@@ -10,6 +10,7 @@ cask "panda" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-28", because: :unmaintained
+  disable! date: "2025-07-28", because: :unmaintained
 
   auto_updates true
 
