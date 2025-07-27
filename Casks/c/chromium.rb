fad7421b6694e57b1f49c6b4e33a52e2204034c0
Bevan Kay <email@bevankay.me>
2025-07-27 15:58:40 +1000

    chromium: deprecate

--- Casks/c/chromium.rb
+++ Casks/c/chromium.rb
@@ -10,6 +10,8 @@ cask "chromium" do
   desc "Free and open-source web browser"
   homepage "https://www.chromium.org/Home"
 
+  disable! date: "2026-09-01", because: :unsigned
+
   conflicts_with cask: "ungoogled-chromium"
   depends_on macos: ">= :monterey"
 
