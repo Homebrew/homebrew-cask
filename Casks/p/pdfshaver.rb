3c4b8e09ea275fdccfbdcedb5ce2158d726396dc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-27 19:58:17 -0400

    pdfshaver: disable

--- Casks/p/pdfshaver.rb
+++ Casks/p/pdfshaver.rb
@@ -10,6 +10,7 @@ cask "pdfshaver" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-28", because: :unmaintained
+  disable! date: "2025-07-28", because: :unmaintained
 
   app "PDFShaver.app"
 
