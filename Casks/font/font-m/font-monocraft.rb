4eb8160253f6fd9f6bf323c29c666a11d5684a6f
botantony <antonsm21@gmail.com>
2025-07-27 13:49:35 +0200

    font-monocraft 4.1

--- Casks/font/font-m/font-monocraft.rb
+++ Casks/font/font-m/font-monocraft.rb
@@ -1,12 +1,10 @@
 cask "font-monocraft" do
-  version "4.0"
-  sha256 "481ce5fd7d8f40eab5718e1d96a3bcf410f00ab3fefcb63067d5f6e29d8b0c2b"
+  version "4.1"
+  sha256 "09eb83095442e8068ec843f15ccb35717fa783616f0a6079605c4c4522920381"
 
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft.ttc"
   name "Monocraft"
   homepage "https://github.com/IdreesInc/Monocraft"
-
-  no_autobump! because: :requires_manual_review
 
   font "Monocraft.ttc"
 
