9e3406e43bf79681e20da991300f967813d9fb11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 16:27:56 +0000

    windscribe 2.16.14

--- Casks/w/windscribe.rb
+++ Casks/w/windscribe.rb
@@ -1,6 +1,6 @@
 cask "windscribe" do
-  version "2.16.11"
-  sha256 "596b1889eaf52fdb2c67f51b443493b105d6e53bfb5c33066b70977080dee961"
+  version "2.16.14"
+  sha256 "5c1a9600407d8db9da769c02e4b9d39b3344b18ab662f95dd2eeb4303c0e232a"
 
   url "https://deploy.totallyacdn.com/desktop-apps/#{version}/Windscribe_#{version}_universal.dmg",
       verified: "deploy.totallyacdn.com/desktop-apps/"
