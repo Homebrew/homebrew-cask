dc0a485296caf6ab7c4ff8a885572072d72087f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 21:59:57 +0000

    repo-prompt 1.3.24

--- Casks/r/repo-prompt.rb
+++ Casks/r/repo-prompt.rb
@@ -1,6 +1,6 @@
 cask "repo-prompt" do
-  version "1.3.23"
-  sha256 "cd0bf93270a6a72e54489247669a2f6107ee84a6ecfa63a8f5a778c6d9b2b57c"
+  version "1.3.24"
+  sha256 "c67124849abf05ebca62b35791cb12f8d94fc92da4051ead28296ca0ee3e8de0"
 
   url "https://repoprompt.s3.us-east-2.amazonaws.com/RepoPrompt-#{version}.dmg",
       verified: "repoprompt.s3.us-east-2.amazonaws.com/"
