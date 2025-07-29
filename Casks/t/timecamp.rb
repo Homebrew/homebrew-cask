0a2f0210807e16ff4d8300aba1301e7e6071c4df
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-28 22:42:03 -0400

    timecamp: fix sha256, add minimum macOS version

--- Casks/t/timecamp.rb
+++ Casks/t/timecamp.rb
@@ -1,6 +1,6 @@
 cask "timecamp" do
   version "1.8.0.0"
-  sha256 "4989d37fc2805d67245111ec725bc0a69e2b5af2b39895fc296ed439e74e6dfa"
+  sha256 "2bba2a29157f4f18802296ad4807db001bb2f06f7a66b831254049615086623e"
 
   url "https://timecamp.s3.amazonaws.com/downloadsoft/#{version}/TimeCampSetup_macOS.dmg",
       verified: "timecamp.s3.amazonaws.com/"
@@ -13,7 +13,7 @@ cask "timecamp" do
     strategy :header_match
   end
 
-  no_autobump! because: :requires_manual_review
+  depends_on macos: ">= :high_sierra"
 
   app "TimeCamp.app"
 
