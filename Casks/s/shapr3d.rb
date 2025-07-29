bf81f0d11ce132fc581d763532cd801bd7339122
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 10:11:07 +0000

    shapr3d 5.911.0.9224

--- Casks/s/shapr3d.rb
+++ Casks/s/shapr3d.rb
@@ -1,6 +1,6 @@
 cask "shapr3d" do
-  version "5.910.0.9193"
-  sha256 "682c4034f4527bf6e211954bd9f1ce1edd61edbdaf303c1a7ee694f4d1e8cb94"
+  version "5.911.0.9224"
+  sha256 "6cc7cd550f62d27289cd84fec4e5d1687e1188d8ed5af71a64b357cbb729dc61"
 
   url "https://download.shapr3d.com/mac/Shapr3D-#{version}.dmg"
   name "Shapr3D"
