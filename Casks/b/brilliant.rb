5bcac0d9e0b598b0f0b56f0e1eeb5b2f14b7f1c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 12:46:47 +0000

    brilliant 1.8.0

--- Casks/b/brilliant.rb
+++ Casks/b/brilliant.rb
@@ -1,6 +1,6 @@
 cask "brilliant" do
-  version "1.7.4"
-  sha256 "493fedc1e31a33b1cc53be0fdfce7e897c7c5d838e7566ac3f8df4ff438606a5"
+  version "1.8.0"
+  sha256 "60d11ec41d72c896effd792fdc652611f15a0352e4685af4b44f2fad9ddaa559"
 
   url "https://s3.eu-north-1.amazonaws.com/brilliant.design/Brilliant.Design-#{version}-Installer.dmg",
       verified: "s3.eu-north-1.amazonaws.com/brilliant.design/"
