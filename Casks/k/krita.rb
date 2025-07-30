776bf6d42672d904b7871590a00052ba977ac867
Sean Molenaar <smillerdev@me.com>
2025-07-30 22:32:05 +0200

    krita: add linux appimage support

--- Casks/k/krita.rb
+++ Casks/k/krita.rb
@@ -1,9 +1,13 @@
 cask "krita" do
   version "5.2.13"
-  sha256 "17ccfbf2b72fcca314ce59c6d6f0b62dda1dc230ece71aa42f56031ef75ea6e8"
+  download_suffix = on_system_conditional linux: "-x86_64.AppImage", macos: "_signed.dmg"
+  sha256 arm: "17ccfbf2b72fcca314ce59c6d6f0b62dda1dc230ece71aa42f56031ef75ea6e8",
+         x86_64: "17ccfbf2b72fcca314ce59c6d6f0b62dda1dc230ece71aa42f56031ef75ea6e8",
+         x86_64_linux: "5fd9e52a25caa5cd7d3d3269898cceb1465b3ce497abd8eee0e84d7bd8668c91"
 
-  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}_signed.dmg",
-      verified: "download.kde.org/stable/krita/"
+  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}#{download_suffix}",
+        verified: "download.kde.org/stable/krita/"
+
   name "Krita"
   desc "Free and open-source painting and sketching program"
   homepage "https://krita.org/"
@@ -13,7 +17,14 @@ cask "krita" do
     regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)(?:[._-]signed|[._-]release)?\.dmg/i)
   end
 
-  app "krita.app"
+  on_macos do
+    app "krita.app"
+  end
+
+  on_linux do
+    depends_on arch: [:x86_64]
+    app_image "krita-#{version}#{download_suffix}"
+  end
 
   zap trash: [
     "~/Library/Application Support/krita",
