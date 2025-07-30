fbb7ed44421c4c49f8b85a14eda674c20dd72c58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-30 13:23:00 +0000

    visualboyadvance-m 2.2.2

--- Casks/v/visualboyadvance-m.rb
+++ Casks/v/visualboyadvance-m.rb
@@ -1,9 +1,9 @@
 cask "visualboyadvance-m" do
   arch arm: "ARM64", intel: "x86_64"
 
-  version "2.2.1"
-  sha256 arm:   "c4486e7cf7daf5fa40babf44f369c350cc6cc9945703615b5a2d68aff5db46cc",
-         intel: "5078da6545c660ed8c7b2d22598120677e0b227ec1eb4407f07fa10525c3fd1c"
+  version "2.2.2"
+  sha256 arm:   "1c3d1bf702d48c4a6e0c363ed07381d293c0a704eb2958e2b4d598235af7d5aa",
+         intel: "57ba43178a624d20c4df4879802675a75ae7579c2e10de7c1211e2c338ab71d7"
 
   url "https://github.com/visualboyadvance-m/visualboyadvance-m/releases/download/v#{version}/visualboyadvance-m-Mac-#{arch}.zip",
       verified: "github.com/visualboyadvance-m/visualboyadvance-m/"
