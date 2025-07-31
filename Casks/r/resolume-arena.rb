713e1c60a983aabc4be5ab7ffe603b4e29adf6e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 19:17:57 +0000

    resolume-arena 7.23.1

--- Casks/r/resolume-arena.rb
+++ Casks/r/resolume-arena.rb
@@ -1,6 +1,6 @@
 cask "resolume-arena" do
-  version "7.23.0,49330"
-  sha256 "c1f74a6f3a4ffb33ab46ce901b105190abc8b17d35a822c3240d6c8d5796ebab"
+  version "7.23.1,49731"
+  sha256 "de69062297e68e59359657677241ea3e38122f2d5bb5897160a134229ab24471"
 
   url "https://dd5sgwxv3xok.cloudfront.net/Resolume_Arena_#{version.csv.first.dots_to_underscores}_rev_#{version.csv.second}_Installer.dmg",
       verified: "dd5sgwxv3xok.cloudfront.net/"
