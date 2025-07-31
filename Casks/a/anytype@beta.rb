3385706bb25e30b3c48d37ca010a4b08655b47ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 12:44:11 +0000

    anytype@beta 0.47.53-beta

--- Casks/a/anytype@beta.rb
+++ Casks/a/anytype@beta.rb
@@ -1,9 +1,9 @@
 cask "anytype@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.47.49-beta"
-  sha256 arm:   "0b9be6f61e788ff98c8a8dba4cc6ddc06c84e1cc4c55dd474844e24a4e5be7bc",
-         intel: "df254e127154061812c4b61fdaa287ba8366bff810d47b5d4ba8d816688446f5"
+  version "0.47.53-beta"
+  sha256 arm:   "83e79409320559af5fcdd3a7e2e8e28b5e9aa704eee08a4c1ee8bb9aa246b586",
+         intel: "659df3f4032dd019370d6fdd36241af04541a804c679e925e946a42e2cf32502"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
