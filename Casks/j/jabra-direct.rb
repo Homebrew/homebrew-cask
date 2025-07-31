7c987cf54bf0cebf3abe070b7fcd56c8cb758192
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 21:57:20 +0000

    jabra-direct 6.24.20901

--- Casks/j/jabra-direct.rb
+++ Casks/j/jabra-direct.rb
@@ -1,5 +1,5 @@
 cask "jabra-direct" do
-  version "6.23.16101"
+  version "6.24.20901"
   sha256 :no_check
 
   url "https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg",
