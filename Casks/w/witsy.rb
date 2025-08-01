5bb5e3eeada87fa50ff3f5f247b06c04929d82d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-01 04:38:45 +0000

    witsy 2.12.4

--- Casks/w/witsy.rb
+++ Casks/w/witsy.rb
@@ -1,9 +1,9 @@
 cask "witsy" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.12.3"
-  sha256 arm:   "e64234c8d780a7a6149819f3a0c28018870c4db9876f006ccb741a8093967b10",
-         intel: "9bd84ed8f93e5fbfb5d35208a18e6247c99fcf3172160c0f4d86a294972b7475"
+  version "2.12.4"
+  sha256 arm:   "1bcfed461e3560ebc15189646caa7b06fdcdea8a70dd342a1905475e2f097f5e",
+         intel: "e5bfa1f56a1216cbe14f741fa3264197ffb10a0d7cfbe9bb1b7d56724f2634fe"
 
   url "https://github.com/nbonamy/witsy/releases/download/v#{version}/Witsy-#{version}-darwin-#{arch}.dmg",
       verified: "github.com/nbonamy/witsy/"
