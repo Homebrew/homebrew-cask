9433d9d089921644e4cbfea491b18763f61ef358
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-01 13:01:50 +0000

    font-maple-mono-normal-cn 7.5

--- Casks/font/font-m/font-maple-mono-normal-cn.rb
+++ Casks/font/font-m/font-maple-mono-normal-cn.rb
@@ -1,6 +1,6 @@
 cask "font-maple-mono-normal-cn" do
-  version "7.4"
-  sha256 "84d25f86c6b1467cefff763c1b369fe729bd5f10c4c953ab2d1dc4f45a4597cb"
+  version "7.5"
+  sha256 "6ee1638126b1d4064dc1db31abbcc2e25297c3fb5b79b9ea213c7f234846f9a8"
 
   url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMonoNormal-CN-unhinted.zip",
       verified: "github.com/subframe7536/Maple-font/"
