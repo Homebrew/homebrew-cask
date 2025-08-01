5ce00dd41d10b2514eb3f855cd98454d17920626
botantony <antonsm21@gmail.com>
2025-08-01 21:10:22 +0200

    x-swiftformat 2.1

--- Casks/x/x-swiftformat.rb
+++ Casks/x/x-swiftformat.rb
@@ -1,13 +1,11 @@
 cask "x-swiftformat" do
-  version "2.0.1"
-  sha256 "987fb40b489fd78d563ea16afa510e75ff6a8b73677459eb164baeb5446809dd"
+  version "2.1"
+  sha256 "06569a2abac144e1c3b9cee75fec9e3d6f4d70c41d3d55f48aa8bd924cb4cb75"
 
   url "https://github.com/ruiaureliano/X-SwiftFormat/releases/download/#{version}/x-swiftformat_#{version}.zip"
   name "X-SwiftFormat"
   desc "Xcode extension to format Swift code"
   homepage "https://github.com/ruiaureliano/X-SwiftFormat"
-
-  no_autobump! because: :requires_manual_review
 
   depends_on macos: ">= :ventura"
 
