1b552fb059853c3052a4f9c717b364b1ee82dbe4
bevanjkay <email@bevankay.me>
2025-08-02 17:45:00 +1000

    font-codicon 0.0.39

--- Casks/font/font-c/font-codicon.rb
+++ Casks/font/font-c/font-codicon.rb
@@ -1,14 +1,13 @@
 cask "font-codicon" do
-  version "0.0.37-1"
-  sha256 "24bb11a8641928b76c15246623a11ec74840e8a727de8c64b51fdca8ed4cdc4d"
+  version "0.0.39"
+  sha256 "27c3a63377c63504ae70eb1a70ecfc7f7503a3ed6a2f1b396950f3b97049ff04"
 
-  url "https://github.com/microsoft/vscode-codicons/releases/download/v#{version}/codicon-font-v#{version}.ttf"
+  url "https://registry.npmjs.org/@vscode/codicons/-/codicons-#{version}.tgz",
+      verified: "registry.npmjs.org/@vscode/codicons/"
   name "Codicon"
   homepage "https://github.com/microsoft/vscode-codicons"
 
-  no_autobump! because: :requires_manual_review
-
-  font "codicon-font-v#{version}.ttf"
+  font "package/dist/codicon.ttf"
 
   # No zap stanza required
 end
