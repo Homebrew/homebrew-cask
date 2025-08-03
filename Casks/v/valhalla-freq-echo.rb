eecf0e13fe526d1705e36599dd3cdb5b5748ea70
Bevan Kay <email@bevankay.me>
2025-08-03 23:36:23 +1000

    valhalla-freq-echo: update livecheck

--- Casks/v/valhalla-freq-echo.rb
+++ Casks/v/valhalla-freq-echo.rb
@@ -10,13 +10,11 @@ cask "valhalla-freq-echo" do
 
   livecheck do
     url "https://valhalladsp.com/demos-downloads/"
-    regex(/href=.*?ValhallaFreqEchoOSX[._-]v?(\d+(?:[._]\d+)+)v?\d?\.dmg/i)
+    regex(/ValhallaFreqEchoOSX[._-]v?(\d+(?:[._]\d+)+)v?\d?\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| match&.first&.tr("_", ".") }
     end
   end
-
-  no_autobump! because: :requires_manual_review
 
   pkg "ValhallaFreqEchoOSX.pkg"
 
