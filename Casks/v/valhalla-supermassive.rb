9eb18ecb8a3b78fd1f89113f229ca2364cbca519
Bevan Kay <email@bevankay.me>
2025-08-03 23:36:24 +1000

    valhalla-supermassive: update livecheck

--- Casks/v/valhalla-supermassive.rb
+++ Casks/v/valhalla-supermassive.rb
@@ -10,13 +10,11 @@ cask "valhalla-supermassive" do
 
   livecheck do
     url "https://valhalladsp.com/demos-downloads/"
-    regex(/href=.*?ValhallaSupermassiveOSX[._-]v?(\d+(?:[._]\d+)+)v?\d+\.dmg/i)
+    regex(/ValhallaSupermassiveOSX[._-]v?(\d+(?:[._]\d+)+)v?\d+\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| match&.first&.tr("_", ".") }
     end
   end
-
-  no_autobump! because: :requires_manual_review
 
   pkg "ValhallaSupermassiveOSX.pkg"
 
