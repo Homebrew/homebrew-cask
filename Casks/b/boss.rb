f61c68443e5dbb1055f8f79546e72e3f41b4af20
Shivang <shivang.iitk@gmail.com>
2025-08-02 20:28:26 -0700

    boss 8.10.1 (new cask)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/b/boss.rb
@@ -0,0 +1,22 @@
+cask "boss" do
+  version "8.10.1"
+  sha256 "9f8dab6a64731a5771044a0b2193441ae2f8226e0953418a19acc8f94cb012d9"
+
+  url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
+      verified: "github.com/risa-labs-inc/BOSS-Releases/"
+  name "BOSS"
+  desc "AI-powered workspace for complex business operations"
+  homepage "https://www.risalabs.ai/"
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "BOSS.app"
+
+  zap trash: [
+    "~/Library/Application Support/BOSS",
+    "~/Library/Caches/ai.rever.boss",
+    "~/Library/Preferences/ai.rever.boss.plist",
+    "~/Library/Saved Application State/ai.rever.boss.savedState",
+  ]
+end
