805657dea06fdfbd385bd6947fde8d1bae3eddf6
Bevan Kay <email@bevankay.me>
2025-08-03 23:08:37 +1000

    polkadot-js: deprecate

--- Casks/p/polkadot-js.rb
+++ Casks/p/polkadot-js.rb
@@ -8,26 +8,9 @@ cask "polkadot-js" do
   desc "Portal into the Polkadot and Substrate networks"
   homepage "https://polkadot.js.org/"
 
-  # Not every GitHub release provides a file for macOS, so we check multiple
-  # recent releases instead of only the "latest" release.
-  livecheck do
-    url :url
-    regex(/^Polkadot[._-]JS[._-]Apps[._-]mac[._-](\d+(?:\.\d+)*)\.(?:dmg|pkg|zip)$/i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"] || release["prerelease"]
-
-        release["assets"]&.map do |asset|
-          match = asset["name"]&.match(regex)
-          next if match.blank?
-
-          match[1]
-        end
-      end.flatten
-    end
-  end
-
-  no_autobump! because: :requires_manual_review
+  # Upstream is not currently building for MacOS
+  # ref: https://github.com/polkadot-js/apps/pull/11084
+  deprecate! date: "2025-08-03", because: :discontinued
 
   depends_on macos: ">= :high_sierra"
 
