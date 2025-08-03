fe6c7096dda21b569a3f2ceacc6ad3cd3e594804
Bevan Kay <email@bevankay.me>
2025-08-04 01:05:50 +1000

    kodi: refine livecheck comment
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/k/kodi.rb
+++ Casks/k/kodi.rb
@@ -10,8 +10,9 @@ cask "kodi" do
   desc "Free and open-source media player"
   homepage "https://kodi.tv/"
 
-  # The upstream website is protected by Cloudflare, which prevents
-  # us from fetching the download page. So we check the GitHub repository.
+  # The upstream website is protected by Cloudflare, which prevents us from
+  # fetching the download page. The GitHub releases link to Kodi releases, so
+  # we check the latest GitHub release as a workaround.
   livecheck do
     url "https://github.com/xbmc/xbmc/"
     regex(/^v?(\d+(?:\.\d+)+(?:-\w+))$/i)
