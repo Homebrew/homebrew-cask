54c3288661e2f26a5fc5df912f29b1e7ba6d0d1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 00:18:52 +0000

    font-nata-sans: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-nata-sans.rb
+++ Casks/font/font-n/font-nata-sans.rb
@@ -2,9 +2,10 @@ cask "font-nata-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/natasans/NataSans%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/natasans/NataSans%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Nata Sans"
-  homepage "https://github.com/dnlzqn/nata-sans"
+  homepage "https://fonts.google.com/specimen/Nata+Sans"
 
   font "NataSans[wght].ttf"
 
