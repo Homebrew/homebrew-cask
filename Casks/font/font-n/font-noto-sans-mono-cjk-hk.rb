4d588f8b3b2623732cb79fd5c7628eaedc4cbdc8
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:40 -0700

    font-noto-sans-mono-cjk-hk: remove `no_autobump!`

--- Casks/font/font-n/font-noto-sans-mono-cjk-hk.rb
+++ Casks/font/font-n/font-noto-sans-mono-cjk-hk.rb
@@ -10,8 +10,6 @@ cask "font-noto-sans-mono-cjk-hk" do
     cask "font-noto-sans-cjk"
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "NotoSansMonoCJKhk-Bold.otf"
   font "NotoSansMonoCJKhk-Regular.otf"
 
