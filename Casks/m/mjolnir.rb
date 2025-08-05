36488919931bf01f945e619045a4edc4d841177b
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:13 -0700

    mjolnir: remove `no_autobump!`

--- Casks/m/mjolnir.rb
+++ Casks/m/mjolnir.rb
@@ -8,8 +8,6 @@ cask "mjolnir" do
   desc "Lightweight automation and productivity app"
   homepage "https://mjolnir.rocks/"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :sierra"
 
   app "Mjolnir.app"
