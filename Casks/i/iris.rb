2334dd2c73ecc8fc9664c9b0446f61cc343caffe
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:54 -0700

    iris: remove `no_autobump!`

--- Casks/i/iris.rb
+++ Casks/i/iris.rb
@@ -13,8 +13,6 @@ cask "iris" do
     regex(/Iris[._-]?v?(\d+(?:\.\d+)+)[._-]?OSX\.zip/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Iris.app"
 
   uninstall launchctl: "co.iristech.Iris",
