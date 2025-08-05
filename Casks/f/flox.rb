76c9b2ff135952f658e8aac3547763a367c3fd75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 16:03:33 +0000

    flox 1.6.1

--- Casks/f/flox.rb
+++ Casks/f/flox.rb
@@ -1,9 +1,9 @@
 cask "flox" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.6.0"
-  sha256 arm:   "33edb2c1205b50bbb6a1b213eeca0e1cf9c7571944e5e6cfde09e81131ce9732",
-         intel: "dd253272ed42475db609f275b72afd1a2ead6fd45ffd358222e01cd900b26db6"
+  version "1.6.1"
+  sha256 arm:   "3594d2bc622b5bdc3a6168d1a8ddaea56a5997b1eb20aa70f300892822e61336",
+         intel: "a1fba2930bfc7b6aeeb81aa3a4e1593659a670d0e33451adc9483f35a3d1f323"
 
   url "https://downloads.flox.dev/by-env/stable/osx/flox-#{version}.#{arch}-darwin.pkg"
   name "flox"
