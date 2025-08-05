0b3f30785ed8c996a0636c4f1e2221f74076874e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:56 -0700

    datazenit: remove `no_autobump!`

--- Casks/d/datazenit.rb
+++ Casks/d/datazenit.rb
@@ -7,8 +7,6 @@ cask "datazenit" do
   name "Datazenit"
   homepage "https://datazenit.com/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-10", because: :unmaintained
   disable! date: "2025-07-10", because: :unmaintained
 
