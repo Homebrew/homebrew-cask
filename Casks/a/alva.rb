1797912bb568dd532e8bb7ef55024062db698a3e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:22 -0700

    alva: remove `no_autobump!`

--- Casks/a/alva.rb
+++ Casks/a/alva.rb
@@ -8,8 +8,6 @@ cask "alva" do
   desc "Create living prototypes with code components"
   homepage "https://meetalva.io/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Alva.app"
