f5d8d9b164594e2ee1ee1f11d5ffe1dc2288ef7b
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:48 -0700

    machacha: remove `no_autobump!`

--- Casks/m/machacha.rb
+++ Casks/m/machacha.rb
@@ -7,8 +7,6 @@ cask "machacha" do
   desc "Split archives into smaller parts and join them when requested"
   homepage "http://www.julifos.com/soft/machacha/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-05", because: :unmaintained
   disable! date: "2025-07-05", because: :unmaintained
 
