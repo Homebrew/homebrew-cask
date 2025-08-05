9bb91e9439815b5a9cbbf10df48e9b719d943684
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:00 -0700

    fromscratch: remove `no_autobump!`

--- Casks/f/fromscratch.rb
+++ Casks/f/fromscratch.rb
@@ -8,8 +8,6 @@ cask "fromscratch" do
   desc "Autosaving Scratchpad. A simple but smart note-taking app"
   homepage "https://fromscratch.rocks/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-10", because: :unmaintained
   disable! date: "2025-07-10", because: :unmaintained
 
