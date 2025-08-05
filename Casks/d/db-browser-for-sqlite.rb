a9cbd912407031d6c6130efbd7deb3a5967289f8
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:57 -0700

    db-browser-for-sqlite: remove `no_autobump!`

--- Casks/d/db-browser-for-sqlite.rb
+++ Casks/d/db-browser-for-sqlite.rb
@@ -13,8 +13,6 @@ cask "db-browser-for-sqlite" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "DB Browser for SQLite.app"
