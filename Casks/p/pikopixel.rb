53fe95c922c69b703a9ebabea5143ca10d0273c6
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:22 -0700

    pikopixel: remove `no_autobump!`

--- Casks/p/pikopixel.rb
+++ Casks/p/pikopixel.rb
@@ -13,8 +13,6 @@ cask "pikopixel" do
     regex(/PikoPixel[._-]?(\d+(?:\.\d+)*(?:-b\d+)?)\.dmg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "PikoPixel.app"
 
   zap trash: [
