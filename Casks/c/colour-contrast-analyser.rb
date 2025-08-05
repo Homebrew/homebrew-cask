cbdb05ca4965ba04d5d554f546f155d7f6d86b79
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:40 -0700

    colour-contrast-analyser: remove `no_autobump!`

--- Casks/c/colour-contrast-analyser.rb
+++ Casks/c/colour-contrast-analyser.rb
@@ -13,8 +13,6 @@ cask "colour-contrast-analyser" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :catalina"
 
   app "Colour Contrast Analyser.app"
