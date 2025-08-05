44a11fd48245910f1c4a5164526fce7745b51af1
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:38 -0700

    font-moralerspace-jpdoc: remove `no_autobump!`

--- Casks/font/font-m/font-moralerspace-jpdoc.rb
+++ Casks/font/font-m/font-moralerspace-jpdoc.rb
@@ -6,8 +6,6 @@ cask "font-moralerspace-jpdoc" do
   name "Moralerspace JPDOC"
   homepage "https://github.com/yuru7/moralerspace"
 
-  no_autobump! because: :requires_manual_review
-
   font "MoralerspaceJPDOC_v#{version}/MoralerspaceArgonJPDOC-Bold.ttf"
   font "MoralerspaceJPDOC_v#{version}/MoralerspaceArgonJPDOC-BoldItalic.ttf"
   font "MoralerspaceJPDOC_v#{version}/MoralerspaceArgonJPDOC-Italic.ttf"
