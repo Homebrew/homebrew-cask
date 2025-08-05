7da726a811a1ff845c016d1fd7af181a83295dd9
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:22 -0700

    koala: remove `no_autobump!`

--- Casks/k/koala.rb
+++ Casks/k/koala.rb
@@ -7,8 +7,6 @@ cask "koala" do
   name "Koala"
   homepage "http://koala-app.com/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-17", because: :unmaintained
   disable! date: "2025-07-17", because: :unmaintained
 
