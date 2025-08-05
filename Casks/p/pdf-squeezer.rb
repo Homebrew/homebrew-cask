723cb81a0ba392cf528cd2e6aad0f36c9521e2a9
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:15 -0700

    pdf-squeezer: remove `no_autobump!`

--- Casks/p/pdf-squeezer.rb
+++ Casks/p/pdf-squeezer.rb
@@ -12,8 +12,6 @@ cask "pdf-squeezer" do
     strategy :sparkle, &:short_version
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "PDF Squeezer.app"
