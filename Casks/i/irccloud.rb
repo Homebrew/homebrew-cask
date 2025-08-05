4006b97327d08ea49430a4d3f170932f1b1c0462
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:54 -0700

    irccloud: remove `no_autobump!`

--- Casks/i/irccloud.rb
+++ Casks/i/irccloud.rb
@@ -7,8 +7,6 @@ cask "irccloud" do
   desc "IRC client"
   homepage "https://github.com/irccloud/irccloud-desktop"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "IRCCloud.app"
