6ffd076d79ad3d4e8c92e4c4b31af03923d39d81
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:14 -0700

    pd-l2ork: remove `no_autobump!`

--- Casks/p/pd-l2ork.rb
+++ Casks/p/pd-l2ork.rb
@@ -14,8 +14,6 @@ cask "pd-l2ork" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Purr-Data.app"
   binary "#{appdir}/Purr-Data.app/Contents/Resources/app.nw/bin/pd-l2ork"
 
