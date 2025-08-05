12bd0de8da347494ce2791cea7a8bd05785a4842
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:11 -0700

    kakapo: remove `no_autobump!`

--- Casks/k/kakapo.rb
+++ Casks/k/kakapo.rb
@@ -7,8 +7,6 @@ cask "kakapo" do
   desc "Open-source ambient sound mixer"
   homepage "https://github.com/bluedaniel/Kakapo-app"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-17", because: :unmaintained
   disable! date: "2025-07-17", because: :unmaintained
 
