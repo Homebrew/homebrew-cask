5b3d4dd7fe2a50e3a921ac5fcec691fd58aa8f29
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:18 -0700

    persepolis-download-manager: remove `no_autobump!`

--- Casks/p/persepolis-download-manager.rb
+++ Casks/p/persepolis-download-manager.rb
@@ -16,8 +16,6 @@ cask "persepolis-download-manager" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Persepolis Download Manager.app"
 
   zap trash: [
