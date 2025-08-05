5b4b458cb60ab2e5f8d2697dc558fd61fc657ebf
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:57 -0700

    font-victor-mono: remove `no_autobump!`

--- Casks/font/font-v/font-victor-mono.rb
+++ Casks/font/font-v/font-victor-mono.rb
@@ -10,8 +10,6 @@ cask "font-victor-mono" do
     url "https://github.com/rubjo/victor-mono/releases"
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "OTF/VictorMono-Thin.otf"
   font "OTF/VictorMono-ExtraLight.otf"
   font "OTF/VictorMono-Light.otf"
