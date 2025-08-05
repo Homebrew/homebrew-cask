49d2225c3f6f9e356dcd5d776fa5dbb34f46676a
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:40 -0700

    logmein-client: remove `no_autobump!`

--- Casks/l/logmein-client.rb
+++ Casks/l/logmein-client.rb
@@ -12,8 +12,6 @@ cask "logmein-client" do
     regex(/Version:\s+v?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "LogMeIn Client.app"
