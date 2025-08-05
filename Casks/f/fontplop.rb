f1de69974ce72c70f0bb34f235a8ccd721c6a2c5
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:17:55 -0700

    fontplop: remove `no_autobump!`

--- Casks/f/fontplop.rb
+++ Casks/f/fontplop.rb
@@ -7,8 +7,6 @@ cask "fontplop" do
   desc "Open Source Webfont Converter"
   homepage "https://github.com/matthewgonzalez/fontplop"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-10", because: :unmaintained
   disable! date: "2025-07-10", because: :unmaintained
 
