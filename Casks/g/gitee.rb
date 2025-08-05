1aa4cb4f7612b6f78898b4d2b2b3d8bdeb5f4810
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:10 -0700

    gitee: remove `no_autobump!`

--- Casks/g/gitee.rb
+++ Casks/g/gitee.rb
@@ -7,8 +7,6 @@ cask "gitee" do
   desc "Status bar application for GitHub"
   homepage "https://github.com/Nightonke/Gitee"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-11", because: :unmaintained
   disable! date: "2025-07-11", because: :unmaintained
 
