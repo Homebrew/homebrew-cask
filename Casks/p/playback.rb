5ed90fb2de7b0b0ac4072e1ad81a2e05ec045457
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:25 -0700

    playback: remove `no_autobump!`

--- Casks/p/playback.rb
+++ Casks/p/playback.rb
@@ -8,8 +8,6 @@ cask "playback" do
   desc "Video player"
   homepage "https://mafintosh.github.io/playback/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-28", because: :unmaintained
   disable! date: "2025-07-28", because: :unmaintained
 
