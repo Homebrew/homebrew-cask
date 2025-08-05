fd484baa008ec906aa622156a337531b5b076fbb
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:53 -0700

    mail-assistant: remove `no_autobump!`

--- Casks/m/mail-assistant.rb
+++ Casks/m/mail-assistant.rb
@@ -13,8 +13,6 @@ cask "mail-assistant" do
     regex(%r{Download\s+Mail\s+Assistant</strong>.*</a>.*\(v?(\d+(?:\.\d+)+)[, <]}im)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "Mail Assistant.app"
