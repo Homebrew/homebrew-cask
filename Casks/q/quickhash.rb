23efdb55a7d54aa5f8a7ddba2118aeebb73f3ed4
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:51 -0700

    quickhash: remove `no_autobump!`

--- Casks/q/quickhash.rb
+++ Casks/q/quickhash.rb
@@ -19,8 +19,6 @@ cask "quickhash" do
     end
   end
 
-  no_autobump! because: :requires_manual_review
-
   container nested: "Quickhash-GUI_#{version.csv.third}#{version.csv.fourth}#{version.csv.fifth}.dmg"
 
   app "Quickhash-GUI.app"
