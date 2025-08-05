0663ead2dde48ba9be5acb4ad060a1dad1bb9997
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:13 -0700

    gltfquicklook: remove `no_autobump!`

--- Casks/g/gltfquicklook.rb
+++ Casks/g/gltfquicklook.rb
@@ -7,8 +7,6 @@ cask "gltfquicklook" do
   desc "Quick Look plugin for glTF files"
   homepage "https://github.com/magicien/GLTFQuickLook"
 
-  no_autobump! because: :requires_manual_review
-
   qlplugin "GLTFQuickLook.qlgenerator"
 
   # No zap stanza required
