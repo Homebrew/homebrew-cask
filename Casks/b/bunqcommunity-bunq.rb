654dc60de571aebb57cfd6aa8d0d4f9414d67055
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:11 -0700

    bunqcommunity-bunq: remove `no_autobump!`

--- Casks/b/bunqcommunity-bunq.rb
+++ Casks/b/bunqcommunity-bunq.rb
@@ -8,8 +8,6 @@ cask "bunqcommunity-bunq" do
   desc "Unofficial desktop application for the bunq API"
   homepage "https://bunqdesk.top/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "bunqDesktop.app"
