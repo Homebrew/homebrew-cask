d148068f354a5a0e70b2b6b95e84c7b24086ac2f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:17:00 -0700

    dejalu: remove `no_autobump!`

--- Casks/d/dejalu.rb
+++ Casks/d/dejalu.rb
@@ -6,8 +6,6 @@ cask "dejalu" do
   name "DejaLu"
   homepage "https://dejalu.me/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-10", because: :unmaintained
   disable! date: "2025-07-10", because: :unmaintained
 
