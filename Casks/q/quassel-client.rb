0dfd67e7d70aca206585788837aae9e048c112e6
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:50 -0700

    quassel-client: remove `no_autobump!`

--- Casks/q/quassel-client.rb
+++ Casks/q/quassel-client.rb
@@ -13,8 +13,6 @@ cask "quassel-client" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "Quassel Client.app"
