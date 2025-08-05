f561cad57b95848a96fbb35bdc134a3b3cc75456
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:15 -0700

    kextviewr: remove `no_autobump!`

--- Casks/k/kextviewr.rb
+++ Casks/k/kextviewr.rb
@@ -8,8 +8,6 @@ cask "kextviewr" do
   desc "Display all currently loaded kexts"
   homepage "https://objective-see.org/products/kextviewr.html"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "KextViewr.app"
