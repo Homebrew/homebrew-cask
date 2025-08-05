89c5a82cf29471fe680b387bf18e7c378a974e23
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:25 -0700

    font-ibm-plex-sans-kr: remove `no_autobump!`

--- Casks/font/font-i/font-ibm-plex-sans-kr.rb
+++ Casks/font/font-i/font-ibm-plex-sans-kr.rb
@@ -11,8 +11,6 @@ cask "font-ibm-plex-sans-kr" do
     regex(%r{^@ibm/plex-sans-kr@?(\d+(?:\.\d+)+)$}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Bold.otf"
   font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-ExtraLight.otf"
   font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Light.otf"
