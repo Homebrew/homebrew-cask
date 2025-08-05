58dab54402db58d3ce103edc2ed2a48a1c9bf4bd
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:26 -0700

    font-ibm-plex-sans-thai: remove `no_autobump!`

--- Casks/font/font-i/font-ibm-plex-sans-thai.rb
+++ Casks/font/font-i/font-ibm-plex-sans-thai.rb
@@ -11,8 +11,6 @@ cask "font-ibm-plex-sans-thai" do
     regex(%r{^@ibm/plex-sans-thai@?(\d+(?:\.\d+)+)$}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "ibm-plex-sans-thai/fonts/complete/otf/IBMPlexSansThai-Bold.otf"
   font "ibm-plex-sans-thai/fonts/complete/otf/IBMPlexSansThai-ExtraLight.otf"
   font "ibm-plex-sans-thai/fonts/complete/otf/IBMPlexSansThai-Light.otf"
