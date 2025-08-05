c0764147be7bab937e0d761a35b294f4518ac45f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:52 -0700

    quicklook-pfm: remove `no_autobump!`

--- Casks/q/quicklook-pfm.rb
+++ Casks/q/quicklook-pfm.rb
@@ -12,8 +12,6 @@ cask "quicklook-pfm" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   qlplugin "Quicklook-PFM.qlgenerator"
 
   # No zap stanza required
