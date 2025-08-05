92396828bcc600e672da2f063ab853b7dfd57da2
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:38 -0700

    prowritingaid: remove `no_autobump!`

--- Casks/p/prowritingaid.rb
+++ Casks/p/prowritingaid.rb
@@ -12,8 +12,6 @@ cask "prowritingaid" do
     regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "ProWritingAid.app"
