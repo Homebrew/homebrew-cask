822f2ae4837c9cd33599aa8174f12b7981b84e5c
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:49 -0700

    macmorpheus: remove `no_autobump!`

--- Casks/m/macmorpheus.rb
+++ Casks/m/macmorpheus.rb
@@ -7,8 +7,6 @@ cask "macmorpheus" do
   desc "3D 180/360 video player using PSVR"
   homepage "https://github.com/emoRaivis/MacMorpheus"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
