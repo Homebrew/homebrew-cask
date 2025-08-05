8bdaa2c0a991537084454a0537d80c9a63417c8d
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:53 -0700

    iphoto-library-manager: remove `no_autobump!`

--- Casks/i/iphoto-library-manager.rb
+++ Casks/i/iphoto-library-manager.rb
@@ -7,8 +7,6 @@ cask "iphoto-library-manager" do
   desc "App for organising photos among multiple iPhoto libraries"
   homepage "https://www.fatcatsoftware.com/iplm/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-17", because: :discontinued
   disable! date: "2025-07-17", because: :discontinued
 
