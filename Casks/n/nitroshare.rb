e9413b794071d6bd57d30fa41a667b4e0e8cf083
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:39 -0700

    nitroshare: remove `no_autobump!`

--- Casks/n/nitroshare.rb
+++ Casks/n/nitroshare.rb
@@ -7,8 +7,6 @@ cask "nitroshare" do
   desc "Network file transfer application"
   homepage "https://launchpad.net/nitroshare"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
