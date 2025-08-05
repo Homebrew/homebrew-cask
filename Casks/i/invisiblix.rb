ada8ff6169920cc6587c7b9e482cbb8f31c8374d
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:51 -0700

    invisiblix: remove `no_autobump!`

--- Casks/i/invisiblix.rb
+++ Casks/i/invisiblix.rb
@@ -8,8 +8,6 @@ cask "invisiblix" do
   desc "Allows viewing and manipulation of hidden files in Finder"
   homepage "https://invisiblix.read-write.fr/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-17", because: :unmaintained
   disable! date: "2025-07-17", because: :unmaintained
 
