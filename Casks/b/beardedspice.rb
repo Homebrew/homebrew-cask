2c150af90c2aa146a795807db0a4d01309488e9a
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:52 -0700

    beardedspice: remove `no_autobump!`

--- Casks/b/beardedspice.rb
+++ Casks/b/beardedspice.rb
@@ -7,8 +7,6 @@ cask "beardedspice" do
   desc "Control web-based media players with media keys"
   homepage "https://github.com/beardedspice/beardedspice/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-09", because: :unmaintained
   disable! date: "2025-07-09", because: :unmaintained
 
