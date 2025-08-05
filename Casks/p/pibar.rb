db49ef2fca50935dc6c309ed4df42f20dda6af11
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:21 -0700

    pibar: remove `no_autobump!`

--- Casks/p/pibar.rb
+++ Casks/p/pibar.rb
@@ -8,8 +8,6 @@ cask "pibar" do
   desc "Pi-hole(s) management in the menu bar"
   homepage "https://github.com/amiantos/pibar"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :sierra"
 
   app "PiBar.app"
