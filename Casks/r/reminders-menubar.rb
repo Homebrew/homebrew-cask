b38aee7c75f627dca152bd9d0aa8eebc8b7d03c0
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:48:51 -0700

    reminders-menubar: remove `no_autobump!`

--- Casks/r/reminders-menubar.rb
+++ Casks/r/reminders-menubar.rb
@@ -7,7 +7,7 @@ cask "reminders-menubar" do
   desc "Simple menu bar app to view and interact with reminders"
   homepage "https://github.com/DamascenoRafael/reminders-menubar"
 
-  no_autobump! because: :requires_manual_review
+
 
   depends_on macos: ">= :big_sur"
 
