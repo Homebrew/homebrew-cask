2688a414c99b4ad081cfc97c51e6b364df039e7c
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:03 -0700

    otx: remove `no_autobump!`

--- Casks/o/otx.rb
+++ Casks/o/otx.rb
@@ -7,8 +7,6 @@ cask "otx" do
   desc "Mach-O disassembler"
   homepage "https://github.com/x43x61x69/otx"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
