80f8200ee63fdb5f0a24f43933571601a03ac707
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:17:47 -0700

    findergo: remove `no_autobump!`

--- Casks/f/findergo.rb
+++ Casks/f/findergo.rb
@@ -7,8 +7,6 @@ cask "findergo" do
   desc "Open terminal quickly from Finder"
   homepage "https://github.com/onmyway133/FinderGo"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2025-02-22", because: :unmaintained
 
   depends_on macos: ">= :sierra"
