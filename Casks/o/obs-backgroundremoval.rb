e5cb216f7352d0748fde4b1e48fcfce5eaa3e2db
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:48 -0700

    obs-backgroundremoval: remove `no_autobump!`

--- Casks/o/obs-backgroundremoval.rb
+++ Casks/o/obs-backgroundremoval.rb
@@ -13,8 +13,6 @@ cask "obs-backgroundremoval" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   auto_updates true
 
   pkg "obs-backgroundremoval-#{version}-macos-universal.pkg"
