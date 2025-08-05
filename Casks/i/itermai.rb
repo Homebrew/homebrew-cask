d049692d73210edbfd6fdb2bc9b7f9787dfa689f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:57 -0700

    itermai: remove `no_autobump!`

--- Casks/i/itermai.rb
+++ Casks/i/itermai.rb
@@ -12,8 +12,6 @@ cask "itermai" do
     regex(%r{href=.*?/iTermAI[._-](\d+(?:\.\d+)*)\.zip}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :catalina"
 
   app "iTermAI.app"
