bcc2b3f9ba244ac75f68046babe99c92775aa1e0
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:36 -0700

    font-miracode: remove `no_autobump!`

--- Casks/font/font-m/font-miracode.rb
+++ Casks/font/font-m/font-miracode.rb
@@ -6,8 +6,6 @@ cask "font-miracode" do
   name "Miracode"
   homepage "https://github.com/IdreesInc/Miracode"
 
-  no_autobump! because: :requires_manual_review
-
   font "Miracode.ttf"
 
   # No zap stanza required
