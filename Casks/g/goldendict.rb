cf49e17270ba5eab89e351032b8c87dde90657ca
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:14 -0700

    goldendict: remove `no_autobump!`

--- Casks/g/goldendict.rb
+++ Casks/g/goldendict.rb
@@ -8,8 +8,6 @@ cask "goldendict" do
   desc "Feature-rich dictionary lookup program"
   homepage "http://goldendict.org/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   depends_on macos: ">= :sierra"
