93176ec638dc8bff4a3fe3973f56a077c49e7456
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:14 -0700

    mkvtools: remove `no_autobump!`

--- Casks/m/mkvtools.rb
+++ Casks/m/mkvtools.rb
@@ -12,8 +12,6 @@ cask "mkvtools" do
     regex(%r{href=.*?/mkvtools(\d+(?:\.\d+)*)\.zip}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "mkvtools#{version}/MKVtools.app"
 
   zap trash: [
