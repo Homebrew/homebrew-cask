6c19b21fa41039a7166c573fd0e9ec620431c908
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:40 -0700

    nocturnal: remove `no_autobump!`

--- Casks/n/nocturnal.rb
+++ Casks/n/nocturnal.rb
@@ -7,8 +7,6 @@ cask "nocturnal" do
   desc "Simple app to toggle dark mode with one click"
   homepage "https://github.com/HarshilShah/Nocturnal"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
