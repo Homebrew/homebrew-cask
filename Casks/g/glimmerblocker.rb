8628026e41fa2121977afa8e81cdf17e14a2235d
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:13 -0700

    glimmerblocker: remove `no_autobump!`

--- Casks/g/glimmerblocker.rb
+++ Casks/g/glimmerblocker.rb
@@ -7,8 +7,6 @@ cask "glimmerblocker" do
   desc "HTTP based ad blocker"
   homepage "https://glimmerblocker.org/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   pkg "GlimmerBlocker.pkg"
