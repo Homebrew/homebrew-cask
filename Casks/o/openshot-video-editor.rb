e3c55c9d674c7aa3e1466218344b67244cd4a96e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:57 -0700

    openshot-video-editor: remove `no_autobump!`

--- Casks/o/openshot-video-editor.rb
+++ Casks/o/openshot-video-editor.rb
@@ -13,8 +13,6 @@ cask "openshot-video-editor" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   conflicts_with cask: "openshot-video-editor@daily"
   depends_on macos: ">= :catalina"
 
