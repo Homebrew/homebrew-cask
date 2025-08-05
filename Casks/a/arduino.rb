61a2d9b48bbcf2e04242cd77cd768066c5c243e5
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:34 -0700

    arduino: remove `no_autobump!`

--- Casks/a/arduino.rb
+++ Casks/a/arduino.rb
@@ -7,8 +7,6 @@ cask "arduino" do
   desc "Electronics prototyping platform"
   homepage "https://www.arduino.cc/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Arduino.app"
