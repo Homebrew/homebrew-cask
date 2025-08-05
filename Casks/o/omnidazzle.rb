cf94346778aa3851b197400eec87d02f513aed5d
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:51 -0700

    omnidazzle: remove `no_autobump!`

--- Casks/o/omnidazzle.rb
+++ Casks/o/omnidazzle.rb
@@ -7,8 +7,6 @@ cask "omnidazzle" do
   desc "Set of plug-ins to highlight areas of your screen and your mouse pointer"
   homepage "https://support.omnigroup.com/omnidazzle-troubleshooting/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "OmniDazzle.app"
