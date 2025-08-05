dcfc21497ebe9c1eab24091d29ef0d606ec63442
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:06 -0700

    boost-note: remove `no_autobump!`

--- Casks/b/boost-note.rb
+++ Casks/b/boost-note.rb
@@ -13,8 +13,6 @@ cask "boost-note" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Boost Note.app"
 
   uninstall signal: ["TERM", "com.boostio.boostnote"]
