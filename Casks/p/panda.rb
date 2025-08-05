65068464dfbdbda67d1be90ab63b2ec85e10bea8
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:09 -0700

    panda: remove `no_autobump!`

--- Casks/p/panda.rb
+++ Casks/p/panda.rb
@@ -7,8 +7,6 @@ cask "panda" do
   desc "Utility to switch from light to dark mode"
   homepage "https://github.com/pablosproject/Panda-Mac-app"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-28", because: :unmaintained
   disable! date: "2025-07-28", because: :unmaintained
 
