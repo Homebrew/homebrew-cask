dd817e6200ebe7e28c6058b17271edaa242c11e9
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:57 -0700

    dbeaver-enterprise: remove `no_autobump!`

--- Casks/d/dbeaver-enterprise.rb
+++ Casks/d/dbeaver-enterprise.rb
@@ -17,8 +17,6 @@ cask "dbeaver-enterprise" do
     end
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "DBeaverEE.app"
 
   uninstall signal: ["TERM", "com.dbeaver.product.enterprise"]
