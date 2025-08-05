ad095e94af7be28d72e018444485dd92af0eb5c4
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:44 -0700

    iglance: remove `no_autobump!`

--- Casks/i/iglance.rb
+++ Casks/i/iglance.rb
@@ -7,8 +7,6 @@ cask "iglance" do
   desc "System monitor for the status bar"
   homepage "https://github.com/iglance/iGlance"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-17", because: :unmaintained
   disable! date: "2025-07-17", because: :unmaintained
 
