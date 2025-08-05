37e7f6c404ad4fcbf418a45ca34a5ae24d726825
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:48 -0700

    criptext: remove `no_autobump!`

--- Casks/c/criptext.rb
+++ Casks/c/criptext.rb
@@ -7,8 +7,6 @@ cask "criptext" do
   desc "Email service that's built around privacy"
   homepage "https://criptext.com/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
