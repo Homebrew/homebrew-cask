0402580399db103d7d0f7090fff0ecc30a9be38f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:17:48 -0700

    fireworks: remove `no_autobump!`

--- Casks/f/fireworks.rb
+++ Casks/f/fireworks.rb
@@ -12,8 +12,6 @@ cask "fireworks" do
     regex(/Download\s+v?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "Fireworks.app"
