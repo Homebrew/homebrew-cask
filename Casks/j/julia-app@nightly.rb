d945ca99bd5a888bec28f25887ab19a75e73bb10
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:09 -0700

    julia-app@nightly: remove `no_autobump!`

--- Casks/j/julia-app@nightly.rb
+++ Casks/j/julia-app@nightly.rb
@@ -14,8 +14,6 @@ cask "julia-app@nightly" do
     regex(/v?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Julia-#{version.major_minor}.app"
   binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia", target: "julia-nightly"
 
