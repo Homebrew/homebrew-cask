0b863e57a66e56913f6d124f533972dcf21d704f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:45 -0700

    nullpomino: remove `no_autobump!`

--- Casks/n/nullpomino.rb
+++ Casks/n/nullpomino.rb
@@ -7,8 +7,6 @@ cask "nullpomino" do
   desc "Action puzzle game"
   homepage "https://github.com/nullpomino/nullpomino"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
