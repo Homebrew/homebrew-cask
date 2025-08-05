700c61ae3c0c0c5cf3c3ae799344191671363079
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:18 -0700

    perimeter81: remove `no_autobump!`

--- Casks/p/perimeter81.rb
+++ Casks/p/perimeter81.rb
@@ -13,8 +13,6 @@ cask "perimeter81" do
     regex(/href=.*?Harmony[._-]SASE[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   pkg "Harmony_SASE_#{version}.pkg"
