e34090a50bac81bc437b919f47c6a462010841a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 22:01:45 +0000

    microsoft-edge@canary 140.0.3480.0

--- Casks/m/microsoft-edge@canary.rb
+++ Casks/m/microsoft-edge@canary.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge@canary" do
-  version "140.0.3479.0,f929dd0e-7101-484e-9feb-8425fc44914b"
-  sha256 "af53ab066365e9488dd8c0016c746bc5be6c09295e28adca88fc6f43181820c9"
+  version "140.0.3480.0,3fad82a2-9bbe-492c-84b9-a5cfc9fc6f43"
+  sha256 "7b39bf41a5ca01a0763ef4bb2c8d1f4f360e1c994f8d169a591dd9aec4b3db87"
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdgeCanary-#{version.csv.first}.dmg"
   name "Microsoft Edge Canary"
