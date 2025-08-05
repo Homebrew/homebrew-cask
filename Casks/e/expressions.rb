71d7e2f382f47a0ea562ad19b37f904f9084df48
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:17:38 -0700

    expressions: remove `no_autobump!`

--- Casks/e/expressions.rb
+++ Casks/e/expressions.rb
@@ -12,8 +12,6 @@ cask "expressions" do
     regex(%r{href=.*?/Expressions-(\d+(?:\.\d+)*)\.zip}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :el_capitan"
 
   app "Expressions.app"
