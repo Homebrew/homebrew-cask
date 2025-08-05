3d5856068f7c1a8a3cc80fca481b58fea722a191
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:38 -0700

    lockrattler: remove `no_autobump!`

--- Casks/l/lockrattler.rb
+++ Casks/l/lockrattler.rb
@@ -23,8 +23,6 @@ cask "lockrattler" do
     end
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "lockrattler#{version.csv.first.major}#{version.csv.first.minor}/LockRattler.app"
