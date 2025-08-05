be4a559197468496f2345723b3e96e083ae07b8e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:05 -0700

    geburtstagschecker: remove `no_autobump!`

--- Casks/g/geburtstagschecker.rb
+++ Casks/g/geburtstagschecker.rb
@@ -11,8 +11,6 @@ cask "geburtstagschecker" do
     strategy :sparkle
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "GeburtstagsChecker #{version.csv.first} (#{version.csv.second})/GeburtstagsChecker.app"
 
   zap trash: [
