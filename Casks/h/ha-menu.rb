dbbbf0ebd6400ddbe2ee36ce688d321aea9ee19e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:26 -0700

    ha-menu: remove `no_autobump!`

--- Casks/h/ha-menu.rb
+++ Casks/h/ha-menu.rb
@@ -7,8 +7,6 @@ cask "ha-menu" do
   desc "Menu Bar app to perform common Home Assistant functions"
   homepage "https://github.com/codechimp-org/ha-menu"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "HA Menu.app"
