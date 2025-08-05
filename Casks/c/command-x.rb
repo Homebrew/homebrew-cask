9d1e25e09310e4aeac3acde02474c01a2f520bbb
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:41 -0700

    command-x: remove `no_autobump!`

--- Casks/c/command-x.rb
+++ Casks/c/command-x.rb
@@ -8,8 +8,6 @@ cask "command-x" do
   desc "Cut and paste files in Finder"
   homepage "https://sindresorhus.com/command-x"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-09", because: :moved_to_mas
   disable! date: "2025-07-09", because: :moved_to_mas
 
