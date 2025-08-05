bc81c5fbd6d7d7f02e0c5f65252439f45e84acd5
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:48:44 -0700

    radiant-player: remove `no_autobump!`

--- Casks/r/radiant-player.rb
+++ Casks/r/radiant-player.rb
@@ -8,7 +8,7 @@ cask "radiant-player" do
   desc "App wrapper for Google Play Music"
   homepage "https://radiant-player.github.io/radiant-player-mac/"
 
-  no_autobump! because: :requires_manual_review
+
 
   disable! date: "2024-12-16", because: :discontinued
 
