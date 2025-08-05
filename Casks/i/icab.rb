ba9ae70854b91044f0f6c35c019f9d3e2e6a374f
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:40 -0700

    icab: remove `no_autobump!`

--- Casks/i/icab.rb
+++ Casks/i/icab.rb
@@ -13,8 +13,6 @@ cask "icab" do
     regex(/iCab\sv?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "iCab #{version}/iCab.app"
