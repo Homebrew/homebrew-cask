d24b5a6763bf79970d09f724a83f0128156f9a8c
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:28 -0700

    my-budget: remove `no_autobump!`

--- Casks/m/my-budget.rb
+++ Casks/m/my-budget.rb
@@ -8,8 +8,6 @@ cask "my-budget" do
   desc "Budgeting tool"
   homepage "https://rezach.github.io/my-budget/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-27", because: :unmaintained
   disable! date: "2025-07-27", because: :unmaintained
 
