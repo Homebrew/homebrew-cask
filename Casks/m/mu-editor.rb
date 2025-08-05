eb41ac8983e28a798a62816f14121b208a9252ff
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:21:24 -0700

    mu-editor: remove `no_autobump!`

--- Casks/m/mu-editor.rb
+++ Casks/m/mu-editor.rb
@@ -8,8 +8,6 @@ cask "mu-editor" do
   desc "Small, simple editor for beginner Python programmers"
   homepage "https://codewith.mu/"
 
-  no_autobump! because: :requires_manual_review
-
   app "Mu Editor.app"
 
   zap trash: [
