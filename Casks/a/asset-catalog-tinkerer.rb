5c6c36f2912bf98f0ceeb979c86c97f72f9ef413
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:36 -0700

    asset-catalog-tinkerer: remove `no_autobump!`

--- Casks/a/asset-catalog-tinkerer.rb
+++ Casks/a/asset-catalog-tinkerer.rb
@@ -12,8 +12,6 @@ cask "asset-catalog-tinkerer" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "Asset Catalog Tinkerer.app"
