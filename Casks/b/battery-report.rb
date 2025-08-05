f7e46d51a9aa69e989f2c585b93283a36a2ab00a
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:49 -0700

    battery-report: remove `no_autobump!`

--- Casks/b/battery-report.rb
+++ Casks/b/battery-report.rb
@@ -7,8 +7,6 @@ cask "battery-report" do
   desc "Creates technical summaries of power supplies"
   homepage "https://www.dssw.co.uk/batteryreport/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Battery Report.app"
