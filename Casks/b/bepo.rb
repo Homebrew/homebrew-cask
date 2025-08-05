0171c60369877fa26e0f7488ec65a14729a501c8
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:53 -0700

    bepo: remove `no_autobump!`

--- Casks/b/bepo.rb
+++ Casks/b/bepo.rb
@@ -12,8 +12,6 @@ cask "bepo" do
     regex(/href=.*?fr[._-]dvorak[._-]bepo[._-]macosx[._-]v?(\d+(?:\.\d+)+(?:rc\d+)?)\.dmg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   keyboard_layout "bepo #{version.gsub(/rc\d+/i, "")}.bundle"
 
   # No zap stanza required
