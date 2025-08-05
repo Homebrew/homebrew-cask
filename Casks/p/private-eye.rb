d870e59b42662cbe61506a273492229f40dc7a23
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:34 -0700

    private-eye: remove `no_autobump!`

--- Casks/p/private-eye.rb
+++ Casks/p/private-eye.rb
@@ -7,8 +7,6 @@ cask "private-eye" do
   desc "Network monitor"
   homepage "https://radiosilenceapp.com/private-eye"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-06", because: :unmaintained
   disable! date: "2025-07-06", because: :unmaintained
 
