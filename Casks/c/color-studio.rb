3d250b8440727d9469a0143c590c8dee853aa85b
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:16:39 -0700

    color-studio: remove `no_autobump!`

--- Casks/c/color-studio.rb
+++ Casks/c/color-studio.rb
@@ -7,8 +7,6 @@ cask "color-studio" do
   desc "Coherent colour scheme creator"
   homepage "https://github.com/bernaferrari/color-studio"
 
-  no_autobump! because: :requires_manual_review
-
   app "Color Studio/Color Studio.app"
 
   zap trash: [
