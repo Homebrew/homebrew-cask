a96ff98810b210c9efac367a80d6951ca6931132
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:23 -0700

    pineapple: remove `no_autobump!`

--- Casks/p/pineapple.rb
+++ Casks/p/pineapple.rb
@@ -7,8 +7,6 @@ cask "pineapple" do
   name "Pineapple"
   homepage "https://nwhitehead.github.io/pineapple/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-28", because: :unmaintained
   disable! date: "2025-07-28", because: :unmaintained
 
