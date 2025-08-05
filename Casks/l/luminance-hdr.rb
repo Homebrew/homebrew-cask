7952d43de3209f768a81f6fc6f5ed4818f582421
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:42 -0700

    luminance-hdr: remove `no_autobump!`

--- Casks/l/luminance-hdr.rb
+++ Casks/l/luminance-hdr.rb
@@ -13,8 +13,6 @@ cask "luminance-hdr" do
     regex(%r{url=.*?/Luminance[_-]?HDR[._-]v?(\d+(?:\.\d+)+)[^"' ]*?\.dmg}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :sierra"
 
   app "Luminance HDR #{version.major_minor_patch}.app"
