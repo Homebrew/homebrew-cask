2b08c559dd749dda89884fefb3414b2866de1321
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:05 -0700

    jgrasp: remove `no_autobump!`

--- Casks/j/jgrasp.rb
+++ Casks/j/jgrasp.rb
@@ -12,8 +12,6 @@ cask "jgrasp" do
     regex(/jGRASP\s*(\d+(?:\.\d+)*_\d+)\s(?!Beta)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   pkg "jgrasp#{version.no_dots}.pkg"
 
   uninstall pkgutil: "jgrasp"
