415a3812f62202f9cff28803c179edbb1c035022
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:39 -0700

    logicsniffer: remove `no_autobump!`

--- Casks/l/logicsniffer.rb
+++ Casks/l/logicsniffer.rb
@@ -7,8 +7,6 @@ cask "logicsniffer" do
   desc "Software client for the Open Bench Logic Sniffer logic analyser hardware"
   homepage "https://lxtreme.nl/projects/ols/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-09-08", because: :unmaintained
 
   app "LogicSniffer.app"
