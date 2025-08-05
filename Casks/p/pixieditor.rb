05e256af69ae1cf6106015049dda580916cc8ddd
Martin Müller <mail@mueller-martin.net>
2025-08-06 00:28:59 +0200

    pixieditor 2.0.1.7 (new cask)

---
+++ Casks/p/pixieditor.rb
@@ -0,0 +1,24 @@
+cask "pixieditor" do
+  version "2.0.1.7"
+  sha256 "e44c37043aadac1d173d711ad7bc0d04e91f57e87a90cb4844404f5f7b0bae6d"
+
+  url "https://github.com/PixiEditor/PixiEditor/releases/download/#{version}/PixiEditor-#{version}-universal-macos.dmg",
+      verified: "github.com/PixiEditor/PixiEditor/"
+  name "PixiEditor"
+  desc "Open Source Universal 2D Graphics Editor"
+  homepage "https://pixieditor.net/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "PixiEditor.app"
+
+  zap trash: [
+    "~/Library/Application Support/PixiEditor",
+    "~/Library/Preferences/com.pixieditor.plist",
+  ]
+end
