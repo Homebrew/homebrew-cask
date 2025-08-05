24ad6561e579c1315c7791a2fe6c3188e645cf87
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:40 -0700

    font-noto-sans-cjk-tc: remove `no_autobump!`

--- Casks/font/font-n/font-noto-sans-cjk-tc.rb
+++ Casks/font/font-n/font-noto-sans-cjk-tc.rb
@@ -10,8 +10,6 @@ cask "font-noto-sans-cjk-tc" do
     cask "font-noto-sans-cjk"
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "NotoSansCJKtc-Black.otf"
   font "NotoSansCJKtc-Bold.otf"
   font "NotoSansCJKtc-DemiLight.otf"
