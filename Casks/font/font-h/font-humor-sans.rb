11da27d381d21942e0809c4ff1bf9f893392a149
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:24 -0700

    font-humor-sans: remove `no_autobump!`

--- Casks/font/font-h/font-humor-sans.rb
+++ Casks/font/font-h/font-humor-sans.rb
@@ -12,8 +12,6 @@ cask "font-humor-sans" do
     regex(/href=.*Humor[._-]Sans[._-]v?(\d+(?:\.\d+)+)\.ttf/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "Humor-Sans-#{version}.ttf"
 
   # No zap stanza required
