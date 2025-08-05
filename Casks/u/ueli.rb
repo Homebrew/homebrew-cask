6ffa34e56ca922cfc27e4d0642216cac33695797
Patrick Linnane <patrick@linnane.io>
2025-08-05 13:48:27 -0700

    ueli 9.25.0 & deprecate
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/u/ueli.rb
+++ Casks/u/ueli.rb
@@ -1,9 +1,9 @@
 cask "ueli" do
   arch arm: "-arm64"
 
-  version "9.24.0"
-  sha256 arm:   "de33fc534be8b59d7b5a37293df21b58ec8816ee29dfcab3a0ebf4c1a0d4414f",
-         intel: "f76c44b7bdf82b3adde142e49ff641c59959ffe85d838bc1134172c945afdea2"
+  version "9.25.0"
+  sha256 arm:   "449dcba9366cf2f8b79f37f687c3525f6e613a688e3d5e5dbf2dad095a9b17ca",
+         intel: "7f2ddf6e8755bae9e8e5e99cfeade11ad861c3aae136e6e0bc63160479fd9a94"
 
   url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/Ueli-#{version}#{arch}.dmg",
       verified: "github.com/oliverschwendener/ueli/"
@@ -11,10 +11,7 @@ cask "ueli" do
   desc "Keystroke launcher"
   homepage "https://ueli.app/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  disable! date: "2026-09-01", because: :unsigned
 
   depends_on macos: ">= :big_sur"
 
