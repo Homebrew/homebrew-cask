4aebdbb773acd9c31d29c3af0b9bdac91fc0b53d
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:19 -0700

    pg-commander: remove `no_autobump!`

--- Casks/p/pg-commander.rb
+++ Casks/p/pg-commander.rb
@@ -8,8 +8,6 @@ cask "pg-commander" do
   desc "PostgreSQL client"
   homepage "https://eggerapps.at/pgcommander/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "PG Commander.app"
