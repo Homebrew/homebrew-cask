9f2772367744761153850b17e94dbadeddc56338
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:18:41 -0700

    font-noto-sans-mono: remove `no_autobump!`

--- Casks/font/font-n/font-noto-sans-mono.rb
+++ Casks/font/font-n/font-noto-sans-mono.rb
@@ -12,8 +12,6 @@ cask "font-noto-sans-mono" do
     regex(/^NotoSansMono-v?(\d+(?:\.\d+)+)$/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   font "NotoSansMono/unhinted/variable/NotoSansMono[wdth,wght].ttf"
 
   # No zap stanza required
