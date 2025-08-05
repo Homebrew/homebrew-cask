bdf3fa3db5e5a1d5743c8490405bcd61fffb7d98
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:15:45 -0700

    backblaze-downloader: remove `no_autobump!`

--- Casks/b/backblaze-downloader.rb
+++ Casks/b/backblaze-downloader.rb
@@ -12,8 +12,6 @@ cask "backblaze-downloader" do
     regex(/Version\s+v?(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "BackblazeDownloader.app"
 
   uninstall quit: "com.backblaze.BackblazeDownloader"
