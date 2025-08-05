ff8c8c535cd12c87078aa972cacf4db559608ab3
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:20:34 -0700

    lightgallery: remove `no_autobump!`

--- Casks/l/lightgallery.rb
+++ Casks/l/lightgallery.rb
@@ -7,8 +7,6 @@ cask "lightgallery" do
   name "lightgallery"
   homepage "https://sachinchoolur.github.io/lightgallery-desktop/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-18", because: :unmaintained
   disable! date: "2025-07-18", because: :unmaintained
 
