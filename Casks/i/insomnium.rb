73b287dd3caf2dc6b012d7e734b3842a25d41f41
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:49 -0700

    insomnium: remove `no_autobump!`

--- Casks/i/insomnium.rb
+++ Casks/i/insomnium.rb
@@ -7,8 +7,6 @@ cask "insomnium" do
   desc "HTTP and GraphQL Client"
   homepage "https://github.com/ArchGPT/insomnium"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-28", because: :discontinued
   disable! date: "2025-07-28", because: :discontinued
 
