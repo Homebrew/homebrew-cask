c1705508d8d90e5ce0860565546bad9f15ebbd2e
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:19:43 -0700

    iem-plugin-suite: remove `no_autobump!`

--- Casks/i/iem-plugin-suite.rb
+++ Casks/i/iem-plugin-suite.rb
@@ -12,8 +12,6 @@ cask "iem-plugin-suite" do
     regex(/href=.*?IEMPluginSuite[._-]v?(\d+(?:\.\d+)+)\.(?:dmg|pkg)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   auto_updates true
   depends_on macos: ">= :el_capitan"
 
