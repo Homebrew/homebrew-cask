14a7d19d09d4fc2628f97505f6cf0abf2d6d9881
Patrick Linnane <patrick@linnane.io>
2025-08-05 14:22:50 -0700

    qtspim: remove `no_autobump!`

--- Casks/q/qtspim.rb
+++ Casks/q/qtspim.rb
@@ -12,8 +12,6 @@ cask "qtspim" do
     regex(%r{url=.*?/QtSpim[._-]v?(\d+(?:\.\d+)+)[._-]mac\.(?:m?pkg(?:\.zip)?|dmg)}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   pkg "QtSpim.mpkg/Contents/Packages/QtSpim.pkg"
 
   uninstall pkgutil: "org.larusstone.pkg.QtSpim"
