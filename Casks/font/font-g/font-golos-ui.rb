56667f5ff605b3c78007740f5d567e978daa1ec8
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2025-08-06 19:17:16 +0800

    font-golos-ui: new cask

---
+++ Casks/font/font-g/font-golos-ui.rb
@@ -0,0 +1,13 @@
+cask "font-golos-ui" do
+  version :latest
+  sha256 :no_check
+
+  url "https://api.paratype.com/api/editions/ofl/download/66961",
+      referer: "https://www.paratype.com/"
+  name "Golos UI"
+  homepage "https://www.paratype.com/fonts/pt/pt-root-ui"
+
+  font "PT/PT/Golos-UI/golos-ui_vf.ttf"
+
+  # No zap stanza required
+end
