7f13ac511c99f7398fb364690a375faeffd4d70d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 13:00:01 +0000

    earnapp 1.561.208

--- Casks/e/earnapp.rb
+++ Casks/e/earnapp.rb
@@ -1,6 +1,6 @@
 cask "earnapp" do
-  version "1.541.343"
-  sha256 "253733c5e1a53f97a158174fdb662ccdebb9e999855da958833542a95ff6f841"
+  version "1.561.208"
+  sha256 "7d3984140ca98b77e3f8a50edf6bab193db31941f14611876b038f8468d93118"
 
   url "https://cdn.earnapp.com/static/earnapp-macos-#{version}.dmg"
   name "EarnApp"
