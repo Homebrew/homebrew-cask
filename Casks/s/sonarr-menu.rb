f4f5a125aa9995653fe1255495a7b79da725bbda
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:46 -0700

    sonarr-menu: remove `no_autobump!`

--- Casks/s/sonarr-menu.rb
+++ Casks/s/sonarr-menu.rb
@@ -7,8 +7,6 @@ cask "sonarr-menu" do
   desc "Utility that adds a menu to the Status Bar for managing Sonarr"
   homepage "https://github.com/jefbarn/Sonarr-Menu/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   depends_on cask: "sonarr"
