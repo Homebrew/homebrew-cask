e8f205d91898f88d8f7806078746b6ad04b1545b
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:06 -0700

    supersync: remove `no_autobump!`

--- Casks/s/supersync.rb
+++ Casks/s/supersync.rb
@@ -6,8 +6,6 @@ cask "supersync" do
   name "SuperSync"
   homepage "https://supersync.com/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-09-08", because: :unmaintained
 
   app "SuperSync.app"
