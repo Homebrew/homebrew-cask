97205183c7ef086d70237bb3acba0543ad1c2eb2
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:32 -0700

    xampp@7: remove `no_autobump!`

--- Casks/x/xampp@7.rb
+++ Casks/x/xampp@7.rb
@@ -13,8 +13,6 @@ cask "xampp@7" do
     regex(/xampp[._-]osx[._-]v?(7(?:\.\d+)*-\d+)[._-]installer\.dmg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   conflicts_with cask: "xampp"
 
   installer script: {
