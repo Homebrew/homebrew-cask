0b37abd60589331e88e8ee7579f832402a9b7fd6
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:46 -0700

    sonarr@beta: remove `no_autobump!`

--- Casks/s/sonarr@beta.rb
+++ Casks/s/sonarr@beta.rb
@@ -12,8 +12,6 @@ cask "sonarr@beta" do
     regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2026-09-01", because: :unsigned
 
   conflicts_with cask: "sonarr"
