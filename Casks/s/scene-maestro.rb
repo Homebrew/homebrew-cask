35e5d9e45069ac3a1539b5fa5112f34f27a59a4e
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:18 -0700

    scene-maestro: remove `no_autobump!`

--- Casks/s/scene-maestro.rb
+++ Casks/s/scene-maestro.rb
@@ -12,8 +12,6 @@ cask "scene-maestro" do
     regex(/href=.*?scene-maestro[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "Scene Maestro.app"
