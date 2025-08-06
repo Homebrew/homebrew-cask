0e601922147ab54d4ae0b6391ddbf9edbd986a12
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:19 -0700

    wey: remove `no_autobump!`

--- Casks/w/wey.rb
+++ Casks/w/wey.rb
@@ -7,8 +7,6 @@ cask "wey" do
   desc "Open-source Slack desktop app"
   homepage "https://github.com/yue/wey"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Wey.app"
