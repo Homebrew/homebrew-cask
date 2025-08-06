5081a2b7f3e5ede9961545363c021236a232c3cc
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:43 -0700

    trinity: remove `no_autobump!`

--- Casks/t/trinity.rb
+++ Casks/t/trinity.rb
@@ -8,8 +8,6 @@ cask "trinity" do
   desc "Cryptocurrency wallet"
   homepage "https://trinity.iota.org/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Trinity.app"
