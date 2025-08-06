49c3fccd8d13f91900ac5958abd9eb9dc431029d
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:51 -0700

    zsa-wally: remove `no_autobump!`

--- Casks/z/zsa-wally.rb
+++ Casks/z/zsa-wally.rb
@@ -8,8 +8,6 @@ cask "zsa-wally" do
   desc "Flash tool for ZSA keyboards"
   homepage "https://ergodox-ez.com/pages/wally"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued, replacement_cask: "keymapp"
 
   app "Wally.app"
