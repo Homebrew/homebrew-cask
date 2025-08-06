08f0b40c589e58a7832fb2f25d0185a835bcbde3
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:05 -0700

    supermjograph: remove `no_autobump!`

--- Casks/s/supermjograph.rb
+++ Casks/s/supermjograph.rb
@@ -13,8 +13,6 @@ cask "supermjograph" do
     regex(%r{url=.*?/SuperMjograph[._-]v?(\d+(?:\.\d+)+)\.zip}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "SuperMjograph.app"
