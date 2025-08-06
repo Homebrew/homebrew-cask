d1825d7150d56fd14a1f5c5ba0016fec27a2a073
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:51 -0700

    sparkleshare: remove `no_autobump!`

--- Casks/s/sparkleshare.rb
+++ Casks/s/sparkleshare.rb
@@ -8,8 +8,6 @@ cask "sparkleshare" do
   desc "Tool to sync with any Git repository instantly"
   homepage "https://sparkleshare.org/"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-10-14", because: :discontinued
 
   app "SparkleShare.app"
