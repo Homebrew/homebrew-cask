bafdbc300892245349bd69a2aa9d185a85a042e9
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:53 -0700

    unetbootin: remove `no_autobump!`

--- Casks/u/unetbootin.rb
+++ Casks/u/unetbootin.rb
@@ -13,8 +13,6 @@ cask "unetbootin" do
     regex(/^v?(\d+(?:\.\d+)*)$/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "unetbootin.app"
 
   zap trash: "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.yourcompany.unetbootin.sfl*"
