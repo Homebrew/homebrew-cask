e5984560e0ed983573f747cc84b7a2c99be55353
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:11 -0700

    rosaimagewriter: remove `no_autobump!`

--- Casks/r/rosaimagewriter.rb
+++ Casks/r/rosaimagewriter.rb
@@ -6,8 +6,6 @@ cask "rosaimagewriter" do
   name "ROSA ImageWriter"
   homepage "http://wiki.rosalab.ru/en/index.php/ROSA_ImageWriter"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-09-08", because: :unmaintained
 
   app "RosaImageWriter.app"
