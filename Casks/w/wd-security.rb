f6e41c37dd430e23cee98f40b8e5a22704972590
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:16 -0700

    wd-security: remove `no_autobump!`

--- Casks/w/wd-security.rb
+++ Casks/w/wd-security.rb
@@ -13,8 +13,6 @@ cask "wd-security" do
     regex(/Version:?\s*(\d+(?:\.\d+)+)/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   container nested: "WD Security Installer.dmg"
 
   installer script: {
