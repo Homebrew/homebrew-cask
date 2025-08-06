fd3264cee2768457f6221fdcd102049f5239a6c0
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:40 -0700

    yellowdot: remove `no_autobump!`

--- Casks/y/yellowdot.rb
+++ Casks/y/yellowdot.rb
@@ -22,8 +22,6 @@ cask "yellowdot" do
   desc "Hides privacy indicators"
   homepage "https://lowtechguys.com/yellowdot"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "YellowDot.app"
