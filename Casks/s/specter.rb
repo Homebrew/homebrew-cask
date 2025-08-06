e0c41f4bf75058aa4382f5d0564d16f9e9f5250f
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:52 -0700

    specter: remove `no_autobump!`

--- Casks/s/specter.rb
+++ Casks/s/specter.rb
@@ -16,8 +16,6 @@ cask "specter" do
     regex(/href=.*?Specter[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :catalina"
 
   app "Specter.app"
