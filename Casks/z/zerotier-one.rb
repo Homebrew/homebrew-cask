35a9c06dc68b6442c702c66cd4fca9ac9e3f3edc
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:47 -0700

    zerotier-one: remove `no_autobump!`

--- Casks/z/zerotier-one.rb
+++ Casks/z/zerotier-one.rb
@@ -12,8 +12,6 @@ cask "zerotier-one" do
     regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   pkg "ZeroTier One.pkg"
