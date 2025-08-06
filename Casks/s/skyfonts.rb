45636a81221bdf7bb456c90687fbac63a1e9bc52
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:39 -0700

    skyfonts: remove `no_autobump!`

--- Casks/s/skyfonts.rb
+++ Casks/s/skyfonts.rb
@@ -12,8 +12,6 @@ cask "skyfonts" do
     regex(%r{href=.*?/Monotype_SkyFonts_Mac64_(\d+(?:\.\d+)*)\.dmg}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   installer manual: "Install SkyFonts.app"
 
   uninstall launchctl: "com.mti.Monotype-SkyFontsHelper",
