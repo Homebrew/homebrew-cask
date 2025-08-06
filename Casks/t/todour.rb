c0812abbfa547e39a5922247178950c86f86972d
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:36 -0700

    todour: remove `no_autobump!`

--- Casks/t/todour.rb
+++ Casks/t/todour.rb
@@ -12,8 +12,6 @@ cask "todour" do
     regex(/href=.*?Todour[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :high_sierra"
 
   app "Todour.app"
