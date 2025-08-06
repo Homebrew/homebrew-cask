2e54b1e44a2838039304c0e18986236e46282151
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:53 -0700

    splitshow: remove `no_autobump!`

--- Casks/s/splitshow.rb
+++ Casks/s/splitshow.rb
@@ -7,8 +7,6 @@ cask "splitshow" do
   desc "Dual-head presentation of PDF slides"
   homepage "https://github.com/mpflanzer/splitshow"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-11-16", because: :unmaintained
 
   app "SplitShow.app"
