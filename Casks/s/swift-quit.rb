9aab27e36ae2b76d2372061f2fbe8bb954f84412
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:08 -0700

    swift-quit: remove `no_autobump!`

--- Casks/s/swift-quit.rb
+++ Casks/s/swift-quit.rb
@@ -7,8 +7,6 @@ cask "swift-quit" do
   desc "Enable Windows-like program quitting when all windows are closed"
   homepage "https://github.com/onebadidea/swiftquit"
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "Swift Quit.app"
