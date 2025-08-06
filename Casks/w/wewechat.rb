2930fe0b674609aac0dd5fa2825d7eb741b7d31a
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:19 -0700

    wewechat: remove `no_autobump!`

--- Casks/w/wewechat.rb
+++ Casks/w/wewechat.rb
@@ -7,8 +7,6 @@ cask "wewechat" do
   desc "Unofficial WeChat client"
   homepage "https://github.com/trazyn/weweChat"
 
-  no_autobump! because: :requires_manual_review
-
   deprecate! date: "2024-07-07", because: :discontinued
   disable! date: "2025-07-07", because: :discontinued
 
