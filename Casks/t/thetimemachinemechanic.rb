a2f244c134ce9369a4fa25b032c1f47b11da3603
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:27 -0700

    thetimemachinemechanic: remove `no_autobump!`

--- Casks/t/thetimemachinemechanic.rb
+++ Casks/t/thetimemachinemechanic.rb
@@ -24,8 +24,6 @@ cask "thetimemachinemechanic" do
     end
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :big_sur"
 
   app "t2m2#{version.csv.first.major}#{version.csv.first.minor}/TheTimeMachineMechanic.app"
