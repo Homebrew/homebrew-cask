35fc18165d65116ec848e02601412e1d36cffb93
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:04 -0700

    resilio-sync: remove `no_autobump!`

--- Casks/r/resilio-sync.rb
+++ Casks/r/resilio-sync.rb
@@ -12,8 +12,6 @@ cask "resilio-sync" do
     regex(/u003ev?(\d+(?:\.\d+)+)[\\ "<]/i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
