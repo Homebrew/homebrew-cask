1c14002c9617ce2a694316dfeae0a354be9c3030
Patrick Linnane <patrick@linnane.io>
2025-08-05 17:59:48 -0700

    sonobus: remove `no_autobump!`

--- Casks/s/sonobus.rb
+++ Casks/s/sonobus.rb
@@ -12,8 +12,6 @@ cask "sonobus" do
     regex(%r{href=.*?/sonobus[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg}i)
   end
 
-  no_autobump! because: :requires_manual_review
-
   pkg "SonoBus Installer.pkg"
 
   uninstall pkgutil: [
