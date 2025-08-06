c30502198dd0cd94e5844ff46556f1204c06debd
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:31 -0700

    timelapze: remove `no_autobump!`

--- Casks/t/timelapze.rb
+++ Casks/t/timelapze.rb
@@ -7,8 +7,6 @@ cask "timelapze" do
   desc "Record screen and camera time lapses in a menu bar interface"
   homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
 
-  no_autobump! because: :requires_manual_review
-
   auto_updates true
   depends_on macos: ">= :sonoma"
 
