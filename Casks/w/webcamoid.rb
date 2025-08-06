ce29b3b92472012a1f0049c44db4c87aedc7035b
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:01:16 -0700

    webcamoid: remove `no_autobump!`

--- Casks/w/webcamoid.rb
+++ Casks/w/webcamoid.rb
@@ -8,8 +8,6 @@ cask "webcamoid" do
   desc "Webcam suite"
   homepage "https://webcamoid.github.io/"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   app "Webcamoid.app"
