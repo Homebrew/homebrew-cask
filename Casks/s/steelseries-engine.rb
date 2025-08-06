66bf82e13a1e8098dfaf092ba2a4b3b75985b14e
Patrick Linnane <patrick@linnane.io>
2025-08-05 18:00:00 -0700

    steelseries-engine: remove `no_autobump!`

--- Casks/s/steelseries-engine.rb
+++ Casks/s/steelseries-engine.rb
@@ -8,8 +8,6 @@ cask "steelseries-engine" do
   desc "Settings for SteelSeries peripherals and accessories"
   homepage "https://steelseries.com/engine"
 
-  no_autobump! because: :requires_manual_review
-
   disable! date: "2024-12-16", because: :discontinued
 
   auto_updates true
