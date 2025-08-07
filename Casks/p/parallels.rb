2be1c42e0758462e1c936af32aa67a3ed9d3a106
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 16:31:42 +0000

    parallels 20.4.1-55996

--- Casks/p/parallels.rb
+++ Casks/p/parallels.rb
@@ -1,6 +1,6 @@
 cask "parallels" do
-  version "20.4.0-55980"
-  sha256 "153950353751e52a6e945f5fd2ab669beca7a2f683e2d85334093de876c8cc54"
+  version "20.4.1-55996"
+  sha256 "084c8ff1864f2f38d50008ce694ab0438225ce4f726c0b534d950665b491aca4"
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsDesktop-#{version}.dmg"
   name "Parallels Desktop"
