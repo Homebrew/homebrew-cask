1e76a042050e63aa13105840760cb4d95e349da0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 13:45:39 +0000

    luanti 5.13.0

--- Casks/l/luanti.rb
+++ Casks/l/luanti.rb
@@ -1,9 +1,9 @@
 cask "luanti" do
   arch arm: "arm64", intel: "x86_64_flag_O1"
 
-  version "5.12.0,11.3"
-  sha256 arm:   "59b04cfef02fcd2ce0529f3ff1a48256730a322ca16018af9059154b4dd72606",
-         intel: "ce2d1174edaa4f2c3684cfacd6e686d90c299b775445dd11bad6daf9428f119b"
+  version "5.13.0,11.3"
+  sha256 arm:   "0fb67080d10404028c36dd5359bccb58114baf28dc6ffbeaab3c2be6eb475982",
+         intel: "0d8c3d761355a5343f090df3fe47af2062e2d902d26519fc318ad9c16c16c366"
 
   url "https://github.com/minetest/minetest/releases/download/#{version.csv.first}/luanti_#{version.csv.first}-macos#{version.csv.second}_#{arch}.zip",
       verified: "github.com/minetest/minetest/"
