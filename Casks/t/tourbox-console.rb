c19c00af7e9f0a00be28edf144de44cc9827c756
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 10:38:51 +0000

    tourbox-console 5.9.2

--- Casks/t/tourbox-console.rb
+++ Casks/t/tourbox-console.rb
@@ -1,6 +1,6 @@
 cask "tourbox-console" do
-  version "5.9.1"
-  sha256 "a9b5cada0ab216e35bc4f0c8944b7dfe3772a72e255e0fd76202dd13ad9846a0"
+  version "5.9.2"
+  sha256 "b257fa7d565681013cecf6743d681680428d3b85a161adf4baa6d516d3c8dec2"
 
   url "https://tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/TourBoxInstall#{version}.zip",
       verified: "tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/"
