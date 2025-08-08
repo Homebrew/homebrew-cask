c1e67ceabdcfd015cf972ff06168899d6a24bdc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 13:13:41 +0000

    hoppscotch 25.7.1-0

--- Casks/h/hoppscotch.rb
+++ Casks/h/hoppscotch.rb
@@ -1,9 +1,9 @@
 cask "hoppscotch" do
   arch arm: "aarch64", intel: "x64"
 
-  version "25.7.0-0"
-  sha256 arm:   "a0cfc6a65ed9f2d9603ecba341cd1152537c6a75a88907e0f9b4b183ea0ad801",
-         intel: "c85ccc32926906531c57534ca86d8bf17392c82d9c4f8f96319c1da6160f145e"
+  version "25.7.1-0"
+  sha256 arm:   "42bef1bf75f29de03d473bceeccaad6e5045fa83b86ae622e435ff1749ff2347",
+         intel: "ff9fbee227d1db1702994ee383eaa6e33b22f2cc125d36d4ec7773f34a7e5023"
 
   url "https://github.com/hoppscotch/releases/releases/download/v#{version}/Hoppscotch_mac_#{arch}.dmg",
       verified: "github.com/hoppscotch/releases/"
