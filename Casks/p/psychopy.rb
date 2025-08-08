9001ac2ec435c543924632232de364113f539160
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-08 13:10:44 -0400

    psychopy 2025.2.0
    `psychopy` is autobumped but the workflow is failing to update to
    2025.2.0 because the filename omits `py` from the `-py3.10` suffix
    (upstream is inconsistent from one version to the next).
    
    This also updates the `livecheck` block to check the first-party
    download page, as it links to the latest dmg file on GitHub, so we
    don't have to use the `GithubReleases` strategy.

--- Casks/p/psychopy.rb
+++ Casks/p/psychopy.rb
@@ -1,27 +1,20 @@
 cask "psychopy" do
-  version "2025.1.1"
-  sha256 "9e791fb5323b2ae56f470a37f2a47738282bf2e853b653f44ff793094f3402fc"
+  version "2025.2.0"
+  sha256 "d47410cc67ff2382278c34082300c0811a13ab5a8ce0b96df689094298df507b"
 
-  url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macOS#{"_#{version.csv.second}" if version.csv.second}-py3.10.dmg",
+  url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macOS#{"_#{version.csv.second}" if version.csv.second}-3.10.dmg",
       verified: "github.com/psychopy/psychopy/"
   name "PsychoPy"
   desc "Create experiments in behavioral science"
   homepage "https://www.psychopy.org/"
 
   livecheck do
-    url :url
+    url "https://www.psychopy.org/download.html"
     regex(/StandalonePsychoPy[._-]v?(\d+(?:\.\d+)+)[._-]macOS[._-]?(\d+(?:[._-]\d+)+)?[._-](?:py)?3\.10\.dmg/i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"] || release["prerelease"]
-
-        release["assets"]&.map do |asset|
-          match = asset["name"]&.match(regex)
-          next if match.blank?
-
-          match[2].present? ? "#{match[1]},#{match[2]}" : match[1]
-        end
-      end.flatten
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
     end
   end
 
