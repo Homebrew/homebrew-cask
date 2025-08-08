fa3ee3fd76f65b0233a809d262a0201c7cabb9e6
Rohan Patra <rohanpatra@berkeley.edu>
2025-08-08 11:22:01 -0700

    update hashes to match version

--- Casks/c/cursor-cli.rb
+++ Casks/c/cursor-cli.rb
@@ -2,8 +2,8 @@ cask "cursor-cli" do
   arch arm: "arm64", intel: "x64"
 
   version "2025.08.08-f57cb59"
-  sha256 arm:   "bc06b2336c7bb55c1a5037cbec15cb64605beb87a90053fb261ade4f33181c30",
-         intel: "07a2b0724ca4767eabc647148efc415bd40b0cd0fddc06258a881bb2728b1857"
+  sha256 arm:   "8ead37ee1e7205f2887310d12ec000956159dd653e7f0c93c51324362b6087f7",
+         intel: "7373847d6fbe0cfb989c2e1fae7af2b49292bae3ed279cd70b0ffec9e4d86fcc"
 
   url "https://downloads.cursor.com/lab/#{version}/darwin/#{arch}/agent-cli-package.tar.gz"
   name "Cursor CLI"
