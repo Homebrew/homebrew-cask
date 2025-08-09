9b392abfbdf773df80080e75ef57707ae49d59ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-09 07:22:41 +0000

    proxyman 5.23.1

--- Casks/p/proxyman.rb
+++ Casks/p/proxyman.rb
@@ -1,6 +1,6 @@
 cask "proxyman" do
-  version "5.23.0,52300"
-  sha256 "54524501428af5d674a69a1ba2b05b56ec22b79578ef7aa9a9ebf5199094aa9a"
+  version "5.23.1,52301"
+  sha256 "c2f11d625b2422ea8cbecac4fa195c5c3fe1a266f14fab46a10fa363a707a37a"
 
   url "https://download.proxyman.com/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
   name "Proxyman"
