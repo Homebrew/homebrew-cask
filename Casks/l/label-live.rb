ad6f603c16d564df5e9e5e9be19c19a93bcdbaed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-10 04:27:30 +0000

    label-live 4.0.2

--- Casks/l/label-live.rb
+++ Casks/l/label-live.rb
@@ -1,6 +1,6 @@
 cask "label-live" do
-  version "4.0.1"
-  sha256 "370208935ebd010d4e4811a5c6c5159f736cebc1a5309cf0b76040f7fc27842b"
+  version "4.0.2"
+  sha256 "eca8c38cf1ed04ec7a0185ce4607042c63a9927578597819194647e6f76e5473"
 
   url "https://download.label.live/Label-LIVE-#{version}.dmg"
   name "Label LIVE"
