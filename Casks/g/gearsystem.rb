6d6e42e277cd16a848a05d0ff8e3d64221996e4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-10 19:01:53 +0000

    gearsystem 3.8.3

--- Casks/g/gearsystem.rb
+++ Casks/g/gearsystem.rb
@@ -1,9 +1,9 @@
 cask "gearsystem" do
   arch arm: "arm", intel: "intel"
 
-  version "3.8.2"
-  sha256 arm:   "7f2d9e81d038cf8d98c804031b6e6b3d4c3ef3e6f3927a6130b781e083e769c0",
-         intel: "1655e543bd6df55acbe598e38cc08413c4e8efccc277c2bf4fae2205d0ab9e49"
+  version "3.8.3"
+  sha256 arm:   "de69aea5f64d06064621d98d4f29c2a9de2c258bc8342d689283bb23c884ff3d",
+         intel: "763bc9eab374ae6ed40c480fa6ff41a9e83fdd3edea2a59d2192150195548155"
 
   url "https://github.com/drhelius/Gearsystem/releases/download/#{version}/Gearsystem-#{version}-macos-#{arch}.zip"
   name "Gearsystem"
