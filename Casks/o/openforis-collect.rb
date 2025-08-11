c6a46cc4c952c2f856db040a6687e53a6e48530a
cypher-fox <dev@cypher-fox.com>
2025-08-11 13:01:09 +0200

    openforis-collect: add latest

---
+++ Casks/o/openforis-collect.rb
@@ -0,0 +1,36 @@
+cask "openforis-collect" do
+  arch arm: "osx-x86_64", intel: "osx-x86_64"
+
+  version "4.0.101"
+  sha256 "12a50bc37d0b8760175ac04c9b6a4d2cbefbe887405019d03598f40ab26121f3"
+
+  url "https://search.maven.org/remotecontent?filepath=org/openforis/collect/collect-installer/#{version}/collect-installer-#{version}-osx.dmg",
+      verified: "search.maven.org/"
+  name "Open Foris Collect"
+  desc "Data management for field-based inventories"
+  homepage "https://openforis.org/solutions/collect/"
+
+  livecheck do
+    url "https://github.com/openforis/collect"
+  end
+
+  rename "OpenForisCollect-*-osx-installer.app", "OpenForisCollect-osx-installer.app"
+
+  installer script: {
+    executable: "#{staged_path}/OpenForisCollect-osx-installer.app/Contents/MacOS/#{arch}",
+    args:       [
+      "--mode", "unattended"
+    ],
+  }
+
+  uninstall quit:   "OpenForisCollect-launcher.app",
+            signal: [
+              ["TERM", "OpenForisCollect-launcher.app"],
+            ],
+            script: {
+              executable: "~/OpenForis/Collect/uninstall.app/Contents/MacOS/#{arch}",
+              args:       ["--mode", "unattended"],
+            }
+
+  zap trash: "~/OpenForis/Collect"
+end
