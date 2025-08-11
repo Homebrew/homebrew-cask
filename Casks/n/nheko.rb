32243ded5104f92a07f47280f3e588ce5cb11a72
ThomsenDrake <drake.t98@proton.me>
2025-08-11 19:32:12 -0400

    nheko 0.12.1: update arm64 checksum

--- Casks/n/nheko.rb
+++ Casks/n/nheko.rb
@@ -2,7 +2,7 @@ cask "nheko" do
   arch arm: "apple-silicon", intel: "intel"
 
   version "0.12.1"
-  sha256 arm:   "b1be965ea57e538ee77088f9b7190524b4bf7ce9c348ab5dc707482884d80b5d",
+  sha256 arm:   "07390b869a5cef5226281b215a06c7d5c87b20b7bdfec9882c71ee005b2ac949",
          intel: "b0aea76c05876bddc7286ed717fd522e5ef24e076dc33b7b8149afb01bc1bd97"
 
   url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}-#{arch}.dmg",
