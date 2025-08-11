74cd318a21eca00051aeb0a35d98576248cb1ee3
Noah Liu <noahxp@gmail.com>
2025-08-11 13:36:53 +0800

    Add KaTrain cask
    KaTrain is a Go/Baduk/Weiqi playing and teaching app with AI analysis,
    supporting various AI opponents and comprehensive game analysis features.

--- Casks/k/katrain.rb
+++ Casks/k/katrain.rb
@@ -1,41 +1,22 @@
 cask "katrain" do
-  version "1.14.0"
-  sha256 "38cfe45fd4440983d3cbf4ca1305fa977c8479a16f6696bd22742ad451fa4cd7"
+  version "1.17.0"
+  sha256 "fe4594e286d94e46e0adb64616cbba910f65285ae5be27e326f07d8d532acfa3"
 
-  on_arm do
-    depends_on formula: "katago"
-  end
-
-  url "https://github.com/sanderland/katrain/releases/download/v#{version}/KaTrainOSX.dmg"
+  url "https://github.com/sanderland/katrain/releases/download/v#{version}/KaTrain-#{version}.dmg"
   name "KaTrain"
-  desc "Tool for analyzing games and playing go with AI feedback from KataGo"
+  desc "Go/Baduk/Weiqi playing and teaching app with AI analysis"
   homepage "https://github.com/sanderland/katrain"
 
-  # Most recent release doesn't provide a file for macOS, so we check multiple
-  # recent releases instead of only the "latest" release. NOTE: We should be
-  # able to remove this next release when upstream provides a file for macOS again.
   livecheck do
     url :url
-    regex(%r{/v?(\d+(?:\.\d+)+)/KaTrainOSX\.(?:dmg|pkg)$}i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"] || release["prerelease"]
-
-        release["assets"]&.map do |asset|
-          match = asset["browser_download_url"]&.match(regex)
-          next if match.blank?
-
-          match[1]
-        end
-      end.flatten
-    end
+    strategy :github_latest
   end
 
   app "KaTrain.app"
 
-  zap trash: "~/.katrain"
-
-  caveats do
-    requires_rosetta
-  end
+  zap trash: [
+    "~/.katrain",
+    "~/Library/Preferences/org.katrain.KaTrain.plist",
+    "~/Library/Application Support/KaTrain",
+  ]
 end
