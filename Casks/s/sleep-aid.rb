e05656a9d5e3b881096a34738521f8a805e24df2
singularitti <singularitti@outlook.com>
2025-08-12 22:14:44 -0500

    New cask: Add `sleep-aid` v1.5

---
+++ Casks/s/sleep-aid.rb
@@ -0,0 +1,34 @@
+cask "sleep-aid" do
+  version "1.5"
+  sha256 "3169a049a5571af432dcf41b8c1e678f8b06d37c1d3e0e937d5a6e4061c25f56"
+
+  url "https://ohanaware.com/sleepaid/release/Sleep_Aid_1_4_1_UB.dmg"
+  name "Sleep Aid"
+  desc "Monitor computer's sleeping habits"
+  homepage "https://ohanaware.com/sleepaid/"
+
+  livecheck do
+    url "https://ohanaware.com/sleepaid/versionHistory.html"
+    strategy :page_match do |page|
+      match = page.scan(/Version (\d+(?:\.\d+)*),/i)
+      match.map(&:first).max_by { |v| Version.new(v) }
+    end
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Sleep Aid.app"
+
+  uninstall quit: [
+    "com.ohanaware.sleepAid-Help",
+    "com.ohanaware.sleepAidRG2",
+  ]
+
+  zap trash: [
+    "~/Library/Application Support/com.ohanaware.sleepAidRG2",
+    "~/Library/Caches/com.ohanaware.sleepAidRG2",
+    "~/Library/HTTPStorages/com.ohanaware.sleepAidRG2",
+    "~/Library/Preferences/com.ohanaware.sleepAidRG2.plist",
+    "~/Library/WebKit/com.ohanaware.sleepAidRG2",
+  ]
+end
