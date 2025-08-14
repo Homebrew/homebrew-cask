e828bd707a692789c85f63eeb7e3b589d3a6fd8f
Steef Hegeman <mail@steefhegeman.com>
2025-08-14 14:49:39 +0200

    font-stix 2.13 b171
    As 2.14 was withdrawn [1], this is currently the newest version.
    
    [1]: https://github.com/stipub/stixfonts/issues/279

--- Casks/font/font-s/font-stix.rb
+++ Casks/font/font-s/font-stix.rb
@@ -1,11 +1,17 @@
 cask "font-stix" do
-  version "2.13"
-  sha256 "c4671ec4a116d887c8ebf91b2706301bebc43e776ac00f549c36ad1f34964c98"
+  version "2.13b171"
+  sha256 "1e76b9ab0bb08372ff73ad5b58d9116260e9058d1fce4b83fe1e213c3b9c947f"
 
   url "https://github.com/stipub/stixfonts/archive/refs/tags/v#{version}.tar.gz",
       verified: "github.com/stipub/stixfonts/"
   name "STIX"
   homepage "https://stixfonts.org/"
+
+  livecheck do
+    url :url
+    regex(/v?(\d+(?:\.\d+)+b?\d*)/i)
+    strategy :github_latest
+  end
 
   font "stixfonts-#{version}/fonts/static_otf/STIXTwoMath-Regular.otf"
   font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-Bold.otf"
