204cebd6bef288f06a547450e9c33684511d5cf7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-14 16:29:45 +0000

    messenger 520.0.0.67.107

--- Casks/m/messenger.rb
+++ Casks/m/messenger.rb
@@ -1,5 +1,5 @@
 cask "messenger" do
-  version "518.0.0.43.107"
+  version "520.0.0.67.107"
   sha256 :no_check
 
   url "https://www.messenger.com/messenger/desktop/downloadV2/?platform=mac&variant=catalyst"
