15a9d670b8740bc87d751f7e5558bb2b037321d9
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-16 10:02:44 -0400

    picoscope@beta 7.2.3.6884
    `picoscope@beta` is autobumped but the workflow is unable to update to
    the latest version because the `livecheck` block regex isn't able to
    match the current file URL on the upstream download page. This updates
    the regex to be able to match the current file URL and updates the
    cask to the latest version.

--- Casks/p/picoscope@beta.rb
+++ Casks/p/picoscope@beta.rb
@@ -1,20 +1,20 @@
 cask "picoscope@beta" do
-  version "7.1.58.6335"
-  sha256 "4039275fc877e258bfc50f3dee577a709a11da87432d8bf8a6b91f032e76c281"
+  version "7.2.3.6884"
+  sha256 "52a61fcd36c39c1c7c04dd5028220f22dffaddb3565bc25f65f5a6c8e5287097"
 
-  url "https://www.picotech.com/download/software/beta/PicoScope_#{version.major}_TandM_Early_Access_#{version}.pkg"
+  url "https://www.picotech.com/download/software/beta/PicoScope_#{version.major}_TandM_Early_Access_#{version}.x64.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscopes"
   homepage "https://www.picotech.com/"
 
   livecheck do
     url "https://www.picotech.com/downloads/picoscope#{version.major}-early-access"
-    regex(/href=.*?PicoScope[._-]#{version.major}.*?Access[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+    regex(/href=.*?PicoScope[._-][^"' >]*?[._-]v?(#{version.major}(?:\.\d+)+)[^"' >]*?\.pkg/i)
   end
 
   conflicts_with cask: "picoscope"
 
-  pkg "PicoScope_#{version.major}_TandM_Early_Access_#{version}.pkg"
+  pkg "PicoScope_#{version.major}_TandM_Early_Access_#{version}.x64.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}tnmbeta"
 
