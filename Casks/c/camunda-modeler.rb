be1ed8e63db47b1c59fc467371f833dc8bed4467
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-16 15:42:50 +0000

    camunda-modeler 5.38.1

--- Casks/c/camunda-modeler.rb
+++ Casks/c/camunda-modeler.rb
@@ -1,9 +1,9 @@
 cask "camunda-modeler" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.38.0"
-  sha256 arm:   "d8af452a926e082bbb8d3784c36ab43a2c0508a436598989605c006f663426bb",
-         intel: "14072be04c6e079791facc88b41495543b2e71d9ace803692b0f83fbc0bb68b6"
+  version "5.38.1"
+  sha256 arm:   "467cc433ea4049b2cd30e28f242251e9f606152c3801e6afb9f9bdb2bf03d6f7",
+         intel: "ccc117325b357a71f447879598642c25cf691c499cf06b3bcb87b1701389b5d2"
 
   url "https://downloads.camunda.cloud/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac-#{arch}.zip",
       verified: "downloads.camunda.cloud/release/camunda-modeler/"
