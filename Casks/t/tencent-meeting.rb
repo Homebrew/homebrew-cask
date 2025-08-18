df71035193a726e4695ad57389953a60b674f42d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 04:47:42 +0000

    tencent-meeting 3.36.10.475

--- Casks/t/tencent-meeting.rb
+++ Casks/t/tencent-meeting.rb
@@ -2,12 +2,12 @@ cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "3.36.1.417,7b066b02bd8525d98277923b7c91a2e1"
-    sha256 "18a92a3a0dab2375fc668525f123701a3fcb0a71095f71dcc931c21f6c8c9395"
+    version "3.36.10.475,40a205798fe92747ae7698b8976f60ba"
+    sha256 "57d70c07e74f174693cf60ed733766c680dc4c0eb407b35e4d3ba92eefa40786"
   end
   on_intel do
-    version "3.36.1.417,63479f7f15e0cb125c4625fc5ed0825f"
-    sha256 "fa7b2592eed53b8a24b95c6d78007f670fdfa68d835d0f60599560e07f1b7183"
+    version "3.36.10.475,071921254e9910aff398e3bba57bc13e"
+    sha256 "2a8707a75dc8f4c62f99e7cf4891a11ceda68ae903b109e2411e256795386316"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.officialwebsite.dmg",
