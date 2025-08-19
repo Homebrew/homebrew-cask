34c82c51ebbf4a4c6d925881ae22d3d40531e905
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-19 21:58:01 +0000

    fossa 3.11.3

--- Casks/f/fossa.rb
+++ Casks/f/fossa.rb
@@ -1,9 +1,9 @@
 cask "fossa" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.11.2"
-  sha256 arm:   "23fe7a5a2aecbdfaba3bda8b9740f52eee872a6ebd04b3c61659dd7222454414",
-         intel: "d87cbf987b98a805a9f2a560c21e198834a8b2b19d0d513d4273ac45d5b1e17c"
+  version "3.11.3"
+  sha256 arm:   "7d0a333635e39cff5be0d8e7392b72775bb9ef5333f21ae056e613706079f292",
+         intel: "ab5ee4a5c1cdb562506a8b0c5bd86bd7ce6a3cba2dc135707612885c3fc228ea"
 
   url "https://github.com/fossas/fossa-cli/releases/download/v#{version}/fossa_#{version}_darwin_#{arch}.zip",
       verified: "github.com/fossas/fossa-cli/"
