1dbfcc4cd5f4597c4b887d67866aab3b7a63f7f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-19 19:53:35 +0000

    stratoshark@dev 0.9.3rc0,687,c7112251c3ab

--- Casks/s/stratoshark@dev.rb
+++ Casks/s/stratoshark@dev.rb
@@ -1,6 +1,6 @@
 cask "stratoshark@dev" do
-  version "0.9.3rc0,638,74ef672b04d6"
-  sha256 "c4bd735ad45f6d2f9c1c29bf19722add4ef6a6ddc085f28de1ded75442c2d817"
+  version "0.9.3rc0,687,c7112251c3ab"
+  sha256 "fb9dede453b754516219002eb418c59fde5de0949c962fe16bcf411bda7f7de0"
 
   url "https://www.wireshark.org/download/automated/osx/Stratoshark%20#{version.csv.first}-#{version.csv.second}-g#{version.csv.third}.dmg",
       verified: "wireshark.org/download/"
