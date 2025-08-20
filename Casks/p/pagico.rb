e25777c5d4c7a83ce2e3cf8755ae0d1adeb4dc21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 01:32:50 +0000

    pagico 10,20250819

--- Casks/p/pagico.rb
+++ Casks/p/pagico.rb
@@ -1,6 +1,6 @@
 cask "pagico" do
-  version "10,20250801"
-  sha256 "0e8c794a107f8ba1161744abb980daf85774c76b46713747f0b815aa2fd2476b"
+  version "10,20250819"
+  sha256 "ee7438a4c6a1916b8c5fef2b9c903c89879a4da5ede3345097c38c84950397a1"
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.csv.second}.dmg"
   name "Pagico"
