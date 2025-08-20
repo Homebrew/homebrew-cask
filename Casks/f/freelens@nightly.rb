269e23de70a74ba52545c276f3f76b1c0c7cc072
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 04:08:31 +0000

    freelens@nightly 1.5.4-0-nightly-2025-08-20

--- Casks/f/freelens@nightly.rb
+++ Casks/f/freelens@nightly.rb
@@ -1,9 +1,9 @@
 cask "freelens@nightly" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.5.4-0-nightly-2025-08-19"
-  sha256 arm:   "609e51b608f12ab433446aeca6f262f6ef6b84f41d1d35c2ab6f6f40e618a6d7",
-         intel: "1d985ab4ec77c13fa74809cb78df128578ac44e2d31d9b6902ef994a53bb6001"
+  version "1.5.4-0-nightly-2025-08-20"
+  sha256 arm:   "60f9acefa98e6bbbce1a101b16b4f30035f1983564cfc17d4db6b8f6149b84e7",
+         intel: "0d5b42151e1416f43e6dd58d6652cc4fa0c09013a730b866b3ec8fbf4e0052b6"
 
   url "https://github.com/freelensapp/freelens-nightly-builds/releases/download/v#{version}/Freelens-#{version}-macos-#{arch}.dmg",
       verified: "github.com/freelensapp/freelens-nightly-builds/"
