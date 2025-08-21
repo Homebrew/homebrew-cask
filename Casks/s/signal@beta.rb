94ab96da6b12450e87708ea4549570c9a71ddcc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 19:42:24 +0000

    signal@beta 7.68.0-beta.2

--- Casks/s/signal@beta.rb
+++ Casks/s/signal@beta.rb
@@ -1,9 +1,9 @@
 cask "signal@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.68.0-beta.1"
-  sha256 arm:   "c6c1b48d079373e81c3b714a2063246d562967a9eb75452e1a18c2ae91c8d428",
-         intel: "1197bcf55e01e4639765422c0408a9ab00ecf4b3f58a77e2c19515748fb721bf"
+  version "7.68.0-beta.2"
+  sha256 arm:   "b34610e7bb6538999da7eb45413ab0b23f2c3780f5782e145aea0b877f22fec3",
+         intel: "79ca32845259dceed6700c968c816d20789739a92e6dcd3662facf1ac1b03267"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.zip"
   name "Signal Beta"
