df8a274a8bbf12aa340d3ab363d3d8e52a669cbb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 14:11:02 +0000

    tuist 4.64.1

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
-  version "4.64.0"
-  sha256 "74dc93a5b21ab10d1de589d98eb6df884a77f0857dc98694942c5f0096e33aef"
+  version "4.64.1"
+  sha256 "bb8ab62a8306d0fca08ed86b5bdf0512ab23f0e940c5b7270de5bd1123b81f57"
 
   url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
       verified: "github.com/tuist/tuist/"
