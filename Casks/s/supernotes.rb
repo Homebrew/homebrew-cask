46c07c6d06735ce3567bdfca19bfe9bcfe291574
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 16:52:52 +0000

    supernotes 3.2.1

--- Casks/s/supernotes.rb
+++ Casks/s/supernotes.rb
@@ -1,9 +1,9 @@
 cask "supernotes" do
   arch arm: "-arm64"
 
-  version "3.2.0"
-  sha256 arm:   "783243f63257274550d8c3ad6c24c06d93b56352eb983403455e2c7522387fff",
-         intel: "e3ce6088a9db32fecccdc5538070d26f2448c07a1f2e4688899b0cf8c006f715"
+  version "3.2.1"
+  sha256 arm:   "1975824ec7d23cf8147378fa681ce2d000b77b55867dd8cf06b1c123913860c0",
+         intel: "e61787cd9a37aee7ba973d782c87e21b89556e2c4eea9db2d9906c91ceba9faa"
 
   url "https://download.supernotes.app/Supernotes-#{version}#{arch}.dmg"
   name "Supernotes"
