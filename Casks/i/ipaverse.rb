ffd8bf1e899e2b04dbc84ecac86ba3341fc13ab7
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2025-08-22 23:47:58 +0300

    no message

---
+++ Casks/i/ipaverse.rb
@@ -0,0 +1,20 @@
+cask "ipaverse" do
+  version "1.0.0"
+  sha256 "e3e28f60eb9bfc000977e6e5f0e1b455854daebd034770567eefae537e15d4f4"
+
+  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg"
+  name "ipaverse"
+  desc "Modern macOS app for downloading and managing iOS apps from the App Store"
+  homepage "https://github.com/bahattinkoc/ipaverse"
+  license "MIT"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ipaverse.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.ipaverse.plist",
+    "~/Library/Application Support/ipaverse",
+    "~/Library/Caches/ipaverse"
+  ]
+end
