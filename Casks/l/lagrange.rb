40ef31e5c904c1b27fe6157fbb39296ebc79cc98
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-08-22 16:47:11 +0000

    lagrange 1.18.8

--- Casks/l/lagrange.rb
+++ Casks/l/lagrange.rb
@@ -2,9 +2,9 @@ cask "lagrange" do
   arch arm: "11.0-arm64", intel: "10.13-x86_64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
 
-  version "1.18.7"
-  sha256 arm:   "c4973da765c3a0abe898973261391e70a256446cf2ca1966abce7ac104620710",
-         intel: "a28e72ba088801453179852899771706571a78861f15d06cd9149aee1f7a1499"
+  version "1.18.8"
+  sha256 arm:   "d89d76f94069c1d62eb27b7164c77b8c81db86e691de601993416321d179abc6",
+         intel: "1f2fd3d51981b0bd2d440ea475f7f130d023be948c465755cafd502ad7029c1c"
 
   url "https://git.skyjake.fi/gemini/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos#{arch}.tbz"
   name "Lagrange"
