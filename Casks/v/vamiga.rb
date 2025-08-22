efb0fa2204607c28c1330f39aa0c149f565d73f3
ganona <112239200+ganona@users.noreply.github.com>
2025-08-22 23:22:32 +0200

    Update hash for vAmiga

--- Casks/v/vamiga.rb
+++ Casks/v/vamiga.rb
@@ -1,6 +1,6 @@
 cask "vamiga" do
   version "4.3"
-  sha256 "0191fa3a7b0510552182c8a37197a324d2c759b128e2e94fbba8f773486087fd"
+  sha256 "d08ac5a98ed0b4f097c8d628b142986d85c5838a27af72d0db692c87eefc31d1"
 
   url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip",
       verified: "github.com/dirkwhoffmann/vAmiga/"
