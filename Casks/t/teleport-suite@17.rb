e0bd2bc221905288003d07c080f2031b16e8e58d
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-08-22 13:10:54 +0300

    teleport-suite@17 17.7.2

--- Casks/t/teleport-suite@17.rb
+++ Casks/t/teleport-suite@17.rb
@@ -1,6 +1,6 @@
 cask "teleport-suite@17" do
-  version "17.7.1"
-  sha256 "5a12e675449be7d36161a90efef6b47f4b1bd51cf5a446bdef3217af344db832"
+  version "17.7.2"
+  sha256 "79fcbfb7b315faa37661682811f2278326dfe165d0d055f4a73ad080d7f532c2"
 
   url "https://cdn.teleport.dev/teleport-#{version}.pkg",
       verified: "cdn.teleport.dev/"
