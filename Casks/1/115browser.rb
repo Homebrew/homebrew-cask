f330b4e69df80fd24ce793badd38ad183442fe71
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-08-22 14:20:17 +0000

    115browser 36.0.0

--- Casks/1/115browser.rb
+++ Casks/1/115browser.rb
@@ -1,9 +1,9 @@
 cask "115browser" do
   arch arm: "arm64", intel: "x64"
 
-  version "35.30.0"
-  sha256 arm:   "dd50a66ae71531ffc10eda849ba8a0649283827215ac9dcbb45de389838fcb57",
-         intel: "e94965e79e23bd5c66138e004cc56fd0302f1c14cadb0aef0aa977d19fe7ab6f"
+  version "36.0.0"
+  sha256 arm:   "4854217ba2455d5848f11c7087c2cff15d7a2f44fc35400f96a5e38e4e4ed539",
+         intel: "8b9c149426e9a9954f9da71d51e7e57c01480076218d9124b806738b49cb12eb"
 
   url "https://down.115.com/client/mac/115br_v#{version}_#{arch}.dmg"
   name "115Browser"
