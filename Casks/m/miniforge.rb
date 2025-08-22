1ad2e836ccc636923dde689e3da3240d90fbb20f
CI Auto Tagger <ci@example.com>
2025-08-22 14:45:20 +0100

    Environment preservation during upgrades and reinstalls.
    Signed-off-by: CI Auto Tagger <ci@example.com>

--- Casks/m/miniforge.rb
+++ Casks/m/miniforge.rb
@@ -1,5 +1,5 @@
 cask "miniforge" do
-  arch arm: "arm64", intel: "x86_64"
+  arch arm:   "arm64", intel: "x86_64"
 
   version "25.3.1-0"
   sha256 arm:   "d9eabd1868030589a1d74017b8723b01cf81b5fec1b9da8021b6fa44be7bbeae",
@@ -7,7 +7,7 @@ cask "miniforge" do
 
   url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-#{arch}.sh"
   name "miniforge"
-  desc "Community-driven minimal conda installer"
+  desc "Minimal installer for conda specific to conda-forge"
   homepage "https://github.com/conda-forge/miniforge"
 
   livecheck do
@@ -15,52 +15,63 @@ cask "miniforge" do
     regex(/v?(\d+(?:[.-]\d+)+)/i)
     strategy :github_latest
   end
+
   auto_updates true
   conflicts_with cask: [
     "mambaforge",
     "miniconda",
   ]
+
   container type: :naked
 
-  install_root = ENV["MINIFORGE_ROOT"] || "#{caskroom_path}/base"
+  install_root = ENV["MINIFORGE_ROOT"] || "#{HOMEBREW_PREFIX}/miniforge3"
 
   installer script: {
     executable: "Miniforge3-#{version}-MacOSX-#{arch}.sh",
-    args:       ["-b", "-p", "#{caskroom_path}/base"],
     args:       ["-b", "-p", install_root],
   }
 
-   uninstall delete: "#{caskroom_path}/base"
   binary "#{install_root}/condabin/conda"
   binary "#{install_root}/condabin/mamba"
 
+  preflight do
+    if File.directory?("#{HOMEBREW_PREFIX}/envs-backup")
+      FileUtils.mv(
+        "#{HOMEBREW_PREFIX}/envs-backup",
+        "#{install_root}/envs",
+      )
+    end
+  end
+
   uninstall_preflight do
     if File.directory?("#{install_root}/envs")
-      FileUtils.mv("#{install_root}/envs", "#{caskroom_path}/envs-backup")
+      FileUtils.mv(
+        "#{install_root}/envs",
+        "#{HOMEBREW_PREFIX}/envs-backup",
+      )
     end
   end
 
-  preflight do
-    if File.directory?("#{caskroom_path}/envs-backup")
-      FileUtils.mv("#{caskroom_path}/envs-backup", "#{install_root}/envs")
-    end
-  end
-
-  uninstall rmdir: "#{caskroom_path}"
+  uninstall delete: install_root
+  uninstall rmdir:  install_root
 
   zap trash: [
-     "~/.conda",
+    "~/.conda",
     "~/.condarc",
-    "#{caskroom_path}/envs-backup",
+    "#{HOMEBREW_PREFIX}/envs-backup",
   ]
 
-  caveats <<~EOS
-    Please run the following to set up your shell:
-      conda init "$(basename "${SHELL}")"
-    To install into your home dir (and preserve envs across uninstall/reinstall), run:
-      export MINIFORGE_ROOT="${HOME}/miniforge3"
-    Then:
-      brew install --cask miniforge
-    Your conda envs will now be auto-saved on upgrade and safely restored.
-  EOS
+  caveats do
+    files_in_usr_local
+    <<~EOS
+      To install into your home directory instead (preserving envs across
+      upgrades and reinstalls), set:
+
+        export MINIFORGE_ROOT="${HOME}/miniforge3"
+
+      Then run:
+
+        brew install --cask miniforge
+    EOS
+  end
 end
