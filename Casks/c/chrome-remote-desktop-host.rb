2d30b859e08365cad75282adbae6c8fa77926dbd
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-08-22 12:48:11 +0300

    chrome-remote-desktop-host 140.0.7339.17

--- Casks/c/chrome-remote-desktop-host.rb
+++ Casks/c/chrome-remote-desktop-host.rb
@@ -1,5 +1,5 @@
 cask "chrome-remote-desktop-host" do
-  version "139.0.7258.8"
+  version "140.0.7339.17"
   sha256 :no_check
 
   url "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg"
