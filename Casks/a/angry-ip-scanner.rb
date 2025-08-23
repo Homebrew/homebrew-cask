5ffb4aab43f33804845e162ae5109076474f8335
Bevan Kay <email@bevankay.me>
2025-08-23 12:16:54 +1000

    angry-ip-scanner: replace `unsigned` reason with `fails_gatekeeper_check`

--- Casks/a/angry-ip-scanner.rb
+++ Casks/a/angry-ip-scanner.rb
@@ -11,7 +11,7 @@ cask "angry-ip-scanner" do
   desc "Network scanner"
   homepage "https://angryip.org/"
 
-  disable! date: "2026-09-01", because: :unsigned
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: ">= :big_sur"
 
