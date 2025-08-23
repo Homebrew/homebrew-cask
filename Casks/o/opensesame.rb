0a066df62ea4c502d670e3cac1cae0ee215f5236
bevanjkay <email@bevankay.me>
2025-08-23 12:11:51 +1000

    opensesame 4.1.0

--- Casks/o/opensesame.rb
+++ Casks/o/opensesame.rb
@@ -1,6 +1,6 @@
 cask "opensesame" do
-  version "4.0.24"
-  sha256 "8b55cbba3350afe254c978127c98b7ac2562221b36531065bb21b4ce42037e1b"
+  version "4.1.0"
+  sha256 "6bccd27ea150144f67e46cd39ef720435de99faf559a55b94371739e4e45fdd5"
 
   url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py311-macos-x64-1.dmg",
       verified: "github.com/open-cogsci/OpenSesame/"
@@ -33,8 +33,4 @@ cask "opensesame" do
     "~/.opensesame",
     "~/Library/Preferences/com.cogscinl.default.plist",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
