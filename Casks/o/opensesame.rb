509287b48445773bde5ea9647a7b04183dddf461
Bevan Kay <email@bevankay.me>
2025-08-23 12:17:13 +1000

    opensesame: replace `unsigned` reason with `fails_gatekeeper_check`

--- Casks/o/opensesame.rb
+++ Casks/o/opensesame.rb
@@ -25,7 +25,7 @@ cask "opensesame" do
     end
   end
 
-  disable! date: "2026-09-01", because: :unsigned
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "OpenSesame.app"
 
