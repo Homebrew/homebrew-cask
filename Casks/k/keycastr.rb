e9c30e3771b62db467ced155b33b5392b5c74510
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-24 01:13:14 +0000

    keycastr 0.10.4

--- Casks/k/keycastr.rb
+++ Casks/k/keycastr.rb
@@ -1,6 +1,6 @@
 cask "keycastr" do
-  version "0.10.3"
-  sha256 "e3384bb08686d2b2bba509d0d119a1c2b714046b5407871adfc1a51574298a25"
+  version "0.10.4"
+  sha256 "9724866bf7c044e32caa88bffc97df512a21d31ee88c3885c21a1a925f201318"
 
   url "https://github.com/keycastr/keycastr/releases/download/v#{version}/KeyCastr.app.zip"
   name "KeyCastr"
