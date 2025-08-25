deaf73ea667d69fae0e0c0739af68b747147e03f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-08-24 20:52:19 -0400

    clone-hero: disable due to failing Gatekeeper checks

--- Casks/c/clone-hero.rb
+++ Casks/c/clone-hero.rb
@@ -13,6 +13,8 @@ cask "clone-hero" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on macos: ">= :high_sierra"
 
   app "Clone Hero.app"
