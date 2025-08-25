95387842437d7987d15f9dd151d1704ada164337
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 01:34:08 +0000

    solar2d 2025.3722

--- Casks/s/solar2d.rb
+++ Casks/s/solar2d.rb
@@ -1,6 +1,6 @@
 cask "solar2d" do
-  version "2025.3721"
-  sha256 "aca2e105e89c57d7438c1d304b6e05578cd1be3fc264a96be0d245df6a337f8e"
+  version "2025.3722"
+  sha256 "31d2edb547fe0167c5da0c7d31562d6841ced2e27fceac9eddaf46ee1c11698d"
 
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg",
       verified: "github.com/coronalabs/corona/"
