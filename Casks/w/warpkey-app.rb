1527b488c09c21597bda49a60817704125f58a2e
Musa Matini <musamat00@gmail.com>
2025-08-25 12:02:08 +0300

    Add warpkey-app v2.0.2

---
+++ Casks/w/warpkey-app.rb
@@ -0,0 +1,19 @@
+cask "warpkey-app" do
+  version "2.0.2"
+  sha256 "58abadfab929b5ef595328afa388e05cae9d7d0f87ddcc7c011b84b9bae95f71"
+
+  url "https://github.com/musamatini/WarpKey/releases/download/v#{version}/WarpKey-Universal-Stable-v#{version}.zip"
+  name "WarpKey"
+  desc "Keyboard-centric application and window switcher"
+  homepage "https://github.com/musamatini/WarpKey/"
+
+  auto_updates true
+
+  app "WrapKey.app"
+
+  zap trash: [
+    "~/Library/Application Support/WarpKey",
+    "~/Library/Caches/dev.WarpKey",
+    "~/Library/Preferences/dev.WarpKey.plist",
+  ]
+end
