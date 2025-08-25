d11a652876c7b25527306bfab31c084d3f460336
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-08-25 12:39:07 +0200

    autopip 1.0

---
+++ Casks/a/autopip.rb
@@ -0,0 +1,20 @@
+cask "autopip" do
+  version "1.0"
+  sha256 "7fefc8e052643fe5b7dfdaf3ef4c774d7681a495ac2922d726d2f16fc830eb86"
+
+  url "https://github.com/vordenken/AutoPiP/releases/download/v#{version}/AutoPiP.dmg"
+  name "AutoPiP"
+  desc "Picture-in-Picture mode when switching tab in Safari"
+  homepage "https://github.com/vordenken/AutoPiP/"
+
+  depends_on macos: ">= :sonoma"
+
+  app "AutoPiP.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.vd.AutoPiP",
+    "~/Library/Application Scripts/com.vd.AutoPiP.Extension",
+    "~/Library/Containers/com.vd.AutoPiP",
+    "~/Library/Containers/com.vd.AutoPiP.Extension",
+  ]
+end
