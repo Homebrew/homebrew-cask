c311b7076f85cda772c1554e52d56a2c47415940
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 16:14:36 +0000

    highlight-ai 1.3.84

--- Casks/h/highlight-ai.rb
+++ Casks/h/highlight-ai.rb
@@ -2,9 +2,9 @@ cask "highlight-ai" do
   arch arm: "arm64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "1.3.82"
-  sha256 arm:   "70e4959f800974ff94680be15b7553df1cce2421584029f723dfdab2cc2a6752",
-         intel: "cc912cb685c01ff80e4427ff186fad97832834ec796def963e5b0f700fa53a8b"
+  version "1.3.84"
+  sha256 arm:   "0f0fb72e7fdc89c27dcdcdd5789d4f0062db521d90f457a0c1d91232c42bed5f",
+         intel: "6f3691758f89c3e4ff4d189cba7a7a077e9e5bb3f5802363baf77852085189ba"
 
   url "https://cdn.highlightai.com/releases/darwin/#{arch}/Highlight-#{version}#{arch_suffix}.dmg"
   name "Highlight"
