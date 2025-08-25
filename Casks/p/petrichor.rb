668c42325b1a505980c0e5cfa26bc59a27b4603b
Kushal Pandya <kushalspandya@gmail.com>
2025-08-24 21:47:30 -0400

    Add Petrichor 1.0.0

---
+++ Casks/p/petrichor.rb
@@ -0,0 +1,24 @@
+cask "petrichor" do
+  version "1.0.0"
+  sha256 "5c57c896edcd5549961354984040d48c91449b36b57af3821ea428f47e4e8f35"
+
+  url "https://github.com/kushalpandya/Petrichor/releases/download/v#{version}/Petrichor-#{version}-Universal.dmg"
+  name "Petrichor"
+  desc "Offline Music Player"
+  homepage "https://github.com/kushalpandya/Petrichor"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Petrichor.app"
+
+  zap trash: [
+    "~/Library/Containers/org.Petrichor",
+    "~/Library/Saved Application State/org.Petrichor.savedState",
+  ]
+end
