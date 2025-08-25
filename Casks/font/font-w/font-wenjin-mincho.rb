e1c454405b03758351c27dcb48e7fc173f12e943
Joseph Jin-Chuan Tang <jinntrance@gmail.com>
2025-08-25 23:56:06 +0800

    Add font-wenjin-mincho

---
+++ Casks/font/font-w/font-wenjin-mincho.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "font-wenjin-mincho" do
+  version "1.100"
+  sha256 "6f547297d89dce2ec1777228baa43a36b5f3a078611a12ac375da1db43f48c8c"
+
+  url "https://github.com/takushun-wu/WenJinMincho/releases/download/v#{version}/WenJinMincho-OTC.7z"
+  name "font-wenjin-mincho"
+  desc "可免费商用的大字符集宋体字库"
+  homepage "https://github.com/takushun-wu/WenJinMincho"
+
+  font "WenJinMincho-OTF.ttc"
+
+end
