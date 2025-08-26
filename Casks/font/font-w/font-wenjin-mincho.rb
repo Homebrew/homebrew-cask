d720ec2549763a1e7aac9fe274ebe02c09fb6e60
Joseph Jin-Chuan Tang <jinntrance@gmail.com>
2025-08-26 17:32:48 +0800

    Update Casks/font/font-w/font-wenjin-mincho.rb
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/font/font-w/font-wenjin-mincho.rb
+++ Casks/font/font-w/font-wenjin-mincho.rb
@@ -3,7 +3,7 @@ cask "font-wenjin-mincho" do
   sha256 "6f547297d89dce2ec1777228baa43a36b5f3a078611a12ac375da1db43f48c8c"
 
   url "https://github.com/takushun-wu/WenJinMincho/releases/download/v#{version}/WenJinMincho-OTC.7z"
-  name "font-wenjin-mincho"
+  name "WenJin Mincho"
   desc "可免费商用的大字符集宋体字库"
   homepage "https://github.com/takushun-wu/WenJinMincho"
 
