1cb9a16352c768822a7f7a8536844958bbb6c06a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-26 11:35:58 -0400

    soulseek 2024-6-30
    `soulseek` is autobumped but the workflow failed to update to version
    2024-6-30 because the download files are now hosted on Backblaze B2.
    This updates the version and URL accordingly.
    
    Besides that, this tweaks the `livecheck` block regex to use a more
    standard pattern, which will also continue to match if the `href`
    attribute only contains the filename instead of being absolute. In
    this particular context, we don't need to strictly delimit the start
    of the filename (i.e., using `/`) to accurately match the dmg version.

--- Casks/s/soulseek.rb
+++ Casks/s/soulseek.rb
@@ -1,15 +1,16 @@
 cask "soulseek" do
-  version "2024-2-1"
-  sha256 "2daa09953aa9bbb62ffa4dee87b8cc219c3f8f88f349309d47faa3aaed213e70"
+  version "2024-6-30"
+  sha256 "43a788bf49e50f7a1d23bd4b9df89db5835edf431328b88f502619a7a5818cf1"
 
-  url "https://www.slsknet.org/SoulseekQt/Mac/SoulseekQt-#{version}.dmg"
+  url "https://f004.backblazeb2.com/file/SoulseekQt/SoulseekQt-#{version}.dmg",
+      verified: "f004.backblazeb2.com/file/SoulseekQt/"
   name "Soulseek"
   desc "File sharing network"
   homepage "https://www.slsknet.org/"
 
   livecheck do
     url "https://www.slsknet.org/news/node/1"
-    regex(%r{href=.*?/SoulseekQt[._-]v?(\d+(?:-\d+)+)\.dmg}i)
+    regex(/href=.*?SoulseekQt[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :big_sur"
