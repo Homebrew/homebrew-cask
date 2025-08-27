e76cfd29c7c45832db75e204ce84f34cb15b5e99
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-27 17:32:56 -0400

    dyad 0.18.0
    `dyad` is autobumped but the workflow failed to update to version
    0.18.0 because the `livecheck` block simply removes any leading `v`
    from the `name` field in the JSON response and incorrectly returns
    "0.18.0 - create your own library of prompts to reuse across apps!"
    as the newest version. This updates the version and reworks the
    `livecheck` block to match the version from the tag in the file URL
    path instead.

--- Casks/d/dyad.rb
+++ Casks/d/dyad.rb
@@ -1,9 +1,9 @@
 cask "dyad" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.17.0"
-  sha256 arm:   "42389c3993f0591d5f36a08d7863d59cb91c8089e45986a7a286208e00fc0d8d",
-         intel: "9a6f6e8a1c53084241f89acfce760ecd4b1db3351f25a6eecdf27488aee0b3ff"
+  version "0.18.0"
+  sha256 arm:   "5bd7d5a6d563f8a59f1035fd55d0640d8887f42e3508a7bc5b89a7564fda23d8",
+         intel: "36ac0c0bceffa172cc47839813d078c7a7c357c62fca8bb782ecc51d6d6133bc"
 
   url "https://github.com/dyad-sh/dyad/releases/download/v#{version}/dyad-darwin-#{arch}-#{version}.zip",
       verified: "github.com/dyad-sh/dyad/"
@@ -13,8 +13,9 @@ cask "dyad" do
 
   livecheck do
     url "https://api.dyad.sh/v1/update/stable/dyad-sh/dyad/darwin-#{arch}/0.0.0"
+    regex(%r{/v?(\d+(?:\.\d+)+)/}i)
     strategy :json do |json|
-      json["name"]&.tr("v", "")
+      json["url"]&.[](regex, 1)
     end
   end
 
