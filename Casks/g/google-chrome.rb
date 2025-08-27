704245c1cca4a7de213402df8b5bf7b039a38943
buaazyl <dev@zhangyl.com>
2025-08-27 08:38:09 +0800

    google-chrome 139.0.7258.155

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "139.0.7258.139"
+  version "139.0.7258.155"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
