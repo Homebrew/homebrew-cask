ff4117a77f49adf0577c7e709da739eeaa43afa8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-28 19:34:45 +0000

    microsoft-edge 139.0.3405.125

--- Casks/m/microsoft-edge.rb
+++ Casks/m/microsoft-edge.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge" do
-  version "139.0.3405.119,4411985f-e977-4af3-bd60-a2e52dc0af91"
-  sha256 "154baf94e7b8512782ec76efa448819446f0a90c55c34a362a3e5f53c4942c0e"
+  version "139.0.3405.125,54100b99-6d2e-42e1-bc8e-35e863acb814"
+  sha256 "a08943dcc87be93f76a9bd526e58453bb5ccfa2037e959096e813f1b237ba85b"
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdge-#{version.csv.first}.dmg"
   name "Microsoft Edge"
