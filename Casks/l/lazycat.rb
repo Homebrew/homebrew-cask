f10cf04ea29de7821ff723fda1cf26e788d04658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-29 10:22:07 +0000

    lazycat 1.4.4

--- Casks/l/lazycat.rb
+++ Casks/l/lazycat.rb
@@ -1,9 +1,9 @@
 cask "lazycat" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.3"
-  sha256 arm:   "4f2587909f6503b8aa40690562ed639b986317315e1598ce69f9b7cbaaac6089",
-         intel: "c45ed1c3c8f26e72cf4b1aaae3486cc35a1af9b45d1cf004ad3c13a22284235d"
+  version "1.4.4"
+  sha256 arm:   "a1d76660d1705af4ae8edf3582ce9ae2d1bbd923aab3dd84943d474e4600b3c0",
+         intel: "abcc3e94f6bfcd22ac1d6dbf85717b046f667e629980f8b2a5f2ec5b8aa0ac8f"
 
   url "https://dl.lazycat.cloud/client/desktop/stable/lzc-client-desktop_v#{version}_#{arch}.dmg"
   name "LazyCat"
