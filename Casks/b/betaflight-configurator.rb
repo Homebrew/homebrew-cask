1838371977f1bfb284d964a89512cd0340c39700
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-08-30 11:47:30 -0400

    betaflight-configurator: disable due to failing Gatekeeper check

--- Casks/b/betaflight-configurator.rb
+++ Casks/b/betaflight-configurator.rb
@@ -12,6 +12,8 @@ cask "betaflight-configurator" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on macos: ">= :high_sierra"
 
   app "Betaflight Configurator.app"
