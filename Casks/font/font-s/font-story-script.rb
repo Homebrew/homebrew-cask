c9a7ddbc404aa8e6d6e809d4f19700f228c8d511
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-31 00:16:57 +0000

    font-story-script: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-story-script.rb
+++ Casks/font/font-s/font-story-script.rb
@@ -2,9 +2,10 @@ cask "font-story-script" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/storyscript/StoryScript-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/storyscript/StoryScript-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Story Script"
-  homepage "https://github.com/lroulh/story-script"
+  homepage "https://fonts.google.com/specimen/Story+Script"
 
   font "StoryScript-Regular.ttf"
 
