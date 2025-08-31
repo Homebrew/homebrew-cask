a3f3f741406ecc500b63340a7c779554b6996022
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-31 07:20:42 +0000

    portfolioperformance 0.79.1

--- Casks/p/portfolioperformance.rb
+++ Casks/p/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.79.0"
-  sha256 arm:   "7490ddd46fc04daae1b6e80c655c69dd2973953e62882cf196e1e2eea85e58e4",
-         intel: "768d96435c99d51e1566a1fc25a040d82b679be29f945413e75333e1c61d9922"
+  version "0.79.1"
+  sha256 arm:   "2d37ae15e19797d5751216f30cd745bc86ea20c1fc4b32b3fd6a3724f6fbc6ec",
+         intel: "c21105237f4e79cf9880263cd42f667ef68682f8bb01a446af719e0cf26eb498"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
