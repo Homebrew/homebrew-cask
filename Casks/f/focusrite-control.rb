c7ec864509f20ff46fc8118e737541f1092c8376
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-09-01 22:33:43 +0200

    focusrite-control: 3.26.0 and fix livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/f/focusrite-control.rb
+++ Casks/f/focusrite-control.rb
@@ -1,15 +1,21 @@
 cask "focusrite-control" do
-  version "3.25.0"
-  sha256 "f890b6ed8ec32611104b8ea65e5b1ce360175aa7f9083fd7d085034674b95984"
+  version "3.26.0"
+  sha256 "8dbe0f82b9335f74b8276dfaaa1e76c02e7ae837457d4509d8ff4571f4658f4a"
 
-  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_#{version}.dmg"
+  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_#{version.dots_to_underscores}.dmg"
   name "Focusrite Control"
   desc "Focusrite interface controller"
   homepage "https://focusrite.com/en"
 
   livecheck do
     url "https://downloads.focusrite.com/focusrite/scarlett-3rd-gen/scarlett-solo-3rd-gen"
-    regex(%r{href=.*?/focusrite[._-]control[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(/href=.*?focusrite[._-]control[._-]v?(\d+(?:[._]\d+)+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      match[1].tr("_", ".")
+    end
   end
 
   pkg "Focusrite Control.pkg"
