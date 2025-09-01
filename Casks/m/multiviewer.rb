34e57e901cdef8fff35c54b5a70e62a19275fd82
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-31 22:41:42 -0400

    multiviewer 2.0.2
    `multiviewer-for-f1` is autobumped but the workflow is unable to
    update to version 2.0.2 because the app has been rebranded to simply
    MultiViewer. This renames and modifies the cask as part of updating
    it to 2.0.2.

--- Casks/m/multiviewer.rb
+++ Casks/m/multiviewer.rb
@@ -1,24 +1,23 @@
-cask "multiviewer-for-f1" do
-  # NOTE: "1" is not a version number, but an intrinsic part of the product name
+cask "multiviewer" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "1.43.2,243288028"
-    sha256 "bf1b7209015b6cc071c9754a36bfc604455874edfb065187e3d7ac39c165844d"
+    version "2.0.2,287987261"
+    sha256 "074a16e626f67b6018bd06a3674e651b95781bf5e5e59886e933e133dccf4bb0"
   end
   on_intel do
-    version "1.43.2,243289372"
-    sha256 "134cbf24967956ae0781f8ca3f9c7ef92af1ca4fb5f14265fa78962e7a56e4ff"
+    version "2.0.2,287988055"
+    sha256 "d1cd6ad1584ce890a7343fe503570ad144a7dd0151b642efbce1c4db03880f7f"
   end
 
   url "https://releases.multiviewer.app/download/#{version.csv.second}/MultiViewer.for.F1-#{version.csv.first}-#{arch}.dmg"
-  name "MultiViewer for F1"
-  desc "Unofficial desktop client for F1TV"
+  name "MultiViewer"
+  desc "Unofficial desktop client for F1 TV"
   homepage "https://multiviewer.app/"
 
   livecheck do
     url "https://api.multiviewer.dev/api/v1/releases/latest"
-    regex(%r{/([^/]+?)/MultiViewer[._-]for[._-]F1[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
+    regex(%r{/([^/]+?)/MultiViewer[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
     strategy :json do |json, regex|
       json["downloads"]&.flat_map do |item|
         match = item["url"]&.match(regex)
@@ -32,10 +31,10 @@ cask "multiviewer-for-f1" do
   auto_updates true
   depends_on macos: ">= :big_sur"
 
-  app "MultiViewer for F1.app"
+  app "MultiViewer.app"
 
   zap trash: [
-    "~/Library/Application Support/MultiViewer for F1",
+    "~/Library/Application Support/MultiViewer",
     "~/Library/Caches/com.electron.multiviewer-for-f1",
     "~/Library/Caches/com.electron.multiviewer-for-f1.ShipIt",
     "~/Library/HTTPStorages/com.electron.multiviewer-for-f1",
