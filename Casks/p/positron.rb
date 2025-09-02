150aa37d25785a6dec197532810c6abf50f18f66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 16:35:12 +0000

    positron 2025.09.0-139

--- Casks/p/positron.rb
+++ Casks/p/positron.rb
@@ -1,9 +1,9 @@
 cask "positron" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.08.0-130"
-  sha256 arm:   "99fa687e500420b1986d23a0ea5efb09c9ac2a684f701584e1ad24ac1adea6f5",
-         intel: "04b2bb33c99465755d50698e3bcaf9bed650b20e33dbacc6d4a56ccd3aac217a"
+  version "2025.09.0-139"
+  sha256 arm:   "8d64a4d26d671f55fdd86bca99f41430d2dd0d8a67f7d2324b3cfab583f6bc5e",
+         intel: "8950c4ab0393e1d47b6e5b9c05c6611fcb457fdfa2424710062fd2bd351af710"
 
   url "https://cdn.posit.co/positron/releases/mac/#{arch}/Positron-darwin-#{version}-#{arch}.zip"
   name "Positron"
