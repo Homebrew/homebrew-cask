4c6209f90d6c9ab2051c64d22f94bb4d50608359
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 07:44:58 +0000

    wondershare-uniconverter 16.7.1

--- Casks/w/wondershare-uniconverter.rb
+++ Casks/w/wondershare-uniconverter.rb
@@ -1,9 +1,9 @@
 cask "wondershare-uniconverter" do
   arch arm: "arm_"
 
-  version "16.7.0"
-  sha256 arm:   "118af8ec998cf0aa210c343aac4359032fadf8f439062d8c1781753183e7eb86",
-         intel: "4595dfe5806240ebb575b141a20fd2b9bc9e3120036d838b3ff8a19e33f59316"
+  version "16.7.1"
+  sha256 arm:   "3ac1f777b76bb75ee72a5f4a1c9a704932f2a0f0099904c6700e742acfeaf233",
+         intel: "7f4cc5e5380589715eed84eadba7c9202a8196ac0c48321a3a3839a688425acb"
 
   url "https://download.wondershare.com/cbs_down/uniconverter#{version.major}-mac_#{arch}#{version}_full14207.zip"
   name "UniConverter"
