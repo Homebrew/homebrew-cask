388c6a699883910beca1d07ef05108f094c2ec14
Vijay A <vraravam@users.noreply.github.com>
2025-09-02 08:24:30 +0530

    Fix zen@twilight cask recognizing a new build based on 'buildID'

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -1,5 +1,5 @@
 cask "zen@twilight" do
-  version "1.15t"
+  version "1.15t,20250901110611"
   sha256 :no_check
 
   url "https://github.com/zen-browser/desktop/releases/download/twilight/zen.macos-universal.dmg",
@@ -11,7 +11,7 @@ cask "zen@twilight" do
   livecheck do
     url "https://updates.zen-browser.app/updates/browser/Darwin_aarch64-gcc3/twilight/update.xml"
     strategy :xml do |xml|
-      xml.get_elements("//update").map { |item| item.attributes["appVersion"] }
+      xml.get_elements("//update").map { |item| "#{item.attributes["appVersion"]},#{item.attributes["buildID"]}" }
     end
   end
 
