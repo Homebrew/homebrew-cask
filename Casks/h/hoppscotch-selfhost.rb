89d36a0ff9d45bcc18025041015e86bed9c17931
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 13:06:44 +0000

    hoppscotch-selfhost 25.8.1-0

--- Casks/h/hoppscotch-selfhost.rb
+++ Casks/h/hoppscotch-selfhost.rb
@@ -1,9 +1,9 @@
 cask "hoppscotch-selfhost" do
   arch arm: "aarch64", intel: "x64"
 
-  version "25.8.0-0"
-  sha256 arm:   "84a4a1e0b07b10b5b5d63f5f1ad9cb7aa634ace51d73c7713c87d2dbb6954bb7",
-         intel: "7a48e5a5f62fc6fb39f892e8982bb6522a5721d3dcfbede121ae330d61800445"
+  version "25.8.1-0"
+  sha256 arm:   "14187974ac71580c6f7f1846031fa59ba4af771a8d21307427512fc68a8c78c9",
+         intel: "26bb0e29492adeae997bfa99b23ef72f692e675c2baddd8772d68510c5c7447c"
 
   url "https://github.com/hoppscotch/releases/releases/download/v#{version}/Hoppscotch_SelfHost_mac_#{arch}.dmg",
       verified: "github.com/hoppscotch/releases/"
