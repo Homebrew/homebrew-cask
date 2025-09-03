015075a0dbab7af21feb94162e6fcae114828051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-03 14:13:56 +0000

    wiso-steuer-2023 30.16.5130-RC1

--- Casks/w/wiso-steuer-2023.rb
+++ Casks/w/wiso-steuer-2023.rb
@@ -1,7 +1,7 @@
 cask "wiso-steuer-2023" do
   # NOTE: "2023" is not a version number, but an intrinsic part of the product name
-  version "30.15.4880-RC1"
-  sha256 "8bfc8e151f097d8b0a20c004db762c1f3f54fc73f0252abcaf04a8bb7647378f"
+  version "30.16.5130-RC1"
+  sha256 "c9dce5d0098d36d797d91b97b3d8b4cf062c9e1cc934d2a3c8defd0713b46feb"
 
   url "https://update.buhl-data.com/Updates/Steuer/2023/Mac/Files/#{version}/SteuerMac2023-#{version.split("-").first}.dmg",
       verified: "update.buhl-data.com/Updates/Steuer/"
