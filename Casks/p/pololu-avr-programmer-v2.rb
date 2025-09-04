abbcf3a2e90bdb40aeacd48551490a2ba2dfd664
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-09-03 20:56:21 -0400

    pololu-avr-programmer-v2: disable due to failing Gatekeeper check

--- Casks/p/pololu-avr-programmer-v2.rb
+++ Casks/p/pololu-avr-programmer-v2.rb
@@ -15,6 +15,8 @@ cask "pololu-avr-programmer-v2" do
     end
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on macos: ">= :el_capitan"
 
   pkg "pololu-usb-avr-programmer-v2-#{version.csv.second}-macos.pkg"
