f175b845459e4644704fd397df758e07888fb184
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 15:46:05 +0000

    dbvisualizer 25.2.3

--- Casks/d/dbvisualizer.rb
+++ Casks/d/dbvisualizer.rb
@@ -1,9 +1,9 @@
 cask "dbvisualizer" do
   arch arm: "aarch64", intel: "x64"
 
-  version "25.2.2"
-  sha256 arm:   "cc09532f662808ec1bf6ec7010015048d954dacf5745eee87473d6c3716ef4d7",
-         intel: "563a100d9ec3d5db359df347687443d33fc27bc3ed5a08045af4f0cc12317be9"
+  version "25.2.3"
+  sha256 arm:   "2db28085927892f2fb9349a0b8d99d19d53288a393def7ea029d319ad3cc627a",
+         intel: "53e806339a1531a05f08193e531ba02d5087a985547a1b07cd26b2038e8368d1"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos-#{arch}_#{version.dots_to_underscores}.dmg"
   name "DbVisualizer"
