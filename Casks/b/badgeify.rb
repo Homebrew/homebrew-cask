eeeb1165fb82d672c8d28abcb889d34591d03ddf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 03:42:32 +0000

    badgeify 1.9.0

--- Casks/b/badgeify.rb
+++ Casks/b/badgeify.rb
@@ -1,9 +1,9 @@
 cask "badgeify" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.8.9"
-  sha256 arm:   "fe7fc6f21e75d430272788f70f39133b7910e2e2126a15122471e66c76ba05ef",
-         intel: "04fe1b2566459bafbea8a47fe61be9dead41fa090cb84cdad9e1b6da40e454da"
+  version "1.9.0"
+  sha256 arm:   "b79701dba98786ab64632bcff3324833cee51e25ba9abdfe00c602b861e2799e",
+         intel: "d98b51581b0322d612de08a9bb9a5672f6fbfc36780baf71c5851a10e4dfdb7c"
 
   url "https://api.badgeify.app/release/download/darwin/#{arch}/#{version}"
   name "Badgeify"
