2afe46a926346b26d40a9369e372b95c57b4b33c
bevanjkay <email@bevankay.me>
2025-09-06 18:40:36 +0800

    visual-paradigm 17.3,20250901

--- Casks/v/visual-paradigm.rb
+++ Casks/v/visual-paradigm.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.3,20250824"
-  sha256 arm:   "a47df65a6dc21d03897b7fb71d19303c6c2e7e6cb26fa573ae437dba21168f5c",
-         intel: "7764a29f9bd2ab8b345093f949e5cbb698e4ae56e86257b2356840b5a477efbc"
+  version "17.3,20250901"
+  sha256 arm:   "981d17045498843173a6d0936af7286bba27e2b49f74b442c7d1c1b73c8310e9",
+         intel: "57f2b0af81a264bb6ccaf4a2e24f6b15d275df883d3ab538d173f097aa117717"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm"
@@ -20,8 +20,6 @@ cask "visual-paradigm" do
       "#{match[1]},#{match[2]}"
     end
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "Visual Paradigm.app"
 
