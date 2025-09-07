d488f7b275feaea1dddbeda06b5696f66b76f7ba
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-09-07 11:11:43 -0400

    readyapi 3.62.0

--- Casks/r/readyapi.rb
+++ Casks/r/readyapi.rb
@@ -1,15 +1,15 @@
 cask "readyapi" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.61.0"
-  sha256 arm:   "0e7c79c0921d0d6d926afb81fb66dfea305d2f734d11369a8af65d236b651250",
-         intel: "f2acc6eb9dad152d8ed5e9fb4ae227b8ad96c9fff47117e2d294b0b1be133af5"
+  version "3.62.0"
+  sha256 arm:   "66c73191a5353db667f790f62d2f2a0cbb20343268b59cb3165f0c61dda8c07b",
+         intel: "6d795a18c79e7c1ccb3dd4b4b660f80ac47ad81a39675157118ec3d5082d0f2d"
 
   url "https://dl.eviware.com/ready-api/#{version}/ReadyAPI-#{arch}-#{version}.dmg",
       verified: "dl.eviware.com/ready-api/"
   name "ReadyAPI Desktop"
   desc "Automated API testing platform"
-  homepage "https://smartbear.com/product/ready-api/overview/"
+  homepage "https://smartbear.com/product/ready-api/"
 
   livecheck do
     url "https://support.smartbear.com/readyapi/docs/en/what-s-new/version-history.html"
