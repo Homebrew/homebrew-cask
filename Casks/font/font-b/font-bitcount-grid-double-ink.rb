0f7cfa9e7195e4ee43fac042cd7b084a6146cdcc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-07 00:16:07 +0000

    font-bitcount-grid-double-ink: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-b/font-bitcount-grid-double-ink.rb
@@ -0,0 +1,12 @@
+cask "font-bitcount-grid-double-ink" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bitcountgriddoubleink/BitcountGridDoubleInk%5BCRSV%2CELSH%2CELXP%2CSZP1%2CSZP2%2CXPN1%2CXPN2%2CYPN1%2CYPN2%2Cslnt%2Cwght%5D.ttf"
+  name "Bitcount Grid Double Ink"
+  homepage "https://github.com/petrvanblokland/TYPETR-Bitcount"
+
+  font "BitcountGridDoubleInk[CRSV,ELSH,ELXP,SZP1,SZP2,XPN1,XPN2,YPN1,YPN2,slnt,wght].ttf"
+
+  # No zap stanza required
+end
