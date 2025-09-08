362fdd3094276388dae5d935d4d572928c81fb96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 21:42:15 +0000

    dbeaverlite 25.2.0

--- Casks/d/dbeaverlite.rb
+++ Casks/d/dbeaverlite.rb
@@ -1,9 +1,9 @@
 cask "dbeaverlite" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "25.1.0"
-  sha256 arm:   "d81717ff64f619a3f18d50713be4e6467959b385acc5e1a50ffca23c20f4ceac",
-         intel: "83b95cc60ee08c182f174666835b47ce6f2164446a850c2b302b0419c19e8eba"
+  version "25.2.0"
+  sha256 arm:   "cb2f2833abb216cac122c9dd368d1d0002b2dc671a677e02fc940c8ef065258f",
+         intel: "c3a40f1242b06222e65aa7d084ffb553b9ccc75af0f41d0ae9faf270d643d745"
 
   url "https://dbeaver.com/downloads-lite/#{version}/dbeaver-le-#{version}-macos-#{arch}.dmg"
   name "DBeaver Lite Edition"
