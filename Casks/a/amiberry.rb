d16a150001b9829c0b33a98694043418568c4781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 18:44:10 +0000

    amiberry 7.1.1

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -1,9 +1,9 @@
 cask "amiberry" do
   arch arm: "apple-silicon", intel: "x86_64"
 
-  version "7.1.0"
-  sha256 arm:   "3b3d5ce513e922fc4860d2aaffc44edb14be4578874d4455209f3f5b92ff226e",
-         intel: "d1e389ea6e021007135b28ce6c6ebcac0ab43fc0add6455318c4d4abd7bb0953"
+  version "7.1.1"
+  sha256 arm:   "6df092a58bc78f06409b0de6d3aaa3bddd9577b0df8554443e8a2117369c2e67",
+         intel: "6e12ae55a2d791aa009a384c707142b5d3a5da2e6df9289a69fa5aafa927ce0d"
 
   url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-v#{version}-macOS-#{arch}.zip",
       verified: "github.com/BlitterStudio/amiberry/"
