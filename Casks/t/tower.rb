35cfd54f11a5e643062102498eaba7f290321731
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-09 19:44:43 +0000

    tower 14.4

--- Casks/t/tower.rb
+++ Casks/t/tower.rb
@@ -1,6 +1,6 @@
 cask "tower" do
-  version "14.3,492,2270568e"
-  sha256 "d33af6bd75f96625c071554f0ff3ce913d8223f580821b89b4ae9a2532db86ce"
+  version "14.4,495,bcbf3019"
+  sha256 "669aa0a5ec2c532f6f31acc034229e92608a6942f2b1051a8fdce18fe239d750"
 
   url "https://www.git-tower.com/apps/tower3-mac/#{version.csv.second}-#{version.csv.third}/Tower-#{version.csv.first}-#{version.csv.second}.zip"
   name "Tower"
