8722ac940b63ddee828adef0d1c73a84ddd459c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 15:54:32 +0000

    eclipse-installer 4.37.0

--- Casks/e/eclipse-installer.rb
+++ Casks/e/eclipse-installer.rb
@@ -1,9 +1,9 @@
 cask "eclipse-installer" do
   arch arm: "mac-aarch64", intel: "mac64"
 
-  version "4.36.0,2025-06"
-  sha256 arm:   "ab63c4bb8815c18d7b0be455ba7093deb0dfeff0263af8f2f668cb550f3e1493",
-         intel: "c963ae263b2af3fb3920fe781d4996493db9b2fb9f779cc720818df041522376"
+  version "4.37.0,2025-09"
+  sha256 arm:   "d5f05a764d7c4e36bf530e24606bb9e447843b88b8221f1974f225618569e924",
+         intel: "41b72561dd058a9afb670fe682954eba9eefee8e5bde6f4f5a527ab2c504181e"
 
   url "https://www.eclipse.org/downloads/download.php?file=/oomph/epp/#{version.csv.second}/R/eclipse-inst-jre-#{arch}.dmg&r=1"
   name "Eclipse Installer"
