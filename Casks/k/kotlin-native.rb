41414883bd8b2faa37faea489b622de6c087f087
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 10:08:31 +0000

    kotlin-native 2.2.20
    There is no `jsinterop` binary in the 2.2.20 release.
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/k/kotlin-native.rb
+++ Casks/k/kotlin-native.rb
@@ -1,9 +1,9 @@
 cask "kotlin-native" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.2.10"
-  sha256 arm:   "952a056d5bed20c7a36d2621ac344d76fb8509d1fc1cbad7d3e625f69cf58581",
-         intel: "1191534b32d488325661e53d626cf336a1fe4ddc834d972d4640b874a41614aa"
+  version "2.2.20"
+  sha256 arm:   "5270e5f708ffed15eb11a029b8068b73321fcf496c7103dffa909e49d27125e6",
+         intel: "9a6b0142bc74c8aaafbe19c3f0253ea31aa15ac393d51cefcd29e237709e1bb8"
 
   url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-prebuilt-macos-#{arch}-#{version}.tar.gz",
       verified: "github.com/JetBrains/kotlin/"
@@ -18,7 +18,6 @@ cask "kotlin-native" do
 
   binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/cinterop"
   binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/generate-platform"
-  binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/jsinterop"
   binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/klib"
   binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/konan-lldb"
   binary "kotlin-native-prebuilt-macos-#{arch}-#{version}/bin/konanc"
