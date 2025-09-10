9650bdc8bf07c36c5fccf3ab1c0363df755b8a77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 15:55:41 +0000

    eclipse-java 4.37.0

--- Casks/e/eclipse-java.rb
+++ Casks/e/eclipse-java.rb
@@ -1,9 +1,9 @@
 cask "eclipse-java" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.36.0,2025-06"
-  sha256 arm:   "104aba52f8f4702ffdd46637339b05d2bfe5d14a81446eb515395a4b6f2a8d78",
-         intel: "4f6a2241f4b5d6d3cd93c29dd92a671510980f79e7de894c2b95a0a1eaad7ba5"
+  version "4.37.0,2025-09"
+  sha256 arm:   "3e99fd5718887a5ea5c26003d14207331027b8dda284db02a5e5fd8e7bb88b9e",
+         intel: "c58d7dd1a1f8f71a9eae46e5e506a90d3edb8496ce700d2fc30ac20995f5e255"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java Developers"
