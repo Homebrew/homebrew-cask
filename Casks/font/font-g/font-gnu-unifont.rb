7867333d0cc30165faaccf02549d3d4ef44c2227
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 02:38:57 +0000

    font-gnu-unifont 17.0.01

--- Casks/font/font-g/font-gnu-unifont.rb
+++ Casks/font/font-g/font-gnu-unifont.rb
@@ -1,6 +1,6 @@
 cask "font-gnu-unifont" do
-  version "16.0.04"
-  sha256 "2bd4e4679757126f48e1bf2c1be40b09aa92162bfedda4683ce5fbc70a2a5972"
+  version "17.0.01"
+  sha256 "234af86e7a0e851f020900db5ad0afc32691c248db362df9b2914cf50c42d940"
 
   url "https://ftpmirror.gnu.org/gnu/unifont/unifont-#{version}/unifont-#{version}.tar.gz",
       verified: "ftpmirror.gnu.org/gnu/unifont/"
