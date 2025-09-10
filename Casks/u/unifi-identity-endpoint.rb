80d9f5d3a6c620d69e5153ba9a4d73dcac8a6731
Nils Streedain <25465133+nilsstreedain@users.noreply.github.com>
2025-09-10 11:46:23 -0700

    Add UniFi Identity Endpoint cask

---
+++ Casks/u/unifi-identity-endpoint.rb
@@ -0,0 +1,33 @@
+cask "unifi-identity-endpoint" do
+  version "3.3.1,16645096-85aa-42af-9593-2c13f3280dda,a73c"
+  sha256 "4e6476bb27808f38d1a3769d49b5c0862cec8a8d9e0c0b0ab74913804643b675"
+
+  url "https://fw-download.ubnt.com/data/uid-identity-standard-desktop-app/#{version.csv.third}-macOS-#{version.csv.first}-#{version.csv.second}.pkg",
+      verified: "fw-download.ubnt.com/data/uid-identity-standard-desktop-app/"
+  name "UniFi Identity Endpoint"
+  desc "License free Wi-Fi, VPN, and Access Application for Orgnizations"
+  homepage "https://www.ui.com/identity"
+
+  livecheck do
+    url "https://download.uid.ui.com/?app=DESKTOP-IDENTITY-STANDARD-MACOS"
+    regex(/(\w+)[._-]macOS[._-](\d+(?:\.\d+)+)[._-](\h{8}-\h{4}-\h{4}-\h{4}-\h{12})/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[2]},#{match[3]},#{match[1]}"
+    end
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  pkg "#{version.csv.third}-macOS-#{version.csv.first}-#{version.csv.second}.pkg"
+
+  uninstall launchctl: [
+              "application.com.ui.uid.standard-desktop.93942643.93942648",
+              "com.ui.uid.standard-desktop.startup",
+            ],
+            quit:      "com.ui.uid.standard-desktop",
+            pkgutil:   "com.ui.uid.standard-desktop",
+            delete:    "/Applications/Identity.app"
+end
