4ea3f69aa287f28718ec588b74357c45abc85531
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 07:21:27 +0000

    netnewswire@beta 6.1.11b1

--- Casks/n/netnewswire@beta.rb
+++ Casks/n/netnewswire@beta.rb
@@ -1,6 +1,6 @@
 cask "netnewswire@beta" do
-  version "6.1.10"
-  sha256 "276e7a49aee248579c9e6d65e2c5cdded52cf27d0c8a123e2d5930b0241aa90b"
+  version "6.1.11b1"
+  sha256 "3ca4a746520ec2422a2b141654ee09c1a5dfdf5014ae2b6f80788aeb9c9833a8"
 
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
       verified: "github.com/brentsimmons/NetNewsWire/"
