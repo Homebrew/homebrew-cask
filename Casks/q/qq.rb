7b56f55b601e73c95188fd1a7fe85cde36f12617
Andy_Allan <58987282+andya1lan@users.noreply.github.com>
2025-09-12 02:20:01 +0800

    qq 6.9.75_250626_01

--- Casks/q/qq.rb
+++ Casks/q/qq.rb
@@ -1,6 +1,6 @@
 cask "qq" do
-  version "6.9.80_250904_01"
-  sha256 "5548e059023169b8d7917261c5089026560391b2c334b69541e45a678586388b"
+  version "6.9.75_250626_01"
+  sha256 "b564f447cf2d5f3d32a4f4130f3012ae1e339ef06fabfa21061a5e0efd8f9557"
 
   url "https://dldir1v6.qq.com/qqfile/qq/QQNT/Mac/QQ_#{version}.dmg"
   name "QQ"
