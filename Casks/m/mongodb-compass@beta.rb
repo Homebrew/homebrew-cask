375d56eaaff011ed2e62558a78a4eb61aed10551
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-11 19:29:04 +0000

    mongodb-compass@beta 1.46.10-beta.1

--- Casks/m/mongodb-compass@beta.rb
+++ Casks/m/mongodb-compass@beta.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.46.9-beta.4"
-  sha256 arm:   "282955f1420f1d96afe3d9ed01c389536f2c4c56594aafa74aafe463b1be647e",
-         intel: "d1c6505f994db141b4ba5e4698619b06c23adb43ab32ebc20d0a2a59d984af4c"
+  version "1.46.10-beta.1"
+  sha256 arm:   "7e829154774160d8a5050b4aeefa147b1232a05bd1211596eb2ad8a13c202587",
+         intel: "c111729cb53029acbb0b850e7e3fe86b44e5c7c4a44d33ca6abe7f1f20f40fc7"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass"
