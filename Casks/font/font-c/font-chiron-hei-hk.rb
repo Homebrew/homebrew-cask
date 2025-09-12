08e375e53d553c6fa1bfef053674611889563fd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 12:58:34 +0000

    font-chiron-hei-hk 2.602
    The OTF files used in the cask are now found in a `VAR_OTF` directory
    (instead of `VAR`).
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/font/font-c/font-chiron-hei-hk.rb
+++ Casks/font/font-c/font-chiron-hei-hk.rb
@@ -1,14 +1,14 @@
 cask "font-chiron-hei-hk" do
-  version "2.531"
-  sha256 "b519724d159ce4bb55f7ac69a07b299f7da61b9e9b8d64701147157571eab6fc"
+  version "2.602"
+  sha256 "27d2955e8ce957953b0163a8e27de32ff81c4aac0bcbc66566a4db84650adecc"
 
   url "https://github.com/chiron-fonts/chiron-hei-hk/archive/refs/tags/v#{version}.tar.gz"
   name "Chiron Hei HK"
   name "昭源黑體"
   homepage "https://github.com/chiron-fonts/chiron-hei-hk"
 
-  font "chiron-hei-hk-#{version}/VAR/ChironHeiHKItVF.otf"
-  font "chiron-hei-hk-#{version}/VAR/ChironHeiHKVF.otf"
+  font "chiron-hei-hk-#{version}/VAR_OTF/ChironHeiHKItVF.otf"
+  font "chiron-hei-hk-#{version}/VAR_OTF/ChironHeiHKVF.otf"
 
   # No zap stanza required
 end
