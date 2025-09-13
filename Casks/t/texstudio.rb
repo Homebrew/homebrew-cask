dc74ecc3763e751145c0855d754528339c8ed4f1
tommychinjr <>
2025-09-13 14:32:36 -0400

    texstudio 4.8.9

--- Casks/t/texstudio.rb
+++ Casks/t/texstudio.rb
@@ -1,15 +1,17 @@
 cask "texstudio" do
   arch arm: "-m1"
 
-  version "4.8.8"
-  sha256 arm:   "d60619d3cf4ec8195c7c0c1f890879d72aacaac6149f6f4179c9fab952ca487a",
-         intel: "77e13a73e001c6528e1235429fa23f3858c35ae8b276b0621e09c8503aa3eb5a"
+  version "4.8.9"
+  sha256 arm:   "b39d5e764f491a5f5ed2d39647b46228f2cb455311c2389cd1ff47948f71c6d9",
+         intel: "06091fab6ad8954c7f56486011041444f908c10722e68a6705be6f1f996fffa8"
 
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx#{arch}.zip",
       verified: "github.com/texstudio-org/texstudio/"
   name "TeXstudio"
   desc "LaTeX editor"
   homepage "https://texstudio.org/"
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: ">= :monterey"
 
