280398aca8af3a2ca8e6b9f243f5db3ddfe2432a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-09-13 07:24:06 -0400

    powershell: revert future disable date

--- Casks/p/powershell.rb
+++ Casks/p/powershell.rb
@@ -15,8 +15,6 @@ cask "powershell" do
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   depends_on macos: ">= :mojave"
 
   pkg "powershell-#{version}-osx-#{arch}.pkg"
