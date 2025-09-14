bebc2a9d7d38ac3b525c6a0cd2241ddffbf2ddef
Sam Ford <1584702+samford@users.noreply.github.com>
2025-09-14 09:41:54 -0400

    longplay 1.1.0
    `longplay` is autobumped but the workflow failed to update to version
    1.1.0 because the appcast uses a 1.1 `shortVersionString` while the
    file name uses 1.1.0 and also the file type has changed from zip to
    dmg. This updates the cask and modifies the `livecheck` block to match
    the version from the file name instead.

--- Casks/l/longplay.rb
+++ Casks/l/longplay.rb
@@ -1,8 +1,8 @@
 cask "longplay" do
-  version "1.0.5,485"
-  sha256 "a6d42edb19029209979c5a5942dc5d160fceb8e2b36877bb8373170455f4dbc6"
+  version "1.1.0,490"
+  sha256 "808ecd2e5b7a24ace0ff6bf7790af096d4740ca3f3f540334b472205031303bd"
 
-  url "https://download.longplay.app/mac/longplay-#{version.csv.first}-#{version.csv.second}.zip",
+  url "https://download.longplay.app/mac/longplay-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "download.longplay.app/"
   name "Longplay"
   desc "Album-focused music player"
@@ -10,8 +10,12 @@ cask "longplay" do
 
   livecheck do
     url "https://download.longplay.app/mac/appcast.xml"
+    regex(/longplay[._-]v?(\d+(?:[.-]\d+)+)/i)
     strategy :sparkle do |items|
-      items.find { |item| item.channel.nil? }&.nice_version
+      stable_item = items.find { |item| item.channel.nil? }
+      next unless stable_item
+
+      stable_item.url&.[](regex, 1)&.tr("-", ",")
     end
   end
 
