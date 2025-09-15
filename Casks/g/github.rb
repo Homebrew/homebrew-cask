85003f61245193a3a89bdaaf6e468839623d0d3a
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:27:34 -0700

    github: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/github.rb
+++ Casks/g/github.rb
@@ -2,25 +2,9 @@ cask "github" do
   arch arm: "arm64", intel: "x64"
   platform = on_arch_conditional arm: "darwin-arm64", intel: "darwin"
 
-  on_mojave :or_older do
-    version "3.3.13-1b0804db"
-    sha256 "df85436557e7b3d709cc702b751f180f48655a3241cce6a864e55cf5161d9a7a"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_catalina :or_newer do
-    version "3.5.2-14087268"
-    sha256 arm:   "c44c80c8197ae11a3e6029232a28aaa9f3a1f8c1cafde238681fdae97b944c5e",
-           intel: "26ab82296b66aff53aecc7baa71d423364edbcb98d675059a28260e97e55e444"
-
-    livecheck do
-      url "https://central.github.com/deployments/desktop/desktop/latest/#{platform}"
-      regex(%r{(\d+(?:\.\d+)[^/]*)/GitHubDesktop[._-]#{arch}\.zip}i)
-      strategy :header_match
-    end
-  end
+  version "3.5.2-14087268"
+  sha256 arm:   "c44c80c8197ae11a3e6029232a28aaa9f3a1f8c1cafde238681fdae97b944c5e",
+         intel: "26ab82296b66aff53aecc7baa71d423364edbcb98d675059a28260e97e55e444"
 
   url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop-#{arch}.zip",
       verified: "desktop.githubusercontent.com/"
@@ -28,9 +12,14 @@ cask "github" do
   desc "Desktop client for GitHub repositories"
   homepage "https://desktop.github.com/"
 
+  livecheck do
+    url "https://central.github.com/deployments/desktop/desktop/latest/#{platform}"
+    regex(%r{(\d+(?:\.\d+)[^/]*)/GitHubDesktop[._-]#{arch}\.zip}i)
+    strategy :header_match
+  end
+
   auto_updates true
   conflicts_with cask: "github@beta"
-  depends_on macos: ">= :high_sierra"
 
   app "GitHub Desktop.app"
   binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: "github"
