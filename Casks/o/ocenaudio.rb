a4e7b516ba57861e2d8ac758a8e7a2fb92023aca
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:30:27 -0700

    ocenaudio: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/o/ocenaudio.rb
+++ Casks/o/ocenaudio.rb
@@ -3,12 +3,7 @@ cask "ocenaudio" do
   sha256 :no_check
 
   on_big_sur :or_older do
-    on_high_sierra :or_older do
-      url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg"
-    end
-    on_mojave :or_newer do
-      url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_universal_legacy.dmg"
-    end
+    url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_universal_legacy.dmg"
   end
   on_monterey :or_newer do
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_universal.dmg"
@@ -22,8 +17,6 @@ cask "ocenaudio" do
     url :url
     strategy :header_match
   end
-
-  depends_on macos: ">= :sierra"
 
   app "ocenaudio.app"
 
