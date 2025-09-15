af8ac31adf0a9fbe7fa7eac8375fc063a81215e1
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:29:07 -0700

    logi-options+: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/logi-options+.rb
+++ Casks/l/logi-options+.rb
@@ -41,7 +41,6 @@ cask "logi-options+" do
   homepage "https://www.logitech.com/en-us/software/logi-options-plus.html"
 
   auto_updates true
-  depends_on macos: ">= :catalina"
 
   # The installer path can be inconsistent between versions/systems without notice
   rename "Logi Options+ Installer.app", "logioptionsplus_installer.app"
