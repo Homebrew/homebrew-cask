f3cf06946b1c4d25c09ac7ed6fbeee2ed942519d
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:28:25 -0700

    jedit-omega: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/j/jedit-omega.rb
+++ Casks/j/jedit-omega.rb
@@ -1,16 +1,6 @@
 cask "jedit-omega" do
-  on_el_capitan :or_older do
-    version "1.32"
-    sha256 "251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9"
-  end
-  on_sierra do
-    version "2.48"
-    sha256 "fbcebb742f060e4941d901d2e6b9fcd79e575828cafc38f7808ead048a3451ce"
-  end
-  on_high_sierra :or_newer do
-    version "3.11"
-    sha256 "21ea1aec73ac72577b9100cc6be1b3597cd88c88db624fa498b55b66bcb2a7fb"
-  end
+  version "3.11"
+  sha256 "21ea1aec73ac72577b9100cc6be1b3597cd88c88db624fa498b55b66bcb2a7fb"
 
   url "https://artman21.site/JeditOmega#{version.no_dots}.pkg",
       verified: "artman21.site/"
