81ae601a553c263c1d38b1fd964e75989e5804d8
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:25:09 -0700

    brewlet: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/b/brewlet.rb
+++ Casks/b/brewlet.rb
@@ -14,8 +14,6 @@ cask "brewlet" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :mojave"
-
   app "Brewlet.app"
 
   zap trash: "~/Library/Preferences/zzada.Brewlet.plist"
