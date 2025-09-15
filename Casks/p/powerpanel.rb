44162d315da005add5ece62d3bef39a157b52d7a
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:31:15 -0700

    powerpanel: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/powerpanel.rb
+++ Casks/p/powerpanel.rb
@@ -13,8 +13,6 @@ cask "powerpanel" do
     regex(/href=.*?PPP[._-]Mac[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
   end
 
-  depends_on macos: ">= :mojave"
-
   pkg "PPP_Mac_v#{version}.pkg"
 
   uninstall launchctl: [
