23cb04b4df518be752fd4e2fcfea3827d1c48ef5
bevanjkay <email@bevankay.me>
2025-09-15 21:03:47 +1000

    omnifocus 4.8.2

--- Casks/o/omnifocus.rb
+++ Casks/o/omnifocus.rb
@@ -54,14 +54,17 @@ cask "omnifocus" do
     end
   end
   on_sonoma :or_newer do
-    version "4.8.1"
-    sha256 "2928cc3fb897145dbda76eb7ad86726161323b2796d94408f2833126f9063543"
+    version "4.8.2,185.0.16"
+    sha256 "ba8236aa5b75ff56e881b6c34b073409b086e2565f18e24a3bfceddee5e813e9"
 
-    url "https://downloads.omnigroup.com/software/macOS/14/OmniFocus-#{version}.dmg"
+    url "https://downloads.omnigroup.com/software/macOS/14/OmniFocus-#{version.csv.first}-v#{version.csv.second}.dmg"
 
     livecheck do
       url "https://www.omnigroup.com/download/latest/omnifocus/"
-      strategy :header_match
+      regex(/OmniFocus[._-]v?(\d+(?:\.\d+)+)[._-]v?(\d+(?:\.\d+)+)\.dmg/)
+      strategy :header_match do |headers, regex|
+        headers["location"]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+      end
     end
   end
 
