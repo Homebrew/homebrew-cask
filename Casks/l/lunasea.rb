056963cb005169f0f15a61820831d293245f1acd
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:29:12 -0700

    lunasea: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/lunasea.rb
+++ Casks/l/lunasea.rb
@@ -1,12 +1,6 @@
 cask "lunasea" do
-  on_mojave :or_older do
-    version "10.2.2"
-    sha256 "61ef622b70c31550ec6f700eba1e938c23fb97717344e8876d56e2d856a51be4"
-  end
-  on_catalina :or_newer do
-    version "11.0.0"
-    sha256 "fa4ecb5bdf57d6f1326e356e248232040f1b2d0d409ea93ac96dc560eded980c"
-  end
+  version "11.0.0"
+  sha256 "fa4ecb5bdf57d6f1326e356e248232040f1b2d0d409ea93ac96dc560eded980c"
 
   url "https://github.com/JagandeepBrar/LunaSea/releases/download/v#{version}/lunasea-macos-amd64.zip",
       verified: "github.com/JagandeepBrar/LunaSea/"
