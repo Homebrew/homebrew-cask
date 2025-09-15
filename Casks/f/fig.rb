d79d521338fe9ad6a268a21699da986f2b8d41b2
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:27:00 -0700

    fig: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/fig.rb
+++ Casks/f/fig.rb
@@ -11,7 +11,6 @@ cask "fig" do
   disable! date: "2025-08-03", because: :discontinued
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
 
   app "Fig.app"
   binary "#{appdir}/Fig.app/Contents/MacOS/fig-darwin-universal", target: "fig"
