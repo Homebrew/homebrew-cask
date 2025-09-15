b633cdda927e6b71c08b9c0b7a3311c968f71e59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 23:33:04 +0000

    rotki 1.40.1

--- Casks/r/rotki.rb
+++ Casks/r/rotki.rb
@@ -1,9 +1,9 @@
 cask "rotki" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.40.0"
-  sha256 arm:   "92d596aeb4d0cd79d22362f726a20fec1d95c4d2fe22cbb17dde61d8877fa4b2",
-         intel: "77a6f8252d7c151eee8fb3c9e355ec9e46561ed86f7471d67054d222d0343088"
+  version "1.40.1"
+  sha256 arm:   "5efe7a946b35c1729c31086c969af95fb0b5b531a4e0d2b6eb1db9549eb6d16e",
+         intel: "a6fa77896855e59b9707dd9db980f857b4b830c93bfce8aaffb02d8dfb24c903"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_#{arch}-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
