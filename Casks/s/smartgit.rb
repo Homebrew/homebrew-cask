a4ff0733902222ead69c028f87137ac47e15de23
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:32:26 -0700

    smartgit: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/smartgit.rb
+++ Casks/s/smartgit.rb
@@ -1,26 +1,11 @@
 cask "smartgit" do
   arch arm: "aarch64", intel: "x86_64"
 
-  on_sierra :or_older do
-    version "20.2.6"
-    sha256 "af5fbf8db26edde3d996d99c6e82287332598359fe63ff2cd97c712a1685a2ea"
+  version "24.1.4"
+  sha256 arm:   "330274ba9923d64298cad928026c3db69fe3a899dac5e8cb995ccbe6644c48c0",
+         intel: "b44e5450edc9559fc5e5e7b7874318dbdfc682fdc80e4ae410ff8665dc998487"
 
-    url "https://www.syntevo.com/downloads/smartgit/archive/smartgit-macosx-#{version.dots_to_underscores}.dmg"
-  end
-  on_high_sierra do
-    version "22.1.8"
-    sha256 "d98688ee937bbc02c17e08f7d300c6f3e237cb638b64c46944f46895812ed679"
-
-    url "https://www.syntevo.com/downloads/smartgit/archive/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
-  end
-  on_mojave :or_newer do
-    version "24.1.4"
-    sha256 arm:   "330274ba9923d64298cad928026c3db69fe3a899dac5e8cb995ccbe6644c48c0",
-           intel: "b44e5450edc9559fc5e5e7b7874318dbdfc682fdc80e4ae410ff8665dc998487"
-
-    url "https://www.syntevo.com/downloads/smartgit/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
-  end
-
+  url "https://www.syntevo.com/downloads/smartgit/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
   name "SmartGit"
   desc "Git client"
   homepage "https://www.syntevo.com/smartgit/"
