7b551b096893908f77012c26bee335a2b6ef2a91
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:27:44 -0700

    grandperspective: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/grandperspective.rb
+++ Casks/g/grandperspective.rb
@@ -1,17 +1,7 @@
 cask "grandperspective" do
   on_catalina :or_older do
-    on_high_sierra :or_older do
-      version "3.0.1"
-      sha256 "64faab94df5ac39abbeb9e869a6c429d3441c3796ef67f79dab232ba7f0cb222"
-    end
-    on_mojave do
-      version "3.3"
-      sha256 "2e4a0f3b12be447cfdb1496c0292a57631acd7b24f568cb7d7c9d992458e90cf"
-    end
-    on_catalina do
-      version "3.3"
-      sha256 "2e4a0f3b12be447cfdb1496c0292a57631acd7b24f568cb7d7c9d992458e90cf"
-    end
+    version "3.3"
+    sha256 "2e4a0f3b12be447cfdb1496c0292a57631acd7b24f568cb7d7c9d992458e90cf"
 
     livecheck do
       skip "Legacy version"
@@ -26,8 +16,6 @@ cask "grandperspective" do
   name "GrandPerspective"
   desc "Graphically shows disk usage within a file system"
   homepage "https://grandperspectiv.sourceforge.net/"
-
-  depends_on macos: ">= :el_capitan"
 
   app "GrandPerspective.app"
 
