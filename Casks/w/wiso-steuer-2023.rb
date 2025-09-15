6fff16244496cdfd2dc6cc24a2640b24e95369ee
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:34:09 -0700

    wiso-steuer-2023: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/w/wiso-steuer-2023.rb
+++ Casks/w/wiso-steuer-2023.rb
@@ -21,7 +21,6 @@ cask "wiso-steuer-2023" do
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
 
   # Renamed for consistency: app name differs in Finder to shell
   app "SteuerMac 2023.app", target: "WISO Steuer 2023.app"
