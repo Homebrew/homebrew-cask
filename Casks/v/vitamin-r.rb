9fa5f8b57d67f8c2729d95bd1eedaed8b5b7138a
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:33:50 -0700

    vitamin-r: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/v/vitamin-r.rb
+++ Casks/v/vitamin-r.rb
@@ -1,12 +1,6 @@
 cask "vitamin-r" do
-  on_high_sierra :or_older do
-    version "3.31"
-    sha256 "6c5ce3926060b7e3616527fc3c1f0d2a5cd8a0be4d9f5496c099a33be993312b"
-  end
-  on_mojave :or_newer do
-    version "4.20"
-    sha256 "b2e40fcc8ae457e1be450d3f0e9bfad1ad979edd75ceea9658a6412d23282d91"
-  end
+  version "4.20"
+  sha256 "b2e40fcc8ae457e1be450d3f0e9bfad1ad979edd75ceea9658a6412d23282d91"
 
   url "https://www.publicspace.net/download/signedVitamin#{version.major}.zip"
   name "Vitamin-R"
