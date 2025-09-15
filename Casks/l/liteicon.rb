6cb06c6c5fd17ec846ce441b4261122ed37e4a2b
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:29:03 -0700

    liteicon: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/liteicon.rb
+++ Casks/l/liteicon.rb
@@ -1,16 +1,6 @@
 cask "liteicon" do
-  on_sierra :or_older do
-    version "3.7.1"
-    sha256 "b457521a698a0ef55cd3d9c044c82c28984eeebc20d8baf05a9c21b0fa1df432"
-  end
-  on_high_sierra do
-    version "3.9"
-    sha256 "d185503d1c6cbbc6f770517853bd9ef08dc620f4e7ce3de913251a57e4d450d9"
-  end
-  on_mojave :or_newer do
-    version "4.1"
-    sha256 "545cff53df31b63fe28e794fb7f45e4c891885f5de57422b1483724f6d7ed4e0"
-  end
+  version "4.1"
+  sha256 "545cff53df31b63fe28e794fb7f45e4c891885f5de57422b1483724f6d7ed4e0"
 
   url "https://www.freemacsoft.net/downloads/LiteIcon_#{version}.zip"
   name "LiteIcon"
