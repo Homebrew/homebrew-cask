a54b703983307c9ce861aad32aa7f6f7ce525565
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:29:01 -0700

    lingon-x: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/lingon-x.rb
+++ Casks/l/lingon-x.rb
@@ -1,13 +1,5 @@
 cask "lingon-x" do
-  on_high_sierra :or_older do
-    version "6.6.5"
-    sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
-  end
-  on_mojave do
-    version "8.4.9"
-    sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
-  end
-  on_catalina do
+  on_catalina :or_older do
     version "8.4.9"
     sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
   end
@@ -33,7 +25,6 @@ cask "lingon-x" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
 
   app "Lingon X.app"
 
