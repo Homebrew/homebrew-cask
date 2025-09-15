7391518b8f39e1331d3181bd0b93224aac3f3ee4
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:32:54 -0700

    suspicious-package: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/suspicious-package.rb
+++ Casks/s/suspicious-package.rb
@@ -1,18 +1,6 @@
 cask "suspicious-package" do
   on_big_sur :or_older do
-    on_sierra :or_older do
-      version "3.4.1"
-      sha256 "e4673a0c590e7dcb711789d98fcadd2283c2152d262b7809dfd8c8a1b3e9094b"
-    end
-    on_high_sierra do
-      version "3.5.3"
-      sha256 "fad69db99a60058f8136954653fa2de81667f12cb731957a6d921d36ceaf195d"
-    end
-    on_mojave do
-      version "4.0"
-      sha256 "844708fb75f8aa102f3ede8ddef3c20180f469b7bc8ec65bbc0370ce9f7db33c"
-    end
-    on_catalina do
+    on_catalina :or_older do
       version "4.2.1"
       sha256 "5c05df9bf3d56758a3eefa972597e3138afdea4c3774f91fe2849482b7112823"
     end
