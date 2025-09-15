cddfb172c24f1de99777fdd551e25d642a62d70b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 14:09:15 +0000

    cyberduck 9.2.4

--- Casks/c/cyberduck.rb
+++ Casks/c/cyberduck.rb
@@ -1,6 +1,6 @@
 cask "cyberduck" do
-  version "9.2.1,43578"
-  sha256 "68f997590ecc22419b7cdf7df623ad3252165235f1615c5249677af491182424"
+  version "9.2.4,43667"
+  sha256 "7d326835d829e8459d9687a393b5c6da5261d4d12bc45466bf1d9f662c2f5ae7"
 
   url "https://update.cyberduck.io/Cyberduck-#{version.csv.first}.#{version.csv.second}.zip"
   name "Cyberduck"
