d0ad8cab39acd1eebad829f3445667f5c61bc611
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:27:07 -0700

    fiscript: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/fiscript.rb
+++ Casks/f/fiscript.rb
@@ -9,8 +9,6 @@ cask "fiscript" do
   deprecate! date: "2024-07-10", because: :unmaintained
   disable! date: "2025-07-10", because: :unmaintained
 
-  depends_on macos: ">= :sierra"
-
   app "FiScript.app"
 
   zap trash: [
