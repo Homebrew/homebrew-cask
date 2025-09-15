a6754365d5753a28b746f869d791acc906f73cb6
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:26:52 -0700

    fabfilter-pro-mb: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/fabfilter-pro-mb.rb
+++ Casks/f/fabfilter-pro-mb.rb
@@ -12,8 +12,6 @@ cask "fabfilter-pro-mb" do
     regex(/FabFilter\s+Pro-MB.*?v?(\d+(?:\.\d+)+)/im)
   end
 
-  depends_on macos: ">= :sierra"
-
   pkg "FabFilter Pro-MB #{version} Installer.pkg"
 
   uninstall pkgutil: "com.fabfilter.Pro-MB.#{version.major}"
