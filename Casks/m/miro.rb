6db606c0c9979a2fd52ad151e07a80581dbdd5a8
bevanjkay <email@bevankay.me>
2025-09-15 19:34:44 +1000

    miro 0.11.114

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.11.112"
+  version "0.11.114"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Install-Miro.dmg"
