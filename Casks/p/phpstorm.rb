9bb745d7c032a103e77e72a2a8f7d20ceaad47c0
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:30:59 -0700

    phpstorm: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/phpstorm.rb
+++ Casks/p/phpstorm.rb
@@ -24,7 +24,6 @@ cask "phpstorm" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
 
   app "PhpStorm.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
