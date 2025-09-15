c48a9c59ec02f77f91828e547d59eaa7c223d2cc
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:26:38 -0700

    elgato-game-capture-hd: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/e/elgato-game-capture-hd.rb
+++ Casks/e/elgato-game-capture-hd.rb
@@ -1,29 +1,16 @@
 cask "elgato-game-capture-hd" do
-  on_el_capitan :or_older do
-    version "2.5.2,1130"
-    sha256 "8497802cf07b4cd0e7d8b58610c15cad24fb1f18e230e2138303998f4d8397a6"
+  version "2.11.14"
+  sha256 "e00efce3433cad902400c610f4816fbecce414868a53aec70ef2d8ded9c1ba74"
 
-    url "https://gc-updates.elgato.com/mac/download.php?build=#{version.csv.second}"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_sierra :or_newer do
-    version "2.11.14"
-    sha256 "e00efce3433cad902400c610f4816fbecce414868a53aec70ef2d8ded9c1ba74"
-
-    url "https://edge.elgato.com/egc/macos/egcm/#{version}/final/Game_Capture_HD_#{version}.zip"
-
-    livecheck do
-      url "https://gc-updates.elgato.com/mac/egcm-update#{version.major}-rss/?dbg=0&lang=English"
-      strategy :sparkle, &:short_version
-    end
-  end
-
+  url "https://edge.elgato.com/egc/macos/egcm/#{version}/final/Game_Capture_HD_#{version}.zip"
   name "Elgato Game Capture HD"
   desc "Elgato video capture and streaming app"
   homepage "https://www.elgato.com/ww/en/s/downloads"
+
+  livecheck do
+    url "https://gc-updates.elgato.com/mac/egcm-update#{version.major}-rss/?dbg=0&lang=English"
+    strategy :sparkle, &:short_version
+  end
 
   app "Game Capture HD.app"
 
