4cc4422e3136b45862e9905c81d86ecfd688c830
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:26:20 -0700

    displaylink: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/d/displaylink.rb
+++ Casks/d/displaylink.rb
@@ -1,30 +1,6 @@
 cask "displaylink" do
   on_big_sur :or_older do
-    on_sierra :or_older do
-      version "4.3.1,2021-02"
-      sha256 "d5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97"
-
-      url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20Mac%20OS%20X%20and%20macOS#{version.csv.first}-EXE.dmg"
-
-      pkg "DisplayLink Software Installer.pkg"
-    end
-    on_high_sierra do
-      version "5.1.1,2021-02"
-      sha256 "1ac9093f8113af8c35d6f3ff5b1ae3f119a5aff0d5309d75c7a1742f159184b5"
-
-      url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
-
-      pkg "DisplayLink Software Installer.pkg"
-    end
-    on_mojave do
-      version "5.2.6,2021-05"
-      sha256 "9f1854cd5720105d6d45c91172419c503358543e4a23d7113387aedf16a39cbb"
-
-      url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
-
-      pkg "DisplayLink Software Installer.pkg"
-    end
-    on_catalina do
+    on_catalina :or_older do
       version "1.5,2021-09"
       sha256 "d703cc8e9093e4d163c5e612326c0907a02c6d4eec6aaca8d0727503859ef95d"
 
