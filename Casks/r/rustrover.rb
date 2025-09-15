ab1721f6c7025e1f8fe28b37fef570e5b4a8f70b
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:32:00 -0700

    rustrover: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/r/rustrover.rb
+++ Casks/r/rustrover.rb
@@ -24,7 +24,6 @@ cask "rustrover" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
 
   app "RustRover.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
