ad264ddfeab409ea80a5aec535b865d5b605917a
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:26:50 -0700

    f-bar: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/f-bar.rb
+++ Casks/f/f-bar.rb
@@ -1,27 +1,17 @@
 cask "f-bar" do
-  on_el_capitan :or_older do
-    version "3.1.1"
-    sha256 "dd2cbcc8bfb9244cf952d29354f6cd7f442472ca4ed8a11063c006c9f83b07af"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_sierra :or_newer do
-    version "5.1"
-    sha256 "82d6a186209c2b70317b444eea278b570843b09db2373ebefa1c025a73a937e6"
-
-    livecheck do
-      url "https://apps.eastwest.se/fbar/updates/FBarAppcast.xml"
-      strategy :sparkle, &:short_version
-    end
-  end
+  version "5.1"
+  sha256 "82d6a186209c2b70317b444eea278b570843b09db2373ebefa1c025a73a937e6"
 
   url "https://apps.eastwest.se/fbar/updates/F-Bar_#{version}.zip",
       verified: "apps.eastwest.se/fbar/"
   name "F-Bar"
   desc "Manage Laravel Forge servers from the menubar"
   homepage "https://laravel-forge-menubar.com/"
+
+  livecheck do
+    url "https://apps.eastwest.se/fbar/updates/FBarAppcast.xml"
+    strategy :sparkle, &:short_version
+  end
 
   auto_updates true
 
