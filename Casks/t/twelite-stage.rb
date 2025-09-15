fcf61104f213e4689dd56d7f1c93b07c21adcb01
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:33:32 -0700

    twelite-stage: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/t/twelite-stage.rb
+++ Casks/t/twelite-stage.rb
@@ -12,8 +12,6 @@ cask "twelite-stage" do
     regex(/MWSTAGEv?(\d+(?:[._]\d+)+)/i)
   end
 
-  depends_on macos: ">= :mojave"
-
   # It is an SDK with a shell-based application that
   # includes source code and other user resources.
   # It is neither an "app" nor a "suite".
