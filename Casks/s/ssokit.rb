90312f7edc3f686c641ae1cdc8c90704f3b30942
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:32:43 -0700

    ssokit: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/ssokit.rb
+++ Casks/s/ssokit.rb
@@ -12,8 +12,6 @@ cask "ssokit" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :high_sierra"
-
   app "SSokit.app"
 
   zap trash: "~/Library/Preferences/cn.rangaofei.SSokit.plist"
