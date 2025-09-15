80e365e87578b0a2a4dc07eb4c47892ad1f537ec
Patrick Linnane <patrick@linnane.io>
2025-09-15 10:30:30 -0700

    omniplan: remove pre-Catalina references
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/o/omniplan.rb
+++ Casks/o/omniplan.rb
@@ -1,30 +1,6 @@
 cask "omniplan" do
   on_big_sur :or_older do
-    on_el_capitan :or_older do
-      version "3.7.3"
-      sha256 "1a3ab3a1ea22bdbdf9c1afda8cafc9a2fdf60cb4414f142b621c8758f81720bd"
-
-      url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniPlan-#{version}.dmg"
-    end
-    on_sierra do
-      version "3.10.4"
-      sha256 "a30728e72ae970dbf37b2ef9942a6b54267aa3456288dcc1815f20b44667e9e5"
-
-      url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniPlan-#{version}.dmg"
-    end
-    on_high_sierra do
-      version "3.13"
-      sha256 "82e0d7db2626d751f93f97d80dc032e4bc01bba1e05ea52c553e4771c8cfeec5"
-
-      url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniPlan-#{version}.dmg"
-    end
-    on_mojave do
-      version "4.2.7"
-      sha256 "157cbea0055a87b2c078c336ea9f5d9aaa9caa242c92265f410e5d7ac534883f"
-
-      url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
-    end
-    on_catalina do
+    on_catalina :or_older do
       version "4.2.7"
       sha256 "157cbea0055a87b2c078c336ea9f5d9aaa9caa242c92265f410e5d7ac534883f"
 
