35e0be21cfc57d415a7697bfa29cb76c1c52121e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-16 16:51:01 +0000

    mp3tag 1.9.8

--- Casks/m/mp3tag.rb
+++ Casks/m/mp3tag.rb
@@ -1,6 +1,6 @@
 cask "mp3tag" do
-  version "1.9.7"
-  sha256 "b7b19f890f91aa2e6e0a612c979b4d610f0239a8f174119e34d844e3426a09b7"
+  version "1.9.8"
+  sha256 "50ac0e66dcfb1daae6b53806dc0589ddf16afc5e45823e4fcc2d421342190473"
 
   url "https://updates.mp3tag.app/Mp3tag-#{version}.zip"
   name "Mp3tag"
