f140b5c468abe1be25fa940a02b1922cb1c01b20
LazareC <18364305+Lazare-C@users.noreply.github.com>
2025-09-16 16:25:02 +0200

    Apply suggestions from code review
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/v/virtual-desktop-streamer.rb
+++ Casks/v/virtual-desktop-streamer.rb
@@ -1,11 +1,16 @@
 cask "virtual-desktop-streamer" do
-  version :latest
+  version "1.34.8"
   sha256 :no_check
 
   url "https://files.vrdesktop.net/files/VirtualDesktop.Streamer.Setup.pkg"
   name "Virtual Desktop Streamer"
   desc "VR Virtual Desktop Streamer"
   homepage "https://www.vrdesktop.net/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "VirtualDesktop.Streamer.Setup.pkg"
 
