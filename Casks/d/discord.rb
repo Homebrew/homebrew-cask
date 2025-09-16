3ba0d2612c05577b00b9bc0aebb4972dbe6afb59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-16 00:55:40 +0000

    discord 0.0.361

--- Casks/d/discord.rb
+++ Casks/d/discord.rb
@@ -8,8 +8,8 @@ cask "discord" do
     end
   end
   on_big_sur :or_newer do
-    version "0.0.360"
-    sha256 "89b641e38132e5dcabf2783e470af4144d0a27a15faa457fefc95f0f3fd3063d"
+    version "0.0.361"
+    sha256 "0789041dfdfef86b37b10f9650f0399661515b4442066487869a67a1f5da6c36"
 
     livecheck do
       url "https://discord.com/api/download/stable?platform=osx"
