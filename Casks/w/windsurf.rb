f09e9e486e5b297f8e3b5f1a9ac917ecbbd68506
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-16 20:18:59 +0000

    windsurf 1.12.6
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/w/windsurf.rb
+++ Casks/w/windsurf.rb
@@ -1,9 +1,9 @@
 cask "windsurf" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.12.5,64804081c3f9a1652d6d325c28c01c3f5882f6fb"
-  sha256 arm:   "3ec4c1f710febeea8e452332976003331fdad3908ab94d981ca160f4394b45da",
-         intel: "794e2f571238cc4d9f81fbc5b59bca186f4628a7d5f785e3db42eddd831ec3c0"
+  version "1.12.6,9a3d24f8ef44e756d945e890becee97bc90c6482"
+  sha256 arm:   "2b226c2aad7fd97ed2b4bc1d838d83db9efc4f1841de6d16dbed37436032284c",
+         intel: "281c12f6555506a45fd5b5e56c00196bddda8641a7e5bc2e923b5ecd689dafa2"
 
   url "https://windsurf-stable.codeiumdata.com/darwin-#{arch}-dmg/stable/#{version.csv.second}/Windsurf-darwin-#{arch}-#{version.csv.first}.dmg",
       verified: "windsurf-stable.codeiumdata.com/"
@@ -23,6 +23,7 @@ cask "windsurf" do
   end
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "Windsurf.app"
   binary "#{appdir}/Windsurf.app/Contents/Resources/app/bin/windsurf"
