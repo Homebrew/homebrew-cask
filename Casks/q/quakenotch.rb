9808e14590c123ce9976a4604bf407dba4a801f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 04:42:19 +0000

    quakenotch 1.5.2

--- Casks/q/quakenotch.rb
+++ Casks/q/quakenotch.rb
@@ -1,6 +1,6 @@
 cask "quakenotch" do
-  version "1.5.1"
-  sha256 "3f9876e927f6957b02131a1c19a8417dbc334e4c0beef3717636225f192a6ba3"
+  version "1.5.2"
+  sha256 "a8227c25b338e60f4987ddc31abe8157981ca6a4e2f2e7fce8884be62b5ea79c"
 
   url "https://github.com/rohanrhu/QuakeNotch/releases/download/v#{version}/QuakeNotch.zip",
       verified: "github.com/rohanrhu/QuakeNotch/"
