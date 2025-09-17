4500e0d22bd7eda3be438ed8710a93ee5539c03e
Maxwell Krohn <themax@gmail.com>
2025-09-17 13:00:07 -0400

    foks v0.1.4

--- Casks/f/foks.rb
+++ Casks/f/foks.rb
@@ -1,9 +1,9 @@
 cask "foks" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.1.3"
-  sha256 arm:   "a09b69f055f0607a6e06db4c7c10de6127dc42cca697c18a8b3372c83507eb28",
-         intel: "bacc7a2711bb332ce95b16fcb26aad610e8e1bafaf4d93bd2cb2507a18789938"
+  version "0.1.4"
+  sha256 arm:   "61d26c22ff3b241ff99be4cd38dbb81ceb411b14cd49d584c8e7d4658e122822",
+         intel: "2503f6ecc93794b51e256c4abd784cb440d1a333709434fc344a001eb06fecf2"
 
   url "https://pkgs.foks.pub/stable/darwin/foks-v#{version}-darwin-brew-#{arch}.zip"
   name "FOKS"
