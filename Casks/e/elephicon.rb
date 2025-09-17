67ceb8fe073399d4a9f97f33df734cbdc701d4e9
bevanjkay <email@bevankay.me>
2025-09-17 18:42:42 +1000

    elephicon 3.8.2

--- Casks/e/elephicon.rb
+++ Casks/e/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.8.1"
-  sha256 arm:   "f1fa0205a95fcb605b0d37c4aa54c9431651e9462eb476482dc0d8e7895f2e21",
-         intel: "5eca045a13196df22944644eb56bd71add6f796821f78dbb295a5fb29f4837b0"
+  version "3.8.2"
+  sha256 arm:   "4399c20a42fa402be839e454adca7425fbbfd5d659685d080d914a6efc69de56",
+         intel: "4d32f53f4fd34c33274d4ea22d3e98c8d0a9c516abeeae528ef69a7d64051711"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
@@ -11,7 +11,7 @@ cask "elephicon" do
   homepage "https://github.com/sprout2000/elephicon/"
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Elephicon.app"
 
