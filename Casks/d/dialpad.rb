08cda37c5fa5f0705036f458473f84e3fd21f4fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 21:45:08 +0000

    dialpad 2509.0.2

--- Casks/d/dialpad.rb
+++ Casks/d/dialpad.rb
@@ -1,9 +1,9 @@
 cask "dialpad" do
   arch arm: "arm64", intel: "x64"
 
-  version "2508.2.3"
-  sha256 arm:   "73fff7ebbd1a40cbf7ff1bfac1d8ef529517d1e4fa6711d6a40cb95d0ad7091f",
-         intel: "a23e49a75db9857cec8940a0511aa1347e9ea87bced4ea18f404f20d5a610417"
+  version "2509.0.2"
+  sha256 arm:   "0c8f6c89e226874385e69a6bfbeac1d370905a5065cd5a60d3c00b0572f92f9a",
+         intel: "4572beb3dcd0cef27dcd18ae63618e4a309fe19029a6ad3357cfbcef3e9505da"
 
   url "https://storage.googleapis.com/dialpad_native/osx/#{arch}/Dialpad.#{version}.zip",
       verified: "storage.googleapis.com/dialpad_native/osx/#{arch}"
