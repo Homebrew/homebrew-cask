696a4f44b5afcff083c38bc71fbea6a0eb902320
宇佐美いちこ <143856109+ichikoAI@users.noreply.github.com>
2025-09-18 19:40:31 -0400

    figma 125.8.8

--- Casks/f/figma.rb
+++ Casks/f/figma.rb
@@ -1,9 +1,9 @@
 cask "figma" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "125.8.6"
-  sha256 arm:   "33c83b5323fed4794ab3b119fcb2241b2bf662c9782b8ab7b53f5078f00635ca",
-         intel: "406182c6cfa8f7f60488e91d3bf27a32d09c334c89eca7cc0ee2959302a5c8ed"
+  version "125.8.8"
+  sha256 arm:   "4b4e57f6c6405b09daaa8cd8d9c9ca1da398cd4ab7d7d51ae17eb2bf3c858e44",
+         intel: "5b062d5c795d58d1a955f2eea7906822800c4cdb07ea2277ac0411d5a8fc4d72"
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
