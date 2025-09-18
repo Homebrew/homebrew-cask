f31fb9a84cece8b0f5b187264f5aeaa412714d85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 16:26:28 +0000

    openphone 4.1.9

--- Casks/o/openphone.rb
+++ Casks/o/openphone.rb
@@ -1,6 +1,6 @@
 cask "openphone" do
-  version "4.1.3"
-  sha256 "a6f322caf636c577e90b223eb954cc5046a0f53c6521af21f4ba809eef0303e8"
+  version "4.1.9"
+  sha256 "a71600d182ced62dc4488aae87bb91695880a68899bfb8e27ac46ba0d9bfb002"
 
   url "https://download.openphone.co/OpenPhone-#{version}-universal.dmg"
   name "OpenPhone"
