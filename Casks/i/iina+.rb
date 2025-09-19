faf6ffccd1b94574ccbca73d940fb53107a40d68
bevanjkay <email@bevankay.me>
2025-09-19 21:50:44 +1000

    iina+ 0.8.13

--- Casks/i/iina+.rb
+++ Casks/i/iina+.rb
@@ -1,6 +1,6 @@
 cask "iina+" do
-  version "0.8.12"
-  sha256 "b56a5bd5867680fcc394a99cbe81d1fa08346717f8b452c109addc74543cf581"
+  version "0.8.13"
+  sha256 "edada730a515924d0403c457d0323f81d80cdb09edd36117142d006968e4bc02"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version}/IINA+.#{version}.dmg"
   name "IINA+"
@@ -10,7 +10,7 @@ cask "iina+" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :ventura"
 
   app "IINA+.app"
 
