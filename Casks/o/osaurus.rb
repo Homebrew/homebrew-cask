09266225df948b311bd74d9282560922dcdc01b3
Robin Curbelo <curbelorobin@gmail.com>
2025-09-20 13:46:12 -0400

    add verified URL
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/o/osaurus.rb
+++ Casks/o/osaurus.rb
@@ -2,10 +2,11 @@ cask "osaurus" do
   version "0.0.40"
   sha256 "e2975fb7e7e6abf825276e84b3f0d47e974e00368ff9f26e36c30e686f59ed96"
 
-  url "https://github.com/dinoki-ai/osaurus/releases/download/#{version}/Osaurus-#{version}.dmg"
+  url "https://github.com/dinoki-ai/osaurus/releases/download/#{version}/Osaurus-#{version}.dmg",
+      verified: "github.com/dinoki-ai/osaurus/"
   name "Osaurus"
   desc "Native Apple Silicon LLM server"
-  homepage "https://github.com/dinoki-ai/osaurus"
+  homepage "https://dinoki.ai/osaurus"
 
   depends_on macos: ">= :sequoia"
   depends_on arch: :arm64
