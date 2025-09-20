01c0da2e320ede6cf387f6292088bc876ea16103
Klaus Hipp <khipp@users.noreply.github.com>
2025-09-20 18:49:21 +0200

    macai: update url and livecheck

--- Casks/m/macai.rb
+++ Casks/m/macai.rb
@@ -2,10 +2,16 @@ cask "macai" do
   version "2.2.1"
   sha256 "f7949b98f9bb9ac0c46dff3f3642819727875005ecff5a726d4e7db3ab6d7dd2"
 
-  url "https://github.com/Renset/macai/releases/download/v#{version}/macai.#{version}.Universal.zip"
+  url "https://renset.dev/macai/macai%20#{version}%20Universal.zip",
+      verified: "renset.dev/macai/"
   name "macai"
   desc "Native chat application for all major LLM APIs"
   homepage "https://github.com/Renset/macai"
+
+  livecheck do
+    url "https://renset.dev/macai/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sonoma"
