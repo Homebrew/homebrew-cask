c6b06fb050e5942006013f4e6319f225c5b60941
Patrick Linnane <patrick@linnane.io>
2025-09-19 21:39:22 -0700

    clion@eap: add minimum macOS
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/c/clion@eap.rb
+++ Casks/c/clion@eap.rb
@@ -24,6 +24,7 @@ cask "clion@eap" do
   end
 
   auto_updates true
+  depends_on macos: ">= :monterey"
 
   app "CLion #{version.csv.first} EAP.app"
   binary "#{appdir}/CLion #{version.csv.first} EAP.app/Contents/MacOS/clion", target: "clion-eap"
