242b65cdf8bcf7d6f80c4f0834c02890f241397f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-21 00:16:45 +0000

    font-alan-sans: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-alan-sans.rb
+++ Casks/font/font-a/font-alan-sans.rb
@@ -2,9 +2,10 @@ cask "font-alan-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alansans/AlanSans%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/alansans/AlanSans%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Alan Sans"
-  homepage "https://github.com/alan-eu/Alan-Sans"
+  homepage "https://fonts.google.com/specimen/Alan+Sans"
 
   font "AlanSans[wght].ttf"
 
