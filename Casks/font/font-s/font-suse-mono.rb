5dce5a23f19defaa1e4088d51fb806bb29fcd867
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-21 00:16:45 +0000

    font-suse-mono: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-suse-mono.rb
+++ Casks/font/font-s/font-suse-mono.rb
@@ -3,10 +3,11 @@ cask "font-suse-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/susemono"
   name "SUSE Mono"
-  homepage "https://github.com/SUSE/suse-font"
+  homepage "https://fonts.google.com/specimen/SUSE+Mono"
 
   font "SUSEMono-Italic[wght].ttf"
   font "SUSEMono[wght].ttf"
