0b5675bcc676fbdad0ef65d26d67d1f681aaa9f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-22 13:44:23 +0000

    smartgit 24.1.5

--- Casks/s/smartgit.rb
+++ Casks/s/smartgit.rb
@@ -1,9 +1,9 @@
 cask "smartgit" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "24.1.4"
-  sha256 arm:   "330274ba9923d64298cad928026c3db69fe3a899dac5e8cb995ccbe6644c48c0",
-         intel: "b44e5450edc9559fc5e5e7b7874318dbdfc682fdc80e4ae410ff8665dc998487"
+  version "24.1.5"
+  sha256 arm:   "036c1df4939b53ffe8a3f8558bfe9f7484159d018959c3b3fa1592e695a27635",
+         intel: "bc108b9fe01323d8d795545423221e89e72c07414ebc7eca96cb807c4ada5c49"
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
   name "SmartGit"
