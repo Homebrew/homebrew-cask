53bcc2cd866f7cfd6b8f8e0eb2b6503ac0c80139
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-22 09:57:06 +0000

    framer 2025.38.0

--- Casks/f/framer.rb
+++ Casks/f/framer.rb
@@ -1,9 +1,9 @@
 cask "framer" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.37.2"
-  sha256 arm:   "74a47a4e5b15035a96c4d80ff6b59300081590e75e0d8d08a46d760fbb6d2081",
-         intel: "5b6da06fe3d17374bc7f571c7bf03347a32d55cd151c43708384c3ca5fad33c9"
+  version "2025.38.0"
+  sha256 arm:   "ca1e31fb15176c980606db0d1b70f917d6dbd608681a2dc500ba822f6c435e48",
+         intel: "57bc07a89539858381493c05220c011fcadc1ad7fec6f1b1b427385dafa52106"
 
   url "https://updates.framer.com/electron/darwin/#{arch}/Framer-#{version}.zip"
   name "Framer"
