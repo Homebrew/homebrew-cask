deedb9c92dd10df5042ffb4be2b83fc848f345db
Vadim Khoroshev <vs.khoroshev@gmail.com>
2025-09-24 17:33:42 +0300

    temurin@25 25+36

---
+++ Casks/t/temurin@25.rb
@@ -0,0 +1,32 @@
+cask "temurin@25" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "25,36"
+  sha256 arm:   "c55bcb78dee95f0482e96762dc0912b57d3b1213de0d7e7542321952c8cdd89f",
+         intel: "651dcef5d1bc4fe920042adfa42171846cbe0f264f01c1e1957387008106e224"
+
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
+      verified: "github.com/adoptium/"
+  name "Eclipse Temurin 25"
+  desc "JDK from the Eclipse Foundation (Adoptium)"
+  homepage "https://adoptium.net/"
+
+  livecheck do
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    regex(/^jdk-(\d+(?:\.\d+)*?)\+(\d+(?:\.\d+)*)(?:-LTS)?$/i)
+    strategy :json do |json, regex|
+      json.map do |release|
+        match = release["release_name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
+  pkg "OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg"
+
+  uninstall pkgutil: "net.temurin.#{version.major}.jdk"
+
+  # No zap stanza required
+end
