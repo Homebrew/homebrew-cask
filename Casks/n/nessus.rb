659a358548a6c5ec3d72c5b15c762c2e44a4f909
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 19:30:55 +0000

    nessus 10.9.4

--- Casks/n/nessus.rb
+++ Casks/n/nessus.rb
@@ -1,6 +1,6 @@
 cask "nessus" do
-  version "10.9.3"
-  sha256 "c508e233cb54f16acd1d1a487b356b32c9e546a745d6d03837743d4fda12be76"
+  version "10.9.4"
+  sha256 "8c9a9fb94422ecb40b741187a5e620d398787214aaac555bb55d125e48e7b791"
 
   url "https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-#{version}.dmg"
   name "Tenable Nessus"
@@ -12,7 +12,7 @@ cask "nessus" do
     regex(/Nessus[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  pkg ".Nessus.pkg"
+  pkg "Install Nessus.pkg"
   binary "/Library/Nessus/run/bin/nasl"
   binary "/Library/Nessus/run/bin/ndbg"
   binary "/Library/Nessus/run/sbin/nessuscli", target: "#{HOMEBREW_PREFIX}/sbin/nessuscli"
