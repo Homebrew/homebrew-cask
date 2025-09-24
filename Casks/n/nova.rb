7dacbb5aec42bb80e4cc19fbd7c51917c4452e53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 22:18:49 +0000

    nova 13.3

--- Casks/n/nova.rb
+++ Casks/n/nova.rb
@@ -1,6 +1,6 @@
 cask "nova" do
-  version "13.2"
-  sha256 "91290ff747afd61b9ccadb3d797569ed27503726ee84e25bc6986ed4eaf17c16"
+  version "13.3"
+  sha256 "13f154ed44f15a55013de18072196c16c6673fbb38cd14a143c7dab55af575af"
 
   url "https://panic.com/download/nova/Nova%20#{version}.zip",
       verified:   "panic.com/download/nova/",
