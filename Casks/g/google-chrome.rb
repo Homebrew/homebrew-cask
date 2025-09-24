0cc94801921cabbe565374f8ab3521b68261ce2b
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-09-24 10:11:44 -0400

    google-chrome 140.0.7339.208

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "140.0.7339.186"
+  version "140.0.7339.208"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
