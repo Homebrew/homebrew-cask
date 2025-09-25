cdc023e9047f062fd38a7535c670da4760fa5ba9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-09-24 21:33:37 -0400

    signal 7.72.0

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.71.0"
-  sha256 arm:   "65030c30750fa0039941e16241b916f86eb1af519c385b1a03643710fb2fb8e2",
-         intel: "53b121bced97bf384bfadc474963dc4e435db169e033b43d2bc711eeebbe4ed4"
+  version "7.72.0"
+  sha256 arm:   "8e6432e9461ea556fa6d18f02fc5c241516407620f460e65630cb3b3f36c39a3",
+         intel: "d4bcbd02ee9dc2fd487bfd1b1fcabe56b72038d78dff9d3bc7fdd4fa189c7271"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.zip"
   name "Signal"
@@ -16,7 +16,7 @@ cask "signal" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Signal.app"
 
