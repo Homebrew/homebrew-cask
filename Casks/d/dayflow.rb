d9ac12ae9e9a4a89af39771dd0da31a2d482e9b9
Rui Chen <rui@chenrui.dev>
2025-09-25 19:11:07 -0400

    dayflow 1.0.31 (new cask)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Casks/d/dayflow.rb
@@ -0,0 +1,25 @@
+cask "dayflow" do
+  version "1.0.31"
+  sha256 "cc36ba3cff77e36eef8c48abaf5789551041d394251bdd2b974150dec62de5de"
+
+  url "https://github.com/JerryZLiu/Dayflow/releases/download/v#{version}/Dayflow.dmg"
+  name "Dayflow"
+  desc "Generate a timeline of your day, automatically"
+  homepage "https://github.com/JerryZLiu/Dayflow"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Dayflow.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/teleportlabs.com.Dayflow",
+    "~/Library/Application Support/Dayflow",
+    "~/Library/Containers/teleportlabs.com.Dayflow",
+  ]
+end
