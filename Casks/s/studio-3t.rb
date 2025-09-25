e1322519479939f527d42d69dccc4e8298a0b78c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 10:36:59 +0000

    studio-3t 2025.17.1

--- Casks/s/studio-3t.rb
+++ Casks/s/studio-3t.rb
@@ -2,9 +2,9 @@ cask "studio-3t" do
   arch arm: "-aarch64"
   livecheckarch = on_arch_conditional arm: "_aarch64"
 
-  version "2025.17.0"
-  sha256 arm:   "78f2e231924eae9ee9d11e902f49fbddcee9cf80234a2fa5f691d03b4ae02811",
-         intel: "d015140af943e73fa24baeb3c2bb272db2dc8619eeed97630d9ee256234ece75"
+  version "2025.17.1"
+  sha256 arm:   "1cdd7d65ce10e4061fda4849ddbe03423245a2946e418caba1d8580e042f75a7",
+         intel: "0827ff283c3787b5d2be8b1ec2b363cb146dee6227d38f78d7b1ddb06bee80c7"
 
   url "https://download.studio3t.com/studio-3t/mac#{arch}/#{version}/Studio-3T.dmg"
   name "Studio 3T"
