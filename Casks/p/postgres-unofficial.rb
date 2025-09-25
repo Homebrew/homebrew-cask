6e0c18638c898b62c92efab0cfbff8ca71911274
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 16:37:45 +0000

    postgres-unofficial 2.9

--- Casks/p/postgres-unofficial.rb
+++ Casks/p/postgres-unofficial.rb
@@ -1,6 +1,6 @@
 cask "postgres-unofficial" do
-  version "2.8.5,13-14-15-16-17"
-  sha256 "0b4ae9165d152256bae78159b047407b1db448b7b1d269d217e98d379b666a7c"
+  version "2.9,13-14-15-16-17-18"
+  sha256 "370354cc51b6713fdbae343e9552b51adb6fa938d0144535ccae20a1331d827f"
 
   url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version.csv.first}/Postgres-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/PostgresApp/PostgresApp/"
