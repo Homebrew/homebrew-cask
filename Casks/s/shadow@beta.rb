fb78b41f826d598198ec32f3c80a3a6d89bc2ce1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 16:30:52 +0000

    shadow@beta 9.9.10301

--- Casks/s/shadow@beta.rb
+++ Casks/s/shadow@beta.rb
@@ -1,9 +1,9 @@
 cask "shadow@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.9.10299"
-  sha256 arm:   "5916a8efd6b85e49f665e37d34be5226344438b60fd6ae1a81714b2386de8f1a",
-         intel: "380c376213ceef8955dddd6abec4157acea52dcda7ba39a5335672d7ab11e2cb"
+  version "9.9.10301"
+  sha256 arm:   "6e51beab2eca6ef604f6f84ca4bda7e70daca75f88ae5746310c10071f852303",
+         intel: "31acc938f829af695690760bc9cdd910932cc01eaecdcefac608579562c562e4"
 
   url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
   name "Shadow PC Beta"
