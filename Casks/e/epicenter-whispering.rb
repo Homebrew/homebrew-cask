1658bda7d70c7e1d89f3dd8c4d921cc1238fcdf7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 03:54:04 +0000

    epicenter-whispering 7.5.1

--- Casks/e/epicenter-whispering.rb
+++ Casks/e/epicenter-whispering.rb
@@ -1,9 +1,9 @@
 cask "epicenter-whispering" do
   arch arm: "aarch64", intel: "x64"
 
-  version "7.5.0"
-  sha256 arm:   "15331593a4261db0bf4ffd73703fb756b6b4d93221eb36217163ef9895c70c61",
-         intel: "1a3035ce9d216977cc1f6695d168db5e21e571e900b513d04ca1ea3b2ca49036"
+  version "7.5.1"
+  sha256 arm:   "2daf0baa7c79bd9b5dfbeb7744e8d4b1ab941b83ab73d0562c08a14984e7efdb",
+         intel: "5d96f166c621eaaa120e5571338cfdfe34752e4ca3692c18b37c133fc9d9b330"
 
   url "https://github.com/epicenter-os/epicenter/releases/download/v#{version}/Whispering_#{version}_#{arch}.dmg",
       verified: "github.com/epicenter-os/epicenter/"
