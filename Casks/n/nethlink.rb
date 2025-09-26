7ae7a28aba40fcf11570348e193eb4bf32d896ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 10:21:58 +0000

    nethlink 1.3.0

--- Casks/n/nethlink.rb
+++ Casks/n/nethlink.rb
@@ -1,9 +1,9 @@
 cask "nethlink" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.3"
-  sha256 arm:   "66a21ddd7a54c43dc5a7df41a6bf27ffb964ea05392506fe463bf8a02714ba66",
-         intel: "e5b299174ea6f6b44063a94bdcab01cbe59383f78caedd8637d0c21ee064b0f4"
+  version "1.3.0"
+  sha256 arm:   "623770ba076e365b96b0010cdf718fa2b38ca38ac9f2a430f1376808eb2ef2db",
+         intel: "51e0789660f03e155e236091b78c1c1a7a7f4b4af4573efba28e383ac9de9a88"
 
   url "https://github.com/NethServer/nethlink/releases/download/v#{version}/nethlink-#{version}-#{arch}.dmg"
   name "NethLink"
