03287a49342f2eca508ab56d174bb8f3eaafd1ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-27 21:46:55 +0000

    font-iosevka-aile 33.3.1

--- Casks/font/font-i/font-iosevka-aile.rb
+++ Casks/font/font-i/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "33.3.0"
-  sha256 "053213948119ee6e603cc11fe8a5a1d5f343d9c3d370f395e01779cf3ce59c72"
+  version "33.3.1"
+  sha256 "79af9b2918646fa3c954a7be92b664a0adfca1cb0d10936f999285124e1c5821"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/SuperTTC-IosevkaAile-#{version}.zip"
   name "Iosevka Aile"
