1be2beef8aa12e2767a7dbc10937b1a5c07a9b77
guy <guy@Meraki-Secure.local>
2025-09-28 20:58:32 +0200

    Fix audit/style: add verified, remove postflight, add zap

--- Casks/img2jpg-optimizer.rb
+++ Casks/img2jpg-optimizer.rb
@@ -1,10 +1,11 @@
 cask "img2jpg-optimizer" do
   version "1.0.0"
-  sha256 "0351516d75fb27b49d6e46ec5df63c3454ed1f09f4637bccb8924af20224d9e5"
+  sha256 "035151d675fb27b49d6e46ec5df63c3454ed1f09f4637bccb8924af20224d9e5"
 
-  url "https://github.com/chupchupchup/IMG2JPG-Optimizer/releases/download/v#{version}/IMG2JPG-Optimizer-final.dmg"
+  url "https://github.com/chupchupchup/IMG2JPG-Optimizer/releases/download/v#{version}/IMG2JPG-Optimizer-final.dmg",
+      verified: "github.com/chupchupchup/IMG2JPG-Optimizer/"
   name "IMG2JPG Optimizer"
-  desc "Optimiseur d’images MozJPEG (progressif, Huffman optimisé) pour macOS Apple Silicon"
+  desc "MozJPEG-based JPEG optimizer for macOS (progressive + optimized Huffman)"
   homepage "https://github.com/chupchupchup/IMG2JPG-Optimizer"
 
   depends_on arch: :arm64
@@ -16,10 +17,4 @@ cask "img2jpg-optimizer" do
     "~/Library/Preferences/org.guy.IMG2JPG-Optimizer.plist",
     "~/Library/Saved Application State/org.guy.IMG2JPG-Optimizer.savedState",
   ]
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/IMG2JPG-Optimizer.app"],
-                   sudo: false
-  end
 end
