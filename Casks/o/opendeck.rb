652d6c5ae99c66258aada0c12058a5da56acd236
wielorzeczownik <47759875+wielorzeczownik@users.noreply.github.com>
2025-09-28 20:46:30 +0200

    fix style (brew audit/verified)

--- Casks/o/opendeck.rb
+++ Casks/o/opendeck.rb
@@ -1,12 +1,10 @@
 cask "opendeck" do
   version "2.6.0"
-
   arch arm: "aarch64", intel: "x64"
-
   sha256 arm:   "b72353667ba7696557b61c83b20b9c157bdcb300fa71cc7a0448426f032924f5",
          intel: "efdf914330c3b54bfadd347879fc876cf7fd1c620d93cf723cb848505b978edd"
-
-  url "https://github.com/nekename/OpenDeck/releases/download/v#{version}/OpenDeck_#{version}_#{arch}.dmg"
+  url "https://github.com/nekename/OpenDeck/releases/download/v#{version}/OpenDeck_#{version}_#{arch}.dmg",
+      verified: "github.com/nekename/OpenDeck/"
   name "OpenDeck"
   desc "Stream controller utility supporting Elgato Stream Deck plugins"
   homepage "https://github.com/nekename/OpenDeck"
