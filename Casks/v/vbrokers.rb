dce9476ad3b87daa12b02f7c251cb781f7c5de22
Klaus Hipp <khipp@users.noreply.github.com>
2025-09-28 07:22:35 +0200

    vbrokers 2.7.110,1110127,1759029298

--- Casks/v/vbrokers.rb
+++ Casks/v/vbrokers.rb
@@ -1,14 +1,15 @@
 cask "vbrokers" do
-  version "2.6.915,600001,1757381897"
-  sha256 "9bd6f25c7c57a7d9bfee800470bc9af31b9ce82121bb105ea54414e4b887af4c"
+  version "2.7.110,1110127,1759029298"
+  sha256 "4cc1a2c5532550f7532de3c778a41cb8b4dc96b32b907e1c2f9ed0aa8e6af7fb"
 
-  url "https://r.hstong.com/public/app/Mac/HSTONG_Mac_#{version.csv.second}_#{version.csv.first}_#{version.csv.third}.dmg"
+  url "https://r.hstong.com/public/app/Mac/HSTONG_Mac_#{version.csv.second}_#{version.csv.first}_#{version.csv.third}.dmg",
+      verified: "r.hstong.com/"
   name "VBrokers"
   desc "Trading platform"
-  homepage "https://www.hstong.com/"
+  homepage "https://www.vbkr.com/"
 
   livecheck do
-    url "https://www.hstong.com/download"
+    url "https://www.vbkr.com/download"
     regex(%r{href=['"].*?/HSTONG[-_]?Mac[-_]?(\d+)[-_]?(\d+(?:\.\d+)+)[-_]?(\d+)\.dmg}i)
     strategy :page_match do |page, regex|
       page.scan(regex)
