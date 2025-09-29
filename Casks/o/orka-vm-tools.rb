37e2f0c6c1e314a3692240eeafa77ecf89cdf4f0
Lucia Smith <lsmith@macstadium.com>
2025-09-29 14:28:01 -0700

    orka-vm-tools: update cask to reflect website changes

--- Casks/o/orka-vm-tools.rb
+++ Casks/o/orka-vm-tools.rb
@@ -1,16 +1,18 @@
 cask "orka-vm-tools" do
-  version "3.3.0"
-  sha256 "3aaea2511e9390abd115989c8fd1c005045b5b78f2f95f33761ea1497d4b518a"
+  version "3.5.0"
+  sha256 "bd5086e233c34977a2b0085fc47086be74636768cc8097ec7951a938ed1a3987"
 
   url "https://orka-tools.s3.amazonaws.com/orka-vm-tools/official/#{version}/orka-vm-tools.pkg",
       verified: "orka-tools.s3.amazonaws.com/orka-vm-tools/official/"
   name "Orka VM TOOLS"
   desc "Orchestration with Kubernetes on Apple"
-  homepage "https://orkadocs.macstadium.com/docs"
+  homepage "https://support.macstadium.com/hc/en-us"
 
   livecheck do
-    url "https://orkadocs.macstadium.com/docs/downloads"
-    regex(%r{href=.*?/official/(\d+(?:\.\d+)+)/orka-vm-tools\.pkg}i)
+    url "https://orka-tools.s3.us-east-1.amazonaws.com/orka-vm-tools/official/latest.json"
+    strategy :json do |json|
+      json["version"]
+    end
   end
 
   pkg "orka-vm-tools.pkg"
