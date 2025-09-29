b733555de0dece98e2178db0fdc25b7976be539a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-29 14:10:35 +0000

    trainerroad 2025.38.1.408

--- Casks/t/trainerroad.rb
+++ Casks/t/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2025.37.4.407"
-  sha256 "c091ebd0c1e816ebc485ebd62d5a51ff5757617978e67fd459ffdeb93c335c02"
+  version "2025.38.1.408"
+  sha256 "a494933f98a65f837d2a23d2328c301a7d6b6ae230c4dd9338dbdf88ab88eb6c"
 
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
       verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
