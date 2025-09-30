165d1cfbfbd04c970d1f19ec2b4c02e66be4b9ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 13:57:39 +0000

    tev 2.5.1

--- Casks/t/tev.rb
+++ Casks/t/tev.rb
@@ -1,9 +1,9 @@
 cask "tev" do
   arch intel: "-intel"
 
-  version "2.5.0"
-  sha256 arm:   "d86d03fd8fbed6fceaa6de9c45ab3ffab3fcefdad18ce0a14e466acc65d5699c",
-         intel: "7c4d68dbde3cd620dd79d78da1e5b76fded130764313045a78a1f94a1f658771"
+  version "2.5.1"
+  sha256 arm:   "5e366d228c8a7df747100b8553a89fc3e27570968259308ac2c8848338f632b9",
+         intel: "d82c74f6a8ecbd56efd539aa55b2ab12d0973ff1d23ecd224d64aff0d19f5aae"
 
   url "https://github.com/Tom94/tev/releases/download/v#{version}/tev#{arch}.dmg"
   name "tev"
