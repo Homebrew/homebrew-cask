edb070e31fce8508a571ac3ff35fb2d821dc0f9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 19:32:20 +0000

    sim-genie 2025.2

--- Casks/s/sim-genie.rb
+++ Casks/s/sim-genie.rb
@@ -1,6 +1,6 @@
 cask "sim-genie" do
-  version "2025.1,317"
-  sha256 "878438ad658171a99ea713db2257ede74fd028ab1cb0798570860555c675b3bf"
+  version "2025.2,322"
+  sha256 "661fa527d528cd984e45e933d8ce8df0fef9a8fa5770b7380298ec3ed75f93d5"
 
   url "https://d1b3av25dd6jdq.cloudfront.net/releases/macos/SimGenie.#{version.csv.first}.#{version.csv.second}.zip",
       verified: "d1b3av25dd6jdq.cloudfront.net/"
