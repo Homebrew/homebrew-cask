6675f9e711d81ffd58ab707659b823f5890c3eb5
Klaus Hipp <khipp@users.noreply.github.com>
2025-09-30 13:04:05 +0200

    kimis 1.18.167

--- Casks/k/kimis.rb
+++ Casks/k/kimis.rb
@@ -1,8 +1,8 @@
 cask "kimis" do
-  version "1.18.166"
-  sha256 "6e93e7780744660ed21b35693233300fe9b1b6bad562fa1c76738ca0b07db2ff"
+  version "1.18.167"
+  sha256 "a76c3605e45df17d55a65c69eab6a8cc05b0941772c703d3789826b90d2b74e0"
 
-  url "https://github.com/Lakr233/Kimis/releases/download/#{version}/Kimis.zip"
+  url "https://github.com/Lakr233/Kimis/releases/download/#{version}/Kimis-v#{version}.zip"
   name "Kimis"
   desc "Desktop client for Misskey"
   homepage "https://github.com/Lakr233/Kimis"
