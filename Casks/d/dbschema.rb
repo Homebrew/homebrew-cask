35607dc3155fa428c880c8602696827045836452
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 15:56:22 +0000

    dbschema 9.9.3

--- Casks/d/dbschema.rb
+++ Casks/d/dbschema.rb
@@ -1,5 +1,5 @@
 cask "dbschema" do
-  version "9.9.2"
+  version "9.9.3"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://dbschema.com/download/dbschema_macos_#{version.dots_to_underscores}.dmg"
