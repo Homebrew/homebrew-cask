d58b5cfa0c32971dcc5158179b8002e99006950c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-02 12:40:33 +0000

    actual 25.10.0

--- Casks/a/actual.rb
+++ Casks/a/actual.rb
@@ -1,15 +1,17 @@
 cask "actual" do
   arch arm: "arm64", intel: "x64"
 
-  version "25.9.0"
-  sha256 arm:   "fe5f1c4318816a900cda06472b9f961e3a600cdb004c55f238a27aa5e79ce48b",
-         intel: "022713f5a50c5bc10c6ec9077afd2d3cc4f1cc226cbde4da9b5b84c88b31d0fa"
+  version "25.10.0"
+  sha256 arm:   "a305e9e9c9e7349e8cc3b4710506e397755a8b90922ab55095badc01db0e9143",
+         intel: "6fe502b5d2c3d46c1bf76bcfbae19cf3d9168e93cf14632774175d032dccea80"
 
   url "https://github.com/actualbudget/actual/releases/download/v#{version}/Actual-mac-#{arch}.dmg",
       verified: "github.com/actualbudget/actual/"
   name "Actual"
   desc "Privacy-focused app for managing your finances"
   homepage "https://actualbudget.org/"
+
+  depends_on macos: ">= :monterey"
 
   app "Actual.app"
 
