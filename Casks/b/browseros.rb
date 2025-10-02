0bc72eb0773c611cd0a4a6899296e52392421ad3
Julio Napurí <julionc@gmail.com>
2025-10-02 01:20:11 -0500

    browseros 0.26.0 (new cask)

---
+++ Casks/b/browseros.rb
@@ -0,0 +1,29 @@
+cask "browseros" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.26.0"
+  sha256 arm:   "f5ee40b027474441ecbf2b91230cac35486c668c6ae1fd1c9b68f5270d21a1b2",
+         intel: "4367f131de9e27bbacf822dc6307b01c665ff06b4c4d2807e4ed1dc379febd67"
+
+  url "https://github.com/browseros-ai/BrowserOS/releases/download/v#{version}/BrowserOS_v#{version}_#{arch}.dmg",
+      verified: "github.com/browseros-ai/BrowserOS/"
+  name "BrowserOS"
+  desc "Open-source Agentic browser"
+  homepage "https://www.browseros.com/"
+
+  livecheck do
+    url :url
+    regex(/v?(\d+(?:\.\d+)+)/i)
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "BrowserOS.app"
+
+  zap trash: [
+    "~/Library/Application Support/BrowserOS",
+    "~/Library/Caches/BrowserOS",
+    "~/Library/Preferences/com.browseros.BrowserOS.plist",
+  ]
+end
