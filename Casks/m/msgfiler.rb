d6cfc1b25315e8884d3c7d300188ac98d29559d6
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-02 22:27:24 -0400

    msgfiler: downgrade to 4.2.0.41
    The 4.3.0 version of `msgfiler` is listed as beta on the first-party
    download page but livecheck has been returning those versions as
    newest because the dmg file name uses the same format as the stable
    version. This downgrades the cask to the newest stable version and
    updates the `livecheck` block to only match the stable versions.

--- Casks/m/msgfiler.rb
+++ Casks/m/msgfiler.rb
@@ -1,15 +1,21 @@
 cask "msgfiler" do
-  version "4.3.0.58,20251001,113245"
-  sha256 "800903b5a2b86f734e679e6a55d915abfe2d39ff599b37f0c2355cf87ff7e48c"
+  version "4.2.0.41,20250910,133221"
+  sha256 "edc967f6018212da25694daf45960e55a89f955d46fa06a6d5e47f3757cf5474"
 
   url "https://files.msgfiler.com/MsgFiler_#{version.csv.first}_#{version.csv.second}_#{version.csv.third}.dmg"
   name "MsgFiler"
   desc "Keyboard-based email filing application for Apple Mail"
   homepage "https://msgfiler.com/"
 
+  # The download page may link to an unstable version that uses the same file
+  # name format as the stable version, so we also check the inner text of the
+  # links to try to only match the stable version.
   livecheck do
     url "https://files.msgfiler.com/"
-    regex(/href=.*?MsgFiler[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-](\d+)\.dmg/i)
+    regex(/
+      href=.*?MsgFiler[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-](\d+)\.dmg[^>]*?>
+      \s*MsgFiler\s+v?\d+(?:\.\d+)+(?:\s+on\s+Gumroad)?\s*<
+    /imx)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
     end
