70dcdbfc27a954ded9ed667461dd49e0caf84890
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 07:27:43 +0000

    servo 2025-10-03

--- Casks/s/servo.rb
+++ Casks/s/servo.rb
@@ -1,6 +1,6 @@
 cask "servo" do
-  version "2025-10-02"
-  sha256 "1066e4cfcf9757a5d0a3ce956a422c83d587d98ee56a211abf1d46701a3b2ed5"
+  version "2025-10-03"
+  sha256 "4126107a984e9286dddf6abbdbf446ab6bcfad84372cf88edd08f0b6fc52bfe8"
 
   url "https://github.com/servo/servo-nightly-builds/releases/download/#{version}/servo-latest.dmg",
       verified: "github.com/servo/servo-nightly-builds/"
