08a1fd479a8fe5df5da3328a2eac39c3a39ee0eb
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-10-03 22:55:41 -0700

    Create container-compose.rb

---
+++ Casks/c/container-compose.rb
@@ -0,0 +1,20 @@
+cask "container-compose" do
+  version "0.4.1"
+  sha256 "4f5bcd3332c256a1c2a4c60e5f3c2fe6fb7cdbc48927eef6e21e0bffd37ba3dd"
+
+  url "https://github.com/Mcrich23/container-compose/releases/download/#{version}/container-compose-macos-arm64"
+  name "Container Compose"
+  desc "Manage Apple Container with Docker Compose files"
+  homepage "https://github.com/mcrich23/container-compose"
+
+  license "MIT"
+
+  # Install the binary into /usr/local/bin (or /opt/homebrew/bin on ARM Macs)
+  binary "container-compose-macos-arm64", target: "container-compose"
+
+  # Livecheck block for version updates
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+end
