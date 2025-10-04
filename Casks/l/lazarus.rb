f3c42dc6083d01a464616079d2d723ab76884b3d
Gregor Zurowski <gregor@zurowski.org>
2025-10-04 19:36:17 +0200

    Upgrade Lazarus to 4.2
    - Upgrade Lazarus to latest version 4.2
    - Support both Intel and ARM64 platforms.
    - Unset quarantine bits in preflight block

--- Casks/l/lazarus.rb
+++ Casks/l/lazarus.rb
@@ -1,29 +1,34 @@
 cask "lazarus" do
-  version "3.6"
-  sha256 "c32f1fcfb16a969153d660a92e2200f32f5e40ec65d42bbd6af829cd9fe74afb"
+  arch arm: "aarch64", intel: "x86_64"
+  folder = on_arch_conditional arm: "aarch64", intel: "x86-64"
 
-  url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version}/Lazarus-#{version}-macosx-x86_64.pkg",
-      verified: "sourceforge.net/lazarus/"
+  version "4.2"
+  sha256 arm:   "5bde40a73b869766d7177522555a7158e805a7c36ce4e154f86e094aaa437135",
+         intel: "aaa966c0d61f07291a92cd3a621a4f5d9862cc7f695246d5062fe1603ea2bf2e"
+
+  url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20#{folder}/Lazarus%20#{version}/lazarus-darwin-#{arch}-#{version}.zip",
+      verified: "downloads.sourceforge.net/lazarus/"
   name "Lazarus"
   desc "IDE for rapid application development"
   homepage "https://www.lazarus-ide.org/"
 
-  deprecate! date: "2025-01-26", because: :no_longer_meets_criteria
+  depends_on formula: "fpc"
 
-  depends_on cask: "fpc-laz"
-  depends_on cask: "fpc-src-laz"
+  app "lazarus/lazarus.app", target: "Lazarus.app"
 
-  pkg "Lazarus-#{version}-macosx-x86_64.pkg"
-
-  uninstall pkgutil: [
-              "org.freepascal.pkg.fpcsrc-320-laz",
-              "org.lazarus-ide.pkg.LazarusIDE",
-            ],
-            delete:  "/Applications/Lazarus.app"
-
-  zap trash: "~/.lazarus"
-
-  caveats do
-    requires_rosetta
+  # Unmark quarantine attributes
+  preflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", staged_path]
   end
+
+  zap trash: [
+    "~/.lazarus",
+    "~/Library/Preferences/lazarus.plist",
+  ]
+
+  caveats <<~EOS
+    You need Xcode Command Line Tools:
+      xcode-select --install
+  EOS
 end
