677b56190427491be5b95ab41e050f334103ed3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 04:10:29 +0000

    manico 3.10,512

--- Casks/m/manico.rb
+++ Casks/m/manico.rb
@@ -1,6 +1,6 @@
 cask "manico" do
-  version "3.10,511"
-  sha256 "a7b63c5035bddceb4d926b754b623a7e24c6f0f8750cb2242d8ddb55a6a0efef"
+  version "3.10,512"
+  sha256 "3b8de36fa74d62b89ef7954af761c94a3a1e94ae2ad4a14ceb99866efa9f03fd"
 
   url "https://manico.im/api/release_manager/downloads/im.manico.Manico/#{version.csv.second}.zip"
   name "Manico"
