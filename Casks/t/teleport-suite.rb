1c096e2addc9060696767326a5f801007a28eb3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 19:46:23 +0000

    teleport-suite 18.2.6

--- Casks/t/teleport-suite.rb
+++ Casks/t/teleport-suite.rb
@@ -1,6 +1,6 @@
 cask "teleport-suite" do
-  version "18.2.5"
-  sha256 "c2ce347f86b58adf9bb26d442fbd3e91fa5d28c9ab9533ea667b1becff169998"
+  version "18.2.6"
+  sha256 "499c84ea338b74dba2c7fd27de842f6b81934be9606e334f8c60aa5afcee15e2"
 
   url "https://cdn.teleport.dev/teleport-#{version}.pkg",
       verified: "cdn.teleport.dev/"
