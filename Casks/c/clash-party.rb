41a83246bb29784152068200b65530d886f9dbfc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 06:46:16 +0000

    clash-party 1.8.8

--- Casks/c/clash-party.rb
+++ Casks/c/clash-party.rb
@@ -1,7 +1,7 @@
 cask "clash-party" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.7"
+  version "1.8.8"
 
   on_catalina :or_older do
     sha256 arm:   "f188e7d39b0d39a65a9d02e6ae4d5b4d4b11edab20c3cc29995ef0f8ee870edd",
@@ -13,8 +13,8 @@ cask "clash-party" do
     pkg "clash-party-catalina-#{version}-#{arch}.pkg"
   end
   on_big_sur :or_newer do
-    sha256 arm:   "992944a6316b88895d7a68ad557ec4867e2670f17f00c3120e1eeb75b2c9e7ed",
-           intel: "e9d232731f924fa8bebed2adad69a3df975d5d41f1ecc4e661d2e8d6bbc6ada9"
+    sha256 arm:   "09544ac95dff9b0fe943119d7dcc6558e82ccfdeda3759ba1ede96d13ace2e0e",
+           intel: "fb516694b3f98ae2168d7055dd43e6fc6d9966e2cf8e157506fc1a7e51f03586"
 
     url "https://github.com/mihomo-party-org/clash-party/releases/download/v#{version}/clash-party-macos-#{version}-#{arch}.pkg",
         verified: "github.com/mihomo-party-org/clash-party/"
