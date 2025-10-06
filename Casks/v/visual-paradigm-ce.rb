cbc31b12fff37bdd5238e3e65b1503fb99b286cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 04:33:14 +0000

    visual-paradigm-ce 17.3,20250943

--- Casks/v/visual-paradigm-ce.rb
+++ Casks/v/visual-paradigm-ce.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm-ce" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.3,20250942"
-  sha256 arm:   "9b864bb43383a8555a8f14ba66374a24a0f3449937aea18bd8929e932a798200",
-         intel: "b40b3eb433e5b0e096ab39ddf3a09770f0d3e9f2404474b34531e45e176491b9"
+  version "17.3,20250943"
+  sha256 arm:   "9ab568814ca031ce669f68887d46ab32660b9acf6e80052bfcfb04b555919887",
+         intel: "50cf242fc1b734a05a16e3b2b3bf3126c15dff83fb314570071f354a5ff938d1"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm Community Edition"
