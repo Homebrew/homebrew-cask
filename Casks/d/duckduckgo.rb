43ac873a7aeb580522b4e48a57d911f04b681e8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 18:54:32 +0000

    duckduckgo 1.159.0

--- Casks/d/duckduckgo.rb
+++ Casks/d/duckduckgo.rb
@@ -1,6 +1,6 @@
 cask "duckduckgo" do
-  version "1.158.0,544"
-  sha256 "add6ceaa98f67317fa98e9d99378c40728173b2414b741bc91073b6f78999187"
+  version "1.159.0,548"
+  sha256 "315633d22d424d7db4d28487e7342ca308a9fa64da34ede1fe09387a3c580552"
 
   url "https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo-#{version.csv.first}.#{version.csv.second}.dmg"
   name "DuckDuckGo"
