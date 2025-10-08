f88161c20ee88692e3eec86584b7c318f2cfa191
Sean Molenaar <smillerdev@me.com>
2025-10-08 18:54:07 +0200

    gnucash: 5.13-3 (intel)

--- Casks/g/gnucash.rb
+++ Casks/g/gnucash.rb
@@ -1,9 +1,9 @@
 cask "gnucash" do
   arch arm: "Arm", intel: "Intel"
 
-  version "5.13-2"
+  version on_arch_conditional arm: "5.13-2", intel: "5.13-3"
   sha256 arm:   "f392c641c8747c27d706552c3f97164302bd98dd42141dca002e3af1c1c3c204",
-         intel: "b8ec717d6f2cd9ed903b78781d534a56ff38027067189ae45fb5b702f31f4b96"
+         intel: "6bc9ffc888a10fc39a25dc529024d14f3c56533612fe8e4bef7c7601342f702b"
 
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.hyphens_to_dots.major_minor}/Gnucash-#{arch}-#{version}.dmg",
       verified: "github.com/Gnucash/gnucash/"
