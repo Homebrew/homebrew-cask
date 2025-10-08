0fad2d0ffd0aee0f066af488f80dbef03eceb09a
Patrick Linnane <patrick@linnane.io>
2025-10-08 14:19:04 -0700

    gnucash: split versions by arch
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/gnucash.rb
+++ Casks/g/gnucash.rb
@@ -1,9 +1,14 @@
 cask "gnucash" do
   arch arm: "Arm", intel: "Intel"
 
-  version on_arch_conditional arm: "5.13-2", intel: "5.13-3"
-  sha256 arm:   "f392c641c8747c27d706552c3f97164302bd98dd42141dca002e3af1c1c3c204",
-         intel: "6bc9ffc888a10fc39a25dc529024d14f3c56533612fe8e4bef7c7601342f702b"
+  on_arm do
+    version "5.13-2"
+    sha256 "f392c641c8747c27d706552c3f97164302bd98dd42141dca002e3af1c1c3c204"
+  end
+  on_intel do
+    version "5.13-3"
+    sha256 "6bc9ffc888a10fc39a25dc529024d14f3c56533612fe8e4bef7c7601342f702b"
+  end
 
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.hyphens_to_dots.major_minor}/Gnucash-#{arch}-#{version}.dmg",
       verified: "github.com/Gnucash/gnucash/"
