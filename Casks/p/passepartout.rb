2417db4617ede61abc1b18a710184c98be926f96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 01:29:00 +0000

    passepartout 3.5.14

--- Casks/p/passepartout.rb
+++ Casks/p/passepartout.rb
@@ -1,9 +1,9 @@
 cask "passepartout" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.5.13"
-  sha256 arm:   "c914078fc7d0a0625e3142cada468079dd8c1574b667eb1f3cf1084f1e039025",
-         intel: "acb39cf4d79b37b78a881bd6fdb004865c2088b932930ca3af4459c4be25fd79"
+  version "3.5.14"
+  sha256 arm:   "cac37d7890c189f35cd9002e48a1acb823701a2213c0a73cdc43ac7836715dbd",
+         intel: "dd14048f761e76245f1a58f50acac06abd725b8402335bbfbb989e52fc696b54"
 
   url "https://github.com/passepartoutvpn/passepartout/releases/download/v#{version}/Passepartout.#{arch}.dmg",
       verified: "github.com/passepartoutvpn/passepartout/"
