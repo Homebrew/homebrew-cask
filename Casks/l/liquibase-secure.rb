7c7ed0b822f40f30a61e27d30ef876f748d774f5
Alejandro Alvarez <avazquez@liquibase.com>
2025-10-09 08:28:16 +0200

    Update Casks/l/liquibase-secure.rb
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/liquibase-secure.rb
+++ Casks/l/liquibase-secure.rb
@@ -5,7 +5,7 @@ cask "liquibase-secure" do
   url "https://package.liquibase.com/downloads/secure/homebrew/liquibase-secure-#{version}.tar.gz"
   name "Liquibase Secure"
   desc "Database change management tool"
-  homepage "https://www.liquibase.com/"
+  homepage "https://www.liquibase.com/liquibase-secure"
 
   livecheck do
     url "https://repo.liquibase.com/releases/secure/"
