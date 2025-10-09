3edf6c2f171dc247750fec6006222c0c909d15f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 16:38:11 +0000

    lunatask 2.1.12

--- Casks/l/lunatask.rb
+++ Casks/l/lunatask.rb
@@ -1,6 +1,6 @@
 cask "lunatask" do
-  version "2.1.10"
-  sha256 "94b6b946f16a30e0a53a2974a0f3622b85a5d2161d5dedfdf87f48aeffc877bc"
+  version "2.1.12"
+  sha256 "dc8ace729121db4abdd04bffd2758d8da380e2abf56a7d0e7f0ee9037fa053c6"
 
   url "https://github.com/lunatask/lunatask/releases/download/v#{version}/Lunatask-#{version}-universal.dmg",
       verified: "github.com/lunatask/lunatask/"
