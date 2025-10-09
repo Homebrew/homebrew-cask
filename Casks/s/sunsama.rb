ccd7abb5f04e1fe79b16b0957f6223d7e2e4aaad
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-08 20:20:37 -0400

    sunsama 3.2.0
    `sunsama` is autobumped but the workflow failed to update to version
    3.2.0 because `brew audit` failed with an "Artifact defined :monterey
    as the minimum macOS version but the cask declared a depends_on stanza
    with a minimum macOS version of :big_sur" error. This updates the
    version and `depends_on macos:` value.

--- Casks/s/sunsama.rb
+++ Casks/s/sunsama.rb
@@ -1,9 +1,9 @@
 cask "sunsama" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.1.2,250711rlkq87w2m"
-  sha256 arm:   "e0a72b354b0254871ac2fffc0ca3047975dfe38b365cbdd0455d7805368cc0c6",
-         intel: "5ad36323ba47b2a3b9b514ad89d87399ba7aa2d05619be00b149e404574adfa0"
+  version "3.2.0,251008ae7l1ezsz"
+  sha256 arm:   "68464061b7566d7d480dea231216ea00913c279c388ade74334ec501d21e115e",
+         intel: "a6388236d9477d43e55b908bdbb4d2596aed4e0841041093826e9d7d82cac7cf"
 
   url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/2003096gmmnl0g1/"
@@ -25,7 +25,7 @@ cask "sunsama" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Sunsama.app"
 
