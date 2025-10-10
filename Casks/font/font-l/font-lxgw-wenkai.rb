32dc1f6f1db479c6ae90399a2041b42c6d4253cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 16:01:09 +0000

    font-lxgw-wenkai 1.521

--- Casks/font/font-l/font-lxgw-wenkai.rb
+++ Casks/font/font-l/font-lxgw-wenkai.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai" do
-  version "1.520"
-  sha256 "3a763543bec896e3c1badc9808bc804116a5e3d26f9f9592dacc834c9e799d8c"
+  version "1.521"
+  sha256 "b4470748b925ed0f7a943c4fa64fc080200e8a15fcd9d67036d5398506b75534"
 
   url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip"
   name "LXGW WenKai"
