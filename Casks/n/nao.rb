95815ecbbad2036f1ec4d6f5ed30b85037716d5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 19:24:53 +0000

    nao 0.8.4

--- Casks/n/nao.rb
+++ Casks/n/nao.rb
@@ -1,9 +1,9 @@
 cask "nao" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.8.3"
-  sha256 arm:   "4d2fd266116918a699cc339839f6d01405cf61d357c5298d819cf34946ac9041",
-         intel: "a8e11c565219e4230526dbdb151287389589e0d1e31ca33dce6520b05f71920a"
+  version "0.8.4"
+  sha256 arm:   "9734e08c270bf03f66c5a52f917ab104ef8557f9478d0266c5c179807b99d833",
+         intel: "1fc10013346fa33fda9de118290e4f5f211d9504238eb05c6647b25c35459e33"
 
   url "https://storage.googleapis.com/nao-releases/nao/darwin-#{arch}/#{version}/nao.#{arch}.#{version}.dmg",
       verified: "storage.googleapis.com/nao-releases/nao/"
