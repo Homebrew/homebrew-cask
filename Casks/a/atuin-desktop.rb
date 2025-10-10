312fc61a04c89399861a4ed71423038ffcafbd3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 00:52:49 +0000

    atuin-desktop 0.1.6

--- Casks/a/atuin-desktop.rb
+++ Casks/a/atuin-desktop.rb
@@ -1,9 +1,9 @@
 cask "atuin-desktop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.1.5"
-  sha256 arm:   "942e8aa3e8c9c36568e586be40769abab483cde7f73a0078d4804344a98c986f",
-         intel: "785af5e6213e158b292b6304ad7d31e0c810786360ceb62adc22cea6354c145b"
+  version "0.1.6"
+  sha256 arm:   "33c291cf1c28d89828fb61b62db87a0a2e0d8acfb90c80bb063ff940acb65fff",
+         intel: "905711fe1d0103ca274c5d45cc6298897b40076bc403125ecef7ee290ff82b72"
 
   url "https://github.com/atuinsh/desktop/releases/download/v#{version}/Atuin_#{version}_#{arch}.dmg",
       verified: "github.com/atuinsh/desktop/"
