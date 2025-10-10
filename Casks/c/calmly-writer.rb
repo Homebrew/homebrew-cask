11861186b1c2b241fe3c7e70f61bc2dbcc5f77e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 12:49:06 +0000

    calmly-writer 2.0.61

--- Casks/c/calmly-writer.rb
+++ Casks/c/calmly-writer.rb
@@ -2,9 +2,9 @@ cask "calmly-writer" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "2.0.60"
-  sha256 arm:   "5eb30d1a3a7b4fa9a356a3edc525bf428645b0e50509a687264efb9650f78ebd",
-         intel: "1f94895515314c655cf5b8b8622046482764355f0c56a306051cc30f6b4b6096"
+  version "2.0.61"
+  sha256 arm:   "ed47e07a70bf8ff242aaef9e310ae743bcd878f268e23638c3f7dda846e933e0",
+         intel: "a2507d3c97672a50e049fd8be391e81d2b16a28ca5ad35f202fbf96e41705a91"
 
   url "https://www.calmlywriter.com/releases/Calmly%20Writer-#{version}#{arch}.dmg"
   name "Calmly Writer"
