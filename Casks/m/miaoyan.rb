44c9d50c9bde2ed5eb7ee28eeae0cbb5535de052
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-11 10:08:08 +0000

    miaoyan 2.1.2

--- Casks/m/miaoyan.rb
+++ Casks/m/miaoyan.rb
@@ -1,6 +1,6 @@
 cask "miaoyan" do
-  version "2.0.1"
-  sha256 "8d902c97cc063474ef291a2ffdb28ea7ea19e009c06a304808b8a927477ae30c"
+  version "2.1.2"
+  sha256 "ccdb08a303b3409b1cbac66c245a4894ef7df32562dedf057e4860886700f7f5"
 
   url "https://gw.alipayobjects.com/os/k/app/MiaoYan_V#{version}.zip",
       verified: "gw.alipayobjects.com/"
