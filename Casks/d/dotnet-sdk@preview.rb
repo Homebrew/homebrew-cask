d39908b85f31bbbd7e263edba4caf7050d22123e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 22:00:06 +0000

    dotnet-sdk@preview 9.0.306

--- Casks/d/dotnet-sdk@preview.rb
+++ Casks/d/dotnet-sdk@preview.rb
@@ -1,9 +1,9 @@
 cask "dotnet-sdk@preview" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.0.305"
-  sha256 arm:   "7bbaf37675331cc58704bd4201800e8049658f4670ba256c86e7c42d49c7389b",
-         intel: "02eaf71a1485949b95d8b75e47c92439726e30eba3565e5dace0e120e9a2384d"
+  version "9.0.306"
+  sha256 arm:   "7bb202952ee7f64873c2f195fc2eea5a0191c443b1644f382c44c9f3b2bcd3e7",
+         intel: "5f50fe83c5b3ca85c48276c1c35402be7c83916b0f0f5fef3fa2c893539746df"
 
   url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
   name ".NET SDK"
