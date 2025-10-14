0d07150ec86ca51c4dd1adf94a39c2b803f44984
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 06:48:49 +0000

    desktime 6.1.2

--- Casks/d/desktime.rb
+++ Casks/d/desktime.rb
@@ -1,9 +1,9 @@
 cask "desktime" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.1.1"
-  sha256 arm:   "ce00895af8e15d1daa846b0afc91f0f5957e6db366eaf81f6205fe1280dbbc1b",
-         intel: "1edfc602e62cac8b7263c0f8d5c61d0182748680b526f6b9ae17d7e1fb313b94"
+  version "6.1.2"
+  sha256 arm:   "1731ed0dda0364bb601deeaa29db95de4c0b4beeaaebfd398d137bca013532ad",
+         intel: "a2c8b19952663a06b0971176d4130716f3ba7ff0c3708ddd439a1f8dbf337b87"
 
   url "https://desktime.com/storage/updates/electro-builder-auto-updater/stable/DeskTime-#{version}-#{arch}.dmg"
   name "DeskTime"
