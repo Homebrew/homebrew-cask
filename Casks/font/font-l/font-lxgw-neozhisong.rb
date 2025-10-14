5809dfa778f99e43957bceb6047b8ac4f885e678
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 16:26:07 +0000

    font-lxgw-neozhisong 1.040

--- Casks/font/font-l/font-lxgw-neozhisong.rb
+++ Casks/font/font-l/font-lxgw-neozhisong.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neozhisong" do
-  version "1.039"
-  sha256 "d12338ee2219f787b44b675b49908ea5b2477c97e1ce2cce64c416afb01b6222"
+  version "1.040"
+  sha256 "9c91f91340996f3dc28d12a3883c68913b76410cc2d6d2073400a6189d3895ea"
 
   url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSong.ttf"
   name "LXGW Neo ZhiSong"
