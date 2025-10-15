b36b0e5aaeb53c95eb86f49baf46f61ce409478f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-10-15 13:33:49 -0400

    fishing-funds 8.6.2

--- Casks/f/fishing-funds.rb
+++ Casks/f/fishing-funds.rb
@@ -1,9 +1,9 @@
 cask "fishing-funds" do
   arch arm: "-arm64"
 
-  version "8.6.1"
-  sha256 arm:   "0a8c6f48c5c27f050fe47161346b76d31944d00f58c094a15e9fe1a6dab650c9",
-         intel: "bdcacc4b06e6c5d9efedd0d7f513932aa24ce7e9aed608d2e4ad81f8a0da10cb"
+  version "8.6.2"
+  sha256 arm:   "5e063fbcefada24a51cae22c1b49b584c37f47ee130bf3cf00ada7a78e39dbdb",
+         intel: "6be7de87e291a2a5443572075995c031049b09c146aa603a2ba5b1ca296fbaf8"
 
   url "https://github.com/1zilc/fishing-funds/releases/download/v#{version}/Fishing-Funds-#{version}#{arch}.dmg",
       verified: "github.com/1zilc/fishing-funds/"
@@ -13,7 +13,7 @@ cask "fishing-funds" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Fishing Funds.app"
 
