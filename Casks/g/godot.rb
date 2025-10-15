22a856aa58e7c0e634c4c9a5525697680cf68567
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-10-15 13:34:17 -0400

    godot 4.5.1

--- Casks/g/godot.rb
+++ Casks/g/godot.rb
@@ -1,6 +1,6 @@
 cask "godot" do
-  version "4.5"
-  sha256 "abb7cabe99fead0bd63ce03bd8c611734b2941f9b90051e8e3b5c6691527954d"
+  version "4.5.1"
+  sha256 "65c27959d02aaacfc131ec7ecb90179ba8045200cb02982bf2be96d117010b8a"
 
   url "https://github.com/godotengine/godot/releases/download/#{version}-stable/Godot_v#{version}-stable_macos.universal.zip",
       verified: "github.com/godotengine/godot/"
@@ -15,6 +15,7 @@ cask "godot" do
   end
 
   conflicts_with cask: "godot@3"
+  depends_on macos: ">= :big_sur"
 
   app "Godot.app"
   binary "#{appdir}/Godot.app/Contents/MacOS/Godot", target: "godot"
