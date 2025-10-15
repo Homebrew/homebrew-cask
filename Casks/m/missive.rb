a33a6c4fb2fcf33486afcce436505ca12dad4285
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-10-15 19:27:08 -0400

    missive 11.12.4

--- Casks/m/missive.rb
+++ Casks/m/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
-  version "11.12.0"
-  sha256 "a8f7ce60ab01fa641a3dd9dbb6e4a351909774eb16d9dc6fe6471d12f857228a"
+  version "11.12.4"
+  sha256 "d4cfc97624a02f38844d2d28dba14f92299ba546359236f59e322d571a5fbd4e"
 
   url "https://downloads.missiveapp.com/#{version}/Missive-#{version}.dmg"
   name "Missive"
@@ -13,6 +13,7 @@ cask "missive" do
   end
 
   auto_updates true
+  depends_on macos: ">= :monterey"
 
   app "Missive.app"
 
