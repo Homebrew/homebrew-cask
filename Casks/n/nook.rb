9e685f862a72299a3929412704cbb8f924eb10a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-16 22:19:36 +0000

    nook 1.0.4

--- Casks/n/nook.rb
+++ Casks/n/nook.rb
@@ -1,6 +1,6 @@
 cask "nook" do
-  version "1.0.3"
-  sha256 "c4304c35e4a1d9e931b428616c257dc7ae9769dd95d5c7ef0f1a2a9e5330b141"
+  version "1.0.4"
+  sha256 "3f8fb50abfad46a3daa8787640312f601b7a11d3072df4324ff9b57e7c92e147"
 
   url "https://github.com/nook-browser/Nook/releases/download/v#{version}/Nook-v#{version}.dmg",
       verified: "github.com/nook-browser/Nook/"
