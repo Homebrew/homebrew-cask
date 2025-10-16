db63b37726ce726bf3b08a7adf74a7637f68cf90
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-16 12:42:42 -0400

    anytype@alpha 0.50.10-alpha

--- Casks/a/anytype@alpha.rb
+++ Casks/a/anytype@alpha.rb
@@ -1,9 +1,9 @@
 cask "anytype@alpha" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.50.9-alpha"
-  sha256 arm:   "44d16636a59b64d4bbeb8bf46aa1b202089f719e7f6b3bb102b51ef14816f866",
-         intel: "82fe7437f96d57023e00fed4203da07794666d012f12fdf4dce3792fbcd0eaea"
+  version "0.50.10-alpha"
+  sha256 arm:   "155c66e8e399ae3a659e99eefd3034a01b5a46337d003a3e30d47dea62839043",
+         intel: "626ccc32a43fd60151c074bddbb206d47a4d9ee3fd4dfdce559d134c0a348c5a"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
