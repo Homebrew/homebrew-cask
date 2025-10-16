4f474022238e40672f1bf3760f5bf60e346f2571
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-16 07:05:31 +0000

    jan 0.7.2

--- Casks/j/jan.rb
+++ Casks/j/jan.rb
@@ -1,6 +1,6 @@
 cask "jan" do
-  version "0.7.1"
-  sha256 "007bdff6cef6e8545ebcaabaa0270553474813d58f117c554e9eca22aa9651ae"
+  version "0.7.2"
+  sha256 "7a31e69415a43fcabde0903544adbab45734bfbda5babd7c66de3b771ed8fce9"
 
   url "https://github.com/janhq/jan/releases/download/v#{version}/jan-mac-universal-#{version}.zip",
       verified: "github.com/janhq/jan/"
