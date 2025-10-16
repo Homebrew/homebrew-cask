b87cf39c3f428292b9933e2109871f129ef3c04a
Klaus Hipp <khipp@users.noreply.github.com>
2025-10-16 16:06:39 +0200

    scilab 2026.0.0

--- Casks/s/scilab.rb
+++ Casks/s/scilab.rb
@@ -1,26 +1,19 @@
 cask "scilab" do
   arch arm: "arm64", intel: "x86_64"
 
-  on_arm do
-    version "2025.1.0"
-    sha256 "506d698fe7c16d6645b981100c5a95bb6dc03606b15ad01e1798add2e87c05b3"
-    livecheck do
-      url "https://www.scilab.org/download/latest/"
-      regex(/scilab[._-]v?(\d+(?:\.\d+)+)/i)
-    end
-  end
-  on_intel do
-    version "2025.0.0"
-    sha256 "d02ae625f8d3b7b1a4a3a7ad0671fd6142edab0c01ee90d81c3c04fda92e5ae0"
-    livecheck do
-      skip "Legacy version"
-    end
-  end
+  version "2026.0.0"
+  sha256 arm:   "a267f1b7341f930980a43c5b1a54ca9f3729b5f7618c52519705bda065b0d4aa",
+         intel: "6af9e86f7805552ffac8c4276fa34178a790a205fc9fa8492168a34901066f1b"
 
   url "https://www.scilab.org/download/#{version}/scilab-#{version}-#{arch}.dmg"
   name "Scilab"
   desc "Software for numerical computation"
   homepage "https://www.scilab.org/"
+
+  livecheck do
+    url "https://www.scilab.org/download/latest/"
+    regex(/scilab[._-]v?(\d+(?:\.\d+)+)/i)
+  end
 
   app "scilab-#{version}.app"
   binary "#{appdir}/scilab-#{version}.app/Contents/bin/scilab"
