023fc489060e5978b3d4b766985a2409c92ea788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-16 01:20:12 +0000

    mattermost 5.13.2

--- Casks/m/mattermost.rb
+++ Casks/m/mattermost.rb
@@ -1,9 +1,9 @@
 cask "mattermost" do
   arch arm: "m1", intel: "x64"
 
-  version "5.13.1"
-  sha256 arm:   "0485f5f57ec848f4d40183e7db867cfe4bc17b3fe385675c49861579c8e2aa2b",
-         intel: "1ed1338e4e0611e0ed5760a585caed33a4a93672108e0771cdb5d375efae52a7"
+  version "5.13.2"
+  sha256 arm:   "1bc0ac5cbb8e70409f2572f9a2643a67822f85c1cc8f25c9850b80733170ecc0",
+         intel: "757742fc832dddb43aece57500019a40ba47c7e4f4be4037deb3bdcbd38db26d"
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-#{arch}.zip"
   name "Mattermost"
