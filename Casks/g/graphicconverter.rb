953cc3e4ae7a9bcd216306f37aaee3f9057a03a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-17 16:14:36 +0000

    graphicconverter 12.4.3

--- Casks/g/graphicconverter.rb
+++ Casks/g/graphicconverter.rb
@@ -1,6 +1,6 @@
 cask "graphicconverter" do
-  version "12.4.2,6956"
-  sha256 "60f34a2a39b777c6c4b9bfc7062dbddbcd6c3e98ebf79ee041eb5cfb7ee9abf7"
+  version "12.4.3,7002"
+  sha256 "bb6ba0afec17095f1e8de6465282998c37e46a4c36a68fb4518b57289a876d17"
 
   url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.csv.second}.zip",
       verified: "lemkesoft.info/"
