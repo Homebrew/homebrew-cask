2450cb801060758c29eaab2f48aa2d4fda272b42
Fernando Olivares <fernando.olivares@me.com>
2025-10-17 11:58:42 -0600

    hey-desktop 1.2.18

--- Casks/h/hey-desktop.rb
+++ Casks/h/hey-desktop.rb
@@ -5,18 +5,18 @@ cask "hey-desktop" do
   sha256 arm:   "36e747be2cd253cff7c3008ab7dee6bdb18b57df6e8b790945a06f1138daaf88",
          intel: "7f9b3444e544ac33060a97c9a963ab56050387209fafb1692df530db2fa6d41e"
 
-  url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}#{arch}-mac.zip",
-      verified: "hey-desktop.s3.amazonaws.com/"
+  url "https://hey.com/desktop/HEY-#{version}#{arch}-mac.zip"
   name "HEY"
   desc "Access the HEY email service"
   homepage "https://hey.com/"
 
   livecheck do
-    url "https://hey-desktop.s3.amazonaws.com/latest-mac.yml"
+    url "https://hey.com/desktop/latest-mac.yml"
     strategy :electron_builder
   end
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "HEY.app"
 
