47c42e7075727c49fd5016328d0b4f3b75385ba8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 01:26:08 +0000

    powerpanel 2.7.0

--- Casks/p/powerpanel.rb
+++ Casks/p/powerpanel.rb
@@ -1,6 +1,6 @@
 cask "powerpanel" do
-  version "2.6.1"
-  sha256 "2208c48df9b1c893a66de7b1652e1ff30ca1728948860db9fcf919c7d066fed4"
+  version "2.7.0"
+  sha256 "101673d616bfcb11b6b3b82ac98246d22f7c9fa56200740789812a7dba01d628"
 
   url "https://dl4jz3rbrsfum.cloudfront.net/software/PPP_Mac_v#{version}.pkg",
       verified: "dl4jz3rbrsfum.cloudfront.net/"
