dc5f1495f8fa277869ad80ff90a9aa0dd5f93910
John Benninghoff <jbenninghoff@mac.com>
2025-10-18 09:04:23 -0500

    display-pilot 1.8.0.0

--- Casks/d/display-pilot.rb
+++ Casks/d/display-pilot.rb
@@ -1,8 +1,8 @@
 cask "display-pilot" do
-  version "1.7.0.0,250820135838"
-  sha256 "fedd1a079fc0bca3736d12ffb44f465f7025e93e6f9787b7108364cb1861b92d"
+  version "1.8.0.0,251016161541"
+  sha256 "a04ad18faa024e1fa8c4e8a2b8e79e1fe1bb3c57c922d6cadc5067c310713a04"
 
-  url "https://esupportdownload.benq.com/esupport/VERTICAL%20&%20PROFESSIONAL%20DISPLAY/Software/Display%20Pilot%202/Display%20Pilot%202%20for%20Mac_V#{version.csv.first}_Mac_#{version.csv.second}.zip"
+  url "https://esupportdownload.benq.com/esupport/VERTICAL%20&%20PROFESSIONAL%20DISPLAY/Software/Display%20Pilot%202/Display%20Pilot%202%20for%20Mac_v#{version.csv.first}_Mac_#{version.csv.second}.zip"
   name "Display Pilot 2"
   desc "Display control utility"
   homepage "https://www.benq.com/en-ap/monitor/software/display-pilot-2.html"
@@ -15,7 +15,6 @@ cask "display-pilot" do
   end
 
   depends_on macos: ">= :big_sur"
-  container nested: "DisplayPilot2-Setup-#{version.csv.first}/Display Pilot 2Setup-#{version.csv.first}-release.dmg"
 
   app "Display Pilot 2.app"
 
