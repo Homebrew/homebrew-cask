263040b933c151e7d34121e6f766fda226be9583
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 21:37:33 +0000

    cap 0.3.75

--- Casks/c/cap.rb
+++ Casks/c/cap.rb
@@ -2,12 +2,12 @@ cask "cap" do
   arch arm: "aarch64", intel: "x86_64"
 
   on_arm do
-    version "0.3.74,01K7T7GHBXEN31WXDD30SMQ3CS"
-    sha256 "0164b207a3820e9b1006d066fd55f04571edd3628b6a9abb3e6f1fee8bc301c3"
+    version "0.3.75,01K7YZ7FXTR5G8DNM1ZVTZ6F3F"
+    sha256 "76255dcc70afdd971fad2f25c1fa108a73a62410317b0e81a7103da6ec8bfa95"
   end
   on_intel do
-    version "0.3.74,01K7T7JBW1126XT4FBF6HT5SM5"
-    sha256 "6cf4a6151fc8ab28eeef3ff4d7bab43202f935bd7e9f13aa370f13580599ed66"
+    version "0.3.75,01K7YZF91DRJP77VWTMWF1BVPF"
+    sha256 "0496a63a86ae1f2ddb59fed15275336f9e37024ed4a16b8d389e1c740313880d"
   end
 
   url "https://cdn.crabnebula.app/asset/#{version.csv.second}",
