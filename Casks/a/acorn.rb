3bcb7f8fc4b6a1cbe8720178431f605483bc2040
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-20 18:39:26 +0000

    acorn 8.3.1

--- Casks/a/acorn.rb
+++ Casks/a/acorn.rb
@@ -1,6 +1,6 @@
 cask "acorn" do
-  version "8.3"
-  sha256 "1e1ce377df8700e5533c751a46b26a503884ef84981df90b4fd0a77a4a4a1d67"
+  version "8.3.1"
+  sha256 "a3e5515a88e6480d6af802da4f4f3aa81f21214b18164c7338adea66f58db958"
 
   url "https://flyingmeat.com/download/Acorn-#{version}.zip"
   name "Acorn"
