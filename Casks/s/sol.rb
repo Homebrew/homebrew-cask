3889e61413b37ba940af253962040264603f03fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-20 07:34:24 +0000

    sol 2.1.276

--- Casks/s/sol.rb
+++ Casks/s/sol.rb
@@ -1,6 +1,6 @@
 cask "sol" do
-  version "2.1.275"
-  sha256 "58b9887e3bb89ce9b4921f0a1923b918bd5673cb231761b530f2b49ed2f13973"
+  version "2.1.276"
+  sha256 "385d6ec54948588a72aefb13da042bcb650a96ccd4c2432eba4afc997faadc82"
 
   url "https://github.com/ospfranco/sol/releases/download/#{version}/#{version}.zip"
   name "Sol"
