9f00955f94d62fe8045d4922dd145caf3bbb5d06
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 18:57:23 +0000

    elasticvue 1.9.0

--- Casks/e/elasticvue.rb
+++ Casks/e/elasticvue.rb
@@ -1,9 +1,9 @@
 cask "elasticvue" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.8.0"
-  sha256 arm:   "cd36394a4a64b658123c377b3c61ca79cc448183e295aabdbf01c48f1325e54e",
-         intel: "5cd7d9844f73b86592f3a9a30323293c5a4e17b8b40df6f5383cd4bcd03f12aa"
+  version "1.9.0"
+  sha256 arm:   "0960fbe9ce21a55cfbd2b2f40684d4d8b6870c2a557b8ca15adca60d20dda069",
+         intel: "46172e147960c69df1706ac513e2cbceb4d454d7544f98a2ab0ecf3dd87d7a97"
 
   url "https://github.com/cars10/elasticvue/releases/download/v#{version}/elasticvue_#{version}_#{arch}.dmg"
   name "Elasticvue"
