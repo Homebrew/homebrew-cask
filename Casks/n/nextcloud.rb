03f0f832428a3ba942ce965a6b25303fa316cdca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 16:12:38 +0000

    nextcloud 4.0.0

--- Casks/n/nextcloud.rb
+++ Casks/n/nextcloud.rb
@@ -8,8 +8,8 @@ cask "nextcloud" do
     end
   end
   on_monterey :or_newer do
-    version "3.17.3"
-    sha256 "3dcefb69edd449646a5bea67a676f2c2738c2ed082d1e1219de9d7da434d10fd"
+    version "4.0.0"
+    sha256 "35c6eec1215a9f4f8bb2efdafcd1ca7e42e04ffa39b0e64bcf0b6de43732502e"
 
     livecheck do
       url "https://download.nextcloud.com/desktop/releases/Mac/Installer/"
