c1a5a0425a64fc8f9e4ef379dcb33322c6558677
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 07:04:33 +0000

    imgotv 6.5.7

--- Casks/i/imgotv.rb
+++ Casks/i/imgotv.rb
@@ -1,6 +1,6 @@
 cask "imgotv" do
-  version "6.5.4"
-  sha256 "6ce547cd60e9a2156dd3e0d21091b057964b1a380bfef9994353186e866bee3c"
+  version "6.5.7"
+  sha256 "36e882b10da45f09a199d5cea6e7838ad05cab66e7736176e75744706c91c8c8"
 
   url "https://download.imgo.tv/app/mac/#{version}/mgtv-mango-#{version}.dmg",
       verified: "download.imgo.tv/"
