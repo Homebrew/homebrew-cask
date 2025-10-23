85f6ca0ad8f0b40780598e813ae69e44d3cc704d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 18:41:21 +0000

    aqua-voice 0.10.1

--- Casks/a/aqua-voice.rb
+++ Casks/a/aqua-voice.rb
@@ -1,9 +1,9 @@
 cask "aqua-voice" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.10.0"
-  sha256 arm:   "b000fdad6d6f7ce8eea7ddce1913b3f7f7f181d93470f1d3ce0bc91d23dfcc24",
-         intel: "4e7d758a77b44b006555b433ce50c820aacf0def8c349d64efeb6b4be0724fcc"
+  version "0.10.1"
+  sha256 arm:   "350c27b77c2a0ba6fd69f0835dd00d1ee7befaa47687fd1dac8a4f8a9eca04c9",
+         intel: "9aa39590db38c2908ec80bcf1bb2608f2e6d1888f1f0bc3c401edcd4834abe41"
 
   url "https://d1a1dx1sgvjqrz.cloudfront.net/aqua-voice-updates/darwin/#{arch}/Aqua+Voice-#{version}-#{arch}.dmg",
       verified: "d1a1dx1sgvjqrz.cloudfront.net/"
