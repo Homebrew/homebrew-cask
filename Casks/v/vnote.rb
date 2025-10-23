c2b86f34df45ba309354c0833cfdc56fdb2466a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 17:11:37 +0000

    vnote 3.20.0

--- Casks/v/vnote.rb
+++ Casks/v/vnote.rb
@@ -1,6 +1,6 @@
 cask "vnote" do
-  version "3.19.2"
-  sha256 "404ef3a753429cc63a37b114a9c7d7c711b43a6fd7987748acc4760126a12e15"
+  version "3.20.0"
+  sha256 "e75d74da3704e2c1742bd0c76297ca5ba4d5b40c483332c4906f3ea8bfb825a4"
 
   url "https://github.com/vnotex/vnote/releases/download/v#{version}/VNote-#{version}-mac-universal.zip",
       verified: "github.com/vnotex/vnote/"
