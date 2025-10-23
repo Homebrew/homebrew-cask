e6d7ff12f2ebadb1402075df1836b55870508af7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 13:43:07 +0000

    smoothcsv 3.7.0

--- Casks/s/smoothcsv.rb
+++ Casks/s/smoothcsv.rb
@@ -1,6 +1,6 @@
 cask "smoothcsv" do
-  version "3.6.1"
-  sha256 "c52297c7cb8eb9d0c08f5f5e7543f9b0a0db6cae492758ee15219ba46ee9e26b"
+  version "3.7.0"
+  sha256 "aedecfed09e988d767e821021a142222db5bb1183cb728f28dcd3f2e1fc90392"
 
   url "https://github.com/kohii/smoothcsv3/releases/download/v#{version}/SmoothCSV_#{version}_universal.dmg",
       verified: "github.com/kohii/smoothcsv3/"
