5b991d29a2078e772a2d4c74391ce5751c82f6ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 14:07:00 +0000

    zoho-mail 1.7.1

--- Casks/z/zoho-mail.rb
+++ Casks/z/zoho-mail.rb
@@ -2,9 +2,9 @@ cask "zoho-mail" do
   arch arm: "arm64-"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "1.7.0"
-  sha256 arm:   "7c92b502b47a52b5ac15fb3a536bf4ccbd1a4a160ed4ba50411fa9532bd01299",
-         intel: "df4468529c217bc5b9ec124da8f95fb3b0aff72e6dce1f171dbec054c1f45254"
+  version "1.7.1"
+  sha256 arm:   "68bff06db5e8f2c4010fd7ef207d42c0fc5ac41d48266ffbded52049b2d3b2de",
+         intel: "6c55913f75000c37a9dec49107e7286f7f68f63a3a6b9c137a372ddd1c2e67f1"
 
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-lite-installer-#{arch}v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
