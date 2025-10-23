5de318459590b2db9715dd8b094b37c26f99440b
Rui Chen <rui@chenrui.dev>
2025-10-22 23:03:34 -0400

    chainner 0.25.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/chainner.rb
+++ Casks/c/chainner.rb
@@ -1,8 +1,8 @@
 cask "chainner" do
-  version "0.25.0"
-  sha256 "517bebe7abc144b43729045e68671821a066874881f93ab300efdccfa2eb3bfd"
+  version "0.25.1"
+  sha256 "18f1c38f86ec1c41a311a0e9db866b715bfca605b0e132acb325a45663ceabda"
 
-  url "https://github.com/chaiNNer-org/chaiNNer/releases/download/v#{version}/chaiNNer-#{version}-universal-macos.dmg",
+  url "https://github.com/chaiNNer-org/chaiNNer/releases/download/v#{version}/chaiNNer-#{version}-macos-universal.dmg",
       verified: "github.com/chaiNNer-org/chaiNNer/"
   name "chaiNNer"
   desc "Flowchart-based image processing GUI"
@@ -12,6 +12,8 @@ cask "chainner" do
     url :url
     strategy :github_latest
   end
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: ">= :big_sur"
 
