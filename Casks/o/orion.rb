b9cea2dd00d0ee3ce3b0a1333d2022c8c69373c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-25 13:16:25 +0000

    orion 0.99,137
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/o/orion.rb
+++ Casks/o/orion.rb
@@ -1,7 +1,7 @@
 cask "orion" do
   on_catalina :or_older do
-    version "0.99,136"
-    sha256 "6f7906497cbac3e9f6356b1b8c451cc5acbaec1b92d8574dd738d3577c1eab20"
+    version "0.99,137"
+    sha256 "2a92f0b83632585572fdf5677a99a6e89c4514a7bddf894f1d30ba6ac4773a9a"
 
     url "https://cdn.kagi.com/updates/10_15/#{version.csv.second}.zip"
 
@@ -11,8 +11,8 @@ cask "orion" do
     end
   end
   on_big_sur do
-    version "0.99,136"
-    sha256 "983df8d31e5f45ffd12402c3fe5d6b356baf5be50f6003609490e5ce83b20049"
+    version "0.99,137"
+    sha256 "bd956362e77b5dd997d277b87e03ff996a7fff32c84d4c5ced69aa58e69a38a5"
 
     url "https://cdn.kagi.com/updates/11_0/#{version.csv.second}.zip"
 
@@ -22,8 +22,8 @@ cask "orion" do
     end
   end
   on_monterey do
-    version "0.99,136"
-    sha256 "598c6af3141d7cfabf82107dbdfbb1f1ed360ceb1634779cda725eb0d24a3d7c"
+    version "0.99,137"
+    sha256 "fe7f1bc428a05f10dd5679139483f4331266ca275b2fa3a93dafda0c1b5203e9"
 
     url "https://cdn.kagi.com/updates/12_0/#{version.csv.second}.zip"
 
@@ -33,8 +33,8 @@ cask "orion" do
     end
   end
   on_ventura do
-    version "0.99,136"
-    sha256 "feb5bb9840de8be9f3cc977c4d723e7f0d21b9df9944a3705dad5351172899bd"
+    version "0.99,137"
+    sha256 "2ee64d2fddf897ad9677abfe260d7c90d2c2a62da2e0c5e6ceceb2f02f6bbd87"
 
     url "https://cdn.kagi.com/updates/13_0/#{version.csv.second}.zip"
 
@@ -44,8 +44,8 @@ cask "orion" do
     end
   end
   on_sonoma do
-    version "0.99,136"
-    sha256 "8098d6ba30227897928f85a0194515ded85fb92587f9f8cb30f469ca66990782"
+    version "0.99,137"
+    sha256 "8bf88d6a6348b0dc354a4244ec7bafedff4c1ba7723200c6f2ddf29d2723842b"
 
     url "https://cdn.kagi.com/updates/14_0/#{version.csv.second}.zip"
 
@@ -55,8 +55,8 @@ cask "orion" do
     end
   end
   on_sequoia :or_newer do
-    version "0.99,136"
-    sha256 "475d879b6ce14801f52e513738e7bea214d1a4499a9584d4ca52656ba54bc0e1"
+    version "0.99,137"
+    sha256 "e1b7d59c55a256a648ac6b832b3faedc761e41281b48de439d5f67ef27ba7205"
 
     url "https://cdn.kagi.com/updates/15_0/#{version.csv.second}.zip"
 
