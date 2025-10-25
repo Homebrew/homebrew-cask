637f6a09625a5c1334b8463bf97e375920b6c2d0
Rui Chen <rui@chenrui.dev>
2025-10-25 16:51:48 -0400

    gpgfrontend: fix audit failure by unifying minimum macOS to :ventura
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/g/gpgfrontend.rb
+++ Casks/g/gpgfrontend.rb
@@ -1,48 +1,33 @@
 cask "gpgfrontend" do
   macos_version = nil
 
-  on_ventura :or_older do
-    on_monterey :or_older do
-      macos_version = 12
+  version "2.1.10"
 
-      version "2.1.5"
-      sha256 "731acf48fea4fed6fc4a0065b8e50655e8cff911c62e31f1fc5f4b8c2b478db2"
-
-      livecheck do
-        skip "Legacy version"
-      end
-    end
-    on_ventura do
+  on_arm do
+    on_ventura :or_older do
       macos_version = 13
 
-      version "2.1.10"
       sha256 "f611956a9b3d7b8a2dfda11a93ebe10ef64100e144890df5efcbd61a4aeb5a37"
-    end
 
-    caveats do
-      requires_rosetta
+      caveats do
+        requires_rosetta
+      end
+    end
+    on_sonoma do
+      macos_version = 14
+
+      sha256 "ae8af5945ac28ec214cb1eee8ae9e034de24f7e4519d5e6bce22bde17a983393"
+    end
+    on_sequoia :or_newer do
+      macos_version = 15
+
+      sha256 "28b05ae0b5e013e3de3d47006a1557d52557d09234313c586c3e4b2dd9ffa0a1"
     end
   end
-  on_sonoma :or_newer do
-    version "2.1.10"
+  on_intel do
+    macos_version = 13
 
-    on_arm do
-      on_sonoma do
-        macos_version = 14
-
-        sha256 "ae8af5945ac28ec214cb1eee8ae9e034de24f7e4519d5e6bce22bde17a983393"
-      end
-      on_sequoia :or_newer do
-        macos_version = 15
-
-        sha256 "28b05ae0b5e013e3de3d47006a1557d52557d09234313c586c3e4b2dd9ffa0a1"
-      end
-    end
-    on_intel do
-      macos_version = 13
-
-      sha256 "f611956a9b3d7b8a2dfda11a93ebe10ef64100e144890df5efcbd61a4aeb5a37"
-    end
+    sha256 "f611956a9b3d7b8a2dfda11a93ebe10ef64100e144890df5efcbd61a4aeb5a37"
   end
 
   url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-#{macos_version}.dmg",
@@ -52,7 +37,7 @@ cask "gpgfrontend" do
   homepage "https://gpgfrontend.bktus.com/"
 
   depends_on formula: "gnupg"
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :ventura"
 
   app "GpgFrontend.app"
 
