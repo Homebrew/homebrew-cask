545bbb66ee063b6b0a4823c646350ed15a93f884
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 18:38:11 +0000

    alt-tab 7.31.0

--- Casks/a/alt-tab.rb
+++ Casks/a/alt-tab.rb
@@ -1,6 +1,6 @@
 cask "alt-tab" do
-  version "7.30.0"
-  sha256 "4963cf5332500876f441a50cd7b5912cd4bc663bd09540e1910193f4eb373c41"
+  version "7.31.0"
+  sha256 "4388994aa5a42adbd899142a823b210cb7ec9ec76adb87b6407d79d695ed1ff7"
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip",
       verified: "github.com/lwouis/alt-tab-macos/"
