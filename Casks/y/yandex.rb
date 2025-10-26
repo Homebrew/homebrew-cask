a835cc2f9f89071a2893e476854ac9f949b3ee8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 01:41:19 +0000

    yandex 25.8.5.977

--- Casks/y/yandex.rb
+++ Casks/y/yandex.rb
@@ -2,12 +2,12 @@ cask "yandex" do
   arch arm: "Arm64"
 
   on_arm do
-    version "25.8.5.892,90215"
-    sha256 "2c5b45a850be9d7065bd39edb4d78adb440db1ad0ccc44e22accbcb68be7d67e"
+    version "25.8.5.977,90909"
+    sha256 "92191445f08430d56b61f0cc3c7fdccc7be123dd18a3db380868683921a25544"
   end
   on_intel do
-    version "25.8.5.890,90226"
-    sha256 "d7fa053ec39779258bff4bc7d7e47ca4de37515feb08ed35c6c10101f15c1758"
+    version "25.8.5.974,90912"
+    sha256 "d32c55e8669965f1c6c00a6e43524d72ff2105e838e1e63c97ae177074b654c6"
   end
 
   url "https://download.cdn.yandex.net/browser/update/#{version.csv.first.dots_to_underscores}_#{version.csv.second}_m_s_r/yandex.dmg",
