35f823fab1103ea624b96a8c12ef48b8b777f427
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 00:49:48 +0000

    boss 8.12.20

--- Casks/b/boss.rb
+++ Casks/b/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "8.12.17"
-  sha256 "b7f8e7d29072044ba04d6613da04d8408fee70e3ecd3d459c11a01d33457213d"
+  version "8.12.20"
+  sha256 "957dcd7c97f3ce255599bbd12886aa43c5082ea564181e621b7e876b020f12de"
 
   url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BOSS-Releases/"
