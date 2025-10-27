19dc3b52f99a3957b9f44268e9d9b94c9dc78cc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 09:52:53 +0000

    focusany 1.1.0

--- Casks/f/focusany.rb
+++ Casks/f/focusany.rb
@@ -1,9 +1,9 @@
 cask "focusany" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.0"
-  sha256 arm:   "ee2d1ddd7183878aa3f6571291f9308b47a7196fd1751d4c7acd91f0b968adc6",
-         intel: "6711f1642d162a81538be42c8b70757803cfd135b29dac5849ae000abed3c400"
+  version "1.1.0"
+  sha256 arm:   "2bd3213064b3dc021cd3f69b042b896ce2537abfebab3a1c84c8554d04e389ca",
+         intel: "894f5cad795e8ffeb4d03e0be719de4ccab01e6035615fd2ed337798371dc80d"
 
   url "https://github.com/modstart-lib/focusany/releases/download/v#{version}/FocusAny-#{version}-mac-#{arch}.dmg",
       verified: "github.com/modstart-lib/focusany/"
