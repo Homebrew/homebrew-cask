0abcc13d41e7c4fab623ebffd5acbcc33ca023e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 16:52:21 +0000

    rapidweaver 9.6.5

--- Casks/r/rapidweaver.rb
+++ Casks/r/rapidweaver.rb
@@ -1,6 +1,6 @@
 cask "rapidweaver" do
-  version "9.6.4,21148"
-  sha256 "63265c7883c61a78f12ba6809a1fd3e816d43c048c4fa8b360a0cf883757e8bc"
+  version "9.6.5,21150"
+  sha256 "098c33369f1e22d1d6b9d6193461db3b8d6e3dca6dfe62f2d95a6ba8ed726c8b"
 
   url "https://dl.devant.io/v1/3c53887f-427a-4af7-9144-ee16178c62f4/#{version.csv.second}/RapidWeaver.zip",
       verified: "dl.devant.io/v1/3c53887f-427a-4af7-9144-ee16178c62f4/"
