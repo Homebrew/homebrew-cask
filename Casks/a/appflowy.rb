d9b7c66a1da1bc7147452110971acda29105fa14
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-31 15:25:45 -0400

    appflowy 0.10.3
    `appflowy` is autobumped but the workflow failed to update to version
    0.10.3 because the GitHub release asset for the Intel zip file
    mistakenly uses a 0.10.2 version in the file name. This works around
    the issue by switching the cask to use the dmg files instead, which
    doesn't have the same problem for this release.
    
    Besides that, this adds a `depends_on macos:` value to address the
    related `brew audit` error ("Artifact defined :big_sur as the minimum
    macOS version but the cask declared no minimum macOS version").

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,11 +1,11 @@
 cask "appflowy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.10.2"
-  sha256 arm:   "6eadada93493402a0973194e5b0f2353fb414bbd8dc74ecc4b0db94ea315df79",
-         intel: "0107ab9d55bbfa1447edfe3b90efac0e1081df6c1d56a131be0670cbe1149178"
+  version "0.10.3"
+  sha256 arm:   "d3cdb8451c452bfb3fc745239e4a539c10c874c291faae5f08fd487b0238a228",
+         intel: "cd2a179475e7a8c64734988fd835ed098bbf22b79ab89f0dd6c0db11114f66ff"
 
-  url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-#{arch}.zip",
+  url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-#{arch}.dmg",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
   name "AppFlowy"
   desc "Open-source project and knowledge management tool"
@@ -15,6 +15,8 @@ cask "appflowy" do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "AppFlowy.app"
 
