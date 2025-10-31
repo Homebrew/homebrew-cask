5073ff227bae154a8db41ce4944854b2d2f4c549
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-31 11:23:59 -0400

    epicenter-whispering 7.6.0
    `epicenter-whispering` is autobumped but the workflow failed to update
    to version 7.6.0 because the dmg file names now include a `_darwin`
    suffix. This updates the version and `url` accordingly.

--- Casks/e/epicenter-whispering.rb
+++ Casks/e/epicenter-whispering.rb
@@ -1,11 +1,11 @@
 cask "epicenter-whispering" do
   arch arm: "aarch64", intel: "x64"
 
-  version "7.5.5"
-  sha256 arm:   "b2bb7c93f977c2848c27f19b9dc764dd24e6335260bb445a1aead5e16660ebee",
-         intel: "b194906a19cdd139ae774fe38dd5d68359060020c3e03ebfc59f5f13fca2594d"
+  version "7.6.0"
+  sha256 arm:   "37c28dc2175142fb4607b9fbdc33c79b97e5a83ef90533e1fa6b8ac435da8196",
+         intel: "cf8996d22d8cfa983b35188a2632687d4966ab04cd6fc513106110ef9841ac5c"
 
-  url "https://github.com/epicenter-os/epicenter/releases/download/v#{version}/Whispering_#{version}_#{arch}.dmg",
+  url "https://github.com/epicenter-os/epicenter/releases/download/v#{version}/Whispering_#{version}_#{arch}_darwin.dmg",
       verified: "github.com/epicenter-os/epicenter/"
   name "Epicenter Whispering"
   desc "Audio transcription that works with local and cloud models"
