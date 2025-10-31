34141b839ebe30677b35509bb7aed13b725d2774
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-31 16:08:27 -0400

    keeper-password-manager 17.4.1
    `keeper-password-manager` is autobumped but the workflow failed to
    update to version 17.4.1 because `brew audit` failed with an
    "Artifact defined :big_sur as the minimum macOS version but the cask
    declared no minimum macOS version" error. This updates the version
    and adds an appropriate `depends_on macos:` value.

--- Casks/k/keeper-password-manager.rb
+++ Casks/k/keeper-password-manager.rb
@@ -1,5 +1,5 @@
 cask "keeper-password-manager" do
-  version "17.4"
+  version "17.4.1"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
@@ -11,6 +11,8 @@ cask "keeper-password-manager" do
     url "https://docs.keeper.io/en/release-notes/desktop"
     regex(/Release\s+v?(\d+(?:\.\d+)+)/i)
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Keeper Password Manager.app"
 
