abadd39d2a3da7cdd37c7bcd05872c20fc68090a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-10-31 17:01:49 -0400

    gearsystem 3.8.5
    `gearsystem` is autobumped but the workflow failed to update to
    version 3.8.5 because the Intel zip file didn't exist at the time.
    Both zip files are available now, so this manually updates the cask
    version.

--- Casks/g/gearsystem.rb
+++ Casks/g/gearsystem.rb
@@ -1,9 +1,9 @@
 cask "gearsystem" do
   arch arm: "arm", intel: "intel"
 
-  version "3.8.4"
-  sha256 arm:   "75886881e369c2b1a95782b1957106fff639f8705e27d06627250d3fee1ada04",
-         intel: "bfe62acc85b750956440ef0eac67c817d9f3088820cd22c2fe383917104907ec"
+  version "3.8.5"
+  sha256 arm:   "04f87767f09fcfb43119038a59b84c1416c35ed3329eab326ad70c094b9d12ee",
+         intel: "a1c64a02c155d11e215d71377f8d92a33985f99207b81b267aecd7ac2d3c9846"
 
   url "https://github.com/drhelius/Gearsystem/releases/download/#{version}/Gearsystem-#{version}-macos-#{arch}.zip"
   name "Gearsystem"
