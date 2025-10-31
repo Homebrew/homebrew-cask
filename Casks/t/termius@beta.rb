3201ba7be6a300e7455582279ffa73f34d1f3667
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-31 04:59:09 +0000

    termius@beta 9.32.4

--- Casks/t/termius@beta.rb
+++ Casks/t/termius@beta.rb
@@ -1,7 +1,7 @@
 cask "termius@beta" do
   arch arm: "mac-beta-arm64", intel: "mac-beta"
 
-  version "9.32.3"
+  version "9.32.4"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
