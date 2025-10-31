4f4c834562b9b34bb4529b1ff2b3741719fc3a3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-31 01:51:05 +0000

    pearcleaner 5.3.5

--- Casks/p/pearcleaner.rb
+++ Casks/p/pearcleaner.rb
@@ -1,6 +1,6 @@
 cask "pearcleaner" do
-  version "5.3.4"
-  sha256 "1e174a47cb7059aa4083415116893ea254b49d681010413c8a526ea5d6e9d787"
+  version "5.3.5"
+  sha256 "1e1f4a6920f73d9e9eed6d5d836b2ee470f84d45a1b9d1ef2f7a5b345bad7295"
 
   url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip",
       verified: "github.com/alienator88/Pearcleaner/"
