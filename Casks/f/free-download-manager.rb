3cc1802d57e9900bbb15850600745c9563b25d8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 09:54:11 +0000

    free-download-manager 6.30.3

--- Casks/f/free-download-manager.rb
+++ Casks/f/free-download-manager.rb
@@ -1,5 +1,5 @@
 cask "free-download-manager" do
-  version "6.30.2"
+  version "6.30.3"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://files2.freedownloadmanager.org/#{version.major}/latest/fdm.dmg"
