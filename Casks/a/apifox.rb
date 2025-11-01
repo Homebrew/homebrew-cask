12e44ecaabb1e71c1734eebb85ce53f38a8d191d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 12:45:11 +0000

    apifox 2.7.46

--- Casks/a/apifox.rb
+++ Casks/a/apifox.rb
@@ -2,9 +2,9 @@ cask "apifox" do
   arch arm: "-macOS-arm64"
   livecheck_arch = on_arch_conditional arm: "-arm64"
 
-  version "2.7.45"
-  sha256 arm:   "2096e495fd0a96440485900e34984c309f9eeb62b60ee26ce886e6c84aac1687",
-         intel: "239b201aa6a1f9e76010e21f72c76f16f618ede2b8481d09c50bc30ead4479ce"
+  version "2.7.46"
+  sha256 arm:   "d22ad7dfd8c414836c4225c6b2a31a1f06d19aec9089c3cb0a6b55b873d1111d",
+         intel: "6df9ca9840dfaf759f61768431e928ff844fc371c7477fa079073f1a15e2939b"
 
   url "https://file-assets.apifox.com/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
