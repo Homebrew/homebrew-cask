81e58d8d71632ae7bb890b94cfff2f66aaa7f74f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 16:42:19 +0000

    zipic 1.8.2

--- Casks/z/zipic.rb
+++ Casks/z/zipic.rb
@@ -1,6 +1,6 @@
 cask "zipic" do
-  version "1.8.1"
-  sha256 "a0ae1c48c4acf336a3b5b2b71a75cf4c99e40bf65432b29d2c3671755ebd561f"
+  version "1.8.2"
+  sha256 "20b5a9a095ef7e6048eaabe4c1d53dc2a31a4be158b87bbc2c20680b5db42698"
 
   url "https://zipic.5km.tech/Zipic%20#{version}.dmg"
   name "Zipic"
