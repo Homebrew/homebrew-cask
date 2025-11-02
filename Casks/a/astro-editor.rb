52ef678d15ce79eb1c06da979a9b8023ac863dfc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-11-01 21:34:06 -0400

    astro-editor 0.1.32 (new cask)

---
+++ Casks/a/astro-editor.rb
@@ -0,0 +1,19 @@
+cask "astro-editor" do
+  version "0.1.32"
+  sha256 "9b779c178a4adb0a3f51ae85768bbdb48d32ad41fc175ad4d1e585fd36aa09c0"
+
+  url "https://github.com/dannysmith/astro-editor/releases/download/v#{version}/Astro.Editor_#{version}_universal.dmg",
+      verified: "github.com/dannysmith/astro-editor/"
+  name "Astro Editor"
+  desc "Markdown editor for Astro content collections"
+  homepage "https://astroeditor.danny.is/"
+
+  app "Astro Editor.app"
+
+  zap trash: [
+    "~/Library/Application Support/is.danny.astroeditor",
+    "~/Library/Caches/is.danny.astroeditor",
+    "~/Library/Logs/is.danny.astroeditor",
+    "~/Library/WebKit/is.danny.astroeditor",
+  ]
+end
