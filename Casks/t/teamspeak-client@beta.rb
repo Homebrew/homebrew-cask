ca5f3d73d15c2cad6a67f8e083f0e9b4c7bee07a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-02 15:31:46 -0500

    teamspeak-client@beta 6.0.0-beta3.2
    `teamspeak-client@beta` is autobumped but the workflow failed to
    update to version 6.0.0-beta3.2 because the `livecheck` block is
    producing an "Unable to get versions" error. The existing regex is
    unable to match the current dmg file names because they include an
    arch suffix now that there's also a dmg for ARM. This updates the
    version and adjusts the cask accordingly.
    
    Besides that, the app fails Gatekeeper checks, so this also adds a
    `disable!` call with the future date we've been using for this
    scenario.

--- Casks/t/teamspeak-client@beta.rb
+++ Casks/t/teamspeak-client@beta.rb
@@ -1,8 +1,11 @@
 cask "teamspeak-client@beta" do
-  version "6.0.0-beta2"
-  sha256 "7fe0109391515076c97f48503aabbae4032cf0877648aa19ddd81688ac6dff0d"
+  arch arm: "arm", intel: "intel"
 
-  url "https://files.teamspeak-services.com/pre_releases/client/#{version}/teamspeak-client.dmg",
+  version "6.0.0-beta3.2"
+  sha256 arm:   "308f342f51580202e566fd3d42729d5cc517071640809e2545ab5464ea9d23a2",
+         intel: "df0d13ef3095e3d2c239b225beeb5addd27fa26206bc1f698982611458268c0b"
+
+  url "https://files.teamspeak-services.com/pre_releases/client/#{version}/teamspeak-client-#{arch}.dmg",
       verified: "files.teamspeak-services.com/"
   name "TeamSpeak Beta"
   desc "Voice communication client"
@@ -10,8 +13,10 @@ cask "teamspeak-client@beta" do
 
   livecheck do
     url "https://teamspeak.com/en/downloads/"
-    regex(%r{href=.*?/(\d+(?:\.\d+)+[^/]*)/teamspeak[._-]client\.dmg}i)
+    regex(%r{href=.*?/(\d+(?:\.\d+)+[^/]*)/teamspeak[._-]client-#{arch}\.dmg}i)
   end
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
 
@@ -22,8 +27,4 @@ cask "teamspeak-client@beta" do
     "~/Library/Preferences/TeamSpeak",
     "~/Library/Saved Application State/com.teamspeak.#{version.major}.client.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
