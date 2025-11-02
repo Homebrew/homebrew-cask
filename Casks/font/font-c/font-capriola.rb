368d463a88ddd4027040160b738d530e0fb1fc29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 00:16:53 +0000

    font-capriola: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-c/font-capriola.rb
+++ Casks/font/font-c/font-capriola.rb
@@ -2,12 +2,12 @@ cask "font-capriola" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/capriola/Capriola-Regular.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/capriola/Capriola%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Capriola"
   homepage "https://fonts.google.com/specimen/Capriola"
 
-  font "Capriola-Regular.ttf"
+  font "Capriola[wght].ttf"
 
   # No zap stanza required
 end
