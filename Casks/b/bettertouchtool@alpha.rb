c75d92c5f4c7a450222798e982e9d97a1bc104b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 00:48:20 +0000

    bettertouchtool@alpha 5.745

--- Casks/b/bettertouchtool@alpha.rb
+++ Casks/b/bettertouchtool@alpha.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool@alpha" do
-  version "5.740,2025103001"
-  sha256 "b2e46e14c6b6ff3524d77593b1580210a6b7ae508cd1a1920a5c2ab2a21904c6"
+  version "5.745,2025110301"
+  sha256 "a4668a9095df7c7f2e03a28e178d27251f843328d51c8e8639d4afc63b59e299"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
