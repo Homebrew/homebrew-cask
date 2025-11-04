168f7071607439f6f2df3a0731840d1c62491431
zstar1003 <1198768105@qq.com>
2025-11-04 22:25:30 +0800

    freepdf 5.1.0

---
+++ Casks/f/freepdf.rb
@@ -0,0 +1,23 @@
+cask "freepdf" do
+  version "5.1.0"
+  sha256 "a4072e874854a22428070674af156a6510cc4c0f2ccd0b7075bfa852f6e06287"
+
+  url "https://github.com/zstar1003/FreePDF/releases/download/v#{version}/FreePDF_v#{version}_macOS.dmg"
+  name "FreePDF"
+  desc "Reader that supports translating PDF documents"
+  homepage "https://github.com/zstar1003/FreePDF"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "FreePDF.app"
+
+  zap trash: [
+    "~/Library/Application Support/FreePDF",
+    "~/Library/Caches/FreePDF",
+  ]
+end
