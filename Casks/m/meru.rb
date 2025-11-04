e2fc1b17d5033ddd905ab2db4b659c6932c83ac3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 01:17:10 +0000

    meru 3.14.2

--- Casks/m/meru.rb
+++ Casks/m/meru.rb
@@ -1,9 +1,9 @@
 cask "meru" do
   arch arm: "-arm64"
 
-  version "3.14.1"
-  sha256 arm:   "2664f6e4e0f911220f5d34ab77838358813ee6b8419c8c5d193752cd0e262317",
-         intel: "4a03e756e711e33e3cf1d3d36addb48bc9c041eeaae91c87628ef9bb09128c36"
+  version "3.14.2"
+  sha256 arm:   "459317ae49bdaacab163a82b40248906f55413fefad6959b82e7fc859da437c5",
+         intel: "0acb389c1de040464e7165ecda1b75fd006663409bf5eb96db0758b151c504d5"
 
   url "https://github.com/zoidsh/meru/releases/download/v#{version}/Meru-#{version}#{arch}.dmg",
       verified: "github.com/zoidsh/meru/"
