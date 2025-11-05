d7e3c506fbeaa0932d11a0b330243521500c2ac8
SpoiledSneeze <53020446+SpoiledSneeze@users.noreply.github.com>
2025-11-04 19:13:37 -0500

    fix: branch master returns current version

--- Casks/w/whatsapp.rb
+++ Casks/w/whatsapp.rb
@@ -2,13 +2,13 @@ cask "whatsapp" do
   version "2.25.31.75"
   sha256 "3b34f29d6d2446ba20e6264075b218b4e6857b41e04421f9b0d9b97b930a71a1"
 
-  url "https://web.whatsapp.com/desktop/mac_native/release/?version=#{version}&extension=zip&configuration=Release&branch=relbranch"
+  url "https://web.whatsapp.com/desktop/mac_native/release/?version=#{version}&extension=zip&configuration=Release&branch=master"
   name "WhatsApp"
   desc "Native desktop client for WhatsApp"
   homepage "https://www.whatsapp.com/"
 
   livecheck do
-    url "https://web.whatsapp.com/desktop/mac_native/updates/?branch=relbranch&configuration=Release"
+    url "https://web.whatsapp.com/desktop/mac_native/updates/?branch=master&configuration=Release"
     regex(/version=v?(\d+(?:\.\d+)+)/i)
     strategy :sparkle do |item, regex|
       item.url.scan(regex).map(&:first)
