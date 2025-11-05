894e2e5918b68d6a0894533020188382e86c4b1e
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-05 12:02:46 -0500

    qlc+ 5.0.1
    `qlc+` is autobumped but the workflow failed to update to version
    5.0.1 because `brew bump` doesn't handle skipped `livecheck` blocks
    in a reasonable manner when only one arch is skipped (i.e., the
    "skipped" string is treated as a version and this produces an artifact
    URL that will predictably fail). This manually updates the version for
    the time being.
    
    Besides that, this adds a `depends_on macos:` value for ARM. The macOS
    requirement for Intel is 10.13 or later and it can use the implicit
    macOS requirement for casks but we have to make it explicit here for
    `brew style` to pass (as we can't have only an ARM value).
    
    Lastly, this updates the qlcplus.org URLs to use the www subdomain,
    as the file links on the download page use it. The website works fine
    with or without it and doesn't redirect to one or the other, so this
    is just a preemptive change in case it redirects to the www subdomain
    in the future.

--- Casks/q/qlc+.rb
+++ Casks/q/qlc+.rb
@@ -2,13 +2,15 @@ cask "qlc+" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "5.0.0"
-    sha256 "f42b26a1328ce4f6120c160c43e4aabd5a5f950fe662eb8b75f74c321d716288"
+    version "5.0.1"
+    sha256 "00bab5cd80f3140cc981be72f3d0ad092f30436931e223955255d0a36205b254"
 
     livecheck do
-      url "https://qlcplus.org/download"
+      url "https://www.qlcplus.org/download"
       regex(/href=.*?QLC\+[._-]v?(\d+(?:[.-]\d+)+)[._-]#{arch}\.dmg/i)
     end
+
+    depends_on macos: ">= :monterey"
   end
   on_intel do
     version "4.14.3"
@@ -17,12 +19,14 @@ cask "qlc+" do
     livecheck do
       skip "Legacy version"
     end
+
+    depends_on macos: ">= :catalina"
   end
 
-  url "https://qlcplus.org/downloads/#{version.split("-").first}/QLC+_#{version}_#{arch}.dmg"
+  url "https://www.qlcplus.org/downloads/#{version.split("-").first}/QLC+_#{version}_#{arch}.dmg"
   name "Q Light Controller+"
   desc "Control DMX or analogue lighting systems"
-  homepage "https://qlcplus.org/"
+  homepage "https://www.qlcplus.org/"
 
   app "QLC+.app"
 
