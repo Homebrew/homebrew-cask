ad8110c268c29668d9d1c593a14043012e0c1235
Patrick Linnane <patrick@linnane.io>
2025-11-05 08:10:36 -0800

    lemonlime: add minimum macOS versions
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/lemonlime.rb
+++ Casks/l/lemonlime.rb
@@ -5,6 +5,13 @@ cask "lemonlime" do
   sha256 arm:   "2d064df6c68de9b5dfd5a5fa56d21da94c631e294c3264ca1c21c4bf40a7fd04",
          intel: "10465401db07944e154201c6125f5098dc6e8db8700c5cc74f27ad7ba29f90e7"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :catalina"
+  end
+
   url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version}/lemon-Qt6.9.3-Release-#{arch}.dmg"
   name "lemonlime"
   desc "Tiny judging environment for OI contest based on Lemon + LemonPlus"
