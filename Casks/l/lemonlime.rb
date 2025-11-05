509e10ece847735a8730d9df4fde91374b026ca0
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-05 11:39:09 -0500

    lemonlime: include Qt version in cask version
    `lemonlime` is autobumped but the workflow failed to update to version
    0.3.6 because the Qt version in the cask `url` is hardcoded and it has
    predictably changed in the current file name. This updates the cask to
    include the Qt version in the `version` and to interpolate it in the
    `url`.
    
    I've added a `livecheck` block that matches the version from the tag
    and the Qt version (if any) from the file name. Upstream has used a
    variety of dmg file name formats, so I've tried to make the regex
    fairly flexible in hopes of reducing the chance of this breaking in
    the future but there's still a fair chance of that happening.
    
    It's worth noting that the cask `url` requires a second `version` part
    but the `livecheck` block can return a version with only one part. I
    set it up this way so that `livecheck` will still surface a new
    version if the file name doesn't include a Qt version but the regex
    still matches (i.e., we will be able to see the new version and adjust
    the `url` accordingly). It's technically possible to fall back to the
    tag version if no files match but some upstream projects create a
    release and add assets to it afterward, so that approach may
    prematurely surface a new version. I'm not sure how upstream does
    their releases but maybe better to be safe than sorry.

--- Casks/l/lemonlime.rb
+++ Casks/l/lemonlime.rb
@@ -1,7 +1,7 @@
 cask "lemonlime" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.3.6"
+  version "0.3.6,6.9.3"
   sha256 arm:   "2d064df6c68de9b5dfd5a5fa56d21da94c631e294c3264ca1c21c4bf40a7fd04",
          intel: "10465401db07944e154201c6125f5098dc6e8db8700c5cc74f27ad7ba29f90e7"
 
@@ -12,10 +12,23 @@ cask "lemonlime" do
     depends_on macos: ">= :catalina"
   end
 
-  url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version}/lemon-Qt6.9.3-Release-#{arch}.dmg"
+  url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version.csv.first}/lemon-Qt#{version.csv.second}-Release-#{arch}.dmg"
   name "lemonlime"
   desc "Tiny judging environment for OI contest based on Lemon + LemonPlus"
   homepage "https://github.com/Project-LemonLime/Project_LemonLime"
+
+  livecheck do
+    url :url
+    regex(%r{/v?(\d+(?:\.\d+)+[a-z]?)/lemon(?:[._-]Qt(\d+(?:\.\d+)+))?(?:[._-]Release)?(?:[._-]#{arch})?\.dmg$}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        match[2] ? "#{match[1]},#{match[2]}" : match[1]
+      end
+    end
+  end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
