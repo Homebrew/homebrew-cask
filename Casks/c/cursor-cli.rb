818f54d3deacc845987f671c296ae556bebd08f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-06 06:48:10 +0000

    cursor-cli 2025.11.06-8fe8a63

--- Casks/c/cursor-cli.rb
+++ Casks/c/cursor-cli.rb
@@ -1,9 +1,9 @@
 cask "cursor-cli" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.10.28-0a91dc2"
-  sha256 arm:   "556efa2f1d952aca621a4a61a7be226f8f85e4509dbbcefdddffb1bc3ebb3a93",
-         intel: "3d6c79400b3ce2058d3876192f6d2e1675be9b8ac0a514fa8382ed69b8cbfa5c"
+  version "2025.11.06-8fe8a63"
+  sha256 arm:   "b79b3d4df2cb03f54b098345f9fb1ff7081f9365bde9e41221b5bf71d50a32e6",
+         intel: "51547e8a899d67398f7e3e28e0de057d44826bdb6d26b7bb229b28e707889f59"
 
   url "https://downloads.cursor.com/lab/#{version}/darwin/#{arch}/agent-cli-package.tar.gz"
   name "Cursor CLI"
