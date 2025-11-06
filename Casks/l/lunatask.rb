46aa1171b6bbed254b17412fbfecdb494e143aed
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-06 13:55:11 -0500

    lunatask 2.1.15
    `lunatask` is autobumped but the workflow failed to update to version
    2.1.14 because it was apparently a pre-release on GitHub. At time of
    writing, there is a 2.1.14 tag but no corresponding release, so
    upstream my have deleted it in the interim time. There's now a 2.1.15
    release, so this updates the version. This also adds a `livecheck`
    block that uses the `GithubLatest` strategy, which should avoid the
    aforementioned situations going forward.

--- Casks/l/lunatask.rb
+++ Casks/l/lunatask.rb
@@ -1,12 +1,17 @@
 cask "lunatask" do
-  version "2.1.13"
-  sha256 "5ce230145c1d266d52cf02789ce89d6ec06bc5fe5bfddc4eba5ff3aee2a0efc9"
+  version "2.1.15"
+  sha256 "e0c2d496ab49382bc939cd2eadef1926dd6e57fcd171a2609d4d36d3d1b8dcf3"
 
   url "https://github.com/lunatask/lunatask/releases/download/v#{version}/Lunatask-#{version}-universal.dmg",
       verified: "github.com/lunatask/lunatask/"
   name "Lunatask"
   desc "Encrypted to-do list, habit tracker, journaling, life-tracking and notes app"
   homepage "https://lunatask.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :big_sur"
 
