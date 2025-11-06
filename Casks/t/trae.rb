f0995a07e05ad082f17454f06d1e06c64ab45cbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-06 10:44:47 +0000

    trae 1.0.22672

--- Casks/t/trae.rb
+++ Casks/t/trae.rb
@@ -1,9 +1,9 @@
 cask "trae" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.22462"
-  sha256 arm:   "b19ca699c4ff4279323a0f764f2bae0bfd6ca565be399e33456b9aeac6f83f41",
-         intel: "dd34a37c0464d49018d8d3c2bc6e864b01e1b58e453ac2aeead31c8a7528d154"
+  version "1.0.22672"
+  sha256 arm:   "363cb329918bc3658b8ce768530735eb7882c35a9af564dd2bd5e62116cd09de",
+         intel: "74d07cc97ede7e9144689a6f0df421560bc57a25dd411738a4b40714f0b212a5"
 
   url "https://lf-cdn.trae.ai/obj/trae-ai-us/pkg/app/releases/stable/#{version}/darwin/Trae-darwin-#{arch}.dmg"
   name "Trae"
