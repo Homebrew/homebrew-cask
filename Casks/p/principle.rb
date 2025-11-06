1a4377dc32a823dc35ea7f6f585d8d7a8a46ea24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-06 19:33:01 +0000

    principle 6.42

--- Casks/p/principle.rb
+++ Casks/p/principle.rb
@@ -1,6 +1,6 @@
 cask "principle" do
-  version "6.41"
-  sha256 "1d2f102a4f834c1df46b15d551fe7040016bf178e389c070341a785cfdf28a07"
+  version "6.42"
+  sha256 "2f5a4f46d9fc5151ebf14dd8c2f4807bd1dd47023b3e41489c6ac9571beaa290"
 
   url "https://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   name "Principle"
