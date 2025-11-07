0768b5115d8fc19d50092220d558febe0c4d449b
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-07 09:13:44 -0500

    bison-wallet 1.0.4
    `bison-wallet` is autobumped but the workflow failed to update to
    versin 1.0.4 because the app fails Gatekeeper checks. This updates the
    version and adds a `disable!` call with the date we've been using for
    this scenario.

--- Casks/b/bison-wallet.rb
+++ Casks/b/bison-wallet.rb
@@ -1,14 +1,16 @@
 cask "bison-wallet" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.3"
-  sha256 arm:   "0dcafd9509b435a5e762aeb11cbc63baa084dc71c58d017072707bc292c72bbc",
-         intel: "c0433ab203a5b9c631bb4c6bc21edaa27b90d6266203ef1c16c6c6a7a6a53c74"
+  version "1.0.4"
+  sha256 arm:   "31e5945adc602840aeb3e853909a8003b185a95e31fbc5c8a7db4a23f7c29c2d",
+         intel: "889d54f752c96e6a079db29b66d14d8b8e4a5577c91d1b3d499c4e781f75554e"
 
   url "https://github.com/decred/dcrdex/releases/download/v#{version}/bisonw-desktop-darwin-#{arch}-v#{version}.dmg"
   name "Bison Wallet"
   desc "Multi-coin wallet with feeless DEX, atomic swaps, and arbitrage tools"
   homepage "https://github.com/decred/dcrdex"
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "Bison Wallet.app"
 
