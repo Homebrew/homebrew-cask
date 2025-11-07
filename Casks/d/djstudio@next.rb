57d1e3f6049cf098a520fc52829d42276b21331b
viking1304 <viking1304@gmail.com>
2025-11-07 20:41:52 +0100

    djstudio@next 4.0.7-beta.0 (new cask)
    DAW for DJs (beta channel)

---
+++ Casks/d/djstudio@next.rb
@@ -0,0 +1,39 @@
+cask "djstudio@next" do
+  arch arm: "-arm64", intel: ""
+  livecheck_arch = on_arch_conditional arm: "-apple", intel: ""
+
+  version "4.0.7-beta.0"
+  sha256 arm:   "75e0f62d6719b6a4835a6f9c31f24f4c1aa6ea0d2e23e3922a238a277e5f355f",
+         intel: "9d9be5b671bf3bb2ec23fb4029b418d3e270ea4d8b192de933f53ee6b8600ec9"
+
+  url "https://download.next.dj.studio/DJ.Studio%20Next-#{version}#{arch}.dmg"
+  name "DJ.Studio Next"
+  desc "DAW for DJs (beta channel)"
+  homepage "https://dj.studio/"
+
+  livecheck do
+    url "https://downloads.dj.studio/djstudio/download/latest/mac#{livecheck_arch}?next=true"
+    strategy :header_match
+    regex(/DJ\.Studio[ _-]Next[._-](\d+(?:\.\d+)+-beta\.\d+)(?:#{arch})?\.dmg/i)
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "DJ.Studio Next.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/dj.studio.app.sfl*",
+    "~/Library/Application Support/DJ.Studio",
+    "~/Library/Application Support/dj.studio.app",
+    "~/Library/Preferences/dj.studio.app.plist",
+    "~/Library/Saved Application State/dj.studio.app.savedState",
+    "~/Music/DJ.Studio",
+  ]
+
+  caveats <<~EOS
+    DJ.Studio Next uses the same default project and audio directory as the stable version:
+      ~/Music/DJ.Studio
+
+    This folder is intentionally not removed during `brew zap` to avoid data loss.
+  EOS
+end
