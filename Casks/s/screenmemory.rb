fa086395e9feed459a3b5c18c32dee506d237461
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 22:28:55 +0000

    screenmemory 2.2.6

--- Casks/s/screenmemory.rb
+++ Casks/s/screenmemory.rb
@@ -1,6 +1,6 @@
 cask "screenmemory" do
-  version "2.2.5"
-  sha256 "30ddb40ee1064f9afae08619ac4bee3263230a0f05b202a24637ef704015a9a7"
+  version "2.2.6"
+  sha256 "eecd3aea01a09cefe27bc21ad4946ad61078621b564cbe49f9428146d9794c32"
 
   url "https://f005.backblazeb2.com/file/screenmemory/ScreenMemory.#{version}.dmg",
       verified: "f005.backblazeb2.com/file/screenmemory/"
