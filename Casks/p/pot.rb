6a04d77b55aa0588668c5c94b61f1550afcc231c
Zeldris <80939130+zeldrisho@users.noreply.github.com>
2025-11-08 01:13:36 +0700

    Update Casks/p/pot.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/p/pot.rb
+++ Casks/p/pot.rb
@@ -5,10 +5,11 @@ cask "pot" do
   sha256 arm:   "b3561a92ab5bf338291c25b42bf2eb30bf4b013be379d78ffb275edbb899b0a9",
          intel: "9ce47c9488126d3bdffbb0ec66c2979f4b9d759eb60078127cb294fd6fd07024"
 
-  url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
-  name "pot"
-  desc "Cross-platform software for text translation and recognize"
-  homepage "https://github.com/pot-app/pot-desktop"
+  url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg",
+      verified: "github.com/pot-app/pot-desktop/"
+  name "Pot"
+  desc "Software for text translation and recognition"
+  homepage "https://pot-app.com/"
 
   livecheck do
     url :url
