c18e74fd0353c5dfe4179b593173a508ff8458b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 22:25:24 +0000

    robloxstudio 0.698.1.6980943

--- Casks/r/robloxstudio.rb
+++ Casks/r/robloxstudio.rb
@@ -1,9 +1,9 @@
 cask "robloxstudio" do
   arch arm: "arm64/"
 
-  version "0.698.0.6980940,416da624edce4fa1"
-  sha256 arm:   "3697744727f3898b6e1b7159366970ac0293430f3907831b23ef361294c81201",
-         intel: "69b32eb21c375015594d3c48e870823f70556d4ba07b6baf371a4e43a330426b"
+  version "0.698.1.6980943,b4f90e79fdbf4191"
+  sha256 arm:   "8996d424ecda918a65a34b4ae0c0ec620cd12f5855d67fd1a12b35e89e33c47d",
+         intel: "85b154fe2d79c1480daa0c3754c0bec15ba67709212b734b9799838ea206f5d0"
 
   url "https://setup.rbxcdn.com/mac/#{arch}version-#{version.csv.second}-RobloxStudioApp.zip",
       verified: "setup.rbxcdn.com/"
