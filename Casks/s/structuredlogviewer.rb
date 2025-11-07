5188827e27727d084820f3d05a2040a17706fe92
Patrick Linnane <patrick@linnane.io>
2025-11-06 20:47:18 -0800

    structuredlogviewer: add minimum macOS version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/structuredlogviewer.rb
+++ Casks/s/structuredlogviewer.rb
@@ -5,6 +5,13 @@ cask "structuredlogviewer" do
   sha256 arm:   "db49e5e6142f8d0a683fd6abef085eddaf1f28ef17c9c514e400b162b358065f",
          intel: "228f34f53b8ea5b524b2862407d0691fb71beb1a82fa54d544e47eb437d0081a"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :catalina"
+  end
+
   url "https://github.com/KirillOsenkov/MSBuildStructuredLog/releases/download/v#{version}/StructuredLogViewer-#{arch}.dmg",
       verified: "github.com/KirillOsenkov/MSBuildStructuredLog/"
   name "Structured Log Viewer"
