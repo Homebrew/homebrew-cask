d0bd7e384419609526357c354b682b8d568999ae
Félix MARQUET <72651575+BreizhHardware@users.noreply.github.com>
2025-11-07 13:03:54 +0100

    Update Atoll to 1.0.2-beta

--- Casks/a/atoll@beta.rb
+++ Casks/a/atoll@beta.rb
@@ -1,8 +1,8 @@
 cask "atoll@beta" do
-  version "1.0.1-beta"
-  sha256 "fe4a5feffbc77f6abf4efc0a8ea35879297f4b7ce6ea5905b35648d73dcff9ab"
+  version "1.0.2-beta"
+  sha256 "sha256:1030ec27bb583f36d98e8002d0ea6b9226474caeaea838e7d373b68ae6d4fe53"
 
-  url "https://github.com/Ebullioscopic/Atoll/releases/download/v#{version}/DynamicIsland_v#{version}.dmg"
+  url "https://github.com/Ebullioscopic/Atoll/releases/download/v#{version}/Atoll.dmg"
   name "Atoll Beta"
   desc "Atoll transforms the MacBook notch into an interactive command surface (Beta)"
   homepage "https://github.com/Ebullioscopic/Atoll"
@@ -14,12 +14,9 @@ cask "atoll@beta" do
 
   depends_on macos: ">= :sequoia"
 
-  app "DynamicIsland.app"
+  app "Atoll.app"
 
   zap delete: [
-    "~/Library/Application Support/DynamicIsland",
-    "~/Library/Caches/com.karthikinformationtechnology.DynamicIsland",
-    "~/Library/HTTPStorages/com.karthikinformationtechnology.DynamicIsland",
-    "~/Library/Preferences/com.karthikinformationtechnology.DynamicIsland.plist",
+    "~/Library/Preferences/com.Ebullioscopic.Atoll.plist",
   ]
 end
