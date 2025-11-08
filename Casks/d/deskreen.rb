2dbd23ecf29cc04a2c76ac223c538753a2813f86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-08 15:45:14 +0000

    deskreen 3.1.2

--- Casks/d/deskreen.rb
+++ Casks/d/deskreen.rb
@@ -1,9 +1,9 @@
 cask "deskreen" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.1.0"
-  sha256 arm:   "deeb4101ba3dccc0c5d1fae52db8416d203af98944b1e14cc9640ecaeb9e76c6",
-         intel: "f7119e717ffa69dc848e99f10fa47b6c5db3acc7013b91e681ff11288d69991f"
+  version "3.1.2"
+  sha256 arm:   "44769396764718133f932983e3128c4d5941d407883009399d5fcbc41a6349d2",
+         intel: "d857e755d7578b94af78076a4d7f81ba65291763707c93470fb71ed7b68924b6"
 
   url "https://github.com/pavlobu/deskreen/releases/download/v#{version}/Deskreen-ce-#{version}-#{arch}.dmg",
       verified: "github.com/pavlobu/deskreen/"
