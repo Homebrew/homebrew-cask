4d7df08b0530a0b9ec43b6e24d976d3eb2bdeb11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 01:46:31 +0000

    rstudio@daily 2025.12.0-daily-322
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/r/rstudio@daily.rb
+++ Casks/r/rstudio@daily.rb
@@ -1,6 +1,6 @@
 cask "rstudio@daily" do
-  version "2025.12.0-daily-320"
-  sha256 "45983d418dbb63a0622d5f113463931ac88e60d99f2f9461176dd7be37eb62b8"
+  version "2025.12.0-daily-322"
+  sha256 "5d35723fcc2fd8148270d2f02224632ec34c9a5f3a7b4e8b1cb4c580f60088e8"
 
   url "https://rstudio-ide-build.s3.amazonaws.com/electron/macos/RStudio-#{version}.dmg",
       verified: "rstudio-ide-build.s3.amazonaws.com/electron/macos/"
