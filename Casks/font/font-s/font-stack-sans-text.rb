232327250b18279b37daac848c6acca8b7a05a2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 00:17:12 +0000

    font-stack-sans-text: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-stack-sans-text.rb
+++ Casks/font/font-s/font-stack-sans-text.rb
@@ -2,9 +2,10 @@ cask "font-stack-sans-text" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/stacksanstext/StackSansText%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/stacksanstext/StackSansText%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Stack Sans Text"
-  homepage "https://github.com/DylanYoungKoto/Stack-Sans"
+  homepage "https://fonts.google.com/specimen/Stack+Sans+Text"
 
   font "StackSansText[wght].ttf"
 
