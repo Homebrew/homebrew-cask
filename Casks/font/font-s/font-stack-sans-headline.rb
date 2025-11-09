5e409b0d56e35a398013ffc101274483d3db8fbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 00:17:12 +0000

    font-stack-sans-headline: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-stack-sans-headline.rb
+++ Casks/font/font-s/font-stack-sans-headline.rb
@@ -2,9 +2,10 @@ cask "font-stack-sans-headline" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/stacksansheadline/StackSansHeadline%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/stacksansheadline/StackSansHeadline%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Stack Sans Headline"
-  homepage "https://github.com/DylanYoungKoto/Stack-Sans"
+  homepage "https://fonts.google.com/specimen/Stack+Sans+Headline"
 
   font "StackSansHeadline[wght].ttf"
 
