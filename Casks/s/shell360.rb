079d983c60577a0b8faff3d29a5a5cd6266bb0eb
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-11-10 23:51:16 +0800

    Add shell360 0.1.13

---
+++ Casks/s/shell360.rb
@@ -0,0 +1,26 @@
+cask "shell360" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.1.13"
+  sha256 arm:   "EF21F976BE57858705E8EE80F5A1C6D6306F798652FF447FB85B8422D7BCCBDE",
+         intel: "67C505A18553EB34C57D51EF3F521A0631BBCF3CF301F260B4BFE4821636D0CE"
+
+  url "https://github.com/nashaofu/shell360/releases/download/v#{version}/Shell360_#{version}_#{arch}.dmg",
+      verified: "github.com/nashaofu/shell360/"
+  name "Shell360"
+  desc "Shell360 is a cross-platform SSH & SFTP client supporting Windows, macOS, Linux, Android, and iOS"
+  homepage "https://github.com/nashaofu/shell360"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Shell360.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.nashaofu.shell360"
+  ]
+end
