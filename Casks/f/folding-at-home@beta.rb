ea72a8da1a88d21a0ffef45556011b6291949c51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 16:04:28 +0000

    folding-at-home@beta 8.5.4

--- Casks/f/folding-at-home@beta.rb
+++ Casks/f/folding-at-home@beta.rb
@@ -1,6 +1,6 @@
 cask "folding-at-home@beta" do
-  version "8.5.3"
-  sha256 "3fda5e3c7ef918f5f4c7f22bdf72bcb4509c66193a842130086d49f76b386548"
+  version "8.5.4"
+  sha256 "2078751951ae03acdcff493bd5029b8104ee4f0f49487c3d3f92c23307364ca3"
 
   url "https://download.foldingathome.org/releases/beta/fah-client/macos-12-universal/release/fah-client_#{version}_universal.pkg"
   name "Folding@home Client Beta"
