4319afac8257ef27351a8e08263c4e48a6eb07e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 07:02:32 +0000

    hotovo-aider-desk 0.39.0

--- Casks/h/hotovo-aider-desk.rb
+++ Casks/h/hotovo-aider-desk.rb
@@ -1,9 +1,9 @@
 cask "hotovo-aider-desk" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.38.1"
-  sha256 arm:   "0e2be569de2c938a9044fd610a39b5b80bbeed28ad5a1091b8fddfdcd975f52f",
-         intel: "57839bc5bc8dbab15e879a9713933b01779d02a9e6110ec9f092e9b8a107cca2"
+  version "0.39.0"
+  sha256 arm:   "9e43e0f4ccc4b3d1273f24f4eb573e83c7076feb49e75c53f2f81bdf6537d977",
+         intel: "e28a66130727d2120897799532d3caa015c678fe73b7cbdaac68b5f5ff409104"
 
   url "https://github.com/hotovo/aider-desk/releases/download/v#{version}/aider-desk-#{version}-macos-#{arch}.dmg"
   name "AiderDesk"
