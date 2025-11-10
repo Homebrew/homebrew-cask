1079236e891c2cd741756fc1ddb00f430950bddf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 14:10:46 +0000

    unicodechecker 1.25

--- Casks/u/unicodechecker.rb
+++ Casks/u/unicodechecker.rb
@@ -1,6 +1,6 @@
 cask "unicodechecker" do
-  version "1.24,835"
-  sha256 "9bfd37706cbcf5c0d579bb4dab904cf9e31d69fa6f1efa7923aa67cf6ebd2ef0"
+  version "1.25,858"
+  sha256 "affa818e149022ded63f808c633a423aec815d665c5fe3665c7c54d4882a5529"
 
   url "https://earthlingsoft.net/UnicodeChecker/UnicodeChecker%20#{version.csv.first}%20(#{version.csv.second}).zip"
   name "UnicodeChecker"
