ebadc8776061daf45a4cab77d01b97d5f95a7acb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 10:14:02 +0000

    launchie 1.2.0

--- Casks/l/launchie.rb
+++ Casks/l/launchie.rb
@@ -1,6 +1,6 @@
 cask "launchie" do
-  version "1.1.6"
-  sha256 "279761ece29bd86df6aa1f856acd4b004d23cf441258a406cb8594389ff8be3e"
+  version "1.2.0"
+  sha256 "400ccfb67c6ad4730736386605b102dad1fc72fdd8e25d069fa9f603828a6f5f"
 
   url "https://github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/releases/download/#{version}/Launchie_#{version}.dmg",
       verified: "github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/"
