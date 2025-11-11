46724005b53dfead78df6eb2ef7a9a9fafcaec3b
izzaldev <rizal.fadlullah@gmail.com>
2025-11-11 15:23:20 +0700

    flutter: add Linux support
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/flutter.rb
+++ Casks/f/flutter.rb
@@ -1,32 +1,30 @@
 cask "flutter" do
-  arch arm: "_arm64"
-
   version "3.38.1"
-  sha256 arm:   "1eab8511396e252420efd46854a843203960a14fb8bb7c65ea16cd8df5a66efc",
-         intel: "8b153a253fe0e2d785d12a7a430b4fb1fda93b6f8326ed75c0a7fb6fd2508ed9"
+  sha256 :no_check
 
-  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
-      verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
+  url "https://github.com/flutter/flutter.git",
+      branch:   "stable",
+      verified: "github.com/flutter/"
   name "Flutter SDK"
-  desc "UI toolkit for building applications for mobile, web and desktop"
+  desc "UI toolkit for building applications for mobile, web, and desktop"
   homepage "https://flutter.dev/"
 
   livecheck do
-    url "https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json"
-    strategy :json do |json|
-      json["releases"]&.map do |release|
-        next if release["channel"] != "stable"
-
-        release["version"]
-      end
-    end
+    url "https://github.com/flutter/flutter.git"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   auto_updates true
 
-  suite "flutter", target: "#{HOMEBREW_PREFIX}/share/flutter"
-  binary "flutter/bin/dart"
-  binary "flutter/bin/flutter"
+  binary "bin/flutter"
+  binary "bin/dart"
+  artifact ".", target: "#{HOMEBREW_PREFIX}/share/flutter"
+
+  postflight do
+    system_command "#{HOMEBREW_PREFIX}/share/flutter/bin/flutter",
+                   args:         ["precache"],
+                   print_stdout: true
+  end
 
   zap trash: "~/.flutter"
 end
