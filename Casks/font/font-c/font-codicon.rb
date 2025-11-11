3b5e172ae3570b8429f3dcc43230f57087f76ee2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-11 19:37:05 +0000

    font-codicon 0.0.42

--- Casks/font/font-c/font-codicon.rb
+++ Casks/font/font-c/font-codicon.rb
@@ -1,6 +1,6 @@
 cask "font-codicon" do
-  version "0.0.41"
-  sha256 "8fc0843471360a465ca7fe857ab43ccc3dfe3863ce02d75bdad476ae0df07d7e"
+  version "0.0.42"
+  sha256 "4024edec0cf71cf9f9fff7d34393ed8ccff48b4159f72c567a1173d843e0f97d"
 
   url "https://registry.npmjs.org/@vscode/codicons/-/codicons-#{version}.tgz",
       verified: "registry.npmjs.org/@vscode/codicons/"
