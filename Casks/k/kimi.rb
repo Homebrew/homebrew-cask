9e5c32521e8028c74a8d91b03882ff3512b9ed3a
Dengjunzeng <dengjunzeng@bwton.com>
2025-11-11 15:54:23 +0800

    kimi 2.0.4 (new cask)

---
+++ Casks/k/kimi.rb
@@ -0,0 +1,24 @@
+cask "kimi" do
+  version "2.0.4"
+  sha256 "3de7ec7e0ee5848482b3655c4cf96e184cc0e6bcb4da9e1bcacf5201cf2a7709"
+
+  url "https://kimi-img.moonshot.cn/app/download/macos/kimi_#{version}.dmg"
+  name "kimi"
+  desc "AI chat assistant from Moonshot"
+  homepage "https://www.moonshot.cn/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Kimi.app"
+
+  zap trash: [
+    "~/Library/Application Support/kimi",
+    "~/Library/Caches/KimiAppCache",
+    "~/Library/HTTPStorages/com.moonshot.kimichat",
+    "~/Library/Preferences/com.moonshot.kimichat.plist",
+  ]
+end
