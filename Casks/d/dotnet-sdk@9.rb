5de6488d48996f48ee93f14ebe4afba0cfe55d80
botantony <antonsm21@gmail.com>
2025-11-11 22:24:34 +0100

    dotnet-sdk@9 9.0.307 (new cask)
    Signed-off-by: botantony <antonsm21@gmail.com>

---
+++ Casks/d/dotnet-sdk@9.rb
@@ -0,0 +1,56 @@
+cask "dotnet-sdk@9" do
+  arch arm: "arm64", intel: "x64"
+
+  version "9.0.307"
+  sha256 arm:   "68d49cde9f8c9b6202056914356bc8396951af398a823ad53e96f6a13bda650b",
+         intel: "4191f95f9435a754435c8118f36b3947d98a737f1e563b8b4a98876d3cb900f4"
+
+  url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
+  name ".NET SDK"
+  desc "Developer platform"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  # This identifies releases with the same major/minor version as the current
+  # cask version. New major/minor releases occur annually in November and the
+  # check will automatically update its behavior when the cask is updated.
+  livecheck do
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
+    end
+  end
+
+  conflicts_with cask: [
+    "dotnet-runtime",
+    "dotnet-runtime@preview",
+    "dotnet-sdk",
+    "dotnet-sdk@8",
+    "dotnet-sdk@preview",
+  ]
+  depends_on macos: ">= :ventura"
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+  binary "/usr/local/share/dotnet/dotnet"
+
+  uninstall pkgutil: [
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
+    "com.microsoft.dotnet.sharedhost*#{arch}",
+    "com.microsoft.netstandard.pack.targeting.*",
+  ]
+
+  zap pkgutil: "com.microsoft.dotnet.*",
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
+end
