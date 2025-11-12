e93c2e606f30e3b927f5a47c5d2aa493ba9deb11
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-11 23:31:06 -0500

    archi: update url
    The existing `url` for `archi` no longer works as the
    archimatetool/archi.io GitHub repository has been deleted. The
    upstream download page now links to dmg files from the same website,
    so this updates the cask accordingly.

--- Casks/a/archi.rb
+++ Casks/a/archi.rb
@@ -5,8 +5,7 @@ cask "archi" do
   sha256 arm:   "dbfc38f9a29f8df4c62e38e5fe73a8b85b67bfbfe48e67dece4ae649ae1a6e57",
          intel: "c0896127a5e684be6b31be73c07258c1a982c15d773cae77c22f6e1172206b92"
 
-  url "https://github.com/archimatetool/archi.io/releases/download/#{version.csv.second}/Archi-Mac#{arch}-#{version.csv.first}.dmg",
-      verified: "github.com/archimatetool/archi.io/"
+  url "https://www.archimatetool.com/downloads/archi/#{version.csv.second}/Archi-Mac#{arch}-#{version.csv.first}.dmg"
   name "Archi"
   desc "Open-source ArchiMate modelling toolkit"
   homepage "https://www.archimatetool.com/"
