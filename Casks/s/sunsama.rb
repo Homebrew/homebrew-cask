81f9354d3c914f6ebf5547621286bcdf211e3937
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-12 02:18:04 +0000

    sunsama 3.2.4

--- Casks/s/sunsama.rb
+++ Casks/s/sunsama.rb
@@ -1,9 +1,9 @@
 cask "sunsama" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.2.3,251028bz2iafdd4"
-  sha256 arm:   "8e226b8fa691d1fc7f7a10d74adbd0e1f6f89a9a12d7904902ebc9d419cfe82a",
-         intel: "7ab46c1f9f040c79b532185f5b88a75c850329a327d51547bcc6e0f8cc163328"
+  version "3.2.4,251111ez0l9jd55"
+  sha256 arm:   "304aac5cb23ccb93559348da10803337b9abfc9c3f10ef26dadaf1d7e6fcb750",
+         intel: "af02aa0a60c2cbfda4d68a89724bb9ed664a7ab95d7c8035eaa22ff8cb455219"
 
   url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/2003096gmmnl0g1/"
