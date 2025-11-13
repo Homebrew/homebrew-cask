ab1999e35f9080de0dba6ad38db3e904f4c75619
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 19:11:08 +0000

    flutter 3.38.1

--- Casks/f/flutter.rb
+++ Casks/f/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64"
 
-  version "3.38.0"
-  sha256 arm:   "f3ddfeca3d44efd3de49ae293238c7c80b967a1d17529abae744c5ad9b5c2c5e",
-         intel: "d60297964c1079c29fb5a62fb9eccd249da45fddb7df15f6914b37af723dbacf"
+  version "3.38.1"
+  sha256 arm:   "1eab8511396e252420efd46854a843203960a14fb8bb7c65ea16cd8df5a66efc",
+         intel: "8b153a253fe0e2d785d12a7a430b4fb1fda93b6f8326ed75c0a7fb6fd2508ed9"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
