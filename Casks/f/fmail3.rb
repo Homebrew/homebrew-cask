ac890cc240fe6e714b6a429ea7c45b7affc83e42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 09:58:11 +0000

    fmail3 1.4.2

--- Casks/f/fmail3.rb
+++ Casks/f/fmail3.rb
@@ -1,6 +1,6 @@
 cask "fmail3" do
-  version "1.4.1"
-  sha256 "9acf63e2b2686842f4b6722a61ca8e5e6362a09b9fec7d1459234256d32e73b9"
+  version "1.4.2"
+  sha256 "27b32b335c3a6b26ba915e07090aa4702ec28dbaff25f12fa9aefa4e385b9a98"
 
   url "https://fmail3.appmac.fr/update/sparkle/FMail3_#{version.no_dots}.zip"
   name "FMail3"
