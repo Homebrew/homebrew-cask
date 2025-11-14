af91865ef0ed3f6082c12925edcb40d5ee0529c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-14 12:53:58 +0000

    electerm 2.3.103

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,9 +1,9 @@
 cask "electerm" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.100"
-  sha256 arm:   "f30e63fe86b238830ddbf4db87947b091a57d9b1bee7d52f202d29548939e0ef",
-         intel: "e4e41533624ec625bb2a2dd246460770d8b8e4c51f792fe9c25d3f7e125e6716"
+  version "2.3.103"
+  sha256 arm:   "70eafcc7cb2b60883ee0ca586e0ab1fbe9dfb1e64b158d76d10753f9959cc2ff",
+         intel: "d5c968fd85e2ccab33f0e3d917a9d2bd937f6bb8086a2c114eb33480c4a2692e"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg",
       verified: "github.com/electerm/electerm/"
