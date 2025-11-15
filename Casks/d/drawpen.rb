9dec239a66f64457295c2537c2d331066da5bdad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-15 18:49:14 +0000

    drawpen 0.0.31

--- Casks/d/drawpen.rb
+++ Casks/d/drawpen.rb
@@ -1,9 +1,9 @@
 cask "drawpen" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.30"
-  sha256 arm:   "b331412a58108986679926a1155fede6bbf533f687653aaee75e3042dbc82fde",
-         intel: "1caab7f5f289ce6562803b814dc98322596e7ac54d0cf39717d3080fc2bacdc4"
+  version "0.0.31"
+  sha256 arm:   "98c2f7dce0acdf316a6f1c1b6613c06d475b6bc911458a438d2770c548ff6417",
+         intel: "267ae1e054a6d34f48883b4a2edf5027540ad99cc8533b14caba68a1c3adcae2"
 
   url "https://github.com/DmytroVasin/DrawPen/releases/download/v#{version}/DrawPen-#{version}-#{arch}.dmg"
   name "DrawPen"
