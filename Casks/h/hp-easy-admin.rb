25e38816efd5a96fb147609472a82d94721a13a0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-11-14 21:36:06 -0500

    hp-easy-admin 2.16.0

--- Casks/h/hp-easy-admin.rb
+++ Casks/h/hp-easy-admin.rb
@@ -1,5 +1,5 @@
 cask "hp-easy-admin" do
-  version "2.15.0,240916"
+  version "2.16.0,251010"
   sha256 :no_check
 
   url "https://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Admin.app.zip"
