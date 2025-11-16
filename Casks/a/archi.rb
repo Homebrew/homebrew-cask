cf37e43398d075dc5e6959fb020c9506694a602e
Al3nMicL <al3nmicl@users.noreply.github.com>
2025-11-16 16:14:29 -0500

    archi: fix download URL to use GitHub releases

--- Casks/a/archi.rb
+++ Casks/a/archi.rb
@@ -1,19 +1,21 @@
 cask "archi" do
   arch arm: "-Silicon"
 
-  version "5.7.0"
+  version "5.7.0,5"
   sha256 arm:   "dbfc38f9a29f8df4c62e38e5fe73a8b85b67bfbfe48e67dece4ae649ae1a6e57",
          intel: "c0896127a5e684be6b31be73c07258c1a982c15d773cae77c22f6e1172206b92"
 
-  url "https://www.archimatetool.com/downloads/archi/#{version.major_minor}/Archi-Mac#{arch}-#{version}.dmg"
+  url "https://github.com/archimatetool/archi.io/releases/download/#{version.csv.second}/Archi-Mac#{arch}-#{version.csv.first}.dmg"
   name "Archi"
   desc "Open-source ArchiMate modelling toolkit"
   homepage "https://www.archimatetool.com/"
 
   livecheck do
-    url "https://www.archimatetool.com/download/"
-    regex(%r{href=.*?/Archi[._-]Mac#{arch}[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-    strategy :page_match
+    url "https://github.com/archimatetool/archi.io/releases"
+    regex(%r{/releases/download/v?(\d+)/Archi[._-]Mac#{arch}[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
+    end
   end
 
   depends_on macos: ">= :big_sur"
