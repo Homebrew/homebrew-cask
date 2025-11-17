008527f857b2c1715e76382d9ea37ba904a755f6
Patrick Linnane <patrick@linnane.io>
2025-11-17 09:12:44 -0800

    lexicon-dj: add minimum macOS versions
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/l/lexicon-dj.rb
+++ Casks/l/lexicon-dj.rb
@@ -5,6 +5,13 @@ cask "lexicon-dj" do
   sha256 arm:   "572c0303ee5449a3592bcc7b95b348dfd12ecb443c3ba1508bb8e46230724f90",
          intel: "d3fa349b13417cd89890b221f0e6a8930657f0ca10d75fe393da44b9468c165f"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :catalina"
+  end
+
   url "https://update.rekord.cloud/releases/lexicon/latest/lexicon-#{version}-mac-#{arch}.zip",
       verified: "update.rekord.cloud/releases/lexicon/latest/"
   name "Lexicon"
