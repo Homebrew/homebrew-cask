3b323a5a2b38a1459233d8a35aad531fe062551e
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-17 11:56:15 -0500

    Rename eid-viewer to beid-viewer
    `eid-viewer` was merged before `beid-token` but these are related and
    should use the same naming convention. We decided on the `beid-`
    prefix in the `beid-token` PR and I suggested that `eid-viewer` should
    be renamed and updated to reflect feedback from that PR but this did
    not happen. This renames `eid-viewer` to `beid-viewer` to align it
    with `beid-token`.

--- Casks/b/beid-viewer.rb
+++ Casks/b/beid-viewer.rb
@@ -1,9 +1,9 @@
-cask "eid-viewer" do
+cask "beid-viewer" do
   version "5.1.22"
   sha256 "925bf141569f0f27c28b54f25f2cfe61c5733a5bc5be4d1a2e827cdf3da21a80"
 
   url "https://eid.belgium.be/sites/default/files/software/eID%20Viewer-#{version}.dmg"
-  name "Belgian EID Viewer"
+  name "Belgian eID Viewer"
   desc "Belgian ID card reader"
   homepage "https://eid.belgium.be/"
 
