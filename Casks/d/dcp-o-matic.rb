999ba94760e06ed68a788f3fe2df9fbe7ea4744a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-17 10:35:42 -0500

    dcp-o-matic 2.18.31
    I disabled `dcp-o-matic` on 2025-07-28 because the Cloudflare
    protections on the website prevented brew from being able to fetch the
    dmg file (or check for new versions), so the cask was effectively
    unusable. The website is now accessible and has remained that way
    during the two weeks I've been testing it, so this re-enables the cask
    and updates it to 2.18.31.

--- Casks/d/dcp-o-matic.rb
+++ Casks/d/dcp-o-matic.rb
@@ -1,13 +1,18 @@
 cask "dcp-o-matic" do
-  version "2.18.21"
-  sha256 "2fb593b6dbd47fec63f7df6eef65c03eed7a495b1ed15156d58026297d3a3f03"
+  version "2.18.31"
+  sha256 "13bbcbeb335da648082acad7c6b4b808b9fb0c96d7aeebaef205900da07c76b9"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
   name "DCP-o-matic"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Package)"
   homepage "https://dcpomatic.com/"
 
-  disable! date: "2025-07-28", because: "cannot be reliably fetched due to Cloudflare protections"
+  livecheck do
+    url "https://dcpomatic.com/download"
+    regex(/Stable\s+release:\s*v?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "DCP-o-matic #{version.major}.app"
 
