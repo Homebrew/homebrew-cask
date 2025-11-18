5ca6a861ccbf9fe7b676d02d5bc8b9cd368c773c
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-18 12:32:08 -0500

    cmake-app: add macOS dependency
    `cmake-app` uses a universal app and the `Info.plist` doesn't specify
    a minimum system version, so `brew audit` identifies the requirements
    from the binary. In this case, the minimum macOS version is 10.13 or
    later for Intel and 11 or later for ARM but the audit simply gives an
    "Artifact defined :big_sur as the minimum macOS version but the cask
    declared no minimum macOS version" error when run on ARM. We've been
    dealing with a similar situation with separate ARM/Intel apps, where
    the Intel app can use the implicit macOS dependency but the ARM
    version depends on a higher macOS version, so we also have to add a
    `depends_on macos:` value for Intel that makes the implicit value
    explicit. This is effectively the same issue but with a universal app
    instead, so this addresses the issue using on_os blocks (with the
    Intel value being added to appease `brew style`).

--- Casks/c/cmake-app.rb
+++ Casks/c/cmake-app.rb
@@ -2,6 +2,13 @@ cask "cmake-app" do
   version "4.1.3"
   sha256 "c8a8bf120c438a8ace960d6caf22481f9432d23fb5176bef9fb9ab4dfeb5abe8"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :catalina"
+  end
+
   url "https://cmake.org/files/v#{version.major_minor}/cmake-#{version}-macos-universal.dmg"
   name "CMake"
   desc "Family of tools to build, test and package software"
