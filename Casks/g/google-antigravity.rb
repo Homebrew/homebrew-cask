6bd98939a8bdbcc0fb00b1dcd2285d60baba2825
Alex Koch <kochalex@users.noreply.github.com>
2025-11-18 15:03:03 -0700

    get livecheck working, use zip download matching what is returned from livecheck

--- Casks/g/google-antigravity.rb
+++ Casks/g/google-antigravity.rb
@@ -4,16 +4,20 @@ cask "google-antigravity" do
   version "1.11.2,6251250307170304"
   sha256 :no_check
 
-  url "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/#{version.csv.first}-#{version.csv.second}/#{arch}/Antigravity.dmg",
+  url "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/#{version.csv.first}-#{version.csv.second}/#{arch}/Antigravity.zip",
       verified: "edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/"
   name "Google Antigravity"
   desc "AI Coding Agent IDE"
   homepage "https://antigravity.google/"
 
   livecheck do
-    # version in artifact is only the short version
-    # download website pulls link from fingerprinted javascript file
-    skip "No version information available"
+    url "https://antigravity-auto-updater-974169037036.us-central1.run.app/api/update/darwin-arm64/stable/latest"
+    strategy :json do |json|
+      match = json["url"]&.match(%r{/stable/([\d.]+-(\d+))/})
+      next if match.blank?
+
+      "#{match[1].split("-").first},#{match[2]}"
+    end
   end
 
   auto_updates true
