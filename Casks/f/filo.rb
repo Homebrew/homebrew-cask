d491a4a287a504d3c6e0886e5988c73d524b7c65
Carolyn Sun <117613176+carolyn-sun@users.noreply.github.com>
2025-11-18 13:47:07 +0800

    fix: audit issue

--- Casks/f/filo.rb
+++ Casks/f/filo.rb
@@ -2,8 +2,7 @@ cask "filo" do
   arch arm: "arm64", intel: "x64"
 
   version "1.2.5"
-  sha256 arm:   "d19dcf0031b52d5687570d9f569ea9e75b13d201a3bb65127fceec8f66ff1b07",
-         intel: "cff6db5f649c61ab02798a5936acf7631f60bc45f38642583d96b40f4042fe15"
+  sha256 :no_check
 
   url "https://download.filomail.com/mac_upgrade/versions/latest/prod/#{arch}/Filo-#{arch}.dmg"
   name "FiloMail"
@@ -15,7 +14,7 @@ cask "filo" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :big_sur"
 
   app "Filo.app"
 
