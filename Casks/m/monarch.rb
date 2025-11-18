17077ced8ec567d1a2b71d784299ef2a4164d32a
Michael Law <rmdashrfv@hey.com>
2025-11-18 13:14:09 -0500

    monarch 0.8.31 livecheck updated

--- Casks/m/monarch.rb
+++ Casks/m/monarch.rb
@@ -23,7 +23,7 @@ cask "monarch" do
         verified: "storage.googleapis.com/monarchlauncher/"
 
     livecheck do
-      url "https://storage.googleapis.com/monarchlauncher/#{arch}/latest.json"
+      url "https://storage.googleapis.com/monarchlauncher/#{arch}/latest.yml"
       strategy :electron_builder
     end
   end
