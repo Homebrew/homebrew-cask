5a89e05f415a84829021b50d628ba870476b346d
Klaus Hipp <khipp@users.noreply.github.com>
2025-11-18 14:55:49 +0100

    pdl 0.9.1

--- Casks/p/pdl.rb
+++ Casks/p/pdl.rb
@@ -1,8 +1,8 @@
 cask "pdl" do
-  version "0.9.0"
-  sha256 "4c3bbbb7504e9e4ee2e5e776017f6e0dbf70c7791cee34cdfd9ff20291ae37e6"
+  version "0.9.1"
+  sha256 "7ab2bcfe633c23656395e35d80e053933abe5adcf95ee7ecfcd37de6e05d6adc"
 
-  url "https://github.com/IBM/prompt-declaration-language/releases/download/v#{version}/PDL_#{version}_universal_darwin.dmg",
+  url "https://github.com/IBM/prompt-declaration-language/releases/download/v#{version}/PDL_#{version}_universal.dmg",
       verified: "github.com/IBM/prompt-declaration-language/"
   name "PDL"
   desc "Declarative language for creating reliable, composable LLM prompts"
