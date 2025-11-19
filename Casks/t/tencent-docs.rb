ffe7ea5c68cd69d8e2b087ef0f63c84c5385779e
RyanHuV <koeniy@outlook.com>
2025-11-19 15:01:30 +0800

    tencent-docs: fix url and livecheck

--- Casks/t/tencent-docs.rb
+++ Casks/t/tencent-docs.rb
@@ -1,20 +1,20 @@
 cask "tencent-docs" do
-  arch arm: "arm64", intel: "x64"
+  version "3.10.15"
+  sha256 "94705039e8882f5d63ae01150b0b55f1adb068466e48b5ffbc84b3f6eb13d90b"
 
-  version "3.10.1"
-  sha256 arm:   "43946030f8e2f63ccb4f2d7ebc887a84bd1134ea997ece994499f29d6698b776",
-         intel: "bbb13d2d970688b85a274d8a27ec8f3948e6ae68d1b75d4ab7fef00f12593490"
-
-  url "https://desktop.docs.qq.com/Installer/30001/#{version}/TencentDocs-#{arch}.dmg"
+  url "https://desktop.docs.qq.com/update/release/#{version}/TencentDocs#{version}.30002.zip"
   name "Tencent Docs"
   name "腾讯文档"
   desc "Online editor for Word, Excel and PPT documents"
   homepage "https://docs.qq.com/"
 
-  # Newer versions of TencentDocs use a URL that doesn't work in the cask, so
-  # there doesn't appear to be any usable upstream dmg file to continue updating
-  # this cask beyond the current version.
-  deprecate! date: "2025-10-05", because: :unreachable
+  livecheck do
+    url "https://docs.qq.com/api/packageupgrade/update_manual"
+    strategy :json do |json|
+      update_info = json.dig("result", "update_info")
+      update_info&.match(/"version":\s*"([0-9.]+)"/)&.[](1)
+    end
+  end
 
   auto_updates true
 
