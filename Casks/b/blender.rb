61a57990857de532c7a97ee7c87b57ab72824d1a
bevanjkay <email@bevankay.me>
2025-11-19 14:59:21 +1100

    blender 5.0.0

--- Casks/b/blender.rb
+++ Casks/b/blender.rb
@@ -4,39 +4,52 @@ cask "blender" do
   # NOTE: Please manually check for a new `blender@lts` version at
   # https://www.blender.org/download/lts/ when updating this cask, as we cannot
   # identify LTS versions using livecheck.
-  version "4.5.5"
-  sha256 arm:   "de1d239d1326378744326bdf1a55f935cdd3d7e808eadd0d601bba28e06e05f6",
-         intel: "f60c27f9a52ccde87e9e68ae8184e5c8c56f8b622d0657b345c64ad2b1232174"
+  on_arm do
+    version "5.0.0"
+    sha256 "b23984753ec8354878acc25cd4073a41ae195522013f1649903a593bccbfa54b"
+
+    # The upstream download page (https://www.blender.org/download/) cannot be
+    # fetched due to Cloudflare protections, so we have to naively assume a
+    # version is released when the assets are uploaded.
+    livecheck do
+      url "https://download.blender.org/release/"
+      regex(/href=.*?blender[._-]v?(\d+(?:\.\d+)+)(?:[._-]macos)?[._-]#{arch}\.dmg/i)
+      strategy :page_match do |page, regex|
+        # Match major/minor versions from stable directory names
+        major_minor_versions =
+          page.scan(%r{href=["']?[^"' >]*?Blender[._-]?v?(\d+(?:\.\d+)+)/?["' >]}i)
+              .flatten
+              .uniq
+              .sort_by { |v| Version.new(v) }
+        next if major_minor_versions.blank?
+
+        # Check the directory listing page for the highest major/minor version
+        directory_page = Homebrew::Livecheck::Strategy.page_content(
+          URI.join(@url, "Blender#{major_minor_versions.last}/").to_s,
+        )[:content]
+        next if directory_page.blank?
+
+        directory_page.scan(regex).map { |match| match[0] }
+      end
+    end
+  end
+  on_intel do
+    version "4.5.5"
+    sha256 "f60c27f9a52ccde87e9e68ae8184e5c8c56f8b622d0657b345c64ad2b1232174"
+
+    # Intel support is limited to version 4.5.x series.
+    livecheck do
+      url "https://download.blender.org/release/Blender4.5/"
+      regex(/href=.*?blender[._-]v?(\d+(?:\.\d+)+)(?:[._-]macos)?[._-]#{arch}\.dmg/i)
+    end
+
+    deprecate! date: "2027-07-01", because: :unsupported
+  end
 
   url "https://download.blender.org/release/Blender#{version.major_minor}/blender-#{version}-macos-#{arch}.dmg"
   name "Blender"
   desc "3D creation suite"
   homepage "https://www.blender.org/"
-
-  # The upstream download page (https://www.blender.org/download/) cannot be
-  # fetched due to Cloudflare protections, so we have to naively assume a
-  # version is released when the assets are uploaded.
-  livecheck do
-    url "https://download.blender.org/release/"
-    regex(/href=.*?blender[._-]v?(\d+(?:\.\d+)+)(?:[._-]macos)?[._-]#{arch}\.dmg/i)
-    strategy :page_match do |page, regex|
-      # Match major/minor versions from stable directory names
-      major_minor_versions =
-        page.scan(%r{href=["']?[^"' >]*?Blender[._-]?v?(\d+(?:\.\d+)+)/?["' >]}i)
-            .flatten
-            .uniq
-            .sort_by { |v| Version.new(v) }
-      next if major_minor_versions.blank?
-
-      # Check the directory listing page for the highest major/minor version
-      directory_page = Homebrew::Livecheck::Strategy.page_content(
-        URI.join(@url, "Blender#{major_minor_versions.last}/").to_s,
-      )[:content]
-      next if directory_page.blank?
-
-      directory_page.scan(regex).map { |match| match[0] }
-    end
-  end
 
   conflicts_with cask: "blender@lts"
   depends_on macos: ">= :big_sur"
