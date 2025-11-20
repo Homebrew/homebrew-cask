90a8cad0d5a5eb6006eb17420faa27d08e6088fd
十里 <okooo5km@gmail.com>
2025-11-20 10:47:01 +0800

    Update Zipic cask URLs and homepage

--- Casks/z/zipic.rb
+++ Casks/z/zipic.rb
@@ -2,13 +2,13 @@ cask "zipic" do
   version "1.8.4"
   sha256 "5c78290d61f50cf54a3cee1edc824c61bc114f48c5d77e3650b94649518c93e6"
 
-  url "https://zipic.5km.tech/Zipic%20#{version}.dmg"
+  url "https://releases.5km.tech/zipic/Zipic%20#{version}.dmg"
   name "Zipic"
   desc "Image compression tool"
-  homepage "https://zipic.5km.tech/"
+  homepage "https://zipic.app/"
 
   livecheck do
-    url "https://zipic.5km.tech/appcast.xml"
+    url "https://releases.5km.tech/zipic/appcast.xml"
     strategy :sparkle, &:short_version
   end
 
@@ -18,7 +18,9 @@ cask "zipic" do
   app "Zipic.app"
 
   zap trash: [
+    "~/Library/Application Support/zipic",
     "~/Library/Preferences/studio.5km.zipic.plist",
     "~/Library/Saved Application State/studio.5km.zipic.savedState",
+    "~/Library/zipic",
   ]
 end
