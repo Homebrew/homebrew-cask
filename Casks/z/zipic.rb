207afde3aabe6db564f1079cf06f6d4a177e0afa
十里 <okooo5km@gmail.com>
2025-11-20 10:55:12 +0800

    Add URL verification for zipic cask
    Added verification for the download URL to enhance security.

--- Casks/z/zipic.rb
+++ Casks/z/zipic.rb
@@ -2,7 +2,8 @@ cask "zipic" do
   version "1.8.4"
   sha256 "5c78290d61f50cf54a3cee1edc824c61bc114f48c5d77e3650b94649518c93e6"
 
-  url "https://releases.5km.tech/zipic/Zipic%20#{version}.dmg"
+  url "https://releases.5km.tech/zipic/Zipic%20#{version}.dmg",
+      verified: "releases.5km.tech/"
   name "Zipic"
   desc "Image compression tool"
   homepage "https://zipic.app/"
