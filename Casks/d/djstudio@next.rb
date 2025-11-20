41432384f3d8c05143f8659efa7be6202ec46a63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-20 12:56:45 +0000

    djstudio@next 4.0.10-beta.0

--- Casks/d/djstudio@next.rb
+++ Casks/d/djstudio@next.rb
@@ -2,9 +2,9 @@ cask "djstudio@next" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "-apple"
 
-  version "4.0.9-beta.0"
-  sha256 arm:   "2b554efca660b89a996ab5195ea552b4be1063509d078f0e363799fc14baf4c8",
-         intel: "5955ddfbbdff6ce10bfe005210d66885db2876ee01f3fc0394e9d34b93fd3ea1"
+  version "4.0.10-beta.0"
+  sha256 arm:   "6a680286b55466b9c5f5565d1ee1bbf6284eeff0135fb2a29d3f8646db646973",
+         intel: "7379b25be2f598529ce032530683f0b8b122ed9e831d6acb55f69f6370a10f25"
 
   url "https://download.next.dj.studio/DJ.Studio%20Next-#{version}#{arch}.dmg"
   name "DJ.Studio Next"
