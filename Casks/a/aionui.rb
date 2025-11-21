87ce88aa93ffd86de603e2d3b96d251cb9e28615
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 03:43:41 +0000

    aionui 1.5.5

--- Casks/a/aionui.rb
+++ Casks/a/aionui.rb
@@ -1,9 +1,9 @@
 cask "aionui" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.4"
-  sha256 arm:   "a915382b95c82eb18403d610437434e7ea34bca6b56489c549e8455391738722",
-         intel: "8e3afc382399e3473567457d9dd1be39f904adb88377022334c8828786f77690"
+  version "1.5.5"
+  sha256 arm:   "8b636d327101130570c59c8d881757f80134c9e24f3db9a741e8c2c552b1c174",
+         intel: "9b50832b1b04d097de367855b06deaf6cbe6ac3cd5a338e0c3bcec717b7bc18b"
 
   url "https://github.com/iOfficeAI/AionUi/releases/download/v#{version}/AionUi-#{version}-mac-#{arch}.dmg"
   name "AionUI"
