605fe5313d489895084000bb52e2408959fbeb90
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 01:51:06 +0000

    postman-agent 0.4.77

--- Casks/p/postman-agent.rb
+++ Casks/p/postman-agent.rb
@@ -1,9 +1,9 @@
 cask "postman-agent" do
   arch arm: "osx_arm64", intel: "osx_64"
 
-  version "0.4.76"
-  sha256 arm:   "557755530ec99e434a226d93f1f893e59a3d9bbe17d70b7c8c8e36a86d01dd54",
-         intel: "ce3e748ca4d604388257c9a4e0618682f95660bf368150e0d1aee0de584085d9"
+  version "0.4.77"
+  sha256 arm:   "f77926e79ba3242b6291ceb149ee2ffcc647be678f036b156042cee76644c433",
+         intel: "228a4d5bdb524412d1cefa29ff518d79c38920f14eada8427d1a7d8d3f048223"
 
   url "https://dl-agent.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl-agent.pstmn.io/download/version/"
