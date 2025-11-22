a367dc7a39d01f9aa1cdf565edd61e5ae90b6abd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-11-22 11:02:25 -0500

    blood-on-the-clocktower-online 3.48.0

--- Casks/b/blood-on-the-clocktower-online.rb
+++ Casks/b/blood-on-the-clocktower-online.rb
@@ -1,11 +1,11 @@
 cask "blood-on-the-clocktower-online" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.47.4"
-  sha256 arm:   "6b5b79dcc0ac7eaf428f737859ef11347130af5b3ed4c65bf94302216a1e7dcf",
-         intel: "b13b1ab6018f5fc1a3dfaa974fa585ad360a3f4cd1aa03ddb9f93a0de4c6c2cf"
+  version "3.48.0"
+  sha256 arm:   "9d5f3e01a94dab1af9fe102ab6bcd2a3faee0fcbc5d1ed34411986bc2d46815c",
+         intel: "93c7b73559862f296dd37c71d428657f4c1df8c81467709a4cc978d46f7a4f85"
 
-  url "https://github.com/ThePandemoniumInstitute/botc-release/releases/download/v#{version}/Blood.on.the.Clocktower.Online_#{version}_#{arch}_darwin.dmg",
+  url "https://github.com/ThePandemoniumInstitute/botc-release/releases/download/v#{version}/Blood.on.the.Clocktower.Online_#{version}_#{arch}.dmg",
       verified: "github.com/ThePandemoniumInstitute/botc-release/"
   name "Blood on the Clocktower Online"
   desc "Client for the game Blood on the Clocktower"
