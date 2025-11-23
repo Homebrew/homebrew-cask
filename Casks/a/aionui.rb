8cd1520ee23f01e9a849e3db10edb7f19edb0555
Rui Chen <rui@chenrui.dev>
2025-11-23 11:22:56 -0500

    aionui: fails gatekeeper check
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/a/aionui.rb
+++ Casks/a/aionui.rb
@@ -5,6 +5,10 @@ cask "aionui" do
   sha256 arm:   "8b636d327101130570c59c8d881757f80134c9e24f3db9a741e8c2c552b1c174",
          intel: "9b50832b1b04d097de367855b06deaf6cbe6ac3cd5a338e0c3bcec717b7bc18b"
 
+  on_intel do
+    disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  end
+
   url "https://github.com/iOfficeAI/AionUi/releases/download/v#{version}/AionUi-#{version}-mac-#{arch}.dmg"
   name "AionUI"
   desc "GUI for Gemini CLI"
