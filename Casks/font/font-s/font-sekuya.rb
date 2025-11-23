3444b5bd74b497846852a0c8ff5630aa6f2638e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-23 00:18:07 +0000

    font-sekuya: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-s/font-sekuya.rb
@@ -0,0 +1,12 @@
+cask "font-sekuya" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/sekuya/Sekuya-Regular.ttf"
+  name "Sekuya"
+  homepage "https://github.com/kevinnseptian/SEKUYA"
+
+  font "Sekuya-Regular.ttf"
+
+  # No zap stanza required
+end
