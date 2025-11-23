a9be8ee17b9ab610a880e536d08e1693326961ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-23 00:18:18 +0000

    font-kedebideri: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-k/font-kedebideri.rb
+++ Casks/font/font-k/font-kedebideri.rb
@@ -3,10 +3,11 @@ cask "font-kedebideri" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/kedebideri"
   name "Kedebideri"
-  homepage "https://github.com/silnrsi/font-kedebideri"
+  homepage "https://fonts.google.com/specimen/Kedebideri"
 
   font "Kedebideri-Black.ttf"
   font "Kedebideri-Bold.ttf"
