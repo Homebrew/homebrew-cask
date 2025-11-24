ff324d9632489a7f075a8b7f9618060a7db71db3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-24 18:50:41 +0000

    claude-code 2.0.51

--- Casks/c/claude-code.rb
+++ Casks/c/claude-code.rb
@@ -2,11 +2,11 @@ cask "claude-code" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.0.50"
-  sha256 arm:          "a8d06f2ddb2f09817eca397c11efdd960e7e25638d17c59760f1cfd371d04430",
-         x86_64:       "3ec59e209e45f86f0f63e08736142c220e86c5e79dffa21ca8392e187cbf6bce",
-         x86_64_linux: "2c3399e8a22e607c61ae007b3ffe11633648cda195e790568ec75f19cf550833",
-         arm64_linux:  "1488d9c228c0c97f651c6fcb37053c5dffa86f31f77b232d47e61bff7da5622e"
+  version "2.0.51"
+  sha256 arm:          "5c1a45b0e9793034df03e3a480fc9b388b554491d863d33e8c2d47312880580b",
+         x86_64:       "b1bbd28a5ca33c1a411cb5376888485883ef913717e6e80179ac59ce8b6d0f9a",
+         x86_64_linux: "1e05cb6a6fd29b87c12a023231de65ed0b0ecdf43afb6fda88271f27949b72e4",
+         arm64_linux:  "9752961c3bcb9f319eec9c308cdcce5bbbdd686288a2c952013768386107a574"
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
