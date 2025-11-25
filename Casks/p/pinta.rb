212485a9ffa897a3ccd8bc752698bd779c151e34
Patrick Linnane <patrick@linnane.io>
2025-11-24 21:53:11 -0800

    pinta: add minimum macOS versions
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/pinta.rb
+++ Casks/p/pinta.rb
@@ -5,6 +5,13 @@ cask "pinta" do
   sha256 arm:   "c5ba1f3bb094a63a7a54cb64ea7864aee474e46c2f86292dead5dbc8b1e9a351",
          intel: "114f119158c31139709d083e12a825aebb798d51630789ec703f4e29dfb93377"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :catalina"
+  end
+
   url "https://github.com/PintaProject/Pinta/releases/download/#{version}/Pinta-macos-#{arch}.dmg",
       verified: "github.com/PintaProject/Pinta/"
   name "Pinta"
