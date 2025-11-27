aa06863d652538cb800a0f4fed42a0298aab32ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 16:18:51 +0000

    kontur-talk 3.3.0

--- Casks/k/kontur-talk.rb
+++ Casks/k/kontur-talk.rb
@@ -1,6 +1,6 @@
 cask "kontur-talk" do
-  version "3.2.1"
-  sha256 "80e125cddf12e1b15a9512498ad9c48cd07a27c8f91222392fb63913b421b7f3"
+  version "3.3.0"
+  sha256 "202c8d6e299446b5e0789104f8d587bb1a6d33846d693b69a140161c4d2f7e06"
 
   url "https://st.ktalk.host/ktalk-app/mac/ktalk.#{version}-mac.dmg",
       verified: "st.ktalk.host/"
