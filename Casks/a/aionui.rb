3d203e05fc770c31480ee9991e0533bcaa1d432a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 03:44:11 +0000

    aionui 1.6.0

--- Casks/a/aionui.rb
+++ Casks/a/aionui.rb
@@ -1,13 +1,9 @@
 cask "aionui" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.5"
-  sha256 arm:   "8b636d327101130570c59c8d881757f80134c9e24f3db9a741e8c2c552b1c174",
-         intel: "9b50832b1b04d097de367855b06deaf6cbe6ac3cd5a338e0c3bcec717b7bc18b"
-
-  on_intel do
-    disable! date: "2026-09-01", because: :fails_gatekeeper_check
-  end
+  version "1.6.0"
+  sha256 arm:   "4937bfbf70ea728212bfd44c78be527b431b39b302baa5b0830105b438e7a3c2",
+         intel: "4e16bb06b72381e6353f888d055b69305e04e27929c67fed96b66655eefe64ce"
 
   url "https://github.com/iOfficeAI/AionUi/releases/download/v#{version}/AionUi-#{version}-mac-#{arch}.dmg"
   name "AionUI"
