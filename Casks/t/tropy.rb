f490ff45f7d2ba13e03f099a223b9571e1923e05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 14:09:00 +0000

    tropy 1.17.2

--- Casks/t/tropy.rb
+++ Casks/t/tropy.rb
@@ -1,9 +1,9 @@
 cask "tropy" do
   arch arm: "-arm64"
 
-  version "1.17.1"
-  sha256 arm:   "a618c3f6d684ba26787a9599ceb2864b85bc962d80b26082b85ed6f4e201b0ca",
-         intel: "0216b06f4f7afedacad4675f268524195a3dbf0cec8dca9cbf392bda54ddbb66"
+  version "1.17.2"
+  sha256 arm:   "ba9e6b73a7bdc0570b4e66f4d328888971c5da3f8b50e66f1570288d2e789983",
+         intel: "0d21d546037585690382c60ac5ecfe5e8a8fff9b8ebec4ae3bcd7f05b3226406"
 
   url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}#{arch}.dmg",
       verified: "github.com/tropy/tropy/"
