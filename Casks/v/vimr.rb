0dc5907ae09c67ea5166a1dea7d46e99532c8ec6
Patrick Linnane <patrick@linnane.io>
2025-11-27 12:05:47 -0800

    vimr: bump minimum macOS version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/v/vimr.rb
+++ Casks/v/vimr.rb
@@ -15,7 +15,7 @@ cask "vimr" do
   end
 
   auto_updates true
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :sonoma"
 
   app "VimR.app"
   binary "#{appdir}/VimR.app/Contents/Resources/vimr"
