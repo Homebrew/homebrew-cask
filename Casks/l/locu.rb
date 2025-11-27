da0aff1244d74133fd11725419f7a7024acdc4ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 16:22:59 +0000

    locu 0.24.0

--- Casks/l/locu.rb
+++ Casks/l/locu.rb
@@ -1,9 +1,9 @@
 cask "locu" do
   arch arm: "-arm64"
 
-  version "0.23.3"
-  sha256 arm:   "7f8581435b06546e88755b1e46b5b09a4c207820208e55d1e81b24e9524186be",
-         intel: "74517eb574b69165235c59e5dcead9959c290f68c242eff3604afc874e040707"
+  version "0.24.0"
+  sha256 arm:   "6deeb1c223d4dc2f56674c9530afaf2d7e045adbbb559a4693d8fc59ca732bcb",
+         intel: "a5db72dbe0cff05f43402e159de5c4176be481e2b5b1b81271f537a5fa9e3a18"
 
   url "https://locu.sfo2.digitaloceanspaces.com/Locu-#{version}#{arch}-mac.zip",
       verified: "locu.sfo2.digitaloceanspaces.com/"
