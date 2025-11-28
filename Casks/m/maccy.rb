e862a232e1cf38c7ff939fa5181af31755982868
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 01:17:30 +0000

    maccy 2.6.1

--- Casks/m/maccy.rb
+++ Casks/m/maccy.rb
@@ -1,6 +1,6 @@
 cask "maccy" do
-  version "2.6.0"
-  sha256 "70b99199b4b41097f550f7e5ae3d80c9fb2e55f074c5284471beae25c467c1cb"
+  version "2.6.1"
+  sha256 "84b95baf1961bdf30045188c855237f90c1426ac8f123b4ae8f74191f9f38682"
 
   url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip",
       verified: "github.com/p0deje/Maccy/"
