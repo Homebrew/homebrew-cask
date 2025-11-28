930dff8bbd0e94b60d6abd63a7d348affb041ff5
John Benninghoff <jbenninghoff@mac.com>
2025-11-28 10:55:54 -0600

    display-pilot 1.9.0.0

--- Casks/d/display-pilot.rb
+++ Casks/d/display-pilot.rb
@@ -1,6 +1,6 @@
 cask "display-pilot" do
-  version "1.8.0.0,251016161541"
-  sha256 "a04ad18faa024e1fa8c4e8a2b8e79e1fe1bb3c57c922d6cadc5067c310713a04"
+  version "1.9.0.0,251126155504"
+  sha256 "9e436f02957b68ac3b8247ef4933799e940bb5352f08b95b4549b3fa0e5316bc"
 
   url "https://esupportdownload.benq.com/esupport/VERTICAL%20&%20PROFESSIONAL%20DISPLAY/Software/Display%20Pilot%202/Display%20Pilot%202%20for%20Mac_v#{version.csv.first}_Mac_#{version.csv.second}.zip"
   name "Display Pilot 2"
