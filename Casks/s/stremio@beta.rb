f024d5f013b2ec9afcb4f37ca3ba4f0a7ce40034
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 07:28:01 +0000

    stremio@beta 5.1.8

--- Casks/s/stremio@beta.rb
+++ Casks/s/stremio@beta.rb
@@ -1,9 +1,9 @@
 cask "stremio@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.1.7"
-  sha256 arm:   "1886dde77a31e3b762b0c799c69d3b6567f7d186ca25eb3b60c15c6e32efd044",
-         intel: "cf5345e9d42337c6f7a67d97d822445b97cd3e9c587e66c80770004f528e84ca"
+  version "5.1.8"
+  sha256 arm:   "d80feb2e5553fc61336d303437e81290bcd2fbac12cf295cd05eb5c68f1085f2",
+         intel: "d757a1faa512ead945d2e8655240f08a9201c8406dd6beaac913e35d7fd0012b"
 
   url "https://dl.strem.io/stremio-shell-macos/v#{version}/Stremio_#{arch}.dmg"
   name "Stremio"
