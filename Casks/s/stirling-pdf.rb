24b077b812128d6d577d1c836e5e89a00bb5e603
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 22:21:43 +0000

    stirling-pdf 2.0.2

--- Casks/s/stirling-pdf.rb
+++ Casks/s/stirling-pdf.rb
@@ -1,9 +1,9 @@
 cask "stirling-pdf" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.0.1"
-  sha256 arm:   "48cd6a87cf13cafbaacd536285947c81cc43f81fd91ecaf70c77109d435c0182",
-         intel: "78b19559f4d246cfadc28977450ded95b4d4bb846ca64847abeb43a7e896b717"
+  version "2.0.2"
+  sha256 arm:   "43d3e6d5c77c9d8b26f2977bc21e5515277d9aef68dfa9146e8955db8e66463b",
+         intel: "efa6e0296f1535a2f6f97c53b2456b52ac533df3a67182050bdd3893580c896f"
 
   url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF-macos-#{arch}.dmg",
       verified: "github.com/Stirling-Tools/Stirling-PDF/"
