a8a90687efbd91b10e3fd09bc142abc375efb50e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 00:18:06 +0000

    font-bbh-bogle: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-b/font-bbh-bogle.rb
@@ -0,0 +1,12 @@
+cask "font-bbh-bogle" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bbhbogle/BBHBogle-Regular.ttf"
+  name "BBH Bogle"
+  homepage "https://github.com/Studio-DRAMA/BBH"
+
+  font "BBHBogle-Regular.ttf"
+
+  # No zap stanza required
+end
