b8aa6ba68c3bdc9064a15b1849d697b4f32ec4c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 22:06:57 +0000

    mediahuman-youtube-downloader 3.9.18

--- Casks/m/mediahuman-youtube-downloader.rb
+++ Casks/m/mediahuman-youtube-downloader.rb
@@ -1,7 +1,7 @@
 cask "mediahuman-youtube-downloader" do
   arch arm: "-arm"
 
-  version "3.9.17"
+  version "3.9.18"
   sha256 :no_check
 
   url "https://www.mediahuman.net/files/YouTubeDownloader#{arch}.dmg"
