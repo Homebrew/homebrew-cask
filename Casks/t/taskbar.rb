bd00358e7d70c651287489b64f03958130ea78a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 02:07:46 +0000

    taskbar 1.5.2.1

--- Casks/t/taskbar.rb
+++ Casks/t/taskbar.rb
@@ -1,6 +1,6 @@
 cask "taskbar" do
-  version "1.5.2,2025,11"
-  sha256 "2026ef3c58a0bd7b5d56b317c0ef27e5f91a760bba756243c2477c9310fd8764"
+  version "1.5.2.1,2025,11"
+  sha256 "c46f7b0e9e43b113a2af40cfc57636e9876d2317b26f0d95265305029894e99d"
 
   url "https://lawand.io/wp-content/uploads/#{version.csv.second}/#{version.csv.third}/taskbar-#{version.csv.first}.zip"
   name "Taskbar"
