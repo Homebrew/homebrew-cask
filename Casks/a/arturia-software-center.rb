011a0a955ae8cf59e9f8b529d7f343e26d9fd3a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 09:43:18 +0000

    arturia-software-center 2.12.0.3157

--- Casks/a/arturia-software-center.rb
+++ Casks/a/arturia-software-center.rb
@@ -1,6 +1,6 @@
 cask "arturia-software-center" do
-  version "2.11.0.3103"
-  sha256 "79feae63271adaeb77ccec52350bbe1004b14f8c478ecd25ca0c37ad37844464"
+  version "2.12.0.3157"
+  sha256 "a164c345c6c916179a3ff4c04fd3380dd41ad08f72c1fc3cc6ff66026e35871f"
 
   url "https://dl.arturia.net/products/asc/soft/Arturia_Software_Center__#{version.dots_to_underscores}.pkg",
       verified: "dl.arturia.net/"
