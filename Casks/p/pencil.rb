47954d5faa656aa8da9e0ab012eba5d62b278784
Rui Chen <rui@chenrui.dev>
2025-11-30 23:51:56 -0500

    pencil: disable
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/p/pencil.rb
+++ Casks/p/pencil.rb
@@ -13,7 +13,7 @@ cask "pencil" do
     regex(/href=.*?Pencil[._-]v?(\d+(?:\.\d+)+)[^"' >]*?\.dmg/i)
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  disable! date: "2025-11-30", because: :discontinued
 
   app "Pencil.app"
 
