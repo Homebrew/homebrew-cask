de337507c0670543a712682b2f9b3b9d8d1f0307
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-02 12:41:46 +0000

    5ire 0.15.1

--- Casks/5/5ire.rb
+++ Casks/5/5ire.rb
@@ -1,9 +1,9 @@
 cask "5ire" do
   arch arm: "-arm64"
 
-  version "0.15.0"
-  sha256 arm:   "6e7b6490ae9859006779b5fa1637c88a2e6c3a377ab0de6c48f5b5e8a992b726",
-         intel: "74178c07c5af0c5e0dd824a1cda2b44df6b6fbe9659da15ec5022be3b94c523d"
+  version "0.15.1"
+  sha256 arm:   "617a55c4b3fa5fd5872861cd5cda8f6dfdb8039b1468345dff8cc8d5411324cd",
+         intel: "97eee400ca4fd4207717fc034c086a22d75ee6fdf2ecf0ad387dac5135cf6482"
 
   url "https://github.com/nanbingxyz/5ire/releases/download/v#{version}/5ire-#{version}#{arch}.dmg",
       verified: "github.com/nanbingxyz/5ire/"
