47820d04d87cd00a5e78fcc05fd259377828bf76
Jérémie Payet <payet.jeremie@gmail.com>
2025-12-02 13:08:15 -1000

    codex 0.64.0

--- Casks/c/codex.rb
+++ Casks/c/codex.rb
@@ -2,11 +2,11 @@ cask "codex" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-musl"
 
-  version "0.63.0"
-  sha256 arm:          "a7a66c8fe132c5634cdffccb79f3bb67ea7d487d9d1052e818fb0ba56a7e1e86",
-         intel:        "e76fc4a5022f87f621ccf7434b44a439fc866636bf6348242d0869c9a5d6b543",
-         arm64_linux:  "cd4469675263fe7a536483d76ec37dd529f30683f55c30d81f4ca6b7e3a899d5",
-         x86_64_linux: "ebbeb9b5fb391fdb6300ea02b8ca9ac70e8681e13e5e6c73ad3766be28e58db1"
+  version "0.64.0"
+  sha256 arm:          "6712a002d32e050f42919e1552a316821ada9d9b347584bb971b840ff7295e71",
+         intel:        "a73ef8e63c9e666e975a1757ae3789083a5e34eb3948bc292e378ef2cbcc3d73",
+         arm64_linux:  "e8eaa653d4fc6dc1290c8704b999c93328b8339a3a171522c31713365a09bf67",
+         x86_64_linux: "96da930be0dd55ccb34bf7acd8e310aa236baabb10cbb7f6e9ae2506caf11901"
 
   url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-#{arch}-#{os}.tar.gz"
   name "Codex"
