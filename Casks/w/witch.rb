aea54f22fd393ca8f789fc87266cb2516294d7b0
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-02 22:17:00 -0500

    witch 4.7.0
    `witch` is autobumped but the workflow failed to update to version
    4.7.0 because the dotless version in the file name uses
    major/minor/patch (470) but the appcast `shortVersionString` only uses
    major/minor (4.7). This updates the version and modifies the
    `livecheck` block to add `.0` to the end of the version string if it
    only includes major/minor (as we do in some other casks in the same
    situation).

--- Casks/w/witch.rb
+++ Casks/w/witch.rb
@@ -1,15 +1,25 @@
 cask "witch" do
-  version "4.6.2"
-  sha256 "cb9d946400e00221c72706487ac7ba3760e75a8e0f0e46b26bf9a5da79784b25"
+  version "4.7.0"
+  sha256 "aad6812460df8c670999691e244b111bfad19c2544180093ecf2019ca45cb7fe"
 
   url "https://manytricks.com/download/_do_not_hotlink_/witch#{version.no_dots}.dmg"
   name "Witch"
   desc "Switch apps, windows, or tabs"
   homepage "https://manytricks.com/witch/"
 
+  # The dotless version in the file name uses major/minor/patch but the appcast
+  # `shortVersionString` may omit patch for new major/minor versions (e.g.
+  # 470 and 4.7 respectively). This adds `.0` to versions without a patch to
+  # avoid this mismatch.
   livecheck do
     url "https://manytricks.com/witch/appcast/"
-    strategy :sparkle, &:short_version
+    regex(/^v?(\d+\.\d+)(\.\d+)*$/i)
+    strategy :sparkle do |item, regex|
+      match = item.short_version&.match(regex)
+      next unless match
+
+      "#{match[1]}#{match[2].presence || ".0"}"
+    end
   end
 
   auto_updates true
