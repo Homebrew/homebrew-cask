2aa04eed78d2247773c0cdd322aa20e6f85819db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-03 16:41:55 +0000

    scrolla 28

--- Casks/s/scrolla.rb
+++ Casks/s/scrolla.rb
@@ -1,5 +1,5 @@
 cask "scrolla" do
-  version "27"
+  version "28"
   sha256 :no_check
 
   url "https://releases.scrolla.app/Scrolla.zip"
@@ -13,7 +13,7 @@ cask "scrolla" do
   end
 
   auto_updates true
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :sonoma"
 
   app "Scrolla.app"
 
