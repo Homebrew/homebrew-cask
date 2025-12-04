5ed1bc181098d0392a74eddbce97159d30309b64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 04:26:17 +0000

    notion-mail 0.0.44

--- Casks/n/notion-mail.rb
+++ Casks/n/notion-mail.rb
@@ -2,9 +2,9 @@ cask "notion-mail" do
   arch arm: "arm64", intel: "universal"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "latest"
 
-  version "0.0.43"
-  sha256 arm:   "6738c917bcae1fdaececbdac51b3c6993c14f2efa14f6b26636420123a3630d4",
-         intel: "15cb58d81f24209395e1aca06f07e9e614d70536c10a295b9c76ac3247e1939d"
+  version "0.0.44"
+  sha256 arm:   "c896c8de573298433257a5a8761c334fa62c2b78b332c107fb12a222080b43af",
+         intel: "59db3333a4cd1570c87d0357bf6e401fe1111149264458351b43b35ff2b81b23"
 
   url "https://mail-desktop-release.notion-static.com/Notion%20Mail-#{version}-#{arch}.zip",
       verified: "mail-desktop-release.notion-static.com/"
