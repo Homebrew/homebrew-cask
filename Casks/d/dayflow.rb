71dc02ba89e93549e682cf61fa7614d27c06880b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 21:51:31 +0000

    dayflow 1.3.5

--- Casks/d/dayflow.rb
+++ Casks/d/dayflow.rb
@@ -1,6 +1,6 @@
 cask "dayflow" do
-  version "1.3.4"
-  sha256 "d49f8b5f8bd26715005e2e57508a5cf695143c895486a3e4249587c32ba59424"
+  version "1.3.5"
+  sha256 "7d7b617922056f963559d0c3c6404434ff63e657d229fca5fdfb5fcc42308e5b"
 
   url "https://github.com/JerryZLiu/Dayflow/releases/download/v#{version}/Dayflow.dmg"
   name "Dayflow"
