52b75c69b7c346719278e88ce36293435335335f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 01:12:20 +0000

    kiro 0.7.5

--- Casks/k/kiro.rb
+++ Casks/k/kiro.rb
@@ -1,9 +1,9 @@
 cask "kiro" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.7.0"
-  sha256  arm:   "857633051e066a3b8692abc0ada19704e4b1f961643a3a5d39a81d770747b8fa",
-          intel: "1f977ed54b3cda35e6105f7fec2b1bff6512a09e92b0ed08d6ec130acdebaed6"
+  version "0.7.5"
+  sha256  arm:   "102645de63970aeb1eb3131057dffa620e40a5af1a94ca14b1003bbea1095363",
+          intel: "59220efdeda7485fa10930d99f247dd0253029896628fb36eb3fd0f507baa852"
 
   url "https://prod.download.desktop.kiro.dev/releases/stable/darwin-#{arch}/signed/#{version}/kiro-ide-#{version}-stable-darwin-#{arch}.dmg"
   name "kiro"
