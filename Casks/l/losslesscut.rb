c4db0e2cf8fdb267b721346730c09b70343f14e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 19:26:20 +0000

    losslesscut 3.67.2

--- Casks/l/losslesscut.rb
+++ Casks/l/losslesscut.rb
@@ -1,9 +1,9 @@
 cask "losslesscut" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.67.1"
-  sha256 arm:   "c7cc4fc8ec0e31a676e3935e1401772e50d10e480fc5e7981261883c641999f4",
-         intel: "9235dd3abb7b049773b9e661cd5638064deb5104e298d42d6ea4a99bd77510a6"
+  version "3.67.2"
+  sha256 arm:   "7ab28094b0400cf4ed884abed1e1794fdf1469db41652784432963097b4b7728",
+         intel: "469595e81e2531b40c839d308a806d1b68f711c6ec072d462bcc557fd67fa10b"
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac-#{arch}.dmg"
   name "LosslessCut"
