c84af64528de068023a4b9a43a3fb63b522e6132
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 04:05:40 +0000

    font-monocraft-nerd-font 4.2

--- Casks/font/font-m/font-monocraft-nerd-font.rb
+++ Casks/font/font-m/font-monocraft-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-monocraft-nerd-font" do
-  version "4.1"
-  sha256 "114879c755a8fed955e658868998371cabbb531b0ba7b994d5478c63d29b22d8"
+  version "4.2"
+  sha256 "1208252be677af800142ad553512cd8081c8245b11f89476519109ebb503c69e"
 
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft-nerd-fonts-patched.ttc"
   name "Monocraft with Nerd glyphs"
