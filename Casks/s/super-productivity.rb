51b3212f52b177a50ab44d68519cf51b72355c01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 19:37:17 +0000

    super-productivity 16.5.0

--- Casks/s/super-productivity.rb
+++ Casks/s/super-productivity.rb
@@ -1,9 +1,9 @@
 cask "super-productivity" do
   arch arm: "arm64", intel: "x64"
 
-  version "16.4.3"
-  sha256 arm:   "288ded9221b3ab9e3a4e58b75c9fc02905ca9b3e2aa6cf4db23cf84806d3198d",
-         intel: "c3f0a547d195eef322bffbd48aaea4d11d9b18799ba0eb64629c2926e74deec5"
+  version "16.5.0"
+  sha256 arm:   "c4e6955cc152c70e66ec473fedbedb8c24660ccbb98706995bbcc8ce8fad864f",
+         intel: "0a710c7f190361062581296627ef2b61667d9b8c68a990f3705c39fd47518c54"
 
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{arch}.dmg",
       verified: "github.com/johannesjo/super-productivity/"
@@ -16,7 +16,7 @@ cask "super-productivity" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Super Productivity.app"
 
