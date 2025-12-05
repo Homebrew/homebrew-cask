64d56f3d57af46698fc7571d7051e9b4fcef84fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 04:35:00 +0000

    qianwen 1.2.2

--- Casks/q/qianwen.rb
+++ Casks/q/qianwen.rb
@@ -1,6 +1,6 @@
 cask "qianwen" do
-  version "1.2.1,251121206"
-  sha256 "5fbd320c52f8a3f7a990ee7b461f11d154562c64ee7b96cecc0ea1772ab9e5ec"
+  version "1.2.2,2512042113"
+  sha256 "ec4d592ec55283a3955547ebf1a65c9320a1a7421cca87a326928b75ce58f2e1"
 
   url "https://qianwen-portal-cdn.tongyi.com/native/app/#{version.csv.first}/darwin/universal/qwenclient_setup_#{version.csv.first}.#{version.csv.second}.dmg"
   name "qianwen"
