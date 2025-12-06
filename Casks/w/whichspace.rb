ee422b2e3924cb4b8423b684e3e16156ba417e96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-06 19:35:49 +0000

    whichspace 0.8.3

--- Casks/w/whichspace.rb
+++ Casks/w/whichspace.rb
@@ -1,16 +1,21 @@
 cask "whichspace" do
-  version "0.7.1"
-  sha256 "d2c9bc0488e40038f0661923a5aa1dba7e2c2353bcedacb488d9c6fa029d2f19"
+  version "0.8.3"
+  sha256 "f707b1f7acbb43ee18f06d8b8252c7429ddfd866be8e11a18b84734fae8a6dfc"
 
   url "https://github.com/gechr/WhichSpace/releases/download/v#{version}/WhichSpace.zip"
   name "WhichSpace"
   desc "Active space menu bar icon"
   homepage "https://github.com/gechr/WhichSpace"
 
+  livecheck do
+    url "https://raw.githubusercontent.com/gechr/WhichSpace/updates/appcast.xml"
+    strategy :sparkle
+  end
+
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :sonoma"
 
   app "WhichSpace.app"
 
