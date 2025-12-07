c5c86100bef9dc37a753551f8ff8b9f0aa755d0c
Harsh <hp424805@gmail.com>
2025-12-07 23:34:44 +0530

    fix: formatting style

--- Casks/c/conar.rb
+++ Casks/c/conar.rb
@@ -3,19 +3,20 @@ cask "conar" do
 
   on_arm do
     sha256 "5f28685dcb64793848c730aaf835d6af3f142e53547078492f447860062ba921"
+
     url "https://github.com/wannabespace/conar/releases/download/v#{version}/Conar-Mac-arm64-#{version}-Installer.dmg",
         verified: "github.com/wannabespace/conar/"
   end
-
   on_intel do
     sha256 "6dc0405fed39e6ad2c99fee6ab0ee1387b14a31aea128972618d3d85d3a8654c"
+
     url "https://github.com/wannabespace/conar/releases/download/v#{version}/Conar-Mac-x64-#{version}-Installer.dmg",
         verified: "github.com/wannabespace/conar/"
   end
 
   name "Conar"
   desc "AI-powered database and data management tool"
-  homepage "https://conar.app"
+  homepage "https://conar.app/"
 
   livecheck do
     url :url
