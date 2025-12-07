6ba100afe242d0a49b3b134ddcf7bebca0a61f54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-07 10:27:41 +0000

    slideshower 3.13

--- Casks/s/slideshower.rb
+++ Casks/s/slideshower.rb
@@ -1,6 +1,6 @@
 cask "slideshower" do
-  version "3.12"
-  sha256 "f7d9b0f2f31db65f3a95ff7f40189749c1acb4c40f68e955fe0696e0f509f12b"
+  version "3.13"
+  sha256 "005a79ee3ecd68d7e258fb90565381deea4979d53791c1dc60906c26b0d96432"
 
   url "https://slideshower.com/slideshower_#{version.dots_to_underscores}.dmg"
   name "Slideshower for macOS"
