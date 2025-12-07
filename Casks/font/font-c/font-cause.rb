f87b8553e907ebfb60e139af01390dc21d6e40f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-07 00:18:04 +0000

    font-cause: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-c/font-cause.rb
@@ -0,0 +1,12 @@
+cask "font-cause" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/cause/Cause%5Bwght%5D.ttf"
+  name "Cause"
+  homepage "https://github.com/xconsau/Cause"
+
+  font "Cause[wght].ttf"
+
+  # No zap stanza required
+end
