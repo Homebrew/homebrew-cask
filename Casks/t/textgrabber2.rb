47ec2b864055276e9c6ac33b44bc4d7aaf3f757d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-07 13:36:43 +0000

    textgrabber2 1.8.0

--- Casks/t/textgrabber2.rb
+++ Casks/t/textgrabber2.rb
@@ -1,7 +1,7 @@
 cask "textgrabber2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.7.0"
-  sha256 "e2a5bddaea63c35de18d819a57e997fdc00d44c8dd1746ecb21e61d663485f27"
+  version "1.8.0"
+  sha256 "b7d4f994d7e9954a7dbdac545c31246f727dc2b25c77f0bd6be321fdbb835863"
 
   url "https://github.com/TextGrabber2-app/TextGrabber2/releases/download/v#{version}/TextGrabber2-#{version}.dmg"
   name "TextGrabber2"
