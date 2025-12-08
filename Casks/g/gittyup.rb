7d3059bbe3ac9bfe4738fa5cba9b5163d55849d4
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-07 23:28:17 -0500

    gittyup: fix audit errors
    `brew audit` failed for `gittyup` with "Artifact defined :monterey as
    the minimum macOS version but the cask declared no minimum macOS
    version" and "Signature verification failed" errors. This addresses
    the errors by adding an appropriate `depends_on macos:` value and a
    `disable!` call with the future date we've been using for apps that
    fail Gatekeeper checks (this app is unsigned).

--- Casks/g/gittyup.rb
+++ Casks/g/gittyup.rb
@@ -29,7 +29,10 @@ cask "gittyup" do
     end
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   auto_updates true
+  depends_on macos: ">= :monterey"
 
   app "Gittyup.app"
 
