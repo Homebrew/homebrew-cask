878f6ff747f1e1a64344dd6ef8dd3ed8388c092f
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-08 16:11:35 -0500

    visual-paradigm-ce 17.3,20251211
    `visual-paradigm-ce` is autobumped but the workflow failed to update
    to version 17.3,20251211 because the app is failing Gatekeeper checks
    again. This updates the version and adds a `disable!` call (again)
    with the future date we've been using for this scenario.

--- Casks/v/visual-paradigm-ce.rb
+++ Casks/v/visual-paradigm-ce.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm-ce" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.3,20251203"
-  sha256 arm:   "8ec22d3f24c9145d28ad3c0563bf8f888d8e099823562c3e3fe18463ba462b46",
-         intel: "7822a58f29e2a9fe76c8461b1212dd7099b0bdc5fcb2ef3364acfc67b8c34d11"
+  version "17.3,20251211"
+  sha256 arm:   "3923b1884beb10a5961e27da0a13cf3f2eec322a6b3ea7b75458af3d03b7e901",
+         intel: "da9c71e1dab450fe03bae816d8c6f51689b362b2616757267ae8bf2c26febf61"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm Community Edition"
@@ -20,6 +20,8 @@ cask "visual-paradigm-ce" do
       "#{match[1]},#{match[2]}"
     end
   end
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   # Renamed to avoid conflict with visual-paradigm.
   app "Visual Paradigm.app", target: "Visual Paradigm CE.app"
