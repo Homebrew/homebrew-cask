381ccf04146a7d4ab907be8be367f8d8cf642bd7
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-08 00:14:22 -0500

    leader-key: update urls
    The GitHub repository for `leader-key` has changed from LeaderKey.app
    to simply LeaderKey. The homepage and artifact URL redirect but the
    `livecheck` block produces an "Unable to get versions" error. This
    updates all of the URLs accordingly which resolves the redirections
    and fixes the `livecheck` block.

--- Casks/l/leader-key.rb
+++ Casks/l/leader-key.rb
@@ -2,13 +2,13 @@ cask "leader-key" do
   version "1.17.3"
   sha256 "07fabeef4a0704b7568f323389b509d3ff5f79df6f8b59f27abec1174886346f"
 
-  url "https://github.com/mikker/LeaderKey.app/releases/download/v#{version}/Leader.Key.app.zip"
+  url "https://github.com/mikker/LeaderKey/releases/download/v#{version}/Leader.Key.app.zip"
   name "Leader Key"
   desc "Application launcher"
-  homepage "https://github.com/mikker/LeaderKey.app"
+  homepage "https://github.com/mikker/LeaderKey"
 
   livecheck do
-    url "https://mikker.github.io/LeaderKey.app/appcast.xml"
+    url "https://mikker.github.io/LeaderKey/appcast.xml"
     strategy :sparkle, &:short_version
   end
 
