49900a9ffad73232de44b25cbc6b5905727026d9
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-12-09 00:13:48 +0100

    Add Cask for WailBrew

---
+++ Casks/w/wailbrew.rb
@@ -0,0 +1,19 @@
+cask "wailbrew" do
+  version "0.9.0"
+  sha256 "832841df1e464f614149e1b4e7d05b2aff9ea6bef49d9f8ac78d397604547b48"
+
+  url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
+  name "WailBrew"
+  desc "Manage Homebrew packages with a UI"
+  homepage "https://github.com/wickenico/WailBrew"
+
+  depends_on macos: ">= :big_sur"
+
+  app "WailBrew.app"
+
+  zap trash: [
+    "~/Library/Application Support/WailBrew",
+    "~/Library/Preferences/dev.wailbrew.plist",
+    "~/Library/Saved Application State/dev.wailbrew.savedState",
+  ]
+end
