a05e5df79d2ed93d4f07d83bbc24ea1390386bb6
samraza <sam.rizae@gmail.com>
2025-12-08 21:24:02 +0100

    Working config

--- Casks/t/terraprep@beta.rb
+++ Casks/t/terraprep@beta.rb
@@ -2,15 +2,18 @@ cask "terraprep@beta" do
   arch arm: "darwin_arm64", intel: "darwin_amd64"
 
   version "beta_0.8.0"
-  sha256 arm:   "18062981dae851f9ca9b8d8348efecc8efccde689839a69c9aacb4a3418fff88",
-         intel: "34560bd2c96fe7148e72f10949e9d9a2183257b2e99785cb7e377bf61b131f4a"
+  sha256 arm:   "126defedf3a7b52402580d29240fd904cbeaf661fc59243c633d95a2bc151be4",
+         intel: "a630b3d9ea0fbe89a9fc6105eb836da2cf950e251635266c8322b39240be6dea"
 
   url "https://s3.eu-west-1.wasabisys.com/noss/terraprep/#{version}/terraprep_#{version}_#{arch}.zip",
       verified: "s3.eu-west-1.wasabisys.com/noss/terraprep/"
-
   name "Terraprep Beta"
   desc "OpenTofu & Terraform remote backend creation client"
-  homepage "https://www.terraprep.xyz"
+  homepage "https://www.terraprep.xyz/"
+
+  livecheck do
+    skip "Beta channel is manually managed"
+  end
 
   depends_on macos: ">= :sonoma"
 
@@ -19,11 +22,5 @@ cask "terraprep@beta" do
   # Install it as `terraprep` in /opt/homebrew/bin (or /usr/local/bin on Intel)
   binary "terraprep_#{version}_#{arch}", target: "terraprep"
 
-  livecheck do
-    skip "Beta channel is manually managed"
-  end
-
-  zap trash: [
-    "~/.terraprep",
-  ]
+  zap trash: "~/.terraprep"
 end
