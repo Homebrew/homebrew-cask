54ac1abec7f937f29bf88fcd774c009c010bf8d8
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-08 15:00:10 -0500

    second-life-viewer 7.2.3.19375695301
    `second-life-viewer` is autobumped but the workflow failed to update
    to version  7.2.3.19375695301 because the `livecheck` block is
    producing an "Unable to get versions" error due to the dmg file name
    now being found in JavaScript and the file name suffix changing to
    `_universal`. This updates the version, `url`, and `livecheck` block
    regex accordingly.

--- Casks/s/second-life-viewer.rb
+++ Casks/s/second-life-viewer.rb
@@ -1,15 +1,15 @@
 cask "second-life-viewer" do
-  version "7.2.2.18475198968"
-  sha256 "29e649f42cbe15ff5e91a2dff27cac5aebf63e13f1f991ba3de45f46a70b7507"
+  version "7.2.3.19375695301"
+  sha256 "23d34f4747bea9a88432b68572424fc6dc933f37fcc668c3495c641d20113871"
 
-  url "https://viewer-download.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
+  url "https://viewer-download.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_universal.dmg"
   name "Linden Lab Second Life Viewer"
   desc "3D browsing software for Second Life online virtual world"
   homepage "https://secondlife.com/"
 
   livecheck do
     url "https://secondlife.com/downloads"
-    regex(/href=.*?Second[._-]Life[._-]v?(\d+(?:[._]\d+)+)(?:[._-]x86_64)?\.dmg/i)
+    regex(/Second[._-]Life[._-]v?(\d+(?:[._]\d+)+)(?:[._-](?:universal|x86_64))?\.dmg/i)
     strategy :page_match do |page, regex|
       match = page.match(regex)
       next if match.blank?
