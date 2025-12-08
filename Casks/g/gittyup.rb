160e31ff9313f0c2ffee2b222b75f68e9f089773
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-07 23:10:46 -0500

    gittyup: update livecheck
    The existing `livecheck` block for `gittyup` uses the `GithubLatest`
    strategy but it has been producing an `Unable to get versions` error
    for recent versions because there is no dmg file in the release
    assets. Upstream has acknowledged that macOS builds are broken but
    this may not change because they don't have a macOS system to fix the
    issues.
    
    This addresses the issue by modifying the `livecheck` block to use the
    `GithubReleases` strategy and match versions with a dmg file. We can
    reevaluate this when upstream fixes macOS builds or there are enough
    releases without a dmg file to prevent `GithubReleases` from returning
    version information (i.e., 30 releases after 1.4.0).

--- Casks/g/gittyup.rb
+++ Casks/g/gittyup.rb
@@ -8,16 +8,24 @@ cask "gittyup" do
   desc "Graphical Git client"
   homepage "https://murmele.github.io/Gittyup/"
 
+  # Upstream macOS builds are broken and the developer doesn't have a macOS
+  # system to fix the issues. This checks multiple releases to identify the
+  # newest version with a dmg asset, either until the upstream situation changes
+  # or there are enough releases without a dmg file to break this check.
   livecheck do
     url :url
     regex(/^Gittyup[._-]v?(\d+(?:\.\d+)+)\.dmg$/i)
-    strategy :github_latest do |json, regex|
-      json["assets"]&.map do |asset|
-        match = asset["name"]&.match(regex)
-        next if match.blank?
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
 
-        match[1]
-      end
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
     end
   end
 
