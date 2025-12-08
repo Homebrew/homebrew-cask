f92c60a4f73e21441404ff661fa0d46571c000ca
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-08 15:54:32 -0500

    wordpresscom-studio 1.6.5
    `wordpresscom-studio` is autobumped but the workflow failed to update
    to version 1.6.5 because the `livecheck` block produces an "Unable to
    get versions" error due to the regex not matching the URL in the JSON
    response. The file name format has changed and the URL path now
    includes a number that we need to capture in the cask `version`, so
    this updates the version, `url`, and `livecheck` block accordingly.

--- Casks/w/wordpresscom-studio.rb
+++ Casks/w/wordpresscom-studio.rb
@@ -1,24 +1,24 @@
 cask "wordpresscom-studio" do
   arch arm: "arm64", intel: "x64"
+  folder_arch = on_arch_conditional arm: "silicon", intel: "intel"
 
-  version "1.6.3"
-  sha256 arm:   "f151209431e79cb8912714b0598a516be57a2ba3cdbbd7a163c0acf2e6c29f64",
-         intel: "b0fdbdb9aee71ff5b949dd12cb9d5eaaca5ad946cdfb364094523fa4833b1dfe"
+  version "1.6.5,9289"
+  sha256 arm:   "ce788d92802a7c65bbe50cd72ab8b1195d04954b943e35c25931fae64b4314ed",
+         intel: "730bab3ba81e5b2a5a78e7eac329bc067a36432f42bfae9e2ace0a6685d919d6"
 
-  url "https://cdn.a8c-ci.services/studio/studio-darwin-#{arch}-v#{version}.app.zip",
-      verified: "cdn.a8c-ci.services/studio/"
+  url "https://appscdn.wordpress.com/downloads/wordpress-com-studio/mac-#{folder_arch}/v#{version.csv.first}/#{version.csv.second}/update/studio-#{arch}-v#{version.csv.first}.zip"
   name "Wordpress Studio"
   desc "WordPress local development environment"
   homepage "https://developer.wordpress.com/studio/"
 
   livecheck do
     url "https://public-api.wordpress.com/wpcom/v2/studio-app/updates?platform=darwin&arch=#{arch}&version=0.0.0"
-    regex(/studio[._-]darwin[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)\.app\.zip/i)
+    regex(%r{/(\d+(?:\.\d+)*)/update/studio(?:[._-]darwin)?[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)(?:\.app)?\.zip}i)
     strategy :json do |json, regex|
       match = json["url"]&.match(regex)
-      next if match.blank?
+      next unless match
 
-      match[1]
+      "#{match[2]},#{match[1]}"
     end
   end
 
