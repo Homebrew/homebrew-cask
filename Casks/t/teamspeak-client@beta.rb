c6087eace18bb2edb141cd55777ef720528e9de2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-09 17:43:20 +0000

    teamspeak-client@beta 6.0.0-beta3.3

--- Casks/t/teamspeak-client@beta.rb
+++ Casks/t/teamspeak-client@beta.rb
@@ -1,9 +1,9 @@
 cask "teamspeak-client@beta" do
   arch arm: "arm", intel: "intel"
 
-  version "6.0.0-beta3.2"
-  sha256 arm:   "308f342f51580202e566fd3d42729d5cc517071640809e2545ab5464ea9d23a2",
-         intel: "df0d13ef3095e3d2c239b225beeb5addd27fa26206bc1f698982611458268c0b"
+  version "6.0.0-beta3.3"
+  sha256 arm:   "4ff6bc17d82dcdf2a09f2191f1c35ec9c58857475902189a62ac17c7165aefe5",
+         intel: "4f339d8bf20b6339c8888676c04b11806d684015dd83c68f3c1531963ec28233"
 
   url "https://files.teamspeak-services.com/pre_releases/client/#{version}/teamspeak-client-#{arch}.dmg",
       verified: "files.teamspeak-services.com/"
