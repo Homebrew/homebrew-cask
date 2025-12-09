e025efe04364b987c29b884ae25580952b3373a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-09 01:56:30 +0000

    sculptor 0.3.0

--- Casks/s/sculptor.rb
+++ Casks/s/sculptor.rb
@@ -1,9 +1,9 @@
 cask "sculptor" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.35"
-  sha256 arm:   "d18aa8dc39ca29fd0a73fe06e75497a96f55d46dddb42fb967376647144d1cb0",
-         intel: "25af5fa186125e98179437fc9cc9d4a58d557634c500642453d2a762bd78e4ba"
+  version "0.3.0"
+  sha256 arm:   "0dd491c5bb28d18361174dccbff1250bcc10aa7b811ecaa84b8da0ac5f20bc18",
+         intel: "cfae34b82fd13507d79e712088ebf827540cc46c58fdd31932d5b8c160f2bea1"
 
   url "https://imbue-sculptor-releases.s3.us-west-2.amazonaws.com/sculptor/zip/darwin/#{arch}/Sculptor-darwin-#{arch}-#{version}.zip",
       verified: "imbue-sculptor-releases.s3.us-west-2.amazonaws.com/"
