5481ba01f819f0a30609d9b008a8827acffa55e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 21:59:39 +0000

    gitkraken 11.7.0

--- Casks/g/gitkraken.rb
+++ Casks/g/gitkraken.rb
@@ -2,9 +2,9 @@ cask "gitkraken" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "-arm64"
 
-  version "11.6.0"
-  sha256 arm:   "990fa28332244482f16e0460765579a862e91a30b3ab8292870530847ad7af58",
-         intel: "b024c15eb668d94dab1794389922ec5c965681ef711175c0cf6e85797b3b5ae8"
+  version "11.7.0"
+  sha256 arm:   "3502864f0397d1abb7544de7e7f3a8eb45a7a75fd73e29a6c8521b6f7013e14c",
+         intel: "1e1753f976a324f3a75930f23e72762eca8bc1a268c0ca3f902ab4669220b733"
 
   url "https://api.gitkraken.dev/releases/production/darwin/#{arch}/#{version}/GitKraken-v#{version}.zip",
       verified: "api.gitkraken.dev/releases/production/"
