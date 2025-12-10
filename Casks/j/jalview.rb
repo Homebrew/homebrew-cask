7f389cbef05054d6180b41d26efbe873fbfad9e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 13:14:23 +0000

    jalview 2.11.5.1

--- Casks/j/jalview.rb
+++ Casks/j/jalview.rb
@@ -1,9 +1,9 @@
 cask "jalview" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.11.5.0"
-  sha256 arm:   "144adb8e649954403281bde18445beafc6a0f2fd74d8d00c7141cd398345d388",
-         intel: "3f2301f322700b821fac8353ce72d286e25d2b261a0a0b7bc582875730c6618f"
+  version "2.11.5.1"
+  sha256 arm:   "341a3b2e52cfc0512df6767892a9c1dc8a458dccab1256f3686b3ad9288a7f47",
+         intel: "3808f040215d353b2367882dc5e458a6449451e21625df6e8e96bc9f99337341"
 
   url "https://www.jalview.org/downloads/installers/release/Jalview-#{version.dots_to_underscores}-macos-#{arch}-java_8.dmg"
   name "Jalview"
