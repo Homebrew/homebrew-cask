db3939782d810fdb09727dc1780018ad6cf0e8d8
Rui Chen <rui@chenrui.dev>
2025-12-10 13:09:03 -0500

    cilicon 2.4.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/cilicon.rb
+++ Casks/c/cilicon.rb
@@ -1,11 +1,24 @@
 cask "cilicon" do
-  version "2.3.0"
-  sha256 "bc8217c2429341e196ef1cba48a29cae56f2f5c03fd69dd0c3cc2a5360041d5e"
+  version "2.4.1,24"
+  sha256 "bcc175eb4e41c960e77d22a3bf356d7a3565b46a1872f1e626ceafd104334769"
 
-  url "https://github.com/traderepublic/Cilicon/releases/download/v#{version}/Cilicon.zip"
+  url "https://github.com/traderepublic/Cilicon/releases/download/v#{version.csv.first}/Cilicon_#{version.csv.first}_#{version.csv.second}_adhoc.zip"
   name "Cilicon"
   desc "Self-Hosted ephemeral CI on Apple Silicon"
   homepage "https://github.com/traderepublic/Cilicon"
+
+  livecheck do
+    url :url
+    regex(/Cilicon[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-].*\.zip/i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
