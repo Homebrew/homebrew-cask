4cf6275137e254229e6e4dcfee7985e03b0ed78a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 12:47:06 +0000

    bcut 3.11.16

--- Casks/b/bcut.rb
+++ Casks/b/bcut.rb
@@ -3,12 +3,12 @@ cask "bcut" do
   pkg_key = on_arch_conditional arm: "arm_pkg_url", intel: "package_download_url"
 
   on_arm do
-    version "3.11.15,21880488,1bc7f89be5c6987b497f9875720bc7e2"
-    sha256 "dadf2e0ac79bb5cf1fb048e9fa1f09a85ad67e6dc5f56e8abe42f4a7db8682f0"
+    version "3.11.16,22039053,e6ef6a84b7a28d652ee2e63ec164c57b"
+    sha256 "7126b30dc79b53d69775361b48a758c6d24f128170e88b58d7882064e8b485be"
   end
   on_intel do
-    version "3.11.15,21885737,cb12c006413fe4d098044934d8442f70"
-    sha256 "70a7b3375842bcd6ad623c1c4dfef601e295eb06b3160fa750e56e9f1b872c1f"
+    version "3.11.16,22039052,714cc0b026cbaf09d57f35b541b65c0f"
+    sha256 "31e1ef58105d9176bf63315b43033bc692bd4a9e8909e92a42166ae098ac96f7"
   end
 
   url "https://boss.hdslb.com/bcut_pc_pkg/static/#{version.csv.third}/BCUT-#{version.csv.first}-#{version.csv.second}-#{arch}.dmg",
