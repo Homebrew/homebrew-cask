058996771bf2642729c9d3a3186b7a551901a6f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 01:54:10 +0000

    whichspace 0.11.0

--- Casks/w/whichspace.rb
+++ Casks/w/whichspace.rb
@@ -1,6 +1,6 @@
 cask "whichspace" do
-  version "0.10.10"
-  sha256 "7aec65ca86a4ff502b7f1d7faa6bef79a76c31a5c4be69f94b9054b5e81f2809"
+  version "0.11.0"
+  sha256 "bb1dae5dd7ac0ae6904e394eba0844ba9a1729fa0b9797a8c92d2b7626b102f3"
 
   url "https://github.com/gechr/WhichSpace/releases/download/v#{version}/WhichSpace.zip"
   name "WhichSpace"
