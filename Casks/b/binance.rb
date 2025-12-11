46f70421852cf0f6f7fae6a8b8914b0bc6759637
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 03:53:27 +0000

    binance 2.1.0

--- Casks/b/binance.rb
+++ Casks/b/binance.rb
@@ -1,9 +1,9 @@
 cask "binance" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.2"
-  sha256 arm:   "ebae9f0661a1d577f3c2880f87a99f84a01e8ede3b375c4d635f1d1f14eb33db",
-         intel: "5f4f03e11a4547279825db2db751c9a36a7c938346ff2cca666bcdef12452fa5"
+  version "2.1.0"
+  sha256 arm:   "d44f84827c45cb0b0d8a8fef38030dd1a213cfe4f5150afc209c524c29143fcd",
+         intel: "b07f1163d6f7869525e1a1b45159b020e2f8d0cfa810d87bb6e0bde9d4b5239e"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}-#{arch}.dmg"
   name "Binance"
