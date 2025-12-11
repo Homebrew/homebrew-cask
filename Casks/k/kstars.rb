0b6220e398de1ba611fd48999fff662654bb6b35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 13:36:21 +0000

    kstars 3.8.0

--- Casks/k/kstars.rb
+++ Casks/k/kstars.rb
@@ -1,5 +1,5 @@
 cask "kstars" do
-  version "3.7.9"
+  version "3.8.0"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
