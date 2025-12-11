21509eedbef93c407f67aa16dba3d10d4472aaf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 23:01:47 +0000

    warp@preview 0.2025.12.10.08.12.preview_03

--- Casks/w/warp@preview.rb
+++ Casks/w/warp@preview.rb
@@ -1,6 +1,6 @@
 cask "warp@preview" do
-  version "0.2025.12.03.08.12.preview_04"
-  sha256 "bf7ec3dc1e8bce41b950bfacfe771b335bf7bda87894403766de5cbafd69a17d"
+  version "0.2025.12.10.08.12.preview_03"
+  sha256 "f4bc997926465050eb645d353a2ebcdc0c5ae9f4ea695fb62af29cd45b1586d3"
 
   url "https://releases.warp.dev/preview/v#{version}/WarpPreview.dmg"
   name "Warp Preview"
