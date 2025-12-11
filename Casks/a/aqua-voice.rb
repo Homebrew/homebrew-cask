da46f94617639ad4bb63eda68d3efd301da61c8b
Rui Chen <rui@chenrui.dev>
2025-12-10 22:29:10 -0500

    aqua-voice: on_system blocks

--- Casks/a/aqua-voice.rb
+++ Casks/a/aqua-voice.rb
@@ -1,14 +1,9 @@
 cask "aqua-voice" do
   arch arm: "arm64", intel: "x64"
 
-  on_arm do
-    version "0.10.11"
-    sha256 "e2c222c5a6a37d84a0c3e68dbc1845342ff8915b6e68cca0dc2d72511ce2ab15"
-  end
-  on_intel do
-    version "0.10.11"
-    sha256 "a9d53deb4f53ab59f97b264e858215681369765ac199582e8b4d9403ec649445"
-  end
+  version "0.10.11"
+  sha256 arm:   "e2c222c5a6a37d84a0c3e68dbc1845342ff8915b6e68cca0dc2d72511ce2ab15",
+         intel: "a9d53deb4f53ab59f97b264e858215681369765ac199582e8b4d9403ec649445"
 
   url "https://d1a1dx1sgvjqrz.cloudfront.net/aqua-voice-updates/darwin/#{arch}/Aqua+Voice-#{version}-#{arch}.dmg",
       verified: "d1a1dx1sgvjqrz.cloudfront.net/"
