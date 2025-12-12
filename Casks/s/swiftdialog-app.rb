de6aac7103db878eb736225b6da25baa672fb2b8
Bart Reardon <bart.reardon@csiro.au>
2025-12-12 11:18:25 +1100

    Add swiftDialog project
    This adds swiftdialog and swiftdialog-app to homebrew
    `swiftdialog` (new cask) - standard pkg installer
    `swiftdialog-app` (new cask) - standalone app bundle in a dmg allowing for install
    to a custom path, e.g. `brew install --cask --appdir="/custom/path" swiftdialog-app`
    
    if only one of these is appropriate then use `swiftdialog`

---
+++ Casks/s/swiftdialog-app.rb
@@ -0,0 +1,23 @@
+cask "swiftdialog-app" do
+  version "2.5.6"
+  sha256 "53f143b04de7333831a14f457161826e9f72571524329dadf5457ac2aa496bdd"
+
+  url "https://github.com/swiftDialog/swiftDialog/releases/download/v#{version}/swiftDialog.dmg",
+      verified: "github.com/swiftDialog/swiftDialog/"
+  name "swiftDialog App Bundle"
+  desc "Admin utility for macOS that presents custom dialogs or displays informative messages from shell scripts"
+  homepage "https://swiftdialog.app"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Dialog.app"
+
+  zap trash: [
+    "~/Library/Preferences/au.csiro.dialog.plist",
+  ]
+end
---
+++ Casks/s/swiftdialog.rb
@@ -0,0 +1,31 @@
+cask "swiftdialog" do
+  version "2.5.6,4805"
+  sha256 "69bd7f6412a1b2393906fc059369e65c5ae58af5cc8d289467effd11d8862b2a"
+
+  url "https://github.com/swiftDialog/swiftDialog/releases/download/v#{version.csv.first}/dialog-#{version.csv.first}-#{version.csv.second}.pkg",
+    verified: "github.com/swiftDialog/swiftDialog/"
+  name "swiftDialog"
+  desc "Admin utility for macOS that presents custom dialogs or displays informative messages from shell scripts"
+  homepage "https://swiftdialog.app"
+
+  livecheck do
+    url "https://api.github.com/repos/swiftDialog/swiftDialog/releases/latest"
+    regex(/dialog[._-](\d+(?:\.\d+)+)[._-](\d+)\.pkg/i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+        "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+
+  pkg "dialog-#{version.csv.first}-#{version.csv.second}.pkg"
+
+  uninstall pkgutil: "au.csiro.dialogcli"
+
+  zap trash: [
+	"/Library/Application Support/Dialog",
+        "/Library/Preferences/au.csiro.dialog.plist",
+  ]
+end
