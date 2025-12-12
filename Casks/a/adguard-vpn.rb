76c7f6bf04cffc2134f7a21b6b4904335fc3ba1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-12 15:36:39 +0000

    adguard-vpn 2.8.2.922

--- Casks/a/adguard-vpn.rb
+++ Casks/a/adguard-vpn.rb
@@ -1,6 +1,6 @@
 cask "adguard-vpn" do
-  version "2.8.1.887"
-  sha256 "084c70875a2314db5bd1d88f3d0f2223cfb2388f92efca9e813d56d2e8d4dc6d"
+  version "2.8.2.922"
+  sha256 "5bf02e97057415d4040c0539f657ff893d7bb4655af57cde08cf941e6bb2bd7e"
 
   url "https://static.adguard-vpn.com/mac/release/AdGuardVPN-#{version}.dmg"
   name "AdGuard VPN"
