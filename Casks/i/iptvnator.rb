8fd19796a0ae1f23239ef4fbcdb1b271ebd20b70
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-12-13 15:13:43 -0500

    iptvnator 0.17.0

--- Casks/i/iptvnator.rb
+++ Casks/i/iptvnator.rb
@@ -1,8 +1,11 @@
 cask "iptvnator" do
-  version "0.16.0"
-  sha256 "ccea0edb6237fc4e94be5119d111b2479774cc459fb758ab14f7abb7031180c5"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/4gray/iptvnator/releases/download/v#{version}/IPTVnator-#{version}-universal.dmg"
+  version "0.17.0"
+  sha256 arm:   "7022eb675f40486836800bc87cef00542afab35a370598130c29407f84de6ac5",
+         intel: "267e70e3767126ea711963df2e888ad73bc7929c8874008f2e3d7aa78af2e18a"
+
+  url "https://github.com/4gray/iptvnator/releases/download/v#{version}/iptvnator-#{version}-mac-#{arch}.dmg"
   name "IPTVnator"
   desc "Open Source m3u, m3u8 player"
   homepage "https://github.com/4gray/iptvnator"
@@ -11,6 +14,10 @@ cask "iptvnator" do
     url :url
     strategy :github_latest
   end
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
+  depends_on macos: ">= :monterey"
 
   app "iptvnator.app"
 
