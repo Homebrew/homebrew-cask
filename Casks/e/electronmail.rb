e61c675bf4583fa07aea68744cc73676e1958fe1
Bevan Kay <email@bevankay.me>
2025-12-13 09:05:25 +0800

    electronmail: update `depends_on`

--- Casks/e/electronmail.rb
+++ Casks/e/electronmail.rb
@@ -12,7 +12,7 @@ cask "electronmail" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "electron-mail.app"
 
