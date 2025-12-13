a804741a7d4cba06da15f0f3ad9c0dbd6d02952f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 01:57:13 +0000

    zulip 5.12.3

--- Casks/z/zulip.rb
+++ Casks/z/zulip.rb
@@ -1,9 +1,9 @@
 cask "zulip" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.12.2"
-  sha256 arm:   "37ad1a5fdbcdec749b0c7b6bedad497b5dc3724e1fbb1f12bd2bb97c0635176d",
-         intel: "1a424896845dae6572b26c5c8e8a8adbb4d996b9b136f4268952a9817cf8a4bb"
+  version "5.12.3"
+  sha256 arm:   "d2fe145f0d226ba124945e25f9c9a16051e61e15fc489f20a1ce15f22c96b699",
+         intel: "2812423a61990d489929ba14e3b7064277cca879de9b487c437a77d1bd201ac5"
 
   url "https://github.com/zulip/zulip-desktop/releases/download/v#{version}/Zulip-#{version}-#{arch}.dmg",
       verified: "github.com/zulip/zulip-desktop/"
