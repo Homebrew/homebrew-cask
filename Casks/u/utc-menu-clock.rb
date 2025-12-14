175b9517a534053039f1175b1e4c36b202003264
Bevan Kay <email@bevankay.me>
2025-12-14 20:46:43 +0800

    utc-menu-clock: add `depends_on`

--- Casks/u/utc-menu-clock.rb
+++ Casks/u/utc-menu-clock.rb
@@ -15,6 +15,8 @@ cask "utc-menu-clock" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
+  depends_on macos: ">= :sequoia"
+
   app "UTCMenuClock.app"
 
   zap trash: "~/Library/Preferences/net.retina.UTCMenuClock.plist"
