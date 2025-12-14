b79e0b6aab2298ab4c11efb3328e87b72d5f0e26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 18:58:51 +0000

    font-lxgw-simxihei 1.026

--- Casks/font/font-l/font-lxgw-simxihei.rb
+++ Casks/font/font-l/font-lxgw-simxihei.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-simxihei" do
-  version "1.025.4"
-  sha256 "e9973de825147ec2ab05f6793d0dd27e8734ca84a7ceafd72277346c1adc5c15"
+  version "1.026"
+  sha256 "94b0d72e068b7885ea8ac40224e0e40500f3c7e05d0979f3ed4b303089d0b144"
 
   url "https://github.com/lxgw/SimXiZhi/releases/download/v#{version}/SimXiHei.ttf"
   name "LXGW SimXiHei"
