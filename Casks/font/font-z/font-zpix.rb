e6e402eea438146e2a78842c147e3ed4467bd8d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 19:00:53 +0000

    font-zpix 3.1.10

--- Casks/font/font-z/font-zpix.rb
+++ Casks/font/font-z/font-zpix.rb
@@ -1,6 +1,6 @@
 cask "font-zpix" do
-  version "3.1.9"
-  sha256 "d8a830e19aacfe989b7d0de45f050657e83b1840ccbab0f8600a5352dbacf9e3"
+  version "3.1.10"
+  sha256 "4f5d731b71e389e757f7a2816b8d37470ff401ced2e96ab2a336db5ee41c03c7"
 
   url "https://github.com/SolidZORO/zpix-pixel-font/releases/download/v#{version}/zpix.ttf",
       verified: "github.com/SolidZORO/zpix-pixel-font/"
