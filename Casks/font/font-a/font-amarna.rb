1de4b68eef1adb96d78f068872d41644a2f2900f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 00:18:10 +0000

    font-amarna: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-amarna.rb
+++ Casks/font/font-a/font-amarna.rb
@@ -3,10 +3,11 @@ cask "font-amarna" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/amarna"
   name "Amarna"
-  homepage "https://github.com/ijvanl/Amarna"
+  homepage "https://fonts.google.com/specimen/Amarna"
 
   font "Amarna-Italic[wght].ttf"
   font "Amarna[wght].ttf"
