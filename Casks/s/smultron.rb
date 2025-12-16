2e0d8d84e681e92b5807214a68cc514ad7bfc9d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-16 22:40:54 +0000

    smultron 14.4.5

--- Casks/s/smultron.rb
+++ Casks/s/smultron.rb
@@ -1,5 +1,5 @@
 cask "smultron" do
-  version "14.4.4,14440"
+  version "14.4.5,14450"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.peterborgapps.com/downloads/Smultron#{version.major}.zip"
