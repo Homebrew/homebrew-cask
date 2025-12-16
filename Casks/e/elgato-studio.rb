1ae2ac26c8aaf16a0239f4db431d3def024f4508
Nacho Lopez <nacho@nlopez.io>
2025-12-16 10:12:02 +0100

    elgato-studio 1.0.3,877 (new)

---
+++ Casks/e/elgato-studio.rb
@@ -0,0 +1,33 @@
+cask "elgato-studio" do
+  version "1.0.3.877"
+  sha256 "0e997a4bc5b029b6adfba607264be38f2142d9254e1ae1c68bdd9e71b56bb132"
+
+  url "https://edge.elgato.com/egc/macos/estm/release/ElgatoStudio-#{version}.dmg"
+  name "Elgato Studio"
+  desc "Capture and manage Elgato devices for content creation"
+  homepage "https://www.elgato.com/ww/en/s/downloads"
+
+  livecheck do
+    url "https://help.elgato.com/api/v2/help_center/en-us/sections/36773146296465/articles.json"
+    strategy :json do |json|
+      articles = json["articles"] || []
+      body = articles.first&.dig("body")
+      match = body&.match(/ElgatoStudio-(\d+(?:\.\d+)+)\.(\d+)\.dmg/i)
+      next unless match
+
+      "#{match[1]}.#{match[2]}"
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Elgato Studio.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.elgato.studio",
+    "~/Library/Caches/com.elgato.studio",
+    "~/Library/Preferences/com.elgato.studio.plist",
+    "~/Library/Saved Application State/com.elgato.studio.savedState",
+  ]
+end
