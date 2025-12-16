515daad4147bc3149071bb7f246734fe26f8a450
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-16 01:50:00 +0000

    splice 5.4.4.1

--- Casks/s/splice.rb
+++ Casks/s/splice.rb
@@ -1,9 +1,9 @@
 cask "splice" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "5.4.4"
-  sha256 arm:   "b040b20ac81bcdc11692c0cba3daf8eddf4ac1d6fa3dcdf9bb774d3246ea883a",
-         intel: "6f6220301d4ef513d631fd9a8f227a5a4c89392f0134f57e68a97616b10ac445"
+  version "5.4.4.1"
+  sha256 arm:   "71441bd975f2af904860cb8d2fa222516d7d49d0216eccb070fdb164a3fd3c57",
+         intel: "54b0cdabd8d99e34d3d40e710deb7fd0bb5c65af3eac0301505546a37d9e5ed3"
 
   url "https://desktop.splice.com/conveyor/stable/splice-#{version}-mac-#{arch}.zip"
   name "Splice"
