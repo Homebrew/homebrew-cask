95bc7bd9ec93da1902477ebf913c0878db2c423a
Boshi Yuan <542629877@qq.com>
2025-12-16 22:20:47 +0800

    font-noto-sans-cjk-jp: cancel deprecation status since repo becomes active

--- Casks/font/font-n/font-noto-sans-cjk-jp.rb
+++ Casks/font/font-n/font-noto-sans-cjk-jp.rb
@@ -6,8 +6,9 @@ cask "font-noto-sans-cjk-jp" do
   name "Noto Sans CJK JP"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
 
-  deprecate! date: "2025-11-22", because: :discontinued, replacement_cask: "font-noto-sans-jp"
-  disable! date: "2026-11-22", because: :discontinued, replacement_cask: "font-noto-sans-jp"
+  livecheck do
+    cask "font-noto-sans-cjk"
+  end
 
   font "NotoSansCJKjp-Black.otf"
   font "NotoSansCJKjp-Bold.otf"
