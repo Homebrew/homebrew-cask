b123fd56a5b13413e1673c188afec266c04e8d0d
Bevan Kay <email@bevankay.me>
2025-12-17 12:20:30 +1100

    orangedrangon-android-messages: deprecate

--- Casks/o/orangedrangon-android-messages.rb
+++ Casks/o/orangedrangon-android-messages.rb
@@ -12,6 +12,8 @@ cask "orangedrangon-android-messages" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on macos: ">= :big_sur"
 
   app "Android Messages.app"
