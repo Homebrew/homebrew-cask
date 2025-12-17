681de96749ea11e47f684aa237fcbc7fcf075be9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 22:08:01 +0000

    iterm2@beta 3.6.7beta2

--- Casks/i/iterm2@beta.rb
+++ Casks/i/iterm2@beta.rb
@@ -1,7 +1,7 @@
 cask "iterm2@beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.6.7beta1"
-  sha256 "24e600e9ce856c1226110f43b2344857dcc5f44e877dfb138819d67ab79eea5d"
+  version "3.6.7beta2"
+  sha256 "e4adc160269a9d31132435b20fbe1aca021e55399cc12c02645288e7eefad2e8"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
