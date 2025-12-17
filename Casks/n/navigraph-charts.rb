cc783f12152e5501e6bc053f64a7e4633a58cf76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 16:50:53 +0000

    navigraph-charts 8.40.1

--- Casks/n/navigraph-charts.rb
+++ Casks/n/navigraph-charts.rb
@@ -1,6 +1,6 @@
 cask "navigraph-charts" do
-  version "8.40.0"
-  sha256 "f36ea5e6aec1d83730a4609e31497e2f3e7a25f1eb3bcf44784609fad62f5c4e"
+  version "8.40.1"
+  sha256 "46cbc5d71ee2e500bff434fb4a57ab93d5c0dd18160a82f0c70b773b577a4b55"
 
   url "https://download.navigraph.com/software/charts/mac/Navigraph%20Charts%20#{version}.dmg"
   name "Navigraph Charts"
