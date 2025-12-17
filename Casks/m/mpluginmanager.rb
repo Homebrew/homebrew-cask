39f4c3e8ee577d6e1104552b8c30d00ff1f5c006
Ethan Bayer <studioethanb@gmail.com>
2025-12-16 20:27:09 -0600

    Modify livecheck, url, and uninstall with feedback
    This commit updates the mpluginmanager cask's download URL from a
    long, query-based link to a shorter, direct download link from
    the app's CDN. This simplifies the link and livecheck regex.
    
    The livecheck block is modified to use more straightforward regex
    based on the new link.
    
    The uninstall stanza is updated to remove the functionality of
    uninstalling with pkgutil. That approach didn't work because the
    original install pkg installs to a /tmp directory first and runs
    a postinstall script to move the files, causing pkgutil to not
    recognize the real installed files. Instead, the uninstall
    directly deletes the application binary and the deletion of
    other files has been moved to the zap stanza to avoid conflicts
    with installed audio plugins from the manager.
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/m/mpluginmanager.rb
+++ Casks/m/mpluginmanager.rb
@@ -2,28 +2,33 @@ cask "mpluginmanager" do
   version "02.15"
   sha256 "541d06685c530279c2ee71051c3b04c4141a47acac46504e461ab2c404ed57c5"
 
-  url "https://www.meldaproduction.com/downloads/down?name=MPluginManager_#{version.dots_to_underscores}_setupmac.zip&platform=mac&version=#{version}&mirror=bunnycdn&url=https%3A%2F%2Fmeldaproduction.b-cdn.net%2Fdownload%2Fmpluginmanager%2FMPluginManager_#{version.dots_to_underscores}_setupmac.zip&checksum=8a265945c2b263f916e622791cce101372fee1fd"
+  url "https://meldaproduction.b-cdn.net/download/mpluginmanager/MPluginManager_#{version.dots_to_underscores}_setupmac.zip",
+      verified: "meldaproduction.b-cdn.net/download/mpluginmanager/"
   name "MPluginManager"
   desc "Installer for MeldaProduction audio plugins"
   homepage "https://www.meldaproduction.com/downloads"
 
   livecheck do
     url :homepage
-    regex(/href=.*?platform=mac.*?version=v?(\d+(?:[._-]\d+)*)/i)
+    regex(/href=.*?MPluginManager[._-]v?(\d+(?:[._]\d+)+)[._-]setupmac\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
   end
 
   auto_updates true
 
   pkg "MPluginManager_#{version.dots_to_underscores}_setupmac.pkg"
 
-  uninstall pkgutil: [
-    "com.MeldaProduction.MPluginManager.pkg",
-    "com.MeldaProduction.MPluginManagerData.pkg",
-  ]
+  uninstall quit:    "com.meldaproduction.MPluginManager",
+            pkgutil: [
+              "com.MeldaProduction.MPluginManager.pkg",
+              "com.MeldaProduction.MPluginManagerData.pkg",
+            ],
+            delete:  "/Applications/MeldaProduction"
 
   zap trash: [
     "/Library/Logs/DiagnosticReports/MPluginManager.bin*",
-    "/private/var/db/receipts/com.MeldaProduction.MPluginManager*",
     "~/Library/Application Support/MeldaProduction/Downloader",
     "~/Library/Application Support/MeldaProduction/Menu",
     "~/Library/Application Support/MeldaProduction/MPluginManager",
