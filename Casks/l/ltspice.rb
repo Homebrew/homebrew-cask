95f3af8a71d68db5e70f68d7c3931f7b167da19d
Bevan Kay <email@bevankay.me>
2025-12-18 13:13:24 +1100

    ltspice: update livecheck

--- Casks/l/ltspice.rb
+++ Casks/l/ltspice.rb
@@ -9,7 +9,7 @@ cask "ltspice" do
 
   livecheck do
     url :homepage
-    regex(/for\s+MacOS[\s\S]*Version\s+v?(\d+(?:\.\d+)+)/i)
+    regex(/for\s+MacOS[\s\S]*?Version\s+v?(\d+(?:\.\d+)+)/i)
   end
 
   pkg "LTspice.pkg"
