28a43c75fe9b05cf2e627976cc281c8bd440205d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-18 13:42:22 +0000

    google-chrome@canary 145.0.7584.0

--- Casks/g/google-chrome@canary.rb
+++ Casks/g/google-chrome@canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome@canary" do
-  version "145.0.7583.2"
+  version "145.0.7584.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
