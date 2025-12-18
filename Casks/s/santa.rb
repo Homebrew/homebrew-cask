c6462c528a930139f1b9ed68b8a52b75c6c597b3
W25X80 <78695319+W25X80@users.noreply.github.com>
2025-12-18 19:35:32 +0100

    santa: require ventura

--- Casks/s/santa.rb
+++ Casks/s/santa.rb
@@ -7,6 +7,8 @@ cask "santa" do
   desc "Binary authorization system"
   homepage "https://github.com/northpolesec/santa"
 
+  depends_on macos: ">= :ventura"
+
   pkg "santa-#{version}.pkg"
 
   uninstall early_script: {
