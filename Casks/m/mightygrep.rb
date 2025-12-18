07fc7c402ff331997a794c1c37c6ca01d41beb88
Kyle Sylvestre <kyles3133@gmail.com>
2025-12-18 00:26:34 -0500

    MightyGrep 0.14.0 (new cask)

---
+++ Casks/m/mightygrep.rb
@@ -0,0 +1,23 @@
+cask "mightygrep" do
+  version "0.14.0"
+  sha256 "4878a396c356cff72cf23596bc98ae6c587e185b032edd8a0f2d5c7baf033253"
+
+
+  url "https://github.com/kyle-sylvestre/mightygrep-misc/releases/download/#{version}/mightygrep-macos.dmg",
+      verified: "github.com/kyle-sylvestre/mightygrep-misc"
+  name "MightyGrep"
+  desc "Fast file searching in a GUI"
+  homepage "https://ksylvestre.itch.io/mightygrep"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "MightyGrep Demo.app"
+
+  zap trash: [
+    "~/Library/Application Support/MightyGrep",
+    "~/Library/Preferences/mightygrep.plist",
+  ]
+end
