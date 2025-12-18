b2a51d9f6849e35c63fd8cdefb08720775bcf2e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-18 05:07:01 +0000

    todoist-app 9.26.0

--- Casks/t/todoist-app.rb
+++ Casks/t/todoist-app.rb
@@ -1,9 +1,9 @@
 cask "todoist-app" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.25.1"
-  sha256 arm:   "0cebad48e6596d9668ff017fb1dff1ed6b63784002d89411bceec243e335c2a6",
-         intel: "2a3d28438ebef2df4244c822e2410d825c6e10578d4a10fd2c4a1a3c8e0f8ac8"
+  version "9.26.0"
+  sha256 arm:   "62b5b7bcf49fec6cd64cf4a85abe0541b8ac4cb75f216f2b6cedd6a4326834fd",
+         intel: "188a046d3c31aad693b889bac2c1f2bc5e45e94cb504a7c3c9d29b3a087c4ccc"
 
   url "https://electron-dl.todoist.com/mac/Todoist-darwin-#{version}-#{arch}-latest.dmg"
   name "Todoist"
