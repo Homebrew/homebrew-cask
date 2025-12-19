e9e0f86876f8456b3b6c8ce0d805ee37c3ec8af2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 15:27:14 +0000

    wootility 5.2.2

--- Casks/w/wootility.rb
+++ Casks/w/wootility.rb
@@ -1,9 +1,9 @@
 cask "wootility" do
   arch arm: "-arm64"
 
-  version "5.2.1"
-  sha256 arm:   "59f02bc9f39707467defff834ca3fa11aa7b4d2cb319f760485deb235aa334d6",
-         intel: "98cc20f7a1118bae71ec03b40703a06974b8d8f6cd11ed3bb8033d62236447c7"
+  version "5.2.2"
+  sha256 arm:   "3a5c8a8d3a86a5565f4e6b195af3dcfff099b4058777600c8673c6b8e1c7c847",
+         intel: "a86fb426c0544810d10a06f950fe2dbdb2b0ec30823884308d6f3a9eefee814c"
 
   url "https://wootility-updates.ams3.cdn.digitaloceanspaces.com/wootility-mac/Wootility-#{version}#{arch}.dmg",
       verified: "wootility-updates.ams3.cdn.digitaloceanspaces.com/wootility-mac/"
