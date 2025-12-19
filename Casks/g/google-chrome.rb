cd0dd7ea0cc4cff8ecc2a111b4d71876dd778b84
MoonFruit <dkmoonfruit@gmail.com>
2025-12-19 11:40:27 +0800

    google-chrome 143.0.7499.170

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "143.0.7499.147"
+  version "143.0.7499.170"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
