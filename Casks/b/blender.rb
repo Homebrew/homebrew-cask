4a6f815b23ca6d743a5b228f75e3088805f75757
Alistair McMillan <alistairmcmillan75@gmail.com>
2025-12-19 21:43:53 +0000

    Update Blender version to 5.0.1

--- Casks/b/blender.rb
+++ Casks/b/blender.rb
@@ -5,8 +5,8 @@ cask "blender" do
   # https://www.blender.org/download/lts/ when updating this cask, as we cannot
   # identify LTS versions using livecheck.
   on_arm do
-    version "5.0.0"
-    sha256 "b23984753ec8354878acc25cd4073a41ae195522013f1649903a593bccbfa54b"
+    version "5.0.1"
+    sha256 "102a81ddee5346c96339c6a529069a2d52df05f330eb9bfd431c8dd79fb4afb6"
 
     # The upstream download page (https://www.blender.org/download/) cannot be
     # fetched due to Cloudflare protections, so we have to naively assume a
