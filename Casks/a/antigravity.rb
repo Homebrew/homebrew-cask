8eb92bebd287fe8b28014158c8961968941d7f7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 00:50:10 +0000

    antigravity 1.13.3

--- Casks/a/antigravity.rb
+++ Casks/a/antigravity.rb
@@ -2,9 +2,9 @@ cask "antigravity" do
   arch arm: "arm", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "-arm64"
 
-  version "1.12.4,5388582906101760"
-  sha256 arm:   "e534a76f29dbf4d986180606386d39b92730dc03553ac9b29a2ad5c90a456095",
-         intel: "9f6168a7e6990098256f9085f30a8853d445a68ee29ab9fc5e51892b3bb96131"
+  version "1.13.3,4533425205018624"
+  sha256 arm:   "ed95de92e852cac9ef1b264a0a304cf755a803129b54c1618682ec7bae1893dc",
+         intel: "8e6ba418883dccd8a4a70fd5d6882f8a919630d3166d83327180165ff7b4eb55"
 
   url "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/#{version.csv.first}-#{version.csv.second}/darwin-#{arch}/Antigravity.zip",
       verified: "edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/"
