bc3d18bfcbb9e00d6de6688eeff3ee707a245ae5
Klaus Hipp <khipp@users.noreply.github.com>
2025-12-21 10:58:27 +0100

    reflect 3.1.7

--- Casks/r/reflect.rb
+++ Casks/r/reflect.rb
@@ -1,11 +1,8 @@
 cask "reflect" do
-  arch arm: "arm64", intel: "x64"
+  version "3.1.7"
+  sha256 "1d543288c1a60f1ffa75c1c9138b617797b1e9c17900c7cc90aabfce09a7b2c7"
 
-  version "3.1.3"
-  sha256 arm:   "c9df9ed72e1b56e0ed5c31e3e995aa1f2249087b1961a413947c645053d07378",
-         intel: "651a069690156be46cce2b72152117048b97a5e0f5cff1e1ef8260a5edebbb49"
-
-  url "https://github.com/team-reflect/reflect-electron-updates/releases/download/v#{version}/Reflect-darwin-#{arch}-#{version}.zip",
+  url "https://github.com/team-reflect/reflect-electron-updates/releases/download/v#{version}/Reflect-darwin-universal-#{version}.zip",
       verified: "github.com/team-reflect/reflect-electron-updates/"
   name "Reflect Notes"
   desc "Note taking app for meetings, ideas, journalling, and research"
