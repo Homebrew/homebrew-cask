d7906bba9a5a4848656232dc6a67b0886809f261
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-21 22:30:46 +0000

    xournal++ 1.3.1

--- Casks/x/xournal++.rb
+++ Casks/x/xournal++.rb
@@ -1,9 +1,9 @@
 cask "xournal++" do
   arch arm: "ARM64", intel: "X64"
 
-  version "1.3.0"
-  sha256 arm:   "f52c68fa772f35041a48a6428b5e13d2897a67816c070d2b53cbb30a93d4ca7e",
-         intel: "ecb200d4f0f7587968c69345fe342e123614eb6b52373facbf608dcb8126ebe7"
+  version "1.3.1"
+  sha256 arm:   "b87e710e4158711c34cbae0b5a999b552724c338050b62f0f7b3fd2ed01c0051",
+         intel: "4c8f3cd1a4fee86466deeef33c6a81f0cc25a410102e1b5f3c16424f15c92d01"
 
   url "https://github.com/xournalpp/xournalpp/releases/download/v#{version}/xournalpp-#{version}-macOS-#{arch}.dmg"
   name "Xournal++"
