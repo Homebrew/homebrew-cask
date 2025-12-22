8a6aff89204f77ec467bb40f6aa23e7bfbd8a206
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 16:02:24 +0000

    font-inconsolata-lgc 1.220

--- Casks/font/font-i/font-inconsolata-lgc.rb
+++ Casks/font/font-i/font-inconsolata-lgc.rb
@@ -1,6 +1,6 @@
 cask "font-inconsolata-lgc" do
-  version "1.210"
-  sha256 "bf87a0ba62a8390bcc2938b774261c99368f38a7adfb8bfc84684b2272a2fc98"
+  version "1.220"
+  sha256 "c71812e8cdb7255c2b6c9aa0092f482577b817d8758c991e6a55841bc7858e62"
 
   url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-#{version}/InconsolataLGC-#{version}.tar.xz"
   name "Inconsolata LGC"
