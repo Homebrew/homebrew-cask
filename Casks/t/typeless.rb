a5c741fed520a0926c2f6948094b350139d28c2f
Micah Laaker <micah@laaker.com>
2025-12-22 17:10:56 -0600

    typeless 0.8.1 (new cask)
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

---
+++ Casks/t/typeless.rb
@@ -0,0 +1,30 @@
+cask "typeless" do
+  arch arm: "arm64", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "arm64", intel: "latest"
+
+  version "0.8.1"
+  sha256 arm:   "c7603e47f4d446dac220255ab61f0d4fb95a4aad10c98aead09d4273af58d951",
+         intel: "a25279072c3455ebfd9de292ee489e3a39ec92416b589b48fd3f750d03305c95"
+
+  url "https://typeless-static.com/desktop-release/Typeless-#{version}-#{arch}.dmg",
+      verified: "typeless-static.com/"
+  name "Typeless"
+  desc "AI voice dictation that turns speech into polished text"
+  homepage "https://typeless.com/"
+
+  livecheck do
+    url "https://typeless-static.com/desktop-release/#{livecheck_arch}-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Typeless.app"
+
+  zap trash: [
+    "~/Library/Application Support/typeless",
+    "~/Library/Caches/typeless",
+    "~/Library/Preferences/com.typeless.plist",
+  ]
+end
