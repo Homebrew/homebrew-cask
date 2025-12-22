85585a44b45bbd08428ce6674641c9b539dc4baf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 07:46:39 +0000

    vimr 0.59.3

--- Casks/v/vimr.rb
+++ Casks/v/vimr.rb
@@ -1,6 +1,6 @@
 cask "vimr" do
-  version "0.59.2,20251201.200738"
-  sha256 "1cb3422ffb6b161109b1d15070158e2a7b6978a5314e698f226cff42b951b7bb"
+  version "0.59.3,20251222.155246"
+  sha256 "24d12e5db4748ddb83a97b71a206b297b45a0dc0a57a257e4466e3e4b03c7a9b"
 
   url "https://github.com/qvacua/vimr/releases/download/v#{version.csv.first}-#{version.csv.second}/VimR-v#{version.csv.first}.tar.bz2"
   name "VimR"
