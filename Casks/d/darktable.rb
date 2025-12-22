310d4a4329c2c71494ff47e43a37058e01c812f4
tim@mbp <timforrer16@gmail.com>
2025-12-22 15:06:14 +0900

    Split dependencies between arm64 and x86_64

--- Casks/d/darktable.rb
+++ Casks/d/darktable.rb
@@ -1,14 +1,17 @@
 cask "darktable" do
   arch arm: "arm64", intel: "x86_64"
-  
+
   on_arm do
     version "5.4.0"
     sha256 "17e1eaf2bd7ab0ada2e93d4bba2ba6b81345e99eb338635682d08dae64d7a150"
-  end
 
+    depends_on macos: ">= :sonoma"
+  end
   on_intel do
     version "5.2.1"
     sha256 "5060d73c03b3c7f4ca69a77039e69653fd2310df8cb649d66fc89f4c45289b0c"
+
+    depends_on macos: ">= :ventura"
   end
 
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}-#{arch}.dmg",
@@ -23,8 +26,6 @@ cask "darktable" do
   end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
-  depends_on macos: ">= :ventura"
 
   app "darktable.app"
 
