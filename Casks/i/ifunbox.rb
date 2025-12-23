a2068718adaa4293f520b90da7522325f4a239f9
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-23 15:43:23 -0500

    ifunbox: update url
    The artifact URL for `ifunbox` uses HTTP but it works with HTTPS, so
    this updates the URL accordingly.

--- Casks/i/ifunbox.rb
+++ Casks/i/ifunbox.rb
@@ -2,7 +2,7 @@ cask "ifunbox" do
   version "1.8"
   sha256 "19bef7c6079cb3d13dc109478c473e420643e3164ed02b668f76220f60884a11"
 
-  url "http://dl.i-funbox.com/mac/ifunboxmac.#{version}.dmg"
+  url "https://dl.i-funbox.com/mac/ifunboxmac.#{version}.dmg"
   name "iFunBox"
   desc "File management software for iPhone and other Apple products"
   homepage "https://www.i-funbox.com/"
