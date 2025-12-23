9a7ba54da9e54728823de2959d782d93f529a8d6
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-23 01:24:16 -0500

    unite-phone: disable
    The artifact download URL for `unite-phone` is inaccessible in the CI
    environment, so this disables the cask as unreachable.

--- Casks/u/unite-phone.rb
+++ Casks/u/unite-phone.rb
@@ -3,17 +3,13 @@ cask "unite-phone" do
   sha256 "09ca6ee8263baf87e137e8129caf79e7e4496904c08c3e550f4ac795c2223b34"
 
   url "https://update.unitephone.nl/updates/unite_phone-#{version}-universal-mac.zip",
-      user_agent: :fake
+      user_agent: :browser
   name "Unite Phone"
   desc "Video and voice calling application"
   homepage "https://unitephone.nl/"
 
-  livecheck do
-    url "https://update.unitephone.nl/updates/latest-mac.yml"
-    strategy :electron_builder
-  end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  # Artifact URL is consistently unreachable in the homebrew/cask CI environment
+  disable! date: "2025-12-31", because: :unreachable
 
   depends_on macos: ">= :big_sur"
 
