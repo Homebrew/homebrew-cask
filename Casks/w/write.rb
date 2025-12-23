dbf24b91b055f6cfb0af4366d071754a2adf4f77
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-23 18:26:03 -0500

    write: update urls
    The www.styluslabs.com URLs in the `write` cask use HTTP but they can
    use HTTPS, so this updates the URLs accordingly.

--- Casks/w/write.rb
+++ Casks/w/write.rb
@@ -2,13 +2,13 @@ cask "write" do
   version "3.0.0"
   sha256 "036e75f00f47c3dc33cdddfe7b2449d5bcff696992389138467de1f9757b6c57"
 
-  url "http://www.styluslabs.com/write/write#{version.no_dots}.dmg"
+  url "https://www.styluslabs.com/write/write#{version.no_dots}.dmg"
   name "Write"
   desc "Word processor for handwriting"
-  homepage "http://www.styluslabs.com/"
+  homepage "https://www.styluslabs.com/"
 
   livecheck do
-    url "http://www.styluslabs.com/download/write-dmg"
+    url "https://www.styluslabs.com/download/write-dmg"
     regex(/write[._-]?v?(\d+)\.dmg/i)
     strategy :header_match do |header, regex|
       match = header["location"]&.match(regex)
