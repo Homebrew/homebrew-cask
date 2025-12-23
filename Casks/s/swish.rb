a86ac505384d2d33d7e7be7a112f0b7cf1742825
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 01:44:32 +0000

    swish 1.13.2

--- Casks/s/swish.rb
+++ Casks/s/swish.rb
@@ -1,6 +1,6 @@
 cask "swish" do
-  version "1.13.1"
-  sha256 "078c14183dbe06e5eb505e22a7c3e043beccfd73d51cb948f425113ce52110d4"
+  version "1.13.2"
+  sha256 "1db6ddcbc950f71d869114cee5dc069485e682d1be7bce494dee2fc3cb04f333"
 
   url "https://github.com/chrenn/swish-dl/releases/download/#{version}/Swish.dmg",
       verified: "github.com/chrenn/swish-dl/"
