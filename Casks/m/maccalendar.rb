94075d51347872177d41a1bf48a40d788a4ccaed
叉叉 <ruihe.lin@live.com>
2025-12-24 09:26:00 +0800

    New Cask: MacCalendar

---
+++ Casks/m/maccalendar.rb
@@ -0,0 +1,12 @@
+cask "maccalendar" do
+  version "1.1.10"
+  sha256 "4db6752c1c22fd9912618508187ae711731d3c3576a47ad8aba25383f93473b5"
+
+  url "https://github.com/bylinxx/MacCalendar/releases/download/v#{version}/MacCalendar_#{version}.dmg"
+
+  name "MacCalendar"
+  desc "完全免费&开源的离线小而美 macOS 菜单栏日历app，支持中国农历、节假日、放假安排、系统日程等"
+  homepage "https://github.com/bylinxx/MacCalendar"
+
+  app "MacCalendar.app"
+end
