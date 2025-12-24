f4f0dd873a80877b306d1307f2f8062d25450da2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-24 19:02:50 +0000

    groestlcoin-core 30.1

--- Casks/g/groestlcoin-core.rb
+++ Casks/g/groestlcoin-core.rb
@@ -1,9 +1,9 @@
 cask "groestlcoin-core" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "29.0"
-  sha256 arm:   "381446d49527fe2291498a01773ab44615bb970e0e7ea3bd8e14e0458f02e072",
-         intel: "2195198790178321d3f0233e4ad7439c6f387f0e5d5f6e6c80ed338a54df2014"
+  version "30.1"
+  sha256 arm:   "311d22f17bba25fb624e9d11f98e92b3ea038e3ebfe5610bacf15ef024073fc2",
+         intel: "5210639970c2e4072bc56368a18408f0c9bff3f0ef9b757554bee4c0bae2e619"
 
   url "https://github.com/groestlcoin/groestlcoin/releases/download/v#{version}/groestlcoin-#{version}-#{arch}-apple-darwin.zip",
       verified: "github.com/groestlcoin/groestlcoin/"
@@ -11,7 +11,7 @@ cask "groestlcoin-core" do
   desc "Groestlcoin client and wallet"
   homepage "https://www.groestlcoin.org/groestlcoin-core-wallet/"
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :ventura"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "Groestlcoin-Qt.app", target: "Groestlcoin Core.app"
