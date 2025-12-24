6b9e1d038e6c1f79f8f4db1f1185356f94fe3017
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-24 07:52:33 +0000

    visualdiffer 2.1.2

--- Casks/v/visualdiffer.rb
+++ Casks/v/visualdiffer.rb
@@ -1,6 +1,6 @@
 cask "visualdiffer" do
-  version "2.1.1"
-  sha256 "814a72a1f8fc83c70216522af034cca464cb6e272e2a29860c700816e726dce7"
+  version "2.1.2"
+  sha256 "e4eb0521fa9969a2bf44661dfc712665b820038e0900672f5bef6a03cbbcc929"
 
   url "https://github.com/visualdiffer/visualdiffer/releases/download/v#{version}/VisualDiffer-#{version}.zip",
       verified: "github.com/visualdiffer/visualdiffer/"
