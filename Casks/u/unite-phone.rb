d4cf6068b814cc345425dbc58db394a9d05e1b17
Bevan Kay <email@bevankay.me>
2025-12-24 23:01:45 +1100

    unite-phone: try different url, remove checksum

--- Casks/u/unite-phone.rb
+++ Casks/u/unite-phone.rb
@@ -1,15 +1,14 @@
 cask "unite-phone" do
   version "2025.12.4"
-  sha256 "070af9bec214f12d56d807585262aa0cac4e9ae99201d13529bdc779a72ed101"
+  sha256 :no_check
 
-  url "https://update.unitephone.nl/updates/unite_phone-#{version}-universal-mac.zip",
-      referer: "https://unitephone.nl/unitephone-app/macos"
+  url "https://unitephone.nl/unitephone-app/macos"
   name "Unite Phone"
   desc "Video and voice calling application"
   homepage "https://unitephone.nl/"
 
   livecheck do
-    url "https://unitephone.nl/unitephone-app/macos"
+    url :url
     strategy :header_match
   end
 
