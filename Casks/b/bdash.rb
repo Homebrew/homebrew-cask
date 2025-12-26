ab6869347d7192f500b1e84de7d6143f2959f8cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 06:43:33 +0000

    bdash 1.31.0

--- Casks/b/bdash.rb
+++ Casks/b/bdash.rb
@@ -1,6 +1,6 @@
 cask "bdash" do
-  version "1.30.1"
-  sha256 "d3895636fcdfdbfa6d88a04034dc12d9fe7bbae4e167f09b2f71b24e08aded47"
+  version "1.31.0"
+  sha256 "a0a5b7591cc46bb1d35bdcc9c376bc91f4dbe36db052c0aca8eea2685d247286"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-universal-mac.zip"
   name "Bdash"
