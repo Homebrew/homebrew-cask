23a2df8bf812fa3acb33bd95771e695eec1a8d84
Jack <jack.chen@ef.cn>
2025-12-26 11:16:52 +0800

    yingfu-online 1.2.1

--- Casks/y/yingfu-online.rb
+++ Casks/y/yingfu-online.rb
@@ -1,9 +1,9 @@
 cask "yingfu-online" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.0"
-  sha256 arm:   "e3fcc3a189d43dcbee51eb320aa29883b8d111e61f60be4c5b44b0f35dafd4f1",
-         intel: "31d7c2c90b7e607fb6ebcabe15089473fce8b5bd96b5249f80c34f6a2eb4ea58"
+  version "1.2.1"
+  sha256 arm:   "18ec50717fc2c1b06b20f95837b70c33dfe7280b1f2bcd624e9162c1fc8b510e",
+         intel: "b9f8a26ef400660e7a5c0375af707265493f8649c292e0a1245ec1bf4a5be376"
 
   url "https://online.yingfu.com.cn/cdn/desktop-release/stable/darwin/#{arch}/%E8%8B%B1%E5%AD%9A%E4%B9%90%E5%AD%A6%20#{version}%20(#{arch}).dmg"
   name "Yingfu Online"
