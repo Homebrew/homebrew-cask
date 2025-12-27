7a6cfd18608f0d7d1504d2f3875503d1a948b168
Sam Ford <1584702+samford@users.noreply.github.com>
2025-12-27 09:42:41 -0500

    astah-professional 11.0.0
    `astah-professional` is autobumped but the workflow failed to update
    to version 11.0.0 because the `livecheck` block produces an "Unable to
    get versions" error due to the regex not matching the current file
    name. This updates the cask for 11.0.0 and adjusts the `livecheck`
    block regex accordingly.
    
    It's worth noting that upstream seems to only offer an ARM version for
    macOS, so this keeps the previous 10.x version around for Intel for
    the time being.

--- Casks/a/astah-professional.rb
+++ Casks/a/astah-professional.rb
@@ -1,25 +1,41 @@
 cask "astah-professional" do
-  version "10.1.0,9ceee1"
-  sha256 "4d9c28800d6ccf6c7ff10488e1cc6e856392347a7191feeda7ccf0e305aeb0e0"
+  on_arm do
+    version "11.0.0,ba221e"
+    sha256 "220f7057cef479f7fb28841bb22ae7552771ab3c5295e471719c1ac9912590d2"
 
-  url "https://cdn.change-vision.com/files/astah-professional-#{version.csv.first.dots_to_underscores}-#{version.csv.second}-MacOs.dmg",
-      verified: "cdn.change-vision.com/files/"
+    url "https://cdn.change-vision.com/files/astah-professional-#{version.csv.first.dots_to_underscores}-#{version.csv.second}-MacOs-aarch64.dmg",
+        verified: "cdn.change-vision.com/files/"
+
+    livecheck do
+      url "https://members.change-vision.com/download/files/astah_professional/latest/mac_pkg"
+      regex(/astah[._-]professional[._-]v?(\d+(?:[._]\d+)+)[._-](\h+)[._-]MacOs.*?\.dmg/i)
+      strategy :header_match do |headers, regex|
+        match = headers["location"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1].tr("_", ".")},#{match[2]}"
+      end
+    end
+
+    pkg "astah professional aarch64 ver #{version.csv.first.dots_to_underscores}.pkg"
+  end
+  on_intel do
+    version "10.1.0,9ceee1"
+    sha256 "4d9c28800d6ccf6c7ff10488e1cc6e856392347a7191feeda7ccf0e305aeb0e0"
+
+    url "https://cdn.change-vision.com/files/astah-professional-#{version.csv.first.dots_to_underscores}-#{version.csv.second}-MacOs.dmg",
+        verified: "cdn.change-vision.com/files/"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    pkg "astah professional ver #{version.csv.first.dots_to_underscores}.pkg"
+  end
+
   name "Change Vision Astah Professional"
   desc "Software modelling tool"
   homepage "https://astah.net/editions/professional"
-
-  livecheck do
-    url "https://members.change-vision.com/download/files/astah_professional/latest/mac_pkg"
-    regex(/astah[._-]professional[._-]v?(\d+(?:[._]\d+)+)[._-](\h+)[._-]MacOs\.dmg/i)
-    strategy :header_match do |headers, regex|
-      match = headers["location"]&.match(regex)
-      next if match.blank?
-
-      "#{match[1].tr("_", ".")},#{match[2]}"
-    end
-  end
-
-  pkg "astah professional ver #{version.csv.first.dots_to_underscores}.pkg"
 
   uninstall pkgutil: "com.change-vision.astah.professional"
 
