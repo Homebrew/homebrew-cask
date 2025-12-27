9fb7cc8ac91d418a1e244484dbfbabfe8ca54b1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-27 19:19:58 +0000

    satyrn 0.0.117

--- Casks/s/satyrn.rb
+++ Casks/s/satyrn.rb
@@ -1,9 +1,9 @@
 cask "satyrn" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.116"
-  sha256 arm:   "9896a941fa14a6ad71c7f497977b97dc575aac21f0901fc1a69a831f0fe065ec",
-         intel: "ba206085c123fa464e7f493d0f9fff7caeab31ef4a3b826760c20d9bc340b986"
+  version "0.0.117"
+  sha256 arm:   "216b7e85c01966893c454f17975f4d574dcd0958ba541b3dac851bb00b2f6fbf",
+         intel: "b5bd38d4e93054b4b0616cea78b05703287773411a1145ad98fcd4435428ccde"
 
   url "https://satyrn-updates.s3.amazonaws.com/satyrn-#{version}-#{arch}.dmg",
       verified: "satyrn-updates.s3.amazonaws.com/"
