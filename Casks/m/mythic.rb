d4d0843d7765862f63b55d44a220920d95580cf8
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-12-28 02:17:13 +0100

    mythic 0.6.0

--- Casks/m/mythic.rb
+++ Casks/m/mythic.rb
@@ -1,6 +1,6 @@
 cask "mythic" do
-  version "0.5.0,cfbc3111-f5b3-469d-823b-393f0793c550"
-  sha256 "782a07d9d83f686c351a3867661757447480e52fd65904816760a38b7775b30e"
+  version "0.6.0,92033dfd-7a35-4629-9ca5-60d66576fb65"
+  sha256 "bcdb543d56e71e6ae3e102ff5bebbbdecdb1066549228d4c5f1a3c5cfa82c816"
 
   url "https://dl.getmythic.app/updates/#{version.csv.second}/Mythic.zip"
   name "Mythic"
