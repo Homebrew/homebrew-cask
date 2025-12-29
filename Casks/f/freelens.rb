279d635b5707fbfc1b280015ca86d8b471aea55e
Patrick Linnane <patrick@linnane.io>
2025-12-29 11:32:55 -0800

    freelens: bump minimum macOS version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/freelens.rb
+++ Casks/f/freelens.rb
@@ -12,7 +12,7 @@ cask "freelens" do
   homepage "https://freelens.app/"
 
   conflicts_with cask: "freelens@nightly"
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Freelens.app"
 
