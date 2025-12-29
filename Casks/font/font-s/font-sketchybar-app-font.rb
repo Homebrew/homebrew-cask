dd77b643f31eb12464d363ebd25135d82fa4f064
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 07:00:23 +0000

    font-sketchybar-app-font 2.0.50

--- Casks/font/font-s/font-sketchybar-app-font.rb
+++ Casks/font/font-s/font-sketchybar-app-font.rb
@@ -1,6 +1,6 @@
 cask "font-sketchybar-app-font" do
-  version "2.0.49"
-  sha256 "6da10bd7c4093631930d12fd78cb622d1d14768fedf153385d80381f302327a1"
+  version "2.0.50"
+  sha256 "7459d51c93e23bc18365aa610a27690d35bc1959b634ae0d84950ef22696fa5b"
 
   url "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v#{version}/sketchybar-app-font.ttf"
   name "sketchybar-app-font"
