3f69530e592e8746f3847c29484709f9e71a712f
Patrick Linnane <patrick@linnane.io>
2025-12-29 11:09:42 -0800

    eclipse-java: add minimum macOS version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/e/eclipse-java.rb
+++ Casks/e/eclipse-java.rb
@@ -14,6 +14,8 @@ cask "eclipse-java" do
     cask "eclipse-ide"
   end
 
+  depends_on macos: ">= :big_sur"
+
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse Java.app"
 
