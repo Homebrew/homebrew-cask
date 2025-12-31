c43426353f805ef7b6732dc36ee5b8d89e3aabc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 13:35:31 +0000

    orcasheets 26.1.1

--- Casks/o/orcasheets.rb
+++ Casks/o/orcasheets.rb
@@ -1,9 +1,9 @@
 cask "orcasheets" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "25.12.2"
-  sha256 arm:   "5824f7214be1d59e1a87a0f033e283aeceb1d2fbf52c04cf9025de060b5a8f2b",
-         intel: "56c481958f2cdd9481cf38ac1180f2a843d2ce46b4d16b55b7e12af3cb7681f9"
+  version "26.1.1"
+  sha256 arm:   "b61e5a6d03aecab01ee332181634e15a3f4eee8c56d2dd9a5690152d0be9289f",
+         intel: "48cd31b8a67789187ca72bf99f9f6389b16f085b846de956e67d164770375711"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
       verified: "github.com/dataorchestration/homebrew-orcasheets/"
