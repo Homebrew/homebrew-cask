982575730a5671113c8e94791ac751042f636192
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-01 13:21:32 +0000

    only-switch 2.6.4

--- Casks/o/only-switch.rb
+++ Casks/o/only-switch.rb
@@ -1,6 +1,6 @@
 cask "only-switch" do
-  version "2.6.3"
-  sha256 "a5f5c5d9f8f4f257077e46e5af48cd12b4dc7968084f7800808d3848b437c61a"
+  version "2.6.4"
+  sha256 "5a7a121f38af499342d68050af094ebcd61e8aed944a53633d3985cdb33d56c4"
 
   url "https://github.com/jacklandrin/OnlySwitch/releases/download/release_#{version}/OnlySwitch.dmg"
   name "OnlySwitch"
@@ -13,7 +13,7 @@ cask "only-switch" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :ventura"
 
   app "Only Switch.app"
 
