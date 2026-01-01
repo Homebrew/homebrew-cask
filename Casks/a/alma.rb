9b3655cfed57ce269a5185ae9ad7db1f47b235f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-01 09:39:10 +0000

    alma 0.0.169

--- Casks/a/alma.rb
+++ Casks/a/alma.rb
@@ -1,9 +1,9 @@
 cask "alma" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.168"
-  sha256 arm:   "10a6b4829b82ed0a7ffec92c8b49bff2b2be98b8616af5f3c2c183266443b3f8",
-         intel: "ed12c06766e3597324a7b1dac9ce00fbdc5d8b5a29c8d827166ea95def0f1200"
+  version "0.0.169"
+  sha256 arm:   "6203ffc7508b1d8abab436382289ac4230a08aac578e1fa9fa744e2476190677",
+         intel: "f97e50fbf254eb8e0e4894e17b932792ef950f4e9f2e01e48a9c252170a13adb"
 
   url "https://updates.alma.now/alma-#{version}-mac-#{arch}.dmg"
   name "Alma"
