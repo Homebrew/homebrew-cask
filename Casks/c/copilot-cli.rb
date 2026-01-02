ead46174526015911b43e62c09f77fc6c64129eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-02 21:43:15 +0000

    copilot-cli 0.0.374

--- Casks/c/copilot-cli.rb
+++ Casks/c/copilot-cli.rb
@@ -2,11 +2,11 @@ cask "copilot-cli" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "0.0.373"
-  sha256 arm:          "a821b8947695cf06aa5820cf4bd0be9b19bf0938e2baee5bcb507fc58029d59b",
-         intel:        "f087b359a03c00a2df6ba9f9025730d14ed717430589e1fe8b4c4bf9c824376c",
-         arm64_linux:  "1cfab963ffdab1ef666c7543850fc2b479922dc4125395c910c2b475f4163035",
-         x86_64_linux: "76f6bf05801d5c968a3c8694b7a205c149ae9bc6c2b81442f38a6cf4d3239cb2"
+  version "0.0.374"
+  sha256 arm:          "222c5aeb4c4cd69c0d882ce91ac727b8833c6546a4b48cf78a5fd0533611e621",
+         intel:        "d889f2268ac8ea649de4b7f5083d5bfaec0fb4aaa52a3709c0ab703225fe969c",
+         arm64_linux:  "56298f7b396a3ebf5ef58b1676078781f9dc25954e2ae185f49cedef156d2323",
+         x86_64_linux: "44e6a162ffabd33af933342a294c83dc2c8b7f4e2bba10d18c85f839001000a5"
 
   url "https://github.com/github/copilot-cli/releases/download/v#{version}/copilot-#{os}-#{arch}.tar.gz"
   name "GitHub Copilot CLI"
