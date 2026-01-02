60881bbf316ae731f88d93048c1741f67e0d41eb
Klaus Hipp <khipp@users.noreply.github.com>
2026-01-02 13:09:43 +0100

    xemu 0.8.130

--- Casks/x/xemu.rb
+++ Casks/x/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.8.129"
-  sha256 "f960dc1c78c291a5690d3b270c8c3516731c32570b0e3b949c517b6d37e5cea7"
+  version "0.8.130"
+  sha256 "9b2b313ba14272b6ad151d44cacd7617f65bec39b9431dcbeccc9eb67e8134ad"
 
   url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/xemu-project/xemu/"
@@ -12,8 +12,6 @@ cask "xemu" do
     url :url
     strategy :github_latest
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "Xemu.app"
 
