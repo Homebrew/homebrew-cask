dc97a22c121d66ca3f5028ebb332523cc64b6cde
Kuingsmile <96409857+Kuingsmile@users.noreply.github.com>
2026-01-03 18:39:12 +0800

    Update PicList to version 3.1.1

--- Casks/p/piclist.rb
+++ Casks/p/piclist.rb
@@ -1,9 +1,9 @@
 cask "piclist" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.1.0"
-  sha256 arm:   "ce4bae12e9c1704190fe27497352f4f36227330a86812a0e2654533a7dde8471",
-         intel: "b30019736b3b319ccce994a4fbfa8ccd1c43a1b1a4376c836b7a28182ed6e12b"
+  version "3.1.1"
+  sha256 arm:   "1fd2baa0f8f39b0fa6b6e14e6d20812266aa6b9385c7ab00ef115094510e5ac7",
+         intel: "a621a1988bc12d6292d6feeee5f258016a26d338b98ba582565a21f11e18a5f7"
 
   url "https://github.com/Kuingsmile/PicList/releases/download/v#{version}/PicList-#{version}-#{arch}.dmg",
       verified: "github.com/Kuingsmile/PicList/"
