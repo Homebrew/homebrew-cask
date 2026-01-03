c536a495f8c884f773396cfe8a09fe8f557e2e5e
Bevan Kay <email@bevankay.me>
2026-01-03 13:52:46 +0800

    grayjay: undeprecate

--- Casks/g/grayjay.rb
+++ Casks/g/grayjay.rb
@@ -5,10 +5,6 @@ cask "grayjay" do
   sha256 arm:   "555d5b88f2036ea9a844a3edadbe1aa5bee21cadd9f64e2e5773ee582912a7eb",
          intel: "23fc172bff8693819e8b0e9c485c5d6cf8354561dc033ffa395e24d686deb572"
 
-  on_intel do
-    disable! date: "2026-09-01", because: :fails_gatekeeper_check
-  end
-
   url "https://updater.grayjay.app/Apps/Grayjay.Desktop/#{version}/Grayjay.Desktop-osx-#{arch}-v#{version}.zip"
   name "Grayjay desktop"
   desc "Multi-platform video player"
