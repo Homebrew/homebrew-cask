c493726c040dbb90f038c02a89ade42de09e58bf
Alexander Avdonin <avdonin@gmail.com>
2026-01-04 20:58:26 +0300

    Fix regex for -mac ending and description

--- Casks/c/clipaste.rb
+++ Casks/c/clipaste.rb
@@ -4,15 +4,23 @@ cask "clipaste" do
 
   url "https://www.ntwind.com/files/Clipaste_#{version}-mac.dmg"
   name "Clipaste"
-  desc "Clipboard manager"
+  desc "Clipboard history manager"
   homepage "https://www.ntwind.com/cross-platform/clipaste.html"
 
   livecheck do
     url "https://www.ntwind.com/download/Clipaste_latest-mac.dmg"
     strategy :header_match
+    regex(/Clipaste_(\d+(?:\.\d+)*)-mac\.dmg/i)
   end
 
   depends_on macos: ">= :big_sur"
 
   app "Clipaste.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.ntwind.clipaste",
+    "~/Library/Caches/com.ntwind.clipaste",
+    "~/Library/Preferences/com.ntwind.clipaste.plist",
+    "~/Library/Saved Application State/com.ntwind.clipaste.savedState",
+  ]
 end
