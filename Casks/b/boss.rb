e02cd2678b1800df0706c1dacf16fa93892fe607
kshivang <shivang.iitk@gmail.com>
2026-01-05 02:40:14 +0000

    boss: update 8.15.0
      Release notes:
      - Updated to version 8.15.0
      - SHA256: b624c69244bf4da72a554cbec7bf1fdffb19907d539979b066edb68f20bf73dd
      - Release: https://github.com/risa-labs-inc/BossConsole-Releases/releases/tag/v8.15.0

--- Casks/b/boss.rb
+++ Casks/b/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "8.14.10"
-  sha256 "48de6e8516d585b56e91500c5c448a6c53aae894cdb8b92b1514fb2ed8ce861d"
+  version "8.15.0"
+  sha256 "b624c69244bf4da72a554cbec7bf1fdffb19907d539979b066edb68f20bf73dd"
 
   url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BOSS-Releases/"
