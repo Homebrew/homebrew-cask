cd5bd68b48d63bba46e9e2daf2698370a4b052aa
Zeldris <0trims.vehicle@icloud.com>
2026-01-05 15:17:45 +0700

    aya: fix url

--- Casks/a/aya.rb
+++ Casks/a/aya.rb
@@ -5,7 +5,7 @@ cask "aya" do
   sha256 arm:   "44343bea5e4bcbba09deae815698765a7831bfcde358bf6a32a5f5162619577d",
          intel: "45411d655f18eb06b60e7d29f102fbcc3ac56fc2028caf3e86fb47ec8fd9f9c2"
 
-  url "https://github.com/liriliri/aya/releases/download/#{version}/AYA-v#{version}-mac-#{arch}.dmg",
+  url "https://github.com/liriliri/aya/releases/download/v#{version}/AYA-#{version}-mac-#{arch}.dmg",
       verified: "github.com/liriliri/aya/"
   name "AYA"
   desc "Android ADB Desktop App"
