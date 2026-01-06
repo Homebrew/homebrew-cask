59381f1b7e976cab3fc19ee2ab4e0f26d13b555d
Yihe Li <winmikedows@hotmail.com>
2026-01-06 13:34:48 +0800

    Update prismlauncher to 10.0.0

--- Casks/p/prismlauncher.rb
+++ Casks/p/prismlauncher.rb
@@ -1,14 +1,29 @@
 cask "prismlauncher" do
-  version "9.4"
-
   on_catalina :or_older do
+    version "9.4"
     sha256 "ad5e4d12d91631e4aeec69499e244b0c6fc255d9abe3e26f4f45571a6736206c"
 
     url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-Legacy-#{version}.zip",
         verified: "github.com/PrismLauncher/PrismLauncher/"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
-  on_big_sur :or_newer do
+  on_big_sur do
+    version "9.4"
     sha256 "5cc0148e427d28c632978a9e83e2da3fc02f5072990d9e7732dff3fdb1912ae4"
+
+    url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-#{version}.zip",
+        verified: "github.com/PrismLauncher/PrismLauncher/"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
+    version "10.0.0"
+    sha256 "46013c4b6f81775f0ea4d27a19fd49ca5ee67872e604db76fed02c1eb21152ef"
 
     url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-#{version}.zip",
         verified: "github.com/PrismLauncher/PrismLauncher/"
