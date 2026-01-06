e27d83056e10b98d496cfd6fb82292b23dd1bba1
Dominik Kandravý <dominik@Dominiks-MacBook-Pro.local>
2026-01-06 11:15:22 +0100

    Update monocle-app to 3.0.3

--- Casks/m/monocle-app.rb
+++ Casks/m/monocle-app.rb
@@ -2,13 +2,13 @@ cask "monocle-app" do
   version "3.0.2"
   sha256 "0cfa3ba106e5733afe53519df43edc0293e7abfce2e73ce9686a06a618028a5e"
 
-  url "https://heyiam.dk/updates/monocle/Monocle-#{version.dots_to_hyphens}.zip"
+  url "https://files.heyiam.dk/updates/monocle/Monocle-#{version.dots_to_hyphens}.zip"
   name "Monocle"
   desc "Window dimming utility"
   homepage "https://monocle.heyiam.dk/"
 
   livecheck do
-    url "https://heyiam.dk/updates/monocle/appcast.xml"
+    url "https://files.heyiam.dk/updates/monocle/appcast.xml"
     strategy :sparkle, &:short_version
   end
 
