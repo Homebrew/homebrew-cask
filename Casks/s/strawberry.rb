5df20686d0802fc1f8ffa23fffa3f53960f57eee
abbmarand <markusndrssn@gmail.com>
2026-01-07 18:12:51 +0100

    strawberry: update download URL domain

--- Casks/s/strawberry.rb
+++ Casks/s/strawberry.rb
@@ -2,14 +2,14 @@ cask "strawberry" do
   version "0.0.75"
   sha256 "aede8d30626561b74fd95f19b5cf12c0be84386d29e6df6223b89da8d23edb88"
 
-  url "https://strawberrybucket.xyz/strawberry-#{version}.dmg",
-      verified: "strawberrybucket.xyz/"
+  url "https://strawberrybucket.com/strawberry-#{version}.dmg",
+      verified: "strawberrybucket.com/"
   name "Strawberry"
   desc "AI-powered web browser"
   homepage "https://strawberrybrowser.com/"
 
   livecheck do
-    url "https://strawberrybucket.xyz/latest-mac.yml"
+    url "https://strawberrybucket.com/latest-mac.yml"
     strategy :electron_builder
   end
 
