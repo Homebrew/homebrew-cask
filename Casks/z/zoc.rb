2edb7954b54a68376cca941168affc181c6c7921
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-08 13:59:25 +0000

    zoc 9.02.5

--- Casks/z/zoc.rb
+++ Casks/z/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "9.02.4"
-  sha256 "98bae92530d5e3978ba85e6162cd2b7277d869f76cac0f68f8f87b48667e322b"
+  version "9.02.5"
+  sha256 "d83a6349f8ec6c99d9500b03b876a4762e636c233e54e6e9238e1ea179b70afe"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
