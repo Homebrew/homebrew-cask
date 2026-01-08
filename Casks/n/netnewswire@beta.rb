0fa22c719600bdb1b373014989561282689bcbb8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-01-08 14:51:44 -0500

    update depends_on

--- Casks/n/netnewswire@beta.rb
+++ Casks/n/netnewswire@beta.rb
@@ -15,7 +15,7 @@ cask "netnewswire@beta" do
 
   auto_updates true
   conflicts_with cask: "netnewswire"
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :tahoe"
 
   app "NetNewsWire.app"
 
