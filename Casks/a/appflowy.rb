b95e7786f3b9bd174c903f46fcedd317fabc975d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 12:45:21 +0000

    appflowy 0.10.9

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,9 +1,9 @@
 cask "appflowy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.10.8"
-  sha256 arm:   "f475de4b249ff9eeb0343db699f4860989a42b285cf3c027c067afbc48b1686b",
-         intel: "c8c6cc6657ce3d854a00d45b6c4559c7c07dda1ee856ade270580bae1be9a9d5"
+  version "0.10.9"
+  sha256 arm:   "9d14392384f90954680fb6ec32989e07cfaae7d481c5eb324e9167a602e76595",
+         intel: "50539c9ba863d2af1334c4f9616fb908e8ed3fb02c9b6d089f4f5a8851c3a03b"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-#{arch}.dmg",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
