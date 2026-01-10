ce57bfcf57774f7de67b087117a677c6ec0fad22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-10 13:49:12 +0000

    zedis 0.1.7

--- Casks/z/zedis.rb
+++ Casks/z/zedis.rb
@@ -1,6 +1,6 @@
 cask "zedis" do
-  version "0.1.6"
-  sha256 "3a2886d3a5ecd0144c81a5919a688eb36604353cdb0c75379f19af0f2e10756f"
+  version "0.1.7"
+  sha256 "8f0c03e025c40c1b24f9acba53b91ef6378107ee602648a69cb375ce037917c1"
 
   url "https://github.com/vicanso/zedis/releases/download/v#{version}/Zedis-aarch64.dmg"
   name "Zedis"
