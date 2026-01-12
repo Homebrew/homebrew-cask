668903d947c4f396a1b2f8b86c808116a3e185e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-12 16:38:40 +0000

    obsidian 1.11.4

--- Casks/o/obsidian.rb
+++ Casks/o/obsidian.rb
@@ -1,6 +1,6 @@
 cask "obsidian" do
-  version "1.10.6"
-  sha256 "a7fbd87353d7802933a13e3d90f55c39114c419ac48d459a356bdab73c128a8a"
+  version "1.11.4"
+  sha256 "d025d210a3251b5655826d62bee276cb3870f940908a4cb0f36a346c94d62a30"
 
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg",
       verified: "github.com/obsidianmd/"
@@ -16,7 +16,7 @@ cask "obsidian" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Obsidian.app"
 
