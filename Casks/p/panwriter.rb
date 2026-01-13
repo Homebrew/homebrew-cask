d63adc58389fe32d4afe9cf2c66356308b4182cb
Bevan Kay <email@bevankay.me>
2026-01-13 20:41:17 +0800

    panwriter: update `depends_on`

--- Casks/p/panwriter.rb
+++ Casks/p/panwriter.rb
@@ -19,7 +19,7 @@ cask "panwriter" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on formula: "pandoc"
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "PanWriter.app"
 
