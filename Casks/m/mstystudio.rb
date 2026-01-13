1b0cc26439d037456ddaf090481e69cd679129c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-13 22:22:52 +0000

    mstystudio 2.3.0

--- Casks/m/mstystudio.rb
+++ Casks/m/mstystudio.rb
@@ -1,7 +1,7 @@
 cask "mstystudio" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.2.1"
+  version "2.3.0"
   sha256 :no_check
 
   url "https://next-assets.msty.studio/app/latest/mac/MstyStudio_#{arch}.dmg",
