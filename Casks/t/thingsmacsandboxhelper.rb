24f3f820dc2cdeba6cf2c6f29ba792c085913f0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-14 10:32:55 +0000

    thingsmacsandboxhelper 3.47

--- Casks/t/thingsmacsandboxhelper.rb
+++ Casks/t/thingsmacsandboxhelper.rb
@@ -1,6 +1,6 @@
 cask "thingsmacsandboxhelper" do
-  version "3.46"
-  sha256 "a8da1f5650f2e6d414f61903c260d964eb34e72b3aebf41f829b248b1867cd23"
+  version "3.47"
+  sha256 "d1e75ef442caf4701b1a870db335795f68a280ec49141aaf8f2d2463124de476"
 
   url "https://static.culturedcode.com/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
   name "Things Helper"
