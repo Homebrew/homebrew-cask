1cb9ade091ea19deae48a967c122f064a19c5f33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-14 07:25:06 +0000

    pareto-security 1.19.0

--- Casks/p/pareto-security.rb
+++ Casks/p/pareto-security.rb
@@ -1,6 +1,6 @@
 cask "pareto-security" do
-  version "1.18.0"
-  sha256 "43a21570dfc4e63ad13e05041dcdf2a89f14830ab8286f18aa6a0af3f12395d1"
+  version "1.19.0"
+  sha256 "1001be03db61f7ab56d7a909b6c2969d40c148b25a3d7362fbcfe9d80b939e8b"
 
   url "https://github.com/ParetoSecurity/pareto-mac/releases/download/#{version}/ParetoSecurity.dmg",
       verified: "github.com/ParetoSecurity/pareto-mac/"
