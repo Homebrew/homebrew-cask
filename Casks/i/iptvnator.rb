1e125a4243e7b3aa5b9d94e44ba835974da86317
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-14 19:20:25 +0000

    iptvnator 0.18.0

--- Casks/i/iptvnator.rb
+++ Casks/i/iptvnator.rb
@@ -1,9 +1,9 @@
 cask "iptvnator" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.17.1"
-  sha256 arm:   "fe80e4daea326ac34dc200910c7fafb8843e13cd3ba25371fe04aa74fecf7895",
-         intel: "378523ba7560ce053ae274fdca8f0e01bde8bcbfb1c630bd95d4755889b98fbf"
+  version "0.18.0"
+  sha256 arm:   "540d9fa8f1d0bcbafb1e9e6455756af8f058a1e3e3ad864a04ee501ac77704c0",
+         intel: "4482f67d2cc10d748e76e109c43aa447e7da4b6e0bc934f8aa1e8cbeec30001f"
 
   url "https://github.com/4gray/iptvnator/releases/download/v#{version}/iptvnator-#{version}-mac-#{arch}.dmg"
   name "IPTVnator"
