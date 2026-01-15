91fce1e2442761c1ea0301037b6bd138c166eaa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-15 01:24:18 +0000

    mailspring 1.17.0

--- Casks/m/mailspring.rb
+++ Casks/m/mailspring.rb
@@ -1,9 +1,9 @@
 cask "mailspring" do
   arch arm: "-AppleSilicon"
 
-  version "1.16.0"
-  sha256 arm:   "8dbb14f294af84f14a16322bfb66471133a284a2a8a9089998a43a786b4020a9",
-         intel: "e3e57723f3f7c9864d02effc49dcf3839f8cb9d710625c03f5ebf2ae0550d914"
+  version "1.17.0"
+  sha256 arm:   "908f2119aad36ee7be41fa3011c0565e4b03b5f45e757a71838d0eae47ec6608",
+         intel: "a5df6c7a0b823cc3be4e640d5a60cdad4a34fa85303d17e065f8886af53447e9"
 
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring#{arch}.zip",
       verified: "github.com/Foundry376/Mailspring/"
