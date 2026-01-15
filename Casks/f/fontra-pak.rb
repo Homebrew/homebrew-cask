ea73de04ab50fd9d9c7104dbdb99e6f2153ea5bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-15 10:33:34 +0000

    fontra-pak 2026.1.2

--- Casks/f/fontra-pak.rb
+++ Casks/f/fontra-pak.rb
@@ -1,6 +1,6 @@
 cask "fontra-pak" do
-  version "2026.1.1"
-  sha256 "17cafde35b6657e06d50459d1297a24dd17e8c0db5d3c64b85686714d0aa7cca"
+  version "2026.1.2"
+  sha256 "6db6d92907e86ca190a84c801a5ece1e3104c796697ba605b981dd5162aacc3e"
 
   url "https://github.com/fontra/fontra-pak/releases/download/#{version}/FontraPak-macOS.dmg",
       verified: "github.com/fontra/fontra-pak/"
