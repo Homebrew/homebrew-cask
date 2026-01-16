146bfed3934ec4d0f3af9d2f3baa585699686b34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-16 21:38:04 +0000

    akuity 0.26.2-0.20260116180725-a0e80213f416

--- Casks/a/akuity.rb
+++ Casks/a/akuity.rb
@@ -2,11 +2,11 @@ cask "akuity" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.26.2-0.20260114233947-f6f10a69cdd6"
-  sha256 arm:          "8dbb6e9dd7d0eaaf8787ecf242169288679050eb10fce4178db1c4fcb7145450",
-         intel:        "597fad63635fc54ade49e583b2777dda6c68b5098f0c85fc4b3f4dac48c9edfe",
-         arm64_linux:  "0cb4f01c666157df22c01c16bf87a378e0f6c9130651b65abe05051f87fa8424",
-         x86_64_linux: "d3c23f02ae89bd9343787f13c20bbfe87622647b7094090a8c28dbcfca89b69c"
+  version "0.26.2-0.20260116180725-a0e80213f416"
+  sha256 arm:          "0568cd3db0bd5602278b6e0f05acab61b310d6003eea349c235c322071d046e6",
+         intel:        "e57c72c16bafb9180af7167d3b02780e9f33514ac16eabb6c7c0db512050d579",
+         arm64_linux:  "a70ca527dfc2953beb5e5ac74c67c127be8f8507f71501126329fa91940d6db5",
+         x86_64_linux: "098ecb5c53b57fc4da85e5b5dd2b400c0653262f3784e2cd51ba27c0f4b0dce4"
 
   url "https://dl.akuity.io/akuity-cli/v#{version}/#{os}/#{arch}/akuity"
   name "Akuity"
