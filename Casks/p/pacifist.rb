8b6068e2069d94ead6e420f85b706fa77363d399
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-17 19:31:40 +0000

    pacifist 4.1.4

--- Casks/p/pacifist.rb
+++ Casks/p/pacifist.rb
@@ -1,6 +1,6 @@
 cask "pacifist" do
-  version "4.1.3"
-  sha256 "4b299bbb4724fe541a5597f30932463101409b0218641b72a5194ee5d5885a41"
+  version "4.1.4"
+  sha256 "d76e51f10a98460809c7f5711e7f44742a7cc7ed7372d7f1f4c14a8e41c249ea"
 
   url "https://www.charlessoft.com/pacifist_download/Pacifist_#{version}.dmg"
   name "Pacifist"
