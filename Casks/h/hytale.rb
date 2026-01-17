4566b994c42e6442f258b61b6e9e1ad2df25724d
Cayce House <cayce@caycehouse.com>
2026-01-17 01:48:35 -0500

    Update hytale livecheck and sha256, configure url to use versioned dmgs

--- Casks/h/hytale.rb
+++ Casks/h/hytale.rb
@@ -1,15 +1,17 @@
 cask "hytale" do
   version "2026.01.16-2e2291a"
-  sha256 :no_check
+  sha256 "a6b1fe58fbdb964368ca5b3556d44ffe7bd3c41006ba18c6f969b8cd5311cbe9"
 
-  url "https://launcher.hytale.com/builds/release/darwin/arm64/hytale-launcher-latest.dmg"
+  url "https://launcher.hytale.com/builds/release/darwin/arm64/hytale-launcher-#{version}.dmg"
   name "Hytale"
   desc "Official Hytale Launcher"
   homepage "https://hytale.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://launcher.hytale.com/version/release/launcher.json"
+    strategy :json do |json|
+      json["version"]
+    end
   end
 
   auto_updates true
