675e523874dd196f959390c95c1ddd53c82c4ce5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 13:05:35 +0000

    freetube 0.23.13

--- Casks/f/freetube.rb
+++ Casks/f/freetube.rb
@@ -13,9 +13,9 @@ cask "freetube" do
     end
   end
   on_big_sur :or_newer do
-    version "0.23.12"
-    sha256 arm:   "6b70398d0453186b0f2087ac684eebb0c17eb847c75f4ed4e1507cea6d8eed47",
-           intel: "93b4d0553653a7c243b7bf7fd875441b689ddc57430b5a6e88dcc2ba865eb100"
+    version "0.23.13"
+    sha256 arm:   "aad0886f11381f35fc5802e9133fe9fd4e99d92da61d4f565b894f693bd14905",
+           intel: "0954bed77c8b940fbfe9a2736396847acb68ec417948f9089bd085464d062e79"
 
     url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-beta-mac-#{arch}.dmg",
         verified: "github.com/FreeTubeApp/FreeTube/"
