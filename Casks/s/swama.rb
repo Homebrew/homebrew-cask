89b4ba039bc652f34665c14862625567ea269b56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 05:00:11 +0000

    swama 2.0.1

--- Casks/s/swama.rb
+++ Casks/s/swama.rb
@@ -1,6 +1,6 @@
 cask "swama" do
-  version "2.0.0"
-  sha256 "68c504efefb53765b823395912c251a3ac2cae682b36fc48ea4ea1ffe370c69d"
+  version "2.0.1"
+  sha256 "570ddd8bc03ed49394a3229c86eda3f40141c6e03b7497fc80fad9f359d9649b"
 
   url "https://github.com/Trans-N-ai/swama/releases/download/v#{version}/Swama.dmg"
   name "Swama"
