6809ee49d4286e71818e57ad2e43c73c914d224b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-01-19 18:01:23 -0500

    update depends_on

--- Casks/p/proxyman.rb
+++ Casks/p/proxyman.rb
@@ -13,7 +13,7 @@ cask "proxyman" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :ventura"
 
   app "Proxyman.app"
   binary "#{appdir}/Proxyman.app/Contents/MacOS/proxyman-cli"
