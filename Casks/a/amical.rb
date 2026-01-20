c029f74a2208c9bda981beca3f89e9b7ca04efbe
Bevan Kay <email@bevankay.me>
2026-01-20 22:17:28 +1100

    amical: various updates

--- Casks/a/amical.rb
+++ Casks/a/amical.rb
@@ -5,24 +5,32 @@ cask "amical" do
   sha256 arm:   "f97bb3891fab66be1e706c85d16a6dc9cd1f04a1e17975fe3c42811ce0df3c9e",
          intel: "f071ce95bdafdd44c42c9f8b640fa63e184fcf0cbae122f9b0bd570547500495"
 
-  url "https://github.com/amicalhq/amical/releases/download/v#{version}/Amical-#{version}-#{arch}.dmg"
+  url "https://github.com/amicalhq/amical/releases/download/v#{version}/Amical-#{version}-#{arch}.dmg",
+      verified: "github.com/amicalhq/amical/"
   name "Amical"
-  desc "AI dictation app - open source and local-first"
-  homepage "https://github.com/amicalhq/amical"
+  desc "AI dictation app"
+  homepage "https://amical.ai/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://update.electronjs.org/amicalhq/amical/darwin-#{arch}/0.0.0"
+    regex(/v?(\d+(?:\.\d+)+)/i)
+    strategy :json do |json, regex|
+      json["name"][regex, 1]
+    end
   end
 
+  auto_updates true
   depends_on macos: ">= :monterey"
 
   app "Amical.app"
 
   zap trash: [
     "~/Library/Application Support/Amical",
-    "~/Library/Caches/Amical",
-    "~/Library/Preferences/com.amical.app.plist",
-    "~/Library/Saved Application State/com.amical.app.savedState",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.amical.desktop.sfl*",
+    "~/Library/Caches/com.amical.desktop",
+    "~/Library/Caches/com.amical.desktop.ShipIt",
+    "~/Library/HTTPStorages/com.amical.desktop",
+    "~/Library/Logs/Amical",
+    "~/Library/Preferences/com.amical.desktop.plist",
   ]
 end
