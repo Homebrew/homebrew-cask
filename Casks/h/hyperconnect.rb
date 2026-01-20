9312292b793fc0a4d002906bd996b6bb018f3da1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-20 16:28:20 +0000

    hyperconnect 2.6.300

--- Casks/h/hyperconnect.rb
+++ Casks/h/hyperconnect.rb
@@ -1,6 +1,6 @@
 cask "hyperconnect" do
-  version "2.5.310"
-  sha256 "f80e8e64dc700132484fa83c8a3e5741b1a64b36ba7583afb138f2f564f5ce1b"
+  version "2.6.300"
+  sha256 "cb1c5b555743ed01d2c3154877154ea4e646ea2c73da84fa95efb0e46bbce039"
 
   url "https://cdn.cnbj1.fds.api.mi-img.com/mijia-ios-adhoc/hyperconnect/HyperConnect-#{version}.dmg",
       verified: "mi-img.com/mijia-ios-adhoc/hyperconnect/"
