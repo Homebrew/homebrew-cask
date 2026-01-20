1c940938019c791227d4baed02157d38dbd94fb6
Chris Bongers <chrisbongers@gmail.com>
2026-01-20 16:45:22 +0200

    Update gitfit.rb

--- Casks/g/gitfit.rb
+++ Casks/g/gitfit.rb
@@ -1,6 +1,6 @@
 cask "gitfit" do
-  version "1.1.0"
-  sha256 "a277bc997421fdf5a4da6ad228fe5fe472de9371c3115f9a3b62fad9aab34dcd"
+  version "1.2.0"
+  sha256 "3ae76e4c475bf81c495d13ba19591f405a7c07c767364ce68022a04835cf8d26"
 
   url "https://github.com/rebelchris/git-fit/releases/download/v#{version}/GitFit-#{version}.dmg",
       verified: "github.com/rebelchris/git-fit/"
