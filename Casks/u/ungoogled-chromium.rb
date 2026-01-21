9f98a3745ae5c48face11f6f4400a3e32700849f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 17:29:37 +0000

    ungoogled-chromium 144.0.7559.59-1.1

--- Casks/u/ungoogled-chromium.rb
+++ Casks/u/ungoogled-chromium.rb
@@ -1,9 +1,9 @@
 cask "ungoogled-chromium" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "143.0.7499.169-1.1"
-  sha256 arm:   "603eaa5b8677a35cd118ec26f3c92ccc5fdc2a86c2318e2b50e28cdbcb888bf1",
-         intel: "9233ed5042bca37a235701a9fdd6d62b79c0f230704aa7118df6c6512de17b2b"
+  version "144.0.7559.59-1.1"
+  sha256 arm:   "6b6acfacea51fb6e456f33f831f1bd085035df3c9185f1a934b37658991befe0",
+         intel: "6457043f91c656a7cc9f3a2396309aa495db0061ee6c77efed72d05527e30e3c"
 
   url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_#{arch}-macos.dmg",
       verified: "github.com/ungoogled-software/ungoogled-chromium-macos/"
