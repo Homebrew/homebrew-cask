02c7404074c656fa5e8329168a53ad9d2e1e02ec
Casper Rogild Storm <casper+github@rogildstorm.com>
2026-01-21 08:35:54 +0100

    halloy: 2026.1

--- Casks/h/halloy.rb
+++ Casks/h/halloy.rb
@@ -1,6 +1,6 @@
 cask "halloy" do
-  version "2025.12"
-  sha256 "2dc90e71ef17f9f88751fdbc101c96189840445bbbefc923a4e02e59c9f75fe7"
+  version "2026.1"
+  sha256 "a593a18592e03ebd1f9d5dce35c7868cb9ab09427349ef147152ec353d72a22b"
 
   url "https://github.com/squidowl/halloy/releases/download/#{version}/halloy.dmg",
       verified: "github.com/squidowl/halloy/"
