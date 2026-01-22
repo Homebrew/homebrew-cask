4f87c2b507d5bedf33e48f2dc89397719c2a2bbd
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-coptic: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-coptic.rb
+++ Casks/font/font-n/font-noto-sans-coptic.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-coptic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansCoptic-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanscoptic/NotoSansCoptic-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Coptic"
-  homepage "https://fonts.google.com/#sans-copt"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Coptic"
 
   font "NotoSansCoptic-Regular.ttf"
 
