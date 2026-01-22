422328f0ef3b1f6c8d1cb8e4fcda52d16d0d70fd
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-naskh-arabic: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-naskh-arabic.rb
+++ Casks/font/font-n/font-noto-naskh-arabic.rb
@@ -2,13 +2,12 @@ cask "font-noto-naskh-arabic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoNaskhArabic-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notonaskharabic/NotoNaskhArabic%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Naskh Arabic"
-  homepage "https://font.google.com"
+  homepage "https://fonts.google.com/specimen/Noto+Naskh+Arabic"
 
-  font "NotoNaskhArabic-Bold.ttf"
-  font "NotoNaskhArabic-Regular.ttf"
+  font "NotoNaskhArabic[wght].ttf"
 
   # No zap stanza required
 end
