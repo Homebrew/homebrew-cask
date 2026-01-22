831e4f89e1ef114048a058c9070eb1b9a455f66e
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:01 +0000

    font-noto-sans-tagbanwa: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-tagbanwa.rb
+++ Casks/font/font-n/font-noto-sans-tagbanwa.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-tagbanwa" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansTagbanwa-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanstagbanwa/NotoSansTagbanwa-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Tagbanwa"
-  homepage "https://fonts.google.com/#sans-tagb"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Tagbanwa"
 
   font "NotoSansTagbanwa-Regular.ttf"
 
