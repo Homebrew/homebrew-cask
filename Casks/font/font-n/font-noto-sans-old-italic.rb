dda637c46321de9e5ceb5dcc54b34c285322c6bb
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:01 +0000

    font-noto-sans-old-italic: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-old-italic.rb
+++ Casks/font/font-n/font-noto-sans-old-italic.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-old-italic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansOldItalic-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansolditalic/NotoSansOldItalic-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Old Italic"
-  homepage "https://fonts.google.com/#sans-ital"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Old+Italic"
 
   font "NotoSansOldItalic-Regular.ttf"
 
