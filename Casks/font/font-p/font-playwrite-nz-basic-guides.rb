5f45df06643b920722135a119abb82cc0dc7dad3
Klaus Hipp <9824526+khipp@users.noreply.github.com>
2026-01-22 08:03:40 +0000

    font-playwrite-nz-basic-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-p/font-playwrite-nz-basic-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-nz-basic-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwritenzbasicguides/PlaywriteNZBasicGuides-Regular.ttf"
+  name "Playwrite NZ Basic Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteNZBasicGuides-Regular.ttf"
+
+  # No zap stanza required
+end
