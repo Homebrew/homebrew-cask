833d6255bf499e7942dddb02922886667338e36d
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-egyptian-hieroglyphs: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-egyptian-hieroglyphs.rb
+++ Casks/font/font-n/font-noto-sans-egyptian-hieroglyphs.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-egyptian-hieroglyphs" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansEgyptianHieroglyphs-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansegyptianhieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Egyptian Hieroglyphs"
-  homepage "https://fonts.google.com/#sans-egyp"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Egyptian+Hieroglyphs"
 
   font "NotoSansEgyptianHieroglyphs-Regular.ttf"
 
