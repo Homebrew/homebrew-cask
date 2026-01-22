dda6a83033b325b926267e735631b9a3c4ef9ec7
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-cuneiform: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-cuneiform.rb
+++ Casks/font/font-n/font-noto-sans-cuneiform.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-cuneiform" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansCuneiform-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanscuneiform/NotoSansCuneiform-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Cuneiform"
-  homepage "https://fonts.google.com/#sans-xsux"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Cuneiform"
 
   font "NotoSansCuneiform-Regular.ttf"
 
