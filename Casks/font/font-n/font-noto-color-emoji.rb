634a14c2734edd11e6872a8b49b8a89bd2db9da8
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-color-emoji: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-color-emoji.rb
+++ Casks/font/font-n/font-noto-color-emoji.rb
@@ -2,12 +2,12 @@ cask "font-noto-color-emoji" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoColorEmoji-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notocoloremoji/NotoColorEmoji-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Color Emoji"
-  homepage "https://fonts.google.com/#emoji-zsye-color"
+  homepage "https://fonts.google.com/specimen/Noto+Color+Emoji"
 
-  font "NotoColorEmoji.ttf"
+  font "NotoColorEmoji-Regular.ttf"
 
   # No zap stanza required
 end
