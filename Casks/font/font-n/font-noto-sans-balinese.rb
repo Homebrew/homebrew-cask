08858bfc42ab8c8aa60dcecff10daa523a7fa338
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-balinese: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-balinese.rb
+++ Casks/font/font-n/font-noto-sans-balinese.rb
@@ -2,12 +2,12 @@ cask "font-noto-sans-balinese" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansBalinese-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansbalinese/NotoSansBalinese%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Balinese"
-  homepage "https://font.google.com"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Balinese"
 
-  font "NotoSansBalinese-Regular.ttf"
+  font "NotoSansBalinese[wght].ttf"
 
   # No zap stanza required
 end
