0ef2fefabfbece030f9a4d5836459db822243f2b
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-kharoshthi: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-kharoshthi.rb
+++ Casks/font/font-n/font-noto-sans-kharoshthi.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-kharoshthi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansKharoshthi-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanskharoshthi/NotoSansKharoshthi-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Kharoshthi"
-  homepage "https://fonts.google.com/#sans-khar"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Kharoshthi"
 
   font "NotoSansKharoshthi-Regular.ttf"
 
