09156c1ee52d6f9fe59c788f0e3f5b2ef69d88a5
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:01 +0000

    font-noto-sans-ol-chiki: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-ol-chiki.rb
+++ Casks/font/font-n/font-noto-sans-ol-chiki.rb
@@ -2,12 +2,12 @@ cask "font-noto-sans-ol-chiki" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansOlChiki-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansolchiki/NotoSansOlChiki%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Ol Chiki"
-  homepage "https://fonts.google.com/#sans-olck"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Ol+Chiki"
 
-  font "NotoSansOlChiki-Regular.ttf"
+  font "NotoSansOlChiki[wght].ttf"
 
   # No zap stanza required
 end
