d2dbf8f923d8f1e758cb29a8daa43791dbe8194f
Bevan Kay <email@bevankay.me>
2026-01-22 13:05:00 +0000

    font-noto-sans-brahmi: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-n/font-noto-sans-brahmi.rb
+++ Casks/font/font-n/font-noto-sans-brahmi.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-brahmi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/pkgs/NotoSansBrahmi-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansbrahmi/NotoSansBrahmi-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Brahmi"
-  homepage "https://font.google.com"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+Brahmi"
 
   font "NotoSansBrahmi-Regular.ttf"
 
