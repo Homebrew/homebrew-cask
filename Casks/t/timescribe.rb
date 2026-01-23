d3128deb88b43581364815a2dfed149d9cf1540d
WINBIGFOX <winbigfox@t-online.de>
2026-01-23 12:42:25 +0100

    timescribe 1.9.0

--- Casks/t/timescribe.rb
+++ Casks/t/timescribe.rb
@@ -1,9 +1,9 @@
 cask "timescribe" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.0"
-  sha256 arm:   "8c7f7e9e754244e6d68760302fc294c4873fc331333535b443f5302d588ff34d",
-         intel: "bceacbf8374af73af4fbf1e176f086b7a454fbb27b9e5672fc7d97f5637b30f2"
+  version "1.9.0"
+  sha256 arm:   "94d22018d7ddc449d4632c11864c74d6bc39d600c45acb4e12a10ea68d8f7a17",
+         intel: "196da6b934803544d509eebd9e27e6db8d92dec2335c4f4838ef5d03ac703899"
 
   url "https://github.com/WINBIGFOX/TimeScribe/releases/download/v#{version}/TimeScribe-#{version}-#{arch}.zip",
       verified: "github.com/WINBIGFOX/TimeScribe/"
