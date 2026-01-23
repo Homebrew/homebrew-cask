cc94ff55a10fcfaad14a1a9cedc6397e6a64c503
Goooler <wangzongler@gmail.com>
2026-01-23 14:31:28 +0800

    Polish livecheck URLs for JetBrains EAP
    Unify them to the formats like:
    
    ```
    https://data.services.jetbrains.com/products/releases?code=II&latest=true&type=eap
    ```
    
    refs https://github.com/Homebrew/homebrew-cask/blob/627a7bd4e1d236fa89511ab07aaed22ad73822bb/Casks/i/intellij-idea.rb#L14.

--- Casks/c/clion@eap.rb
+++ Casks/c/clion@eap.rb
@@ -11,7 +11,7 @@ cask "clion@eap" do
   homepage "https://www.jetbrains.com/clion/nextversion"
 
   livecheck do
-    url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&release.type=eap"
+    url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=eap"
     strategy :json do |json|
       json["CL"]&.map do |release|
         version = release["version"]
