11bbb30de0dd4343667c0cbe8f1f828d0d002012
CI Auto Tagger <ci@example.com>
2026-01-23 15:35:37 +0000

    EDFbrowser: updated cask

--- Casks/e/edfbrowser.rb
+++ Casks/e/edfbrowser.rb
@@ -1,13 +1,14 @@
 cask "edfbrowser" do
   version "2.14"
-  sha256 :no_check
-  url "https://gitlab.com/sjg2203/edfbrowser-silicon/-/jobs/artifacts/v#{version}/raw/build_output/EDFbrowser-v#{version}.dmg?job=package_dmg"
+  sha256 "1b5651c6f09dd3186e9c3007466ac9d66b4066eff137ea76cb6b7e37fba072f8"
+
+  url "https://gitlab.com/sjg2203/edfbrowser-silicon/-/releases/v#{version}/downloads/EDFbrowser-v#{version}.dmg"
   name "EDFbrowser"
   desc "EDF+ and BDF+ viewer and toolbox – Apple Silicon build"
   homepage "https://gitlab.com/sjg2203/edfbrowser-silicon"
 
   livecheck do
-    url "https://gitlab.com/sjg2203/edfbrowser-silicon.git"
+    url :homepage
     strategy :git
   end
 
