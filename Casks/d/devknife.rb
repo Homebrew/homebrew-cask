029cc9699fb4bb8256dd0fe759a8c14f12a1d7d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-23 18:56:26 +0000

    devknife 1.9.1

--- Casks/d/devknife.rb
+++ Casks/d/devknife.rb
@@ -1,6 +1,6 @@
 cask "devknife" do
-  version "1.9.0"
-  sha256 "7d62b28202325d2fbd5f1e9ea2ceb9664c674c50cb9c524947e97dffdba2d630"
+  version "1.9.1"
+  sha256 "ce054512db7f77d730b419787ee9f0de4c642bfc22c372775d62c080e7592e47"
 
   url "https://files.solotuna.com/devknife/DevKnife-#{version}.dmg",
       verified: "files.solotuna.com/devknife/"
