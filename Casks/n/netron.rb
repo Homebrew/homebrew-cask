06656ef7ada123691d6b934cf5a42c6dc32c33cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-23 19:24:42 +0000

    netron 8.8.4

--- Casks/n/netron.rb
+++ Casks/n/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "8.8.3"
-  sha256 "1e19b8546d55b74d0f04aad361ac60e32e53eed8b7112c287c0289a51bc599f7"
+  version "8.8.4"
+  sha256 "5c97791bfbb0444e0bab49b642ab5a2cd03e02b5b0d5917effb6495778907d17"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
