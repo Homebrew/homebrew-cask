2ef23652f4d1b321d0cdf1b641a360ebd491c12d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-24 18:45:20 +0000

    bloom 1.5.24

--- Casks/b/bloom.rb
+++ Casks/b/bloom.rb
@@ -1,6 +1,6 @@
 cask "bloom" do
-  version "1.5.23"
-  sha256 "ae827ce36b6ad9251f47ef490950b596042b145720ff6ba46df5f5be9e937f6f"
+  version "1.5.24"
+  sha256 "759dd1a3ffafea771805348374400f4ce913ef4be3dfba95bc294b62d6b3fbad"
 
   url "https://bloomapp.club/downloads/bloom/Bloom-v#{version}.dmg",
       referer: "https://bloomapp.club"
