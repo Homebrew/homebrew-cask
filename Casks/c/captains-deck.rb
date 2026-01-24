4f5ec0de8b1829fa1cce702aec14790b97e737b9
jupe69 <178208403+jupe69@users.noreply.github.com>
2026-01-24 06:38:45 +0200

    Add Cask for Captain's Deck version 1.0.8

---
+++ Casks/c/captains-deck.rb
@@ -0,0 +1,27 @@
+cask "captains-deck" do                                                                                                                                                   
+    version "1.0.8"                                                                                                                                                         
+    sha256 "7375e0ea351c1f9d0f7bd07eeec342e57ea2728126666b02de949d88185e6772"                                                                                               
+                                                                                                                                                                            
+    url "https://captains-deck.com/downloads/CaptainsDeck.dmg"                                                                                                              
+    name "Captain's Deck"                                                                                                                                                   
+    desc "Dual-pane file manager for macOS inspired by Norton Commander"                                                                                                    
+    homepage "https://captains-deck.com/"                                                                                                                                   
+                                                                                                                                                                            
+    livecheck do                                                                                                                                                            
+      url "https://captains-deck.com/appcast.xml"                                                                                                                           
+      strategy :sparkle                                                                                                                                                     
+    end                                                                                                                                                                     
+                                                                                                                                                                            
+    auto_updates true                                                                                                                                                       
+    depends_on macos: ">= :sonoma"                                                                                                                                          
+                                                                                                                                                                            
+    app "Captain's Deck.app"                                                                                                                                                
+                                                                                                                                                                            
+    zap trash: [                                                                                                                                                            
+      "~/Library/Application Support/Captain's Deck",                                                                                                                       
+      "~/Library/Caches/com.captainsdeck.app",                                                                                                                              
+      "~/Library/HTTPStorages/com.captainsdeck.app",                                                                                                                        
+      "~/Library/Preferences/com.captainsdeck.app.plist",                                                                                                                   
+      "~/Library/Saved Application State/com.captainsdeck.app.savedState",                                                                                                  
+    ]                                                                                                                                                                       
+  end
