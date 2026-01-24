0285a0ad23b8c4b8339a0216382c72f5002ae455
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-24 21:43:18 +0000

    claude 1.1.886

--- Casks/c/claude.rb
+++ Casks/c/claude.rb
@@ -1,6 +1,6 @@
 cask "claude" do
-  version "1.1.799,2e02b656cbbb1f14a5a81a4ed79b1c5ea1427507"
-  sha256 "9bdf703a569cc19d356c8c0d6823ee3140cb94cf3d21a57386ae7e339252f04b"
+  version "1.1.886,ef5d267b24cd6ead766647763ca5bcfae49c823b"
+  sha256 "18df72ba1283a4194a7095734a73a58e19565b130f4b669c90bca86a25b26f48"
 
   url "https://downloads.claude.ai/releases/darwin/universal/#{version.csv.first}/Claude-#{version.csv.second}.zip",
       verified: "downloads.claude.ai/releases/darwin/universal/"
