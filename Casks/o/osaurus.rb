e6fe793f7ea673bb9762873a9016d3879cac1b10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 01:37:21 +0000

    osaurus 0.8.5

--- Casks/o/osaurus.rb
+++ Casks/o/osaurus.rb
@@ -1,6 +1,6 @@
 cask "osaurus" do
-  version "0.8.4"
-  sha256 "06d5afc77d43d89a4ea427426416a3243105c1a46835375b1b574c69fad63607"
+  version "0.8.5"
+  sha256 "8e36955479cccdf4f506eb8d53369a1986ed6a8b213aedfb37bd5e4569bc1a23"
 
   url "https://github.com/dinoki-ai/osaurus/releases/download/#{version}/Osaurus-#{version}.dmg",
       verified: "github.com/dinoki-ai/osaurus/"
