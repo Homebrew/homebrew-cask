7a620facb7c42c8486d5ac0e608dbe9b39477749
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2026-01-26 17:56:27 +0300

    Fix cask url style

--- Casks/i/ipaverse.rb
+++ Casks/i/ipaverse.rb
@@ -2,7 +2,7 @@ cask "ipaverse" do
   version "1.0.0"
   sha256 "ca79f788f14acf9b5be9dac1f18981e3d6e26801f7ab3767e4f66f09974a2b87"
 
-  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg", verified: "github.com/bahattinkoc/ipaverse/"
+  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg"
   name "ipaverse"
   desc "Tool for downloading and managing iOS apps from the App Store"
   homepage "https://github.com/bahattinkoc/ipaverse"
