a9ec6ecd70aa07809313ffa31d546aa08a0cbf7d
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2026-01-26 17:34:18 +0300

    fixed sha256 and url

--- Casks/i/ipaverse.rb
+++ Casks/i/ipaverse.rb
@@ -1,8 +1,8 @@
 cask "ipaverse" do
   version "1.0.0"
-  sha256 "e3e28f60eb9bfc000977e6e5f0e1b455854daebd034770567eefae537e15d4f4"
+  sha256 "ca79f788f14acf9b5be9dac1f18981e3d6e26801f7ab3767e4f66f09974a2b87"
 
-  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg"
+  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg", verified: "github.com/bahattinkoc/ipaverse/"
   name "ipaverse"
   desc "Tool for downloading and managing iOS apps from the App Store"
   homepage "https://github.com/bahattinkoc/ipaverse"
