dff12c9c0ae7f1fffb07f72c8677b49893928d5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 16:42:00 +0000

    ireal-pro 2026.1.3

--- Casks/i/ireal-pro.rb
+++ Casks/i/ireal-pro.rb
@@ -22,8 +22,8 @@ cask "ireal-pro" do
     end
   end
   on_monterey :or_newer do
-    version "2026.1,20260101"
-    sha256 "a63ca6aeef57092e2b734b213c54629918398b24380c263d16c9476fcc39cef8"
+    version "2026.1.3,20260131"
+    sha256 "957a7259d4e6e256bfc41ec002a59f588c1d8bc3ddf47894abd3b8595bfd5641"
 
     url "https://ireal-pro.s3.amazonaws.com/iReal%20Pro-#{version.csv.first}-#{version.csv.second}.dmg",
         verified: "ireal-pro.s3.amazonaws.com/"
