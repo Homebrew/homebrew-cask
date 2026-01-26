60a938668505caca1c1de83cde8db9d84a120643
iamrajiv <rajivperfect007@gmail.com>
2026-01-27 05:12:29 +0530

    fix(keybase): use DMG download URLs instead of broken ZIP URLs
    The versioned ZIP URLs on S3 return 403 Forbidden. Switch to the stable
    DMG download URLs which are publicly accessible:
    - Intel: https://prerelease.keybase.io/Keybase.dmg
    - ARM64: https://prerelease.keybase.io/Keybase-arm64.dmg

--- Casks/k/keybase.rb
+++ Casks/k/keybase.rb
@@ -3,14 +3,15 @@ cask "keybase" do
 
   on_arm do
     version "6.5.4,20250917154415,52400b6f28"
-    sha256 "6e76006267d80b40e182d3110bb18c940a05aca41b9bb29a2a4ccc64967f8745"
+    url "https://prerelease.keybase.io/Keybase-arm64.dmg"
   end
   on_intel do
     version "6.5.4,20250917153314,52400b6f28"
-    sha256 "4dc3a84c3ba33467928b5c340d2296ad6d368009b47f4e24cb8baa75ae90da44"
+    url "https://prerelease.keybase.io/Keybase.dmg"
   end
 
-  url "https://prerelease.keybase.io/darwin-#{arch}updates/Keybase-#{version.csv.first}-#{version.csv.second}%2B#{version.csv.third}.zip"
+  sha256 :no_check
+
   name "Keybase"
   desc "End-to-end encryption software"
   homepage "https://keybase.io/"
