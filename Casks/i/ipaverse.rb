9fe2172a1bef9a0ec703aa458fe95e31c909eab3
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2026-01-26 17:48:30 +0300

    ipaverse 1.0.0 (new cask)

---
+++ Casks/i/ipaverse.rb
@@ -0,0 +1,19 @@
+cask "ipaverse" do
+  version "1.0.0"
+  sha256 "ca79f788f14acf9b5be9dac1f18981e3d6e26801f7ab3767e4f66f09974a2b87"
+
+  url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg", verified: "github.com/bahattinkoc/ipaverse/"
+  name "ipaverse"
+  desc "Tool for downloading and managing iOS apps from the App Store"
+  homepage "https://github.com/bahattinkoc/ipaverse"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ipaverse.app"
+
+  zap trash: [
+    "~/Library/Application Support/ipaverse",
+    "~/Library/Caches/ipaverse",
+    "~/Library/Preferences/com.ipaverse.plist",
+  ]
+end
