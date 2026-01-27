c959d3b94a86fd460873ddca58a1a2a7f26e542d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 22:50:06 +0000

    workflowy 4.3.2601271641

--- Casks/w/workflowy.rb
+++ Casks/w/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "4.3.2601230747"
-  sha256 "56f18d34e0592436a82d51d47d7dd92a61e32196687f1728963f6d38760293c8"
+  version "4.3.2601271641"
+  sha256 "577d2b5d25e5b86e7fa683b6612db787ae9879bcf54d039aab4b078240fcd191"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
