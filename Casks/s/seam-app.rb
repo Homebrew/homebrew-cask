b0c049ce9ed10fa423d266c4426f43023533281a
heyramzi <hello@getseam.app>
2026-01-27 00:22:39 +0000

    seam-app 0.0.5 (new cask)

---
+++ Casks/s/seam-app.rb
@@ -0,0 +1,28 @@
+cask "seam-app" do
+  version "0.0.5"
+  sha256 "833844229459d22357acbd2a12972f540403e14019592ca2668faa22bda293ae"
+
+  url "https://releases.getseam.app/#{version}/Seam.dmg"
+  name "Seam"
+  desc "Dynamic Island with system HUDs and notifications"
+  homepage "https://getseam.app/"
+
+  livecheck do
+    url "https://releases.getseam.app/latest/release.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+  depends_on arch: :arm64
+
+  app "Seam.app"
+
+  zap trash: [
+    "~/Library/Caches/app.seam",
+    "~/Library/Logs/Seam",
+    "~/Library/Preferences/seam.app.plist",
+  ]
+end
