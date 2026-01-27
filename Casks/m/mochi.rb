eed53e1c25bc7ae2ccbef67146461c83ca363204
aashish2057 <aashish2057@gmail.com>
2026-01-27 16:59:21 -0600

    mochi: fix 1.20.8 sha256

--- Casks/m/mochi.rb
+++ Casks/m/mochi.rb
@@ -2,8 +2,8 @@ cask "mochi" do
   arch arm: "-arm64"
 
   version "1.20.8"
-  sha256 arm:   "a4d57699732d0fd3a594aa2935b3efab4c57808b8b0df98962c5615ab8c39c87",
-         intel: "989ecbc36bd5da9ed065584b3eb46666cad808d64d4215fa585cf1931e0cb580"
+  sha256 arm:   "714ec8c00bf2a9995b125f03201f97bb053155abb2be8871f04d906303482089",
+         intel: "92afffd256d47e6ec62b4241d0b70081f6d3b48a41a4510b469ac982a3cdb7b3"
 
   url "https://download.mochi.cards/releases/Mochi-#{version}#{arch}.dmg"
   name "Mochi"
