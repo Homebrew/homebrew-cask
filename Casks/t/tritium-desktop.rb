e6d50ca4f037052e908a7a6999793595cb8a6147
Drew Miller <drew@tritium.legal>
2026-01-27 09:41:24 -0800

    Adding Tritium desktop application

---
+++ Casks/t/tritium-desktop.rb
@@ -0,0 +1,20 @@
+cask "tritium-desktop" do
+  version "0.2.25"
+
+  on_arm do
+    sha256 "c13d46084897237235f5be3c3c331b007d78d493f9ddf546d1b57b500d12bc7c"
+
+    url "https://tritium.legal/static/releases/tritium-macos-arm64.#{version}.zip"
+  end
+  on_intel do
+    sha256 "e9639e3c4681ce85f852fbac48e2eeee5ba51296dbfec57c200d59b76237ab80"
+
+    url "https://tritium.legal/static/releases/tritium-macos-x64.#{version}.zip"
+  end
+
+  name "Tritium"
+  desc "Integrated drafting environment for legal professionals"
+  homepage "https://tritium.legal/"
+
+  app "tritium.app"
+end
