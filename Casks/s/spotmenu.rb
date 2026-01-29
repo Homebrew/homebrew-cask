697566fa523143e424a0c0ebf48485599a598c96
Patrick Linnane <patrick@linnane.io>
2026-01-29 09:42:14 -0800

    spotmenu: lower minimum macOS version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/spotmenu.rb
+++ Casks/s/spotmenu.rb
@@ -15,7 +15,7 @@ cask "spotmenu" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
-  depends_on macos: ">= :tahoe"
+  depends_on macos: ">= :ventura"
 
   app "SpotMenu.app"
 
