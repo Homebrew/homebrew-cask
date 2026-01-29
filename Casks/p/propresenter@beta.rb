64f4db6dc95319e1a26cf444195596aab91239b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-29 22:31:23 +0000

    propresenter@beta 21.2,352452642

--- Casks/p/propresenter@beta.rb
+++ Casks/p/propresenter@beta.rb
@@ -34,8 +34,8 @@ cask "propresenter@beta" do
     end
   end
   on_ventura :or_newer do
-    version "21.2,352452641"
-    sha256 "8673dcd835741243666e5f49193885e681845c96dbb6dd64681cf37354f1a376"
+    version "21.2,352452642"
+    sha256 "97625b6c532fc044c225a44f6c2cfc02b80a1abfb07b160070dc2597bc05e11b"
 
     url "https://renewedvision.com/downloads//propresenter/mac/ProPresenter_#{version.csv.first}_#{version.csv.second}.zip"
 
