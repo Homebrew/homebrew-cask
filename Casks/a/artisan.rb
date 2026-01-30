eedd88f81371a4135736b496b45c9cb543d51a79
Sam Ford <1584702+samford@users.noreply.github.com>
2026-01-29 22:11:41 -0500

    artisan 4.0.0
    `artisan` is autobumped but the workflow failed to update to version
    4.0.0 because the dmg files are now split based on architecture.
    Besides that, the ARM app requires macOS Sonoma or higher but the
    Intel app still only requires Ventura, so this fails `brew audit`
    anyway. This updates the version and adjusts the cask accordingly.

--- Casks/a/artisan.rb
+++ Casks/a/artisan.rb
@@ -1,8 +1,18 @@
 cask "artisan" do
-  version "3.4.0"
-  sha256 "18acf891e582e475a908295c027870f40b78c8f1f41345b4e34dbf15be511f5b"
+  arch arm: "arm", intel: "intel"
 
-  url "https://github.com/artisan-roaster-scope/artisan/releases/download/v#{version}/artisan-mac-#{version}.dmg",
+  version "4.0.0"
+  sha256 arm:   "068c4223d4bdb3d82dd3b348c1b84f03de5b0ea9382545ad64752477a278ebf2",
+         intel: "2fc4ad31cebfb6bb00aeb0c2f4adf834f64084cf809840d6c5957e81a4824f3f"
+
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :ventura"
+  end
+
+  url "https://github.com/artisan-roaster-scope/artisan/releases/download/v#{version}/artisan-mac-#{arch}-#{version}.dmg",
       verified: "github.com/artisan-roaster-scope/artisan/"
   name "Artisan"
   desc "Visual scope for coffee roasters"
@@ -12,8 +22,6 @@ cask "artisan" do
     url :url
     strategy :github_latest
   end
-
-  depends_on macos: ">= :ventura"
 
   app "Artisan.app"
 
