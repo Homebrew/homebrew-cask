7e51cba1da15e03709b573a8f59f2064d5693995
Sam Ford <1584702+samford@users.noreply.github.com>
2026-01-29 22:48:33 -0500

    lobehub 2.0.9
    `lobehub` is autobumped but the workflow failed to update to stable
    2.x versions because the file (and app) name doesn't include the
    "-Beta" suffix now but also some of the the releases on GitHub don't
    have any release assets. This updates the version and adds a
    `livecheck` block that uses the `GithubReleases` strategy, as it's
    necessary to go through recent releases to find the newest version
    that has the files the cask uses.

--- Casks/l/lobehub.rb
+++ Casks/l/lobehub.rb
@@ -1,24 +1,38 @@
 cask "lobehub" do
   arch arm: "-arm64"
 
-  version "1.143.2"
-  sha256 arm:   "695d687e62debcaba8c1a20156d7155b4434ca54a269beaed93166ab7468479d",
-         intel: "62dba5fb0bd457d04df1ed67e675b0e72a217ab61e3ee0c0edd1567799c51d71"
+  version "2.0.9"
+  sha256 arm:   "84ecbff254f1d648aafa2b5910c02450a3fe54a9c314ff9febd3a4a71fda428a",
+         intel: "63551a2deeee020b41bf5e6001dae4e1f5176174e8ea97741799131d566e065c"
 
-  url "https://github.com/lobehub/lobe-chat/releases/download/v#{version}/LobeHub-Beta-#{version}#{arch}-mac.zip"
+  url "https://github.com/lobehub/lobe-chat/releases/download/v#{version}/LobeHub-#{version}#{arch}-mac.zip"
   name "LobeHub"
   desc "AI chat framework"
   homepage "https://github.com/lobehub/lobe-chat"
 
+  # Not every release on GitHub has assets, so we have to find the newest one
+  # with the files the cask uses.
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/LobeHub[._-]v?(\d+(?:\.\d+)+)#{arch}[._-]mac\.zip/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["browser_download_url"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
   end
 
   auto_updates true
   depends_on macos: ">= :monterey"
 
-  app "LobeHub-Beta.app"
+  app "LobeHub.app"
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lobehub.lobehub-desktop-beta.sfl*",
