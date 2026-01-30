552680c5c1849820f0ef27b7886c121896eb6588
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 07:30:55 +0000

    postman 11.82.5

--- Casks/p/postman.rb
+++ Casks/p/postman.rb
@@ -1,9 +1,9 @@
 cask "postman" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "11.82.1"
-  sha256 arm:   "ee7ffd7b33e0a689f70044179a82bf89acce3ef461fe90d5cbf601554c236e42",
-         intel: "8cde936cf4641051fd2b2537a3072cad66c9bf222a313d1ff3357d3b29a23736"
+  version "11.82.5"
+  sha256 arm:   "280c9a5e4a97d0902d6016860b473a5a4ff1a63247522af343892d7fd6cc6582",
+         intel: "d18aedd1f8068f59e9aab440a3923dd9a6611e1db292ecb4d23b950fda770938"
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl.pstmn.io/download/version/"
