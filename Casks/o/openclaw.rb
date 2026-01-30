e48f6c5850819e7bdf338ec5f7953a089e5156f7
Klaus Hipp <khipp@users.noreply.github.com>
2026-01-30 07:29:30 +0100

    clawdbot: rename to openclaw

---
+++ Casks/o/openclaw.rb
@@ -0,0 +1,23 @@
+cask "openclaw" do
+  version "2026.1.29"
+  sha256 "e22b1ed9cb96bfe4247542fc995b8f9fc252cf399526968b48e56d035ef9798d"
+
+  url "https://github.com/openclaw/openclaw/releases/download/v#{version}/OpenClaw-#{version}.dmg",
+      verified: "github.com/openclaw/openclaw/"
+  name "OpenClaw"
+  desc "Personal AI assistant"
+  homepage "https://openclaw.ai/"
+
+  depends_on macos: ">= :sequoia"
+
+  app "OpenClaw.app"
+
+  zap trash: [
+    "~/.openclaw",
+    "~/Library/Application Support/OpenClaw",
+    "~/Library/HTTPStorages/bot.molt.mac",
+    "~/Library/Logs/DiagnosticReports/OpenClaw*",
+    "~/Library/Preferences/bot.molt.mac.plist",
+    "~/Library/Preferences/bot.molt.shared.plist",
+  ]
+end
