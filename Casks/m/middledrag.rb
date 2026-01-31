ce5b85227c0f8d1336732bb91cc56e6a4e2b52d0
NullPointerDepressiveDisorder <96403086+NullPointerDepressiveDisorder@users.noreply.github.com>
2026-01-30 20:14:22 -0800

    MiddleDrag: 1.4.1.1 (new cask)

---
+++ Casks/m/middledrag.rb
@@ -0,0 +1,32 @@
+cask "middledrag" do
+  version "1.4.1.1"
+  sha256 "df9b43e8725f0910f56068a5f79cc33df4e634e99f60610482a3b60906757190"
+
+  url "https://github.com/NullPointerDepressiveDisorder/MiddleDrag/releases/download/v#{version}/MiddleDrag-#{version}.pkg",
+      verified: "github.com/NullPointerDepressiveDisorder/MiddleDrag/"
+  name "MiddleDrag"
+  desc "Middle-click and middle-drag via three-finger trackpad gestures"
+  homepage "https://middledrag.app/"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/NullPointerDepressiveDisorder/MiddleDrag/main/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sequoia"
+
+  pkg "MiddleDrag-#{version}.pkg"
+
+  postflight do
+      ohai "MiddleDrag requires Accessibility permissions to function."
+      puts "Please grant Accessibility permissions in System Settings -> Privacy & Security -> Accessibility."
+  end
+
+  uninstall pkgutil: "app.middledrag.MiddleDrag"
+
+  zap trash: [
+    "~/Library/Application Support/MiddleDrag",
+    "~/Library/Preferences/com.middledrag.MiddleDrag.plist",
+  ]
+end
