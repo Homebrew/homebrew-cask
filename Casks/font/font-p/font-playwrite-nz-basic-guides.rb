02ba7e5e262b07e11604d3a3d9347f554513aa1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-01 00:22:37 +0000

    font-playwrite-nz-basic-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-p/font-playwrite-nz-basic-guides.rb
+++ Casks/font/font-p/font-playwrite-nz-basic-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-nz-basic-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritenzbasicguides/PlaywriteNZBasicGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritenzbasicguides/PlaywriteNZBasicGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite NZ Basic Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+NZ+Basic+Guides"
 
   font "PlaywriteNZBasicGuides-Regular.ttf"
 
