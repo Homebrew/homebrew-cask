5e003aae73d111c5cc6aec1a402b56573eb2221d
Zeldris <0trims.vehicle@icloud.com>
2026-02-02 15:02:55 +0700

    repobar 0.2.0 (new cask)

---
+++ Casks/r/repobar.rb
@@ -0,0 +1,24 @@
+cask "repobar" do
+  version "0.2.0"
+  sha256 "dcc628c8073185ab0ea669190dfe88828368c307b3f7f21ca38f5ce320ccce2f"
+
+  url "https://github.com/steipete/RepoBar/releases/download/v#{version}/RepoBar-#{version}.zip",
+      verified: "github.com/steipete/RepoBar/"
+  name "RepoBar"
+  desc "Menu bar dashboard for GitHub repository health"
+  homepage "https://repobar.app/"
+
+  depends_on macos: ">= :sequoia"
+
+  app "RepoBar.app"
+
+  zap trash: [
+    "~/Library/Application Support/RepoBar",
+    "~/Library/Application Support/com.steipete.repobar",
+    "~/Library/Caches/RepoBar",
+    "~/Library/Caches/com.steipete.repobar",
+    "~/Library/HTTPStorages/com.steipete.repobar",
+    "~/Library/Preferences/com.steipete.repobar.plist",
+    "~/Library/Saved Application State/com.steipete.repobar.savedState",
+  ]
+end
