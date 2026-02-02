01d534910e09bd5ebd7b5ddcefcb080a51c0086e
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2026-02-02 06:36:17 +0300

    yandextelemost 2.25.8,7484

--- Casks/y/yandextelemost.rb
+++ Casks/y/yandextelemost.rb
@@ -1,11 +1,16 @@
 cask "yandextelemost" do
-  version :latest
+  version "2.25.8,7484"
   sha256 :no_check
 
   url "https://telemost.yandex.com/download-desktop"
   name "Yandex Telemost"
   desc "Yandex video calls and meetings platform"
   homepage "https://telemost.yandex.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :big_sur"
 
