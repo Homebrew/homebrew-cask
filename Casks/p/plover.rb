4e1d20e11b23ecc5eb951d67ac3135d7da781ede
bevanjkay <email@bevankay.me>
2026-02-02 14:51:18 +1100

    plover 5.2.0

--- Casks/p/plover.rb
+++ Casks/p/plover.rb
@@ -1,6 +1,6 @@
 cask "plover" do
-  version "5.1.0"
-  sha256 "0925e872052601b54fb483cfe08bb92b8126b2a9ca5e523ae61b2bc52a1158b6"
+  version "5.2.0"
+  sha256 "f234b699e04986c6edaf83db87f1b626ff03c9bca75426ae56c9a66490ac9a7b"
 
   on_sequoia do
     disable! date: "2026-09-01", because: :fails_gatekeeper_check
@@ -10,13 +10,15 @@ cask "plover" do
       verified: "github.com/openstenoproject/plover/"
   name "Plover"
   desc "Stenotype engine"
-  homepage "https://www.openstenoproject.org/plover/"
+  homepage "https://opensteno.org/plover/"
 
   livecheck do
     url :url
     regex(/v?(\d+(?:\.\d+)+[\w.]+)/i)
     strategy :github_latest
   end
+
+  depends_on macos: ">= :monterey"
 
   app "Plover.app"
 
