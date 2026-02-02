0fdfc2cd88d15f3079bcab93503b5809bbd59e0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-02 20:05:56 +0000

    wireframe-sketcher 7.4.2

--- Casks/w/wireframe-sketcher.rb
+++ Casks/w/wireframe-sketcher.rb
@@ -1,9 +1,9 @@
 cask "wireframe-sketcher" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "7.4.1"
-  sha256 arm:   "b126441da75043dd7f72bc36622825109be5222727e9074801f9f8512245eca4",
-         intel: "9766163748fb0f15c81b9aa83ff9a2d54cd736b69e3ed50cb9a7e0dec3a2f453"
+  version "7.4.2"
+  sha256 arm:   "0c5304b2e8bcbca7de7c705673bee0f7f204a6f0895e6b10016d107db95897f0",
+         intel: "2a7d7863b4dfd88f94074b24082b00a192bd6f0d1c6b76afa5ce5f0e360ed9cc"
 
   url "https://wireframesketcher.com/downloads/studio/dist/WireframeSketcher-#{version}-macosx.#{arch}.zip"
   name "WireframeSketcher"
