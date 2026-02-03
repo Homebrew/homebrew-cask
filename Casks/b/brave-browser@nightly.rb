3c8e6f739dcdfbb45c59552ca62684b67e69b12e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 12:48:24 +0000

    brave-browser@nightly 1.88.91.0

--- Casks/b/brave-browser@nightly.rb
+++ Casks/b/brave-browser@nightly.rb
@@ -2,9 +2,9 @@ cask "brave-browser@nightly" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  version "1.88.77.0"
-  sha256 arm:   "9f2c316ee2e874c158a0458974900e2c3999dfd8c8c5edcc7bdf6067a2271661",
-         intel: "8010cbbfa4d44f6a8c1bbf70eaa83573c98f87afbd8a87785a76cd67daba281b"
+  version "1.88.91.0"
+  sha256 arm:   "95e2c06e07da2b7e748098a9ade75969a8d51ae1607bbd0fd0aceb3a4730ce73",
+         intel: "025d2aecfd540a79ecd59e6abc5c27ee59113de50950cd2c44969cc77494983b"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-Nightly-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
