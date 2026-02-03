4166912003c605cee2d930d65e830d53e915726f
Sean Molenaar <SMillerDev@users.noreply.github.com>
2026-02-03 12:44:33 +0100

    Update macOS dependency from Big Sur to Monterey

--- Casks/z/zoho-mail.rb
+++ Casks/z/zoho-mail.rb
@@ -27,7 +27,7 @@ cask "zoho-mail" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Zoho Mail - Desktop.app"
 
