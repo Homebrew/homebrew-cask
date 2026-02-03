5542687d0708080acd7397ddce7bc1ea8e7c61ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 04:39:13 +0000

    launchos 1.5.1

--- Casks/l/launchos.rb
+++ Casks/l/launchos.rb
@@ -1,6 +1,6 @@
 cask "launchos" do
-  version "1.5.0,152"
-  sha256 "a8d5c1a3ac243daf83426d44ae3e53ffd16e8e44bbe6c55db7b05a96ba0e852a"
+  version "1.5.1,162"
+  sha256 "88c46ec5cd14c26409a7d62fb61e44d3bb856ddab4d50d54f52396588c309aca"
 
   url "https://static.remixdesign.app/launchos/LaunchOS-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "static.remixdesign.app/launchos/"
