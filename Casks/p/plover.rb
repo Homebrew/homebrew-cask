6cf94a13343df3ccb36bbeca18357a5f9956798a
Patrick Linnane <patrick@linnane.io>
2026-02-03 17:44:47 -0600

    plover: remove deprecation on Sequoia
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/plover.rb
+++ Casks/p/plover.rb
@@ -2,10 +2,6 @@ cask "plover" do
   version "5.2.0"
   sha256 "f234b699e04986c6edaf83db87f1b626ff03c9bca75426ae56c9a66490ac9a7b"
 
-  on_sequoia do
-    disable! date: "2026-09-01", because: :fails_gatekeeper_check
-  end
-
   url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-macosx_12_0_universal2.dmg",
       verified: "github.com/openstenoproject/plover/"
   name "Plover"
