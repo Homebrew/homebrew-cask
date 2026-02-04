954c89b7e551b8f88844d1130eaf06d0ac5d8eeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-04 13:57:53 +0000

    novation-components 1.63.0

--- Casks/n/novation-components.rb
+++ Casks/n/novation-components.rb
@@ -1,6 +1,6 @@
 cask "novation-components" do
-  version "1.62.0"
-  sha256 "37257472d211a97cc75ad91f4d41167f3aba7a08699a227d510fcc11b6fa275a"
+  version "1.63.0"
+  sha256 "e3c9d269bf184221778ba350300b53a2f7be4fd0f5f4668c7a0cab6dbace604c"
 
   url "https://components-updates.novationmusic.com/download/version/#{version}/osx_64?filetype=zip"
   name "Novation Components"
