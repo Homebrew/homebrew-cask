6cbffdf384b07f9df5f921a115278cd46f7c3b6a
Klaus Hipp <khipp@users.noreply.github.com>
2026-02-04 04:59:22 +0100

    ayugram: update sha256

--- Casks/a/ayugram.rb
+++ Casks/a/ayugram.rb
@@ -1,11 +1,13 @@
 cask "ayugram" do
   version "6.3.10"
-  sha256 "07296be7e660b2fb3884dce1ad5a8efeda4b21d45f9d60ddb8245d74e86520da"
+  sha256 "9ac1c4f4e7ca32fa0fdc07006639d17fffe346de59898dfe0a217d588879c62a"
 
   url "https://github.com/AyuGram/AyuGramDesktop/releases/download/v#{version}/AyuGram.dmg"
   name "AyuGram"
   desc "Telegram client with ghost mode and message history"
   homepage "https://github.com/AyuGram/AyuGramDesktop"
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "AyuGram.app"
 
