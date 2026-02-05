5bc9947533ffc49b4aac5d0cf121ecdb3916b1b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-05 21:46:48 +0000

    conductor 0.34.0

--- Casks/c/conductor.rb
+++ Casks/c/conductor.rb
@@ -2,12 +2,12 @@ cask "conductor" do
   arch arm: "aarch64", intel: "x86_64"
 
   on_arm do
-    version "0.33.5,01KGJNPABMYK1ZKY6B4SJ0WEKK"
-    sha256 "487ee71a27f484509348f4ef17e093a6d444f45785d42566d9c6f56d0b8fffdb"
+    version "0.34.0,01KGQJYT9087RT8E1JYJ0320GP"
+    sha256 "5880015ea8ce629b95767459d3381b3ebddc2bdcd890065d8bcb8551491c62f1"
   end
   on_intel do
-    version "0.33.5,01KGJNSVDQT5K98F7VCXCR36FE"
-    sha256 "677a2b9b0929505dc64071af559d24427c087a39a1c65213f9722c593ab676e8"
+    version "0.34.0,01KGQJQTA3WBW7BBF1E6WHDQ2W"
+    sha256 "e2c99a3a2133cca76e19a4d3ab4edfebbfc4068acb829a232d32160b12ac26ee"
   end
 
   url "https://cdn.crabnebula.app/asset/#{version.csv.second}",
