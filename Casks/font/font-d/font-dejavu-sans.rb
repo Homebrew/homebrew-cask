5d4637ea2f61ab5eb8acc2fd27a18146e044adf9
Benjamin <1159333+benjaminburzan@users.noreply.github.com>
2026-02-05 21:50:07 +0100

    Fix URL: GitHub tag uses underscores not dots
    Use version.dots_to_underscores to convert 2.37 to 2_37 for the GitHub
    release tag format (version_2_37).
    
    Tested: brew install --cask font-dejavu-sans now works correctly.

--- Casks/font/font-d/font-dejavu-sans.rb
+++ Casks/font/font-d/font-dejavu-sans.rb
@@ -2,7 +2,7 @@ cask "font-dejavu-sans" do
   version "2.37"
   sha256 "5c6e497a2f36552cb5ffb112c413a6af39c0f3c47653662b90b4fa6499822fd7"
 
-  url "https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_#{version}/dejavu-sans-ttf-#{version}.zip"
+  url "https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_#{version.dots_to_underscores}/dejavu-sans-ttf-#{version}.zip"
   name "DejaVu Sans"
   homepage "https://github.com/dejavu-fonts/dejavu-fonts"
 
