fb0c9838f3e2b50faacae8c36a68930be085a36b
Patrick Linnane <patrick@linnane.io>
2026-02-05 21:50:48 -0800

    darktable: split minimum macOS version by architecture
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/d/darktable.rb
+++ Casks/d/darktable.rb
@@ -5,6 +5,13 @@ cask "darktable" do
   sha256 arm:   "23ce74a4d7cbab30dc5e55043f97480b2a4eb1d96d602d529c9f9a428b99d041",
          intel: "d615f7e78af9fb23d7c8017a5d35be50ea13f1ccf89491ef4b927e23eff0f43a"
 
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :sequoia"
+  end
+
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}-#{arch}.dmg",
       verified: "github.com/darktable-org/darktable/"
   name "darktable"
@@ -17,8 +24,6 @@ cask "darktable" do
   end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
-  depends_on macos: ">= :sequoia"
 
   app "darktable.app"
 
