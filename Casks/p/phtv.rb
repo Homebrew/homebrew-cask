902233f87334ca800926092c2b3d6c7339521434
Zeldris <0trims.vehicle@icloud.com>
2026-02-07 19:20:25 +0700

    phtv 2.4.5 (new cask)

---
+++ Casks/p/phtv.rb
@@ -0,0 +1,26 @@
+cask "phtv" do
+  version "2.4.5"
+  sha256 "30313b160511b37cc08e19bab0b3bd5eabdaa1656cc7e2d4dde1641abea64e1e"
+
+  url "https://github.com/PhamHungTien/PHTV/releases/download/v#{version}/PHTV-#{version}.dmg",
+      verified: "github.com/PhamHungTien/PHTV/"
+  name "PHTV"
+  desc "Modern Vietnamese input method"
+  homepage "https://phamhungtien.github.io/PHTV/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "PHTV.app"
+
+  zap trash: [
+    "~/Library/Application Support/PHTV",
+    "~/Library/Caches/com.phamhungtien.phtv",
+    "~/Library/Logs/PHTV",
+    "~/Library/Preferences/com.phamhungtien.phtv.plist",
+  ]
+end
