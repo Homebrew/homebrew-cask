8c453b5c7a62606873107ac7e7303c59b7dde0ca
rakleed <19418601+rakleed@users.noreply.github.com>
2026-02-07 23:08:16 +0300

    vk-messenger 6.0.55

--- Casks/v/vk-messenger.rb
+++ Casks/v/vk-messenger.rb
@@ -1,33 +1,19 @@
 cask "vk-messenger" do
-  version "5.3.2,723"
+  version "6.0.55"
   sha256 :no_check
 
-  url "https://desktop.userapi.com/mac/master/vk.dmg",
-      verified: "desktop.userapi.com/mac/master/"
+  url "https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/vk-messenger-universal.dmg",
+      verified: "upload.object2.vk-apps.com/"
   name "VK Messenger"
   desc "Messenger app"
-  homepage "https://vk.com/messenger"
+  homepage "https://vk.me/app"
 
   livecheck do
-    url "https://desktop.userapi.com/mac/master/latest.json"
-    strategy :json do |json|
-      version = json["version"]
-      build = json["build"]
-      next if version.blank? || build.blank?
-
-      "#{version},#{build}"
-    end
+    url "https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/latest-mac.yml"
+    strategy :electron_builder
   end
 
-  app "VK Messenger.app"
+  app "VK Мессенджер.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.vk.messages.helper.plist",
-    "~/Library/Preferences/com.vk.messages.plist",
-    "~/Library/Saved Application State/com.vk.messages.savedState",
-  ]
-
-  caveats do
-    requires_rosetta
-  end
+  zap trash: "~/Library/Preferences/com.vk.medesktop.plist"
 end
