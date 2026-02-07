91c41dc87b83983b3b97f793674b48c5dfd61b45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-07 16:08:28 +0000

    nani 1.0.64

--- Casks/n/nani.rb
+++ Casks/n/nani.rb
@@ -1,6 +1,6 @@
 cask "nani" do
-  version "1.0.63"
-  sha256 "c8776bf789d0340f5285efcd650f48cbfb30d00bf0baaae8d6d6bdbc3722b6c5"
+  version "1.0.64"
+  sha256 "1f5b6094ddad0c561d41c55c1351ed54f8f9ca51ed6ee5c55cc0cc567a994752"
 
   url "https://nani-desktop.kiok.jp/artifacts/nani-#{version}.dmg",
       verified: "nani-desktop.kiok.jp/artifacts/"
