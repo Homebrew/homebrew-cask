e9571f0ecf17c6d1d451cd6ebe8c712344f7f2f9
Zeldris <0trims.vehicle@icloud.com>
2026-02-07 18:48:56 +0700

    owocr: fix lint

--- Casks/o/owocr.rb
+++ Casks/o/owocr.rb
@@ -5,7 +5,7 @@ cask "owocr" do
   sha256 arm:   "f9ee51ec05d76cbeb1c17ae45ab1a84b1b46f5355b537078bc02bd5dcbc429b0",
          intel: "1a2dd996214512101bec38507fe23192f70e12170931c8a54c10eb2132df0c52"
 
-  url "https://github.com/AuroraWright/owocr/releases/download/#{version}/owocr-mac_#{arch}.dmg"
+  url "https://github.com/AuroraWright/owocr/releases/download/#{version}/owocr-mac_#{arch}.dmg",
   name "OwOCR"
   desc "Text recognition tool"
   homepage "https://github.com/AuroraWright/owocr/"
@@ -15,7 +15,7 @@ cask "owocr" do
     strategy :github_latest
   end
 
-  app "owocr.app"
+  app "OwOCR.app"
 
   zap trash: [
     "~/Library/Application Support/com.aury.owocr",
