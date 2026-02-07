6f8c641d502fd9b9a79c053080721ec07c785827
Sachin <vcs@tuta.com>
2026-02-07 11:40:27 +0000

    refactor(url): updated anypoint studio download url to new format

--- Casks/a/anypointstudio.rb
+++ Casks/a/anypointstudio.rb
@@ -5,7 +5,7 @@ cask "anypointstudio" do
   sha256 arm:   "057ef254deb53003744ac9304b723877b6e598447f587fa8aae58d874d87d401",
          intel: "2c3a31b24487a96d7b84d43a468a1f353278ad7f18022a5b33d62c7afb3ed9a0"
 
-  url "https://mule-studio.s3.amazonaws.com/#{version}-GA/AnypointStudio-#{version}-macos#{arch}.zip",
+  url "https://www.mulesoft.com/downloads/studio/latest/AnypointStudio-#{version}-macos#{arch}.zip",
       verified: "mule-studio.s3.amazonaws.com/"
   name "Anypoint Studio"
   desc "Eclipse-based IDE for designing and testing Mule applications"
