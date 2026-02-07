74e30fff35cefd4285ec38071c673679f88ef275
Zeldris <0trims.vehicle@icloud.com>
2026-02-07 12:49:29 +0000

    xkey 1.2.20,20260206.1 (new cask)

---
+++ Casks/x/xkey.rb
@@ -0,0 +1,30 @@
+cask "xkey" do
+  version "1.2.20,20260206.1"
+  sha256 "ddab41e0291765ddd639ed2de9871a729ae67030cb689ed07cf6a7e84edb919b"
+
+  url "https://github.com/xmannv/xkey/releases/download/v#{version.csv.first}-#{version.csv.second}/XKey.dmg"
+  name "XKey"
+  desc "Vietnamese input method engine"
+  homepage "https://github.com/xmannv/xkey/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)-(.+)$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "XKey.app"
+
+  zap trash: [
+    "~/Library/Application Support/XKey",
+    "~/Library/Preferences/com.codetay.XKey.plist",
+  ]
+end
