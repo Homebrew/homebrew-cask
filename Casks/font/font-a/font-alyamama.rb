0bc0ed5f28c190b1d6bbdc12d13dd826b0f2354f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 00:26:22 +0000

    font-alyamama: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-a/font-alyamama.rb
@@ -0,0 +1,12 @@
+cask "font-alyamama" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/alyamama/Alyamama%5Bwght%5D.ttf"
+  name "Alyamama"
+  homepage "https://github.com/Mestaratype/AlYamama"
+
+  font "Alyamama[wght].ttf"
+
+  # No zap stanza required
+end
