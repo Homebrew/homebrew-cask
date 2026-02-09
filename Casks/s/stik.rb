580f6cd559bdc522082afc54f25f27f14d0d964e
Valerio <massimianivalerio1@gmail.com>
2026-02-09 21:31:21 +0100

    cask: add verified URL for stik

--- Casks/s/stik.rb
+++ Casks/s/stik.rb
@@ -5,7 +5,8 @@ cask "stik" do
   sha256 arm:   "ac7a1da2a3a5fa1ea63d8e158bca726dc0af5401f36a276295842bd3cc7bb422",
          intel: "f15e2360df4b2d1b9708a9d5548a85777e1d313c1d4b60d98061786753d11cb8"
 
-  url "https://github.com/0xMassi/stik_app/releases/download/v#{version}/Stik_#{version}_#{arch}.dmg"
+  url "https://github.com/0xMassi/stik_app/releases/download/v#{version}/Stik_#{version}_#{arch}.dmg",
+      verified: "github.com/0xMassi/stik_app/"
   name "Stik"
   desc "Keyboard-first note capture app for macOS"
   homepage "https://www.stik.ink"
