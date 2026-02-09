d5390ae73e887fd1c38cff21f004f4b2fdf0f814
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-09 10:43:13 +0000

    pyzo 4.21.0

--- Casks/p/pyzo.rb
+++ Casks/p/pyzo.rb
@@ -1,9 +1,9 @@
 cask "pyzo" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "4.20.0"
-  sha256 arm:   "6af555adbe3a3b3e42639b5b171c1c047f7517f9d6f6baacb4e21c60596d0803",
-         intel: "14f4be6d69afd0db8502e8e706c4d207a8df4aea3718666efb66ec3c5cb1f040"
+  version "4.21.0"
+  sha256 arm:   "2b73d1a2a0dc61d144852349e4701c2324d3329fda7bbdd1299a26292abdf973",
+         intel: "94883a4133a1ab767163b1bf56db2ad12469e423bc0301d5659f2b38573f3cfe"
 
   url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos_#{arch}.dmg",
       verified: "github.com/pyzo/pyzo/"
