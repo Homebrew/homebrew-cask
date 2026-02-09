6f7546c48fa0c6fdbcc56591124b0275393d270a
khoi <6994441+khoi@users.noreply.github.com>
2026-02-09 21:52:48 +0700

    supacode 0.5.8 (new cask)

---
+++ Casks/s/supacode.rb
@@ -0,0 +1,29 @@
+cask "supacode" do
+  version "0.5.8"
+  sha256 "5bcaa00b0b78771d4d0715dc2a3eb459e299a187da3c566aa8044fe11f4ac032"
+
+  url "https://supacode.sh/download/v#{version}/supacode.dmg"
+  name "supacode"
+  desc "Native terminal coding agents command center"
+  homepage "https://supacode.sh/"
+
+  livecheck do
+    url "https://supacode.sh/download/latest/appcast.xml"
+    strategy :sparkle do |items|
+      items.max_by { |item| Gem::Version.new(item.short_version) }&.short_version
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :tahoe"
+
+  app "supacode.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.supabit.supacode",
+    "~/Library/Caches/app.supabit.supacode",
+    "~/Library/HTTPStorages/app.supabit.supacode",
+    "~/Library/HTTPStorages/app.supabit.supacode.binarycookies",
+    "~/Library/Preferences/app.supabit.supacode.plist",
+  ]
+end
