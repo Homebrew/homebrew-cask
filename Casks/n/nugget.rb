75db013b036bd5ca2f466840993116aa4f861610
AlmondMan <github@amcloud.dev>
2026-02-10 12:29:01 -0500

    Please enter the commit message for your changes. Lines starting

--- Casks/n/nugget.rb
+++ Casks/n/nugget.rb
@@ -3,7 +3,6 @@ cask "nugget" do
   sha256 "6340f130d48064eca6625e06363f2bdc9091c50878ae0a67b52f6213a32114cc"
 
   url "https://github.com/leminlimez/Nugget/releases/download/v#{version}/Nugget_macOS_arm.dmg"
-
   name "Nugget"
   desc "Unlock the fullest potential of your device"
   homepage "https://github.com/leminlimez/Nugget"
@@ -19,6 +18,6 @@ cask "nugget" do
   zap trash: [
     "~/Library/Preferences/com.nugget.MGA Data.plist",
     "~/Library/Preferences/com.nugget.Nugget.plist",
-    "~/Library/Preferences/com.nugget.settings.plist"
+    "~/Library/Preferences/com.nugget.settings.plist",
   ]
 end
