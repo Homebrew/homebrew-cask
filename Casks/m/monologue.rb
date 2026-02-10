ee36865fa0b243b857546b5365baf00a7a80cece
Bevan Kay <email@bevankay.me>
2026-02-10 15:33:09 +0800

    monologue: disable

--- Casks/m/monologue.rb
+++ Casks/m/monologue.rb
@@ -13,6 +13,8 @@ cask "monologue" do
     strategy :sparkle, &:short_version
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   auto_updates true
   depends_on macos: ">= :sonoma"
 
