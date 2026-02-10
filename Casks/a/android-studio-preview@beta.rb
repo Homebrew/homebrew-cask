dc260e4fb2fb844111c9e937bad13d1656d6b4d5
Sam Ford <1584702+samford@users.noreply.github.com>
2026-02-09 19:13:31 -0500

    android-studio-preview@beta 2025.3.1.6
    The `livecheck` block for `android-studio-preview@beta` is producing
    an "Unable to get versions" error, as the regex doesn't match the
    current dmg file names and the beta/RC text comes before the link.
    The file name format has changed and now the dmg URLs include a code
    name/number (e.g., `panda-rc1`) instead of the version. This modifies
    the `livecheck` block to capture the code name/number text from the
    file name and include it in the cask `version`. This updates the cask
    to 2025.3.1.6 and adjusts the `url` to handle this new setup.

--- Casks/a/android-studio-preview@beta.rb
+++ Casks/a/android-studio-preview@beta.rb
@@ -1,19 +1,24 @@
 cask "android-studio-preview@beta" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2025.2.3.8"
-  sha256 arm:   "b5a3897c72ccd1ded40fdb0b461104b34733dbcb5b893f0fbda012ab5dfaa128",
-         intel: "586c2790a190ba8ad82222b5050e25a66e7ee5f7546d3cb8004db322f463783f"
+  version "2025.3.1.6,panda1-rc1"
+  sha256 arm:   "3a011fbac92dbad24553974c5116dcaabcdf2e2e1fc6890e56b039b1a7cfc469",
+         intel: "d53c7dbf6eba2e825c55326f9075956a20ca15dd70e1a576485b1583689aff8e"
 
-  url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
-      verified: "redirector.gvt1.com/edgedl/android/studio/install/"
+  url "https://edgedl.me.gvt1.com/android/studio/install/#{version.csv.first}/android-studio#{"-#{version.csv.second}" if version.csv.second}-#{arch}.dmg",
+      verified: "edgedl.me.gvt1.com/android/studio/install/"
   name "Android Studio Preview (Beta)"
   desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/preview/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg(.*\n*\s*.*)(Beta|RC)}i)
+    regex(%r{(?:Beta|RC).*href=.*?/v?(\d+(?:\.\d+)+)/android[._-]studio(?:[._-]([^"' >]+))?[._-]#{arch}\.dmg}im)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
   end
 
   auto_updates true
