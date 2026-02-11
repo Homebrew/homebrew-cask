01761683ff29c4f5722027663a28b2442a3e040b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 16:40:06 +0000

    seam-app 1.0.4

--- Casks/s/seam-app.rb
+++ Casks/s/seam-app.rb
@@ -1,6 +1,6 @@
 cask "seam-app" do
-  version "1.0.3"
-  sha256 "5c6795f1671108cc2fc1d4107917a9f61440d1433453d689bf146b34b503c5cc"
+  version "1.0.4"
+  sha256 "a9c16b14675e43aeae63b250c9895452126486ca04327a63d5aef8b061f6f40d"
 
   url "https://releases.getseam.app/#{version}/Seam.dmg"
   name "Seam"
