8fa88567d7db9796f0eb81bcd04f46ece14c575f
ClathW <140804675+ClathW@users.noreply.github.com>
2026-02-11 20:44:44 +0800

    Update Casks/font/font-j/font-jetbrains-maple-mono-nf.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/font/font-j/font-jetbrains-maple-mono-nf.rb
+++ Casks/font/font-j/font-jetbrains-maple-mono-nf.rb
@@ -1,6 +1,6 @@
 cask "font-jetbrains-maple-mono-nf" do
   version "1.2304.79"
-  sha256 :no_check
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://github.com/SpaceTimee/Fusion-JetBrainsMapleMono/releases/download/#{version}/JetBrainsMapleMono-NF-XX-XX-XX.zip"
   name "JetBrains Maple Mono NF"
