cc8fac007b3acdbf8e2215d4efe50152b95e363b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 10:39:20 +0000

    theiaide 1.68.200

--- Casks/t/theiaide.rb
+++ Casks/t/theiaide.rb
@@ -1,9 +1,9 @@
 cask "theiaide" do
   arch arm: "-arm"
 
-  version "1.67.100"
-  sha256 arm:   "8bc6bb2bd0b925a32fcca70158c9bec6e9ce30b238c4133c64718ff5dfc924e3",
-         intel: "9db46e4953c19f83946bfab575ff5e3835b97a64f86810948aeaca09b8024eb2"
+  version "1.68.200"
+  sha256 arm:   "d19e26de43abd81d175167df2b7557f85ae12d4f65587c7963ac9b2d91c0a6db",
+         intel: "748840940799d4d19c201bbfad9429a83860a932b451d0cf13887c5b5a1f8b5d"
 
   url "https://download.eclipse.org/theia/ide/#{version}/macos#{arch}/TheiaIDE.dmg",
       verified: "download.eclipse.org/theia/ide/"
