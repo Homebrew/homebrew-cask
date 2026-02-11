3a8c27bfbf7944aea152fb4f7951047bf5f7d098
moabits <45351488+moabits@users.noreply.github.com>
2026-02-11 09:57:18 +0100

    Create threema-work@beta.rb

---
+++ Casks/t/threema-work@beta.rb
@@ -0,0 +1,29 @@
+cask "threema-work@beta" do
+  arch arm: "arm64", intel: "x64"
+
+  version "2.0-beta59"
+  sha256 arm:   "20ec1ee9716e26d68f1c1ab6de0750507925a02c6ecf533a54aac1c536645918",
+         intel: "1630f63cc2c7ed29e154a181a12b2c4e6fbdd30b0cda6399cc656605aad0ac02"
+
+  url "https://releases.threema.ch/desktop/#{version}/threema-work-desktop-v#{version}-macos-#{arch}.dmg"
+  name "Threema"
+  desc "End-to-end encrypted instant messaging application"
+  homepage "https://three.ma/mdw"
+
+  livecheck do
+    url "https://releases.threema.ch/desktop/latest-version-work-macos.json"
+    strategy :json do |json|
+      json.dig("latestVersion", "version")
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Threema Work Beta.app"
+
+  zap trash: [
+    "~/Library/Application Support/ThreemaDesktop",
+    "~/Library/Preferences/ch.threema.threema-desktop.plist",
+    "~/Library/Saved Application State/ch.threema.threema-desktop.savedState",
+  ]
+end
