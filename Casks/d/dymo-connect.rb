af076a909b6f91a306579f2b5408d0e52d870182
bevanjkay <email@bevankay.me>
2026-02-13 10:25:27 +1100

    dymo-connect 1.6.0.41

--- Casks/d/dymo-connect.rb
+++ Casks/d/dymo-connect.rb
@@ -1,8 +1,12 @@
 cask "dymo-connect" do
-  version "1.5.1.15"
-  sha256 "04090b1a1cc357791a29463e2b15331bfaff69dd64fe62f6afa5294b7f2a3a6b"
+  arch arm: "Arm64", intel: "X64"
 
-  url "https://download.dymo.com/dymo/Software/Mac/DCDMac#{version}.pkg"
+  version "1.6.0.41"
+  # At present, the checksum are the same across architectures, but this may change in future.
+  sha256 "a42f4da175b26b4017384ac41ed978470078aee84b1f43bb591b3c9855d2c919"
+
+  url "https://dymoreleasecontent.blob.core.windows.net/dymo-release/DCDMAC/DCDMac#{version}-#{arch}.pkg",
+      verified: "dymoreleasecontent.blob.core.windows.net/dymo-release/"
   name "Dymo Connect"
   desc "Software for DYMO LabelWriters"
   homepage "https://www.dymo.com/support?cfid=online-support"
