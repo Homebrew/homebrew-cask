a8520e595ddcd2d6b1ee0299ed405a6a333c2b92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 04:54:08 +0000

    sourcegit 2026.04

--- Casks/s/sourcegit.rb
+++ Casks/s/sourcegit.rb
@@ -1,9 +1,9 @@
 cask "sourcegit" do
   arch arm: "arm64", intel: "x64"
 
-  version "2026.03"
-  sha256 arm:   "19712ae1c3488522b25d79d1e3fb088384fb9b075eced29ab493deed1282811e",
-         intel: "285adf21a2dbc5beb1cec459617cf71b122bcb0ee46c7a5015b438183a16fdb8"
+  version "2026.04"
+  sha256 arm:   "e5af1d406dc5746e93cdd9eacb1b6bbe40802664762713759937d4b921320881",
+         intel: "3891c68fac34eb9b86d6d4fff47ffd91c52c2770024b01f8d64d289547525817"
 
   url "https://github.com/ybeapps/homebrew-sourcegit/releases/download/v#{version}-signed/SourceGit-v#{version}-#{arch}-signed.dmg"
   name "SourceGit"
