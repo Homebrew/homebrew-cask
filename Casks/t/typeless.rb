fde482a9be572b05377762ad0d0e3a3ea1f264ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 16:48:59 +0000

    typeless 1.0.2

--- Casks/t/typeless.rb
+++ Casks/t/typeless.rb
@@ -2,9 +2,9 @@ cask "typeless" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "latest"
 
-  version "1.0.1"
-  sha256 arm:   "46204c6b75414af36b751fea5d120c13940be68d0ee72dc4406fe84d524972b9",
-         intel: "1fb08c67b235198f092219a520c5f91b3c21d36e2761909bed4c1df91d75dbaf"
+  version "1.0.2"
+  sha256 arm:   "303abc025529a9fc228202398fa68329f3edfdcafd010e41687b621c7fdd72f4",
+         intel: "052050d30b34effb3becf3827b8f6b448364fe4985d551f8c88c530e14256af3"
 
   url "https://typeless-static.com/desktop-release/Typeless-#{version}-#{arch}.dmg",
       verified: "typeless-static.com/"
