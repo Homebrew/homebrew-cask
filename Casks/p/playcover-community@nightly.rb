ad4cb079c023f36a29beda90f0e99a8d346571bb
gnought <1684105+gnought@users.noreply.github.com>
2026-02-14 22:21:15 +0800

    playcover-community@nightly 1390

---
+++ Casks/p/playcover-community@nightly.rb
@@ -0,0 +1,33 @@
+cask "playcover-community@nightly" do
+  version "1390"
+  sha256 :no_check
+
+  url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop/PlayCover_nightly_#{version}.dmg.zip",
+      verified: "nightly.link/PlayCover/PlayCover/"
+  name "PlayCover"
+  desc "Sideload iOS apps and games"
+  homepage "https://github.com/PlayCover/PlayCover"
+
+  livecheck do
+    url "https://nightly.link/playcover/playcover/workflows/2.nightly_release/develop"
+    regex(/PlayCover_nightly_(\d+)\.dmg\.zip/i)
+  end
+
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
+  auto_updates true
+  conflicts_with cask: ["playcover-community", "playcover-community@beta"]
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
+end
