168bc32d222b697ea6a5f913cf81cef309720afe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-14 01:04:18 +0000

    commander 0.6.621

--- Casks/c/commander.rb
+++ Casks/c/commander.rb
@@ -1,6 +1,6 @@
 cask "commander" do
-  version "0.6.612"
-  sha256 "037a7cbf2cd5c9002562c223b68981bdc2e64f80d6e37719ba1b3d9492dd6f2b"
+  version "0.6.621"
+  sha256 "83b1d8000acab0e65b9250e358394bb5006830b5e0302d6defbd2297e88fef87"
 
   url "https://download.commanderai.app/release/Commander-#{version}.zip"
   name "Commander"
