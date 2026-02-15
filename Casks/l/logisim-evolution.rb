f1c7671bdcc37d9fda375ac12adaaccafba35708
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 10:15:38 +0000

    logisim-evolution 4.1.0

--- Casks/l/logisim-evolution.rb
+++ Casks/l/logisim-evolution.rb
@@ -1,9 +1,9 @@
 cask "logisim-evolution" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.0.0"
-  sha256 arm:   "a92fd373e9dae96f00a7db3e494b36a17a7f9cd37e3cd9fa691ea956e099363a",
-         intel: "8681ec2ac4e57435f77c1106b484cbfce24670a103ddad36187d7dffdd7de51f"
+  version "4.1.0"
+  sha256 arm:   "064497e336aca4d7d85bcb1ff1f77ca109de751c17c22717d1e76cc2dcce9518",
+         intel: "d5c329c73c3d8d62e9419613f0d4b7d0cd59f6318a8f918ab32b6c5d76cc23af"
 
   url "https://github.com/logisim-evolution/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}-#{arch}.dmg"
   name "Logisim Evolution"
