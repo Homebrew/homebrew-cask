d59515aee85e1d665690d64e8825606f2daa80d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 01:01:03 +0000

    astro-editor 1.0.8

--- Casks/a/astro-editor.rb
+++ Casks/a/astro-editor.rb
@@ -1,6 +1,6 @@
 cask "astro-editor" do
-  version "1.0.7"
-  sha256 "d0ccbe1429dc960f60575f3837f0020b29cb6c3e5641141dba86738c04b905f8"
+  version "1.0.8"
+  sha256 "93c5615bfc40b853c62ec423ff30be0403b7e4178454e92a6046698d79c03d82"
 
   url "https://github.com/dannysmith/astro-editor/releases/download/v#{version}/Astro.Editor_#{version}_universal.dmg",
       verified: "github.com/dannysmith/astro-editor/"
