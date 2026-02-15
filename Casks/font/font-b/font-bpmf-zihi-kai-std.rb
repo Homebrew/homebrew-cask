bfcae410b45497915009caecba9ffd03e834a07a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 00:22:04 +0000

    font-bpmf-zihi-kai-std: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-b/font-bpmf-zihi-kai-std.rb
@@ -0,0 +1,12 @@
+cask "font-bpmf-zihi-kai-std" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bpmfzihikaistd/BpmfZihiKaiStd-Regular.ttf"
+  name "Bpmf Zihi Kai Std"
+  homepage "https://github.com/ButTaiwan/bpmfvs"
+
+  font "BpmfZihiKaiStd-Regular.ttf"
+
+  # No zap stanza required
+end
