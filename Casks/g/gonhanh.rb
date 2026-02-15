9846cda854b14ba7c1f195b6433b74ce87a8b4c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 19:02:57 +0000

    gonhanh 1.0.121

--- Casks/g/gonhanh.rb
+++ Casks/g/gonhanh.rb
@@ -1,6 +1,6 @@
 cask "gonhanh" do
-  version "1.0.120"
-  sha256 "a389589852cf5c9be4db7e1e8c28eecd231992f0c47519f45d93767640a6f9cc"
+  version "1.0.121"
+  sha256 "88b2e8696687e989a6cab364675ac264052ba474d2ef861ce684c63434a89bd5"
 
   url "https://github.com/khaphanspace/gonhanh.org/releases/download/v#{version}/GoNhanh.dmg"
   name "Gõ Nhanh"
