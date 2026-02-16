f4e6c674c7bf6270c5c3c66f2a21e73ce3b9f27f
zhom <2717306+zhom@users.noreply.github.com>
2026-02-16 20:33:47 +0400

    feat: add donutbrowser cask

---
+++ Casks/d/donutbrowser.rb
@@ -0,0 +1,35 @@
+cask "donutbrowser" do
+  version "0.14.2"
+  sha256 "a0e50119a69828bbde6a0a4a7ab23ebf77659347f137df3f3154e09383467687"
+
+  url "https://github.com/zhom/donutbrowser/releases/download/v#{version}/Donut_#{version}_aarch64.dmg",
+      verified: "github.com/zhom/donutbrowser/"
+  name "Donut Browser"
+  desc "Simple yet powerful anti-detect browser"
+  homepage "https://donutbrowser.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  conflicts_with cask: "donutbrowser@nightly"
+  depends_on arch: :arm64
+  depends_on macos: ">= :big_sur"
+
+  app "Donut.app"
+
+  uninstall quit: "com.donutbrowser"
+
+  zap trash: [
+    "~/Library/Application Support/com.donutbrowser.Donut-Browser",
+    "~/Library/Application Support/DonutBrowser",
+    "~/Library/Caches/com.donutbrowser",
+    "~/Library/Caches/DonutBrowser",
+    "~/Library/LaunchAgents/com.donutbrowser.daemon.plist",
+    "~/Library/Logs/com.donutbrowser",
+    "~/Library/Preferences/com.donutbrowser.plist",
+    "~/Library/WebKit/com.donutbrowser",
+  ]
+end
