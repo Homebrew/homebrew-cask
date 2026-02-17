b5f7ab710f0aa103f6e87a19212eabb3c8530cf8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 13:50:20 +0000

    supacode 0.5.17

--- Casks/s/supacode.rb
+++ Casks/s/supacode.rb
@@ -1,6 +1,6 @@
 cask "supacode" do
-  version "0.5.15"
-  sha256 "b69af9ec5b631c397484f8fc6385d2fc7ccbdaf7bb3b568bc184c18f35a32949"
+  version "0.5.17"
+  sha256 "c27ebc3daea120213caaeee4e2c281a174b947199e955a7f11c3bee3da9bed86"
 
   url "https://supacode.sh/download/v#{version}/supacode.dmg"
   name "supacode"
