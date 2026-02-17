ce97a5eaa7dd19127df0df77d7796ccb0635c00d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 12:52:28 +0000

    burp-suite 2026.1.4

--- Casks/b/burp-suite.rb
+++ Casks/b/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2026.1.3"
-  sha256 arm:   "86dde612338b24ebc37ad64b24a439d9ed0ff10f780f9e58ded885cca4bdab63",
-         intel: "4a66665141d0f8e04aae147f4ccf8d0755f030a825253fbc827b8647a7827445"
+  version "2026.1.4"
+  sha256 arm:   "de9a3a5fb9aa4f14cd70132d66d0beb48c5c70d044f5bd656d49c262a3f0b7f4",
+         intel: "77845795b8b5ffe9c0ab7d832428f86a5c9eb5199f2cd7ddd3e3b865a6319784"
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
       verified: "portswigger-cdn.net/burp/releases/"
