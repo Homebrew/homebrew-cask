55e50172033cca0840bde9a0f351950b70d0eb63
Zeldris <80939130+zeldrisho@users.noreply.github.com>
2026-02-17 13:55:45 +0700

    donut: fix url

--- Casks/d/donut.rb
+++ Casks/d/donut.rb
@@ -5,7 +5,7 @@ cask "donut" do
   sha256 arm:   "b3561a92ab5bf338291c25b42bf2eb30bf4b013be379d78ffb275edbb899b0a9",
          intel: "9ce47c9488126d3bdffbb0ec66c2979f4b9d759eb60078127cb294fd6fd07024"
 
-  url "https://github.com/zhom/donutbrowser/releases/download/#{version}/Donut_#{version}_#{arch}.dmg",
+  url "https://github.com/zhom/donutbrowser/releases/download/v#{version}/Donut_#{version}_#{arch}.dmg",
       verified: "github.com/zhom/donutbrowser/"
   name "Donut"
   desc "Simple yet powerful anti-detect browser"
