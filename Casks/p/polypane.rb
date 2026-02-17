58741267da4050d6e8c9313ab6cf1d5997b2e9c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 16:34:25 +0000

    polypane 28.0.1

--- Casks/p/polypane.rb
+++ Casks/p/polypane.rb
@@ -1,9 +1,9 @@
 cask "polypane" do
   arch arm: "-arm64"
 
-  version "28.0.0"
-  sha256 arm:   "75744bbb6c60bcc8970035f47ecdc1527cb1e3cd33fd656c8f60ae9c45faba49",
-         intel: "9d1d5d030b0cf942029cb3a0f2819a0e04d920fc83ff995705b9dcd1ecfaf929"
+  version "28.0.1"
+  sha256 arm:   "0882a4fb086808db09eaa67fde8095fa09286c739f9112adccc09c23efc83879",
+         intel: "2f0d024c9307a379cf0b09d6c03a5a0721f34e8da4b180f2eb6b744e2dac13f3"
 
   url "https://github.com/firstversionist/polypane/releases/download/v#{version}/Polypane-#{version}#{arch}.dmg",
       verified: "github.com/firstversionist/polypane/"
