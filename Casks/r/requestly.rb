a806324af6206803613fe90544ef20ac9fab7a4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 16:38:17 +0000

    requestly 26.2.17

--- Casks/r/requestly.rb
+++ Casks/r/requestly.rb
@@ -1,9 +1,9 @@
 cask "requestly" do
   arch arm: "-arm64"
 
-  version "26.2.6"
-  sha256 arm:   "72f3126123df5d6f000ccb8cd36954b89c709213f739a7d95fc44ea970fa3709",
-         intel: "876129338d3adb6125507263d19e89829b9f2f66502590c467e38f4274b66b60"
+  version "26.2.17"
+  sha256 arm:   "93f6e1ba7bcb0733889bc42a8cfcfafb8828a0a71262a20f26a2d93012dbea23",
+         intel: "7cda959cdb203bd523f178efcab68f69db85c85279f6c8b5a9eae8aa9006d75c"
 
   url "https://github.com/requestly/requestly-desktop-app/releases/download/v#{version}/Requestly-#{version}#{arch}.dmg",
       verified: "github.com/requestly/requestly-desktop-app/"
