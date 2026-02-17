3b8a2f18571344981b30e78ac13322fecbae160c
Rui Chen <rui@chenrui.dev>
2026-02-17 07:18:01 -0500

    orcasheets: fis sha for arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/o/orcasheets.rb
+++ Casks/o/orcasheets.rb
@@ -2,7 +2,7 @@ cask "orcasheets" do
   arch arm: "arm64", intel: "x86_64"
 
   version "26.2.2"
-  sha256 arm:   "314d25b8ed472c79947ce0df22c3135b90d3acae335de38602ee0c1c42ecd445",
+  sha256 arm:   "ebd84b483c3c9212f34721952635eabee01559b086579f0c2ff2477abf69c575",
          intel: "b31abda1593a57b232c2c08b74e82e1480e638b59f6cb021cf89f30afc497b6e"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
