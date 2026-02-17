27a981212036ea3954782f21df03db01d6e7d2de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 18:58:34 +0000

    flutterflow 6.5.21

--- Casks/f/flutterflow.rb
+++ Casks/f/flutterflow.rb
@@ -1,6 +1,6 @@
 cask "flutterflow" do
-  version "6.5.15"
-  sha256 "bd347db999954eabbecfaa8f619fed4797e5ae21c714ee5d7f27305969254d9e"
+  version "6.5.21"
+  sha256 "19002d40508a4c3a1098ec78e2322e2a72f3308424a346b9c8ba746e177c5a4e"
 
   url "https://storage.googleapis.com/flutterflow-downloads/macos/flutterflow-#{version}-macos.zip",
       verified: "storage.googleapis.com/flutterflow-downloads/"
