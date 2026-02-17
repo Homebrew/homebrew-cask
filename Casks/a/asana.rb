8a4e61be8fecdac2b04b4680d71c40accf024cb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 12:47:05 +0000

    asana 2.6.0

--- Casks/a/asana.rb
+++ Casks/a/asana.rb
@@ -1,9 +1,9 @@
 cask "asana" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.5.1"
-  sha256 arm:   "fc2ebd6d4f1aa489dc3fe5ba70d0d30a7bbf0c0e8905d64d06508b63c00c282d",
-         intel: "0ec4019b3f0505c48750e14a6fab6909b35c5ffd1fd8d1d9e38867020b1d6a77"
+  version "2.6.0"
+  sha256 arm:   "086f0dd28a291f1d7affbe99d725eee799951c25917cd7ff5a021e1a7c4210da",
+         intel: "b1a714a7d0949701bb7402d3b43d5725fc77dc946728000068ec8cf576cfdb74"
 
   url "https://desktop-downloads.asana.com/darwin_#{arch}/prod/v#{version}/Asana-darwin-#{arch}-#{version}.zip"
   name "Asana"
