3bc10e06b19caf8838d6c182181dcdb87c621a2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-18 07:17:36 +0000

    luxury-yacht 1.3.8

--- Casks/l/luxury-yacht.rb
+++ Casks/l/luxury-yacht.rb
@@ -1,9 +1,9 @@
 cask "luxury-yacht" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.3.7"
-  sha256 arm:   "83c18d0bfb564846cc808f14cd7ceefa34654d6fd25eacfc27e47d474283e4e0",
-         intel: "ba05d35371c1420f6b6c89162514569c8610c475c0b1af502737387a6d2b1cfc"
+  version "1.3.8"
+  sha256 arm:   "fa99e403c55378c77b1c2b39ae63101adeb117d30686589a6a3f02bec04dea5d",
+         intel: "ec189a836d425a59006d907f73b8d445e3f06a1d17b330dbb7ad8699358552ae"
 
   url "https://github.com/luxury-yacht/app/releases/download/v#{version}/luxury-yacht-v#{version}-macos-#{arch}.dmg",
       verified: "github.com/luxury-yacht/app/"
