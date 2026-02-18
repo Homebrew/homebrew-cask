dead8f64580940439f8a18a528eb903b1c7a25de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-18 01:19:57 +0000

    ibkr 2.1d

--- Casks/i/ibkr.rb
+++ Casks/i/ibkr.rb
@@ -1,7 +1,7 @@
 cask "ibkr" do
   arch arm: "-arm", intel: "x-x64"
 
-  version "2.0e"
+  version "2.1d"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-macos#{arch}.dmg"
