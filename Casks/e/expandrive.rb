e596165cd4b80bb4c20f0ec7b3e90bdf617249d8
Bart van Andel <bavanandel@gmail.com>
2026-02-19 12:34:24 +0100

    fix: download expandrive using their api url
    Expandrive API url redirects to a parameterized Amazon AWS download
    link, with limited validity. The unparameterized version no longer
    works.
    
    Fixes #250166

--- Casks/e/expandrive.rb
+++ Casks/e/expandrive.rb
@@ -1,9 +1,8 @@
 cask "expandrive" do
   version "2026.02.09.821"
-  sha256 "62892ba317fe186befed61204894fb2316a4691f7a0b72405c0372c661ac90c9"
+  sha256 :no_check
 
-  url "https://files-com-public-builds.s3.us-east-1.amazonaws.com/builds/ExpanDrive/#{version}/mac/ExpanDrive_#{version.major_minor_patch}.dmg",
-      verified: "files-com-public-builds.s3.us-east-1.amazonaws.com/builds/ExpanDrive/"
+  url "https://www.expandrive.com/api/download/expandrive?platform=macos"
   name "ExpanDrive"
   desc "Network drive and browser for cloud storage"
   homepage "https://www.expandrive.com/apps/expandrive/"
