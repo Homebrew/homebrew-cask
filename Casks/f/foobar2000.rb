f160132b44fafecc85d1bc19eb2dbdb09a04df5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 10:05:09 +0000

    foobar2000 2.25.7

--- Casks/f/foobar2000.rb
+++ Casks/f/foobar2000.rb
@@ -1,7 +1,7 @@
 cask "foobar2000" do
   # NOTE: "2000" is not a version number, but an intrinsic part of the product name
-  version "2.25.6"
-  sha256 "b5072ee796813c1bcdc2d1a190bbe6cd0ba3e805ff84c1be4a991441356290e6"
+  version "2.25.7"
+  sha256 "e25627fe7cedda210f98f15b4767d938df846b6e24d8083c30de0694d5affa51"
 
   url "https://www.foobar2000.org/files/foobar2000-v#{version}.dmg"
   name "foobar2000"
