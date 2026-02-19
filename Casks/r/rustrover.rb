4ed5d35bf7494d7073b0dced66c4c19f2746f2bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 16:56:07 +0000

    rustrover 2025.3.4

--- Casks/r/rustrover.rb
+++ Casks/r/rustrover.rb
@@ -1,9 +1,9 @@
 cask "rustrover" do
   arch arm: "-aarch64"
 
-  version "2025.3.3,253.30387.122"
-  sha256 arm:   "7ed26ad0a364c23fd83f0d5a6cf077c1170358caf264012fa9b35d63545a4fd8",
-         intel: "e438b895781b030abc235fba5cac41e89b26c04a5cf2a5cd8d488f2be37aade3"
+  version "2025.3.4,253.31033.132"
+  sha256 arm:   "7e88458e38273399c7eb3c70c8ce0078a2ed19e34b894639e136496da326362e",
+         intel: "536a46c5668aa7af14da03df642a9ba6392cee23393d3d6518cc47cc11627cc0"
 
   url "https://download.jetbrains.com/rustrover/RustRover-#{version.csv.first}#{arch}.dmg"
   name "RustRover"
