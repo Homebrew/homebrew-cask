046cb91f15418072a803edc7bd03497be8f68733
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 19:41:15 +0000

    kogiqa 0.5.1006

--- Casks/k/kogiqa.rb
+++ Casks/k/kogiqa.rb
@@ -1,6 +1,6 @@
 cask "kogiqa" do
-  version "0.5.982"
-  sha256 "4159d69ed15a7ca5bbda5fe7bdfbc2fc2eef83a612e9bfdaa275a969144e6446"
+  version "0.5.1006"
+  sha256 "91da4b3bc7992781c82e72a41fb1da1c637ea23f3d62b7b6457da0716008f47f"
 
   url "https://updater.kogiqa.com/release/kogi-qa-#{version}-universal.dmg"
   name "kogiQA"
