4c96b4e4881f064f2a1461f1c9bb91e613779c43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 12:56:20 +0000

    devonthink 4.2.1

--- Casks/d/devonthink.rb
+++ Casks/d/devonthink.rb
@@ -6,7 +6,7 @@ cask "devonthink" do
     end
     on_big_sur :or_newer do
       version "3.9.16"
-      sha256 "5cd5e47582eec2222a3ae9fa1099b98caff370acfc31f0b45ab9c744d9bed25e"
+      sha256 "6dbd0f6893c1e94e54edcf747f8f4619b7aa6502ca7b879e0a89b5175ca9f991"
     end
 
     url "https://download.devontechnologies.com/download/devonthink/#{version}/DEVONthink_#{version.major}.app.zip"
@@ -18,8 +18,8 @@ cask "devonthink" do
     app "DEVONthink #{version.major}.app"
   end
   on_ventura :or_newer do
-    version "4.2"
-    sha256 "5cd5e47582eec2222a3ae9fa1099b98caff370acfc31f0b45ab9c744d9bed25e"
+    version "4.2.1"
+    sha256 "6dbd0f6893c1e94e54edcf747f8f4619b7aa6502ca7b879e0a89b5175ca9f991"
 
     url "https://download.devontechnologies.com/download/devonthink/#{version}/DEVONthink.app.zip"
 
