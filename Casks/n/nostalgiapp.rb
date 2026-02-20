80855be8cfaee88699546545253b2cdada2c5de5
jupe69 <178208403+jupe69@users.noreply.github.com>
2026-02-20 15:45:08 +0200

    Add nostalgiapp 1.0.0

---
+++ Casks/n/nostalgiapp.rb
@@ -0,0 +1,25 @@
+cask "nostalgiapp" do
+  version "1.0.0"
+  sha256 "547df6a3398164d81b0f0b7e9d26fc94bb69c1f17d8b5715f1fd05b8cd7cd2de"
+
+  url "https://www.nostalgi.app/downloads/NostalgiApp-#{version}.dmg"
+  name "NostalgiApp"
+  desc "Launcher for eXoDOS and retro game collections"
+  homepage "https://nostalgi.app/"
+
+  livecheck do
+    url "https://nostalgi.app/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "NostalgiApp.app"
+
+  zap trash: [
+    "~/Library/Application Support/NostalgiApp",
+    "~/Library/Caches/com.nostalgi.app",
+    "~/Library/Logs/NostalgiApp",
+    "~/Library/Preferences/com.nostalgi.app.plist",
+  ]
+end
