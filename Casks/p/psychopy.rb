56dd6528c32560dc87fd80afec723c99eb82e728
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 13:31:11 +0000

    psychopy 2026.1.0

--- Casks/p/psychopy.rb
+++ Casks/p/psychopy.rb
@@ -1,6 +1,6 @@
 cask "psychopy" do
-  version "2025.2.4"
-  sha256 "473bbf912b26de6a4b0e213c28c966595e1e0c75bb5976597f6cd23d89df2fa9"
+  version "2026.1.0"
+  sha256 "37ae9fe703894d14280cf163fc155fe442c46ca354487583ff66322e8b9e90cc"
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macOS#{"_#{version.csv.second}" if version.csv.second}-3.10.dmg",
       verified: "github.com/psychopy/psychopy/"
