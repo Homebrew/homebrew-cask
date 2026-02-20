cc6183deff25147b04d57f7a1e64dd917d173fce
Jaakko Rantamäki <rantamaki@live.fi>
2026-02-20 15:13:51 +0200

    Fix Android studio versioning
    Using same regex as canary version

--- Casks/a/android-studio.rb
+++ Casks/a/android-studio.rb
@@ -5,7 +5,7 @@ cask "android-studio" do
   sha256 arm:   "b6285cdee418492658a4f5ee68beb52896e81ad27e7d10b3486b9b6e02b33e88",
          intel: "fb204243b011309526cc041a479177010e22ef6f77a28e1c08f98aee95dc37d1"
 
-  url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
+  url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version.csv.first}/android-studio#{"-#{version.csv.second}" if version.csv.second}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/"
   name "Android Studio"
   desc "Tools for building Android applications"
@@ -13,7 +13,12 @@ cask "android-studio" do
 
   livecheck do
     url :homepage
-    regex(/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/android[._-]studio(?:[._-]([^"' >]+))?[._-]#{arch}\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
   end
 
   auto_updates true
