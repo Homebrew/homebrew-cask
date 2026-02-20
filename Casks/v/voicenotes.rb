fbf750e6bc95ec249ae30d35961c24647805135a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 10:49:29 +0000

    voicenotes 1.7.5

--- Casks/v/voicenotes.rb
+++ Casks/v/voicenotes.rb
@@ -1,9 +1,9 @@
 cask "voicenotes" do
   arch arm: "-arm64"
 
-  version "1.7.2"
-  sha256 arm:   "a060e447d317ad9211b4381f9a5b0b1fd403eebc592e915fca4b9284ead00db7",
-         intel: "fe5af1d73be721ed348acb147bd3887faf270472508366a8eeccd3cd5253df1b"
+  version "1.7.5"
+  sha256 arm:   "6e6cc39e3784dd32157be78e40d656f69a730f2135af0977d17b6ccdb7f0881f",
+         intel: "d8a81bcb81ee05b35a85418e6a932a4b737c91a488b25982e193221f11e5a258"
 
   url "https://github.com/brewdotcom/vn-apps-release/releases/download/#{version}/Voicenotes-#{version}#{arch}.dmg",
       verified: "github.com/brewdotcom/vn-apps-release/"
