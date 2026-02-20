320f2c19b7696b59f28826ce711857602d4465ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 19:10:28 +0000

    dorso 1.9.1

--- Casks/d/dorso.rb
+++ Casks/d/dorso.rb
@@ -1,6 +1,6 @@
 cask "dorso" do
-  version "1.9.0"
-  sha256 "6e6ee294229da09ddf0935edf676c21504e05cf4a1a97d7568c9ee14c455c858"
+  version "1.9.1"
+  sha256 "8e4a4b30cc483e93168eece055b81a5c5bac09fda84472a63443a23501f03e4c"
 
   url "https://github.com/tldev/dorso/releases/download/v#{version}/Dorso-v#{version}.dmg"
   name "Dorso"
