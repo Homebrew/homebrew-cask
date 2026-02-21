d44df847cde06221abe37514ea0f95b1f091cc1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 01:08:01 +0000

    find-any-file 2.6.1

--- Casks/f/find-any-file.rb
+++ Casks/f/find-any-file.rb
@@ -1,5 +1,5 @@
 cask "find-any-file" do
-  version "2.6,420"
+  version "2.6.1,423"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://s3.amazonaws.com/files.tempel.org/FindAnyFile_#{version.csv.first}.zip",
