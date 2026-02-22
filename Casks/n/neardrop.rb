a3573bfc6fae5325c380340426bac6ecec1b304d
onimaca <onimaca@MacBook-Air-Andres.local>
2026-02-23 00:10:53 +0100

    Add neardrop 2.2.0 (new cask)

---
+++ Casks/n/neardrop.rb
@@ -0,0 +1,23 @@
+cask "neardrop" do
+  version "2.2.0"
+  sha256 "ee6fed09714487789f605198c0415bfd097b61efa527dd21560e10af5c56a6a5"
+
+  url "https://github.com/grishka/NearDrop/releases/download/v#{version}/NearDrop.app.zip"
+  name "NearDrop"
+  desc "Unofficial Google Nearby Share app for macOS"
+  homepage "https://github.com/grishka/NearDrop"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "NearDrop.app"
+
+  uninstall quit: "me.grishka.NearDrop"
+
+  zap trash: [
+    "~/Library/Application Support/NearDrop",
+    "~/Library/Preferences/me.grishka.NearDrop.plist",
+  ]
+end
