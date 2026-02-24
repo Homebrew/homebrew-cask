b2638a37fa9f6e49a2203b1eccbb5e29f086ef7d
tennyson-mccalla <tennyson.mccalla@gmail.com>
2026-02-24 16:48:26 -0600

    Add quickdown cask (0.4.0)

---
+++ Casks/q/quickdown.rb
@@ -0,0 +1,15 @@
+cask "quickdown" do
+  version "0.4.0"
+  sha256 "1d8985324b525864f682fdc7042791141f77606640a2e7385c25835267649c8d"
+
+  url "https://github.com/tennyson-mccalla/QuickDown/releases/download/v#{version}/QuickDown.dmg"
+  name "QuickDown"
+  desc "Fast, native Markdown previewer for macOS with QuickLook integration"
+  homepage "https://github.com/tennyson-mccalla/QuickDown"
+
+  app "QuickDown.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.tennyson.QuickDown.plist",
+  ]
+end
