adb1939f14b77c992deb3cda7c0a2093c67f94c1
Kshetez Vinayak <vinayak.kshetez0102@gmail.com>
2026-02-26 08:04:38 +0530

    Add supasidebar

---
+++ Casks/s/supasidebar.rb
@@ -0,0 +1,26 @@
+cask "supasidebar" do
+  version "0.15.1"
+  sha256 "5f66646cb8ccbae566095abb4e67c6a695d53f21fd4d854cc931b279b7647fa9"
+
+  url "https://github.com/auspy/supasidebar-updates/releases/download/v#{version}/supasidebar_v#{version}.dmg",
+      verified: "github.com/auspy/supasidebar-updates/"
+  name "SupaSidebar"
+  desc "Arc-like sidebar to save links, files and folders from any browser"
+  homepage "https://supasidebar.com/"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "supasidebar.app"
+
+  uninstall quit: "com.supasidebar"
+
+  zap trash: [
+    "~/Library/Application Support/com.supasidebar",
+    "~/Library/Caches/com.supasidebar",
+    "~/Library/HTTPStorages/com.supasidebar",
+    "~/Library/HTTPStorages/com.supasidebar.binarycookies",
+    "~/Library/Preferences/com.supasidebar.plist",
+    "~/Library/Saved Application State/com.supasidebar.savedState",
+  ]
+end
