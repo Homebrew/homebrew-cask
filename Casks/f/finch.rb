257c3546e805417a314db820ba090ee38b3ec018
Austin Howard <austin@ahoward.io>
2026-02-26 16:38:01 -0600

    * Set up shell completion for Finch installation (bash/zsh) in postflight command (includes version bump to pass brew audit, but should be no-op once BrewTestBot runs)

--- Casks/f/finch.rb
+++ Casks/f/finch.rb
@@ -1,9 +1,9 @@
 cask "finch" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.15.0"
-  sha256 arm:   "c59a329b28d3bea7c9f8e67c46dba549011282534704ef37f550cd373f65236f",
-         intel: "849edf92fd713000a196a1aefb2cf82a402c3981f4946aa125800661941854e2"
+  version "1.15.1"
+  sha256 arm:   "e16d31debc03fb914c3df49e6dff43c4ad60521d3ad357a911933016bb073367",
+         intel: "11f9d3549ca84770542ffcce3ffa421009b23054825cf0bbc38eb520732cc545"
 
   url "https://github.com/runfinch/finch/releases/download/v#{version}/Finch-v#{version}-#{arch}.pkg"
   name "Finch"
@@ -16,6 +16,27 @@ cask "finch" do
   end
 
   pkg "Finch-v#{version}-#{arch}.pkg"
+
+  # Generate shell completion after installation
+  postflight do
+    # bash
+    completion_dir = Pathname("#{HOMEBREW_PREFIX}/etc/bash_completion.d").expand_path
+    completion_dir.mkpath
+
+    result = system_command "/Applications/Finch/bin/finch",
+                            args:         ["completion", "bash"],
+                            print_stderr: false
+    (completion_dir/"finch").write result.stdout
+
+    # zsh
+    completion_dir = Pathname("#{HOMEBREW_PREFIX}/share/zsh/site-functions").expand_path
+    completion_dir.mkpath
+
+    result = system_command "/Applications/Finch/bin/finch",
+                            args:         ["completion", "zsh"],
+                            print_stderr: false
+    (completion_dir/"_finch").write result.stdout
+  end
 
   uninstall script: {
     executable: "/Applications/Finch/uninstall.sh",
