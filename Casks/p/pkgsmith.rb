a6d42f80dc49a2c7d83d923cde51be4447f03c4d
Anders Fogh Eriksen <afe@adminbyrequest.com>
2026-03-01 21:30:55 +0100

    Add pkgsmith cask

---
+++ Casks/p/pkgsmith.rb
@@ -0,0 +1,15 @@
+cask "pkgsmith" do
+  version "1.0.0"
+  sha256 "b6879d30121f348c22cf39ce425eaff0a1fed3270f8f348006046f6c744c259a"
+
+  url "https://github.com/Fogh/pkg-updates/releases/download/#{version}/PKGSmith-#{version}-macos.zip"
+  name "PKGSmith"
+  desc "Create signed PKG and DMG installers from reusable project definitions"
+  homepage "https://pkgsmith.app/"
+
+  auto_updates true
+  depends_on macos: ">= :tahoe"
+
+  app "PKGSmith.app"
+  binary "PKGSmith.app/Contents/MacOS/PKGSmith", target: "pkgsmith"
+end
