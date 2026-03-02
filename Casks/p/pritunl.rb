6d0559635b9f00460aa221ed32953dcd9cf1f4ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-02 19:42:06 +0000

    pritunl 1.3.4554.60

--- Casks/p/pritunl.rb
+++ Casks/p/pritunl.rb
@@ -1,6 +1,6 @@
 cask "pritunl" do
-  version "1.3.4466.51"
-  sha256 "a29c045d80ef1cb585b654ee4305e9e6e54c868575700e29b9d1b0870bab857f"
+  version "1.3.4554.60"
+  sha256 "f6f4fc275fee7f45ee4c63124dc367b12df84b17854dd05105a71bdc61c958b1"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
