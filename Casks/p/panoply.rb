47ebbd51e812b64ad5f863ad56fa7cde8cf4ff60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-02 10:43:47 +0000

    panoply 5.9.1

--- Casks/p/panoply.rb
+++ Casks/p/panoply.rb
@@ -1,9 +1,9 @@
 cask "panoply" do
   arch arm: "arm64-"
 
-  version "5.9.0"
-  sha256 arm:   "43dc9f44bb42742d88dee279581cc2ab3fac78a1785bd96746f6c4d2412e3cf2",
-         intel: "8569b097d90113d7ed4a19bbf6571e92e9c3b288b09e6356c1aa62bdd9114f7d"
+  version "5.9.1"
+  sha256 arm:   "ee0af052dde865eaf89ae5ce6eeb63310cdbb7e168ebae450c77d92d1ee45529",
+         intel: "687565bde69e810557cac44ead0155b6c0e2bfffa8ef17bae1cc69135de6ddba"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{arch}#{version}.dmg"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
