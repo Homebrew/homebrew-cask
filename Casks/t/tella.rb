46c2ec4706e6c549028bbeec3c04e55f7bded949
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-02 19:57:56 +0000

    tella 2.11.1

--- Casks/t/tella.rb
+++ Casks/t/tella.rb
@@ -1,6 +1,6 @@
 cask "tella" do
-  version "2.10,228"
-  sha256 "2e974e0a8398e1c895b13f8a65c54c52f0888078682db46e83d5fc4b572d40eb"
+  version "2.11.1,230"
+  sha256 "79025fb22b08f34f72cc207ce8b81293ee20d2242f6a684b2835b8ecd32e9719"
 
   url "https://mac.tella.tv/Tella-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Tella"
