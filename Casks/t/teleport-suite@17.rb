d2a67a971ea5c339c5e1fbc44b704f73587bc610
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2026-03-02 15:07:52 +0100

    teleport-suite@17: 17.7.19, livecheck and eol update

--- Casks/t/teleport-suite@17.rb
+++ Casks/t/teleport-suite@17.rb
@@ -1,21 +1,20 @@
 cask "teleport-suite@17" do
-  version "17.7.12"
-  sha256 "79ca30161108e0f3fba5536e822c009afc80c0031c8e076167b45b5f1680dd52"
+  version "17.7.19"
+  sha256 "5ada804e1ace296e9665ed6d7c61a0297d12bb508e265096871a5e90458d16d0"
 
-  url "https://cdn.teleport.dev/teleport-#{version}.pkg",
+  url "https://cdn.teleport.dev/teleport-tools-#{version}.pkg",
       verified: "cdn.teleport.dev/"
   name "Teleport"
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://goteleport.com/"
 
   livecheck do
-    url "https://goteleport.com/download/",
-        user_agent: :browser
-    regex(/teleport[._-]v?(17(?:\.\d+)+)\.pkg/i)
+    url "https://github.com/gravitational/teleport/"
+    regex(/^v?(17(?:\.\d+)+)$/i)
   end
 
   # Support window taken from https://goteleport.com/docs/upcoming-releases/
-  disable! date: "2026-02-28", because: :discontinued
+  disable! date: "2026-08-01", because: :discontinued
 
   conflicts_with cask: [
     "teleport-suite",
@@ -23,7 +22,7 @@ cask "teleport-suite@17" do
     "tsh",
   ]
 
-  pkg "teleport-#{version}.pkg"
+  pkg "teleport-tools-#{version}.pkg"
 
   uninstall pkgutil: [
               "(.*).com.gravitational.teleport.tctl",
