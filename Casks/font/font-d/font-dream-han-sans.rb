71d6d58f6dcdbd7242fa3c0a78a88e53f024c522
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-03 13:01:14 +0000

    font-dream-han-sans 3.03

--- Casks/font/font-d/font-dream-han-sans.rb
+++ Casks/font/font-d/font-dream-han-sans.rb
@@ -1,6 +1,6 @@
 cask "font-dream-han-sans" do
-  version "3.02,2.004,2.003"
-  sha256 "3c40a9966252220e862f9d36285745b0e87bd0959791d2d2594127153d703ab8"
+  version "3.03,2.005,2.003"
+  sha256 "d8660a3b6df8a0d761642eabceb89b7cdf1ba210e966cda2e0c3b19d3a755a4d"
 
   url "https://github.com/Pal3love/dream-han-cjk/releases/download/dream-#{version.csv.first}-sans-#{version.csv.second}-serif-#{version.csv.third}/DreamHanSans.zip"
   name "Dream Han Sans"
