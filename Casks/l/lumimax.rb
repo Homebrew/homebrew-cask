da01ee06db697cede77e069328d1bb38c87698a1
Ramster <ramster@Ramster-MacBook-Pro.local>
2026-03-04 18:54:42 +0900

    Fix: add verified URL and macOS minimum version

--- Casks/l/lumimax.rb
+++ Casks/l/lumimax.rb
@@ -2,10 +2,13 @@ cask "lumimax" do
   version "1.3.2"
   sha256 "6733d9e404122d265d4349a9af61f011629023ac830a8353e07464784a070511"
 
-  url "https://github.com/Joowonoil/LumiMax/releases/download/v#{version}/LumiMax.dmg"
+  url "https://github.com/Joowonoil/LumiMax/releases/download/v#{version}/LumiMax.dmg",
+      verified: "github.com/Joowonoil/LumiMax/"
   name "LumiMax"
   desc "XDR brightness boost for Mac displays up to 1600 nits"
   homepage "https://ramterstudio.com/lumimax/"
+
+  depends_on macos: ">= :ventura"
 
   livecheck do
     url :url
