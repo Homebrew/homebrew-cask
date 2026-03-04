454886672bcd498a71ffc5d6a3d9f8c52cbe13cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 21:45:51 +0000

    blockstream 3.1.0

--- Casks/b/blockstream.rb
+++ Casks/b/blockstream.rb
@@ -1,9 +1,9 @@
 cask "blockstream" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.0.1"
-  sha256 arm:   "79721f1d754f4ac9fb6973d9b827bb10bcb79f278307202b6970c549da2efa3e",
-         intel: "130f8e0e34312a9558ff235f7dca6b2125ae051655e837d114aaba07dbde271a"
+  version "3.1.0"
+  sha256 arm:   "1766850e79d566a5c58fb440e43c0c22f7f35f7f40e5b679519cef3b9ffbaa90",
+         intel: "0a7091b0c5c88a72f5cae9c68b18d8fb76ccd2629f19ea5efd785a2a08938501"
 
   url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/Blockstream-#{arch}.dmg",
       verified: "github.com/Blockstream/green_qt/"
