283574d276f0a72f6b12dd6ec6b57fd0fa9379d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 01:51:43 +0000

    rclone-ui 3.5.0

--- Casks/r/rclone-ui.rb
+++ Casks/r/rclone-ui.rb
@@ -1,9 +1,9 @@
 cask "rclone-ui" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.4.3"
-  sha256 arm:   "6228534bcc2a25e3b03da293936b9a7495ae381ac9232c53459d35db4cff2c8d",
-         intel: "cf9749619a4296a728fddf2f23c1b1c747ddd1c37c15480182dff972cb07e9ed"
+  version "3.5.0"
+  sha256 arm:   "08cc0fe760e38ba9142d0b94de9834b6ee8726e4a0b1f842dc801735a2c880a2",
+         intel: "9c8e540748dae4f629debfc3b1fc8524530c569ce299b446d5ee6d2a660a83c5"
 
   url "https://github.com/rclone-ui/rclone-ui/releases/download/v#{version}/Rclone.UI_#{arch}.dmg"
   name "Rclone UI"
