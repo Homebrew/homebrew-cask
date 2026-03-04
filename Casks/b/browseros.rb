644d506a6c2a16827400668d0daaab8d914666cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 18:48:07 +0000

    browseros 0.41.0

--- Casks/b/browseros.rb
+++ Casks/b/browseros.rb
@@ -1,9 +1,9 @@
 cask "browseros" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.40.1"
-  sha256 arm:   "b03f7c859b0da20dca4f10a64c3919c8b0fc0f6e4d5ae1447d583ae137bb1bc4",
-         intel: "ba2aba0b03369e37b55e91dadceea580ed24a55e2ddd2c44b34c3dfc524f26f4"
+  version "0.41.0"
+  sha256 arm:   "4acf84ea041d791ea17330578811415d906dba391b1311f28273d1273b7634b1",
+         intel: "6465bff3001bba59a7878b5fc7e62b4ab0a5ed31055625b34aa7be1d0dff0b82"
 
   url "https://github.com/browseros-ai/BrowserOS/releases/download/v#{version.csv.second || version.csv.first}/BrowserOS_v#{version.csv.first}_#{arch}.dmg",
       verified: "github.com/browseros-ai/BrowserOS/"
