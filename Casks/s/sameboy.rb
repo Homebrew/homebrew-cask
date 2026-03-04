d7a6124670150491b437594534fc49a9d4b5eb57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 22:28:18 +0000

    sameboy 1.0.3

--- Casks/s/sameboy.rb
+++ Casks/s/sameboy.rb
@@ -1,6 +1,6 @@
 cask "sameboy" do
-  version "1.0.2"
-  sha256 "9fb1fd71efbc74e1946b463d582f7237458c29c2c714ec8806e27f3546e0fbd0"
+  version "1.0.3"
+  sha256 "09ed6eb475f0d6b0dd52217f878741e6551aa7022352947a0b7d62787316a6bf"
 
   url "https://github.com/LIJI32/SameBoy/releases/download/v#{version}/sameboy_cocoa_v#{version}.zip",
       verified: "github.com/LIJI32/SameBoy/"
