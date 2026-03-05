eb7a2995f8fa43f8c5b6e3305fa54d378f7dfedf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 02:04:23 +0000

    visual-paradigm 18.0,20260302

--- Casks/v/visual-paradigm.rb
+++ Casks/v/visual-paradigm.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "18.0,20260301"
-  sha256 arm:   "028bb271ede6acb79a7f2af91494139606a8cfa632c72ec7dd47747cc5c12f86",
-         intel: "ff97fefb17024ecd15e7410920bb1e670c5af68521ccc13c05844a46c4aeed71"
+  version "18.0,20260302"
+  sha256 arm:   "9bea6da74b01eaf5d6acda7259e7a7c287729757fecd1fdd7ed0e2403d9fd7a5",
+         intel: "73b81f093114c32067751bfc5e53aaf5af48378e4f0f9a72daca5d4e85ebb4f1"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm"
