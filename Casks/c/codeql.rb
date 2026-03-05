aea029de317cf2d5b29ab27a974c1eda5f2c734e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 19:09:42 +0000

    codeql 2.24.3

--- Casks/c/codeql.rb
+++ Casks/c/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.24.2"
-  sha256 "960de1aebf12a61cb6b7a4833a142840b3f95e4da369de055022b7068e79f224"
+  version "2.24.3"
+  sha256 "663ccd69db762f64b8e431ddad798c50d6fd844ae28ab631a89a7545f0077f90"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
