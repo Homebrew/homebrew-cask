c6e20b21f8dd7aebe7c561a697a0c60d2deb2b7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-06 19:48:44 +0000

    vimcal 1.0.45

--- Casks/v/vimcal.rb
+++ Casks/v/vimcal.rb
@@ -3,12 +3,12 @@ cask "vimcal" do
   host_suffix = on_arch_conditional arm: "m1", intel: "production"
 
   on_arm do
-    version "1.0.42"
-    sha256 "dd05e6c0249eb6d41e6499df7ac949d9649583d373466e9b964a3e9f7e5b9613"
+    version "1.0.45"
+    sha256 "4151242d0d6ab9304fcdb22be8e680022a64b3709d893860088b10989c75854b"
   end
   on_intel do
-    version "1.0.44"
-    sha256 "50a160b3b2e5d61654bd926cc825087c292d9620d662c993bb56b79b0642c770"
+    version "1.0.45"
+    sha256 "e2b06d0933da575053faedf0c18d0abca75ffc286ff4d47eac6f88c533ccec8a"
   end
 
   url "https://vimcal-#{host_suffix}.s3.amazonaws.com/Vimcal-#{version}#{arch}.dmg",
