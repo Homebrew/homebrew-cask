afd41166da289541d41e8155609e0ab817b51b52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-06 13:38:21 +0000

    reqable 3.0.39

--- Casks/r/reqable.rb
+++ Casks/r/reqable.rb
@@ -1,9 +1,9 @@
 cask "reqable" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.0.38"
-  sha256 arm:   "8aafa1cb8961d8d084de0e1a3ec946b3a0be63d72a59191e877cf6c725d89092",
-         intel: "761922fce14c7f74491d70546e624705e910b8bac16e323d684b0d2fb6b003fe"
+  version "3.0.39"
+  sha256 arm:   "1f0e759fa0ba683d27f493bcdf61bd2786ab2f6509498b99ebb3816ea92ac679",
+         intel: "0b53e1b3e3478bc5a5e6a417b7b74da2b00d659b33a96e80a3cdb3d624e81a65"
 
   url "https://github.com/reqable/reqable-app/releases/download/#{version}/reqable-app-macos-#{arch}.dmg",
       verified: "github.com/reqable/reqable-app/"
