0e6e4adf21f1e0da1b9fff0e1080ca391e67b6be
Illia Grybkov <illia@grybkov.com>
2026-03-05 20:56:45 -0600

    Fix typo in microsoft-azure-storage-explorer url

--- Casks/m/microsoft-azure-storage-explorer.rb
+++ Casks/m/microsoft-azure-storage-explorer.rb
@@ -5,7 +5,7 @@ cask "microsoft-azure-storage-explorer" do
   sha256 arm:   "2b9dbf310c36decf781c17630429a98cac46f48c731b8363da0283f75a2fe416",
          intel: "76325654299cecdafde4235b0205e9cedc794883473a0e402cfd01e24e147095"
 
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/#{version}/StorageExplorer-darwin-#{arch}.zip",
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer-darwin-#{arch}.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
   desc "Explorer for Azure Storage"
