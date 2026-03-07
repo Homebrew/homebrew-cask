ac2a5631c66edcbeb568349fc1f474f6f8d3f878
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-07 11:37:57 -0500

    protege: add macos dependency

--- Casks/p/protege.rb
+++ Casks/p/protege.rb
@@ -10,6 +10,8 @@ cask "protege" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
+  depends_on macos: ">= :big_sur"
+
   app "Protege-#{version}/Protégé.app"
 
   zap trash: [
