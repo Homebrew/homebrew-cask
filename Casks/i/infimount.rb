b1527014a65648d505adb0fe7ccade8e4b50d962
roylkng <rajan.kadeval@gmail.com>
2026-03-07 14:59:08 +0530

    infimount 0.1.0 (new cask): align metadata with upstream

--- Casks/i/infimount.rb
+++ Casks/i/infimount.rb
@@ -2,9 +2,10 @@ cask "infimount" do
   version "0.1.0"
   sha256 "72610f728661a199ca1e8f6d8a29bfd71dc5715de9a823a3c1cf289fda41181e"
 
-  url "https://github.com/infimount/infimount/releases/download/v#{version}/Infimount.dmg"
+  url "https://github.com/infimount/infimount/releases/download/v#{version}/Infimount.dmg",
+      verified: "github.com/infimount/infimount/"
   name "Infimount"
-  desc "Unified storage browser"
+  desc "Browse local and cloud storage through a single interface"
   homepage "https://github.com/infimount/infimount"
 
   livecheck do
