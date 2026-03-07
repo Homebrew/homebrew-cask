e16cb38b8297684098a6d39c12af429331352c62
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-07 09:38:56 -0500

    aqua-voice 0.12.1
    `aqua-voice` is autobumped but the workflow failed to update to
    version 0.12.1, as the versioned dmg file URLs don't resolve. I wasn't
    able to find new versioned dmg files, so this updates the version and
    modifies the cask to use the versioned zip files that are referenced
    in the JSON file that livecheck checks.

--- Casks/a/aqua-voice.rb
+++ Casks/a/aqua-voice.rb
@@ -1,15 +1,15 @@
 cask "aqua-voice" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.11.9"
-  sha256 arm:   "d1f8a3f0febcbbe49081dfde016a841faca8fb724b4706ba6d616fb4a230969b",
-         intel: "59f65c28bf6843ea10d3105285ca331b2255c72c3d33901d2d8586521c198c2f"
+  version "0.12.1"
+  sha256 arm:   "a2fc413fe92642b569f9cc0359757db177c0fb3d2f0d047c5ae23cc5d51c6818",
+         intel: "caa2a132a7644374ebafd55822b6aa1d344a4e9d84a66e5a6fce2498f564d1ac"
 
-  url "https://d1a1dx1sgvjqrz.cloudfront.net/aqua-voice-updates/darwin/#{arch}/Aqua+Voice-#{version}-#{arch}.dmg",
+  url "https://d1a1dx1sgvjqrz.cloudfront.net/aqua-voice-updates/darwin/#{arch}/Aqua%20Voice-darwin-#{arch}-#{version}.zip",
       verified: "d1a1dx1sgvjqrz.cloudfront.net/"
   name "Aqua Voice"
   desc "Speech-to-text system"
-  homepage "https://withaqua.com/"
+  homepage "https://aquavoice.com/"
 
   livecheck do
     url "https://aqua-desktop-builds.s3.amazonaws.com/aqua-voice-updates/darwin/#{arch}/RELEASES.json"
