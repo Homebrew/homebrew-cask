4e2c8c4d69a3812b30d06f96b42524977da226ba
Rui Chen <rui@chenrui.dev>
2026-03-07 16:02:09 -0500

    t3-code: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/t/t3-code.rb
+++ Casks/t/t3-code.rb
@@ -5,10 +5,11 @@ cask "t3-code" do
   sha256 arm:   "14ab774434354b02dfe9f3adb4ae5f2029ddf1965612f209a825ada8da9dff99",
          intel: "9c7d7bd2a71bc5213599b98e96180cfe1677afde823617218e18c75bec3cb84b"
 
-  url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/T3-Code-#{version}-#{arch}.dmg"
+  url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/T3-Code-#{version}-#{arch}.dmg",
+      verified: "github.com/pingdotgg/t3code/"
   name "T3 Code"
   desc "Minimal GUI for code agents like Codex and Claude Code"
-  homepage "https://github.com/pingdotgg/t3code"
+  homepage "https://t3.codes/"
 
   livecheck do
     url :url
