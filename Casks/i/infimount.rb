ea6922d1ddbcf89ecf1c73109c4452dd8df02588
roylkng <rajan.kadeval@gmail.com>
2026-03-07 14:13:36 +0530

    infimount 0.1.0

---
+++ Casks/i/infimount.rb
@@ -0,0 +1,25 @@
+cask "infimount" do
+  version "0.1.0"
+  sha256 "72610f728661a199ca1e8f6d8a29bfd71dc5715de9a823a3c1cf289fda41181e"
+
+  url "https://github.com/infimount/infimount/releases/download/v#{version}/Infimount.dmg"
+  name "Infimount"
+  desc "Unified storage browser"
+  homepage "https://github.com/infimount/infimount"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Infimount.app"
+
+  zap trash: [
+    "~/.infimount",
+    "~/Library/Application Support/com.infimount.desktop",
+    "~/Library/Preferences/com.infimount.desktop.plist",
+    "~/Library/Saved Application State/com.infimount.desktop.savedState",
+  ]
+end
