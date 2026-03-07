ff26c67910e7a355ce65b4726f4cf3ca4946d7b9
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-07 15:34:50 -0500

    vitalsource-bookshelf 11.3.0.3872
    `vitalsource-bookshelf` is autobumped but the workflow failed to
    update to version 11.3.0.3872 because the URL path only uses
    major/minor instead of the expected major/minor/patch. It seems that
    upstream may omit the patch if it's zero. This updates the version and
    adjusts the URL to conditionally include or omit the patch in the path
    depending on if it's zero. We can revisit it in the future if this
    ends up being a one-off typo, otherwise this setup may allow us to
    automatically handle this difference.

--- Casks/v/vitalsource-bookshelf.rb
+++ Casks/v/vitalsource-bookshelf.rb
@@ -1,8 +1,8 @@
 cask "vitalsource-bookshelf" do
-  version "11.2.1.3625"
-  sha256 "d3db099651f9e42f74bce0fa27283260e40feedd5f8621f9f96f69c05c2325ea"
+  version "11.3.0.3872"
+  sha256 "8f7221e870cacc89cd16fa11c9de9ca4ae8876a0b4449e65fecd3d5210f2c354"
 
-  url "https://downloads.vitalbook.com/vsti/bookshelf/#{version.major_minor_patch}/mac/bookshelf/VitalSource-Bookshelf_#{version}.dmg",
+  url "https://downloads.vitalbook.com/vsti/bookshelf/#{(version.patch == "0") ? version.major_minor : version.major_minor_patch}/mac/bookshelf/VitalSource-Bookshelf_#{version}.dmg",
       verified: "downloads.vitalbook.com/vsti/bookshelf/"
   name "VitalSource Bookshelf"
   desc "Access etextbooks"
