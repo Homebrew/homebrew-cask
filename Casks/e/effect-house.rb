a4f3e51361e8961c4eea144d791ffae49146b9b5
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-07 10:09:25 -0500

    effect-house 5.8.0
    `effect-house` is autobumped but the workflow failed to update to
    version 5.8.0 because the dmg file name now omits the "v" before the
    version. This updates the version and adjusts the `url` accordingly.

--- Casks/e/effect-house.rb
+++ Casks/e/effect-house.rb
@@ -3,15 +3,15 @@ cask "effect-house" do
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "5.7.1,0416,01292026,104"
-    sha256 "8f5ea1fd893139512aa8e19510ae39124c989a43354d6b5d2b52fee734236b7d"
+    version "5.8.0,4940,02252026,104"
+    sha256 "8e998472417b9e1c34aa2284a41f8f6df75193ecbf4b71615043e55ddbfd3f8c"
   end
   on_intel do
-    version "5.7.1,0421,01292026,104"
-    sha256 "ddb48961a13dad701616f9d357690df144032450de7519eeaff725eb5680e629"
+    version "5.8.0,5152,02252026,104"
+    sha256 "04776be613bc62445cbed96446d86a784bca9f6da9aa5fccbd100a78b3911f83"
   end
 
-  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/V#{version.csv.first.no_dots}_External_Release_Builds_#{version.csv.third}/Effect_House_v#{version.csv.first}.#{version.csv.second}_#{arch}_#{version.csv.fourth}.dmg",
+  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/V#{version.csv.first.no_dots}_External_Release_Builds_#{version.csv.third}/Effect_House_#{version.csv.first}.#{version.csv.second}_#{arch}_#{version.csv.fourth}.dmg",
       verified: "sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/"
   name "TikTok Effect House"
   desc "Create vibrant AR effects for TikTok"
