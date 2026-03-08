7d843e98309f908fd7aeffeabea78ba2fe2af1da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 16:35:48 +0000

    wizcli 1.34.0

--- Casks/w/wizcli.rb
+++ Casks/w/wizcli.rb
@@ -2,11 +2,11 @@ cask "wizcli" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "1.33.0"
-  sha256 arm:          "d0bc4fad9fde71cdea9511a9581ae0ebe4f5ae0c8214329dc42d7f933d2d9921",
-         x86_64:       "dc74cf2c6fdd9e26f02a4691f562034dcde7ee45de26eb601aa1b30bcf5fbf50",
-         arm64_linux:  "c92640ceb46eba8fe5e928b34351e3cdcf600e41286fe2e8ce6ed23ffa48a809",
-         x86_64_linux: "bfd06be0f83b955009912f26b3b9b4911e4bd677526fe602544b11a82bbf65b5"
+  version "1.34.0"
+  sha256 arm:          "e7bdc09e1d8e9ccc1c89757423f8b392e8079b10e6029e619d5f7203810113d2",
+         x86_64:       "4003627f5276a20cdb6c558e66c9724500498a15ac8eb1de57635a16cfbeeb50",
+         arm64_linux:  "5d0e758e2d598828d40e8835b4321632de5a169bcb5b0d6d3b79a1a67f7dd984",
+         x86_64_linux: "9ff24fbd22b031bf5a634c79439fb93b8a41d5a3f0deb2e410a034c749c5ff81"
 
   url "https://downloads.wiz.io/v#{version.major}/wizcli/#{version}/wizcli-#{os}-#{arch}"
   name "Wiz CLI"
