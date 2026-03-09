3c1ddbc8af53a8bbc669a9630c811dd63ab9f1dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 10:33:55 +0000

    monal@beta 1083

--- Casks/m/monal@beta.rb
+++ Casks/m/monal@beta.rb
@@ -1,6 +1,6 @@
 cask "monal@beta" do
-  version "1082"
-  sha256 "96323251c3c7943d4eb4a4f7000fe8689656b1090e003bbe0c62f8588c99437d"
+  version "1083"
+  sha256 "a1721ec84fdfd098e462cc7f4e28f1e0dc127124e02721b230cab4b0f48ebef1"
 
   url "https://downloads.monal-im.org/monal-im/beta/macOS/Monal-#{version}.zip"
   name "Monal"
