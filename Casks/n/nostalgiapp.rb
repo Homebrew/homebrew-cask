3919367065d8f7df02ceec43d5c54a3676dbc03a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 07:25:04 +0000

    nostalgiapp 1.0.7.3

--- Casks/n/nostalgiapp.rb
+++ Casks/n/nostalgiapp.rb
@@ -1,6 +1,6 @@
 cask "nostalgiapp" do
-  version "1.0.7.2"
-  sha256 "9fc05935e565fd585944fba4ccd53450d0645171d7fc1ac1eedd90767f6f99a1"
+  version "1.0.7.3"
+  sha256 "a64dcb503f7bc06e819b94d54f0cc9d4c4ce75c77b5c76bab5a4e21511395866"
 
   url "https://www.nostalgi.app/downloads/NostalgiApp-#{version}.dmg"
   name "NostalgiApp"
