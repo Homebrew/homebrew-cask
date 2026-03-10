620ed6af562aec298b6a1ed203d9660cc7285921
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 19:28:47 +0000

    obscura-vpn 1.156

--- Casks/o/obscura-vpn.rb
+++ Casks/o/obscura-vpn.rb
@@ -1,6 +1,6 @@
 cask "obscura-vpn" do
-  version "1.152"
-  sha256 "7cde5fd61ca4bc6fc47b5df3191b35f439d02e4f38c974620b3644557739b10f"
+  version "1.156"
+  sha256 "77016a1671d6748cbea758b3a15fc022215c2785e115021e1b4caf00bf4bdfcf"
 
   url "https://pkgs.obscura.net/macos/obscura-#{version}.dmg"
   name "Obscura VPN"
