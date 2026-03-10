5cb66d6b34269a182bf57219d96904d341b00f02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 13:52:32 +0000

    typinator 10.0

--- Casks/t/typinator.rb
+++ Casks/t/typinator.rb
@@ -1,6 +1,6 @@
 cask "typinator" do
-  version "9.2"
-  sha256 "2558efab1afa701b4760ebf83a5f97960603b08a6ce5a3bc68be0f386c79fd64"
+  version "10.0"
+  sha256 "dc01024d8b36bbb5770b63336eaf1873ba0620265554ba6a973f96650065e6d8"
 
   url "https://www.ergonis.com/downloads/products/typinator/Typinator#{version.no_dots}-Install.dmg",
       user_agent: :fake
