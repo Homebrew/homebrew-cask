be52bd100a1ed67e2758c5044716760157af1d55
David Lim <37480082+dlim04@users.noreply.github.com>
2026-03-10 01:16:28 +0000

    seamly2d 2026.3.2.214

---
+++ Casks/s/seamly2d.rb
@@ -0,0 +1,27 @@
+cask "seamly2d" do
+  version "2026.3.9.214"
+  sha256 "e5655c427b950a94e6b01c2add5fb980661c6acaa6c5cf7472a613d3eb68e211"
+
+  url "https://github.com/FashionFreedom/Seamly2D/releases/download/v#{version}/Seamly2D-macos.zip",
+      verified: "github.com/FashionFreedom/Seamly2D/"
+  name "Seamly2D"
+  desc "Open source patternmaking software to democratize fashion"
+  homepage "https://seamly.io/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+  container nested: "Seamly2D.dmg"
+
+  app "Seamly2D.app"
+
+  zap trash: [
+    "~/.config/Seamly2DTeam",
+    "~/Library/Application Support/Seamly2D",
+    "~/Library/Preferences/org.seamly2dproject.Seamly2D.plist",
+    "~/Library/Saved Application State/org.seamly2dproject.Seamly2D.savedState",
+  ]
+end
