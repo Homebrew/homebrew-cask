abaeb8c3c8243d601b806d6a8ba7b370664d32c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 02:01:46 +0000

    touchdesigner 2025.32440

--- Casks/t/touchdesigner.rb
+++ Casks/t/touchdesigner.rb
@@ -1,9 +1,9 @@
 cask "touchdesigner" do
   arch arm: "arm64", intel: "intel"
 
-  version "2025.32280"
-  sha256 arm:   "bc2d652763e733ef840c96f202160d666fd36f2bdf450e482c8037e03c70c1d5",
-         intel: "3b91e589957b2686d1273b1b9559b42f9b4431e51d19bb2de42aa89f272d5420"
+  version "2025.32440"
+  sha256 arm:   "e13958e01f2b4efdf8bee6197d0234fcb1cc14e3d9e14f5b3f6a00a9f3da13f7",
+         intel: "1cf94731eefdbed2277d2d8f744421aafe70e14f3bbdfc2d7b266d39e16209fd"
 
   url "https://download.derivative.ca/TouchDesigner.#{version}.#{arch}.dmg"
   name "Derivative TouchDesigner"
