67ff26c284c280dd3838f8ce45c77c851f82a730
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 16:04:25 +0000

    eclipse-rcp 4.39
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/e/eclipse-rcp.rb
+++ Casks/e/eclipse-rcp.rb
@@ -1,9 +1,9 @@
 cask "eclipse-rcp" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.38,2025-12"
-  sha256 arm:   "35cf161e13086ad018f5aea8680801e6f1bb269eca41d95888ad5bae08989758",
-         intel: "60f3b36b8f531f69c40d515f0eaff18c8806ea2f0cb8b7bbe4fd9c858c046e68"
+  version "4.39,2026-03"
+  sha256 arm:   "48f6f94a30a1f658385baae054478ad5dad0be95e0cde37a060047419569a11e",
+         intel: "10d1daced2719bcb40ced889d29110e85b9175095c0a28fbaf134317f6c37de7"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-rcp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse for RCP and RAP Developers"
