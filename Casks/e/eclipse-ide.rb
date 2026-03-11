fa192ea7a1acc12ccf282a4605d48c9bac097534
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-11 13:19:19 -0400

    eclipse-ide 4.39
    The `livecheck` block for `eclipse-ide` infers the year-month part of
    the version from the `release["date"]` value but this doesn't appear
    to be guaranteed to strictly align with the release date of Eclipse
    IDE (i.e., the `data.json` file is used to render the information on
    https://download.eclipse.org/eclipse/downloads/ but Eclipse IDE
    downloads are on https://www.eclipse.org/downloads/packages/). The
    2026-02 date from the `data.json` file doesn't work but the upstream
    server returns a 200 response (instead of 404) and serves a "file
    unavailable" HTML page, which makes it seem like the file URL has
    resolved. This predictably fails, as we haven't received a dmg file.
    
    With this in mind, we have to obtain the version from the `data.json`
    file as well as the year-month date from the Eclipse IDE download
    page. The `livecheck` block previously checked the latter, so we
    basically have to integrate the current and the previous approaches.
    
    This updates the `eclipse-ide` version and modifies the `livecheck`
    block to merge the version information from the aforementioned
    sources. Hopefully there will be a single source that reliably
    provides all of this version information but this works for now.

--- Casks/e/eclipse-ide.rb
+++ Casks/e/eclipse-ide.rb
@@ -1,9 +1,9 @@
 cask "eclipse-ide" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.38,2025-12"
-  sha256 arm:   "9c2f8151949725c6bc63651018d3df54d1453f57009724f1db5eee1d53f7e2d9",
-         intel: "c6b594cba3e7f1bc8f21c4ffc8f176bae741708c74c2c1d6d6cf3ad4327a0e29"
+  version "4.39,2026-03"
+  sha256 arm:   "3f59b7088f78b865215ff04cf57d2a2b8def175965b0cb1c22b0cbe75abda7f8",
+         intel: "d306dfcb72773ae558e9ff0d9631b37691a8b9494ce79719124df8ea6c302239"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-committers-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Eclipse Committers"
@@ -12,17 +12,21 @@ cask "eclipse-ide" do
 
   livecheck do
     url "https://download.eclipse.org/eclipse/downloads/data.json"
-    regex(/^(\d{4}-\d{2})/i)
+    regex(/href=.*?eclipse-committers-(\d+-\d+)-R-mac/i)
     strategy :json do |json, regex|
-      json["releases"]&.map do |release|
-        version = release["label"]
-        next if version.blank?
+      versions = json["releases"]&.filter_map { |release| release["label"] }&.uniq
+      version = versions.max_by { |v| Version.new(v) }
+      next unless version
 
-        match = release["date"]&.match(regex)
-        next if match.blank?
+      download_page = Homebrew::Livecheck::Strategy.page_content(
+        "https://www.eclipse.org/downloads/packages/",
+      )[:content]
+      next if download_page.blank?
 
-        "#{version},#{match[1]}"
-      end
+      date = download_page[regex, 1]
+      next unless date
+
+      "#{version},#{date}"
     end
   end
 
