397c969723da935b33c27974013906dfe83e2d80
William Song <30965609+Freakwill@users.noreply.github.com>
2026-03-11 17:07:35 +0800

    Update autoclaw.rb

--- Casks/a/autoclaw.rb
+++ Casks/a/autoclaw.rb
@@ -2,7 +2,7 @@ cask "autoclaw" do
   version "0.2.14"
   sha256 "d0a5b50a636d4cd4136a3449b7f5990bc9b0d4b223e8fef15727a5ce1d1dbde6"
 
-  url "file://#{Dir.home}/autoclaw-#{version}.dmg"
+  url "https://autoglm.aminer.cn/autoclaw/updates/autoclaw-#{version}.dmg"
   name "AutoClaw"
   desc "Desktop app for AutoClaw"
   homepage "https://github.com/Freakwill/homebrew-cask"
