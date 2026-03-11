5ab92443ce8c546c42cb51e32a87b92662ea442c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 19:40:45 +0000

    t3-code 0.0.10

--- Casks/t/t3-code.rb
+++ Casks/t/t3-code.rb
@@ -1,9 +1,9 @@
 cask "t3-code" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.9"
-  sha256 arm:   "88c36790d728f6b26eeaf0c76cc942f0bd931e9c5ce6f97d2cf9b874f906344d",
-         intel: "635666da054b3c9b45a372b15ed307dbef108e835e6b562d739f512dbeefd726"
+  version "0.0.10"
+  sha256 arm:   "651e1323f0d4cfc60de9c4018da4b5e30d1a74a3cd69468a7fd3bf756eef705b",
+         intel: "efa250039f67928eb16e4b490178c38f0105d545ca26a435825e5a35a707a2a1"
 
   url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/T3-Code-#{version}-#{arch}.dmg",
       verified: "github.com/pingdotgg/t3code/"
