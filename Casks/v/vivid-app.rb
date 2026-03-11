b6ce0615e9d9144645761c957202ba13f5c10c45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 22:48:17 +0000

    vivid-app 2.18.1

--- Casks/v/vivid-app.rb
+++ Casks/v/vivid-app.rb
@@ -1,6 +1,6 @@
 cask "vivid-app" do
-  version "2.18"
-  sha256 "055837e7dc2d1c0743b3c7d03c9ce40c61b839238ae12ce812fd3d9df51d9c6d"
+  version "2.18.1"
+  sha256 "2e71ef01a5d707bb204a8755f27ba166228692bca9893d2df462e25c35670292"
 
   url "https://lumen-digital.com/apps/vivid/releases/Vivid#{version}.zip",
       verified: "lumen-digital.com/apps/vivid/releases/"
