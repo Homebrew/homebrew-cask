f8c98fa644244ef6fc7036b7c11cc96c96faab52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 21:54:19 +0000

    deltawalker 2.8.0

--- Casks/d/deltawalker.rb
+++ Casks/d/deltawalker.rb
@@ -1,9 +1,9 @@
 cask "deltawalker" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.7.0"
-  sha256 arm:   "804c509f0813e28c3b960a24ab57f200331ba2333ed9ef9dee479f29ac3bf554",
-         intel: "e1000806db159cd760fd4d812f475581851d5c02a6f4f7bc1040835bcc6668ad"
+  version "2.8.0"
+  sha256 arm:   "7000de87a713ed172c94c0471e90d57a2a73c1d585182247efd410db61edb95b",
+         intel: "722ed3534ae90151d398042374ec2137decec5b737bfb2e56bb778f955d497f9"
 
   url "https://deltawalker.s3.amazonaws.com/DeltaWalker-#{version}_#{arch}.dmg",
       verified: "deltawalker.s3.amazonaws.com/"
