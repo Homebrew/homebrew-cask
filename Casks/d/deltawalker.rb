4d9d05adb8688668f21424cee08fae98901776af
Bevan Kay <email@bevankay.me>
2026-03-12 06:07:25 +0800

    deltawalker: add `depends_on`

--- Casks/d/deltawalker.rb
+++ Casks/d/deltawalker.rb
@@ -2,8 +2,8 @@ cask "deltawalker" do
   arch arm: "aarch64", intel: "x64"
 
   version "2.8.0"
-  sha256 arm:   "7000de87a713ed172c94c0471e90d57a2a73c1d585182247efd410db61edb95b",
-         intel: "722ed3534ae90151d398042374ec2137decec5b737bfb2e56bb778f955d497f9"
+  sha256 arm:   "e6d0b2e8617902fc05b1516c4209ee23bf42fc20dd3dc30d7605fdb3e1649976",
+         intel: "4f897341924bf65b3cbe76361c5fe9c0d4d51cf63cbdcd46fc3ed5a551cc7528"
 
   url "https://deltawalker.s3.amazonaws.com/DeltaWalker-#{version}_#{arch}.dmg",
       verified: "deltawalker.s3.amazonaws.com/"
@@ -15,6 +15,8 @@ cask "deltawalker" do
     url "https://www.deltawalker.com/download"
     regex(/href=.*?DeltaWalker[._-]?v?(\d+(?:\.\d+)+)_#{arch}\.dmg/i)
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "DeltaWalker.app"
 
