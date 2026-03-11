3d96eb24810db9838ffa40ec7b54f8ed256e936e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 13:43:07 +0000

    superlist 1.51.0

--- Casks/s/superlist.rb
+++ Casks/s/superlist.rb
@@ -1,6 +1,6 @@
 cask "superlist" do
-  version "1.50.0"
-  sha256 "dd8bafc16984f79474087c1377516ddfb74529ce2576752d32d11c575ff53c86"
+  version "1.51.0"
+  sha256 "b972c567174e33256fbe02ac2dc8a710c0c33e7a60017b9fdc8016213809d99b"
 
   url "https://storage.googleapis.com/superlist-appcast/beta/updates/Superlist-#{version}.zip",
       verified: "storage.googleapis.com/superlist-appcast/beta/updates/"
