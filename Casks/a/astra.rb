5a4d0e61be9806e0c427b23df45022be736337a0
Boof2015 <75185879+Boof2015@users.noreply.github.com>
2026-03-12 13:56:55 -0400

    Add astra 0.4.0-beta

---
+++ Casks/a/astra.rb
@@ -0,0 +1,12 @@
+cask "astra" do
+  version "0.4.0-beta"
+  sha256 "cb0972eed075f0f4bae943eeb5c0b06f60dbdbd7f73d79b0e59803997a77a47a"
+
+  url "https://github.com/Boof2015/astra/releases/download/v#{version}/Astra-#{version}-Mac-arm64.dmg"
+
+  name "Astra"
+  desc "Audiophile music player with advanced visualization"
+  homepage "https://github.com/Boof2015/astra"
+
+  app "Astra.app"
+end
