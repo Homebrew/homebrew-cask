f33f45ca54aec3df1416d16777a3968ddb23f8d5
Srinivas Garisha <ssrinivas_7774@outlook.com>
2026-03-13 01:12:32 +1100

    Create ulaa.rb

---
+++ Casks/u/ulaa.rb
@@ -0,0 +1,26 @@
+cask "ulaa" do
+  version "2.40.5"
+  sha256 :no_check
+
+  url "https://ulaa.com/release/mac/stable/arm64/latest?isDownload=true"
+  name "Ulaa Browser"
+  desc "Privacy-centric browser with advanced tracking protection"
+  homepage "https://ulaa.com/"
+
+  livecheck do
+    url :url
+    strategy :header_match
+    regex(/Ulaa[._-]Browser[._-]v?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Ulaa.app"
+
+  zap trash: [
+    "~/Library/Application Support/Ulaa",
+    "~/Library/Caches/Ulaa",
+    "~/Library/Preferences/com.zoho.ulaa.plist",
+    "~/Library/Saved Application State/com.zoho.ulaa.savedState",
+  ]
+end
