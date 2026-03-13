1c8530cab7fe93c5cd0cb5a2c087954bb0c75c07
Charles Snow <1126104236@qq.com>
2026-03-13 17:09:34 +0800

    Fix lint: remove platform from desc, alphabetize zap array

--- Casks/c/clashx.rb
+++ Casks/c/clashx.rb
@@ -5,7 +5,7 @@ cask "clashx" do
   url "https://github.com/ClashX-Pro/ClashX/releases/download/#{version}/ClashX.dmg",
       verified: "github.com/ClashX-Pro/ClashX/"
   name "ClashX"
-  desc "Rule-based custom proxy with GUI based on Clash for macOS"
+  desc "Rule-based custom proxy with GUI based on Clash"
   homepage "https://clashx.tech/"
 
   livecheck do
@@ -18,9 +18,9 @@ cask "clashx" do
   app "ClashX.app"
 
   zap trash: [
+    "~/.config/clash",
     "~/Library/Application Support/com.west2online.ClashX",
     "~/Library/Caches/com.west2online.ClashX",
     "~/Library/Preferences/com.west2online.ClashX.plist",
-    "~/.config/clash",
   ]
 end
