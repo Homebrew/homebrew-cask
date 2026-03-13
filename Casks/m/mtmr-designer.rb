73d119696f86be23744baaaf68168c444786d289
josetwentyfour <jose.viscasillas.on24@gmail.com>
2026-03-12 23:31:16 -0400

    Fix URL construction for release download

--- Casks/m/mtmr-designer.rb
+++ Casks/m/mtmr-designer.rb
@@ -2,7 +2,7 @@ cask "mtmr-designer" do
   version "2026.1,build.16"
   sha256 "20821b01fbe109e8ec34e3fceef8623ca25771d2bbf0dfa6067faeda9d3f6990"
 
-  url "https://github.com/josmanvis/mtmr-designer/releases/download/v#{version.csv.second}/MTMR-2026-#{version.csv.first}.dmg"
+  url "https://github.com/josmanvis/mtmr-designer/releases/download/v#{version.csv.first}-#{version.csv.second}/MTMR-2026-#{version.csv.first}.dmg"
   name "MTMR Designer"
   desc "Visual drag-and-drop designer for Touch Bar presets"
   homepage "https://github.com/josmanvis/mtmr-designer"
