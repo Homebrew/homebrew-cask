a788867c10704e22e5a5e29ad6bddbd1e03deccc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 04:19:44 +0000

    cncjs 1.11.0

--- Casks/c/cncjs.rb
+++ Casks/c/cncjs.rb
@@ -1,9 +1,9 @@
 cask "cncjs" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.10.9"
-  sha256 arm:   "18318b0327f30e7d2d403328777764ffc75e7eb08f03a1a4d814e6e0477d1c24",
-         intel: "e17b3318755451f9c6277ee936450df0b31cbc940d97b3200676b0b743a91530"
+  version "1.11.0"
+  sha256 arm:   "8a46dcedc78cf8ac89dadbe47c9ec6c0eae252300a2eeebf0c84da0b28c32f48",
+         intel: "3793b520dbacbf97b1e137a6e6bf1b62e71e70b0f01f02b80ea2a2cc9726abd8"
 
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-macos-#{arch}.dmg",
       verified: "github.com/cncjs/cncjs/"
