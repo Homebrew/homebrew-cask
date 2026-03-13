18a85952b9d021b286f53313e7ec46160f8d5c37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 19:17:08 +0000

    keyguard 2.5.0

--- Casks/k/keyguard.rb
+++ Casks/k/keyguard.rb
@@ -1,9 +1,9 @@
 cask "keyguard" do
   arch arm: "apple", intel: "intel"
 
-  version "2.4.1,20260228.1"
-  sha256 arm:   "7049a81d77a4cf61f601273fe384e6ad5c967a89147c8e95365b0b0d88dd0e43",
-         intel: "3c37b5951e0a9d7999a5247f276588fc665c41e35011b7731ce203342d2a56b8"
+  version "2.5.0,20260313.1"
+  sha256 arm:   "37435af943c8b8bab670f4b84e7d5c35d0df05d6f2cefc1c9d0c1c3dc0502d2e",
+         intel: "ec5f6b0a5c8cb9777498a20747a3e25091302034a5fe00fa346b086bf5711531"
 
   url "https://github.com/AChep/keyguard-app/releases/download/r#{version.csv.second}/Keyguard-#{version.csv.first}-#{arch}.dmg"
   name "Keyguard"
