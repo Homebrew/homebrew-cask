67cd92f1091aec9df1f6db13a81c69dddd91244d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 19:27:51 +0000

    taphouse 1.5.0.1

--- Casks/t/taphouse.rb
+++ Casks/t/taphouse.rb
@@ -1,6 +1,6 @@
 cask "taphouse" do
-  version "1.5.0"
-  sha256 "f2f923b248783d48eb5708d2c5ceae8925d2f915c92b48d3d2cb0459a708fac8"
+  version "1.5.0.1"
+  sha256 "4c2cb63a711620f426949f09300d49e8195ef8d5f7750a2772244aa65291ee65"
 
   url "https://taphouse.multimodalsolutions.gr/downloads/Taphouse-#{version}.dmg"
   name "Taphouse"
