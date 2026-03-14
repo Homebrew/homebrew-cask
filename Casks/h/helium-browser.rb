0a505e49a9d997a756a6cf7077cc23b24a0a1587
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 07:06:37 +0000

    helium-browser 0.10.4.1

--- Casks/h/helium-browser.rb
+++ Casks/h/helium-browser.rb
@@ -1,9 +1,9 @@
 cask "helium-browser" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.10.2.1"
-  sha256 arm:   "4df95a010960b0207e7e0a40f506e3ff9b7bc7620c1748171823f5f4c6f2c2cd",
-         intel: "a3f3fcb1f1b2b45fbd6ec596821cbad95826b36ed119214b96406e48e306ee07"
+  version "0.10.4.1"
+  sha256 arm:   "377ddda5ade58f6e2dd3dfe0bd8fb9a5735055e3572a1699970644cf578ff55e",
+         intel: "18b61cb134a582d7fce3270e29a12155d525bef3cda0dacc5ac297718afc3fef"
 
   url "https://github.com/imputnet/helium-macos/releases/download/#{version}/helium_#{version}_#{arch}-macos.dmg",
       verified: "github.com/imputnet/helium-macos/"
