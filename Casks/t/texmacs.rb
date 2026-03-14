94a9e761924b693b9609b84847ea5d04a4dd71ed
Klaus Hipp <khipp@users.noreply.github.com>
2026-03-14 21:34:29 +0100

    texmacs 2.1.5

--- Casks/t/texmacs.rb
+++ Casks/t/texmacs.rb
@@ -1,11 +1,11 @@
 cask "texmacs" do
-  arch arm: "-arm"
+  arch arm: "arm", intel: "intel"
 
-  version "2.1.4"
-  sha256 arm:   "71bd118626d78adaaeeb06bbe083b5e2702a5b1db433531b93d3d2211d229171",
-         intel: "ac5127eb5a2fa4ec32dfae403e96e9223c6fd227b115626b60925944a3cc2257"
+  version "2.1.5"
+  sha256 arm:   "a2eee6a1cb47bfa8f770fc8160e871e882e29588befd3f639cd3885e62a3ae4a",
+         intel: "feeb117943e6269e6b6f201d782e17f4bf80fbef9483ffb95b3c0ab363fd2636"
 
-  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}#{arch}.dmg"
+  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}-#{arch}-os11.0.dmg"
   name "GNU TeXmacs"
   desc "Scientific editing platform"
   homepage "https://www.texmacs.org/"
@@ -14,6 +14,9 @@ cask "texmacs" do
     url "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
     strategy :sparkle
   end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "TeXmacs.app"
 
