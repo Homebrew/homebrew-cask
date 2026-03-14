6a63f03102f2896ab9c070255942bb409162d360
Kevin Bimonte <kbimonte@gmail.com>
2026-03-14 19:50:29 -0400

    prodx: switch fly to arm version

--- Casks/f/fly.rb
+++ Casks/f/fly.rb
@@ -1,8 +1,8 @@
 cask "fly" do
   version "8.1.0"
-  sha256 "328a408c4d9ac4ec8d576e6b69bb716b027927eb3b529a318694e33a866f7921"
+  sha256 "d2bc8ef99b8cf92e403b34f6e17faa98a1501f6cef2a3d216c387e7bad53bd24"
 
-  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
+  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-arm64.tgz"
   name "fly"
   desc "Official CLI tool for Concourse CI"
   homepage "https://github.com/concourse/concourse"
@@ -12,8 +12,4 @@ cask "fly" do
   binary "fly"
 
   # No zap stanza required
-
-  caveats do
-    requires_rosetta
-  end
 end
