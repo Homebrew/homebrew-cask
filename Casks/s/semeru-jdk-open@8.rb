03889b3b567fa7f4fbf85ab5b5f04cbe38f645ac
Klaus Hipp <khipp@users.noreply.github.com>
2026-03-14 18:03:55 +0100

    semeru-jdk-open@8 8.0.482.1

--- Casks/s/semeru-jdk-open@8.rb
+++ Casks/s/semeru-jdk-open@8.rb
@@ -1,22 +1,30 @@
 cask "semeru-jdk-open@8" do
-  version "8u482-b08,openj9-0.57.0"
-  sha256 "b63d78a08d1bebb87cc4215288701eb6b4aa77733c3fb9eb57a4c3fe8070f563"
+  version "8.0.482.1,8u482,b08.1,openj9-0.57.0"
+  sha256 "3273fca81b8a06265cc9ca6d973785adbe4bcc995798009da7c2ef07e2a3f972"
 
-  url "https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("-", "")}_#{version.csv.second}.pkg",
+  url "https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk#{version.csv.second}-#{version.csv.third}_#{version.csv.fourth}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first}.pkg",
       verified: "github.com/ibmruntimes/semeru8-binaries/"
   name "IBM Semeru Runtime (JDK 8) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
-  homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
+  homepage "https://developer.ibm.com/languages/semeru-runtimes/"
 
   livecheck do
     url :url
-    regex(/^(?:jdk)?(\d+u\d+)[._-](b\d+)[._-](.+?)$/i)
+    regex(%r{
+      /(?:jdk)?(\d+u\d+)[._-](b(?:\d+(?:\.\d+)*))[._-]([^/]+)/
+      ibm[._-]semeru[._-]open[._-]jdk[._-]x64[._-]mac[._-]v?(\d+(?:\.\d+)+)\.pkg
+    }ix)
     strategy :github_latest do |json, regex|
-      json["tag_name"]&.scan(regex)&.map { |match| "#{match[0]}-#{match[1]},#{match[2]}" }
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        "#{match[4]},#{match[1]},#{match[2]},#{match[3]}"
+      end
     end
   end
 
-  pkg "ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("-", "")}_#{version.csv.second}.pkg"
+  pkg "ibm-semeru-open-jdk_x64_mac_#{version.csv.first}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.8.jdk"
 
