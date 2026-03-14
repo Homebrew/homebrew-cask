0c106b96d6f8600f93ccc0f5c7b4ce893e44a4a1
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-14 09:55:31 -0400

    proton-drive: downgrade to 2.10.3
    It was brought to my attention that the `proton-drive` appcast uses
    separate `gradual-rollout` and `stable` channels. The upstream
    download page serves the stable version but autobump recently updated
    the cask to the `gradual-rollout` version, 2.11.1, because it's the
    newest release in the appcast.
    
    This downgrades the cask to the newest stable version, 2.10.3, and
    modifies the `livecheck` block to track the newest version in the
    `stable` channel.

--- Casks/p/proton-drive.rb
+++ Casks/p/proton-drive.rb
@@ -1,15 +1,20 @@
 cask "proton-drive" do
-  version "2.11.1"
-  sha256 "004878bb816f86627d80f48c3f3fce0870806aba45a17ff62d5617d42fcf25cf"
+  version "2.10.3"
+  sha256 "7ccf585786a66f6173aec359cf5b91c59c7c4db7aa4268a9f96618dd9afd213a"
 
   url "https://proton.me/download/drive/macos/#{version}/ProtonDrive-#{version}.dmg"
   name "Proton Drive"
   desc "Client for Proton Drive"
   homepage "https://proton.me/drive"
 
+  # The appcast publishes versions in `gradual-rollout` and `stable` channals.
+  # The upstream download page serves the `stable` version, so we track the
+  # `stable` channel.
   livecheck do
     url "https://proton.me/download/drive/macos/appcast.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle do |items|
+      items.find { |item| item.channel == "stable" }&.short_version
+    end
   end
 
   auto_updates true
