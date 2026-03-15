ed2d244dc3d55948f54490df904c6289c07dda0b
Srinivas Garisha <95307763+Ssri7774@users.noreply.github.com>
2026-03-16 10:52:50 +1100

    Update Ulaa version to 2.41.1 and SHA256 hashes

--- Casks/u/ulaa.rb
+++ Casks/u/ulaa.rb
@@ -1,11 +1,12 @@
 cask "ulaa" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.41.0"
-  sha256 arm:   "b921095dcedaebf3465381c2fe32909f8b49fb6f8b88ba06ba247291b233b51e",
-         intel: "f92815f7f64197604422bd37f3ff1aba44fc047d1272ac044860b18f69d5cbdf"
+  version "2.41.1"
+  sha256 arm:   "598e43ac5c0f349e8cdd1dce2159b633e5d884d0dc1750daca2e1f6ca73af2fc",
+         intel: "25bdcf7f2c4dcc9fcbfa16551009da42128ab5bcaf833eb0e5fd655cfcefe789"
 
-  url "https://downloads.zohocdn.com/ulaa-browser/release/mac/stable/#{arch}/Ulaa-Browser-v#{version}-#{arch}.dmg"
+  url "https://downloads.zohocdn.com/ulaa-browser/release/mac/stable/#{arch}/Ulaa-Browser-v#{version}-#{arch}.dmg",
+      verified: "downloads.zohocdn.com/ulaa-browser/"
   name "Ulaa Browser"
   desc "Privacy-centric browser with advanced tracking protection"
   homepage "https://ulaa.com/"
