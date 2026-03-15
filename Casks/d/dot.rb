737abebf11fe6b2ef20c46abcc3b3a784841bae0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-15 21:50:36 +0000

    dot 2.2.0

--- Casks/d/dot.rb
+++ Casks/d/dot.rb
@@ -1,6 +1,6 @@
 cask "dot" do
-  version "2.1.1"
-  sha256 "36f78239aadf728f001116a1e6d9281ccd0df75a9f730de0641b327b35c97992"
+  version "2.2.0"
+  sha256 "7338ea500dd253b2767bfb5a95dc76b0ea291b512cb162497cf80f3b2edd2baf"
 
   url "https://github.com/prateekkeshari/dot-releases/releases/download/v#{version}/Dot-#{version}.dmg",
       verified: "github.com/prateekkeshari/dot-releases/"
