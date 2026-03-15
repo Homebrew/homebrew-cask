1bddd61ac549346ed66f66648a23b22ec2f6fe88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-15 16:07:10 +0000

    launchcontrol 2.10.3

--- Casks/l/launchcontrol.rb
+++ Casks/l/launchcontrol.rb
@@ -8,8 +8,8 @@ cask "launchcontrol" do
     end
   end
   on_big_sur :or_newer do
-    version "2.10.2"
-    sha256 "b76a2127b181371a9ce91ea1796f00380cdf9d29e54bc8727b95be4848ef51b1"
+    version "2.10.3"
+    sha256 "433f53f9fd9d006496e3cba7e3f8e9bda3cff8bcd00421989b0149db23838fcb"
 
     livecheck do
       url "https://www.soma-zone.com/LaunchControl/a/appcast-update-#{version.major}.xml"
