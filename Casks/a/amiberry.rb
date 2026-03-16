4bdb811a2df1efb13f635f6ec3ebb24b18653266
Dimitris Panokostas <midwan@gmail.com>
2026-03-16 08:57:50 +0100

    amiberry 8.0.0: use DMG directly instead of nested zip

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -1,8 +1,8 @@
 cask "amiberry" do
   version "8.0.0"
-  sha256 "6a40360afb5fa391445ade177815e711677b8f83adf105897cf104c7e4bf37f0"
+  sha256 "701b75af6d0e612fa77d476ad0aa81ae1fcef85c2e95615177fad7c2b4c8fa44"
 
-  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-v#{version}-macOS-universal.zip",
+  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-#{version}-Darwin.dmg",
       verified: "github.com/BlitterStudio/amiberry/"
   name "Amiberry"
   desc "Amiga emulator"
@@ -12,8 +12,6 @@ cask "amiberry" do
     url :url
     strategy :github_latest
   end
-
-  container nested: "Amiberry-#{version}-Darwin.dmg"
 
   app "Amiberry.app"
 
