1169a425086a4b7f7c74f47a0c40920ab397df47
Dimitris Panokostas <midwan@gmail.com>
2026-03-16 08:50:17 +0100

    amiberry 8.0.0

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -1,11 +1,8 @@
 cask "amiberry" do
-  arch arm: "apple-silicon", intel: "x86_64"
+  version "8.0.0"
+  sha256 "6a40360afb5fa391445ade177815e711677b8f83adf105897cf104c7e4bf37f0"
 
-  version "7.1.1"
-  sha256 arm:   "6df092a58bc78f06409b0de6d3aaa3bddd9577b0df8554443e8a2117369c2e67",
-         intel: "6e12ae55a2d791aa009a384c707142b5d3a5da2e6df9289a69fa5aafa927ce0d"
-
-  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-v#{version}-macOS-#{arch}.zip",
+  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-v#{version}-macOS-universal.zip",
       verified: "github.com/BlitterStudio/amiberry/"
   name "Amiberry"
   desc "Amiga emulator"
@@ -15,6 +12,8 @@ cask "amiberry" do
     url :url
     strategy :github_latest
   end
+
+  container nested: "Amiberry-#{version}-Darwin.dmg"
 
   app "Amiberry.app"
 
