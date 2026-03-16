d490c9a34faf4a12dd2543e4e5a419bda267b9da
Sam <sammy_kramer@icloud.com>
2026-03-16 12:23:42 -0400

    voicemod 3.15.19

--- Casks/v/voicemod.rb
+++ Casks/v/voicemod.rb
@@ -1,17 +1,23 @@
 cask "voicemod" do
-  version "1.6.6"
-  sha256 "eb9a2c46703e94632d785b7473db633dffc62b4b0592b96ef44829d8995c0ea4"
+  arch arm: "AppleSilicon", intel: "Intel"
 
-  url "https://flagship-versions.voicemod.net/#{version}/Voicemod_#{version}-AppleSilicon.pkg"
+  version "3.15.19"
+  sha256 arm:   "41ba859605c517ab6865f36010a8f75b96b0a7a097a871eceb7252b05161d6f0",
+         intel: "243a10081f98389ffd11859202ce5e0bf8f525c51a1bb343e4d4c6d99892bd85"
+
+  url "https://flagship-versions.voicemod.net/#{version}/Voicemod_#{version}-#{arch}.pkg"
   name "Voicemod"
   desc "Real-time voice changer and soundboard"
   homepage "https://www.voicemod.net/"
 
   livecheck do
-    skip "No version information available"
+    url "https://api.voicemod.net/v1/nextgen/update/{}"
+    strategy :json do |json|
+      json["appVersion"]
+    end
   end
 
-  pkg "Voicemod_#{version}-AppleSilicon.pkg"
+  pkg "Voicemod_#{version}-#{arch}.pkg"
 
   uninstall pkgutil: "net.voicemod.voicemod"
 
