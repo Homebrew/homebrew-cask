171030c7738feac6574bd97746e9a477a12a63c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 21:42:25 +0000

    aks-desktop 0.3.0-beta

--- Casks/a/aks-desktop.rb
+++ Casks/a/aks-desktop.rb
@@ -1,9 +1,9 @@
 cask "aks-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.0-alpha"
-  sha256 arm:   "52036ea4eee782b9f55313a59cae50744feb211bd2913e5b13ae48aec6f21f04",
-         intel: "0bd98cb146d09e4c7208ec320416374efe33ccb8901578b92ba43194b035b81e"
+  version "0.3.0-beta"
+  sha256 arm:   "b9ec490ce407cf84f778b8e80d4393059b7344834faeeb6ac36ac98765681313",
+         intel: "c57d9645d4c000d3fa0a7528a88f6032d153a0a0679ae5038bf0049215f423a7"
 
   url "https://github.com/Azure/aks-desktop/releases/download/v#{version}/aks-desktop-#{version}-mac-#{arch}.dmg"
   name "AKS desktop"
