c2059a8cb80268e5705fab15352f0ad4a179b2db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 13:32:35 +0000

    owocr 1.26.4

--- Casks/o/owocr.rb
+++ Casks/o/owocr.rb
@@ -1,9 +1,9 @@
 cask "owocr" do
   arch arm: "applesilicon", intel: "intel"
 
-  version "1.26.3"
-  sha256 arm:   "bc0f4325569bd1f1d14ab08c5cd892d0069ea639913ed028622968f70c26b5f3",
-         intel: "b485261c64473025e9ce76bb5a5b597f5570deea3e272c5f68cd4205b8a82835"
+  version "1.26.4"
+  sha256 arm:   "bd2c1af491c33af3c0bf3224614d282a8f25130bbc026094c0f08ded9063a8d0",
+         intel: "8eaea49f77f193422a06dc1ce4ebd2833915c081d5a2bf5915792132ffa64fca"
 
   url "https://github.com/AuroraWright/owocr/releases/download/#{version}/owocr-mac_#{arch}.dmg"
   name "OwOCR"
