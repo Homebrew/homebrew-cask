7a1e9e3ca0bc14fdc54cdc96b2fd69b6c73cd788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 01:27:37 +0000

    melodics 5.0.95

--- Casks/m/melodics.rb
+++ Casks/m/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "5.0.79,CB2FE6AB-27B3-44D9-8A21-1301283E8BEE"
-  sha256 "97d4453044cea00b8408fe42fd2c0842cc650af5c2e5a402f23e00052ec5f038"
+  version "5.0.95,1F0974AD-90E0-442D-9829-820F63C698F1"
+  sha256 "7c8f3031808166712c1cc6ab4c68a08eb1cd58578b81dd32e3aa1ecd3f4052c1"
 
   url "https://web-cdn.melodics.com/download/#{version.csv.second}.zip"
   name "Melodics"
