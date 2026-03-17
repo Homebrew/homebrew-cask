b34b74e48f309541f7dbb781153f98cc2aa13706
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 21:44:01 +0000

    archy 2.38.0

--- Casks/a/archy.rb
+++ Casks/a/archy.rb
@@ -1,6 +1,6 @@
 cask "archy" do
-  version "2.37.0"
-  sha256 "f1955e668c58b9391954193c41cea0423f4c42d9156f29520b60bd843a69b41d"
+  version "2.38.0"
+  sha256 "7b089520d27125a528189da49a5a14416ccab1f83a2fbb6372f9bbf78bbf360d"
 
   url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
       verified: "sdk-cdn.mypurecloud.com/archy/"
