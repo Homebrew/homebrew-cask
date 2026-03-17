c70b1450893b226a40360665fd3f453d3c32d846
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 07:12:55 +0000

    handy 0.7.11

--- Casks/h/handy.rb
+++ Casks/h/handy.rb
@@ -1,9 +1,9 @@
 cask "handy" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.7.10"
-  sha256 arm:   "d253476609d62936d345e75414f3fc8d9e932b119d872ac853a6b27cc50223e4",
-         intel: "357c67f467423b050f2385942280cf50dbf6953494a1e94f0a091b6229ae458c"
+  version "0.7.11"
+  sha256 arm:   "7f929c3f288bd599034435cd473d83b03af64e1ed7e54733ea225801bbd74e8c",
+         intel: "1e7c68e6f3d29483a4b6545404530a25e26b57ff93e469b3955385c6676cfda6"
 
   url "https://github.com/cjpais/Handy/releases/download/v#{version}/Handy_#{version}_#{arch}.dmg",
       verified: "github.com/cjpais/Handy/"
