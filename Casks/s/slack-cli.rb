dfa9a6aededed8d7183e2776eb508f99d235102c
Bevan Kay <bevanjkay@gmail.com>
2026-03-18 17:33:10 +0800

    slack-cli: rework

--- Casks/s/slack-cli.rb
+++ Casks/s/slack-cli.rb
@@ -3,19 +3,23 @@ cask "slack-cli" do
   os macos: "macOS", linux: "linux"
 
   version "3.14.0"
-  sha256 arm:          "f52829396dd6ecc875a026a6ab5944e8d2129ceef6ff9e534ae9e96e17a3e1c8",
-         intel:        "effdbe158444419de38923d44c3147982b1425c108b5ee604b0a504bf91b3f7e",
-         x86_64_linux: "9f4bce2953a227208eb343ae098154a4489cb31a03dc73df05cb4ccb0270581d"
 
-  url do
-    on_macos do
-      "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_#{arch}.tar.gz"
-    end
-
-    on_linux do
-      "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_64-bit.tar.gz"
-    end
+  on_macos do
+    url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_#{arch}.tar.gz",
+        verified: "downloads.slack-edge.com/"
+    sha256 arm:   "f52829396dd6ecc875a026a6ab5944e8d2129ceef6ff9e534ae9e96e17a3e1c8",
+           intel: "effdbe158444419de38923d44c3147982b1425c108b5ee604b0a504bf91b3f7e"
   end
+
+  on_linux do
+    sha256 "9f4bce2953a227208eb343ae098154a4489cb31a03dc73df05cb4ccb0270581d"
+
+    url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_64-bit.tar.gz",
+        verified: "downloads.slack-edge.com/"
+
+    depends_on arch: :x86_64
+  end
+
   name "Slack CLI"
   desc "CLI to create, run, and deploy Slack apps"
   homepage "https://docs.slack.dev/tools/slack-cli/"
@@ -26,8 +30,6 @@ cask "slack-cli" do
       json.dig("slack-cli", "releases")&.map { |release| release["version"] }
     end
   end
-
-  depends_on arch: :x86_64 if OS.linux? # No arm64 build for Linux yet
 
   binary "bin/slack"
 
