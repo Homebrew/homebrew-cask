f747ead469dccee03d9068517f1f37c33479891d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 04:19:49 +0000

    cog-app 3414

--- Casks/c/cog-app.rb
+++ Casks/c/cog-app.rb
@@ -1,6 +1,6 @@
 cask "cog-app" do
-  version "3409,b29414436"
-  sha256 "01a1890c9399bdba0fe413040d0b91af362e1cbc2f4a774dd9b422aa822ae5cb"
+  version "3414,2d46d7fa0"
+  sha256 "28aed8c09a0a5129984215d0f51695ffe5d2eeaaa65b1321ad07258aca1bbaa3"
 
   url "https://cogcdn.cog.losno.co/Cog-#{version.csv.second}.zip"
   name "Cog"
