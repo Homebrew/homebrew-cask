bf108b456e837f833cc9a6d061710cd7e1583893
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 15:50:44 +0000

    android-studio-preview@beta 2025.3.3.5

--- Casks/a/android-studio-preview@beta.rb
+++ Casks/a/android-studio-preview@beta.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview@beta" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2025.3.2.5,panda2-rc1"
-  sha256 arm:   "7150779f62a71a8540d4928901e540c7413f715b7b45619e70c32fa89d5f5f33",
-         intel: "5bcf7bb17b7e6201281d3d64858e7ebe5769c78d49655bd7d82000ab18549b5c"
+  version "2025.3.3.5,panda3-rc1"
+  sha256 arm:   "039e4421c7ed91f39ee82c7fe872c3f8b8069c90465ae549181951eddda6c41c",
+         intel: "a0b676f2032ae5959f5078b9e7e22c847ea2baa70d75b4ed58a7b10aad973473"
 
   url "https://edgedl.me.gvt1.com/android/studio/install/#{version.csv.first}/android-studio#{"-#{version.csv.second}" if version.csv.second}-#{arch}.dmg",
       verified: "edgedl.me.gvt1.com/android/studio/install/"
