9fa15bc129ed10e1a3a6e053050df2d638630196
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 13:33:21 +0000

    plaud 1.0.10

--- Casks/p/plaud.rb
+++ Casks/p/plaud.rb
@@ -1,9 +1,9 @@
 cask "plaud" do
   arch arm: "-arm64"
 
-  version "1.0.9"
-  sha256 arm:   "9600d932fec42d0d78cb482597ac7dac9ed7b356aa3c1797babcc4b092632787",
-         intel: "f89c6a1597de84f9652d6fd71ff50f9d822eb063ac90a5e23232e1a6e162ff47"
+  version "1.0.10"
+  sha256 arm:   "d116a54721434d33ee699407ce9818bcb6e510a9b4fd882d865d034731971ef5",
+         intel: "7a5ae7b891f76d2f4bdf45d522ddc05574d809ef5baae3eeff55aed872d23416"
 
   url "https://client-download.plaud.ai/desktop/v#{version.major}/Plaud-#{version}#{arch}.dmg"
   name "Plaud"
