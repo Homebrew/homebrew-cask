5f925e330081d9311a317e3572d9c313fa8ae0d5
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-19 09:40:01 -0400

    canon-ufrii-driver 0.19.24
    `canon-ufrii-driver` is autobumped but the workflow failed to update to
    version 0.19.24 because the year in the asset URL path is now 2026
    instead of 2025. This updates the version and adjusts the cask to
    include the year from the URL in the `version`, as this changes over
    time.

--- Casks/c/canon-ufrii-driver.rb
+++ Casks/c/canon-ufrii-driver.rb
@@ -1,8 +1,8 @@
 cask "canon-ufrii-driver" do
-  version "10.19.23"
-  sha256 "aa6cb2d87c2c16baf890dd14bedf9fa3a2f4cc6d990dd247c050bb153d13b5a5"
+  version "10.19.24,2026"
+  sha256 "8681b6aab54f09938497f625b4f43048d26a09497c52f565976dac96d233d621"
 
-  url "https://downloads.canon.com/sss2025/drivers/UFRII_v#{version}_mac.zip",
+  url "https://downloads.canon.com/sss#{version.csv.second}/drivers/UFRII_v#{version.csv.first}_mac.zip",
       verified: "downloads.canon.com/"
   name "Canon UFR II/UFRII LT/LIPSLX/CARPS2 Printer Driver"
   desc "Printer driver for Canon imageRUNNER office printers"
@@ -11,13 +11,13 @@ cask "canon-ufrii-driver" do
   livecheck do
     url "https://www.usa.canon.com/bin/canon/support/getsoftwarediver.ds.MACOS_14.39319.All.English.json",
         user_agent: :browser
-    regex(/UFRII[._-]v?(\d+(?:\.\d+)+)[._-]mac\.zip/i)
+    regex(%r{(\d+)/drivers/UFRII[._-]v?(\d+(?:\.\d+)+)[._-]mac\.zip}i)
     strategy :json do |json, regex|
       json.map do |item|
         match = item["fileUrl"]&.match(regex)
         next if match.blank?
 
-        match[1]
+        "#{match[2]},#{match[1]}"
       end
     end
   end
