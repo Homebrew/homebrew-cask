fa8f3c228c4a48da65289cbf7deaee2ec657d700
pHequals7 <ce17b115@smail.iitm.ac.in>
2026-03-19 11:54:54 +0530

    muesli: fix description, alphabetize zap, correct sha256

--- Casks/m/muesli.rb
+++ Casks/m/muesli.rb
@@ -1,11 +1,11 @@
 cask "muesli" do
   version "0.5.3"
-  sha256 "b33748a2bb05009a7aa4d805605239e9ce10c410562f3534455ed9777ee7c115"
+  sha256 "a82d2099d24c4ae507a20f1504b64d93b5d7a22b5d13967d2af596b8d964cc02"
 
   url "https://github.com/pHequals7/muesli/releases/download/v#{version}/Muesli-#{version}.dmg",
       verified: "github.com/pHequals7/muesli/"
   name "Muesli"
-  desc "Local-first macOS dictation and meeting transcription on Apple Silicon"
+  desc "Local-first dictation and meeting transcription"
   homepage "https://freedspeech.xyz/"
 
   livecheck do
@@ -19,7 +19,7 @@ cask "muesli" do
   app "Muesli.app"
 
   zap trash: [
-    "~/Library/Application Support/Muesli",
     "~/.cache/muesli",
+    "~/Library/Application Support/Muesli",
   ]
 end
