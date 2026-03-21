34e667a4146a6d885d9895bea87bf1e81c3bdf86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 01:19:53 +0000

    impactor 2.1.2

--- Casks/i/impactor.rb
+++ Casks/i/impactor.rb
@@ -1,6 +1,6 @@
 cask "impactor" do
-  version "2.1.1"
-  sha256 "99fc87c65a7b2a0b12cf90b8ddfeac83a4d2d86e7c5fc9791d3d441194feffdd"
+  version "2.1.2"
+  sha256 "a6db7ba92186587a64229f493b6987ce2a90f0668ad3763e5f305df1e6f8b75a"
 
   url "https://github.com/khcrysalis/Impactor/releases/download/v#{version}/Impactor-macos-universal.dmg"
   name "Impactor"
