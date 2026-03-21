e02f4ca4278ad054b6a59bfbbd573f9d323f48f3
Bevan Kay <email@bevankay.me>
2026-03-21 20:53:48 +1100

    futurerestore-gui: deprecate

--- Casks/f/futurerestore-gui.rb
+++ Casks/f/futurerestore-gui.rb
@@ -12,6 +12,8 @@ cask "futurerestore-gui" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   app "FutureRestore GUI.app"
 
   zap trash: "~/FutureRestoreGUI"
