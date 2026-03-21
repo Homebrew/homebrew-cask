44826f2db9335c54dfbb732f13ab3e4d38a9d887
Bevan Kay <email@bevankay.me>
2026-03-21 10:04:39 +0800

    Apply suggestions from code review
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/n/nugget.rb
+++ Casks/n/nugget.rb
@@ -1,12 +1,11 @@
 cask "nugget" do
-  arch arm: "arm64", intel: "x86_64"
+  arch arm: "arm", intel: "intel"
 
   version "7.3"
   sha256 arm:   "ae0f43b70e7a76cbbd8a93052143dd48f8be42f33aad983fa630c0a92350077c",
          intel: "5bbe1951f22cd0c3f803fb8696555c005102811ef06116799d9d5795bbf4ec05"
 
-  url arm:   "https://github.com/leminlimez/Nugget/releases/download/v#{version}/Nugget_macOS_arm.dmg",
-      intel: "https://github.com/leminlimez/Nugget/releases/download/v#{version}/Nugget_macOS_intel.dmg"
+  url "https://github.com/leminlimez/Nugget/releases/download/v#{version}/Nugget_macOS_#{arch}.dmg"
   name "Nugget"
   desc "Customise your iOS device with animated wallpapers, disable daemons and more"
   homepage "https://github.com/leminlimez/Nugget"
