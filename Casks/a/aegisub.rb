16c4a5ae48b6e26bad6d0d3a710859e5b761a53c
Bevan Kay <email@bevankay.me>
2026-03-21 10:08:20 +0800

    aegisub: remove `url` verified parameter

--- Casks/a/aegisub.rb
+++ Casks/a/aegisub.rb
@@ -2,8 +2,7 @@ cask "aegisub" do
   version "3.4.2"
   sha256 "cbbfd3276e0414b540f6b1bc12a69abd6b8a96b0a452de3b08c290d553754ad3"
 
-  url "https://github.com/TypesettingTools/Aegisub/releases/download/v#{version}/Aegisub-#{version}.dmg",
-      verified: "github.com/TypesettingTools/Aegisub/"
+  url "https://github.com/TypesettingTools/Aegisub/releases/download/v#{version}/Aegisub-#{version}.dmg"
   name "Aegisub"
   desc "Create and modify subtitles"
   homepage "https://github.com/TypesettingTools/Aegisub/"
