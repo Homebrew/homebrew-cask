bb29a7bac569e1901d9fe5f93e725404dd5af344
Bevan Kay <email@bevankay.me>
2026-03-21 21:00:58 +1100

    gingko: deprecate

--- Casks/g/gingko.rb
+++ Casks/g/gingko.rb
@@ -13,6 +13,8 @@ cask "gingko" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   app "Gingko.app"
 
   zap trash: "~/Library/Application Support/Gingko"
