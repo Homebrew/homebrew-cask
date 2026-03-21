66a51118b952fec18642dce22a64521feb74f59f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 18:42:22 +0000

    apidog-europe 2.8.18

--- Casks/a/apidog-europe.rb
+++ Casks/a/apidog-europe.rb
@@ -2,9 +2,9 @@ cask "apidog-europe" do
   arch arm: "-macOS-arm64"
   livecheck_folder = on_arch_conditional arm: "-arm64"
 
-  version "2.8.15"
-  sha256 arm:   "101f48474779e13e7d75844f323d19f092157f690e4f23131aff1b59aa7a68a1",
-         intel: "a8a79d053c9546351bdbacf63ae90908fa233bdad047d6f4e46958a28eaff66e"
+  version "2.8.18"
+  sha256 arm:   "be731856ead5b9806b262da070d4066ba904159a82c6332a91ccd5e76fc141a9",
+         intel: "d755352018777dd503a18b500a005a3b96910ea120b184590cea732bf80ef636"
 
   url "https://file-assets.eu.apidog.com/download/#{version}/Apidog%20Europe#{arch}-#{version}.dmg"
   name "Apidog Europe"
