eab33a36dc416b4b9f989bbb0ea6b21499811755
Bevan Kay <email@bevankay.me>
2026-03-21 09:37:01 +0800

    ace-link: fix typo

--- Casks/a/ace-link.rb
+++ Casks/a/ace-link.rb
@@ -12,7 +12,7 @@ cask "ace-link" do
     strategy :github_latest
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeer_check
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on cask: "docker"
 
