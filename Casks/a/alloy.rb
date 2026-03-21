c4eb541c16a5323a4bd7666e9565451b740cedb8
Bevan Kay <email@bevankay.me>
2026-03-21 10:00:34 +0800

    alloy: add depends_on

--- Casks/a/alloy.rb
+++ Casks/a/alloy.rb
@@ -18,6 +18,8 @@ cask "alloy" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
+  depends_on macos: ">= :ventura"
+
   app "Alloy.app"
 
   zap trash: "~/Library/Saved Application State/org.alloytools.alloy.savedState"
