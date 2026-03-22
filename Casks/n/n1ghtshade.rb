8837aa09e9e5818986442b93f7190746f8ca7d66
Bevan Kay <email@bevankay.me>
2026-03-22 22:46:38 +1100

    n1ghtshade: deprecate

--- Casks/n/n1ghtshade.rb
+++ Casks/n/n1ghtshade.rb
@@ -13,6 +13,8 @@ cask "n1ghtshade" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on formula: %w[
     libimobiledevice
     libirecovery
