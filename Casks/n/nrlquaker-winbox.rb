b3e234925cd2add70dd92699e45aa8ab29ce1585
Bevan Kay <email@bevankay.me>
2026-03-22 23:29:26 +1100

    nrlquaker-winbox: deprecate

--- Casks/n/nrlquaker-winbox.rb
+++ Casks/n/nrlquaker-winbox.rb
@@ -12,6 +12,8 @@ cask "nrlquaker-winbox" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   app "Winbox-mac.app"
 
   zap trash: "~/Library/Application Support/com.mikrotik.winbox"
