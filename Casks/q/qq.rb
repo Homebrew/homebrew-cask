dc64bcf1ee24428dd970c156598fc4da3a4f9f61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 10:23:56 +0000

    qq 6.9.91_260319_01

--- Casks/q/qq.rb
+++ Casks/q/qq.rb
@@ -1,6 +1,6 @@
 cask "qq" do
-  version "6.9.89_260205_01"
-  sha256 "197b19823d2158d911eb9e06e46279798d0ba9bac8b63c67d2981d6247bd29a9"
+  version "6.9.91_260319_01"
+  sha256 "a42e20f5528c306f52e88ec69508a7f122d64a78bd47689df7e83d35503bf8e0"
 
   url "https://dldir1v6.qq.com/qqfile/qq/QQNT/Mac/QQ_#{version}.dmg"
   name "QQ"
