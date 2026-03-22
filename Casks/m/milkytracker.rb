1a27b64f7cc7bceef15c57ff0046d6c221765c6d
Bevan Kay <email@bevankay.me>
2026-03-22 18:43:47 +1100

    milkytracker: deprecate

--- Casks/m/milkytracker.rb
+++ Casks/m/milkytracker.rb
@@ -13,6 +13,8 @@ cask "milkytracker" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   app "MilkyTracker.app"
 
   zap trash: "~/Library/Preferences/com.Titan.MilkyTracker.plist"
