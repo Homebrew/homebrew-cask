0ddeb6ab6de5b0b924838c3bd5cab7b925573075
Bevan Kay <email@bevankay.me>
2026-03-22 23:29:55 +1100

    nulloy: deprecate

--- Casks/n/nulloy.rb
+++ Casks/n/nulloy.rb
@@ -13,6 +13,8 @@ cask "nulloy" do
     strategy :github_latest
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   app "Nulloy.app"
 
   zap trash: "~/Library/Saved Application State/com.nulloy.savedState"
