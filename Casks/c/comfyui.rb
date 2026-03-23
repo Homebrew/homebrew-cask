ef0a5ad56693816242c7dc094eb3ef93e56712e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 09:55:48 +0000

    comfyui 0.8.25

--- Casks/c/comfyui.rb
+++ Casks/c/comfyui.rb
@@ -1,6 +1,6 @@
 cask "comfyui" do
-  version "0.8.24,260321brf44qvgs"
-  sha256 "11386fce7a7011efca2122c66f0fc749e70e58e1f52a9f56ce27e19f9117ae84"
+  version "0.8.25,26032337av0wd0b"
+  sha256 "fd15f31e1cd96564262d099cfd81ea1069c1f5fedafb7d3443267e6784983e3b"
 
   url "https://download.todesktop.com/241012ess7yxs0e/ComfyUI%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-arm64-mac.zip",
       verified: "download.todesktop.com/241012ess7yxs0e/"
