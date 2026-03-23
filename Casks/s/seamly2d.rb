75401313e4e384aba80baddad3132c6678ca72ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 05:03:03 +0000

    seamly2d 2026.3.23.214

--- Casks/s/seamly2d.rb
+++ Casks/s/seamly2d.rb
@@ -1,6 +1,6 @@
 cask "seamly2d" do
-  version "2026.3.16.214"
-  sha256 "5e6666b99b2fc8fe6a8cbaa2de4f5bc2b7d6a6e3259046610180bea55b439a0f"
+  version "2026.3.23.214"
+  sha256 "2c6111e824dfcd57656aa49b405fe5f783af338021ba143e04eafd2f1b62edf3"
 
   url "https://github.com/FashionFreedom/Seamly2D/releases/download/v#{version}/Seamly2D-macos.zip",
       verified: "github.com/FashionFreedom/Seamly2D/"
