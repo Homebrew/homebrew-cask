b46b5621ef3a31fca4a5c6bf06a6eaf301b1c6c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 07:48:56 +0000

    universal-gcode-platform 2.1.22

--- Casks/u/universal-gcode-platform.rb
+++ Casks/u/universal-gcode-platform.rb
@@ -1,9 +1,9 @@
 cask "universal-gcode-platform" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.1.21"
-  sha256 arm:   "d0f523188526b93438296ace0d020dbe49154375d35c41181ae064868c76cf85",
-         intel: "ce2f6a826616dae7be1d5c44837d24cf0871321c0e5566cefe6f1f104e5350b4"
+  version "2.1.22"
+  sha256 arm:   "2c43a524b9c544e477e95e5cd0157933ab07e44f8b594127b40f6f9c16b64940",
+         intel: "2f3e45eebef5253d5b9db26081a23afe4fbbab55c59eadee3039ac9b30bf1660"
 
   url "https://github.com/winder/Universal-G-Code-Sender/releases/download/v#{version}/macosx-#{arch}-ugs-platform-app-#{version}.dmg",
       verified: "github.com/winder/Universal-G-Code-Sender/"
