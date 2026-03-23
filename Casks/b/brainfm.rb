507bc6ee9644a3f6cf1c1e7508db93e3a9b98b37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 15:52:52 +0000

    brainfm 0.0.258

--- Casks/b/brainfm.rb
+++ Casks/b/brainfm.rb
@@ -1,9 +1,9 @@
 cask "brainfm" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.252"
-  sha256 arm:   "f9e2a04e48f80adedc006cd5406021a8516c3f5d6c096cd6fb176d1b3dfaf4e3",
-         intel: "dec4f1fdf095171e0e2cf9359ab268e33262d86fd5f0ddfbfdd6fc5e1295d102"
+  version "0.0.258"
+  sha256 arm:   "a8b41272cd2022abc9a1e6069b8ccdd8a0530fa68f55c93120440fc255282fc8",
+         intel: "b0a768a02f5e2bd5744279cf75c2f34be5a2453f0a0b0d87acdf722decea17d2"
 
   url "https://download.brain.fm/darwin/#{arch}/Brain.fm-#{version}-#{arch}.dmg"
   name "Brain.fm"
