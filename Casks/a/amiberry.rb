99322d7f5770ab6480722954f64db5d14197d000
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 21:41:09 +0000

    amiberry 8.1.0

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -1,6 +1,6 @@
 cask "amiberry" do
-  version "8.0.0"
-  sha256 "701b75af6d0e612fa77d476ad0aa81ae1fcef85c2e95615177fad7c2b4c8fa44"
+  version "8.1.0"
+  sha256 "7e7d00e107bf5f6207cd7f40f31d74ebaced5b14444db829f030ef00d1823e0f"
 
   url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-#{version}-Darwin.dmg",
       verified: "github.com/BlitterStudio/amiberry/"
