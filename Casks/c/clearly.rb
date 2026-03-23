cb11afa31530504c8494d308863d8667234d7cae
Aulia L <alionar.13@gmail.com>
2026-03-23 18:56:51 +0700

    clearly 1.6.0 (new cask)

---
+++ Casks/c/clearly.rb
@@ -0,0 +1,26 @@
+cask "clearly" do
+  version "1.6.0"
+  sha256 "33f07501517048edd66c4ad04c4c3e2d00ac996847df842427fc13697efa5dc1"
+
+  url "https://github.com/Shpigford/clearly/releases/download/v#{version}/Clearly.dmg",
+      verified: "github.com/Shpigford/clearly/"
+  name "Clearly"
+  desc "Clean, native markdown editor"
+  homepage "https://clearly.md/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "Clearly.app"
+
+  zap trash: [
+        "~/Library/Application Scripts/com.sabotage.clearly*",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.sabotage.clearly.sfl*",
+        "~/Library/Containers/com.sabotage.clearly*",
+      ],
+      rmdir: "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments"
+end
