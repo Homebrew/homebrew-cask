2a4367c8b6e3c268ce835d37d14a574310521d1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 13:23:00 +0000

    mindjet-mindmanager 25.2.105

--- Casks/m/mindjet-mindmanager.rb
+++ Casks/m/mindjet-mindmanager.rb
@@ -1,6 +1,6 @@
 cask "mindjet-mindmanager" do
-  version "25.1.105"
-  sha256 "ae56b92eac43bef9aa5a06a871b0aae8d8f91c04ea66afef14136335e1e17c70"
+  version "25.2.105"
+  sha256 "99034e012d02e6ffd0f92a08e7b351afbe1940d9f8d6a858cec4d6f7a9568d47"
 
   url "https://download.mindjet.com/MindManager_Mac_#{version}.dmg"
   name "Mindmanager"
