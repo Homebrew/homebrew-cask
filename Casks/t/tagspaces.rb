656b1b2791155983a8a1dc5f956f74d1e0354969
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 10:41:38 +0000

    tagspaces 6.10.3

--- Casks/t/tagspaces.rb
+++ Casks/t/tagspaces.rb
@@ -1,9 +1,9 @@
 cask "tagspaces" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.10.2"
-  sha256 arm:   "63a124f9df87eab058d59a664bcff0c265e5e35f2e5f53227d423fef7e111983",
-         intel: "55a3b37de3389fc12febb21cf277bcc2ce46afe73a2da69da78f51b05c44a44e"
+  version "6.10.3"
+  sha256 arm:   "1ac62a386c0f1e5f53686cbf8201e5ae721ee4acdb24fbb3053223692b26d548",
+         intel: "d5d30c669cbe1bbe306cebbdc9c52545e434fa724e0ebddff4940e8ee18455ba"
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
       verified: "github.com/tagspaces/tagspaces/"
