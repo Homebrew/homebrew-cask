6762459ed82297ddeeb41e221d7cbeafb7969b18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 19:35:27 +0000

    pyfa 2.66.1

--- Casks/p/pyfa.rb
+++ Casks/p/pyfa.rb
@@ -1,6 +1,6 @@
 cask "pyfa" do
-  version "2.66.0"
-  sha256 "c65f1bc33298ad33f29c6f1033e4e4e29e7eb5c0d2efb40ce0ed00ca4695c91f"
+  version "2.66.1"
+  sha256 "538f83e0ab7946f88ef280e77de949c74b469431bf21325e5948bb7f15a1e563"
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   name "pyfa"
