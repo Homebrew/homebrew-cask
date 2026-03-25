0b612f05771b3cb92064949a66e5bb04c0c3bb82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 16:44:52 +0000

    parallels-virtualization-sdk 26.3.0-57392

--- Casks/p/parallels-virtualization-sdk.rb
+++ Casks/p/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask "parallels-virtualization-sdk" do
-  version "26.2.2-57373"
-  sha256 "bd3b56038eab8d7b09f8814f7c3145f2e89c5e2e3a567eaf5d335f63bcd80806"
+  version "26.3.0-57392"
+  sha256 "5f281cd8911771e0167e3a801bf351c2bbf45faacab03ef21e17f9c4b0d1d5ba"
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name "Parallels Virtualization SDK"
