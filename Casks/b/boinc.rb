de12229cbca25afe7c96453cf9ff90803e938365
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 04:17:26 +0000

    boinc 8.2.9

--- Casks/b/boinc.rb
+++ Casks/b/boinc.rb
@@ -1,6 +1,6 @@
 cask "boinc" do
-  version "8.2.5"
-  sha256 "04a20e59c1b942858aa4bc13c6898ccdce0a0641fa5986c98d3b6ee9799090a4"
+  version "8.2.9"
+  sha256 "b4658d4cb32b4c8e83a767552660c73916d762dc20e5551ec82faa7cfcb4550d"
 
   url "https://boinc.berkeley.edu/dl/boinc_#{version}_macOSX_universal.zip"
   name "Berkeley Open Infrastructure for Network Computing"
