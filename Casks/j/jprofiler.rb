ce60f3718aad3aa2dc91df3473f688750aa5b38e
jmaki <jmaki@magnite.com>
2026-03-25 15:19:37 -0400

    Update jprofiler to install on linux

--- Casks/j/jprofiler.rb
+++ Casks/j/jprofiler.rb
@@ -1,8 +1,18 @@
 cask "jprofiler" do
   version "16.0"
-  sha256 "1504b089704961b242ff7f13045f51f1530b39c5fea3e880ba278709ba7998ef"
 
-  url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
+  on_macos do
+    sha256 "1504b089704961b242ff7f13045f51f1530b39c5fea3e880ba278709ba7998ef"
+
+    url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
+  end
+
+  on_linux do
+    sha256 "c7ff6d6130b36f360e953f88c45c447400e3ca984b05d72d0db9811566a478c0"
+
+    url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_linux-x64_#{version.dots_to_underscores}.tar.gz"
+  end
+
   name "JProfiler"
   desc "Java profiler"
   homepage "https://www.ej-technologies.com/jprofiler"
@@ -20,14 +30,24 @@ cask "jprofiler" do
     end
   end
 
-  depends_on macos: ">= :big_sur"
+  on_macos do
+    depends_on macos: ">= :big_sur"
 
-  app "JProfiler.app"
+    app "JProfiler.app"
 
-  zap trash: [
-    "~/.jprofiler*",
-    "~/Library/Preferences/com.jprofiler.history.plist",
-    "~/Library/Preferences/com.jprofiler.v*.plist",
-    "~/Library/Preferences/jprofiler.vmoptions",
-  ]
+    zap trash: [
+      "~/.jprofiler*",
+      "~/Library/Preferences/com.jprofiler.history.plist",
+      "~/Library/Preferences/com.jprofiler.v*.plist",
+      "~/Library/Preferences/jprofiler.vmoptions",
+    ]
+  end
+
+  on_linux do
+    depends_on arch: :x86_64
+
+    binary "jprofiler#{version}/bin/jprofiler"
+
+    zap trash: "~/.jprofiler*"
+  end
 end
