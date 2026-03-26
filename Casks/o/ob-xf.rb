96474f23664e2956f398630d9c68c36aff63ece6
Paul Walker <paul@pwjw.com>
2026-03-25 20:50:24 -0400

    Fix upstream to have better versioned assets; Move to 1.0.2

--- Casks/o/ob-xf.rb
+++ Casks/o/ob-xf.rb
@@ -1,14 +1,14 @@
 cask "ob-xf" do
-  version "1.0.1"
-  sha256 "255378d5ee3cfff89c169d00bbbec7ffa8a809bec0676768b07ee9f9bfebf1a6"
+  version "1.0.2"
+  sha256 "e069c10a1b45b8ac3d4a2243969fad030c91b661ed30b7ea9daa7d1316c42d4f"
 
-  url "https://github.com/surge-synthesizer/OB-Xf/releases/download/v#{version}/ob-xf-macOS-2026-03-24-89eb632.dmg",
+  url "https://github.com/surge-synthesizer/OB-Xf/releases/download/v#{version}/ob-xf-macOS-v#{version}.dmg",
       verified: "github.com/surge-synthesizer/OB-Xf/"
   name "OB-Xf"
   desc "Virtual analog synthesizer"
   homepage "https://surge-synth-team.org/ob-xf/"
 
-  pkg "ob-xf-macOS-2026-03-24-89eb632.pkg"
+  pkg "ob-xf-macOS-v#{version}.pkg"
 
   uninstall pkgutil: [
               "org.surge-synth-team.ob-xf.app.pkg",
