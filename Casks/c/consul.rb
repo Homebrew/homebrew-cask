bae5b10f230546cf400c5f45d010c206ef1f4e85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-26 12:55:06 +0000

    consul 1.22.6

--- Casks/c/consul.rb
+++ Casks/c/consul.rb
@@ -1,9 +1,9 @@
 cask "consul" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.22.5"
-  sha256 arm:   "a728b166849ce51d107c22073c89217d27510dd9adad3326454273ac80ac2aef",
-         intel: "88028cf5c040a24193b2efd195efa6a6ca043c8340c7eb6b00343eecdf88ca40"
+  version "1.22.6"
+  sha256 arm:   "6a9b1e76ef814ee0abb9a414fcc83f15c3c7d60ac93d1516fe4e0b41a3613ee9",
+         intel: "fc9c25898282f51e8e9b1a50f5f77bfbd0ae8556882fbf4338fa9f5f66b68a3c"
 
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_#{arch}.zip",
       verified: "releases.hashicorp.com/consul/"
