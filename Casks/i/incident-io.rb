b6eeea8dbb263108b837af75657d653c4bde7330
Vladimir Danilov <diasonti@gmail.com>
2026-03-27 16:37:46 +0100

    incident-io Fix livecheck, add zap

--- Casks/i/incident-io.rb
+++ Casks/i/incident-io.rb
@@ -1,19 +1,21 @@
 cask "incident-io" do
   version "1.2.17"
-  sha256 :no_check
+  sha256 "b0dacfca6ccf9d27de0edfb3a0a970575823f854d558565be6a5b93f2f47b872"
 
-  url "https://storage.googleapis.com/incident-io-production-downloads/incident.io.dmg",
+  url "https://storage.googleapis.com/incident-io-production-downloads/incident.io-#{version}.zip",
       verified: "storage.googleapis.com/incident-io-production-downloads/"
   name "incident.io"
   desc "Incident management platform"
   homepage "https://incident.io/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://storage.googleapis.com/incident-io-production-downloads/appcast.xml"
+    strategy :sparkle
   end
 
   depends_on macos: ">= :sequoia"
 
   app "incident.io.app"
+
+  zap trash: "~/Library/Application Support/incident.io"
 end
