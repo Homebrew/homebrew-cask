bcda9d00514749fb53cc5b86930b10c702e7df47
Bevan Kay <email@bevankay.me>
2026-03-27 23:33:50 +1100

    slicer@preview: deprecate

--- Casks/s/slicer@preview.rb
+++ Casks/s/slicer@preview.rb
@@ -7,6 +7,8 @@ cask "slicer@preview" do
   desc "Medical image processing and visualization system"
   homepage "https://www.slicer.org/"
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   conflicts_with cask: "slicer"
   depends_on macos: ">= :ventura"
 
