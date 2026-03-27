a8328e36b71174a9df5b59df5675df213f0fed30
Vladimir Danilov <diasonti@gmail.com>
2026-03-27 13:21:39 +0100

    incident-io 1.2.17 (new cask)

---
+++ Casks/i/incident-io.rb
@@ -0,0 +1,21 @@
+cask "incident-io" do
+  version "1.2.17"
+  sha256 :no_check
+
+  url "https://storage.googleapis.com/incident-io-production-downloads/incident.io.dmg",
+      verified: "storage.googleapis.com/incident-io-production-downloads/"
+  name "incident.io"
+  desc "Incident management platform"
+  homepage "https://incident.io/"
+
+  depends_on macos: ">= :sequoia"
+
+  app "incident.io.app"
+
+  zap trash: [
+    "~/Library/Application Support/incident.io",
+    "~/Library/Caches/com.incidentio.desktop",
+    "~/Library/Preferences/com.incidentio.desktop.plist",
+    "~/Library/Saved Application State/com.incidentio.desktop.savedState",
+  ]
+end
