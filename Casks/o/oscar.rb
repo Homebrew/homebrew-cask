9c103ae66e2a28277330efa7c78d424e3efe3fdc
Alex Egan <>
2026-03-28 01:16:59 +1030

    oscar 1.7.1: fix download urls

--- Casks/o/oscar.rb
+++ Casks/o/oscar.rb
@@ -1,18 +1,23 @@
 cask "oscar" do
-  version "1.7.1"
-  sha256 "fa6171ddbef287490469abcdd9793e4303fb116af7c125c487ec571ff787db7d"
+  arch arm: "AppleSilicon", intel: "Intel"
 
-  url "https://www.sleepfiles.com/OSCAR/#{version}/OSCAR-#{version}-Qt5.dmg"
+  version "1.7.1"
+  sha256 arm:   "f4ff813ccfca6eafe7b3102df84344b4197c84f56e6c81f9f39ea937fea2c5fe",
+         intel: "fa6171ddbef287490469abcdd9793e4303fb116af7c125c487ec571ff787db7d"
+
+  on_intel do
+    disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  end
+
+  url "https://www.sleepfiles.com/OSCAR/#{version}/OSCAR-#{version}-#{arch}.dmg"
   name "OSCAR"
   desc "CPAP Analysis Reporter"
   homepage "https://www.sleepfiles.com/OSCAR/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/OSCAR[._-]v?(\d+(?:\.\d+)+)(?:[._-]Qt5)?\.dmg}i)
+    regex(%r{href=.*?/OSCAR[._-]v?(\d+(?:\.\d+)+)(?:[._-]#{arch})?\.dmg}i)
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   app "OSCAR.app"
 
@@ -20,8 +25,4 @@ cask "oscar" do
     "~/Library/Preferences/org.oscar-team.OSCAR.plist",
     "~/Library/Saved Application State/org.oscar-team.OSCAR.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
