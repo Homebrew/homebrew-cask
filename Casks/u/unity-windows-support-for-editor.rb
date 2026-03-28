03fa33eb76d81f3c5c2765f5426643d430a198d5
Bevan Kay <email@bevankay.me>
2026-03-28 12:51:31 +1100

    unity-windows-support-for-editor: deprecate

--- Casks/u/unity-windows-support-for-editor.rb
+++ Casks/u/unity-windows-support-for-editor.rb
@@ -12,6 +12,8 @@ cask "unity-windows-support-for-editor" do
     cask "unity"
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on cask: "unity"
 
   pkg "UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg"
