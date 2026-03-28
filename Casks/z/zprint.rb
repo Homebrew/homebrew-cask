2bd1c18d5feb1ea9cd71fcc8f7eec969977858e9
Bevan Kay <email@bevankay.me>
2026-03-28 16:39:53 +1100

    zprint: deprecate

--- Casks/z/zprint.rb
+++ Casks/z/zprint.rb
@@ -10,6 +10,8 @@ cask "zprint" do
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   binary "zprintm#{arch}-#{version}", target: "zprint"
 
   # No zap stanza required
