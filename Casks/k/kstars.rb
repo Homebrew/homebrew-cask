33033da561ba48e5a2e67ed1027b7157df7793ca
Bevan Kay <email@bevankay.me>
2026-03-28 18:09:56 +1100

    kstars: undeprecate

--- Casks/k/kstars.rb
+++ Casks/k/kstars.rb
@@ -14,8 +14,6 @@ cask "kstars" do
     regex(/href=.*?kstars[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   depends_on macos: ">= :ventura"
 
   app "kstars.app"
