a59c793843fb33b9c61f1b56eee439bb596a4011
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-28 05:15:34 +0000

    zen@twilight 1.20t,20260327110858

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -1,5 +1,5 @@
 cask "zen@twilight" do
-  version "1.20t,20260325094447"
+  version "1.20t,20260327110858"
   sha256 :no_check
 
   url "https://github.com/zen-browser/desktop/releases/download/twilight-1/zen.macos-universal.dmg",
