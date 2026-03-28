2ef494d39c6fc556ed71234e6b6959f4ab8c244a
Bevan Kay <email@bevankay.me>
2026-03-28 12:45:38 +1100

    temurin@19: disable

--- Casks/t/temurin@19.rb
+++ Casks/t/temurin@19.rb
@@ -12,6 +12,7 @@ cask "temurin@19" do
   homepage "https://adoptium.net/"
 
   deprecate! date: "2025-03-21", because: :discontinued
+  disable! date: "2026-03-28", because: :discontinued
 
   pkg "OpenJDK19U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg"
 
