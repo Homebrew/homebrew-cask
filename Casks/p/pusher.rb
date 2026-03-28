52795dfca5a7e44649b031a8d7d38883310a9fb8
Bevan Kay <email@bevankay.me>
2026-03-28 18:10:09 +1100

    pusher: undeprecate

--- Casks/p/pusher.rb
+++ Casks/p/pusher.rb
@@ -7,8 +7,6 @@ cask "pusher" do
   desc "Send push notifications through Apple Push Notification Service"
   homepage "https://github.com/noodlewerk/NWPusher"
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   app "Pusher.app"
 
   zap trash: [
