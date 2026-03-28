32592aa849c71045734a94ed015ed5c7e6e1d548
Bevan Kay <email@bevankay.me>
2026-03-28 12:42:07 +1100

    unity-ios-support-for-editor: deprecate

--- Casks/u/unity-ios-support-for-editor.rb
+++ Casks/u/unity-ios-support-for-editor.rb
@@ -12,6 +12,8 @@ cask "unity-ios-support-for-editor" do
     cask "unity"
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on cask: "unity"
 
   pkg "UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg"
