6db97a119b18539ca04045767296d5e9183651d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-29 21:57:58 +0000

    fuse-t 1.1.0

--- Casks/f/fuse-t.rb
+++ Casks/f/fuse-t.rb
@@ -1,6 +1,6 @@
 cask "fuse-t" do
-  version "1.0.54"
-  sha256 "1bb02e4a2903d576d3088997e0c2fac74e7565e1072378e6d34aa4af4afce7a6"
+  version "1.1.0"
+  sha256 "b30ab75f57087e19c2f35385d5b1f439232f7d8664a8654372184666bb176f65"
 
   url "https://github.com/macos-fuse-t/fuse-t/releases/download/#{version}/fuse-t-macos-installer-#{version}.pkg",
       verified: "github.com/macos-fuse-t/fuse-t/"
