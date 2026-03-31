1e530a52cbe4f1215e473e2eacaef61148e3fa4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 01:24:43 +0000

    gridtracker2 2.260330.2

--- Casks/g/gridtracker2.rb
+++ Casks/g/gridtracker2.rb
@@ -1,6 +1,6 @@
 cask "gridtracker2" do
-  version "2.260323.0"
-  sha256 "9ab004273ad7f255bf3d4ccb31224a089f9b30f6a9939b491c76b985e4d8a2f5"
+  version "2.260330.2"
+  sha256 "d3fbe7d501738538d5c3772f79bd72a739c8538f072041d8cd938d72f4578172"
 
   url "https://download2.gridtracker.org/GridTracker2-#{version}-universal-mac.zip"
   name "GridTracker2"
