b482398cc3ca02fca888d1ce11fa2818355cfa57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 16:09:52 +0000

    ea 13.676.0.6189

--- Casks/e/ea.rb
+++ Casks/e/ea.rb
@@ -1,5 +1,5 @@
 cask "ea" do
-  version "13.671.3.6185"
+  version "13.676.0.6189"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EA%20app.dmg",
