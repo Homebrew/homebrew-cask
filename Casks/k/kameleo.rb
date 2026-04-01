69c021493ce90e345b64ae750b11afd3efaa55b3
ferenc-a <140697579+ferenc-a@users.noreply.github.com>
2026-04-01 09:56:14 +0200

    Update Kameleo URL to point to the new repo

--- Casks/k/kameleo.rb
+++ Casks/k/kameleo.rb
@@ -2,8 +2,8 @@ cask "kameleo" do
   version "4.3.0"
   sha256 "d184faa065b89350bf0aeb4e4647556964288499c4f6db4c51305c137990b4c2"
 
-  url "https://github.com/kameleo-io/releases/releases/download/#{version}/kameleo-#{version}-osx-arm64.dmg",
-      verified: "github.com/kameleo-io/releases/"
+  url "https://github.com/kameleo-io/kameleo/releases/download/#{version}/kameleo-#{version}-osx-arm64.dmg",
+      verified: "github.com/kameleo-io/kameleo/"
   name "Kameleo"
   desc "Antidetect browser to bypass anti-bot systems"
   homepage "https://kameleo.io/"
