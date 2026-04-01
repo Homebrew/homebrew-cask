b42ec8e3ebb67f232f81e59454be545506e1f11e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 13:40:17 +0000

    pdf-expert@beta 3.11.3

--- Casks/p/pdf-expert@beta.rb
+++ Casks/p/pdf-expert@beta.rb
@@ -1,6 +1,6 @@
 cask "pdf-expert@beta" do
-  version "3.11.2,1150"
-  sha256 "c097688827f6a7095e049f1fe1628f6022ba5c9acf682662a4021357609e61a1"
+  version "3.11.3,1151"
+  sha256 "8bfb270a3522b911f395a454fcef35807636976296f546a446430055624d76b0"
 
   url "https://downloads.pdfexpert.com/pem#{version.major}/versions/#{version.csv.second}/PDFExpert.zip"
   name "PDF Expert"
