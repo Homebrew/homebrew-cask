33a09165cb56ca258475040fcd005f4547d196d9
Gabriel <gabriel.hdos@icloud.com>
2026-04-01 16:29:32 -0300

    Add IRPF2026 fix syntax

--- Casks/i/irpf2026.rb
+++ Casks/i/irpf2026.rb
@@ -1,16 +1,11 @@
 cask "irpf2026" do
+  arch arm: "_arm"
+
   version "1.0"
+  sha256 arm:   "7871009285ed63d191e6ebca075daf521b002136dedfde54b1e4383d42b8635a",
+         intel: "1ba4166a3389bfce2653b2660ea166e7f2e3e2a98e9f177c58b53e347d42a15e"
 
-  on_arm do
-    url "https://downloadirpf.receita.fazenda.gov.br/irpf/2026/irpf/arquivos/IRPF2026-v#{version}_arm.dmg"
-    sha256 "7871009285ed63d191e6ebca075daf521b002136dedfde54b1e4383d42b8635a"
-  end
-
-  on_intel do
-    url "https://downloadirpf.receita.fazenda.gov.br/irpf/2026/irpf/arquivos/IRPF2026-v#{version}.dmg"
-    sha256 "1ba4166a3389bfce2653b2660ea166e7f2e3e2a98e9f177c58b53e347d42a15e"
-  end
-
+  url "https://downloadirpf.receita.fazenda.gov.br/irpf/2026/irpf/arquivos/IRPF2026-v#{version}#{arch}.dmg"
   name "IRPF 2026"
   desc "Fill your Tax Report (DIRPF) for the Brazilian Revenue Service (RFB)"
   homepage "https://www.gov.br/receitafederal/pt-br/centrais-de-conteudo/download/pgd/dirpf"
