3f497a0257c6363d8a12e088234cc63bd2328a39
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 17:06:50 +0000

    utools 7.7.0

--- Casks/u/utools.rb
+++ Casks/u/utools.rb
@@ -1,9 +1,9 @@
 cask "utools" do
   arch arm: "-arm64"
 
-  version "7.6.1"
-  sha256 arm:   "81a987fc28072fd88bfc974a5e1a30ccd690b3331faaa5b2922697461862baa6",
-         intel: "a805ffa963084bc05990e207177e7fe85b6a5ffa353fe13cd08c85406f0e99b4"
+  version "7.7.0"
+  sha256 arm:   "c2b274590011e5df038311261093c051fc6aa2252473e03d9a6583946e2b76b5",
+         intel: "46a505ea8513684b800f54c034f8dd177f6afdab5455347d5fc87e6bb85cab0b"
 
   url "https://open.u-tools.cn/download/uTools-#{version}#{arch}.dmg"
   name "uTools"
