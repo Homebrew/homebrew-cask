103ded51cb7b9618a0966d2a023606a4ed69fa20
buaazyl <dev@zhangyl.com>
2026-04-01 08:42:01 +0800

    google-chrome 146.0.7680.178

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "146.0.7680.165"
+  version "146.0.7680.178"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
