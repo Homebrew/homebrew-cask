d2979ec02acfeecfcd55b682008468df7d839758
swanpark8538 <160217149+swanpark8538@users.noreply.github.com>
2026-04-01 15:15:07 +0900

    Update Casks/e/egovframedev.rb for maintainablity on version updates
    maintainable on version updates
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/e/egovframedev.rb
+++ Casks/e/egovframedev.rb
@@ -5,7 +5,7 @@ cask "egovframedev" do
   sha256 arm:   "0f46f4b8a6949ef2ac333ec751679e8de8db3cdef1ec16865924c687ac6cbfb7",
          intel: "ab266af6fc7a1259fe908f5142b28a81ab13e1a2a8fa697d56ca89b4a4eed87d"
 
-  url "https://maven.egovframe.go.kr/publist/HDD1/public/egovframework_v5.0/2_DevelopmentEnvironment/eGovFrame-Dev/5.0.0/eGovFrameDev-#{version}-macOS-#{arch}.dmg",
+  url "https://maven.egovframe.go.kr/publist/HDD1/public/egovframework_v#{version.major_minor}/2_DevelopmentEnvironment/eGovFrame-Dev/#{version}/eGovFrameDev-#{version}-macOS-#{arch}.dmg",
       user_agent: :fake
   name "eGovFrameDev"
   desc "Open-source framework by South Korea for web-based public service development"
