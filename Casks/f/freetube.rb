50a645da9b420126434ac60b369d8005751101b1
Bevan Kay <email@bevankay.me>
2026-04-01 17:07:36 +1100

    freetube: fix min_os boundaries

--- Casks/f/freetube.rb
+++ Casks/f/freetube.rb
@@ -1,18 +1,29 @@
 cask "freetube" do
   arch arm: "arm64", intel: "x64"
 
-  on_catalina do
-    version "0.22.1"
-    sha256 "0e9eb9db841f36671c81fedff4580c39dbbd6bd541d5158ed4897218c4134946"
+  on_big_sur :or_older do
+    on_catalina do
+      version "0.22.1"
+      sha256 "0e9eb9db841f36671c81fedff4580c39dbbd6bd541d5158ed4897218c4134946"
 
-    url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-x64.dmg",
-        verified: "github.com/FreeTubeApp/FreeTube/"
+      url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-x64.dmg",
+          verified: "github.com/FreeTubeApp/FreeTube/"
+    end
+
+    on_big_sur do
+      version "0.23.15"
+      sha256 arm:   "6a25f689ecf0382c9b3c98be308f5d97eda1c362035dd1d715307a25ab68320a",
+             intel: "ebade3bc4b3cb7c994201cf1bd50459c33c8b4d85ae0e714b2c5a32612c70e69"
+
+      url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-beta-mac-#{arch}.dmg",
+          verified: "github.com/FreeTubeApp/FreeTube/"
+    end
 
     livecheck do
       skip "Legacy version"
     end
   end
-  on_big_sur :or_newer do
+  on_monterey :or_newer do
     version "0.24.0"
     sha256 arm:   "996dcd274bcbab024286748407cac4397624384afcc54d018edaf381b32a8fea",
            intel: "17d675409b45dc3a6f5b5934beb0eb568fc05dfa6d128345a3cd46805a84d221"
