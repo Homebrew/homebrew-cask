ad4bb03d621baf84892b78ed31ae079eb16f2b6d
Bevan Kay <email@bevankay.me>
2026-04-01 22:42:10 +1100

    tuck: disable

--- Casks/t/tuck.rb
+++ Casks/t/tuck.rb
@@ -8,6 +8,7 @@ cask "tuck" do
   homepage "https://www.irradiatedsoftware.com/tuck/"
 
   deprecate! date: "2025-03-31", because: :unmaintained
+  disable! date: "2026-04-01", because: :unmaintained
 
   app "Tuck.app"
 
