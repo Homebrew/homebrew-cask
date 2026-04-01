39dba7676df690c12b7536180c7d5f4947e8b53a
Gregor Majcen <majcn.m@gmail.com>
2026-04-01 16:09:10 +0200

    remanager 1.2.5

---
+++ Casks/r/remanager.rb
@@ -0,0 +1,22 @@
+cask "remanager" do
+  version "1.2.5"
+  sha256 "56eec18fa4e0d562ec23ba91236c35f349734860c4b08bd7088798149d21450d"
+
+  url "https://github.com/rmitchellscott/reManager/releases/download/v#{version}/reManager-macos-universal.zip"
+  name "reManager"
+  desc "Desktop app for managing mods on reMarkable tablets"
+  homepage "https://github.com/rmitchellscott/reManager"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "reManager.app"
+
+  zap trash: [
+    "~/Library/Application Support/reManager",
+    "~/Library/Caches/com.wails.reManager",
+    "~/Library/Preferences/com.wails.reManager.plist",
+  ]
+end
