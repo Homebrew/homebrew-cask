8cbafce86ed93f41d4666ffe4857930a1e7772d4
Gustav Lübker <gustav@lubker.dk>
2026-04-02 17:44:21 +0200

    radial 4.1.0 (new cask)

---
+++ Casks/r/radial.rb
@@ -0,0 +1,22 @@
+cask "radial" do
+  version "4.1.0"
+  sha256 :no_check
+
+  url "https://github.com/Glubker/radial-releases/releases/download/v#{version}/Radial.dmg",
+    verified: "github.com/Glubker/radial-releases/"
+  name "Radial"
+  desc "Gesture-based launcher for apps, text snippets, and scripts on macOS"
+  homepage "https://radial.appverge.net"
+
+  auto_updates true
+
+  depends_on macos: ">= :sequoia"
+
+  app "Radial.app"
+
+  zap trash: [
+    "~/Library/Preferences/dk.FirstForm.Radial.plist",
+    "~/Library/Caches/dk.FirstForm.Radial",
+    "~/Library/HTTPStorages/dk.FirstForm.Radial",
+  ]
+end
