6f6bd0f402f9bf32f8500ba76d0136a338acacfc
vmrgvn <64679360+vmrgvn@users.noreply.github.com>
2026-04-02 12:41:52 +0500

    Update yandex-music host
    Update the download URL to a new host for yandex-music app

--- Casks/y/yandex-music.rb
+++ Casks/y/yandex-music.rb
@@ -2,14 +2,14 @@ cask "yandex-music" do
   version "5.93.1"
   sha256 "e597a11100b871d51d64ebd9e60972a5ec301075cfcca8f333eabb467d8f0a35"
 
-  url "https://music-desktop-application.s3.yandex.net/stable/Yandex_Music_universal_#{version}.dmg",
-      verified: "music-desktop-application.s3.yandex.net/stable/"
+  url "https://desktop.app.music.yandex.net/stable/Yandex_Music_universal_#{version}.dmg",
+      verified: "desktop.app.music.yandex.net/stable/"
   name "Yandex Music"
   desc "Tune in to Yandex Music and get personal recommendations"
   homepage "https://music.yandex.ru/"
 
   livecheck do
-    url "https://music-desktop-application.s3.yandex.net/stable/latest-mac.yml"
+    url "https://desktop.app.music.yandex.net/stable/latest-mac.yml"
     strategy :electron_builder
   end
 
