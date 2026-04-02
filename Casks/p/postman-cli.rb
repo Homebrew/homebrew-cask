23ec23efbdaa36e6c780d30a02a4aaa48c98da26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 16:47:30 +0000

    postman-cli 1.33.3

--- Casks/p/postman-cli.rb
+++ Casks/p/postman-cli.rb
@@ -1,9 +1,9 @@
 cask "postman-cli" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "1.33.2"
-  sha256 arm:   "3a7aee9dee68ff248b888efc40a792d994dbcd59fe806bba404cb152e2020bb4",
-         intel: "a915796c24c2d1e887f316709999da36f398c290967c330fc920d1286c37e44f"
+  version "1.33.3"
+  sha256 arm:   "f834ef94bbdf64ca7bb54e345a4dc0332fb1a9748fc2ab4ab8e597ae1df694e0",
+         intel: "613b89ed7e164bdfedca229f7a35830df3cf28a2a4cdcc0cabc0163fdd4a35d5"
 
   url "https://dl-cli.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl-cli.pstmn.io/download/"
