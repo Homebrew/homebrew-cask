3115ddf1a7d06a48206a299f5c1205c04c50c4f9
Gustav Lübker <gustav@lubker.dk>
2026-04-02 18:16:13 +0200

    radial 4.1.0 (update url to stable download)

--- Casks/r/radial.rb
+++ Casks/r/radial.rb
@@ -2,8 +2,7 @@ cask "radial" do
   version "4.1.0"
   sha256 :no_check
 
-  url "https://github.com/Glubker/radial-releases/releases/download/v#{version}/Radial.dmg",
-    verified: "github.com/Glubker/radial-releases/"
+  url "https://radial.appverge.net/downloads/Radial.dmg"
   name "Radial"
   desc "Gesture-based launcher for apps, text snippets, and scripts on macOS"
   homepage "https://radial.appverge.net"
