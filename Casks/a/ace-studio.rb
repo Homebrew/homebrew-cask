b5d13377c66fda0a161a40d36263434f375810cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 12:45:34 +0000

    ace-studio 2.0.8

--- Casks/a/ace-studio.rb
+++ Casks/a/ace-studio.rb
@@ -1,9 +1,9 @@
 cask "ace-studio" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.0.7,1825"
-  sha256 arm:   "8bf4516e4408d9873b405640241ef1976c45badfc17d407f35451ebf425feb82",
-         intel: "5942390b244dd9fb533d86de92c42cb61d5e05dbfcd7bd4248077a5512590391"
+  version "2.0.8,1874"
+  sha256 arm:   "67b3ea7750e5544c599fa3c139932e3bfb32cb7c5106d7d131ee786ed28ce5c1",
+         intel: "17fc20bd1e614f0fd2af9011aa0c6d41ee5ba0e3b5be5ef54392c4c73c27ee85"
 
   url "https://as-api-ws-cdn-1.tdacestudio.com/web/app/ACE_Studio_#{version.csv.first}_#{version.csv.second}_#{arch}_general.dmg",
       verified: "as-api-ws-cdn-1.tdacestudio.com/web/app/"
