76b46fd20ebea57d0c3a44eeb2fec373f304d95a
Klaus Hipp <khipp@users.noreply.github.com>
2026-04-03 13:05:39 +0200

    f-bar 6.0.4

--- Casks/f/f-bar.rb
+++ Casks/f/f-bar.rb
@@ -1,19 +1,19 @@
 cask "f-bar" do
-  version "5.1"
-  sha256 "82d6a186209c2b70317b444eea278b570843b09db2373ebefa1c025a73a937e6"
+  version "6.0.4"
+  sha256 "ea0fd6f72e4e7a1aefaec28cd9454333ddf839c10d73511555550c94d5ef7d92"
 
-  url "https://apps.eastwest.se/fbar/updates/F-Bar_#{version}.zip",
-      verified: "apps.eastwest.se/fbar/"
+  url "https://app.laravel-forge-menubar.com/storage/releases/F-Bar_#{version}.zip"
   name "F-Bar"
   desc "Manage Laravel Forge servers from the menubar"
   homepage "https://laravel-forge-menubar.com/"
 
   livecheck do
-    url "https://apps.eastwest.se/fbar/updates/FBarAppcast.xml"
+    url "https://app.laravel-forge-menubar.com/appcast.xml"
     strategy :sparkle, &:short_version
   end
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "F-Bar.app"
 
