edb1823d4eedf5b0b7b471da0d117866dbaec4ba
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-02 23:31:51 -0400

    tourbox-console 5.11.2
    `tourbox-console` is autobumped but the workflow failed to update to
    version 5.11.2 because the URL has changed and the version in the file
    name now includes an additional numeric suffix. This updates the
    version, adjusts the `url`, and modifies the `livecheck` block to
    match the version and suffix from the asset URL in the JSON data.

--- Casks/t/tourbox-console.rb
+++ Casks/t/tourbox-console.rb
@@ -1,23 +1,24 @@
 cask "tourbox-console" do
-  version "5.11.1"
-  sha256 "b835dc633e37c04bf468d182250459d5978b4288656bf28d2e6334003a17618a"
+  version "5.11.2,260402120540"
+  sha256 "5c908696af3c44d88ffa7e654608a1a65f2d268c1fcd2d7a5a8b4c27df53b931"
 
-  url "https://tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/TourBoxInstall#{version}.zip",
-      verified: "tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/"
+  url "https://cdn.tourboxtech.com/prod/console/TourBoxInstall#{version.csv.join("_")}.zip"
   name "TourBox Console"
   desc "Configuration app for TourBox devices"
   homepage "https://www.tourboxtech.com/"
 
   livecheck do
-    url "https://www.tourboxtech.com/tbmall/download/newest?local=US", post_json: {
-      softName: "TourBox Console",
-    }
-    strategy :json do |json|
-      json.dig("result", "normalSoft", "version")
+    url "https://www.tourboxtech.com/tbmall/download/newest?local=US",
+        post_json: {
+          softName: "TourBox Console",
+        }
+    regex(/TourBoxInstall[._-]?v?(\d+(?:[._]\d+)+)/i)
+    strategy :json do |json, regex|
+      json.dig("result", "normalSoft", "macPath")&.[](regex, 1)&.tr("_", ",")
     end
   end
 
-  pkg "TourBoxInstall#{version}/TourBoxInstall#{version}.pkg"
+  pkg "TourBoxInstall#{version.csv.first}/TourBoxInstall#{version.csv.first}.pkg"
 
   uninstall quit:    "com.tourbox.ui.launch",
             pkgutil: "com.tourbox.ui.launch"
