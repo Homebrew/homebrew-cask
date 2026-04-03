786b293e6b61ed9bd8eb3932c6de65fa0bf63d3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 21:44:20 +0000

    bettermouse 1.6,8780

--- Casks/b/bettermouse.rb
+++ Casks/b/bettermouse.rb
@@ -1,6 +1,6 @@
 cask "bettermouse" do
-  version "1.6,8779"
-  sha256 "abfbe5bb15c48d870f4a8e29972398346728908755f90559661b4a929265de5e"
+  version "1.6,8780"
+  sha256 "483dcf5093c177b5c4fad5c2469e2b3ac89b666c1d51bfecadd4a2d419ad9e58"
 
   url "https://better-mouse.com/wp-content/uploads/BetterMouse.#{version.csv.first}.#{version.csv.second}.zip"
   name "BetterMouse"
