8b65ceece0d840f9ee906d868fd65e9eb1b64083
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 19:45:54 +0000

    superhuman 1038.0.31

--- Casks/s/superhuman.rb
+++ Casks/s/superhuman.rb
@@ -1,9 +1,9 @@
 cask "superhuman" do
   arch arm: "-arm64"
 
-  version "1038.0.30"
-  sha256 arm:   "577421b58c340f898ef41da2b3bbe06120adf103ccc8e92816e8615ec81e1be5",
-         intel: "9ba0b93ab6f1787fd752b45e971e844ec8d7f3a012b93a057cf82c6b2220818b"
+  version "1038.0.31"
+  sha256 arm:   "7da77340b465e33c7bcdd0f5f50ca3b4147d4eaa3d03c94d86b39dcbe69dade7",
+         intel: "94f2752da4f3ede4b3e476142083bcadab38e6d5d776693a062d47563201ee7e"
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
       verified: "storage.googleapis.com/download.superhuman.com/supertron-update/"
