35512b9cdfc88ac5a291139bc2d8c1a767ea89a9
Bartosz Sroczynski <b.sroczynski@pm.me>
2026-04-03 14:36:41 +0200

    quickmd: fix desc (no platform) and add verified URL

--- Casks/q/quickmd.rb
+++ Casks/q/quickmd.rb
@@ -2,9 +2,10 @@ cask "quickmd" do
   version "1.4.1"
   sha256 "04f3d3cf53c441cfdd0084d2a50d8343effd272b4fcc86ac6f886939861e186f"
 
-  url "https://github.com/b451c/quickmd/releases/download/v#{version}/QuickMD-v#{version}.zip"
+  url "https://github.com/b451c/quickmd/releases/download/v#{version}/QuickMD-v#{version}.zip",
+      verified: "github.com/b451c/quickmd/"
   name "QuickMD"
-  desc "Lightning-fast native macOS Markdown viewer"
+  desc "Lightning-fast native Markdown viewer"
   homepage "https://qmd.app/"
 
   livecheck do
