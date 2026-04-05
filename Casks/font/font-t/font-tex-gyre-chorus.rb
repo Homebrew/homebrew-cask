47eec923b08c26ddb7ffc43155470b54308cc5d7
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-05 14:16:08 -0400

    font-tex-gyre-chorus 2.609
    `font-tex-gyre-chorus` is autobumped but the workflow failed to
    update to version 2.609 because the zip file name format has changed
    and now also includes a date suffix. This updates the version and
    adjusts the `url`. This modifies the `livecheck` block to check the
    homepage, as we have to match the asset URL to capture both the
    version and date suffix.

--- Casks/font/font-t/font-tex-gyre-chorus.rb
+++ Casks/font/font-t/font-tex-gyre-chorus.rb
@@ -1,14 +1,20 @@
 cask "font-tex-gyre-chorus" do
-  version "2.003"
-  sha256 "fbd905a504e6f86df9c38b42c6ed58681a10e6debae6afa6308bc031695cdbd8"
+  version "2.609,31_03_2026"
+  sha256 "4f00506e0b630b2f3b4c9d35f786c17de3cfb11d8265065b79aee2a3e354387b"
 
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/qzc#{version}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus/tg_chorus-otf-#{version.csv.first.dots_to_underscores}#{"-#{version.csv.second}" if version.csv.second}.zip"
   name "TeX Gyre Chorus"
   homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/chorus"
 
   livecheck do
-    url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/whole"
-    regex(%r{Chorus</a>,\sver\.\s(\d+(?:\.\d+)+)}i)
+    url :homepage
+    regex(/href=.*?chorus-otf[._-]v?(\d+(?:[._]\d+)+)(?:-(\d+(?:[._]\d+)+))?\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        version = match[0].tr("_", ".")
+        match[1].present? ? "#{version},#{match[1]}" : version
+      end
+    end
   end
 
   font "texgyrechorus-mediumitalic.otf"
