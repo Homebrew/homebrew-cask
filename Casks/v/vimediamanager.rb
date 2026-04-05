fd836c7ce749a8fec052d5e1c4fd59f361d106b4
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-04 23:27:12 -0400

    vimediamanager 0.7a24
    `vimediamanager` is autobumped but the workflow failed to update to
    version 0.7a24 because the zip file name now includes a date. This
    updates the version, adjusts the URL, and modifies the `livecheck`
    block to match both the version and date from the asset URL.

--- Casks/v/vimediamanager.rb
+++ Casks/v/vimediamanager.rb
@@ -1,16 +1,23 @@
 cask "vimediamanager" do
-  version "0.7a23"
-  sha256 "e5c1a6960f60936edc0b07cf4b9fdddbf013fa6840162ab3c4b0aa266a17d570"
+  version "0.7a24,2026_04_01"
+  sha256 "217bec52ba71d7d046ce6b0913652162b5e652112778a3895743962bee451729"
 
-  url "https://github.com/vidalvanbergen/ViMediaManager/releases/download/v#{version}/vimediamanager.zip"
+  url "https://github.com/vidalvanbergen/ViMediaManager/releases/download/v#{version.csv.first}/ViMediaManager#{"-#{version.csv.second}" if version.csv.second}.zip"
   name "ViMediaManager"
   desc "Manage digital artifacts for your movie, television and anime collections"
   homepage "https://github.com/vidalvanbergen/ViMediaManager"
 
   livecheck do
     url :url
-    regex(/v?(\d+(?:\.\d+)+[a-z]\d+)/i)
-    strategy :github_latest
+    regex(%r{/v?(\d+(?:\.\d+)+[a-z]\d+)/ViMediaManager[._-]v?(\d+(?:[._]\d+)+)\.zip}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        match[2].present? ? "#{match[1]},#{match[2]}" : match[1]
+      end
+    end
   end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
