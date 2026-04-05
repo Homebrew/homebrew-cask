7eeb3ea3a0ccb45db1cf59f1858953e9866a16af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-05 22:08:12 +0000

    jellyfin 10.11.8

--- Casks/j/jellyfin.rb
+++ Casks/j/jellyfin.rb
@@ -1,9 +1,9 @@
 cask "jellyfin" do
   arch arm: "arm64", intel: "amd64"
 
-  version "10.11.7"
-  sha256 arm:   "89c11a1afdf0fe73a636fea49e5722866f23bd4f8a607b667ffb7aa06bbf601b",
-         intel: "1a1bea435a858d16b6b6ff3faedde6de5f19e96170a3d739e0e67850c8885e23"
+  version "10.11.8"
+  sha256 arm:   "72c7944dd6b7317c6024d1cbe0b82acbfa1ca271157d711ae255adbd5d12fcb4",
+         intel: "ff6a61fcfdf0db384879e6d7a122b1145af47eadac67ccf582e62bf8a95e9f2d"
 
   url "https://repo.jellyfin.org/files/server/macos/stable/v#{version}/#{arch}/jellyfin_#{version}-#{arch}.dmg"
   name "Jellyfin"
