d1748a3d4668e6ea77f9eaf481127c4a8b5f54d1
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-05 14:19:59 -0400

    font-tex-gyre-pagella 2.609
    `font-tex-gyre-pagella` is autobumped but the workflow failed to
    update to version 2.609 because the zip file name format has changed
    and now also includes a date suffix. This updates the version and
    adjusts the `url`. This modifies the `livecheck` block to check the
    homepage, as we have to match the asset URL to capture both the
    version and date suffix.

--- Casks/font/font-t/font-tex-gyre-pagella.rb
+++ Casks/font/font-t/font-tex-gyre-pagella.rb
@@ -1,20 +1,26 @@
 cask "font-tex-gyre-pagella" do
-  version "2.501"
-  sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
+  version "2.609,31_03_2026"
+  sha256 "1fa1284bf30aefa1885ec5ce6c26b24d160104b331b1978a85a2a8b3c781737d"
 
-  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.dots_to_underscores}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/tg_pagella-otf-#{version.csv.first.dots_to_underscores}#{"-#{version.csv.second}" if version.csv.second}.zip"
   name "TeX Gyre Pagella"
   homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
 
   livecheck do
-    url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/whole"
-    regex(%r{Pagella</a>,\sver\.\s(\d+(?:\.\d+)+)}i)
+    url :homepage
+    regex(/href=.*?pagella-otf[._-]v?(\d+(?:[._]\d+)+)(?:-(\d+(?:[._]\d+)+))?\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        version = match[0].tr("_", ".")
+        match[1].present? ? "#{version},#{match[1]}" : version
+      end
+    end
   end
 
-  font "qpl#{version.dots_to_underscores}otf/texgyrepagella-bold.otf"
-  font "qpl#{version.dots_to_underscores}otf/texgyrepagella-bolditalic.otf"
-  font "qpl#{version.dots_to_underscores}otf/texgyrepagella-italic.otf"
-  font "qpl#{version.dots_to_underscores}otf/texgyrepagella-regular.otf"
+  font "texgyrepagella-bold.otf"
+  font "texgyrepagella-bolditalic.otf"
+  font "texgyrepagella-italic.otf"
+  font "texgyrepagella-regular.otf"
 
   # No zap stanza required
 end
