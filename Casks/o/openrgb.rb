481e445a2092f62db761489eae1340f84a10a6a5
DiabloSat <dev@diablosat.cc>
2026-04-07 00:08:05 +0300

    add todo

--- Casks/o/openrgb.rb
+++ Casks/o/openrgb.rb
@@ -5,6 +5,8 @@ cask "openrgb" do
   sha256 arm:   "0846520bdc6d7bf1abc6eabbf4f44c5e2507761fcf1e1a220e85c138056f33eb",
          intel: "fb1b000293ecd2981134fdc6b26e9453c9db33977a0442a82605ec4f7ab2e5ef"
 
+  # TODO: Remove the `candidate` part of this url when updating to the next
+  # stable version, so we only match stable versions going forward.
   url "https://codeberg.org/OpenRGB/OpenRGB/releases/download/release_candidate_#{version.csv.first}/OpenRGB_#{version.csv.first}_MacOS_#{arch}_#{version.csv.second}.zip",
       verified: "codeberg.org/"
   name "OpenRGB"
