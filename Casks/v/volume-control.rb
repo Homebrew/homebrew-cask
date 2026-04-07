98561c5d5352692b4e91f6879dc4051a9a966328
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 17:17:03 +0000

    volume-control 2.2.1

--- Casks/v/volume-control.rb
+++ Casks/v/volume-control.rb
@@ -1,6 +1,6 @@
 cask "volume-control" do
-  version "2.2.0"
-  sha256 "38803634ded70ae599eff187d702c5182c29871d0203e3c67fc981ed83d0bc37"
+  version "2.2.1"
+  sha256 "7862979f92a7112d8602f353dc321b69be85a3227afc4c8552945c900d526403"
 
   url "https://github.com/alberti42/Volume-Control/releases/download/v#{version}/VolumeControl.zip"
   name "Volume Control"
