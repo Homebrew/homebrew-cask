44d9fa79748927092bf6dbaf870f94fa214fb072
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 16:47:20 +0000

    nosql-workbench 3.20.2

--- Casks/n/nosql-workbench.rb
+++ Casks/n/nosql-workbench.rb
@@ -1,9 +1,9 @@
 cask "nosql-workbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.20.1"
-  sha256 arm:   "8829807cee1bdd444350f3176c0f719fb318f7a219e6e9ee7f262759ed46fe43",
-         intel: "0b162a4bd923068faf3db217e2590acd799c17ab3160f109f4da79b4cd1fb813"
+  version "3.20.2"
+  sha256 arm:   "53a19e6bcf7f71763d15de89016ad4b5fed0e157c45c33f36e30907073118e21",
+         intel: "e0b52309ed1a74d9ac31e2015db02cae2526b3fa9c79ef503b2dca9572448a9f"
 
   url "https://nosql-workbench.s3.amazonaws.com/NoSQL%20Workbench-mac-#{arch}-#{version}.dmg",
       verified: "nosql-workbench.s3.amazonaws.com/"
