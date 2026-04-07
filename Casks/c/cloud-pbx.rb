fdfb82b910731a2530d329f2e0c4371878372b11
Bevan Kay <email@bevankay.me>
2026-04-07 21:31:26 +0800

    cloud-pbx: add depends_on

--- Casks/c/cloud-pbx.rb
+++ Casks/c/cloud-pbx.rb
@@ -16,6 +16,8 @@ cask "cloud-pbx" do
     regex(%r{href=.*?/cloud[._-]pbx[._-]2\.0[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)}i)
   end
 
+  depends_on macos: ">= :monterey"
+
   app "Cloud PBX 2.0.app"
 
   zap trash: [
