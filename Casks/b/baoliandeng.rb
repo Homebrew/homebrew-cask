929304383915134a7a725993955fb693cc23609b
Zongle Wang <wangzongler@gmail.com>
2026-04-07 22:03:21 +0800

    Apply suggestions from code review
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/b/baoliandeng.rb
+++ Casks/b/baoliandeng.rb
@@ -8,18 +8,9 @@ cask "baoliandeng" do
   desc "VPN proxy powered by Mihomo (Clash Meta)"
   homepage "https://madeye.github.io/BaoLianDeng/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
   depends_on macos: ">= :sonoma"
 
   pkg "BaoLianDeng-#{version}.pkg"
-
-  postflight do
-    system "open", "x-apple.systempreferences:com.apple.LoginItems-Settings.extension"
-  end
 
   uninstall quit:    [
               "io.github.baoliandeng.macos",
