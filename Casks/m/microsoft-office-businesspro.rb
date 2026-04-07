99d9475bbabc90916388b704a51938fe938ee988
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 22:31:24 +0000

    microsoft-office-businesspro 16.107.26040410

--- Casks/m/microsoft-office-businesspro.rb
+++ Casks/m/microsoft-office-businesspro.rb
@@ -1,6 +1,6 @@
 cask "microsoft-office-businesspro" do
-  version "16.107.26032814"
-  sha256 "f954a88e2f71051bfc25e4140ed1dfd41ccca27790c5f99f0c9cdc3f9f992a4b"
+  version "16.107.26040410"
+  sha256 "cbc3140b4fc132f62b826bae5caa7cd3e1b118a6dbb776585b563235f4187260"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_365_and_Office_#{version}_BusinessPro_Installer.pkg"
   name "Microsoft Office BusinessPro"
