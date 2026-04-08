0137de3242e2e1fa0d90661ee4d250b5cb9a7e6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 12:52:27 +0000

    biscuit 1.2.36

--- Casks/b/biscuit.rb
+++ Casks/b/biscuit.rb
@@ -1,9 +1,9 @@
 cask "biscuit" do
   arch arm: "-arm64"
 
-  version "1.2.35"
-  sha256 arm:   "bdf14210f4b7aa9c96eed6257e72b6dcf0e65c892ceae80c0eee3b7964bcc49c",
-         intel: "ea2b285266272cf21d33b2d1388a7d068970b1311f955e53a3a379655f1454f0"
+  version "1.2.36"
+  sha256 arm:   "23e4d747d8f53d7fed9168d6188b980dc87658c37367c79249bdd4f556e6d81f",
+         intel: "ba32802300f6555889061ff51cd0f3f061eb7d48f3238fef74e3d8c3864526ce"
 
   url "https://github.com/agata/dl.biscuit/releases/download/#{version}/Biscuit-#{version}#{arch}.dmg",
       verified: "github.com/agata/dl.biscuit/"
