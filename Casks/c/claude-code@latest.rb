6d882acfd01cc8905c360078987f077e9ea931e3
Mladen Djuric <mladen@bluefish.rs>
2026-04-08 09:22:59 +0200

    Update claude-code@latest 2.1.94 -> 2.1.96

--- Casks/c/claude-code@latest.rb
+++ Casks/c/claude-code@latest.rb
@@ -2,11 +2,11 @@ cask "claude-code@latest" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.1.94"
-  sha256 arm:          "cf1c5db5e87610eca1e8f06dc2b2a2a9c0c04c179c55c7e1fcdfc9a2c014b31c",
-         x86_64:       "e3d29d0febf982112e977b7d732adf3167f229521c8697e5253c90a6d2d4a332",
-         x86_64_linux: "7cbc939cb52e09d49fd82092061543d21100b04c321e19fecf06f087dc908667",
-         arm64_linux:  "b3524791b50b314b17dac9978cb9568fa2e11a46562ae6f798d0dac453a0604a"
+  version "2.1.96"
+  sha256 arm:          "31bad8c237dd561566620595227a734d9736c979a7ddb52c6c65d9ef93b300e3",
+         x86_64:       "666aad5da68e8623b31834e65ae6a14144c45af538c7329df228a95ce99a3349",
+         arm64_linux:  "c8876673205945df1bcb33415827d169785b22ac10150cfaf3a7f5e2d58bf08e",
+         x86_64_linux: "9bcad6249b4e9c1a4db52430e2834f14b59349c87de0ecb0b9278cf1f4a20534"
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
