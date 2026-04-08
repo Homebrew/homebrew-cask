97fc70e19c3bfef15a4af6caf4941876921b37e7
Robin <hello@robinren.me>
2026-04-08 15:37:21 +0800

    fix: add verified to url, remove platform from desc

--- Casks/m/mino.rb
+++ Casks/m/mino.rb
@@ -2,9 +2,10 @@ cask "mino" do
   version "0.1.1"
   sha256 "254efc1a641c692592f44fca4e9ef34533b6631301bb483e58bc73cb442cb989"
 
-  url "https://github.com/robinv8/mino/releases/download/v#{version}/Mino-#{version}.dmg"
+  url "https://github.com/robinv8/mino/releases/download/v#{version}/Mino-#{version}.dmg",
+      verified: "github.com/robinv8/mino/"
   name "Mino"
-  desc "Open-source macOS client for AI agents"
+  desc "Open-source client for AI agents"
   homepage "https://robinv8.github.io/mino/"
 
   depends_on macos: ">= :sonoma"
