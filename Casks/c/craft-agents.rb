f074f6924b0759eca0c4d4c9661567d8d11cc034
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 21:53:43 +0000

    craft-agents 0.8.4

--- Casks/c/craft-agents.rb
+++ Casks/c/craft-agents.rb
@@ -1,9 +1,9 @@
 cask "craft-agents" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.8.3"
-  sha256 arm:   "62bc713ba43c6d05cd38332a2d153f5ace9702cc4c51452c16ec9fd2f667d550",
-         intel: "13d42bd8cad70c14cafbfdb7737f0b4b4181086ad1c8a534e8f0d0a158b6f0cb"
+  version "0.8.4"
+  sha256 arm:   "30d0771fbca25180f1c3d889845b3de83ad1d2f80c092e512e9a8a6a81fdba81",
+         intel: "31f0cc16980c0e49fdcb87ccdeeb6bfa34e713b6d758757f3e98d1187f2342eb"
 
   url "https://github.com/lukilabs/craft-agents-oss/releases/download/v#{version}/Craft-Agents-#{version}-mac-#{arch}.dmg",
       verified: "github.com/lukilabs/craft-agents-oss/"
