8eb29e2b12ab7d1f0aee425071fb1f61fba93c6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-09 14:12:16 +0000

    voiden@beta 1.4.4

--- Casks/v/voiden@beta.rb
+++ Casks/v/voiden@beta.rb
@@ -1,9 +1,9 @@
 cask "voiden@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.3-beta.1"
-  sha256 arm:   "235dd9f26044df0d55904ba1472bf058cadc0b94d59eca78364aff665f73d565",
-         intel: "06549f8732871f1db955ac2a7fc27442725f3e8ba3c63174048610077dd85b34"
+  version "1.4.4"
+  sha256 arm:   "c65458752e804cae8e17cd32db07c9426a1265e3bed884a8c9efb0e8f598baa6",
+         intel: "7ee183d89ab9327b9865675a552a647e0dbbb5aae543cd11049642f3ba984508"
 
   url "https://voiden-beta-releases.s3.eu-west-1.amazonaws.com/voiden/darwin/#{arch}/Voiden-darwin-#{arch}-#{version}.zip",
       verified: "voiden-beta-releases.s3.eu-west-1.amazonaws.com/"
