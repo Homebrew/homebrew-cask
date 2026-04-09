dc6ca9770eabd8d6e648a7c4d237031e4029b419
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-09 05:24:59 +0000

    vrew 3.8.2

--- Casks/v/vrew.rb
+++ Casks/v/vrew.rb
@@ -1,6 +1,6 @@
 cask "vrew" do
-  version "3.8.1"
-  sha256 "193ae274abd7638f817f561c75167c37fad78efed0eb32c28fb0b079b5e9a02f"
+  version "3.8.2"
+  sha256 "6b78c0bebcc4c8b2df4bf4f4471885b83c3a11b8a440a0661e74d78eb7b2d312"
 
   url "https://vrew-files.voyagerx.com/Vrew-#{version}.dmg"
   name "Vrew"
