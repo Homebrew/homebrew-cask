e108fb1c497d32e01d02e2964d47c53cc89a134f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-09 19:01:16 +0000

    discord@canary 0.0.1066

--- Casks/d/discord@canary.rb
+++ Casks/d/discord@canary.rb
@@ -1,6 +1,6 @@
 cask "discord@canary" do
-  version "0.0.1065"
-  sha256 "484cc998e64e758493ce6f77ae300d18fc7e0529f5f2a7513cb8a3422f13a084"
+  version "0.0.1066"
+  sha256 "274f5724a964f5cf08f0f62f317414438ed6296a54758d2f25ae699f237a2e6c"
 
   url "https://dl-canary.discordapp.net/apps/osx/#{version}/DiscordCanary.dmg",
       verified: "dl-canary.discordapp.net/"
