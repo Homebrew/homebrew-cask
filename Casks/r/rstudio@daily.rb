0dd584bdb687e3e0f996e705a48f1f448f821fb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-10 19:42:47 +0000

    rstudio@daily 2026.04.0-500

--- Casks/r/rstudio@daily.rb
+++ Casks/r/rstudio@daily.rb
@@ -1,6 +1,6 @@
 cask "rstudio@daily" do
-  version "2026.04.0-498"
-  sha256 "ce1882fa944dfebd84d3ae57bfe7c30ac0eca97f53863937a8109f7d6d5cb57e"
+  version "2026.04.0-500"
+  sha256 "255b13afc7c32f5fcd0a4b2d816364dfa8918334f012ff78cf1a019f97c5c08d"
 
   url "https://rstudio-ide-build.s3.amazonaws.com/electron/macos/RStudio-#{version}.dmg",
       verified: "rstudio-ide-build.s3.amazonaws.com/electron/macos/"
