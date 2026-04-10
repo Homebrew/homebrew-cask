487217659da84596e4c78afd77eb3e9488d0d4d9
Chun Cai <amoycaic@gmail.com>
2026-04-10 17:59:57 +0800

    Update SHA256 checksums and URL format

--- Casks/n/nvidia-nsight-compute.rb
+++ Casks/n/nvidia-nsight-compute.rb
@@ -2,10 +2,10 @@ cask "nvidia-nsight-compute" do
   arch arm: "arm64", intel: "x86_64"
 
   version "2026.1.0.2"
-  sha256 arm:   "e7df8a120a66f72a9a740c81350d365e3acb1f9cff8eab6a9db49f2d702e269d",
-         intel: "03ea2e18bbf3e3bcbe552b15ba57159dda795e073efc8dbe1201c7006051e01f"
+  sha256 arm:   "9ce222f251ead0110f89c136030232e313a0df2ba5b26dc28fd16b7945057376",
+         intel: "15acf4f2f7855554e1341ee62713cc79e248a58700eab5a17edeed0dce255acc"
 
-  url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-compute/#{version.major_minor_patch.dots_to_underscores}/nsight-compute-mac-#{arch}-#{version}.dmg"
+  url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-compute/#{version.major_minor_patch.dots_to_underscores}/nsight_compute-mac-#{arch}-#{version}.dmg"
   name "NVIDIA Nsight Compute"
   desc "Interactive profiler for CUDA and NVIDIA OptiX"
   homepage "https://developer.nvidia.com/nsight-compute"
