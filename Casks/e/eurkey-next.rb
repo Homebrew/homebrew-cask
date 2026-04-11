1c366efacd2f6263a5ae114d65f74cfbcf8e1310
Jan Simson <git@simson.io>
2026-04-11 19:17:56 +0200

    Add eurkey-next 2026.03.22

---
+++ Casks/e/eurkey-next.rb
@@ -0,0 +1,23 @@
+cask "eurkey-next" do
+  version "2026.03.22"
+  sha256 "c951f9bfcfc426d38991d1bf8e0be1142bb902dddd84f97a575958f08057b243"
+
+  url "https://github.com/felixfoertsch/EurKEY-Next/releases/download/#{version}/EurKEY-Next-#{version}.dmg",
+      verified: "github.com/felixfoertsch/EurKEY-Next/"
+  name "EurKEY Next keyboard layout"
+  desc "The keyboard layout for Europeans, coders, and translators."
+  homepage "https://eurkey-macos.eu/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keyboard_layout "EurKEY-Next.bundle"
+
+  # No zap stanza required
+
+  caveats do
+    reboot
+  end
+end
