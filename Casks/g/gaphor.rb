701db30fa316becef5709c125cd5c983a69d5e22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-12 19:05:45 +0000

    gaphor 3.3.0

--- Casks/g/gaphor.rb
+++ Casks/g/gaphor.rb
@@ -1,9 +1,9 @@
 cask "gaphor" do
   arch arm: "arm", intel: "intel"
 
-  version "3.2.0"
-  sha256 arm:   "ae59e7f232084e7091bbb31946900cd50d508faa12a27346c5bed96cab698c4e",
-         intel: "4fb5d766211d9688f1d7eb331a71fbb673453db9195643a225bf80596892d155"
+  version "3.3.0"
+  sha256 arm:   "360bb8bfba18a53199b535c2e257a8ddffe1bc6b1013a37421645f9b48e53d10",
+         intel: "1e2dbfe8c48a4b00070c17eac16afe062152c16e86d0c7717345630b07365660"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}-#{arch}.dmg",
       verified: "github.com/gaphor/gaphor/"
