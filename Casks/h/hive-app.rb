a6f633fef886bd953b274445b11ff06fa7e0221d
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2026-04-12 16:37:52 +0200

    hive-app: update 1.0.104

--- Casks/h/hive-app.rb
+++ Casks/h/hive-app.rb
@@ -1,9 +1,9 @@
 cask "hive-app" do
   arch arm: "-arm64"
 
-  version "1.0.103"
-  sha256 arm:   "deec1470a0a3131db722f6b69f657eb008c78baaceff4758524ae7d71b5e139c",
-         intel: "1a9b6f74b19a87b5c32bed441f96c4594f20452eca691aa3c860e35cd7dc1623"
+  version "1.0.104"
+  sha256 arm:   "d2fe10b1cb962baff925c3a740cc7793ab3050e6a0467d18bed6af91b039b7e0",
+         intel: "ace955cb4a3a95f0baf5d77c832dfea8d6ba389ffa9c2cfcff3369fd8d5bae06"
 
   url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}#{arch}.dmg"
   name "Hive"
