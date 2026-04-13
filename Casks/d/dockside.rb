6ad54a7fd702dfd800badec60d967c95f080f411
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 19:00:47 +0000

    dockside 2.6.0

--- Casks/d/dockside.rb
+++ Casks/d/dockside.rb
@@ -1,6 +1,6 @@
 cask "dockside" do
-  version "2.5.4"
-  sha256 "b5650de0bdecc966a6fc9d9148ea1388567b4e8938334f33941cee5de76905f3"
+  version "2.6.0"
+  sha256 "97cbc3f239d2443f5b943d7ac0433134d9cfd6e655b019975082fd5a702d608d"
 
   url "https://github.com/PrajwalSD/Dockside/releases/download/v#{version}/Dockside.dmg",
       verified: "github.com/PrajwalSD/Dockside/"
