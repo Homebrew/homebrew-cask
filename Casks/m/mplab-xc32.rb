a29d089304d9480022ade581f435f46250009991
bevanjkay <email@bevankay.me>
2026-04-13 23:24:03 +1000

    mplab-xc32 5.10

--- Casks/m/mplab-xc32.rb
+++ Casks/m/mplab-xc32.rb
@@ -1,16 +1,15 @@
 cask "mplab-xc32" do
   # NOTE: "32" is not a version number, but an intrinsic part of the product name
-  version "5.00"
-  sha256 "e5871c0c4627bb48bcd06dfce2ccb1b5ef0ddc1da50692b373c70a42a165763d"
+  version "5.10"
+  sha256 "914ad0c474595f452e7c1e9fb042934c347d10f12d31479e9bebe73a1091e671"
 
   url "https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc32-v#{version}-full-install-osx-installer.dmg"
   name "MPLab XC32 Compiler"
   desc "Compiler for 32-bit PIC and SAM MCUs and MPUs"
-  homepage "https://www.microchip.com/mplab/compilers"
+  homepage "https://www.microchip.com/en-us/tools-resources/develop/mplab-xc-compilers/xc32"
 
   livecheck do
-    url "https://www.microchip.com/en-us/tools-resources/develop/mplab-xc-compilers/xc32",
-        user_agent: :browser
+    url :homepage, user_agent: :browser
     regex(%r{href=.*?SoftwareTools/xc32[._-]v?(\d+(?:\.\d+)+)[._-]full[._-]install[._-]osx[._-]installer\.dmg}i)
   end
 
