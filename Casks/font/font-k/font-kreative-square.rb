88b100d1a11ac6b1b30748049f4ba74652323d3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 07:34:06 +0000

    font-kreative-square 2026-04-12

--- Casks/font/font-k/font-kreative-square.rb
+++ Casks/font/font-k/font-kreative-square.rb
@@ -1,6 +1,6 @@
 cask "font-kreative-square" do
-  version "2026-02-08"
-  sha256 "4b6e487c7d6f13a65bb3f4101e3ecb19a73019814ed5874133da6eda128a6a3e"
+  version "2026-04-12"
+  sha256 "e6f393e2ec82073fb297da9b1c601ef6225729fbc109a0121f0e64fb3d773e05"
 
   url "https://github.com/kreativekorp/open-relay/releases/download/#{version}/KreativeSquare.zip",
       verified: "github.com/kreativekorp/open-relay/"
