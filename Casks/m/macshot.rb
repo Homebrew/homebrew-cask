dca658a42ae414071416f218e5fb3480fa7d80f9
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-04-14 19:40:19 +0200

    macshot 4.0.4 (new cask)

---
+++ Casks/m/macshot.rb
@@ -0,0 +1,28 @@
+cask "macshot" do
+  version "4.0.4"
+  sha256 "3a25a3d64cf0b462067f1071a4d0d845c3c970e7f11638e3e7f1a5fa7a0dd23f"
+
+  url "https://github.com/sw33tLie/macshot/releases/download/v#{version}/MacShot.dmg"
+  name "macshot"
+  desc "Native screenshot tool inspired by Flameshot"
+  homepage "https://github.com/sw33tLie/macshot"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/sw33tLie/macshot/main/appcast.xml"
+    strategy :sparkle do |items|
+      items.find { |item| item.channel.nil? }&.short_version
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "macshot.app"
+
+  uninstall quit: "com.sw33tlie.macshot.macshot"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.sw33tlie.macshot.macshot",
+    "~/Library/Containers/com.sw33tlie.macshot.macshot",
+  ]
+end
