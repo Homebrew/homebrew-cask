8d190db06289ba208df4c5423f6cc7e7634854f7
Bevan Kay <email@bevankay.me>
2026-04-14 13:54:36 +0000

    a-better-finder-rename 12.28

--- Casks/a/a-better-finder-rename.rb
+++ Casks/a/a-better-finder-rename.rb
@@ -1,5 +1,5 @@
 cask "a-better-finder-rename" do
-  version "12.26"
+  version "12.28"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.publicspace.net/download/ABFRX#{version.major}.dmg"
