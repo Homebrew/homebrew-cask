19eadfbb76f2f558430192f7e4162431e7a707cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 13:18:08 +0000

    djstudio 4.1.5

--- Casks/d/djstudio.rb
+++ Casks/d/djstudio.rb
@@ -2,9 +2,9 @@ cask "djstudio" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "-apple"
 
-  version "4.1.4"
-  sha256 arm:   "87602a50fdd14d059546cf8237cfab6d4eecce6f418f7450be1f37bb094b684d",
-         intel: "ba5b34eb70a33138edf6b78805b2f97df44b4a7444a1e53daf633c1b4f1d4753"
+  version "4.1.5"
+  sha256 arm:   "b79010d45b1d344b2d81b5f90c1175a17dd0bd0eb625adbd901e48e807e64d85",
+         intel: "653a6d7e9d78f034e17da879b3118f4b4983895ea5f7edb16ffa1ead081d97e1"
 
   url "https://download.dj.studio/DJ.Studio-#{version}#{arch}.dmg"
   name "DJ.Studio"
