a83a7252cdb1857d2e37a0c2b93bdf21cf2db400
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 04:49:56 +0000

    get-iplayer-automator 1.28.2

--- Casks/g/get-iplayer-automator.rb
+++ Casks/g/get-iplayer-automator.rb
@@ -1,6 +1,6 @@
 cask "get-iplayer-automator" do
-  version "1.28.1,20260413002"
-  sha256 "711d0caef504655d73a1a423358e552fb0fb90ce753e57556de5960bd192afb4"
+  version "1.28.2,20260414001"
+  sha256 "7502a03ddccb831d00d67cda65ac26cc95d0f8614b414631965c5d0825b58851"
 
   url "https://github.com/Ascoware/get-iplayer-automator/releases/download/v#{version.csv.first}/Get.iPlayer.Automator.v#{version.csv.first}.b#{version.csv.second}.zip"
   name "Get iPlayer Automator"
