b3d6c63c7e0a8d44b03c680c1edd6ce6a0eb1ff7
Connor Adams <connorads@users.noreply.github.com>
2026-04-15 23:20:50 +0100

    Update depends on
    Co-authored-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/g/google-gemini.rb
+++ Casks/g/google-gemini.rb
@@ -16,8 +16,8 @@ cask "google-gemini" do
   end
 
   auto_updates true
-  depends_on arch:  :arm64,
-             macos: ">= :sequoia"
+  depends_on macos: ">= :sequoia"
+  depends_on arch: :arm64
 
   app "Gemini.app"
 
