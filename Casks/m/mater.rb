251758dd8d6269f68591079b14957004bd38db6f
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-14 22:22:31 -0400

    mater 3.0.0
    `mater` is autobumped but the workflow failed to update to version
    3.0.0 because the app has been rewritten and upstream now publishes
    only a single universal binary in a zip file using a different file
    name format. This updates the version and adjusts the cask
    accordingly.

--- Casks/m/mater.rb
+++ Casks/m/mater.rb
@@ -1,34 +1,15 @@
 cask "mater" do
-  on_arm do
-    version "2.0.3"
-    sha256 "3475eac057163616e5f2ad7d91ff02faeca9bfd267bfc963e78ca284212a20f3"
+  version "3.0.0"
+  sha256 "7aaa096ab307da5123641f9238083c413de3929625cf1ecd1cd080814ce855ff"
 
-    url "https://github.com/jasonlong/mater/releases/download/v#{version}/Mater-#{version}-arm64.dmg"
-
-    depends_on macos: ">= :monterey"
-
-    app "Mater.app"
-  end
-  on_intel do
-    version "1.0.10"
-    sha256 "613dba1cd8ca8dee74b30a456d3d2cb87896020b5305d6ff25f5f324499c4ee7"
-
-    url "https://github.com/jasonlong/mater/releases/download/#{version}/Mater-darwin-x64.zip"
-
-    livecheck do
-      skip "Legacy version"
-    end
-
-    depends_on macos: ">= :catalina"
-
-    app "Mater-darwin-x64/Mater.app"
-  end
-
+  url "https://github.com/jasonlong/mater/releases/download/v#{version}/Mater-v#{version}-macos.zip"
   name "Mater"
   desc "Menubar pomodoro app"
   homepage "https://github.com/jasonlong/mater"
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  depends_on macos: ">= :sonoma"
+
+  app "Mater.app"
 
   zap trash: [
     "~/Library/Application Support/mater",
