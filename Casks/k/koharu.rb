0a4ecde4c39b2758c299207c7a2ff696ac381c54
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-04-14 20:27:04 -0600

    koharu 0.44.3 (new cask)

---
+++ Casks/k/koharu.rb
@@ -0,0 +1,20 @@
+cask "koharu" do
+  version "0.44.3"
+  sha256 "cd8494273bc36f3d22e7c64cf21672a4adc5e7e64fcd07c3e045b68781ea532c"
+
+  url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg",
+      verified: "github.com/mayocream/koharu/"
+  name "Koharu"
+  desc "ML-powered manga translator"
+  homepage "https://koharu.rs/"
+
+  depends_on arch: :arm64
+
+  app "Koharu.app"
+
+  zap trash: [
+    "~/Library/Application Support/Koharu",
+    "~/Library/Caches/Koharu",
+    "~/Library/WebKit/Koharu",
+  ]
+end
