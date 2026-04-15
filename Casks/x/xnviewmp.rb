71da36269f5ca4ab566826139cdb0dc12d1b045a
Christoph Wiese <chwiese@users.noreply.github.com>
2026-04-15 15:37:16 +0200

    xnviewmp: use versioned url, add sha256

--- Casks/x/xnviewmp.rb
+++ Casks/x/xnviewmp.rb
@@ -1,8 +1,8 @@
 cask "xnviewmp" do
   version "1.10.5"
-  sha256 :no_check
+  sha256 "9972b02570b93b33d5b26c8aad8fcaaa947f9679762c768e762f0a44e6aa8ef4"
 
-  url "https://download.xnview.com/XnViewMP-mac.dmg"
+  url "https://download.xnview.com/old_versions/XnView_MP/XnView_MP-#{version}-mac.dmg"
   name "XnViewMP"
   desc "Photo viewer, image manager, image resiser and more"
   homepage "https://www.xnview.com/en/xnviewmp/"
