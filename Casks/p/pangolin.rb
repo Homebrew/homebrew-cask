4d928d6de41882aea7b8dd10eb31c95806dcee3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 22:24:43 +0000

    pangolin 0.7.0

--- Casks/p/pangolin.rb
+++ Casks/p/pangolin.rb
@@ -1,6 +1,6 @@
 cask "pangolin" do
-  version "0.6.2"
-  sha256 "1e688c192acc87a7c5ebab9347bee955da2f02d8ddf224261b0af614fbacda5e"
+  version "0.7.0"
+  sha256 "18eca7bd1de7a0fa7ad1e3b8d913ccc91716c738f11ded726a0a48c812be784c"
 
   url "https://pangolin.net/api/downloads/proxy?url=https%3A%2F%2Fgithub.com%2Ffosrl%2Fapple%2Freleases%2Fdownload%2Fmac-#{version}%2FPangolin_Installer_#{version}.dmg"
   name "Pangolin"
