f9a27e8fd0c5cc4acd2466af2e8727698fb6021b
Rui Chen <rui@chenrui.dev>
2026-04-16 15:15:04 -0400

    tritium: add fails_gatekeeper_check
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/t/tritium.rb
+++ Casks/t/tritium.rb
@@ -15,6 +15,8 @@ cask "tritium" do
     strategy :page_match, &:strip
   end
 
+  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   auto_updates true
   depends_on macos: ">= :big_sur"
 
