42bb66af72d6ddddae5ba88c60cae0c5bdc1bd7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 10:02:22 +0000

    bambu-connect 2.4.0-beta.10

--- Casks/b/bambu-connect.rb
+++ Casks/b/bambu-connect.rb
@@ -1,9 +1,9 @@
 cask "bambu-connect" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.0-beta.6"
-  sha256 arm:   "b5f1bf2099b2274150ec013a01f9267978fcb68f941d41ee134b78c430abf7d2",
-         intel: "8b441638f5c351447c81c7d893507aa937d2b413e0922408898d8b6d7855f454"
+  version "2.4.0-beta.10"
+  sha256 arm:   "8d8124c4c0cf4434f54a6b70a803e95fe941bb3fd5211c86293545a97ab1ef72",
+         intel: "00bd0f54aba664b35f6d1bf9d7163cf65d91bd88fb48f067183643f60a20b8a3"
 
   url "https://public-cdn.bblmw.com/upgrade/bambu-connect/updates/versions/#{version}/bambu-connect-v#{version}-darwin-#{arch}.dmg",
       verified: "public-cdn.bblmw.com/upgrade/bambu-connect/updates/versions/"
