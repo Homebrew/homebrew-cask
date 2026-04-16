a0b14b68af1ffbc8f991e245e74db793ef720e90
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 10:05:36 +0000

    cables 0.10.6

--- Casks/c/cables.rb
+++ Casks/c/cables.rb
@@ -1,9 +1,9 @@
 cask "cables" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "0.10.5"
-  sha256 arm:   "49e43e47474a7bfded15437f998745d6328ed77155edbe175827e0159667496b",
-         intel: "659ff1a384c98c2231c8923cca16d3469b8a8bbbe3ebfbc7826f6991bf434b59"
+  version "0.10.6"
+  sha256 arm:   "a42fc8dedd206b97a43210f4be8f0ebdf2421c5ce1cdf98c5fadb6a869bb32cb",
+         intel: "a11bf1067b64fbcac0837c73cb928f2113a4dd5c63817c742b27498631e7163e"
 
   url "https://github.com/cables-gl/cables_electron/releases/download/v#{version}/cables-#{version}-mac#{arch}.dmg"
   name "Cables"
