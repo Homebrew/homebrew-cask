ed5ba84f05832f7ba486b3e05276dcdfbc703920
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 13:12:35 +0000

    fabfilter-saturn 2.13

--- Casks/f/fabfilter-saturn.rb
+++ Casks/f/fabfilter-saturn.rb
@@ -1,6 +1,6 @@
 cask "fabfilter-saturn" do
-  version "2.12"
-  sha256 "ca100f12b563e48d726b5d547ddab8cd64a2558533c09b31d59be24b47e8f454"
+  version "2.13"
+  sha256 "2937c0054e748807db6e2b3d5ffeece0a21ea696aa844c70007ab789258f6888"
 
   url "https://cdn-b.fabfilter.com/downloads/ffsaturn#{version.no_dots}.dmg"
   name "FabFilter Saturn"
