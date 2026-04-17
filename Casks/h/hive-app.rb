7297e78eb4b2e1c4928a84c01c0a7fb74b760b36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 16:17:13 +0000

    hive-app 1.0.117

--- Casks/h/hive-app.rb
+++ Casks/h/hive-app.rb
@@ -1,9 +1,9 @@
 cask "hive-app" do
   arch arm: "-arm64"
 
-  version "1.0.116"
-  sha256 arm:   "28941d97b7d1433104b419ef45b61367f0413c6514b1bac379afdef29878c7f8",
-         intel: "638ed2c4cd82883335c0225ea820e0b685c6c8b27b0a937fc9a0ca430f946169"
+  version "1.0.117"
+  sha256 arm:   "6ea70e70d2297c85f791248813efeb2ead0ffc3ad32a95d431bd463533765cfb",
+         intel: "b8df08e4bbbe8f0734e271a1d801ae4f3d3c31ea0c39bbe8b7c8fd9b1a9a518d"
 
   url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}#{arch}.dmg"
   name "Hive"
