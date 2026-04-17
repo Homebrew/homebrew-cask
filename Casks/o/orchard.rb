e85206a3cd7e70b7ff9430393ca58b50206f63b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 22:26:56 +0000

    orchard 1.11.4

--- Casks/o/orchard.rb
+++ Casks/o/orchard.rb
@@ -1,6 +1,6 @@
 cask "orchard" do
-  version "1.11.3"
-  sha256 "5cd9b84239a436c29814c852c4ba822112e9e0dca9eed7e9a0c28a2a50dd50de"
+  version "1.11.4"
+  sha256 "45f86b2461620879f6e4a1d4f73151663f58939916598598f632cbaff2506907"
 
   url "https://github.com/andrew-waters/orchard/releases/download/v#{version}/Orchard-#{version}.dmg"
   name "Orchard"
