5e09f76f35149795b5b84c0df00bc46b97ea203b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 22:09:13 +0000

    geph 5.6.0

--- Casks/g/geph.rb
+++ Casks/g/geph.rb
@@ -1,6 +1,6 @@
 cask "geph" do
-  version "5.5.0"
-  sha256 "bbe4cb71fd266348f993abede06a74f3c5e43624e77e57adcbffdb80b281831d"
+  version "5.6.0"
+  sha256 "0f803340b8c067aab1a68690a8e0350b094b1bb85e94aa3f12aaff7a6cce415c"
 
   url "https://dl.geph.io/geph-releases/macos-stable/#{version}/geph-macos.dmg"
   name "Geph"
