df909f78150fedf24f11939da9eeedc0ecb821f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-18 01:27:42 +0000

    gitkraken-cli 3.1.59

--- Casks/g/gitkraken-cli.rb
+++ Casks/g/gitkraken-cli.rb
@@ -2,11 +2,11 @@ cask "gitkraken-cli" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "3.1.55"
-  sha256 arm:          "739beb2fb966214710a0202ee9608335ad7945cfedd099d1d68978d8b64bf49c",
-         intel:        "b1b28cceb6b5aaa49db687ab176a99d9bfa8db5bcd32624bad67bcc25bf26ca5",
-         arm64_linux:  "1d9f8d13b6a2a1f50916c2aec1c7e47b97439a7b38e2baa5520dadf2b544e0f9",
-         x86_64_linux: "94181f08dfbec21159428a8959eaf374fdbbbd273b0029d8807d0c5073687686"
+  version "3.1.59"
+  sha256 arm:          "0c7dec740bf79234212e81bc6fe93c0a19cc05c6d89946e4d5fdbf22bea11873",
+         intel:        "2103da9e964c9b9858e605b3db716f3dacd2a93e13af205547ab02ffc0489925",
+         arm64_linux:  "00b007827554383caf080e499c409d05b5df6085d7f15f1b6562ef9306f07681",
+         x86_64_linux: "9973cff99eab0403d94d18be282cfd7b3cbe9cac54ab26fae992070bcc71b5ce"
 
   url "https://github.com/gitkraken/gk-cli/releases/download/v#{version}/gk_#{version}_#{os}_#{arch}.zip"
   name "GitKraken CLI"
