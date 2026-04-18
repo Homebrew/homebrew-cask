e5d29b362ba215749091bc602f68c64e73bf79a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-18 16:28:30 +0000

    sky 0.5.2

--- Casks/s/sky.rb
+++ Casks/s/sky.rb
@@ -1,6 +1,6 @@
 cask "sky" do
-  version "0.5.1"
-  sha256 "4d20a4618f1adc5a3f07f372e3293c35137f3452ea6a5d3fdc57fe7d2c775b39"
+  version "0.5.2"
+  sha256 "ae823d8926471c64f35735f6b36a07e9d342ce4057d27dc4242e19a1f454b209"
 
   url "https://github.com/jcsalterego/Sky.app/releases/download/#{version}/Sky.#{version}.dmg"
   name "Sky"
