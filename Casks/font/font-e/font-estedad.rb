99c5645693a681ade5c6cd5ef2cb7070e0848233
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-19 00:28:07 +0000

    font-estedad: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-e/font-estedad.rb
@@ -0,0 +1,12 @@
+cask "font-estedad" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/estedad/Estedad%5Bwght%5D.ttf"
+  name "Estedad"
+  homepage "https://github.com/aminabedi68/Estedad"
+
+  font "Estedad[wght].ttf"
+
+  # No zap stanza required
+end
