6329d8930c363734c5a9714430ad7d965a213bab
Jannes Bikker <jannesbikker@gmail.com>
2026-04-20 10:11:22 +0200

    mujoco 3.7.0 (new cask)

---
+++ Casks/m/mujoco.rb
@@ -0,0 +1,19 @@
+cask "mujoco" do
+  version "3.7.0"
+  sha256 "da863520e171c1a3c8005810529058b8c9bb7bc0b51a54731d12b614053c5c2e"
+
+  url "https://github.com/google-deepmind/mujoco/releases/download/#{version}/mujoco-#{version}-macos-universal2.dmg",
+      verified: "github.com/google-deepmind/mujoco/releases/download/"
+  name "MuJoCo"
+  desc "General purpose physics engine"
+  homepage "https://mujoco.org/"
+
+  livecheck do
+    url "https://github.com/google-deepmind/mujoco"
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "MuJoCo.app"
+end
