f0e420c4359a47c75d0171bcebe053fbd757bd04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-20 16:30:38 +0000

    koharu 0.47.1

--- Casks/k/koharu.rb
+++ Casks/k/koharu.rb
@@ -1,6 +1,6 @@
 cask "koharu" do
-  version "0.47.0"
-  sha256 "7e2bd873e74acf0e47e4251a29510506738e96aa622b61e8f4269d7310ad9377"
+  version "0.47.1"
+  sha256 "8c6192eb0a5be9eb9151fc076cb1362179626e6e83ff38fe30d27a5e774c2178"
 
   url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg",
       verified: "github.com/mayocream/koharu/"
