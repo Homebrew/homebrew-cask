d1632d7cfbdd83a93947e887580f106659e3eff2
Jannes Bikker <jannesbikker@gmail.com>
2026-04-20 12:28:44 +0200

    mujoco: simplify cask metadata

--- Casks/m/mujoco.rb
+++ Casks/m/mujoco.rb
@@ -3,15 +3,10 @@ cask "mujoco" do
   sha256 "da863520e171c1a3c8005810529058b8c9bb7bc0b51a54731d12b614053c5c2e"
 
   url "https://github.com/google-deepmind/mujoco/releases/download/#{version}/mujoco-#{version}-macos-universal2.dmg",
-      verified: "github.com/google-deepmind/mujoco/releases/download/"
+      verified: "github.com/google-deepmind/mujoco/"
   name "MuJoCo"
   desc "General purpose physics engine"
   homepage "https://mujoco.org/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   depends_on macos: ">= :big_sur"
 
