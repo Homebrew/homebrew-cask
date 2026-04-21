9593736215cce0d779fc11e98c36a0a939df783d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 05:07:15 +0000

    nessie-app 1.4.124

--- Casks/n/nessie-app.rb
+++ Casks/n/nessie-app.rb
@@ -1,6 +1,6 @@
 cask "nessie-app" do
-  version "1.4.121"
-  sha256 "837a6f9a8f169a8330e8fbffebc2105d8d9eff63aa1c1c69ac24ec3465832f0b"
+  version "1.4.124"
+  sha256 "348b14e0e1515b851719cc69900c1aac6b6abd5f7a924c1316d21e8194237abc"
 
   url "https://nessie-notes-app-auto-updates.s3.us-west-2.amazonaws.com/Nessie_#{version}.zip",
       verified: "nessie-notes-app-auto-updates.s3.us-west-2.amazonaws.com/"
