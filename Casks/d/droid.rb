930d440a65578f0fb3061024643d34eb07e8aa8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 01:21:17 +0000

    droid 0.105.0

--- Casks/d/droid.rb
+++ Casks/d/droid.rb
@@ -1,9 +1,9 @@
 cask "droid" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.104.1"
-  sha256 arm:   "f7b71d93b69ec9b6edad87e06a31cfbf2b29fe0dc4c7f905e647869beb21640c",
-         intel: "22d800861e3736b0e0e0c4bb3b56ecc84dcb1386de0b7d094c5df080ae62d5d3"
+  version "0.105.0"
+  sha256 arm:   "7c6e4f3a15ed0d441069758d58f9c2abbee4ae50c30a52228063962156704e5f",
+         intel: "c4bd188b6ce8f08cc7aef24ed3dbae95692fbaf9ca826ff0362d327d9a2290ce"
 
   url "https://downloads.factory.ai/factory-cli/releases/#{version}/darwin/#{arch}/droid"
   name "Droid"
