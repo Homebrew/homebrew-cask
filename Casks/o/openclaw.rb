235921802332b49d07a73d6bc784213ba89cc0ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 22:43:00 +0000

    openclaw 2026.4.20

--- Casks/o/openclaw.rb
+++ Casks/o/openclaw.rb
@@ -1,6 +1,6 @@
 cask "openclaw" do
-  version "2026.4.15"
-  sha256 "8e455b5b9b6be56104f0aa8502904f686f7e14322f49fac9b265e02e51de5684"
+  version "2026.4.20"
+  sha256 "6094b616980bb6bf8a4a4928c1075bdff731dd621547f92c836d329c0d4bfcf7"
 
   url "https://github.com/openclaw/openclaw/releases/download/v#{version}/OpenClaw-#{version}.dmg",
       verified: "github.com/openclaw/openclaw/"
