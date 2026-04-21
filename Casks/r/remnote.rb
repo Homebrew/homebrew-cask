72ee45559e6661a460324613c7e1eb356c144baf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 01:56:12 +0000

    remnote 1.25.18

--- Casks/r/remnote.rb
+++ Casks/r/remnote.rb
@@ -2,9 +2,9 @@ cask "remnote" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "_m1"
 
-  version "1.25.7"
-  sha256 arm:   "8be799b16cdc60740c334b56b71542a4d05d0eb715f1b36b50ec596d2371a1bd",
-         intel: "d572e394628f80b01ab1a9777775451f701d01587ff6a8ddc6a80506efd42189"
+  version "1.25.18"
+  sha256 arm:   "43e7faed28c169b68cfb2160b1577ddcdf1b238058ab2c37cd3d98dd76d7f04f",
+         intel: "4603a2b145b9e312cbbdf6316bc502b3a8ed8d6d47379c3e8a78ddbc34be363d"
 
   url "https://download2.remnote.io/remnote-desktop2/RemNote-#{version}#{arch}-mac.zip",
       verified: "download2.remnote.io/"
