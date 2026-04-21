035fd09d3efb0abc9d16c3f42558d0c33db9f031
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 16:41:15 +0000

    kameleo 4.4.1

--- Casks/k/kameleo.rb
+++ Casks/k/kameleo.rb
@@ -1,6 +1,6 @@
 cask "kameleo" do
-  version "4.4.0"
-  sha256 "effe2420c98e4c08ae5d55793b437c056d0e61462440ca1e9c47f2682c31d7a8"
+  version "4.4.1"
+  sha256 "673b50d7d55a19f76aafb020cb4d96420898e9d8a0dd7d82361be98fb7b41aa3"
 
   url "https://github.com/kameleo-io/kameleo/releases/download/#{version}/kameleo-#{version}-osx-arm64.dmg",
       verified: "github.com/kameleo-io/kameleo/"
