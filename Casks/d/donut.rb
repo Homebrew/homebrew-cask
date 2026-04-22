72d9e54c6cd71b6ab934b8aee73bedb35911ed40
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-22 01:18:42 +0000

    donut 0.21.2

--- Casks/d/donut.rb
+++ Casks/d/donut.rb
@@ -1,9 +1,9 @@
 cask "donut" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.21.1"
-  sha256 arm:   "e6ec67e614654e06822b8a3bf1674ca6c32ddf82ed6fdc0d898f15b6b29d2822",
-         intel: "6fa9f6889a0a73fe070811efebf84dc92e28ba14c571e41f7852e7cd41b86e58"
+  version "0.21.2"
+  sha256 arm:   "654028f631540939056aae95befb87bf16dd346642514b7d1b09f8b0640d5d7f",
+         intel: "037e1f47fc5dc5bd26ac7581941a380b66287a59e96ecd2c70f519da771be58b"
 
   url "https://github.com/zhom/donutbrowser/releases/download/v#{version}/Donut_#{version}_#{arch}.dmg",
       verified: "github.com/zhom/donutbrowser/"
