81689a6df2ed1f7f8198b09f64aff04c9222799a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-22 13:35:11 +0000

    oxygen-xml-editor 28.1,2026042105

--- Casks/o/oxygen-xml-editor.rb
+++ Casks/o/oxygen-xml-editor.rb
@@ -1,5 +1,5 @@
 cask "oxygen-xml-editor" do
-  version "28.1,2026031705"
+  version "28.1,2026042105"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen-openjdk.dmg"
