57b541eb0e41e0ec0920ef53e0550b1cf944e00e
hejamu <info@jaegerhenrik.de>
2026-04-22 11:08:06 +0200

    dcp-o-matic-combiner 2.18.38

--- Casks/d/dcp-o-matic-combiner.rb
+++ Casks/d/dcp-o-matic-combiner.rb
@@ -1,13 +1,17 @@
 cask "dcp-o-matic-combiner" do
-  version "2.18.21"
-  sha256 "99c12466dcad32958c3c162a207d843a60e3505e0b39b5fe9b58ddfcaaeebdc4"
+  version "2.18.38"
+  sha256 "412c21a9b807134ebb39fa3163d5a25d4852cb5ecc4169a5f2e391f86e26cce5"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.10-combiner&version=#{version}"
+  url "https://download.dcpomatic.com/dl.php?id=osx-10.10-combiner&version=#{version}"
   name "DCP-o-matic-combiner"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Package)"
   homepage "https://dcpomatic.com/"
 
-  disable! date: "2025-07-28", because: "cannot be reliably fetched due to Cloudflare protections"
+  livecheck do
+    cask "dcp-o-matic"
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "DCP-o-matic #{version.major} Combiner.app"
 
