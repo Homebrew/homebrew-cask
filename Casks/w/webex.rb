6c2556167278b723c9f19f583db3397797c34078
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-22 22:50:02 +0000

    webex 46.4.0.34752

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch arm: "apple-silicon", intel: "intel"
 
-  version "46.4.0.34620"
+  version "46.4.0.34752"
   sha256 :no_check
 
   url "https://binaries.webex.com/webex-macos-#{arch}/Webex.dmg"
