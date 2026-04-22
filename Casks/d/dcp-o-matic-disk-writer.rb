5a6cadddba4e254b228a366b20cefe33c5f3d303
hejamu <info@jaegerhenrik.de>
2026-04-22 11:09:05 +0200

    dcp-o-matic-disk-writer 2.18.38

--- Casks/d/dcp-o-matic-disk-writer.rb
+++ Casks/d/dcp-o-matic-disk-writer.rb
@@ -1,13 +1,17 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.18.21"
-  sha256 "a84f5404478892c7b3b0e277b5f9746bb84f9e782b4015c0bb0693ae380c4152"
+  version "2.18.38"
+  sha256 "2dca356da0e13911b1b41e3562324ad1fd78096c4165e537b80634e65d565c5a"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
+  url "https://download.dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Package)"
   homepage "https://dcpomatic.com/"
 
-  disable! date: "2025-07-28", because: "cannot be reliably fetched due to Cloudflare protections"
+  livecheck do
+    cask "dcp-o-matic"
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "DCP-o-matic #{version.major} Disk Writer.app"
 
