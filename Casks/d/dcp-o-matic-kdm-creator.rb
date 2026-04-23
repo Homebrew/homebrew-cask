567a33aff5273b8efa2ad2abd31ad1927ec79ccc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 19:05:21 +0000

    dcp-o-matic-kdm-creator 2.18.39

--- Casks/d/dcp-o-matic-kdm-creator.rb
+++ Casks/d/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.18.38"
-  sha256 "094081cef22976e50898175f7b9be3d43cf909f10d796f64f6015d66e3f098f4"
+  version "2.18.39"
+  sha256 "4ad13a8a5b00356a6ad92c8658612ab2af83f2b865f9cec03c58cddd142ca112"
 
   url "https://download.dcpomatic.com/dl.php?id=osx-10.10-kdm&version=#{version}"
   name "DCP-o-matic KDM Creator"
