dd26e56e4d19096e0172e0c1c6fd4d8750965fb5
Max Hoffmann <mjhoffmann@gmail.com>
2026-04-23 16:45:30 +0200

    gaphor: update checksum

--- Casks/g/gaphor.rb
+++ Casks/g/gaphor.rb
@@ -2,8 +2,8 @@ cask "gaphor" do
   arch arm: "arm", intel: "intel"
 
   version "3.3.1"
-  sha256 arm:   "4c25f33900c416d60cb51c19eff0e8e07144e1c9c69c0ecadcb4860aa218edb2",
-         intel: "a5a6fbb61abb5720828e0c299d40c74fb7cc6c09a6a97ec6f55e5f72559a079a"
+  sha256 arm:   "3fa7aa35f41081f5e3868dbb7778e58ada71d7678fff144cbd50ab0808e97f99",
+         intel: "ec315358592b2d6193b0cf540b631510d1b8aa9712ca5879f24883ce602ff56c"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}-#{arch}.dmg",
       verified: "github.com/gaphor/gaphor/"
