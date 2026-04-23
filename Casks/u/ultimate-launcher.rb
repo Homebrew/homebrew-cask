d3f0d705bb75191348d0478a266e7649256c4b99
Mukunth P.R <90772408+mukunthpr@users.noreply.github.com>
2026-04-23 13:29:57 +0530

    Add files via upload

---
+++ Casks/u/ultimate-launcher.rb
@@ -0,0 +1,11 @@
+cask "ultimate-launcher" do
+  version "1.0.0"
+  sha256 "7fbc7dce01633ff8ef4a9aadc4f22c92130d3fcf0aad5167cc19079672d5cc4f"
+
+  url "https://github.com/mukunthpr/UltimateLauncher/releases/download/v1.0.0/UltimateLauncher-macOS-x64.dmg"
+  name "Ultimate Launcher"
+  desc "A simple launcher application"
+  homepage "https://github.com/mukunthpr/UltimateLauncher"
+
+  app "UltimateLauncher.app"
+end
