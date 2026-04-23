cb10a243679984348f045109b843441abc806d04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 10:26:14 +0000

    fontbase 2026.4.6

--- Casks/f/fontbase.rb
+++ Casks/f/fontbase.rb
@@ -1,6 +1,6 @@
 cask "fontbase" do
-  version "2026.4.1"
-  sha256 "3bf48c378947791adf18c73e8d1d2be5135b8d646e5c369c7b8bd174e05e4803"
+  version "2026.4.6"
+  sha256 "57739584a1b266b1ae75f615fb847417d6216b7349c3ad87dd64a2c78f7bae03"
 
   url "https://releases.fontba.se/mac/FontBase-#{version}.dmg"
   name "FontBase"
