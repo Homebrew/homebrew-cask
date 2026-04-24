d0a8e90a646286ee11b6b35986f07ffa924cab70
Mukunth P.R <90772408+mukunthpr@users.noreply.github.com>
2026-04-24 16:37:09 +0530

    Change SHA256 to no check and update description
    Updated SHA256 to no check and modified description.

--- Casks/u/ultimate-launcher.rb
+++ Casks/u/ultimate-launcher.rb
@@ -1,10 +1,10 @@
 cask "ultimate-launcher" do
   version "1.0.0"
-  sha256 "7fbc7dce01633ff8ef4a9aadc4f22c92130d3fcf0aad5167cc19079672d5cc4f"
+  sha256 :no_check
 
   url "https://github.com/mukunthpr/UltimateLauncher/releases/download/v1.0.0/UltimateLauncher-macOS-x64.dmg"
   name "Ultimate Launcher"
-  desc "A simple launcher application"
+  desc "simple launcher application"
   homepage "https://github.com/mukunthpr/UltimateLauncher"
 
   app "UltimateLauncher.app"
