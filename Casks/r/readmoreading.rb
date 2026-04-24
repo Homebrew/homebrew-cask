2380d9b2a2cf43a933a86f2025b48240dbb54761
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 05:24:09 +0000

    readmoreading 1.7.4

--- Casks/r/readmoreading.rb
+++ Casks/r/readmoreading.rb
@@ -1,9 +1,9 @@
 cask "readmoreading" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.7.2"
-  sha256 arm:   "7531deb995bcf102560c9861b9fc7166ae0a2fae3e97f9f62c876e000850e789",
-         intel: "62c68ad7b6c956f99d6a00f21351a13f3b26bb7ae1e1419ffa31493a672a946d"
+  version "1.7.4"
+  sha256 arm:   "ae879e6f543a0f386476b36ef8777dd88060931fca697076539173ac18271d74",
+         intel: "800b1d2d68f35a6e069074f4d4dffe45c7cf6feabaa94e6f5e3f2429b087aed8"
 
   url "https://github.com/eCrowdMedia/remake/releases/download/v#{version}/Readmoo.-#{version}-#{arch}.dmg",
       verified: "github.com/eCrowdMedia/remake/"
