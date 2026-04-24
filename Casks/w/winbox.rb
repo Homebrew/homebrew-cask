d0b103f94537da4b6227e467ac79b0a033a8b476
Oliver Kuckertz <oliver@kuckertz.cloud>
2026-04-24 23:31:49 +0200

    winbox: add Linux support

--- Casks/w/winbox.rb
+++ Casks/w/winbox.rb
@@ -1,8 +1,32 @@
 cask "winbox" do
-  version "4.1"
-  sha256 "45715942a89261d4b14cf5edb13c73d65458c853bdef6d1dfc9da500259cacb2"
+  os linux: "_Linux"
 
-  url "https://download.mikrotik.com/routeros/winbox/#{version}/WinBox.dmg"
+  version "4.1"
+
+  url "https://download.mikrotik.com/routeros/winbox/#{version}/WinBox#{os}.dmg"
+  on_macos do
+    sha256 "45715942a89261d4b14cf5edb13c73d65458c853bdef6d1dfc9da500259cacb2"
+
+    depends_on macos: ">= :monterey"
+
+    app "WinBox.app"
+
+    zap trash: [
+      "~/Library/Application Support/MikroTik/WinBox",
+      "~/Library/Caches/MikroTik/WinBox",
+      "~/Library/Saved Application State/com.mikrotik.winbox.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "28d35b661c321f5b618936546b7edf6593292549ed4a9584788dadff39a54d8f"
+
+    depends_on arch: :x86_64
+
+    binary "WinBox", target: "winbox"
+
+    zap trash: "#{ENV.fetch("HOMEBREW_XDG_DATA_HOME", "~/.local/share")}/MikroTik/WinBox"
+  end
+
   name "WinBox"
   desc "Administration tool for MikroTik RouterOS"
   homepage "https://mikrotik.com/"
@@ -11,14 +35,4 @@ cask "winbox" do
     url "https://upgrade.mikrotik.com/routeros/winbox/LATEST.#{version.major}"
     regex(/v?(\d+(?:\.\d+)+((?:beta|rc)\d+)?)/i)
   end
-
-  depends_on macos: ">= :monterey"
-
-  app "WinBox.app"
-
-  zap trash: [
-    "~/Library/Application Support/MikroTik/WinBox",
-    "~/Library/Caches/MikroTik/WinBox",
-    "~/Library/Saved Application State/my.example.com.savedState",
-  ]
 end
