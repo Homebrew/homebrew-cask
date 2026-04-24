33949615043a696174e88eca71d0e9bde962077b
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2026-04-24 13:56:52 +0800

    serverbox 1.0.1365 (new cask)

---
+++ Casks/s/serverbox.rb
@@ -0,0 +1,11 @@
+cask "serverbox" do
+  version "1.0.1365"
+  sha256 "241a8ad2e2474f1db83569464d1b084f3c693be424b50b6043116bea3dbb8878"
+
+  url "https://github.com/lollipopkit/flutter_server_box/releases/download/v#{version}/ServerBox-#{version}.dmg"
+  name "ServerBox"
+  desc "App for monitoring server status with SSH terminal, SFTP, Container management"
+  homepage "https://github.com/lollipopkit/flutter_server_box"
+
+  app "Server Box.app"
+end
