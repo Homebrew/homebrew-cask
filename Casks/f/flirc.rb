55db62239d24d2374ee267c2f1c6388556ef595b
Zach Leventer <zleventer@gmail.com>
2026-04-24 09:21:19 -0400

    Add verified param for url/homepage domain mismatch
    The download URL uses flirc.com while the homepage is flirc.tv,
    so brew audit requires a verified parameter on the url stanza.

--- Casks/f/flirc.rb
+++ Casks/f/flirc.rb
@@ -2,7 +2,8 @@ cask "flirc" do
   version "3.27.19"
   sha256 "da00f51a8b64f1e7fbb75bc229e715428efe5059d06607fe5571a4e1bc011454"
 
-  url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
+  url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg",
+      verified: "flirc.com/"
   name "Flirc"
   desc "IR USB receiver configurator"
   homepage "https://flirc.tv/"
