42bbcb60658fa5039cd03f0d47d69e98e898cbc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 17:02:17 +0000

    temurin@25 25.0.3

--- Casks/t/temurin@25.rb
+++ Casks/t/temurin@25.rb
@@ -1,9 +1,9 @@
 cask "temurin@25" do
   arch arm: "aarch64", intel: "x64"
 
-  version "25.0.2,10"
-  sha256 arm:   "d20140badab27735f895b26a490294bcbd1b4e570c3d16aab87f730865fec5d1",
-         intel: "2638e2f69e3306e891a0cbc12e3176923e4b5f11f2a8f5b035cabcb7f21128db"
+  version "25.0.3,9"
+  sha256 arm:   "e355dfa6fc03030f1fb8ab82cd7c382bcb72a7857122cca5244ada89a6dc4f48",
+         intel: "0822721bb986ec15fa4a1ab0ac81b6b1022e35596deef287deb7edeedb29a253"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
