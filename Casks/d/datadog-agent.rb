f1ee55fdb0b2f3b774fac537216de1ee2f305fb3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-04-24 09:34:59 -0400

    remove disable for Gatekeeper failure
    Removed the disable directive for gatekeeper check.

--- Casks/d/datadog-agent.rb
+++ Casks/d/datadog-agent.rb
@@ -24,8 +24,6 @@ cask "datadog-agent" do
     end
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   depends_on macos: ">= :monterey"
 
   installer manual: "datadog-agent-#{version}.#{arch}.pkg"
