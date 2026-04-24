c93500c67943c0ed0934d866bf6fb5a463ca2b66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 04:58:40 +0000

    kindle-comic-converter 10.0.1

--- Casks/k/kindle-comic-converter.rb
+++ Casks/k/kindle-comic-converter.rb
@@ -1,9 +1,9 @@
 cask "kindle-comic-converter" do
   arch arm: "arm", intel: "i386"
 
-  version "9.7.2"
-  sha256 arm:   "84a091030ba3fb5ff3a3ba90b79cc59902dca28231c6a757a18aada07ebd3bc5",
-         intel: "fab8f807b074384dceb2c041a4d0bd1be8b25472da3cb53691eb2a823b0b48d3"
+  version "10.0.1"
+  sha256 arm:   "5ddfe05e9ec325685e395986f781ff578b18d1e1e18a4d196d758e0a3443872c",
+         intel: "74e6c08b0390ff1429ea2beeb29b8e855c5425662f50516b682bc3f69f6e6d43"
 
   on_arm do
     depends_on macos: ">= :big_sur"
