fe8f7838e6c32a7b9a7aa956c8294ea1ef8431f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 19:13:36 +0000

    hop 0.1.8

--- Casks/h/hop.rb
+++ Casks/h/hop.rb
@@ -1,9 +1,9 @@
 cask "hop" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.6"
-  sha256 arm:   "80d4a872d39262ec02721eaf9f2cb2e65737533393e7792a59df3f8a09844695",
-         intel: "124e73279f3c63bfb7de1dedaf54fe17378b6ab98e48e358641010c8c977eab4"
+  version "0.1.8"
+  sha256 arm:   "73566974e3bd209eb51b0e024c4f45f4ccd1d77297396bfbbe284d18b6ad0861",
+         intel: "2298e22c22aef88ce156a7e17661980736d88761492323f4eba758dbc8e8745b"
 
   url "https://github.com/golbin/hop/releases/download/v#{version}/HOP-macos-#{arch}.dmg",
       verified: "github.com/golbin/hop/"
