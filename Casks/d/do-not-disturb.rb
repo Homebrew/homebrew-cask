dc06f032fb436308fb9b5489418e337d853a7f99
Giovanni Merlino <gmerlino@unime.it>
2026-04-25 15:03:28 +0200

    do-not-disturb: bump to 2.0.0
    - update version and sha256
    - fix GitHub release URL (v#{version})
    - update installer path (app renamed upstream)
    - remove deprecate!/disable!: project is actively maintained again (2.0.0 release)
    - remove requires_rosetta: native Apple Silicon support

--- Casks/d/do-not-disturb.rb
+++ Casks/d/do-not-disturb.rb
@@ -1,33 +1,27 @@
 cask "do-not-disturb" do
-  version "1.3.0"
-  sha256 "000e3ce8f5abd1313bbb603c401a1be0b5cf4f11644d36f1d5d382745079fdc3"
+  version "2.0.0"
+  sha256 "3ea3b66705e2a373c734435981e74908361cfcd0957591d9e0aa8ac94df2cf2f"
 
-  url "https://github.com/objective-see/DoNotDisturb/releases/download/#{version}/DoNotDisturb_#{version}.zip",
+  url "https://github.com/objective-see/DoNotDisturb/releases/download/v#{version}/DoNotDisturb_#{version}.zip",
       verified: "github.com/objective-see/DoNotDisturb/"
   name "Do Not Disturb"
   desc "Open-source physical access (aka 'evil maid') attack detector"
   homepage "https://objective-see.org/products/dnd.html"
 
-  deprecate! date: "2024-11-16", because: :unmaintained
-  disable! date: "2025-11-16", because: :unmaintained
-
   depends_on :macos
 
   installer script: {
-    executable: "#{staged_path}/Do Not Disturb Installer.app/Contents/MacOS/Do Not Disturb Installer",
+    executable: "#{staged_path}/DoNotDisturb Installer.app/Contents/MacOS/DoNotDisturb Installer",
     args:       ["-install"],
     sudo:       true,
   }
 
   uninstall script: {
-    executable: "#{staged_path}/Do Not Disturb Installer.app/Contents/MacOS/Do Not Disturb Installer",
+    executable: "#{staged_path}/DoNotDisturb Installer.app/Contents/MacOS/DoNotDisturb Installer",
     args:       ["-uninstall"],
     sudo:       true,
   }
 
   # No zap stanza required
 
-  caveats do
-    requires_rosetta
-  end
 end
