96a6fef59991abebb4ca5923064aa2f3a37a4fe5
Klaus Hipp <khipp@users.noreply.github.com>
2026-04-25 10:18:36 +0200

    easy-move+resize 1.8.1

--- Casks/e/easy-move+resize.rb
+++ Casks/e/easy-move+resize.rb
@@ -1,8 +1,8 @@
 cask "easy-move+resize" do
-  version "1.8.0"
-  sha256 "c8a93e5917f13ea1dd474e6fb0f2f8a5333202d74fa4e05f711c08fd9af38eb9"
+  version "1.8.1"
+  sha256 "34f48f465017d2aed58fb3c681aa0d132d0bf121f744f6864bd9819694e0ff76"
 
-  url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/Easy.Move+Resize.app.zip"
+  url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/EasyMoveResize-signed.zip"
   name "Easy Move+Resize"
   desc "Utility to support moving and resizing using a modifier key and mouse drag"
   homepage "https://github.com/dmarcotte/easy-move-resize"
@@ -11,8 +11,6 @@ cask "easy-move+resize" do
     url :url
     strategy :github_latest
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on :macos
 
