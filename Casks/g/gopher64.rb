9689d782277b514043df99051e5bc828f136bb41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-25 07:16:41 +0000

    gopher64 1.1.19

--- Casks/g/gopher64.rb
+++ Casks/g/gopher64.rb
@@ -1,6 +1,6 @@
 cask "gopher64" do
-  version "1.1.18"
-  sha256 "e86dadf453b7cb32d4b3cdcb570b5bc34b18c17d67f8e4ffe9e057480e175c9f"
+  version "1.1.19"
+  sha256 "631de3eea53e0b5c3e6dd1a4cd12d04f0759beb10bcb50b91b92c61e8091769b"
 
   url "https://github.com/gopher64/gopher64/releases/download/v#{version}/gopher64-macos-aarch64.zip"
   name "Gopher64"
