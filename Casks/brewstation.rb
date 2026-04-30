3dcafd990998b9c10171139086a44b3e19d5906e
hreinssondev <hreinssondev@gmail.com>
2026-04-30 22:07:48 +0200

    Add cask for BrewStation 1.3

---
+++ Casks/brewstation.rb
@@ -0,0 +1,11 @@
+cask "brewstation" do
+  version "1.3"
+  sha256 "1c1b1d698df59218475170638bfe7d98b5c226ffcae148ce4955871c1dfb9df6"
+
+  url "https://github.com/hreinssondev/brewstation/releases/download/#{version}/BrewStation.dmg"
+  name "BrewStation"
+  desc "Homebrew GUI manager with snapshot saving and restoring"
+  homepage "https://github.com/hreinssondev/brewstation"
+
+  app "BrewStation.app"
+end
