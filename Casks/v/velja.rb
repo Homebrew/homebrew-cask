22620d51831312758c12aabb8bc460d0d396f310
Andrew Breckenridge <asbreckenridge@me.com>
2026-04-29 22:14:09 -0400

    Add sha256 checksum for Velja cask

--- Casks/v/velja.rb
+++ Casks/v/velja.rb
@@ -1,6 +1,6 @@
 cask "velja" do
   version "3.1.0,1770404437"
-  sha256 :
+  sha256 "5ad1c30d412b7e32a01e9745ced6e9ba5984b452eb2bdd701e58fa43d7db7225"
 
   url "https://www.dropbox.com/scl/fi/3a9nj35coz3ohyizixxkg/Velja-3.1.0-trial-1770404437.zip?rlkey=qp07o6we2quz4715z2li7vdnu&raw=1",
       verified: "dropbox.com/"
