1345aec119a536bb9f51a96c1aa967aa8ce7c5c8
Andrew Breckenridge <asbreckenridge@me.com>
2026-04-29 22:10:43 -0400

    Velja 3.1.0 (new cask)

---
+++ Casks/v/velja.rb
@@ -0,0 +1,12 @@
+cask "velja" do
+  version "3.1.0,1770404437"
+  sha256 :
+
+  url "https://www.dropbox.com/scl/fi/3a9nj35coz3ohyizixxkg/Velja-3.1.0-trial-1770404437.zip?rlkey=qp07o6we2quz4715z2li7vdnu&raw=1",
+      verified: "dropbox.com/"
+  name "Velja"
+  desc "Open links in a specific browser"
+  homepage "https://sindresorhus.com/velja"
+
+  app "Velja.app"
+end
