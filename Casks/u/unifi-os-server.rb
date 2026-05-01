9403f8064e0750b39674f0755b18417d10ed57af
SINeWang <im.sinewang@gmail.com>
2026-05-01 19:18:08 +0800

    Fix StanzaGrouping: add blank line between sha256 and url

--- Casks/u/unifi-os-server.rb
+++ Casks/u/unifi-os-server.rb
@@ -1,9 +1,9 @@
 cask "unifi-os-server" do
   version "5.0.6"
   sha256 "8e030977c6bf4e9c1e2e65ee3afd71033b67264167cf50add3fad48ab9544d40"
+
   url "https://fw-download.ubnt.com/data/unifi-os-server/b290-macOS-dmg-amd64-#{version}-a165bb5f-f865-476a-b4bd-703e0d3d4ca1.dmg",
       verified: "fw-download.ubnt.com/data/unifi-os-server/"
-
   name "UniFi OS Server"
   desc "Server for managing UniFi networks and devices"
   homepage "https://www.ui.com/download/software/unifi-os-server/"
