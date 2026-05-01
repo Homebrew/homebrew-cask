48fc2d9ce3e5cfe1d3776c12917ce4f6133abe1e
SINeWang <im.sinewang@gmail.com>
2026-05-01 18:18:49 +0800

    Fix livecheck: return single latest version string via .uniq.max

--- Casks/u/unifi-os-server.rb
+++ Casks/u/unifi-os-server.rb
@@ -20,11 +20,12 @@ cask "unifi-os-server" do
   livecheck do
     url "https://fw-update.ui.com/api/firmware?filter=eq~~product~~unifi-os-server&filter=eq~~channel~~release"
     strategy :json do |json|
-      versions = json.dig("_embedded", "firmware")&.filter_map do |item|
+      firmware = json.dig("_embedded", "firmware") || []
+      versions = firmware.filter_map do |item|
         next unless item["platform"]&.start_with?("macOS-dmg-")
         item["version"]&.delete_prefix("v")
-      end&.uniq
-      versions&.max
+      end
+      versions.uniq.max
     end
   end
 
