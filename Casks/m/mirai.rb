733c5c6f426ea3f0636234f56cc7116336292bb9
Aleksei Savin <savin.alexey.m@gmail.com>
2026-05-06 22:11:24 +0100

    mirai 0.4.6

---
+++ Casks/m/mirai.rb
@@ -0,0 +1,21 @@
+cask "mirai" do
+  arch arm: "aarch64", intel: "x86_64"
+  os macos: "apple-darwin"
+
+  version "0.4.6"
+  sha256 arm:   "877c0967f31ce0be82d4e4c8d2130482193a141bf75fa85d9a52fa02da8b7281",
+         intel: "7cf84238a902f94c55afc187fff4e2b3672cc2ee22a18805fad2e96c17f9b51a"
+
+  url "https://github.com/trymirai/uzu/releases/download/#{version}/mirai-#{arch}-#{os}.tar.gz"
+  name "Mirai"
+  desc "High-performance inference engine for AI models"
+  homepage "https://github.com/trymirai/uzu"
+
+  livecheck do
+    url "https://github.com/trymirai/uzu"
+    regex(/^(\d+(?:\.\d+)+)$/i)
+    strategy :github_latest
+  end
+
+  binary "mirai-#{arch}-#{os}", target: "mirai"
+end
