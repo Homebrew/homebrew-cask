4d130558e556afb4ec788f819758e03e877c7e73
scollovati <20740642+scollovati@users.noreply.github.com>
2026-05-07 15:35:11 +0200

    fix string length

--- Casks/p/proton-drive.rb
+++ Casks/p/proton-drive.rb
@@ -30,5 +30,7 @@ cask "proton-drive" do
     "~/Library/Preferences/ch.protonmail.drive*",
   ]
 
-  caveats "After logging out or uninstalling it, the client creates a folder with all files that failed to get uploaded or ones that were made available locally at the location: ~/Library/CloudStorage/ProtonDrive-{UserEmail} ({TimeStamp})."
+  caveats "After logging out or uninstalling it, the client creates a folder with all files that failed \
+  to get uploaded or ones that were made available locally at the location: \
+  ~/Library/CloudStorage/ProtonDrive-{UserEmail} ({TimeStamp})."
 end
