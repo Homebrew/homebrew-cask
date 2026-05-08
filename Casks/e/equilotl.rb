4086df683ee2603a0cb7c70f58767e8f5fbe46f7
Emily <143989797+sdhEmily@users.noreply.github.com>
2026-05-08 09:20:20 +0100

    urls have changed

--- Casks/e/equilotl.rb
+++ Casks/e/equilotl.rb
@@ -1,22 +1,23 @@
 cask "equilotl" do
-  version "2.1.4"
-  sha256 "70cc2f072fb45782023cea77a7d352a13f79ab30252ea188765b3b0301639601"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/Equicord/Equilotl/releases/download/v#{version}/Equilotl.MacOS.zip"
+  version "2.1.9"
+  sha256 arm:   "a4b682c5bb8cb89d0f8e305a9a9abacd552379e164f77fdb19748016f823a60e",
+         intel: "6fe3c0d01d22354da7245e00944f005652e348f6722a8aef63b3f84f027c4431"
+
+  url "https://github.com/Equicord/Equilotl/releases/download/v#{version}/Equilotl-darwin-#{arch}.zip"
   name "Equilotl"
   desc "Cross platform gui app for installing Equicord"
   homepage "https://github.com/Equicord/Equilotl"
 
   livecheck do
-    url :url
+    url :homepage
     strategy :github_latest
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Equilotl.app"
 
   zap trash: "~/Library/Application Support/Equicord"
-
-  caveats do
-    requires_rosetta
-  end
 end
