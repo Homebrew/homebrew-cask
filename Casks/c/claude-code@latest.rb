ef5733f99f396afaf970291d27b8bd4a70bdef3b
Rui Chen <rui@chenrui.dev>
2026-05-09 00:22:14 +0000

    claude-code@latest 2.1.137

--- Casks/c/claude-code@latest.rb
+++ Casks/c/claude-code@latest.rb
@@ -2,11 +2,11 @@ cask "claude-code@latest" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.1.136"
-  sha256 arm:          "9ef618487dc9e0cb766e8d0d51cd5fd3d06c3d038f4f04f3e714791f32a3cda5",
-         x86_64:       "d540e416e7f8562c99c336ba703d3374d41f79be6d1a93f1c6bd3a88686d4ae7",
-         x86_64_linux: "d8e5337966ae43b1832d1368823bddc77aba08a5f9ffabe44c75e34a6b83a8bb",
-         arm64_linux:  "1d5e30b263959f8c32bf11532601ad468948c560c4c49236eda90da1bf8285ea"
+  version "2.1.137"
+  sha256 arm:          "6d91ce741b8aa129fd43c2f844b39dcc1fec8cfd77e8e5a1ed0f0e7ba54cfea9",
+         x86_64:       "bc71e2701a196c1eee65d0cda675f40118aaf11ce469831bb45092fc342527ff",
+         x86_64_linux: "ae29f87fdee2d42b5e9ff05c84256bf50a0e7edaa2d58975f9b4b2bd2c29897c",
+         arm64_linux:  "8198e7c845a4f3806504b7350424158970c24c56724de400675d6597507d6183"
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
