38f5066d3846e3404373f0236d927621019e6686
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 01:23:49 +0000

    codex-app 26.506.31421

--- Casks/c/codex-app.rb
+++ Casks/c/codex-app.rb
@@ -2,9 +2,9 @@ cask "codex-app" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional intel: "-x64"
 
-  version "26.506.31004"
-  sha256 arm:   "9a09529d4937d1f24c78f78c7ef7f4b501c594c55b4beb4fecc076d03524dad5",
-         intel: "138819bfd3d13032ef108ab51194fd4a6137392c571b38bb6f79639005453e73"
+  version "26.506.31421"
+  sha256 arm:   "13eba9c4d5a903a7ee80cad99a14a20ef081417b94fc9d0446a22e11bdd56be9",
+         intel: "7bfc56e4b00de3323aa9e0946cbaf5991bee070949552838242d167c2e915a94"
 
   url "https://persistent.oaistatic.com/codex-app-prod/Codex-darwin-#{arch}-#{version}.zip",
       verified: "persistent.oaistatic.com/codex-app-prod/"
