236d2c9838f9e1558c3c4b3176ef7229ff3c5a25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 04:52:02 +0000

    font-constructium 2026-05-08

--- Casks/font/font-c/font-constructium.rb
+++ Casks/font/font-c/font-constructium.rb
@@ -1,5 +1,5 @@
 cask "font-constructium" do
-  version "2026-04-12"
+  version "2026-05-08"
   sha256 "1622c9cbba22bfe8ae2b1a1fe69f43a315210470bf24f349a2e9eef0171cd710"
 
   url "https://github.com/kreativekorp/open-relay/releases/download/#{version}/Constructium.zip",
