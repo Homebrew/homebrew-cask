01d552b89b6fd7794d83d3feff9899369ddd2854
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 01:45:07 +0000

    jgrennison-openttd 0.72.2

--- Casks/j/jgrennison-openttd.rb
+++ Casks/j/jgrennison-openttd.rb
@@ -1,6 +1,6 @@
 cask "jgrennison-openttd" do
-  version "0.72.1"
-  sha256 "65c881e8107d964cf33c8a872f04f59a49f876b3f86c70821a005cc8fc6c9058"
+  version "0.72.2"
+  sha256 "cf5a44cfe299595496c60f581e4dafa7eeb3f1310f484180b2278ec4819139e1"
 
   url "https://github.com/JGRennison/OpenTTD-patches/releases/download/jgrpp-#{version}/openttd-jgrpp-#{version}-macos-universal.dmg"
   name "JGR's OpenTTD Patchpack"
