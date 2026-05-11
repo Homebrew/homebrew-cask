427a33fb3b658b5e5325e1acad05da733f57b181
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 17:12:16 +0000

    idevice-pair 0.1.13

--- Casks/i/idevice-pair.rb
+++ Casks/i/idevice-pair.rb
@@ -1,6 +1,6 @@
 cask "idevice-pair" do
-  version "0.1.12"
-  sha256 "11fdc7b6ee0f48e34503c7d4451444142eff69fec62be6e374470d5d2e76ff09"
+  version "0.1.13"
+  sha256 "d1501f7da6d8156bf739fe2dafb4e0b38b3027ddabedcdc4f61544902a9491c4"
 
   url "https://github.com/jkcoxson/idevice_pair/releases/download/#{version}/idevice_pair--macos-universal.dmg"
   name "idevice_pair"
