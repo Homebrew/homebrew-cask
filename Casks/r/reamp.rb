de7431d4e4e947b7993ebdeae38166873ea0f737
Sam Ford <1584702+samford@users.noreply.github.com>
2026-05-10 22:30:37 -0400

    reamp 1.7.0
    `reamp` is autobumped but the workflow failed to update to version
    1.7.0 because the asset URL is different and the file name format now
    includes a secondary number in parentheses. This updates the version
    and adjusts the cask accordingly.

--- Casks/r/reamp.rb
+++ Casks/r/reamp.rb
@@ -1,15 +1,15 @@
 cask "reamp" do
-  version "1.6.3"
-  sha256 "f31f664d512fd248d479fc82e17670d1e99bd4f37174698b39faf74ab3b05460"
+  version "1.7.0,990"
+  sha256 "eeff97b12857cc76a3c86f75d63b5e90cf3f9aef03f38e50221a9e8a3dfc173c"
 
-  url "https://re-amp.ru/app/releases/reAMP-#{version}.zip"
+  url "https://update.re-amp.ru/downloads/Reamp-#{version.csv.first}(#{version.csv.second}).zip"
   name "re:AMP"
   desc "WinAMP clone written in SwiftUI"
   homepage "https://re-amp.ru/"
 
   livecheck do
     url "https://re-amp.ru/app/appcast.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle, &:nice_version
   end
 
   depends_on :macos
