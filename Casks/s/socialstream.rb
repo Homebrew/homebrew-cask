75c121c960ceed4418766220791d0b4d9139c73e
Sam Ford <1584702+samford@users.noreply.github.com>
2026-05-10 22:40:31 -0400

    socialstream 0.3.113
    `socialstream` is autobumped but the workflow failed to update to
    version 0.3.113 because the tag has a leading "v". This updates the
    version and `url` accordingly.

--- Casks/s/socialstream.rb
+++ Casks/s/socialstream.rb
@@ -1,11 +1,11 @@
 cask "socialstream" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.3.98"
-  sha256 arm:   "1650a6bcbb27e31a481492e4b1651264c75534ae3a007fd91255794c846d685c",
-         intel: "4c35e94686022d3cec88426726be174772335333caa9e75323218cd5a9e070a0"
+  version "0.3.113"
+  sha256 arm:   "e267d7089b5ecf61878ba3ff833944a66d43872400450cf7ff5dc53caa3a568f",
+         intel: "1ec798aeb54557e8c677cb8d98f89d619b8f5b9b69a3654eb7e2f6f0cb1546fc"
 
-  url "https://github.com/steveseguin/social_stream/releases/download/#{version}/socialstreamninja_mac_v#{version}_#{arch}.dmg",
+  url "https://github.com/steveseguin/social_stream/releases/download/v#{version}/socialstreamninja_mac_v#{version}_#{arch}.dmg",
       verified: "github.com/steveseguin/social_stream/"
   name "Social Stream"
   name "Social Stream Ninja"
