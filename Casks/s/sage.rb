c285aef69a09264a213e5de723a01d3e73f70ea0
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 10:07:07 +0100

    sage: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/s/sage.rb
+++ Casks/s/sage.rb
@@ -24,7 +24,7 @@ cask "sage" do
     end
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "SageMath-#{version.csv.first.dots_to_hyphens}.app"
   pkg "Recommended_#{version.csv.first.dots_to_underscores}.pkg"
