f38a0fd5151278fe20367ec342c0c6298f85870b
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:44:22 +0100

    dcp-o-matic: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/d/dcp-o-matic.rb
+++ Casks/d/dcp-o-matic.rb
@@ -12,7 +12,7 @@ cask "dcp-o-matic" do
     regex(%r{href=.*?/tag/\?h=v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "DCP-o-matic #{version.major}.app"
 
