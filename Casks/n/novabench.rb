24c877c031d19b20114bf525c1ec8306b76b32a7
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 10:01:51 +0100

    novabench: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/n/novabench.rb
+++ Casks/n/novabench.rb
@@ -12,7 +12,7 @@ cask "novabench" do
     regex(/Novabench\s*(?:<!--[^>]*-->)?\s*(\d+(?:\.\d+)*)/i)
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "Novabench.app"
 
