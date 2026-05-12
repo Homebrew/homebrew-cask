65d4c4375e1389b405202ed509c1db2eafb74865
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 10:07:38 +0100

    simple-web-server: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/s/simple-web-server.rb
+++ Casks/s/simple-web-server.rb
@@ -11,7 +11,7 @@ cask "simple-web-server" do
   desc "Create local web servers"
   homepage "https://simplewebserver.org/"
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "Simple Web Server.app"
 
