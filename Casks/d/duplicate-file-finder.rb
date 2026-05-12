e0fc85f9acc28d7542cb032334d2929156a37681
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:44:39 +0100

    duplicate-file-finder: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/d/duplicate-file-finder.rb
+++ Casks/d/duplicate-file-finder.rb
@@ -13,7 +13,7 @@ cask "duplicate-file-finder" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "Duplicate File Finder #{version.major}.app"
 
