943638d286ecdd042506ec26d7705300a9d89fa5
superbling <me@liukeliang.com>
2026-05-12 20:26:09 +0800

    font-space-mono: deprecate

--- Casks/font/font-s/font-space-mono.rb
+++ Casks/font/font-s/font-space-mono.rb
@@ -2,9 +2,11 @@ cask "font-space-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/googlefonts/spacemono/archive/refs/heads/master.tar.gz"
+  url "https://github.com/googlefonts/spacemono/archive/refs/heads/main.tar.gz"
   name "Space Mono"
   homepage "https://github.com/googlefonts/spacemono"
+
+  deprecate! date: "2026-05-12", because: :discontinued
 
   font "spacemono-main/fonts/ttf/SpaceMono-Bold.ttf"
   font "spacemono-main/fonts/ttf/SpaceMono-BoldItalic.ttf"
