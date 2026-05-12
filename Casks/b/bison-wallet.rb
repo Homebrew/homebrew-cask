e3719d59fe8f705535a4e79e7615b8c47aef1276
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:43:17 +0100

    bison-wallet: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/b/bison-wallet.rb
+++ Casks/b/bison-wallet.rb
@@ -26,7 +26,7 @@ cask "bison-wallet" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "Bison Wallet.app"
 
