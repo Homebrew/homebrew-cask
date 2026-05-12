ed2317283c1de1700887f0d3445a4f56932390cd
Sam Ford <1584702+samford@users.noreply.github.com>
2026-05-11 22:32:21 -0400

    clash-party: update macOS requirement

--- Casks/c/clash-party.rb
+++ Casks/c/clash-party.rb
@@ -3,7 +3,7 @@ cask "clash-party" do
 
   version "1.9.5"
 
-  on_catalina :or_older do
+  on_big_sur :or_older do
     sha256 arm:   "d0eaaea4956325f42aa77bbabf7d902d613aaccbef923f5cf9e0287401112963",
            intel: "2b80a2a7311e71e6d70661f3975a4b700eb021dd6ea9a702cbf798965bd8c500"
 
@@ -12,7 +12,7 @@ cask "clash-party" do
 
     pkg "clash-party-catalina-#{version}-#{arch}.pkg"
   end
-  on_big_sur :or_newer do
+  on_monterey :or_newer do
     sha256 arm:   "8ee492664162468e1cd8345b163c1e0e04936a8dff494f6f7c334a409bf7f414",
            intel: "ed367f69c1c471a4cb91fe683968b07f13a08c5a297d8cb8d7eeb0bda51de9e7"
 
