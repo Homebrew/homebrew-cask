3ad5560dd04cb8d6b57f24b5752e8df6d52af367
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:42:56 +0100

    appflowy: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -16,7 +16,7 @@ cask "appflowy" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "AppFlowy-#{arch}.app", target: "AppFlowy.app"
 
