d68f7c5dbaf59bcbe7a33797e2deaceefcdf1663
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 10:10:35 +0100

    widelands-app: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/w/widelands-app.rb
+++ Casks/w/widelands-app.rb
@@ -45,7 +45,7 @@ cask "widelands-app" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "Widelands.app"
 
