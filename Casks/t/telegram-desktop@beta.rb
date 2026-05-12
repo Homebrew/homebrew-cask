6ccae10b183ad1bef03a0dad619c6973399e69bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 11:37:41 +0000

    telegram-desktop@beta 6.8.2

--- Casks/t/telegram-desktop@beta.rb
+++ Casks/t/telegram-desktop@beta.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop@beta" do
-  version "6.8.1"
-  sha256 "440675ff2ea7aa350e30d2ef4f54cd2c393b5769963a8b8122bb3783fb3f74a3"
+  version "6.8.2"
+  sha256 "115f0bf894ac61f19b15bf4c60c1e43166f5931f2b63a1051abd3e6fa53d5d15"
 
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version.major_minor_patch}/tsetup.#{version}.dmg",
       verified: "github.com/telegramdesktop/tdesktop/"
