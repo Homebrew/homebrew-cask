1cc419b8a610711cf7e25d11bbdb06583ee87f2b
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:43:26 +0100

    bookmacster@beta: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/b/bookmacster@beta.rb
+++ Casks/b/bookmacster@beta.rb
@@ -18,7 +18,7 @@ cask "bookmacster@beta" do
 
   auto_updates true
   conflicts_with cask: "bookmacster"
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "BookMacster.app"
 
