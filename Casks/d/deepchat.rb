4658ecffa175bc2e5a6afeebe2f3847cec0c14e3
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:44:23 +0100

    deepchat: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/d/deepchat.rb
+++ Casks/d/deepchat.rb
@@ -20,7 +20,7 @@ cask "deepchat" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "DeepChat.app"
   binary "#{appdir}/DeepChat.app/Contents/MacOS/DeepChat", target: "deepchat"
