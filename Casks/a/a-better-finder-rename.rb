bf02f06783d12ceee51e9c429a9ef576590a2204
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:42:38 +0100

    a-better-finder-rename: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/a/a-better-finder-rename.rb
+++ Casks/a/a-better-finder-rename.rb
@@ -13,7 +13,7 @@ cask "a-better-finder-rename" do
   end
 
   auto_updates true
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "A Better Finder Rename #{version.major}.app"
 
