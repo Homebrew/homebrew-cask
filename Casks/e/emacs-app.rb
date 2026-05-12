0291a8f584f7057d01db259e9d5fee1ff10ac47e
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:44:50 +0100

    emacs-app: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/e/emacs-app.rb
+++ Casks/e/emacs-app.rb
@@ -18,7 +18,7 @@ cask "emacs-app" do
     "emacs-app@nightly",
     "emacs-app@pretest",
   ]
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "Emacs.app"
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
