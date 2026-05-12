8da5a5de63a816ece9bfe7536860bb0bf35213fd
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:48:09 +0100

    makemkv: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/m/makemkv.rb
+++ Casks/m/makemkv.rb
@@ -14,7 +14,7 @@ cask "makemkv" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "MakeMKV.app"
   binary "#{appdir}/MakeMKV.app/Contents/MacOS/makemkvcon"
