ead63ed3c2e3c6c87ded845d7f6a529c8695e9fe
Mike McQuaid <mike@mikemcquaid.com>
2026-05-12 09:44:38 +0100

    dropshare: fix macOS dependencies.
    See https://github.com/Homebrew/brew/pull/22199

--- Casks/d/dropshare.rb
+++ Casks/d/dropshare.rb
@@ -17,7 +17,7 @@ cask "dropshare" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "Dropshare #{version.major}.app"
   binary "#{appdir}/Dropshare #{version.major}.app/Contents/Resources/ds.sh", target: "ds"
