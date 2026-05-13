669cadad7e77854fa87471547c34b9398f373555
DeLoeribas <43988075+DeLoeribas@users.noreply.github.com>
2026-05-13 16:37:43 +0200

    Add Linker 1.1.1

---
+++ Casks/l/linker.rb
@@ -0,0 +1,11 @@
+cask "linker" do
+  version "1.1.1"
+  sha256 "20a6123264113894b05ded67e3dd6fa913a69b6ff15718f4d60041f75e7e66ef"
+
+  url "https://linker.appmac.fr/update/download/"
+  name "Linker"
+  desc "Connect files, folders, emails, websites and more — bidirectionally"
+  homepage "https://linker.appmac.fr"
+
+  app "Linker.app"
+end
