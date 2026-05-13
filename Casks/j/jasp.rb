34e5ef8b822e8ebb3d76fdff73455bcd49828ae7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 16:50:24 +0000

    jasp 0.97.0.0

--- Casks/j/jasp.rb
+++ Casks/j/jasp.rb
@@ -2,9 +2,9 @@ cask "jasp" do
   arch arm: "arm64", intel: "x86_64"
   livecheck_folder = on_arch_conditional arm: "-apple-silicon"
 
-  version "0.96.0.0"
-  sha256 arm:   "3e7f6b2a1cb8805030e35fea9c365877cc7d44aea6dcebf7d614b55729271c7a",
-         intel: "5e68810615325ea35f13528217a31440272c9c8071db5d33fd45c4d04da84936"
+  version "0.97.0.0"
+  sha256 arm:   "dae1452a6cca56c72b37f7d811748905c9840b54e3df07dbe26fc1176aa06ca7",
+         intel: "89130c356e4b3a24a616b336eec747972f8e5e3a8cb4c11439142e13c0d74f30"
 
   url "https://github.com/jasp-stats/jasp-desktop/releases/download/v#{version.csv.first.major_minor_patch}/JASP-#{version.csv.first}-macOS-#{arch}.dmg",
       verified: "github.com/jasp-stats/jasp-desktop/"
