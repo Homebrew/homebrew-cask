5f6752389b419ba067704afc88bbd525507b0149
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 13:43:36 +0000

    getoutline 1.7.0

--- Casks/g/getoutline.rb
+++ Casks/g/getoutline.rb
@@ -1,9 +1,9 @@
 cask "getoutline" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.0,260416fn931b31i"
-  sha256 arm:   "6aa6257a95e594a65a370532d04ebe8a53983d4accfc1a6921bb03ac035cd88f",
-         intel: "8952696635378326aa14859406ee758209d6a22cbcdeced733e758f3e40727ac"
+  version "1.7.0,260513d06ppernw"
+  sha256 arm:   "156793f14b30d6eff78bd6f95156e964d822885b1ea250770d5fdedca3f9e35f",
+         intel: "a9a5f7df7482e86dcb9e8dbe883b7dd3e32d687cd07fb42e662b9f87d87370f6"
 
   url "https://download.todesktop.com/2211128hgkdcltv/Outline%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}.dmg",
       verified: "download.todesktop.com/2211128hgkdcltv/"
