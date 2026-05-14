fb0515a1c2c03edec9933a2000901590449cc77f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-14 13:44:26 +0000

    idrive 4.0.0.76

--- Casks/i/idrive.rb
+++ Casks/i/idrive.rb
@@ -1,6 +1,6 @@
 cask "idrive" do
-  version "4.0.0.75,050826"
-  sha256 "b010a363268d73a35d5d10012e146bdbb3fd5eb5d5263b38050a5c920da29c8a"
+  version "4.0.0.76,051426"
+  sha256 "10aa8d2cf208038ceb3e80868356cd07a48fa9be46e39c4262b509c89107f86c"
 
   url "https://static.idriveonlinebackup.com/downloads/#{version.csv.second}/IDrive.dmg",
       verified: "static.idriveonlinebackup.com/downloads/"
