d663de018b108ea40d90360a7c7a13cfee582720
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-14 07:59:15 +0000

    logitech-g-hub 2026.3.880543

--- Casks/l/logitech-g-hub.rb
+++ Casks/l/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2026.2.861820"
+  version "2026.3.880543"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
