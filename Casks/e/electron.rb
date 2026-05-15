52493c69bc7ae4c540f184c5a017810285176754
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 04:57:09 +0000

    electron 42.1.0

--- Casks/e/electron.rb
+++ Casks/e/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "42.0.1"
-  sha256 arm:   "0ed37214478e49e2865bffd686dea38335bc46067169ee39a5c1658d0734806e",
-         intel: "9a32e6bb055af7295c32d82dc3c49e1b64dde3d1eee85b83e5d2019445938630"
+  version "42.1.0"
+  sha256 arm:   "98d097299eb08094d0df3312b2d6e8677069d8defdab891143628d4f82f46117",
+         intel: "63b938cbe6696f67f172d8f7cb6c31a58c38853d03d33f047fcba8c628cc700f"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
