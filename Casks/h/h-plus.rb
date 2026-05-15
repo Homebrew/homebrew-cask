4c24d1886ecd44ce7c05bdde9ee7ad8aacfa6b8b
NurikDz <nurikjim11@gmail.com>
2026-05-15 12:16:45 +0800

    Update H+ cask to use v1.0 release URL

--- Casks/h/h-plus.rb
+++ Casks/h/h-plus.rb
@@ -2,10 +2,15 @@ cask "h-plus" do
   version "1.0"
   sha256 "08dde89ead7f723eb84ff8da9b24734426c655fb4d60bb3a8261b989c12f3861"
 
-  url "https://github.com/NurikDz/H-Plus/releases/download/H%2B/H%2B.dmg"
+  url "https://github.com/NurikDz/H-Plus/releases/download/v#{version}/H+.dmg"
   name "H+"
   desc "Streaming app with liquid glass interface for movies and series"
   homepage "https://github.com/NurikDz/H-Plus"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: :big_sur
 
