a85cfdb74b7a6885f5e21bcb8358195296bbe409
rwbaber <97636743+rwbaber@users.noreply.github.com>
2026-05-15 16:10:09 +0100

    revpdf-editor: use arch DSL, fix livecheck url

--- Casks/r/revpdf-editor.rb
+++ Casks/r/revpdf-editor.rb
@@ -1,29 +1,22 @@
 cask "revpdf-editor" do
+  arch arm: "arm64", intel: "x64"
+
   version "4.0.0"
+  sha256 arm:   "63c20ba610a1459cda4a53dcc3a467a1d12caf2f9dc590d755d661051b162697",
+         intel: "8c71f8374a9db608c773938308fd582646fe44a15e56b18aa22b383249911b42"
 
-  on_arm do
-    sha256 "63c20ba610a1459cda4a53dcc3a467a1d12caf2f9dc590d755d661051b162697"
-
-    url "https://github.com/Pawandeep-prog/revpdf-release/releases/download/v#{version}/RevPDF_Editor_arm64.dmg",
-        verified: "github.com/Pawandeep-prog/revpdf-release/"
-  end
-  on_intel do
-    sha256 "8c71f8374a9db608c773938308fd582646fe44a15e56b18aa22b383249911b42"
-
-    url "https://github.com/Pawandeep-prog/revpdf-release/releases/download/v#{version}/RevPDF_Editor_x64.dmg",
-        verified: "github.com/Pawandeep-prog/revpdf-release/"
-  end
-
+  url "https://github.com/Pawandeep-prog/revpdf-release/releases/download/v#{version}/RevPDF_Editor_#{arch}.dmg",
+      verified: "github.com/Pawandeep-prog/revpdf-release/"
   name "RevPDF Editor"
   desc "PDF editor for annotation and editing"
   homepage "https://revpdf.com/"
 
   livecheck do
-    url "https://github.com/Pawandeep-prog/revpdf-release"
+    url :url
     strategy :github_latest
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
 
   app "RevPDF Editor.app"
 
