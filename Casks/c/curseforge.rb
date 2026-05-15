d141e99862e186dfaaff346d7dc996105543f3af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 07:44:05 +0000

    curseforge 1.304.5-33794

--- Casks/c/curseforge.rb
+++ Casks/c/curseforge.rb
@@ -1,6 +1,6 @@
 cask "curseforge" do
-  version "1.303.2-33421"
-  sha256 "0742d7bbfad5d02cf473365e616e8fc70727c9f9f1033c8b9331f795aa3a5941"
+  version "1.304.5-33794"
+  sha256 "713b6975505bd3379e665b0f8a1cc4e1426ef51baca90e4783d0ec52f0a2826f"
 
   url "https://curseforge.overwolf.com/electron/mac/CurseForge-#{version}-universal-mac.zip"
   name "CurseForge"
