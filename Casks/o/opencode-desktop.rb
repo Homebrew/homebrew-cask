506a5f7edc1c20579cee8f4bac0caf802492fa28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 05:32:27 +0000

    opencode-desktop 1.15.0

--- Casks/o/opencode-desktop.rb
+++ Casks/o/opencode-desktop.rb
@@ -1,9 +1,9 @@
 cask "opencode-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.14.51"
-  sha256 arm:   "0af183125c5044c2102285ec0128d30ade1e4356e17f9b2652c370d98d554b36",
-         intel: "9e47b40a77b584abf7d34e6f333a148ba5751e2684c40dca758cf30170c15d6f"
+  version "1.15.0"
+  sha256 arm:   "976cf2d877c0d063a4c20ac44c7b94091a521c45a5fbc8fe8182d9b7c20de34d",
+         intel: "23706a5f3980ea809ca5dd7455ec4486f4168e50768cf28d8c4107285d461350"
 
   url "https://github.com/anomalyco/opencode/releases/download/v#{version}/opencode-desktop-mac-#{arch}.dmg",
       verified: "github.com/anomalyco/opencode/"
