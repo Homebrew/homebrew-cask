44b1783eea09e7c25a93fa217d5ec0733e148b94
singularitti <singularitti@outlook.com>
2026-05-16 03:21:53 -0500

    mole-app 1.3.0 (new cask)

---
+++ Casks/m/mole-app.rb
@@ -0,0 +1,25 @@
+cask "mole-app" do
+  version "1.3.0"
+  sha256 :no_check
+
+  url "https://mole.fit/Mole.dmg"
+  name "Mole"
+  desc "Deep clean, analyze, and optimize app"
+  homepage "https://mole.fit/"
+
+  livecheck do
+    url "https://mole.fit/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: :sonoma
+
+  app "Mole.app"
+
+  zap trash: [
+    "~/.config/mole/",
+    "~/Library/Caches/com.tw93.MoleApp",
+    "~/Library/HTTPStorages/com.tw93.MoleApp",
+    "~/Library/Preferences/com.tw93.MoleApp.plist",
+  ]
+end
