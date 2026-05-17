f037624d7e96650064249fe8559ccd51a35383b0
Stephen <bystephenx@gmail.com>
2026-05-17 12:43:27 +0700

    rockxy 0.19.0,28

--- Casks/r/rockxy.rb
+++ Casks/r/rockxy.rb
@@ -1,6 +1,6 @@
 cask "rockxy" do
-  version "0.18.0,27"
-  sha256 "0d8eefa4b36dd336ba869d3a50c675b98b9a15d83844e39fcc89b9d48c95c6bb"
+  version "0.19.0,28"
+  sha256 "47e61989895ec2155816ad52602ddb7428241a668af6792caa99274cb56479af"
 
   url "https://github.com/RockxyApp/Rockxy/releases/download/v#{version.csv.first}/Rockxy-#{version.tr(",", "-")}.dmg",
       verified: "github.com/RockxyApp/Rockxy/"
