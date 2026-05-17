b461e15f8099a81b1c2c8dc4a5908d0e740c929a
Sean Molenaar <smillerdev@me.com>
2026-05-17 11:27:33 +0300

    agentsview: switch to app_image for linux

--- Casks/a/agentsview.rb
+++ Casks/a/agentsview.rb
@@ -5,18 +5,18 @@ cask "agentsview" do
     arch arm: "aarch64", intel: "x64"
   end
   on_linux do
-    arch arm: "arm64", intel: "amd64"
+    arch arm: "aarch64", intel: "amd64"
   end
 
   version "0.29.0"
   sha256 arm:          "168e80b3637519b26255297957c76d08af39f6f8fa0c11b3c22556e7a2561428",
          x86_64:       "3e04ca70f189be32e770ef089f79bcd4cb99016f1371d20db503697ae7e13f61",
-         x86_64_linux: "54ba039142f55ddc4c9dd0e7092706ad9fa61731d8d8168e92e410d28d7b5d2c",
-         arm64_linux:  "5c43844b5c299ebddfbce39c4a3dd713fe9f3494a149f1aaf16e34658048a999"
+         x86_64_linux: "c75f19f29ee969cfcd337c47203de43e6b8ca28ffbbf9edaa040e7033481389c",
+         arm64_linux:  "baca96af0f2ac346124f399ff8f8d08d3aed5a9d095b5a2f02e3a181ced6e0bd"
 
-  url_end = on_system_conditional linux: "#{os}_#{arch}.tar.gz", macos: "#{arch}.dmg"
+  url_end = on_system_conditional linux: ".AppImage", macos: ".dmg"
 
-  url "https://github.com/wesm/agentsview/releases/download/v#{version}/agentsview_#{version}_#{url_end}",
+  url "https://github.com/wesm/agentsview/releases/download/v#{version}/AgentsView_#{version}_#{arch}#{url_end}",
       verified: "github.com/wesm/agentsview/"
   name "AgentsView"
   desc "Browse, search and analyse your past AI coding sessions"
@@ -41,7 +41,6 @@ cask "agentsview" do
   end
 
   on_linux do
-    # TODO: could use AppImage if/when we add support.
-    binary "agentsview"
+    app_image "AgentsView_#{version}_#{arch}.AppImage", target: "AgentsView.AppImage"
   end
 end
