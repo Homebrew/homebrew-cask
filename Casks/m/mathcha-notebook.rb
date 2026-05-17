6589672c121ebcf4a363b8bafcf1854cec3eed18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-17 05:16:40 +0000

    mathcha-notebook 1.0.452

--- Casks/m/mathcha-notebook.rb
+++ Casks/m/mathcha-notebook.rb
@@ -1,9 +1,9 @@
 cask "mathcha-notebook" do
   arch arm: "-arm64"
 
-  version "1.0.451"
-  sha256 arm:   "05a77058268e242225fc7cbbb82cc99f3ed0eb00a34e34258fa32608f8d858a8",
-         intel: "778a444235a1465ee982b855534348092e85cc74b4b04e27d71dab4db4a92550"
+  version "1.0.452"
+  sha256 arm:   "43ae2a41f39ba0179db7f08fbf8e05d15ea6c5630e656dd8afdd794f0ba86bc0",
+         intel: "e5f07e162641dde33b8fc3d377163e630d747b5a20863292a0c2337bac67d3e1"
 
   url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}#{arch}-mac.zip"
   name "Mathcha Notebook"
