2133f0f119f1e15dc3262bf88817979a5c264f30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-17 00:37:18 +0000

    font-estedad: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-e/font-estedad.rb
+++ Casks/font/font-e/font-estedad.rb
@@ -2,9 +2,10 @@ cask "font-estedad" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/estedad/Estedad%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/estedad/Estedad%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Estedad"
-  homepage "https://github.com/aminabedi68/Estedad"
+  homepage "https://fonts.google.com/specimen/Estedad"
 
   font "Estedad[wght].ttf"
 
