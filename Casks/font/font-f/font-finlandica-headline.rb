04d6be32b56bdc75068b0facfb718cf53c6ba2cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-17 00:37:18 +0000

    font-finlandica-headline: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-f/font-finlandica-headline.rb
+++ Casks/font/font-f/font-finlandica-headline.rb
@@ -3,10 +3,11 @@ cask "font-finlandica-headline" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/finlandicaheadline"
   name "Finlandica Headline"
-  homepage "https://github.com/HelsinkiTypeStudio/FinlandicaHeadline"
+  homepage "https://fonts.google.com/specimen/Finlandica+Headline"
 
   font "FinlandicaHeadline-Italic[wght].ttf"
   font "FinlandicaHeadline[wght].ttf"
