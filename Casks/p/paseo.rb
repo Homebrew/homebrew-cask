b66b493ea9f27155ec2ac3490a7aae43e69f4658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 11:40:43 +0000

    paseo 0.1.78

--- Casks/p/paseo.rb
+++ Casks/p/paseo.rb
@@ -1,9 +1,9 @@
 cask "paseo" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.77"
-  sha256 arm:   "c7b0e4db14667a84e45eb9be6025d087068e7946ae3a9cf0bb520edefdc96c30",
-         intel: "c796aff4d1c53332ab03def9b425bcc3bd5b00402b73ef91428b9ea27600a41f"
+  version "0.1.78"
+  sha256 arm:   "982f566c8a0702750e9fa7069f44dc80466b35936c81f13de3b5b8354479df14",
+         intel: "d2dbc9f12763f9ab07ef2bc71cee8aa9ba47c93255aa3d0495db1148a3ddf10f"
 
   url "https://github.com/getpaseo/paseo/releases/download/v#{version}/Paseo-#{version}-#{arch}.dmg",
       verified: "github.com/getpaseo/paseo/"
