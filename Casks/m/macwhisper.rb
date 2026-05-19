a8a4f35da94ddbbcf5a1a2349b330d77775746fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 17:15:39 +0000

    macwhisper 13.21.1

--- Casks/m/macwhisper.rb
+++ Casks/m/macwhisper.rb
@@ -1,6 +1,6 @@
 cask "macwhisper" do
-  version "13.21,1421"
-  sha256 "05774f8ed2f54028d440ff8db8c8758acea8de2e7d3ce0e3ed3aa3422f3b4454"
+  version "13.21.1,1424"
+  sha256 "2c6747acf4597bfe2231bf248f762d801864215a96a42f932623183b22294410"
 
   url "https://cdn.macwhisper.com/macwhisper/MacWhisper-#{version.csv.second}#{"_#{version.csv.third}" if version.csv.third}.zip",
       verified: "cdn.macwhisper.com/"
