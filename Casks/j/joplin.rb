c8998526892912e5456eb312b1685f4bc8593faf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 22:28:38 +0000

    joplin 3.6.14

--- Casks/j/joplin.rb
+++ Casks/j/joplin.rb
@@ -1,9 +1,9 @@
 cask "joplin" do
   arch arm: "-arm64"
 
-  version "3.6.13"
-  sha256 arm:   "151446e61db182554f22436b5fcd1e14622d9c419eea9a17e35ae15e7a0432e1",
-         intel: "b97b2181fed78fc7e20a3e7f3af265e02127dde75bc191332bf32663c6329c6d"
+  version "3.6.14"
+  sha256 arm:   "7c620afe2598a89ebfeaa1949267e4757eb57138b734e45d8c8a8ac8b6655fd4",
+         intel: "65517db3d09118065cb8b6bbaf00de6a90d1da4bd214be86d4bcf56f4626d034"
 
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}#{arch}.DMG",
       verified: "github.com/laurent22/joplin/"
