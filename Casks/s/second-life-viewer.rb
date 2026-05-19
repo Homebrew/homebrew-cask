a39f5564e58ca9c68bf10f0b50014681c7996c6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 22:49:23 +0000

    second-life-viewer 26.2.0.25386466510

--- Casks/s/second-life-viewer.rb
+++ Casks/s/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask "second-life-viewer" do
-  version "26.1.1.23806384790"
-  sha256 "2607b5dc5d05cdb130ee689a820ba2916106fa289e540b8489e9facc5ee76305"
+  version "26.2.0.25386466510"
+  sha256 "2b5914bc16d83687df643225d77d273a3e7d8c26a737184eaa4ebc2a09945be4"
 
   url "https://viewer-download.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_universal.dmg"
   name "Linden Lab Second Life Viewer"
