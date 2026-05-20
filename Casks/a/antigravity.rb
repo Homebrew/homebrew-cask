29261fe4edee1a312557b32445ca9c5138fa2207
TheBenchMark <g@attl.us>
2026-05-20 22:25:49 +0900

    antigravity 2.0.0

--- Casks/a/antigravity.rb
+++ Casks/a/antigravity.rb
@@ -2,19 +2,19 @@ cask "antigravity" do
   arch arm: "arm", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "-arm64"
 
-  version "1.23.2,4781536860569600"
-  sha256 arm:   "caa35ad282741cc9350fb6234e9b86aef54cd4d2f75715a21ef27180182aa50f",
-         intel: "4ec781e8e94ec714c307a06c4ce925bf761dd0e610ba45e173747fbbe3423ad6"
+  version "2.0.0,6324554176528384"
+  sha256 arm:   "f96c360be0dc419186f987276b0aa1f8c22def1b76eec0892537c193e6bf4fdd",
+         intel: "7416561b81866656453d51810ff64c19bfdc41b5fabca2ca253e9f835e7b20a6"
 
-  url "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/#{version.csv.first}-#{version.csv.second}/darwin-#{arch}/Antigravity.dmg",
-      verified: "edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/"
+  url "https://storage.googleapis.com/antigravity-public/antigravity-hub/#{version.csv.first}-#{version.csv.second}/darwin-#{arch}/Antigravity.dmg",
+      verified: "storage.googleapis.com/antigravity-public/antigravity-hub/"
   name "Google Antigravity"
-  desc "AI Coding Agent IDE"
-  homepage "https://antigravity.google/"
+  desc "Agent orchestration platform"
+  homepage "https://antigravity.google/product/antigravity-2"
 
   livecheck do
     url "https://antigravity-auto-updater-974169037036.us-central1.run.app/api/update/darwin#{livecheck_arch}/stable/latest"
-    regex(%r{/stable/([^/]+)/}i)
+    regex(%r{/antigravity-hub/([^/]+)/}i)
     strategy :json do |json, regex|
       match = json["url"]&.match(regex)
       next if match.blank?
@@ -27,21 +27,12 @@ cask "antigravity" do
   depends_on macos: :monterey
 
   app "Antigravity.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  agy_shimscript = "#{staged_path}/agy.wrapper.sh"
-  binary agy_shimscript, target: "agy"
-
-  preflight do
-    File.write agy_shimscript, <<~EOS
-      #!/bin/sh
-      exec '#{appdir}/Antigravity.app/Contents/Resources/app/bin/antigravity' "$@"
-    EOS
-  end
 
   uninstall quit: "com.google.antigravity"
 
   zap trash: [
     "~/.antigravity/",
+    "~/.gemini/antigravity/",
     "~/Library/Application Support/Antigravity",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.google.antigravity.sfl*",
     "~/Library/Caches/com.google.antigravity",
