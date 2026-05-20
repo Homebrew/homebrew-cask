ed8874c19fd236ec1f980def6ee3ca0b7bb6d530
zr_jin <peter.jin.cn@gmail.com>
2026-05-20 09:52:04 +0800

    Update codex version to 0.131.0

--- Casks/c/codex.rb
+++ Casks/c/codex.rb
@@ -2,7 +2,7 @@ cask "codex" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-musl"
 
-  version "0.130.0"
+  version "0.131.0"
   sha256 arm:          "bc50a4b7f9a0c8ca99179189e4659b601107830770e21547dc0c246bce733577",
          intel:        "feddb116bd96d7d83f8bb19b34fbabe6843cc64461baf2e49c017e1206ad5e67",
          arm64_linux:  "1d7e00f2c22c3016b5bcb71c61010947b022a90e2901bc6baafe82256492c767",
