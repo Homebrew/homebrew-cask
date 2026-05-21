4fdb5bb613a40cbeb446dc21e3f78d2609da6889
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 05:24:08 +0000

    ghostpepper 2.4.0

--- Casks/g/ghostpepper.rb
+++ Casks/g/ghostpepper.rb
@@ -1,6 +1,6 @@
 cask "ghostpepper" do
-  version "2.3.0"
-  sha256 "d7c508f70b00cc518dfd40e875fbcfd8666d1dbb6086fb3e3c2159aafa223979"
+  version "2.4.0"
+  sha256 "2be0ea17477e62b22c0a99d307b30811e3dd24e9b6ee26ec28bc5056284f917a"
 
   url "https://github.com/matthartman/ghost-pepper/releases/download/v#{version}/GhostPepper.dmg"
   name "GhostPepper"
