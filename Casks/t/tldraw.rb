410148bfec7e5dab021a9573b5c8780b7b72efdb
Justin <159104807+Justin24506@users.noreply.github.com>
2026-05-21 12:22:33 +0530

    Add tldraw 1.2.3

---
+++ Casks/t/tldraw.rb
@@ -0,0 +1,24 @@
+cask "tldraw" do
+  version "1.2.3"
+  sha256 "e10d8c78a1bdf1539ff68d399bba0e30f80fef021c9a767b7b6b6cfc9886ba2f"
+
+  url "https://github.com/tldraw/tldraw-desktop/releases/download/v#{version}/tldraw-#{version}-universal.dmg"
+  name "tldraw-desktop"
+  desc "Desktop editor for .tldr files, built with  and Electron"
+  homepage "https://github.com/tldraw/tldraw-desktop"
+
+  auto_updates true
+  depends_on macos: :monterey
+
+  app "tldraw.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tldraw.desktop.sfl*",
+    "~/Library/Application Support/tldraw",
+    "~/Library/Caches/@tldesktop-updater",
+    "~/Library/Caches/com.tldraw.desktop",
+    "~/Library/Caches/com.tldraw.desktop.ShipIt",
+    "~/Library/HTTPStorages/com.tldraw.desktop",
+    "~/Library/Preferences/com.tldraw.desktop.plist",
+  ]
+end
