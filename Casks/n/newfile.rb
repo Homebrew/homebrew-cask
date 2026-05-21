38d15117b91bb7d8d33f56acf2bb7def844777c0
Marius Mevold <mariusmevold@gmail.com>
2026-05-21 12:48:04 -0400

    Add NewFile 0.2.0

---
+++ Casks/n/newfile.rb
@@ -0,0 +1,24 @@
+cask "newfile" do
+  version "0.2.0"
+  sha256 "2458aa21a538eb00857f32ca17afa575e61381aaf097f1b3fb3ca3d37b02b8ef"
+
+  url "https://github.com/mariusgm/newfile/releases/download/v#{version}/NewFile.dmg"
+  name "NewFile"
+  desc "'New File' button for the macOS Finder"
+  homepage "https://github.com/mariusgm/newfile"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "NewFile.app"
+
+  zap trash: [
+    "~/Library/Containers/dev.newfile.NewFile",
+    "~/Library/Containers/dev.newfile.NewFile.NewFileExtension",
+    "~/Library/Group Containers/Q7VD7MTRL8.group.dev.newfile.NewFile",
+  ]
+end
