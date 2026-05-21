cb3ef1b7497b4fe80fc02deff97a58e99ff7c566
Qi Zhang <singularitti@outlook.com>
2026-05-21 04:12:10 -0500

    alt-tab 11.0.0

--- Casks/a/alt-tab.rb
+++ Casks/a/alt-tab.rb
@@ -1,6 +1,6 @@
 cask "alt-tab" do
-  version "10.12.0"
-  sha256 "e7aea75cf1dd30dba6b5a9ef50da03f389bc5db74089e67af9112938a4192c14"
+  version "11.0.0"
+  sha256 "c022ccbe9ff2bb96c8407d4d176fe2c4fa31ec43790b1f60429a34d030ceb186"
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip",
       verified: "github.com/lwouis/alt-tab-macos/"
