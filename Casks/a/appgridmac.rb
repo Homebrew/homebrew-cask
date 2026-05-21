96c9d02596a2a1a83e4422e06c00ecd9525831c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 07:46:20 +0000

    appgridmac 1.1.20

--- Casks/a/appgridmac.rb
+++ Casks/a/appgridmac.rb
@@ -1,6 +1,6 @@
 cask "appgridmac" do
-  version "1.1.19"
-  sha256 "5e4a146a9201e96feed52e480258b0caaf1329b598206b0c6d0e8491303ca013"
+  version "1.1.20"
+  sha256 "f91aabe5d9c261a57a63a325e7c6b1e8592014834ce91e369642b2a7d1a394e0"
 
   url "https://zekalogic.com/appgrid/app/AppGridMac-#{version}.zip",
       verified: "zekalogic.com/appgrid/app/"
