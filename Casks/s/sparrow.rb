84ab2162ca11313137ef6862d781b4a7ca42462d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 17:16:42 +0000

    sparrow 2.5.1

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.5.0"
-  sha256 arm:   "3b8d23bc5e68527cd8364cf9492f29de5c633651217aa571a689095c1f4da92d",
-         intel: "44067b1913337b970a33eb304fd2736955e297ae87b53822b4c2be9d439b7a43"
+  version "2.5.1"
+  sha256 arm:   "9c2f82824570e0c3f1c3fa9cc66ab942a130c234b5a972bf8b37c5857c0f409b",
+         intel: "b352aa80b7f553cd1e8ad9b685a840d82b1d7f9e66d69c7732bb878554ec00f6"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
