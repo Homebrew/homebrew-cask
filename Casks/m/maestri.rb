d73c947133a900dc52f5d8c34761832ba7a819a4
Justin <159104807+Justin24506@users.noreply.github.com>
2026-05-22 20:59:47 +0530

    maestri 0.27.4 (new cask)

---
+++ Casks/m/maestri.rb
@@ -0,0 +1,26 @@
+cask "maestri" do
+  version "0.27.4"
+  sha256 "05f10aaf2c499314a3b315a39a56d05f301ea4593300ee51c45a5b3cb353d446"
+
+  url "https://pub-84eb0b1f3fb3420bbe57a88de7443b71.r2.dev/downloads/Maestri-#{version}.zip",
+      verified: "pub-84eb0b1f3fb3420bbe57a88de7443b71.r2.dev/downloads/"
+  name "Maestri"
+  desc "Infinite canvas where your coding agents work in concert"
+  homepage "https://www.themaestri.app/"
+
+  livecheck do
+    url "https://pub-84eb0b1f3fb3420bbe57a88de7443b71.r2.dev/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: :tahoe
+
+  app "Maestri.app"
+
+  zap trash: [
+    "~/.maestri",
+    "~/Library/Caches/com.evercraftlabs.Maestro",
+    "~/Library/HTTPStorages/com.evercraftlabs.Maestro",
+    "~/Library/Preferences/com.evercraftlabs.Maestro.plist",
+  ]
+end
