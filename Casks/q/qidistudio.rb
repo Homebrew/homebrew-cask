5580da112af89b58591abaab11a015825973ac7f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-05-21 22:58:14 -0400

    qidistudio: remove disable reason

--- Casks/q/qidistudio.rb
+++ Casks/q/qidistudio.rb
@@ -13,8 +13,6 @@ cask "qidistudio" do
     strategy :github_latest
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   depends_on macos: :big_sur
 
   app "QIDIStudio.app"
