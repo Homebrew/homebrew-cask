07792f06e340f4269ef6e308684f40f30322503b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 13:51:57 +0000

    macpacker 0.15
    macpacker

--- Casks/m/macpacker.rb
+++ Casks/m/macpacker.rb
@@ -1,6 +1,6 @@
 cask "macpacker" do
-  version "0.14.1"
-  sha256 "78e8664bfa530062c2cf26cc4891f37d73929ff89d2a2bdf5f36140d21d074cc"
+  version "0.15"
+  sha256 "8b8f1047443174b266607a3eb87023ee4056c775ad2c38ed4683c877edcffddb"
 
   url "https://macpacker-releases.s3.amazonaws.com/MacPacker_v#{version}.zip",
       verified: "macpacker-releases.s3.amazonaws.com/"
@@ -16,7 +16,7 @@ cask "macpacker" do
   end
 
   auto_updates true
-  depends_on macos: :ventura
+  depends_on macos: :sonoma
 
   app "MacPacker.app"
 
