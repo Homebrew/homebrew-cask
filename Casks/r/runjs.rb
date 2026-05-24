a8e23be289b68108975438e3e4282023796cd233
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-24 16:43:28 +0000

    runjs 4.0.5

--- Casks/r/runjs.rb
+++ Casks/r/runjs.rb
@@ -1,6 +1,6 @@
 cask "runjs" do
-  version "4.0.4"
-  sha256 "9b87425d5b22626a3abaa2626555dbb41bdd92285ca6d18638313e9c69a45cc6"
+  version "4.0.5"
+  sha256 "8c3a9900a37a88d801c16c4bcd5f19b4aee544b45754e69868188fecc4f57617"
 
   url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
       verified: "github.com/lukehaas/RunJS/"
