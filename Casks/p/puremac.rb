a82b5051f9abeb66061ab48c54c04003ee5ad1ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-24 05:39:10 +0000

    puremac 2.5.0

--- Casks/p/puremac.rb
+++ Casks/p/puremac.rb
@@ -1,6 +1,6 @@
 cask "puremac" do
-  version "2.3.0"
-  sha256 "0942467fe931bc21244ff4a528e2f7223594be7131ee7f3a0f2828e04bb8697d"
+  version "2.5.0"
+  sha256 "7ceb934ce8d2408fdf7e252359b3ae6b9de33efcd7da95fe381e0ce7fb82eaf7"
 
   url "https://github.com/momenbasel/PureMac/releases/download/v#{version}/PureMac-#{version}.zip"
   name "PureMac"
