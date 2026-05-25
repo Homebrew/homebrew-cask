86f4a2c751603a782f9927b0c4bd6e97c2269ddc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 16:46:56 +0000

    itsytv 1.6.0

--- Casks/i/itsytv.rb
+++ Casks/i/itsytv.rb
@@ -1,6 +1,6 @@
 cask "itsytv" do
-  version "1.5.4"
-  sha256 "0194c00aadc65add2f913658702f14445a9ce1ef4bfb3daff34cad52175c1cab"
+  version "1.6.0"
+  sha256 "53fdc9941d8843b7429f2a9e91450ad1ec5430f989760f15f9151cd46c14ce56"
 
   url "https://github.com/nickustinov/itsytv-macos/releases/download/v#{version}/Itsytv-#{version}.dmg",
       verified: "github.com/nickustinov/itsytv-macos/"
