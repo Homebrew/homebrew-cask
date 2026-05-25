1df19bc81fba6bb8ea2c7ac2c3c146ef63a7fb80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 19:47:43 +0000

    poker-copilot 8.14

--- Casks/p/poker-copilot.rb
+++ Casks/p/poker-copilot.rb
@@ -1,6 +1,6 @@
 cask "poker-copilot" do
-  version "8.12,10000"
-  sha256 "a2dcc74451d962f2dffed014c42abf2a6f3ee579d7c288c68e9cb491b0af67fa"
+  version "8.14,10034"
+  sha256 "5acc42817594de52255013ac63f5eb0eb46f6b11e507f70623a2de6c0c7aec4b"
 
   url "https://static.pokercopilot.com/pokercopilot_macos_#{version.csv.first.dots_to_underscores}_build_#{version.csv.second}.dmg"
   name "Poker Copilot"
