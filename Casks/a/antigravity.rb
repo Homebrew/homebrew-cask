3f1db8570938c798563fe88ab8a55307041b1020
TheBenchMark <iam@workstat.us>
2026-05-25 13:21:06 +0900

    antigravity: use updater manifest for livecheck

--- Casks/a/antigravity.rb
+++ Casks/a/antigravity.rb
@@ -1,5 +1,6 @@
 cask "antigravity" do
   arch arm: "arm", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
   version "2.0.6,5413878570549248"
   sha256 arm:   "bf2ec5b31f03d1fbc98213e45a613319e7527d97badb7f09cb8357d9c1e86a9d",
@@ -12,14 +13,15 @@ cask "antigravity" do
   homepage "https://antigravity.google/product/antigravity-2"
 
   livecheck do
-    url "https://antigravity.google/product/antigravity-2"
-    regex(%r{/antigravity-hub/(\d+(?:\.\d+)+)-(\d+)/darwin-(?:arm|x64)/Antigravity\.dmg}i)
-    strategy :page_match do |page, regex|
-      js_file = page[/src=["']([^"']*main[._-][^"']+\.js)["']/i, 1]
-      next if js_file.blank?
+    url "https://antigravity-hub-auto-updater-974169037036.us-central1.run.app/manifest/latest-#{livecheck_arch}-mac.yml?noCache=#{Time.now.to_i}"
+    regex(%r{/v?(\d+(?:\.\d+)+)(?:-(\d+)?)/darwin-#{arch}/Antigravity\.dmg}i)
+    strategy :electron_builder do |yaml, regex|
+      yaml["files"]&.map do |item|
+        match = item["url"]&.match(regex)
+        next if match.blank?
 
-      js_page = Homebrew::Livecheck::Strategy.page_content(URI.join("https://antigravity.google/", js_file).to_s)
-      js_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+        match[2].present? ? "#{match[1]},#{match[2]}" : match[1]
+      end
     end
   end
 
