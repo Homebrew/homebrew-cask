90f80861567725587666ca1372f3b319dfc06e69
Ben Fairless <ben@oaf.org.au>
2026-05-26 18:48:06 +0800

    activitywatch@beta 0.14.0b1 (new cask)

---
+++ Casks/a/activitywatch@beta.rb
@@ -0,0 +1,36 @@
+cask "activitywatch@beta" do
+  version "0.14.0b1"
+  sha256 "50a9cffaf7833bb0221c9b7889db50ee432310a536e9302d12d2bb9ac393c48d"
+
+  url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-tauri-v#{version}-macos-arm64.dmg",
+      verified: "github.com/ActivityWatch/activitywatch/"
+  name "ActivityWatch"
+  desc "Time tracker"
+  homepage "https://activitywatch.net/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+(?:b\d+)?)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  conflicts_with cask: "activitywatch"
+  depends_on macos: :big_sur, arch: :arm64
+
+  app "ActivityWatch.app"
+
+  zap trash: [
+    "~/Library/Application Support/activitywatch",
+    "~/Library/Caches/activitywatch",
+    "~/Library/Logs/activitywatch",
+  ]
+end
