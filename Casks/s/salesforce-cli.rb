9b400d851977f1a976c0aee6ab9dfabec0e376b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 20:17:32 +0000

    salesforce-cli 2.136.8

--- Casks/s/salesforce-cli.rb
+++ Casks/s/salesforce-cli.rb
@@ -1,9 +1,9 @@
 cask "salesforce-cli" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.135.7,577bcf6"
-  sha256 arm:   "5ad2fc321063ae25da7e3dde4c4a9643722c4f4875c36c452d820b3ca8d75a5f",
-         intel: "20161f71904d22d83ea9991e493822c073a2b86a1bcad99d121d458306c6e706"
+  version "2.136.8,266ec84"
+  sha256 arm:   "85fbfe27c825ce720f1a8d68c14125c728042a8ca2f698ee41243dbb3fef0d9d",
+         intel: "8a6d359d33f90769ac3fc6b32a857877cabc8d12d0fd0b805f35df4b339f4a98"
 
   url "https://github.com/salesforcecli/cli/releases/download/#{version.csv.first}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg",
       verified: "github.com/salesforcecli/cli/"
