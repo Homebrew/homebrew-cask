7e8d4b5f13358083370a2963f3d4a5e5b096bf34
fieldtech <fieldtech@Mac.lan>
2026-05-27 11:58:35 -0700

    Add cask for airtime

---
+++ Casks/a/airtime.rb
@@ -0,0 +1,28 @@
+cask "airtime" do
+  version "2.8.0"
+  sha256 :no_check
+  url "https://updates.airtimetools.com/mac/hybrid/Airtime.dmg"
+  name "Airtime"
+  desc "Video tools for camera, recording, and presentation"
+  homepage "https://www.airtime.com/"
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Airtime.app"
+
+  uninstall quit: "app.mmhmm.hybrid"
+
+  zap trash: [
+    "~/Library/Application Support/mmhmm",
+    "~/Library/Application Scripts/M3KUT44L48.app.mmhmm.hybrid",
+    "~/Library/Application Scripts/M3KUT44L48.app.mmhmm.hybrid.menu",
+    "~/Library/Caches/app.mmhmm.hybrid",
+    "~/Library/Caches/app.mmhmm.hybrid.helper.plugin",
+    "~/Library/Caches/mmhmm",
+    "~/Library/Caches/SentryCrash/Airtime",
+    "~/Library/Containers/M3KUT44L48.app.mmhmm.hybrid.menu",
+    "~/Library/Group Containers/M3KUT44L48.app.mmhmm.hybrid",
+    "~/Library/HTTPStorages/app.mmhmm.hybrid",
+    "~/Library/Preferences/app.mmhmm.hybrid.plist",
+  ]
+end
