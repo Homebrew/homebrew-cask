e6a30cec87de04ece84a0833920a1501a7899756
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 17:47:17 +0000

    tiled 1.12.2

--- Casks/t/tiled.rb
+++ Casks/t/tiled.rb
@@ -1,5 +1,5 @@
 cask "tiled" do
-  version "1.12.1"
+  version "1.12.2"
 
   on_monterey :or_older do
     sha256 "7ccac675b6e71e7a87d558f5b3330040c59a091eafba9cff313261eb84eb8214"
@@ -12,7 +12,7 @@ cask "tiled" do
     end
   end
   on_ventura :or_newer do
-    sha256 "bc6b4958d4fa8701137ff9c61327f53b708f2b8b2feb8db51c925b8908eb1298"
+    sha256 "75712ec3a892701a9b2b7a12e44d636a1dbbe4caa6bb8bba729e697bd8a80433"
 
     url "https://github.com/mapeditor/tiled/releases/download/v#{version}/Tiled-#{version}_macOS-13+.zip",
         verified: "github.com/mapeditor/tiled/"
