d88bb858cea755a2f842c4ad2b1943ffec1c1b7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 05:12:37 +0000

    elegoo-slicer 1.5.1.6

--- Casks/e/elegoo-slicer.rb
+++ Casks/e/elegoo-slicer.rb
@@ -1,9 +1,9 @@
 cask "elegoo-slicer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.5.0.7"
-  sha256 arm:   "8caf6febb33fadfb553288f2fbaedd9370a0880b564b1db6eca80b99ca8d28f9",
-         intel: "cf4d0fac4fb96e760c7262a9045abba5321455180f853b49593677981841d50e"
+  version "1.5.1.6"
+  sha256 arm:   "968214dd1ee00784c8fed30ac90e22f7c81045b1f560e2fbc0ce5bb3b53da981",
+         intel: "9dc3681869f44660fada57f67d380a9d90972f1e38c5e7d49c9ca43d3c952cec"
 
   url "https://github.com/ELEGOO-3D/ElegooSlicer/releases/download/v#{version}/ElegooSlicer_Mac_#{arch}_V#{version}.dmg"
   name "ElegooSlicer"
