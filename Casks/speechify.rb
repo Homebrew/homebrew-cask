bc5f64b4cee703d7bf0e1cf477c05ac4a2779998
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 23:00:55 +0200

    Create speechify.rb

---
+++ Casks/speechify.rb
@@ -0,0 +1,23 @@
+cask "speechify" do
+  version "3.3.0"
+  sha256 :no_check
+
+  url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
+      verified: "storage.googleapis.com/speechifymobile.appspot.com/"
+  name "Speechify AI Assistant"
+  desc "AI-powered reading and voice assistant"
+  homepage "https://www.speechify.com/"
+
+  depends_on macos: :sonoma
+
+  app "Speechify AI Assistant.app"
+
+  zap delete: [
+    "~/Library/Caches/com.cliffweitzman.speechifymacagent",
+    "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
+  ],
+      trash:  [
+        "~/Library/Application Support/com.cliffweitzman.speechifymacagent",
+        "~/Library/Preferences/com.cliffweitzman.speechifymacagent.plist",
+      ]
+end
