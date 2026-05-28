d973c7d61cad65cc03705e6c92bb0369d9589f74
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 22:22:37 +0200

    Update verified URL for Speechify Cask

--- Casks/speechify.rb
+++ Casks/speechify.rb
@@ -4,7 +4,7 @@ cask "speechify" do
   sha256 :no_check
 
   url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
-      verified: "speechify.com"
+      verified: "storage.googleapis.com/speechifymobile.appspot.com"
   # Appcast for reference: https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/appcast.xml
   name "Speechify AI Assistant"
   homepage "https://www.speechify.com/"
