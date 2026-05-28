21298a53951d84066d529222ae899e6c3d7b4524
1stPlayer <phoenix_vio@live.com>
2026-05-29 01:27:11 +0800

    mpv-macWrapper 1.0.0 (new cask)

---
+++ Casks/m/mpv-macWrapper.rb
@@ -0,0 +1,21 @@
+cask "mpv-macWrapper" do
+  version "1.0.0"
+  sha256 "3ccb716615bd5e2f90093835569be85f3ff31dc76abf255c9206cf2734846398"
+
+  url "https://github.com/IstPlayer/mpv-macWrapper/releases/download/v#{version}/mpv.dmg"
+  name "mpv-macWrapper"
+  desc "Wrap mpv CLI as a native macOS .app bundle"
+  homepage "https://github.com/IstPlayer/mpv-macWrapper"
+
+  app "mpv.app"
+
+  caveats <<~EOS
+    mpv must be installed separately:
+      brew install mpv
+
+    If mpv is not found at the default paths, the app will
+    prompt you to locate it on first launch.  The chosen path
+    is saved to ~/.config/mpv-macWrapper/path.conf so you only
+    need to do this once.
+  EOS
+end
