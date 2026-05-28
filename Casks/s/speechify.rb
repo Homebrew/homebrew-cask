5d722453d280eb98c3ab6cbedd8fd1dbef69a72b
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 23:14:14 +0200

    Update speechify.rb

--- Casks/s/speechify.rb
+++ Casks/s/speechify.rb
@@ -18,9 +18,9 @@ cask "speechify" do
   app "Speechify AI Assistant.app"
 
   zap delete: [
-    "~/Library/Caches/com.cliffweitzman.speechifymacagent",
-    "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
-  ],
+        "~/Library/Caches/com.cliffweitzman.speechifymacagent",
+        "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
+      ],
       trash:  [
         "~/Library/Application Support/com.cliffweitzman.speechifymacagent",
         "~/Library/Preferences/com.cliffweitzman.speechifymacagent.plist",
