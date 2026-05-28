28fff344c94a96b76c8000fb8dd2ba09ccf21852
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 22:19:21 +0200

    Add description to Speechify cask

--- Casks/speechify.rb
+++ Casks/speechify.rb
@@ -1,9 +1,11 @@
 cask "speechify" do
+  desc "AI-powered reading and voice assistant"
   version "3.3.0"
   sha256 :no_check
 
   url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
       verified: "speechify.com"
+  # Appcast for reference: https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/appcast.xml
   name "Speechify AI Assistant"
   homepage "https://www.speechify.com/"
 
