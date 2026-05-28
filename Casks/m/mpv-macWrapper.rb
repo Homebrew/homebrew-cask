cdf4d736f86fb4a9e87043b219fbf63cf87b0187
1stPlayer <phoenix_vio@live.com>
2026-05-29 01:29:03 +0800

    mpv-macWrapper 1.0.0: use on_arm block for Apple Silicon only

--- Casks/m/mpv-macWrapper.rb
+++ Casks/m/mpv-macWrapper.rb
@@ -1,6 +1,9 @@
 cask "mpv-macWrapper" do
   version "1.0.0"
-  sha256 "3ccb716615bd5e2f90093835569be85f3ff31dc76abf255c9206cf2734846398"
+
+  on_arm do
+    sha256 "3ccb716615bd5e2f90093835569be85f3ff31dc76abf255c9206cf2734846398"
+  end
 
   url "https://github.com/IstPlayer/mpv-macWrapper/releases/download/v#{version}/mpv.dmg"
   name "mpv-macWrapper"
@@ -19,7 +22,5 @@ cask "mpv-macWrapper" do
     prompt you to locate it on first launch.  The chosen path
     is saved to ~/.config/mpv-macWrapper/path.conf so you only
     need to do this once.
-
-    This cask provides an Apple Silicon (arm64) binary only.
   EOS
 end
