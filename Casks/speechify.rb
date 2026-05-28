8f41995ce8c0b6e0ca082a354621dbb1bc35d4b3
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 22:13:57 +0200

    Update Speechify cask for version and verification

--- Casks/speechify.rb
+++ Casks/speechify.rb
@@ -1,12 +1,13 @@
 cask "speechify" do
   version "3.3.0"
-  sha256 "ed22099543618d62041bf9c381b7dce976526093a12e0a9cff82997dd6fca63b"
+  sha256 :no_check
 
-  url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg"
+  url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
+      verified: "speechify.com"
   name "Speechify AI Assistant"
   homepage "https://www.speechify.com/"
 
-  depends_on :macos
+  depends_on macos: "sonoma"
 
   app "Speechify AI Assistant.app"
 end
