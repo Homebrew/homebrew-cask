2e698901bfe6c0f5a27db3d5456af4b80a66cbdb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 04:56:56 +0000

    antigravity-cli 1.0.3

--- Casks/a/antigravity-cli.rb
+++ Casks/a/antigravity-cli.rb
@@ -3,9 +3,9 @@ cask "antigravity-cli" do
   file_arch = on_arch_conditional arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "amd64"
 
-  version "1.0.2,6109799369277440"
-  sha256 arm:   "8ef15505e65a917c4a21d139cbee6ac394fbb9b3124f3c6d746bd2c34a2a28db",
-         intel: "75d8c7b1792bad7efa4f0540ec7cba3310b24bfe6d7e9c05116912fa338f2ee8"
+  version "1.0.3,6260531212976128"
+  sha256 arm:   "4ffb454399224a3344a599f085d0e1470674e865429b96c2bb4a9b2066a72c70",
+         intel: "02249e863db7bb99afdd595b772d613e7e8b3595c84d350b0f26c5c9b26b8bc1"
 
   url "https://storage.googleapis.com/antigravity-public/antigravity-cli/#{version.csv.first}-#{version.csv.second}/darwin-#{folder_arch}/cli_mac_#{file_arch}.tar.gz",
       verified: "storage.googleapis.com/antigravity-public/antigravity-cli/"
