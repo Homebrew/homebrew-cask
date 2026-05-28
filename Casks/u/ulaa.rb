966b4be18a3ec5a84f13da6a04ec7ebee1e234dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 15:12:44 +0000

    ulaa 2.43.5

--- Casks/u/ulaa.rb
+++ Casks/u/ulaa.rb
@@ -1,9 +1,9 @@
 cask "ulaa" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.43.4"
-  sha256 arm:   "21785b1f63fef9fd9d3fb5fb28ed2a2eb41dd10c743f874d778bc00fab7646a5",
-         intel: "cf51050495dbe39e019c02aad6a3eea809da07b08ec78f34db239379f4823f94"
+  version "2.43.5"
+  sha256 arm:   "77ae81b523ea0f2b86ad05f3e211050ec290c9659efbcefe57d3417d3a9043bd",
+         intel: "e958fe5aa4ebf81d8a8f24dfc87e71c27215ad094fa35375854bd82a572f7324"
 
   url "https://downloads.zohocdn.com/ulaa-browser/release/mac/stable/#{arch}/Ulaa-Browser-v#{version}-#{arch}.dmg",
       verified: "downloads.zohocdn.com/ulaa-browser/"
