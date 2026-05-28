03cc7ca90b3791e590cc5291475613b876139cf0
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 22:07:02 +0200

    Update speechify.rb

--- Casks/speechify.rb
+++ Casks/speechify.rb
@@ -3,7 +3,6 @@ cask 'speechify' do
   sha256 'ed22099543618d62041bf9c381b7dce976526093a12e0a9cff82997dd6fca63b'
 
   url 'https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg'
-  appcast 'https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/appcast.xml'
   name 'Speechify AI Assistant'
   homepage 'https://www.speechify.com/'
 
