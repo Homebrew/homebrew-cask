4f7cc7be16f73faed0bfcd5937796e2385de6ffa
Ben Fairless <ben@oaf.org.au>
2026-05-28 14:34:01 +0800

    Update Casks/a/activitywatch@beta.rb
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/a/activitywatch@beta.rb
+++ Casks/a/activitywatch@beta.rb
@@ -24,7 +24,8 @@ cask "activitywatch@beta" do
   end
 
   conflicts_with cask: "activitywatch"
-  depends_on macos: :big_sur, arch: :arm64
+  depends_on macos: :big_sur
+  depends_on arch: :arm64
 
   app "ActivityWatch.app"
 
