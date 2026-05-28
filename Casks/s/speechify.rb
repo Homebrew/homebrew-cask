d90ac2ac978ada4fa3374c421003512ad5bbc3ff
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 23:13:26 +0200

    Update speechify.rb

--- Casks/s/speechify.rb
+++ Casks/s/speechify.rb
@@ -8,14 +8,19 @@ cask "speechify" do
   desc "AI-powered reading and voice assistant"
   homepage "https://www.speechify.com/"
 
+  livecheck do
+    url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
   depends_on macos: :sonoma
 
   app "Speechify AI Assistant.app"
 
   zap delete: [
-        "~/Library/Caches/com.cliffweitzman.speechifymacagent",
-        "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
-      ],
+    "~/Library/Caches/com.cliffweitzman.speechifymacagent",
+    "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
+  ],
       trash:  [
         "~/Library/Application Support/com.cliffweitzman.speechifymacagent",
         "~/Library/Preferences/com.cliffweitzman.speechifymacagent.plist",
