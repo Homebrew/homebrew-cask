4e70f48a308db8d5854a3fb94ae597e8f58c4f06
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2026-05-28 22:45:18 +0200

    Update Speechify Cask for new agent paths

--- Casks/speechify.rb
+++ Casks/speechify.rb
@@ -5,7 +5,6 @@ cask "speechify" do
 
   url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
       verified: "storage.googleapis.com/speechifymobile.appspot.com"
-  # Appcast for reference: https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/appcast.xml
   name "Speechify AI Assistant"
   homepage "https://www.speechify.com/"
 
@@ -14,13 +13,12 @@ cask "speechify" do
   app "Speechify AI Assistant.app"
 
   zap delete: [
-               "~/Library/Caches/com.cliffweitzman.speechifydesktop1",
-               "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifydesktop1",
-               "~/Library/Caches/io.fabric.sdk.mac.data/com.cliffweitzman.speechifydesktop1",
-               "~/Library/Cookies/com.cliffweitzman.speechifydesktop1.binarycookies",
-             ],
+    "~/Library/Caches/com.cliffweitzman.speechifymacagent",
+    "~/Library/Caches/com.crashlytics.data/com.cliffweitzman.speechifymacagent",
+  ],
       trash:  [
-               "~/Library/Application Support/com.cliffweitzman.speechifydesktop1",
-               "~/Library/Preferences/com.cliffweitzman.speechifydesktop1.plist",
-             ]
+        "~/Library/Application Support/com.cliffweitzman.speechifymacagent",
+        "~/Library/Preferences/com.cliffweitzman.speechifymacagent.plist",
+      ]
+
 end
