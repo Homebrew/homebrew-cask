f9a1c33e7ac3c2ac3df47dd4209b5edf723aee63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 01:53:14 +0000

    github-copilot-app 0.2.14

--- Casks/g/github-copilot-app.rb
+++ Casks/g/github-copilot-app.rb
@@ -1,9 +1,9 @@
 cask "github-copilot-app" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.13"
-  sha256 arm:   "a7d735c045420322e7a85318a777a35fea025f1b0a9b00a8fab6e35188e4c178",
-         intel: "5c25281e0eea4120225d484fa9e49b8688e5a666a1f7900c72bf7545ca4a3c6f"
+  version "0.2.14"
+  sha256 arm:   "41576622a9e8cbcb61a7ee5b47d4f1af21204b2740f7587e911677484b970131",
+         intel: "b507eaf79edd09c0c98bdfb98bd9aaf1d425800711d4ce7ebd62a9d0f04021fb"
 
   url "https://github.com/github/app/releases/download/v#{version}/GitHub-Copilot-darwin-#{arch}.dmg"
   name "GitHub Copilot"
