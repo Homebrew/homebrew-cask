3feace1392ef97c31826e052eb518fa99107665b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 14:05:06 +0000

    hstracker 3.5.10

--- Casks/h/hstracker.rb
+++ Casks/h/hstracker.rb
@@ -1,6 +1,6 @@
 cask "hstracker" do
-  version "3.5.9"
-  sha256 "28c3b11a3d05f3a2f8f5d5db87cae11c2511604f6a15c41169ab5a8b72b2d7f5"
+  version "3.5.10"
+  sha256 "136857eba84faf47c23c997ef156868da8ef138fbc88d3fe425284aab36a363b"
 
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip",
       verified: "github.com/HearthSim/HSTracker/"
