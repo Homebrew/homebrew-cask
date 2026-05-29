2762cf5851aa21a0834f9087a4616a962eca6b19
kk <tzhwg0829@126.com>
2026-05-30 07:29:58 +0800

    Fix cask audit issues

--- Casks/m/mirrorkit.rb
+++ Casks/m/mirrorkit.rb
@@ -2,10 +2,12 @@ cask "mirrorkit" do
   version "1.0.0"
   sha256 "62cc6d175ae84ed19dc5116186e65fba879f5b01aafdabb88b17123fb14e934a"
 
-  url "https://github.com/silica-labs/mirrorkit/releases/download/v#{version}/MirrorKit-#{version}.dmg"
+  url "https://github.com/silica-labs/mirrorkit/releases/download/v#{version}/MirrorKit-v#{version}.dmg",verified: "github.com/silica-labs/mirrorkit"
   name "MirrorKit"
-  desc "Developer tool mirror switcher for macOS"
+  desc "Developer tool mirror switcher"
   homepage "https://mirrorkit.app/"
+
+  depends_on macos: ">= :sequoia"
 
   livecheck do
     url :url
