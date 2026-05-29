4cbc2c04c71941098a352d4f19ad1841bd5770b8
Jan Arends <jan.arends@mailbox.org>
2026-05-29 12:30:34 +0200

    add tangleguard-cli cask

---
+++ Casks/t/tangleguard-cli.rb
@@ -0,0 +1,20 @@
+cask "tangleguard-cli" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.6.1"
+  sha256 arm:   "e7b37219dbf8f51e62fbe6385d70ed171c433b910d741087a769f4fa145c7e16",
+         intel: "1c5de31713e9087fcfa5e78168213d553b74e18fcc329a7624daaca0805bd855"
+
+  url "https://tangleguard-cli-builds.s3.eu-central-1.amazonaws.com/v#{version}/tangleguard-cli_#{version}_#{arch}-apple-darwin.tar.gz",
+      verified: "tangleguard-cli-builds.s3.eu-central-1.amazonaws.com/"
+  name "Tangleguard CLI"
+  desc "Codebase Architecture Context via the CLI for LLMs and Humans"
+  homepage "https://tangleguard.com/"
+
+  livecheck do
+    url "https://tangleguard-cli-builds.s3.eu-central-1.amazonaws.com/latest/VERSION"
+    regex(/v?(\d+(?:\.\d+)+)/i)
+  end
+
+  binary "tangleguard-cli"
+end
