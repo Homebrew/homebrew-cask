165df5da0e88077d384511b7d135ebae7002f836
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-30 10:26:50 +0000

    flacon 13.0.1

--- Casks/f/flacon.rb
+++ Casks/f/flacon.rb
@@ -1,6 +1,6 @@
 cask "flacon" do
-  version "12.0.0"
-  sha256 "3fb524089b3fe3ae26559bba36d462d39d5cef9330f27487d083b95eb94323a5"
+  version "13.0.1"
+  sha256 "e185f6be7896bb2679a51dc7676b8a63a45d45648c7819bb21ffeb23d5125519"
 
   url "https://github.com/flacon/flacon/releases/download/v#{version}/Flacon_#{version}.dmg",
       verified: "github.com/flacon/flacon/"
