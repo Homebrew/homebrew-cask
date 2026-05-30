3f76134e6be1fa0bc0cd4fc08888fb539a43ecf0
Pylogmon <pylogmon@outlook.com>
2026-05-30 18:55:19 +0800

    Fix formatting of istart.rb cask file

--- Casks/i/istart.rb
+++ Casks/i/istart.rb
@@ -1,18 +1,16 @@
 cask "istart" do
   version "1.2"
   sha256 "d0dfd87146aaf8ae1419593ce5b5f6dbe9ebfcc82e7864dc906446a094c86da5"
-  
-  url
-  "https://github.com/Pylogmon/iStart/releases/download/#{version}/iStart.app.zip",
-      verified: "github.com/Pylogmon/iStart/"
+
+  url "https://github.com/Pylogmon/iStart/releases/download/#{version}/iStart.app.zip"
   name "iStart"
   desc "Lightweight Start menu and application launcher for macOS"
   homepage "https://github.com/Pylogmon/iStart"
-  
+
   depends_on macos: ">= :sonoma"
-  
+
   app "iStart.app"
-  
+
   zap trash: [
     "~/Library/Preferences/com.pylogmon.iStart.plist",
     "~/Library/Caches/com.pylogmon.iStart",
