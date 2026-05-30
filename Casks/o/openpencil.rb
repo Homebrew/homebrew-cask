7b57bcddc583dfe31538f63cf83239777c08ef3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-30 05:24:14 +0000

    openpencil 0.13.1

--- Casks/o/openpencil.rb
+++ Casks/o/openpencil.rb
@@ -1,9 +1,9 @@
 cask "openpencil" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.12.2"
-  sha256 arm:   "4f94ded4a8b2f8a46dadf7e2ae36eaace5858f97ff94ebec9d11e4ca004a770e",
-         intel: "16b20be733d5cf81dc741d6f15c06afb41872fe194bd09ba07e1e844c03a92ef"
+  version "0.13.1"
+  sha256 arm:   "71821a58983d94e81455f7b5dd57c6832bceb0d77a880972ef45e67e9cea500e",
+         intel: "0a6dfcc0d719d48e97c84111172b05c4a474632d220063db87d070fda482c4f1"
 
   url "https://github.com/open-pencil/open-pencil/releases/download/v#{version}/OpenPencil_#{arch}.app.tar.gz",
       verified: "github.com/open-pencil/open-pencil/"
