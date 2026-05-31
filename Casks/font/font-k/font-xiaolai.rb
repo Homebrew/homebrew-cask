2a9ca37ddcd2a95c57a5c1c92c83afe498e4d2ba
Samuel Huang <hi@sgh.ng>
2026-05-31 23:56:11 +0800

    font-xiaolai 3.126 (new cask)

---
+++ Casks/font/font-k/font-xiaolai.rb
@@ -0,0 +1,21 @@
+cask "font-xiaolai" do
+  version "3.126"
+  sha256 "e2f68daf0e72777a8cf58bc83de1b98634b251e537ddbfca24b0ae50d1802da2"
+
+  url "https://github.com/lxgw/kose-font/releases/download/v#{version}/Xiaolai-Regular.ttf"
+  name "Xiaolai Font"
+  name "小赖字体"
+  name "小瀨字體"
+  name "Kose Font"
+  desc "Chinese handwriting font derived from SetoFont"
+  homepage "https://github.com/lxgw/kose-font"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "Xiaolai-Regular.ttf"
+
+  # No zap stanza required
+end
