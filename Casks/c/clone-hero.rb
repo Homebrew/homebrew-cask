d2f73d7eef00d7cfc6e8f1b4b5087d401021067f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-31 21:55:04 +0000

    clone-hero 1.1.0.6135

--- Casks/c/clone-hero.rb
+++ Casks/c/clone-hero.rb
@@ -1,6 +1,6 @@
 cask "clone-hero" do
-  version "1.1.0.6085,-final"
-  sha256 "ca84b001de1e9205a9599b88355b6e0d005b6da7e28bef95d8baf3aa7b4bb819"
+  version "1.1.0.6135,-final"
+  sha256 "796b66b5875f9bf7fc5345d57a0ca4dfd5c9904abe39901ebee8ab40563b202a"
 
   url "https://github.com/clonehero-game/releases/releases/download/v#{version.csv.first}#{version.csv.second if version.csv.second}/CloneHero.v#{version.csv.first}#{version.csv.second if version.csv.second}.dmg",
       verified: "github.com/clonehero-game/"
