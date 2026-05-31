9abfa2496d80c4c04ba85655e3a78138cabd10a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-31 06:02:53 +0000

    zedis 0.4.1

--- Casks/z/zedis.rb
+++ Casks/z/zedis.rb
@@ -1,9 +1,9 @@
 cask "zedis" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.4.0"
-  sha256 arm:   "993822bd9665c79bc09458113613b1c21ccf800c8a04198ebd433e456ef552bf",
-         intel: "8cea83cd161345cec9322f0f7b7b6abf143622eaa0fa98151f07be8513588d72"
+  version "0.4.1"
+  sha256 arm:   "b36a7a69ff01b459c6070b05cd7236e4169a34b760a52925ea8adc8671a327bf",
+         intel: "1ab78a88f4893a42c9e412c307e57ef7c338ee65f49f8eea00502fdc44b9fff4"
 
   url "https://github.com/vicanso/zedis/releases/download/v#{version}/Zedis-#{arch}.dmg"
   name "Zedis"
