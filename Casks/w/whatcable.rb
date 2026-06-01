42ede424e0bdf127939c4af8f2e5fb5a29767c8e
MoonFruit <dkmoonfruit@gmail.com>
2026-06-01 14:11:35 +0800

    whatcable 0.15.1 (new cask)

---
+++ Casks/w/whatcable.rb
@@ -0,0 +1,24 @@
+cask "whatcable" do
+  version "0.15.1"
+  sha256 "7724cf50fbdf0bd29d03fc163116db67cf070d6b052d1de79ad7827d43bc2102"
+
+  url "https://github.com/darrylmorley/whatcable/releases/download/v#{version}/WhatCable.zip"
+  name "WhatCable"
+  desc "Menu bar app for USB-C cable diagnostics"
+  homepage "https://github.com/darrylmorley/whatcable"
+
+  depends_on macos: :sonoma
+
+  app "WhatCable.app"
+  binary "#{appdir}/WhatCable.app/Contents/Helpers/whatcable"
+
+  zap trash: [
+    "~/Library/Application Support/WhatCable",
+    "~/Library/Caches/com.bitmoor.whatcable",
+    "~/Library/Caches/uk.whatcable.whatcable",
+    "~/Library/HTTPStorages/com.bitmoor.whatcable",
+    "~/Library/HTTPStorages/uk.whatcable.whatcable",
+    "~/Library/Preferences/com.bitmoor.whatcable.plist",
+    "~/Library/Preferences/uk.whatcable.whatcable.plist",
+  ]
+end
