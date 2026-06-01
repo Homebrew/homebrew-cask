a4ddedc31af1a85d09df24ffde6f3fca32edf278
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 16:00:42 +0000

    tolaria 2026.06.01

--- Casks/t/tolaria.rb
+++ Casks/t/tolaria.rb
@@ -1,9 +1,9 @@
 cask "tolaria" do
   arch arm: "Silicon", intel: "Intel"
 
-  version "2026.05.29,2026.5.29"
-  sha256 arm:   "91d3ab219b5338f127d94afaed9c702e2cac2196c46a0d6286ecc8d1c41576de",
-         intel: "75eeafa1507dcdb4e63da758de9320cfea7646a1936f64fae8936cc2721e35cb"
+  version "2026.06.01,2026.6.1"
+  sha256 arm:   "ff17a4d48f374c43407a28a4921ca432732623e9e89f2967b607bae9db65ce08",
+         intel: "b8c129f1663b19dab9a615448cb1082acbb90845787453d160b7c13318474438"
 
   url "https://github.com/refactoringhq/tolaria/releases/download/v#{version.csv.first.dots_to_hyphens}/Tolaria_#{version.csv.second || version}_macOS_#{arch}.dmg",
       verified: "github.com/refactoringhq/tolaria/"
