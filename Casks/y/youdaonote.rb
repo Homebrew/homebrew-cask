b231443b7243d93b9a215fdeab0a75aa38169e76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 16:20:29 +0000

    youdaonote 8.2.63

--- Casks/y/youdaonote.rb
+++ Casks/y/youdaonote.rb
@@ -1,9 +1,9 @@
 cask "youdaonote" do
   arch arm: "-arm64"
 
-  version "8.2.62"
-  sha256 arm:   "5046e00dc946fed383b99e78cbbc736315c2bc230e3128cb3771136e203c7cd3",
-         intel: "b682b98eb7962c3f7eba89072a40e75654a4f0f0059850141c0cef7504f52da0"
+  version "8.2.63"
+  sha256 arm:   "9c0051902fb47bb82bf994e108b1c7ef5c66b3df65aa8972b0bea8c885b8e8f3",
+         intel: "fb60a0aadec62010841d9c46a1f3d0d86a18295ce7a2720418ee61314395c08b"
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}#{arch}.dmg",
       user_agent: :fake,
