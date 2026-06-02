c3ac7dd89f1480f78973236522caa41acbf1fd6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 17:20:46 +0000

    dcp-o-matic-editor 2.18.43

--- Casks/d/dcp-o-matic-editor.rb
+++ Casks/d/dcp-o-matic-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-editor" do
-  version "2.18.42"
-  sha256 "f8b28354affa93835c3e2bcc0328b190147c1027481d05cf725dc25547cab4c9"
+  version "2.18.43"
+  sha256 "9b8de17a353dd5a93f49a94bed6bb639aee4e911ef253bdacf8a7af8ace7cc59"
 
   url "https://download.dcpomatic.com/dl.php?id=osx-10.10-editor&version=#{version}"
   name "DCP-o-matic Editor"
