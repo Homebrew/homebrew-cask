edaecddd98149f6c7136d31b48cebe51697d67c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 21:00:06 +0000

    whatcable 0.19.1

--- Casks/w/whatcable.rb
+++ Casks/w/whatcable.rb
@@ -1,6 +1,6 @@
 cask "whatcable" do
-  version "0.18.0"
-  sha256 "b962f4902d0e45c8cda241d1230c1704c7bd27f627e328fd04b7962b2000dad9"
+  version "0.19.1"
+  sha256 "91b793cc5396f72a9792815f5ad6a98aade513a3519376b0a36418f17be3d917"
 
   url "https://github.com/darrylmorley/whatcable/releases/download/v#{version}/WhatCable.zip"
   name "WhatCable"
