04306f58f1e6993fdf484ab60b6ab964c9e3bcc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 01:48:21 +0000

    factory 0.95.0

--- Casks/f/factory.rb
+++ Casks/f/factory.rb
@@ -1,9 +1,9 @@
 cask "factory" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.94.1"
-  sha256 arm:   "25944b7686db22ed1bb5015970a41e68ce96339d6bf513a411aeb82c167b9517",
-         intel: "b20cf0104e96f61866f593bb9edae1995be15eb6f2d9ee653a9d0b7e1865a128"
+  version "0.95.0"
+  sha256 arm:   "9fb044850e2c38f04cf7fd086f4f7c78435c35a86e258363f50313db80b6dd6d",
+         intel: "c4f6d82349b34d221d7e780909f97bfe839f7f67906f87f33271650ca2bfda82"
 
   url "https://downloads.factory.ai/factory-desktop/releases/#{version}/darwin/#{arch}/Factory-#{version}-#{arch}.dmg"
   name "Factory"
