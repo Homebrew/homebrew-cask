565cee384046270256c64dc5b3d729c05b91418a
Berkcan Tezcaner <43934527+berkctezc@users.noreply.github.com>
2026-06-03 13:44:33 +0300

    uad-ng 1.2.0 (new cask)

---
+++ Casks/u/uad-ng.rb
@@ -0,0 +1,15 @@
+cask "uad-ng" do
+  version "1.2.0"
+
+  sha256 "bba4bfdae1f716717e6094fbd9fd88ed859a43ae023bdecd4c42abacc523a7bd"
+
+  url "https://github.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/download/v#{version}/uad-ng-macos.tar.gz"
+
+  name "Universal Android Debloater NG"
+  desc "Android debloating tool (Next Generation)"
+  homepage "https://github.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation"
+
+  #depends_on formula: "android-platform-tools"
+
+  binary "uad-ng-macos", target: "uad-ng"
+end
