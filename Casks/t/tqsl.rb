c67cbaf088603c1a257d3a15329d6106cca32f03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 21:01:39 +0000

    tqsl 2.8.6

--- Casks/t/tqsl.rb
+++ Casks/t/tqsl.rb
@@ -1,6 +1,6 @@
 cask "tqsl" do
-  version "2.8.5"
-  sha256 "18fc25b5bc88a6b3148ac2b287bd4388ee6a5be8454d131737cdd23878055e1a"
+  version "2.8.6"
+  sha256 "eda4a02d307651928e240a6415d96e7bcf5efca71ec54f7262c7a24f8e69258b"
 
   url "https://www.arrl.org/tqsl/tqsl-#{version}.pkg"
   name "Trusted QSL"
