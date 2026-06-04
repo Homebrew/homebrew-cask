39d890e4215b9cb0891f9b73f6045a2a6e8423b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 15:00:47 +0000

    whodb 0.113.0

--- Casks/w/whodb.rb
+++ Casks/w/whodb.rb
@@ -1,6 +1,6 @@
 cask "whodb" do
-  version "0.112.0"
-  sha256 "7ffa4ba9494d2082a2efc843457e9498ce5a6a0b64cfce74e8793bcb8f875172"
+  version "0.113.0"
+  sha256 "07ef15ed90bc8ac536a41cf7099cce860d83014a40a31eb53810e576024fa208"
 
   url "https://github.com/clidey/whodb/releases/download/#{version}/whodb.dmg"
   name "WhoDB"
