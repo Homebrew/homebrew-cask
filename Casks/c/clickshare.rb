5f6aa1e54754d2f7c5be231fd89ca7e1e8a34cd0
Adam Baali <45665341+AdamBaali@users.noreply.github.com>
2026-06-04 09:47:59 +0200

    Fix version parsing in ClickShare livecheck
    failed checks with previous suggestion

--- Casks/c/clickshare.rb
+++ Casks/c/clickshare.rb
@@ -10,7 +10,7 @@ cask "clickshare" do
   livecheck do
     url "https://assets.cloud.barco.com/clickshare/release/release.mac"
     strategy :json do |json|
-      json["version"].tr("-b", ",")
+      json["version"].to_s.sub(/-?b/, ",")
     end
   end
 
