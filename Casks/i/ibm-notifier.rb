f9a9ce2618feba3081a69213637bef3073e529e0
Adam Baali <45665341+AdamBaali@users.noreply.github.com>
2026-06-06 01:06:49 +0200

    Add ibm-notifier (new cask)

---
+++ Casks/i/ibm-notifier.rb
@@ -0,0 +1,26 @@
+cask "ibm-notifier" do
+  version "3.2.3,135"
+  sha256 "2b4d13398d68a2305574567f486c34204998641206cd21ff2c2d816e346ab257"
+
+  url "https://github.com/IBM/mac-ibm-notifications/releases/download/v-#{version.csv.first}-b-#{version.csv.second}/IBM.Notifier.zip"
+  name "IBM Notifier"
+  desc "macOS agent that displays custom notifications and alerts to end users"
+  homepage "https://github.com/IBM/mac-ibm-notifications"
+
+  livecheck do
+    url :url
+    regex(/v-(\d+(?:\.\d+)+)-b-(\d+)/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: :big_sur
+
+  app "IBM Notifier.app"
+
+  zap trash: "~/Library/Preferences/com.ibm.cio.notifier.plist"
+end
