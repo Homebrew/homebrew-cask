1c63c2eea35e8cabd9d2200bda036084826f1a83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 08:24:37 +0000

    insomnia@alpha 13.0.0-beta.0

--- Casks/i/insomnia@alpha.rb
+++ Casks/i/insomnia@alpha.rb
@@ -1,6 +1,6 @@
 cask "insomnia@alpha" do
-  version "12.6.0"
-  sha256 "a922a54282f063da82775826389c05c751f74e3259a9f69b8eaa2bf252531fef"
+  version "13.0.0-beta.0"
+  sha256 "818061e253c2ca841fdfb88410d2d0d18a1ea6cb4dbe013583977a69c25a64aa"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
