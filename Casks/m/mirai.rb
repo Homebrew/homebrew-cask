fbbfc4fbce94eec34ff055d17b3a794b5843e555
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 08:34:32 +0000

    mirai 0.3.3

--- Casks/m/mirai.rb
+++ Casks/m/mirai.rb
@@ -1,6 +1,6 @@
 cask "mirai" do
-  version "0.3.2"
-  sha256 "fa98bc2aa3fb43ad08dc88541535fe96752a38e0a8ca8bc12bb7aa8dfd047e97"
+  version "0.3.3"
+  sha256 "c62b3801cfa39a3cccb8f93baa9c76cea8d8b395fafed7e81d80573a253f6def"
 
   url "https://assets.trymirai.com/app/mirai-#{version}-aarch64-apple-darwin.dmg"
   name "Mirai"
