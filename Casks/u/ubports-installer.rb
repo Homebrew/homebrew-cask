0215f2c1cef075c0d85ca8a8d10dbbf58daf5f08
Sakutaro Shina <companyemailetc@gmail.com>
2026-06-08 20:34:41 +0900

    ubports-installer: fix macOS download

--- Casks/u/ubports-installer.rb
+++ Casks/u/ubports-installer.rb
@@ -1,21 +1,30 @@
 cask "ubports-installer" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.11.2"
-  sha256 arm:   "21f40396f8f8d1b0da9b3b7b029cd6d15988603c64cdfceb5d9ba1eb4232cd3c",
-         intel: "cd5532fff5c2a35e04f55b6b0ad03918404bfbaa1065e54a87cf9fb69609807b"
+  on_arm do
+    version "0.11.1"
+    sha256 "18f9bdfe73a56f37cd90a43c0978ab98840ae0f2e5f27d8bbaa396d037fc63fe"
+
+    livecheck do
+      skip "No Apple Silicon release available after 0.11.1"
+    end
+  end
+  on_intel do
+    version "0.11.2"
+    sha256 "cd5532fff5c2a35e04f55b6b0ad03918404bfbaa1065e54a87cf9fb69609807b"
+
+    livecheck do
+      url :url
+      regex(/v?(\d+(?:\.\d+)+(?:-beta)?)/i)
+      strategy :github_latest
+    end
+  end
 
   url "https://github.com/ubports/ubports-installer/releases/download/#{version}/ubports-installer_#{version}_mac_#{arch}.dmg",
       verified: "github.com/ubports/ubports-installer/"
   name "ubports installer"
   desc "Application to install ubports on mobile devices"
   homepage "https://ubports.com/"
-
-  livecheck do
-    url :url
-    regex(/v?(\d+(?:\.\d+)+(?:-beta)?)/i)
-    strategy :github_latest
-  end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
