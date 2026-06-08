ea33e227c7574ab07be540e404bd18da44ab2d77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 05:51:13 +0000

    openlogi 0.6.0

--- Casks/o/openlogi.rb
+++ Casks/o/openlogi.rb
@@ -1,9 +1,9 @@
 cask "openlogi" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.5.3"
-  sha256 arm:   "de83a7a80e5dea05fcccee25897d0e554b35cdfd6ab8b9c9ddaafb561a8d439c",
-         intel: "bf236f0ead2d9e68aaac9f46e804953cfe70f452abbe4d2aca346d3ba027e647"
+  version "0.6.0"
+  sha256 arm:   "b66bdf2f4dec715478aed46b25a10123dcb6b9b6ce5c127576bced17ca0de489",
+         intel: "b43c4c29174887eefab717d6cffee47781bccd5fee340bc5e8da743bd5940949"
 
   url "https://updates.openlogi.org/releases/v#{version}/OpenLogi-v#{version}-macos-#{arch}.dmg"
   name "OpenLogi"
