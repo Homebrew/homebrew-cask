f68301eaf32f1757255402d08e05faf83cd11db4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 17:03:26 +0000

    figma@beta 126.6.3

--- Casks/f/figma@beta.rb
+++ Casks/f/figma@beta.rb
@@ -1,9 +1,9 @@
 cask "figma@beta" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "126.6.2"
-  sha256 arm:   "6f988a54f242a1ac1ead2474dd01bfe34b870329206f04fdc7514a5b9503a8f4",
-         intel: "110f23bace83435b19b626bf49281782c2fb4d2cebbb9f9ca292b4d73da68dd9"
+  version "126.6.3"
+  sha256 arm:   "69dc3452f7a4aba10bc199778b3a035b255545a5c2f9274da0962c4ba2265b92",
+         intel: "c1ba58c73d164b887b2a7f302a1eae9f17b2e13a3a4bfb31376890c6cb1b2363"
 
   url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
   name "Figma Beta"
