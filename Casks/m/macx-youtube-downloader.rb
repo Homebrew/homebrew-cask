da692b76a058a2f7b72bb9ff5ab8fb3d76272580
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 08:47:14 +0000

    macx-youtube-downloader 5.5.0

--- Casks/m/macx-youtube-downloader.rb
+++ Casks/m/macx-youtube-downloader.rb
@@ -1,5 +1,5 @@
 cask "macx-youtube-downloader" do
-  version "5.4.1"
+  version "5.5.0"
   sha256 :no_check
 
   url "https://www.macxdvd.com/download/macx-youtube-downloader-free.dmg"
@@ -26,8 +26,4 @@ cask "macx-youtube-downloader" do
     "~/Library/Caches/com.digiarty.youtubedownloader#{version.major}",
     "~/Library/Preferences/com.digiarty.youtubedownloader#{version.major}.plist",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
