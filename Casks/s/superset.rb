1f939a7a7e75aa907fe199722b665f3cf577d21a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-09 20:18:14 +0000

    superset 1.12.5

--- Casks/s/superset.rb
+++ Casks/s/superset.rb
@@ -1,9 +1,9 @@
 cask "superset" do
   arch arm: "-arm64"
 
-  version "1.12.4"
-  sha256 arm:   "bd1fbe4536ec7aad12b5d54e7cdb71694362ac75a0f28b4dd94ab25a5f743316",
-         intel: "a59e1501ea8bc2c945911bbb57288834b28755bbbe929570b905dc269aee5809"
+  version "1.12.5"
+  sha256 arm:   "775e58d5fcee00b99fb20eaaf1882eaadccb5a404c8059f0e84325923ba3366c",
+         intel: "95ebc96188304d1b9fbd6cc7a662e18988a395e4dd36b83a5c16395877623880"
 
   url "https://github.com/superset-sh/superset/releases/download/desktop-v#{version}/Superset-#{version}#{arch}.dmg",
       verified: "github.com/superset-sh/superset/"
