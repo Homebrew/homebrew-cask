4bfa04c43698fdf79c5b03a651b7287f5d5ebfd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-09 08:21:34 +0000

    lagrange 1.20.7

--- Casks/l/lagrange.rb
+++ Casks/l/lagrange.rb
@@ -2,9 +2,9 @@ cask "lagrange" do
   arch arm: "11.0-arm64", intel: "10.13-x86_64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
 
-  version "1.20.6"
-  sha256 arm:   "004c62a97031b3292081977bd4bce94fb0a846f64c42b40d9fecf6c29311953b",
-         intel: "16c3c99611ef5e9b0ef03aeef96b5a0a3dfa21c4b216c4c605ccee09cac1df4b"
+  version "1.20.7"
+  sha256 arm:   "e8896c9d52daca021e69ae41a228a2d6a9ce1583f25b8dea006268724dcc7dc0",
+         intel: "a942830a026c86aae998458728e24b0cd1156caf4b861d05d52a4b2193534572"
 
   url "https://git.skyjake.fi/gemini/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos#{arch}.tbz"
   name "Lagrange"
