16400a9bfc76fe8b74b6291f8419d781e7489e29
Romain Ferrali <rferrali@gmail.com>
2026-06-10 11:47:49 +0200

    quarto: use os and arch blocks

--- Casks/q/quarto.rb
+++ Casks/q/quarto.rb
@@ -1,12 +1,27 @@
 cask "quarto" do
+  arch arm: "arm64", intel: "amd64"
+  os macos: "macos", linux: "linux-#{arch}"
+
+  container_ext = on_system_conditional macos: "pkg", linux: "tar.gz"
+
   version "1.9.38"
+  sha256 arm:          "de056ec2407c1b79832bc8fd6181ad6077fde433728285b2a0ae62710d853b84",
+         x86_64:       "de056ec2407c1b79832bc8fd6181ad6077fde433728285b2a0ae62710d853b84",
+         arm64_linux:  "75fbc5c1121ffe65e564e9d24711db2ad8f617f9552f5dc7d8a06307d72dde38",
+         x86_64_linux: "ea8c897368791ad9f200010c087ea3111b2e556b12a960487dd4e216902aa102"
+
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-#{os}.#{container_ext}",
+      verified: "github.com/quarto-dev/quarto-cli/"
+  name "quarto"
+  desc "Scientific and technical publishing system built on Pandoc"
+  homepage "https://www.quarto.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   on_macos do
-    sha256 "de056ec2407c1b79832bc8fd6181ad6077fde433728285b2a0ae62710d853b84"
-
-    url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
-        verified: "github.com/quarto-dev/quarto-cli/"
-
     pkg "quarto-#{version}-macos.pkg"
 
     uninstall pkgutil: "org.rstudio.quarto"
@@ -19,28 +34,6 @@ cask "quarto" do
   end
 
   on_linux do
-    on_arm do
-      sha256 "75fbc5c1121ffe65e564e9d24711db2ad8f617f9552f5dc7d8a06307d72dde38"
-
-      url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-linux-arm64.tar.gz",
-          verified: "github.com/quarto-dev/quarto-cli/"
-    end
-    on_intel do
-      sha256 "ea8c897368791ad9f200010c087ea3111b2e556b12a960487dd4e216902aa102"
-
-      url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-linux-amd64.tar.gz",
-          verified: "github.com/quarto-dev/quarto-cli/"
-    end
-
     binary "quarto-#{version}/bin/quarto"
-  end
-
-  name "quarto"
-  desc "Scientific and technical publishing system built on Pandoc"
-  homepage "https://www.quarto.org/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
   end
 end
