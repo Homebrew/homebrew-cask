a460e367e2f347a8a525630921aa55621ecc9449
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-10 11:32:11 +0000

    opencat 2.93.2

--- Casks/o/opencat.rb
+++ Casks/o/opencat.rb
@@ -1,6 +1,6 @@
 cask "opencat" do
-  version "2.93.1,2379"
-  sha256 "602d0b2c425ce25635de320bedf634303e4844a67d5e256ba88fe84418423bd3"
+  version "2.93.2,2395"
+  sha256 "87856dfd788e108787c8836fe8e7094bf62c536e6214520df8bbcb0188c546d5"
 
   url "https://releases.opencat.app/OpenCat-#{version.csv.first}.#{version.csv.second}.dmg"
   name "OpenCat"
