1b79793fa20dcde9109313dd57c43f64305e7f62
AdamBaali <adam@mpc.ad>
2026-06-10 14:34:53 +0200

    Add utiluti (new cask)

---
+++ Casks/u/utiluti.rb
@@ -0,0 +1,20 @@
+cask "utiluti" do
+  version "1.5"
+  sha256 "70184b51e8ec0017ac6e2deae239ed82658902582982db574912ca8884fe061f"
+
+  url "https://github.com/scriptingosx/utiluti/releases/download/v#{version}/utiluti-#{version}.pkg"
+  name "utiluti"
+  desc "Command-line tool to manage default apps and URL handlers"
+  homepage "https://github.com/scriptingosx/utiluti"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :big_sur
+
+  pkg "utiluti-1.5.pkg"
+
+  uninstall pkgutil: "com.scriptingosx.utiluti"
+end
