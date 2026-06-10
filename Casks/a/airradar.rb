c14cba40c0a4eac865c84840240078ebbbac400c
AdamBaali <adam@mpc.ad>
2026-06-10 14:35:47 +0200

    Add airradar (new cask)

---
+++ Casks/a/airradar.rb
@@ -0,0 +1,26 @@
+cask "airradar" do
+  version "8.0"
+  sha256 :no_check
+
+  url "https://www.koingosw.com/products/airradar/download/airradar.dmg"
+  name "AirRadar"
+  desc "Scans for wireless networks and maps signal strength with GPS"
+  homepage "https://www.koingosw.com/products/airradar/"
+
+  livecheck do
+    url "https://www.koingosw.com/products/airradar/"
+    regex(/Version (\d+(?:\.\d+)+)/i)
+    strategy :page_match
+  end
+
+  depends_on macos: :big_sur
+
+  app "AirRadar.app"
+
+  zap trash: [
+    "~/Library/Caches/com.koingosw.AirRadar",
+    "~/Library/HTTPStorages/com.koingosw.AirRadar",
+    "~/Library/Preferences/com.koingosw.AirRadar.plist",
+    "~/Library/Saved Application State/com.koingosw.AirRadar.savedState",
+  ]
+end
