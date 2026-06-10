a988d18a3db5d89d4c1e65bc01903bb66dd57751
maumeerally <mike.rally@nduo.co.uk>
2026-06-10 15:25:31 +0100

    add baseline 3.0 (new cask)

---
+++ Casks/b/baseline.rb
@@ -0,0 +1,31 @@
+cask "baseline" do
+  version "3.0"
+  sha256 "ec9bcc7b1363638f87973c13198fa9a52f630999fbade71ce4637daafbf31dd3"
+
+  url "https://github.com/SecondSonConsulting/Baseline/releases/download/v#{version}/Baseline_v#{version}.pkg"
+  name "Baseline"
+  desc "Automate onboardings by installing apps and running scripts"
+  homepage "https://github.com/SecondSonConsulting/Baseline"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on cask: "swiftdialog"
+  depends_on macos: :sequoia
+
+  pkg "Baseline_v#{version}.pkg"
+
+  uninstall launchctl: "com.secondsonconsulting.baseline",
+            pkgutil:   "com.secondsonconsulting.baseline",
+            delete:    [
+              "/Library/LaunchDaemons/com.secondsonconsulting.baseline.plist",
+              "/usr/local/Baseline",
+            ]
+
+  zap trash: [
+    "/var/log/Baseline.log",
+    "/var/log/BaselineOutput.log",
+  ]
+end
