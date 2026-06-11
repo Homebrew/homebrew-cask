c6513c598186775a2651e3d891652836002bac4c
Sakutaro Shina <companyemailetc@gmail.com>
2026-06-11 15:40:41 +0900

    chrome-remote-desktop-host 149.0.7827.18

--- Casks/c/chrome-remote-desktop-host.rb
+++ Casks/c/chrome-remote-desktop-host.rb
@@ -1,5 +1,5 @@
 cask "chrome-remote-desktop-host" do
-  version "147.0.7727.3"
+  version "149.0.7827.18"
   sha256 :no_check
 
   url "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg"
