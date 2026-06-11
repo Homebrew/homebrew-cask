31e88bd40ea720a06fddf7feeb1a1614fa5d3f18
Bevan Kay <email@bevankay.me>
2026-06-11 12:10:14 +1000

    lightworks: update livecheck and url

--- Casks/l/lightworks.rb
+++ Casks/l/lightworks.rb
@@ -2,16 +2,17 @@ cask "lightworks" do
   version "2025.2,56356"
   sha256 "8771f90dfc4a1872567e320679729df1a9a06b14ad47f69a4b1200528eef0ef1"
 
-  url "https://cdn.lwks.com/releases/#{version.csv.first}/Lightworks-#{version.csv.first.major_minor}-#{version.csv.second}.dmg"
+  url "https://cdn.lwks.com/releases/#{version.major_minor}/Lightworks-#{version}.dmg"
   name "Lightworks"
   desc "Complete video creation package"
   homepage "https://www.lwks.com/"
 
   livecheck do
     url "https://forum.lwks.com/forums/product-releases.19/index.rss"
-    regex(/v?(\d+(?:\.\d+)+).*?revision\s+(\d+)/i)
+    # v2025-2-56356-now-available
+    regex(/v?(\d+(?:[._-]\d+)+)[._-]now[._-]available/i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+      page.scan(regex).map { |match| match.first&.tr("-", ".") }
     end
   end
 
