6ceb2bdc86e37fc19c0f4a1d4dd46d3d54998ff3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 23:01:11 +0000

    remanager 1.5.4

--- Casks/r/remanager.rb
+++ Casks/r/remanager.rb
@@ -1,6 +1,6 @@
 cask "remanager" do
-  version "1.5.3"
-  sha256 "24bbdc05eebaeba64aabaed45c5ac56e245ad327ecf9bc734caf5d21c531f195"
+  version "1.5.4"
+  sha256 "025ac7b59e9fd0e35be8aebb30f3b09b0e5f798bcc920dc61e455e23ee4d299f"
 
   url "https://github.com/rmitchellscott/reManager/releases/download/v#{version}/reManager-macos-universal.zip"
   name "reManager"
