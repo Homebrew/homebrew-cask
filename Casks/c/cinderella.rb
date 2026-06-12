cf194a93572c9c82432f94e10552245ffc3123a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 08:14:01 +0000

    cinderella 3.0.2133

--- Casks/c/cinderella.rb
+++ Casks/c/cinderella.rb
@@ -1,6 +1,6 @@
 cask "cinderella" do
-  version "3.0.2132"
-  sha256 "9ed3789a7ff337c3fee0951138e0c6751b7100d39eab1609ba09095a7ef866a8"
+  version "3.0.2133"
+  sha256 "29fcf85e0b0723184a1439cfa8df77c1ceaea87b9e3182790d76c82ef5738a71"
 
   url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
