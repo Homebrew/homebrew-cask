f1cf8e25ce2af9e9c48dd22ec02f392bc1e91bb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 22:29:04 +0000

    gologin 4.3.3

--- Casks/g/gologin.rb
+++ Casks/g/gologin.rb
@@ -2,9 +2,9 @@ cask "gologin" do
   arch arm: "-arm64"
   livecheck_arch = on_arch_conditional arm: "-arm"
 
-  version "4.3.2"
-  sha256 arm:   "220b0e8ceaec4b98fefc0da572db704014e8bd019c7429277b30215636e8193d",
-         intel: "728cec2a12898bb7ee395fbb73cbc38cedf406032cf417f12bda781df57530eb"
+  version "4.3.3"
+  sha256 arm:   "76ae38955abd027e32d2aa472a691c9df9b64a30b8f920273c16fe879b5fa3e7",
+         intel: "bce3b29396837580dd297b8a746d37e37c86b40131440d87109543fabec26882"
 
   url "https://releases#{livecheck_arch}.gologin.com/Gologin-#{version}#{arch}.dmg"
   name "Gologin"
