cf663b90cab1d0218cc61edafe6032cd4c980f79
Davy <95214375+thedavidweng@users.noreply.github.com>
2026-06-12 02:00:14 -0700

    lyricsfinder: add Apple Silicon support

--- Casks/l/lyricsfinder.rb
+++ Casks/l/lyricsfinder.rb
@@ -1,8 +1,16 @@
 cask "lyricsfinder" do
-  version "1.7"
-  sha256 :no_check
+  arch arm: "-arm"
 
-  url "https://www.mediahuman.com/download/LyricsFinder.dmg"
+  version "1.7"
+
+  on_arm do
+    sha256 "905eea7adb297b2e55716ba096144be52529de6fd7987c36fe36ec8625157722"
+  end
+  on_intel do
+    sha256 "6e6e436de4c5d8ef1505b9afe4bf0aa8bd942caf847141c550878f8fe7276e05"
+  end
+
+  url "https://www.mediahuman.com/download/LyricsFinder#{arch}.dmg"
   name "Lyrics Finder"
   desc "Find and download song lyrics"
   homepage "https://www.mediahuman.com/lyrics-finder/"
@@ -17,8 +25,4 @@ cask "lyricsfinder" do
   app "LyricsFinder.app"
 
   zap trash: "~/Library/Preferences/com.mediahuman.Lyrics Finder.plist"
-
-  caveats do
-    requires_rosetta
-  end
 end
