909a8ab47ffe93f9b1a0935e931ee9b57a0e41f5
Davy <95214375+thedavidweng@users.noreply.github.com>
2026-06-12 02:11:11 -0700

    lyricsfinder: fix audit and style issues

--- Casks/l/lyricsfinder.rb
+++ Casks/l/lyricsfinder.rb
@@ -2,13 +2,7 @@ cask "lyricsfinder" do
   arch arm: "-arm"
 
   version "1.7"
-
-  on_arm do
-    sha256 "905eea7adb297b2e55716ba096144be52529de6fd7987c36fe36ec8625157722"
-  end
-  on_intel do
-    sha256 "6e6e436de4c5d8ef1505b9afe4bf0aa8bd942caf847141c550878f8fe7276e05"
-  end
+  sha256 :no_check
 
   url "https://www.mediahuman.com/download/LyricsFinder#{arch}.dmg"
   name "Lyrics Finder"
@@ -20,7 +14,7 @@ cask "lyricsfinder" do
     regex(/"softwareVersion">(\d+(?:\.\d+)+)</i)
   end
 
-  depends_on :macos
+  depends_on macos: :monterey
 
   app "LyricsFinder.app"
 
