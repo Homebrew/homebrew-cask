0fd4f7ec08e222d840ec899e54e5a4a21089b155
TheBenchMark <g@attl.us>
2026-06-12 12:11:20 +0900

    Address Hermes Desktop review feedback

--- Casks/h/hermes-desktop.rb
+++ Casks/h/hermes-desktop.rb
@@ -1,5 +1,5 @@
 cask "hermes-desktop" do
-  version "0.16.0,c37c6eaf296a"
+  version "0.16.0,2ee69d05795d"
   sha256 "b61e047efe3059faf1c55fec3252e661f2d2a993a7a3eebf5cc6a9aa5c1790f5"
 
   url "https://hermes-assets.nousresearch.com/Hermes-Setup.dmg?build=#{version.csv.second}"
@@ -9,12 +9,13 @@ cask "hermes-desktop" do
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      version_match = page.match(/Hermes Agent v?(\d+(?:\.\d+)+)/i)
-      next if version_match.blank?
+    regex(/Hermes\s+Agent\s+v?(\d+(?:\.\d+)+)/i)
+    strategy :page_match do |page, regex|
+      version_match = page.match(regex)
+      next unless version_match
 
-      build_match = page.match(/Hermes[._-]Setup\.dmg\?build=([a-z0-9]+)/i)
-      next if build_match.blank?
+      build_match = page.match(/Hermes[._-]Setup\.dmg\?build=(\h+)/i)
+      next unless build_match
 
       "#{version_match[1]},#{build_match[1]}"
     end
