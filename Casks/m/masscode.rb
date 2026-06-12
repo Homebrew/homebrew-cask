5cff1b4308d2459687a86db2d45b8ea813bd710a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 08:41:51 +0000

    masscode 5.6.0

--- Casks/m/masscode.rb
+++ Casks/m/masscode.rb
@@ -1,9 +1,9 @@
 cask "masscode" do
   arch arm: "-arm64"
 
-  version "5.5.0"
-  sha256 arm:   "a135aab85b0dd03d450cfd352d5e50c1ce17b9fd7d1b7143a219f0e33d5483df",
-         intel: "5a1eb74d7900b157487dd7242686ca5d4a2d86e1cd6d7c77b8297a3344c39113"
+  version "5.6.0"
+  sha256 arm:   "ce0a955e013ea60a929df005822be2cecbb8adc97d84275fd5313c27cedf07f8",
+         intel: "97a22a604913f3e7defbec59425959614531223ad03c7f5580ce314c738e60b2"
 
   url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
       verified: "github.com/massCodeIO/massCode/"
@@ -15,8 +15,6 @@ cask "masscode" do
     url :url
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: :big_sur
 
