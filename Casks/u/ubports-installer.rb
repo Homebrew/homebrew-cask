8b1eb68b8cc09079f4848a3b6b42eec70e9791ea
Sakutaro Shina <companyemailetc@gmail.com>
2026-06-14 00:21:26 +0900

    ubports-installer: fix macOS download

--- Casks/u/ubports-installer.rb
+++ Casks/u/ubports-installer.rb
@@ -4,20 +4,10 @@ cask "ubports-installer" do
   on_arm do
     version "0.11.1"
     sha256 "18f9bdfe73a56f37cd90a43c0978ab98840ae0f2e5f27d8bbaa396d037fc63fe"
-
-    livecheck do
-      skip "No Apple Silicon release available after 0.11.1"
-    end
   end
   on_intel do
     version "0.11.2"
     sha256 "cd5532fff5c2a35e04f55b6b0ad03918404bfbaa1065e54a87cf9fb69609807b"
-
-    livecheck do
-      url :url
-      regex(/v?(\d+(?:\.\d+)+(?:-beta)?)/i)
-      strategy :github_latest
-    end
   end
 
   url "https://github.com/ubports/ubports-installer/releases/download/#{version}/ubports-installer_#{version}_mac_#{arch}.dmg",
@@ -25,6 +15,23 @@ cask "ubports-installer" do
   name "ubports installer"
   desc "Application to install ubports on mobile devices"
   homepage "https://ubports.com/"
+
+  livecheck do
+    url :url
+    regex(/^ubports-installer_(\d+(?:\.\d+)+)_mac_#{arch}\.dmg$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
