3a3fd22b5596b0670e174db3504c81e82d58b28f
Sam Ford <1584702+samford@users.noreply.github.com>
2026-06-13 10:47:28 -0400

    routeconverter 3.4.1
    `routeconverter` is autobumped but the workflow failed to update to
    version 3.4.1 because upstream now publishes arch-specific zip files.
    Unfortunately, the aarch64 zip contains the same Intel app as the x64
    zip. I've updated the cask to use an arch-specific setup but retained
    the Rosetta caveat for now. This allows us to update the version and
    we can remove the Rosetta caveat if/when the aarch64 zip file
    correctly contains an ARM app in the future.

--- Casks/r/routeconverter.rb
+++ Casks/r/routeconverter.rb
@@ -1,8 +1,11 @@
 cask "routeconverter" do
-  version "3.4"
-  sha256 "33459efd3381b1baddf1c45d6f44ab907c0727345dd32e51511120d601681227"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://static.routeconverter.com/download/previous-releases/#{version}/RouteConverterMacOpenSource.app.zip"
+  version "3.4.1"
+  sha256 arm:   "968d1900456306fd5ba0d4adea74db6a0e52b01b975180272902603766ded45d",
+         intel: "e0089f0d0c68542b887e3871df1a55db47777866ff29b5305a9777af3b61b61a"
+
+  url "https://static.routeconverter.com/download/previous-releases/#{version}/RouteConverterMacOpenSource-#{arch}.app.zip"
   name "RouteConverter"
   desc "GPS tool to display, edit, enrich and convert routes, tracks and waypoints"
   homepage "https://www.routeconverter.com/"
@@ -21,6 +24,7 @@ cask "routeconverter" do
 
   zap trash: "~/.routeconverter"
 
+  # The aarch64 zip contains the same Intel app as the x64 zip for now.
   caveats do
     requires_rosetta
   end
