602883cbca8970922b322da5aa0bd712359dc8d9
楼兰 <wuming497735138@gmail.com>
2026-06-13 21:04:47 +0800

    resolve curl failed

--- Casks/m/mailmaster.rb
+++ Casks/m/mailmaster.rb
@@ -1,11 +1,24 @@
+class MailmasterInsecureDownloadStrategy < CurlDownloadStrategy
+  def initialize(url, name, version, **meta)
+    super
+  end
+
+  # 重写核心的 curl_args 方法
+  # 这是最优雅的注入方式，既保留了父类(Homebrew核心)指定的所有输出路径(-o)，又安全地追加了参数
+  def curl_args
+    # super 会返回类似 ["--location", "--remote-time", "--continue-at", "-", "-o", "/path/to/cache"]
+    # 我们在其基础上追加 "--insecure" (-k) 
+    super + ["--insecure"]
+  end
+end
+
 cask "mailmaster" do
   version "5.5.6.1476"
   sha256 "95b8d8aa22eba8c1dbb497eb92cc16975b1af071e62fef4f94259e11bcf21791"
 
   url "https://fm.dl.126.net/mailmaster/updatemac/mailmaster-#{version}.dmg",
       verified: "fm.dl.126.net/mailmaster/updatemac/"
-  ENV["HOMEBREW_CURL_RC"] = "1"
-  ENV["curl_args"] = "-k"
+      using: MailmasterInsecureDownloadStrategy
   name "NetEase Mail Master"
   name "网易邮箱大师"
   desc "Email client"
