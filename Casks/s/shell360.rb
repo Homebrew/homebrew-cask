8f6e870cf64d80e023038cca0bbbb1af22a0b409
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 16:49:43 +0000

    shell360 0.2.3

--- Casks/s/shell360.rb
+++ Casks/s/shell360.rb
@@ -1,9 +1,9 @@
 cask "shell360" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.2.2"
-  sha256 arm:   "3ad5aaa08d2bef7611be9c895ad19446b1a0807aed78973273ded726dd6a8649",
-         intel: "4963d882460df18424dd6f595b4bb0f8ad457a9d793d76384159cec0fde1d00f"
+  version "0.2.3"
+  sha256 arm:   "ce68b66004b66da2ae95236b01ea36fa130f7fa88e488d8a88c94029649e5541",
+         intel: "5b85c93eb665b169006318a4504aba9be38de3dfdeaf3723e864b04d79dbf101"
 
   url "https://github.com/nashaofu/shell360/releases/download/v#{version}/Shell360_#{version}_#{arch}.dmg"
   name "Shell360"
