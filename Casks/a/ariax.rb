302c7aae8c5061e1862a9945119dc77277b13fdd
endpne <ourfor@qq.com>
2026-06-15 21:18:14 +0800

    ariax 1.0.7

--- Casks/a/ariax.rb
+++ Casks/a/ariax.rb
@@ -1,8 +1,8 @@
 cask "ariax" do
-  version "1.0.3"
-  sha256 "9b08b7bee08446307fb358367fa7dd20a08a7a0a38aafbf48419213c9eb8e9d0"
+  version "1.0.7"
+  sha256 "f6495f4ad5e753e19b7e796abc9738293232e190290ffc26a74ea35689a08698"
 
-  url "https://github.com/saltpi/Aria.X/releases/download/#{version}/AriaX.zip"
+  url "https://github.com/saltpi/Aria.X/releases/download/#{version}/AriaX.dmg"
   name "AriaX"
   desc "Aria2 download manager"
   homepage "https://github.com/saltpi/Aria.X"
