4fe3a02663a8a491c495872b3e53658246a50ae3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-15 20:21:42 +0000

    nimbalyst 0.65.4

--- Casks/n/nimbalyst.rb
+++ Casks/n/nimbalyst.rb
@@ -1,9 +1,9 @@
 cask "nimbalyst" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.64.4"
-  sha256 arm:   "6ba8c7df3c9eea30b41560536d770a581a5a4b49566c8bdaa472873b848641ab",
-         intel: "e870adf540d2f6ba5576fd83617a8e45ebda5c5e7ad3c53d4ca4f7f15a0d02a6"
+  version "0.65.4"
+  sha256 arm:   "5201bc620321a90a594f9adbc5e20c19d070d082bcbc6df0786f443f89baf557",
+         intel: "6529dd44827baae02bd818b149553a56e7ca9300ecc87f71b05d21629717b9eb"
 
   url "https://github.com/Nimbalyst/nimbalyst/releases/download/v#{version}/Nimbalyst-macOS-#{arch}.dmg",
       verified: "github.com/Nimbalyst/nimbalyst/"
