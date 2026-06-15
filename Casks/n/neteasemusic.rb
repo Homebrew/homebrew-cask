ad6ee0712f3d3c3fe707fc036545d1e1aebe81f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-15 06:00:52 +0000

    neteasemusic 3.1.8

--- Casks/n/neteasemusic.rb
+++ Casks/n/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask "neteasemusic" do
-  version "3.1.7,3283"
-  sha256 "283de565b1031102616aefa1475a6b1c94fa9d9ea65f8d5e5bf5041340ef358d"
+  version "3.1.8,3330"
+  sha256 "86d708532a0eb8443f9206830698582e20b91b502d52c16a4dea8e3dc49dfefb"
 
   url "https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_#{version.csv.join(".")}.dmg",
       verified:   "d1.music.126.net/",
