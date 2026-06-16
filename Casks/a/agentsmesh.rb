17f2c8a4ec6f665cac153b960c28ac6780982379
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 08:27:08 +0000

    agentsmesh 0.42.3

--- Casks/a/agentsmesh.rb
+++ Casks/a/agentsmesh.rb
@@ -1,9 +1,9 @@
 cask "agentsmesh" do
   arch arm: "-arm64"
 
-  version "0.42.2"
-  sha256 arm:   "daf5f3927b4d74145329653b348f8eafe39445b18e0f4c468c842dedc975810a",
-         intel: "227283b36184d7d8f3faeaa18aa342daa50f258b3834a96c0b07e5e9a4cbfdb5"
+  version "0.42.3"
+  sha256 arm:   "2aa5c2ae7daec738539fbb0cd0cff1ee3ed32df644406df5f9df688ff5eb72f3",
+         intel: "f77e9c81a8ec2627d1e7b5ce5ef27b6c7343636fa3a9ece53664e71916ad7755"
 
   url "https://github.com/AgentsMesh/AgentsMesh/releases/download/v#{version}/AgentsMesh-#{version}#{arch}.dmg",
       verified: "github.com/AgentsMesh/AgentsMesh/"
