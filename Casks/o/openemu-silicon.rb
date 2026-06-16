d45c886a7a3ae79fc301bbbe1d4fb4a7feacdb3b
nickybmon <nick.r.blackmon@gmail.com>
2026-06-16 10:32:45 -0400

    Add openemu-silicon 1.2.3 (new cask)

---
+++ Casks/o/openemu-silicon.rb
@@ -0,0 +1,29 @@
+cask "openemu-silicon" do
+  version "1.2.3"
+  sha256 "cbc9169e131a37abcee7dad4558e1f43990678b9b3603ea4f23d54f4467768cc"
+
+  url "https://github.com/nickybmon/OpenEmu-Silicon/releases/download/v#{version}/OpenEmu-Silicon.dmg"
+  name "OpenEmu Silicon"
+  desc "Native Apple Silicon port of the OpenEmu multi-system emulator"
+  homepage "https://github.com/nickybmon/OpenEmu-Silicon"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/nickybmon/OpenEmu-Silicon/main/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: :big_sur
+
+  app "OpenEmu.app"
+
+  zap trash: [
+    "~/Library/Application Support/OpenEmu",
+    "~/Library/Application Support/org.openemu.OEXPCCAgent.Agents",
+    "~/Library/Caches/org.openemu.OpenEmu",
+    "~/Library/HTTPStorages/org.openemu.OpenEmu",
+    "~/Library/HTTPStorages/org.openemu.OpenEmu.binarycookies",
+    "~/Library/Preferences/org.openemu.OpenEmu.plist",
+    "~/Library/Saved Application State/org.openemu.OpenEmu.savedState",
+  ]
+end
