478afeb8b20e2f66ba4487b4207a5f261b72ca7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-17 12:00:25 +0000

    openwork 0.17.1

--- Casks/o/openwork.rb
+++ Casks/o/openwork.rb
@@ -1,9 +1,9 @@
 cask "openwork" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.17.0"
-  sha256 arm:   "00b784cf8507e270281e8cec4617326ee02457f30ac395b0a585e935ca61dc3b",
-         intel: "3750b4517e37d890d9358f67dd189b551866beb140b68f366ce1224d79deca7d"
+  version "0.17.1"
+  sha256 arm:   "ab78124b2625aff78b31f06fc8fbb0e52f60b7d7f250e6d9753966deece43a40",
+         intel: "14130d4699714933963a6fef64d58cd4e0236b720f961868e287730b65fbff3a"
 
   url "https://github.com/different-ai/openwork/releases/download/v#{version}/openwork-mac-#{arch}-#{version}.dmg",
       verified: "github.com/different-ai/openwork/"
