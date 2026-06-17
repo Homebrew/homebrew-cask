9cffa957286720d6c4813c18c547b039f08e04bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-17 19:55:03 +0000

    notion-calendar 1.137.0

--- Casks/n/notion-calendar.rb
+++ Casks/n/notion-calendar.rb
@@ -1,9 +1,9 @@
 cask "notion-calendar" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.136.0"
-  sha256 arm:   "a374ab86c75c403baf3dc4c8f4797b1762986ae9c55624f1060e063437ae82f4",
-         intel: "76ea1cd7756388e092bfc26c01db32b5a75ed28d9730ef4cc5d2c3c1105028cc"
+  version "1.137.0"
+  sha256 arm:   "ef47a67c1cef5fca17f24ef18e93884f5a9d1499c5fe189ae416db02884f7bc1",
+         intel: "972be775fb5a30a140053ccca592dc1256edafd33cd0d6e9866e3002d7c4c9b7"
 
   url "https://calendar-desktop-release.notion-static.com/Notion%20Calendar-darwin-#{arch}-#{version}.zip",
       verified: "calendar-desktop-release.notion-static.com/"
