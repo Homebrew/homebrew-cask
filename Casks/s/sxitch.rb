8cff16754f7d77c962495b6acd7bef858bac7a51
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-06-18 22:53:09 +0800

    Add files via upload

---
+++ Casks/s/sxitch.rb
@@ -0,0 +1,24 @@
+cask "sxitch" do
+  version "1.0.7-alpha"
+
+  sha256 "b015eba192c950bbf739b2e20dbaa6e58a0b7762550bfbdec6033130d1e9b626"
+
+  url "https://storage.sxitch.app/Sxitch-universal-macos.app.zip"
+  name "Sxitch"
+  desc "Tree based app switcher"
+  homepage "https://sxitch.app"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  auto_updates false
+  depends_on macos: :big_sur
+
+  app "target/release/macos/Sxitch.app"
+
+  zap trash: [
+    "~/Library/Application Support/Sxitch/*",
+  ]
+end
