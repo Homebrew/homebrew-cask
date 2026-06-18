7de6370a503b640648da725ba2474c8c33c6d6d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 16:52:55 +0000

    atomcode 4.25.2

--- Casks/a/atomcode.rb
+++ Casks/a/atomcode.rb
@@ -2,11 +2,11 @@ cask "atomcode" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "4.25.1"
-  sha256 arm:          "7702b663270e580efa14af4d128a2725a759f93e0c85d4c8cec65c87de52e2b6",
-         intel:        "b52990ab717cd102c50e2fa8b97aa6dfda7deb3e4a67ecf4c495a1f47a52cc77",
-         arm64_linux:  "dc096a343b5c64d67cd57adfe8630359193816fd8d87307347f887105b383711",
-         x86_64_linux: "d510a3ebd3e08506fccd24af469e6af50a52a6d28f738dca83b4081dcc34cc8d"
+  version "4.25.2"
+  sha256 arm:          "a033ae7f599609efcf6033109e53965427a59a735c445ea7cad7c8b78a7d23f5",
+         intel:        "ebadd40a27566d48d82cec342717f34f1a2292e04be8c70d34a267c8a7e137a1",
+         arm64_linux:  "9a9a912a393b34ef48e98dc1fc0db9ccc717fad5386bbcc1cb12c405fc6266c3",
+         x86_64_linux: "fd9cd5ae68664556710cd6581310de55c810220b72508af2f2f56033fd515003"
 
   url "https://atomgit.com/atomgit_atomcode/atomcode/releases/download/v#{version}/atomcode-v#{version}-#{os}-#{arch}.tar.gz"
   name "AtomCode"
