184750a2e625be191308d63746f49a8ffdd145a5
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-06-18 23:19:45 +0800

    Update sxitch.rb

--- Casks/s/sxitch.rb
+++ Casks/s/sxitch.rb
@@ -1,24 +1,23 @@
 cask "sxitch" do
   version "1.0.7-alpha"
-
   sha256 "b015eba192c950bbf739b2e20dbaa6e58a0b7762550bfbdec6033130d1e9b626"
 
-  url "https://storage.sxitch.app/Sxitch-universal-macos.app.zip"
+  url "https://storage.sxitch.app/v#{version}/Sxitch-universal-macos.app.zip"
   name "Sxitch"
   desc "Tree based app switcher"
   homepage "https://sxitch.app"
 
   livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    url "https://storage.sxitch.app/version.txt"
+    regex(/^v?(\d+(?:\.\d+)+[\w-]*)$/i)
   end
 
   auto_updates false
-  depends_on macos: :big_sur
+  depends_on macos: :monterey
 
   app "target/release/macos/Sxitch.app"
 
   zap trash: [
-    "~/Library/Application Support/Sxitch/*",
+    "~/Library/Application Support/Sxitch",
   ]
 end
