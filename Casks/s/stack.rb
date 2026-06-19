7c7c56a54885758cc63be967528a9680d38226e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 14:39:33 +0000

    stack 6.0.3-20260619

--- Casks/s/stack.rb
+++ Casks/s/stack.rb
@@ -1,9 +1,9 @@
 cask "stack" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "6.0.3-20260618"
-  sha256 arm:   "3353444d37321ed4e94c3dfe9b0c99d2e125179126a6c9e0ce35253f847321dc",
-         intel: "c45c39eda23816539fb3f13d220fb847575ba8ada69710f8d7111ec1baf81690"
+  version "6.0.3-20260619"
+  sha256 arm:   "bf297418c881a822d97e8cde849c69d9f796fc99e4e6e12120f7599b83899143",
+         intel: "12363dbd10b8fd027bf6bb90ef0cfee77ea2e7ebef832af317ba66eeae148d11"
 
   url "https://filehosting-client.transip.nl/packages/stack/v#{version}/macos/stack-v#{version}.#{arch}.dmg"
   name "STACK"
