aa3edcdab4f1c105a061f14407482d210989e80b
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-06-20 16:15:17 +0800

    Update sxitch.rb

--- Casks/s/sxitch.rb
+++ Casks/s/sxitch.rb
@@ -7,11 +7,15 @@ cask "sxitch" do
   name "Sxitch"
   desc "Tree based app switcher"
   homepage "https://sxitch.app/"
+
   livecheck do
     url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/version.txt"
     regex(/^v?(\d+(?:\.\d+)+[\w-]*)$/i)
   end
-  depends_on macos: :monterey
-  app "target/release/macos/Sxitch.app"
+
+  depends_on macos: ">= :monterey"
+
+  app "Sxitch.app"
+
   zap trash: "~/Library/Application Support/Sxitch"
 end
