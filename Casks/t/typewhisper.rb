5cc5667ad159a40ab4b13987290cbc71b5d4a5bf
Marco Hillger <marco@seofood.de>
2026-06-20 12:36:37 +0200

    Add TypeWhisper cask

---
+++ Casks/t/typewhisper.rb
@@ -0,0 +1,27 @@
+cask "typewhisper" do
+  version "1.4.0"
+  sha256 "7158c9169f366affcfc235eeac58f14564063f6cf3d1dc602ab5cf09ea8995db"
+
+  url "https://github.com/TypeWhisper/typewhisper-mac/releases/download/v#{version}/TypeWhisper-v#{version}.dmg",
+      verified: "github.com/TypeWhisper/typewhisper-mac/"
+  name "TypeWhisper"
+  desc "Speech-to-text and AI text processing"
+  homepage "https://www.typewhisper.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: :sonoma
+
+  app "TypeWhisper.app"
+
+  zap trash: [
+    "~/Library/Application Support/TypeWhisper",
+    "~/Library/Caches/com.typewhisper.mac",
+    "~/Library/HTTPStorages/com.typewhisper.mac",
+    "~/Library/Preferences/com.typewhisper.mac.plist",
+  ]
+end
