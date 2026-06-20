b435bf0094b7fcacfa018c78cb55225807bfd407
Justin <159104807+Justin24506@users.noreply.github.com>
2026-06-20 12:12:43 +0530

    factory: fix SHA-256 mismatch

--- Casks/f/factory.rb
+++ Casks/f/factory.rb
@@ -2,8 +2,8 @@ cask "factory" do
   arch arm: "arm64", intel: "x64"
 
   version "0.110.0"
-  sha256 arm:   "4266d05d0d90025ba207a7c1d942060e639c97d7cc3b296b5780bb1c79830403",
-         intel: "77d52224020162763276d1b163de189d4ff3582aa7692578c87b7d46395c0273"
+  sha256 arm:   "17203eab80beae6f5b5dd79ad625e6f28ad5617356bc323daa3a704475815138",
+         intel: "ae21170fb47f4dcd9f43f59566922572f5276f192757c5fdf351f3345dcf424a"
 
   url "https://downloads.factory.ai/factory-desktop/releases/#{version}/darwin/#{arch}/Factory-#{version}-#{arch}.dmg"
   name "Factory"
