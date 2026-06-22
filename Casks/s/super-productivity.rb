5ed52bb9a9cd3884ac9b076ab5b98a07658e10bf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-06-22 17:03:09 -0400

    super-productivity: update download url to new org

--- Casks/s/super-productivity.rb
+++ Casks/s/super-productivity.rb
@@ -5,8 +5,8 @@ cask "super-productivity" do
   sha256 arm:   "1d15de1322b6efc2833bd7496186f5d9ce3dcdb58a09c9e4d33e71f5131c3ccc",
          intel: "30fe9d3eff9a96f6870dd718f7275b2424a34955a87695f696382f0c04173658"
 
-  url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{arch}.dmg",
-      verified: "github.com/johannesjo/super-productivity/"
+  url "https://github.com/super-productivity/super-productivity/releases/download/v#{version}/superProductivity-#{arch}.dmg",
+      verified: "github.com/super-productivity/super-productivity/"
   name "Super Productivity"
   desc "To-do list and time tracker"
   homepage "https://super-productivity.com/"
