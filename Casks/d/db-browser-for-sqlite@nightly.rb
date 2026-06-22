a3038cb8beacc16af9ee917a4a667d715ef3bb87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 01:48:34 +0000

    db-browser-for-sqlite@nightly 20260622

--- Casks/d/db-browser-for-sqlite@nightly.rb
+++ Casks/d/db-browser-for-sqlite@nightly.rb
@@ -1,6 +1,6 @@
 cask "db-browser-for-sqlite@nightly" do
-  version "20260610"
-  sha256 "26d03c56761658e8f317e081457883c072bf47fd311039cb61b94033ffb6ea54"
+  version "20260622"
+  sha256 "d2988557296c2188e02a27892d4a75fdcee371bdda944f13681437ae6cf23c30"
 
   url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/nightly/DB.Browser.for.SQLite-universal_#{version}.dmg",
       verified: "github.com/sqlitebrowser/sqlitebrowser/"
