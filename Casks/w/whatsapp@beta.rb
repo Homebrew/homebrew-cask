c6c8196405f394dd8cd3fe9e333a0641d0970a7c
bevanjkay <email@bevankay.me>
2026-06-22 13:55:53 +0200

    whatsapp@beta: align version with `info.plist`

--- Casks/w/whatsapp@beta.rb
+++ Casks/w/whatsapp@beta.rb
@@ -1,18 +1,15 @@
 cask "whatsapp@beta" do
-  version "2.26.24.21"
+  version "26.24.21"
   sha256 "bd03c5cc43c7472a5c5f3f32097647668f7c2cacd7058c33d81f87daeba40071"
 
-  url "https://web.whatsapp.com/desktop/mac_native/release/?version=#{version}&extension=zip&configuration=Beta&branch=master"
+  url "https://web.whatsapp.com/desktop/mac_native/release/?version=2.#{version}&extension=zip&configuration=Beta&branch=master"
   name "WhatsApp Beta"
   desc "Native desktop client for WhatsApp"
   homepage "https://www.whatsapp.com/"
 
   livecheck do
     url "https://web.whatsapp.com/desktop/mac_native/updates/?configuration=Beta&branch=master"
-    regex(/version=v?(\d+(?:\.\d+)+)/i)
-    strategy :sparkle do |item, regex|
-      item.url.scan(regex).map(&:first)
-    end
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
