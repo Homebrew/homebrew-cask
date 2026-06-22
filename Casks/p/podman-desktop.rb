cb2a301323bc5ed22c20f07ada37567dbd4b9009
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 18:11:23 +0000

    podman-desktop 1.28.2

--- Casks/p/podman-desktop.rb
+++ Casks/p/podman-desktop.rb
@@ -1,9 +1,9 @@
 cask "podman-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.27.2"
-  sha256 arm:   "bbacf1a268c8f7ac8a67c6e24ea500c96dfb0e2b825ae2a187a60434a37cb045",
-         intel: "8916497c9863b32631a90958514ae2c8127d5f5db109e3891c1b0ca60bfef805"
+  version "1.28.2"
+  sha256 arm:   "d7851363cd244dfb3e77a5fd5375b37ded086d587ea921bb650e7a12ce84fc1a",
+         intel: "7f1763264fbcf99a7092e7db0e13997d0a9b002f0165bd8fbeeb88231d12d6c2"
 
   url "https://github.com/containers/podman-desktop/releases/download/v#{version}/podman-desktop-#{version}-#{arch}.dmg",
       verified: "github.com/containers/podman-desktop/"
