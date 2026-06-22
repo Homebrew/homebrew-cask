a07e3f011f8e57e02e62c9b669a8061caf489de5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 09:14:41 +0000

    linkandroid 2.0.0

--- Casks/l/linkandroid.rb
+++ Casks/l/linkandroid.rb
@@ -1,9 +1,9 @@
 cask "linkandroid" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.3"
-  sha256 arm:   "10d158d341ab666259a7235b52e382dfe4529d744c0641d418f69afba74903d4",
-         intel: "9a530d3d26f93b55a4e9a55b941ce12632407aa5270939a187380bec82d4a0b4"
+  version "2.0.0"
+  sha256 arm:   "00fe70ce155612b18d1d7bbdb3321a141410bf7c2f7386f933d5203bc1778a01",
+         intel: "bbc1e4cb3785361dfb14659edcb27d8113afb51db11d430f51192ecb6e84b0e5"
 
   url "https://github.com/modstart-lib/linkandroid/releases/download/v#{version}/LinkAndroid-#{version}-mac-#{arch}.dmg",
       verified: "github.com/modstart-lib/linkandroid/"
