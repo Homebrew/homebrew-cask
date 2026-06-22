56c6ece740751e4fe486b10c50d79d2ff2f38112
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-22 17:03:52 -0400

    Add CaDoodle to brew

---
+++ Casks/c/cadoodle.rb
@@ -0,0 +1,11 @@
+cask "cadoodle" do
+  version "1.7.0"
+  sha256 "358f89cfba4a21555943271d00d963cd84acff6c8b486c255d1468ea38c10fc2"
+
+  url "https://github.com/CommonWealthRobotics/CaDoodle/releases/download/#{version}/CaDoodle-MacOS-arm64.dmg"
+  name "CaDoodle"
+  desc "3D CAD tool for robotics and makers"
+  homepage "https://github.com/CommonWealthRobotics/CaDoodle"
+
+  app "CaDoodle.app"
+end
