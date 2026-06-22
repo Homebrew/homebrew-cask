911f8d00cedf3df68c53d18530050d3a0f6b6eb9
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-22 17:25:44 -0400

    Correcting file based on CI feedback

--- Casks/c/cadoodle.rb
+++ Casks/c/cadoodle.rb
@@ -1,11 +1,14 @@
 cask "cadoodle" do
   version "1.7.0"
-  sha256 "358f89cfba4a21555943271d00d963cd84acff6c8b486c255d1468ea38c10fc2"
+  sha256 "ad75ed89a9ecfe79043e7a60fabb40a3b4fce3229d1eabd7f1ad700dd6855eb7"
 
-  url "https://github.com/CommonWealthRobotics/CaDoodle/releases/download/#{version}/CaDoodle-MacOS-arm64.dmg"
+  url "https://github.com/CommonWealthRobotics/CaDoodle/releases/download/#{version}/CaDoodle-MacOS-arm64.dmg",
+      verified: "github.com/CommonWealthRobotics/CaDoodle/"
   name "CaDoodle"
   desc "3D CAD tool for robotics and makers"
-  homepage "https://cadoodlecad.com/"
+  homepage "https://cadoodlecad.com"
+
+  depends_on :macos
 
   app "CaDoodle.app"
 end
