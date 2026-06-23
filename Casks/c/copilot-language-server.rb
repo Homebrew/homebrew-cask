51587088a0c771a9fa36a9cc4d6b5e9f832d78e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 04:59:58 +0000

    copilot-language-server 1.509.1

--- Casks/c/copilot-language-server.rb
+++ Casks/c/copilot-language-server.rb
@@ -6,11 +6,11 @@ cask "copilot-language-server" do
     depends_on macos: :big_sur
   end
 
-  version "1.509.0"
-  sha256 arm:          "16799c3f0735471f526eac9dfb26e02bed78e4f1b1ea9b2a17843e2c9a769c2c",
-         intel:        "b94776df8b842d8d8a9d77a0d750a0336f33ffe0b1e9512867f0233c347aaec1",
-         arm64_linux:  "39e366dd488fb9f907ae7ac3d899b1a8dfc883c5a3e6bef0cbab2c484fc6b43a",
-         x86_64_linux: "0407e48d969455c54c0e92f6a7c18aa2c167f21b4729f2e08e29050299e2574e"
+  version "1.509.1"
+  sha256 arm:          "391a3a2ac987262a66d9262e928c640356af28e0c269251e5cef3ff0a6ed8549",
+         intel:        "9755c9b2dcfb03c2d8a6bd9bfcbe147ce647ba302a57d3806319261f05626754",
+         arm64_linux:  "b9443a80d53a7b9ec10a68550cfd6d8ab06ebcb927cc11d36e75b957cdc261b0",
+         x86_64_linux: "6f7b7a3d5c8c0db44738aa87004d19b2a31f6976af6451ab0d37044c74a977d8"
 
   url "https://github.com/github/copilot-language-server-release/releases/download/#{version}/copilot-language-server-#{os}-#{arch}-#{version}.zip"
   name "GitHub Copilot Language Server"
