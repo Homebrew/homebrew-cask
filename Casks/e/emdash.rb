32ccdd3e3b8b82a955aeb8a624382409fb6e6cf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 05:08:45 +0000

    emdash 1.1.35

--- Casks/e/emdash.rb
+++ Casks/e/emdash.rb
@@ -1,9 +1,9 @@
 cask "emdash" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.1.34"
-  sha256 arm:   "201ee316b9603e824780ccfa32cd75388698f4eff9cb622c8e5193ca3dd60ea0",
-         intel: "f0360f10660bfbfe7085eee6a88e7cba7d3865123cd937d10e85867de3ae4d5d"
+  version "1.1.35"
+  sha256 arm:   "e496086c5a4c8cedf950ffe94429b9c582d28ac1d8adf212ca2fc971bb2ae1d4",
+         intel: "b982ca378326f0d8470b37595294ff9dc968f9e6179aa5ce96cb45bc32e12d88"
 
   url "https://github.com/generalaction/emdash/releases/download/v#{version}/emdash-#{arch}.dmg",
       verified: "github.com/generalaction/emdash/"
