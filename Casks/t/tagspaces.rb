7bf62ad279f5cd207249d4759beddd1dcc79a1f2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-06-23 12:52:12 -0400

    tagspaces: add Linux AppImage

--- Casks/t/tagspaces.rb
+++ Casks/t/tagspaces.rb
@@ -1,11 +1,21 @@
 cask "tagspaces" do
-  arch arm: "arm64", intel: "x64"
+  os macos: "mac", linux: "linux"
+
+  on_macos do
+    arch arm: "arm64", intel: "x64"
+  end
+  on_linux do
+    arch arm: "arm64", intel: "x86_64"
+  end
 
   version "6.13.3"
-  sha256 arm:   "afad9de23092915d3d6c39f0689cef28d93ab58cb089112891ac9278f6bb8e92",
-         intel: "d4d9915ac78a56c48682e165f648ba339c6855a609afa4515d85ed3b17cd07bc"
+  sha256 arm:          "afad9de23092915d3d6c39f0689cef28d93ab58cb089112891ac9278f6bb8e92",
+         intel:        "d4d9915ac78a56c48682e165f648ba339c6855a609afa4515d85ed3b17cd07bc",
+         arm64_linux:  "862d072a8f5d2722bd35626b81fb8c0aa54b305a4523fedc0e829a8322ecd005",
+         x86_64_linux: "b69a362d585d82576c2f2b065969a85967bc82ec0b76200581115b7ffe8c7717"
 
-  url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
+  url_end = on_system_conditional linux: ".AppImage", macos: ".dmg"
+  url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-#{os}-#{arch}-#{version}#{url_end}",
       verified: "github.com/tagspaces/tagspaces/"
   name "TagSpaces"
   desc "Offline, open-source, document manager with tagging support"
@@ -16,13 +26,19 @@ cask "tagspaces" do
     strategy :github_latest
   end
 
-  depends_on macos: :monterey
+  on_macos do
+    depends_on macos: :monterey
 
-  app "TagSpaces.app"
+    app "TagSpaces.app"
 
-  zap trash: [
-    "~/Library/Application Support/TagSpaces",
-    "~/Library/Preferences/org.tagspaces.desktopapp.plist",
-    "~/Library/Saved Application State/org.tagspaces.desktopapp.savedState",
-  ]
+    zap trash: [
+      "~/Library/Application Support/TagSpaces",
+      "~/Library/Preferences/org.tagspaces.desktopapp.plist",
+      "~/Library/Saved Application State/org.tagspaces.desktopapp.savedState",
+    ]
+  end
+
+  on_linux do
+    app_image "tagspaces-linux-#{arch}-#{version}.AppImage", target: "TagSpaces.AppImage"
+  end
 end
