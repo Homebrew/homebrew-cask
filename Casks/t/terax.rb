114bccdb02bb2d525a8fd0ef0a97a67f212eaf24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 14:54:45 +0000

    terax 0.8.2

--- Casks/t/terax.rb
+++ Casks/t/terax.rb
@@ -1,9 +1,9 @@
 cask "terax" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.8.1"
-  sha256 arm:   "c2f45d7e9334e9537b32540b5442883e3c27bf800f9ca94ffb1addeda944d08a",
-         intel: "bd0871eea0912df56859a04e1aa74846a0a16dda9314ea7819a47ab9bebc909f"
+  version "0.8.2"
+  sha256 arm:   "a25cc6d2e2dd958bccbbb83045d31cda55780af2f617850a6df3c59e077774c4",
+         intel: "d80372ab32d6782e6192ffd9bdafd3756efc44ad01391d8d59b60c2c142688bb"
 
   url "https://github.com/crynta/terax-ai/releases/download/v#{version}/Terax_#{version}_#{arch}.dmg",
       verified: "github.com/crynta/terax-ai/"
