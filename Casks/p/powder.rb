b0bccc586cf4ba71d68560f99f4696883b25db66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 02:13:01 +0000

    powder 100.0.399

--- Casks/p/powder.rb
+++ Casks/p/powder.rb
@@ -1,9 +1,9 @@
 cask "powder" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "99.5.394"
-  sha256 arm:   "355ac24b9af8f55f8ecef2a0f2cefa89c04efd171dc501f113862d901f4c881a",
-         intel: "00ecc496e488aa5cacd8d365b9202c6cb7441606224d2a0f1b20fed386b49696"
+  version "100.0.399"
+  sha256 arm:   "44820c7c066a8ca059f95834b2f63a81a9f5ee51532a8f725e4a718f51b2720c",
+         intel: "ce526877f2283fc3d204dffeb61f70d08377309ba6989d6049469333bf637f18"
 
   url "https://github.com/The-Powder-Toy/The-Powder-Toy/releases/download/v#{version}/powder-v#{version}-#{arch}-darwin-macos.dmg",
       verified: "github.com/The-Powder-Toy/The-Powder-Toy/"
