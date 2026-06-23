4aa9ed52e98d88e895486bda34e0a96a8b8cf867
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 22:08:36 +0000

    coderabbit 0.6.3

--- Casks/c/coderabbit.rb
+++ Casks/c/coderabbit.rb
@@ -1,9 +1,9 @@
 cask "coderabbit" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.6.2"
-  sha256 arm:   "9b85b249570e7569451683858b195308113e8d0e528fd093be74c2bf972d2cda",
-         intel: "bf3b45c456df8a0e76f3a7b96d971c3e86bc52c3d718c10d23a07306910785d5"
+  version "0.6.3"
+  sha256 arm:   "123479715795e6d400f2e5b9b0187ad307ab33089d1e1a65b4d63253215b0889",
+         intel: "66e643ef10e0ca29e03d4bc7e76507e678810a45c0412312f9c7a2d21a11105d"
 
   url "https://cli.coderabbit.ai/releases/#{version}/coderabbit-darwin-#{arch}.zip"
   name "CodeRabbit"
