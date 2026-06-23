7e7125bac96e8f423e205f7f54a5441f6ca8604d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 20:03:06 +0000

    orion 1.1
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/o/orion.rb
+++ Casks/o/orion.rb
@@ -1,7 +1,7 @@
 cask "orion" do
   on_catalina :or_older do
-    version "1.0.8,147.1"
-    sha256 "85fbe203eec748da8ec763574aee42d0785bbe48684597b95010609467132db7"
+    version "1.1,148"
+    sha256 "c2d55b9ffa9531e868c76f6fae9fc6ae37420e74238b339c48e5e305ebb4bf96"
 
     url "https://cdn.kagi.com/updates/10_15/#{version.csv.second}.zip"
 
@@ -11,8 +11,8 @@ cask "orion" do
     end
   end
   on_big_sur do
-    version "1.0.8,147.1"
-    sha256 "db8a7a6f3f9923a05ce9f2b5804950b25f9e92dcffa7cbb3bef155df25aef7c7"
+    version "1.1,148"
+    sha256 "e68a8270ac308ea8a808dd5b1dbfe518cb0103603ac9adccea37529fc51e1860"
 
     url "https://cdn.kagi.com/updates/11_0/#{version.csv.second}.zip"
 
@@ -22,8 +22,8 @@ cask "orion" do
     end
   end
   on_monterey do
-    version "1.0.8,147.1"
-    sha256 "ebeec9662989f517e432b19daf9c1c1c3699bdbf4598f79d5dafd594f945e655"
+    version "1.1,148"
+    sha256 "ff9ac1c197599e126b028619be3d451906f7bc10b615c311de55f2a5685bd631"
 
     url "https://cdn.kagi.com/updates/12_0/#{version.csv.second}.zip"
 
@@ -33,8 +33,8 @@ cask "orion" do
     end
   end
   on_ventura do
-    version "1.0.8,147.1"
-    sha256 "83f89e88a2c3bb90c824a45e140e9a6485f84351bfa3ae8bc8208f109c7cfd73"
+    version "1.1,148"
+    sha256 "307fd25815c9343d4c18190d94d51e6dab49469d643ced2c8fd2b6452796f8ae"
 
     url "https://cdn.kagi.com/updates/13_0/#{version.csv.second}.zip"
 
@@ -44,8 +44,8 @@ cask "orion" do
     end
   end
   on_sonoma do
-    version "1.0.8,147.1"
-    sha256 "306e8697daae08e6b4da005d12622dae258ec3ac0e5db4dfea73f2c0053c7331"
+    version "1.1,148"
+    sha256 "816ee0d7e6288eda107e189c6c7fadfc0a5012aa21247007bd51321c40fa376a"
 
     url "https://cdn.kagi.com/updates/14_0/#{version.csv.second}.zip"
 
@@ -55,8 +55,8 @@ cask "orion" do
     end
   end
   on_sequoia do
-    version "1.0.8,147.1"
-    sha256 "8a94632542ce809c996c60e5634fa2804254b1c71430cc8ae3a910f43f15450b"
+    version "1.1,148"
+    sha256 "572be3565129e83b6994feeddca871a4ae17e7efe084503b5740004deebf9e45"
 
     url "https://cdn.kagi.com/updates/15_0/#{version.csv.second}.zip"
 
@@ -66,8 +66,8 @@ cask "orion" do
     end
   end
   on_tahoe :or_newer do
-    version "1.0.8,147.1"
-    sha256 "4015b14fb2faf8b2fdb4af1224cf317f172330ca11706a9d4210749cd4cdf24a"
+    version "1.1,148"
+    sha256 "1bb2005787d9eb7bbc7bd79d38e3ffe41a0e29c46d7d4a6c6a68fac832190555"
 
     url "https://cdn.kagi.com/updates/26_0/#{version.csv.second}.zip"
 
