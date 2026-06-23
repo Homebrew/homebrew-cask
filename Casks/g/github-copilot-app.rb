b8f9a0959803a785eaab264e1f02800e60fb0bf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 01:52:02 +0000

    github-copilot-app 1.0.5

--- Casks/g/github-copilot-app.rb
+++ Casks/g/github-copilot-app.rb
@@ -2,11 +2,11 @@ cask "github-copilot-app" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "1.0.4"
-  sha256 arm:          "c09e092342bc6c17e422eb4478fe263749767126be9eb53003f9c8f96d8e6ace",
-         intel:        "43c127d2226d6020aa99059698cba1c1357f54c7ff3470e2d1ae6dd072187f09",
-         arm64_linux:  "2304b870f391cba478ac233b77fd0fc9829aa86618144e4f3f26486962e99128",
-         x86_64_linux: "6120bcbd928377c5e1d1e046bf01829eea60cf48c5f0f2fe70e614bcea90fcaa"
+  version "1.0.5"
+  sha256 arm:          "468cb6af98c6e392a00c827a26180a26652e12bc00368082624ad7782c5a341f",
+         intel:        "43e90db064fbf99251ae503ad7e23d8367960c144b457127eca321dd7bac931b",
+         arm64_linux:  "ef4c05ad68982887081d09e7e767eed15122c330e70a9b3392d1776e57ba89a0",
+         x86_64_linux: "bbb4c91e601a81baa070eeb39438ae6edc5333e2e1b9a4c3f8759cac7d763cac"
 
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
