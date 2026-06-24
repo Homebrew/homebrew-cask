51bfd3010fe3b6c89b12d50bcf8404cfb83474ad
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-06-23 22:18:44 -0400

    cursor: add Linux AppImage

--- Casks/c/cursor.rb
+++ Casks/c/cursor.rb
@@ -1,18 +1,55 @@
 cask "cursor" do
   arch arm: "arm64", intel: "x64"
+  os macos: "darwin", linux: "linux"
 
   version "3.8.23,7cf19b7482706625cdb70db3211b7dd035b7aa35"
-  sha256 arm:   "76911e3cc7e61ee63253afef49a4d37dae52b085a78ed5d5faaf264c4c57b2fb",
-         intel: "5505638bcefd48d76b86d257e23f3950f8f92ed5f175fd2365a0954db75f601c"
+  sha256 arm:          "76911e3cc7e61ee63253afef49a4d37dae52b085a78ed5d5faaf264c4c57b2fb",
+         intel:        "5505638bcefd48d76b86d257e23f3950f8f92ed5f175fd2365a0954db75f601c",
+         arm64_linux:  "1a5f3bece62aa6789d0bc1fbc31654bd63a716a4ff52f85c96dbc9485ae92520",
+         x86_64_linux: "64ba1c6db38568a3ed059b8e326baaa910c37b904c34135b381994f23ac71de2"
 
-  url "https://downloads.cursor.com/production/#{version.csv.second}/darwin/#{arch}/Cursor-darwin-#{arch}.zip"
+  url_end = on_system_conditional linux: "AppImage", macos: "zip"
+
+  on_macos do
+    url "https://downloads.cursor.com/production/#{version.csv.second}/#{os}/#{arch}/Cursor-darwin-#{arch}.#{url_end}"
+
+    auto_updates true
+    depends_on macos: :monterey
+
+    app "Cursor.app"
+    binary "#{appdir}/Cursor.app/Contents/Resources/app/bin/code", target: "cursor"
+
+    zap trash: [
+      "~/.cursor",
+      "~/.cursor-tutor",
+      "~/Library/Application Support/Caches/cursor-updater",
+      "~/Library/Application Support/Cursor",
+      "~/Library/Caches/com.todesktop.*",
+      "~/Library/Caches/com.todesktop.*.ShipIt",
+      "~/Library/HTTPStorages/com.todesktop.*",
+      "~/Library/Logs/Cursor",
+      "~/Library/Preferences/ByHost/com.todesktop.*.ShipIt.*.plist",
+      "~/Library/Preferences/com.todesktop.*.plist",
+      "~/Library/Saved Application State/com.todesktop.*.savedState",
+      "~/Library/Saved Application State/todesktop.com.ToDesktop-Installer.savedState",
+    ]
+  end
+
+  on_linux do
+    artifact_arch = on_arch_conditional arm: "aarch64", intel: "x86_64"
+
+    url "https://downloads.cursor.com/production/#{version.csv.second}/#{os}/#{arch}/Cursor-#{version.csv.first}-#{artifact_arch}.#{url_end}"
+
+    app_image "Cursor-#{version.csv.first}-#{artifact_arch}.AppImage", target: "Cursor.AppImage"
+  end
+
   name "Cursor"
   desc "Write, edit, and chat about your code with AI"
   homepage "https://www.cursor.com/"
 
   livecheck do
-    url "https://api2.cursor.sh/updates/api/update/darwin-#{arch}/cursor/0.0.0/stable"
-    regex(%r{/production/(\h+)/darwin/#{arch}/Cursor[._-]darwin[._-]#{arch}\.zip}i)
+    url "https://api2.cursor.sh/updates/api/update/#{os}-#{arch}/cursor/0.0.0/stable"
+    regex(%r{/production/(\h+)/#{os}/#{arch}/Cursor[._-]#{os}[._-]#{arch}\.#{url_end}}i)
     strategy :json do |json, regex|
       match = json["url"]&.match(regex)
       next if match.blank?
@@ -20,25 +57,4 @@ cask "cursor" do
       "#{json["name"]},#{match[1]}"
     end
   end
-
-  auto_updates true
-  depends_on macos: :monterey
-
-  app "Cursor.app"
-  binary "#{appdir}/Cursor.app/Contents/Resources/app/bin/code", target: "cursor"
-
-  zap trash: [
-    "~/.cursor",
-    "~/.cursor-tutor",
-    "~/Library/Application Support/Caches/cursor-updater",
-    "~/Library/Application Support/Cursor",
-    "~/Library/Caches/com.todesktop.*",
-    "~/Library/Caches/com.todesktop.*.ShipIt",
-    "~/Library/HTTPStorages/com.todesktop.*",
-    "~/Library/Logs/Cursor",
-    "~/Library/Preferences/ByHost/com.todesktop.*.ShipIt.*.plist",
-    "~/Library/Preferences/com.todesktop.*.plist",
-    "~/Library/Saved Application State/com.todesktop.*.savedState",
-    "~/Library/Saved Application State/todesktop.com.ToDesktop-Installer.savedState",
-  ]
 end
