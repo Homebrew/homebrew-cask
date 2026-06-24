caff42eb7d72a70622a089c3a9eb51a88a5fbc92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 01:35:02 +0000

    devtunnel 1.0.1941+fa8849cecf

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -2,11 +2,11 @@ cask "devtunnel" do
   arch arm: "arm64", intel: "x64"
   os macos: "osx", linux: "linux"
 
-  version "1.0.1886+37d3b95bd3"
-  sha256 arm:          "b31a2b25c51b79031b518aa0920d4869ee8052bcbde0116be7bc27dc134e85c4",
-         intel:        "b1dafd7fce4a0eb57588abd1ece1545951e9f0aaba62badbb010beb81ff8c67e",
-         arm64_linux:  "aa9c2ed0eae492521fcbd5e73286c5265661f2cca9e6ddd3f4710853a26934da",
-         x86_64_linux: "a7558f0fb0167cccbb9da26543d3e6a2ebeff63f8934b73a7a4e1541d026d510"
+  version "1.0.1941+fa8849cecf"
+  sha256 arm:          "16fa0d6a4468a2264724d5e242b6f3ab59905a890abbe5b3265a115d02a97ef7",
+         intel:        "6a8442eb746928f1125b3dc0862963ef03a1acff829345bb16168a8657e0de55",
+         arm64_linux:  "7fc85b81d13710846da50001f190ece30a16c7e58960b8f792f94cc0d387ba74",
+         x86_64_linux: "14bb504df772e96c356c3745fadfaac18bc5cf9d2136a62192d9f0550077dd0f"
 
   on_macos do
     url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/#{os}-#{arch}-devtunnel-zip",
