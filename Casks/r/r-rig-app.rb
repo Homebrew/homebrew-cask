4dcb7bcc2466a410f779717518a7cf03bce24170
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 11:17:06 +0000

    r-rig-app 0.8.1

--- Casks/r/r-rig-app.rb
+++ Casks/r/r-rig-app.rb
@@ -1,9 +1,9 @@
 cask "r-rig-app" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.8.0"
-  sha256 arm:   "9504c7b3f3fdd364c92462115003ec48c36f39425e823b3076b8c1dfe4f11638",
-         intel: "ef11eae03c6262bf904842327fc97c2d8321652c01c25d0c2b5167f408c42d0a"
+  version "0.8.1"
+  sha256 arm:   "65a6cd859b22e8a0bd2a1e99ed8a7cab73460c1ef8b7ac27e4697961dcc1de74",
+         intel: "fada7177d6c5aeb66353ade8cdb59f2905cfb982b8de9cdc2179f8220e010e25"
 
   url "https://github.com/r-lib/rig/releases/download/v#{version}/rig-#{version}-macOS-#{arch}.pkg"
   name "r-rig-app"
