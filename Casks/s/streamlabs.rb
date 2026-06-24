853616c08002b79638391ae120af3f1702252072
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 20:02:01 +0000

    streamlabs 1.21.6

--- Casks/s/streamlabs.rb
+++ Casks/s/streamlabs.rb
@@ -1,9 +1,9 @@
 cask "streamlabs" do
   arch arm: "-arm64"
 
-  version "1.20.7"
-  sha256 arm:   "1cbae6b436ed6bffe0925eddaf02e55b74b14ad518ea694a68009c17a814615c",
-         intel: "9cf5b89a615da8629ac5bc9c47f68486da3c008690335e73fbefba9bf30b054f"
+  version "1.21.6"
+  sha256 arm:   "60be6614cf6e32d065cb04b63e29e54f7af25539604296ca17eb3b7b48d2c569",
+         intel: "79ccb708da01a409a50effbde17a2066d37e4c96b3f0940137851732fa83c1a2"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+Desktop-#{version}#{arch}.dmg"
   name "Streamlabs Desktop"
