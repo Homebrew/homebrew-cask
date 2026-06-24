1cac46fa0289c1ba3e1c4ad31045ba1413baaf37
Sam Ford <1584702+samford@users.noreply.github.com>
2026-06-24 19:05:26 -0400

    slicer: update macOS dependency

--- Casks/s/slicer.rb
+++ Casks/s/slicer.rb
@@ -22,7 +22,7 @@ cask "slicer" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   conflicts_with cask: "slicer@preview"
-  depends_on macos: :ventura
+  depends_on macos: :sonoma
 
   app "Slicer.app"
 
