0f02cdb79b50051912c45ffaeca341515e28987d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 11:27:37 +0000

    tencent-ugit 5.50.1

--- Casks/t/tencent-ugit.rb
+++ Casks/t/tencent-ugit.rb
@@ -1,9 +1,9 @@
 cask "tencent-ugit" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.50.0"
-  sha256 arm:   "8494bde388efb15ab83478d0f5ac6e3e7c82cdc826cce36e6612e00464955892",
-         intel: "242857f35e4e4e95244b7881a9446a0efd54f8ec869ceeb641ca6c48d1010efc"
+  version "5.50.1"
+  sha256 arm:   "c5187b1393007810bf2cb18e11776b0cff4e0b59567eee2c3441a56769d6e0df",
+         intel: "c0431193184c973f2e812bbd2b62ffb59df61a0316b0e5d9c42122eea43f81b5"
 
   url "https://ugit.qq.com/downloads/ugit_public/UGit-#{arch}-#{version}.dmg"
   name "UGit"
