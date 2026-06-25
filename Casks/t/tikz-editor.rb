a1aa13d4b91bb4c4e02ccb263797beefe687f637
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 17:35:31 +0000

    tikz-editor 0.5.0

--- Casks/t/tikz-editor.rb
+++ Casks/t/tikz-editor.rb
@@ -1,9 +1,9 @@
 cask "tikz-editor" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.4.0"
-  sha256 arm:   "d8cd038813a13ff25639a4134b7fc368dc4334e019f04cebccef1588484849cd",
-         intel: "07c4f14030126ec186b748ebc083ebc1a3c930e31aff2eb9c4fa74cf0ad02cd2"
+  version "0.5.0"
+  sha256 arm:   "6cd4d86c62cd3acb9bdb8652e8e7bb15bd552bc90eab0f11f138acabb5bd3503",
+         intel: "c88476b008a699c475fbdc34359f66ca00286beed0ff3f48c41ffd3449714ee0"
 
   url "https://github.com/DominikPeters/tikz-editor/releases/download/app-v#{version}/TikZ.Editor_#{version}_#{arch}.dmg",
       verified: "github.com/DominikPeters/tikz-editor/"
