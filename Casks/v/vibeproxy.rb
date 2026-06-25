5056230cb707c06510b5c9a21a55ee8c5437bc12
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-06-24 22:48:45 -0400

    vibeproxy: add Intel build

--- Casks/v/vibeproxy.rb
+++ Casks/v/vibeproxy.rb
@@ -1,14 +1,16 @@
 cask "vibeproxy" do
-  version "1.8.211"
-  sha256 "57dc8c2e8957ac5235195afe1c856e5ee27d38f4839bb8d4edf44ff7503da6c5"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/automazeio/vibeproxy/releases/download/v#{version}/VibeProxy-arm64.dmg"
+  version "1.8.211"
+  sha256 arm:   "57dc8c2e8957ac5235195afe1c856e5ee27d38f4839bb8d4edf44ff7503da6c5",
+         intel: "d923b73cd769ce3a91678c70d6bbbe9e185f52c9605b9d3d3f2e235aee8f0f24"
+
+  url "https://github.com/automazeio/vibeproxy/releases/download/v#{version}/VibeProxy-#{arch}.dmg"
   name "VibeProxy"
   desc "Menu bar app for using AI subscriptions with coding tools"
   homepage "https://github.com/automazeio/vibeproxy"
 
   auto_updates true
-  depends_on arch: :arm64
   depends_on macos: :ventura
 
   app "VibeProxy.app"
