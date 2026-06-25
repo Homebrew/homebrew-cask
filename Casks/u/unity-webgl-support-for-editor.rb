10ed2d80f4f788f9d5bddbd9548f5e535e1713e1
Sam Ford <1584702+samford@users.noreply.github.com>
2026-06-25 15:57:41 -0400

    unity-webgl-support-for-editor: undeprecate

--- Casks/u/unity-webgl-support-for-editor.rb
+++ Casks/u/unity-webgl-support-for-editor.rb
@@ -12,8 +12,6 @@ cask "unity-webgl-support-for-editor" do
     cask "unity"
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   depends_on :macos
   depends_on cask: "unity"
 
