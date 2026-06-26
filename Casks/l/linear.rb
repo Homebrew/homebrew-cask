2f91e53ab8ecef881cc7ca36cb9ca22f1aac1082
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 17:01:05 +0000

    linear 1.31.1

--- Casks/l/linear.rb
+++ Casks/l/linear.rb
@@ -1,6 +1,6 @@
 cask "linear" do
-  version "1.31.0"
-  sha256 "ef98de3dc1a9ecd7a11a90783cd466a1b9683bb0ce62be48acf2f38246561e78"
+  version "1.31.1"
+  sha256 "85a673f5175b7108856c2a9dcbf4b6e5a0ac16848a9f774502460bf0d8284d8c"
 
   url "https://releases.linear.app/Linear-#{version}-universal.dmg"
   name "Linear"
