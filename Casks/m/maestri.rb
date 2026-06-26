8804e749e0d9c97900c08f29396a783ff75a09a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 19:44:02 +0000

    maestri 0.30.11

--- Casks/m/maestri.rb
+++ Casks/m/maestri.rb
@@ -1,6 +1,6 @@
 cask "maestri" do
-  version "0.30.10"
-  sha256 "66a16d8c6d8c8cbca351fff20464a66a417e34d1cb8f8a689ef42892f2de9392"
+  version "0.30.11"
+  sha256 "59ff04f84791767be096f1f8cc9b4e883f3734e752d7a12b7ab3723cf99b1b9d"
 
   url "https://pub-84eb0b1f3fb3420bbe57a88de7443b71.r2.dev/downloads/Maestri-#{version}.zip",
       verified: "pub-84eb0b1f3fb3420bbe57a88de7443b71.r2.dev/downloads/"
