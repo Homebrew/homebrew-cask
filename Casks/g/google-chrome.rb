81d632208b1c4e94c72b90f7331971c61d9761ed
MoonFruit <dkmoonfruit@gmail.com>
2026-06-26 10:56:51 +0800

    google-chrome 149.0.7827.201

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "149.0.7827.197"
+  version "149.0.7827.201"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
