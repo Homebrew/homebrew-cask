6cd059bda1801a68589d2083beaabf2b2c365399
Aleksandar Milanovic <viking1304@gmail.com>
2026-06-26 08:57:15 +0200

    cloudmounter: update zap
    Add com.eltima.activator.xml to zap stanza.
    
    Also fix depends_on macos style offense corrected by brew style --fix.

--- Casks/c/cloudmounter.rb
+++ Casks/c/cloudmounter.rb
@@ -28,6 +28,7 @@ cask "cloudmounter" do
     "~/Library/Group Containers/*.com.eltima.cloudmounter",
     "~/Library/HTTPStorages/com.eltima.cloudmounter",
     "~/Library/Logs/CloudMounter.log",
+    "~/Library/Preferences/com.eltima.activator.xml",
     "~/Library/Preferences/com.eltima.cloudmounter.plist",
   ]
 end
