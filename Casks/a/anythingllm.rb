44a6e4e6314b71cac97d048c29c1f9c3d6b3440d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 01:28:47 +0000

    anythingllm 1.15.0

--- Casks/a/anythingllm.rb
+++ Casks/a/anythingllm.rb
@@ -1,7 +1,7 @@
 cask "anythingllm" do
   arch arm: "-Silicon"
 
-  version "1.14.2"
+  version "1.15.0"
   sha256 :no_check
 
   url "https://cdn.anythingllm.com/latest/AnythingLLMDesktop#{arch}.dmg",
