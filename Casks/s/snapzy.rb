35a8f60e2d9f5189129d1c6859358c17552a29a9
singularitti <singularitti@outlook.com>
2026-06-27 01:16:19 -0500

    snapzy 1.25.2 (new cask)

---
+++ Casks/s/snapzy.rb
@@ -0,0 +1,22 @@
+cask "snapzy" do
+  version "1.25.2"
+  sha256 "49e7d3a74ed23e66eec1a11c42c28b6ef3f7c929e9130490fb4b3586c785b8e4"
+
+  url "https://github.com/duongductrong/Snapzy/releases/download/v#{version}/Snapzy-v#{version}.dmg",
+      verified: "github.com/duongductrong/Snapzy/"
+  name "Snapzy"
+  desc "Native screenshots, recording, annotation, and editing from the menu bar"
+  homepage "https://snapzy.app/"
+
+  depends_on macos: :ventura
+
+  app "Snapzy.app"
+
+  zap trash: [
+    "~/.config/snapzy",
+    "~/Library/Application Support/Snapzy",
+    "~/Library/HTTPStorages/com.trongduong.snapzy",
+    "~/Library/Logs/Snapzy",
+    "~/Library/Preferences/com.trongduong.snapzy.plist",
+  ]
+end
