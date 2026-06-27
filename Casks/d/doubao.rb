07d31c8ea86359cfc94897ad0c635c2172787475
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 07:45:53 +0000

    doubao 2.15.11

--- Casks/d/doubao.rb
+++ Casks/d/doubao.rb
@@ -1,6 +1,6 @@
 cask "doubao" do
-  version "2.15.9"
-  sha256 "948130ea7f3933230b858bdfa203a9eb9d72ea306032ea110eeb149badfadb42"
+  version "2.15.11"
+  sha256 "6756da5dff8e62a391d1202d9b9f69c06a73126c3b738e0799f80ea436aa7365"
 
   url "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao_pc/#{version}/Doubao_universal_#{version}.dmg"
   name "doubao"
