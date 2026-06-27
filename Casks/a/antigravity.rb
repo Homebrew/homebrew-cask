c1eda76b3409ac0705fc7ed79d3c85de53765ade
Sakutaro Shina <companyemailetc@gmail.com>
2026-06-28 03:34:55 +0900

    antigravity: update livecheck

--- Casks/a/antigravity.rb
+++ Casks/a/antigravity.rb
@@ -2,9 +2,9 @@ cask "antigravity" do
   arch arm: "arm", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "2.1.4,6481382726303744"
-  sha256 arm:   "e9ebd1534944f563e58b84cf67f2225938031c6587f01cc3a3ddfce754016fed",
-         intel: "e043c0529301ce3e132faf2237974c0286c60632bf278d6279ae012204926dc5"
+  version "2.2.1,5287492581195776"
+  sha256 arm:   "cb1955fdaa58ff851bf2f75078c318b90304d0a643eecb836aeb8783e2e89b12",
+         intel: "a7540a2ce23397cdbba634c704bd0982a665643b5078797413a615e4f2b0b858"
 
   url "https://storage.googleapis.com/antigravity-public/antigravity-hub/#{version.csv.first}-#{version.csv.second}/darwin-#{arch}/Antigravity.dmg",
       verified: "storage.googleapis.com/antigravity-public/antigravity-hub/"
@@ -14,7 +14,7 @@ cask "antigravity" do
 
   livecheck do
     url "https://antigravity-hub-auto-updater-974169037036.us-central1.run.app/manifest/latest-#{livecheck_arch}-mac.yml?noCache=#{Time.now.to_i}"
-    regex(%r{/v?(\d+(?:\.\d+)+)(?:-(\d+)?)/darwin-#{arch}/Antigravity\.dmg}i)
+    regex(%r{/v?(\d+(?:\.\d+)+)(?:-(\d+)?)/darwin-#{arch}/Antigravity\.(?:dmg|zip)}i)
     strategy :electron_builder do |yaml, regex|
       yaml["files"]&.map do |item|
         match = item["url"]&.match(regex)
