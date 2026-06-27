bd180e6e010f3248828811de882135c829d2cff3
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-06-27 20:21:20 +0800

    Update sxitch.rb

--- Casks/s/sxitch.rb
+++ Casks/s/sxitch.rb
@@ -1,21 +1,20 @@
 cask "sxitch" do
-  version "1.0.7-alpha"
-  sha256 "b015eba192c950bbf739b2e20dbaa6e58a0b7762550bfbdec6033130d1e9b626"
+  version :latest
+  sha256 :no_check
 
-  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch-universal-macos.app.zip",
-      verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
+  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip",
+    verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev"
   name "Sxitch"
-  desc "Tree based app switcher"
+  desc "Tree-based app switcher"
   homepage "https://sxitch.app/"
 
-  livecheck do
-    url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/version.txt"
-    regex(/^v?(\d+(?:\.\d+)+[\w-]*)$/i)
-  end
+  depends_on macos: :sequoia
 
-  depends_on macos: :monterey
+  app "Sxitch.app"
 
-  app "target/release/macos/Sxitch.app"
-
-  zap trash: "~/Library/Application Support/Sxitch"
+  zap trash: [
+    "~/Library/Application Support/Sxitch",
+    "~/Library/Caches/app.sxitch",
+    "~/Library/Preferences/app.sxitch.plist",
+  ]
 end
