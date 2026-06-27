dffc20abb104f3597446c0a950cda9d9eb3f9d0a
yiguo <yiguo@example.com>
2026-06-27 22:49:48 +0800

    Add onexray

---
+++ Casks/o/onexray.rb
@@ -0,0 +1,19 @@
+cask "onexray" do
+  version "26.6.3"
+  sha256 "09e10e0d33bf864f70e9e359bd3bbb9c6e9f110061ea2ad355ed02266510a05f"
+
+  url "https://github.com/OneXray/OneXray/releases/download/v#{version}/OneXray-macos-universal.zip",
+      verified: "github.com/OneXray/OneXray/"
+  name "OneXray"
+  desc "Cross-platform Xray-core client"
+  homepage "https://onexray.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :monterey
+
+  app "OneXraySE.app"
+end
