13c7f697702bc7db3ecfc9864d920fb741db7f97
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-06-27 14:40:53 +0200

    terax: add Linux AppImage support

--- Casks/t/terax.rb
+++ Casks/t/terax.rb
@@ -2,10 +2,11 @@ cask "terax" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.8.2"
-  sha256 arm:   "a25cc6d2e2dd958bccbbb83045d31cda55780af2f617850a6df3c59e077774c4",
-         intel: "d80372ab32d6782e6192ffd9bdafd3756efc44ad01391d8d59b60c2c142688bb"
 
-  url "https://github.com/crynta/terax-ai/releases/download/v#{version}/Terax_#{version}_#{arch}.dmg",
+  artifact = on_system_conditional linux: "Terax_#{version}_amd64.AppImage",
+                                   macos: "Terax_#{version}_#{arch}.dmg"
+
+  url "https://github.com/crynta/terax-ai/releases/download/v#{version}/#{artifact}",
       verified: "github.com/crynta/terax-ai/"
   name "Terax"
   desc "Terminal-first AI-native developer workspace"
@@ -17,14 +18,28 @@ cask "terax" do
   end
 
   auto_updates true
-  depends_on macos: :ventura
 
-  app "Terax.app"
+  on_macos do
+    sha256 arm:   "a25cc6d2e2dd958bccbbb83045d31cda55780af2f617850a6df3c59e077774c4",
+           intel: "d80372ab32d6782e6192ffd9bdafd3756efc44ad01391d8d59b60c2c142688bb"
 
-  zap trash: [
-    "~/Library/Application Support/app.crynta.terax",
-    "~/Library/Caches/app.crynta.terax",
-    "~/Library/Logs/app.crynta.terax",
-    "~/Library/WebKit/app.crynta.terax",
-  ]
+    depends_on macos: :ventura
+
+    app "Terax.app"
+
+    zap trash: [
+      "~/Library/Application Support/app.crynta.terax",
+      "~/Library/Caches/app.crynta.terax",
+      "~/Library/Logs/app.crynta.terax",
+      "~/Library/WebKit/app.crynta.terax",
+    ]
+  end
+
+  on_linux do
+    sha256 "a6e9ea9e26bdd47ce76563396c31386a85658bafc88206e30dca58b367de85b7"
+
+    depends_on arch: :x86_64
+
+    app_image artifact, target: "Terax.AppImage"
+  end
 end
