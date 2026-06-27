f881e70bf15e7c8c0c95044ede1d7c2f5a1d211b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 19:22:20 +0000

    lumen 1.3.1

--- Casks/l/lumen.rb
+++ Casks/l/lumen.rb
@@ -1,6 +1,6 @@
 cask "lumen" do
-  version "1.3.0"
-  sha256 "354cc6f83728a4bb4d2d469195afe7abcc5160ea1ca915ec84aee72c7907cd52"
+  version "1.3.1"
+  sha256 "3e22d1a8c646c414f7b99cb880730123ad0a5c66c2f2774c060e0c22cff5af0b"
 
   url "https://github.com/anishathalye/lumen/releases/download/v#{version}/Lumen.zip"
   name "Lumen"
