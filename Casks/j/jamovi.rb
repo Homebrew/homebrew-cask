45ed05dbc0e2c00944c1afca82ba2976ec34e596
Sam Ford <1584702+samford@users.noreply.github.com>
2026-06-27 15:30:35 -0400

    jamovi 2.7.34.0

--- Casks/j/jamovi.rb
+++ Casks/j/jamovi.rb
@@ -1,9 +1,9 @@
 cask "jamovi" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.31.0"
-  sha256 arm:   "d4b4cdde56616eea9aa28198ac891727ff1adc5b37ce1730b82dfaa827a6f69e",
-         intel: "e3ae78d57de912dab5915352372b6ed797a1c2bc0ce1eb73a5c6aae44ad0de64"
+  version "2.7.34.0"
+  sha256 arm:   "f75c2035f17b111fc2b3d4d7a7659472451ae22ee7c01f3219d26b490983d80d",
+         intel: "a40773fd74626ad4eaf4994be1879bd9c1ee3cda1c55c4a681f725373a65c00f"
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos-#{arch}.dmg",
       referer: "https://www.jamovi.org/download.html"
