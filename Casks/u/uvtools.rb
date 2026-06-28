126e68e740505ce1a6b79d9ea3116bd03c582b59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-28 02:30:35 +0000

    uvtools 6.1.0

--- Casks/u/uvtools.rb
+++ Casks/u/uvtools.rb
@@ -1,9 +1,9 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.0.3"
-  sha256 arm:   "39028cec6cb998a56161bc4127e089f90d71265fda235e3e9ef4c84e53a38f3b",
-         intel: "2ffa29cf18fb88cf5b6fe646954de6c6c26fbd797a0483b67ccf35612865fcc3"
+  version "6.1.0"
+  sha256 arm:   "fe2920396d46ae4329e94e5e84e75660682c587770a0a6bdadc6c4d1e56ee174",
+         intel: "fbf46739fe4de0ec1c43e375f885b19720a6156559c7a27f740108995d9128bc"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
@@ -13,7 +13,7 @@ cask "uvtools" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
-  depends_on :macos
+  depends_on macos: :ventura
 
   app "UVtools.app"
 
