17cd2702cc1b01c9fa1d5ecb239aa15d6fc31239
Sam Ford <1584702+samford@users.noreply.github.com>
2026-06-28 15:49:22 -0400

    server-box 1.0.1450
    `server-box` is autobumped but the workflow failed to update to
    version 1.0.1450 because the dmg file name doesn't include a "v"
    before the version this time. This updates the version and `url`
    accordingly.

--- Casks/s/server-box.rb
+++ Casks/s/server-box.rb
@@ -1,8 +1,8 @@
 cask "server-box" do
-  version "1.0.1426"
-  sha256 "f8f997158e0631e72c7dab6b0493d365bf227a7fcedd3c51fd83db588eb691c2"
+  version "1.0.1450"
+  sha256 "496035764db99a314029f18df64ecfc6f332f0f8e0e5641794b03713c3994c84"
 
-  url "https://github.com/lollipopkit/flutter_server_box/releases/download/v#{version}/ServerBox-v#{version}.dmg"
+  url "https://github.com/lollipopkit/flutter_server_box/releases/download/v#{version}/ServerBox-#{version}.dmg"
   name "ServerBox"
   desc "App for monitoring server status with SSH terminal, SFTP, Container management"
   homepage "https://github.com/lollipopkit/flutter_server_box"
