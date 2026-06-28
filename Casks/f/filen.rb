bc21bc60a2e8c2da2c0003949ba2756164fb62f8
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-06-28 16:22:44 +0200

    filen: add Linux AppImage support

--- Casks/f/filen.rb
+++ Casks/f/filen.rb
@@ -1,11 +1,22 @@
 cask "filen" do
-  arch arm: "arm64", intel: "x64"
+  os macos: "mac", linux: "linux"
+
+  on_macos do
+    arch arm: "arm64", intel: "x64"
+  end
+  on_linux do
+    arch arm: "arm64", intel: "x86_64"
+  end
 
   version "3.0.47"
-  sha256 arm:   "041f30b361fb5b07f9039d380cf9f3e56b1180491a1ebf711cc5d2d81d5f741b",
-         intel: "405a75b94f26814562f568e5098735ce28bbe80317f47eef920b993bc6b4ef69"
+  sha256 arm:          "041f30b361fb5b07f9039d380cf9f3e56b1180491a1ebf711cc5d2d81d5f741b",
+         intel:        "405a75b94f26814562f568e5098735ce28bbe80317f47eef920b993bc6b4ef69",
+         x86_64_linux: "91e683e4f52392816b1530ae6a9e03be662ae57dd38e7abe9e3b622e0019f56f",
+         arm64_linux:  "e28d9fbe1458f7050647bf6117ac8182d9bac851239e5f49fb241d74597e6ac7"
 
-  url "https://cdn.filen.io/@filen/desktop/release/v#{version}/Filen_mac_#{arch}.dmg"
+  url_end = on_system_conditional macos: ".dmg", linux: ".AppImage"
+
+  url "https://cdn.filen.io/@filen/desktop/release/v#{version}/Filen_#{os}_#{arch}#{url_end}"
   name "Filen"
   desc "Desktop client for Filen.io"
   homepage "https://filen.io/"
@@ -15,20 +26,26 @@ cask "filen" do
     strategy :electron_builder
   end
 
-  auto_updates true
-  depends_on macos: :big_sur
+  on_macos do
+    depends_on macos: :big_sur
+    auto_updates true
 
-  app "Filen.app"
+    app "Filen.app"
 
-  zap trash: [
-    "~/Library/Application Support/@filen",
-    "~/Library/Application Support/filen-desktop",
-    "~/Library/Caches/@filendesktop-updater",
-    "~/Library/Caches/io.filen.desktop",
-    "~/Library/Caches/io.filen.desktop.ShipIt",
-    "~/Library/HTTPStorages/io.filen.desktop",
-    "~/Library/Logs/filen-desktop",
-    "~/Library/Preferences/io.filen.desktop.plist",
-    "~/Library/Saved Application State/io.filen.desktop.savedState",
-  ]
+    zap trash: [
+      "~/Library/Application Support/@filen",
+      "~/Library/Application Support/filen-desktop",
+      "~/Library/Caches/@filendesktop-updater",
+      "~/Library/Caches/io.filen.desktop",
+      "~/Library/Caches/io.filen.desktop.ShipIt",
+      "~/Library/HTTPStorages/io.filen.desktop",
+      "~/Library/Logs/filen-desktop",
+      "~/Library/Preferences/io.filen.desktop.plist",
+      "~/Library/Saved Application State/io.filen.desktop.savedState",
+    ]
+  end
+
+  on_linux do
+    app_image "Filen_#{os}_#{arch}.AppImage", target: "Filen.AppImage"
+  end
 end
