59ab4f7e818f0cf4a68823b8a9eb06bbd246d0f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-28 16:26:51 +0000

    maa 6.13.0

--- Casks/m/maa.rb
+++ Casks/m/maa.rb
@@ -1,6 +1,6 @@
 cask "maa" do
-  version "6.12.2"
-  sha256 "0d6366942c90d49d80f31ac39eed9c7549353401be61c52a489967343b113b73"
+  version "6.13.0"
+  sha256 "f670bb5c75690a994d7b977ee4f2f86d581fe70444920504b62fe207d9e51a44"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg"
   name "MAA"
