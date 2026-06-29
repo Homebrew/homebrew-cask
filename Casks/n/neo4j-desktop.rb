4a462f11d0046c0301b2234e7f7b5d95f71119ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 19:55:38 +0000

    neo4j-desktop 2.2.0

--- Casks/n/neo4j-desktop.rb
+++ Casks/n/neo4j-desktop.rb
@@ -1,7 +1,7 @@
 cask "neo4j-desktop" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "2.1.4"
-  sha256 "d76ab5358546998909ad475b21c75a852913725994699709d7d839fd6a5a66ef"
+  version "2.2.0"
+  sha256 "3c30cf3c4309b796c6e61978b398cc6a071e530f9b6c2ccbfb66480a6c749f3d"
 
   url "https://dist.neo4j.org/neo4j-desktop-#{version.major}/mac/neo4j-desktop-#{version}-universal.dmg",
       verified: "dist.neo4j.org/"
