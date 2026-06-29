1bd6041498d192ff0e87b70018a0fccc3a83046f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 09:10:54 +0000

    shichizip-zs 0.2.6

--- Casks/s/shichizip-zs.rb
+++ Casks/s/shichizip-zs.rb
@@ -1,9 +1,9 @@
 cask "shichizip-zs" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.2.5"
-  sha256 arm:   "82a21e58de7cfff6beaf897718c5bf9bd0bf80c621692f91d30630dd5d13187a",
-         intel: "64fe0a75e4ae3042cf3de9ecdf4cac987de74a5f4708b2af1710497fa33d42ff"
+  version "0.2.6"
+  sha256 arm:   "ec043f02a647a56abe853e059ed8ff201b0abb0c48d3030fd9dc0bf2fd72d242",
+         intel: "0e575a0c4543c7e20de7701e4ea4ac259c97fc343c48c58b25b9dba85fc9384e"
 
   url "https://github.com/idawnlight/ShichiZip/releases/download/v#{version}/ShichiZipZS-v#{version}-#{arch}.zip"
   name "ShichiZip ZS"
