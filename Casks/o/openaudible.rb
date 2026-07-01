b196705feaecc6d68d627cace6e274c95d85ae62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 05:43:34 +0000

    openaudible 4.8.7

--- Casks/o/openaudible.rb
+++ Casks/o/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "4.8.6"
-  sha256 "c707c98a1456926a6772e1a937d52cd5eb08970011b53ce4f078c6c64e2c3b33"
+  version "4.8.7"
+  sha256 "0fd35949777c55b01d50c412b97dd11e90d9719ace79ec3ae221742a1c2c8522"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
