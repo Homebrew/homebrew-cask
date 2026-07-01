5fe9f5c61c79a930cd2d58b527a25545c43b8fc9
GregoryMc86 <44270263+GregoryMc86@users.noreply.github.com>
2026-07-01 21:04:11 +0200

    gmacFTP 0.0.14: rename public bundle to gmacFTP.app + fix zap paths

--- Casks/g/gmacftp.rb
+++ Casks/g/gmacftp.rb
@@ -1,15 +1,15 @@
 cask "gmacftp" do
-  version "0.0.13"
-  sha256 "10e3f55dfe237e3962571ff6e72967dacfd2cdb6fb84e72250a160f98e5276a3"
+  version "0.0.14"
+  sha256 "a386fa85fbd3afdbeb3bd9f9c65bdf3524d097e76da1a8629996684d64281f5d"
 
   url "https://github.com/GMAC-pl/gmacFTP/releases/download/v#{version}/gmacFTP-#{version}.dmg",
       verified: "github.com/GMAC-pl/gmacFTP/"
   name "gmacFTP"
-  desc "Dual-pane FTP/FTPS/SFTP client for macOS, written in Rust"
+  desc "Dual-pane FTP/FTPS/SFTP client"
   homepage "https://github.com/GMAC-pl/gmacFTP"
 
   livecheck do
-    url :url_latest
+    url :url
     strategy :github_latest
   end
 
@@ -18,8 +18,8 @@ cask "gmacftp" do
   app "gmacFTP.app"
 
   zap trash: [
-    "~/Library/Caches/com.gmac.gmacftp",
-    "~/Library/Preferences/com.gmac.gmacftp.plist",
-    "~/Library/Saved Application State/com.gmac.gmacftp.savedState",
+    "~/Library/Application Support/mackftp/client",
+    "~/Library/Mobile Documents/com~apple~CloudDocs/gmacFTP",
+    "~/Library/Saved Application State/app.mackftp.client.savedState",
   ]
 end
