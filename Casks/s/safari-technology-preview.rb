a4d5147328d32fc8a829bb4ba3aef6fa1e4a88b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 22:51:45 +0000

    safari-technology-preview 247

--- Casks/s/safari-technology-preview.rb
+++ Casks/s/safari-technology-preview.rb
@@ -1,25 +1,31 @@
 cask "safari-technology-preview" do
-  # TODO: When possible, update this to remove the `on_sequoia :or_older` block,
-  # update the Tahoe block to `on_tahoe :or_older`, add an
-  # `on_golden_gate :or_newer` block, and use `depends_on macos: :tahoe`.
-  on_sequoia :or_older do
-    version "244,122-88534-20260521-6dc38dc5-09a4-4572-985a-13bccab2baca"
-    sha256 "31967983d9557954828224869f2bc446395d3f33f383202f89799344a5d2ed2b"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  # when adjusting the on_{os} scoping, also update the livecheck regex
-  on_tahoe :or_newer do
-    version "246,140-18555-20260617-903317e6-560d-4370-9252-0be4d87523ea"
-    sha256 "bf34c0da8804ecfad524df1f0d271a7201cba138bb8130c9225aa0347a5b0c64"
+  on_tahoe :or_older do
+    version "247,140-34382-20260630-9f5e899d-3f85-450d-ab63-4f657b833cf9"
+    sha256 "c667953f7b84226cf7985db954b6743c90b71e4b6046bd7ebda494b841ebf50d"
 
     livecheck do
       url :homepage
       regex(%r{
         href=.*?/([^/]+)/Safari(?:%20|\+)?Technology(?:%20|\+)?Preview\.dmg
         .*?macOS(?:\s|&nbsp;)*26[\s.<]
+      }ix)
+      strategy :page_match do |page, regex|
+        release = page[%r{>\s*Release\s*</p>\s*<p[^>]*>\s*(\d+)[^<]*<}i, 1]
+        id = page[regex, 1]
+        "#{release},#{id}"
+      end
+    end
+  end
+  # when adjusting the on_{os} scoping, also update the livecheck regex
+  on_golden_gate :or_newer do
+    version "247,140-34366-20260630-87ebbe6a-e3ef-4af1-9d0e-848f2ab2ec7a"
+    sha256 "65b28478653fd40a51c6488814793e785e07cc7632d7ddc337e6d1a1f4ec9e2b"
+
+    livecheck do
+      url :homepage
+      regex(%r{
+        href=.*?/([^/]+)/Safari(?:%20|\+)?Technology(?:%20|\+)?Preview\.dmg
+        .*?macOS(?:\s|&nbsp;)*27[\s.<]
       }ix)
       strategy :page_match do |page, regex|
         release = page[%r{>\s*Release\s*</p>\s*<p[^>]*>\s*(\d+)[^<]*<}i, 1]
@@ -35,7 +41,7 @@ cask "safari-technology-preview" do
   homepage "https://developer.apple.com/safari/resources/"
 
   auto_updates true
-  depends_on macos: :sequoia
+  depends_on macos: :tahoe
 
   pkg "Safari Technology Preview.pkg"
 
