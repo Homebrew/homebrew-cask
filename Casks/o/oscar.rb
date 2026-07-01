d98633e3ca271b686d5dd4670e98955d7842a6b9
Wasith "BaiPhai" Theerapattrathamrong <contacts@wasith.me>
2026-07-01 21:59:01 +0700

    chore: update socar to 2.0.1 form 2.0.0

--- Casks/o/oscar.rb
+++ Casks/o/oscar.rb
@@ -1,18 +1,24 @@
 cask "oscar" do
   arch arm: "ARM", intel: "Intel"
 
-  version "2.0.0"
-  sha256 arm:   "26ce5039ee73a10364a09d92ccfbc24720d75ffb57e40ea829129e5817638813",
-         intel: "0a08b55ca66c272b127d9dd1399fd3a18b61abae5d876554d3ee0632f5614e34"
+  version "2.0.1"
+  sha256 arm:   "c52994d58254242c58a9d532d03753bb597575272c1a6fe3129d79f686c05f86",
+         intel: "16d518bb04e32cf58d748cb4f05e5c703a36f3da654d70b77896cebd1683c5da"
 
-  url "https://www.sleepfiles.com/OSCAR/#{version.major_minor}/OSCAR-#{version}-#{arch}.dmg"
+  on_arm do
+    url "https://www.sleepfiles.com/OSCAR/#{version}/OSCAR-#{version}-#{arch}.dmg"
+  end
+  on_intel do
+    url "https://www.sleepfiles.com/OSCAR/#{version}/OSCAR#{version.major_minor.no_dots}-#{version}-#{arch}.dmg"
+  end
+
   name "OSCAR"
   desc "CPAP Analysis Reporter"
   homepage "https://www.sleepfiles.com/OSCAR/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/OSCAR[._-]v?(\d+(?:\.\d+)+)(?:[._-]#{arch})?\.dmg}i)
+    regex(%r{href=.*?/OSCAR.*?v?(\d+(?:\.\d+)+)(?:[._-]#{arch})?\.dmg}i)
   end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
