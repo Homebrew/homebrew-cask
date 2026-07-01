a228ea42682dd4256d21a81a424a558853871143
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 05:11:53 +0000

    dbx 0.5.42

--- Casks/d/dbx.rb
+++ Casks/d/dbx.rb
@@ -6,11 +6,11 @@ cask "dbx" do
     arch arm: "aarch64", intel: "amd64"
   end
 
-  version "0.5.41"
-  sha256 arm:          "5ab6879d9f764dd6a05320d1d5fe4474086fa487a9a4d1447ea4a652ae57d86e",
-         intel:        "b912b0c50d526c714f670b9168de74312816c19d2834df515093a0522bc5f94b",
-         arm64_linux:  "1de0ce73958d744b11b7346a3826780a1ece6ba44a027a22fbd9d3d7991492ad",
-         x86_64_linux: "8d53f763b8c7431c538c255b4be8c6bff1ecc689f026719dacb2440918e28d50"
+  version "0.5.42"
+  sha256 arm:          "009ee3106c4a7395eaa167852f2d973861c2acb9c8fdb0cf98a129adde576faf",
+         intel:        "44f8525f27c46e96933ecc95ac5dfe4640832d1e3a9bc9bee6be1d7b0e6dffc7",
+         arm64_linux:  "7d8c0aea63c75fa31f619c439b7b256bbabe8f7d40baf2a0e5eaa85385e9bb4a",
+         x86_64_linux: "6fc02b276e7e74217fd6391c160b94164e3244f557dcdcf9fde74a8ffaa5ffbf"
 
   url_end = on_system_conditional macos: ".dmg", linux: ".AppImage"
 
