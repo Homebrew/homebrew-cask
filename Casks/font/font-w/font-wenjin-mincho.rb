97eff590535c62e87bcd6d7fce57902ae772a426
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 13:34:44 +0000

    font-wenjin-mincho 2.012

--- Casks/font/font-w/font-wenjin-mincho.rb
+++ Casks/font/font-w/font-wenjin-mincho.rb
@@ -1,6 +1,6 @@
 cask "font-wenjin-mincho" do
-  version "2.011"
-  sha256 "78e8063fe22794b26cecbd4b1c9a6924140989e032f3045a233f11ad9e5cbc0f"
+  version "2.012"
+  sha256 "92cf0a7583042798c3ef3302892096e97f76dfc970053bbdc675971dfa1b7b50"
 
   url "https://github.com/takushun-wu/WenJinMincho/releases/download/v#{version}/WenJinMincho-OTC.7z"
   name "WenJin Mincho"
