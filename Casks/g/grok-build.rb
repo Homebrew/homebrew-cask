e24fc32fcfb7e332a3c9b42f1b1f6e85769fb500
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 05:09:44 +0000

    grok-build 0.2.82

--- Casks/g/grok-build.rb
+++ Casks/g/grok-build.rb
@@ -2,11 +2,11 @@ cask "grok-build" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "0.2.81"
-  sha256 arm:          "2bc9b8e5a15eeac6475f779263f14595590c8e523a611700a9eaa56406e546b4",
-         intel:        "4da76724d03944cb3f74a738a50869dfa785e6166b52f85a288803daaaeab649",
-         arm64_linux:  "a252d36b6a891184f60707e99246c19c9d34b3b8c30b6126bbd1f78a740b3740",
-         x86_64_linux: "8c76802cf17c2aed23252a599f8d81178c30ffddeabfef3be50abee3d52b5672"
+  version "0.2.82"
+  sha256 arm:          "1e4c8a78b050d93563e20f622a41a34f8e96b80575b1c958ad3f095316fe6189",
+         intel:        "c028866cfb79d301ff85ed9526176a143dab5225339d92f41e3f21528e3eb396",
+         arm64_linux:  "b4727c600786d64a3efb19f73a6c8b951b075b35d6fa7ca9519a089411507a58",
+         x86_64_linux: "c74faf275141e16548802418ebf10763947ebaa00f2427bb80f022aba63688fe"
 
   url "https://x.ai/cli/grok-#{version}-#{os}-#{arch}"
   name "Grok Build"
