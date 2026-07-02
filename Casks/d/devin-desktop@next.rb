1e31f40a8e9d24b0c27215a69e46119226ac3ff3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 01:35:32 +0000

    devin-desktop@next 3.4.1016+next.bb945563e5

--- Casks/d/devin-desktop@next.rb
+++ Casks/d/devin-desktop@next.rb
@@ -1,9 +1,9 @@
 cask "devin-desktop@next" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.3.1018+next.16737566f5,16737566f57f3b53bde136375fe0544eca12fac4"
-  sha256 arm:   "e77b7529ffa9cdf6813e3e75e006f1b4be8cff6a580d755738442638efad8dc6",
-         intel: "9f5d27a2cd424cdf01280daff12f5348e5e9ca9d94622a7214cb996015f6bea6"
+  version "3.4.1016+next.bb945563e5,bb945563e5a1ab2f12e2de27fae2daed9dcba18c"
+  sha256 arm:   "17e5d3f99bbcf2d26a8b5300f55bf66cc7a62d49c23ee2ac130ccf83fbed3b64",
+         intel: "3eaca5458792af10580f26471b4b35a65b8dd8f7d272dea67c94be092dc44afb"
 
   url "https://windsurf-stable.codeiumdata.com/darwin-#{arch}-dmg/next/#{version.csv.second}/Devin-darwin-#{arch}-#{version.csv.first}.dmg",
       verified: "windsurf-stable.codeiumdata.com/"
