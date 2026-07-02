46e951db5cb3e4c6467b10428880d79e46e4a891
Kam Lane <22482081+kamlane@users.noreply.github.com>
2026-07-02 00:14:11 -0500

    Fixing brew style

--- Casks/t/toolhive-studio.rb
+++ Casks/t/toolhive-studio.rb
@@ -1,7 +1,7 @@
 cask "toolhive-studio" do
-  version "0.36.7"
-
   arch arm: "arm64"
+
+  version "0.36.7"
   sha256 arm: "7b2818824985b0a1481caaa6857318611f11dde918ea80da771d87a75e530628"
 
   url "https://github.com/stacklok/toolhive-studio/releases/download/v#{version}/ToolHive-#{arch}.dmg"
@@ -9,19 +9,19 @@ cask "toolhive-studio" do
   desc "Desktop application to install, manage, and run MCP servers"
   homepage "https://github.com/stacklok/toolhive-studio"
 
-  depends_on arch: :arm64
-  depends_on :macos
-
   livecheck do
     url :url
     strategy :github_latest
   end
 
+  depends_on arch: :arm64
+  depends_on macos: :monterey
+
   app "ToolHive.app"
 
   zap trash: [
     "~/Library/Application Support/ToolHive",
-    "~/Library/Preferences/dev.toolhive.studio.plist",
     "~/Library/Logs/ToolHive",
+    "~/Library/Preferences/dev.toolhive.studio.plist",
   ]
 end
