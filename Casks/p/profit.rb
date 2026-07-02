e0824e03bfc0a54bc068a1ffafa0ab89aac68ef6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 17:12:27 +0000

    profit 1.0.53

--- Casks/p/profit.rb
+++ Casks/p/profit.rb
@@ -1,7 +1,7 @@
 cask "profit" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.52"
+  version "1.0.53"
   sha256 :no_check
 
   url "https://downloadserver-cdn.nelogica.com.br/content/mac/stable/#{arch}/profit.dmg",
