4121e6994e7a56f9f1d84ee7bb8270d9eaa4eb43
Kam Lane <22482081+kamlane@users.noreply.github.com>
2026-07-02 10:54:42 -0500

    Update Casks/t/toolhive-studio.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/t/toolhive-studio.rb
+++ Casks/t/toolhive-studio.rb
@@ -1,8 +1,9 @@
 cask "toolhive-studio" do
-  arch arm: "arm64"
+  arch arm: "arm64", intel: "x64"
 
   version "0.36.7"
-  sha256 "7b2818824985b0a1481caaa6857318611f11dde918ea80da771d87a75e530628"
+  sha256 arm:   "7b2818824985b0a1481caaa6857318611f11dde918ea80da771d87a75e530628",
+         intel: "c1790e02379f93918c1970fe97be92f7116441652ab6932edcbe4b48f6fff5d2"
 
   url "https://github.com/stacklok/toolhive-studio/releases/download/v#{version}/ToolHive-#{arch}.dmg"
   name "ToolHive"
