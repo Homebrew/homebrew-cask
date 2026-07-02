fb365a66a3ce83443749101e3eb42b9f03f0a2d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 17:33:07 +0000

    webstorm 2026.1.4

--- Casks/w/webstorm.rb
+++ Casks/w/webstorm.rb
@@ -1,9 +1,9 @@
 cask "webstorm" do
   arch arm: "-aarch64"
 
-  version "2026.1.3,261.25134.101"
-  sha256 arm:   "d4dd51b4dd502efb89d502fc8db3794dd6ce1c01d237ce16f5710ad8f10f8a32",
-         intel: "2947104566140fffb81c75a7906baaa25b6a2f66a7d164245047d472db708c22"
+  version "2026.1.4,261.26222.58"
+  sha256 arm:   "6587a7e84c341986c1026b860830023c1b3283167ab13efcee8ea0a85f43273c",
+         intel: "486768e9660c09c081b99523bd445c3136c952a859e0ccc59522e0e998ebf382"
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.first}#{arch}.dmg"
   name "WebStorm"
