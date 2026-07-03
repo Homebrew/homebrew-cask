b94be58167fb11652500c44bf3bad070ea4db522
lirik <kirill.isachenko@gmail.com>
2026-07-03 12:11:33 +0700

    zush 3.6.1 (new cask)

---
+++ Casks/z/zush.rb
@@ -0,0 +1,35 @@
+cask "zush" do
+  version "3.6.1"
+  sha256 "86cc2a07addab162c47b5f9ac3a7104ceeac167bbefb4bef5ccdc0dcd4d99299"
+
+  url "https://zushapp.com/releases/Zush-#{version}.dmg"
+  name "Zush"
+  desc "AI-powered file renamer and organizer"
+  homepage "https://zushapp.com/"
+
+  livecheck do
+    url "https://zushapp.com/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: :sequoia
+
+  app "Zush.app"
+
+  uninstall quit: "com.lirik.Zush"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.lirik.Zush",
+    "~/Library/Application Support/com.lirik.Zush",
+    "~/Library/Application Support/Zush",
+    "~/Library/Caches/com.lirik.Zush",
+    "~/Library/Caches/Zush",
+    "~/Library/Containers/com.lirik.Zush",
+    "~/Library/HTTPStorages/com.lirik.Zush",
+    "~/Library/Preferences/ByHost/com.lirik.Zush.*.plist",
+    "~/Library/Preferences/com.lirik.Zush.plist",
+    "~/Library/Saved Application State/com.lirik.Zush.savedState",
+    "~/Library/WebKit/com.lirik.Zush",
+  ]
+end
