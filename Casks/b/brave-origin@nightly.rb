7a8793415307e6906bc54143aafff8b8c96db98e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 10:30:23 +0000

    brave-origin@nightly 1.94.33.0 (intel only)

--- Casks/b/brave-origin@nightly.rb
+++ Casks/b/brave-origin@nightly.rb
@@ -3,13 +3,13 @@ cask "brave-origin@nightly" do
   folder_arch = on_arch_conditional arm: "-arm64"
 
   sha256 arm:   "814e336f899a4f729dd16ba33a87e3e16b693ec5735dc19b51a03d0dc2f1af8d",
-         intel: "3f8ea56338acc6d7cfb483dec2c606c2b82eb96b6c5c613427ee5458f46259f0"
+         intel: "24963b3abeff2ee828a7b9165a86e4bdb3d1469c13e06acf37f42d15ee729ab5"
 
   on_arm do
     version "1.94.27.0"
   end
   on_intel do
-    version "1.94.30.0"
+    version "1.94.33.0"
   end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Origin/nightly#{folder_arch}/#{version.major_minor_patch.sub(".", "")}/Brave-Origin-Nightly-#{arch}.dmg",
