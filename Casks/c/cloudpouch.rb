91313e279d1bcc1a0991f4a16cad01d3fa421f88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 19:00:28 +0000

    cloudpouch 1.49.0

--- Casks/c/cloudpouch.rb
+++ Casks/c/cloudpouch.rb
@@ -1,6 +1,6 @@
 cask "cloudpouch" do
-  version "1.48.2"
-  sha256 "879f516c1dd78e1c0a067407172536e2138bfef780e294aae203038c43da90de"
+  version "1.49.0"
+  sha256 "407cbb258fedcc170c98175e6e83e23f9674855c9dbf59eebb6378140b713948"
 
   url "https://github.com/CloudPouch/CloudPouch.dev/releases/download/v#{version}/CloudPouch-#{version}-universal-mac.zip",
       verified: "github.com/CloudPouch/CloudPouch.dev/"
@@ -14,7 +14,7 @@ cask "cloudpouch" do
   end
 
   auto_updates true
-  depends_on :macos
+  depends_on macos: :monterey
 
   app "CloudPouch.app"
 
