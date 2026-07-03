881dc8260ffb4bfc08f8ab7065fc7230e3d7b321
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 10:59:07 +0000

    k8studio 3.1.10

--- Casks/k/k8studio.rb
+++ Casks/k/k8studio.rb
@@ -1,9 +1,9 @@
 cask "k8studio" do
   arch arm: "-arm64"
 
-  version "3.1.9"
-  sha256 arm:   "fccc6792b265b244563a54b52915a0707d010d4ae44c60ccceee10928ce9c931",
-         intel: "a33b8ac55972bf456d722a4cda94b829f182164b5751e181f91651bab7639490"
+  version "3.1.10"
+  sha256 arm:   "0c86ccfbea4e2c53ed0e93c1f7d97e73b79908dfe931c732740526eb2a27e12e",
+         intel: "667ba95e7c887e13ea5e69e9bcf35b2f48a0a70359dd1c08f3615de7ea425410"
 
   url "https://releases.k8studio.io/K8Studio-#{version}#{arch}.dmg"
   name "K8studio"
