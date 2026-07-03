da32b7d5c92a85b931d6ed18528a09734ae4b013
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 14:03:20 +0000

    serial-studio 4.0.2

--- Casks/s/serial-studio.rb
+++ Casks/s/serial-studio.rb
@@ -1,6 +1,6 @@
 cask "serial-studio" do
-  version "4.0.1"
-  sha256 "2dad21f5823e7d36fa628c334f54469b370a0e8b91d4f0a12e89c3fa9d5f9489"
+  version "4.0.2"
+  sha256 "fb462bdfc568ef9e6ec6bbb77bd126a46c80ca69880b205c53901d5495f64bc7"
 
   url "https://github.com/Serial-Studio/Serial-Studio/releases/download/v#{version}/Serial-Studio-Pro-#{version}-macOS.dmg",
       verified: "github.com/Serial-Studio/Serial-Studio/"
