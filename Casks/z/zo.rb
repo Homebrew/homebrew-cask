d8a06333cfa9b4d093ff00764d7afad32db85e08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 17:21:30 +0000

    zo 1.5.7

--- Casks/z/zo.rb
+++ Casks/z/zo.rb
@@ -1,6 +1,6 @@
 cask "zo" do
-  version "1.5.6"
-  sha256 "69f3a6055a4911d802b0a89f40d69f3a863937a46665b08b65ca73ae2cda77c1"
+  version "1.5.7"
+  sha256 "51d02153b0918c2b8e89e467d6650799bd63099249e8a650062ec73bec98db57"
 
   url "https://github.com/zocomputer/Zo/releases/download/v#{version}/Zo-#{version}-universal-mac.zip",
       verified: "github.com/zocomputer/Zo/"
