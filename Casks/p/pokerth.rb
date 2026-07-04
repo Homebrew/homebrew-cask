95b6d33d81b2e0bb42ce205fa453b3db14c5297f
Sam Ford <1584702+samford@users.noreply.github.com>
2026-07-04 13:17:12 -0400

    pokerth 2.1.0
    `pokerth` is autobumped but the workflow failed to update to version
    2.1.0 because the dmg file now includes the Widget and QML client
    combined. This updates the version and adjusts the `url` accordingly.
    
    Besides that, this adds a `livecheck` block that uses a SourceForge
    RSS feed URL that's restricted to the `/pokerth` directory where the
    release files are found. There are other directories in the project
    that we don't need/want to check, so this helps to ensure that release
    files aren't pushed out of the feed by unrelated files.

--- Casks/p/pokerth.rb
+++ Casks/p/pokerth.rb
@@ -1,12 +1,16 @@
 cask "pokerth" do
-  version "2.0.8"
-  sha256 "c35ba20313883caac2cce5be6910b3c05f3db5f284f509ff12340d7fc0da07c3"
+  version "2.1.0"
+  sha256 "9c2384aaf5a08b52ce2cc0f39a2843027e516a79cf0d2075fb7362810d4095db"
 
-  url "https://downloads.sourceforge.net/pokerth/PokerTH-Widget-#{version}.dmg",
+  url "https://downloads.sourceforge.net/pokerth/PokerTH-#{version}-Combined.dmg",
       verified: "downloads.sourceforge.net/pokerth/"
   name "PokerTH"
   desc "Free Texas hold'em poker"
   homepage "https://www.pokerth.net/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/pokerth/rss?path=/pokerth"
+  end
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
