6f6b91e5cffe6f77b29fde9ea9c1235f7576fc57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 07:47:28 +0000

    gpxsee 16.11

--- Casks/g/gpxsee.rb
+++ Casks/g/gpxsee.rb
@@ -1,6 +1,6 @@
 cask "gpxsee" do
-  version "16.10"
-  sha256 "ec7b359db94beb3adcc809e60b65fa361f888b17106f6a8413058ae3deab1530"
+  version "16.11"
+  sha256 "d249ed93e1844f4e72d4c99a1e898feaeea833fbd057318966c3d91c362f4691"
 
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
       verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
