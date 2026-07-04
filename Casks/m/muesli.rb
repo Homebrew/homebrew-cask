a2e62836d33977642ed409776b3b2cf66302fd09
pHequals7 <ce17b115@smail.iitm.ac.in>
2026-07-03 20:23:50 -0700

    muesli 0.7.1 (new cask)

---
+++ Casks/m/muesli.rb
@@ -0,0 +1,26 @@
+cask "muesli" do
+  version "0.7.1"
+  sha256 "b911e5e14874cf971590f8943eb55eba2bba839fc8a881544a8939eaafa1e2ee"
+
+  url "https://github.com/Muesli-HQ/muesli/releases/download/v#{version}/Muesli-#{version}.dmg",
+      verified: "github.com/Muesli-HQ/muesli/"
+  name "Muesli"
+  desc "Local-first dictation and meeting transcription"
+  homepage "https://muesli.works/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  app "Muesli.app"
+
+  zap trash: [
+    "~/.cache/muesli",
+    "~/Library/Application Support/Muesli",
+  ]
+end
