5aa7a14a61426d451d0080b365153b27f4d81e76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 01:14:56 +0000

    blackhole-64ch 0.7.1

--- Casks/b/blackhole-64ch.rb
+++ Casks/b/blackhole-64ch.rb
@@ -1,6 +1,6 @@
 cask "blackhole-64ch" do
-  version "0.7.0"
-  sha256 "441ec510570425442f70ea86c4ee60ec9cdf55f16807a2b92c536efe568aebbe"
+  version "0.7.1"
+  sha256 "586b614cffb8b84ac39b2b926b69d5416ab9a4d64c63217b71ce2f5f5d7aabd1"
 
   url "https://existential.audio/downloads/BlackHole64ch-#{version}.pkg"
   name "BlackHole 64ch"
