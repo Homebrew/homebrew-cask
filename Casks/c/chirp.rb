5489a87fd3ec88d4bbd2f9e9492e0903b00a617f
Wasith "BaiPhai" Theerapattrathamrong <contacts@wasith.me>
2026-07-05 11:00:46 +0700

    chore(CHIRP): update to 20260626 (latest version

--- Casks/c/chirp.rb
+++ Casks/c/chirp.rb
@@ -1,13 +1,13 @@
 cask "chirp" do
-  version "20250801"
-  sha256 "07809e2558bfd248e65c69f4b56a7c9a08b5d262730900b5f5020912ed282868"
+  version "20260626"
+  sha256 "3a7bc4bfd390895179dec265a42f30dab2813b2e88307d20accdd112cca5dc6d"
 
   url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
       verified: "archive.chirpmyradio.com/",
       referer:  "https://archive.chirpmyradio.com/chirp_next/next-#{version}/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
-  homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
+  homepage "https://chirpmyradio.com/projects/chirp/wiki/Home"
 
   disable! date: "2025-08-05", because: "cannot be reliably fetched due to Cloudflare protections"
 
