bbccc9e10182a638baa4acdd19a98a41aeb94f4f
Sam Ford <1584702+samford@users.noreply.github.com>
2026-07-06 10:35:47 -0400

    mkvtoolnix-app: update livecheck
    The `livecheck` block for `mkvtoolnix-app` checks the releases
    directory listing page and attempts to identify the highest version
    before fetching the directory listing for that but it's incorrectly
    returning 99.0-1 as newest instead of 100.0-1. This occurs because the
    `strategy` block uses `max` to identify the highest version but this
    doesn't work as intended because it's comparing version _strings_ and
    "99" is seen as higher than "100".
    
    This addresses the issue by updating the related logic to use
    `#max_by` and compare the versions as `Version` objects, which will
    correctly treat 100 as higher than 99. This is what we do in other
    `strategy` blocks (for this specific reason), so this was an
    oversight.
    
    Besides that, this updates the regex to better align with prevailing
    standards. This also enames `major_version` to `main_version` for the
    sake of clarity, as it's a string like "100.0" (i.e., the part of the
    version without the numeric suffix) and that's more than a major
    version (e.g., 100).

--- Casks/m/mkvtoolnix-app.rb
+++ Casks/m/mkvtoolnix-app.rb
@@ -12,12 +12,14 @@ cask "mkvtoolnix-app" do
 
   livecheck do
     url "https://mkvtoolnix.download/macos/releases/"
-    regex(%r{.*?/MKVToolNix[._-]v?(\d+(?:[.-]\d+)+)[._-]#{arch}\.dmg}i)
+    regex(/href=.*?MKVToolNix[._-]v?(\d+(?:[.-]\d+)+)[._-]#{arch}\.dmg/i)
     strategy :page_match do |page, regex|
-      major_version = page.scan(%r{href=".*?releases/(\d+(?:\.\d+)+)/}i)&.max&.first
-      next if major_version.blank?
+      main_version = page.scan(%r{href=.*?releases/v?(\d+(?:\.\d+)+)/}i)
+                         .max_by { |match| Version.new(match[0]) }
+                         &.first
+      next if main_version.blank?
 
-      version_directory = Homebrew::Livecheck::Strategy.page_content("https://mkvtoolnix.download/macos/releases/#{major_version}/")
+      version_directory = Homebrew::Livecheck::Strategy.page_content("https://mkvtoolnix.download/macos/releases/#{main_version}/")
       version_directory[:content]&.scan(regex)&.map { |match| match[0] }
     end
   end
