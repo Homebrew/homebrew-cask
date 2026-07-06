7e17d57a19a07c9de1b962c4c67163bc413fba4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-06 08:58:25 +0000

    speechify-voice-ai 3.7.0

--- Casks/s/speechify-voice-ai.rb
+++ Casks/s/speechify-voice-ai.rb
@@ -1,5 +1,5 @@
 cask "speechify-voice-ai" do
-  version "3.6.0"
+  version "3.7.0"
   sha256 :no_check
 
   url "https://storage.googleapis.com/speechifymobile.appspot.com/macAgentSparkle/SpeechifyVoiceAssistant.dmg",
