b6b2c6bab2086102c4a021a1c21d1117c71b712e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-06 13:57:34 +0000

    ente 1.7.26

--- Casks/e/ente.rb
+++ Casks/e/ente.rb
@@ -1,6 +1,6 @@
 cask "ente" do
-  version "1.7.25"
-  sha256 "3cdabd9a79a06d3fea31073c5b8e26756f579ae925faca88923273d37219fb65"
+  version "1.7.26"
+  sha256 "63a98b91807557d2e887b33c520c9a9610af7cee83af878d4ed21c739cf88f69"
 
   url "https://github.com/ente-io/photos-desktop/releases/download/v#{version}/ente-#{version}-universal.dmg",
       verified: "github.com/ente-io/photos-desktop/"
