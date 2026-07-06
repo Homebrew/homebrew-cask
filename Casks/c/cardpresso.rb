2575a250b75820f32eb9da3197f8bff51dd7186e
Sakutaro Shina <companyemailetc@gmail.com>
2026-07-06 18:50:14 +0900

    cardpresso: update livecheck

--- Casks/c/cardpresso.rb
+++ Casks/c/cardpresso.rb
@@ -1,15 +1,16 @@
 cask "cardpresso" do
   version "1.7.140"
-  sha256 "5e8ca104db9a16aa8ae446d9e882b74e9feaf07ff29da5418e526ec46e9045d1"
+  sha256 "fccb6c5de6de364136b49d84bbac9e337ab220a7e53846304cb85e6e81dcfc4c"
 
-  url "https://www.cardpresso.com/downloads/cardpresso_releases/for_mac_osx/cardPresso#{version}.dmg"
+  url "https://www.cardpressodownloads.com/downloads/cardpresso_releases/for_mac_osx/cardPresso#{version}.zip",
+      verified: "cardpressodownloads.com/downloads/cardpresso_releases/for_mac_osx/"
   name "cardPresso"
   desc "Card software tool for professional card production"
   homepage "https://www.cardpresso.com/"
 
   livecheck do
     url "https://www.cardpresso.com/cardpresso-software/latest-release/"
-    regex(/href=.*?cardPresso[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/href=.*?cardPresso[._-]?v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
   auto_updates true
