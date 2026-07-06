d07aff98ba5db1fc7b9a2875f6ab29388cc9ff9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-06 05:54:19 +0000

    slashy 1.3.47

--- Casks/s/slashy.rb
+++ Casks/s/slashy.rb
@@ -1,6 +1,6 @@
 cask "slashy" do
-  version "1.3.46"
-  sha256 "fd4f53d86a4f998c81d8d0538f2bec2959c7b14d63383e414d14b86665671cfe"
+  version "1.3.47"
+  sha256 "f2051f5460974f4ba1e5c0044a0a8fded655288a6ec1d6155aff39d96ba8f98c"
 
   url "https://dl.slashy.com/Slashy-#{version}-universal.dmg"
   name "Slashy"
