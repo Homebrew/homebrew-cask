c2f22603fa5f26b5a88c7ad02aa1c5e5cc63063a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-07 01:45:06 +0000

    mouseless 1.0.0

--- Casks/m/mouseless.rb
+++ Casks/m/mouseless.rb
@@ -1,6 +1,6 @@
 cask "mouseless" do
-  version "0.4.4"
-  sha256 "303ed86b35c9fb530d655c8f2f139d9f034ba421e6828e4ddaae2124a1281260"
+  version "1.0.0"
+  sha256 "6893aede575f47665bb37c127f0f1372e007ad7cad692c44c75e19803f08560b"
 
   url "https://github.com/croian/mouseless/releases/download/v#{version}/mouseless-installer_v#{version}.dmg",
       verified: "github.com/croian/mouseless/"
