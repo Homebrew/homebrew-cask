963f1484c6ac214f886b5a5afbb42f5dd4c93a25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 05:42:15 +0000

    toolhive-studio 0.37.0

--- Casks/t/toolhive-studio.rb
+++ Casks/t/toolhive-studio.rb
@@ -1,9 +1,9 @@
 cask "toolhive-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.36.7"
-  sha256 arm:   "7b2818824985b0a1481caaa6857318611f11dde918ea80da771d87a75e530628",
-         intel: "c1790e02379f93918c1970fe97be92f7116441652ab6932edcbe4b48f6fff5d2"
+  version "0.37.0"
+  sha256 arm:   "3b95caea82d9de8746eafbbe93d10a77ce0b5662c2de64f0de6c7b0cbd44b996",
+         intel: "f938b056a1ff536f31a41c52221fd817b49f326019a6a8d539b7d22f45484e6e"
 
   url "https://github.com/stacklok/toolhive-studio/releases/download/v#{version}/ToolHive-#{arch}.dmg"
   name "ToolHive"
