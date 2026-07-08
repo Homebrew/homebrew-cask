27627c39e77743b118ba2b4631634c311ff5b313
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 16:50:44 +0000

    mac-sai 1.15.2

--- Casks/m/mac-sai.rb
+++ Casks/m/mac-sai.rb
@@ -1,6 +1,6 @@
 cask "mac-sai" do
-  version "1.15.0"
-  sha256 "159d85764d7bd4b4e6dcb98a92ab3b2bd6b7d4e0e95d05283b91cb67b1dcbfc0"
+  version "1.15.2"
+  sha256 "04fd78eac0372e046c5febc662298e954ef1e29b99b1e01c32c7d5b3dd43069f"
 
   url "https://github.com/iliyami/MacSai/releases/download/v#{version}/MacSai-#{version}.dmg"
   name "Mac Sai"
