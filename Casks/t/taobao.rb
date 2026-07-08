35a256cf6a6a61811e6635002c01229b9f1e17d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 19:41:32 +0000

    taobao 2.5.1 (intel only)

--- Casks/t/taobao.rb
+++ Casks/t/taobao.rb
@@ -1,9 +1,15 @@
 cask "taobao" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.1.0"
   sha256 arm:   "2200655677636c6d5ffd0cfd156ceda3fa49382b8fdbbba870236f5ddb7c1c81",
-         intel: "9422e959e40004fb0c89ea5093e0f27f0a934d53e7fff4e808587dd3ed3b1f03"
+         intel: "51640fd574a9ff8e097bd14e8a085a095f9e72c6fb5619884ea61740b64625c7"
+
+  on_arm do
+    version "2.1.0"
+  end
+  on_intel do
+    version "2.5.1"
+  end
 
   url "https://tblifecdn.taobao.com/taobaopc/shop/taobao-setup-darwin-#{arch}-#{version}.dmg"
   name "taobao"
