a6203cc918c282d376716dbd9dbd6ebe99c09acc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 13:43:19 +0000

    nuclear 1.42.2

--- Casks/n/nuclear.rb
+++ Casks/n/nuclear.rb
@@ -1,9 +1,9 @@
 cask "nuclear" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.42.1"
-  sha256 arm:   "51d156ac63b2d2e00ccd58f74ae9cd95adcb42c2fe1c7c1b7ab649aa3a4ede18",
-         intel: "e11b1086c781c59be0fc1798062b4170a79dc72879f08505eb8c0fc12f901be5"
+  version "1.42.2"
+  sha256 arm:   "47f1dbd7f97a9abca4824fb77dfcb1a0c1f1d4d5bbdf8cca4d6d29bde27b4ae5",
+         intel: "f52b0c278d5db8f59ce6813a1428481959e7c3209a5c94428b984f8fdb3a2a85"
 
   url "https://github.com/nukeop/nuclear/releases/download/player%40#{version}/Nuclear_#{version}_#{arch}.dmg",
       verified: "github.com/nukeop/nuclear/"
