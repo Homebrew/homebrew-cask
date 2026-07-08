a1a367c1e9ada4f00c72c81a2d0701a33625abae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 08:22:28 +0000

    unity-ios-support-for-editor 6000.5.3f1

--- Casks/u/unity-ios-support-for-editor.rb
+++ Casks/u/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-ios-support-for-editor" do
-  version "6000.5.2f1,eb73d3b415a1"
-  sha256 "6c5c77cfbdda9152b7a904818b08c775188cbfef5be54c9fee802bcfde3b9821"
+  version "6000.5.3f1,c2eb47b3a2a9"
+  sha256 "179b8fe0f13c9ce61da5e381fd883668ce2f01734b009c5b6ef34f6918661f96"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
