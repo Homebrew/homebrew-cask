c4760d4100a0f64188b9aaab78706709de3ab215
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 17:19:12 +0000

    showyedge 6.4.0

--- Casks/s/showyedge.rb
+++ Casks/s/showyedge.rb
@@ -1,6 +1,6 @@
 cask "showyedge" do
-  version "6.3.0"
-  sha256 "72a51e8ba1b758c8178bad272c704fbb3d4d87526f999e5c2d05c0d0a0e9a269"
+  version "6.4.0"
+  sha256 "c0bc769204c53c8729a35c74161fb08f089cb517bf00832fc386ba29c5cc6feb"
 
   url "https://github.com/pqrs-org/ShowyEdge/releases/download/v#{version}/ShowyEdge-#{version}.dmg",
       verified: "github.com/pqrs-org/ShowyEdge/"
