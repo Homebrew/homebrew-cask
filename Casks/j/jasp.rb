a28b6780694c90207dd2be1b66a47e80b2cc08b8
Bevan Kay <email@bevankay.me>
2026-07-09 10:11:23 +1000

    jasp: deprecate on intel

--- Casks/j/jasp.rb
+++ Casks/j/jasp.rb
@@ -6,6 +6,10 @@ cask "jasp" do
   sha256 arm:   "e980236d1ec6d58571b1f120d48f6c4c8ffc71bc893250a4988e96637220f257",
          intel: "d39665e1c1478ae04c1b1c5c92754806b0824f31dcb65d4c5f7497b9e1f2fb07"
 
+  on_intel do
+    disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  end
+
   url "https://github.com/jasp-stats/jasp-desktop/releases/download/v#{version.csv.first.major_minor_patch}/JASP-#{version.csv.first}-macOS-#{arch}.dmg",
       verified: "github.com/jasp-stats/jasp-desktop/"
   name "JASP"
