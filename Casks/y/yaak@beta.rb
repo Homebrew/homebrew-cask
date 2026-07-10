82ecc9861257bfd3b2a97f7367f882c6d0df2751
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 17:33:01 +0000

    yaak@beta 2026.5.0-beta.7

--- Casks/y/yaak@beta.rb
+++ Casks/y/yaak@beta.rb
@@ -6,11 +6,11 @@ cask "yaak@beta" do
     arch arm: "aarch64", intel: "amd64"
   end
 
-  version "2026.5.0-beta.6"
-  sha256 arm:          "6dbafe1fd115ff88bdadf1ee47ea44169b3de84959fd0b531f8cc846f8672cd0",
-         intel:        "7054a4fcffe126956d9d789a1fcf05a829d9fa0c60949afe7d4406b3a8924b49",
-         arm64_linux:  "bc64f6bd6548ac232477c7b76d27123a0c3348489f98254f1302f39ca936983e",
-         x86_64_linux: "2e3995550cc550491c193fabc8c3c9b963a5bd84d8f9b8c54fcd0730af082db4"
+  version "2026.5.0-beta.7"
+  sha256 arm:          "d27bb5a34ea14d8fa7eabb314e34575e87ca3ad47b2ec0da1966cb9f818083c8",
+         intel:        "0dd5ab55a13983f246321bd99ddda8a7b420508292871a4d902c2fa7f7967a56",
+         arm64_linux:  "9b9bf12701f3bed8600ed09f27ec85b02e45d01d168e942d5d0f1aae923b9ce4",
+         x86_64_linux: "5792d26d79d327b393d68f81c7cfacfbceff9aed6d590952673acc5d9ad000e4"
 
   artifact = on_system_conditional macos: "Yaak_#{version}_#{arch}.dmg",
                                    linux: "yaak_#{version}_#{arch}.AppImage"
