790d55f7042654e4328c74d3f78f1608cb1c743e
Mike McQuaid <mike@mikemcquaid.com>
2026-07-10 16:32:31 +0100

    parallels@14: use structured permission steps.

--- Casks/p/parallels@14.rb
+++ Casks/p/parallels@14.rb
@@ -47,8 +47,8 @@ cask "parallels@14" do
                    sudo: true
   end
 
-  uninstall_preflight do
-    set_ownership "#{appdir}/Parallels Desktop.app"
+  uninstall_preflight_steps do
+    set_ownership "Parallels Desktop.app", base: :appdir
   end
 
   uninstall delete: [
