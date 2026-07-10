63b0a6aa8d3598c529a2ad9eb2c9571d1f7e9141
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 12:00:53 +0000

    yakit 1.4.8-0710

--- Casks/y/yakit.rb
+++ Casks/y/yakit.rb
@@ -1,9 +1,9 @@
 cask "yakit" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.8-0703"
-  sha256 arm:   "24376c410fbb0c082864a6d0037973dd4ac978534b9c4e7e1279bc1ca1193c2a",
-         intel: "a493e0a0dea627da533bc650047e2386128158949b598d9c5e9527db732ed6b5"
+  version "1.4.8-0710"
+  sha256 arm:   "ed8f63de57a0e910c16c7313baae8fcf967002de04b79ece86e5a3b6d5aeeaa9",
+         intel: "e20b871cb65d3cb9c2c0609d7aa8d3a996ab5ec618a38ec7c4f2c7bc63eec69a"
 
   url "https://github.com/yaklang/yakit/releases/download/v#{version}/Yakit-#{version}-darwin-#{arch}.dmg"
   name "Yakit"
