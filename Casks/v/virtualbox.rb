9e0637a006e831bd5f9d871ba1885f0eccf0c1e9
Mike McQuaid <mike@mikemcquaid.com>
2026-07-10 16:32:34 +0100

    virtualbox: use structured permission steps.

--- Casks/v/virtualbox.rb
+++ Casks/v/virtualbox.rb
@@ -47,7 +47,7 @@ cask "virtualbox" do
         },
       ]
 
-  postflight do
+  postflight_steps do
     # If VirtualBox is installed before `/usr/local/lib/pkgconfig` is created by Homebrew,
     # it creates it itself with incorrect permissions that break other packages.
     # See https://github.com/Homebrew/homebrew-cask/issues/68730#issuecomment-534363026
