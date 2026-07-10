1f635f71322b443c3a4df7aabad26239e5b96f5b
Sam Ford <1584702+samford@users.noreply.github.com>
2026-07-10 12:32:05 -0400

    boss 9.2.40
    `boss` is autobumped but the workflow failed to update to versions
    after 9.2.37 because livecheck is erroneously reporting 9.3.0 as
    newest from a `chromium-v9.3.0` tag. This updates the version and
    adds a `livecheck` block with a regex that only matches the main
    tags, which correctly returns 9.2.40 as newest.

--- Casks/b/boss.rb
+++ Casks/b/boss.rb
@@ -1,12 +1,17 @@
 cask "boss" do
-  version "9.2.37"
-  sha256 "dfcb8ca9dcab3df4cef54c4310d65999068bbeedd2e4bc0b22dadc5e58135d55"
+  version "9.2.40"
+  sha256 "48eeca3ed024762aa4d67fba7ed7ff47ee35a2287bca921d77655b8427a5e8aa"
 
   url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BOSS-Releases/"
   name "BOSS"
   desc "AI-powered workspace for complex business operations"
   homepage "https://www.risalabs.ai/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   auto_updates true
   depends_on :macos
