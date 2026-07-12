67d3e57d71c76b6836d6afa212be080fc8c83946
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 04:55:27 +0000

    font-mengshen-regular 2.0.0,20260712-013306

--- Casks/font/font-m/font-mengshen-regular.rb
+++ Casks/font/font-m/font-mengshen-regular.rb
@@ -1,6 +1,6 @@
 cask "font-mengshen-regular" do
-  version "2.0.0,20260307-164439"
-  sha256 "e5af0a531cd6c395904f4f57def224204da3773f70dffaf28da0418e744d4027"
+  version "2.0.0,20260712-013306"
+  sha256 "5e8de93c3eacc7dee893a1f7173a05d0aceddaa60a7abaa742e6a1be5ee44605"
 
   url "https://github.com/MaruTama/Mengshen-pinyin-font/archive/refs/tags/v#{version.csv.second}.tar.gz"
   name "Mengshen Pinyin"
