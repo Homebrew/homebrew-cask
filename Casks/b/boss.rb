6ff843b59253c95a6b6f28d6459125e2c2c38889
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 21:46:29 +0000

    boss 9.2.45

--- Casks/b/boss.rb
+++ Casks/b/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "9.2.44"
-  sha256 "28d0f2a10af42b20e1bb14e695554d179143cb324b549c604c437dba3b1f4e21"
+  version "9.2.45"
+  sha256 "bb8c9af17a311a321503d11ee728a09bd063d6b660114968ffe46a0f99960f3c"
 
   url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BOSS-Releases/"
