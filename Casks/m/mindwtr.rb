f123302dbc2a11c8edd5c1fe6416e404be2647aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 05:13:55 +0000

    mindwtr 1.1.0

--- Casks/m/mindwtr.rb
+++ Casks/m/mindwtr.rb
@@ -1,9 +1,9 @@
 cask "mindwtr" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.5"
-  sha256 arm:   "06c2144de71ef25d234da85c616015d61c60473d7e79148d50f10404ef25849c",
-         intel: "1a004c890237c84dac47413d9b67a69c6819ebfb158b5d7a6f84f998c7b10c34"
+  version "1.1.0"
+  sha256 arm:   "e23613dbc31e7c036e327cd55120c2a828405599175eb81d49fcd2f489320ec8",
+         intel: "5ae369f374abb8288c5a84c2581fd268f70798c21f72e7239c8c9ac9b61e78a9"
 
   url "https://github.com/dongdongbh/Mindwtr/releases/download/v#{version}/mindwtr_#{version}_#{arch}.dmg"
   name "Mindwtr"
