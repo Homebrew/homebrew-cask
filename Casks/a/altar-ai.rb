99b98068e6c3706d30bf9fc868f62811fa872a25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 07:36:02 +0000

    altar-ai 1.15.0

--- Casks/a/altar-ai.rb
+++ Casks/a/altar-ai.rb
@@ -1,6 +1,6 @@
 cask "altar-ai" do
-  version "1.14.0"
-  sha256 "d7163a654ceacc2b6519bc0fd65730c8dcf963aa00857a95bf0e02eb634e85f3"
+  version "1.15.0"
+  sha256 "84ad8a9ed14b84e7e81a8d27d51b00642d87fd138d5fc0de4789b157604374b4"
 
   url "https://altar-prototype.sgp1.cdn.digitaloceanspaces.com/releases/v#{version}/altar-app-#{version}.dmg",
       verified: "altar-prototype.sgp1.cdn.digitaloceanspaces.com/"
