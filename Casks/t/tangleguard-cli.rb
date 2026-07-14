e61a6a14e70b87ba9d270be41822332b1849d031
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-14 22:48:09 +0000

    tangleguard-cli 0.12.0

--- Casks/t/tangleguard-cli.rb
+++ Casks/t/tangleguard-cli.rb
@@ -2,11 +2,11 @@ cask "tangleguard-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-gnu"
 
-  version "0.11.0"
-  sha256 arm:          "8759b5d6e4351166704da156809f26bbdefa0e48a624c799118c6f844f1469db",
-         x86_64:       "0d1ca5713b91f95a96bed524a3e0672fd48aa50fe104a8e7e1af525cd65ad5ec",
-         arm64_linux:  "5c2194d5480757c8be3428a21b5859de3926ad31d65b00a3d6bb35c8dfb4977d",
-         x86_64_linux: "14218242ac9a8e56de0ac70b0bcf43f619fb369d40e5eb78de3e2e8c83b220ef"
+  version "0.12.0"
+  sha256 arm:          "c11dfb64b495d1c5457cbbb811c12fd7a1d7040fe3e83fb16cc1b4f27e015b8b",
+         x86_64:       "77b54c34920ad562210de0d6845bc13d24171bd9610b0b090f82c2bddd6b174f",
+         arm64_linux:  "09a1c22f87f538a738cb197c6a6b1c3a4dff39d8098367b480c5797a4d56966e",
+         x86_64_linux: "e3ed587f290ae3c6ba1908995a142a5e809ed1e1d934a110c4984457518f49e2"
 
   url "https://tangleguard-cli-builds.s3.eu-central-1.amazonaws.com/v#{version}/tangleguard-cli_#{version}_#{arch}-#{os}.tar.gz",
       verified: "tangleguard-cli-builds.s3.eu-central-1.amazonaws.com/"
