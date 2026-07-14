beeb6131a0840f3082c35545ab535f8074a5518b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-13 22:12:51 -0500

    update ARM minimum macOS

--- Casks/s/structuredlogviewer.rb
+++ Casks/s/structuredlogviewer.rb
@@ -5,20 +5,13 @@ cask "structuredlogviewer" do
   sha256 arm:   "18d1ee4cc557a838a11bc9214aa33fa34035e279d0ae24f0a7f37b6bc5057e0a",
          intel: "69dc706368a2004f5ca421cd700c4b2e865989b1ef6d9649de222fba97af0ff7"
 
-  on_arm do
-    depends_on macos: :big_sur
-  end
-  on_intel do
-    depends_on macos: :catalina
-  end
-
   url "https://github.com/KirillOsenkov/MSBuildStructuredLog/releases/download/v#{version}/StructuredLogViewer-#{arch}.dmg",
       verified: "github.com/KirillOsenkov/MSBuildStructuredLog/"
   name "Structured Log Viewer"
   desc "Interactive log viewer for MSBuild structured logs (*.binlog)"
   homepage "https://msbuildlog.com/"
 
-  depends_on :macos
+  depends_on macos: :monterey
 
   app "StructuredLogViewer.app"
 
