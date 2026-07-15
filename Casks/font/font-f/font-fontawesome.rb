1c916fed2a03edce8d9c0aa8144ef04822d42345
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 19:08:01 +0000

    font-fontawesome 7.3.1

--- Casks/font/font-f/font-fontawesome.rb
+++ Casks/font/font-f/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask "font-fontawesome" do
-  version "7.3.0"
-  sha256 "719c61d1b2b595d166af27dac1fdce1391726b6eed7986076aa13b232976cfcd"
+  version "7.3.1"
+  sha256 "c61edde261707f33376a28e9a30bb8c70c1a20bf0bd975206b809f3b3b70add5"
 
   url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip",
       verified: "github.com/FortAwesome/Font-Awesome/"
