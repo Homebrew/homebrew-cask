1b7640cb91bb326df054e79dd8c535a128ec48fa
ANGRYCONE <theepicmanofficial@gmail.com>
2026-07-15 16:30:18 +1000

    onyx@beta 5.1.0,260713

--- Casks/o/onyx@beta.rb
+++ Casks/o/onyx@beta.rb
@@ -1,22 +1,23 @@
 cask "onyx@beta" do
-  version "4.9.0,250902"
+  version "5.1.0,260713"
   sha256 :no_check
 
-  url "https://www.titanium-software.fr/download/beta/OnyX.dmg"
+  url "https://www.titanium-software.fr/download/27/OnyX.dmg"
   name "OnyX Beta"
   desc "Verify system files structure, run miscellaneous maintenance and more"
   homepage "https://www.titanium-software.fr/en/onyx.html"
 
   livecheck do
     url :homepage
-    regex(/>?\s*OnyX\s+v?(\d+(?:\.\d+)+).*?\((\d+)\)\s+for\s+[\w\s]*26/i)
+    regex(/>?\s*OnyX\s+v?(\d+(?:\.\d+)+).*?\((\d+)\)\s+for\s+[\w\s]*27/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
   conflicts_with cask: "onyx"
-  depends_on macos: :tahoe
+  depends_on arch: :arm64
+  depends_on macos: :golden_gate
 
   app "OnyX.app"
 
