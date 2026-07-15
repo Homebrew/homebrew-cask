3acef0351b5dc030739d702f48514439a20155ce
Bevan Kay <bevanjkay@gmail.com>
2026-07-15 12:32:21 +0800

    koreader: fix checksum

--- Casks/k/koreader.rb
+++ Casks/k/koreader.rb
@@ -2,8 +2,8 @@ cask "koreader" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "2026.03"
-  sha256 on_arch_conditional(arm:   "48f582a1a32aed0d431f69c65b60467212d956513fa5846c08f3543eb4226eb6",
-                             intel: "2767db0e38c5ee88b2324bb2de88693982585831ecd9b19bf90d3e3a8436d224")
+  sha256 arm64_linux:  "48f582a1a32aed0d431f69c65b60467212d956513fa5846c08f3543eb4226eb6",
+         x86_64_linux: "2767db0e38c5ee88b2324bb2de88693982585831ecd9b19bf90d3e3a8436d224"
 
   url "https://github.com/koreader/koreader/releases/download/v#{version}/koreader-v#{version}-#{arch}.AppImage",
       verified: "github.com/koreader/koreader/"
