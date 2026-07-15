6d5404d158cfb3a8faafd11d3f6be476fed1d185
Qi Zhang <singularitti@outlook.com>
2026-07-15 00:25:16 -0500

    markdown-preview 0.0.35

--- Casks/m/markdown-preview.rb
+++ Casks/m/markdown-preview.rb
@@ -1,6 +1,6 @@
 cask "markdown-preview" do
-  version "0.0.34"
-  sha256 "786e9941d19ac3662c03d8d20e04753ec5405f485e19d5f7fa8fff1b9f5d1bf2"
+  version "0.0.35"
+  sha256 "e64d4cb4cf23859e4a5916705a258991d1b59f1b61b531de0975d7cb09d61995"
 
   url "https://github.com/pluk-inc/markdown-preview/releases/download/v#{version}/Markdown-Preview.dmg",
       verified: "github.com/pluk-inc/markdown-preview/"
