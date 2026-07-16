33efe535d096fa69a013103bd9ab0bbcbb85aa72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 13:04:21 +0000

    burp-suite@early-adopter 2026.7

--- Casks/b/burp-suite@early-adopter.rb
+++ Casks/b/burp-suite@early-adopter.rb
@@ -1,9 +1,9 @@
 cask "burp-suite@early-adopter" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2026.6"
-  sha256 arm:   "709a014d0398da2b840a200418e0dfe51fcfb1fb70d44ebf1465973c84e17f8b",
-         intel: "3b71b4ba81de250c5589381a8f8708d5985414d49e25c0ffd4cb1e315d4ae545"
+  version "2026.7"
+  sha256 arm:   "fb381299cca50990ab3f62c802e8a44f59f042f6ef8230c563cf453b89cabee5",
+         intel: "0597d32bd2759b4885cddf5dd0e0b13343a1f26bd452a15868af9adf0c7ada7e"
 
   url "https://portswigger-cdn.net/burp/releases/download?product=desktop&version=#{version}&type=#{arch}",
       verified: "portswigger-cdn.net/burp/releases/"
