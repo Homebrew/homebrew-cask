2dbc525830bd02c3854cef0acc2110fd73cea9b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 16:35:07 +0000

    kimi 3.1.1

--- Casks/k/kimi.rb
+++ Casks/k/kimi.rb
@@ -1,6 +1,6 @@
 cask "kimi" do
-  version "3.1.0"
-  sha256 "3292f36cfe4be76723ea0371ec0b078783a649c2f4d41a1ee8f9f2c25cbbe3e0"
+  version "3.1.1"
+  sha256 "c841dba8ebe40cd92e7af8a0a4a91b75c3a423668847f50814cb6906ecb5cd25"
 
   url "https://kimi-img.moonshot.cn/app/download/mac/kimi_#{version}.dmg",
       verified: "kimi-img.moonshot.cn/"
