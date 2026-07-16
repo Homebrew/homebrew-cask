863f32b8369be2c69fafa734f09e8c1643d3a3ff
Tim Hill <timhill@alum.mit.edu>
2026-07-16 18:50:07 -0500

    emacs-app@pretest 31.0.90-2

--- Casks/e/emacs-app@pretest.rb
+++ Casks/e/emacs-app@pretest.rb
@@ -1,12 +1,12 @@
 cask "emacs-app@pretest" do
-  arch arm: "arm64-11", intel: "x86_64-10_12"
+  arch arm: "arm64-11", intel: "x86_64-11"
 
-  version "30.1.90"
-  sha256 "e41b6b4dfbaf7ed1f0b4377546b48a0dc29fd20c948126b4847018069fb4a4a3"
+  version "31.0.90-2"
+  sha256 "827eaf1fc043648601d7654f0be7c9d999c96b4bb8aae40e1592e80561ea92f4"
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-pretest-#{version}-universal.dmg"
   name "Emacs"
-  desc "Text editor"
+  desc "GNU Emacs text editor"
   homepage "https://emacsformacosx.com/"
 
   livecheck do
@@ -18,15 +18,13 @@ cask "emacs-app@pretest" do
     "emacs-app",
     "emacs-app@nightly",
   ]
-  depends_on :macos
+  depends_on macos: :big_sur
 
   app "Emacs.app"
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
-  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ctags"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ebrowse"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/emacsclient"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/etags"
-  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ctags.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
