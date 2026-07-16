009e94246e1accaed3da58a9e2020fad7824edc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 11:02:48 +0000

    server-box 1.0.1466

--- Casks/s/server-box.rb
+++ Casks/s/server-box.rb
@@ -1,6 +1,6 @@
 cask "server-box" do
-  version "1.0.1450"
-  sha256 "496035764db99a314029f18df64ecfc6f332f0f8e0e5641794b03713c3994c84"
+  version "1.0.1466"
+  sha256 "bfa434ecac2dd6f7f27f1511edbc8077f6a9c801896ad9577b1387cb441cea68"
 
   url "https://github.com/lollipopkit/flutter_server_box/releases/download/v#{version}/ServerBox-#{version}.dmg"
   name "ServerBox"
