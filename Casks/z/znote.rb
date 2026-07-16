2dd8aa6c6809eb6e5bf21eed7e0e39a7fb30d420
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 19:51:21 +0000

    znote 4.2.3

--- Casks/z/znote.rb
+++ Casks/z/znote.rb
@@ -1,9 +1,9 @@
 cask "znote" do
   arch arm: "-arm64"
 
-  version "4.2.2"
-  sha256 arm:   "d8dc2079021aad70ded620dac134fa02bcd5d2728cd9ff4a60dfe82d92495521",
-         intel: "4c6ce81d8f7f1871ca8a732f03b1fc24c0bffee3b3d4f5f2c699ef945294c39b"
+  version "4.2.3"
+  sha256 arm:   "25839c4b51b9d0466d36a6b666e65d6609313f96db714178e0abb467004f14f9",
+         intel: "0443e0aa38cf382b1a9637a285c076afa2a0d8d2c185ea1a01ab68563ba15132"
 
   url "https://github.com/alagrede/znote-app/releases/download/v#{version}/znote-#{version}#{arch}.dmg",
       verified: "github.com/alagrede/znote-app/"
