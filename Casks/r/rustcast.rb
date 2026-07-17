057307e345c9892bad996a992b43cfb9deafb4df
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-07-17 12:33:15 +0800

    Update rustcast.rb

--- Casks/r/rustcast.rb
+++ Casks/r/rustcast.rb
@@ -2,11 +2,11 @@ cask "rustcast" do
   version "0.8.4"
   sha256 "34d0db49f2a925667d594d849973b8a0833943489371f4277490cf94fbfe7af6"
 
-  url "https://github.com/RustCastLabs/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip",
-      verified: "github.com/RustCastLabs/rustcast/releases/download/"
+  url "https://github.com/MystikoLab/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip",
+    verified: "github.com/MystikoLab/rustcast"
   name "Rustcast"
   desc "Application and utility launcher"
-  homepage "https://rustcast.app/"
+  homepage "https://rustcast.app"
 
   livecheck do
     url :url
@@ -19,12 +19,11 @@ cask "rustcast" do
   app "target/release/macos/Rustcast.app"
 
   zap trash: [
-    "/tmp/rustcast.log",
-    "~/.config/rustcast/config.toml",
     "~/Library/Application Support/CrashReporter/rustcast_*.plist",
-    "~/Library/Application Support/rustcast/clipboard.db",
     "~/Library/Logs/DiagnosticReports/rustcast-*.ips",
     "~/Library/Preferences/com.*.rustcast.plist",
     "~/Library/Preferences/rustcast.plist",
+    "~/.config/rustcast/config.toml",
+    "/tmp/rustcast.log",
   ]
 end
