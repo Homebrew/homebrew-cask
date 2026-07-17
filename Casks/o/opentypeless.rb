e933a05a723b88ac55688f7173f27fcb2b8d4f02
tover wu <184375629+tover0314-w@users.noreply.github.com>
2026-07-17 16:44:16 +0800

    opentypeless 1.1.50

--- Casks/o/opentypeless.rb
+++ Casks/o/opentypeless.rb
@@ -1,6 +1,6 @@
 cask "opentypeless" do
-  version "1.1.46"
-  sha256 "bb00542ca448a69b9192117be1c479ecdba097999455752d0d8654b72b3de58f"
+  version "1.1.50"
+  sha256 "ebc9f56748d961361445f0962b62793ce5cba47d357ead53f5a31c1a3bcb375e"
 
   url "https://github.com/tover0314-w/opentypeless/releases/download/v#{version}/OpenTypeless_#{version}_universal.dmg",
       verified: "github.com/tover0314-w/opentypeless/"
@@ -16,4 +16,12 @@ cask "opentypeless" do
   depends_on macos: :catalina
 
   app "OpenTypeless.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.opentypeless.app",
+    "~/Library/Caches/com.opentypeless.app",
+    "~/Library/Caches/opentypeless",
+    "~/Library/WebKit/com.opentypeless.app",
+    "~/Library/WebKit/opentypeless",
+  ]
 end
