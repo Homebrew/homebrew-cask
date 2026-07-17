3e2e571f7eab97463d9fdf49263cdd99d0fde5c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 04:45:57 +0000

    fiji 20260716-2317

--- Casks/f/fiji.rb
+++ Casks/f/fiji.rb
@@ -1,9 +1,9 @@
 cask "fiji" do
   arch arm: "-arm64", intel: "64"
 
-  version "20260514-1817"
-  sha256 arm:   "003c425fa90f3c35df0e6bea5a9c157aed6dce8561e63f5485534e1612d23983",
-         intel: "9cea59777dc9c1a8f538afbc2733a62933d7c92911c99bf9ae5db32efc0096f9"
+  version "20260716-2317"
+  sha256 arm:   "f2f26750b5a2047d9398e4b2828c0840203b62902cf5aaf1d192404c9c4d86fe",
+         intel: "53ed626b835d4a79e0e99e83e98df04832d6b265f492fc9dd4b4db99e4f54511"
 
   url "https://downloads.imagej.net/fiji/archive/latest/#{version}/fiji-latest-macos#{arch}-jdk.zip",
       verified: "downloads.imagej.net/fiji/archive/"
