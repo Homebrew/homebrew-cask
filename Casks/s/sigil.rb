e1ce049e82f478de1e2810d92bbaabb029fa7e24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 13:33:07 +0000

    sigil 2.8.1

--- Casks/s/sigil.rb
+++ Casks/s/sigil.rb
@@ -1,9 +1,9 @@
 cask "sigil" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.8.0"
-  sha256 arm:   "cb19ef4f27c78a0168bce37fa73f99cc6d2b0be72a2649bf76b1d4e25222a11c",
-         intel: "4461bd2e1b88b7409500da3d5cc6dc5318552e23fe9ad43535cd1c4409f0d456"
+  version "2.8.1"
+  sha256 arm:   "285addb9abee1d69c913d6a6b504240946b5274fd2193942ed59697536279afe",
+         intel: "32f625fbdace147e5a94cf8c7167439f9e53afaa2a902d93b230363bc8935f53"
 
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil.app-#{version}-Mac-#{arch}.txz",
       verified: "github.com/Sigil-Ebook/Sigil/"
