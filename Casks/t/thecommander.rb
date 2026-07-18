59f8675a6b9352676e546c9f7fbe5ff24f7f77b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 16:31:40 +0000

    thecommander 1.7.1

--- Casks/t/thecommander.rb
+++ Casks/t/thecommander.rb
@@ -1,6 +1,6 @@
 cask "thecommander" do
-  version "1.7.0"
-  sha256 "1d00206a505251826c36ccc9319b69050e7ac0984b778f3a9496699bd92dc827"
+  version "1.7.1"
+  sha256 "05b470328e42332302615c5dcc13261244fff505e9b010d7e617cbe44e88d1b0"
 
   url "https://die-gutbrods.de/thecommander/updates/TheCommander-#{version}.dmg"
   name "TheCommander"
