0cad76f60a3f28ac77bfb99b1e56f83cf15afda1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 10:50:23 +0000

    uniclipboard 0.19.0

--- Casks/u/uniclipboard.rb
+++ Casks/u/uniclipboard.rb
@@ -1,9 +1,9 @@
 cask "uniclipboard" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.18.0"
-  sha256 arm:   "9be2b9157f11419954cc0f72cef1dc65cb04538f777120864b3871313ab1e27c",
-         intel: "f1974fc4c7b2b8401e8924f5fb077a0635427d6b7e3530d09c8234848c4fbc55"
+  version "0.19.0"
+  sha256 arm:   "1efe3665a22e7ddc91b13a9ac54265c41adafc079235b92192dbcefc7aefbe2a",
+         intel: "00a06cebe321b19e9f2915682f31c9b8273822517229091505aee3c3cae9f439"
 
   url "https://github.com/UniClipboard/UniClipboard/releases/download/v#{version}/UniClipboard_#{version}_#{arch}.dmg",
       verified: "github.com/UniClipboard/UniClipboard/"
