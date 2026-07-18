7122baa14792b2186cfbe649cb760de28744d489
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 13:17:36 +0000

    miaoyan 4.1.0

--- Casks/m/miaoyan.rb
+++ Casks/m/miaoyan.rb
@@ -1,6 +1,6 @@
 cask "miaoyan" do
-  version "4.0.0"
-  sha256 "aa05b260b2e765120ab18a9d5d3ec340b3f9e0c3f059126aab3babd88056a2ef"
+  version "4.1.0"
+  sha256 "2d646b98031f3e48a6b803ef44e906b126c25d595027ea7d1ed4cb3a7babd332"
 
   url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan_V#{version}.zip",
       verified: "github.com/tw93/MiaoYan/"
