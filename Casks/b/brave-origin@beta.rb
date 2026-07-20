252603714b73f9ec4f6c52f20d09268bd7ac6524
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-20 13:16:57 +0000

    brave-origin@beta 1.93.122.0

--- Casks/b/brave-origin@beta.rb
+++ Casks/b/brave-origin@beta.rb
@@ -2,9 +2,15 @@ cask "brave-origin@beta" do
   arch arm: "arm64", intel: "x64"
   folder_arch = on_arch_conditional arm: "-arm64"
 
-  version "1.93.118.0"
-  sha256 arm:   "6efe5c84bbcd1c7751256a3ee1c061074cf714955e44e5719b4bc813c8cccf3e",
-         intel: "f00dbbda791ff633a8509f4a769373d7d0e6c2ee3fb87978cb40a32eb773110e"
+  sha256 arm:   "860a8db8e4ea01b5205386bcd1269c47434c7d247c4610a9594a701ccacc6cc0",
+         intel: "6f54d987e7dbd6ba9649ceb2cc55a14d11bbb23906de990a5bf8c3f8403a7aae"
+
+  on_arm do
+    version "1.93.122.0"
+  end
+  on_intel do
+    version "1.93.120.0"
+  end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Origin/beta#{folder_arch}/#{version.major_minor_patch.sub(".", "")}/Brave-Origin-Beta-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Origin/"
