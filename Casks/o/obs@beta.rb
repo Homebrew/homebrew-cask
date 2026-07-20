04f0d438b4b166ac5e889cd36b47ee3fb974485c
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:25 +0100

    obs@beta: migrate command wrappers.

--- Casks/o/obs@beta.rb
+++ Casks/o/obs@beta.rb
@@ -28,16 +28,8 @@ cask "obs@beta" do
   depends_on macos: :monterey
 
   app "OBS.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/obs.wrapper.sh"
-  binary shimscript, target: "obs"
-
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/bash
-      exec '#{appdir}/OBS.app/Contents/MacOS/OBS' "$@"
-    EOS
-  end
+  command_wrapper "obs",
+                  executable: "#{appdir}/OBS.app/Contents/MacOS/OBS"
 
   uninstall delete: "/Library/CoreMediaIO/Plug-Ins/DAL/obs-mac-virtualcam.plugin"
 
