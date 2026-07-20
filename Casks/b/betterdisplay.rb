dbd138ceaa8d8d108d22fccf52e6319232af3b76
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:22 +0100

    betterdisplay: migrate command wrappers.

--- Casks/b/betterdisplay.rb
+++ Casks/b/betterdisplay.rb
@@ -37,16 +37,8 @@ cask "betterdisplay" do
   depends_on :macos
 
   app "BetterDisplay.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/betterdisplay.wrapper.sh"
-  binary shimscript, target: "betterdisplaycli"
-
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/bash
-      exec '#{appdir}/BetterDisplay.app/Contents/MacOS/BetterDisplay' "$@"
-    EOS
-  end
+  command_wrapper "betterdisplaycli",
+                  executable: "#{appdir}/BetterDisplay.app/Contents/MacOS/BetterDisplay"
 
   uninstall quit:       "pro.betterdisplay.BetterDisplay",
             login_item: "BetterDisplay"
