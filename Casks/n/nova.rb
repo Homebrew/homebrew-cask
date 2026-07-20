4e3800f79344b35f54290d70a86c7e0d519860f5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-20 15:51:41 -0400

    update minimum macOS

--- Casks/n/nova.rb
+++ Casks/n/nova.rb
@@ -15,7 +15,7 @@ cask "nova" do
   end
 
   auto_updates true
-  depends_on macos: :ventura
+  depends_on macos: :sequoia
 
   app "Nova.app"
   binary "#{appdir}/Nova.app/Contents/SharedSupport/nova"
