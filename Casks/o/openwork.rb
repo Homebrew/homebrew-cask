c04fa5c3fa33c6c2cb755373133ece5e19251b60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-20 16:49:20 +0000

    openwork 0.17.33

--- Casks/o/openwork.rb
+++ Casks/o/openwork.rb
@@ -8,11 +8,11 @@ cask "openwork" do
     arch arm: "arm64", intel: "x86_64"
   end
 
-  version "0.17.32"
-  sha256 arm:          "867b3273c61376f9510a67ef79a5d70464857c21761d57bf9adb43b9217bf8e6",
-         intel:        "372063563a25ea151d0f140f731038b04674dd03d7bf308fbc80c3a6d8e6b4a8",
-         arm64_linux:  "7417a23dc72ecdc7bd189e668ae29513700f2fb6557519271f2a7b909828daf2",
-         x86_64_linux: "67e506f4d185ceecd2f6394e59226f02918ff2fcd9c727742c9e503ea989d6d5"
+  version "0.17.33"
+  sha256 arm:          "d4eff0c41219cddd73c0e63f5054ba892b9c95fd679a4d966284fe148eebd7c6",
+         intel:        "5f5588a9b63f172a8fcc6b70150eb83738c773db0621764386f4cfb619715b3a",
+         arm64_linux:  "631808e33f482a14c022c7ab6e2267be469b26a6bf1b733d25bb9484cc62a7e5",
+         x86_64_linux: "c949c84427edaed1ae049df36e4db886244846e616117f6533ca7f917fe74879"
 
   url_end = on_system_conditional linux: ".AppImage", macos: ".dmg"
 
