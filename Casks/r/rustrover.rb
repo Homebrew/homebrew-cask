14717ae0e82126cea0911b8f836698d589e85780
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:25 +0100

    rustrover: migrate command wrappers.

--- Casks/r/rustrover.rb
+++ Casks/r/rustrover.rb
@@ -27,16 +27,8 @@ cask "rustrover" do
   depends_on :macos
 
   app "RustRover.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/rustrover.wrapper.sh"
-  binary shimscript, target: "rustrover"
-
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/sh
-      exec '#{appdir}/RustRover.app/Contents/MacOS/rustrover' "$@"
-    EOS
-  end
+  command_wrapper "rustrover",
+                  executable: "#{appdir}/RustRover.app/Contents/MacOS/rustrover"
 
   zap trash: [
     "~/Library/Application Support/JetBrains/RustRover#{version.major_minor}",
