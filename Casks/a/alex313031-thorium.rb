fedfde176c8ac423c1f1d9375b270658b68898ef
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:22 +0100

    alex313031-thorium: migrate command wrappers.

--- Casks/a/alex313031-thorium.rb
+++ Casks/a/alex313031-thorium.rb
@@ -22,16 +22,8 @@ cask "alex313031-thorium" do
   depends_on macos: :big_sur
 
   app "Thorium.app", target: "Thorium Browser.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/thorium.wrapper.sh"
-  binary shimscript, target: "thorium"
-
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/bash
-      exec '#{appdir}/Thorium Browser.app/Contents/MacOS/Thorium' "$@"
-    EOS
-  end
+  command_wrapper "thorium",
+                  executable: "#{appdir}/Thorium Browser.app/Contents/MacOS/Thorium"
 
   zap trash: [
     "~/Library/Application Support/Thorium",
