3ad904e45410ced54be609cff1654cd3e9a05d4a
MAXIME MARTIN CIVET <108419734+itswinancher@users.noreply.github.com>
2026-07-20 20:58:30 +0200

    Add Cask for T2DECODE version 1.0.2.4

---
+++ Casks/t/t2decode.rb
@@ -0,0 +1,19 @@
+cask "t2decode" do
+  version "1.0.2.4"
+  # Tapez "shasum -a 256 T2DECODE-macOS.dmg" dans votre terminal pour obtenir ce code
+  sha256 "3f89f1eca5e1cd76de9a46004327c2c07f1bb175488d09f9a0840bef71a11622"
+
+  url "https://downloads.sourceforge.net/t2decode/v#{version}/T2DECODE-macOS.dmg",
+      verified: "downloads.sourceforge.net/t2decode/"
+  name "T2DECODE"
+  desc "Simulateur et boîte à outils hors-ligne pour les réseaux et la cybersécurité"
+  homepage "https://tutodecode.org/"
+
+  app "T2DECODE.app"
+
+  zap trash: [
+    "~/Library/Application Support/T2DECODE",
+    "~/Library/Preferences/org.t2decode.app.plist",
+    "~/Library/Saved Application State/org.t2decode.app.savedState",
+  ]
+end
