1459a8e2ef653cf7b7fd6eb3410111e1d3ba1cc5
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:25 +0100

    rider: migrate command wrappers.

--- Casks/r/rider.rb
+++ Casks/r/rider.rb
@@ -27,16 +27,8 @@ cask "rider" do
   depends_on :macos
 
   app "Rider.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/rider.wrapper.sh"
-  binary shimscript, target: "rider"
-
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/sh
-      exec '#{appdir}/Rider.app/Contents/MacOS/rider' "$@"
-    EOS
-  end
+  command_wrapper "rider",
+                  executable: "#{appdir}/Rider.app/Contents/MacOS/rider"
 
   zap trash: [
     "~/Library/Application Support/Rider#{version.major_minor}",
