c02109e959232897ac0dcb10bb45ad6ec531bcff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 14:05:15 +0000

    oracle-jdk-javadoc@21 21.0.12

--- Casks/o/oracle-jdk-javadoc@21.rb
+++ Casks/o/oracle-jdk-javadoc@21.rb
@@ -1,6 +1,6 @@
 cask "oracle-jdk-javadoc@21" do
-  version "21.0.10,8,c7cd7020f02e418d8008cdaefb15b4ba"
-  sha256 "118da93f4dc76d486490bc333a987b74c8ee86a573b31f022eececa18d8fe3dc"
+  version "21.0.12,7,2da8a586119d4c35b4216c1266808e45"
+  sha256 "909e3c90eaeaf5f08162346bee6cb01402a2ff41e2c0cb593e06cde257b3012b"
 
   url "https://download.oracle.com/otn_software/java/jdk/#{version.csv.first}+#{version.csv.second}/#{version.csv.third}/jdk-#{version.csv.first}_doc-all.zip",
       cookies: {
