8dbd60db14cf2d4055d363d7b84b0e123d58afe0
JiangWanZhengChouYv <3327218513@qq.com>
2026-07-21 18:24:30 +0800

    cask: add openswift
    OpenSwift is a macOS app accelerator that controls process speed via DYLD injection.

---
+++ Casks/o/openswift.rb
@@ -0,0 +1,13 @@
+cask "openswift" do
+  version "0.1.0"
+  sha256 "c91fc35f3b1ec788612e83d79cc4dbd271a930c7c3114c94bcf4df71049fb23f"
+
+  url "https://github.com/JiangWanZhengChouYv/OpenSwift/releases/download/v#{version}/OpenSwift-v#{version}.zip"
+  name "OpenSwift"
+  desc "macOS app accelerator - control process speed via DYLD injection"
+  homepage "https://github.com/JiangWanZhengChouYv/OpenSwift"
+
+  depends_on macos: :ventura
+
+  app "OpenSwift.app"
+end
