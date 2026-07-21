bc01690ce9f95345416885242b321ed08e2ff73b
VEdaNT2250 <135146486+VEdaNT2250@users.noreply.github.com>
2026-07-21 16:03:17 +0000

    Update automl-ide.rb

--- Casks/a/automl-ide.rb
+++ Casks/a/automl-ide.rb
@@ -2,11 +2,14 @@ cask "automl-ide" do
   version "1.0.14"
   sha256 "7f635c5f7c810cbfa6c4e0e6b87b3338985883a5bd8703def7e0a3d445da1358"
 
-  # Points to your PUBLIC releases repository
+  # Points to your public releases repository for download
   url "https://github.com/VEdaNT2250/AutoML-Installer-files/releases/download/Just_Me/AutoML.IDE_0.1.0_aarch64.dmg"
   name "AutoML IDE"
   desc "Local GPU-Accelerated Tabular Machine Learning Workspace"
-  homepage "https://github.com/Vedant2250/AutoML"
+  # Points to your public releases repository homepage
+  homepage "https://github.com/Vedant2250/AutoML-Installer-files"
+
+  depends_on :macos
 
   app "AutoML IDE.app"
 end
