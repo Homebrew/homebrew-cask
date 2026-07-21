dc66463368931ebad3ff5a79427e4a750c48b7d7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 17:09:18 +0000

    slack-cli 4.5.0

--- Casks/s/slack-cli.rb
+++ Casks/s/slack-cli.rb
@@ -2,17 +2,17 @@ cask "slack-cli" do
   arch arm: "arm64", intel: "amd64"
   os macos: "macOS", linux: "linux"
 
-  version "4.4.0"
+  version "4.5.0"
 
   on_macos do
     url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_#{arch}.tar.gz",
         verified: "downloads.slack-edge.com/"
-    sha256 arm:   "dddb34342f400598348ace7f5c86f85afe5d99666084720f962669d12bc49d4f",
-           intel: "9246c3f1aed003eab700662ea78b58e5ae28f484dba7c2f1fafced2cb8595322"
+    sha256 arm:   "bc1a9933bd6376ee61a7220ac3da8cdf9696b650fb0f8ad52478a51f58f03131",
+           intel: "5ed4bc27f20f279dacc9eac58f0ac51c889a766a1adfdd93b25aa8fc1de23b4a"
   end
 
   on_linux do
-    sha256 "315f6d072e83fe681633761cae1e6b18c4b26faa16d1bf4d89b21e76ba3d3faa"
+    sha256 "e393598b3175006f5d0c27fb87cc5e728111ae95dc67eed696f8c000e2a0a105"
 
     url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_#{os}_64-bit.tar.gz",
         verified: "downloads.slack-edge.com/"
