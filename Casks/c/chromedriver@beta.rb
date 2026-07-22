1142d7cbce92df26893ee40268f6099bc76268e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 18:57:32 +0000

    chromedriver@beta 151.0.7922.47

--- Casks/c/chromedriver@beta.rb
+++ Casks/c/chromedriver@beta.rb
@@ -1,9 +1,9 @@
 cask "chromedriver@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "151.0.7922.34"
-  sha256 arm:   "6f9c61d45acb74a8d7a6c9d49d9fd62fe14fcfa065d7f8f191722bc4d7dd8524",
-         intel: "8d24d0d3421eeebecece5b099bf707eac0593035b1a9219d639c306b3d823949"
+  version "151.0.7922.47"
+  sha256 arm:   "d450678936f5a2b39598a4e7d548177931a7a5c4759c0e4824f2cab1ae26523e",
+         intel: "1d1fe5357438a47f66db3a1e2f3325112a1a2e286463510e778665d39c317449"
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
