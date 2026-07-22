6bc2d911ce22c14e8bd415176a4b27744c97591a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 19:41:18 +0000

    screens-connect 5.4

--- Casks/s/screens-connect.rb
+++ Casks/s/screens-connect.rb
@@ -1,6 +1,6 @@
 cask "screens-connect" do
-  version "5.3.3,22948"
-  sha256 "1390b4c4d09f93d0914e7712c9065dfeae43e7e64d91cab53a3f99d6772b14e0"
+  version "5.4,22962"
+  sha256 "d007305b65e003d08db4a0cc9fedaa539a9d6dfedee357c783b82fa750c46340"
 
   url "https://updates.edovia.com/com.edovia.screens.connect.mac/ScreensConnect_#{version.csv.first}b#{version.csv.second}.zip"
   name "Screens Connect"
