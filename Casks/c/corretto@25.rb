3f9b6e2f29ed6e4c44d382663e4d0f0487e31daf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 01:07:13 +0000

    corretto@25 25.0.4.7.1

--- Casks/c/corretto@25.rb
+++ Casks/c/corretto@25.rb
@@ -1,9 +1,9 @@
 cask "corretto@25" do
   arch arm: "aarch64", intel: "x64"
 
-  version "25.0.3.9.1"
-  sha256 arm:   "f4fccb2f96f979f848c133091f9624fb0dfdd97ba34318f2ab8cab568591fc58",
-         intel: "4181f9b02d461d1e321ed651f92b9685444aa2ccbf434ea5fe9d15c7e57129ae"
+  version "25.0.4.7.1"
+  sha256 arm:   "ded718c287ef3ea6b595576a6532f14ab9919137bc28453ff1b1fba491cbe4ad",
+         intel: "bbdf267d7bf4a258f53662b266b194d652588fa60895a8147b057c26ac277af2"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
   name "AWS Corretto JDK"
