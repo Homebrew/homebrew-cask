4ecc24f5df7cd104ce03fee42638005a85ccf68e
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-07-22 22:14:14 +0800

    Create sxitch.rb

---
+++ Casks/s/sxitch.rb
@@ -0,0 +1,20 @@
+cask "sxitch" do
+  version "1.2"
+  sha256 "74643c98b4f7037a3bb798b8bf667d3880b644f78b0790e1b4c248a8727c72e8"
+
+  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip",
+      verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
+  name "Sxitch"
+  desc "Tree-based app switcher"
+  homepage "https://sxitch.app/"
+
+  depends_on macos: :sequoia
+
+  app "Sxitch.app"
+
+  zap trash: [
+    "~/Library/Application Support/Sxitch",
+    "~/Library/Caches/app.sxitch",
+    "~/Library/Preferences/app.sxitch.plist",
+  ]
+end
