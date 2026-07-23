fd6a6a0bfdbbb6f9466e0cac60b18bff57786ef5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 13:11:03 +0000

    clion 2026.2.0.1

--- Casks/c/clion.rb
+++ Casks/c/clion.rb
@@ -1,9 +1,9 @@
 cask "clion" do
   arch arm: "-aarch64"
 
-  version "2026.2,262.8665.262"
-  sha256 arm:   "090796d71ba0e00c78170c20189c6126dd604676b0b823da03a459b2b430e544",
-         intel: "8d85c00c46ac72c858bae4e78b30abc24da90b4bb634808a382052d404f3c766"
+  version "2026.2.0.1,262.8665.321"
+  sha256 arm:   "9e4909f34adcd5b0291013194f4663650727864cc01b78b5a7d1a1414038813e",
+         intel: "4f94f51d0c6b53ef29a74474440898c69ef946b947a060860f40d6a1f754dd1f"
 
   url "https://download.jetbrains.com/cpp/CLion-#{version.csv.first}#{arch}.dmg"
   name "CLion"
