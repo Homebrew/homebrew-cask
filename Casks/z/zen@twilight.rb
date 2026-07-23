aa1409b3f2990671a6db2a4499260b37d2d62024
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 14:24:52 +0000

    zen@twilight 1.22t,20260723080104

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -1,7 +1,7 @@
 cask "zen@twilight" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.22t,20260717111626"
+  version "1.22t,20260723080104"
   sha256 :no_check
 
   artifact = on_system_conditional macos: "zen.macos-universal.dmg", linux: "zen-#{arch}.AppImage"
