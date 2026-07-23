06c5a7676a0e7583d1f7f742a023d88d9b0dc258
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 10:51:14 +0000

    metasploit 6.4.145,20260723055536

--- Casks/m/metasploit.rb
+++ Casks/m/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.4.145,20260721055542"
-  sha256 "5cec3602ae9364251e5ddce1185009dc55f32dc29e0f7bde794a2c2c4fab357f"
+  version "6.4.145,20260723055536"
+  sha256 "04c4a880c382d67da69f63a8c0945ecf95d149d09ccab9c8de7b16f9cd20d014"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.csv.first}-#{version.csv.second}-1rapid7-1.x86_64.pkg"
   name "Metasploit Framework"
