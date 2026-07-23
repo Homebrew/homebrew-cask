7fa5830134fbd7c0c9f852fe1f0d97e5a4b34a3e
Sam Ford <1584702+samford@users.noreply.github.com>
2026-07-23 10:06:59 -0400

    gog-galaxy: split legacy Intel version
    `gog-galaxy` is ARM-only from version 2.1 onward and upstream states
    that Intel users can stay on version 2.0 while it's available (see
    https://support.gog.com/hc/en-us/articles/36703211085085-GOG-GALAXY-End-of-Support-for-Older-Windows-and-macOS-Versions?product=gog). This splits
    the versions based on arch, retaining the most recent 2.0 version for
    Intel.

--- Casks/g/gog-galaxy.rb
+++ Casks/g/gog-galaxy.rb
@@ -1,16 +1,26 @@
 cask "gog-galaxy" do
-  version "2.1.7.22"
-  sha256 "b672ada25d63574a2221f736c38871c2f7e46a71051c449d4695a7dffc91f8da"
+  on_arm do
+    version "2.1.7.22"
+    sha256 "b672ada25d63574a2221f736c38871c2f7e46a71051c449d4695a7dffc91f8da"
+
+    livecheck do
+      url :homepage
+      regex(/href=.*?galaxy[._-]client[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+    end
+  end
+  on_intel do
+    version "2.0.100.1"
+    sha256 "7485fa4cfbfa97d6a750e9d4c4768063222aeea3208d8dee28264c2c22e2aad6"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
 
   url "https://gog-cdn-fastly.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
   name "GOG Galaxy"
   desc "Game client"
   homepage "https://www.gog.com/galaxy"
-
-  livecheck do
-    url :homepage
-    regex(/href=.*?galaxy[._-]client[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
-  end
 
   auto_updates true
   depends_on macos: :sonoma
@@ -33,8 +43,4 @@ cask "gog-galaxy" do
     "~/Library/Preferences/com.gog.galaxy.plist",
     "~/Library/Saved Application State/com.gog.galaxy.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
