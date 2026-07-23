ab4e16d96b551bdf3ab130a73088afe86ca1b514
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 01:53:23 +0000

    signal 8.20.0

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.19.0"
-  sha256 arm:   "f6f91783d4c1df2b4e9ffb30f6b854879884beb238204817dada372b7ac8deed",
-         intel: "939541499b51319ea7a1210dda9771cbdd086e0fbda77928e8b68ed3219a27ea"
+  version "8.20.0"
+  sha256 arm:   "1365ea7b0b036d8cf706463a09472e5c90bdd308c167fe1313164c8c710f20a3",
+         intel: "bd94f10acf7ab6df033fc87a47aae0b42f4e3083dcbfb820332b755592686bd2"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.zip"
   name "Signal"
