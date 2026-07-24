a330170f3caf6d347df31792843058595d99900d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 01:02:54 +0000

    anypointstudio 7.27.0

--- Casks/a/anypointstudio.rb
+++ Casks/a/anypointstudio.rb
@@ -1,9 +1,9 @@
 cask "anypointstudio" do
   arch arm: "Arm", intel: "64"
 
-  version "7.26.0"
-  sha256 arm:   "04d3316b8175f7482f4b04b4b049a17db48a355bb28c2102cf0358034ff3924d",
-         intel: "86e1ac3c75c0d993ee67800af624c6974fa30864ca953b046138dba9c02b859c"
+  version "7.27.0"
+  sha256 arm:   "9365bd6292342cf433d2fd84970bed4e98711add4f53522b1c89e5e029cb830c",
+         intel: "c2e33db2396d06f4c635963a3dcd9e0d2709a34e039de7a26daa9574cc2d4782"
 
   url "https://www.mulesoft.com/downloads/studio/latest/AnypointStudio-#{version}-macos#{arch}.zip",
       referer: "https://www.mulesoft.com/api/products"
