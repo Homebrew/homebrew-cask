a89cac389d097fa2666825d86577f5936b1f23e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 13:11:33 +0000

    filo 2.2.2

--- Casks/f/filo.rb
+++ Casks/f/filo.rb
@@ -1,7 +1,7 @@
 cask "filo" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.2.1"
+  version "2.2.2"
   sha256 :no_check
 
   url "https://download.filomail.com/mac_upgrade/versions/latest/prod/#{arch}/Filo-#{arch}.dmg"
