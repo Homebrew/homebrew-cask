b6b857b848ee1edead3825cf3de5388b19fa4e26
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-24 19:05:32 +0000

    vassal 3.7.25

--- Casks/v/vassal.rb
+++ Casks/v/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.7.24"
-  sha256 "b94d98bffc55da9338ace9bbf213080ce03158a5b8de9b5d2fe925f6437d9a9b"
+  version "3.7.25"
+  sha256 "e52dafc1856caa05a25ef7d234766f74b7e0790c182747e117115b762141bc0d"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-universal.dmg",
       verified: "github.com/vassalengine/vassal/"
