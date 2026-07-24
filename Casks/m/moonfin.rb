d0fa42657e4a71499fc2bdc35f7bef3f01677033
Xopez <28950736+xopez@users.noreply.github.com>
2026-07-24 19:30:25 +0200

    Update moonfin.rb

--- Casks/m/moonfin.rb
+++ Casks/m/moonfin.rb
@@ -1,6 +1,6 @@
 cask "moonfin" do
-  version "2.3.1"
-  sha256 "2b29bfe33d657204bb5dffc06cf8c9672c853b9e006e28e6676573fc4c817e1c"
+  version "2.3.0"
+  sha256 "fbf22172b2e3a4e13ccf49c07cf07d02eca469c0f63c1ed062fbd099b0b0c9ba"
 
   url "https://github.com/Moonfin-Client/Moonfin-Core/releases/download/v#{version}/Moonfin_macOS_v#{version}.dmg",
       verified: "github.com/Moonfin-Client/Moonfin-Core/"
@@ -12,7 +12,4 @@ cask "moonfin" do
   
   app "Moonfin.app"
 
-  zap trash: [
-    "~/Library/Application Support/Moonfin"
-  ]
 end
