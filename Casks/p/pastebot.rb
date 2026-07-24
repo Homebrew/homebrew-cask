ee6eb676c74e8f4c30c5219f996d03aa3d4b4214
AltCode <altcode@vivaldi.net>
2026-07-24 10:42:41 +0200

    pastebot 3.0

--- Casks/p/pastebot.rb
+++ Casks/p/pastebot.rb
@@ -1,8 +1,8 @@
 cask "pastebot" do
-  version "2.4.8"
-  sha256 "3cd01fee94994cebce198ea81e48dce9b1aef86986836d17fc86f91fa9f047cc"
+  version "3.0,30000"
+  sha256 "22fd4d29482c0c04012ecbbdd918ca60b372d79dd7caabb2eb4671e9ddd69862"
 
-  url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg",
+  url "https://tapbots.net/pastebot#{version.major}/Pastebot.#{version.csv.second}.dmg",
       verified: "tapbots.net/"
   name "Pastebot"
   desc "Workflow application to improve productivity"
@@ -11,23 +11,38 @@ cask "pastebot" do
   livecheck do
     url "https://tapbots.net/pastebot#{version.major}/update.plist"
     strategy :xml do |xml|
-      xml.get_elements("//key[text()='shortVersion']").map { |item| item.next_element&.text&.strip }
+      versions = xml.get_elements("//key[text()='shortVersion']").map { |item| item.next_element&.text&.strip }
+      builds = xml.get_elements("//key[text()='version']").map { |item| item.next_element&.text&.strip }
+
+      versions.zip(builds).map do |version, build|
+        "#{version},#{build}"
+      end
     end
   end
 
   auto_updates true
-  depends_on :macos
+  depends_on macos: :tahoe
 
   app "Pastebot.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/pasteboot-tool.wrapper.sh"
+  binary shimscript, target: "pastebot-tool"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/bash
+      exec '#{appdir}/Pastebot.app/Contents/Helpers/pastebot-tool' "$@"
+    EOS
+  end
 
   uninstall quit: "com.tapbots.Pastebot#{version.major}Mac"
 
   zap trash: [
-    "~/Library/Application Scripts/9JTH7AWHE6.com.tapbots.Pastebot#{version.major}Mac",
+    "~/Library/Application Scripts/*.com.tapbots.Pastebot#{version.major}Mac",
     "~/Library/Application Scripts/com.tapbots.Pastebot#{version.major}Mac*",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tapbots.pastebot#{version.major}mac.launchhelper.sfl*",
     "~/Library/Containers/com.tapbots.Pastebot#{version.major}Mac*",
-    "~/Library/Group Containers/9JTH7AWHE6.com.tapbots.Pastebot#{version.major}Mac",
+    "~/Library/Group Containers/*.com.tapbots.Pastebot#{version.major}Mac",
     "~/Library/Preferences/com.tapbots.Pastebot#{version.major}Mac.plist",
   ]
 end
