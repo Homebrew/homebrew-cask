db3ed02e2c1c44f1c22857909f10d6a9b88a09d1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-24 18:44:22 +0000

    reader 0.1.2983

--- Casks/r/reader.rb
+++ Casks/r/reader.rb
@@ -1,6 +1,6 @@
 cask "reader" do
-  version "0.1.2980"
-  sha256 "b04e92f00ee5c23053cc68e3a8143a3a537c5c9eca42e1b16025cb0ebc5775b9"
+  version "0.1.2983"
+  sha256 "e678baade585b33501285c9bb04c4311d5329432f9b4d989e10e20511102bfab"
 
   url "https://github.com/readwiseio/reader-desktop-releases/releases/download/reader-desktop-v#{version}/Reader_#{version}_universal.dmg",
       verified: "github.com/readwiseio/reader-desktop-releases/"
