419a4c0608503cc1ec6f518a1a88c133dd7a7be7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 22:41:11 +0000

    t3-code@nightly 0.0.29-nightly.20260724.896

--- Casks/t/t3-code@nightly.rb
+++ Casks/t/t3-code@nightly.rb
@@ -1,7 +1,7 @@
 cask "t3-code@nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.29-nightly.20260724.893"
+  version "0.0.29-nightly.20260724.896"
 
   artifact = on_system_conditional linux: "T3-Code-#{version}-x86_64.AppImage",
                                    macos: "T3-Code-#{version}-#{arch}.dmg"
@@ -28,8 +28,8 @@ cask "t3-code@nightly" do
   end
 
   on_macos do
-    sha256 arm:   "3a0cb7701de292a2815e736e036af1ffb3064b26349cb13404c1a6749de630be",
-           intel: "be9c7b3e6ebaf9a0ff29a9747f21c99c678e1be9a8e7675eafb469b1757deadf"
+    sha256 arm:   "73ce79ff0c883841f65373e53061d88b2ff67919b14ea46596408717bc60f7ba",
+           intel: "c6b75b6f41b16650f926a11293577ef577dd173faa148e44ee4c1e1268fe5833"
 
     auto_updates true
     depends_on macos: :monterey
@@ -48,7 +48,7 @@ cask "t3-code@nightly" do
   end
 
   on_linux do
-    sha256 "161d3635326b9b1fe878a54906cb2bd1db6148f90ee75ff4fb1aa519decae69b"
+    sha256 "fbb46ca0ff8acf4d2e6730c1622fb5baf1d6f8bdd2ad4e39b66bf06312ec415e"
 
     depends_on arch: :x86_64
     app_image artifact, target: "T3 Code Nightly.AppImage"
