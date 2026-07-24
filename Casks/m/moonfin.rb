48a0bb1f8955359b74af2ad0e8e073071c60c82a
Xopez <28950736+xopez@users.noreply.github.com>
2026-07-24 19:40:31 +0200

    moonfin: fix GitHub release URL
    Remove 'v' prefix from the release tag in the download URL. GitHub release tags for this project use the format '2.3.0' rather than 'v2.3.0'.

--- Casks/m/moonfin.rb
+++ Casks/m/moonfin.rb
@@ -2,7 +2,7 @@ cask "moonfin" do
   version "2.3.0"
   sha256 "fbf22172b2e3a4e13ccf49c07cf07d02eca469c0f63c1ed062fbd099b0b0c9ba"
 
-  url "https://github.com/Moonfin-Client/Moonfin-Core/releases/download/v#{version}/Moonfin_macOS_v#{version}.dmg",
+  url "https://github.com/Moonfin-Client/Moonfin-Core/releases/download/#{version}/Moonfin_macOS_v#{version}.dmg",
       verified: "github.com/Moonfin-Client/Moonfin-Core/"
   name "Moonfin"
   desc "Premium media streaming clients for Jellyfin and Emby"
