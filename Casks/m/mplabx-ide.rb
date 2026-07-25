389f1ce5325b5e0a1b45cb6850b422fb14305e89
Bevan Kay <email@bevankay.me>
2026-07-25 10:19:52 +1000

    mplabx-ide: update `uninstall`

--- Casks/m/mplabx-ide.rb
+++ Casks/m/mplabx-ide.rb
@@ -60,7 +60,7 @@ cask "mplabx-ide" do
             },
             delete: [
               # The below version number needs to be updated manually each time this Cask is updated
-              "/Applications/microchip/mplabcomm/3.55.00",
+              "/Applications/microchip/mplabcomm/3.56.00",
               "/Applications/microchip/mplabx/#{version}",
             ],
             rmdir:  [
