b1cffba31432ee6e80a92c191cc52a292d8245fe
Niranjan <niranjan.sukumaran@gmail.com>
2026-07-25 23:37:29 +0530

    Add Mainspring 3.0.1

---
+++ Casks/m/mainspring.rb
@@ -0,0 +1,25 @@
+cask "mainspring" do
+  version "3.0.1"
+  sha256 "a9927f0758fd02f65fcdd5c2c8b05a1411cf4275a31f914486cdfd309fb914d8"
+
+  url "https://trymainspring.com/downloads/Mainspring-#{version}.pkg",
+      verified: "trymainspring.com/"
+  name "Mainspring"
+  desc "Utility for reversibly toggling hidden system settings"
+  homepage "https://trymainspring.com/"
+
+  livecheck do
+    url "https://trymainspring.com/downloads/Mainspring.pkg"
+    strategy :header_match do |headers|
+      headers["content-disposition"][/Mainspring[._-]v?(\d+(?:\.\d+)+)\.pkg/i, 1]
+    end
+  end
+
+  depends_on macos: :ventura
+
+  pkg "Mainspring-#{version}.pkg"
+
+  uninstall pkgutil: "app.mainspring.pkg"
+
+  zap trash: "~/Library/Preferences/app.mainspring.plist"
+end
