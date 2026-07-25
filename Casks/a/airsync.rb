1ce131a8da747c0b04a801e5cef95fb7c921031e
sameerasw <sameerasw.com@gmail.com>
2026-07-25 16:44:24 +0530

    Add airsync 4.0.0

---
+++ Casks/a/airsync.rb
@@ -0,0 +1,30 @@
+cask "airsync" do
+  version "4.0.0"
+  sha256 "c502e935191774c6980ab64b6f54a4f88774a1905fdcfc4f1749b87a145085c0"
+
+  url "https://github.com/sameerasw/airsync-mac/releases/download/v#{version}/AirSync.dmg"
+  name "AirSync"
+  desc "Your Android works the best with your mac"
+  homepage "https://github.com/sameerasw/airsync-mac"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sonoma
+  depends_on cask: "android-platform-tools"
+  depends_on formula: [
+    "media-control",
+    "scrcpy",
+  ]
+
+  app "AirSync.app"
+
+  zap trash: [
+    "~/Library/Application Support/AirSync",
+    "~/Library/Caches/com.sameerasw.airsync-mac",
+    "~/Library/Preferences/com.sameerasw.airsync-mac.plist",
+    "~/Library/Saved Application State/com.sameerasw.airsync-mac.savedState",
+  ]
+end
