5a1f674998eb9e595485e30e9b5ca24ca38d7a13
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-24 23:41:05 -0400

    block-buzz: add conflicts_with `buzz`

--- Casks/b/block-buzz.rb
+++ Casks/b/block-buzz.rb
@@ -15,6 +15,8 @@ cask "block-buzz" do
     sha256 arm:   "582f580c5baac5d09c6dc545e5285f6f336f275e4245ab293d17ae8d6b4ccd92",
            intel: "3903c8e35bb06f78d3f12be3a6e96cdbf6731586c55b9a8504ba9f15081d9f8c"
 
+    conflicts_with cask: "buzz"
+
     app "Buzz.app"
 
     uninstall quit: "xyz.block.buzz.app"
