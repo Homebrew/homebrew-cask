77db29a4c0b419997deeb5ce976cfd96e4bbd21e
Xopez <28950736+xopez@users.noreply.github.com>
2026-07-25 09:26:43 +0200

    Change name from 'moonfin' to 'Moonfin'

--- Casks/m/moonfin.rb
+++ Casks/m/moonfin.rb
@@ -3,7 +3,7 @@ cask "moonfin" do
   sha256 "fbf22172b2e3a4e13ccf49c07cf07d02eca469c0f63c1ed062fbd099b0b0c9ba"
 
   url "https://github.com/Moonfin-Client/Moonfin-Core/releases/download/#{version}/Moonfin_macOS_v#{version}.dmg"
-  name "moonfin"
+  name "Moonfin"
   desc "Media streaming client for Jellyfin and Emby"
   homepage "https://moonfin.io/"
 
