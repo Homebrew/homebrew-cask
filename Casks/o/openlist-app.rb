5fa4ffa50bcecf0caf8df705779798d4da1e572b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 05:15:19 +0000

    openlist-app 0.9.1

--- Casks/o/openlist-app.rb
+++ Casks/o/openlist-app.rb
@@ -1,9 +1,9 @@
 cask "openlist-app" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.9.0"
-  sha256 arm:   "7e113bb1bb689bfdc2b9529c1921152d8a6272aebf44f4002aa02695e842846a",
-         intel: "c38a317e956d319b45d1c3751fab543303facedb709b42269ca2f44eaab0ad10"
+  version "0.9.1"
+  sha256 arm:   "961ac84fdec16ea7e2734763f1528159cad32a82281bbb786e905748c74d802b",
+         intel: "4be5e48a20daac219ff354f9ca5e3aefdf0a7f9cb0c266bd5333951db6e24710"
 
   url "https://github.com/OpenListTeam/OpenList-Desktop/releases/download/v#{version}/OpenList-Desktop_#{version}_#{arch}.dmg"
   name "OpenList Desktop"
