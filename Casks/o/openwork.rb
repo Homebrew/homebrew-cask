88197d21bd75ada53fea6a6895587ed0039f407c
AltCode <altcode@vivaldi.net>
2026-07-27 19:20:38 +0200

    openwork: group/order missing stanzas

--- Casks/o/openwork.rb
+++ Casks/o/openwork.rb
@@ -1,31 +1,13 @@
 cask "openwork" do
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
   os macos: "mac", linux: "linux"
-
-  on_macos do
-    arch arm: "arm64", intel: "x64"
-  end
-  on_linux do
-    arch arm: "arm64", intel: "x86_64"
-  end
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "0.18.9"
   sha256 arm:          "1087e1bfffb3ab588abf3f0ac96da2768c064901a34a5c194eb27fb9925d2c4e",
          intel:        "96064ac30d874c4cd070c39b0f48a1ff3b862bb640fa6f455a065dcf4596eab0",
          arm64_linux:  "9acf721f40306a314130f6a702e0f0cfee5baf79cfd50ef0d6de4672e5e57c3e",
          x86_64_linux: "61761830a22a5dbacc2bf125907e693f35e5d7288928ccfccd45061316b5b529"
-
-  url_end = on_system_conditional linux: ".AppImage", macos: ".dmg"
-
-  url "https://github.com/different-ai/openwork/releases/download/v#{version}/openwork-#{os}-#{arch}-#{version}#{url_end}",
-      verified: "github.com/different-ai/openwork/"
-  name "OpenWork"
-  desc "Unofficial desktop GUI for OpenCode"
-  homepage "https://openworklabs.com/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   on_macos do
     auto_updates true
@@ -44,8 +26,17 @@ cask "openwork" do
       "~/Library/Preferences/com.differentai.openwork.ShipIt.plist",
     ]
   end
-
   on_linux do
     app_image "openwork-linux-#{arch}-#{version}.AppImage", target: "OpenWork.AppImage"
+  end
+
+  url "https://github.com/different-ai/openwork/releases/download/v#{version}/openwork-#{os}-#{arch}-#{version}.#{url_end}"
+  name "OpenWork"
+  desc "Unofficial desktop GUI for OpenCode"
+  homepage "https://openworklabs.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 end
