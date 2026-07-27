03a51e0dd8bfe066a390150bfe64e542bb852159
AltCode <altcode@vivaldi.net>
2026-07-27 19:21:58 +0200

    stirling-pdf: group/order missing stanzas

--- Casks/s/stirling-pdf.rb
+++ Casks/s/stirling-pdf.rb
@@ -1,13 +1,8 @@
 cask "stirling-pdf" do
+  arch intel: "x86_64"
+  os macos: "macos-universal.dmg", linux: "linux-#{arch}.AppImage"
+
   version "2.14.2"
-
-  url_end = on_system_conditional macos: "macos-universal.dmg", linux: "linux-x86_64.AppImage"
-
-  url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF-#{url_end}",
-      verified: "github.com/Stirling-Tools/Stirling-PDF/"
-  name "Stirling-PDF"
-  desc "PDF utility"
-  homepage "https://stirling.com/"
 
   on_macos do
     sha256 "d31e0f2e8f6d06d3fd199aa47804f1e05377f9d05680377390bfdb2ac949f67c"
@@ -23,11 +18,16 @@ cask "stirling-pdf" do
       "~/Library/WebKit/stirling.pdf.dev",
     ]
   end
-
   on_linux do
     sha256 "ad534e652e02536ebb1542c6f62d8a38f64a9eff7f8387525a2c8bd203c309f1"
+
     depends_on arch: :x86_64
 
-    app_image "Stirling-PDF-linux-x86_64.AppImage", target: "Stirling-PDF.AppImage"
+    app_image "Stirling-PDF-linux-#{arch}.AppImage", target: "Stirling-PDF.AppImage"
   end
+
+  url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF-#{os}"
+  name "Stirling-PDF"
+  desc "PDF utility"
+  homepage "https://stirling.com/"
 end
