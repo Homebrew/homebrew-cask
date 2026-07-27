1f3d2a72e473b24431ebf0fddb58e688987b3253
AltCode <altcode@vivaldi.net>
2026-07-27 19:23:31 +0200

    trae-cn: group/order missing stanzas

--- Casks/t/trae-cn.rb
+++ Casks/t/trae-cn.rb
@@ -1,26 +1,13 @@
 cask "trae-cn" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
+  url_end = on_system_conditional macos: "dmg", linux: "tar.gz"
 
   version "2.3.59356"
   sha256 arm:          "8e79061cdeec2c6f33b05d8aca9e4df59a5979027b567665989b51630d620d8e",
          intel:        "3514d4ad523d45b4746d837b04bdab38c10689e6d6ecba9e4017d9283c83e3e9",
          arm64_linux:  "9cf0998c01f4f758f20f0b9ee18de24cb4a26a16f942b6a8a8cd3bf8f293317d",
          x86_64_linux: "af696186d199a975c616c3682e58ae7846cead55c01d348c14c3994c8f36a6ce"
-
-  url_end = on_system_conditional linux: "tar.gz", macos: "dmg"
-
-  url "https://lf-cdn.trae.com.cn/obj/trae-com-cn/pkg/app/releases/stable/#{version}/#{os}/Trae_CN-#{os}-#{arch}.#{url_end}"
-  name "Trae CN"
-  desc "Adaptive AI IDE"
-  homepage "https://www.trae.com.cn/"
-
-  livecheck do
-    url "https://api.trae.ai/icube/api/v1/native/version/trae/cn/latest"
-    strategy :json do |json|
-      json.dig("data", "manifest", os, "version")
-    end
-  end
 
   on_macos do
     depends_on macos: :monterey
@@ -43,7 +30,6 @@ cask "trae-cn" do
       "~/Library/Saved Application State/cn.trae.app.savedState",
     ]
   end
-
   on_linux do
     binary "bin/trae-cn"
     bash_completion "#{staged_path}/resources/completions/bash/trae-cn"
@@ -54,6 +40,18 @@ cask "trae-cn" do
       "~/.local/share/Trae CN",
       "~/.trae-cn",
     ]
+  end
+
+  url "https://lf-cdn.trae.com.cn/obj/trae-com-cn/pkg/app/releases/stable/#{version}/#{os}/Trae_CN-#{os}-#{arch}.#{url_end}"
+  name "Trae CN"
+  desc "Adaptive AI IDE"
+  homepage "https://www.trae.com.cn/"
+
+  livecheck do
+    url "https://api.trae.ai/icube/api/v1/native/version/trae/cn/latest"
+    strategy :json do |json|
+      json.dig("data", "manifest", os, "version")
+    end
   end
 
   auto_updates true
