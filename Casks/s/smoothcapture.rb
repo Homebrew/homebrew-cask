339024557d6e3b70095290fd8588db99b2a5774c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 17:25:30 +0000

    smoothcapture 1.2.56

--- Casks/s/smoothcapture.rb
+++ Casks/s/smoothcapture.rb
@@ -1,6 +1,6 @@
 cask "smoothcapture" do
-  version "1.2.55"
-  sha256 "d1f5fedf0a4c5b5da181994616599eada942afbb039963b629374e3a8bf636ea"
+  version "1.2.56"
+  sha256 "cf5f7cdf6f83bb5015c9a5bd898320bdd838e53b8336b5147b84f35800734e95"
 
   url "https://download.smoothcapture.app/SmoothCapture-#{version}.dmg"
   name "Smooth Capture"
