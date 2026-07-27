13accc226c09cb9bea5d275a7698595e35bb0aef
AltCode <altcode@vivaldi.net>
2026-07-27 19:20:50 +0200

    quarto: group/order missing stanzas

--- Casks/q/quarto.rb
+++ Casks/q/quarto.rb
@@ -1,25 +1,13 @@
 cask "quarto" do
   arch arm: "arm64", intel: "amd64"
   os macos: "macos", linux: "linux-#{arch}"
-
-  container_ext = on_system_conditional macos: "pkg", linux: "tar.gz"
+  url_end = on_system_conditional macos: "pkg", linux: "tar.gz"
 
   version "1.10.18"
   sha256 arm:          "c57119e22012a4de14de0addf6da51db455082fcc2524a6dcba9696e7577cf4d",
          x86_64:       "c57119e22012a4de14de0addf6da51db455082fcc2524a6dcba9696e7577cf4d",
          arm64_linux:  "f6a07df68e25330b5df34f65d3df66bca605acce3b830c593a58e91884d4cf6c",
          x86_64_linux: "afad071b5bd22c02f2d300695743189d3650e0537a53073e654b630cff2b0c73"
-
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-#{os}.#{container_ext}",
-      verified: "github.com/quarto-dev/quarto-cli/"
-  name "quarto"
-  desc "Scientific and technical publishing system built on Pandoc"
-  homepage "https://www.quarto.org/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   on_macos do
     pkg "quarto-#{version}-macos.pkg"
@@ -32,8 +20,17 @@ cask "quarto" do
       "~/Library/Caches/quarto",
     ]
   end
-
   on_linux do
     binary "quarto-#{version}/bin/quarto"
+  end
+
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-#{os}.#{url_end}"
+  name "quarto"
+  desc "Scientific and technical publishing system built on Pandoc"
+  homepage "https://www.quarto.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 end
