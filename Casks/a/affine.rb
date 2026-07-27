6632469340f10892fec79bf0d2a52a549459555d
Dušan Tichý <tichy@ics.muni.cz>
2026-07-27 20:54:10 +0200

    affine 0.27.3 (linux support)

--- Casks/a/affine.rb
+++ Casks/a/affine.rb
@@ -2,10 +2,13 @@ cask "affine" do
   arch arm: "arm64", intel: "x64"
 
   version "0.27.3"
-  sha256 arm:   "bdeaa87ae8f200d63c738dfcdc3a8435fd8386e82cb28ef8b1ac5cc975207264",
-         intel: "41ca92935673f2e94f994cbc13af6698c6276886bd5debe09ba19d703eb04090"
 
-  url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-#{version}-stable-macos-#{arch}.zip",
+  artifact = on_system_conditional(
+    macos: "affine-#{version}-stable-macos-#{arch}.zip",
+    linux: "affine-#{version}-stable-linux-x64.appimage",
+  )
+
+  url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/#{artifact}",
       verified: "github.com/toeverything/AFFiNE/"
   name "AFFiNE"
   desc "Note editor and whiteboard"
@@ -17,14 +20,28 @@ cask "affine" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
 
-  app "AFFiNE.app"
+  on_macos do
+    sha256 arm:   "bdeaa87ae8f200d63c738dfcdc3a8435fd8386e82cb28ef8b1ac5cc975207264",
+           intel: "41ca92935673f2e94f994cbc13af6698c6276886bd5debe09ba19d703eb04090"
 
-  zap trash: [
-    "~/Library/Application Support/AFFiNE",
-    "~/Library/Logs/AFFiNE",
-    "~/Library/Preferences/pro.affine.app.plist",
-    "~/Library/Saved Application State/pro.affine.app.savedState",
-  ]
+    depends_on macos: :monterey
+
+    app "AFFiNE.app"
+
+    zap trash: [
+      "~/Library/Application Support/AFFiNE",
+      "~/Library/Logs/AFFiNE",
+      "~/Library/Preferences/pro.affine.app.plist",
+      "~/Library/Saved Application State/pro.affine.app.savedState",
+    ]
+  end
+
+  on_linux do
+    sha256 "bc132e3f8dea5a05c3943da79822d92ed7f3218456eb051bd81d11d4f0580114"
+
+    depends_on arch: :x86_64
+
+    app_image artifact, target: "AFFiNE.AppImage"
+  end
 end
