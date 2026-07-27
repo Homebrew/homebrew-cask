3d08a07cf7b5051b86d940ebb8d1a04daf7e8abb
AltCode <altcode@vivaldi.net>
2026-07-27 13:40:06 +0200

    qbittorrent: add Linux AppImage

--- Casks/q/qbittorrent.rb
+++ Casks/q/qbittorrent.rb
@@ -1,4 +1,8 @@
 cask "qbittorrent" do
+  arch intel: "x86_64"
+  os macos: "mac", linux: "appimage"
+  url_end = on_system_conditional macos: ".dmg", linux: "_#{arch}.AppImage"
+
   on_monterey :or_older do
     on_catalina :or_older do
       version "4.6.7"
@@ -18,31 +22,43 @@ cask "qbittorrent" do
     sha256 "9e37f6c7ff848c7bdd3c10167614c0cb78c00e2ddcc323f1ad3ac6c008a0481f"
 
     livecheck do
-      url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
-      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+      url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-#{os}"
+      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)#{url_end}}i)
     end
   end
+  on_macos do
+    disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
-      verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
+    # Renamed for consistency: app name is different in the Finder and in a shell.
+    app "qbittorrent.app", target: "qBittorrent.app"
+
+    zap trash: [
+      "~/.config/qBittorrent",
+      "~/Library/Application Support/qBittorrent",
+      "~/Library/Caches/qBittorrent",
+      "~/Library/Preferences/org.qbittorrent.qBittorrent.plist",
+      "~/Library/Preferences/qBittorrent",
+      "~/Library/Saved Application State/org.qbittorrent.qBittorrent.savedState",
+    ]
+  end
+  on_linux do
+    version "5.2.3"
+    sha256 "c1467a713929323aaf253e021449992ac299a6c830a933643b023007d8641ed0"
+
+    livecheck do
+      url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-#{os}"
+      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)#{url_end}}i)
+    end
+
+    depends_on arch: :x86_64
+
+    app_image "qbittorrent-#{version}_#{arch}.AppImage", target: "qBittorrent.AppImage"
+  end
+
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-#{os}/qbittorrent-#{version}/qbittorrent-#{version}#{url_end}"
   name "qBittorrent"
   desc "Peer to peer Bitorrent client"
   homepage "https://www.qbittorrent.org/"
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
   conflicts_with cask: "qbittorrent@lt20"
-  depends_on :macos
-
-  # Renamed for consistency: app name is different in the Finder and in a shell.
-  app "qbittorrent.app", target: "qBittorrent.app"
-
-  zap trash: [
-    "~/.config/qBittorrent",
-    "~/Library/Application Support/qBittorrent",
-    "~/Library/Caches/qBittorrent",
-    "~/Library/Preferences/org.qbittorrent.qBittorrent.plist",
-    "~/Library/Preferences/qBittorrent",
-    "~/Library/Saved Application State/org.qbittorrent.qBittorrent.savedState",
-  ]
 end
