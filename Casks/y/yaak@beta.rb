303f3b066d79ac5d75f617b82ed8954e39d02adb
AltCode <altcode@vivaldi.net>
2026-07-27 19:23:52 +0200

    yaak@beta: group/order missing stanzas

--- Casks/y/yaak@beta.rb
+++ Casks/y/yaak@beta.rb
@@ -1,10 +1,7 @@
 cask "yaak@beta" do
-  on_macos do
-    arch arm: "aarch64", intel: "x64"
-  end
-  on_linux do
-    arch arm: "aarch64", intel: "amd64"
-  end
+  arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
+  os macos: "Yaak", linux: "yaak"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "2026.5.0"
   sha256 arm:          "c191f08eb7796475a6fadbe22b0b38bffb2fc211aa5a591d4061d7a0dc4e427a",
@@ -12,11 +9,25 @@ cask "yaak@beta" do
          arm64_linux:  "237820abde1836b338c70c6f5a1dc1f331f47f2950c7aa6e7c39283f41b7d18c",
          x86_64_linux: "fbebec36428fd4528812261b64c23a14de42e80e9e18363fc4ab800c103d556d"
 
-  artifact = on_system_conditional macos: "Yaak_#{version}_#{arch}.dmg",
-                                   linux: "yaak_#{version}_#{arch}.AppImage"
+  on_macos do
+    auto_updates true
+    depends_on macos: :ventura
 
-  url "https://github.com/mountain-loop/yaak/releases/download/v#{version}/#{artifact}",
-      verified: "github.com/mountain-loop/yaak/"
+    app "yaak.app"
+
+    zap trash: [
+      "~/Library/Application Support/app.yaak.desktop",
+      "~/Library/Caches/app.yaak.desktop",
+      "~/Library/Logs/app.yaak.desktop",
+      "~/Library/Saved Application State/app.yaak.desktop.savedState",
+      "~/Library/Webkit/app.yaak.desktop",
+    ]
+  end
+  on_linux do
+    app_image "yaak_#{version}_#{arch}.AppImage", target: "Yaak.AppImage"
+  end
+
+  url "https://github.com/mountain-loop/yaak/releases/download/v#{version}/#{os}_#{version}_#{arch}.#{url_end}"
   name "Yaak Beta"
   desc "REST, GraphQL and gRPC client"
   homepage "https://yaak.app/"
@@ -42,23 +53,4 @@ cask "yaak@beta" do
   end
 
   conflicts_with cask: "yaak"
-
-  on_macos do
-    auto_updates true
-    depends_on macos: :ventura
-
-    app "yaak.app"
-
-    zap trash: [
-      "~/Library/Application Support/app.yaak.desktop",
-      "~/Library/Caches/app.yaak.desktop",
-      "~/Library/Logs/app.yaak.desktop",
-      "~/Library/Saved Application State/app.yaak.desktop.savedState",
-      "~/Library/Webkit/app.yaak.desktop",
-    ]
-  end
-
-  on_linux do
-    app_image artifact, target: "Yaak.AppImage"
-  end
 end
