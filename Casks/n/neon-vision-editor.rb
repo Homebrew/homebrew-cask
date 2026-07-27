eaa3bc0cf74e274081356a7f1fcfda7984dded3d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-07-26 23:51:47 -0400

    neon-vision-editor: add CI zap paths

--- Casks/n/neon-vision-editor.rb
+++ Casks/n/neon-vision-editor.rb
@@ -13,9 +13,12 @@ cask "neon-vision-editor" do
   app "Neon Vision Editor.app"
 
   zap trash: [
+    "~/Library/Application Scripts/group.h3p.Neon-Vision-Editor",
+    "~/Library/Application Scripts/h3p.Neon-Vision-Editor*",
     "~/Library/Application Support/NeonVisionEditor",
     "~/Library/Caches/h3p.Neon-Vision-Editor",
     "~/Library/Containers/h3p.Neon-Vision-Editor",
+    "~/Library/Group Containers/group.h3p.Neon-Vision-Editor",
     "~/Library/Logs/NeonVisionEditorUpdater.log",
     "~/Library/Preferences/h3p.Neon-Vision-Editor.plist",
   ]
