59f680248aa362c0b3c029e8ae36dd9764bcfd62
AltCode <altcode@vivaldi.net>
2026-07-27 19:16:02 +0200

    block-buzz: group/order missing stanzas

--- Casks/b/block-buzz.rb
+++ Casks/b/block-buzz.rb
@@ -1,15 +1,8 @@
 cask "block-buzz" do
-  arch arm: "aarch64", intel: "x64"
+  arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
+  os macos: "dmg", linux: "AppImage"
 
   version "0.4.26"
-
-  artifact = on_system_conditional linux: "Buzz_#{version}_amd64.AppImage",
-                                   macos: "Buzz_#{version}_#{arch}.dmg"
-
-  url "https://github.com/block/buzz/releases/download/v#{version}/#{artifact}"
-  name "Buzz"
-  desc "Workspace for humans and AI agents"
-  homepage "https://github.com/block/buzz"
 
   on_macos do
     sha256 arm:   "abc60e0e44c9bc9b0ce7d668651a955a87cf95390f66bb9b71d36f9c0e5c25a3",
@@ -29,12 +22,16 @@ cask "block-buzz" do
         ],
         rmdir: "~/.buzz"
   end
-
   on_linux do
     sha256 "5ce7a56ee51db66c3d219f4f95bd92021fc689563ea6a425441751f68582820f"
 
     depends_on arch: :x86_64
 
-    app_image artifact, target: "Buzz.AppImage"
+    app_image "Buzz_#{version}_#{arch}.AppImage", target: "Buzz.AppImage"
   end
+
+  url "https://github.com/block/buzz/releases/download/v#{version}/Buzz_#{version}_#{arch}.#{os}"
+  name "Buzz"
+  desc "Workspace for humans and AI agents"
+  homepage "https://github.com/block/buzz"
 end
