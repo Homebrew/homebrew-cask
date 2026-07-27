fbead01577be7208c73eaa3d878dd995eedbeace
AltCode <altcode@vivaldi.net>
2026-07-27 19:22:08 +0200

    t3-code: group/order missing stanzas

--- Casks/t/t3-code.rb
+++ Casks/t/t3-code.rb
@@ -1,21 +1,8 @@
 cask "t3-code" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
+  os macos: "dmg", linux: "AppImage"
 
   version "0.0.29"
-
-  artifact = on_system_conditional linux: "T3-Code-#{version}-x86_64.AppImage",
-                                   macos: "T3-Code-#{version}-#{arch}.dmg"
-
-  url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/#{artifact}",
-      verified: "github.com/pingdotgg/t3code/"
-  name "T3 Code"
-  desc "Minimal GUI for AI code agents"
-  homepage "https://t3.codes/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   on_macos do
     sha256 arm:   "f50f4f33dc1b70369a16c97309bfc9a534a9eaddabad7b8040b2e891538ddf32",
@@ -36,11 +23,21 @@ cask "t3-code" do
       "~/Library/Saved Application State/com.t3tools.t3code.savedState",
     ]
   end
-
   on_linux do
     sha256 "4274f6b6aa36ef1946d4a1c95e32653481007c00d09df727aa31aa6e9c9ffbc9"
 
     depends_on arch: :x86_64
-    app_image artifact, target: "T3 Code.AppImage"
+
+    app_image "T3-Code-#{version}-#{arch}.AppImage", target: "T3 Code.AppImage"
+  end
+
+  url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/T3-Code-#{version}-#{arch}.#{os}"
+  name "T3 Code"
+  desc "Minimal GUI for AI code agents"
+  homepage "https://t3.codes/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 end
