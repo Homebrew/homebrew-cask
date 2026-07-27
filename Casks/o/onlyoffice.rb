e94d81aa7ebb115daf7992d511fa116eaa9703ce
AltCode <altcode@vivaldi.net>
2026-07-27 19:20:22 +0200

    onlyoffice: group/order missing stanzas

--- Casks/o/onlyoffice.rb
+++ Casks/o/onlyoffice.rb
@@ -1,30 +1,12 @@
 cask "onlyoffice" do
-  on_macos do
-    arch arm: "arm", intel: "x86_64"
-  end
-  on_linux do
-    arch arm: "arm64", intel: "x86_64"
-  end
+  arch arm: on_system_conditional(macos: "arm", linux: "arm64"), intel: "x86_64"
+  os macos: "ONLYOFFICE-#{arch}.dmg", linux: "DesktopEditors-#{arch}.AppImage"
 
   version "9.4.0"
   sha256 arm:          "e965be2222609add6b5a70baa2a8cdb599402491fb2925825d9039dcb154beb4",
          x86_64:       "43ac517493c0c316f268ce4b7dc3810b77a7aefe83c0edc1655476d9f21681d2",
          arm64_linux:  "7dfc2fa195ff9912a4022841613b9a99fdba2a238792648f257007a7df887b7c",
          x86_64_linux: "f5faf24552665262fe94486a510b997d2e5a24bde14df94c802c79bf17f9254c"
-
-  artifact = on_system_conditional macos: "ONLYOFFICE-#{arch}.dmg",
-                                   linux: "DesktopEditors-#{arch}.AppImage"
-
-  url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v#{version}/#{artifact}",
-      verified: "github.com/ONLYOFFICE/DesktopEditors/"
-  name "ONLYOFFICE"
-  desc "Document editor"
-  homepage "https://www.onlyoffice.com/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   on_macos do
     auto_updates true
@@ -40,8 +22,17 @@ cask "onlyoffice" do
       "~/Library/Preferences/asc.onlyoffice.ONLYOFFICE.plist",
     ]
   end
-
   on_linux do
-    app_image artifact, target: "ONLYOFFICE.AppImage"
+    app_image "DesktopEditors-#{arch}.AppImage", target: "ONLYOFFICE.AppImage"
+  end
+
+  url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v#{version}/#{os}"
+  name "ONLYOFFICE"
+  desc "Document editor"
+  homepage "https://www.onlyoffice.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 end
