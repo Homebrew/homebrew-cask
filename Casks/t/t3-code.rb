4c2374b05cdec72f011208eeb21fded9ad6dd44e
AltCode <altcode@vivaldi.net>
2026-07-28 21:05:26 +0200

    t3-code: update sha256

--- Casks/t/t3-code.rb
+++ Casks/t/t3-code.rb
@@ -5,8 +5,8 @@ cask "t3-code" do
   version "0.0.29"
 
   on_macos do
-    sha256 arm:   "f50f4f33dc1b70369a16c97309bfc9a534a9eaddabad7b8040b2e891538ddf32",
-           intel: "e1f89635435922c87150da41d2d69abe5d69b60fba6678be62c36fae24af7a05"
+    sha256 arm:   "ec0a309a67b85a9973d5e477eb4a19781d8d7d100f192594a8956f142618ea3b",
+           intel: "19cb6234228784054f543c82b41a06d35376c685474b3770f65544896eba83d4"
 
     auto_updates true
     depends_on macos: :monterey
@@ -24,7 +24,7 @@ cask "t3-code" do
     ]
   end
   on_linux do
-    sha256 "4274f6b6aa36ef1946d4a1c95e32653481007c00d09df727aa31aa6e9c9ffbc9"
+    sha256 "38030aa4f0fe0131426978787442ca903b40b2f928d52a11d16893b9d9ca8cf4"
 
     depends_on arch: :x86_64
 
