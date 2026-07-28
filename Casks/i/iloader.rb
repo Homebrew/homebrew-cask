1b3de29bb193ed36456005e8ccd5adb10a314457
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 01:20:31 +0000

    iloader 2.2.9

--- Casks/i/iloader.rb
+++ Casks/i/iloader.rb
@@ -5,7 +5,7 @@ cask "iloader" do
     arch arm: "aarch64", intel: "amd64"
   end
 
-  version "2.2.8"
+  version "2.2.9"
 
   artifact = on_system_conditional linux: "iloader-#{os}-#{arch}.AppImage",
                                    macos: "iloader-#{os}-universal.dmg"
@@ -22,7 +22,7 @@ cask "iloader" do
   end
 
   on_macos do
-    sha256 "0845132b12f3ab20a6e88fde802b63a04eab32c979edaafca5fffd592e3dc137"
+    sha256 "83c9608a323d354400a2451aaef764ec6a0fe5632155d72d498b3b953f630e5e"
 
     auto_updates true
     app "iloader.app"
@@ -31,8 +31,8 @@ cask "iloader" do
   end
 
   on_linux do
-    sha256 arm64_linux:  "599b0c33c6bd3bb15cfa38a7fe3b85929b6ad353757c41e437ec44f9b932274e",
-           x86_64_linux: "84a1a557bf2c35f4bc8084977fe74bea4e2e560253aee5a9e1501d7c49664ec1"
+    sha256 arm64_linux:  "d35df0f3bdfee88adfc333ae9721967986f1fd6287c0eef59fef0f1aacd87229",
+           x86_64_linux: "b775b9edd5c09a3829d9530834368f0b44d5fbc88392a08d9260cb76d0a5b09f"
 
     app_image artifact, target: "iloader.AppImage"
   end
