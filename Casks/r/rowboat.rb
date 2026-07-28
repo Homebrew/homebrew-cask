692c1f8359f6748ed29e45de7e958e350abcb8b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 17:07:22 +0000

    rowboat 0.8.2

--- Casks/r/rowboat.rb
+++ Casks/r/rowboat.rb
@@ -1,9 +1,9 @@
 cask "rowboat" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.8.0"
-  sha256 arm:   "d344c75b48f2c7b8ee5443f44b30b5abd654cfdf2e6eed00a83dc0b4b3a0a43d",
-         intel: "2b899bd7099f785e6cc82e9e6b416c291b7ecc40ca256d806bed06ee38e2a686"
+  version "0.8.2"
+  sha256 arm:   "c7c9f0b7a04a2e79a1ce012f2b7c1535ffed4b72f1630e7adb860f5f279475fa",
+         intel: "759c675d93cd00c099b0490b65cfba7aed1e9c31a6a8a135554d52e22ead75fc"
 
   url "https://github.com/rowboatlabs/rowboat/releases/download/v#{version}/Rowboat-darwin-#{arch}-#{version}.zip",
       verified: "github.com/rowboatlabs/rowboat/"
