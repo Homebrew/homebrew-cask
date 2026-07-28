edd56c60e2aa0986aa47e88c112a523b66b83d5b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 14:03:23 +0000

    sakura 3.2.0

--- Casks/s/sakura.rb
+++ Casks/s/sakura.rb
@@ -1,9 +1,9 @@
 cask "sakura" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.1.8"
-  sha256 arm:   "874256ff67f9f70ee64cf4a6e22010929ae1baba7a93e0e8b8069bd41b19f5c1",
-         intel: "0d657c20de7e363139600dd1a705d3a90c01f0fd3b0d42628c12a5508b69fcc7"
+  version "3.2.0"
+  sha256 arm:   "ecc60a99c75b3c83e35c7a9c470576dd27e80b30bbe0d91255341fec69df8957",
+         intel: "9083d65ccc3be16bbe667edcd3d67120404e395a39202cf680483d685e0a248d"
 
   url "https://nya.globalslb.net/natfrp/client/launcher-macos/#{version}/SakuraLauncher_macOS_#{arch}.dmg",
       verified: "nya.globalslb.net/natfrp/client/launcher-macos/"
