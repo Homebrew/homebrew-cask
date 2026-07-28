1b45dfc7c9cbaca6bff0cbf5bd0e4979049e0199
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 19:35:57 +0000

    nvidia-nsight-systems 2026.4.1.191-3860507

--- Casks/n/nvidia-nsight-systems.rb
+++ Casks/n/nvidia-nsight-systems.rb
@@ -1,9 +1,9 @@
 cask "nvidia-nsight-systems" do
   arch arm: "-arm64"
 
-  version "2026.4.1.174-3856861"
-  sha256 arm:   "bd089e2394d79369b57867ee3384efed3bcf03471afec31a5711a7c51d11a9ab",
-         intel: "e3061eb474bdcfea237750875079bf5b197656b9a5f67547ad10b8f1564fca48"
+  version "2026.4.1.191-3860507"
+  sha256 arm:   "4b510c087f325c3464f1334d7c86773a55d30eab75d7a043ac0a632ff1dcad44",
+         intel: "82bf6e7eaebd87d11f19da07a8c8e5631cb6b20c3d63eb3c2b4481c19573106f"
 
   url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/#{version.major_minor.dots_to_underscores}/NsightSystems-macos#{arch}-public-#{version}.dmg"
   name "NVIDIA Nsight Systems"
