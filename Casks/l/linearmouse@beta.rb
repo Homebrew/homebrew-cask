be51dc11dce004520318add69a86bea8cc8146e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 16:50:03 +0000

    linearmouse@beta 0.11.4-beta.5

--- Casks/l/linearmouse@beta.rb
+++ Casks/l/linearmouse@beta.rb
@@ -1,6 +1,6 @@
 cask "linearmouse@beta" do
-  version "0.11.4-beta.4"
-  sha256 "5c7c700f5b574e5c2d1571134354d22b002600280a1effffea2dd2c88cc82353"
+  version "0.11.4-beta.5"
+  sha256 "d026be0bc6934ad36547924b278355348a25f34b8903df3ba61c4367835a0aff"
 
   url "https://dl.linearmouse.org/v#{version}/LinearMouse.dmg"
   name "LinearMouse"
