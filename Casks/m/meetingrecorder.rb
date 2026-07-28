cc1de62539af89a8ed1f40f895d83d4c056f50be
Aleksandr <aleksandrmishin@Aleksandrs-MacBook-Air.local>
2026-07-28 17:23:17 +0200

    Add MeetingRecorder

---
+++ Casks/m/meetingrecorder.rb
@@ -0,0 +1,27 @@
+cask "meetingrecorder" do
+  version "1.6.2"
+  sha256 "d1b09afa21f2c4ab90411ab777b7cffc27452fc2d494a8478636be9e2679637d"
+
+  url "https://meetingsrecorder.com/downloads/MeetingRecorder-#{version}.dmg"
+  name "MeetingRecorder"
+  desc "Recorder for meetings capturing mic and system audio, no bot in the call"
+  homepage "https://meetingsrecorder.com/"
+
+  livecheck do
+    url "https://github.com/emishin/meetingrecorder-updates/releases"
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: :sequoia
+  depends_on arch: :arm64
+
+  app "MeetingRecorder.app"
+
+  zap trash: [
+    "~/Library/Application Support/MeetingRecorder",
+    "~/Library/Caches/com.meetingrecorder.app",
+    "~/Library/HTTPStorages/com.meetingrecorder.app",
+    "~/Library/Preferences/com.meetingrecorder.app.plist",
+  ]
+end
