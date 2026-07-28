0122a7d824f4707f9e6d7a440d37669ebf0c1850
AltCode <altcode@vivaldi.net>
2026-07-29 00:15:36 +0200

    devtunnel: update url

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -3,26 +3,12 @@ cask "devtunnel" do
   os macos: "osx", linux: "linux"
 
   version "1.0.1972+07cc55c789"
-  sha256 arm:          "b350b7016b76c5565e633481ce53bfc9ffd7564d8abdf657235e70f7ce390983",
-         intel:        "af16f2dff43a70629af305983a5af8f8cd7a7ab3a344948ae61ba21a1ae557b3",
+  sha256 arm:          "ef4197d8a1d6564fbec87c8752f04a7573d13810d527d547084e232b71ee7b5a",
+         intel:        "283ab1f20f3e6ebf6c3434d3b14e1b909f9b04822069d99ee19d33201ffd3710",
          arm64_linux:  "e23f23e4e68c46ee08a1e9db5d62e5c00785c66cb70236597bee0e0a64120512",
          x86_64_linux: "6380d3e54c81e7e2541900bec3d4f2785dbf81f17cd81effbd54d98f11a874b9"
 
-  on_macos do
-    url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/#{os}-#{arch}-devtunnel-zip",
-        verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
-
-    binary "devtunnel"
-  end
-  on_linux do
-    url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/#{os}-#{arch}-devtunnel",
-        verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
-
-    container type: :naked
-
-    binary "#{os}-#{arch}-devtunnel", target: "devtunnel"
-  end
-
+  url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/#{os}-#{arch}-devtunnel"
   name "Microsoft Dev Tunnels"
   desc "Provides developers secure tunnels to share local web services"
   homepage "https://aka.ms/devtunnels/docs"
@@ -33,6 +19,10 @@ cask "devtunnel" do
       json["version"]
     end
   end
+
+  container type: :naked
+
+  binary "#{os}-#{arch}-devtunnel", target: "devtunnel"
 
   # No zap stanza required
 end
