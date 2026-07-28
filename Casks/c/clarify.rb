9886c694025b5f60acec51effa55a8fa0c4b5531
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 10:23:39 +0000

    clarify 1.6.9

--- Casks/c/clarify.rb
+++ Casks/c/clarify.rb
@@ -1,6 +1,6 @@
 cask "clarify" do
-  version "1.6.8"
-  sha256 "ead365d5a9038241b73f6d1f079da971076dd81a574e662998372e57d7bbf5ec"
+  version "1.6.9"
+  sha256 "1529bb351b13cfe3f2d5d1c3b5a85043152ed66c5b7cf81c676e8aad9e562ad1"
 
   url "https://github.com/clarifyhq/desktop-app-releases/releases/download/v#{version}/Clarify.dmg",
       verified: "github.com/clarifyhq/desktop-app-releases/"
