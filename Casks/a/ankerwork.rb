1fd3e55e0440f444d9920b1605fe3c993d9bbc0e
Sakutaro Shiina <companyemailetc@gmail.com>
2026-07-29 15:29:54 +0900

    ankerwork: update livecheck

--- Casks/a/ankerwork.rb
+++ Casks/a/ankerwork.rb
@@ -4,8 +4,8 @@ cask "ankerwork" do
   version "3.0.4"
   sha256 :no_check
 
-  url "https://ankerwork.s3.amazonaws.com/electron/AnkerWork-Setup-#{arch}.dmg",
-      verified: "ankerwork.s3.amazonaws.com/electron/"
+  url "https://ankerwork.s3.us-west-2.amazonaws.com/electron/AnkerWork-Setup-#{arch}.dmg",
+      verified: "ankerwork.s3.us-west-2.amazonaws.com/electron/"
   name "AnkerWork"
   desc "Webcam & audio device software"
   homepage "https://us.ankerwork.com/pages/download-software"
