fa60b0093e323b1fbb04d1355a4da9eec1d04b7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-29 14:23:59 +0000

    vassal 3.7.26

--- Casks/v/vassal.rb
+++ Casks/v/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.7.24"
-  sha256 "b94d98bffc55da9338ace9bbf213080ce03158a5b8de9b5d2fe925f6437d9a9b"
+  version "3.7.26"
+  sha256 "e46f0957b820bc284bb79de5a325448dbf5c47e0fdebaa2aecff165f9cb7e3e0"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-universal.dmg",
       verified: "github.com/vassalengine/vassal/"
