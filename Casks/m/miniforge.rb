c2f6ba7fc58333b06c1afbf6c18a88b16940fc30
Mike McQuaid <mike@mikemcquaid.com>
2026-07-29 09:48:55 +0100

    miniforge: use install step tokens.
    - Replace Ruby interpolation with serialised install-time tokens.
    - Keep flight-step paths portable through the JSON API.

--- Casks/m/miniforge.rb
+++ Casks/m/miniforge.rb
@@ -29,15 +29,15 @@ cask "miniforge" do
   binary "#{caskroom_path}/base/condabin/mamba"
 
   postflight_steps do
-    if_path_exists "{{temp}}/#{token}-envs" do
+    if_path_exists "{{temp}}/{{token}}-envs" do
       remove "base/envs", base: :caskroom_path, recursive: true
-      move "{{temp}}/#{token}-envs", "base/envs", target_base: :caskroom_path
+      move "{{temp}}/{{token}}-envs", "base/envs", target_base: :caskroom_path
     end
   end
 
   uninstall_preflight_steps do
     if_path_exists "{{caskroom_path}}/base/envs" do
-      move "base/envs", "{{temp}}/#{token}-envs", source_base: :caskroom_path
+      move "base/envs", "{{temp}}/{{token}}-envs", source_base: :caskroom_path
     end
   end
 
