596675cca999b1c00a4a021fe22e064b7eb8efdc
AltCode <altcode@vivaldi.net>
2026-07-29 09:56:31 +0200

    affine: group/order `on_<system>` stanzas

--- Casks/a/affine.rb
+++ Casks/a/affine.rb
@@ -1,23 +1,9 @@
 cask "affine" do
   arch arm: "arm64", intel: "x64"
   os macos: "macos", linux: "linux"
+  url_end = on_system_conditional macos: "zip", linux: "appimage"
 
   version "0.27.3"
-
-  url_end = on_system_conditional macos: ".zip", linux: ".appimage"
-
-  url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-#{version}-stable-#{os}-#{arch}#{url_end}",
-      verified: "github.com/toeverything/AFFiNE/"
-  name "AFFiNE"
-  desc "Note editor and whiteboard"
-  homepage "https://affine.pro/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  auto_updates true
 
   on_macos do
     sha256 arm:   "bdeaa87ae8f200d63c738dfcdc3a8435fd8386e82cb28ef8b1ac5cc975207264",
@@ -34,7 +20,6 @@ cask "affine" do
       "~/Library/Saved Application State/pro.affine.app.savedState",
     ]
   end
-
   on_linux do
     sha256 "bc132e3f8dea5a05c3943da79822d92ed7f3218456eb051bd81d11d4f0580114"
 
@@ -42,4 +27,16 @@ cask "affine" do
 
     app_image "affine-#{version}-stable-linux-#{arch}.appimage", target: "AFFiNE.AppImage"
   end
+
+  url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-#{version}-stable-#{os}-#{arch}.#{url_end}"
+  name "AFFiNE"
+  desc "Note editor and whiteboard"
+  homepage "https://affine.pro/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
 end
