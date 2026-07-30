14a3f285c0fc1ab9678af850c9e14f67043c32c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 16:34:44 +0000

    freeshow@beta 1.6.5-beta.1

--- Casks/f/freeshow@beta.rb
+++ Casks/f/freeshow@beta.rb
@@ -1,9 +1,9 @@
 cask "freeshow@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.4"
-  sha256 arm:   "27ffec3b799395a252ad9125d55ecf7333e82e54840b0ebc80117b3a41e93cd5",
-         intel: "328ed0bb105f7ea98c0a09c12c7ddc859bf7eca8ef46f17a90bd40d89e9a7aaf"
+  version "1.6.5-beta.1"
+  sha256 arm:   "f3b7de26c5ad744185086bc1d02f0a2180c54b871a072763bf4197cb9db30d13",
+         intel: "6a40d11157aa39093002853b862d9713d486abf253fd7967f1037c4f9528dda6"
 
   url "https://github.com/ChurchApps/FreeShow/releases/download/v#{version}/FreeShow-#{version}-#{arch}.zip",
       verified: "github.com/ChurchApps/"
