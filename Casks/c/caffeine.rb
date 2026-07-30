f6ec65a116e32bcecf5ec51c109b15ba55e39ebd
Ryan Stoffel <ryan@rstoffel.com>
2026-07-29 19:19:51 -0700

    caffeine 1.0.0 (new cask)

--- Casks/c/caffeine.rb
+++ Casks/c/caffeine.rb
@@ -1,24 +1,15 @@
 cask "caffeine" do
-  version "1.1.4"
-  sha256 "1ad34c3299a0c866873c4feb432c3e11186375adb6f43c5f481ae9d46d06d723"
+  version "1.0.0"
+  sha256 "c8fae5b494450884038a508d0315af9b2d8bfc8bff1dddea1e09773f069c9ea1"
 
-  url "https://github.com/IntelliScape/caffeine/releases/download/#{version}/Caffeine.dmg",
-      verified: "github.com/IntelliScape/caffeine/"
+  url "https://github.com/RyanStoffel/caffeine/releases/download/v#{version}/Caffeine.zip"
   name "Caffeine"
-  desc "Utility that prevents the system from going to sleep"
-  homepage "https://intelliscapesolutions.com/apps/caffeine"
-
-  conflicts_with cask: "domzilla-caffeine"
-  depends_on macos: :big_sur
+  desc "Tiny menu bar app to keep your Mac's display awake"
+  homepage "https://github.com/RyanStoffel/caffeine"
 
   app "Caffeine.app"
 
-  uninstall quit: "com.intelliscapesolutions.caffeine"
-
   zap trash: [
-    "~/Library/Application Support/com.intelliscapesolutions.caffeine",
-    "~/Library/Caches/com.intelliscapesolutions.caffeine",
-    "~/Library/HTTPStorages/com.intelliscapesolutions.caffeine.binarycookies",
-    "~/Library/Preferences/com.intelliscapesolutions.caffeine.plist",
+    "~/Library/Preferences/com.ryanstoffel.caffeine.plist",
   ]
 end
