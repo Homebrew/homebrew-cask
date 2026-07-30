da487f4d3d0d68bb665a26374cd2ad8202197fff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 20:10:02 +0000

    zen@twilight 1.22t,20260729112005

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -2,7 +2,7 @@ cask "zen@twilight" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: ".macos-universal.dmg", linux: "-#{arch}.AppImage"
 
-  version "1.22t,20260727112205"
+  version "1.22t,20260729112005"
   sha256 :no_check
 
   on_macos do
