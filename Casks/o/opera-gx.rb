ddd139eb974ae815dc04d23c47291b519e83b9d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 08:01:21 +0000

    opera-gx 133.0.5932.109

--- Casks/o/opera-gx.rb
+++ Casks/o/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "133.0.5932.81"
-  sha256 "5284d49ad8f7570300923142875356083aa501d5eea3b06b10afa5a2f6192266"
+  version "133.0.5932.109"
+  sha256 "be16a0b9777ada31c9aabe592221da315b7c733d868f5929e6538ffe569a628b"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
