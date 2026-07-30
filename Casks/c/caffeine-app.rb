1e5e6406955694566369b16efa3e6216cc3fdfac
Ryan Stoffel <ryan@rstoffel.com>
2026-07-29 19:50:56 -0700

    caffeine-app 1.0.0 (new cask)

---
+++ Casks/c/caffeine-app.rb
@@ -0,0 +1,15 @@
+cask "caffeine-app" do
+  version "1.0.0"
+  sha256 "f4d1337c41b1a323cec901dfac1a3abf6ad3c3a98729987684b1be277f513403"
+
+  url "https://github.com/RyanStoffel/caffeine/releases/download/v#{version}/Caffeine.zip"
+  name "Caffeine"
+  desc "Tiny menu bar app to keep display awake"
+  homepage "https://github.com/RyanStoffel/caffeine"
+
+  depends_on macos: :ventura
+
+  app "Caffeine.app"
+
+  zap trash: "~/Library/Preferences/com.ryanstoffel.caffeine.plist"
+end
