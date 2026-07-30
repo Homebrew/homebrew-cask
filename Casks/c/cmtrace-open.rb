87eaa0a5ba6c8e38600c494777485b428ee79ca0
Adam <27519+adamgell@users.noreply.github.com>
2026-07-30 00:48:12 -0400

    cmtrace-open: add missing zap path, drop CMTrace name alias

--- Casks/c/cmtrace-open.rb
+++ Casks/c/cmtrace-open.rb
@@ -5,7 +5,6 @@ cask "cmtrace-open" do
   url "https://github.com/adamgell/cmtraceopen/releases/download/v#{version}/CMTrace.Open_#{version}_aarch64.dmg",
       verified: "github.com/adamgell/cmtraceopen/"
   name "CMTrace Open"
-  name "CMTrace"
   desc "Log viewer for ConfigMgr, Intune, and Windows diagnostic logs"
   homepage "https://cmtraceopen.com/"
 
@@ -21,6 +20,7 @@ cask "cmtrace-open" do
 
   zap trash: [
     "~/Library/Application Support/com.cmtrace.open",
+    "~/Library/Caches/com.cmtrace.open",
     "~/Library/Logs/com.cmtrace.open",
     "~/Library/Preferences/com.cmtrace.open.plist",
     "~/Library/WebKit/com.cmtrace.open",
