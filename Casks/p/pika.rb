d66e0e3e5fd51aa11648ddd938d6c867724d7f3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 01:37:48 +0000

    pika 1.9.0

--- Casks/p/pika.rb
+++ Casks/p/pika.rb
@@ -1,6 +1,6 @@
 cask "pika" do
-  version "1.8.0"
-  sha256 "dab57a153ac06b2fe3c5c6b28e435bd3e1d1fd55e4635628a7128817e26f44ee"
+  version "1.9.0"
+  sha256 "082ca16bdf7f4bf91f41c1d855cbaf7911c3a7c9f076bbe3f663af2f543de222"
 
   url "https://github.com/superhighfives/pika/releases/download/#{version}/Pika-#{version}.dmg",
       verified: "github.com/superhighfives/pika/"
