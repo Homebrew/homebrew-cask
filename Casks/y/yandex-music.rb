a3c8c5fd6b260cc49850431ba690bd19d0a03cf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 14:15:07 +0000

    yandex-music 5.113.0

--- Casks/y/yandex-music.rb
+++ Casks/y/yandex-music.rb
@@ -1,6 +1,6 @@
 cask "yandex-music" do
-  version "5.112.0"
-  sha256 "8572fedc649f4717f5d0cffdce46afd29934e9f25610885a9c6ad65259d77f57"
+  version "5.113.0"
+  sha256 "ee8f42d39ad4c6f4d08316920b0bc68e662553cde907f08d3d4c8c054f7d86c9"
 
   url "https://desktop.app.music.yandex.net/stable/Yandex_Music_universal_#{version}.dmg",
       verified: "desktop.app.music.yandex.net/stable/"
