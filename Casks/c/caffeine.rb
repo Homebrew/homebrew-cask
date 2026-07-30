8d64ae67e2f318c4b35e6d236a844afb86cbdf26
Ryan Stoffel <ryan@rstoffel.com>
2026-07-29 19:40:54 -0700

    caffeine: update sha256 and fix brew style offenses

--- Casks/c/caffeine.rb
+++ Casks/c/caffeine.rb
@@ -1,17 +1,15 @@
 cask "caffeine" do
   version "1.0.0"
-  sha256 "c8fae5b494450884038a508d0315af9b2d8bfc8bff1dddea1e09773f069c9ea1"
+  sha256 "f4d1337c41b1a323cec901dfac1a3abf6ad3c3a98729987684b1be277f513403"
 
   url "https://github.com/RyanStoffel/caffeine/releases/download/v#{version}/Caffeine.zip"
   name "Caffeine"
-  desc "Tiny menu bar app to keep your Mac's display awake"
+  desc "Tiny menu bar app to keep display awake"
   homepage "https://github.com/RyanStoffel/caffeine"
 
   depends_on macos: :ventura
 
   app "Caffeine.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.ryanstoffel.caffeine.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.ryanstoffel.caffeine.plist"
 end
