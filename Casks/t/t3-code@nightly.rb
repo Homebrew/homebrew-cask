15f064f65e7cad8233ee2c204bd6a3eb6353f37c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 18:13:36 +0000

    t3-code@nightly 0.0.32-nightly.20260731.966

--- Casks/t/t3-code@nightly.rb
+++ Casks/t/t3-code@nightly.rb
@@ -2,11 +2,11 @@ cask "t3-code@nightly" do
   arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
   os macos: "dmg", linux: "AppImage"
 
-  version "0.0.32-nightly.20260731.965"
+  version "0.0.32-nightly.20260731.966"
 
   on_macos do
-    sha256 arm:   "c1b069bbee2f5bc081197ce0d6e819e16c70dfee348c0c9288b893b1220fe2f7",
-           intel: "0bfddb35a7fec4bddd75f4baa27de6eb389e37e9ca807eb7671f0368ebc83f33"
+    sha256 arm:   "3b8ae151ec27d0dc551f95c662b99a676f614198c70727a79d1a4c8980d2c014",
+           intel: "35e1f536e749610e20766ca7cae66bc150de843b374bf69f7ead158587faa48a"
 
     auto_updates true
     depends_on macos: :monterey
@@ -25,7 +25,7 @@ cask "t3-code@nightly" do
     ]
   end
   on_linux do
-    sha256 "0ec7b7496a47120ebd65943cabb1fea23060989d27ef2f66c647394626b0d1e1"
+    sha256 "060ec99e015295bfc3697178255ee579dd87a46dbe7052c27734e4ede8e3ef56"
 
     depends_on arch: :x86_64
 
