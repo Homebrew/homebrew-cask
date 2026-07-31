ec5b72e80c017627c0f8265e1edf88171fcfcd9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 08:05:17 +0000

    osu@tachyon 2026.731.0-tachyon

--- Casks/o/osu@tachyon.rb
+++ Casks/o/osu@tachyon.rb
@@ -1,9 +1,9 @@
 cask "osu@tachyon" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2026.726.0-lazer"
-  sha256 arm:   "d271fad4eb5fbf55f99dfb0de6d425c37f1f9f6b360fee99c9b2b4130780efff",
-         intel: "d80d3d9f17ebf921802ee86f19ff7a29469411dc0ecebbf78daa3e41c3f07da8"
+  version "2026.731.0-tachyon"
+  sha256 arm:   "dbf77778356144dbfe601f0ea2998c9acab3708b06ff86ca4796f9d5b01ded3b",
+         intel: "53dc22856bc990afa68bbba27e394f47742ac187da4a8a6ed1fd13b5cdc735bc"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu! (tachyon)"
