a04aab7d62c531077630649426c61e863080c3d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 08:01:43 +0000

    renameclick 2.14.3

--- Casks/r/renameclick.rb
+++ Casks/r/renameclick.rb
@@ -1,9 +1,9 @@
 cask "renameclick" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.14.2"
-  sha256 arm:   "03e60aa48a1817ce5b2c96bac3b5d0d0f4d2011d82b0c7d8ab0f642f342cec01",
-         intel: "7c8ac506e6c4ce0c2cd0a7eae4f26cea4a92f90e98e9dfaa5edf5043e6bde2f6"
+  version "2.14.3"
+  sha256 arm:   "264f266deab5d4f9055a0c9ed26b25841b97ab4401d7e7cfcb8f7b0183c26dd7",
+         intel: "3ddcbbf0b71cd86da01c0aff64607d5c701aec8a550ec748d458c21587b2f287"
 
   url "https://github.com/noemaVision/renameclick/releases/download/v#{version}/RenameClick-#{version}-#{arch}.dmg",
       verified: "github.com/noemaVision/renameclick/"
