2408005eba74b6ce6261a4248907fb772b2716e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 22:17:23 +0000

    openchamber 1.17.2

--- Casks/o/openchamber.rb
+++ Casks/o/openchamber.rb
@@ -1,9 +1,9 @@
 cask "openchamber" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.17.1"
-  sha256 arm:   "bfe72404d1516890ecf2867cb11a56f9feb33b746bf122f0e7605116353d2980",
-         intel: "c4976dc04c143fb02f6e7c4be131003090d3f8a701974b575ab9c4b4f4fd714b"
+  version "1.17.2"
+  sha256 arm:   "f5db42f3cfc1101022e221db0601c2faebbde8faf0c23430a17eaaf436489072",
+         intel: "76c9b5f78e99f199f166fb82c1c7bdc8adc9438c1db9d1e7dc1cf24594a65f19"
 
   url "https://github.com/openchamber/openchamber/releases/download/v#{version}/OpenChamber-#{version}-mac-#{arch}.dmg",
       verified: "github.com/openchamber/openchamber/"
