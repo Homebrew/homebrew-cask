ebc137b1f19690ce6ce732bbc388725f501925cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 07:27:20 +0000

    devin-cli 3000.3.27

--- Casks/d/devin-cli.rb
+++ Casks/d/devin-cli.rb
@@ -2,14 +2,14 @@ cask "devin-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux"
 
-  version "3000.3.22"
+  version "3000.3.27"
 
   on_macos do
-    sha256 arm:   "eefe1f3c970c06d58b5ec2612ac5b36cd350635e0a2e59eb4d4f5baf43427662",
-           intel: "e6d09f77097ebdbdc3f337113395cb13ce091c9932af22032ada2a4fd18530d2"
+    sha256 arm:   "ec047ea396d53fc370c1c7c6de211637585cf99a5c5cd5dcdbf7e4c7f2a20626",
+           intel: "2d67b0c3aeee4fa98d022ce13925573d6c18f2666927fef79672c1abc1aff729"
   end
   on_linux do
-    sha256 "29fe62dc1f7837f1ec56107b3fd5979e2c3e2dc88c981e8d5c7cae4c5c972fb6"
+    sha256 "cad56a13aba260567b67e59a926d234564dc1a61b9dfa854d686fa2e3c3b948b"
 
     depends_on arch: :x86_64
   end
