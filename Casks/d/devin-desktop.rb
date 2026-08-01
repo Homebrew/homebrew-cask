4b98bb2b26498bcf31986cdbf94c0eaed2586188
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 07:27:45 +0000

    devin-desktop 3.6.27

--- Casks/d/devin-desktop.rb
+++ Casks/d/devin-desktop.rb
@@ -1,9 +1,9 @@
 cask "devin-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.6.22,d5152ff5891d78b46644988943b78f408d06b861"
-  sha256 arm:   "debc7af84cbfc1104cc57630edb9c58afdb96968835367bc066a1117a9810622",
-         intel: "f7441860d0ce641a0e7b827a4290dfcd2464d86cdf0eb465cc42756ca55ecc66"
+  version "3.6.27,0becb483ee8498d49deadf6aefe8c24f58b8007e"
+  sha256 arm:   "ab28d18c4bc25befa6ab70e67a1c06eb53ec86101d23d8f27975adc59cf6ea93",
+         intel: "4b465c61e2028422c62741483eeb5db95d88a0be44a06807d43852763cf244df"
 
   url "https://windsurf-stable.codeiumdata.com/darwin-#{arch}-dmg/stable/#{version.csv.second}/Devin-darwin-#{arch}-#{version.csv.first}.dmg",
       verified: "windsurf-stable.codeiumdata.com/"
