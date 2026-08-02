579250f4bc85f5f1041f91e03a7f928722e41cb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 10:13:11 +0000

    busycal 2026.3.1,2026-08-02-11-55

--- Casks/b/busycal.rb
+++ Casks/b/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2026.3.1,2026-08-01-15-26"
-  sha256 "241c53d9d112b3803ede9a61595a029785484831289536b96f487fa199b3fa65"
+  version "2026.3.1,2026-08-02-11-55"
+  sha256 "1bf637812bab5e3529720786d3c3aaf26da4000447eb55eeac5a4be4bf22ad5a"
 
   url "https://downloads.busymac.com/bcl-#{version.csv.first}-#{version.csv.second}.zip"
   name "BusyCal"
