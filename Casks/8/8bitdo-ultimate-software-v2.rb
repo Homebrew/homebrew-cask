d722ab1637bf7d10ac0189b487292a03fccde715
Sam Ford <1584702+samford@users.noreply.github.com>
2026-08-03 11:14:57 -0400

    8bitdo-ultimate-software-v2 1.0.19
    `8bitdo-ultimate-software-v2` is autobumped but the workflow failed to
    update to version 1.0.19 because the asset URL path is different for
    this version. This updates the version and `url` accordingly.

--- Casks/8/8bitdo-ultimate-software-v2.rb
+++ Casks/8/8bitdo-ultimate-software-v2.rb
@@ -1,8 +1,8 @@
 cask "8bitdo-ultimate-software-v2" do
-  version "1.0.17"
-  sha256 "470164410de7a0ecd81e5d171bf497114721c11fabf2df42ffe880104762a5db"
+  version "1.0.19"
+  sha256 "a700ba073d84fb9ba3a8254b2542ebedd3aa6dfb9ce86f2e2dec48b75153bce7"
 
-  url "https://support.8bitdo.com/upfiles/Ultimate-Software-V2/8BitDo_Ultimate_Software_V2_macOS_V#{version}.zip"
+  url "https://support.8bitdo.com/bd-uploads/files/ultimate_soft/8BitDo_Ultimate_Software_V2_macOS_V#{version}.zip"
   name "8BitDo Ultimate Software V2"
   desc "Control every piece of your controller"
   homepage "https://app.8bitdo.com/Ultimate-Software-V2/"
