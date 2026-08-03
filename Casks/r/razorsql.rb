38d1c204c2ad62c4b75675310fd7de14060c90a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 17:16:41 +0000

    razorsql 11.0.1

--- Casks/r/razorsql.rb
+++ Casks/r/razorsql.rb
@@ -1,9 +1,9 @@
 cask "razorsql" do
   arch arm: "_aarch64"
 
-  version "11.0.0"
-  sha256 arm:   "3c801cba7156975c6d91947263edef05a2456899c0fbb85431a9335ad4138e18",
-         intel: "dbfb4f35b8f40cd7d5d277c8a776ab9161ac848b5132898d85d5b104d2a41c46"
+  version "11.0.1"
+  sha256 arm:   "3131a8f56d1245b672b52c735075c1b0ef454cbfc4e3c04289218116ffd236d5",
+         intel: "2ff0a20f74eafbe86455cb1b3b6ef378ce4510c11c44ba5c644ea182d2b6b47a"
 
   url "https://s3.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}#{arch}.dmg",
       verified: "s3.amazonaws.com/downloads.razorsql.com/downloads/"
