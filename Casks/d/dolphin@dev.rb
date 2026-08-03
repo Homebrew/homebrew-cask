aa7a55e5faa7c58eb95d87d8b08c42dc67caf746
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 19:14:24 +0000

    dolphin@dev 2606-280

--- Casks/d/dolphin@dev.rb
+++ Casks/d/dolphin@dev.rb
@@ -1,6 +1,6 @@
 cask "dolphin@dev" do
-  version "2606-278,bb,91"
-  sha256 "1998e0a73b809539120b14827ee4fe40fefcda86d860fc52d85db31f5f1a6b01"
+  version "2606-280,4d,53"
+  sha256 "8f48ad0c1708e9fd9296defc23c78c2ef9ff5066a152afd6ad5edb3f77c6abdd"
 
   url "https://dl.dolphin-emu.org/builds/#{version.csv.second}/#{version.csv.third}/dolphin-master-#{version.csv.first}-universal.dmg"
   name "Dolphin Dev"
