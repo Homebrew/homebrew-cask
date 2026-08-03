7761b433636cc0a136c8950ecaf099ed11ca1e5f
Jonas Pardeyke <jonas@pardeyke.com>
2026-08-03 21:49:03 +0200

    tentacle-sync-studio: fix download URL

--- Casks/t/tentacle-sync-studio.rb
+++ Casks/t/tentacle-sync-studio.rb
@@ -2,7 +2,7 @@ cask "tentacle-sync-studio" do
   version "1.37"
   sha256 "383cd519b647594254ffc6deb744e47c5f939932d96711a60158e6e40d2fe16e"
 
-  url "https://cms.tentaclesync.com/assets/ttsyncstudio-v#{version.dots_to_underscores}.dmg"
+  url "https://cms.tentaclesync.com/assets/downloads/download-files/ttsyncstudio-v#{version.dots_to_underscores}.dmg"
   name "Tentacle Sync Studio"
   desc "Automatically synchronise video and audio via timecode"
   homepage "https://tentaclesync.com/"
