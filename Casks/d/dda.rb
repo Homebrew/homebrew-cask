c735b0c24e795a80357bea90971568cbe25e471c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 22:04:31 +0000

    dda 0.38.0

--- Casks/d/dda.rb
+++ Casks/d/dda.rb
@@ -2,11 +2,11 @@ cask "dda" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-gnu"
 
-  version "0.37.0"
-  sha256 arm:          "8463be751a635a2926e854f6623685607b6ddf57d02a17fd85df4803848b5b54",
-         intel:        "200d5c7926574eb344bf985d9ef108f0f79b7e7f584d501f0eec9bc1cbf28ee4",
-         arm64_linux:  "8e177502c90db0d31557dfbadfb954b3a952a4e455c538a034017ef07ad64065",
-         x86_64_linux: "ee57f86edcc125f5976abd11f836d698dbbaf2490a51b160bafc96163ce61910"
+  version "0.38.0"
+  sha256 arm:          "242d5af17dd2bc5142127fd7c54ca9f45035f0b6817cb1bfd3279077838427d1",
+         intel:        "8aa664344845081c818ea840e00a574795031a8295b654782960d83f16ffabe6",
+         arm64_linux:  "16de2b851744b29dfc2d8290642fec54d75df9e2d6dd5266f79b9e1baa841023",
+         x86_64_linux: "092d691b08321ff1dee1c465234ee8aa1675654cbf2cc926fe603f047b02cf06"
 
   url "https://github.com/DataDog/datadog-agent-dev/releases/download/v#{version}/dda-#{arch}-#{os}.tar.gz"
   name "dda"
