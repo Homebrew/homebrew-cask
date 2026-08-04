9066a47923f69fa8c6bfa13493aeb9fe31cda708
Darkatse <39111369+Darkatse@users.noreply.github.com>
2026-08-04 16:08:41 -0400

    tauritavern: update checksums and zap stanza

--- Casks/t/tauritavern.rb
+++ Casks/t/tauritavern.rb
@@ -2,8 +2,8 @@ cask "tauritavern" do
   arch arm: "arm64", intel: "x64"
 
   version "2.2.0"
-  sha256 arm:   "f9616a391af266edd46576f81e0419c609c41d42e1e55d4980445a67b56c03b0",
-         intel: "e9397e4b2424ed4f14915303286e53e66a5576ab963d208a75a50891af0ccb7e"
+  sha256 arm:   "913756a0de19bb315d85204e026697aaff078d06d6f82d7ca5cff7bd70d0c6d2",
+         intel: "fbf3d80e4921f7abfbb3d56797db8645c14236e8338c96cec66997e5c1d81811"
 
   url "https://github.com/Darkatse/TauriTavern/releases/download/v#{version}/TauriTavern-#{version}-macos-#{arch}.dmg",
       verified: "github.com/Darkatse/TauriTavern/"
@@ -22,8 +22,13 @@ cask "tauritavern" do
 
   zap trash: [
     "~/Library/Application Support/com.tauritavern.client",
+    "~/Library/Application Support/CrashReporter/tauritavern_*.plist",
     "~/Library/Caches/com.tauritavern.client",
+    "~/Library/Caches/tauritavern",
+    "~/Library/HTTPStorages/tauritavern.binarycookies",
     "~/Library/Preferences/com.tauritavern.client.plist",
+    "~/Library/Preferences/tauritavern.plist",
     "~/Library/WebKit/com.tauritavern.client",
+    "~/Library/WebKit/tauritavern",
   ]
 end
