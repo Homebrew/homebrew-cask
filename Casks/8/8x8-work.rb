2e2671f9214b288140a85fc7526a5c25579dfc19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 10:19:38 +0000

    8x8-work 8.36.2-3

--- Casks/8/8x8-work.rb
+++ Casks/8/8x8-work.rb
@@ -1,9 +1,9 @@
 cask "8x8-work" do
   arch arm: "-arm64"
 
-  version "8.35.2-6"
-  sha256 arm:   "24be6f8e2b864f4ca7b9acd661e81a4171278d86b8105f8ada0cc3221b28e87b",
-         intel: "2630392a6d8e1efd5eec1540de3ae9be6b11694a333dec995719476113b3eff4"
+  version "8.36.2-3"
+  sha256 arm:   "2cf54acde15ac58740db4baf8a474b601707023816d3637a616df203092d24a1",
+         intel: "ae5da045ddf28cd9f99e459dcc18033f9bc055c9f8c35ede1b65f63671bb5e7e"
 
   url "https://work-desktop-assets.8x8.com/prod-publish/ga/work#{arch}-dmg-v#{version}.dmg"
   name "8x8_work"
