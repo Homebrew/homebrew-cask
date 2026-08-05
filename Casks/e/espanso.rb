40d676052dcb320d1c792f1f4c13101b7deb3e6f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 23:38:15 -0400

    espanso: add AppImage

--- Casks/e/espanso.rb
+++ Casks/e/espanso.rb
@@ -1,8 +1,35 @@
 cask "espanso" do
-  version "2.4.0"
-  sha256 "aaf81d7573db785e5447b867e0f2f1d6f061ea9fa1756fda02f21b0402407669"
+  url_end = on_system_conditional macos: "Mac-Universal.dmg", linux: "X11.AppImage"
 
-  url "https://github.com/espanso/espanso/releases/download/v#{version}/Espanso-Mac-Universal.dmg",
+  version "2.4.0"
+
+  on_macos do
+    sha256 "aaf81d7573db785e5447b867e0f2f1d6f061ea9fa1756fda02f21b0402407669"
+
+    depends_on macos: :big_sur
+
+    app "Espanso.app"
+    binary "#{appdir}/Espanso.app/Contents/MacOS/espanso"
+
+    zap trash: [
+      "~/Library/Application Support/espanso",
+      "~/Library/Caches/espanso",
+      "~/Library/LaunchAgents/com.federicoterzi.espanso.plist",
+      "~/Library/Preferences/com.federicoterzi.espanso.plist",
+      "~/Library/Preferences/espanso",
+      "~/Library/Preferences/espanso.plist",
+      "~/Library/Saved Application State/com.federicoterzi.espanso.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "42290d0ef9ee4e9ef198e309263f6b8d5478592e8d2792825e49d27c04281048"
+
+    depends_on arch: :x86_64
+
+    app_image "Espanso-X11.AppImage", target: "Espanso.AppImage"
+  end
+
+  url "https://github.com/espanso/espanso/releases/download/v#{version}/Espanso-#{url_end}",
       verified: "github.com/espanso/espanso/"
   name "Espanso"
   desc "Cross-platform Text Expander written in Rust"
@@ -12,19 +39,4 @@ cask "espanso" do
     url :url
     strategy :github_latest
   end
-
-  depends_on macos: :big_sur
-
-  app "Espanso.app"
-  binary "#{appdir}/Espanso.app/Contents/MacOS/espanso"
-
-  zap trash: [
-    "~/Library/Application Support/espanso",
-    "~/Library/Caches/espanso",
-    "~/Library/LaunchAgents/com.federicoterzi.espanso.plist",
-    "~/Library/Preferences/com.federicoterzi.espanso.plist",
-    "~/Library/Preferences/espanso",
-    "~/Library/Preferences/espanso.plist",
-    "~/Library/Saved Application State/com.federicoterzi.espanso.savedState",
-  ]
 end
