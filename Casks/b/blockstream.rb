9bd0f26c2aad6cd531bf4eaad157a1474868710a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 21:59:19 -0400

    blockstream: add AppImage

--- Casks/b/blockstream.rb
+++ Casks/b/blockstream.rb
@@ -1,11 +1,31 @@
 cask "blockstream" do
   arch arm: "arm64", intel: "x86_64"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "3.4.1"
-  sha256 arm:   "6922f5f617d466c37a3a16a354f7a5b052495255fee48f0283e3c31a23a7de8a",
-         intel: "8a0d41abf0995efdf3478413f34d8317040e7659dd2843eb170e3fbb7c958b8e"
 
-  url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/Blockstream-#{arch}.dmg",
+  on_macos do
+    sha256 arm:   "6922f5f617d466c37a3a16a354f7a5b052495255fee48f0283e3c31a23a7de8a",
+           intel: "8a0d41abf0995efdf3478413f34d8317040e7659dd2843eb170e3fbb7c958b8e"
+
+    depends_on macos: :ventura
+
+    app "Blockstream.app"
+
+    zap trash: [
+      "~/Library/Application Support/Blockstream/Green",
+      "~/Library/Caches/Blockstream/Green",
+    ]
+  end
+  on_linux do
+    sha256 "f27ddf2f1c25f87479204200bfb64f1097d4703e1587b5a1ad5f0eea089d6a99"
+
+    depends_on arch: :x86_64
+
+    app_image "Blockstream-#{arch}.AppImage", target: "Blockstream.AppImage"
+  end
+
+  url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/Blockstream-#{arch}.#{url_end}",
       verified: "github.com/Blockstream/green_qt/"
   name "Blockstream Green"
   desc "Multi-platform Bitcoin and Liquid wallet"
@@ -15,13 +35,4 @@ cask "blockstream" do
     url :url
     strategy :github_latest
   end
-
-  depends_on macos: :ventura
-
-  app "Blockstream.app"
-
-  zap trash: [
-    "~/Library/Application Support/Blockstream/Green",
-    "~/Library/Caches/Blockstream/Green",
-  ]
 end
