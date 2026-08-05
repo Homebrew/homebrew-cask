34507ad36410d89ceec8ede58152e5ed687755d2
AltCode <altcode@vivaldi.net>
2026-08-05 20:30:29 +0200

    krita: add Linux AppImage

--- Casks/k/krita.rb
+++ Casks/k/krita.rb
@@ -1,26 +1,41 @@
 cask "krita" do
-  version "5.3.3"
-  sha256 "625e37c01cfb74094ae58353dd9d343cd389a00c33cb65d6ddf1f2f1e2bc3a19"
+  arch intel: on_system_conditional(linux: "x86_64")
+  os macos: "signed"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}-signed.dmg",
-      verified: "download.kde.org/stable/krita/"
+  version "5.3.3"
+
+  on_macos do
+    sha256 "625e37c01cfb74094ae58353dd9d343cd389a00c33cb65d6ddf1f2f1e2bc3a19"
+
+    # Renamed for consistency: app name is different in the Finder and in a shell.
+    app "krita.app", target: "Krita.app"
+
+    zap trash: [
+      "~/Library/Application Scripts/org.krita.*",
+      "~/Library/Application Support/krita*",
+      "~/Library/Caches/krita",
+      "~/Library/Containers/org.krita.*",
+      "~/Library/Preferences/kritadisplayrc",
+      "~/Library/Preferences/kritarc",
+      "~/Library/Saved Application State/org.krita.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "1e3fff5da006c0d2600f98a41aa2c9a7dfa49bd931f3640616f30d762db4f743"
+
+    depends_on arch: :x86_64
+
+    app_image "krita-#{version}-#{arch}.AppImage", target: "Krita.AppImage"
+  end
+
+  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}-#{arch}#{os}.#{url_end}"
   name "Krita"
   desc "Free and open-source painting and sketching program"
   homepage "https://krita.org/"
 
   livecheck do
     url "https://krita.org/en/download/"
-    regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)(?:[._-]signed|[._-]release)?\.dmg/i)
+    regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)[._-](?:#{arch}|#{os}|release)?\.#{url_end}/i)
   end
-
-  depends_on :macos
-
-  app "krita.app"
-
-  zap trash: [
-    "~/Library/Application Support/krita",
-    "~/Library/Preferences/kritadisplayrc",
-    "~/Library/Preferences/kritarc",
-    "~/Library/Saved Application State/org.krita.savedState",
-  ]
 end
