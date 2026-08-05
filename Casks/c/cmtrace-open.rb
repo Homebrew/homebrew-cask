e96e415425a1ca96473e9a8466019963988c60ba
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 23:04:13 -0400

    cmtrace-open: add AppImage

--- Casks/c/cmtrace-open.rb
+++ Casks/c/cmtrace-open.rb
@@ -1,8 +1,32 @@
 cask "cmtrace-open" do
-  version "1.5.1"
-  sha256 "b56f5af09e3c16c0b820b7393e9a393bd59d9999e6d7fcbbd60b91d912d73788"
+  url_end = on_system_conditional macos: "aarch64.dmg", linux: "amd64.AppImage"
 
-  url "https://github.com/adamgell/cmtraceopen/releases/download/v#{version}/CMTrace.Open_#{version}_aarch64.dmg",
+  version "1.5.1"
+
+  on_macos do
+    sha256 "b56f5af09e3c16c0b820b7393e9a393bd59d9999e6d7fcbbd60b91d912d73788"
+
+    depends_on arch: :arm64
+
+    app "CMTrace Open.app"
+
+    zap trash: [
+      "~/Library/Application Support/com.cmtrace.open",
+      "~/Library/Caches/com.cmtrace.open",
+      "~/Library/Logs/com.cmtrace.open",
+      "~/Library/Preferences/com.cmtrace.open.plist",
+      "~/Library/WebKit/com.cmtrace.open",
+    ]
+  end
+  on_linux do
+    sha256 "3596b200a3863e3a882e0ee9a10e172cfab93cc2fd4a656ff6e728c47b9c85d0"
+
+    depends_on arch: :x86_64
+
+    app_image "CMTrace.Open_#{version}_amd64.AppImage", target: "CMTrace Open.AppImage"
+  end
+
+  url "https://github.com/adamgell/cmtraceopen/releases/download/v#{version}/CMTrace.Open_#{version}_#{url_end}",
       verified: "github.com/adamgell/cmtraceopen/"
   name "CMTrace Open"
   desc "Log viewer for ConfigMgr, Intune, and Windows diagnostic logs"
@@ -12,17 +36,4 @@ cask "cmtrace-open" do
     url :url
     strategy :github_latest
   end
-
-  depends_on arch: :arm64
-  depends_on :macos
-
-  app "CMTrace Open.app"
-
-  zap trash: [
-    "~/Library/Application Support/com.cmtrace.open",
-    "~/Library/Caches/com.cmtrace.open",
-    "~/Library/Logs/com.cmtrace.open",
-    "~/Library/Preferences/com.cmtrace.open.plist",
-    "~/Library/WebKit/com.cmtrace.open",
-  ]
 end
