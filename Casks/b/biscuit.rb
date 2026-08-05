eb47cc7b7ee640de614cf1f9846a8cfe818d77d2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 21:59:18 -0400

    biscuit: add AppImage

--- Casks/b/biscuit.rb
+++ Casks/b/biscuit.rb
@@ -1,25 +1,37 @@
 cask "biscuit" do
   arch arm: "-arm64"
+  url_end = on_system_conditional macos: "#{arch}.dmg", linux: ".AppImage"
 
   version "2.1.1"
-  sha256 arm:   "998dcc9785aea8161b6740398a460cb7a0d3d6d8c627728a14ee885dbb946c3e",
-         intel: "8dffe8207ac9056d3c3762d0703c81bb185e68402535ebfbcf982d0638441f15"
 
-  url "https://github.com/agata/dl.biscuit/releases/download/#{version}/Biscuit-#{version}#{arch}.dmg",
+  on_macos do
+    sha256 arm:   "998dcc9785aea8161b6740398a460cb7a0d3d6d8c627728a14ee885dbb946c3e",
+           intel: "8dffe8207ac9056d3c3762d0703c81bb185e68402535ebfbcf982d0638441f15"
+
+    depends_on macos: :monterey
+
+    app "Biscuit.app"
+
+    zap trash: [
+      "~/Library/Application Support/biscuit",
+      "~/Library/Logs/Biscuit",
+      "~/Library/Preferences/com.eatbiscuit.biscuit.plist",
+      "~/Library/Saved Application State/com.eatbiscult.biscult.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "e1716627e572982afb5cfb5ea315cd4e9917721eb4bcdde8554c6a4f84661880"
+
+    depends_on arch: :x86_64
+
+    app_image "Biscuit-#{version}.AppImage", target: "Biscuit.AppImage"
+  end
+
+  url "https://github.com/agata/dl.biscuit/releases/download/#{version}/Biscuit-#{version}#{url_end}",
       verified: "github.com/agata/dl.biscuit/"
   name "Biscuit"
   desc "Browser to organise apps"
   homepage "https://eatbiscuit.com/"
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "Biscuit.app"
-
-  zap trash: [
-    "~/Library/Application Support/biscuit",
-    "~/Library/Logs/Biscuit",
-    "~/Library/Preferences/com.eatbiscuit.biscuit.plist",
-    "~/Library/Saved Application State/com.eatbiscult.biscult.savedState",
-  ]
 end
