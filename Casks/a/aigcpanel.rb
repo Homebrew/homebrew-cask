d7c210998e9c30e88b89e3eec8581f2ddb6ef0ef
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 20:50:31 -0400

    aigcpanel: add AppImage

--- Casks/a/aigcpanel.rb
+++ Casks/a/aigcpanel.rb
@@ -1,11 +1,29 @@
 cask "aigcpanel" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
+  os macos: "mac", linux: "linux"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "2.0.0"
-  sha256 arm:   "5ed88f3ab6ab9fbff56a4967d2d4bc74daff91c867bd4c32c05784d921d73a4a",
-         intel: "d25346bea12380e583e03e4ccb7b94ae7c1aed222fd13f910b23c18db419b7a1"
+  sha256 arm:          "5ed88f3ab6ab9fbff56a4967d2d4bc74daff91c867bd4c32c05784d921d73a4a",
+         intel:        "d25346bea12380e583e03e4ccb7b94ae7c1aed222fd13f910b23c18db419b7a1",
+         arm64_linux:  "b7d4f7ba2b4ac17b0b6dab1a3a41a60b8b9622505e9f01ca58e8f6dc09b8d17c",
+         x86_64_linux: "2afea5d4f14ec2343ceddcfe1b19a95dc1d8be3775c33585f36f64d6d2261819"
 
-  url "https://github.com/modstart-lib/aigcpanel/releases/download/v#{version.csv.first}/AigcPanel-#{version.csv.second || version.csv.first}-mac-#{arch}.dmg",
+  on_macos do
+    app "AigcPanel.app"
+
+    zap trash: [
+      "~/Library/Application Support/aigcpanel",
+      "~/Library/Preferences/AigcPanel.plist",
+      "~/Library/Saved Application State/AigcPanel.savedState",
+    ]
+  end
+  on_linux do
+    app_image "AigcPanel-#{version.csv.second || version.csv.first}-linux-#{arch}.AppImage",
+              target: "AigcPanel.AppImage"
+  end
+
+  url "https://github.com/modstart-lib/aigcpanel/releases/download/v#{version.csv.first}/AigcPanel-#{version.csv.second || version.csv.first}-#{os}-#{arch}.#{url_end}",
       verified: "github.com/modstart-lib/aigcpanel/"
   name "AigcPanel"
   desc "AI video, audio and broadcast generator"
@@ -13,7 +31,7 @@ cask "aigcpanel" do
 
   livecheck do
     url :url
-    regex(%r{/v?(\d+(?:\.\d+)+)/AigcPanel[._-]v?(\d+(?:\.\d+)+)[._-]mac[._-]#{arch}\.dmg}i)
+    regex(%r{/v?(\d+(?:\.\d+)+)/AigcPanel[._-]v?(\d+(?:\.\d+)+)[._-]#{os}[._-]#{arch}\.#{url_end}}i)
     strategy :github_latest do |json, regex|
       json["assets"]&.map do |asset|
         match = asset["browser_download_url"]&.match(regex)
@@ -23,14 +41,4 @@ cask "aigcpanel" do
       end
     end
   end
-
-  depends_on :macos
-
-  app "AigcPanel.app"
-
-  zap trash: [
-    "~/Library/Application Support/aigcpanel",
-    "~/Library/Preferences/AigcPanel.plist",
-    "~/Library/Saved Application State/AigcPanel.savedState",
-  ]
 end
