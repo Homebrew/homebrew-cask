499d89f4fbcd140d607ed62a369146121efcde4b
Sam Ford <1584702+samford@users.noreply.github.com>
2026-08-05 14:23:06 -0400

    suspicious-package: update macOS dependency
    The new version of `suspicious-package` has a minimum macOS version of
    Sonoma, so this reworks the on_os blocks and retains 4.6.2 for
    Monterey and Ventura. Besides that, this updates the URL for the
    legacy versions to use the archives URL, as linked from the FAQ page.

--- Casks/s/suspicious-package.rb
+++ Casks/s/suspicious-package.rb
@@ -1,5 +1,5 @@
 cask "suspicious-package" do
-  on_big_sur :or_older do
+  on_ventura :or_older do
     on_catalina :or_older do
       version "4.2.1"
       sha256 "5c05df9bf3d56758a3eefa972597e3138afdea4c3774f91fe2849482b7112823"
@@ -8,14 +8,18 @@ cask "suspicious-package" do
       version "4.3.3"
       sha256 "a262c317ad2d6949e0d0f2bec9524a4a85e0e69d6aec0373cf185892acac1f69"
     end
+    on_monterey :or_newer do
+      version "4.6.2"
+      sha256 "9bcd1c8169458fa4e370c1f2ea6c2f98ecd6dbafe9cfa71f9fce032a0d1b57bc"
+    end
 
-    url "https://www.mothersruin.com/software/downloads/SuspiciousPackage-#{version}.dmg"
+    url "https://www.mothersruin.com/software/archives/SuspiciousPackage-#{version}.dmg"
 
     livecheck do
       skip "Legacy version"
     end
   end
-  on_monterey :or_newer do
+  on_sonoma :or_newer do
     version "4.7,1415"
     sha256 :no_check
 
