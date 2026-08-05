155355a4f6928aaf5b4fbe38b5c9ee5c380eb825
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 13:39:43 +0000

    koe 1.0.25

--- Casks/k/koe.rb
+++ Casks/k/koe.rb
@@ -1,6 +1,6 @@
 cask "koe" do
-  version "1.0.23"
-  sha256 "00fdd20292730a788c8d2bb9b9b79f0684bf74db8ba2990942c7a644925c7462"
+  version "1.0.25"
+  sha256 "5c4b997dd1b39fc24a28dd8fe38ed193a30c4da8903a8d6027b7d027135307fc"
 
   url "https://github.com/missuo/koe/releases/download/v#{version}/Koe-macOS-arm64.zip"
   name "Koe"
