743f4f4e61235f4767480bc736667f8c1edb57ff
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 22:58:18 -0400

    candy-crisis: add AppImage

--- Casks/c/candy-crisis.rb
+++ Casks/c/candy-crisis.rb
@@ -1,8 +1,28 @@
 cask "candy-crisis" do
-  version "3.0.0"
-  sha256 "4c3359332d950e2836f9279c8ed1cb32e3c271e5dccb182134364976b4ef2095"
+  arch arm: "aarch64", intel: "x86_64"
+  url_end = on_system_conditional macos: "mac.dmg", linux: "linux-#{arch}.AppImage"
 
-  url "https://github.com/jorio/CandyCrisis/releases/download/v#{version}/CandyCrisis-#{version}-mac.dmg",
+  version "3.0.0"
+
+  on_macos do
+    sha256 "4c3359332d950e2836f9279c8ed1cb32e3c271e5dccb182134364976b4ef2095"
+
+    app "Candy Crisis.app"
+
+    zap trash: [
+      "~/Library/Application Support/CandyCrisis",
+      "~/Library/Containers/com.cc.Candy-Crisis",
+      "~/Library/Saved Application State/io.jor.candycrisis.savedState",
+    ]
+  end
+  on_linux do
+    sha256 arm64_linux:  "ac8adb2ba1fdd9c6345f430bd0a01e15c8d3a8f62c482d52da8fa0d5dd321f6a",
+           x86_64_linux: "92e2bea04785fc4a69b3bbd830d3bfe90f35e0659d5dbae1f2cb875fe3bb6feb"
+
+    app_image "CandyCrisis-#{version}-linux-#{arch}.AppImage", target: "Candy Crisis.AppImage"
+  end
+
+  url "https://github.com/jorio/CandyCrisis/releases/download/v#{version}/CandyCrisis-#{version}-#{url_end}",
       verified: "github.com/jorio/CandyCrisis/"
   name "Candy Crisis"
   desc "Tile matching puzzle/action game"
@@ -12,14 +32,4 @@ cask "candy-crisis" do
     url :url
     strategy :github_latest
   end
-
-  depends_on :macos
-
-  app "Candy Crisis.app"
-
-  zap trash: [
-    "~/Library/Application Support/CandyCrisis",
-    "~/Library/Containers/com.cc.Candy-Crisis",
-    "~/Library/Saved Application State/io.jor.candycrisis.savedState",
-  ]
 end
