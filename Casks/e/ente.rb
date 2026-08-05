39950e3387f96e40bda41e73f471b9ed28892984
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 23:38:14 -0400

    ente: add AppImage

--- Casks/e/ente.rb
+++ Casks/e/ente.rb
@@ -1,8 +1,32 @@
 cask "ente" do
-  version "1.7.27"
-  sha256 "13430c4c2a01150bdca8fb8fd180a270428f33c46a7be9956e79e1b408d486d7"
+  arch arm: "arm64", intel: "x86_64"
+  url_end = on_system_conditional macos: "universal.dmg", linux: "#{arch}.AppImage"
 
-  url "https://github.com/ente-io/photos-desktop/releases/download/v#{version}/ente-#{version}-universal.dmg",
+  version "1.7.27"
+
+  on_macos do
+    sha256 "13430c4c2a01150bdca8fb8fd180a270428f33c46a7be9956e79e1b408d486d7"
+
+    depends_on macos: :monterey
+
+    app "ente.app"
+
+    zap trash: [
+      "~/Library/Application Support/ente",
+      "~/Library/Logs/ente",
+      "~/Library/Preferences/io.ente.bhari-frame.helper.plist",
+      "~/Library/Preferences/io.ente.bhari-frame.plist",
+      "~/Library/Saved Application State/io.ente.bhari-frame.savedState",
+    ]
+  end
+  on_linux do
+    sha256 arm64_linux:  "960e19936956d6e7fd4a9a595c14295862cd3a3ca29fadca0bbce8f63282aa98",
+           x86_64_linux: "2acb978ec4ee85dcf71d3b97abfd54741b6b9c331afe1a06ffb9b2290c64eec0"
+
+    app_image "ente-#{version}-#{arch}.AppImage", target: "Ente.AppImage"
+  end
+
+  url "https://github.com/ente-io/photos-desktop/releases/download/v#{version}/ente-#{version}-#{url_end}",
       verified: "github.com/ente-io/photos-desktop/"
   name "Ente"
   desc "Desktop client for Ente Photos"
@@ -14,15 +38,4 @@ cask "ente" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "ente.app"
-
-  zap trash: [
-    "~/Library/Application Support/ente",
-    "~/Library/Logs/ente",
-    "~/Library/Preferences/io.ente.bhari-frame.helper.plist",
-    "~/Library/Preferences/io.ente.bhari-frame.plist",
-    "~/Library/Saved Application State/io.ente.bhari-frame.savedState",
-  ]
 end
