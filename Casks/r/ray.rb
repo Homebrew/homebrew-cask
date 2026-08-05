e0803334fb99d065e97f20cb9d488068ac959c5a
David Rogers <david.rogers@tylertech.com>
2026-08-05 01:39:18 -0700

    Update ray from 2.8.2 to 3.2.10

--- Casks/r/ray.rb
+++ Casks/r/ray.rb
@@ -1,20 +1,26 @@
 cask "ray" do
-  arch arm: "-arm64"
-  folder = on_arch_conditional arm: "arm64/"
+  version "3.2.10"
 
-  version "2.8.2"
-  sha256 arm:   "9557ecc4e9758a499b71b324466659a7e1a42d996edf0e1d4353cd2dd6494cee",
-         intel: "f6d9fb9a2721ec72146cc43967f17714a9c0bf9bf1d4744fb930de4b82e50e00"
+  on_arm do
+    sha256 "8965569f043c36bd7e12decfbb12db1511962b4e7edf423d05599d65c5ccc25a"
 
-  url "https://ray-app.s3.amazonaws.com/#{folder}Ray-#{version}#{arch}.dmg",
-      verified: "ray-app.s3.amazonaws.com/"
+    url "https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-#{version}-latest-darwin-arm64.dmg",
+        verified: "ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/"
+  end
+  on_intel do
+    sha256 "dc7a6fbfcfd6029180dfd0181c834124b714ebb249e0472a9ca0de4060dcbfe1"
+
+    url "https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-#{version}-latest-darwin-x64.dmg",
+        verified: "ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/"
+  end
+
   name "Ray"
   desc "Debug with Ray to fix problems faster"
   homepage "https://myray.app/"
 
   livecheck do
-    url "https://ray-app.s3.amazonaws.com/latest-mac.yml"
-    strategy :electron_builder
+    url "https://amazonaws.com"
+    skip "Upstream tracking file is hardcoded to obsolete 2.x stream"
   end
 
   auto_updates true
