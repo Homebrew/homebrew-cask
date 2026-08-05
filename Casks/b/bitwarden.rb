1465e0260327580f4eb53192a5f722be161ffec8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-04 21:59:18 -0400

    bitwarden: add AppImage

--- Casks/b/bitwarden.rb
+++ Casks/b/bitwarden.rb
@@ -1,8 +1,40 @@
 cask "bitwarden" do
-  version "2026.7.0"
-  sha256 "50a7c03dc6d172bf0099fc0469f74acffab1624f7aad06d3f56b58b030a5be92"
+  url_end = on_system_conditional macos: "universal.dmg", linux: "x86_64.AppImage"
 
-  url "https://github.com/bitwarden/clients/releases/download/desktop-v#{version}/Bitwarden-#{version}-universal.dmg",
+  version "2026.7.0"
+
+  on_macos do
+    sha256 "50a7c03dc6d172bf0099fc0469f74acffab1624f7aad06d3f56b58b030a5be92"
+
+    depends_on macos: :monterey
+
+    app "Bitwarden.app"
+
+    uninstall quit: [
+      "com.bitwarden.desktop",
+      "com.bitwarden.desktop.helper",
+    ]
+
+    zap trash: [
+      "~/Library/Application Support/Bitwarden",
+      "~/Library/Caches/com.bitwarden.desktop",
+      "~/Library/Caches/com.bitwarden.desktop.ShipIt",
+      "~/Library/Logs/Bitwarden",
+      "~/Library/Preferences/ByHost/com.bitwarden.desktop.ShipIt.*.plist",
+      "~/Library/Preferences/com.bitwarden.desktop.helper.plist",
+      "~/Library/Preferences/com.bitwarden.desktop.plist",
+      "~/Library/Saved Application State/com.bitwarden.desktop.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "df956ee0abf75c122ff3a68b815f75514a28eb396ef8bebf5c6d71b00ffa908a"
+
+    depends_on arch: :x86_64
+
+    app_image "Bitwarden-#{version}-x86_64.AppImage", target: "Bitwarden.AppImage"
+  end
+
+  url "https://github.com/bitwarden/clients/releases/download/desktop-v#{version}/Bitwarden-#{version}-#{url_end}",
       verified: "github.com/bitwarden/clients/"
   name "Bitwarden"
   desc "Desktop password and login vault"
@@ -14,23 +46,4 @@ cask "bitwarden" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "Bitwarden.app"
-
-  uninstall quit: [
-    "com.bitwarden.desktop",
-    "com.bitwarden.desktop.helper",
-  ]
-
-  zap trash: [
-    "~/Library/Application Support/Bitwarden",
-    "~/Library/Caches/com.bitwarden.desktop",
-    "~/Library/Caches/com.bitwarden.desktop.ShipIt",
-    "~/Library/Logs/Bitwarden",
-    "~/Library/Preferences/ByHost/com.bitwarden.desktop.ShipIt.*.plist",
-    "~/Library/Preferences/com.bitwarden.desktop.helper.plist",
-    "~/Library/Preferences/com.bitwarden.desktop.plist",
-    "~/Library/Saved Application State/com.bitwarden.desktop.savedState",
-  ]
 end
