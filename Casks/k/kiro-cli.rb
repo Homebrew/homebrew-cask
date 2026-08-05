26a59047f39e81d34066b6c037ee74867e969ce9
Bevan Kay <email@bevankay.me>
2026-08-05 08:42:15 +0000

    kiro-cli 2.16.1

--- Casks/k/kiro-cli.rb
+++ Casks/k/kiro-cli.rb
@@ -1,6 +1,6 @@
 cask "kiro-cli" do
-  version "2.16.0"
-  sha256 "6131f8061f341c51bb546f7b9eea44c22bfe1d79d336bd904b7e2a31a9a0f3c7"
+  version "2.16.1"
+  sha256 "5fce9cf9dca6a74f0c52915176a6421f5948e6903813af90480c6b722cb20d32"
 
   url "https://desktop-release.q.us-east-1.amazonaws.com/#{version}/Kiro%20CLI.dmg",
       verified: "desktop-release.q.us-east-1.amazonaws.com/"
