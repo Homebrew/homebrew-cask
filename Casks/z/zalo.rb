6443f65532b66f39a6339b5693d950a5ad048c8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 05:49:09 +0000

    zalo 26.8.10

--- Casks/z/zalo.rb
+++ Casks/z/zalo.rb
@@ -1,6 +1,6 @@
 cask "zalo" do
-  version "26.7.10"
-  sha256 "945ba046144ab16655516572eb787628044ba3b2b6f1cbd9cdf30b560cb8b3d6"
+  version "26.8.10"
+  sha256 "d0a1b950c6062926b115f8a5c5b4a162bcdbfed9422180a49de3717f3c490965"
 
   url "https://res-download-pc.zadn.vn/mac/ZaloSetup-universal-#{version}.dmg",
       verified: "res-download-pc.zadn.vn/mac/"
