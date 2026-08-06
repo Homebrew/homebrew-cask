986cb14c79b056e732d52337e69ff56c8f7ee617
h3p <26873619+h3pdesign@users.noreply.github.com>
2026-08-06 12:45:12 +0200

    neon-vision-editor: correct 1.2.3 checksum

---
+++ Casks/n/neon-vision-editor.rb
@@ -0,0 +1,25 @@
+cask "neon-vision-editor" do
+  version "1.2.3"
+  sha256 "30da044ccf3e195442e7ba53743024e119fe84788253e71c3d2bd9a613eca478"
+
+  url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
+  name "Neon Vision Editor"
+  desc "Native code and text editor"
+  homepage "https://github.com/h3pdesign/Neon-Vision-Editor"
+
+  auto_updates true
+  depends_on macos: :sonoma
+
+  app "Neon Vision Editor.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/group.h3p.Neon-Vision-Editor",
+    "~/Library/Application Scripts/h3p.Neon-Neon-Vision-Editor*",
+    "~/Library/Application Support/NeonVisionEditor",
+    "~/Library/Caches/h3p.Neon-Vision-Editor",
+    "~/Library/Containers/h3p.Neon-Vision-Editor",
+    "~/Library/Group Containers/group.h3p.Neon-Vision-Editor",
+    "~/Library/Logs/NeonVisionEditorUpdater.log",
+    "~/Library/Preferences/h3p.Neon-Vision-Editor.plist",
+  ]
+end
