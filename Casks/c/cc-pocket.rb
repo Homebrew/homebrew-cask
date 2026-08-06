0cf5c6b96e5738fe0cb35561e42d15ea74eddd34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 23:58:48 +0000

    cc-pocket 1.111.2

--- Casks/c/cc-pocket.rb
+++ Casks/c/cc-pocket.rb
@@ -1,6 +1,6 @@
 cask "cc-pocket" do
-  version "1.111.1,206"
-  sha256 "4a10f7f4dedeb0f17871467d8c7d06d2ac19b3cf03481c9eee6c42988e89c018"
+  version "1.111.2,207"
+  sha256 "780693fd3f7bd6531514955d7419674ec2283cd96febea453b004753a3b04cae"
 
   url "https://github.com/K9i-0/ccpocket/releases/download/macos/v#{version.csv.first}%2B#{version.csv.second}/CC-Pocket-macos-v#{version.csv.first}.dmg",
       verified: "github.com/K9i-0/ccpocket/"
