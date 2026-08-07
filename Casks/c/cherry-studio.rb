d484de4e4cc1ec41e7f5db2f28f693c507537931
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 09:55:36 +0000

    cherry-studio 2.0.2

--- Casks/c/cherry-studio.rb
+++ Casks/c/cherry-studio.rb
@@ -1,9 +1,9 @@
 cask "cherry-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.1"
-  sha256 arm:   "7dafd976a693183ec1b46ec2806dd7a81ba4432e023f9993caad5322dd566dde",
-         intel: "50d4b05220a87fc6d019b88112061dd6ac11320dbbb8e68e3f4b18e8d95ac741"
+  version "2.0.2"
+  sha256 arm:   "f3bc808fd03382671e8d7f542300bfcc8058091399654cba5bd2f4942d057b34",
+         intel: "32a8a858b7cc102b8f091f68e0db663098a7bcef7914032cf63a03d885be558f"
 
   url "https://github.com/CherryHQ/cherry-studio/releases/download/v#{version}/Cherry-Studio-#{version}-#{arch}.dmg",
       verified: "github.com/CherryHQ/cherry-studio/"
