cca9a200d88a07a5ef2a59b03be7a3b2b08f15e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 07:57:59 +0000

    vamiga 4.5

--- Casks/v/vamiga.rb
+++ Casks/v/vamiga.rb
@@ -1,6 +1,6 @@
 cask "vamiga" do
-  version "4.4"
-  sha256 "ee5e3d264fdedd4876ad22e20ce84ef725a0964f6d7f0cc7fb96d4af8ac53994"
+  version "4.5"
+  sha256 "8e4d98ee27cc1b10a478389c3f56da05ed60082114c3a249d8907b63a45e1bdb"
 
   url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip",
       verified: "github.com/dirkwhoffmann/vAmiga/"
