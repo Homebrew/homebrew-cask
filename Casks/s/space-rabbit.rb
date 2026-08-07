82ccc58a73fa0c092abaafeda11531a729315f23
David Guyon <dguyon@gmail.com>
2026-08-07 15:53:23 +0200

    space-rabbit 2.2.3 (new cask)

---
+++ Casks/s/space-rabbit.rb
@@ -0,0 +1,19 @@
+cask "space-rabbit" do
+  version "2.2.3"
+  sha256 "d25e661b589270b06d741ddf1c53ce1ea41272823b1c49fffcd2d28efd77b741"
+
+  url "https://github.com/Tahul/space-rabbit/releases/download/v#{version}/Space-Rabbit.dmg"
+  name "Space Rabbit"
+  desc "Removes animations when switching between Spaces"
+  homepage "https://space-rabbit.app/"
+
+  auto_updates true
+  depends_on macos: :sequoia
+
+  app "Space Rabbit.app"
+
+  uninstall quit:       "app.spacerabbit",
+            login_item: "Space Rabbit"
+
+  zap trash: "~/Library/Preferences/app.spacerabbit.plist"
+end
