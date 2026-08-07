b37565ba1bc3879e3255ad4650a4a25e4bbb3bd9
Haz Hubble <98895817+hhubble@users.noreply.github.com>
2026-08-06 22:33:43 -0700

    pally 0.1.14,181.1

--- Casks/p/pally.rb
+++ Casks/p/pally.rb
@@ -1,26 +1,29 @@
 cask "pally" do
-  version "1.99.1"
-  sha256 :no_check
+  version "0.1.14,181.1"
+  sha256 "7ef44a98e3fd4685d5a024173da38b4dab3d45cb2b9698129e8801500d2c722f"
 
-  url "https://pally.com/sparkle/Pally-Installer-Latest.dmg"
-  name "Pally"
-  desc "AI Relationship Management"
+  url "https://downloads.pally.com/companion/Pally-#{version.csv.first}-#{version.csv.second}.dmg"
+  name "Pally Companion"
+  desc "Companion app for the Pally personal assistant"
   homepage "https://pally.com/"
 
   livecheck do
-    url "https://sparkle-pally-updates.pages.dev/appcast.xml"
-    strategy :sparkle, &:short_version
+    url "https://downloads.pally.com/companion/appcast.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}"
+    end
   end
 
   auto_updates true
-  depends_on macos: :sonoma
+  depends_on macos: :ventura
 
-  app "Pally.app"
+  app "PallyCompanion.app"
 
   zap trash: [
-    "~/Library/Application Support/Pally",
-    "~/Library/Caches/pally.Pally",
-    "~/Library/Preferences/pally.Pally.plist",
-    "~/Library/Saved Application State/pally.Pally.savedState",
+    "~/Library/Application Support/pallykit",
+    "~/Library/Caches/com.pally.companion-kit",
+    "~/Library/Logs/pallykit",
+    "~/Library/Preferences/com.pally.companion-kit.plist",
+    "~/Library/Saved Application State/com.pally.companion-kit.savedState",
   ]
 end
