a39e8952679e5163b36d077eeff61f5e29ca4f24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 07:55:52 +0000

    tritium 0.2.52

--- Casks/t/tritium.rb
+++ Casks/t/tritium.rb
@@ -1,9 +1,9 @@
 cask "tritium" do
   arch arm: "arm64", intel: "x86"
 
-  version "0.2.48"
-  sha256 arm:   "bf105fffd0cd2b2e60182184b6e0e36b239903ade26637fd13fcda0226808921",
-         intel: "37cf932ad1007ddc7b3a9800dae4aa429f908bae2115b58a6b1a27a32a2c92a3"
+  version "0.2.52"
+  sha256 arm:   "c70502a8f803193bbffdcb3725d1781f5398b268c4c8ec23653da67a4040612a",
+         intel: "1399f7603645523af36c8aaddeabda214810a4b1f3186095588047c851017038"
 
   url "https://tritium.legal/static/releases/tritium-macos-#{arch}.#{version}.zip"
   name "Tritium"
