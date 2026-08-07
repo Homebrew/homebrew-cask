7a1c42778d00804dcac443a5c2cee7863a2555e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 01:02:25 +0000

    p4v 2026.2,3029854

--- Casks/p/p4v.rb
+++ Casks/p/p4v.rb
@@ -1,6 +1,6 @@
 cask "p4v" do
-  version "2026.2,2984595"
-  sha256 "9c9367ab96f667d4f1c0fd7e283e4e3678fbed1f570c8238121914b204ad3aac"
+  version "2026.2,3029854"
+  sha256 "9bec7eefac9fd1aa747fbee6602a1720a4ab82177a290358780e63f2edf9d1de"
 
   url "https://filehost.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx12u/P4V.dmg"
   name "Perforce Helix Visual Client"
