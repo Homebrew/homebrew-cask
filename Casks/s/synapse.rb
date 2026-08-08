a0a9ecbd4f132c78545b803b35dfbaa3b4756222
Harshana Amuwatte <45476434+harshanacz@users.noreply.github.com>
2026-08-08 20:41:45 +0530

    Update Synapse cask SHA256 and remove livecheck

--- Casks/s/synapse.rb
+++ Casks/s/synapse.rb
@@ -1,16 +1,11 @@
 cask "synapse" do
   version "2.2.2"
-  sha256 "cf73fcc11de44e44cef1a4ab6f1d60e9060bc84eb10dc989ceee12c22186764d"
+  sha256 "cf73fcc11de44e44cef1a4ab6f1d60e9060bc84eb10dc989ceee12c22186764"
 
   url "https://www.synapsemac.com/downloads/Synapse-#{version}.dmg"
   name "Synapse"
   desc "Every tool you need in your daily workflow"
   homepage "https://www.synapsemac.com"
-
-  livecheck do
-    url "https://www.synapsemac.com/api/appcast"
-    strategy :sparkle, &:short_version
-  end
 
   depends_on macos: :sonoma
 
