31952b738259fead2ec1971eeb87f186c759bafd
minhtc <2053300+minhtc@users.noreply.github.com>
2026-08-09 02:18:01 +0700

    Update MountMate version and homepage URL

--- Casks/m/mountmate.rb
+++ Casks/m/mountmate.rb
@@ -1,15 +1,15 @@
 cask "mountmate" do
-  version "5.11"
-  sha256 "3a7cbdca0bd11335f41b8f15ce764acd21316bcbef784a7587b573da19d04bda"
+  version "5.15"
+  sha256 "09a4de64fdde6003d9c2f69c952eff56d20bee3f87cb3f6481648694856707bb"
 
   url "https://github.com/homielab/mountmate/releases/download/v#{version}/MountMate_#{version}.dmg",
       verified: "github.com/homielab/mountmate/"
   name "MountMate"
   desc "Menubar app to easily manage external drives"
-  homepage "https://mountmate.homielab.com/"
+  homepage "https://homielab.com/en/page/mountmate"
 
   livecheck do
-    url "https://mountmate.homielab.com/appcast.xml"
+    url "https://raw.githubusercontent.com/homielab/mountmate/main/docs/appcast.xml"
     strategy :sparkle, &:title
   end
 
