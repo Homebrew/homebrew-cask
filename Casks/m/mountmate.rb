a0b4c9df9bbed43e4ed3e1c6b5eb6318c1c47c24
minhtc <2053300+minhtc@users.noreply.github.com>
2026-08-09 02:34:36 +0700

    Update MountMate version and livecheck URL

--- Casks/m/mountmate.rb
+++ Casks/m/mountmate.rb
@@ -1,6 +1,6 @@
 cask "mountmate" do
-  version "5.15"
-  sha256 "09a4de64fdde6003d9c2f69c952eff56d20bee3f87cb3f6481648694856707bb"
+  version "5.16"
+  sha256 "dea38ecc9233e7dec22cac5b13f6618a40171b1514e3dccd02a7e2e6a64c431a"
 
   url "https://github.com/homielab/mountmate/releases/download/v#{version}/MountMate_#{version}.dmg",
       verified: "github.com/homielab/mountmate/"
@@ -9,7 +9,7 @@ cask "mountmate" do
   homepage "https://homielab.com/en/page/mountmate"
 
   livecheck do
-    url "https://raw.githubusercontent.com/homielab/mountmate/main/docs/appcast.xml"
+    url "https://mountmate.homielab.com/appcast.xml"
     strategy :sparkle, &:title
   end
 
