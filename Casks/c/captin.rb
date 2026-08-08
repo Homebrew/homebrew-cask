a7156ca58e4cfe0f76101bcfc5200783f8f42789
cool8jay <cool8jay@users.noreply.github.com>
2026-08-08 17:02:59 +0800

    captin: fix audit (sha256 :no_check, macos >= big_sur)

--- Casks/c/captin.rb
+++ Casks/c/captin.rb
@@ -1,6 +1,6 @@
 cask "captin" do
   version "2.0.1"
-  sha256 "a56ee238b94f21d99c250ca95d11aba41f270ac68d6e888a4bff6628a361890a"
+  sha256 :no_check
 
   url "https://raw.githubusercontent.com/cool8jay/public/master/captin/Captin.zip",
       verified: "raw.githubusercontent.com/cool8jay/public/master/captin/"
@@ -9,7 +9,7 @@ cask "captin" do
   # https://captin.mystrikingly.com/ got redirected to some phishing site
   homepage "https://github.com/cool8jay/public"
 
-  depends_on :macos
+  depends_on macos: ">= :big_sur"
 
   app "Captin.app"
 
