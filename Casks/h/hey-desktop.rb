83c830e22c22481add05458f9d4f4581c1ee768e
Sam Ford <1584702+samford@users.noreply.github.com>
2026-08-08 18:26:17 -0400

    hey-desktop 1.3.6
    `hey-desktop` is autobumped but the workflow failed to update to
    version 1.3.6 because the `livecheck` block is encountering a curl
    error due to the `Content-Encoding: aws-chunked` response header:
    "(56) Unrecognized content encoding type. libcurl understands deflate,
    gzip content encodings." This updates the version and disables
    response compression in the `livecheck` block, which avoids the
    `aws-chunked` issue.

--- Casks/h/hey-desktop.rb
+++ Casks/h/hey-desktop.rb
@@ -1,17 +1,21 @@
 cask "hey-desktop" do
   arch arm: "-arm64"
 
-  version "1.3.3"
-  sha256 arm:   "dd20814f404850484721db5af932b1ef7e5335b4208a9e8e42421703096f049c",
-         intel: "bad2af93086131b56289965556ab52ffa2d1fafbe2262fa2dbcc96e2acbf8eaa"
+  version "1.3.6"
+  sha256 arm:   "81a10fc4901c7f402d583eaf2fc71c0b4cc2fbd42287d434b4b1ba43037c5a2b",
+         intel: "bff544af4559384b1780c9090b5da9972b6b96e54c3376c764ab116fd9139f6e"
 
   url "https://hey.com/desktop/HEY-#{version}#{arch}-mac.zip"
   name "HEY"
   desc "Access the HEY email service"
   homepage "https://hey.com/"
 
+  # This file is served with a `Content-Encoding: aws-chunked` header when
+  # compression is requested but that causes curl to error because it doesn't
+  # understand what decompression to apply.
   livecheck do
-    url "https://hey.com/desktop/latest-mac.yml"
+    url "https://hey.com/desktop/latest-mac.yml",
+        compressed: false
     strategy :electron_builder
   end
 
