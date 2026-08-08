e196b1dc41b49232e13b2e70dbf3f3ea887dc958
Harshana Amuwatte <45476434+harshanacz@users.noreply.github.com>
2026-08-08 20:03:43 +0530

    Update Synapse version and URL format

--- Casks/s/synapse.rb
+++ Casks/s/synapse.rb
@@ -1,8 +1,8 @@
 cask "synapse" do
-  version "2.2.2"
+  version "2.2.2,12"
   sha256 "cf73fcc11de44e44cef1a4ab6f1d60e9060bc84eb10dc989ceee12c22186764d"
 
-  url "https://www.synapsemac.com/downloads/Synapse-#{version}.dmg"
+  url "https://www.synapsemac.com/downloads/Synapse-#{version.csv.first}.dmg"
   name "Synapse"
   desc "Every tool you need in your daily workflow"
   homepage "https://www.synapsemac.com"
