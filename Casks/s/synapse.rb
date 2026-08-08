1f8a3e8803548cc3fbf187914a3698b4f231333c
Harshana Amuwatte <45476434+harshanacz@users.noreply.github.com>
2026-08-08 19:37:09 +0530

    Create cask for Synapse 2.2.2
    Add cask for Synapse version 2.2.2 with associated metadata.

---
+++ Casks/s/synapse.rb
@@ -0,0 +1,16 @@
+cask "synapse" do
+  version "2.2.2"
+  sha256 "cf73fcc11de44e44cef1a4ab6f1d60e9060bc84eb10dc989ceee12c22186764d"
+
+  url "https://www.synapsemac.com/downloads/Synapse-#{version}.dmg"
+  name "Synapse"
+  desc "Every tool you need in your daily workflow"
+  homepage "https://www.synapsemac.com"
+
+  app "Synapse.app"
+
+  zap trash: [
+    "~/Library/Application Support/Synapse",
+    "~/Library/Preferences/com.harshana.Synapse.plist",
+  ]
+end
