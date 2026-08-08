44789e2be97c3aeab4aae2200aa3a74a0e70c5f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-08 10:13:06 +0000

    keyboardholder 1.14.11

--- Casks/k/keyboardholder.rb
+++ Casks/k/keyboardholder.rb
@@ -1,6 +1,6 @@
 cask "keyboardholder" do
-  version "1.14.10"
-  sha256 "4a436c1a1064905f74b7587c716e19b391df06d668e363ba7b1033ea2ea3752a"
+  version "1.14.11"
+  sha256 "3331f2203fb0b03bdebf646264cf015cd4452fc707bd4d6129b6f062052a3699"
 
   url "https://github.com/leaves615/KeyboardHolder/releases/download/v#{version}/KeyboardHolder-#{version}.zip",
       verified: "github.com/leaves615/KeyboardHolder/"
