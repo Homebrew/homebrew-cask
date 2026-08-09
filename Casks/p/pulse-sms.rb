e68fe5080f666f9485a4d77b5d6f95543acc8482
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-08 20:28:31 -0400

    pulse-sms: update livecheck

--- Casks/p/pulse-sms.rb
+++ Casks/p/pulse-sms.rb
@@ -8,6 +8,23 @@ cask "pulse-sms" do
   desc "Desktop client for Pulse SMS"
   homepage "https://home.pulsesms.app/overview/"
 
+  livecheck do
+    url :url
+    regex(/^pulse[._-]sms[._-]v?(\d+(?:\.\d+)+)\.dmg$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
+
   depends_on :macos
 
   app "Pulse SMS.app"
