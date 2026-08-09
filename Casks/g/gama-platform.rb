cfcc53bed35d588b2e3565a9979aa1f61fd2e833
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-08 22:53:03 -0400

    gama-platform 2025.09.0

--- Casks/g/gama-platform.rb
+++ Casks/g/gama-platform.rb
@@ -1,11 +1,11 @@
 cask "gama-platform" do
   arch arm: "_M1"
 
-  version "2025.06.4"
-  sha256 arm:   "2778f3434d26b4035737026cd4077e81c18aae3773119480e907f9fd714d66f7",
-         intel: "b0e732958030cdac0942dd4a105befc08ec1fc353f66c076d5e78d8212765917"
+  version "2025.09.0,17.09.25_860732f"
+  sha256 arm:   "e88709354ba28b05b6cf01e879401b1d964a3f4a6bbd8026ae8afc9e7c55ca05",
+         intel: "9740ae814009e6d0ecb1bb560395bd083c9498916b5a54787b24aad962f3167e"
 
-  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_MacOS#{arch}.dmg",
+  url "https://github.com/gama-platform/gama/releases/download/#{version.csv.first}/GAMA_#{version.csv.first}_MacOS#{arch}_#{version.csv.second}.dmg",
       verified: "github.com/gama-platform/gama/"
   name "GAMA Platform"
   desc "IDE for building spatially explicit agent-based simulations"
@@ -14,11 +14,19 @@ cask "gama-platform" do
   # Using :github_latest as repo contains pre-release tags
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/GAMA[._-]v?(\d+(?:\.\d+)+)[._-]MacOS[._-](\d+(?:\.\d+)+[._-]\h+)\.dmg/i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
   end
 
   conflicts_with cask: "gama-jdk"
-  depends_on :macos
+  depends_on macos: :big_sur
 
   app "Gama.app"
 
