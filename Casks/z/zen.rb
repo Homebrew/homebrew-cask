e5c59877270c8e81b67f24a3f795cc589df276a4
AltCode <altcode@vivaldi.net>
2026-08-09 15:31:36 +0200

    zen: revert to 1.21.12b

--- Casks/z/zen.rb
+++ Casks/z/zen.rb
@@ -4,10 +4,10 @@ cask "zen" do
   livecheck_os = on_system_conditional macos: "Darwin", linux: "Linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "1.21.13b"
+  version "1.21.12b"
 
   on_macos do
-    sha256 "79bcaf03e6faaec08dda50ae42116c59b42be419bfb9c4a1ae8f56c2d098d252"
+    sha256 "dba6a9674d3199ac9b1ea8753c5f08486fe91de36ff113f7339f34423ffe5ea0"
 
     conflicts_with cask: "zen-privacy"
 
@@ -29,8 +29,8 @@ cask "zen" do
         rmdir: "~/Library/Caches/Mozilla"
   end
   on_linux do
-    sha256 arm64_linux:  "d48365d1f0925e62c59f064e1a7ef360ebbf916c8236206c80e61d0ec20a72e1",
-           x86_64_linux: "f9652c2eefb214971dc824937bbb16f7a767a26510d16b430d6803651372bfb4"
+    sha256 arm64_linux:  "390e64fc54a0ee4783052dc4cd099bc8c3835c99af60a98e0c08292c7725c6bd",
+           x86_64_linux: "401a83b89f3d889a2c230b5c350b733a453410401bffbd09f16e421013a5d5f3"
 
     app_image "zen-#{arch}.AppImage", target: "Zen.AppImage"
   end
