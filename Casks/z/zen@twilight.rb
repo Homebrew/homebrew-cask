6448dad8999b958ace0c531c7025ebd31629e564
AltCode <altcode@vivaldi.net>
2026-08-09 15:35:58 +0200

    zen@twilight: update livecheck

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -1,6 +1,8 @@
 cask "zen@twilight" do
   arch arm: "aarch64", intel: "x86_64"
-  os macos: ".macos-universal.dmg", linux: "-#{arch}.AppImage"
+  os macos: ".macos-universal", linux: "-#{arch}"
+  livecheck_os = on_system_conditional macos: "Darwin", linux: "Linux"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "1.22t,20260808062813"
   sha256 :no_check
@@ -27,13 +29,13 @@ cask "zen@twilight" do
     app_image "zen-#{arch}.AppImage", target: "Zen Twilight.AppImage"
   end
 
-  url "https://github.com/zen-browser/desktop/releases/download/twilight-1/zen#{os}"
+  url "https://github.com/zen-browser/desktop/releases/download/twilight-1/zen#{os}.#{url_end}"
   name "Zen Twilight"
   desc "Gecko based web browser"
   homepage "https://zen-browser.app/"
 
   livecheck do
-    url "https://updates.zen-browser.app/updates/browser/Darwin_aarch64-gcc3/twilight/update.xml"
+    url "https://updates.zen-browser.app/updates/browser/#{livecheck_os}_#{arch}-gcc3/twilight/update.xml"
     strategy :xml do |xml|
       xml.get_elements("//update").map { |item| "#{item.attributes["appVersion"]},#{item.attributes["buildID"]}" }
     end
