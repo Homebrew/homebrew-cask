b60ac15e0e2dbd68979cf48c784cc07709be1cbd
Sakutaro Shiina <companyemailetc@gmail.com>
2026-08-10 14:32:22 +0900

    paragon-extfs 15.0.2

--- Casks/p/paragon-extfs.rb
+++ Casks/p/paragon-extfs.rb
@@ -1,8 +1,8 @@
 cask "paragon-extfs" do
-  version "14.0.46"
+  version "15.0.2"
   sha256 :no_check
 
-  url "https://dl.paragon-software.com/demo/trial_extfs#{version.major}.dmg"
+  url "https://dl.paragon-software.com/demo/trial_extfs14.dmg"
   name "extFS for Mac by Paragon Software"
   desc "Read/write support for ext2/3/4 formatted volumes"
   homepage "https://www.paragon-software.com/home/extfs-mac/"
