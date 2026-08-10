02e5b909650a08a3f97a8f52c72e79c885477344
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 22:20:55 +0000

    microsoft-edge@beta 152.0.4191.19

--- Casks/m/microsoft-edge@beta.rb
+++ Casks/m/microsoft-edge@beta.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge@beta" do
-  version "152.0.4191.10,f661af0d-e317-4c39-ba44-0343325aa63c"
-  sha256 "7ef3a8db8b224af032ff641bda11384fef4a5a3a2c1928b8d85b5c0d32ea21bd"
+  version "152.0.4191.19,529b6b0e-10d6-4f27-a0dc-cd678c862418"
+  sha256 "c9df91455566afa3e2bf6ff6868fd514255d64681e5525510366f6e73050deee"
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdgeBeta-#{version.csv.first}.dmg"
   name "Microsoft Edge Beta"
