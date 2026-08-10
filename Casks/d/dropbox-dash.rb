65491248662b70c3683f22c8eb8444bafc0d33f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 21:56:58 +0000

    dropbox-dash 3.174.23

--- Casks/d/dropbox-dash.rb
+++ Casks/d/dropbox-dash.rb
@@ -1,6 +1,6 @@
 cask "dropbox-dash" do
-  version "3.167.12"
-  sha256 "8735ac0cfd7b31f8df60695c06694802dc11eb7340f5a259ec793130e42058ef"
+  version "3.174.23"
+  sha256 "732483eca97edd66fdb4ef15a2a39ddd7fad6927d903d07cbf83154898a0f740"
 
   url "https://edge.dropboxstatic.com/dbx-releng/products/dash-tesla/#{version}/mac.x86_64/Dropbox%20Dash-#{version}.dmg",
       verified: "edge.dropboxstatic.com/dbx-releng/products/dash-tesla/"
