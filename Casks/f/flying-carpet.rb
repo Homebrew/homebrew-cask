19a0b988bff8d5a13620d3d34d0b060722945fdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 04:24:24 +0000

    flying-carpet 10.0.4

--- Casks/f/flying-carpet.rb
+++ Casks/f/flying-carpet.rb
@@ -1,6 +1,6 @@
 cask "flying-carpet" do
-  version "10.0.0"
-  sha256 "866fb33e5df55cd1b78b4f6d710824a86ff94361a2f4d43cd8a5e4fb09d72f93"
+  version "10.0.4,10.0.0"
+  sha256 "b6db75afed626081275fd56215330fcfaf16af3070ddcfeda09c91a8c9ba2771"
 
   url "https://github.com/spieglt/FlyingCarpet/releases/download/v#{version.csv.second || version.csv.first}/macOS_FlyingCarpet_#{version.csv.first}.zip"
   name "Flying Carpet"
