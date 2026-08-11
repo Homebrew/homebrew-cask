1672c6a92a4591b5e6c61e69f713d6c9007a248c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 15:52:45 +0000

    bazecor 1.10.0

--- Casks/b/bazecor.rb
+++ Casks/b/bazecor.rb
@@ -2,11 +2,11 @@ cask "bazecor" do
   arch arm: "arm64", intel: "x64"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "1.9.2"
+  version "1.10.0"
 
   on_macos do
-    sha256 arm:   "55699405c2e2572cacc48db2fe141a421b052b26b15ad5b9df3ba9be1ee9cad6",
-           intel: "2a9b1377b7be417c14d1499d2e152e467dbbc26e0339fac14ff6f0cfa4b011ac"
+    sha256 arm:   "021ec9e364d63c4fb3d900f123c4b552cd20b356d8471a56897d5c808e15aa48",
+           intel: "0830aa2b37abf8fb33669eaa26dac5f12d3ad8fc3a82e733252de9ca34801687"
 
     depends_on macos: :big_sur
 
@@ -21,7 +21,7 @@ cask "bazecor" do
     ]
   end
   on_linux do
-    sha256 "f5e9b54bbffed9c58962a3844a0195fe4247964f9aec039a87289b37734326ff"
+    sha256 "b5d92e67b6087477ad74b8b911493d1ccbd6d265b43aab1bdbcc6c7a2b1eb364"
 
     depends_on arch: :x86_64
 
