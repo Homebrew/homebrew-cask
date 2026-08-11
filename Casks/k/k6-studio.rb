1651c31f6ecfb423a9fa4ec5d5b73ac83969bcbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 13:31:02 +0000

    k6-studio 2.1.0

--- Casks/k/k6-studio.rb
+++ Casks/k/k6-studio.rb
@@ -1,9 +1,9 @@
 cask "k6-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.2"
-  sha256 arm:   "53407a79feb4e2cf12f4e6071af5775296ccf223780dcaf433ba59007cdd31d6",
-         intel: "85a244482dda38daf5b6ea44a15815c3b9be68332426e6563da8926b99c8ee66"
+  version "2.1.0"
+  sha256 arm:   "380133688e774540a974ea037cdefaf1340b12284c77092ff06fd2390e3348a8",
+         intel: "20dcf6f3dd2e5e5e5e7380e9e5b5250f48ff9a12ef6231cafb25679a26290df0"
 
   url "https://github.com/grafana/k6-studio/releases/download/v#{version}/k6.Studio-#{version}-#{arch}.dmg",
       verified: "github.com/grafana/k6-studio/"
