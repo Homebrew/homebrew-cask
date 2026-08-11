90d06d0a246f7b08799f605c4db632f4f2004ca4
Ike Sterzinger <ike.sterzinger@worksheetcrafter.com>
2026-08-11 13:12:56 +0200

    worksheetcrafter: renamed from worksheet-crafter

--- Casks/w/worksheetcrafter.rb
+++ Casks/w/worksheetcrafter.rb
@@ -1,10 +1,9 @@
-cask "worksheet-crafter" do
+cask "worksheetcrafter" do
   version "2026.2.5"
   sha256 "591681374f78f55e6c9f1fc258209eebab1a1a0f0d7044588dd7209ae70056f2"
 
-  url "https://website.cdn.getschoolcraft.com/downloads/worksheet-crafter_#{version}.pkg",
-      verified: "website.cdn.getschoolcraft.com/downloads/"
-  name "Worksheet Crafter"
+  url "https://website.cdn.getschoolcraft.com/downloads/worksheet-crafter_#{version}.pkg"
+  name "WorksheetCrafter"
   desc "Worksheet and lesson material creator"
   homepage "https://worksheetcrafter.com/"
 
