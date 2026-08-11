a77b3122140f8ebd5a0b7700431091f16643d4a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 10:11:50 +0000

    font-ark-pixel-10px-proportional 2026.08.11

--- Casks/font/font-a/font-ark-pixel-10px-proportional.rb
+++ Casks/font/font-a/font-ark-pixel-10px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-proportional" do
-  version "2026.07.20"
-  sha256 "9bcb74f7dc66e4672d61873172f78c5b6439b060638d3ba36c2b3b48c54f7741"
+  version "2026.08.11"
+  sha256 "988038c93356405c51aab1303c6014458685a10dfac70a2beb74d4a685fdd0a4"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 10px Proportional"
