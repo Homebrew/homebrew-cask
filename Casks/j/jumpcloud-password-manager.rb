c36a4b884edf497b20bfda58ae11630a225686d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 10:23:35 +0000

    jumpcloud-password-manager 3.3.49

--- Casks/j/jumpcloud-password-manager.rb
+++ Casks/j/jumpcloud-password-manager.rb
@@ -1,9 +1,9 @@
 cask "jumpcloud-password-manager" do
   arch arm: "arm64/"
 
-  version "3.3.45"
-  sha256 arm:   "488e99e06330d0fe95272976461184784919cc2ead56766b28853fccac3337bb",
-         intel: "fd0923181bb0ae271ab7d3101f551e1c8efa39f38b5bc666026e99312a3a7c75"
+  version "3.3.49"
+  sha256 arm:   "69934cda2d3245d2d53288ab3051cc3935ed57dc653e8e6ddf1a80a32ea00d00",
+         intel: "d035c0b572857d645d13e95dceceba1adc7a2e54d49f8fb4df6292c5bf0aee55"
 
   url "https://cdn.pwm.jumpcloud.com/DA/release/#{arch}JumpCloud-Password-Manager-#{version}.dmg"
   name "JumpCloud Password Manager"
