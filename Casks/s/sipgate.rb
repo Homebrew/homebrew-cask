49d623921f675585a18b58516b6198c5659e8059
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 14:17:06 +0000

    sipgate 2.43.2

--- Casks/s/sipgate.rb
+++ Casks/s/sipgate.rb
@@ -1,6 +1,6 @@
 cask "sipgate" do
-  version "2.42.0"
-  sha256 "dabca660ad6f0de94e1fc2c00aa342fb30952069519967fa50cb05c1fb41b59e"
+  version "2.43.2"
+  sha256 "9d3a23a9d2fa0852ee02d3e5641278ad5622fcca399a3ec476861ccd7c948f84"
 
   url "https://s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/sipgate-#{version}.zip",
       verified: "s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/"
