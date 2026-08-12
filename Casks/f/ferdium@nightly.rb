5b8f0f02cd6137f327f36065cee1e2ad112d4a68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 16:04:16 +0000

    ferdium@nightly 7.1.3-nightly.10

--- Casks/f/ferdium@nightly.rb
+++ Casks/f/ferdium@nightly.rb
@@ -1,9 +1,9 @@
 cask "ferdium@nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.1.3-nightly.9"
-  sha256 arm:   "62bcc2691d928d449f3252d3b9b72d29a70b24232a3862002a756c26d1ac0ff4",
-         intel: "ed60b09fb53933a9939861fac320558131f74c088fd129839dcd7669936174a1"
+  version "7.1.3-nightly.10"
+  sha256 arm:   "040f91edc109e7f9829bea1fd852c70cc2332fc17feffad2303b9ca5d8947a83",
+         intel: "9c624132bf11262d5664479ce6d2b9ccd3f2022dfbe5beed4881e6b782e20809"
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
       verified: "github.com/ferdium/ferdium-app/"
