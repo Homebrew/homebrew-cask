fa57e3a94cdc7fcfa4d9b9139f1bb61889b5d8bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 13:40:04 +0000

    openwork 0.18.21

--- Casks/o/openwork.rb
+++ Casks/o/openwork.rb
@@ -3,11 +3,11 @@ cask "openwork" do
   os macos: "mac", linux: "linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "0.18.20"
-  sha256 arm:          "68f36e751e77dfa365db8ad52426c0a7b328934fe82eb24ab0663e7b9ed8b0c7",
-         intel:        "be003e07613e8668fa2e330795c9cc7f08fb89d4193bee5c61f8768a24b52ed1",
-         arm64_linux:  "2b6ea2dc4daf16aa9e94cb60348dacfd504aa5f30dd79471cd0871257acad6f8",
-         x86_64_linux: "b05fedc9a432efe172505617a44965a3160548ea7401ecde30f9f3b4a2a0b4a8"
+  version "0.18.21"
+  sha256 arm:          "363d608f7e509a7ac1df5566141b3d0cb12e485830def5963fedce892f38ea55",
+         intel:        "19e5f23273d71ae37a81c26ddc270ca5ba6e6d84e1a14edb6c1d7ae5c2cc0acf",
+         arm64_linux:  "094ce7952873c265ff6d727696c41cd52f5217b475795d28665cb6b7af38b5cb",
+         x86_64_linux: "8645fbf760e59a7a442c83dd6c114ba4440e58577565a63e97c219db93210ffd"
 
   on_macos do
     auto_updates true
