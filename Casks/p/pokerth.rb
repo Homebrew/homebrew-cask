afea9efc1cc87726d40968d6b4755073eb00e4dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 01:40:35 +0000

    pokerth 2.1.7

--- Casks/p/pokerth.rb
+++ Casks/p/pokerth.rb
@@ -1,6 +1,6 @@
 cask "pokerth" do
-  version "2.1.6"
-  sha256 "be7d11d357156b729e9a52dbc53d344a8ee189480b0acff857950570f830ae34"
+  version "2.1.7"
+  sha256 "12dd48e42eb6f629604bb519a79079282d74302de044283e97a7e1dea0954193"
 
   url "https://downloads.sourceforge.net/pokerth/PokerTH-#{version}-Combined.dmg",
       verified: "downloads.sourceforge.net/pokerth/"
