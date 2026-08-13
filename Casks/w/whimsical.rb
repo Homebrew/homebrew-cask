c4ccc3392d2fae68a25533914724eb04310d1976
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 14:09:11 +0000

    whimsical 2026.10.2

--- Casks/w/whimsical.rb
+++ Casks/w/whimsical.rb
@@ -1,9 +1,9 @@
 cask "whimsical" do
   arch arm: "arm64", intel: "x64"
 
-  version "2026.10.1,260729oshpovqhh"
-  sha256 arm:   "60ec80be7f3246e2578f123b1b5e24b7fe84ed4b4d6ce984639812a3055c2b6f",
-         intel: "f0359a24a6ddbe71699fed2527f1f2ad115ace9bee68321cc5f365d3020f865e"
+  version "2026.10.2,260812y9dp5bzsh"
+  sha256 arm:   "bd481850d38356027b8689f4de6ce56369cf2c9f060574227a7b8b489e28cfdb",
+         intel: "a66e0d949f0d312bf1acc80260cdbf4b05d699b20ec781fe0e9aeffaf4a5092c"
 
   url "https://download.todesktop.com/2402209z4j37b73/Whimsical%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/2402209z4j37b73/"
