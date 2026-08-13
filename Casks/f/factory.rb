76bba1c296c3cf871fd39fdc44de87d28b77985f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 04:34:31 +0000

    factory 0.152.0

--- Casks/f/factory.rb
+++ Casks/f/factory.rb
@@ -1,7 +1,7 @@
 cask "factory" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.151.1"
+  version "0.152.0"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://downloads.factory.ai/factory-desktop/releases/#{version}/darwin/#{arch}/Factory-#{version}-#{arch}.dmg"
