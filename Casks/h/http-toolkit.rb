c4df7eeabb552b1f45a21dc6fcc0f9ec0bf13e9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 13:17:05 +0000

    http-toolkit 1.27.1

--- Casks/h/http-toolkit.rb
+++ Casks/h/http-toolkit.rb
@@ -1,9 +1,9 @@
 cask "http-toolkit" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.27.0"
-  sha256 arm:   "07df157f5f5d7e3574003cc0422e150f09554d90d8a6489de35629c4590d8206",
-         intel: "aba38ed0539bd5864d9958a390cee0308d7b47b2c174034a9f23f8181c7177c0"
+  version "1.27.1"
+  sha256 arm:   "9fb15e0de154cb88debbc298bdc96e7c20d657c4eb138555b6c0603e1dcb07ec",
+         intel: "9f670af93770bd85d7507e67d2ec38c85820ecd0a433a8998a090d09fcf65f8e"
 
   url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HttpToolkit-#{version}-#{arch}.dmg",
       verified: "github.com/httptoolkit/httptoolkit-desktop/"
