a2993354587f4f11ec0760d0fb57bb68886a0c59
for13to1 <for13to1@outlook.com>
2026-08-14 02:41:34 +0800

    Add hdrview cask
    HDRView is a research-oriented HDR image viewer with prebuilt macOS DMG releases. Includes livecheck for automatic updates and a zap stanza for preferences/saved state.

---
+++ Casks/h/hdrview.rb
@@ -0,0 +1,26 @@
+cask "hdrview" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "2.8.3"
+  sha256 arm:   "f12c26d7edc26f045fdae7a190e8137d3ca3e5059076c77d6944eb88c40887ba",
+         intel: "5ff65eca6567a310428c7c151dac50729db667e434038f5e480d7a8d15bb813a"
+
+  url "https://github.com/wkjarosz/hdrview/releases/download/v#{version}/HDRView-#{version}-#{arch}.dmg"
+  name "HDRView"
+  desc "Research-oriented, high-dynamic range (HDR) image viewer and comparison tool"
+  homepage "https://github.com/wkjarosz/hdrview"
+
+  livecheck do
+    url "https://github.com/wkjarosz/hdrview/releases"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on macos: :big_sur
+
+  app "HDRView.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.im.hdrview.hdrview.plist",
+    "~/Library/Saved Application State/com.im.hdrview.hdrview.savedState",
+  ]
+end
