2b2267b02e19e7c671c7003f05a06bbc4e4ce00b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 10:30:40 +0000

    openchrom 1.6.28

--- Casks/o/openchrom.rb
+++ Casks/o/openchrom.rb
@@ -1,9 +1,9 @@
 cask "openchrom" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.6.27"
-  sha256 arm:   "e46e521325b89b3470afcba5eeabed64222ee52df01b9c4ebc6f01de3c18bf15",
-         intel: "7f6efa56ccae3ef6c44d7cba3784aecacb144258dbb3a71a002e173698535557"
+  version "1.6.28"
+  sha256 arm:   "89fa385d34bd6524acfe9fee4ecad80be16dcf9a323a352304b9db139392931b",
+         intel: "fd06aa1a6cdd3e523c4e20eeefb73cb5ac297619c5f03ce4c280ba18065b7871"
 
   url "https://products.lablicate.com/openchrom/#{version}/OpenChrom_#{version}_#{arch}.dmg",
       verified: "products.lablicate.com/openchrom/"
