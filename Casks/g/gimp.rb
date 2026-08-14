e4bc72f0907a529a41c6342f5ff3dbac2435df72
AltCode <altcode@vivaldi.net>
2026-08-14 14:48:25 +0200

    gimp: update various

--- Casks/g/gimp.rb
+++ Casks/g/gimp.rb
@@ -1,11 +1,14 @@
 cask "gimp" do
   arch arm: on_system_conditional(macos: "arm64", linux: "aarch64"), intel: "x86_64"
+  os macos: "macos", linux: "linux"
+  livecheck_os = on_system_conditional macos: "macos", linux: "appimage"
+  name_start = on_system_conditional macos: "gimp", linux: "GIMP"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   on_macos do
     on_catalina :or_older do
       version "2.10.38,1"
-      sha256 arm:   "dc1aa78a40695f9f4580ce710960ff411eeef48af45b659b03b51e4cd6cdf4e8",
-             intel: "d2d3ac20c762fe12f0dd0ec8d7c6c2f1f3a43e046ecb4ed815a49afcbaa92b92"
+      sha256 "d2d3ac20c762fe12f0dd0ec8d7c6c2f1f3a43e046ecb4ed815a49afcbaa92b92"
 
       livecheck do
         skip "Legacy version"
@@ -20,7 +23,7 @@ cask "gimp" do
         url "https://www.gimp.org/gimp_versions.json"
         strategy :json do |json|
           json["STABLE"]&.map do |release|
-            release["macos"]&.map do |build|
+            release[livecheck_os]&.map do |build|
               next unless build["filename"]&.match?(/#{arch}/i)
               next release["version"] unless build["revision"]
 
@@ -31,11 +34,8 @@ cask "gimp" do
       end
     end
 
-    url "https://download.gimp.org/gimp/v#{version.major_minor}/macos/gimp-#{version.csv.first}-#{arch}#{"-#{version.csv.second}" if version.csv.second}.dmg"
-
     app "GIMP.app"
-    command_wrapper "gimp",
-                    executable: "#{appdir}/GIMP.app/Contents/MacOS/gimp"
+    command_wrapper "gimp", executable: "#{appdir}/GIMP.app/Contents/MacOS/gimp"
 
     zap trash: [
       "~/Library/Application Support/Gimp",
@@ -48,14 +48,11 @@ cask "gimp" do
     sha256 arm64_linux:  "7cb6f5c2b5a693302beb3e41c987c47562ff146eed4125b19ecf414ba6dca0ab",
            x86_64_linux: "f1ce6dc671ef1c4aad87a0db9d7462e8ca9c0b5f899456337803c6ba32d0babe"
 
-    filename = "GIMP-#{version.csv.first}-#{arch}#{"-#{version.csv.second}" if version.csv.second}.AppImage"
-    url "https://download.gimp.org/gimp/v#{version.major_minor}/linux/#{filename}"
-
     livecheck do
       url "https://www.gimp.org/gimp_versions.json"
       strategy :json do |json|
         json["STABLE"]&.map do |release|
-          release["appimage"]&.map do |build|
+          release[livecheck_os]&.map do |build|
             next unless build["filename"]&.match?(/#{arch}/i)
             next release["version"] unless build["revision"]
 
@@ -65,11 +62,13 @@ cask "gimp" do
       end
     end
 
-    app_image filename, target: "GIMP.AppImage"
+    app_image "GIMP-#{version.csv.first}-#{arch}#{"-#{version.csv.second}" if version.csv.second}.AppImage",
+              target: "GIMP.AppImage"
 
     zap trash: "~/.config/GIMP/#{version.major_minor}"
   end
 
+  url "https://download.gimp.org/gimp/v#{version.major_minor}/#{os}/#{name_start}-#{version.csv.first}-#{arch}#{"-#{version.csv.second}" if version.csv.second}.#{url_end}"
   name "GIMP"
   name "GNU Image Manipulation Program"
   desc "Free and open-source image editor"
