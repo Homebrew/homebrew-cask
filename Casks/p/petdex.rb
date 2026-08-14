2012615c98c88e0c7be58ac7ee39e7400b8b350a
Railly <railly_2009@hotmail.com>
2026-08-14 02:45:48 -0300

    petdex: remove deprecated verified parameter

--- Casks/p/petdex.rb
+++ Casks/p/petdex.rb
@@ -5,8 +5,7 @@ cask "petdex" do
   sha256 arm:   "dc33a6e61fbcf2acb671e3b686e059ea45e6308e28118543d6a6e4318bd17a4d",
          intel: "9377d0e7d5482a21c1c336361e815e6deafc00702d1d20a8ee3fecdc4313bbb3"
 
-  url "https://github.com/crafter-station/petdex/releases/download/desktop-v#{version}/Petdex-#{arch}.dmg",
-      verified: "github.com/crafter-station/petdex/"
+  url "https://github.com/crafter-station/petdex/releases/download/desktop-v#{version}/Petdex-#{arch}.dmg"
   name "Petdex"
   desc "Desktop pet that reflects coding agent activity"
   homepage "https://petdex.dev/"
