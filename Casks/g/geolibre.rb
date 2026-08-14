abbe886e453ab999c48da1b7aca179b7b3e28f76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 04:38:58 +0000

    geolibre 2.6.0

--- Casks/g/geolibre.rb
+++ Casks/g/geolibre.rb
@@ -1,9 +1,9 @@
 cask "geolibre" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.5.0"
-  sha256 arm:   "cb1fd009f553b6dd0cc663597d71f312f43be85ef6a43446437575fc5860a1c8",
-         intel: "8e016ac75b9ec8f267af9980a8e7185ad23d77490b0b8b120c4204e45babb56b"
+  version "2.6.0"
+  sha256 arm:   "b9c5ba1ca89291ed7d6cf739abe3693a0a8e7a036ba0f77826511d02f04ccf20",
+         intel: "8ce72ad5e0feccfdfed22ca4112d11fcda4e72dd7c616eb179c699be5d502e86"
 
   url "https://github.com/opengeos/GeoLibre/releases/download/v#{version}/GeoLibre.Desktop_#{version}_#{arch}.dmg",
       verified: "github.com/opengeos/GeoLibre/"
