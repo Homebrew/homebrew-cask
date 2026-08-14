ebc1cfe022516d411b12ee1a36783ec472646476
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 09:54:30 +0000

    codexbar 0.49.6

--- Casks/c/codexbar.rb
+++ Casks/c/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
-  version "0.49.5"
-  sha256 "586e0783c844aa057c0a37a2a2beb09e031f47395015d77f60f3b1c36fe8a75c"
+  version "0.49.6"
+  sha256 "404f319ccd81f6997c4255952c113330d664ee129c0d15f24323b75319949719"
 
   url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-macos-universal-#{version}.zip",
       verified: "github.com/steipete/CodexBar/"
