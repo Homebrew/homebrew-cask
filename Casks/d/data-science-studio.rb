30ff23718cb7cc408a1e34d5dd3a696c83a44609
Sakutaro Shiina <companyemailetc@gmail.com>
2026-08-14 15:48:35 +0900

    data-science-studio@15.0.1

--- Casks/d/data-science-studio.rb
+++ Casks/d/data-science-studio.rb
@@ -1,18 +1,32 @@
 cask "data-science-studio" do
-  version "14.0.3"
-  sha256 "56a687b84efb970d21ae0d6f0c377814e42d8e81b95b6004eac3020678d892b7"
+  arch arm: "-arm64"
 
-  url "https://cdn.downloads.dataiku.com/public/launcher/macos/DataScienceStudio-#{version}-mac.zip"
+  on_arm do
+    version "15.0.1"
+    sha256 "db79190f424b991af11749c5a17f4f53cc2d005bdc596e9ca315edf58261b1f2"
+    
+    livecheck do
+      url "https://cdn.downloads.dataiku.com/public/launcher/macos/latest-mac.yml"
+      strategy :electron_builder
+    end
+
+    depends_on macos: :monterey
+  end
+  on_intel do
+    version "14.0.3"
+    sha256 "56a687b84efb970d21ae0d6f0c377814e42d8e81b95b6004eac3020678d892b7"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    depends_on macos: :big_sur
+  end
+
+  url "https://cdn.downloads.dataiku.com/public/launcher/macos/DataScienceStudio-#{version}#{arch}-mac.zip"
   name "Dataiku Data Science Studio"
   desc "Quick experimentation and operationalization for machine learning at scale"
   homepage "https://www.dataiku.com/"
-
-  livecheck do
-    url "https://cdn.downloads.dataiku.com/public/launcher/macos/latest-mac.yml"
-    strategy :electron_builder
-  end
-
-  depends_on :macos
 
   app "DataScienceStudio.app"
 
@@ -23,7 +37,4 @@ cask "data-science-studio" do
     "~/Library/Saved Application State/com.dataiku.datasciencestudio.savedState",
   ]
 
-  caveats do
-    requires_rosetta
-  end
 end
