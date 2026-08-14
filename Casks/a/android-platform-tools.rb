9052338f5cee269a82462c812cfd0e53debe5fd8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 19:34:23 -0400

    android-platform-tools: remove duplicate arch sha256

--- Casks/a/android-platform-tools.rb
+++ Casks/a/android-platform-tools.rb
@@ -2,10 +2,15 @@ cask "android-platform-tools" do
   os macos: "darwin", linux: "linux"
 
   version "37.0.1"
-  sha256 arm:          "ee39ad5967e95c2a07f04dbcbde96b1a0c916ba376096db5d2f498b7727a5d1d",
-         x86_64:       "ee39ad5967e95c2a07f04dbcbde96b1a0c916ba376096db5d2f498b7727a5d1d",
-         x86_64_linux: "d230f13842f60f782a8645f9c813f8f845bf36089ea7289f28c48f17979313f1",
-         arm64_linux:  "d230f13842f60f782a8645f9c813f8f845bf36089ea7289f28c48f17979313f1"
+
+  on_macos do
+    sha256 "ee39ad5967e95c2a07f04dbcbde96b1a0c916ba376096db5d2f498b7727a5d1d"
+  end
+  on_linux do
+    sha256 "d230f13842f60f782a8645f9c813f8f845bf36089ea7289f28c48f17979313f1"
+
+    depends_on arch: :x86_64
+  end
 
   url "https://dl.google.com/android/repository/platform-tools_r#{version}-#{os}.zip",
       verified: "google.com/android/repository/"
