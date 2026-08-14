cde18b07c86ad35acd306005b78b5c296b505fcc
Jiawen Geng <technicalcute@gmail.com>
2026-08-14 03:38:38 +0000

    cursor-cli: add linux support

--- Casks/c/cursor-cli.rb
+++ Casks/c/cursor-cli.rb
@@ -1,11 +1,14 @@
 cask "cursor-cli" do
   arch arm: "arm64", intel: "x64"
+  os macos: "darwin", linux: "linux"
 
   version "2026.08.11-e8db854"
-  sha256 arm:   "46044d6d7bcbd7b49a0cf1cd01aa4ca79aaa2ea5f2c7a32965fc0ebe29841790",
-         intel: "d5c1ce96dd36469e0231d818d4ccf390caac52d94e607c56ebeecc247cab2b1b"
+  sha256 arm:          "46044d6d7bcbd7b49a0cf1cd01aa4ca79aaa2ea5f2c7a32965fc0ebe29841790",
+         intel:        "d5c1ce96dd36469e0231d818d4ccf390caac52d94e607c56ebeecc247cab2b1b",
+         arm64_linux:  "ea13f92e295f523a99ce8d8f57d6894d21e5d1e2d030ffad718ccd5955ca2eed",
+         x86_64_linux: "bfff4bf6f4e9dd30c1d0ef0a70b6077b074015dd2948e4c50685d53afdcfce5a"
 
-  url "https://downloads.cursor.com/lab/#{version}/darwin/#{arch}/agent-cli-package.tar.gz"
+  url "https://downloads.cursor.com/lab/#{version}/#{os}/#{arch}/agent-cli-package.tar.gz"
   name "Cursor CLI"
   desc "Command-line agent for Cursor"
   homepage "https://cursor.com/"
@@ -15,8 +18,7 @@ cask "cursor-cli" do
     regex(%r{downloads\.cursor\.com/lab/v?(\d+(?:[.-]\d+)+(?:[._-]\h+)?)/}i)
   end
 
-  depends_on :macos
-
+  binary "#{staged_path}/dist-package/cursor-agent", target: "agent"
   binary "#{staged_path}/dist-package/cursor-agent", target: "cursor-agent"
 
   zap trash: [
