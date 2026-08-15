15d615faee4e9b534977ce203813ad70cfe0cfe3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 22:39:55 -0400

    1password-cli: order sha256 arm64_linux before x86_64_linux

--- Casks/1/1password-cli.rb
+++ Casks/1/1password-cli.rb
@@ -5,8 +5,8 @@ cask "1password-cli" do
   version "2.39.0"
   sha256 arm:          "05391d3388a0c0b4f602691bedc1ab368541c487b6f14d2e3399743b4682af67",
          intel:        "753fbf56b00996426edbb8439d2f3c0be9227b9557cdff468fb144cd3621aa6e",
-         x86_64_linux: "6fba7f376b6c6dec49f41b06408930a43ad064cce103c6a2ce5b3d0413a86434",
-         arm64_linux:  "829baeff1c07e055cfa132031b1d9f2282ccdf5076258e482caf2fda70aea5d0"
+         arm64_linux:  "829baeff1c07e055cfa132031b1d9f2282ccdf5076258e482caf2fda70aea5d0",
+         x86_64_linux: "6fba7f376b6c6dec49f41b06408930a43ad064cce103c6a2ce5b3d0413a86434"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_#{os}_#{arch}_v#{version}.zip"
   name "1Password CLI"
