f9d16d91a45f3120d8d2948bc50a27acb6c3f143
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 20:13:36 -0400

    super: use intel instead of x86_64

--- Casks/s/super.rb
+++ Casks/s/super.rb
@@ -4,7 +4,7 @@ cask "super" do
 
   version "0.3.0"
   sha256 arm:          "b5124ec88e1ba908909902f98c6ebc9754d034cecd0690f078a52f384dbf6eeb",
-         x86_64:       "2c7cf6c21c4c486541b3d34df614a07fc6cea9b45bb8b63f25e3df64136ae3a4",
+         intel:        "2c7cf6c21c4c486541b3d34df614a07fc6cea9b45bb8b63f25e3df64136ae3a4",
          x86_64_linux: "2bbc1fab55be4124a4ffa253eba806590b73509935ac8740599a47caf9d3185d",
          arm64_linux:  "81384df59e7ca8d2a59855e03040c58a7402539b75dc55e78de46bf68cb49257"
 
