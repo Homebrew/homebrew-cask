575630f70b49c4dc064016013f216b5f949b1c24
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 20:13:35 -0400

    duplicacy-cli: use intel instead of x86_64

--- Casks/d/duplicacy-cli.rb
+++ Casks/d/duplicacy-cli.rb
@@ -4,7 +4,7 @@ cask "duplicacy-cli" do
 
   version "3.2.5"
   sha256 arm:          "717ef792f38c3ead9f068e8b9f32e1d67641a974bd46ee29416d27afda8fbe7f",
-         x86_64:       "3621e3fc9ecafe37f500fe0effeaf1b0a79cfe26ea7135fe3bf34ddb8e787bdd",
+         intel:        "3621e3fc9ecafe37f500fe0effeaf1b0a79cfe26ea7135fe3bf34ddb8e787bdd",
          arm64_linux:  "9c27d8ba149e67d0bc58406c6b3218661d870cb07e265aec31563540f8f20598",
          x86_64_linux: "548526d462fb38c23f2bf62ea3b1177b8ad11cc1499fa3dbe092a607d68d84f5"
 
