a8270d9a92db5ff8196a96046f6e6d2891e958b4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-15 18:47:27 -0400

    rowboat 0.8.7

--- Casks/r/rowboat.rb
+++ Casks/r/rowboat.rb
@@ -1,9 +1,9 @@
 cask "rowboat" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.8.3"
-  sha256 arm:   "79c7f7dc53400e89883b1a25848f9ccaea74f0ecb6e3287e97b49d935e3e93d6",
-         intel: "0a5ae6bd1230e0d5ce00cbc4861e0ba515d3ac31ba3deedb050ce0402ea75c64"
+  version "0.8.7"
+  sha256 arm:   "468459950d259bede43241ade0be8efff203f5b0448ee835db20a38879d57058",
+         intel: "eb07fbd706c21ec90c2fba898645c4802e43cbf15e22238986e3e14858f11827"
 
   url "https://github.com/rowboatlabs/rowboat/releases/download/v#{version}/Rowboat-darwin-#{arch}-#{version}.zip",
       verified: "github.com/rowboatlabs/rowboat/"
@@ -18,7 +18,7 @@ cask "rowboat" do
 
   depends_on macos: :monterey
 
-  app "rowboat.app"
+  app "Rowboat.app"
 
   zap trash: [
     "~/.rowboat",
