18093ea7ea17f9b363ca643777b63eaf252bf524
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 20:13:35 -0400

    gitcomet: use intel instead of x86_64

--- Casks/g/gitcomet.rb
+++ Casks/g/gitcomet.rb
@@ -5,7 +5,7 @@ cask "gitcomet" do
 
   version "0.1.16"
   sha256 arm:          "2cd4049ea4a8efc0e6f2ff97c9fbb480496b08f8db44de4be75fde1a26b0e822",
-         x86_64:       "3d0b67aabcf61e866215b5214f5ae98d0bf6cb2a65101034083705cd26d63bba",
+         intel:        "3d0b67aabcf61e866215b5214f5ae98d0bf6cb2a65101034083705cd26d63bba",
          arm64_linux:  "33b5a5221d73d1e98c853d2880173bfd8cf74d88f7384bfc003e00fed1323884",
          x86_64_linux: "b5762a88390c3e8a5498529416252c01babedf8461aed3c65a8cea4aab9ea4ac"
 
