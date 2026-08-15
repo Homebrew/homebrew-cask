66e284a6853e812e6331c77c0e2d015ad92a2408
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 22:39:56 -0400

    claude-code@latest: order sha256 arm64_linux before x86_64_linux

--- Casks/c/claude-code@latest.rb
+++ Casks/c/claude-code@latest.rb
@@ -5,8 +5,8 @@ cask "claude-code@latest" do
   version "2.1.233"
   sha256 arm:          "bc466b6cde63edafc773f471a1fb98787fabb31f52240c8616ce7e1f587b212d",
          intel:        "8b3ae18df411098ce55705c4bb151e9c97e34df55fe379c7b6b3122a69f0ea74",
-         x86_64_linux: "55d281096f57d411ebbdd94dbf5e9ff3accb7c05713e37348c2c11d4b83bf9d9",
-         arm64_linux:  "42df1841f74e9b2ac13f2c1a2a820ef6b9ac5b2efb8646bb25c9a92b8bd69194"
+         arm64_linux:  "42df1841f74e9b2ac13f2c1a2a820ef6b9ac5b2efb8646bb25c9a92b8bd69194",
+         x86_64_linux: "55d281096f57d411ebbdd94dbf5e9ff3accb7c05713e37348c2c11d4b83bf9d9"
 
   url "https://downloads.claude.ai/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "downloads.claude.ai/claude-code-releases/"
