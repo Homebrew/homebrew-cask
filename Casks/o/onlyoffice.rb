1da7afe782bc90e39446cb840995800d9e9f01a0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 20:13:36 -0400

    onlyoffice: use intel instead of x86_64

--- Casks/o/onlyoffice.rb
+++ Casks/o/onlyoffice.rb
@@ -4,7 +4,7 @@ cask "onlyoffice" do
 
   version "9.4.0"
   sha256 arm:          "e965be2222609add6b5a70baa2a8cdb599402491fb2925825d9039dcb154beb4",
-         x86_64:       "43ac517493c0c316f268ce4b7dc3810b77a7aefe83c0edc1655476d9f21681d2",
+         intel:        "43ac517493c0c316f268ce4b7dc3810b77a7aefe83c0edc1655476d9f21681d2",
          arm64_linux:  "7dfc2fa195ff9912a4022841613b9a99fdba2a238792648f257007a7df887b7c",
          x86_64_linux: "f5faf24552665262fe94486a510b997d2e5a24bde14df94c802c79bf17f9254c"
 
