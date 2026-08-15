a1b44c7b7f6d2195f8501dc4909f064952aa4b25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-15 19:29:43 +0000

    sequel-ace 5.4.0

--- Casks/s/sequel-ace.rb
+++ Casks/s/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "5.3.1,20104"
-  sha256 "b66755ec66716e977e295ceb3daca46e081149067adc7e2b087b3dbdf9239f90"
+  version "5.4.0,20109"
+  sha256 "1522e43725f4a990a7cd7f24cc7ad7b2979a6c891f0cc72d4b6554774820b5bb"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.csv.first}-#{version.csv.second}/Sequel-Ace-#{version.csv.first}.zip"
   name "Sequel Ace"
