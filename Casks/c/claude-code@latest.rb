d4ed43dd15ec5ac16a5f266eefcddd3714d83378
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-14 20:13:34 -0400

    claude-code@latest: use intel instead of x86_64

--- Casks/c/claude-code@latest.rb
+++ Casks/c/claude-code@latest.rb
@@ -4,7 +4,7 @@ cask "claude-code@latest" do
 
   version "2.1.232"
   sha256 arm:          "7b39c1588df919d001dea3ffd5651adb682f2451b5a0e18d42d4233296b53cc7",
-         x86_64:       "aa3d606d7bf0ea9739a6d0de11810e72a662e7a4e5061d67ee7f8bc47c8890f9",
+         intel:        "aa3d606d7bf0ea9739a6d0de11810e72a662e7a4e5061d67ee7f8bc47c8890f9",
          x86_64_linux: "61d23f8749136907d586d5b11831ea8a5234d4c1dea40a5e55c33b52e204c6d1",
          arm64_linux:  "20797ebc644dfc47a69865c46d5cf702c7dbedd48d4268063b8828ebd55b39d0"
 
