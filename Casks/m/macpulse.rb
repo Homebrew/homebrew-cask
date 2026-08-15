1412c4c689625b3572d101f7d55a5d8550203bac
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-15 18:18:29 -0400

    macpulse: add uninstall

--- Casks/m/macpulse.rb
+++ Casks/m/macpulse.rb
@@ -17,10 +17,16 @@ cask "macpulse" do
 
   app "MacPulse.app"
 
+  uninstall quit: "com.jupe.MacPulse"
+
   zap trash: [
+    "~/Library/Application Scripts/com.jupe.MacPulse.MacPulseWidgets",
+    "~/Library/Application Support/com.jupe.MacPulse",
     "~/Library/Application Support/MacPulse",
     "~/Library/Caches/com.jupe.MacPulse",
+    "~/Library/Containers/com.jupe.MacPulse.MacPulseWidgets",
     "~/Library/HTTPStorages/com.jupe.MacPulse",
     "~/Library/Preferences/com.jupe.MacPulse.plist",
+    "~/Library/Preferences/com.jupe.MacPulse.systemcache.plist",
   ]
 end
