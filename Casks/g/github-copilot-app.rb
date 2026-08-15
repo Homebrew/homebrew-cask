61e118aa706dec83e24f5fd91d6c97c268aa5f44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-15 01:14:25 +0000

    github-copilot-app 1.1.10

--- Casks/g/github-copilot-app.rb
+++ Casks/g/github-copilot-app.rb
@@ -3,11 +3,11 @@ cask "github-copilot-app" do
   os macos: "darwin", linux: "linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "1.1.9"
-  sha256 arm:          "1cb342f60e14e32a4280f9bab62fe67549275e122a6a9558052039db6a2e9b79",
-         intel:        "ff6e965f5acb02868118b133336cabfa4c2a75178194d990753327ef6e2caae2",
-         arm64_linux:  "24060de2d72d71ffa0724293836f3201aee169ab6a14036c6edd59d78297bc62",
-         x86_64_linux: "9921f30fe6e9f63c0ebc9fb0642cab08bf3e19dacbbe3ad62acacb301021ba64"
+  version "1.1.10"
+  sha256 arm:          "7cde14aabe4624e876bd0ff4d4d75b87789412f501e33924343c78071f1ff893",
+         intel:        "2e985ed5335d6b8d62b30478d816d1e0c72ab3bc2ef5152a6d8105550e22886d",
+         arm64_linux:  "04c590bea699af34c6d6724bc905155d69b63b0be17cddaa6d1a0095777b54d1",
+         x86_64_linux: "d8d192cf0c7df529139eaa5f3b9fd7aae1828ac4e561d070124662bfb1e65de9"
 
   on_macos do
     auto_updates true
