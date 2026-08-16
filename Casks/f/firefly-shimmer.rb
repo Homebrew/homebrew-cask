9fbc693275370eac4f5ac67486e6c2abad6a6b9c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-16 12:22:40 -0400

    firefly-shimmer: add Linux AppImage support

--- Casks/f/firefly-shimmer.rb
+++ Casks/f/firefly-shimmer.rb
@@ -1,8 +1,32 @@
 cask "firefly-shimmer" do
-  version "2.2.2"
-  sha256 "123a710a8e42a717c29df2f96b722e2507b2ee5593aa699893aee4947dedbb06"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  url "https://github.com/iotaledger/firefly/releases/download/desktop-shimmer-#{version}/firefly-shimmer-desktop-#{version}.dmg",
+  version "2.2.2"
+
+  on_macos do
+    sha256 "123a710a8e42a717c29df2f96b722e2507b2ee5593aa699893aee4947dedbb06"
+
+    app "Firefly Shimmer.app"
+
+    uninstall quit: "org.iota.firefly"
+
+    zap trash: [
+      "~/Library/Application Support/Firefly",
+      "~/Library/Logs/Firefly",
+      "~/Library/Preferences/org.iota.firefly.helper.plist",
+      "~/Library/Preferences/org.iota.firefly.plist",
+      "~/Library/Saved Application State/org.iota.firefly.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "80fdd10d6d9be30bb992906b23c1e637fd61bb7426e1aa8af8207457dfb47779"
+
+    depends_on arch: :x86_64
+
+    app_image "firefly-shimmer-desktop-#{version}.AppImage", target: "Firefly Shimmer.AppImage"
+  end
+
+  url "https://github.com/iotaledger/firefly/releases/download/desktop-shimmer-#{version}/firefly-shimmer-desktop-#{version}.#{url_end}",
       verified: "github.com/iotaledger/firefly/"
   name "Firefly Shimmer"
   desc "Official wallet for IOTA"
@@ -14,19 +38,6 @@ cask "firefly-shimmer" do
   end
 
   auto_updates true
-  depends_on :macos
-
-  app "Firefly Shimmer.app"
-
-  uninstall quit: "org.iota.firefly"
-
-  zap trash: [
-    "~/Library/Application Support/Firefly",
-    "~/Library/Logs/Firefly",
-    "~/Library/Preferences/org.iota.firefly.helper.plist",
-    "~/Library/Preferences/org.iota.firefly.plist",
-    "~/Library/Saved Application State/org.iota.firefly.savedState",
-  ]
 
   caveats do
     requires_rosetta
