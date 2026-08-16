0cfb9283a1a300509c6306240bcc86e2472b3986
Eric Case <eric@vedana.net>
2026-08-16 15:08:57 -0700

    font-case-office-code-pro 1.004 (new cask)

---
+++ Casks/font/font-c/font-case-office-code-pro.rb
@@ -0,0 +1,27 @@
+cask "font-case-office-code-pro" do
+  version "1.004"
+  sha256 "9bca923d17f6c47a586d8e4567d46ccfa58fb8b8e2247b5ee2a19da1597c58f6"
+
+  url "https://github.com/case/font-office-code-pro-mirror/releases/download/#{version}/Office-Code-Pro-#{version}.tar.gz"
+  name "Office Code Pro"
+  homepage "https://github.com/case/font-office-code-pro-mirror"
+
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Bold.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-BoldItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Light.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-LightItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Medium.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-MediumItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Regular.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro D/OTF/OfficeCodeProD-RegularItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-Bold.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-BoldItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-Light.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-LightItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-Medium.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-MediumItalic.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-Regular.otf"
+  font "Office-Code-Pro-#{version}/Fonts/Office Code Pro/OTF/OfficeCodePro-RegularItalic.otf"
+
+  # No zap stanza required
+end
