61bcf46b05b349709af1363918bc9e58ade9c216
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-16 13:04:30 -0400

    feedflow: add Linux AppImage support

--- Casks/f/feedflow.rb
+++ Casks/f/feedflow.rb
@@ -1,8 +1,30 @@
 cask "feedflow" do
-  version "1.16.1,all"
-  sha256 "0d488e15404f5a3b747e562503f766006dabae392915b2d3184a651cc448cadc"
+  url_end = on_system_conditional macos: ".dmg", linux: "-x86_64.AppImage"
 
-  url "https://github.com/prof18/feed-flow/releases/download/#{version.csv.first}-#{version.csv.second}/FeedFlow-#{version.csv.first}.dmg",
+  version "1.16.1,all"
+
+  on_macos do
+    sha256 "0d488e15404f5a3b747e562503f766006dabae392915b2d3184a651cc448cadc"
+
+    depends_on macos: :monterey
+    depends_on arch: :arm64
+
+    app "FeedFlow.app"
+
+    zap trash: [
+      "~/Library/Application Support/FeedFlow",
+      "~/Library/Saved Application State/com.prof18.feedflow.savedState",
+    ]
+  end
+  on_linux do
+    sha256 "3226391b76ce1f30f88b3da75b2a223a8c63e66c47d1a103dad63b66c11c4f48"
+
+    depends_on arch: :x86_64
+
+    app_image "FeedFlow-#{version.csv.first}-x86_64.AppImage", target: "FeedFlow.AppImage"
+  end
+
+  url "https://github.com/prof18/feed-flow/releases/download/#{version.csv.first}-#{version.csv.second}/FeedFlow-#{version.csv.first}#{url_end}",
       verified: "github.com/prof18/feed-flow/"
   name "FeedFlow"
   desc "RSS reader"
@@ -20,14 +42,4 @@ cask "feedflow" do
       end
     end
   end
-
-  depends_on macos: :monterey
-  depends_on arch: :arm64
-
-  app "FeedFlow.app"
-
-  zap trash: [
-    "~/Library/Application Support/FeedFlow",
-    "~/Library/Saved Application State/com.prof18.feedflow.savedState",
-  ]
 end
