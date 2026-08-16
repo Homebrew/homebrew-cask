a245657b6028a452b1489a77f4d132d0a47faf3b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-16 13:04:34 -0400

    fspy: add Linux AppImage support

--- Casks/f/fspy.rb
+++ Casks/f/fspy.rb
@@ -1,18 +1,35 @@
 cask "fspy" do
-  version "1.0.3"
-  sha256 "3aca686ea9d976580936279fbb3637698a3ba832d3cdd74dbbf79129b4dd2f56"
+  os macos: "fSpy", linux: "fspy"
+  url_end = on_system_conditional macos: ".dmg", linux: "-x86_64.AppImage"
 
-  url "https://github.com/stuffmatic/fSpy/releases/download/v#{version}/fSpy-#{version}.dmg",
+  version "1.0.3"
+
+  on_macos do
+    sha256 "3aca686ea9d976580936279fbb3637698a3ba832d3cdd74dbbf79129b4dd2f56"
+
+    app "fSpy.app"
+
+    zap trash: [
+      "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.stuffmatic.fspy.sfl*",
+      "~/Library/Application Support/fspy",
+      "~/Library/Logs/fSpy",
+      "~/Library/Preferences/com.stuffmatic.fspy.helper.plist",
+      "~/Library/Preferences/com.stuffmatic.fspy.plist",
+    ]
+  end
+  on_linux do
+    sha256 "2feaac484c6e124cd99239d5fc9eabad9dc15ea59077e21fb0de9ade4bd01770"
+
+    depends_on arch: :x86_64
+
+    app_image "fspy-#{version}-x86_64.AppImage", target: "fSpy.AppImage"
+  end
+
+  url "https://github.com/stuffmatic/fSpy/releases/download/v#{version}/#{os}-#{version}#{url_end}",
       verified: "github.com/stuffmatic/fSpy/"
   name "fSpy"
   desc "Still image camera matching"
   homepage "https://fspy.io/"
-
-  depends_on :macos
-
-  app "fSpy.app"
-
-  zap trash: "~/Library/Application Support/fspy"
 
   caveats do
     requires_rosetta
