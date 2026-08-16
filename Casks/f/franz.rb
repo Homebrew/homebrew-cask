f2c878f83b623f8f78ee166ec32b703f807b2c23
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-16 13:04:32 -0400

    franz: add Linux AppImage support

--- Casks/f/franz.rb
+++ Casks/f/franz.rb
@@ -1,35 +1,44 @@
 cask "franz" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "6.7.0"
-  sha256 arm:   "5cf8e00465b0c823acfa72d22e6e4246eba70fdcd5750415b4537d39b767b593",
-         intel: "b7ad57e0e10e14637597f1216750e10d6612113be89eb647f098d41368d7fe78"
+  sha256 arm:          "5cf8e00465b0c823acfa72d22e6e4246eba70fdcd5750415b4537d39b767b593",
+         intel:        "b7ad57e0e10e14637597f1216750e10d6612113be89eb647f098d41368d7fe78",
+         arm64_linux:  "38da339208df5c8e045573bc37325c52a60e05b16c4044a549926f6807adcbae",
+         x86_64_linux: "609c980d926d24e5130f1901cd3a780e5de7b3e3ea279fe58de719a891b4dbfa"
 
-  url "https://github.com/meetfranz/franz-6/releases/download/v#{version}/Franz-#{arch}.dmg",
+  on_macos do
+    depends_on macos: :monterey
+
+    app "Franz.app"
+
+    uninstall signal: ["QUIT", "com.meetfranz.franz"],
+              delete: "/Library/Logs/DiagnosticReports/Franz Helper_.*wakeups_resource.diag"
+
+    zap trash: [
+      "~/Library/Application Support/Caches/franz-updater",
+      "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.meetfranz.franz.sfl*",
+      "~/Library/Application Support/Franz",
+      "~/Library/Caches/com.meetfranz.franz",
+      "~/Library/Caches/com.meetfranz.franz.ShipIt",
+      "~/Library/Logs/Franz",
+      "~/Library/Preferences/ByHost/com.meetfranz.franz.ShipIt.*.plist",
+      "~/Library/Preferences/com.electron.franz.helper.plist",
+      "~/Library/Preferences/com.electron.franz.plist",
+      "~/Library/Preferences/com.meetfranz.franz.plist",
+      "~/Library/Saved Application State/com.electron.franz.savedState",
+    ]
+  end
+  on_linux do
+    app_image "Franz-#{arch}.AppImage", target: "Franz.AppImage"
+  end
+
+  url "https://github.com/meetfranz/franz-6/releases/download/v#{version}/Franz-#{arch}.#{url_end}",
       verified: "github.com/meetfranz/franz-6/"
   name "Franz"
   desc "Messaging app for WhatsApp, Facebook Messenger, Slack, Telegram and more"
   homepage "https://meetfranz.com/"
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "Franz.app"
-
-  uninstall signal: ["QUIT", "com.meetfranz.franz"],
-            delete: "/Library/Logs/DiagnosticReports/Franz Helper_.*wakeups_resource.diag"
-
-  zap trash: [
-    "~/Library/Application Support/Caches/franz-updater",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.meetfranz.franz.sfl*",
-    "~/Library/Application Support/Franz",
-    "~/Library/Caches/com.meetfranz.franz",
-    "~/Library/Caches/com.meetfranz.franz.ShipIt",
-    "~/Library/Logs/Franz",
-    "~/Library/Preferences/ByHost/com.meetfranz.franz.ShipIt.*.plist",
-    "~/Library/Preferences/com.electron.franz.helper.plist",
-    "~/Library/Preferences/com.electron.franz.plist",
-    "~/Library/Preferences/com.meetfranz.franz.plist",
-    "~/Library/Saved Application State/com.electron.franz.savedState",
-  ]
 end
