774c7c5f39199f2538329bedf357afe7eefe8111
2cmm7b8ck2-collab <2cmm7b8ck2@privaterelay.appleid.com>
2026-08-17 22:56:34 +0100

    easydmg 2.4.0

--- Casks/e/easydmg.rb
+++ Casks/e/easydmg.rb
@@ -1,6 +1,6 @@
 cask "easydmg" do
   version "2.4.0"
-  sha256 "b8d8247ac9bae063d224f26d88d5fb4b5498a197f0ca55a4d0470cb6eb4bf1"
+  sha256 "b8d8247ac9bae0632df2246d88bd5fba4b5498a197f0ca55a4d0470cb6eb4bf1"
 
   url "https://github.com/jeff-schumann/EasyDMG/releases/download/v#{version}/EasyDMG.dmg"
   name "EasyDMG"
