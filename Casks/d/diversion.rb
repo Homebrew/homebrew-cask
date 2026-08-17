50fe1568afc00e642d21031addca96eb7ab61046
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 21:49:33 +0000

    diversion 1.0.1154

--- Casks/d/diversion.rb
+++ Casks/d/diversion.rb
@@ -1,9 +1,9 @@
 cask "diversion" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.1146"
-  sha256 arm:   "1b54771fcf9631c301d1f1cdc496d958d38fcb1108a579cd8108e357bcea9de9",
-         intel: "bb28ddf7c57b41f980fa5658651cf731419a6ed7ad87646be4520b304298885e"
+  version "1.0.1154"
+  sha256 arm:   "a063ec2012fb4e50ec923892c47e710c1435fd6572cec6011ae2c086f777aaa0",
+         intel: "d346ef71f22c0b44f94a9b9b10b2a20cdc27df8751cf7c91184c8e50148a0ac4"
 
   url "https://get.diversion.dev/update/dv/v#{version}/darwin-#{arch}.gz"
   name "Diversion CLI"
