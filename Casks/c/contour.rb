7aed80c1c65397dd99f77cb77c158dbd70598ac1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 19:00:07 +0000

    contour 0.7.0.8982 (arm only)

--- Casks/c/contour.rb
+++ Casks/c/contour.rb
@@ -1,9 +1,15 @@
 cask "contour" do
   arch arm: "arm", intel: "x86"
 
-  version "0.6.1.7494"
-  sha256 arm:   "71c91a97a1adb1969750a4842295ab4fd9d91f25cc84161fa89670472261793b",
+  sha256 arm:   "fa71252eff85ef5b223861d3aa9918440a684fe3e71418698eacad538b40a383",
          intel: "fd560a5e58f55ac20cd0d440b136365428126ba6fac8edde69b4d36d98467c7e"
+
+  on_arm do
+    version "0.7.0.8982"
+  end
+  on_intel do
+    version "0.6.1.7494"
+  end
 
   url "https://github.com/contour-terminal/contour/releases/download/v#{version}/contour-#{version}-macOS-#{arch}.dmg"
   name "Contour"
