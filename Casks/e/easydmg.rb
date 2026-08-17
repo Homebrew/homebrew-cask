68c9cd15d2b022e2578a7f5182381ea7c85b772d
2cmm7b8ck2-collab <2cmm7b8ck2@privaterelay.appleid.com>
2026-08-17 20:22:36 +0100

    Create easydmg.rb

---
+++ Casks/e/easydmg.rb
@@ -0,0 +1,20 @@
+cask "easydmg" do
+  version "2.4.0"
+  sha256 "b8d8247ac9bae0632df2246d88bd5fba4b5498a197f0ca55a4d0470cb6eb4bf1"
+
+  url "https://github.com/jeff-schumann/EasyDMG/releases/download/v#{version}/EasyDMG.dmg",
+      verified: "github.com/jeff-schumann/EasyDMG/"
+  name "EasyDMG"
+  desc "Automate DMG installation from disk images"
+  homepage "https://easydmg.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: :ventura
+
+  app "EasyDMG.app"
+end
