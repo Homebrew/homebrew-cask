715c63aa5575c27b4b07a5125a4456d21e9fc8d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 14:14:39 +0000

    vibeproxy 1.8.273

--- Casks/v/vibeproxy.rb
+++ Casks/v/vibeproxy.rb
@@ -1,9 +1,9 @@
 cask "vibeproxy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.8.272"
-  sha256 arm:   "4eef56969b3e0a120c1841f320ddc7ae9810a2a5a4b40709abbe6de9c893bacc",
-         intel: "7d27262ad3e4d793d0a3da448f4a2a1963552ed2d5d91e12ef6e25cfb5876d6b"
+  version "1.8.273"
+  sha256 arm:   "2dd5ca441e163e1a55652a59d00df2109ee898f5d87d65ee56f0053cafbb08b6",
+         intel: "d17478319a213659c522fbcf533a60c3b20a374c64a2c7f854810e685a8a765a"
 
   url "https://github.com/automazeio/vibeproxy/releases/download/v#{version}/VibeProxy-#{arch}.dmg"
   name "VibeProxy"
