7e820efcbe433310568f23dc214a49975c803804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 19:45:09 +0000

    raspberry-pi-imager 2.0.11.1

--- Casks/r/raspberry-pi-imager.rb
+++ Casks/r/raspberry-pi-imager.rb
@@ -1,6 +1,6 @@
 cask "raspberry-pi-imager" do
-  version "2.0.11"
-  sha256 "7af0c2c3038c69d1bbc565daf4bfb8830bbf8413a1634ac31b89f90700a7add7"
+  version "2.0.11.1"
+  sha256 "2b4c5324c5ff04aa3bfb216795ae9e01cb54400752727353e13fb21e66c528a9"
 
   url "https://github.com/raspberrypi/rpi-imager/releases/download/v#{version}/rpi-imager-v#{version}.dmg",
       verified: "github.com/raspberrypi/rpi-imager/"
