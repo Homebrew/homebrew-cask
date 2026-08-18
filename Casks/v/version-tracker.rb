bc424a0f9152e0ee2807d5fd87761eba53428f95
Version Tracker <48249776+FlorencioJP@users.noreply.github.com>
2026-08-18 15:14:21 +0100

    version-tracker 1.2.3 (new cask)

---
+++ Casks/v/version-tracker.rb
@@ -0,0 +1,35 @@
+cask "version-tracker" do
+  version "1.2.3"
+  sha256 "6ebc3eec4d7a122be724030b9cea8a6ca4e3243cd3088d01afc318f2220754b0"
+
+  url "https://version-tracker.app/releases/Version-Tracker-#{version}.zip"
+  name "Version Tracker"
+  desc "Tracks and installs updates for apps, packages and system software"
+  homepage "https://version-tracker.app/"
+
+  livecheck do
+    url "https://version-tracker.app/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: :sequoia
+
+  app "Version Tracker.app"
+
+  uninstall launchctl: [
+              "com.jpflorencio.VersionTracker.agent",
+              "com.jpflorencio.VersionTracker.Helper",
+            ],
+            quit:      "com.jpflorencio.VersionTracker"
+
+  zap trash: [
+    "~/Library/Application Support/Version Tracker",
+    "~/Library/Application Support/VersionTracker",
+    "~/Library/Caches/com.jpflorencio.VersionTracker",
+    "~/Library/HTTPStorages/com.jpflorencio.VersionTracker",
+    "~/Library/LaunchAgents/com.jpflorencio.VersionTracker.agent.plist",
+    "~/Library/Preferences/com.jpflorencio.VersionTracker.plist",
+    "~/Library/Preferences/group.com.jpflorencio.VersionTracker.plist",
+  ]
+end
