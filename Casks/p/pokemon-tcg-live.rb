b29e8dd68a2ad0c140fe0e1a068538187b19da0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 16:31:44 +0000

    pokemon-tcg-live 1.41.0

--- Casks/p/pokemon-tcg-live.rb
+++ Casks/p/pokemon-tcg-live.rb
@@ -1,5 +1,5 @@
 cask "pokemon-tcg-live" do
-  version "1.40.0,1093238"
+  version "1.41.0,1167182"
   sha256 :no_check
 
   url "https://installer.studio-prod.pokemon.com/installer/PokemonTCGLiveInstaller_Mac.dmg"
