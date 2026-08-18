208c424de8e0e9a586aaa4b17f03b97080502a97
jdferron <jdferron@users.noreply.github.com>
2026-08-18 17:35:08 -0600

    Do Not Remove App Settings On Upgrade
    Remove /etc/mullvad-vpn from zap trash section.  Including this folder will remove all app section when the app is upgraded.

--- Casks/m/mullvad-vpn@beta.rb
+++ Casks/m/mullvad-vpn@beta.rb
@@ -40,7 +40,6 @@ cask "mullvad-vpn@beta" do
             ]
 
   zap trash: [
-    "/etc/mullvad-vpn",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/net.mullvad.vpn.sfl*",
     "~/Library/Application Support/Mullvad VPN",
     "~/Library/Logs/Mullvad VPN",
