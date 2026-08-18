b4847730526967c87d0e982e4d17c9aaaaf0e946
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 14:10:23 +0000

    strawberry 0.1.33

--- Casks/s/strawberry.rb
+++ Casks/s/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "0.1.32"
-  sha256 "ffdf40c200624c92a4476ac8b513d694ffd6f6d0c46cb67b67add211f4e58a7a"
+  version "0.1.33"
+  sha256 "5a6204748486a80230d9396614ee2f7d25db7b8b8e560868a3dd325cf2b9432e"
 
   url "https://strawberrybucket.com/strawberry-#{version}.dmg",
       verified: "strawberrybucket.com/"
