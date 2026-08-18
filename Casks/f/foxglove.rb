e983be5c8d9ab7bdd020327326966b87562b266c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 13:28:19 +0000

    foxglove 3.0.0

--- Casks/f/foxglove.rb
+++ Casks/f/foxglove.rb
@@ -1,9 +1,9 @@
 cask "foxglove" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.59.0"
-  sha256 arm:   "cce2d09a56d412d866d8c5ce5891782439b60fd127055ed0e04f6ba3e6daede7",
-         intel: "70419200ca68a1086ca03e3bdcd4d040ad98dba111e3c4ba5aee7f80f7a4d65c"
+  version "3.0.0"
+  sha256 arm:   "6e976d7925d4ee6781e0617aef65750087c68343266b698f6d16017b9496cbe4",
+         intel: "24fbd77d032fd391f7a3e7015ad787c75fa8941dd6f4c63e0a8424a7a2bb058e"
 
   url "https://get.foxglove.dev/desktop/latest/foxglove-#{version}-mac-#{arch}.dmg"
   name "Foxglove"
