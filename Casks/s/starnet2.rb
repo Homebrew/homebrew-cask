07b12356fdf1e7529ca3583b6c185032354d0592
AhmedYoussef98 <ahmed.m.youssef98@gmail.com>
2026-08-18 11:55:33 +0300

    starnet2: add Intel support

--- Casks/s/starnet2.rb
+++ Casks/s/starnet2.rb
@@ -1,8 +1,21 @@
 cask "starnet2" do
   version "2.5.4-0214"
-  sha256 "95d318794dac837fcfc21cb87bd7fe55155da515fdaadac4bc680d5f95caaad5"
 
-  url "https://download.starnetastro.com/StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
+  on_arm do
+    sha256 "95d318794dac837fcfc21cb87bd7fe55155da515fdaadac4bc680d5f95caaad5"
+
+    url "https://download.starnetastro.com/StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
+
+    pkg "StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
+  end
+  on_intel do
+    sha256 "81320096875b6814fa565fe42f7c0f99fa288414fe86a1fb710fb768d0d577e4"
+
+    url "https://download.starnetastro.com/StarNet2_macos-x64_#{version}_ORT_x64.pkg"
+
+    pkg "StarNet2_macos-x64_#{version}_ORT_x64.pkg"
+  end
+
   name "starnet2"
   desc "Removes stars from astrophotography images using ML models"
   homepage "https://starnetastro.com/"
@@ -13,9 +26,6 @@ cask "starnet2" do
   end
 
   depends_on :macos
-  depends_on arch: :arm64
-
-  pkg "StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
 
   uninstall pkgutil: "com.starnetastro.starnet2.cli"
 
