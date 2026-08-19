3c72707eb748042a9dacdab80448a2714dd8d19d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 15:55:34 +0000

    baidunetdisk 8.7.9
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/b/baidunetdisk.rb
+++ Casks/b/baidunetdisk.rb
@@ -1,9 +1,9 @@
 cask "baidunetdisk" do
   arch arm: "_arm64", intel: "_x64"
 
-  version "8.7.3"
-  sha256 arm:   "567e1c707f52cb56978766a5573e69365679e703fb30a3baae75413d771f8261",
-         intel: "a24b2cea66040e49627b1bbcbfa87a1aba69233f624a27e577fd16005a41084f"
+  version "8.7.9"
+  sha256 arm:   "588381d189f68caf645a3e033e0e8a3b873dc998f6250707877db415bc762432",
+         intel: "0a2fd714ee093b8adc326576f4663d5cd075c711598c286db1c28fd8f987eefc"
 
   url "https://pkg-ant.baidu.com/issue/netdisk/MACguanjia/#{version}/BaiduNetdisk_mac_#{version}#{arch}.dmg"
   name "Baidu NetDisk"
