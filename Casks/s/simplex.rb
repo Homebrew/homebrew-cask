59e4cfbcbfba9b3f6de97cf9dc30e7ab6cfa1624
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 13:53:41 +0000

    simplex 7.0.1

--- Casks/s/simplex.rb
+++ Casks/s/simplex.rb
@@ -1,9 +1,9 @@
 cask "simplex" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "7.0.0"
-  sha256 arm:   "bc7f89221589ce4ed7fecbbcf4c780e8ad213846c22c13284adeb6fc503275d9",
-         intel: "ef13cc53ff795d3b93ebdb5ca7c57d7ab699b9d57491a8ee3620e253c8bcb60d"
+  version "7.0.1"
+  sha256 arm:   "8c07247cb88f5faee3e64181810548614e96a85d4e1d5749d24b90c91186cd06",
+         intel: "c439526086b855fc5c86d981350f92461ec51788ecb96fdbbe03fdf601e53816"
 
   url "https://github.com/simplex-chat/simplex-chat/releases/download/v#{version}/simplex-desktop-macos-#{arch}.dmg",
       verified: "github.com/simplex-chat/simplex-chat/"
