5676fe0ebf5bc05399aa50b363f2766f0143ac16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 16:10:31 +0000

    ferdium 7.2.0
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/f/ferdium.rb
+++ Casks/f/ferdium.rb
@@ -3,13 +3,13 @@ cask "ferdium" do
   os macos: "mac", linux: "linux-Portable"
   url_end = on_system_conditional macos: "#{arch}.dmg", linux: "x86_64.AppImage"
 
-  version "7.1.2"
+  version "7.2.0"
 
   on_macos do
-    sha256 arm:   "27cb2b3e1b193b2d337db5042d648561ea268a3e25e5973df91d2ae8f08c3f4b",
-           intel: "8eaf2971b1b8a3afaa72aa9c830eace0b55690e266061f373cbe351c8b3116ba"
+    sha256 arm:   "6ebe427689fcfd2770c28c1095b61d5d5b359fe2357d935d22f1cabd3564284c",
+           intel: "f68cce24ce0b1f9c361c25412c8fdf9d0ab1fbfcfe0c0559ef03ca3f50df31c1"
 
-    depends_on macos: :big_sur
+    depends_on macos: :monterey
 
     app "Ferdium.app"
 
@@ -33,7 +33,7 @@ cask "ferdium" do
     ]
   end
   on_linux do
-    sha256 "70271f0cffbf6227fba0958a8189b7b138959ab121537fec43c09b8d0e0d6b29"
+    sha256 "0de75aab457007ec29dd89d8996124ab9b8c3d366e8da8751767bc3e19c8cc50"
 
     depends_on arch: :x86_64
 
