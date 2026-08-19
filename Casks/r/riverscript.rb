309f10bad7fdb4f7b052a1a08ee807de87aff1bf
lexvalo <214685579+lexvalo@users.noreply.github.com>
2026-08-19 22:08:46 +0300

    Fix stanza order: livecheck before depends_on

--- Casks/r/riverscript.rb
+++ Casks/r/riverscript.rb
@@ -7,14 +7,14 @@ cask "riverscript" do
   desc "Capture and transcribe everything you hear on your computer"
   homepage "https://riverscript.com/"
 
-  depends_on macos: :ventura
-
   livecheck do
     url "https://downloads.riverscript.com/latest.json"
     strategy :json do |json|
       json["version"]
     end
   end
+
+  depends_on macos: :ventura
 
   app "RiverScript client.app"
 end
