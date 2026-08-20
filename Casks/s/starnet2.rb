704e3a2e45f1de58cbc25d459ca0c410fe50115a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-08-19 22:55:55 -0400

    starnet2: remove on_system blocks

--- Casks/s/starnet2.rb
+++ Casks/s/starnet2.rb
@@ -1,31 +1,24 @@
 cask "starnet2" do
+  arch arm: "arm64", intel: "x64"
+  runtime = on_arch_conditional arm: "COREML", intel: "ORT"
+
   version "2.5.4-0214"
+  sha256 arm:   "95d318794dac837fcfc21cb87bd7fe55155da515fdaadac4bc680d5f95caaad5",
+         intel: "81320096875b6814fa565fe42f7c0f99fa288414fe86a1fb710fb768d0d577e4"
 
-  on_arm do
-    sha256 "95d318794dac837fcfc21cb87bd7fe55155da515fdaadac4bc680d5f95caaad5"
-
-    url "https://download.starnetastro.com/StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
-
-    pkg "StarNet2_macos-arm64_#{version}_COREML_arm64.pkg"
-  end
-  on_intel do
-    sha256 "81320096875b6814fa565fe42f7c0f99fa288414fe86a1fb710fb768d0d577e4"
-
-    url "https://download.starnetastro.com/StarNet2_macos-x64_#{version}_ORT_x64.pkg"
-
-    pkg "StarNet2_macos-x64_#{version}_ORT_x64.pkg"
-  end
-
+  url "https://download.starnetastro.com/StarNet2_macos-#{arch}_#{version}_#{runtime}_#{arch}.pkg"
   name "starnet2"
   desc "Removes stars from astrophotography images using ML models"
   homepage "https://starnetastro.com/"
 
   livecheck do
     url "https://starnetastro.com/cli-tools/starnet/"
-    regex(/StarNet2_macos-arm64_(\d+(?:\.\d+)*-\d+)_COREML_arm64\.pkg/i)
+    regex(/StarNet2[._-]macos-#{arch}[._-]v?(\d+(?:\.\d+)*-\d+)[._-]#{runtime}/i)
   end
 
   depends_on :macos
+
+  pkg "StarNet2_macos-#{arch}_#{version}_#{runtime}_#{arch}.pkg"
 
   uninstall pkgutil: "com.starnetastro.starnet2.cli"
 
