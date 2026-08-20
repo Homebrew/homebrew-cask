716cd4843bd382ad0981bc671cf4bca964d35325
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 13:26:07 +0000

    mujoco 3.12.0

--- Casks/m/mujoco.rb
+++ Casks/m/mujoco.rb
@@ -1,6 +1,6 @@
 cask "mujoco" do
-  version "3.11.0"
-  sha256 "54933aa1a22a212134eaa663e83f4e81d7d2f14d0a778d0ba4fa14f66633b4f1"
+  version "3.12.0"
+  sha256 "8410882d724c3637b935dc0482b0de90efed44b17bbcfcb4a165ee46285a4865"
 
   url "https://github.com/google-deepmind/mujoco/releases/download/#{version}/mujoco-#{version}-macos-universal2.dmg",
       verified: "github.com/google-deepmind/mujoco/"
