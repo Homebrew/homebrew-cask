c6e377584016bcfae0543cb9232e84ec6771deaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 07:44:55 +0000

    utc-menu-clock 1.5

--- Casks/u/utc-menu-clock.rb
+++ Casks/u/utc-menu-clock.rb
@@ -1,6 +1,6 @@
 cask "utc-menu-clock" do
-  version "1.4"
-  sha256 "bb6104168763b1878e14a2c6583f0cde03549e437c870b096b5147eb20d7d7b1"
+  version "1.5"
+  sha256 "3caf9c44e65fe93da7083b58bb792974c44d9c6d12b934d19370b0003305bf87"
 
   url "https://github.com/netik/UTCMenuClock/raw/master/downloads/UTCMenuClock_v#{version}_universal.zip"
   name "UTCMenuClock"
