34712f82bd5afc3b9300a992324ee7d9b59350d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 16:52:59 +0000

    open-design 0.20.1

--- Casks/o/open-design.rb
+++ Casks/o/open-design.rb
@@ -1,9 +1,9 @@
 cask "open-design" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.20.0"
-  sha256 arm:   "85d30826ba46729f45bf059c76e710e760e2b6bfc2c57218e311027cbb0cbcd5",
-         intel: "b7217e661d14bc2c4d0d7bb7bb996a5bfcfa23ee3f09671385f56baa68263de3"
+  version "0.20.1"
+  sha256 arm:   "1e2bd15f74d9feccdbf40131f874af374e39fd0e5e36c749a4c0ae4835000937",
+         intel: "f5fcd56d7d5050e91fbb9ff0d81e7cd8d2e3bcf823c83ca799023cb8ed8b1fd6"
 
   url "https://github.com/nexu-io/open-design/releases/download/open-design-v#{version}/open-design-#{version}-mac-#{arch}.dmg",
       verified: "github.com/nexu-io/open-design/"
