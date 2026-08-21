14e87db22bbe1e85fee02723a942f8f911a0432f
John Benninghoff <jbenninghoff@mac.com>
2026-08-21 08:27:04 -0500

    google-chrome 151.0.7922.174

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "151.0.7922.170"
+  version "151.0.7922.174"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
