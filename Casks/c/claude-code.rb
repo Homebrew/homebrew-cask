062f9d9002e68c85049ec35d8bc03dd6796ef2d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 21:45:01 +0000

    claude-code 2.1.231

--- Casks/c/claude-code.rb
+++ Casks/c/claude-code.rb
@@ -2,11 +2,11 @@ cask "claude-code" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.1.228"
-  sha256 arm:          "43484b1352cef03a08346f36ef0437755b1aad646ab9313ce187857b794b7247",
-         intel:        "7852f1ae0efb64d46d77a57d8852daddc4a6ffb58aeda6267bd3f3428adc09b3",
-         arm64_linux:  "2664006219497bf7021ac43156519cd42eda64ceb2a66f434ecab83e7831f942",
-         x86_64_linux: "d535985e6941a3eb00179ccd7f52ceb0c6623a0305a518ebc4e6514f84a94c99"
+  version "2.1.231"
+  sha256 arm:          "ba790279cab6ef77b713864d4bf5f764fcea87d3a3eb7591a41f741e45212b5c",
+         intel:        "7c7c6179f55c985409af4c31603d19b9b64af4759d016f86b99bfbdb29042a90",
+         arm64_linux:  "4ee7c484b11dece6521aa2173a19ea913428c1c78599186d62559d2d2aef4e32",
+         x86_64_linux: "47a01daebf794f6c86c13d1875ad6e5be0627029ad8600731161f24018ecde5b"
 
   url "https://downloads.claude.ai/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "downloads.claude.ai/claude-code-releases/"
