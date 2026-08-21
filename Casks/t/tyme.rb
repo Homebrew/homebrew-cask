d90e76b175a2fa7192136402c0fafad39eff0f52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 10:57:32 +0000

    tyme 2026.8

--- Casks/t/tyme.rb
+++ Casks/t/tyme.rb
@@ -1,5 +1,5 @@
 cask "tyme" do
-  version "2026.7"
+  version "2026.8"
   sha256 :no_check
 
   url "https://www.tyme-app.com/app/Tyme.dmg"
@@ -17,18 +17,20 @@ cask "tyme" do
 
   app "Tyme.app"
 
+  uninstall quit: ["com.tyme-app.Tyme3-MenubarHelper",
+                   "com.tyme-app.Tyme3-macOS"]
+
   zap trash: [
-    "~/Library/Application Scripts/com.tyme-app.Tyme3-macOS",
+    "~/Library/Application Scripts/com.tyme-app.Tyme3-macOS.Direct-Widget",
+    "~/Library/Application Scripts/com.tyme-app.Tyme3-MenubarHelper",
+    "~/Library/Application Scripts/HR22V4547K.com.tyme-app",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tyme-app.tyme3-menubarhelper.sfl*",
-    "~/Library/Application Support/com.tyme-app.Tyme3-macOS",
-    "~/Library/Caches/CloudKit/com.tyme-app.Tyme3-macOS",
     "~/Library/Caches/com.tyme-app.Tyme3-macOS",
-    "~/Library/Containers/com.tyme-app.Tyme3-macOS",
+    "~/Library/Containers/com.tyme-app.Tyme3-macOS*",
     "~/Library/Containers/com.tyme-app.Tyme3-MenubarHelper",
-    "~/Library/Group Containers/*.group.com.tyme-app.Tyme3",
+    "~/Library/Group Containers/HR22V4547K.com.tyme-app",
     "~/Library/HTTPStorages/com.tyme-app.Tyme3-macOS",
-    "~/Library/Preferences/3https:/api.tyme-app.com",
+    "~/Library/Logs/DiagnosticReports/Tyme-2026-08-21-110730.ips",
     "~/Library/Preferences/com.tyme-app.Tyme3-macOS.plist",
-    "~/Library/Saved Application State/com.tyme-app.Tyme3-macOS.savedState",
   ]
 end
