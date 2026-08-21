b2369177dbee5ee41c2c6ac7c8a0de05d48512c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 21:41:31 +0000

    bisq 1.10.6

--- Casks/b/bisq.rb
+++ Casks/b/bisq.rb
@@ -1,7 +1,7 @@
 cask "bisq" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.10.5"
+  version "1.10.6"
   sha256 arm:   "0c322e9aed88b6e45400156a73523512c20a6c532e5200d70fdef08399b6b1b8",
          intel: "f81d8653f7e386b526e3571b0f3f9b5eb94221c643f0b572241d51e3a3742f35"
 
