820909f81a9b5887f9a45082f2d745f83d595fa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 18:50:11 +0000

    brave-origin@nightly 1.96.6.0 (arm only)

--- Casks/b/brave-origin@nightly.rb
+++ Casks/b/brave-origin@nightly.rb
@@ -2,11 +2,11 @@ cask "brave-origin@nightly" do
   arch arm: "arm64", intel: "x64"
   folder_arch = on_arch_conditional arm: "-arm64"
 
-  sha256 arm:   "2ca418241a51f3de9cfafcfd72bc4be052b684275a7dca67adff70d60fb75e69",
+  sha256 arm:   "56f6fa8013b5765a425b918223acedaa27cbc0fa981f80264faedbc501cb06fd",
          intel: "f16ee1e9c1ae923657bf7231b078680e191a028915ceb730344426b3ae0f44d4"
 
   on_arm do
-    version "1.96.4.0"
+    version "1.96.6.0"
   end
   on_intel do
     version "1.96.4.0"
