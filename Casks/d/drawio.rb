6885454cba88f5e868f04106c7f2983267301e1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 18:55:28 +0000

    drawio 31.3.2

--- Casks/d/drawio.rb
+++ Casks/d/drawio.rb
@@ -3,11 +3,11 @@ cask "drawio" do
   os macos: "draw.io", linux: "drawio"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "31.3.1"
-  sha256 arm:          "f102bb381509fb3bce4e5bd692f1a329eb2b288dffc54d0f4a01df8f45ec52dc",
-         intel:        "15a7d979533f27f39a5fec5d4740c16fa6f233d1e55e0eb0c5a4a2f6a15649c8",
-         arm64_linux:  "5f404182a5f72454072a0e4c6c9de76273a8d9589e155d9daeca09721ab7f925",
-         x86_64_linux: "407f03edcec02916d02feb38e60d66c76ebe27de8915d646a365e6a22f69a9bb"
+  version "31.3.2"
+  sha256 arm:          "b6eda7e82bc09454274c03533bce30de170dfdee278ddf5e3c966d021fa16aee",
+         intel:        "13f12dddcf9960fb3d3783f2f6498ec5e7b93e7490926167b0590a6bd66d5cd2",
+         arm64_linux:  "6c18c4f83701ce8c9d6b7a07a39764c52acbcfa68d0c52e3be91e11b95fbafd7",
+         x86_64_linux: "ca06cbe33876d22e92fc397d12bc164501016d18d200093690be3b312feec791"
 
   on_macos do
     depends_on macos: :monterey
