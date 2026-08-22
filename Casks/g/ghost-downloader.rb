e6b73d305cac0e927c3db08aa2587f960ff27244
D-L13W <damian.liewjx@gmail.com>
2026-08-23 03:13:28 +0800

    edited ghost-downloader sha256 for version 4.3.4

--- Casks/g/ghost-downloader.rb
+++ Casks/g/ghost-downloader.rb
@@ -2,8 +2,8 @@ cask "ghost-downloader" do
   arch arm: "arm64", intel: "x86_64"
 
   version "4.3.4"
-  sha256 arm:   "ddaf8be985123cf0edb8a7d4e5e30843a670072ebbe93639f3ba04149739859f",
-         intel: "1eb8a3c1100200930120cc1a0b2ed5d6ab24619dd21192b0b49bfb06806bdb66"
+  sha256 arm:   "37b803167c1958293239fc13d6e11098e7de759cd6e8c178e79c37d4ad11d0a5",
+         intel: "cd309db552190a43936829a377065e6fdb0ab11c6efb7ff859b9d56f6edc87f0"
 
   url "https://github.com/XiaoYouChR/Ghost-Downloader-3/releases/download/v#{version}/Ghost-Downloader-v#{version}-macOS-#{arch}.dmg"
   name "Ghost Downloader"
