720a6346919b0944da197eec90fef1747f2f46f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 07:03:27 +0000

    font-sketchybar-app-font 2.0.82

--- Casks/font/font-s/font-sketchybar-app-font.rb
+++ Casks/font/font-s/font-sketchybar-app-font.rb
@@ -1,6 +1,6 @@
 cask "font-sketchybar-app-font" do
-  version "2.0.81"
-  sha256 "857d05bb41b5b7243f5c4451da54a31f24e5056d89c734571ada7e51d8f178d7"
+  version "2.0.82"
+  sha256 "b60e87735dc4330d8bba3bc4b377950ae85226d5d52aed6a528070251f0b26fd"
 
   url "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v#{version}/sketchybar-app-font.ttf"
   name "sketchybar-app-font"
