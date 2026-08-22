9f72c6a07ff39ab1a4db5279cb5abd5beebce803
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 13:03:00 +0000

    gearboy 3.8.13

--- Casks/g/gearboy.rb
+++ Casks/g/gearboy.rb
@@ -1,9 +1,9 @@
 cask "gearboy" do
   arch arm: "arm64", intel: "intel"
 
-  version "3.8.12"
-  sha256 arm:   "5d596c64e354427fe11e5c74d8900fbe843b8fecc00829d7797476a5188361be",
-         intel: "61b81cafb2a4cf5f9c58e6c54d49a2d6b748bf03b5d78a8a783a458dc395abc3"
+  version "3.8.13"
+  sha256 arm:   "aaf675b85cee385e4247dfefef91996b171793db785b2a5b6664c4d722aeebc1",
+         intel: "97a4902e4812357048d66cdebcbb1a3dc56ab974e5c2429cfc560ae064aeb2ec"
 
   url "https://github.com/drhelius/Gearboy/releases/download/#{version}/Gearboy-#{version}-desktop-macos-#{arch}.zip"
   name "Gearboy"
