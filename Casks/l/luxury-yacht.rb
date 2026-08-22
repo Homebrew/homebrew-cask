2eaa290bdded54b9c412ca08c2cc8f2da8aaa275
Bevan Kay <email@bevankay.me>
2026-08-22 17:11:34 +0800

    luxury-yacht: remove `verified`

--- Casks/l/luxury-yacht.rb
+++ Casks/l/luxury-yacht.rb
@@ -5,8 +5,7 @@ cask "luxury-yacht" do
   sha256 arm:   "d47228eec489c8b3b166b2f5ef202c357feb08de75261ea0c97b700d004ff89f",
          intel: "9e6f7652058a9846d7dad3f179f5c3592db14ef6dfb9257ae5b9e4e0bee0784f"
 
-  url "https://github.com/luxury-yacht/app/releases/download/v#{version}/luxury-yacht-v#{version}-macos-#{arch}.dmg",
-      verified: "github.com/luxury-yacht/app/"
+  url "https://github.com/luxury-yacht/app/releases/download/v#{version}/luxury-yacht-v#{version}-macos-#{arch}.dmg"
   name "Luxury Yacht"
   desc "Desktop app for managing Kubernetes clusters"
   homepage "https://luxury-yacht.app/"
