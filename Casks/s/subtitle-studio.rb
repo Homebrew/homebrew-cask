11721c51c79581d02c3728fc52938f3e5bc8395f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 07:34:59 +0000

    subtitle-studio 1.11.0

--- Casks/s/subtitle-studio.rb
+++ Casks/s/subtitle-studio.rb
@@ -1,6 +1,6 @@
 cask "subtitle-studio" do
-  version "1.10.12"
-  sha256 "d98ae90f50c10b52d32c5a4ad7b68d0bb338609725eaa01f63b81256e1df9391"
+  version "1.11.0"
+  sha256 "e818b7cc1e764a517b11339efd4b9ed2068dbec9bbf1fdf147994c03fbed73f0"
 
   url "https://assets.subtitlestudio.ai/releases/Subtitle%20Studio-arm64-#{version}.dmg"
   name "Subtitle Studio"
