f66921cba84bbf4ab4d6e310d7a1c450b65f24b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 07:33:30 +0000

    tev 2.14.0

--- Casks/t/tev.rb
+++ Casks/t/tev.rb
@@ -2,11 +2,11 @@ cask "tev" do
   arch arm: on_system_conditional(linux: "-arm"), intel: on_system_conditional(macos: "-intel")
   os macos: "dmg", linux: "appimage"
 
-  version "2.13.1"
-  sha256 arm:          "9ad099756f6ece21e3a2a3f99529ce19679119b27730a23d022235871f6eda8c",
-         intel:        "ee37fb1340319124a4639d2e63f868c1e95b40608fa38b5181be114fbf5e28c1",
-         arm64_linux:  "59af79f1ef83b2eb447997e5598e7bba1426c0bcce781cf4ac960f736f277803",
-         x86_64_linux: "2d2a833f60c073b027a83d58f78f79dd6f5ae772566d62680738a1d0fc90e962"
+  version "2.14.0"
+  sha256 arm:          "cef00c8a54bebcdeb1de5c03b7384f18da0bcaa2cb67c0ad5773c3c5da305671",
+         intel:        "0bff902e04f410febc0a8b6d90c22fe1a2be596652857dfedc4940c2565c5b5a",
+         arm64_linux:  "df50480fda7019c1d2178ae32060d9ea5bb5f57129406eb0f832eb69baa2ef83",
+         x86_64_linux: "a7d4147c2984b93c135ac2559422cb1f80310cf971b07056320e8fbbd63fdf82"
 
   on_macos do
     app "tev.app"
