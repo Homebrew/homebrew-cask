cab67422dad156ade7da40c0489256952298b07a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 01:54:29 +0000

    zen@twilight 1.22t,20260823110335

--- Casks/z/zen@twilight.rb
+++ Casks/z/zen@twilight.rb
@@ -4,7 +4,7 @@ cask "zen@twilight" do
   livecheck_os = on_system_conditional macos: "Darwin", linux: "Linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "1.22t,20260821111558"
+  version "1.22t,20260823110335"
   sha256 :no_check
 
   on_macos do
