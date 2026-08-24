6e19a1c05cdac5e12ee6b3a48694b84820b57ed5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 00:50:11 +0000

    codex 0.149.1

--- Casks/c/codex.rb
+++ Casks/c/codex.rb
@@ -2,11 +2,11 @@ cask "codex" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-musl"
 
-  version "0.149.0"
-  sha256 arm:          "6c7589a52fe90e3742e35662115a4c55c39715601df0d41345ba8ec8f4221d4e",
-         intel:        "ba332e647cc898e3b4e86a3bc6e8db414a124eb88d8480f4707bbc66b0432f9d",
-         arm64_linux:  "dac03e4db966e94f7083b9eb9f95d8b40da9d568fd2fdbb6885b0ea7d5b0d97f",
-         x86_64_linux: "1c08ba262820b78d49ea7a93f326b6b430b72e5fe46830e433edef12e5123244"
+  version "0.149.1"
+  sha256 arm:          "4cbb17468b5d86b4b182a28c016d62e9d273a241cec04885ccfae76e6983ae3f",
+         intel:        "4c50fb92bb238a4067009d4a99c13351325c8840da91edd0ce4e5b7a21d53bc3",
+         arm64_linux:  "57095f9f4ced36d8e173f67e26c5c142d5b3e1e1984bbcae35684209ed236a9b",
+         x86_64_linux: "1e8531ae5f6dea3c6e11e53e74cc5ac81bf1ba597f9b296fb112d6ea30fdaf5d"
 
   url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-#{arch}-#{os}.tar.gz"
   name "Codex"
