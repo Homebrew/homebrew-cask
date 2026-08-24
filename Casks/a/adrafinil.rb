2fc3a5e230b5d12fb24951d7343697706a319e7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 03:43:13 +0000

    adrafinil 1.7.0

--- Casks/a/adrafinil.rb
+++ Casks/a/adrafinil.rb
@@ -1,6 +1,6 @@
 cask "adrafinil" do
-  version "1.6.0"
-  sha256 "332016003d6caa431d17f272f5d1217a7b99572b7b471414195988704d293d4a"
+  version "1.7.0"
+  sha256 "71de8cbe83cc9b8fd863d32347ba255d840ab299f5fde7082629ea20225ea783"
 
   url "https://github.com/kageroumado/adrafinil/releases/download/v#{version}/Adrafinil-#{version}.dmg",
       verified: "github.com/kageroumado/adrafinil/"
