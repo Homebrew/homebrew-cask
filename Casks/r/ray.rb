a5ced793df30f914fab911601f34cb4b9aa34001
David Rogers <david.rogers@tylertech.com>
2026-08-24 10:22:37 -0700

    Update url, regex, depends_on

--- Casks/r/ray.rb
+++ Casks/r/ray.rb
@@ -5,19 +5,20 @@ cask "ray" do
   sha256 arm:   "8965569f043c36bd7e12decfbb12db1511962b4e7edf423d05599d65c5ccc25a",
          intel: "dc7a6fbfcfd6029180dfd0181c834124b714ebb249e0472a9ca0de4060dcbfe1"
 
-  url "https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-#{version}-latest-darwin-#{arch}.dmg",
-      verified: "ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/"
+  url "https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v#{version.major}/stable/ray-#{version}-latest-darwin-#{arch}.dmg",
+      verified: "ray-app.s3.eu-west-1.amazonaws.com/"
   name "Ray"
   desc "Debug with Ray to fix problems faster"
   homepage "https://myray.app/"
 
   livecheck do
     url "https://spatie.be/products/ray/v3/download/macos-#{arch}/latest"
+    regex(/ray[._-]v?(\d+(?:\.\d+)+).+#{arch}\.dmg/i)
     strategy :header_match
   end
 
   auto_updates true
-  depends_on :macos
+  depends_on macos: :monterey
 
   app "Ray.app"
 
