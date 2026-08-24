bea5f8a713796a11f6947b9a43b118a36935b72d
Sakutaro Shiina <companyemailetc@gmail.com>
2026-08-24 22:16:47 +0900

    keyboardcleantool: update livecheck

--- Casks/k/keyboardcleantool.rb
+++ Casks/k/keyboardcleantool.rb
@@ -1,15 +1,15 @@
 cask "keyboardcleantool" do
-  version "8.1,10"
-  sha256 :no_check
+  version "8.1"
+  sha256 "1c5f6b20021235031323c2d7398fdc3a5e47f27e9671e8a81714ac408912a8e0"
 
-  url "https://folivora.ai/releases/KeyboardCleanTool.zip"
+  url "https://folivora.ai/releases/KeyboardCleanTool-#{version}.zip"
   name "KeyboardCleanTool"
   desc "Blocks all Keyboard and TouchBar input"
   homepage "https://folivora.ai/keyboardcleantool"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://updates.folivora.ai/keyboardcleantool.xml"
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: :ventura
