4de37e8c091fbed6c511e84f824a76130b0640ef
Sam Ford <1584702+samford@users.noreply.github.com>
2026-08-24 18:36:53 -0400

    jazz2-resurrection 3.8.0
    `jazz2-resurrection` is autobumped but the workflow failed to update
    to version 3.8.0 because the homepage is unreachable, as upstream has
    switched to a different domain. This change is referenced in the
    release description on GitHub, so this updates the version and
    homepage accordingly.

--- Casks/j/jazz2-resurrection.rb
+++ Casks/j/jazz2-resurrection.rb
@@ -2,14 +2,14 @@ cask "jazz2-resurrection" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   arch arm: "ARM64", intel: "x64"
 
-  version "3.7.0"
-  sha256 "b737320e4802e9b7fbe125438a10e82c50496729da117cc9354dd506280d43d0"
+  version "3.8.0"
+  sha256 "5a051dab4260b1bd99d2a9285242a6b062c6b5f88dba773c98b4dd786a42bab7"
 
   url "https://github.com/deathkiller/jazz2/releases/download/#{version}/Jazz2_#{version}_MacOS.zip",
       verified: "github.com/deathkiller/jazz2/"
   name "Jazz² Resurrection"
   desc "Open-source re-implementation of Jazz Jackrabbit 2 game engine"
-  homepage "https://deat.tk/jazz2/"
+  homepage "https://de4th.dev/jazz2/"
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
