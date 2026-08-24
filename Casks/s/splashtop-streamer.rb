ab261492c340808bbba093c5e5a860b20962f1ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 04:50:36 +0000

    splashtop-streamer 3.8.6.0

--- Casks/s/splashtop-streamer.rb
+++ Casks/s/splashtop-streamer.rb
@@ -1,6 +1,6 @@
 cask "splashtop-streamer" do
-  version "3.8.4.2"
-  sha256 "93fca9b9f3c03bae0446b6d814a7ed0e8c0e244d71aa68c19efdc364e71b5076"
+  version "3.8.6.0"
+  sha256 "33765596a0e9f992ee22678c39e8d27888e8749d75046331e81e179a1d97e16d"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/mac/Splashtop_Streamer_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/"
