81b1a0836f6d53094c6cb87aca9149637543f8bf
Sakutaro Shiina <companyemailetc@gmail.com>
2026-08-25 15:24:18 +0900

    geph: 5.8.3

--- Casks/g/geph.rb
+++ Casks/g/geph.rb
@@ -1,20 +1,22 @@
 cask "geph" do
-  version "5.7.1"
-  sha256 "2acc6945b6780333abbd47f97ae84310d8f66e16d5cc94d039356c260340baf3"
+  version "5.8.3"
+  sha256 "d2ad08a476a617e6988cd21f84d9f7e1b6c56eaa84f8cc1b182894822de43acd"
 
-  url "https://dl.geph.io/geph-releases/macos-stable/#{version}/geph-macos.dmg"
+  url "https://dl.geph.io/geph-releases/macos-stable/#{version}/geph-macos.pkg"
   name "Geph"
   desc "Modular Internet censorship circumvention system"
   homepage "https://geph.io/en"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/geph[._-]macos\.dmg}i)
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/geph[._-]macos\.pkg}i)
   end
 
   depends_on :macos
 
-  app "Geph.app"
+  pkg "geph-macos.pkg"
+
+  uninstall launchctl: "io.geph.geph-electron"
 
   zap trash: [
     "~/Library/Application Support/gephgui#{version.major}",
