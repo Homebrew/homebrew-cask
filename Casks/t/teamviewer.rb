3ac7dc78f534bbbf5fc7818c7ee0a868e6a78d96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 10:47:57 +0000

    teamviewer 15.81.5

--- Casks/t/teamviewer.rb
+++ Casks/t/teamviewer.rb
@@ -42,8 +42,8 @@ cask "teamviewer" do
     pkg "TeamViewer.pkg"
   end
   on_ventura :or_newer do
-    version "15.80.4"
-    sha256 "22ffe2bb09d4067a6c2b5396cb86366b3f3babbec9e3178f26392bc7c527ff06"
+    version "15.81.5"
+    sha256 "17bd3fd5f5172451685787ff8292e88178ac132c0d58a0c8064bb32ea201bb92"
 
     livecheck do
       url "https://download.teamviewer.com/download/update/macupdates.xml?id=0&lang=en&version=#{version}&os=macos&osversion=13.7&type=1&channel=1"
