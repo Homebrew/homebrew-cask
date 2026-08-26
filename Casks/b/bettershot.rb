8068571970d49d3fc094e749125c9e61fc19b076
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-26 12:47:11 +0000

    bettershot 0.4.1

--- Casks/b/bettershot.rb
+++ Casks/b/bettershot.rb
@@ -1,9 +1,9 @@
 cask "bettershot" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.4.0"
-  sha256 arm:   "3b9273f56839282d76e230fd6751d38299b073c6ddea57773afc710a7943acb8",
-         intel: "d73912c32dab4329346ed0dca057be3ed641a85cb20c2c99e0617e8e0975a6aa"
+  version "0.4.1"
+  sha256 arm:   "faa495c2ee2983d811bb085aad61828985a900e1c93aca0d731d8507985a6f2b",
+         intel: "c8bd17e7712f767c5d717828af47ad999bc32bc20641e9f86417f3d557da2b44"
 
   url "https://github.com/KartikLabhshetwar/better-shot/releases/download/v#{version}/BetterShot-#{version}_#{arch}.dmg",
       verified: "github.com/KartikLabhshetwar/better-shot/"
