1b059cb932ed3f3e67716be388a8d8f2bb2a1bec
foam <282332576@qq.com>
2026-08-26 11:46:41 +0800

    itraffic 0.2.1

--- Casks/i/itraffic.rb
+++ Casks/i/itraffic.rb
@@ -1,13 +1,11 @@
 cask "itraffic" do
-  version "0.2.0"
-  sha256 "60bb8bb2ddcda91b080ce7f023fa41f5cbfd97f854d0d9bc72fa4a9f2b2465a9"
+  version "0.2.1"
+  sha256 "b2eaf0c69f3fc8c9eacfdfbdd642c9f520be94b899f9f8a0ab7369722e93bb87"
 
   url "https://github.com/foamzou/ITraffic-monitor-for-mac/releases/download/v#{version}/ITraffic-v#{version}.zip"
   name "itraffic"
   desc "Monitor for displaying process traffic on status bar"
   homepage "https://github.com/foamzou/ITraffic-monitor-for-mac"
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on :macos
 
