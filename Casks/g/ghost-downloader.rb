1ebcb9b8c267d3bbd97926dc5a087a49dda5496f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-26 10:21:25 +0000

    ghost-downloader 4.3.6

--- Casks/g/ghost-downloader.rb
+++ Casks/g/ghost-downloader.rb
@@ -1,9 +1,9 @@
 cask "ghost-downloader" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "4.3.5"
-  sha256 arm:   "f0ab5404ccd43fdaa1d8a2920eac262ae4d5716f79d47c2fa7260b53c0a46952",
-         intel: "cfdc284c6b0aaf6aef44586c641b10483874aaaf20055d77308bb5c3b4244e8b"
+  version "4.3.6"
+  sha256 arm:   "6c27f765c849dfb49c29dbff4f8762a6c93ba871634cbfeb89c9172ca4afe7b7",
+         intel: "b47132bfbb48c4a1fad43ca35af12d25e611509ff841037f81141b140b6d88c0"
 
   url "https://github.com/XiaoYouChR/Ghost-Downloader-3/releases/download/v#{version}/Ghost-Downloader-v#{version}-macOS-#{arch}.dmg"
   name "Ghost Downloader"
