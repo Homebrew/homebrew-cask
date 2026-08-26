ffcfbb6483b2d48477a139386b6ecd4de8f7f551
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-26 16:03:09 +0000

    aptakube 1.19.3

--- Casks/a/aptakube.rb
+++ Casks/a/aptakube.rb
@@ -1,6 +1,6 @@
 cask "aptakube" do
-  version "1.19.2"
-  sha256 "8bf6ce89f151644905a4c8b1aaee376186cbe44bddde9c4c33d43acf9a907cdf"
+  version "1.19.3"
+  sha256 "4a090b4387471236b9f040d31eceaa8bb086c3a993ded28f350c52d8b2831e7d"
 
   url "https://releases.aptakube.com/Aptakube_#{version}_universal.dmg"
   name "Aptakube"
