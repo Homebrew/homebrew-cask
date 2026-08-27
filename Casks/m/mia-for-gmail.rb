3792d02d4485aa1fc24f8d0b71b9ed76172629ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 17:19:33 +0000

    mia-for-gmail 2.8.2

--- Casks/m/mia-for-gmail.rb
+++ Casks/m/mia-for-gmail.rb
@@ -1,6 +1,6 @@
 cask "mia-for-gmail" do
-  version "2.8.1"
-  sha256 "c23e49232a47f877d582b10efd8f31cfc3832e52aba57316abc3cff11ab6d45a"
+  version "2.8.2"
+  sha256 "0270ca4ff60614806096c8889591d384da11c165b3a30559439ff6d5a0199470"
 
   url "https://www.sovapps.com/application/notifier-pro-for-gmail/mia.#{version}.zip",
       verified: "sovapps.com/application/notifier-pro-for-gmail/"
