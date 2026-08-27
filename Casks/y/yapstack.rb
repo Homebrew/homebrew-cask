60e6123bcecdacd69e28dceb3a0c7f43b4a8c3a0
Prajesh Dineshchandra Rana <prajesh.rana@prajesh.rana>
2026-08-27 16:41:33 +0530

    yapstack: fix homepage and remove deprecated verified

--- Casks/y/yapstack.rb
+++ Casks/y/yapstack.rb
@@ -2,11 +2,10 @@ cask "yapstack" do
   version "1.0.0"
   sha256 "2376842edf6d691cb04f194ea7183253d58e767ae9ae9a61bc02527db83dde77"
 
-  url "https://github.com/yapstackai/yapstack-releases/releases/download/v#{version}/yapstack_aarch64-dmg.dmg",
-      verified: "github.com/yapstackai/yapstack-releases/"
+  url "https://github.com/yapstackai/yapstack-releases/releases/download/v#{version}/yapstack_aarch64-dmg.dmg"
   name "YapStack"
   desc "Cloud speech-to-text desktop app powered by Cove"
-  homepage "https://yapstack.ai/"
+  homepage "https://github.com/yapstackai/yapstack-releases"
 
   livecheck do
     url :url
