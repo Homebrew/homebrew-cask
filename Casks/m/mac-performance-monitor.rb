ad0d0f2c6e5cfbdcacd0886c1cc11a1b93693f30
Neil Johnson <jonno@madcat.co.uk>
2026-08-27 11:49:33 +0100

    mac-performance-monitor 1.5.0.198 (new cask)

---
+++ Casks/m/mac-performance-monitor.rb
@@ -0,0 +1,32 @@
+cask "mac-performance-monitor" do
+  version "1.5.0.198"
+  sha256 "362db29f38a34a1970fcdb9b81b7f62dfd6fd1f3ac8c9cc2b32effb196d86a60"
+
+  url "https://github.com/Zesty0wl/mac-performance-monitor/releases/download/v#{version}/MacPerformanceMonitor.pkg"
+  name "Mac Performance Monitor"
+  desc "Menu bar performance monitor with recorded history and analytics"
+  homepage "https://macperformancemonitor.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: :sequoia
+
+  pkg "MacPerformanceMonitor.pkg"
+
+  uninstall launchctl: "uk.co.bzwrd.macperfmonitor.helper",
+            quit:      "uk.co.bzwrd.macperfmonitor",
+            pkgutil:   "uk.co.bzwrd.macperfmonitor"
+
+  zap trash: [
+    "~/Library/Application Support/MacPerformanceMonitor",
+    "~/Library/Caches/uk.co.bzwrd.macperfmonitor",
+    "~/Library/HTTPStorages/uk.co.bzwrd.macperfmonitor",
+    "~/Library/Preferences/uk.co.bzwrd.macperfmonitor.plist",
+    "~/Library/Saved Application State/uk.co.bzwrd.macperfmonitor.savedState",
+  ]
+end
