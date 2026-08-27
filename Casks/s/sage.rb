a178011fc997a2481e6bda35128c5a63c8254759
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 17:45:05 +0000

    sage 10.9,10.9.1

--- Casks/s/sage.rb
+++ Casks/s/sage.rb
@@ -1,8 +1,8 @@
 cask "sage" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "10.9,10.9.0"
-  sha256 arm:   "84f78143db3fb7c251f6eea906c6efb7793d26e96a3fbdb2104c1f9bb4b1827e",
+  version "10.9,10.9.1"
+  sha256 arm:   "630c028d582626bb18fd74d5bd2499b944649caf6aeb4510d46920c4366e73ac",
          intel: "d8424f0401df2d5efe94c6fc8d8b8aabd53034fc9a050cb442bda09cc10e12e6"
 
   url "https://github.com/3-manifolds/Sage_macOS/releases/download/v#{version.csv.second}/SageMath-#{version.csv.first}_#{arch}.dmg",
