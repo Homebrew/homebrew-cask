8ed3dfb72ee6568c493aa39acc6557a88303550d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 22:00:19 +0000

    codexia 0.48.2

--- Casks/c/codexia.rb
+++ Casks/c/codexia.rb
@@ -2,11 +2,11 @@ cask "codexia" do
   arch arm: "aarch64", intel: "x64"
   url_end = on_system_conditional macos: "#{arch}.dmg", linux: "amd64.AppImage"
 
-  version "0.48.1"
+  version "0.48.2"
 
   on_macos do
-    sha256 arm:   "9aa41c41e4eaca2bbfdcf62ba7899e701b30703718a4f322d043362a8e7e14c0",
-           intel: "022f9c186b2c5cd8c20e27eacbb72d86d5ef1f6c3f9e4e05e46f7755beaf345a"
+    sha256 arm:   "82eca23a582fe169961470bc6c93e137c01591fbf095abd2b15e959f7559269a",
+           intel: "fcfab0184b317c86f96cc66246f1aab78ac791e75e76447ce092f908c163cde2"
 
     app "codexia.app"
 
@@ -20,7 +20,7 @@ cask "codexia" do
     ]
   end
   on_linux do
-    sha256 "bf6c9c9c5897922bf20127af1785087dc68c64b51000088a6dc6fb6e9ffa4d48"
+    sha256 "91794a0767e8eb0eccf54d985c9cb9382a1e58e1c39d755691d0b77c48565386"
 
     depends_on arch: :x86_64
 
