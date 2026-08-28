110ff37d44995290866572d749878eeaa4c67d7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 22:17:24 +0000

    hackolade 8.12.10

--- Casks/h/hackolade.rb
+++ Casks/h/hackolade.rb
@@ -1,9 +1,9 @@
 cask "hackolade" do
   arch arm: "ARM64"
 
-  version "8.12.9"
-  sha256 arm:   "da70752e89d165d4bcf0537478f0c03d0627524b22e04e65539b5ca64e36d7d9",
-         intel: "127b2167520434985af5eac9d224f9f3ab447208073fe7977f8a8c53dc68339b"
+  version "8.12.10"
+  sha256 arm:   "0aeea70f9b606169b64e5a8c0affee01fd3f02a759d7d67293ba24781324ec42",
+         intel: "bd1a1e28ef54bd81e936ba33736b48d9d936972fa34f90e295d08bea2de0f9d0"
 
   url "https://hackolade.s3.amazonaws.com/previous/v#{version}/Hackolade-mac#{arch}-setup-signed.pkg",
       verified: "hackolade.s3.amazonaws.com/"
