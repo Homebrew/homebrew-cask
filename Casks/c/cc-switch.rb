5b9681ee7f3a8a5378529ee593b75dfcf12cc5c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 03:39:58 +0000

    cc-switch 3.20.1

--- Casks/c/cc-switch.rb
+++ Casks/c/cc-switch.rb
@@ -2,10 +2,10 @@ cask "cc-switch" do
   arch arm: "arm64", intel: "x86_64"
   url_end = on_system_conditional macos: "macOS.dmg", linux: "Linux-#{arch}.AppImage"
 
-  version "3.20.0"
+  version "3.20.1"
 
   on_macos do
-    sha256 "a7462c22d6333d896c8200be47b80c05642b96d18f36839d34d10aac7072d971"
+    sha256 "b317ba783f1fa6ea0f97c297c363a8f9193b3a1577a649a1dc1d535774c52dab"
 
     depends_on macos: :monterey
 
@@ -21,8 +21,8 @@ cask "cc-switch" do
     ]
   end
   on_linux do
-    sha256 arm64_linux:  "5cf3d0f2de9d74f7e8c2b0f28e9042219b2d0e446ca6eb899835ed94a6a907e3",
-           x86_64_linux: "fa7d635258d200f3ee43a9f261673f25777b55c3c43a29d488d17a0dbc65c7b4"
+    sha256 arm64_linux:  "ff27d86cf9e4db90d97e18d039c0b8e93493bb1d981750d7fb1111f60bc90c68",
+           x86_64_linux: "d735cd84589d55cf4baf6ca460e7ee512a11b7b03a913deea1fec148050d1c46"
 
     app_image "CC-Switch-v#{version}-Linux-#{arch}.AppImage", target: "CC Switch.AppImage"
   end
