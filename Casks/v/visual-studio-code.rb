1cad8878e87e123dbaba529a561f861bb4fb6583
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:47 -0700

    visual-studio-code: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/v/visual-studio-code.rb
+++ Casks/v/visual-studio-code.rb
@@ -1,16 +1,7 @@
 cask "visual-studio-code" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  on_catalina :or_older do
-    version "1.97.2"
-    sha256 arm:   "567ba4fae5545586a0bff02eea263d59873fcf488368a9a9ccf3d4c22dfa8ebc",
-           intel: "cfe48cf7bce34830cb7a20ee7b5e8fbe575fe95a47ef49f62dce8ccf3087dd89"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_big_sur do
+  on_big_sur :or_older do
     version "1.106.3"
     sha256 arm:   "35dd438808dde1dd1f65490ffe7713ed64102324c0809efbec0b4eb2809b218b",
            intel: "c41872149a205f3a3be3e5d3a8f04920407a0762531e607f78dc93f4d4813cda"
