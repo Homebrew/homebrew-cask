e101be05adfd1a37e6888e9c28b8699a82ed7fac
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:28 -0700

    anki: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/a/anki.rb
+++ Casks/a/anki.rb
@@ -1,18 +1,7 @@
 cask "anki" do
   arch arm: "apple", intel: "intel"
 
-  on_catalina :or_older do
-    version "24.11"
-    sha256 "4d44b763ea114e2aae61aaaa8171fd54a9095a06cd8e84a863675b1bbc75a24a"
-
-    url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt5.dmg",
-        verified: "github.com/ankitects/anki/"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_big_sur do
+  on_big_sur :or_older do
     version "25.02.7"
     sha256 arm:   "b2df44bf951404e1d5d56150ee50e158ca5dfd77416b442ed02218348bf1a43b",
            intel: "0d7eb9781596e44c0a833df71c6948706b97c4e89e8bdb315707de33c33c6052"
