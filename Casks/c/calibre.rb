797d8e765388bbf4b7eecbe310ccb7fcecf2cc2a
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:29 -0700

    calibre: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/c/calibre.rb
+++ Casks/c/calibre.rb
@@ -1,10 +1,6 @@
 cask "calibre" do
   on_ventura :or_older do
-    on_catalina :or_older do
-      version "6.11.0"
-      sha256 "d7c40f3f35ba9043c13303632526f135b2c4086471a5c09ceb8b397c55c076fa"
-    end
-    on_big_sur do
+    on_big_sur :or_older do
       version "6.29.0"
       sha256 "2f76428ae19617875c5725cd892751a80eb2acdda76e06cd19c2f21a63966998"
     end
