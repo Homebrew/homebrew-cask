0e064d24bb1eab4a533769851436f535f1c31c56
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:31 -0700

    defold@alpha: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/d/defold@alpha.rb
+++ Casks/d/defold@alpha.rb
@@ -4,13 +4,6 @@ cask "defold@alpha" do
   version "1.13.2"
   sha256 :no_check # required as upstream package is updated in-place
 
-  on_arm do
-    depends_on macos: :big_sur
-  end
-  on_intel do
-    depends_on macos: :catalina
-  end
-
   url "https://github.com/defold/defold/releases/download/#{version}-alpha/Defold-#{arch}-macos.dmg",
       verified: "github.com/defold/defold/"
   name "Defold"
