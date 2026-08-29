5341027ba43ef1ac9f651bbb19528051e9e75f74
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:32 -0700

    duet: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/d/duet.rb
+++ Casks/d/duet.rb
@@ -1,31 +1,17 @@
 cask "duet" do
-  on_catalina :or_older do
-    version "2.4.7.1"
-    sha256 "3e91729e333ab3e2984bec4c1194e7022d016d91c79cd21509bc00b42b9559bd"
+  version "3.20.3.0"
+  sha256 "2edcc20b4238f1490579595956c1e18dfd441d0e6abbb69c9cba788640e80ab9"
 
-    url "https://duetdownload.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip",
-        verified: "duetdownload.com/Mac/"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_big_sur :or_newer do
-    version "3.20.3.0"
-    sha256 "2edcc20b4238f1490579595956c1e18dfd441d0e6abbb69c9cba788640e80ab9"
-
-    url "https://duetdownload.com/Mac/#{version.major}_x/duet-dd-#{version.dots_to_hyphens}.dmg",
-        verified: "duetdownload.com/Mac/"
-
-    livecheck do
-      url "https://updater.duetdownload.com/dd/sparkle.xml"
-      strategy :sparkle
-    end
-  end
-
+  url "https://duetdownload.com/Mac/#{version.major}_x/duet-dd-#{version.dots_to_hyphens}.dmg",
+      verified: "duetdownload.com/Mac/"
   name "Duet"
   desc "Remote desktop and second display tool"
   homepage "https://www.duetdisplay.com/"
+
+  livecheck do
+    url "https://updater.duetdownload.com/dd/sparkle.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on :macos
