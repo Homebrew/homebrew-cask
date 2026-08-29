b8039f31df01badf693b9e15db73aa4b7b6b107d
Patrick Linnane <patrick@linnane.io>
2026-08-29 14:45:42 -0700

    pinta: remove Catalina
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/pinta.rb
+++ Casks/p/pinta.rb
@@ -5,13 +5,6 @@ cask "pinta" do
   sha256 arm:   "daad4b3550e7d5f27928ccb9ca99392aa46a5ef71cfd54ce72c0b7a810f4f993",
          intel: "2f386c0d77008b1beab5153d877d5dd750f52e866621ef1ee734143f052b9f83"
 
-  on_arm do
-    depends_on macos: :big_sur
-  end
-  on_intel do
-    depends_on macos: :catalina
-  end
-
   url "https://github.com/PintaProject/Pinta/releases/download/#{version}/Pinta-macos-#{arch}.dmg",
       verified: "github.com/PintaProject/Pinta/"
   name "Pinta"
