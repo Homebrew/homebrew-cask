49a4e11b81feb5099952781281bfbd4447da22d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-29 20:05:09 +0000

    poi 12.0.1

--- Casks/p/poi.rb
+++ Casks/p/poi.rb
@@ -1,9 +1,9 @@
 cask "poi" do
   arch arm: "-arm64"
 
-  version "12.0.0"
-  sha256 arm:   "1c3d4300fb08a7cafaf2cdc0ea79d52ebfcf66c4f01570c98a9cf1b046c0d675",
-         intel: "4ab095d2c45bcbb2994ca5d0b400c89737154676f20e34a05e83ad14c881898e"
+  version "12.0.1"
+  sha256 arm:   "d63b94de386ea8835c4e23bd2100ea20e8e33edf52f940d9c0419b71722f1c55",
+         intel: "411e3e76e22b1780b281ae3b58a405e6a9bf950653dc14a484c5b5f5a0ffbe8e"
 
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}#{arch}.dmg",
       verified: "github.com/poooi/poi/"
