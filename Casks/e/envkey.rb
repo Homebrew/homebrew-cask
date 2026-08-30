49a35874e98bd2ed2c7503e863e068421d4b0d47
botantony <antonsm21@gmail.com>
2026-08-31 00:39:29 +0200

    envkey: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/e/envkey.rb
+++ Casks/e/envkey.rb
@@ -2,8 +2,7 @@ cask "envkey" do
   version "1.5.10"
   sha256 "97f672b9473b1f213cb068de147fb2c8b7f01beb2c2d747910dfcde29c0efb7a"
 
-  url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip",
-      verified: "github.com/envkey/envkey-app/"
+  url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip"
   name "EnvKey"
   desc "Protects credentials and syncs configurations"
   homepage "https://www.envkey.com/"
