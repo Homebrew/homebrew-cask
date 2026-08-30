cd248268e89506400c31b7aa15e784a5f4a2c365
botantony <antonsm21@gmail.com>
2026-08-31 00:39:57 +0200

    freac: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/freac.rb
+++ Casks/f/freac.rb
@@ -2,8 +2,7 @@ cask "freac" do
   version "1.1.7"
   sha256 "22e29d73bdaf6dcb851a03bfe344028e38457c85b21e7378ffac8625a8ed4f12"
 
-  url "https://github.com/enzo1982/freac/releases/download/v#{version.csv.first}/freac-#{version}-macos11.dmg",
-      verified: "github.com/enzo1982/freac/"
+  url "https://github.com/enzo1982/freac/releases/download/v#{version.csv.first}/freac-#{version}-macos11.dmg"
   name "fre:ac"
   desc "Audio converter and CD ripper"
   homepage "https://www.freac.org/"
