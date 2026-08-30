7552da631292519c00fb1fcc99699ba943bf2379
botantony <antonsm21@gmail.com>
2026-08-31 00:50:25 +0200

    metabase-app: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/metabase-app.rb
+++ Casks/m/metabase-app.rb
@@ -2,8 +2,7 @@ cask "metabase-app" do
   version "0.41.6"
   sha256 "d9fbdab1cf0529d2b56b83367fed61c1b202438d8e3264669a4ffda223229037"
 
-  url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
-      verified: "s3.amazonaws.com/downloads.metabase.com/"
+  url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip"
   name "Metabase"
   desc "Business intelligence and analytics"
   homepage "https://www.metabase.com/"
