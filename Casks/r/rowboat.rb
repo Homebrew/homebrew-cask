358feb48a1ed531627ce165a5be0ed3e80a296cc
botantony <antonsm21@gmail.com>
2026-08-31 00:51:58 +0200

    rowboat: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/rowboat.rb
+++ Casks/r/rowboat.rb
@@ -5,8 +5,7 @@ cask "rowboat" do
   sha256 arm:   "8128a431f7c9b4dae9dc47356adcd2fb1740f10fe6b9b4dd2b179af79d443adb",
          intel: "1658535749b4cb2db496176f913719beee186e9e23b58d5c7ac0c1d47320e8ac"
 
-  url "https://github.com/rowboatlabs/rowboat/releases/download/v#{version}/Rowboat-darwin-#{arch}-#{version}.zip",
-      verified: "github.com/rowboatlabs/rowboat/"
+  url "https://github.com/rowboatlabs/rowboat/releases/download/v#{version}/Rowboat-darwin-#{arch}-#{version}.zip"
   name "Rowboat"
   desc "Open-source AI coworker, with memory"
   homepage "https://www.rowboatlabs.com/"
