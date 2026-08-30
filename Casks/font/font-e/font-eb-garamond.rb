75647220a4cde5bd87da1c7a1c6b9961b790dda0
botantony <antonsm21@gmail.com>
2026-08-31 00:42:12 +0200

    font-eb-garamond: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-e/font-eb-garamond.rb
+++ Casks/font/font-e/font-eb-garamond.rb
@@ -2,8 +2,7 @@ cask "font-eb-garamond" do
   version "0.016"
   sha256 "a0b77b405f55c10cff078787ef9d2389a9638e7604d53aa80207fe62e104c378"
 
-  url "https://bitbucket.org/georgd/eb-garamond/downloads/EBGaramond-#{version}.zip",
-      verified: "bitbucket.org/georgd/eb-garamond/"
+  url "https://bitbucket.org/georgd/eb-garamond/downloads/EBGaramond-#{version}.zip"
   name "EB Garamond"
   homepage "https://github.com/georgd/EB-Garamond"
 
