df143de76b44eef18b1ac1fb8bdfcf285d43d6c2
botantony <antonsm21@gmail.com>
2026-08-31 00:37:03 +0200

    affinity-designer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/affinity-designer.rb
+++ Casks/a/affinity-designer.rb
@@ -2,8 +2,7 @@ cask "affinity-designer" do
   version "2.6.5,3782"
   sha256 "b07e7237ced47fccabd07ed8d1c282c9918bbffbae23a7df5e1c55b2620834f9"
 
-  url "https://affinity-update.s3.amazonaws.com/mac2/retail/Affinity%20Designer%20#{version.csv.first.major}%20Affinity%20Store%20#{version.csv.second}.zip",
-      verified: "affinity-update.s3.amazonaws.com/"
+  url "https://affinity-update.s3.amazonaws.com/mac2/retail/Affinity%20Designer%20#{version.csv.first.major}%20Affinity%20Store%20#{version.csv.second}.zip"
   name "Affinity Designer #{version.csv.first.major}"
   desc "Professional graphic design software"
   homepage "https://affinity.serif.com/en-us/designer/"
