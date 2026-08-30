8eaf34bdf067d3d9d0c742741d6e7e63d70a27c1
botantony <antonsm21@gmail.com>
2026-08-31 00:47:29 +0200

    font-rubik-burned: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-r/font-rubik-burned.rb
+++ Casks/font/font-r/font-rubik-burned.rb
@@ -2,8 +2,7 @@ cask "font-rubik-burned" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/rubikburned/RubikBurned-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/rubikburned/RubikBurned-Regular.ttf"
   name "Rubik Burned"
   homepage "https://fonts.google.com/specimen/Rubik+Burned"
 
