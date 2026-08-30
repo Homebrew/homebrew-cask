3b020829a7bd4ac3d17f72ad754632b9910af383
botantony <antonsm21@gmail.com>
2026-08-31 00:47:37 +0200

    font-sacramento: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-s/font-sacramento.rb
+++ Casks/font/font-s/font-sacramento.rb
@@ -2,8 +2,7 @@ cask "font-sacramento" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/sacramento/Sacramento-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/sacramento/Sacramento-Regular.ttf"
   name "Sacramento"
   homepage "https://fonts.google.com/specimen/Sacramento"
 
