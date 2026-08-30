c23c337c855245fd857439f49a923b3b78d50247
botantony <antonsm21@gmail.com>
2026-08-31 00:45:09 +0200

    font-nobile: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-nobile.rb
+++ Casks/font/font-n/font-nobile.rb
@@ -3,7 +3,6 @@ cask "font-nobile" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/nobile"
   name "Nobile"
