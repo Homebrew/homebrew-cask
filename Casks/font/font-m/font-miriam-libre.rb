3f47f379c498324c078a10876dc0b2bdbb7eb60e
botantony <antonsm21@gmail.com>
2026-08-31 00:44:44 +0200

    font-miriam-libre: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-m/font-miriam-libre.rb
+++ Casks/font/font-m/font-miriam-libre.rb
@@ -2,8 +2,7 @@ cask "font-miriam-libre" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/miriamlibre/MiriamLibre%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/miriamlibre/MiriamLibre%5Bwght%5D.ttf"
   name "Miriam Libre"
   homepage "https://fonts.google.com/specimen/Miriam+Libre"
 
