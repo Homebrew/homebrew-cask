babbe141bf822d2c456e956e334bafbfbec5057d
botantony <antonsm21@gmail.com>
2026-08-31 00:45:41 +0200

    font-noto-sans-ol-chiki: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-ol-chiki.rb
+++ Casks/font/font-n/font-noto-sans-ol-chiki.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-ol-chiki" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansolchiki/NotoSansOlChiki%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansolchiki/NotoSansOlChiki%5Bwght%5D.ttf"
   name "Noto Sans Ol Chiki"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Ol+Chiki"
 
