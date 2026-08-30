db9d779ed808bfd93f7e50570669159e217b243a
botantony <antonsm21@gmail.com>
2026-08-31 00:42:38 +0200

    font-freeman: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-f/font-freeman.rb
+++ Casks/font/font-f/font-freeman.rb
@@ -2,8 +2,7 @@ cask "font-freeman" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/freeman/Freeman-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/freeman/Freeman-Regular.ttf"
   name "Freeman"
   homepage "https://fonts.google.com/specimen/Freeman"
 
