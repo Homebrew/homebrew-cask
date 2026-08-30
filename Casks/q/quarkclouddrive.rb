f06a741b4ad93002e0abb15e4d663f545fd040e9
Samuel Huang <hi@sgh.ng>
2026-08-30 11:00:54 -0400

    quarkclouddrive: inline channel variable

--- Casks/q/quarkclouddrive.rb
+++ Casks/q/quarkclouddrive.rb
@@ -1,7 +1,5 @@
 cask "quarkclouddrive" do
   arch arm: "arm64", intel: "x64"
-  channel = on_arch_conditional arm:   "pckk@app_downloader_fail_arm64",
-                                intel: "pckk@app_downloader_fail_x64"
 
   on_arm do
     version "7.0.6.771,3120899"
@@ -12,17 +10,17 @@ cask "quarkclouddrive" do
     sha256 "aa21c3ca95f3e3abfa21ac1d1b0e3bc63c801238db6b0e66a3ce9c74757fb9f9"
   end
 
-  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/#{channel}/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_abi#{arch}_(Build#{version.csv.second}).dmg"
+  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@app_downloader_fail_#{arch}/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_abi#{arch}_(Build#{version.csv.second}).dmg"
   name "Quark Cloud Drive"
   name "夸克网盘"
   desc "Cloud storage and file management platform"
   homepage "https://pan.quark.cn/"
 
   livecheck do
-    url "https://download.quark.cn/download/quarkclouddrivemac?platform=mac&ch=#{channel}"
+    url "https://download.quark.cn/download/quarkclouddrivemac?platform=mac&ch=pckk@app_downloader_fail_#{arch}"
     regex(%r{
       /download/37213/quarkclouddrivemac/
-      #{Regexp.escape(channel)}/
+      pckk@app_downloader_fail_#{arch}/
       QuarkCloudDriveMac[._-]v?(\d+(?:\.\d+)+)
       [^/]*\(Build(\d+)\)\.dmg
     }xi)
