78e025c1eabfbca9c0d89f443d70a00122a6ce5c
botantony <antonsm21@gmail.com>
2026-08-31 00:44:13 +0200

    font-love-light: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-love-light.rb
+++ Casks/font/font-l/font-love-light.rb
@@ -2,8 +2,7 @@ cask "font-love-light" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lovelight/LoveLight-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/lovelight/LoveLight-Regular.ttf"
   name "Love Light"
   homepage "https://fonts.google.com/specimen/Love+Light"
 
