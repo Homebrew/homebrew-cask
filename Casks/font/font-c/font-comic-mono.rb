4b1f310e008c32d3f16c2b4ed08e9bed280c0f96
botantony <antonsm21@gmail.com>
2026-08-31 00:41:47 +0200

    font-comic-mono: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-comic-mono.rb
+++ Casks/font/font-c/font-comic-mono.rb
@@ -2,8 +2,7 @@ cask "font-comic-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/dtinth/comic-mono-font/archive/refs/heads/master.tar.gz",
-      verified: "github.com/dtinth/comic-mono-font/"
+  url "https://github.com/dtinth/comic-mono-font/archive/refs/heads/master.tar.gz"
   name "Comic Mono"
   homepage "https://dtinth.github.io/comic-mono-font/"
 
