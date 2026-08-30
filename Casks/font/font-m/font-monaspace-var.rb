61293d007b4bb05bd499586d5018db7f0902600a
botantony <antonsm21@gmail.com>
2026-08-31 00:44:49 +0200

    font-monaspace-var: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-m/font-monaspace-var.rb
+++ Casks/font/font-m/font-monaspace-var.rb
@@ -2,8 +2,7 @@ cask "font-monaspace-var" do
   version "1.400"
   sha256 "b69a7f2a3c455c89dcb6cc23b61bb3fb8beecba728ccdebeccd80577e58de0c8"
 
-  url "https://github.com/githubnext/monaspace/releases/download/v#{version}/monaspace-variable-v#{version}.zip",
-      verified: "github.com/githubnext/monaspace/"
+  url "https://github.com/githubnext/monaspace/releases/download/v#{version}/monaspace-variable-v#{version}.zip"
   name "Monaspace Var"
   homepage "https://monaspace.githubnext.com/"
 
