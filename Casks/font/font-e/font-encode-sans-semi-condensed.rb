a2ce74d049cec5fde40b72017d5d63476b5786b0
botantony <antonsm21@gmail.com>
2026-08-31 00:42:19 +0200

    font-encode-sans-semi-condensed: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-e/font-encode-sans-semi-condensed.rb
+++ Casks/font/font-e/font-encode-sans-semi-condensed.rb
@@ -3,7 +3,6 @@ cask "font-encode-sans-semi-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/encodesanssemicondensed"
   name "Encode Sans Semi Condensed"
