915527e4e016219e7478a68b50e02136d378ceb7
botantony <antonsm21@gmail.com>
2026-08-31 00:39:47 +0200

    flirc: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/flirc.rb
+++ Casks/f/flirc.rb
@@ -2,8 +2,7 @@ cask "flirc" do
   version "3.27.19"
   sha256 "da00f51a8b64f1e7fbb75bc229e715428efe5059d06607fe5571a4e1bc011454"
 
-  url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg",
-      verified: "flirc.com/"
+  url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
   name "Flirc"
   desc "IR USB receiver configurator"
   homepage "https://flirc.tv/"
