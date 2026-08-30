ace7f51b4f8609b6b63f329893d8c39dae1ec045
botantony <antonsm21@gmail.com>
2026-08-31 00:43:27 +0200

    font-jeju-myeongjo: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-j/font-jeju-myeongjo.rb
+++ Casks/font/font-j/font-jeju-myeongjo.rb
@@ -2,8 +2,7 @@ cask "font-jeju-myeongjo" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/jejumyeongjo/JejuMyeongjo-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/jejumyeongjo/JejuMyeongjo-Regular.ttf"
   name "Jeju Myeongjo"
   homepage "https://fonts.google.com/earlyaccess"
 
