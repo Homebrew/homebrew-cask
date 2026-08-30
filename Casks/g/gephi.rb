abdf8ce6da8498a69f0d7af454b487e925f65c5e
botantony <antonsm21@gmail.com>
2026-08-31 00:49:00 +0200

    gephi: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gephi.rb
+++ Casks/g/gephi.rb
@@ -5,8 +5,7 @@ cask "gephi" do
   sha256 arm:   "c5ab54d387386f568b400c03d6918a7a99232de828e29f6eb36c79d86f435fa8",
          intel: "20a858631ae85ed1250e7a3afabe3ac09ec17375adf48d50d37157855ca5fcc3"
 
-  url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos-#{arch}.dmg",
-      verified: "github.com/gephi/gephi/"
+  url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos-#{arch}.dmg"
   name "Gephi"
   desc "Open-source platform for visualizing and manipulating large graphs"
   homepage "https://gephi.org/"
