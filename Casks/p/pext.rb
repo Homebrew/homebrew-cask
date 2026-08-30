93610421e643ffef05afa939d22f7341d0f432fa
botantony <antonsm21@gmail.com>
2026-08-31 00:51:21 +0200

    pext: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/pext.rb
+++ Casks/p/pext.rb
@@ -2,8 +2,7 @@ cask "pext" do
   version "0.35"
   sha256 "eab9c61d0a05b131bec175f08b7c5ef7e4aed07c90405f7b1b14ce8b4a11b605"
 
-  url "https://github.com/Pext/Pext/releases/download/v#{version}/Pext-#{version}.dmg",
-      verified: "github.com/Pext/Pext/"
+  url "https://github.com/Pext/Pext/releases/download/v#{version}/Pext-#{version}.dmg"
   name "Pext"
   desc "Python-based extendable tool"
   homepage "https://pext.io/"
