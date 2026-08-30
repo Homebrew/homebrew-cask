2dfd078662ad22f74cd56486eae484d79e3bd4d5
botantony <antonsm21@gmail.com>
2026-08-31 00:49:50 +0200

    koharu: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/koharu.rb
+++ Casks/k/koharu.rb
@@ -2,8 +2,7 @@ cask "koharu" do
   version "0.80.0"
   sha256 "dc5381d5d4b1dfe635efa7ae4953a64748d54dc2af8d3e200eef691d021d9d72"
 
-  url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg",
-      verified: "github.com/mayocream/koharu/"
+  url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg"
   name "Koharu"
   desc "ML-powered manga translator"
   homepage "https://koharu.rs/"
