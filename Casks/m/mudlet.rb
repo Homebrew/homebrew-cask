9bd838179b215f29f9a95e65bc9c98c8477a8172
botantony <antonsm21@gmail.com>
2026-08-31 00:50:41 +0200

    mudlet: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/mudlet.rb
+++ Casks/m/mudlet.rb
@@ -5,8 +5,7 @@ cask "mudlet" do
   sha256 arm:   "4b2750f9ac2bfe4b5124167b9ae26498dc3c4156c76bbc877e4f9986109dcf74",
          intel: "9ed4ea62c188861fa36cff0523487300d0ef3c2707df3de362e23d3d7b90cb3e"
 
-  url "https://github.com/Mudlet/Mudlet/releases/download/Mudlet-#{version}/Mudlet-#{version}-#{arch}.dmg",
-      verified: "github.com/Mudlet/Mudlet/"
+  url "https://github.com/Mudlet/Mudlet/releases/download/Mudlet-#{version}/Mudlet-#{version}-#{arch}.dmg"
   name "Mudlet"
   desc "Multi-User Dungeon client"
   homepage "https://www.mudlet.org/"
