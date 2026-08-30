dd8b95ec725b4c303ea19ef8a9b7572a24e2566e
botantony <antonsm21@gmail.com>
2026-08-31 00:39:16 +0200

    duckietv: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/d/duckietv.rb
+++ Casks/d/duckietv.rb
@@ -2,8 +2,7 @@ cask "duckietv" do
   version "1.1.5"
   sha256 "9c2f72c011cd477071e51238d5bfa0c202babdd263c1e4ea6b3d4e4605da2907"
 
-  url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg",
-      verified: "github.com/SchizoDuckie/DuckieTV/"
+  url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg"
   name "duckieTV"
   desc "Tool to track TV shows with semi-automagic torrent integration"
   homepage "https://schizoduckie.github.io/DuckieTV/"
