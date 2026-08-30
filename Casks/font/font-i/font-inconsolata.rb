a041779b8153c4451e7b7194b1f22f30f47365fa
botantony <antonsm21@gmail.com>
2026-08-31 00:43:18 +0200

    font-inconsolata: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-i/font-inconsolata.rb
+++ Casks/font/font-i/font-inconsolata.rb
@@ -2,8 +2,7 @@ cask "font-inconsolata" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf"
   name "Inconsolata"
   homepage "https://fonts.google.com/specimen/Inconsolata"
 
