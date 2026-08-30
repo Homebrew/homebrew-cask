7ba3fc5bb618d622be2afd9d62987d51c3dfbf16
botantony <antonsm21@gmail.com>
2026-08-31 00:48:29 +0200

    font-tsukimi-rounded: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-t/font-tsukimi-rounded.rb
+++ Casks/font/font-t/font-tsukimi-rounded.rb
@@ -3,7 +3,6 @@ cask "font-tsukimi-rounded" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/tsukimirounded"
   name "Tsukimi Rounded"
