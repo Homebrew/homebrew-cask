31c9e0a5fe681b287afcd303509319dae21788cc
botantony <antonsm21@gmail.com>
2026-08-31 00:48:27 +0200

    font-train-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-t/font-train-one.rb
+++ Casks/font/font-t/font-train-one.rb
@@ -2,8 +2,7 @@ cask "font-train-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/trainone/TrainOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/trainone/TrainOne-Regular.ttf"
   name "Train One"
   homepage "https://fonts.google.com/specimen/Train+One"
 
