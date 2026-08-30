4471e763bb0ca612627a7c21c0a1b835f25fcaf2
botantony <antonsm21@gmail.com>
2026-08-31 00:37:49 +0200

    bettercapture: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bettercapture.rb
+++ Casks/b/bettercapture.rb
@@ -2,8 +2,7 @@ cask "bettercapture" do
   version "2026.3"
   sha256 "cbb59b9aaeb2d39581c8a3d28f5f63fdd24041bf226685185d3fd6d054eb247e"
 
-  url "https://github.com/jsattler/BetterCapture/releases/download/v#{version}/BetterCapture-#{version}-arm64.dmg",
-      verified: "github.com/jsattler/BetterCapture/"
+  url "https://github.com/jsattler/BetterCapture/releases/download/v#{version}/BetterCapture-#{version}-arm64.dmg"
   name "BetterCapture"
   desc "Screen recorder"
   homepage "https://bettercapture.app/"
