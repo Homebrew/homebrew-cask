c27e466e571a7d18d4b80c8e6b97b24c37333442
botantony <antonsm21@gmail.com>
2026-08-31 00:49:15 +0200

    handy: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/h/handy.rb
+++ Casks/h/handy.rb
@@ -5,8 +5,7 @@ cask "handy" do
   sha256 arm:   "a961b35724f6c860bcdcece1f1d77c21343ad2156525f8800ca968a1aad4d854",
          intel: "f47b184b3052fa6a994e6cae1619b6340cea0e8742e616042444908649659d08"
 
-  url "https://github.com/cjpais/Handy/releases/download/v#{version}/Handy_#{version}_#{arch}.dmg",
-      verified: "github.com/cjpais/Handy/"
+  url "https://github.com/cjpais/Handy/releases/download/v#{version}/Handy_#{version}_#{arch}.dmg"
   name "Handy"
   desc "Speech to text application"
   homepage "https://handy.computer/"
