e008d26acc17a6cba954a2fb40f382deca6f6eef
botantony <antonsm21@gmail.com>
2026-08-31 00:49:26 +0200

    igdm: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/i/igdm.rb
+++ Casks/i/igdm.rb
@@ -2,8 +2,7 @@ cask "igdm" do
   version "3.0.4"
   sha256 "fe032a9657267018efc16697d218dbdcda02564f5d42e4b2e21bf68ea9e715c7"
 
-  url "https://github.com/ifedapoolarewaju/igdm/releases/download/v#{version}/IGdm-#{version}.dmg",
-      verified: "github.com/ifedapoolarewaju/igdm/"
+  url "https://github.com/ifedapoolarewaju/igdm/releases/download/v#{version}/IGdm-#{version}.dmg"
   name "IG:dm"
   desc "Desktop application for Instagram DMs"
   homepage "https://igdm.me/"
