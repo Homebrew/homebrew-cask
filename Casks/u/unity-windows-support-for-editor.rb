5e10dfb510e83fbe65d775ac85e48b6bded7ec8e
botantony <antonsm21@gmail.com>
2026-08-31 00:53:02 +0200

    unity-windows-support-for-editor: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/unity-windows-support-for-editor.rb
+++ Casks/u/unity-windows-support-for-editor.rb
@@ -2,8 +2,7 @@ cask "unity-windows-support-for-editor" do
   version "6000.5.10f1,3bd4f66ad299"
   sha256 "797efe7a14d31ccef1560da2e5cf0cdca24657e834564f864a27f78627d00652"
 
-  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg",
-      verified: "download.unity3d.com/download_unity/"
+  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg"
   name "Unity Windows (Mono) Build Support"
   desc "Windows (Mono) target support for Unity"
   homepage "https://unity.com/products"
