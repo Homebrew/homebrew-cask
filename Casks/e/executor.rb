34b3610d1339a9543a0bea932493f9ed2dbfa97e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 06:56:54 +0000

    executor 1.6.7

--- Casks/e/executor.rb
+++ Casks/e/executor.rb
@@ -1,9 +1,9 @@
 cask "executor" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.5"
-  sha256 arm:   "cca1d66ba837cf41a9a9fef87c7e22ca02a92b83f753acb6099de3cf6a1a4813",
-         intel: "118ab556543b5d6f8abe8185f453a356afb3cc13d36ea2fd1cdf5df6f8f2ba2e"
+  version "1.6.7"
+  sha256 arm:   "3d3336c3cf1e7a49105a04ad2904dcde0f13aac8a3ce1ee268c5c718bb62572c",
+         intel: "7a1edffe4385374f051b8cad2245049862bd25ddeb4a07f63b06788151472c5d"
 
   url "https://github.com/RhysSullivan/executor/releases/download/v#{version}/executor-desktop-mac-#{arch}.dmg",
       verified: "github.com/RhysSullivan/executor/"
