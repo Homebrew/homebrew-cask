351f5c45645bcb424504d8ffd30b7d022341f56c
botantony <antonsm21@gmail.com>
2026-08-31 00:39:55 +0200

    fossa: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/fossa.rb
+++ Casks/f/fossa.rb
@@ -5,8 +5,7 @@ cask "fossa" do
   sha256 arm:   "53e7827502abb2f755d6cf41374967697917e82647ce1801a7d387aa4a908bc8",
          intel: "db4e4d4283e5b45f6e25634175a48f26fef2dc9dbc865425446fc6015c9c00d6"
 
-  url "https://github.com/fossas/fossa-cli/releases/download/v#{version}/fossa_#{version}_darwin_#{arch}.zip",
-      verified: "github.com/fossas/fossa-cli/"
+  url "https://github.com/fossas/fossa-cli/releases/download/v#{version}/fossa_#{version}_darwin_#{arch}.zip"
   name "FOSSA"
   desc "Zero-configuration polyglot dependency analysis tool"
   homepage "https://fossa.com/"
