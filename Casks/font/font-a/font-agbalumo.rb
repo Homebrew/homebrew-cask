b38a1b12e25e282f1456fbdef40cb136596b1961
botantony <antonsm21@gmail.com>
2026-08-31 00:40:10 +0200

    font-agbalumo: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-a/font-agbalumo.rb
+++ Casks/font/font-a/font-agbalumo.rb
@@ -2,8 +2,7 @@ cask "font-agbalumo" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/agbalumo/Agbalumo-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/agbalumo/Agbalumo-Regular.ttf"
   name "Agbalumo"
   homepage "https://fonts.google.com/specimen/Agbalumo"
 
