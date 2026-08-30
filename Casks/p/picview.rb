151f1f054e6e9a6049d3036dfa8031b10b0c0344
botantony <antonsm21@gmail.com>
2026-08-31 00:51:22 +0200

    picview: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/picview.rb
+++ Casks/p/picview.rb
@@ -5,8 +5,7 @@ cask "picview" do
   sha256 arm:   "1adac9ee3d19b02b56c9ce3e47322dba4ea735df08633033a6190aee24e49402",
          intel: "3a699a1df05ceaa9bbfc02ff25933a4999724c9ad9e251383de1222b30a9b5f3"
 
-  url "https://github.com/Ruben2776/PicView/releases/download/#{version}/PicView-#{version}-macOS-#{arch}.dmg",
-      verified: "github.com/Ruben2776/PicView/"
+  url "https://github.com/Ruben2776/PicView/releases/download/#{version}/PicView-#{version}-macOS-#{arch}.dmg"
   name "PicView"
   desc "Picture viewer"
   homepage "https://picview.org/"
