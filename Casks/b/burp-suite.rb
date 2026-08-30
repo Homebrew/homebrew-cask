2e5b4644fe1867a6656612720feb069af66a5949
botantony <antonsm21@gmail.com>
2026-08-31 00:38:10 +0200

    burp-suite: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/burp-suite.rb
+++ Casks/b/burp-suite.rb
@@ -5,8 +5,7 @@ cask "burp-suite" do
   sha256 arm:   "fa2702f50dd6f68faaa2e288cf2b5334c193c177925c8a96ffe6d7f8c958fac6",
          intel: "9e32c719780fcd9f9f72423d2bedcfd622e16fe4575799411115a09172336677"
 
-  url "https://portswigger-cdn.net/burp/releases/download?product=desktop&version=#{version}&type=#{arch}",
-      verified: "portswigger-cdn.net/burp/releases/"
+  url "https://portswigger-cdn.net/burp/releases/download?product=desktop&version=#{version}&type=#{arch}"
   name "Burp Suite Community Edition"
   desc "Web security testing toolkit"
   homepage "https://portswigger.net/burp/"
