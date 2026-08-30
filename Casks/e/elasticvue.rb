43ac1041b332774f8a3da550f7f94af6e0948955
botantony <antonsm21@gmail.com>
2026-08-31 00:39:23 +0200

    elasticvue: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/e/elasticvue.rb
+++ Casks/e/elasticvue.rb
@@ -5,8 +5,7 @@ cask "elasticvue" do
   sha256 arm:   "db40979ec2667c9cb3aced573dc4ff33e1f6b4515fb33da6883350ec8e9a0f25",
          intel: "a635d45129043fd0687d70339512e458776a5aa546e4fa3cbd197ed9a136e6da"
 
-  url "https://github.com/cars10/elasticvue/releases/download/v#{version}/elasticvue_#{version}_#{arch}.dmg",
-      verified: "github.com/cars10/elasticvue/"
+  url "https://github.com/cars10/elasticvue/releases/download/v#{version}/elasticvue_#{version}_#{arch}.dmg"
   name "Elasticvue"
   desc "Elasticsearch GUI"
   homepage "https://elasticvue.com/"
