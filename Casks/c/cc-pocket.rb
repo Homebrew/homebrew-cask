fba5d7c0408e72cf93e4fd0d42628f520be985c5
botantony <antonsm21@gmail.com>
2026-08-31 00:38:20 +0200

    cc-pocket: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/cc-pocket.rb
+++ Casks/c/cc-pocket.rb
@@ -2,8 +2,7 @@ cask "cc-pocket" do
   version "1.126.2,240"
   sha256 "8ee471c69a85a7082a305000779abac2a503780b93968b2eb62db08a2cf49754"
 
-  url "https://github.com/K9i-0/ccpocket/releases/download/macos/v#{version.csv.first}%2B#{version.csv.second}/CC-Pocket-macos-v#{version.csv.first}.dmg",
-      verified: "github.com/K9i-0/ccpocket/"
+  url "https://github.com/K9i-0/ccpocket/releases/download/macos/v#{version.csv.first}%2B#{version.csv.second}/CC-Pocket-macos-v#{version.csv.first}.dmg"
   name "CC Pocket"
   desc "Remote client for Codex and Claude coding agents"
   homepage "https://k9i-0.github.io/ccpocket/install/"
