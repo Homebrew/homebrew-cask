2ddc67b0babebb0439431f95f614f001bfbb3267
botantony <antonsm21@gmail.com>
2026-08-31 00:51:27 +0200

    plover: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/plover.rb
+++ Casks/p/plover.rb
@@ -2,8 +2,7 @@ cask "plover" do
   version "5.4.1"
   sha256 "22a4fc885f947cb1b5954bd5e32b51c55c57b7eb9cb909f18ec7807f2ffa948c"
 
-  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-macosx_12_0_universal2.dmg",
-      verified: "github.com/openstenoproject/plover/"
+  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-macosx_12_0_universal2.dmg"
   name "Plover"
   desc "Stenotype engine"
   homepage "https://opensteno.org/plover/"
