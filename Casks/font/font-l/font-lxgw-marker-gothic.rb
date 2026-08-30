64c33fcb5e7da73f9f088e627870dadb59354d22
botantony <antonsm21@gmail.com>
2026-08-31 00:44:16 +0200

    font-lxgw-marker-gothic: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-lxgw-marker-gothic.rb
+++ Casks/font/font-l/font-lxgw-marker-gothic.rb
@@ -2,8 +2,7 @@ cask "font-lxgw-marker-gothic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lxgwmarkergothic/LXGWMarkerGothic-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/lxgwmarkergothic/LXGWMarkerGothic-Regular.ttf"
   name "LXGW Marker Gothic"
   homepage "https://fonts.google.com/specimen/LXGW+Marker+Gothic"
 
