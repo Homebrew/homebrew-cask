1940b467405e3cfb0b35dae80b45c101f1a1be97
botantony <antonsm21@gmail.com>
2026-08-31 00:51:39 +0200

    qblocker: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/q/qblocker.rb
+++ Casks/q/qblocker.rb
@@ -2,8 +2,7 @@ cask "qblocker" do
   version "1.2,20,1464612307"
   sha256 "fb0202894271c3b63a6cffb6fa72d5d27065a0698a3b8a8038e18b7c43872385"
 
-  url "https://dl.devmate.com/uk.co.wearecocoon.QBlocker/#{version.csv.second}/#{version.csv.third}/QBlocker-#{version.csv.second}.zip",
-      verified: "devmate.com/uk.co.wearecocoon.QBlocker/"
+  url "https://dl.devmate.com/uk.co.wearecocoon.QBlocker/#{version.csv.second}/#{version.csv.third}/QBlocker-#{version.csv.second}.zip"
   name "QBlocker"
   desc "Stops you from accidentally quitting an app"
   homepage "https://qblocker.com/"
