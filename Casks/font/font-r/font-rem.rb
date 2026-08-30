10d7956dfee2f7b61025e16ad548f248617b34b8
botantony <antonsm21@gmail.com>
2026-08-31 00:47:21 +0200

    font-rem: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-r/font-rem.rb
+++ Casks/font/font-r/font-rem.rb
@@ -3,7 +3,6 @@ cask "font-rem" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/rem"
   name "REM"
