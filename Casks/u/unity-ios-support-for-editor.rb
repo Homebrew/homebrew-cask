3f57312cf2feb3ec014417c7594e14b46963112a
botantony <antonsm21@gmail.com>
2026-08-31 00:53:02 +0200

    unity-ios-support-for-editor: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/unity-ios-support-for-editor.rb
+++ Casks/u/unity-ios-support-for-editor.rb
@@ -2,8 +2,7 @@ cask "unity-ios-support-for-editor" do
   version "6000.5.10f1,3bd4f66ad299"
   sha256 "fdff4effc227d28c8393888af7aac59e39960d0e83c110a3dc7939b2b940e052"
 
-  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg",
-      verified: "download.unity3d.com/download_unity/"
+  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg"
   name "Unity iOS Build Support"
   desc "iOS target support for Unity"
   homepage "https://unity.com/products"
