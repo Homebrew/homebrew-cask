55b6937f045f08c4c1446379a96bbf2d0d82f78c
botantony <antonsm21@gmail.com>
2026-08-31 00:48:57 +0200

    gb-studio: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gb-studio.rb
+++ Casks/g/gb-studio.rb
@@ -5,8 +5,7 @@ cask "gb-studio" do
   sha256 arm:   "1b5e2c4402350223cf2810bd917a98025d85b350a48e449d1da3bb4486ff91eb",
          intel: "d56a850c7585f22c99b09d9c03af231fd76604a402b1f6c556979e72aefdba7c"
 
-  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/gb-studio-mac-#{arch}.zip",
-      verified: "github.com/chrismaltby/gb-studio/"
+  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/gb-studio-mac-#{arch}.zip"
   name "GB Studio"
   desc "Drag and drop retro game creator"
   homepage "https://www.gbstudio.dev/"
