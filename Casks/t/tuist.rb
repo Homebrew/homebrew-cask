eb005958f619a96c0d8c445e5f8649343620ea16
botantony <antonsm21@gmail.com>
2026-08-31 00:52:56 +0200

    tuist: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -2,8 +2,7 @@ cask "tuist" do
   version "4.205.0"
   sha256 "f83c742c80112e6d61ee3d4a2fa888c813ad86b94a787b1fe7ab4d7386dcafd5"
 
-  url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
-      verified: "github.com/tuist/tuist/"
+  url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip"
   name "Tuist"
   desc "Create, maintain, and interact with Xcode projects at scale"
   homepage "https://tuist.io/"
