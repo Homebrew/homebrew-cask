eabb651ec41c8a9fc2b45ab1e8b1f8d5828b9a84
botantony <antonsm21@gmail.com>
2026-08-31 00:50:06 +0200

    lookaway: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/lookaway.rb
+++ Casks/l/lookaway.rb
@@ -2,8 +2,7 @@ cask "lookaway" do
   version "2.4.4"
   sha256 "146cd202ebc0026e986d4b7ad138c374ff9a8f723db99e94d202eb74cf4f2f86"
 
-  url "https://github.com/mysticalbits/lookaway-releases/releases/download/#{version}/LookAway.dmg",
-      verified: "github.com/mysticalbits/lookaway-releases/"
+  url "https://github.com/mysticalbits/lookaway-releases/releases/download/#{version}/LookAway.dmg"
   name "LookAway"
   desc "Break time reminder app"
   homepage "https://lookaway.com/"
