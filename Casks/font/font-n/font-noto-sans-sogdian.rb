2b8aa8c461ff208b7dc1571ef9d32ffd559979b3
botantony <antonsm21@gmail.com>
2026-08-31 00:45:49 +0200

    font-noto-sans-sogdian: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-sogdian.rb
+++ Casks/font/font-n/font-noto-sans-sogdian.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-sogdian" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanssogdian/NotoSansSogdian-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanssogdian/NotoSansSogdian-Regular.ttf"
   name "Noto Sans Sogdian"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Sogdian"
 
