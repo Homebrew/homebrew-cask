3a4da1568e246244d25462e2a56d038a28aef01f
botantony <antonsm21@gmail.com>
2026-08-31 00:49:28 +0200

    inso: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/i/inso.rb
+++ Casks/i/inso.rb
@@ -2,8 +2,7 @@ cask "inso" do
   version "13.2.0"
   sha256 "939f749ed909f28a54752b4c21bb66a0cda53354150bb1439036fe4a7d172fef"
 
-  url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/inso-macos-#{version}.zip",
-      verified: "github.com/Kong/insomnia/"
+  url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/inso-macos-#{version}.zip"
   name "inso"
   desc "CLI HTTP and GraphQL Client"
   homepage "https://insomnia.rest/products/inso"
