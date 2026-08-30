f9c411b6f30b273346fd0342b8f15253e06f83f6
botantony <antonsm21@gmail.com>
2026-08-31 00:53:10 +0200

    virtualbuddy@beta: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/virtualbuddy@beta.rb
+++ Casks/v/virtualbuddy@beta.rb
@@ -2,8 +2,7 @@ cask "virtualbuddy@beta" do
   version "2.2,408"
   sha256 "18abf51522408a8bae4ea8b8b9242945a0ed34fd14127bf60066e765e3e2381f"
 
-  url "https://su.virtualbuddy.app/beta/VirtualBuddy_v#{version.csv.first}-#{version.csv.second}.dmg",
-      verified: "su.virtualbuddy.app/"
+  url "https://su.virtualbuddy.app/beta/VirtualBuddy_v#{version.csv.first}-#{version.csv.second}.dmg"
   name "VirtualBuddy"
   desc "Virtualization tool"
   homepage "https://github.com/insidegui/VirtualBuddy"
