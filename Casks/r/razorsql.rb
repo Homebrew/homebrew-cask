eeec1bf784bfc3b8ac8c6dd4e6a5aaa0636afd64
botantony <antonsm21@gmail.com>
2026-08-31 00:51:46 +0200

    razorsql: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/razorsql.rb
+++ Casks/r/razorsql.rb
@@ -5,8 +5,7 @@ cask "razorsql" do
   sha256 arm:   "eb2b2a97b8893272df509382f2764315ad31e6c3641fba5f5213a0c4085ed4a1",
          intel: "80761f9a1af8f2371fd4f3cfb84d39e9c38087d1398d5f875df2ca3a0784a44e"
 
-  url "https://s3.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}#{arch}.dmg",
-      verified: "s3.amazonaws.com/downloads.razorsql.com/downloads/"
+  url "https://s3.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}#{arch}.dmg"
   name "RazorSQL"
   desc "SQL query tool and SQL editor"
   homepage "https://razorsql.com/"
