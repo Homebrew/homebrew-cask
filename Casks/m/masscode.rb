ef79aff8d81d09c62380b3132dbe7c5736b7d604
botantony <antonsm21@gmail.com>
2026-08-31 00:50:20 +0200

    masscode: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/masscode.rb
+++ Casks/m/masscode.rb
@@ -5,8 +5,7 @@ cask "masscode" do
   sha256 arm:   "533a0621491646c849268ff319907e4a6ef884a8b9c775413162fbf9b93f7d64",
          intel: "22f4feb38c1a59f1928f244dacc38efe1f4b9462b1b71243b226f9a6f54bb388"
 
-  url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
-      verified: "github.com/massCodeIO/massCode/"
+  url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg"
   name "massCode"
   desc "Code snippets manager for developers"
   homepage "https://masscode.io/"
