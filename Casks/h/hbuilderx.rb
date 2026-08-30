0b9a99977748aeefbaba4ea758e0933b128afbb0
botantony <antonsm21@gmail.com>
2026-08-31 00:49:17 +0200

    hbuilderx: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/h/hbuilderx.rb
+++ Casks/h/hbuilderx.rb
@@ -5,8 +5,7 @@ cask "hbuilderx" do
   sha256 arm:   "3a1e0041551b25ddedcf00301ae0b00009167821749f953f917636b5f54e877b",
          intel: "1539b4aaeffe1cbddcfc827e7f4928bd65755a35df1bce540fc350e75f868eae"
 
-  url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}#{arch}.dmg",
-      verified: "download1.dcloud.net.cn/download/"
+  url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}#{arch}.dmg"
   name "HBuilderX"
   desc "HTML editor"
   homepage "https://www.dcloud.io/hbuilderx.html"
