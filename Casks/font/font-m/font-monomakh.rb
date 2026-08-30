268c5bcb52d2732f6dac860e4cd9d6ab79915c8a
botantony <antonsm21@gmail.com>
2026-08-31 00:44:50 +0200

    font-monomakh: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-m/font-monomakh.rb
+++ Casks/font/font-m/font-monomakh.rb
@@ -2,8 +2,7 @@ cask "font-monomakh" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/monomakh/Monomakh-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/monomakh/Monomakh-Regular.ttf"
   name "Monomakh"
   homepage "https://fonts.google.com/specimen/Monomakh"
 
