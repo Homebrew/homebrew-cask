35bac6cc87b9573d898cf7b8feb7c20219eb494c
botantony <antonsm21@gmail.com>
2026-08-31 00:52:39 +0200

    tad: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/tad.rb
+++ Casks/t/tad.rb
@@ -5,8 +5,7 @@ cask "tad" do
   sha256 arm:   "18004712674bbd7072137937f2ad02231f71b2b326d48079e72f9a536660d588",
          intel: "b942fc420a3b19240b52f798d29334bbd78f89ebbf7c7702962668eccc39011e"
 
-  url "https://github.com/antonycourtney/tad/releases/download/v#{version}/Tad-#{version}#{arch}.dmg",
-      verified: "github.com/antonycourtney/tad/"
+  url "https://github.com/antonycourtney/tad/releases/download/v#{version}/Tad-#{version}#{arch}.dmg"
   name "Tad"
   desc "Desktop application for viewing and analyzing tabular data"
   homepage "https://www.tadviewer.com/"
