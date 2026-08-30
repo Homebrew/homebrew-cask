03d7ace9b4d81d5526b0ec5426987d931042b920
botantony <antonsm21@gmail.com>
2026-08-31 00:37:50 +0200

    betterdiscord-installer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/betterdiscord-installer.rb
+++ Casks/b/betterdiscord-installer.rb
@@ -2,8 +2,7 @@ cask "betterdiscord-installer" do
   version "2.0.0"
   sha256 "7acd4d8d1408ec2100de7de2a2c19103874e127f9e995bcb46fa06dc809fc5fd"
 
-  url "https://github.com/BetterDiscord/Installer/releases/download/v#{version}/BetterDiscord-Installer-Mac.zip",
-      verified: "github.com/BetterDiscord/Installer/"
+  url "https://github.com/BetterDiscord/Installer/releases/download/v#{version}/BetterDiscord-Installer-Mac.zip"
   name "BetterDiscord"
   desc "Installer for BetterDiscord"
   homepage "https://betterdiscord.app/"
