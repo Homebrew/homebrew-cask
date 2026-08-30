2a832deddffdfd89f7ce8961479bf11b17026d5b
botantony <antonsm21@gmail.com>
2026-08-31 00:48:49 +0200

    font-yuji-hentaigana-akebono: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-y/font-yuji-hentaigana-akebono.rb
+++ Casks/font/font-y/font-yuji-hentaigana-akebono.rb
@@ -2,8 +2,7 @@ cask "font-yuji-hentaigana-akebono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakebono/YujiHentaiganaAkebono-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/yujihentaiganaakebono/YujiHentaiganaAkebono-Regular.ttf"
   name "Yuji Hentaigana Akebono"
   homepage "https://fonts.google.com/specimen/Yuji+Hentaigana+Akebono"
 
