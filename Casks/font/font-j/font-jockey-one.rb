ff72a24281ef761a74fa73f23d3448b378941257
botantony <antonsm21@gmail.com>
2026-08-31 00:43:31 +0200

    font-jockey-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-j/font-jockey-one.rb
+++ Casks/font/font-j/font-jockey-one.rb
@@ -2,8 +2,7 @@ cask "font-jockey-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/jockeyone/JockeyOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/jockeyone/JockeyOne-Regular.ttf"
   name "Jockey One"
   homepage "https://fonts.google.com/specimen/Jockey+One"
 
