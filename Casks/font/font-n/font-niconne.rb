b19d5d3d43038ece6c95ff0f7d806b0f7fcd85b1
botantony <antonsm21@gmail.com>
2026-08-31 00:45:08 +0200

    font-niconne: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-niconne.rb
+++ Casks/font/font-n/font-niconne.rb
@@ -2,8 +2,7 @@ cask "font-niconne" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/niconne/Niconne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/niconne/Niconne-Regular.ttf"
   name "Niconne"
   homepage "https://fonts.google.com/specimen/Niconne"
 
