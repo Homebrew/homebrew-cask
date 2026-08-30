481a0d05678512484c5293ed2b28cca635850eaa
botantony <antonsm21@gmail.com>
2026-08-31 00:47:24 +0200

    font-rock-3d: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-r/font-rock-3d.rb
+++ Casks/font/font-r/font-rock-3d.rb
@@ -2,8 +2,7 @@ cask "font-rock-3d" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/rock3d/Rock3D-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/rock3d/Rock3D-Regular.ttf"
   name "Rock 3D"
   homepage "https://fonts.google.com/specimen/Rock+3D"
 
