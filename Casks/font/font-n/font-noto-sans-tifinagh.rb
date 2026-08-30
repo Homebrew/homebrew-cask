5f8a2e6f9fcfb7cc17f3375f1688c2baf97f9630
botantony <antonsm21@gmail.com>
2026-08-31 00:45:56 +0200

    font-noto-sans-tifinagh: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-tifinagh.rb
+++ Casks/font/font-n/font-noto-sans-tifinagh.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-tifinagh" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanstifinagh/NotoSansTifinagh-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanstifinagh/NotoSansTifinagh-Regular.ttf"
   name "Noto Sans Tifinagh"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Tifinagh"
 
