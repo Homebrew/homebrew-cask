046cc62ed0e19d5dbe90d7d367bb186b416a138c
botantony <antonsm21@gmail.com>
2026-08-31 00:39:34 +0200

    executor: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/e/executor.rb
+++ Casks/e/executor.rb
@@ -5,8 +5,7 @@ cask "executor" do
   sha256 arm:   "3d3336c3cf1e7a49105a04ad2904dcde0f13aac8a3ce1ee268c5c718bb62572c",
          intel: "7a1edffe4385374f051b8cad2245049862bd25ddeb4a07f63b06788151472c5d"
 
-  url "https://github.com/RhysSullivan/executor/releases/download/v#{version}/executor-desktop-mac-#{arch}.dmg",
-      verified: "github.com/RhysSullivan/executor/"
+  url "https://github.com/RhysSullivan/executor/releases/download/v#{version}/executor-desktop-mac-#{arch}.dmg"
   name "Executor"
   desc "Tool discovery and execution layer for AI agents"
   homepage "https://executor.sh/"
