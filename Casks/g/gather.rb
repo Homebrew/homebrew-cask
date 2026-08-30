d8aad788d957b1d02e2b068dd8f34d5e8d52f35b
botantony <antonsm21@gmail.com>
2026-08-31 00:48:57 +0200

    gather: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gather.rb
+++ Casks/g/gather.rb
@@ -5,8 +5,7 @@ cask "gather" do
   sha256 arm:   "292a6f5d7f20a865218055fd3de75f2facc4ce07ca8a35854e348ec069b7cd2f",
          intel: "07995aa1b63b160cf60a473c504664becc13b8bca16a7276c089e8520cef931a"
 
-  url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
-      verified: "github.com/gathertown/gather-town-desktop-releases/"
+  url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip"
   name "Gather Town"
   desc "Virtual video-calling space"
   homepage "https://gather.town/"
