e59f7a763ebab96f341b3036800a528788293b6d
botantony <antonsm21@gmail.com>
2026-08-31 00:51:56 +0200

    robloxstudio: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/robloxstudio.rb
+++ Casks/r/robloxstudio.rb
@@ -5,8 +5,7 @@ cask "robloxstudio" do
   sha256 arm:   "c43c67711ea77d67a8ce5950ce8d08ec7bc49900a4788b89336bceda5a0dbbe3",
          intel: "c3ec7186d45eaa5ad2c75cc0f87d3207c5ca04911dbf1f4fc913654fc7184c63"
 
-  url "https://setup.rbxcdn.com/mac/#{arch}version-#{version.csv.second}-RobloxStudioApp.zip",
-      verified: "setup.rbxcdn.com/"
+  url "https://setup.rbxcdn.com/mac/#{arch}version-#{version.csv.second}-RobloxStudioApp.zip"
   name "Roblox Studio"
   desc "Roblox IDE to build your experiences"
   homepage "https://create.roblox.com/"
