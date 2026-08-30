b1b47b8e047247cddfd883d534017c04fc2ab4a1
botantony <antonsm21@gmail.com>
2026-08-31 00:44:06 +0200

    font-licorice: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-licorice.rb
+++ Casks/font/font-l/font-licorice.rb
@@ -2,8 +2,7 @@ cask "font-licorice" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/licorice/Licorice-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/licorice/Licorice-Regular.ttf"
   name "Licorice"
   homepage "https://fonts.google.com/specimen/Licorice"
 
