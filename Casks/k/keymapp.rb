67499c690b1c9f48a6b077763aebd25e934fc37b
botantony <antonsm21@gmail.com>
2026-08-31 00:49:46 +0200

    keymapp: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/keymapp.rb
+++ Casks/k/keymapp.rb
@@ -2,8 +2,7 @@ cask "keymapp" do
   version "1.3.7"
   sha256 :no_check
 
-  url "https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.dmg",
-      verified: "oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/"
+  url "https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.dmg"
   name "Keymapp"
   desc "ZSA keyboard firmware flasher"
   homepage "https://www.zsa.io/flash"
