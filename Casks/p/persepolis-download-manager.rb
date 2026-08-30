844aee8e76514473926a72889e965c26bcedb240
botantony <antonsm21@gmail.com>
2026-08-31 00:51:20 +0200

    persepolis-download-manager: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/persepolis-download-manager.rb
+++ Casks/p/persepolis-download-manager.rb
@@ -2,8 +2,7 @@ cask "persepolis-download-manager" do
   version "5.2.0"
   sha256 "548b4b07904f8ec57d7f5d746459bf32a1bd0e0ae710e58108a37e3f17043895"
 
-  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_macos.dmg",
-      verified: "github.com/persepolisdm/persepolis/"
+  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_macos.dmg"
   name "Persepolis"
   desc "Download manager"
   homepage "https://persepolisdm.github.io/"
