df2f86ff4b52e76623d4f08148d5bd0227061fc2
botantony <antonsm21@gmail.com>
2026-08-31 00:37:36 +0200

    aviatrix-vpn-client: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/aviatrix-vpn-client.rb
+++ Casks/a/aviatrix-vpn-client.rb
@@ -2,8 +2,7 @@ cask "aviatrix-vpn-client" do
   version "3.0.2"
   sha256 :no_check
 
-  url "https://aviatrix-download.s3.amazonaws.com/AviatrixVPNClient/AVPNC_mac.pkg",
-      verified: "aviatrix-download.s3.amazonaws.com/"
+  url "https://aviatrix-download.s3.amazonaws.com/AviatrixVPNClient/AVPNC_mac.pkg"
   name "Aviatrix VPN Client"
   desc "VPN client that provides SAML authentication"
   homepage "https://docs.aviatrix.com/docs/enterprise/latest/guides/uservpn/user-vpn-client-download"
