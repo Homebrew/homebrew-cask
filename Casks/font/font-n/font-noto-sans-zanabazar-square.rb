22e3e823b61508dd1bdfd863676c27457281ef3b
botantony <antonsm21@gmail.com>
2026-08-31 00:45:58 +0200

    font-noto-sans-zanabazar-square: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-zanabazar-square.rb
+++ Casks/font/font-n/font-noto-sans-zanabazar-square.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-zanabazar-square" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanszanabazarsquare/NotoSansZanabazarSquare-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanszanabazarsquare/NotoSansZanabazarSquare-Regular.ttf"
   name "Noto Sans Zanabazar Square"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Zanabazar+Square"
 
