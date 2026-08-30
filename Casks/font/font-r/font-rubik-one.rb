69feb605a2a165f0e1b8a0ec43c77e5ead5bff6c
botantony <antonsm21@gmail.com>
2026-08-31 00:47:32 +0200

    font-rubik-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-r/font-rubik-one.rb
+++ Casks/font/font-r/font-rubik-one.rb
@@ -2,8 +2,7 @@ cask "font-rubik-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/rubikone/RubikOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/rubikone/RubikOne-Regular.ttf"
   name "Rubik One"
   homepage "https://fonts.google.com/specimen/Rubik+One"
 
