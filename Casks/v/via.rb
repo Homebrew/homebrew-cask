680950975e98e79529689517b21952b02f575287
botantony <antonsm21@gmail.com>
2026-08-31 00:53:07 +0200

    via: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/via.rb
+++ Casks/v/via.rb
@@ -2,8 +2,7 @@ cask "via" do
   version "3.0.0"
   sha256 "30f9f81154a8ee9c0cf19f4fb1a3d6ca9a448f765122845db1e190b9f583d16b"
 
-  url "https://github.com/the-via/releases/releases/download/v#{version}/via-#{version}-mac.dmg",
-      verified: "github.com/the-via/releases/"
+  url "https://github.com/the-via/releases/releases/download/v#{version}/via-#{version}-mac.dmg"
   name "VIA"
   desc "Keyboard configurator"
   homepage "https://caniusevia.com/"
