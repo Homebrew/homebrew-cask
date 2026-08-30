1c412d6dd09fcc394f77150fcc92c9c664f7ea0b
botantony <antonsm21@gmail.com>
2026-08-31 00:41:22 +0200

    font-brawler: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-brawler.rb
+++ Casks/font/font-b/font-brawler.rb
@@ -3,7 +3,6 @@ cask "font-brawler" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/brawler"
   name "Brawler"
