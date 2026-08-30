8ece7b84779e323b009bbabb5d8959169a72481a
botantony <antonsm21@gmail.com>
2026-08-31 00:43:16 +0200

    font-im-fell-french-canon-sc: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-i/font-im-fell-french-canon-sc.rb
+++ Casks/font/font-i/font-im-fell-french-canon-sc.rb
@@ -2,8 +2,7 @@ cask "font-im-fell-french-canon-sc" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf"
   name "IM Fell French Canon SC"
   homepage "https://fonts.google.com/specimen/IM+Fell+French+Canon+SC"
 
