9388fa72fe096f83b452067c02d433362ae05b14
botantony <antonsm21@gmail.com>
2026-08-31 00:41:57 +0200

    font-croissant-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-croissant-one.rb
+++ Casks/font/font-c/font-croissant-one.rb
@@ -2,8 +2,7 @@ cask "font-croissant-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/croissantone/CroissantOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/croissantone/CroissantOne-Regular.ttf"
   name "Croissant One"
   homepage "https://fonts.google.com/specimen/Croissant+One"
 
