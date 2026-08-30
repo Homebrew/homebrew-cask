81683e753a654d9479b00fe218a2ca0f2a605127
botantony <antonsm21@gmail.com>
2026-08-31 00:51:09 +0200

    openshot-video-editor@daily: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/openshot-video-editor@daily.rb
+++ Casks/o/openshot-video-editor@daily.rb
@@ -2,8 +2,7 @@ cask "openshot-video-editor@daily" do
   version "4.0.0,16756-732f1829-56930c67,release-candidate"
   sha256 "bfad1a866265f05024ffee90e5a770d4e584424f2ee6952812c55fcd2133177a"
 
-  url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-#{version.csv.third || "daily"}-#{version.csv.second}-x86_64.dmg",
-      verified: "github.com/OpenShot/openshot-qt/"
+  url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-#{version.csv.third || "daily"}-#{version.csv.second}-x86_64.dmg"
   name "OpenShot Video Editor (Daily Build)"
   desc "Cross-platform video editor"
   homepage "https://openshot.org/"
