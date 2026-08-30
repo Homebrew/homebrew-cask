d7e63b0931b8280dd9b3099f510df970c4104072
botantony <antonsm21@gmail.com>
2026-08-31 00:52:52 +0200

    todometer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/todometer.rb
+++ Casks/t/todometer.rb
@@ -3,8 +3,7 @@ cask "todometer" do
     version "3.0.3"
     sha256 "862cdb44d5fc006d2c858add489c5c17796e4dbad7df154dd79f4c69ffd258da"
 
-    url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer-#{version}-arm64-mac.zip",
-        verified: "github.com/cassidoo/todometer/"
+    url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer-#{version}-arm64-mac.zip"
 
     depends_on macos: :monterey
 
@@ -14,8 +13,7 @@ cask "todometer" do
     version "2.0.1"
     sha256 "b8de5c09dc1d5d026130555f3cc85c949ddd7bd0cbf63a45bd1d4c2aba6cbf31"
 
-    url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer.for.mac.zip",
-        verified: "github.com/cassidoo/todometer/"
+    url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer.for.mac.zip"
 
     livecheck do
       skip "Legacy version"
