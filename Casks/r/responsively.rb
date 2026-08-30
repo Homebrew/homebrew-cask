27926d228d62e71f17f7a65801e3e1232cc9e47a
botantony <antonsm21@gmail.com>
2026-08-31 00:51:52 +0200

    responsively: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/responsively.rb
+++ Casks/r/responsively.rb
@@ -5,8 +5,7 @@ cask "responsively" do
   sha256 arm:   "870ef97ae7b23758411841f0c35058ddd36ba2dce51b1f8a48063664dc12d92b",
          intel: "580a2f680bf438456901d5664ddea08bc3291c3ce0ded67ce64234444c1c9f6b"
 
-  url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
-      verified: "github.com/responsively-org/responsively-app-releases/"
+  url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg"
   name "Responsively"
   desc "Modified browser that helps in responsive web development"
   homepage "https://responsively.app/"
