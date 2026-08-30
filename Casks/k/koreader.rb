1772fb39b1d479e6044d8e792e4a2b65b6a338d6
botantony <antonsm21@gmail.com>
2026-08-31 00:49:51 +0200

    koreader: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/koreader.rb
+++ Casks/k/koreader.rb
@@ -5,8 +5,7 @@ cask "koreader" do
   sha256 arm64_linux:  "fff2394e2d896f4df61501d89c1ae7f53903c34433b5e587551ed2279f90ab10",
          x86_64_linux: "e60ed106cf4e824d50394a259b3aa6c46fb1daee189df73a1734e5732b5dc245"
 
-  url "https://github.com/koreader/koreader/releases/download/v#{version}/koreader-v#{version}-#{arch}.AppImage",
-      verified: "github.com/koreader/koreader/"
+  url "https://github.com/koreader/koreader/releases/download/v#{version}/koreader-v#{version}-#{arch}.AppImage"
   name "KOReader"
   desc "Document viewer for e-ink devices"
   homepage "https://koreader.rocks/"
