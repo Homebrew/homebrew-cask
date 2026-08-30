37ccb590f70a0132b4ea32ce4c56e78f603be75d
botantony <antonsm21@gmail.com>
2026-08-31 00:41:44 +0200

    font-cinzel: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-cinzel.rb
+++ Casks/font/font-c/font-cinzel.rb
@@ -2,8 +2,7 @@ cask "font-cinzel" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/cinzel/Cinzel%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/cinzel/Cinzel%5Bwght%5D.ttf"
   name "Cinzel"
   homepage "https://fonts.google.com/specimen/Cinzel"
 
