9886a71439b576e077e585892091207ee319ff72
botantony <antonsm21@gmail.com>
2026-08-31 00:51:26 +0200

    plexamp: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/plexamp.rb
+++ Casks/p/plexamp.rb
@@ -5,8 +5,7 @@ cask "plexamp" do
   sha256 arm:   "96f0b8537797460153d7cebf8944ddcbde4161a845c34fc814ee647b986bab7c",
          intel: "7aff81136c661a8425e0c438662c4c390b16277ed381b8227c474e7983ea57ab"
 
-  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-#{arch}.dmg",
-      verified: "plexamp.plex.tv/"
+  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-#{arch}.dmg"
   name "Plexamp"
   desc "Music player focusing on visuals"
   homepage "https://plexamp.com/"
