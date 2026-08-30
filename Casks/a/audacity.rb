9ff10e588b790ebc6cdd1d671cfc0242c9c30570
botantony <antonsm21@gmail.com>
2026-08-31 00:37:34 +0200

    audacity: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/audacity.rb
+++ Casks/a/audacity.rb
@@ -5,8 +5,7 @@ cask "audacity" do
   sha256 arm:   "2888d2bef5321990d3a11507f9b5cf9461831725a50f391fffd558f7404ffcf8",
          intel: "6d79d5ec2c1bbb69421b3c2c8ff5f34b34be96997e4cdb6e0fde12948b9fc173"
 
-  url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macOS-#{version}-#{arch}.dmg",
-      verified: "github.com/audacity/audacity/"
+  url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macOS-#{version}-#{arch}.dmg"
   name "Audacity"
   desc "Multi-track audio editor and recorder"
   homepage "https://www.audacityteam.org/"
