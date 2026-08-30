f11fcd4af3f026ed6695a1eee294f84c8f3bc8e4
botantony <antonsm21@gmail.com>
2026-08-31 00:50:08 +0200

    luminance-hdr: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/luminance-hdr.rb
+++ Casks/l/luminance-hdr.rb
@@ -2,8 +2,7 @@ cask "luminance-hdr" do
   version "2.6.0"
   sha256 "14b201123036f826a3d4f2f2d5c00a7ef60938adf8f8cf35ca58f38ee8ef6642"
 
-  url "https://downloads.sourceforge.net/qtpfsgui/luminance/LuminanceHDR_#{version}-Qt5.13.dmg",
-      verified: "downloads.sourceforge.net/qtpfsgui/"
+  url "https://downloads.sourceforge.net/qtpfsgui/luminance/LuminanceHDR_#{version}-Qt5.13.dmg"
   name "Luminance HDR"
   desc "Provides a workflow for HDR imaging"
   homepage "https://qtpfsgui.sourceforge.io/"
