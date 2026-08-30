361fc688272e07be970a86dec86f4d5a4c27b290
botantony <antonsm21@gmail.com>
2026-08-31 00:45:06 +0200

    font-new-amsterdam: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-new-amsterdam.rb
+++ Casks/font/font-n/font-new-amsterdam.rb
@@ -2,8 +2,7 @@ cask "font-new-amsterdam" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/newamsterdam/NewAmsterdam-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/newamsterdam/NewAmsterdam-Regular.ttf"
   name "New Amsterdam"
   homepage "https://fonts.google.com/specimen/New+Amsterdam"
 
