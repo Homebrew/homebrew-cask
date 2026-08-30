457d089137f1cac756b6454309e906e96aec3086
botantony <antonsm21@gmail.com>
2026-08-31 00:46:20 +0200

    font-outfit: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-o/font-outfit.rb
+++ Casks/font/font-o/font-outfit.rb
@@ -2,8 +2,7 @@ cask "font-outfit" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/outfit/Outfit%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/outfit/Outfit%5Bwght%5D.ttf"
   name "Outfit"
   homepage "https://fonts.google.com/specimen/Outfit"
 
