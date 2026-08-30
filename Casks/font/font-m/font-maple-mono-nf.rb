81664136299bcb07c442f7c5b3a9519e58294458
botantony <antonsm21@gmail.com>
2026-08-31 00:44:24 +0200

    font-maple-mono-nf: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-m/font-maple-mono-nf.rb
+++ Casks/font/font-m/font-maple-mono-nf.rb
@@ -2,8 +2,7 @@ cask "font-maple-mono-nf" do
   version "7.9"
   sha256 "8248d6260660be1066b5ebe90d25131582df8383f38db1c284dd1f2dc9d2b46e"
 
-  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-NF-unhinted.zip",
-      verified: "github.com/subframe7536/Maple-font/"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-NF-unhinted.zip"
   name "Maple Mono NF"
   homepage "https://font.subf.dev/en/"
 
