49b0b1c6c8f7ed4e17d7863b5d5e62e9061e4947
botantony <antonsm21@gmail.com>
2026-08-31 00:48:11 +0200

    font-strichpunkt-sans: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-s/font-strichpunkt-sans.rb
+++ Casks/font/font-s/font-strichpunkt-sans.rb
@@ -2,8 +2,7 @@ cask "font-strichpunkt-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/strichpunktsans/StrichpunktSans%5Bwdth%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/strichpunktsans/StrichpunktSans%5Bwdth%2Cwght%5D.ttf"
   name "Strichpunkt Sans"
   homepage "https://fonts.google.com/specimen/Strichpunkt+Sans"
 
