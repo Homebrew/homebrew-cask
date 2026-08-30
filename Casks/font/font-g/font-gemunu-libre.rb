ee3e9d526abfbbb28810e221209472b319319bd4
botantony <antonsm21@gmail.com>
2026-08-31 00:42:45 +0200

    font-gemunu-libre: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-g/font-gemunu-libre.rb
+++ Casks/font/font-g/font-gemunu-libre.rb
@@ -2,8 +2,7 @@ cask "font-gemunu-libre" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gemunulibre/GemunuLibre%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gemunulibre/GemunuLibre%5Bwght%5D.ttf"
   name "Gemunu Libre"
   homepage "https://fonts.google.com/specimen/Gemunu+Libre"
 
