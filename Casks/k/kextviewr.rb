f2e3631a55f0d1fd8765fe99d19153d0a24bbd1e
botantony <antonsm21@gmail.com>
2026-08-31 00:49:45 +0200

    kextviewr: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/kextviewr.rb
+++ Casks/k/kextviewr.rb
@@ -2,8 +2,7 @@ cask "kextviewr" do
   version "2.0.0"
   sha256 "cd92141750812797cffd626f697c133fac84b615b5d4cd5dae56a8160320968e"
 
-  url "https://github.com/objective-see/KextViewr/releases/download/v#{version}/KextViewr_#{version}.zip",
-      verified: "github.com/objective-see/"
+  url "https://github.com/objective-see/KextViewr/releases/download/v#{version}/KextViewr_#{version}.zip"
   name "KextViewr"
   desc "Display all currently loaded kexts"
   homepage "https://objective-see.org/products/kextviewr.html"
