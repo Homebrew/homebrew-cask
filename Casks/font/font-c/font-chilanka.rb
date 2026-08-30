c371eede03f3604003d5c1d09ccbfa5fe250cca3
botantony <antonsm21@gmail.com>
2026-08-31 00:41:42 +0200

    font-chilanka: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-chilanka.rb
+++ Casks/font/font-c/font-chilanka.rb
@@ -2,8 +2,7 @@ cask "font-chilanka" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/chilanka/Chilanka-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/chilanka/Chilanka-Regular.ttf"
   name "Chilanka"
   homepage "https://fonts.google.com/specimen/Chilanka"
 
