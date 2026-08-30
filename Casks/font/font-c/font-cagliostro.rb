f5931c44ac7005a0f840083544a8250529f45ac6
botantony <antonsm21@gmail.com>
2026-08-31 00:41:30 +0200

    font-cagliostro: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-cagliostro.rb
+++ Casks/font/font-c/font-cagliostro.rb
@@ -2,8 +2,7 @@ cask "font-cagliostro" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/cagliostro/Cagliostro-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/cagliostro/Cagliostro-Regular.ttf"
   name "Cagliostro"
   homepage "https://fonts.google.com/specimen/Cagliostro"
 
