b9d7ff2a1e3d103459f8e06abbf962c08fb1d05f
botantony <antonsm21@gmail.com>
2026-08-31 00:53:27 +0200

    writemapper: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/w/writemapper.rb
+++ Casks/w/writemapper.rb
@@ -2,8 +2,7 @@ cask "writemapper" do
   version "4.6.0"
   sha256 "b5feebe6de35bf0cf1e6c1165278038ef2cdc71e4cb046b4772ad141658396b9"
 
-  url "https://writemapper.sfo3.cdn.digitaloceanspaces.com/writemapper-#{version.major}/mac/WriteMapper-#{version}-universal.dmg",
-      verified: "writemapper.sfo3.cdn.digitaloceanspaces.com/"
+  url "https://writemapper.sfo3.cdn.digitaloceanspaces.com/writemapper-#{version.major}/mac/WriteMapper-#{version}-universal.dmg"
   name "WriteMapper"
   desc "Writing tool that helps produce text documents using mind maps"
   homepage "https://writemapper.com/"
