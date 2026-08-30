eba6111660879003e7e0f8e098d938f1b922934b
botantony <antonsm21@gmail.com>
2026-08-31 00:49:22 +0200

    hydrogen: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/h/hydrogen.rb
+++ Casks/h/hydrogen.rb
@@ -2,8 +2,7 @@ cask "hydrogen" do
   version "1.2.6"
   sha256 "05ba60a4c71745c591b22f472b80c79fdcdd0ef38bb3ef0b08b313d722e76811"
 
-  url "https://github.com/hydrogen-music/hydrogen/releases/download/#{version}/Hydrogen-#{version}.dmg",
-      verified: "github.com/hydrogen-music/hydrogen/"
+  url "https://github.com/hydrogen-music/hydrogen/releases/download/#{version}/Hydrogen-#{version}.dmg"
   name "Hydrogen"
   desc "Drum machine and sequencer"
   homepage "http://www.hydrogen-music.org/"
