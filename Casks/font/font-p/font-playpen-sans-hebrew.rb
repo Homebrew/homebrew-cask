d10a0642d3aa1f7ef715a334a2529c9e8e535e28
botantony <antonsm21@gmail.com>
2026-08-31 00:46:34 +0200

    font-playpen-sans-hebrew: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playpen-sans-hebrew.rb
+++ Casks/font/font-p/font-playpen-sans-hebrew.rb
@@ -2,8 +2,7 @@ cask "font-playpen-sans-hebrew" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playpensanshebrew/PlaypenSansHebrew%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playpensanshebrew/PlaypenSansHebrew%5Bwght%5D.ttf"
   name "Playpen Sans Hebrew"
   homepage "https://fonts.google.com/specimen/Playpen+Sans+Hebrew"
 
