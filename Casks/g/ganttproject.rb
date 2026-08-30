5b5100111f853a1da9759387c38d222e29e4242a
botantony <antonsm21@gmail.com>
2026-08-31 00:48:57 +0200

    ganttproject: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/ganttproject.rb
+++ Casks/g/ganttproject.rb
@@ -5,8 +5,7 @@ cask "ganttproject" do
   sha256 arm:   "fd5f8941b73bec53f1f08ef5696480a22f872fc2a098b6aef2defa99817bce25",
          intel: "0bb0e3af5b1e3627ea8cc5f4c33ec84d6829aa682b118369039fbaf95ab4f096"
 
-  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}-#{arch}.dmg",
-      verified: "github.com/bardsoftware/ganttproject/"
+  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}-#{arch}.dmg"
   name "GanttProject"
   desc "Gantt chart and project management application"
   homepage "https://www.ganttproject.biz/"
