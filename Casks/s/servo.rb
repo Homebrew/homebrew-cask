3ff8d45aa1a4108f2d41288d6273b4d60d42d4b6
botantony <antonsm21@gmail.com>
2026-08-31 00:52:08 +0200

    servo: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/servo.rb
+++ Casks/s/servo.rb
@@ -5,8 +5,7 @@ cask "servo" do
   sha256 arm:   "306c27f20bb3f3833a97070baae087b77c88ad5318b8975531e4ee7c576f32f0",
          intel: "ed721a34485bd59171d8c62c959cb485bc7a7c627e348da32f860c0d8b311aa7"
 
-  url "https://github.com/servo/servo-nightly-builds/releases/download/#{version}/servo-#{arch}-apple-darwin.dmg",
-      verified: "github.com/servo/servo-nightly-builds/"
+  url "https://github.com/servo/servo-nightly-builds/releases/download/#{version}/servo-#{arch}-apple-darwin.dmg"
   name "Servo"
   desc "Parallel browser engine"
   homepage "https://servo.org/"
