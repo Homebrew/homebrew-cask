d5952423436665e8e96048dbeb2d30ac7e1f2858
botantony <antonsm21@gmail.com>
2026-08-31 00:49:15 +0200

    happ: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/h/happ.rb
+++ Casks/h/happ.rb
@@ -2,8 +2,7 @@ cask "happ" do
   version "4.1.2"
   sha256 "30532cd12f0bd32cc840f95ab5d7ed88545486d0ced84c784825ec264dab37aa"
 
-  url "https://github.com/Happ-proxy/happ-desktop/releases/download/#{version}/Happ.macOS.universal.dmg",
-      verified: "github.com/Happ-proxy/happ-desktop/"
+  url "https://github.com/Happ-proxy/happ-desktop/releases/download/#{version}/Happ.macOS.universal.dmg"
   name "Happ"
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://www.happ.su/main/"
