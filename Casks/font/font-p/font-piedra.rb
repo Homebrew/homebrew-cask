9df1e6e7a99534ea973ee6de1f3d6a65b72abe68
botantony <antonsm21@gmail.com>
2026-08-31 00:46:31 +0200

    font-piedra: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-piedra.rb
+++ Casks/font/font-p/font-piedra.rb
@@ -2,8 +2,7 @@ cask "font-piedra" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/piedra/Piedra-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/piedra/Piedra-Regular.ttf"
   name "Piedra"
   homepage "https://fonts.google.com/specimen/Piedra"
 
