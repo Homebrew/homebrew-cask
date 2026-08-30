f4049f956da08dfff20504611b0b06e31212cbbe
botantony <antonsm21@gmail.com>
2026-08-31 00:37:28 +0200

    archiver-app: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/archiver-app.rb
+++ Casks/a/archiver-app.rb
@@ -2,8 +2,7 @@ cask "archiver-app" do
   version "5.0.11"
   sha256 "8cf64162fb96598182c08a63142cf842ac9745e2a6c511f9b7cd0ec1ab673be1"
 
-  url "https://github.com/incbee/archiver-#{version.major}-releases/releases/download/v#{version}/Archiver-#{version}-universal-mac.zip",
-      verified: "github.com/incbee/archiver-#{version.major}-releases/"
+  url "https://github.com/incbee/archiver-#{version.major}-releases/releases/download/v#{version}/Archiver-#{version}-universal-mac.zip"
   name "Archiver"
   desc "Open archives, compress files, as well as split and combine files"
   homepage "https://archiverapp.com/"
