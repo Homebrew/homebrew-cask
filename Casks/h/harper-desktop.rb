ed41065c15b50321d194f7b6c00b27d3d9c891d1
botantony <antonsm21@gmail.com>
2026-08-31 00:49:16 +0200

    harper-desktop: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/h/harper-desktop.rb
+++ Casks/h/harper-desktop.rb
@@ -2,8 +2,7 @@ cask "harper-desktop" do
   version "2.9.1"
   sha256 "e00e501ae0840bba015bd4e491ba8a2a843c3236eb94558508dc67d68d8a143b"
 
-  url "https://github.com/Automattic/harper/releases/download/v#{version}/Harper_#{version}_universal.dmg",
-      verified: "github.com/Automattic/harper/"
+  url "https://github.com/Automattic/harper/releases/download/v#{version}/Harper_#{version}_universal.dmg"
   name "Harper Desktop"
   desc "Grammar checker for developers"
   homepage "https://writewithharper.com/"
