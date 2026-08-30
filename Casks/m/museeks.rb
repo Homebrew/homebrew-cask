3c4ace1e138975d51d154f9b4bc137e116672ba9
botantony <antonsm21@gmail.com>
2026-08-31 00:50:43 +0200

    museeks: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/museeks.rb
+++ Casks/m/museeks.rb
@@ -2,8 +2,7 @@ cask "museeks" do
   version "0.23.4"
   sha256 "ff8b3053fefdabb99c54868e74b4857087ac501bf91a026df60776bded2befb7"
 
-  url "https://github.com/martpie/museeks/releases/download/#{version}/Museeks_#{version}_universal.dmg",
-      verified: "github.com/martpie/museeks/"
+  url "https://github.com/martpie/museeks/releases/download/#{version}/Museeks_#{version}_universal.dmg"
   name "Museeks"
   desc "Music player"
   homepage "https://museeks.io/"
