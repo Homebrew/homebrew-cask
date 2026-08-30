4edd35e679468efb7e442be6ddb4e7461100985e
botantony <antonsm21@gmail.com>
2026-08-31 00:50:08 +0200

    lumide: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/lumide.rb
+++ Casks/l/lumide.rb
@@ -2,8 +2,7 @@ cask "lumide" do
   version "0.20.0"
   sha256 "44b1e435203140723355b9758614c2e7d3eb3f2bc3df3167fafc3b3117349402"
 
-  url "https://github.com/SoFluffyOS/lumide/releases/download/#{version}/Lumide-macOS-#{version}.dmg",
-      verified: "github.com/SoFluffyOS/lumide/"
+  url "https://github.com/SoFluffyOS/lumide/releases/download/#{version}/Lumide-macOS-#{version}.dmg"
   name "Lumide"
   desc "Agent-native code editor"
   homepage "https://lumide.dev/"
