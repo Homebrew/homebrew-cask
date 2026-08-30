f6899cbc372b9efff674f7ba127323365e3364e6
botantony <antonsm21@gmail.com>
2026-08-31 00:39:48 +0200

    floorp: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/floorp.rb
+++ Casks/f/floorp.rb
@@ -2,8 +2,7 @@ cask "floorp" do
   version "12.17.1"
   sha256 "95098ffa1b52bfba189ed61fe12b08f6bb66df3c76f3e03477e9d0b028f18162"
 
-  url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-universal.dmg",
-      verified: "github.com/Floorp-Projects/Floorp/"
+  url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-universal.dmg"
   name "Floorp browser"
   desc "Privacy-focused Firefox-based browser"
   homepage "https://floorp.app/"
