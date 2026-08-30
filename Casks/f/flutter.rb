ce9fe8f7afa8c61d3891de97df844c9fe6178afd
botantony <antonsm21@gmail.com>
2026-08-31 00:39:50 +0200

    flutter: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/flutter.rb
+++ Casks/f/flutter.rb
@@ -5,8 +5,7 @@ cask "flutter" do
   sha256 arm:   "f456fd6733053d9301828a2e702d6cbec872923126809aa8c48eb0a696d6cc01",
          intel: "b6fd6ba98c8503d5ee06a6670627b5b1c36167ece3427435ec83b66e9b28c6b5"
 
-  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
-      verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
+  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip"
   name "Flutter SDK"
   desc "UI toolkit for building applications for mobile, web and desktop"
   homepage "https://flutter.dev/"
