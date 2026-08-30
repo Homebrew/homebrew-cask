0eb3be26cbbae33bcf06e58d63ebc7451e6457b2
botantony <antonsm21@gmail.com>
2026-08-31 00:43:01 +0200

    font-gurajada: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-g/font-gurajada.rb
+++ Casks/font/font-g/font-gurajada.rb
@@ -2,8 +2,7 @@ cask "font-gurajada" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gurajada/Gurajada-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gurajada/Gurajada-Regular.ttf"
   name "Gurajada"
   homepage "https://fonts.google.com/specimen/Gurajada"
 
