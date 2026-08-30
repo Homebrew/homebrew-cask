a0a7a3cb1b8e497802e390630bc8cd36124c5c40
botantony <antonsm21@gmail.com>
2026-08-31 00:51:15 +0200

    packetsender: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/packetsender.rb
+++ Casks/p/packetsender.rb
@@ -2,8 +2,7 @@ cask "packetsender" do
   version "8.11.1"
   sha256 "f573d24fa1245e5572bc578d4fa9b1865dccd802e8fe333b7dc0512eb5bb5a30"
 
-  url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg",
-      verified: "github.com/dannagle/PacketSender/"
+  url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
   name "Packet Sender"
   desc "Network utility for sending / receiving TCP, UDP, SSL"
   homepage "https://packetsender.com/"
