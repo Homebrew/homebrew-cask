9c4e9062ab6996f9eb3b7c66739f80ca605c3dda
botantony <antonsm21@gmail.com>
2026-08-31 00:52:23 +0200

    spatterlight: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/spatterlight.rb
+++ Casks/s/spatterlight.rb
@@ -2,8 +2,7 @@ cask "spatterlight" do
   version "1.5.4"
   sha256 "8b35128d471df4255df23300f025d5ab3c29d0430dd247b3d53af74846cb52a0"
 
-  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight.zip",
-      verified: "github.com/angstsmurf/spatterlight/"
+  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight.zip"
   name "Spatterlight"
   desc "Play most kinds of interactive fiction game files"
   homepage "https://ccxvii.net/spatterlight/"
