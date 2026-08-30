34b04de6d0e88fb43985dedc6527e0642d1dba63
botantony <antonsm21@gmail.com>
2026-08-31 00:50:59 +0200

    october: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/october.rb
+++ Casks/o/october.rb
@@ -5,8 +5,7 @@ cask "october" do
   sha256 arm:   "abc5ae23a95455da184f4789d26e8624b798225d54508b37236bcd21ae7ee342",
          intel: "5b1ee9a8b5537d091f911515806cca3eec3f3200802013791a1086fa9acd6a9e"
 
-  url "https://github.com/marcus-crane/october/releases/download/v#{version}/october_#{version}_darwin_#{arch}.zip",
-      verified: "github.com/marcus-crane/october/"
+  url "https://github.com/marcus-crane/october/releases/download/v#{version}/october_#{version}_darwin_#{arch}.zip"
   name "October"
   desc "GUI for retrieving Kobo highlights and syncing them with Readwise"
   homepage "https://october.utf9k.net/"
