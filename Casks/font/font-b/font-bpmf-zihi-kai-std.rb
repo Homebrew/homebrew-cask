06b229a3fbd77c86250b012b6ce300bf280fd6f9
botantony <antonsm21@gmail.com>
2026-08-31 00:41:22 +0200

    font-bpmf-zihi-kai-std: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-bpmf-zihi-kai-std.rb
+++ Casks/font/font-b/font-bpmf-zihi-kai-std.rb
@@ -2,8 +2,7 @@ cask "font-bpmf-zihi-kai-std" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/bpmfzihikaistd/BpmfZihiKaiStd-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/bpmfzihikaistd/BpmfZihiKaiStd-Regular.ttf"
   name "Bpmf Zihi Kai Std"
   homepage "https://fonts.google.com/specimen/Bpmf+Zihi+Kai+Std"
 
