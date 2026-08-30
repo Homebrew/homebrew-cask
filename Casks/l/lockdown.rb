8d45fe481040a9d3ed8503ce75d8dfe93a5403eb
botantony <antonsm21@gmail.com>
2026-08-31 00:50:04 +0200

    lockdown: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/lockdown.rb
+++ Casks/l/lockdown.rb
@@ -2,8 +2,7 @@ cask "lockdown" do
   version "1.0.0"
   sha256 "047f377e2a9495361084268c86cc80719e123bd8958d69fe51cd2be0d7ffd764"
 
-  url "https://bitbucket.org/objective-see/deploy/downloads/Lockdown_#{version}.zip",
-      verified: "bitbucket.org/objective-see/"
+  url "https://bitbucket.org/objective-see/deploy/downloads/Lockdown_#{version}.zip"
   name "Lockdown"
   desc "Audits and remediates security configuration settings"
   homepage "https://objective-see.org/products/lockdown.html"
