edfd64b8abc4818a3205f210272ee0e71de9fd90
botantony <antonsm21@gmail.com>
2026-08-31 00:52:33 +0200

    superhuman: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/superhuman.rb
+++ Casks/s/superhuman.rb
@@ -5,8 +5,7 @@ cask "superhuman" do
   sha256 arm:   "db931f80e2e1e4707425eacbea486835a1d828d090b605a8d423d468008da021",
          intel: "a833af41e019cbdb9ccd1d49e3ac92760951a58a43c212dbd9fdfcf6c7b9b6de"
 
-  url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
-      verified: "storage.googleapis.com/download.superhuman.com/supertron-update/"
+  url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip"
   name "Superhuman"
   desc "Email client"
   homepage "https://superhuman.com/"
