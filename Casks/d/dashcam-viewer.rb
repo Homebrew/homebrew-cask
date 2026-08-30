4bef8c3fea352b0fbe6efce502beb05bff990fdc
botantony <antonsm21@gmail.com>
2026-08-31 00:38:52 +0200

    dashcam-viewer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/d/dashcam-viewer.rb
+++ Casks/d/dashcam-viewer.rb
@@ -5,8 +5,7 @@ cask "dashcam-viewer" do
   sha256 arm:   "4eb8ecd1f4f57700b94232025d38a6cbb978ad05cb99a3a0e3edff3b2d02a7a1",
          intel: "a0b9d103030178ffce30c3946f317b48fa8f970883887466b933080cd0ab63cb"
 
-  url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}#{arch}.dmg",
-      verified: "filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/"
+  url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}#{arch}.dmg"
   name "Dashcam Viewer"
   name "Dashcam Viewer by Earthshine Software"
   desc "View videos, GPS data, and G-force data recorded by dashcams and action cams"
