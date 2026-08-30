4a3bdb38141afb11f42d6d9b5a00f1c370473367
botantony <antonsm21@gmail.com>
2026-08-31 00:53:02 +0200

    unity-webgl-support-for-editor: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/unity-webgl-support-for-editor.rb
+++ Casks/u/unity-webgl-support-for-editor.rb
@@ -2,8 +2,7 @@ cask "unity-webgl-support-for-editor" do
   version "6000.5.10f1,3bd4f66ad299"
   sha256 "a5c9ded3e1ed82a3d34a62543035b3fb0b7ee17b40a38cbece03a115daf317f3"
 
-  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.csv.first}.pkg",
-      verified: "download.unity3d.com/download_unity/"
+  url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.csv.first}.pkg"
   name "Unity WebGL Build Support"
   desc "WebGL target support for Unity"
   homepage "https://unity.com/products"
