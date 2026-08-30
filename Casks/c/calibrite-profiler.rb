7d0381fcde6e00aa2934804dd82154c00843ebbd
botantony <antonsm21@gmail.com>
2026-08-31 00:38:13 +0200

    calibrite-profiler: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/calibrite-profiler.rb
+++ Casks/c/calibrite-profiler.rb
@@ -2,8 +2,7 @@ cask "calibrite-profiler" do
   version "3.1.0"
   sha256 "a35392f3d43562ab03f2e0a20f8befe6d4329fd2c8fb4d27279efda92125db60"
 
-  url "https://github.com/LUMESCA/calibrite-profiler-releases/releases/download/v#{version}/calibrite-PROFILER-#{version}.dmg",
-      verified: "github.com/LUMESCA/calibrite-profiler-releases/"
+  url "https://github.com/LUMESCA/calibrite-profiler-releases/releases/download/v#{version}/calibrite-PROFILER-#{version}.dmg"
   name "calibrite PROFILER"
   desc "Display calibration software for Calibrite, ColorChecker and X-Rite devices"
   homepage "https://calibrite.com/calibrite-profiler/"
