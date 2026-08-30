bb747d24090ae54a2bee56073af82ac97f1c1d6d
botantony <antonsm21@gmail.com>
2026-08-31 00:44:06 +0200

    font-lilita-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-lilita-one.rb
+++ Casks/font/font-l/font-lilita-one.rb
@@ -2,8 +2,7 @@ cask "font-lilita-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lilitaone/LilitaOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/lilitaone/LilitaOne-Regular.ttf"
   name "Lilita One"
   homepage "https://fonts.google.com/specimen/Lilita+One"
 
