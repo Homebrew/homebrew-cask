3b9b53128cd185da8ba8ffb2054b726fe8317d36
botantony <antonsm21@gmail.com>
2026-08-31 00:49:35 +0200

    jamulus: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/j/jamulus.rb
+++ Casks/j/jamulus.rb
@@ -2,8 +2,7 @@ cask "jamulus" do
   version "3.11.0"
   sha256 "4cf680b643dd1bbb2f52655f5fef71ce222024d3c19713997fbe2a3626cd7d6a"
 
-  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg",
-      verified: "downloads.sourceforge.net/llcon/"
+  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg"
   name "Jamulus"
   desc "Play music online with friends"
   homepage "https://jamulus.io/"
