36fde8e7275ea26c9f9f59452c0d022aae240356
botantony <antonsm21@gmail.com>
2026-08-31 00:40:55 +0200

    font-barlow-semi-condensed: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-barlow-semi-condensed.rb
+++ Casks/font/font-b/font-barlow-semi-condensed.rb
@@ -3,7 +3,6 @@ cask "font-barlow-semi-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/barlowsemicondensed"
   name "Barlow Semi Condensed"
