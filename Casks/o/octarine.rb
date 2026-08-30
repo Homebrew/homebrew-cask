7679a7b30c98000cc56711b3b24def2b74099ca4
botantony <antonsm21@gmail.com>
2026-08-31 00:50:59 +0200

    octarine: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/octarine.rb
+++ Casks/o/octarine.rb
@@ -6,8 +6,7 @@ cask "octarine" do
   sha256 arm:   "8c14de111a6e438ae42ccca6ecafdb369825b66a51bbe243b5a85d11b6d1dd08",
          intel: "a4ea7cb45db7edda3df8fd4adc3b29bb8d8966139eba7410e3908f8402968ca5"
 
-  url "https://pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/#{version}/#{folder}/octarine_#{version}_#{arch}.dmg",
-      verified: "pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/"
+  url "https://pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/#{version}/#{folder}/octarine_#{version}_#{arch}.dmg"
   name "Octarine"
   desc "Markdown-based note-taking app"
   homepage "https://octarine.app/"
