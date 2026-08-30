fd20d2e56618d109008b4e50624fcd4829eb0a11
botantony <antonsm21@gmail.com>
2026-08-31 00:38:13 +0200

    camunda-modeler: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/camunda-modeler.rb
+++ Casks/c/camunda-modeler.rb
@@ -5,8 +5,7 @@ cask "camunda-modeler" do
   sha256 arm:   "aa6c69b1b5f562b1119da065cf65090c0af8de0eaab3f4f57f2cc3612d72a679",
          intel: "ddf046f4dd06c7c49c3b7d579019097ce18e3a2b47a736b48c55c496f99cf9ce"
 
-  url "https://downloads.camunda.cloud/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac-#{arch}.dmg",
-      verified: "downloads.camunda.cloud/release/camunda-modeler/"
+  url "https://downloads.camunda.cloud/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac-#{arch}.dmg"
   name "Camunda Modeler"
   desc "Workflow and Decision Automation Platform"
   homepage "https://camunda.com/"
