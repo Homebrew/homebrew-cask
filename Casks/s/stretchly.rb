6d15804ca56f9e217d1d0961d5884b2f42da1ff1
botantony <antonsm21@gmail.com>
2026-08-31 00:52:31 +0200

    stretchly: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/stretchly.rb
+++ Casks/s/stretchly.rb
@@ -5,8 +5,7 @@ cask "stretchly" do
   sha256 arm:   "ca307fe7f8d42776152bd6da39a3897061ece162cd83a2012923b827ef79f30b",
          intel: "84e1d3c1d163e849d46a390f8201fef55808eb674da43578d781661f7a4db88a"
 
-  url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}#{arch}.dmg",
-      verified: "github.com/hovancik/stretchly/"
+  url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}#{arch}.dmg"
   name "Stretchly"
   desc "Break time reminder app"
   homepage "https://hovancik.net/stretchly/"
