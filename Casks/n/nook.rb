da8376f83916e39c2f0fd4c90596110aaf8c40ae
botantony <antonsm21@gmail.com>
2026-08-31 00:50:54 +0200

    nook: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/n/nook.rb
+++ Casks/n/nook.rb
@@ -2,8 +2,7 @@ cask "nook" do
   version "1.0.7"
   sha256 "b6f9e47ddadcffe54e49fb3d4af86833c6ac478071b12c7b1f411bb56c6de914"
 
-  url "https://github.com/nook-browser/Nook/releases/download/v#{version}/Nook-v#{version}.dmg",
-      verified: "github.com/nook-browser/Nook/"
+  url "https://github.com/nook-browser/Nook/releases/download/v#{version}/Nook-v#{version}.dmg"
   name "Nook"
   desc "Minimal browser with a sidebar-first design"
   homepage "https://browsewithnook.com/"
