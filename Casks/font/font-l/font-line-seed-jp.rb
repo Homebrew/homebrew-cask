3dcabd376cd2efb824d7bb2c1bce5d9c55dd3a19
botantony <antonsm21@gmail.com>
2026-08-31 00:44:07 +0200

    font-line-seed-jp: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-line-seed-jp.rb
+++ Casks/font/font-l/font-line-seed-jp.rb
@@ -3,7 +3,6 @@ cask "font-line-seed-jp" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/lineseedjp"
   name "LINE Seed JP"
