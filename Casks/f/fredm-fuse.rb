b89208b8b6accfa8febe4dcee2cdc24c2aca81c5
botantony <antonsm21@gmail.com>
2026-08-31 00:39:57 +0200

    fredm-fuse: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/fredm-fuse.rb
+++ Casks/f/fredm-fuse.rb
@@ -2,8 +2,7 @@ cask "fredm-fuse" do
   version "1.9.1"
   sha256 "5102f43a447bb805092ae80741b79b6c07a96e1076bea07430905ccfd880325e"
 
-  url "https://downloads.sourceforge.net/fuse-for-macosx/fuse-for-macosx/#{version}/FuseForMacOS-#{version}.zip",
-      verified: "downloads.sourceforge.net/fuse-for-macosx/"
+  url "https://downloads.sourceforge.net/fuse-for-macosx/fuse-for-macosx/#{version}/FuseForMacOS-#{version}.zip"
   name "Fuse for Mac OS X"
   desc "Port of the UNIX ZX Spectrum emulator Fuse"
   homepage "https://fuse-for-macosx.sourceforge.io/"
