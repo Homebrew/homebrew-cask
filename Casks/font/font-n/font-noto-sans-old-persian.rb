de971a215cb43853f4876638c07c9b4e37f65108
botantony <antonsm21@gmail.com>
2026-08-31 00:45:42 +0200

    font-noto-sans-old-persian: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-old-persian.rb
+++ Casks/font/font-n/font-noto-sans-old-persian.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-old-persian" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansoldpersian/NotoSansOldPersian-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansoldpersian/NotoSansOldPersian-Regular.ttf"
   name "Noto Sans Old Persian"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Old+Persian"
 
