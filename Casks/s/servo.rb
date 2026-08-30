137786f485d163eb611a407059e6b73fdd03fdb7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 02:05:34 +0000

    servo 2026-08-29

--- Casks/s/servo.rb
+++ Casks/s/servo.rb
@@ -1,9 +1,9 @@
 cask "servo" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2026-08-28"
-  sha256 arm:   "bf1683b636e2ebccbc3de682887defa2e4dfafa6016e91a67cde9044d1ae8368",
-         intel: "cc2fbf3e84ae45e309013f9572d8f40003cae2713e57297bdce99a2de003e37f"
+  version "2026-08-29"
+  sha256 arm:   "fbe40230f969d667d5916b826cd6f8f71b42dd46bf2e2025fd9fba247bc4ce79",
+         intel: "2f1fa17818c42645fc4457fe297a6e5b3165568c81ed646a05860dbd8ef9654b"
 
   url "https://github.com/servo/servo-nightly-builds/releases/download/#{version}/servo-#{arch}-apple-darwin.dmg",
       verified: "github.com/servo/servo-nightly-builds/"
