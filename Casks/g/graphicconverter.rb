07f5e4a516087d584b5265f38ed1cf0939f6d1f8
botantony <antonsm21@gmail.com>
2026-08-31 00:49:10 +0200

    graphicconverter: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/graphicconverter.rb
+++ Casks/g/graphicconverter.rb
@@ -2,8 +2,7 @@ cask "graphicconverter" do
   version "12.5.4,7351"
   sha256 "594ae6c5ea30b425796991f91e09b38183c7b6dcbf5ece97168dfe70df1dd2d1"
 
-  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.csv.second}.zip",
-      verified: "lemkesoft.info/"
+  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.csv.second}.zip"
   name "GraphicConverter"
   desc "For browsing, enhancing and converting images"
   homepage "https://www.lemkesoft.de/en/products/graphicconverter/"
