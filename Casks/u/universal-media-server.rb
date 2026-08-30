fcb3383428b0f22a69f620ded24a805fabd7cb3f
botantony <antonsm21@gmail.com>
2026-08-31 00:53:03 +0200

    universal-media-server: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/universal-media-server.rb
+++ Casks/u/universal-media-server.rb
@@ -5,8 +5,7 @@ cask "universal-media-server" do
   sha256 arm:   "6575816f1e57c69436f7932773d46dfa8210d7e853e4ba1e25b87bf3cf222ebc",
          intel: "832a986f092d1cdd821a9433c9cee592716e182d26439cb8ce0be7fdb3957e8c"
 
-  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-macOS-#{version}-#{arch}.dmg",
-      verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
+  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-macOS-#{version}-#{arch}.dmg"
   name "Universal Media Server"
   desc "Media server supporting DLNA, UPnP and HTTP(S)"
   homepage "https://www.universalmediaserver.com/"
