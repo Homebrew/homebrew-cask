718420b7ae054153ad554e9eb4b69aa864e79355
botantony <antonsm21@gmail.com>
2026-08-31 00:49:47 +0200

    keystore-explorer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/keystore-explorer.rb
+++ Casks/k/keystore-explorer.rb
@@ -5,8 +5,7 @@ cask "keystore-explorer" do
   sha256 arm:   "59ffae1f70f3e4a2eb6d25b4169ee8b7b528d5638dec27b2b60a3287b61f0499",
          intel: "7e8a6ba4a824464494f9e2a1bc26de34dd64b71dd1a0147da6b123540bada25b"
 
-  url "https://github.com/kaikramer/keystore-explorer/releases/download/v#{version}/kse-#{version.no_dots}-#{arch}.dmg",
-      verified: "github.com/kaikramer/keystore-explorer/"
+  url "https://github.com/kaikramer/keystore-explorer/releases/download/v#{version}/kse-#{version.no_dots}-#{arch}.dmg"
   name "KeyStore Explorer"
   desc "GUI replacement for the Java command-line utilities keytool and jarsigner"
   homepage "https://keystore-explorer.org/"
