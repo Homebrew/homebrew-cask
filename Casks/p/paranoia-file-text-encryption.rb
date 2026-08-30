a71c9860c343f4dc1014bf4513b23e78c825afef
botantony <antonsm21@gmail.com>
2026-08-31 00:51:17 +0200

    paranoia-file-text-encryption: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/paranoia-file-text-encryption.rb
+++ Casks/p/paranoia-file-text-encryption.rb
@@ -4,8 +4,7 @@ cask "paranoia-file-text-encryption" do
   version "17.1.43"
   sha256 "6cddc01c894088bcb023dfe95cef8910d6ff13d8bca2f10006cf61b76261c18e"
 
-  url "https://paranoiaworks.com/download/ParanoiaFileTextEnc-#{version}.pkg",
-      verified: "paranoiaworks.com/download/"
+  url "https://paranoiaworks.com/download/ParanoiaFileTextEnc-#{version}.pkg"
   name "Paranoia File & Text Encryption"
   desc "File and text encryptor with steganography and post-quantum key exchange"
   homepage "https://paranoiaworks.mobi/pfte/"
