9d316d77811989b51852d7a587ffc7fcc68a7d45
botantony <antonsm21@gmail.com>
2026-08-31 00:42:09 +0200

    font-dorsa: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-d/font-dorsa.rb
+++ Casks/font/font-d/font-dorsa.rb
@@ -2,8 +2,7 @@ cask "font-dorsa" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/dorsa/Dorsa-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/dorsa/Dorsa-Regular.ttf"
   name "Dorsa"
   homepage "https://fonts.google.com/specimen/Dorsa"
 
