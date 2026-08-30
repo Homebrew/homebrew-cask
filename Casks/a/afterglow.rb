a439176f50d7b45c5dc98f64d18cc61427b7e2ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 06:43:07 +0000

    afterglow 1.0.1

--- Casks/a/afterglow.rb
+++ Casks/a/afterglow.rb
@@ -1,6 +1,6 @@
 cask "afterglow" do
-  version "1.0"
-  sha256 "08d70c2759776ea2d8dd99d444b6317aad6b0c0ffc21ee9e057ae26b6f8f755e"
+  version "1.0.1"
+  sha256 "f0b8a55af180bbcf1a6a08838129859ea831b9e9a582e0ad657fe7ee9951ddfd"
 
   url "https://morphing.cloud/afterglow/Afterglow-v#{version}.dmg"
   name "Afterglow"
@@ -13,7 +13,7 @@ cask "afterglow" do
   end
 
   auto_updates true
-  depends_on macos: :sequoia
+  depends_on macos: :sonoma
 
   app "Afterglow.app"
 
