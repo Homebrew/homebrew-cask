64c812d83d6d24d4d1735e70bc487fa33c0a3105
botantony <antonsm21@gmail.com>
2026-08-31 00:50:41 +0200

    mujoco: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/mujoco.rb
+++ Casks/m/mujoco.rb
@@ -2,8 +2,7 @@ cask "mujoco" do
   version "3.12.0"
   sha256 "8410882d724c3637b935dc0482b0de90efed44b17bbcfcb4a165ee46285a4865"
 
-  url "https://github.com/google-deepmind/mujoco/releases/download/#{version}/mujoco-#{version}-macos-universal2.dmg",
-      verified: "github.com/google-deepmind/mujoco/"
+  url "https://github.com/google-deepmind/mujoco/releases/download/#{version}/mujoco-#{version}-macos-universal2.dmg"
   name "MuJoCo"
   desc "General purpose physics engine"
   homepage "https://mujoco.org/"
