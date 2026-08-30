d4761d5a8530ef5d02094729d8c6e466ec7fc71b
botantony <antonsm21@gmail.com>
2026-08-31 00:43:54 +0200

    font-la-belle-aurore: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-l/font-la-belle-aurore.rb
+++ Casks/font/font-l/font-la-belle-aurore.rb
@@ -2,8 +2,7 @@ cask "font-la-belle-aurore" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/labelleaurore/LaBelleAurore.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/labelleaurore/LaBelleAurore.ttf"
   name "La Belle Aurore"
   homepage "https://fonts.google.com/specimen/La+Belle+Aurore"
 
