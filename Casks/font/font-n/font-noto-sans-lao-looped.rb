3d9a002ec836899cf45e415eb5b67ce1274c54da
botantony <antonsm21@gmail.com>
2026-08-31 00:45:31 +0200

    font-noto-sans-lao-looped: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-lao-looped.rb
+++ Casks/font/font-n/font-noto-sans-lao-looped.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-lao-looped" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf"
   name "Noto Sans Lao Looped"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Lao+Looped"
 
