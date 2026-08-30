2e38c83cdf158da7c1fecacb82ac57523f368b16
botantony <antonsm21@gmail.com>
2026-08-31 00:53:10 +0200

    virtualgl: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/virtualgl.rb
+++ Casks/v/virtualgl.rb
@@ -2,8 +2,7 @@ cask "virtualgl" do
   version "3.1.5"
   sha256 "c4a5e2daabe503b9e346b50bd3a14a78011b6a943490417a0805ce332e4ede35"
 
-  url "https://github.com/VirtualGL/virtualgl/releases/download/#{version}/VirtualGL-#{version}.dmg",
-      verified: "github.com/VirtualGL/virtualgl/"
+  url "https://github.com/VirtualGL/virtualgl/releases/download/#{version}/VirtualGL-#{version}.dmg"
   name "VirtualGL"
   desc "3D without boundaries"
   homepage "https://www.virtualgl.org/"
