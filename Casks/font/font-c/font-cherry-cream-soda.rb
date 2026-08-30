a149fcb6b34d6784ccfa5acaa5d904886608d24f
botantony <antonsm21@gmail.com>
2026-08-31 00:41:41 +0200

    font-cherry-cream-soda: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-cherry-cream-soda.rb
+++ Casks/font/font-c/font-cherry-cream-soda.rb
@@ -2,8 +2,7 @@ cask "font-cherry-cream-soda" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf"
   name "Cherry Cream Soda"
   homepage "https://fonts.google.com/specimen/Cherry+Cream+Soda"
 
