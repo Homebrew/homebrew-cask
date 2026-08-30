bce8294f0b8e8efe651e0ba5127691e571fcfdab
botantony <antonsm21@gmail.com>
2026-08-31 00:47:07 +0200

    font-protest-revolution: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-protest-revolution.rb
+++ Casks/font/font-p/font-protest-revolution.rb
@@ -2,8 +2,7 @@ cask "font-protest-revolution" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/protestrevolution/ProtestRevolution-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/protestrevolution/ProtestRevolution-Regular.ttf"
   name "Protest Revolution"
   homepage "https://fonts.google.com/specimen/Protest+Revolution"
 
