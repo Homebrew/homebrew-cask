baa4dc4d407fad045c973a8e692d87c41ff455f0
botantony <antonsm21@gmail.com>
2026-08-31 00:42:56 +0200

    font-gowun-dodum: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-g/font-gowun-dodum.rb
+++ Casks/font/font-g/font-gowun-dodum.rb
@@ -2,8 +2,7 @@ cask "font-gowun-dodum" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gowundodum/GowunDodum-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gowundodum/GowunDodum-Regular.ttf"
   name "Gowun Dodum"
   homepage "https://fonts.google.com/specimen/Gowun+Dodum"
 
