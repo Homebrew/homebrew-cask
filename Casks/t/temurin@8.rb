78d6e45cb9099934ba48afdf7f4fb3dc0a3da2e4
botantony <antonsm21@gmail.com>
2026-08-31 00:52:44 +0200

    temurin@8: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/temurin@8.rb
+++ Casks/t/temurin@8.rb
@@ -2,8 +2,7 @@ cask "temurin@8" do
   version "8,504,01"
   sha256 "899960835a50e67670c8326df4e6160fbe7194201f48871e1a90fd1ca7aad58f"
 
-  url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.csv.first}u#{version.csv.second}-b#{version.csv.third}/OpenJDK#{version.csv.first}U-jdk_x64_mac_hotspot_#{version.csv.first}u#{version.csv.second}b#{version.csv.third.split(".").first}.pkg",
-      verified: "github.com/adoptium/temurin8-binaries/"
+  url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.csv.first}u#{version.csv.second}-b#{version.csv.third}/OpenJDK#{version.csv.first}U-jdk_x64_mac_hotspot_#{version.csv.first}u#{version.csv.second}b#{version.csv.third.split(".").first}.pkg"
   name "Eclipse Temurin 8"
   desc "JDK from the Eclipse Foundation (Adoptium)"
   homepage "https://adoptium.net/"
