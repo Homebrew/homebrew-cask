d612ccaf8e505b50265e41972e588239fdb6378d
botantony <antonsm21@gmail.com>
2026-08-31 00:39:33 +0200

    excalidrawz: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/e/excalidrawz.rb
+++ Casks/e/excalidrawz.rb
@@ -2,8 +2,7 @@ cask "excalidrawz" do
   version "2.4.3"
   sha256 "7cb3a00f390a10bf0793877f0c85bcfa5c9ba6951631aecae69ec36542fa012c"
 
-  url "https://github.com/chocoford/ExcalidrawZ/releases/download/v#{version.csv.first}/ExcalidrawZ.#{version}.dmg",
-      verified: "github.com/chocoford/ExcalidrawZ/releases/download/"
+  url "https://github.com/chocoford/ExcalidrawZ/releases/download/v#{version.csv.first}/ExcalidrawZ.#{version}.dmg"
   name "ExcalidrawZ"
   desc "Excalidraw client"
   homepage "https://excalidrawz.chocoford.com/"
