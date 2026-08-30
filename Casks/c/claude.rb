82315a1b36cc60045e3ddcfc5752b30bc05dd6e2
botantony <antonsm21@gmail.com>
2026-08-31 00:38:29 +0200

    claude: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/claude.rb
+++ Casks/c/claude.rb
@@ -2,8 +2,7 @@ cask "claude" do
   version "1.40609.0,f65e386db0db64c8f8b39950e25adb11f5f5e3f3"
   sha256 "5dfecd9b46afe8390b9d0c143c56bdd826d6d501c2686feb94e1a0932296d44a"
 
-  url "https://downloads.claude.ai/releases/darwin/universal/#{version.csv.first}/Claude-#{version.csv.second}.zip",
-      verified: "downloads.claude.ai/releases/darwin/universal/"
+  url "https://downloads.claude.ai/releases/darwin/universal/#{version.csv.first}/Claude-#{version.csv.second}.zip"
   name "Claude"
   desc "Anthropic's official Claude AI desktop app"
   homepage "https://claude.com/download"
