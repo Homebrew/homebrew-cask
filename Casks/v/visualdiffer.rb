8a145dea4f6693f2a5bbe7ba827ffbf976edf579
botantony <antonsm21@gmail.com>
2026-08-31 00:53:11 +0200

    visualdiffer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/visualdiffer.rb
+++ Casks/v/visualdiffer.rb
@@ -2,8 +2,7 @@ cask "visualdiffer" do
   version "2.1.22"
   sha256 "16cd75d5dcb902266e4bf18b62db671c83ffb82a9d3399a2a97cc5aa46aeb60e"
 
-  url "https://github.com/visualdiffer/visualdiffer/releases/download/v#{version}/VisualDiffer-#{version}.zip",
-      verified: "github.com/visualdiffer/visualdiffer/"
+  url "https://github.com/visualdiffer/visualdiffer/releases/download/v#{version}/VisualDiffer-#{version}.zip"
   name "VisualDiffer"
   desc "Visually compare folders and files"
   homepage "https://visualdiffer.com/"
