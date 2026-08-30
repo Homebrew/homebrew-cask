1887c7303ae991b7fe4b5793425a66e22b4ec725
botantony <antonsm21@gmail.com>
2026-08-31 00:47:11 +0200

    font-quicksand: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-q/font-quicksand.rb
+++ Casks/font/font-q/font-quicksand.rb
@@ -2,8 +2,7 @@ cask "font-quicksand" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/quicksand/Quicksand%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/quicksand/Quicksand%5Bwght%5D.ttf"
   name "Quicksand"
   homepage "https://fonts.google.com/specimen/Quicksand"
 
