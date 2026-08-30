98d1f26c37697317b7b618308e8c44e9cb8ce4e5
botantony <antonsm21@gmail.com>
2026-08-31 00:40:09 +0200

    font-advent-pro: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-a/font-advent-pro.rb
+++ Casks/font/font-a/font-advent-pro.rb
@@ -3,7 +3,6 @@ cask "font-advent-pro" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/adventpro"
   name "Advent Pro"
