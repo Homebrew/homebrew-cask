36ba973a79e6c46ae17cee77ffbba66fd7e7caa8
botantony <antonsm21@gmail.com>
2026-08-31 00:38:38 +0200

    codeship-jet: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/codeship-jet.rb
+++ Casks/c/codeship-jet.rb
@@ -2,8 +2,7 @@ cask "codeship-jet" do
   version "2.15.0"
   sha256 "1b07ea4c3efd48a030943d0fde81a1e4ec1fa0a1c35a06ba52b9ac031f5aef3f"
 
-  url "https://codeship-jet-releases.s3.amazonaws.com/#{version}/jet-darwin_amd64_#{version}.tar.gz",
-      verified: "codeship-jet-releases.s3.amazonaws.com/"
+  url "https://codeship-jet-releases.s3.amazonaws.com/#{version}/jet-darwin_amd64_#{version}.tar.gz"
   name "Codeship Jet"
   desc "CI/CD as a service"
   homepage "https://docs.cloudbees.com/docs/cloudbees-codeship/latest/"
