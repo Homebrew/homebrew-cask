52e54416bd7934ca9986a42d82ff131f059efab6
botantony <antonsm21@gmail.com>
2026-08-31 00:49:11 +0200

    greenery: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/greenery.rb
+++ Casks/g/greenery.rb
@@ -2,8 +2,7 @@ cask "greenery" do
   version "1.0.0"
   sha256 "c95bce16d9597234374504d081b77a332b67c5edb196a3116428f97384d90572"
 
-  url "https://github.com/GreenfireInc/Releases.Greenery/releases/download/v#{version}/Greenery.#{version}.zip",
-      verified: "github.com/GreenfireInc/Releases.Greenery/"
+  url "https://github.com/GreenfireInc/Releases.Greenery/releases/download/v#{version}/Greenery.#{version}.zip"
   name "Greenery"
   desc "Cryptocurrency bookkeeping and accounting wallet"
   homepage "https://www.greenery.finance/"
