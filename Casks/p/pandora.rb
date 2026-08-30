ddbe976b072db8b79ffe282ca61dd545f3c3574a
botantony <antonsm21@gmail.com>
2026-08-31 00:51:17 +0200

    pandora: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/pandora.rb
+++ Casks/p/pandora.rb
@@ -2,8 +2,7 @@ cask "pandora" do
   version "15.0.3"
   sha256 "1858152de9d5fe18c876093a95c39c92fe878fba633c3e54e27fcdeb70a06c15"
 
-  url "https://p-desktop-app.s3.amazonaws.com/releases/Pandora-#{version}.dmg",
-      verified: "p-desktop-app.s3.amazonaws.com/"
+  url "https://p-desktop-app.s3.amazonaws.com/releases/Pandora-#{version}.dmg"
   name "Pandora"
   desc "Desktop client for the Pandora web radio service"
   homepage "https://www.pandora.com/desktop"
