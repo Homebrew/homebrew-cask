801c2c9caf50b0781bc31c82d05141cc65542929
botantony <antonsm21@gmail.com>
2026-08-31 00:45:35 +0200

    font-noto-sans-marchen: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-marchen.rb
+++ Casks/font/font-n/font-noto-sans-marchen.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-marchen" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansmarchen/NotoSansMarchen-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansmarchen/NotoSansMarchen-Regular.ttf"
   name "Noto Sans Marchen"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Marchen"
 
