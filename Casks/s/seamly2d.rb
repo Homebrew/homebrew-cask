7c0066a4a810c03879cef7097106e381e3f8396e
botantony <antonsm21@gmail.com>
2026-08-31 00:52:05 +0200

    seamly2d: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/seamly2d.rb
+++ Casks/s/seamly2d.rb
@@ -2,8 +2,7 @@ cask "seamly2d" do
   version "2026.8.31.143"
   sha256 "21becf1c47c463603e6bf3cbf3590235b134f88d14d0e4883e4751509792a6da"
 
-  url "https://github.com/FashionFreedom/Seamly2D/releases/download/v#{version}/Seamly2D-macos.zip",
-      verified: "github.com/FashionFreedom/Seamly2D/"
+  url "https://github.com/FashionFreedom/Seamly2D/releases/download/v#{version}/Seamly2D-macos.zip"
   name "Seamly2D"
   desc "Pattern making software"
   homepage "https://seamly.io/"
