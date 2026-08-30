a22ba262ac3a5ff0cdc8010186764c2954b6c6f1
botantony <antonsm21@gmail.com>
2026-08-31 00:37:38 +0200

    axure-rp: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/axure-rp.rb
+++ Casks/a/axure-rp.rb
@@ -2,8 +2,7 @@ cask "axure-rp" do
   version "11.0.0.4137"
   sha256 "fc9a4b3e46b835bab9f4450a382f8fea0e4e4bd2dd2602886f36da38ff9571b4"
 
-  url "https://axure.cachefly.net/versions/#{version.csv.first.major_minor.dots_to_hyphens}/AxureRP-Setup-#{version.split(".")[3]}.dmg",
-      verified: "axure.cachefly.net/"
+  url "https://axure.cachefly.net/versions/#{version.csv.first.major_minor.dots_to_hyphens}/AxureRP-Setup-#{version.split(".")[3]}.dmg"
   name "Axure RP"
   desc "Planning and prototyping tool for developers"
   homepage "https://www.axure.com/"
