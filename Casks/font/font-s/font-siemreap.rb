c161648b08159e8373b1a67986f9675073444abd
botantony <antonsm21@gmail.com>
2026-08-31 00:47:51 +0200

    font-siemreap: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-s/font-siemreap.rb
+++ Casks/font/font-s/font-siemreap.rb
@@ -2,8 +2,7 @@ cask "font-siemreap" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/siemreap/Siemreap.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/siemreap/Siemreap.ttf"
   name "Siemreap"
   homepage "https://fonts.google.com/specimen/Siemreap"
 
