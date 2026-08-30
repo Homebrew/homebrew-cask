fe37c5e37df6e6e82decce84a7e3603ebeb52d9a
botantony <antonsm21@gmail.com>
2026-08-31 00:52:43 +0200

    temurin@21: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/temurin@21.rb
+++ Casks/t/temurin@21.rb
@@ -11,8 +11,7 @@ cask "temurin@21" do
     version "21.0.12.1,1"
   end
 
-  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
-      verified: "github.com/adoptium/"
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg"
   name "Eclipse Temurin 21"
   desc "JDK from the Eclipse Foundation (Adoptium)"
   homepage "https://adoptium.net/"
