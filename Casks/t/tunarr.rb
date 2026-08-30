cebf5128da0ab6b9bba0b52c0f4faebc133f0842
botantony <antonsm21@gmail.com>
2026-08-31 00:52:56 +0200

    tunarr: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/tunarr.rb
+++ Casks/t/tunarr.rb
@@ -5,8 +5,7 @@ cask "tunarr" do
   sha256 arm:   "d6818a7a5c7a3252aaf3a1179a9e4c1d5b88bb950689a205c62a4bd33f8c717e",
          intel: "b0cf318a7a1bca14a0087db23fb83fb7ccbe1def2a6647adc5d1ae7a10ea8861"
 
-  url "https://github.com/chrisbenincasa/tunarr/releases/download/v#{version}/Tunarr-v#{version}-macos-#{arch}.dmg",
-      verified: "github.com/chrisbenincasa/tunarr/"
+  url "https://github.com/chrisbenincasa/tunarr/releases/download/v#{version}/Tunarr-v#{version}-macos-#{arch}.dmg"
   name "tunarr"
   desc "Create your own live TV channels from media on Plex, Jellyfin, Emby"
   homepage "https://tunarr.com/"
