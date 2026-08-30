0fd2ce11ff667db72d10c151f11ea88550909858
botantony <antonsm21@gmail.com>
2026-08-31 00:53:18 +0200

    weasis: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/w/weasis.rb
+++ Casks/w/weasis.rb
@@ -5,8 +5,7 @@ cask "weasis" do
   sha256 arm:   "5dc395a35390798bc8c9174c00afe968fce29c2845d248f9fa08e90e437a1ba7",
          intel: "7580a3c20a0f52b2e3635ef6040cf09db6392c41c9522aa714d12fb7bb654579"
 
-  url "https://github.com/nroduit/Weasis/releases/download/v#{version}/Weasis-#{version}-#{arch}.pkg",
-      verified: "github.com/nroduit/Weasis/"
+  url "https://github.com/nroduit/Weasis/releases/download/v#{version}/Weasis-#{version}-#{arch}.pkg"
   name "Weasis"
   desc "Free DICOM viewer for displaying and analyzing medical images"
   homepage "https://weasis.org/en/index.html"
