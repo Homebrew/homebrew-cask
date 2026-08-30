6c116becaf2fde9b3b7702e989af01be2ef4b781
botantony <antonsm21@gmail.com>
2026-08-31 00:50:21 +0200

    media-converter: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/media-converter.rb
+++ Casks/m/media-converter.rb
@@ -2,8 +2,7 @@ cask "media-converter" do
   version "3.0.2"
   sha256 "e7753762138c2b5552b606d02ece75bb40969d626ea5027dd8165d78fb078369"
 
-  url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip",
-      verified: "downloads.sourceforge.net/media-converter/"
+  url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip"
   name "Media Converter"
   desc "Convert avi, wmv, mkv, rm, mov and more to other formats"
   homepage "https://media-converter.sourceforge.io/"
