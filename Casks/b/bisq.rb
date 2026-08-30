fe05da36c72b11226e2f7201d9244149d212d299
botantony <antonsm21@gmail.com>
2026-08-31 00:37:54 +0200

    bisq: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bisq.rb
+++ Casks/b/bisq.rb
@@ -5,8 +5,7 @@ cask "bisq" do
   sha256 arm:   "71b3d46e56982908e8b64daeb9f8ca62059d6344cc3cecb8f4cfe9b5a0da5873",
          intel: "e4673a579b4912962e269b20c3de54e80de71865f28a2458460823fa30afd0a3"
 
-  url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{arch}-#{version}.dmg",
-      verified: "github.com/bisq-network/bisq/"
+  url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{arch}-#{version}.dmg"
   name "Bisq"
   desc "Decentralised bitcoin exchange network"
   homepage "https://bisq.network/"
