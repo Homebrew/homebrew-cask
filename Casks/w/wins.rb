1140d7a9bfdb541a5062a30b2650b08e98df16a1
botantony <antonsm21@gmail.com>
2026-08-31 00:53:22 +0200

    wins: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/w/wins.rb
+++ Casks/w/wins.rb
@@ -2,8 +2,7 @@ cask "wins" do
   version "3.5.1,53"
   sha256 :no_check
 
-  url "https://winswebsite.s3.us-east-005.backblazeb2.com/Wins.zip",
-      verified: "winswebsite.s3.us-east-005.backblazeb2.com/"
+  url "https://winswebsite.s3.us-east-005.backblazeb2.com/Wins.zip"
   name "Wins"
   desc "Window manager"
   homepage "https://wins.cool/"
