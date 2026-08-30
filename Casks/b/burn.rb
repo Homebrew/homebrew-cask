eccd19a9fd4595ffd009029b77396a936c4a1118
botantony <antonsm21@gmail.com>
2026-08-31 00:38:10 +0200

    burn: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/burn.rb
+++ Casks/b/burn.rb
@@ -2,8 +2,7 @@ cask "burn" do
   version "3.1.9"
   sha256 "baa79cebc3f30224087a5f1a11b1fec4b9e749c5eb19a675d9c8bc7c1cf7dbcc"
 
-  url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip",
-      verified: "downloads.sourceforge.net/burn-osx/"
+  url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip"
   name "Burn"
   desc "CD burning application"
   homepage "https://burn-osx.sourceforge.io/"
