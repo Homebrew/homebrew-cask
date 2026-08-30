95d3a46e256fc5aa23c3ab9ebf096e3bff1d5d9c
botantony <antonsm21@gmail.com>
2026-08-31 00:40:40 +0200

    font-arvo: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-a/font-arvo.rb
+++ Casks/font/font-a/font-arvo.rb
@@ -3,7 +3,6 @@ cask "font-arvo" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/arvo"
   name "Arvo"
