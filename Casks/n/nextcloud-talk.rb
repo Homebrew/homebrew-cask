a32a9cb993aa611ee5306f97068519ce222397b9
botantony <antonsm21@gmail.com>
2026-08-31 00:50:51 +0200

    nextcloud-talk: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/n/nextcloud-talk.rb
+++ Casks/n/nextcloud-talk.rb
@@ -2,8 +2,7 @@ cask "nextcloud-talk" do
   version "2.2.4"
   sha256 "60b0f869d7d6951467750323497c4444ad5a0a32d456ad18a15f5b272bbed010"
 
-  url "https://github.com/nextcloud-releases/talk-desktop/releases/download/v#{version}/Nextcloud.Talk-macos-universal.dmg",
-      verified: "github.com/nextcloud-releases/talk-desktop/releases/download/"
+  url "https://github.com/nextcloud-releases/talk-desktop/releases/download/v#{version}/Nextcloud.Talk-macos-universal.dmg"
   name "Nextcloud Talk Desktop"
   desc "Official Nextcloud Talk Desktop client"
   homepage "https://nextcloud.com/talk/"
