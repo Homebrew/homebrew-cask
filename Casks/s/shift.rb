ff9d302375addac275d7231e9bd2150450488e0f
botantony <antonsm21@gmail.com>
2026-08-31 00:52:10 +0200

    shift: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/shift.rb
+++ Casks/s/shift.rb
@@ -5,8 +5,7 @@ cask "shift" do
   sha256 arm:   "1b567995c2dd75168dee38118a79ff807f7a9622c68efd34e923b9bc9862a981",
          intel: "13c234648a093c3b934d9241bd9dedc24e3225cc86b168d7c4601fb3178ba42e"
 
-  url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg",
-      verified: "updates.tryshift.com/"
+  url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
   name "Shift"
   desc "Workstation to streamline your accounts, apps, and workflows"
   homepage "https://shift.com/"
