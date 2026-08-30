e9d4b1fa2fa0f2b79fc889010864271f850458ac
botantony <antonsm21@gmail.com>
2026-08-31 00:43:46 +0200

    font-kirang-haerang: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-k/font-kirang-haerang.rb
+++ Casks/font/font-k/font-kirang-haerang.rb
@@ -2,8 +2,7 @@ cask "font-kirang-haerang" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/kiranghaerang/KirangHaerang-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/kiranghaerang/KirangHaerang-Regular.ttf"
   name "Kirang Haerang"
   homepage "https://fonts.google.com/specimen/Kirang+Haerang"
 
