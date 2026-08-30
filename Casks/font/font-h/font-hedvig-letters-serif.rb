59a3f74164db22add6d7dba4a1c73949b4f2f954
botantony <antonsm21@gmail.com>
2026-08-31 00:43:06 +0200

    font-hedvig-letters-serif: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-h/font-hedvig-letters-serif.rb
+++ Casks/font/font-h/font-hedvig-letters-serif.rb
@@ -2,8 +2,7 @@ cask "font-hedvig-letters-serif" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/hedviglettersserif/HedvigLettersSerif%5Bopsz%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/hedviglettersserif/HedvigLettersSerif%5Bopsz%5D.ttf"
   name "Hedvig Letters Serif"
   homepage "https://fonts.google.com/specimen/Hedvig+Letters+Serif"
 
