4eecc8458a25cb3d3820cae8c992d11c966cdfdf
botantony <antonsm21@gmail.com>
2026-08-31 00:46:45 +0200

    font-playwrite-es-deco: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-es-deco.rb
+++ Casks/font/font-p/font-playwrite-es-deco.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-es-deco" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwriteesdeco/PlaywriteESDeco%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteesdeco/PlaywriteESDeco%5Bwght%5D.ttf"
   name "Playwrite ES Deco"
   homepage "https://fonts.google.com/specimen/Playwrite+ES+Deco"
 
