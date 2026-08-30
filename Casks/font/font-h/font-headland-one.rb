b05cc1ae7a1e9e3dfd63ae7a6428fd4c8787eb73
botantony <antonsm21@gmail.com>
2026-08-31 00:43:05 +0200

    font-headland-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-h/font-headland-one.rb
+++ Casks/font/font-h/font-headland-one.rb
@@ -2,8 +2,7 @@ cask "font-headland-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/headlandone/HeadlandOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/headlandone/HeadlandOne-Regular.ttf"
   name "Headland One"
   homepage "https://fonts.google.com/specimen/Headland+One"
 
