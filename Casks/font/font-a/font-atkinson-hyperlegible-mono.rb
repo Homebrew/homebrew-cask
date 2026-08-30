e2cc197c5b2e94f5352b95e489ac5a856564247e
botantony <antonsm21@gmail.com>
2026-08-31 00:40:44 +0200

    font-atkinson-hyperlegible-mono: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-a/font-atkinson-hyperlegible-mono.rb
+++ Casks/font/font-a/font-atkinson-hyperlegible-mono.rb
@@ -3,7 +3,6 @@ cask "font-atkinson-hyperlegible-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/atkinsonhyperlegiblemono"
   name "Atkinson Hyperlegible Mono"
