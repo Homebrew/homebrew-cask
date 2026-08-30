8d85be6d09e9f4c942d82090ddc5427c3125f754
botantony <antonsm21@gmail.com>
2026-08-31 00:37:53 +0200

    bindiff: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bindiff.rb
+++ Casks/b/bindiff.rb
@@ -2,8 +2,7 @@ cask "bindiff" do
   version "8"
   sha256 "2962fd337529a59fe4ba3b4a5596be53bfacd1bd0a3952ea7bedd6276eeb4db8"
 
-  url "https://github.com/google/bindiff/releases/download/v#{version}/BinDiff#{version}.dmg",
-      verified: "github.com/google/bindiff/"
+  url "https://github.com/google/bindiff/releases/download/v#{version}/BinDiff#{version}.dmg"
   name "BinDiff"
   desc "Binary diffing tool"
   homepage "https://zynamics.com/bindiff.html"
