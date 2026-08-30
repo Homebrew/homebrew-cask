fe34f7ddeae94028e629777250d72bc1416c048f
botantony <antonsm21@gmail.com>
2026-08-31 00:51:03 +0200

    openaudible: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/openaudible.rb
+++ Casks/o/openaudible.rb
@@ -2,8 +2,7 @@ cask "openaudible" do
   version "4.8.8"
   sha256 "a9197f15293e9fac29a4ace0412423d24ff63ad411a54ef7da3d901f83990acc"
 
-  url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
-      verified: "github.com/openaudible/openaudible/"
+  url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg"
   name "OpenAudible"
   desc "Audiobook manager for Audible users"
   homepage "https://openaudible.org/"
