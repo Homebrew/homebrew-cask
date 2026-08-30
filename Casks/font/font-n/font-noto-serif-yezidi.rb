ce6feb7ec6ab3216e760a7aa0f1152294c59f247
botantony <antonsm21@gmail.com>
2026-08-31 00:46:09 +0200

    font-noto-serif-yezidi: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-serif-yezidi.rb
+++ Casks/font/font-n/font-noto-serif-yezidi.rb
@@ -2,8 +2,7 @@ cask "font-noto-serif-yezidi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notoserifyezidi/NotoSerifYezidi%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifyezidi/NotoSerifYezidi%5Bwght%5D.ttf"
   name "Noto Serif Yezidi"
   homepage "https://fonts.google.com/specimen/Noto+Serif+Yezidi"
 
