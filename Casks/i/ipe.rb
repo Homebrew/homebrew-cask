1df7868be0deccbac8453427726c4bfb58c4c516
botantony <antonsm21@gmail.com>
2026-08-31 00:49:31 +0200

    ipe: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/i/ipe.rb
+++ Casks/i/ipe.rb
@@ -5,8 +5,7 @@ cask "ipe" do
   sha256 arm:   "25afb5d6bd0f4f51e27823d86de470d6caa7600e8ee5111f633fa10879904b8c",
          intel: "8bfb970614be423f1bf716217be3db6faa4676c5fa55a51ef81a2e3c6d6edc4d"
 
-  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipe-#{version}-mac-#{arch}.dmg",
-      verified: "github.com/otfried/ipe/"
+  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipe-#{version}-mac-#{arch}.dmg"
   name "Ipe"
   desc "Drawing editor for creating figures in PDF format"
   homepage "https://ipe.otfried.org/"
