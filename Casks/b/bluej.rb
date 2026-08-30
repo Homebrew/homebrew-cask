dcfbbca73b614d130a5af62b53c9f269ceb20398
botantony <antonsm21@gmail.com>
2026-08-31 00:38:00 +0200

    bluej: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bluej.rb
+++ Casks/b/bluej.rb
@@ -5,8 +5,7 @@ cask "bluej" do
   sha256 arm:   "2151ec9e1ce8d49e80c58267e5691aa71ac638997d338ecf2ea1f134da12d542",
          intel: "a65a4a5ee7470af29e191dfd63edcb88faa1aca1699c22d22ac1102ceaeb90f8"
 
-  url "https://github.com/k-pet-group/BlueJ-Greenfoot/releases/download/BLUEJ-RELEASE-#{version}/BlueJ-mac-#{arch}-#{version}.dmg",
-      verified: "github.com/k-pet-group/BlueJ-Greenfoot/"
+  url "https://github.com/k-pet-group/BlueJ-Greenfoot/releases/download/BLUEJ-RELEASE-#{version}/BlueJ-mac-#{arch}-#{version}.dmg"
   name "BlueJ"
   desc "Java Development Environment designed for beginners"
   homepage "https://www.bluej.org/"
