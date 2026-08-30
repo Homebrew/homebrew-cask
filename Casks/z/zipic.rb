14dfbc17f524c0f9863664d4f7a920adcb5dfacd
botantony <antonsm21@gmail.com>
2026-08-31 00:53:33 +0200

    zipic: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/z/zipic.rb
+++ Casks/z/zipic.rb
@@ -2,8 +2,7 @@ cask "zipic" do
   version "1.10.0"
   sha256 "7a9e1c3a7fcc693e85b7a0f4ac184e829f8138d711fc99dde0857c74784bc1ed"
 
-  url "https://releases.5km.tech/zipic/Zipic%20#{version}.dmg",
-      verified: "releases.5km.tech/"
+  url "https://releases.5km.tech/zipic/Zipic%20#{version}.dmg"
   name "Zipic"
   desc "Image compression tool"
   homepage "https://zipic.app/"
