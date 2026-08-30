ae7e41e375aee41240c5477d734d8a7b70ef45fe
botantony <antonsm21@gmail.com>
2026-08-31 00:50:40 +0200

    mqttx: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/mqttx.rb
+++ Casks/m/mqttx.rb
@@ -5,8 +5,7 @@ cask "mqttx" do
   sha256 arm:   "0c5c012018493194528fa2ada5c5034f2bd24538e1a1e86d58192f768451cb46",
          intel: "8381f33ab8e0f8bf2be18aac2484f4aa2d98d54f876006b93eaa0cba622caa71"
 
-  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}#{arch}.dmg",
-      verified: "github.com/emqx/MQTTX/"
+  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}#{arch}.dmg"
   name "MQTTX"
   desc "Cross-platform MQTT 5.0 Desktop Client"
   homepage "https://mqttx.app/"
