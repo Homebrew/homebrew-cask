afd9a7673bc9b270841fa3ddae50866ca1a8c41f
botantony <antonsm21@gmail.com>
2026-08-31 00:43:01 +0200

    font-gveret-levin: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-g/font-gveret-levin.rb
+++ Casks/font/font-g/font-gveret-levin.rb
@@ -2,8 +2,7 @@ cask "font-gveret-levin" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gveretlevin/GveretLevin-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gveretlevin/GveretLevin-Regular.ttf"
   name "Gveret Levin"
   homepage "https://fonts.google.com/specimen/Gveret+Levin"
 
