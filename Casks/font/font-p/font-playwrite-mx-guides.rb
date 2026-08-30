db90035a37c0660af2b879d07b3e2a4715a747c6
botantony <antonsm21@gmail.com>
2026-08-31 00:46:52 +0200

    font-playwrite-mx-guides: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-mx-guides.rb
+++ Casks/font/font-p/font-playwrite-mx-guides.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-mx-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritemxguides/PlaywriteMXGuides-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritemxguides/PlaywriteMXGuides-Regular.ttf"
   name "Playwrite MX Guides"
   homepage "https://fonts.google.com/specimen/Playwrite+MX+Guides"
 
