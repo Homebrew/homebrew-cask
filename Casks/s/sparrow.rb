da84bce23864a32c63342abeac8295eb81061342
botantony <antonsm21@gmail.com>
2026-08-31 00:52:22 +0200

    sparrow: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -5,8 +5,7 @@ cask "sparrow" do
   sha256 arm:   "e8d8637a737480721bc820a1b96a79483fc4c73bc0095f6ce1cfb93637158173",
          intel: "ab497521e5f7f2d525198df94ef26fdf3d4086ddaa611f578cac2c399a34cce2"
 
-  url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
-      verified: "github.com/sparrowwallet/sparrow/"
+  url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg"
   name "Sparrow Bitcoin Wallet"
   desc "Bitcoin wallet application"
   homepage "https://sparrowwallet.com/"
