e7bfcc79eeced4d591b59d4247c84b2efd2761ef
botantony <antonsm21@gmail.com>
2026-08-31 00:49:41 +0200

    jumpcut: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/j/jumpcut.rb
+++ Casks/j/jumpcut.rb
@@ -2,8 +2,7 @@ cask "jumpcut" do
   version "0.84"
   sha256 "ef43d3fee801188a3f6fb9ba14bf738aa2d103bd4986b390e9820e7390c6178c"
 
-  url "https://github.com/snark/jumpcut/releases/download/v#{version}/Jumpcut-#{version}.tar.bz2",
-      verified: "github.com/snark/jumpcut/"
+  url "https://github.com/snark/jumpcut/releases/download/v#{version}/Jumpcut-#{version}.tar.bz2"
   name "Jumpcut"
   desc "Clipboard manager"
   homepage "https://snark.github.io/jumpcut/"
