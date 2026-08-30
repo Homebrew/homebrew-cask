38a8c08e42b8ff9733b20b78b4803daeb1cc631c
botantony <antonsm21@gmail.com>
2026-08-31 00:50:46 +0200

    nagstamon: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/n/nagstamon.rb
+++ Casks/n/nagstamon.rb
@@ -5,8 +5,7 @@ cask "nagstamon" do
   sha256 arm:   "c15cc9002635cc26e6ee496ab6b89ff3f453b9b9697f40a4b9bfa042a07772c7",
          intel: "1ff751ed5b8801527f916a979a92e36228d4022499736ac6d5874a1d791c949b"
 
-  url "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/Nagstamon-#{version}-#{arch}.dmg",
-      verified: "github.com/HenriWahl/Nagstamon/"
+  url "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/Nagstamon-#{version}-#{arch}.dmg"
   name "Nagstamon"
   desc "Nagios status monitor"
   homepage "https://nagstamon.de/"
