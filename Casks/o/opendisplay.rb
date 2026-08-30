833986de5fad3200716cce85c769860f466db793
botantony <antonsm21@gmail.com>
2026-08-31 00:51:05 +0200

    opendisplay: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/opendisplay.rb
+++ Casks/o/opendisplay.rb
@@ -2,8 +2,7 @@ cask "opendisplay" do
   version "1.17.0"
   sha256 "0ad4c4af41495eff178c8121ef45c66cc45cfbc6265e827a34008d05462a6f44"
 
-  url "https://github.com/peetzweg/opendisplay/releases/download/v#{version}/OpenDisplay.dmg",
-      verified: "github.com/peetzweg/opendisplay/"
+  url "https://github.com/peetzweg/opendisplay/releases/download/v#{version}/OpenDisplay.dmg"
   name "OpenDisplay"
   desc "Second-display utility for iPhone and iPad over USB and Wi-Fi"
   homepage "https://opendisplay.app/"
