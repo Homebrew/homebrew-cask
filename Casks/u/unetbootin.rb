f471f4e154d244a11fbe9b90230a3e0ae519bea7
botantony <antonsm21@gmail.com>
2026-08-31 00:53:01 +0200

    unetbootin: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/unetbootin.rb
+++ Casks/u/unetbootin.rb
@@ -2,8 +2,7 @@ cask "unetbootin" do
   version "702"
   sha256 "204f867e9b2604a5ba8818b7d7f4be83d08fa0c3eb0c22e51c39fc5526bd1aed"
 
-  url "https://github.com/unetbootin/unetbootin/releases/download/#{version}/unetbootin-mac-#{version}.dmg",
-      verified: "github.com/unetbootin/unetbootin/"
+  url "https://github.com/unetbootin/unetbootin/releases/download/#{version}/unetbootin-mac-#{version}.dmg"
   name "UNetbootin"
   desc "Tool to install Linux/BSD distributions to a partition or USB drive"
   homepage "https://unetbootin.github.io/"
