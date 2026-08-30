21c8c0ab8c3bc27aaf6a9f93acef94e44f2b860d
botantony <antonsm21@gmail.com>
2026-08-31 00:52:05 +0200

    seafile-client: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/seafile-client.rb
+++ Casks/s/seafile-client.rb
@@ -2,8 +2,7 @@ cask "seafile-client" do
   version "9.0.21"
   sha256 "7af0726a075ea4280383a5191011e840729aef9fbe1554faa6924a4545e7f1b3"
 
-  url "https://sos-ch-dk-2.exo.io/seafile-downloads/seafile-client-#{version}.dmg",
-      verified: "sos-ch-dk-2.exo.io/seafile-downloads/"
+  url "https://sos-ch-dk-2.exo.io/seafile-downloads/seafile-client-#{version}.dmg"
   name "Seafile Client"
   desc "File syncing client"
   homepage "https://www.seafile.com/"
