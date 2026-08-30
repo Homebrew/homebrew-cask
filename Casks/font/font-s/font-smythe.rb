12b67b656c807d8adf71072999f9f9938e8d40cc
botantony <antonsm21@gmail.com>
2026-08-31 00:47:58 +0200

    font-smythe: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-s/font-smythe.rb
+++ Casks/font/font-s/font-smythe.rb
@@ -2,8 +2,7 @@ cask "font-smythe" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/smythe/Smythe-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/smythe/Smythe-Regular.ttf"
   name "Smythe"
   homepage "https://fonts.google.com/specimen/Smythe"
 
