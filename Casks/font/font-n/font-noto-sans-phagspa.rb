ff59e9e5978cddffb5a34f84dda9f101e6862eac
botantony <antonsm21@gmail.com>
2026-08-31 00:45:45 +0200

    font-noto-sans-phagspa: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-phagspa.rb
+++ Casks/font/font-n/font-noto-sans-phagspa.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-phagspa" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansphagspa/NotoSansPhagsPa-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansphagspa/NotoSansPhagsPa-Regular.ttf"
   name "Noto Sans PhagsPa"
   homepage "https://fonts.google.com/specimen/Noto+Sans+PhagsPa"
 
