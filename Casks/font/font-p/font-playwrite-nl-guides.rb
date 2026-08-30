e411243678c99bfa0bf92b150b0797214bef345c
botantony <antonsm21@gmail.com>
2026-08-31 00:46:53 +0200

    font-playwrite-nl-guides: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-nl-guides.rb
+++ Casks/font/font-p/font-playwrite-nl-guides.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-nl-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritenlguides/PlaywriteNLGuides-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritenlguides/PlaywriteNLGuides-Regular.ttf"
   name "Playwrite NL Guides"
   homepage "https://fonts.google.com/specimen/Playwrite+NL+Guides"
 
