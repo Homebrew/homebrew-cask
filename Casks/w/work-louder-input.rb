253cf3df770a935ca7d006ddc9d88cd62375407f
botantony <antonsm21@gmail.com>
2026-08-31 00:53:25 +0200

    work-louder-input: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/w/work-louder-input.rb
+++ Casks/w/work-louder-input.rb
@@ -5,8 +5,7 @@ cask "work-louder-input" do
   sha256 arm:   "8192f5170cac808e4ecd000c8494a5b9fd44c4a91a5bd773a802a086ddf43690",
          intel: "25d741f24b09fc8715384bfe1934ec1c46c89c23818770f468bc2c0e9f761962"
 
-  url "https://github.com/worklouder/input-releases/releases/download/v#{version}/input-#{version}#{arch}.dmg",
-      verified: "github.com/worklouder/input-releases/"
+  url "https://github.com/worklouder/input-releases/releases/download/v#{version}/input-#{version}#{arch}.dmg"
   name "Input"
   desc "Keyboard configurator for Work Louder devices"
   homepage "https://worklouder.cc/input"
