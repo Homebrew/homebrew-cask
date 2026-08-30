9804360def58293970274217d39d4117911bf0e7
botantony <antonsm21@gmail.com>
2026-08-31 00:40:53 +0200

    font-baloo-tamma-2: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-baloo-tamma-2.rb
+++ Casks/font/font-b/font-baloo-tamma-2.rb
@@ -2,8 +2,7 @@ cask "font-baloo-tamma-2" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/balootamma2/BalooTamma2%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/balootamma2/BalooTamma2%5Bwght%5D.ttf"
   name "Baloo Tamma 2"
   homepage "https://fonts.google.com/specimen/Baloo+Tamma+2"
 
