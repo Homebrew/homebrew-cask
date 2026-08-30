73ce99ed0faaa14c149cd663cce36a5e66fcca32
botantony <antonsm21@gmail.com>
2026-08-31 00:41:21 +0200

    font-bowlby-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-bowlby-one.rb
+++ Casks/font/font-b/font-bowlby-one.rb
@@ -2,8 +2,7 @@ cask "font-bowlby-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/bowlbyone/BowlbyOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/bowlbyone/BowlbyOne-Regular.ttf"
   name "Bowlby One"
   homepage "https://fonts.google.com/specimen/Bowlby+One"
 
