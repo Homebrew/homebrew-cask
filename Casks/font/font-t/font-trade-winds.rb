5992232d7a38a50713d3610935b5557f055ff648
botantony <antonsm21@gmail.com>
2026-08-31 00:48:27 +0200

    font-trade-winds: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-t/font-trade-winds.rb
+++ Casks/font/font-t/font-trade-winds.rb
@@ -2,8 +2,7 @@ cask "font-trade-winds" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/tradewinds/TradeWinds-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/tradewinds/TradeWinds-Regular.ttf"
   name "Trade Winds"
   homepage "https://fonts.google.com/specimen/Trade+Winds"
 
