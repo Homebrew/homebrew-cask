2532984183016dee5f12991a63f20c3a786ebdf1
botantony <antonsm21@gmail.com>
2026-08-31 00:45:24 +0200

    font-noto-sans-gujarati-ui: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-gujarati-ui.rb
+++ Casks/font/font-n/font-noto-sans-gujarati-ui.rb
@@ -3,7 +3,6 @@ cask "font-noto-sans-gujarati-ui" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/notosansgujaratiui"
   name "Noto Sans Gujarati UI"
