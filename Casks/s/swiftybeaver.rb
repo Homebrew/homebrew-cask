94f2092edf49caa6a7e921a2af491140e8a4d369
botantony <antonsm21@gmail.com>
2026-08-31 00:52:36 +0200

    swiftybeaver: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/swiftybeaver.rb
+++ Casks/s/swiftybeaver.rb
@@ -2,8 +2,7 @@ cask "swiftybeaver" do
   version "1.1.0"
   sha256 "1a12cf1061aac8cb455ebce2898b8165f89c5016b3befdbbe71ebbcef6680f0a"
 
-  url "https://swiftybeaver.s3.amazonaws.com/SBMac-#{version}.zip",
-      verified: "swiftybeaver.s3.amazonaws.com/"
+  url "https://swiftybeaver.s3.amazonaws.com/SBMac-#{version}.zip"
   name "SwiftyBeaver"
   desc "Swift logging"
   homepage "https://swiftybeaver.com/"
