693bf9cffed30c30996ef87b2d65e00bcf10d6fc
botantony <antonsm21@gmail.com>
2026-08-31 00:45:20 +0200

    font-noto-sans-cuneiform: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-sans-cuneiform.rb
+++ Casks/font/font-n/font-noto-sans-cuneiform.rb
@@ -2,8 +2,7 @@ cask "font-noto-sans-cuneiform" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanscuneiform/NotoSansCuneiform-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanscuneiform/NotoSansCuneiform-Regular.ttf"
   name "Noto Sans Cuneiform"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Cuneiform"
 
