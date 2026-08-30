fba9963b71921fa797127ea720c6d87ff45b2c40
botantony <antonsm21@gmail.com>
2026-08-31 00:47:21 +0200

    font-rhodium-libre: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-r/font-rhodium-libre.rb
+++ Casks/font/font-r/font-rhodium-libre.rb
@@ -2,8 +2,7 @@ cask "font-rhodium-libre" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf"
   name "Rhodium Libre"
   homepage "https://fonts.google.com/specimen/Rhodium+Libre"
 
