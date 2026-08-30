bc6059fae04961f446e00ea14d7f410e3b765fc0
botantony <antonsm21@gmail.com>
2026-08-31 00:41:09 +0200

    font-biorhyme: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-b/font-biorhyme.rb
+++ Casks/font/font-b/font-biorhyme.rb
@@ -2,8 +2,7 @@ cask "font-biorhyme" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/biorhyme/BioRhyme%5Bwdth%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/biorhyme/BioRhyme%5Bwdth%2Cwght%5D.ttf"
   name "BioRhyme"
   homepage "https://fonts.google.com/specimen/BioRhyme"
 
