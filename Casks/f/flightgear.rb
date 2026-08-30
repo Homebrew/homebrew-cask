596d5acc6e56f431919484341b582d68b12969c6
botantony <antonsm21@gmail.com>
2026-08-31 00:39:47 +0200

    flightgear: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/flightgear.rb
+++ Casks/f/flightgear.rb
@@ -2,8 +2,7 @@ cask "flightgear" do
   version "2024.1.7"
   sha256 "302e9e068342eed1aac6808b2966b5b0e70fa40cef4bb92bddfef709d098d6fc"
 
-  url "https://mirrors.ibiblio.org/flightgear/ftp/release-#{version.major_minor}/flightgear-#{version}-macos-universal.dmg",
-      verified: "mirrors.ibiblio.org/flightgear/"
+  url "https://mirrors.ibiblio.org/flightgear/ftp/release-#{version.major_minor}/flightgear-#{version}-macos-universal.dmg"
   name "FlightGear"
   desc "Flight simulator"
   homepage "https://www.flightgear.org/"
