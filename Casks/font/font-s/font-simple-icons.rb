01dbd47ff87cdbf28bac6b61baa0800eca984d66
botantony <antonsm21@gmail.com>
2026-08-31 00:47:53 +0200

    font-simple-icons: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-s/font-simple-icons.rb
+++ Casks/font/font-s/font-simple-icons.rb
@@ -2,8 +2,7 @@ cask "font-simple-icons" do
   version "16.29.0"
   sha256 "61c3fd01c9401b2a782ae71797a9228e5b3cb4ad5e1a089824dfb188ec3b9ed2"
 
-  url "https://github.com/simple-icons/simple-icons-font/releases/download/#{version}/simple-icons-font-#{version}.zip",
-      verified: "github.com/simple-icons/simple-icons-font/"
+  url "https://github.com/simple-icons/simple-icons-font/releases/download/#{version}/simple-icons-font-#{version}.zip"
   name "Simple Icons"
   homepage "https://simpleicons.org/"
 
