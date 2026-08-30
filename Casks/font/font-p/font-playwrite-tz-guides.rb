25c507d5b109fe83773a933ebcb57d947d4f63e9
botantony <antonsm21@gmail.com>
2026-08-31 00:46:57 +0200

    font-playwrite-tz-guides: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-tz-guides.rb
+++ Casks/font/font-p/font-playwrite-tz-guides.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-tz-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritetzguides/PlaywriteTZGuides-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritetzguides/PlaywriteTZGuides-Regular.ttf"
   name "Playwrite TZ Guides"
   homepage "https://fonts.google.com/specimen/Playwrite+TZ+Guides"
 
