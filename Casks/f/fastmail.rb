893c31183be57312d236f28212751c76d4c5df35
botantony <antonsm21@gmail.com>
2026-08-31 00:39:37 +0200

    fastmail: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/fastmail.rb
+++ Casks/f/fastmail.rb
@@ -6,8 +6,7 @@ cask "fastmail" do
   sha256 arm:   "738257cb04333254b60b61f076186ec08ba7b6b4f7610c77b6d174e2945c1722",
          intel: "01ab078d2aa89dedc377b97b1d2380b30f91b1b6ee8d6f50373e71630c07b9c8"
 
-  url "https://dl.fastmailcdn.com/desktop/production/mac/#{arch}/Fastmail-#{version}#{arch_suffix}-mac.zip",
-      verified: "dl.fastmailcdn.com/"
+  url "https://dl.fastmailcdn.com/desktop/production/mac/#{arch}/Fastmail-#{version}#{arch_suffix}-mac.zip"
   name "Fastmail"
   desc "Email client"
   homepage "https://www.fastmail.com/"
