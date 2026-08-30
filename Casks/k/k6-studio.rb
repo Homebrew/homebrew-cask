054a16e60f7fd181f60b469d3d4524b6973d93f5
botantony <antonsm21@gmail.com>
2026-08-31 00:49:42 +0200

    k6-studio: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/k/k6-studio.rb
+++ Casks/k/k6-studio.rb
@@ -5,8 +5,7 @@ cask "k6-studio" do
   sha256 arm:   "380133688e774540a974ea037cdefaf1340b12284c77092ff06fd2390e3348a8",
          intel: "20dcf6f3dd2e5e5e5e7380e9e5b5250f48ff9a12ef6231cafb25679a26290df0"
 
-  url "https://github.com/grafana/k6-studio/releases/download/v#{version}/k6.Studio-#{version}-#{arch}.dmg",
-      verified: "github.com/grafana/k6-studio/"
+  url "https://github.com/grafana/k6-studio/releases/download/v#{version}/k6.Studio-#{version}-#{arch}.dmg"
   name "k6 Studio"
   desc "Application for generating k6 test scripts"
   homepage "https://grafana.com/docs/k6-studio"
