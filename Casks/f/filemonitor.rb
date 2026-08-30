32ea71f6e0079d133b7941868c76d19158e0ee7e
botantony <antonsm21@gmail.com>
2026-08-31 00:39:40 +0200

    filemonitor: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/filemonitor.rb
+++ Casks/f/filemonitor.rb
@@ -2,8 +2,7 @@ cask "filemonitor" do
   version "1.3.0"
   sha256 "0c2317ba192ec7f768a51787e7486f60a04ddee42071e9168af9b8454fe762b7"
 
-  url "https://bitbucket.org/objective-see/deploy/downloads/FileMonitor_#{version}.zip",
-      verified: "bitbucket.org/objective-see/deploy/downloads/"
+  url "https://bitbucket.org/objective-see/deploy/downloads/FileMonitor_#{version}.zip"
   name "FileMonitor"
   desc "Monitor filesystem activity"
   homepage "https://objective-see.org/products/utilities.html#FileMonitor"
