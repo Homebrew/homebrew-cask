f60e7892ddae3d468ef64d9caf19c207bdc396bb
botantony <antonsm21@gmail.com>
2026-08-31 00:40:17 +0200

    font-alike-angular: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-a/font-alike-angular.rb
+++ Casks/font/font-a/font-alike-angular.rb
@@ -2,8 +2,7 @@ cask "font-alike-angular" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alikeangular/AlikeAngular-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/alikeangular/AlikeAngular-Regular.ttf"
   name "Alike Angular"
   homepage "https://fonts.google.com/specimen/Alike+Angular"
 
