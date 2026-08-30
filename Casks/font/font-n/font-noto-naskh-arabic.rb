21a7ee1bb4416c93ba2b9e199d33d4556ed11118
botantony <antonsm21@gmail.com>
2026-08-31 00:45:12 +0200

    font-noto-naskh-arabic: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-n/font-noto-naskh-arabic.rb
+++ Casks/font/font-n/font-noto-naskh-arabic.rb
@@ -2,8 +2,7 @@ cask "font-noto-naskh-arabic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notonaskharabic/NotoNaskhArabic%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notonaskharabic/NotoNaskhArabic%5Bwght%5D.ttf"
   name "Noto Naskh Arabic"
   homepage "https://fonts.google.com/specimen/Noto+Naskh+Arabic"
 
