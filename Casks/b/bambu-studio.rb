c6a5bea5cee8d107c75944858ea8790b83a24bb2
botantony <antonsm21@gmail.com>
2026-08-31 00:37:41 +0200

    bambu-studio: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bambu-studio.rb
+++ Casks/b/bambu-studio.rb
@@ -2,8 +2,7 @@ cask "bambu-studio" do
   version "02.08.02.61,20260820225108"
   sha256 "cf648a95858fb630e1353c4987038df60d6caab693f18411fb95fb809f2d6926"
 
-  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.third || version.csv.first}/Bambu_Studio_mac-v#{version.csv.first}-#{version.csv.second}.dmg",
-      verified: "github.com/bambulab/BambuStudio/"
+  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.third || version.csv.first}/Bambu_Studio_mac-v#{version.csv.first}-#{version.csv.second}.dmg"
   name "Bambu Studio"
   desc "3D model slicing software for 3D printers, maintained by Bambu Lab"
   homepage "https://bambulab.com/en/download/studio"
