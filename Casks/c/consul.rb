0c892e62f316b7b6b8864fc07f0e2af34dd61341
botantony <antonsm21@gmail.com>
2026-08-31 00:38:44 +0200

    consul: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/consul.rb
+++ Casks/c/consul.rb
@@ -5,8 +5,7 @@ cask "consul" do
   sha256 arm:   "fed05e8e2a296989c1a5c86e3b38d5f4dbdc7d83e0c0acf4c5fe7e302e8f4fde",
          intel: "9a8ced39610be73c4b6c918e64c1589a3fe9747f3a2188b8da55d0a35ddca4e0"
 
-  url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_#{arch}.zip",
-      verified: "releases.hashicorp.com/consul/"
+  url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_#{arch}.zip"
   name "Consul"
   desc "Tool for service discovery, monitoring and configuration"
   homepage "https://www.consul.io/"
