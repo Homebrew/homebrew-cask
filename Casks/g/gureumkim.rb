eaa7f92199141003bf37aa5b6662d4e787fba3cc
botantony <antonsm21@gmail.com>
2026-08-31 00:49:13 +0200

    gureumkim: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gureumkim.rb
+++ Casks/g/gureumkim.rb
@@ -2,8 +2,7 @@ cask "gureumkim" do
   version "1.13.2"
   sha256 "eadbc2904e54885eea08f7c410e2adeec4caf8463c1321706cb3d1a3e42612d1"
 
-  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg",
-      verified: "github.com/gureum/gureum/"
+  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg"
   name "구름 입력기"
   desc "Libhangul-based keyboard input"
   homepage "https://gureum.io/"
