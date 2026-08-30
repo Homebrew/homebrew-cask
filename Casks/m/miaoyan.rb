f57673e90a0f52043513bdb283ec04d3d7d0d7b0
botantony <antonsm21@gmail.com>
2026-08-31 00:50:26 +0200

    miaoyan: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/miaoyan.rb
+++ Casks/m/miaoyan.rb
@@ -2,8 +2,7 @@ cask "miaoyan" do
   version "4.2.0"
   sha256 "d47cd9117800b50a427963fc396478d7ad70eb2c8d7633088f2f4d9e2bcb39a8"
 
-  url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan_V#{version}.zip",
-      verified: "github.com/tw93/MiaoYan/"
+  url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan_V#{version}.zip"
   name "MiaoYan"
   desc "Markdown editor"
   homepage "https://miaoyan.app/"
