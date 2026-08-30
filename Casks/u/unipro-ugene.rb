b80680955c703dcc28194c0d2110599ab87101cb
botantony <antonsm21@gmail.com>
2026-08-31 00:53:01 +0200

    unipro-ugene: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/unipro-ugene.rb
+++ Casks/u/unipro-ugene.rb
@@ -2,8 +2,7 @@ cask "unipro-ugene" do
   version "53.1"
   sha256 "460a194a25e351d477891de812bab820231655778d7572e6b9fe343946c79ef6"
 
-  url "https://github.com/ugeneunipro/ugene/releases/download/#{version}/ugene-#{version}-mac-x86-64.dmg",
-      verified: "github.com/ugeneunipro/ugene/"
+  url "https://github.com/ugeneunipro/ugene/releases/download/#{version}/ugene-#{version}-mac-x86-64.dmg"
   name "Ugene"
   desc "Free open-source cross-platform bioinformatics software"
   homepage "https://ugene.net/"
