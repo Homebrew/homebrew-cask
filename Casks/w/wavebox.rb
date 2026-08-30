4661af272358064b13db0317f5cfbae943af9e94
botantony <antonsm21@gmail.com>
2026-08-31 00:53:17 +0200

    wavebox: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/w/wavebox.rb
+++ Casks/w/wavebox.rb
@@ -5,8 +5,7 @@ cask "wavebox" do
   sha256 arm:   "13e670070481e9d4f4ac5d73d6af3276f40879f5d8cba37e4297e25d8bec1591",
          intel: "eb475469cd2bbea26f6b6a2501974d93adc8e4454671af7758ff851cb13a6cee"
 
-  url "https://download.wavebox.app/stable/mac#{arch}/Wavebox_#{version}.zip",
-      verified: "download.wavebox.app/"
+  url "https://download.wavebox.app/stable/mac#{arch}/Wavebox_#{version}.zip"
   name "Wavebox"
   desc "Web browser"
   homepage "https://wavebox.io/"
