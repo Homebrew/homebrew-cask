1f61de70f7c71680f791e48f173d7c871ace02b9
botantony <antonsm21@gmail.com>
2026-08-31 00:37:14 +0200

    amiberry: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -2,8 +2,7 @@ cask "amiberry" do
   version "8.3.0"
   sha256 "dd797299f70cec573a09b3f5d1bfbaba8f3b4a5421a776d2555e3b8ef897d718"
 
-  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-#{version}-macOS-universal.dmg",
-      verified: "github.com/BlitterStudio/amiberry/"
+  url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-#{version}-macOS-universal.dmg"
   name "Amiberry"
   desc "Amiga emulator"
   homepage "https://amiberry.com/"
