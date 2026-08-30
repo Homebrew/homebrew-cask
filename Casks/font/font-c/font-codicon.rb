c7e61fe5497c28aeabdb20afe066a4463f420ba8
botantony <antonsm21@gmail.com>
2026-08-31 00:41:45 +0200

    font-codicon: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-c/font-codicon.rb
+++ Casks/font/font-c/font-codicon.rb
@@ -2,8 +2,7 @@ cask "font-codicon" do
   version "0.0.46-24"
   sha256 "d77bf2ed152e82c4b81288c5271a3481c61559d1a5416593756e3b0fe8a02bf1"
 
-  url "https://registry.npmjs.org/@vscode/codicons/-/codicons-#{version}.tgz",
-      verified: "registry.npmjs.org/@vscode/codicons/"
+  url "https://registry.npmjs.org/@vscode/codicons/-/codicons-#{version}.tgz"
   name "Codicon"
   homepage "https://github.com/microsoft/vscode-codicons"
 
