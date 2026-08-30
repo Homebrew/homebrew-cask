c368e732ee9333e801066f1fec8ec554bae49806
botantony <antonsm21@gmail.com>
2026-08-31 00:50:23 +0200

    mellel: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/mellel.rb
+++ Casks/m/mellel.rb
@@ -2,8 +2,7 @@ cask "mellel" do
   version "6.7.1,67102"
   sha256 "c29a8f555a43b504fa4ecbfa186cfa526ac29a1f1a67a6b25d48014f4af7400d"
 
-  url "https://d1riogbqt3a9uw.cloudfront.net/mellel_#{version.csv.second}.dmg",
-      verified: "d1riogbqt3a9uw.cloudfront.net/"
+  url "https://d1riogbqt3a9uw.cloudfront.net/mellel_#{version.csv.second}.dmg"
   name "Mellel"
   desc "Advanced word processor built for long and complex documents"
   homepage "https://www.mellel.com/"
