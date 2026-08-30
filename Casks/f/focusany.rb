f09de9ff666c3a12a9b243661d156c18a4eb1426
botantony <antonsm21@gmail.com>
2026-08-31 00:39:51 +0200

    focusany: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/focusany.rb
+++ Casks/f/focusany.rb
@@ -5,8 +5,7 @@ cask "focusany" do
   sha256 arm:   "500748811382d04e1756500ece790385fdec272e2a70162554e6e3dc7300757b",
          intel: "29114bec682c819f469ae667244dfa99a88d040a9ae33ad9dd11e43dd3cf8fef"
 
-  url "https://github.com/modstart-lib/focusany/releases/download/v#{version}/FocusAny-#{version}-mac-#{arch}.dmg",
-      verified: "github.com/modstart-lib/focusany/"
+  url "https://github.com/modstart-lib/focusany/releases/download/v#{version}/FocusAny-#{version}-mac-#{arch}.dmg"
   name "FocusAny"
   desc "Open source desktop toolbox"
   homepage "https://focusany.com/"
