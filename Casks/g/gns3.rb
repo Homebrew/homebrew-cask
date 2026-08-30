c4cc711de8faa73a55545fac9565b7d83de3dd2b
botantony <antonsm21@gmail.com>
2026-08-31 00:49:05 +0200

    gns3: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gns3.rb
+++ Casks/g/gns3.rb
@@ -3,8 +3,7 @@ cask "gns3" do
   version "3.0.6"
   sha256 "0b557edb15127ac942f422ff9bff57640bb7c10e4c0e1db331653a2a05dd12ea"
 
-  url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg",
-      verified: "github.com/GNS3/gns3-gui/"
+  url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg"
   name "GNS3"
   name "Graphical Network Simulator 3"
   desc "GUI for the Dynamips Cisco router emulator"
