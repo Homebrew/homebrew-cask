592e202f9b86b604fa4c3cfba8fa7bc69da0510c
botantony <antonsm21@gmail.com>
2026-08-31 00:52:00 +0200

    sabnzbd: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/sabnzbd.rb
+++ Casks/s/sabnzbd.rb
@@ -2,8 +2,7 @@ cask "sabnzbd" do
   version "5.1.2"
   sha256 "5b8db477635e7d165fd11ec122775182838258b02882349ec3534f655ad0a7f9"
 
-  url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-macos.dmg",
-      verified: "github.com/sabnzbd/sabnzbd/"
+  url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-macos.dmg"
   name "SABnzbd"
   desc "Binary newsreader"
   homepage "https://sabnzbd.org/"
