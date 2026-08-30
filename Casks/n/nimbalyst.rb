f1e0c66b96057cd17fab1e0422340e97a8ffdf05
botantony <antonsm21@gmail.com>
2026-08-31 00:50:53 +0200

    nimbalyst: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/n/nimbalyst.rb
+++ Casks/n/nimbalyst.rb
@@ -5,8 +5,7 @@ cask "nimbalyst" do
   sha256 arm:   "cb7d20a6d3f56285efa5561cd3eb5e29cfa85ca131f23c6b6ff65c5d95e8f1fb",
          intel: "2df9d569bf7b4468c2f3e9914c1aa035a6d1d354e74b3b7ad2e5c73381a82ca2"
 
-  url "https://github.com/Nimbalyst/nimbalyst/releases/download/v#{version}/Nimbalyst-macOS-#{arch}.dmg",
-      verified: "github.com/Nimbalyst/nimbalyst/"
+  url "https://github.com/Nimbalyst/nimbalyst/releases/download/v#{version}/Nimbalyst-macOS-#{arch}.dmg"
   name "Nimbalyst"
   desc "Visual workspace for building with Codex and Claude Code"
   homepage "https://nimbalyst.com/"
