ded7165ea2d8fbae5a55d5ceb7347259eba2dcb3
botantony <antonsm21@gmail.com>
2026-08-31 00:51:03 +0200

    open-webui: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/open-webui.rb
+++ Casks/o/open-webui.rb
@@ -5,8 +5,7 @@ cask "open-webui" do
   sha256 arm:   "454fee910c872168b75f98bf676af4d15b21dfcc6b43a26dfadc94bd9ef10b97",
          intel: "3d98d2d398f0f5388e1d85ffa487f2b5e9fc1540771aba02fa7bcabcbdf52e5b"
 
-  url "https://github.com/open-webui/desktop/releases/download/v#{version}/open-webui-#{arch}.dmg",
-      verified: "github.com/open-webui/desktop/"
+  url "https://github.com/open-webui/desktop/releases/download/v#{version}/open-webui-#{arch}.dmg"
   name "Open WebUI"
   desc "Desktop application for Open WebUI"
   homepage "https://openwebui.com/"
