e78b9e7211c0f9c1843701cfcf7694601ad9654f
botantony <antonsm21@gmail.com>
2026-08-31 00:52:46 +0200

    thedesk: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/thedesk.rb
+++ Casks/t/thedesk.rb
@@ -2,8 +2,7 @@ cask "thedesk" do
   version "25.4.0"
   sha256 "9bd7c57dc212eaa3ec28409d1a2409b72a1f7a2d1f10cd8b957be13e8f867951"
 
-  url "https://github.com/cutls/thedesk-next/releases/download/v#{version}/TheDesk-#{version}-universal.dmg",
-      verified: "github.com/cutls/thedesk-next/"
+  url "https://github.com/cutls/thedesk-next/releases/download/v#{version}/TheDesk-#{version}-universal.dmg"
   name "TheDesk"
   desc "Mastodon/Misskey Client for PC"
   homepage "https://thedesk.top/"
