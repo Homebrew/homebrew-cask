d7726e45c94e8e899704e3c1fb1ea1e984bc3fca
botantony <antonsm21@gmail.com>
2026-08-31 00:46:37 +0200

    font-playwrite-au-tas: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-au-tas.rb
+++ Casks/font/font-p/font-playwrite-au-tas.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-au-tas" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwriteautas/PlaywriteAUTAS%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteautas/PlaywriteAUTAS%5Bwght%5D.ttf"
   name "Playwrite AU TAS"
   homepage "https://fonts.google.com/specimen/Playwrite+AU+TAS"
 
