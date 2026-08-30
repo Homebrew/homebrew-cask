a03035a235f4d9f61d45ab004bab77bee5f5eeb2
botantony <antonsm21@gmail.com>
2026-08-31 00:47:05 +0200

    font-preahvihear: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-preahvihear.rb
+++ Casks/font/font-p/font-preahvihear.rb
@@ -2,8 +2,7 @@ cask "font-preahvihear" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/preahvihear/Preahvihear-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/preahvihear/Preahvihear-Regular.ttf"
   name "Preahvihear"
   homepage "https://fonts.google.com/specimen/Preahvihear"
 
