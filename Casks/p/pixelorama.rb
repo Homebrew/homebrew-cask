c5a2441f15e036895da334cb0809994503d69630
botantony <antonsm21@gmail.com>
2026-08-31 00:51:24 +0200

    pixelorama: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/pixelorama.rb
+++ Casks/p/pixelorama.rb
@@ -2,8 +2,7 @@ cask "pixelorama" do
   version "1.2.1"
   sha256 "0999c2652473ae0688fa1d53de3863059b6d17ed1588a9a6aaf5b22c6c4ccf27"
 
-  url "https://github.com/Orama-Interactive/Pixelorama/releases/download/v#{version}/Pixelorama-Mac.dmg",
-      verified: "github.com/Orama-Interactive/Pixelorama/"
+  url "https://github.com/Orama-Interactive/Pixelorama/releases/download/v#{version}/Pixelorama-Mac.dmg"
   name "Pixelorama"
   desc "2D sprite editor made with the Godot Engine"
   homepage "https://orama-interactive.itch.io/pixelorama"
