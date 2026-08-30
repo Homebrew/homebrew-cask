1dea002004e8fe992d7f460da63dbe295081c493
botantony <antonsm21@gmail.com>
2026-08-31 00:53:06 +0200

    vamiga: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/vamiga.rb
+++ Casks/v/vamiga.rb
@@ -2,8 +2,7 @@ cask "vamiga" do
   version "4.5"
   sha256 "8e4d98ee27cc1b10a478389c3f56da05ed60082114c3a249d8907b63a45e1bdb"
 
-  url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip",
-      verified: "github.com/dirkwhoffmann/vAmiga/"
+  url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip"
   name "vAmiga"
   desc "Amiga 500, 1000, 2000 emulator"
   homepage "https://dirkwhoffmann.github.io/vAmiga"
