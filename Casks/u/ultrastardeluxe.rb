3616fd2bd64058c2647cd64dd00c72f3488a183c
botantony <antonsm21@gmail.com>
2026-08-31 00:53:00 +0200

    ultrastardeluxe: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/ultrastardeluxe.rb
+++ Casks/u/ultrastardeluxe.rb
@@ -5,8 +5,7 @@ cask "ultrastardeluxe" do
   sha256 arm:   "b69cfe311d4ee6004db6d81596812817767b8ef59d5715f9ba08c6012a0bfb16",
          intel: "03d488e988b325cbd4ce16a210d46144372fee463fcb92854c015829b0d7de74"
 
-  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-mac-#{arch}-#{version}.dmg",
-      verified: "github.com/UltraStar-Deluxe/USDX/"
+  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-mac-#{arch}-#{version}.dmg"
   name "UltraStar Deluxe"
   desc "Karaoke game"
   homepage "https://usdx.eu/"
