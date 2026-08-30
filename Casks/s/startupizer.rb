b41f9b320ef8609aeb2db00d99f875c9e8b44b29
botantony <antonsm21@gmail.com>
2026-08-31 00:52:28 +0200

    startupizer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/startupizer.rb
+++ Casks/s/startupizer.rb
@@ -2,8 +2,7 @@ cask "startupizer" do
   version "2,2060,1527510167"
   sha256 "56792a52fc91129f6f192cbbc0dbc6a7175d0699f2dab726baaf7d94f30ce150"
 
-  url "https://dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/#{version.csv.second}/#{version.csv.third}/Startupizer#{version.major}-#{version.csv.second}.zip",
-      verified: "dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/"
+  url "https://dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/#{version.csv.second}/#{version.csv.third}/Startupizer#{version.major}-#{version.csv.second}.zip"
   name "Startupizer#{version.major}"
   desc "Login items handler"
   homepage "http://gentlebytes.com/startupizer/"
