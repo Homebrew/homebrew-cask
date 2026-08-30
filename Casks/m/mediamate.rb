df7134a50968708a01240aeda0a3f3ca0853a792
botantony <antonsm21@gmail.com>
2026-08-31 00:50:22 +0200

    mediamate: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/mediamate.rb
+++ Casks/m/mediamate.rb
@@ -2,8 +2,7 @@ cask "mediamate" do
   version "3.8.4,321"
   sha256 "461abee47383cffaa14a9a4d5e1ccee409d97306f300dd4dc705c3d76828b74e"
 
-  url "https://github.com/Wouter01/MediaMate-Releases/releases/download/v#{version.csv.first}_#{version.csv.second}/MediaMate_v#{version.csv.first}-#{version.csv.second}.zip",
-      verified: "github.com/Wouter01/MediaMate-Releases/"
+  url "https://github.com/Wouter01/MediaMate-Releases/releases/download/v#{version.csv.first}_#{version.csv.second}/MediaMate_v#{version.csv.first}-#{version.csv.second}.zip"
   name "MediaMate"
   desc "UI replacement for volume, brightness and now playing controls"
   homepage "https://wouter01.github.io/MediaMate/"
