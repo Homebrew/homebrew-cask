e07a202fadd12d7001d9710e740bc4ef4e3acc6d
botantony <antonsm21@gmail.com>
2026-08-31 00:52:12 +0200

    silhouette-studio: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/silhouette-studio.rb
+++ Casks/s/silhouette-studio.rb
@@ -2,8 +2,7 @@ cask "silhouette-studio" do
   version "5.0.523.001_M6R"
   sha256 "dad85ff9c3240dedd2e534749bdfb9a6300873c9aeedd3472d809df56000bd2e"
 
-  url "https://d62nvtdcnsm38.cloudfront.net/files/softwares/ss/SS_V#{version}.dmg",
-      verified: "d62nvtdcnsm38.cloudfront.net/files/softwares/ss/"
+  url "https://d62nvtdcnsm38.cloudfront.net/files/softwares/ss/SS_V#{version}.dmg"
   name "Silhouette Studio"
   desc "Design software for Silhouette cutting machines"
   homepage "https://www.silhouetteamerica.com/silhouette-studio"
