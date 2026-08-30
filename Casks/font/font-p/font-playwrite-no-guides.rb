06b361828c5a76fd3dcd3396cb55087fa82ed94d
botantony <antonsm21@gmail.com>
2026-08-31 00:46:54 +0200

    font-playwrite-no-guides: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-p/font-playwrite-no-guides.rb
+++ Casks/font/font-p/font-playwrite-no-guides.rb
@@ -2,8 +2,7 @@ cask "font-playwrite-no-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritenoguides/PlaywriteNOGuides-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritenoguides/PlaywriteNOGuides-Regular.ttf"
   name "Playwrite NO Guides"
   homepage "https://fonts.google.com/specimen/Playwrite+NO+Guides"
 
