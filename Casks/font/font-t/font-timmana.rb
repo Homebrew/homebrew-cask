694fd399939c4f7bd47528e6d1f901554a3911a3
botantony <antonsm21@gmail.com>
2026-08-31 00:48:23 +0200

    font-timmana: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-t/font-timmana.rb
+++ Casks/font/font-t/font-timmana.rb
@@ -2,8 +2,7 @@ cask "font-timmana" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/timmana/Timmana-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/timmana/Timmana-Regular.ttf"
   name "Timmana"
   homepage "https://fonts.google.com/specimen/Timmana"
 
