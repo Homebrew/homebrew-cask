436ddb64af684ece7ce6b2e449a8479e54fda041
botantony <antonsm21@gmail.com>
2026-08-31 00:37:48 +0200

    bestres: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/bestres.rb
+++ Casks/b/bestres.rb
@@ -2,8 +2,7 @@ cask "bestres" do
   version "1.0,100,1426778671"
   sha256 "1b706a3550edbc1411bdc4665490cce3b22cef68a6abae82af0fbd46370ce697"
 
-  url "https://dl.devmate.com/com.icyberon.BestRes/#{version.csv.second}/#{version.csv.third}/BestRes-#{version.csv.second}.zip",
-      verified: "devmate.com/com.icyberon.BestRes/"
+  url "https://dl.devmate.com/com.icyberon.BestRes/#{version.csv.second}/#{version.csv.third}/BestRes-#{version.csv.second}.zip"
   name "BestRes"
   desc "Quickly change your screen resolution from the menubar"
   homepage "https://bestres.wojtek.im/"
