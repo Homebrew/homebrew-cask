482d186113b4c62c0f01160a62934b0811f28a67
botantony <antonsm21@gmail.com>
2026-08-31 00:52:16 +0200

    smoothcsv: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/smoothcsv.rb
+++ Casks/s/smoothcsv.rb
@@ -2,8 +2,7 @@ cask "smoothcsv" do
   version "3.24.0"
   sha256 "d558378a3e6ce31192ab2668fc43e25670f1f563af55c942b2d4f48e723bf898"
 
-  url "https://github.com/kohii/smoothcsv3/releases/download/v#{version}/SmoothCSV_#{version}_universal.dmg",
-      verified: "github.com/kohii/smoothcsv3/"
+  url "https://github.com/kohii/smoothcsv3/releases/download/v#{version}/SmoothCSV_#{version}_universal.dmg"
   name "SmoothCSV"
   desc "CSV editor"
   homepage "https://smoothcsv.com/"
