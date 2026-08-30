093c38271c35eee20425bd04640ea1e2a63d4126
botantony <antonsm21@gmail.com>
2026-08-31 00:49:02 +0200

    gisto: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gisto.rb
+++ Casks/g/gisto.rb
@@ -5,8 +5,7 @@ cask "gisto" do
   sha256 arm:   "4d4a7749a0171e20a5ec5666edd80364755e18dc41636c2a3ba0edbd065dc4c2",
          intel: "95fbef51b6d4fb216787ccb112df3c1d0f6ba9f2506f9857257e82eb6345c5dd"
 
-  url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto_#{version}_#{arch}.dmg",
-      verified: "github.com/Gisto/Gisto/"
+  url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto_#{version}_#{arch}.dmg"
   name "Gisto"
   desc "Snippets management desktop application"
   homepage "https://www.gisto.org/"
