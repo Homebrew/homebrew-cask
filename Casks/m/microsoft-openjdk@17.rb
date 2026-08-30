c4f833bd4ec23f6a6c3816d2429308c20a1cdffd
botantony <antonsm21@gmail.com>
2026-08-31 00:50:29 +0200

    microsoft-openjdk@17: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/microsoft-openjdk@17.rb
+++ Casks/m/microsoft-openjdk@17.rb
@@ -5,8 +5,7 @@ cask "microsoft-openjdk@17" do
   sha256 arm:   "bc2eaccf92d844f8024fcdac02b5284a0538e99eb3b9b779a640ce1b845a381d",
          intel: "708e44f88b81bc46a0c705fbd369a3054ee1b94a3db0331081cb5f5826b9063f"
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macos-#{arch}.pkg",
-      verified: "aka.ms/download-jdk/"
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macos-#{arch}.pkg"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
   homepage "https://microsoft.com/openjdk"
