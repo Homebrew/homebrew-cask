37bfb1ed2ab0f42f4f217e6e1f4c3665f1412eab
botantony <antonsm21@gmail.com>
2026-08-31 00:42:09 +0200

    font-doppio-one: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/font/font-d/font-doppio-one.rb
+++ Casks/font/font-d/font-doppio-one.rb
@@ -2,8 +2,7 @@ cask "font-doppio-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/doppioone/DoppioOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/doppioone/DoppioOne-Regular.ttf"
   name "Doppio One"
   homepage "https://fonts.google.com/specimen/Doppio+One"
 
