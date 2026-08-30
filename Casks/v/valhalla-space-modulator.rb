7bf8f62f98006ba526db0fa9f717a64393fc16b1
botantony <antonsm21@gmail.com>
2026-08-31 00:53:05 +0200

    valhalla-space-modulator: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/valhalla-space-modulator.rb
+++ Casks/v/valhalla-space-modulator.rb
@@ -2,8 +2,7 @@ cask "valhalla-space-modulator" do
   version "1.2.8"
   sha256 "12033f7f35e97e6e06e3852e42f50caff17165bc6e79696d6837d61b34858598"
 
-  url "https://valhallaproduction.s3.us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSX_#{version.dots_to_underscores}.dmg",
-      verified: "valhallaproduction.s3.us-west-2.amazonaws.com"
+  url "https://valhallaproduction.s3.us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSX_#{version.dots_to_underscores}.dmg"
   name "Valhalla Space Modulator"
   desc "Flanger plugin"
   homepage "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
