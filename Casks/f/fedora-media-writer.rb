db1208304fc0a29fbaae4bd8598a2b67d2cd94dd
botantony <antonsm21@gmail.com>
2026-08-31 00:39:37 +0200

    fedora-media-writer: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/f/fedora-media-writer.rb
+++ Casks/f/fedora-media-writer.rb
@@ -5,8 +5,7 @@ cask "fedora-media-writer" do
   sha256 arm:   "0f9f6ab47554f878259867681336a8f9127e098528a385c155a8d4f1f4369138",
          intel: "48958cd464ee632ca6c9df7a3fe5da26cee4b7e4aada790b8372b80a69e8fb19"
 
-  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-macos#{arch}-#{version}.dmg",
-      verified: "github.com/FedoraQt/MediaWriter/"
+  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-macos#{arch}-#{version}.dmg"
   name "Fedora Media Writer"
   desc "Tool to write Fedora images to portable media files"
   homepage "https://docs.fedoraproject.org/en-US/quick-docs/creating-and-using-a-live-installation-image/"
