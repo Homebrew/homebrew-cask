6620658a02faa569291b9e21f22db18e28b6f905
botantony <antonsm21@gmail.com>
2026-08-31 00:48:59 +0200

    geektool: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/geektool.rb
+++ Casks/g/geektool.rb
@@ -2,8 +2,7 @@ cask "geektool" do
   version "3.4-11,0097512"
   sha256 "724fa310891f7fdaa0207dce4abddca9a7044153709d49e0b4faccb0e958cf6a"
 
-  url "https://s3.tynsoe.org/geektool/GeekTool.app-v#{version.csv.first}-g#{version.csv.second}.zip",
-      verified: "dl.devmate.com/org.tynsoe.GeekTool/"
+  url "https://s3.tynsoe.org/geektool/GeekTool.app-v#{version.csv.first}-g#{version.csv.second}.zip"
   name "GeekTool"
   desc "Desktop customization tool"
   homepage "https://www.tynsoe.org/geektool/"
