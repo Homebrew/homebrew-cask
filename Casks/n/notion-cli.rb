5d84515c1522588ddc506a02fd4f7ab9acf99fa5
botantony <antonsm21@gmail.com>
2026-08-31 00:50:56 +0200

    notion-cli: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/n/notion-cli.rb
+++ Casks/n/notion-cli.rb
@@ -8,8 +8,7 @@ cask "notion-cli" do
          arm64_linux:  "ba532e57011a03bd43eb3077a53c3581c8ad5fc73d96ff16aa406c7252d6e69d",
          x86_64_linux: "05fdbd24da2c34a0d1843e26b1c3dd51628b7ebd790630f4d8e5a6ef0076481a"
 
-  url "https://ntn.dev/releases/v#{version}/ntn-#{arch}-#{os}.tar.gz",
-      verified: "ntn.dev/"
+  url "https://ntn.dev/releases/v#{version}/ntn-#{arch}-#{os}.tar.gz"
   name "Notion CLI"
   desc "Command-line interface for Notion"
   homepage "https://www.notion.com/product/dev"
