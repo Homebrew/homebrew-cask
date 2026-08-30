6ac589a4312e6f1602320863537daf4db21f026d
botantony <antonsm21@gmail.com>
2026-08-31 00:37:23 +0200

    apifox: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/apifox.rb
+++ Casks/a/apifox.rb
@@ -6,8 +6,7 @@ cask "apifox" do
   sha256 arm:   "b51e3108989241c93c1b89391581c973a2486bae6dbbbb52c49ebd8ea6c488a9",
          intel: "01e15a3dd6d134bc5382a95a89d78f671cb96e6ad2c42222bcdd7d0e72601c64"
 
-  url "https://file-assets.apifox.com/download/#{version}/Apifox#{arch}-#{version}.dmg",
-      verified: "file-assets.apifox.com/download/"
+  url "https://file-assets.apifox.com/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
   desc "Platform for API documentation, debugging, and testing"
   homepage "https://github.com/apifox/apifox"
