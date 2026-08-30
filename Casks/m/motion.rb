98eab4ffbec9b1e18991d5ae6a0bbc20624ea775
botantony <antonsm21@gmail.com>
2026-08-31 00:50:38 +0200

    motion: remove `verified`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/motion.rb
+++ Casks/m/motion.rb
@@ -5,8 +5,7 @@ cask "motion" do
   sha256 arm:   "6a74ed0749a5089f1303d99fa257cc28e05be1a82c26d7b9fcf37a13902fe199",
          intel: "b38c6f7050e3d69ca3b4bcf525ce3ac5c14a638990a95899de245df598ae2280"
 
-  url "https://github.com/usemotion/desktopapp/releases/download/#{version}/motion-#{version}-mac-#{arch}.zip",
-      verified: "github.com/usemotion/desktopapp/"
+  url "https://github.com/usemotion/desktopapp/releases/download/#{version}/motion-#{version}-mac-#{arch}.zip"
   name "Motion"
   desc "To-do list and project management app"
   homepage "https://www.usemotion.com/"
