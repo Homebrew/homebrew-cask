ee06ebd055e66b52a792db1f2e68e61d5a1fecb4
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-creepster-caps: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-c/font-creepster-caps.rb
+++ Casks/font/font-c/font-creepster-caps.rb
@@ -2,7 +2,8 @@ cask "font-creepster-caps" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/creepstercaps/CreepsterCaps-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/creepstercaps/CreepsterCaps-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Creepster Caps"
   homepage "https://fonts.google.com/specimen/Creepster+Caps"
 
