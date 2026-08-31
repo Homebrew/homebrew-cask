1206ae09312b0ba3ebf43ab1b86d4a6180c8a0af
Justin <159104807+Justin24506@users.noreply.github.com>
2026-08-31 11:58:54 +0530

    tabularis: add Linux support

--- Casks/t/tabularis.rb
+++ Casks/t/tabularis.rb
@@ -1,11 +1,36 @@
 cask "tabularis" do
-  arch arm: "aarch64", intel: "x64"
+  arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
+  os macos: "dmg", linux: "AppImage"
 
   version "0.21.0"
-  sha256 arm:   "575c04dc16a33ad2207dfad9f69df810c9b3bd299adb70f06489a7b2f4f65312",
-         intel: "6273482cfcd4e77cde2294dbea5e79c8cba490d818e5a87b086da2c087991a86"
 
-  url "https://github.com/TabularisDB/tabularis/releases/download/v#{version}/tabularis_#{version}_#{arch}.dmg"
+  on_macos do
+    sha256 arm:   "575c04dc16a33ad2207dfad9f69df810c9b3bd299adb70f06489a7b2f4f65312",
+           intel: "6273482cfcd4e77cde2294dbea5e79c8cba490d818e5a87b086da2c087991a86"
+
+    auto_updates true
+    depends_on macos: :monterey
+
+    app "tabularis.app"
+
+    zap trash: [
+      "~/Library/Application Support/tabularis",
+      "~/Library/Caches/tabularis",
+      "~/Library/Logs/tabularis",
+      "~/Library/Preferences/com.debba.tabularis.plist",
+      "~/Library/Saved Application State/com.debba.tabularis.savedState",
+      "~/Library/WebKit/tabularis",
+    ]
+  end
+  on_linux do
+    sha256 "edd6a9d88fa87e076a80f2e7c5ac40ea9ecbffa935f8f1f00fc847a16df771fb"
+
+    depends_on arch: :x86_64
+
+    app_image "tabularis_#{version}_amd64.AppImage", target: "tabularis.AppImage"
+  end
+
+  url "https://github.com/TabularisDB/tabularis/releases/download/v#{version}/tabularis_#{version}_#{arch}.#{os}"
   name "Tabularis"
   desc "Lightweight database management tool"
   homepage "https://tabularis.dev/"
@@ -14,18 +39,4 @@ cask "tabularis" do
     url :url
     strategy :github_latest
   end
-
-  auto_updates true
-  depends_on macos: :monterey
-
-  app "tabularis.app"
-
-  zap trash: [
-    "~/Library/Application Support/tabularis",
-    "~/Library/Caches/tabularis",
-    "~/Library/Logs/tabularis",
-    "~/Library/Preferences/com.debba.tabularis.plist",
-    "~/Library/Saved Application State/com.debba.tabularis.savedState",
-    "~/Library/WebKit/tabularis",
-  ]
 end
