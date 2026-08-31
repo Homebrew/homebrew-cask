4ff57ad1ad2661488f71e950ca51eeda349ffb76
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-special-elite: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-special-elite.rb
+++ Casks/font/font-s/font-special-elite.rb
@@ -2,7 +2,8 @@ cask "font-special-elite" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/specialelite/SpecialElite-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/specialelite/SpecialElite-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Special Elite"
   homepage "https://fonts.google.com/specimen/Special+Elite"
 
