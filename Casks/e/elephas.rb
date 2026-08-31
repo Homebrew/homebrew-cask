af8ad7676830125bd3f48d54227cc228542c4025
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 07:11:24 +0000

    elephas 12.0001

--- Casks/e/elephas.rb
+++ Casks/e/elephas.rb
@@ -1,6 +1,6 @@
 cask "elephas" do
-  version "11.8002"
-  sha256 "11fb08367487301269c7e4a2197dace2e74260a107139debdbd198d724128f00"
+  version "12.0001"
+  sha256 "085c4188f1a797e737490ff9f09b67ca88f6355d8fa4b21c61522fbdd4de1d42"
 
   url "https://assets.elephas.app/Elephas_#{version.csv.second || version.csv.first}.dmg"
   name "Elephas"
