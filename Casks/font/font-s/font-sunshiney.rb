0bdda1be5ddc34e94cc8f319fde941405c6224f8
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-sunshiney: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-sunshiney.rb
+++ Casks/font/font-s/font-sunshiney.rb
@@ -2,7 +2,8 @@ cask "font-sunshiney" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/sunshiney/Sunshiney-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/sunshiney/Sunshiney-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Sunshiney"
   homepage "https://fonts.google.com/specimen/Sunshiney"
 
