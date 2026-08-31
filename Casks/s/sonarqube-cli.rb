d69e49a3b241c6ab41add4ec1bf39ad2401bc1aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 13:56:54 +0000

    sonarqube-cli 1.7.0.4638

--- Casks/s/sonarqube-cli.rb
+++ Casks/s/sonarqube-cli.rb
@@ -2,16 +2,16 @@ cask "sonarqube-cli" do
   arch arm: "arm64", intel: "x86-64"
   os macos: "macos", linux: "linux"
 
-  version "1.6.0.4255"
+  version "1.7.0.4638"
 
   on_macos do
-    sha256 "5ff39d44b0845e413334718ec37cdf876ceeaa2b5d3b1076f399426fef62ef5b"
+    sha256 "8a4611b2899dafeef2be6c2d820b3d2c004a02acd012bf7b0f1fd08ca9193659"
 
     depends_on arch: :arm64
   end
   on_linux do
-    sha256 arm64_linux:  "e3cdee5a32399564d377a767ce3ea4ffc3f0d56b0075e0490dfb21aa144e6433",
-           x86_64_linux: "dfdedc9efac2b93d2634b971b2723488ccc8c30a10ffed5d7602bdba57a86d65"
+    sha256 arm64_linux:  "f4a69cc7f9c1312567572f674e4343094e6325e1c142d05d3e491482d8979329",
+           x86_64_linux: "42b9dd631120b8776cd63daafd18db8e2d1229fdcde2a3ce6ef258580eb63a8c"
   end
 
   artifact = "sonarqube-cli-#{version}-#{os}-#{arch}.bin"
