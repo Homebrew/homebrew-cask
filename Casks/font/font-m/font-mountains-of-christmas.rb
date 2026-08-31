ea157802b9fe866ee389d2a358e8574e1bab2adc
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-mountains-of-christmas: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-m/font-mountains-of-christmas.rb
+++ Casks/font/font-m/font-mountains-of-christmas.rb
@@ -3,6 +3,7 @@ cask "font-mountains-of-christmas" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "apache/mountainsofchristmas"
   name "Mountains of Christmas"
