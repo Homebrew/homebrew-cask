3a291d28cf3ec3f5f94eeda8b580415532af5c4a
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-alike: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-alike.rb
+++ Casks/font/font-a/font-alike.rb
@@ -2,7 +2,8 @@ cask "font-alike" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alike/Alike-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/alike/Alike-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Alike"
   homepage "https://fonts.google.com/specimen/Alike"
 
