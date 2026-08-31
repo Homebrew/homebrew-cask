d73e69eb6f3690285af12f062d8be8b7c730625b
botantony <antonsm21@gmail.com>
2026-08-31 14:05:25 +0200

    lw-scanner: add Linux support
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,11 +1,19 @@
 cask "lw-scanner" do
   arch arm: "arm64", intel: "amd64"
+  os macos: "darwin", linux: "linux"
+  url_end = on_system_conditional macos: "zip", linux: "tar.gz"
 
   version "0.29.2"
-  sha256 arm:   "620e875a9d225d68dd2412d3d909c0b6b6880b63209f6610451d96159c298b7b",
-         intel: "b8b2e512c1efc037455c710089fcea38d705c083cbbc3e59c99d49d47b186d49"
+  sha256 arm:          "620e875a9d225d68dd2412d3d909c0b6b6880b63209f6610451d96159c298b7b",
+         intel:        "b8b2e512c1efc037455c710089fcea38d705c083cbbc3e59c99d49d47b186d49",
+         arm64_linux:  "cca1b384a038d116a560ac358c9d4bd45e2d0c3b7039b8b861e65e0bc94663df",
+         x86_64_linux: "28f564755a145dafe775b35e8d960464141b304ab6de9bcd3606b2f7c033800f"
 
-  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-#{arch}.zip"
+  on_macos do
+    disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  end
+
+  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-#{os}-#{arch}.#{url_end}"
   name "Lacework vulnerability scanner"
   desc "Lacework inline scanner"
   homepage "https://github.com/lacework/lacework-vulnerability-scanner"
@@ -14,10 +22,6 @@ cask "lw-scanner" do
     url :url
     strategy :github_latest
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
-
-  depends_on :macos
 
   binary "bin/lw-scanner"
 
