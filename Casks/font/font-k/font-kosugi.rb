2a84982936f570b707f4bb7a5410a93cfd0e3b05
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-kosugi: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-k/font-kosugi.rb
+++ Casks/font/font-k/font-kosugi.rb
@@ -2,7 +2,8 @@ cask "font-kosugi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/kosugi/Kosugi-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/kosugi/Kosugi-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Kosugi"
   homepage "https://fonts.google.com/specimen/Kosugi"
 
