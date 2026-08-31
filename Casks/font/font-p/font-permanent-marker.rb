da9a28f408c885ce398ba2801e7c6c00db225b6e
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-permanent-marker: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-p/font-permanent-marker.rb
+++ Casks/font/font-p/font-permanent-marker.rb
@@ -2,7 +2,8 @@ cask "font-permanent-marker" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/permanentmarker/PermanentMarker-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/permanentmarker/PermanentMarker-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Permanent Marker"
   homepage "https://fonts.google.com/specimen/Permanent+Marker"
 
