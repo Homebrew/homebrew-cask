a4d0a421d9689e4ae4d768ee337e0db1ea11a079
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-calligraffitti: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-c/font-calligraffitti.rb
+++ Casks/font/font-c/font-calligraffitti.rb
@@ -2,7 +2,8 @@ cask "font-calligraffitti" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/calligraffitti/Calligraffitti-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/calligraffitti/Calligraffitti-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Calligraffitti"
   homepage "https://fonts.google.com/specimen/Calligraffitti"
 
