c03913649a0203099bb043f9978465262f2d2dd0
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-maiden-orange: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-m/font-maiden-orange.rb
+++ Casks/font/font-m/font-maiden-orange.rb
@@ -2,7 +2,8 @@ cask "font-maiden-orange" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/maidenorange/MaidenOrange-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/maidenorange/MaidenOrange-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Maiden Orange"
   homepage "https://fonts.google.com/specimen/Maiden+Orange"
 
