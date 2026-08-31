71daeb3cd81811844cd4cd64e16e50ded518682d
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-rochester: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-r/font-rochester.rb
+++ Casks/font/font-r/font-rochester.rb
@@ -2,7 +2,8 @@ cask "font-rochester" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/rochester/Rochester-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/rochester/Rochester-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Rochester"
   homepage "https://fonts.google.com/specimen/Rochester"
 
