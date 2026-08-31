be2c89f3e8d843f3f30254f2fe48e5a4525b8847
Alex Kubacki <usernotfound@gmail.com>
2026-08-31 10:52:03 -0600

    aptible 1.0.0

--- Casks/a/aptible.rb
+++ Casks/a/aptible.rb
@@ -1,26 +1,24 @@
 cask "aptible" do
-  version "0.26.8,20260608224702,gh-89"
-  sha256 "0abe0783430ea31172064535eb9a79bd3ecbab1f4a4aacb59f257eee251ef554"
+  arch arm: "arm64", intel: "amd64"
 
-  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.10.15.7-1.pkg"
+  version "1.0.0"
+  sha256 arm:   "3d22cc88ff17c26baa4686a91886afc75384399ccfac5ca566b8c7aa64f4645b",
+         intel: "3178672333bb873fcecd02d5c91b805a26a4fb16f6380f895c9dd537367248f7"
+
+  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/release/aptible-cli-go/aptible-cli-go_#{version}_darwin_#{arch}.pkg"
   name "Aptible Toolbelt"
   desc "Command-line tool for Aptible Deploy, an audit-ready App Deployment Platform"
   homepage "https://www.aptible.com/docs/reference/aptible-cli/overview"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/((?:gh-)?\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[2]},#{match[0]}" }
-    end
+    regex(%r{/release/aptible-cli-go/aptible-cli-go[._-]v?(\d+(?:\.\d+)+)_darwin[._-]arm64\.pkg}i)
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on :macos
   depends_on formula: "libfido2"
 
-  pkg "aptible-toolbelt-#{version.csv.first}+#{version.csv.second}-mac-os-x.10.15.7-1.pkg"
+  pkg "aptible-cli-go_#{version}_darwin_#{arch}.pkg"
 
   uninstall pkgutil: "com.aptible.toolbelt"
 
