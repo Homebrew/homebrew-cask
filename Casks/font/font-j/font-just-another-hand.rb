380b15e02a4fa3dd7a90f836bd4a2b3d816cdb31
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-just-another-hand: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-j/font-just-another-hand.rb
+++ Casks/font/font-j/font-just-another-hand.rb
@@ -2,7 +2,8 @@ cask "font-just-another-hand" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/justanotherhand/JustAnotherHand-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/justanotherhand/JustAnotherHand-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Just Another Hand"
   homepage "https://fonts.google.com/specimen/Just+Another+Hand"
 
