ca24c03ec391a50e55c6d44e9d0f8f32ca9e9bd8
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:11 +0000

    font-valley-sans: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-v/font-valley-sans.rb
@@ -0,0 +1,16 @@
+cask "font-valley-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/valleysans"
+  name "Valley Sans"
+  homepage "https://fonts.google.com/specimen/Valley+Sans"
+
+  font "ValleySans-Italic[wght].ttf"
+  font "ValleySans[wght].ttf"
+
+  # No zap stanza required
+end
