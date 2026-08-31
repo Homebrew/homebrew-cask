68b01c5d62a69301945732ded5fcc426f61c2a68
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-pangolin: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-p/font-pangolin.rb
+++ Casks/font/font-p/font-pangolin.rb
@@ -2,7 +2,8 @@ cask "font-pangolin" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/pangolin/Pangolin-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/pangolin/Pangolin-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Pangolin"
   homepage "https://fonts.google.com/specimen/Pangolin"
 
