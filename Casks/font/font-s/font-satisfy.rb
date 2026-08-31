1a940f6507efaff6aacbb73c068b963a75e0705b
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-satisfy: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-satisfy.rb
+++ Casks/font/font-s/font-satisfy.rb
@@ -2,7 +2,8 @@ cask "font-satisfy" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/satisfy/Satisfy-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/satisfy/Satisfy-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Satisfy"
   homepage "https://fonts.google.com/specimen/Satisfy"
 
