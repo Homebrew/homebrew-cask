4f56981069904c81866948de06130224f488b15b
Justin <159104807+Justin24506@users.noreply.github.com>
2026-08-31 12:14:47 +0530

    tabularis: address review comments

--- Casks/t/tabularis.rb
+++ Casks/t/tabularis.rb
@@ -3,11 +3,11 @@ cask "tabularis" do
   os macos: "dmg", linux: "AppImage"
 
   version "0.21.0"
+  sha256 arm:          "575c04dc16a33ad2207dfad9f69df810c9b3bd299adb70f06489a7b2f4f65312",
+         intel:        "6273482cfcd4e77cde2294dbea5e79c8cba490d818e5a87b086da2c087991a86",
+         x86_64_linux: "edd6a9d88fa87e076a80f2e7c5ac40ea9ecbffa935f8f1f00fc847a16df771fb"
 
   on_macos do
-    sha256 arm:   "575c04dc16a33ad2207dfad9f69df810c9b3bd299adb70f06489a7b2f4f65312",
-           intel: "6273482cfcd4e77cde2294dbea5e79c8cba490d818e5a87b086da2c087991a86"
-
     auto_updates true
     depends_on macos: :monterey
 
@@ -23,8 +23,6 @@ cask "tabularis" do
     ]
   end
   on_linux do
-    sha256 "edd6a9d88fa87e076a80f2e7c5ac40ea9ecbffa935f8f1f00fc847a16df771fb"
-
     depends_on arch: :x86_64
 
     app_image "tabularis_#{version}_amd64.AppImage", target: "tabularis.AppImage"
