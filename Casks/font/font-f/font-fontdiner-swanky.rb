8e813ea2fb9534567ec61900c042994f2ba2c79f
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-fontdiner-swanky: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-f/font-fontdiner-swanky.rb
+++ Casks/font/font-f/font-fontdiner-swanky.rb
@@ -2,7 +2,8 @@ cask "font-fontdiner-swanky" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Fontdiner Swanky"
   homepage "https://fonts.google.com/specimen/Fontdiner+Swanky"
 
