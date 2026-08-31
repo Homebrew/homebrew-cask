1cda44fdc1e9a26ac07df76efed9fdc90318df7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 07:03:18 +0000

    channel-works 12.0.7

--- Casks/c/channel-works.rb
+++ Casks/c/channel-works.rb
@@ -1,6 +1,6 @@
 cask "channel-works" do
-  version "12.0.6"
-  sha256 "31e83e3d4bc502fadb7d0e208256a8c9856db718217c2e28dc72f9babd8e1edf"
+  version "12.0.7"
+  sha256 "15f6b8a09240aa91214e7ec9301a87bf4703b7574ad6c830de79922537ffdf43"
 
   url "https://github.com/channel-io/channel-works-macos/releases/download/v#{version}/Channel.Works-darwin-universal-#{version}.zip"
   name "Channel Works"
