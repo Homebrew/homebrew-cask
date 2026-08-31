152cf45ccadb2d685a8c15acfd2b873fb1c68768
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-redressed: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-r/font-redressed.rb
+++ Casks/font/font-r/font-redressed.rb
@@ -2,7 +2,8 @@ cask "font-redressed" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/redressed/Redressed-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/redressed/Redressed-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Redressed"
   homepage "https://fonts.google.com/specimen/Redressed"
 
