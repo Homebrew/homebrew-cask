3d243cf4808a7d0665976160c2ce24bbe3c002c9
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-open-sans-hebrew: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-o/font-open-sans-hebrew.rb
+++ Casks/font/font-o/font-open-sans-hebrew.rb
@@ -3,6 +3,7 @@ cask "font-open-sans-hebrew" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "apache/opensanshebrew"
   name "Open Sans Hebrew"
