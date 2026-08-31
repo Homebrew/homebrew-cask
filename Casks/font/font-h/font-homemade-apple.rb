a0f52d85d90a388a551a42496783c0e5e27a3018
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-homemade-apple: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-h/font-homemade-apple.rb
+++ Casks/font/font-h/font-homemade-apple.rb
@@ -2,7 +2,8 @@ cask "font-homemade-apple" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/homemadeapple/HomemadeApple-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/homemadeapple/HomemadeApple-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Homemade Apple"
   homepage "https://fonts.google.com/specimen/Homemade+Apple"
 
