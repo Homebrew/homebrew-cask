04b1839d3aa1772a3c72dd212251843070bbb75d
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-rock-salt: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-r/font-rock-salt.rb
+++ Casks/font/font-r/font-rock-salt.rb
@@ -2,7 +2,8 @@ cask "font-rock-salt" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/rocksalt/RockSalt-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/rocksalt/RockSalt-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Rock Salt"
   homepage "https://fonts.google.com/specimen/Rock+Salt"
 
