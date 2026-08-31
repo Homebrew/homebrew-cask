776317922f4b3d4023a3a49eeccd3963f98ed3fd
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-luckiest-guy: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-l/font-luckiest-guy.rb
+++ Casks/font/font-l/font-luckiest-guy.rb
@@ -2,7 +2,8 @@ cask "font-luckiest-guy" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/luckiestguy/LuckiestGuy-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/luckiestguy/LuckiestGuy-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Luckiest Guy"
   homepage "https://fonts.google.com/specimen/Luckiest+Guy"
 
