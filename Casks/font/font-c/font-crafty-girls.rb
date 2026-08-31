ce2c10e44fa4e917782541766997e731faeaf596
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-crafty-girls: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-c/font-crafty-girls.rb
+++ Casks/font/font-c/font-crafty-girls.rb
@@ -2,7 +2,8 @@ cask "font-crafty-girls" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/craftygirls/CraftyGirls-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/craftygirls/CraftyGirls-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Crafty Girls"
   homepage "https://fonts.google.com/specimen/Crafty+Girls"
 
