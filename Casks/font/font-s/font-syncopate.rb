93774ec8056576f8560466f4d692f59c501f5b0b
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-syncopate: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-syncopate.rb
+++ Casks/font/font-s/font-syncopate.rb
@@ -3,6 +3,7 @@ cask "font-syncopate" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "apache/syncopate"
   name "Syncopate"
