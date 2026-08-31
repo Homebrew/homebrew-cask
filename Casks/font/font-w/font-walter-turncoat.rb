5b61c205c9931d9527440d378db349785def60b1
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:03 +0000

    font-walter-turncoat: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-w/font-walter-turncoat.rb
+++ Casks/font/font-w/font-walter-turncoat.rb
@@ -2,7 +2,8 @@ cask "font-walter-turncoat" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/walterturncoat/WalterTurncoat-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/walterturncoat/WalterTurncoat-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Walter Turncoat"
   homepage "https://fonts.google.com/specimen/Walter+Turncoat"
 
