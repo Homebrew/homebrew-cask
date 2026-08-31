facad54f41c255cab35cf6f3ac2e53f9124273b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 12:55:28 +0000

    dbgate 7.2.6

--- Casks/d/dbgate.rb
+++ Casks/d/dbgate.rb
@@ -3,10 +3,10 @@ cask "dbgate" do
   os macos: "mac_universal", linux: "linux_#{arch}"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "7.2.5"
+  version "7.2.6"
 
   on_macos do
-    sha256 "7df321f839c522a33dd8ea79d4125304e6ab696eceb9de6a1547aa236a5ef402"
+    sha256 "6759fb9e4d8a7936d04a2b5d7b0fb881418e50bb1db42b4f4710955a6674d209"
 
     depends_on macos: :monterey
 
@@ -21,8 +21,8 @@ cask "dbgate" do
     ]
   end
   on_linux do
-    sha256 arm64_linux:  "8a4a706c7a20d969d54b5cc1da57db79eca7711bbfdde71844192bd1267db99d",
-           x86_64_linux: "70e945c64443e17c06d3596cd61a896a77d794d31a738e2a7381a4b257f235cd"
+    sha256 arm64_linux:  "406f6630b35d117ddb7998eb6853af657424738572b58cc4a2d0c532e31d9b3b",
+           x86_64_linux: "f5b5311b6cf1868d07a4dd5349dd6c157ad4958120b406cb1da7d4586820278b"
 
     app_image "dbgate-#{version}-linux_#{arch}.AppImage", target: "DbGate.AppImage"
   end
