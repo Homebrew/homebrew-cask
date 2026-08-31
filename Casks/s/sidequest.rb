c64a29ed84a0ff1fdb3baf9459db57bcb36964df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 19:39:14 +0000

    sidequest 1.0.0

--- Casks/s/sidequest.rb
+++ Casks/s/sidequest.rb
@@ -1,9 +1,9 @@
 cask "sidequest" do
   arch arm: "-arm64"
 
-  version "0.10.42"
-  sha256 arm:   "2fd14eb70dafbbe245b6b8f81ac30eb7a6699a8993ec6efcdcadc5a9cde9b9b0",
-         intel: "cdb9d983dd608487989d7a0116ee07549742ccc206989cb261edd22eed1050a8"
+  version "1.0.0"
+  sha256 arm:   "0fdf37fcba8b5ad3c1141abe45286c7adfc99c4932ac1c41a96af34f18fa65df",
+         intel: "5b2d0c2ca61aa073a9ec8756bf507eb748597c23a1b2f95adb1eca023e79002a"
 
   url "https://github.com/SideQuestVR/SideQuest/releases/download/v#{version}/SideQuest-#{version}#{arch}.dmg"
   name "SideQuest"
