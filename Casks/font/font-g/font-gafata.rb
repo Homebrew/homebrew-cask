24cb15725ce76e22aafdf8a7bccf293bdc0ad2c6
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-gafata: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-g/font-gafata.rb
+++ Casks/font/font-g/font-gafata.rb
@@ -2,7 +2,8 @@ cask "font-gafata" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gafata/Gafata-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/gafata/Gafata-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Gafata"
   homepage "https://fonts.google.com/specimen/Gafata"
 
