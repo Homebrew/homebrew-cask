a06741a97a9e77c37c6eb5f268d70cad0e179ae0
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-aclonica: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-aclonica.rb
+++ Casks/font/font-a/font-aclonica.rb
@@ -2,7 +2,8 @@ cask "font-aclonica" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/aclonica/Aclonica-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/aclonica/Aclonica-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Aclonica"
   homepage "https://fonts.google.com/specimen/Aclonica"
 
