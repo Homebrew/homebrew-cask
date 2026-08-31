c3f548add9b95bb852c07218dc5430ffaba95285
Bevan Kay <email@bevankay.me>
2026-08-31 13:05:02 +0000

    font-jsmath-cmsy10: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-j/font-jsmath-cmsy10.rb
+++ Casks/font/font-j/font-jsmath-cmsy10.rb
@@ -2,7 +2,8 @@ cask "font-jsmath-cmsy10" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/jsmathcmsy10/jsMath-cmsy10.ttf"
+  url "https://github.com/google/fonts/raw/main/apache/jsmathcmsy10/jsMath-cmsy10.ttf",
+      verified: "github.com/google/fonts/"
   name "jsMath cmsy10"
   homepage "https://fonts.google.com/specimen/jsMath+cmsy10"
 
