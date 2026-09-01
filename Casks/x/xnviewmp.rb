e0c26cc769bbbbb2d5ad2f27705de8ec461c5f88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 14:38:54 +0000

    xnviewmp 1.11.6

--- Casks/x/xnviewmp.rb
+++ Casks/x/xnviewmp.rb
@@ -1,6 +1,6 @@
 cask "xnviewmp" do
-  version "1.11.5"
-  sha256 "675143eb3784c9fbbe9dbb290a6edb8f3b994e978ed99d3e73fd26a248b17659"
+  version "1.11.6"
+  sha256 "1da40a995db18edf5e0a9904ffb1e0e29306f8ad6bce36853f1830f5ef084b04"
 
   url "https://download.xnview.com/old_versions/XnView_MP/XnView_MP-#{version}-mac.dmg"
   name "XnViewMP"
