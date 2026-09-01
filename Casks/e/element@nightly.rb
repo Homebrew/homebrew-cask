5858917c6e944a0c666e5450e9dc5b8c210086da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 09:55:16 +0000

    element@nightly 2026090101

--- Casks/e/element@nightly.rb
+++ Casks/e/element@nightly.rb
@@ -1,6 +1,6 @@
 cask "element@nightly" do
-  version "2026083101"
-  sha256 "4de0b6ad330984284ab398512c9e9efc191b3ac15db743968c7879909446f5ee"
+  version "2026090101"
+  sha256 "59bf3ae15b485e3d7843d6e56c6d9a572db37507c0fe9a98adb43a1f5b046d15"
 
   url "https://packages.element.io/nightly/update/macos/Element%20Nightly-#{version}-universal-mac.zip"
   name "Element Nightly"
