0e13a7b558efbb689c19c4da5a84a51c5ef5eef2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 14:31:31 +0000

    tyme 2026.9

--- Casks/t/tyme.rb
+++ Casks/t/tyme.rb
@@ -1,5 +1,5 @@
 cask "tyme" do
-  version "2026.8"
+  version "2026.9"
   sha256 :no_check
 
   url "https://www.tyme-app.com/app/Tyme.dmg"
@@ -13,7 +13,7 @@ cask "tyme" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
+  depends_on macos: :ventura
 
   app "Tyme.app"
 
