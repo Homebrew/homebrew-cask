987633380c65c01e154576ff979d5164435d4d49
nikita42 <13642481+rekurt@users.noreply.github.com>
2026-09-01 21:33:26 +0300

    maccoffee: use canonical release host

--- Casks/m/maccoffee.rb
+++ Casks/m/maccoffee.rb
@@ -2,9 +2,9 @@ cask "maccoffee" do
   version "2.0.0"
   sha256 "a165d94a517bc8464d839c56093ed0788afd2083f06eef80aebbf17de2c22243"
 
-  url "https://github.com/rekurt/Mac-Coffee/releases/download/v#{version}/MacCoffee-#{version}.dmg"
+  url "https://github.com/rekurt/maccoffee-dist/releases/download/v#{version}/MacCoffee-#{version}.dmg"
   name "Mac Coffee"
-  desc "Menu bar utility to keep a Mac awake"
+  desc "Utility to prevent idle sleep"
   homepage "https://github.com/rekurt/Mac-Coffee"
 
   livecheck do
