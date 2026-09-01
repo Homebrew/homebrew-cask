28f48860e9b0509df059e393a7073981383aa155
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 19:39:02 +0000

    space-rabbit 2.4.2

--- Casks/s/space-rabbit.rb
+++ Casks/s/space-rabbit.rb
@@ -1,6 +1,6 @@
 cask "space-rabbit" do
-  version "2.4.1"
-  sha256 "a3c4748d634b682dfb78de80654778daf04db9f5a0213417b7e08f76f4e71716"
+  version "2.4.2"
+  sha256 "4a7909fcc89aed1cf16e379a4c7f17c085f0afd7b36c02537d1a6a229218dc67"
 
   url "https://github.com/Tahul/space-rabbit/releases/download/v#{version}/Space-Rabbit.dmg"
   name "Space Rabbit"
