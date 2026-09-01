8c1edcfc6a604dcfab449dfe0c00eea741d24aa5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 01:56:02 +0000

    microsoft-azure-storage-explorer 1.46.0

--- Casks/m/microsoft-azure-storage-explorer.rb
+++ Casks/m/microsoft-azure-storage-explorer.rb
@@ -1,9 +1,9 @@
 cask "microsoft-azure-storage-explorer" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.45.0"
-  sha256 arm:   "5750a3d17014c3185c433dc1f8fbddbd4d486758ea2ecc144c66e50b58199b22",
-         intel: "ccc1aacf43dcdab98b35d9cdf82bc185572399045e1f6e94e2ab6d2045dbfbcd"
+  version "1.46.0"
+  sha256 arm:   "d96f537addf70319d65a7e510242bfcde8b92117ab3a4adf04eda80453939d1d",
+         intel: "b5838336619959398268b7e8cd9ce7528b81773c0028b21f47be26d3323c1d39"
 
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer-darwin-#{arch}.zip"
   name "Microsoft Azure Storage Explorer"
