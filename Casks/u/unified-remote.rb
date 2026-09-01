8f169753d4e7ad70d0e0f1e74c0439f6041feae6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 02:27:05 +0000

    unified-remote 3.14.0.2604

--- Casks/u/unified-remote.rb
+++ Casks/u/unified-remote.rb
@@ -1,6 +1,6 @@
 cask "unified-remote" do
-  version "3.14.0.2599"
-  sha256 "da746d93a9b24419e139e45d71f24de1c640020dc78105926bfcd0f7e71d2170"
+  version "3.14.0.2604"
+  sha256 "30d4928fb40e0e711d6b66df955f714283b8f1db3000afbcfac7615724ca1d98"
 
   url "https://www.unifiedremote.com/static/builds/server/macosx/#{version.split(".").last}/ServerSetup-#{version}.dmg"
   name "Unified Remote"
