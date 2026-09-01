17f357630e7a99ee3cdf3f778476c18d009c702b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 01:31:37 +0000

    fastmail 1.7.1

--- Casks/f/fastmail.rb
+++ Casks/f/fastmail.rb
@@ -2,9 +2,9 @@ cask "fastmail" do
   arch arm: "arm64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-#{arch}"
 
-  version "1.6.0"
-  sha256 arm:   "738257cb04333254b60b61f076186ec08ba7b6b4f7610c77b6d174e2945c1722",
-         intel: "01ab078d2aa89dedc377b97b1d2380b30f91b1b6ee8d6f50373e71630c07b9c8"
+  version "1.7.1"
+  sha256 arm:   "5e628569ee093731e94012aa255721eb4fcf2836c69920e576cd74c8e67772d6",
+         intel: "a0e28017730d40edbfffef4368119b2064032139edaf6570c45450e94a11633b"
 
   url "https://dl.fastmailcdn.com/desktop/production/mac/#{arch}/Fastmail-#{version}#{arch_suffix}-mac.zip"
   name "Fastmail"
