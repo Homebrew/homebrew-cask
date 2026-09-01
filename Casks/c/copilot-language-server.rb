bfe2189cd578f426a70401d39db3f9d29c1073af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 03:46:35 +0000

    copilot-language-server 1.538.0

--- Casks/c/copilot-language-server.rb
+++ Casks/c/copilot-language-server.rb
@@ -2,11 +2,11 @@ cask "copilot-language-server" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "1.537.2"
-  sha256 arm:          "5097b317098e7bf471f07ab8bf55608b470cc3575cda6cedbc74d471f5e9492f",
-         intel:        "13e70b8dccb8575d9dce83135bc3b20e118b02e18eb6ead9b392c49ca25038e9",
-         arm64_linux:  "986ea92d5770120d51bebf29a0c4dc420293ac83fc5a56ca9bd74de7ddcb9b01",
-         x86_64_linux: "6fc702f5c566fe4896af440978e399e344980c6527958c802c9f0763225b9098"
+  version "1.538.0"
+  sha256 arm:          "474fa356ebd795405baa7d51e8850868a6dc0f7911f44abdcfddcfc98ac62118",
+         intel:        "f917b0f9404ed5de53bfd1e136e70e7fc012bc5e87c9477b7026cf5637a6adc2",
+         arm64_linux:  "7c21289a8908b34c505459b0c6dc7929ae2e37deec7b835e5c96f8dfb3176526",
+         x86_64_linux: "1f6775e66988dad188ed3b679b3c46d75b5d4a97403234cd4dc8b9be455c8cdb"
 
   on_macos do
     depends_on macos: :big_sur
