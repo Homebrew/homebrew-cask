5a464a845081ee261f0164fdac01ea6cb922d3e3
nikita42 <13642481+rekurt@users.noreply.github.com>
2026-09-01 21:45:22 +0300

    maccoffee: use GitHub Pages distribution

--- Casks/m/maccoffee.rb
+++ Casks/m/maccoffee.rb
@@ -2,14 +2,14 @@ cask "maccoffee" do
   version "2.0.0"
   sha256 "a165d94a517bc8464d839c56093ed0788afd2083f06eef80aebbf17de2c22243"
 
-  url "https://github.com/rekurt/maccoffee-dist/releases/download/v#{version}/MacCoffee-#{version}.dmg"
+  url "https://rekurt.github.io/maccoffee-dist/MacCoffee-#{version}.dmg"
   name "Mac Coffee"
   desc "Utility to prevent idle sleep"
-  homepage "https://github.com/rekurt/Mac-Coffee"
+  homepage "https://rekurt.github.io/maccoffee-dist/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://rekurt.github.io/maccoffee-dist/"
+    regex(/MacCoffee[ ._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
