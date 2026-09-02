3a8a26cfddfbdcc3f17069422970ce4b866d127d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-09-01 21:59:14 -0400

    afterglow: filter beta releases from livecheck

--- Casks/a/afterglow.rb
+++ Casks/a/afterglow.rb
@@ -9,7 +9,9 @@ cask "afterglow" do
 
   livecheck do
     url "https://morphing.cloud/afterglow/appcast.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle do |items|
+      items.find { |item| item.channel.nil? }&.short_version
+    end
   end
 
   auto_updates true
