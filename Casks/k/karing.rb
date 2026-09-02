988b44aaae2c87aade1688307bf26cd18c623469
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 04:14:35 +0000

    karing 1.2.24.2709

--- Casks/k/karing.rb
+++ Casks/k/karing.rb
@@ -1,6 +1,6 @@
 cask "karing" do
-  version "1.2.23.2606"
-  sha256 "6f421213b6208e3abc1eda6fdd8ddc2014239c7f4241387749ae3a3729c658ea"
+  version "1.2.24.2709"
+  sha256 "7251c079316078a82857785e59b5118df5ae4242898acbc05af57f92cdd5d6e1"
 
   url "https://github.com/KaringX/karing/releases/download/v#{version}/karing_#{version}_macos_universal.dmg"
   name "Karing"
