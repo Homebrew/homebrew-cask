97d2303449bfa056660ea3e86d16db8cb40eb3ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-02 17:15:51 +0000

    xkey 1.2.25,20260828

--- Casks/x/xkey.rb
+++ Casks/x/xkey.rb
@@ -1,6 +1,6 @@
 cask "xkey" do
-  version "1.2.25,20260818"
-  sha256 "f28bc5eb3e6f72a7b61d195ab34368edc4319a11cfe591bb79505ade16f5ee30"
+  version "1.2.25,20260828"
+  sha256 "6d96c0c60c6b86283b2a62df629b8eb3ea79860c5643721ddf2c0ce6b0fd71eb"
 
   url "https://github.com/xmannv/xkey/releases/download/v#{version.csv.first}-#{version.csv.second}/XKey.dmg"
   name "XKey"
