009213672cd8f16f7a9c01ac79d7ed0d59a7078b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 16:01:20 +0000

    helium-browser 0.16.4.1

--- Casks/h/helium-browser.rb
+++ Casks/h/helium-browser.rb
@@ -4,11 +4,11 @@ cask "helium-browser" do
   file_sep = on_system_conditional macos: "_", linux: "-"
   url_end = on_system_conditional macos: "-macos.dmg", linux: ".AppImage"
 
-  version "0.16.3.1"
-  sha256 arm:          "4cc271d1305f08934d9500b672525ae5f9f5aa2ed49986d8bbc625a99bd83220",
-         intel:        "4bc4cecbc19eba8fec87c00cd74edb2446592a1bb8a2fbe0a682020ef6c3f4fc",
-         arm64_linux:  "14e53a5902c53dba4f2c4b902616acbb1138bc3b01e9c58873493d0c07208419",
-         x86_64_linux: "9370a3ac5e39b3b4a2cd1a1ffd7f5e3a73cac24381f5cd40bc14a68e3ed13883"
+  version "0.16.4.1"
+  sha256 arm:          "7c945dcbaca8151d2b2c287d438a7b46bcbe69a3d9d921db87d60cc35b4b8f6c",
+         intel:        "170b4acf31b023f6ac9a7dbc3a52c0a66228f2860fa0da75bdd91ea35ba8fadf",
+         arm64_linux:  "3cd0d7a30f50e1c3f9656de15e45832a29176d51204837fc314f88b9c8cc4dea",
+         x86_64_linux: "cf43a82a65b8f45ff61779b18d997226c4d8e3991ec992408f8a63a0606304ef"
 
   on_macos do
     depends_on macos: :ventura
