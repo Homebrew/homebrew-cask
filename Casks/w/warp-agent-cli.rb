3b076fedb2ab2fe1ef1d12ca5d5e3266959f834a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 22:45:36 +0000

    warp-agent-cli 0.2026.09.02.08.27.stable_01

--- Casks/w/warp-agent-cli.rb
+++ Casks/w/warp-agent-cli.rb
@@ -2,11 +2,11 @@ cask "warp-agent-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "0.2026.08.26.17.59.stable_01"
-  sha256 arm:          "6493bed43df2bf65db1c68794af33052dcee7c34c7f74ee2d8ebdd1496b0a95d",
-         intel:        "dac53bef9b9090c22768690665b440e02fefea8174a876215a9c010a46bd2eb3",
-         arm64_linux:  "ebe3ff0c7dbbbda4e3cdbdcfae019b732495922bfcb48343c090f4a828ed6e31",
-         x86_64_linux: "a8e36dd5160bc16a9358341061b0d6677b702fccf65c020f1d700049558cc757"
+  version "0.2026.09.02.08.27.stable_01"
+  sha256 arm:          "22788b8da86871f501a5dacb90d3d2ba5ef65f6e7628dd9272888918e9042aee",
+         intel:        "942f0dda1ba21bc3270bfe79178b20d6edec0d80f25539e56c88787c556c1d55",
+         arm64_linux:  "1e26c0ae5cb1fcbfa822172f2d4ae104ffb0f03d1a4c45526695aca626745411",
+         x86_64_linux: "ff24ffd32dc60d863832041a1bdcc60b994f5bac2abc208f909546684f707409"
 
   on_macos do
     depends_on macos: :sonoma
