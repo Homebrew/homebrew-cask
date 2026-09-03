f42a0e12cdbfbf0260eb055ed1f56adc95966618
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 19:44:57 +0000

    tablepro 0.72.0

--- Casks/t/tablepro.rb
+++ Casks/t/tablepro.rb
@@ -1,9 +1,9 @@
 cask "tablepro" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.71.0"
-  sha256 arm:   "9f98c07207c3ff5ee812501e07a96be65523c34c91a71e016db349846eb6d052",
-         intel: "2f80c0a1925c64ea69247bad369085dd77031d0e531a78e7b0e9e1200a34aa46"
+  version "0.72.0"
+  sha256 arm:   "529d55ed0da0c0615e0e89e0bc2e2eb8081a2e0a19bac7b66d09f01fac757d5b",
+         intel: "2620863cb9cfcb91337d4555a3013e1f4051558cf64376fc4da62ac76a7efb33"
 
   url "https://github.com/TableProApp/TablePro/releases/download/v#{version}/TablePro-#{version}-#{arch}.dmg"
   name "TablePro"
