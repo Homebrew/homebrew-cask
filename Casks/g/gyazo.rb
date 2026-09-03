77a070f6ad1414eaac1b334611c0e677e2aa35e0
machupicchubeta <machupicchubeta@gmail.com>
2026-09-03 22:27:00 +0900

    gyazo : Move `pkg` stanza to `installer manual`
    Since the installer requires interaction to proceed.
    
    See also:
    - https://github.com/Homebrew/homebrew-cask/pull/284852#issuecomment-5525991442
    
    Thank you @bevanjkay.

--- Casks/g/gyazo.rb
+++ Casks/g/gyazo.rb
@@ -15,7 +15,7 @@ cask "gyazo" do
   depends_on macos: :big_sur
   depends_on maximum_macos: :sequoia
 
-  pkg "Gyazo-#{version}.pkg"
+  installer manual: "Gyazo-#{version}.pkg"
 
   uninstall launchctl: "com.gyazo.menu.helper",
             quit:      "com.gyazo.menu",
