f2a0f96f1fc783bc5d24963b60c68d02caebc320
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 22:48:20 +0000

    wispr-flow 1.6.774

--- Casks/w/wispr-flow.rb
+++ Casks/w/wispr-flow.rb
@@ -1,9 +1,9 @@
 cask "wispr-flow" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.765"
-  sha256 arm:   "13cf2d145716bd76edcc5fe900cc826c8afa142c0966fdb0ab473d492e03cb19",
-         intel: "98c4e3093c7ee4118d0342a257013ca1a9b20c21bb1da2ae3b0240697962e24d"
+  version "1.6.774"
+  sha256 arm:   "eec0e90e3e7c036372a47f2835aa436ffccc3071033c438a16d496db181546f6",
+         intel: "cd583211ff69908d9e9382c3e860f3eeaf41675390a067576a74b686dc0dd593"
 
   url "https://dl.wisprflow.com/wispr-flow/darwin/#{arch}/dmgs/Flow-v#{version}.dmg"
   name "Wispr Flow"
