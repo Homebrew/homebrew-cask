d0cc01750f3edf82f2e61b51f8c3bfaeb1bae89f
ndaemy <yuyaebean@gmail.com>
2026-09-04 02:51:04 +0900

    font-pyeojin-gothic 1.3 (new cask)

---
+++ Casks/font/font-p/font-pyeojin-gothic.rb
@@ -0,0 +1,19 @@
+cask "font-pyeojin-gothic" do
+  version "1.3"
+  sha256 "8e7b4ade193fb7be158d9245fb54b6dfd27e3425de6df4170992c1b5fbb05bcc"
+
+  url "https://github.com/Jihwan-Suh/PyeojinGothic/releases/download/v#{version}/PyeojinGothic.v#{version}.zip"
+  name "Pyeojin Gothic"
+  name "펴진고딕"
+  homepage "https://notforall.tistory.com/7"
+
+  font "static/otf/PyeojinGothic-Light.otf"
+  font "static/otf/PyeojinGothic-Regular.otf"
+  font "static/otf/PyeojinGothic-Medium.otf"
+  font "static/otf/PyeojinGothic-Semibold.otf"
+  font "static/otf/PyeojinGothic-Bold.otf"
+  font "static/otf/PyeojinGothic-Extrabold.otf"
+  font "static/otf/PyeojinGothic-Black.otf"
+
+  # No zap stanza required
+end
