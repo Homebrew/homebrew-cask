8614594b7768a803ee1dd188dd6f6e0177139549
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 13:46:20 +0000

    macloggerdx 6.62

--- Casks/m/macloggerdx.rb
+++ Casks/m/macloggerdx.rb
@@ -1,6 +1,6 @@
 cask "macloggerdx" do
-  version "6.61"
-  sha256 "a0de08eee26f1a5ac34bc8c4c4472c5b98d362ef7199708ef5a61eb31c199d79"
+  version "6.62"
+  sha256 "99c2f8c912fe09d1b2ebae7b7796f06703056509ae411f5a4eb0913ddc979277"
 
   url "https://www.dogparksoftware.com/files/MacLoggerDX#{version.no_dots}.dmg"
   name "MacLoggerDX"
