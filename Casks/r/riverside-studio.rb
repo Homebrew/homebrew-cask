68b565b1d75b5b86411afee7b6c7d2d4aeb0b1e7
Rui Chen <rui@chenrui.dev>
2026-09-02 21:27:29 -0400

    riverside-studio: disable
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/r/riverside-studio.rb
+++ Casks/r/riverside-studio.rb
@@ -12,6 +12,8 @@ cask "riverside-studio" do
     strategy :sparkle, &:short_version
   end
 
+  disable! date: "2026-09-02", because: :fails_gatekeeper_check
+
   auto_updates true
   depends_on macos: :sequoia
 
