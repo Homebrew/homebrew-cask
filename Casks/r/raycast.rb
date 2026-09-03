ffae8fdff910e59361240253eb9b0ec8960fd381
botantony <antonsm21@gmail.com>
2026-09-03 13:05:50 +0200

    raycast 1.104.26 (Ventura-Sequoia)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/raycast.rb
+++ Casks/r/raycast.rb
@@ -25,9 +25,9 @@ cask "raycast" do
   end
   on_ventura :or_newer do
     on_sequoia :or_older do
-      version "1.104.25"
-      sha256 arm:   "972f6de210ffcacfa1feee095b8a30c7eeb972e914c876f65d37d218354a7067",
-             intel: "754c3e367b88b52d7ccb17ae7a56653419616e008f1e002d0761a39c68adc19d"
+      version "1.104.26"
+      sha256 arm:   "9aa424b804db73d6a885734e4aea0991be56519f31022cd8f610b8f176156963",
+             intel: "0b13767315c738177122e932406aaf85e5118cc2d085ad24aa45cc44bbaeaef3"
 
       url "https://releases.raycast.com/releases/#{version}/download?build=#{arch}"
 
