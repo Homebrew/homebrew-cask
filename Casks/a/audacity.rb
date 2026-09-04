a748141c66c74e31499f0b07441f821f9eb5c44c
MarkosTh09 <markos.theocharis2009@gmail.com>
2026-09-04 17:27:27 +0300

    audacity 4.0.0

--- Casks/a/audacity.rb
+++ Casks/a/audacity.rb
@@ -1,16 +1,15 @@
 cask "audacity" do
-  arch arm: "arm64", intel: on_system_conditional(macos: "x86_64", linux: "x64")
+  arch arm: "aarch64", intel: "x86_64"
   os macos: "macOS", linux: "linux"
-  ubuntu_version = on_system_conditional linux: "-22.04"
+  arch_suffix = on_system_conditional macos: "universal", linux: arch
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "3.7.9"
-  sha256 arm:          "fafeb7fa963d3e2ba05ee7aba5290c966362ac5f5feca86eb8b1f61c7819d499",
-         intel:        "142f3900bea5d49a73e66cce06e156b8e5387643e1b718806e85286753d6294f",
-         x86_64_linux: "e03f51163e370277039c2f2f665376a86b56aa51c9056aba00f8712d889ed654"
+  version "4.0.0"
 
   on_macos do
-    app "Audacity.app"
+    sha256 "03bdb378bc333695af8cba1ff0b91775cf17961a6f23a34edc6a94e3ca7c85ab"
+
+    app "Audacity 4.app"
 
     uninstall quit: "org.audacityteam.audacity"
 
@@ -21,12 +20,13 @@ cask "audacity" do
     ]
   end
   on_linux do
-    depends_on arch: :x86_64
+    sha256 arm64_linux:   "e5def4e76febc4ab2dea7525a43aa62ddfa092434d164c1ea039687889a6104d",
+           x86_64_linux: "772663b0b407be44232193b8402cde4da4665c7f6e81edb5b70e3b14e8b9b5b4"
 
-    app_image "audacity-linux-#{version}-#{arch}#{ubuntu_version}.AppImage", target: "Audacity.AppImage"
+    app_image "audacity-linux-#{version}-#{arch}.AppImage", target: "Audacity.AppImage"
   end
 
-  url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-#{os}-#{version}-#{arch}#{ubuntu_version}.#{url_end}"
+  url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-#{os}-#{version}-#{arch_suffix}.#{url_end}"
   name "Audacity"
   desc "Multi-track audio editor and recorder"
   homepage "https://www.audacityteam.org/"
