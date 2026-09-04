93f0c7161c85a12e5407d258bf86478464c17292
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 15:37:59 +0000

    block-buzz 0.5.22

--- Casks/b/block-buzz.rb
+++ Casks/b/block-buzz.rb
@@ -2,11 +2,11 @@ cask "block-buzz" do
   arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
   os macos: "dmg", linux: "AppImage"
 
-  version "0.5.20"
+  version "0.5.22"
 
   on_macos do
-    sha256 arm:   "0471456eaa7c3a4ab83ed93cc75d14b21eb57032f96bf2cfa49c0f9fa847bde6",
-           intel: "bb6c955f1f809aa8dcb166b8ed66f92e98bb9791be4b09ee4f5a094f09723d1e"
+    sha256 arm:   "68581945a4c6f9321e7f9c9541a9529db0acbbd7c3247296d1cdd57f989a2ee0",
+           intel: "9c49402bd2ce4f84417d29939f5c04f40ae0b76892eedb13a2fd5bd2d3bb971f"
 
     conflicts_with cask: "buzz"
 
@@ -23,7 +23,7 @@ cask "block-buzz" do
         rmdir: "~/.buzz"
   end
   on_linux do
-    sha256 "8faaaf04eca01c0e6c15e69d373482775477a6e6f777a9eb98ea1fe9de94b7ce"
+    sha256 "47121001a1c9ee2fbc318672326ee8a2bd93d914785c165a0fc00947e34935b1"
 
     depends_on arch: :x86_64
 
