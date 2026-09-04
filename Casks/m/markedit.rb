7a2c1ca4a61398f5af286e97cb2cd632eac8e5f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 01:51:01 +0000

    markedit 1.35.0

--- Casks/m/markedit.rb
+++ Casks/m/markedit.rb
@@ -1,6 +1,6 @@
 cask "markedit" do
-  version "1.34.0"
-  sha256 "e38962edcca263e31d947987212f25ecc25d85af7f848dcb0cc31587d0332b76"
+  version "1.35.0"
+  sha256 "15c4930969b411de98c3397094d3815b40463781dd01161581f26f54d6108e7e"
 
   url "https://github.com/MarkEdit-app/MarkEdit/releases/download/v#{version}/MarkEdit-#{version}.dmg"
   name "MarkEdit"
