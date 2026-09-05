a59ab95b061ede81143aa5ad2d8fbd922ee9efb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 03:57:18 +0000

    ghostty@tip 17718

--- Casks/g/ghostty@tip.rb
+++ Casks/g/ghostty@tip.rb
@@ -1,6 +1,6 @@
 cask "ghostty@tip" do
-  version "17709,c81f0b26871c7fbbe2fc35549fdad1f64ed29094"
-  sha256 "c22ce2261e9c19e812ae5c60653f7852c0451c007b30c5ce588093fda334d32f"
+  version "17718,492300cad104195411d12217dd22f1cd05f31376"
+  sha256 "629e994b85a6c7e224183ca202f2da62ae742a4671b36ad3904d7ca600da78d5"
 
   url "https://tip.files.ghostty.org/#{version.csv.second}/Ghostty.dmg"
   name "Ghostty"
