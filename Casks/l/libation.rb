385eb7983bdfe7d8b34fc5003511b77433b43701
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 01:40:52 +0000

    libation 14.1.0

--- Casks/l/libation.rb
+++ Casks/l/libation.rb
@@ -1,9 +1,9 @@
 cask "libation" do
   arch arm: "arm64", intel: "x64"
 
-  version "14.0.2"
-  sha256 arm:   "291a8c2bc3c3f7589c4a8d9e31394d8f364a455d278b7af39045ec661b7d9d5e",
-         intel: "c0ae1d335578714e7e43cfbdf560905f46980d7b7073c3048e2a14a0eb2c2ffc"
+  version "14.1.0"
+  sha256 arm:   "fcdddff0da1c793951a6ac3f2e9d2386f20c47e92e079ff056f8678758924887",
+         intel: "542b2b10aada3a89bafb76a223b23963382a301b644ee26205e6aad1df19c9e9"
 
   url "https://github.com/rmcrackan/Libation/releases/download/v#{version}/Libation.#{version}-macOS-chardonnay-#{arch}.dmg"
   name "Libation"
