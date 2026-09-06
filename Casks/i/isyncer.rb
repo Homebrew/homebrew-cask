b8c064559b5c8c92808c7a6ef3f87bd38af33621
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2026-09-06 20:55:56 +0200

    Apply batched suggestions from code review
    Co-authored-by: Anton Melnikov <antonsm21@gmail.com>

--- Casks/i/isyncer.rb
+++ Casks/i/isyncer.rb
@@ -1,23 +1,13 @@
-# typed: strict
-# frozen_string_literal: true
-
 cask "isyncer" do
+  arch arm: "aarch64", intel: "x64"
+  
   version "5.0.0"
+  sha256 arm:   "1b13a20b5b1444b0177ccabdc75161b2dcd2124440ac4a485208522657dea08d",
+         intel: "e5540659c1f728beb45d667edda81fb5097458b131589bb1b659fbaba7137994"
 
-  on_arm do
-    sha256 "1b13a20b5b1444b0177ccabdc75161b2dcd2124440ac4a485208522657dea08d"
-
-    url "https://www.isyncer.de/system/iSyncerV#{version}mac-installer-aarch64.tgz"
-
-    pkg "iSyncer-installer-aarch64-#{version}.pkg"
-  end
-  on_intel do
-    sha256 "e5540659c1f728beb45d667edda81fb5097458b131589bb1b659fbaba7137994"
-
-    url "https://www.isyncer.de/system/iSyncerV#{version}mac-installer-x64.tgz"
-
-    pkg "iSyncer-installer-x64-#{version}.pkg"
-  end
+  pkg "iSyncer-installer-#{arch}-#{version}.pkg"
+  
+  url "https://www.isyncer.de/system/iSyncerV#{version}mac-installer-#{arch}.tgz"
 
   name "iSyncer"
   desc "Utility to export and sync iTunes/Apple Music playlists"
