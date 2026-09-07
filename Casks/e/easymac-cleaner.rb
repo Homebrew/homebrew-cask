ae7dd250b9729a39ba2b5b992f6d1c28f38ec790
Thiago Duarte <hello@martiancat.space>
2026-09-07 18:08:31 -0300

    easymac-cleaner 1.3 (new cask)
    Add cask for EasyMac Cleaner version 1.3 with auto-update and dependencies.

---
+++ Casks/e/easymac-cleaner.rb
@@ -0,0 +1,28 @@
+cask "easymac-cleaner" do
+  version "1.3"
+  sha256 "ff507578f37e0e2afc3f19018c85a32e751cb199c1605bae0fb577cdbc788428"
+
+  url "https://updates.martiancat.space/cleaner/EasyMacCleaner-#{version}.zip"
+  name "EasyMac Cleaner"
+  desc "Cleaning, privacy, and system optimization utility"
+  homepage "https://martiancat.space/"
+
+  livecheck do
+    url "https://updates.martiancat.space/cleaner/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: :big_sur
+
+  app "EasyMac Cleaner.app"
+
+  zap trash: [
+    "~/Library/Application Support/EasyMac Cleaner",
+    "~/Library/Application Support/EasyMacCleaner",
+    "~/Library/Caches/com.martiancat.easymaccleaner",
+    "~/Library/HTTPStorages/com.martiancat.easymaccleaner",
+    "~/Library/Preferences/com.martiancat.easymaccleaner.plist",
+    "~/Library/Saved Application State/com.martiancat.easymaccleaner.savedState",
+  ]
+end
