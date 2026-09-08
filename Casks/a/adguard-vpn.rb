cbd5a08fc324d40a25f0c002a28335e8a10ff6b2
botantony <antonsm21@gmail.com>
2026-09-08 16:28:40 +0200

    adguard-vpn: update livecheck
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/adguard-vpn.rb
+++ Casks/a/adguard-vpn.rb
@@ -9,8 +9,12 @@ cask "adguard-vpn" do
 
   livecheck do
     url "https://static.adguard-vpn.com/mac/adguard-release-appcast.xml"
-    strategy :sparkle do |item|
-      item.short_version.delete_suffix(" release")
+    regex(%r{/AdGuardVPN[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :sparkle do |item, regex|
+      match = item.url&.match(regex)
+      next if match.blank?
+
+      match[1]
     end
   end
 
