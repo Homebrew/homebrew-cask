3a772682aff5300407d3d07f8dcaf702228c627d
Justin <159104807+Justin24506@users.noreply.github.com>
2026-09-08 19:12:06 +0530

    tabularis@nightly: fix style

--- Casks/t/tabularis@nightly.rb
+++ Casks/t/tabularis@nightly.rb
@@ -3,8 +3,8 @@ cask "tabularis@nightly" do
   os macos: "dmg", linux: "AppImage"
 
   version "0.22.1-3,20260904-d470693"
-  sha256 arm:         "5cb1c65590fb866d6d62aa33b9d4e08b4f8ba6f9a2fad74fac0a06263a637ba7",
-         intel:       "edfa761e1ae42165af35d24b07c76fec2e710ba4697b3c5746150b5bafe9e65c",
+  sha256 arm:          "5cb1c65590fb866d6d62aa33b9d4e08b4f8ba6f9a2fad74fac0a06263a637ba7",
+         intel:        "edfa761e1ae42165af35d24b07c76fec2e710ba4697b3c5746150b5bafe9e65c",
          x86_64_linux: "bfd2d608205966f8a73225aac3aa017f1e45a19804db5cc6a56b29590f65d744"
 
   on_macos do
