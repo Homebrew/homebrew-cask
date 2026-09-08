91920fa44f57aa757b45c721fbf5ec4cb5b2b503
Jack <risibility0822@gmail.com>
2026-09-08 14:43:55 +0800

    Add flick 1.0.0

---
+++ Casks/f/flick.rb
@@ -0,0 +1,26 @@
+cask "flick" do
+  version "1.0.0"
+  sha256 "1b0f971b539edb2f7fe19fc3e5103b91d19de6b676aee5182391d669a6250e85"
+
+  url "https://getflick.dev/Flick-#{version}.dmg"
+  name "Flick"
+  desc "Radial command wheel: hold a key, flick the mouse toward an action, release"
+  homepage "https://getflick.dev/"
+
+  livecheck do
+    url "https://getflick.dev/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: :sonoma
+
+  app "Flick.app"
+
+  zap trash: [
+    "~/Library/Application Support/Flick",
+    "~/Library/Caches/dev.getflick.flick",
+    "~/Library/HTTPStorages/dev.getflick.flick",
+    "~/Library/Preferences/dev.getflick.flick.plist",
+  ]
+end
