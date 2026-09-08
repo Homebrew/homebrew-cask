f501ac0e891ffb6b8bb3504f788e4341485593f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 20:32:22 +0000

    tuna 0.98

--- Casks/t/tuna.rb
+++ Casks/t/tuna.rb
@@ -1,6 +1,6 @@
 cask "tuna" do
-  version "0.97,2104"
-  sha256 "eb67be55bf67c4e8e55f8f980673d3897754cee691108a78e76acb1cbf5ab292"
+  version "0.98,2115"
+  sha256 "a595ef4626d5e118692e01f4b2e0e485db5cac252ed1e4f6a5a7d155dc56176c"
 
   url "https://tunaformac.com/download/releases/#{version.csv.second}"
   name "Tuna"
