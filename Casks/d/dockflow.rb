d09ee44edb68ef77334159bef4fcf6c5c3568f31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 13:00:35 +0000

    dockflow 1.81

--- Casks/d/dockflow.rb
+++ Casks/d/dockflow.rb
@@ -1,6 +1,6 @@
 cask "dockflow" do
-  version "1.80"
-  sha256 "5d13f08883676510724063854bd41d6e5bdb6c52f1ffa253682bd3e8ed92e62d"
+  version "1.81"
+  sha256 "d4c68721adfa6e07292c46ae6d72d92149839bababcb5ef2d42b0ac522ba46e9"
 
   url "https://github.com/AppitStudio/dock-flow-updates/releases/download/v#{version}/DockFlow.dmg"
   name "DockFlow"
