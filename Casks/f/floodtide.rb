39fb43b60566885546bba27a307d54eaa3ef74aa
Realm <76455975+Realm9758@users.noreply.github.com>
2026-09-08 18:52:00 +0100

    Add floodtide 1.1.0

--- Casks/f/floodtide.rb
+++ Casks/f/floodtide.rb
@@ -1,1 +1,31 @@
-Add floodtide 1.1.0
+cask "floodtide" do
+  version "1.1.0"
+  sha256 "b0691c8c5f3b0afe9a48f38cd8ee68c264d8cce6d417e3c27db7843c50dd5f6b"
+
+  url "https://bhopstudio.com/floodtide/downloads/Floodtide-#{version}.dmg"
+  name "Floodtide"
+  desc "Find and install updates for installed applications"
+  homepage "https://bhopstudio.com/floodtide"
+
+  livecheck do
+    url "https://bhopstudio.com/floodtide/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: :ventura
+
+  app "Floodtide.app"
+
+  uninstall launchctl:  "com.getfloodtide.floodtide.helper",
+            quit:       "com.getfloodtide.floodtide",
+            login_item: "Floodtide"
+
+  zap trash: [
+    "~/Library/Application Support/Floodtide",
+    "~/Library/Caches/com.getfloodtide.floodtide",
+    "~/Library/HTTPStorages/com.getfloodtide.floodtide",
+    "~/Library/Preferences/com.getfloodtide.floodtide.plist",
+    "~/Library/Saved Application State/com.getfloodtide.floodtide.savedState",
+  ]
+end
