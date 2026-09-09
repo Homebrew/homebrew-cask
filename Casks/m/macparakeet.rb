55208426435f55040755568aa45a31ed0088819a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 22:19:46 +0000

    macparakeet 0.8.0

--- Casks/m/macparakeet.rb
+++ Casks/m/macparakeet.rb
@@ -1,6 +1,6 @@
 cask "macparakeet" do
-  version "0.7.3"
-  sha256 "0c4ccb1b6f0cedae0f7994afce393094596aacdd8548af374ae18ae500ecdd8a"
+  version "0.8.0"
+  sha256 "a82b5f5e1c64272766e09a1985b627aad41960c6eba3ac7b1b9359927b8da474"
 
   url "https://github.com/moona3k/macparakeet/releases/download/v#{version}/MacParakeet.dmg"
   name "MacParakeet"
