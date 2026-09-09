a80415075acf09b6e1aa6fc738b82b15d63f69dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 16:48:21 +0000

    muse-code 1.1.1-R2514.1

--- Casks/m/muse-code.rb
+++ Casks/m/muse-code.rb
@@ -2,11 +2,11 @@ cask "muse-code" do
   arch arm: "aarch64", intel: "x86"
   os macos: "macos", linux: "linux"
 
-  version "1.0.3-R2198.1"
-  sha256 arm:          "4c0f960028b603174af7df7bd5051d8c35d6c1aa372a37d18bc770926a0577a7",
-         intel:        "dbcee07bd234fc19805d5d6a358c591b79cc7d781311d10219f856d934843ac2",
-         arm64_linux:  "4ffcf55f5eb0668643f30c5febd90d188b9a2da65858918444d31f6046940120",
-         x86_64_linux: "75a68f98c437dfd17d264730c5bc72d57e5f1e18d10472a9f53261ffcc091352"
+  version "1.1.1-R2514.1"
+  sha256 arm:          "7dfd75e1e2dd7c763e60b7e379f4146b8b2b10478a2830c8cdbb35ae9961881e",
+         intel:        "103eed33aa1320aab0be5584e5177954f961663e517126ddd567859085aacd5d",
+         arm64_linux:  "8836ca0f525f4d2bfd22e6891dbafa081dbdaa42bff71d090d85ad7ec8465d44",
+         x86_64_linux: "47908f2e5b0ac5b0c0ba147a48b30a9e48c0aebea3c5cc891ad34d95425a4993"
 
   on_macos do
     depends_on macos: :monterey
