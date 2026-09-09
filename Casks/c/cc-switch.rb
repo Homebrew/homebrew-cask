ed400cf16b03dbf1184429e890b97d6e2bd83ade
botantony <antonsm21@gmail.com>
2026-09-09 13:59:33 +0200

    cc-switch: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/cc-switch.rb
+++ Casks/c/cc-switch.rb
@@ -3,10 +3,12 @@ cask "cc-switch" do
   url_end = on_system_conditional macos: "macOS.dmg", linux: "Linux-#{arch}.AppImage"
 
   version "3.20.2"
+  sha256 arm:          "847327c8acf320b8f3e1122676dd3921ff4dc7aa02ed1974579c8d7b00894dec",
+         intel:        "847327c8acf320b8f3e1122676dd3921ff4dc7aa02ed1974579c8d7b00894dec",
+         arm64_linux:  "13718d69fe4d8db6898b5f14e91b0f6e0d4f6a3b308549dcaf16bd2c4711ff51",
+         x86_64_linux: "51b871f2b3464429fe19958613fe1bf63b3bef688499b5676ce7a859e64b5f12"
 
   on_macos do
-    sha256 "847327c8acf320b8f3e1122676dd3921ff4dc7aa02ed1974579c8d7b00894dec"
-
     depends_on macos: :monterey
 
     app "CC Switch.app"
@@ -21,9 +23,6 @@ cask "cc-switch" do
     ]
   end
   on_linux do
-    sha256 arm64_linux:  "13718d69fe4d8db6898b5f14e91b0f6e0d4f6a3b308549dcaf16bd2c4711ff51",
-           x86_64_linux: "51b871f2b3464429fe19958613fe1bf63b3bef688499b5676ce7a859e64b5f12"
-
     app_image "CC-Switch-v#{version}-Linux-#{arch}.AppImage", target: "CC Switch.AppImage"
   end
 
