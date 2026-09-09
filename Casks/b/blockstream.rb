b31c0c326309a7d23a305149f875140b78225e03
botantony <antonsm21@gmail.com>
2026-09-09 13:59:32 +0200

    blockstream: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/blockstream.rb
+++ Casks/b/blockstream.rb
@@ -3,11 +3,11 @@ cask "blockstream" do
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "3.5.3"
+  sha256 arm:          "38d50624aa55ef6e8c08362711af99781643135b87b0479b0c5808711a8fa0d9",
+         intel:        "258904592f787d48e71bb2829695b8079d2f0af46ebd1e9fde74e4b8805ad70c",
+         x86_64_linux: "9e7091654abb460cd8a9fd3fa72324ab5e3422a91f483d89425d9e42325ee7ac"
 
   on_macos do
-    sha256 arm:   "38d50624aa55ef6e8c08362711af99781643135b87b0479b0c5808711a8fa0d9",
-           intel: "258904592f787d48e71bb2829695b8079d2f0af46ebd1e9fde74e4b8805ad70c"
-
     depends_on macos: :ventura
 
     app "Blockstream.app"
@@ -18,8 +18,6 @@ cask "blockstream" do
     ]
   end
   on_linux do
-    sha256 "9e7091654abb460cd8a9fd3fa72324ab5e3422a91f483d89425d9e42325ee7ac"
-
     depends_on arch: :x86_64
 
     app_image "Blockstream-#{arch}.AppImage", target: "Blockstream.AppImage"
