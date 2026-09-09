4de4fbe631a931fe91d3b16b864921a95fe7ed58
botantony <antonsm21@gmail.com>
2026-09-09 13:59:36 +0200

    db-browser-for-sqlcipher@nightly: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/d/db-browser-for-sqlcipher@nightly.rb
+++ Casks/d/db-browser-for-sqlcipher@nightly.rb
@@ -4,10 +4,12 @@ cask "db-browser-for-sqlcipher@nightly" do
   url_end = on_system_conditional macos: ".dmg", linux: "-#{arch}.AppImage"
 
   version "20260909"
+  sha256 arm:          "a5b5f52134376e5bda35dffad77d55788305adf4253519e1818f6bc740a257fd",
+         intel:        "a5b5f52134376e5bda35dffad77d55788305adf4253519e1818f6bc740a257fd",
+         arm64_linux:  "c33fc3fd32654d4695d787cfa65546987fcd02c3a862a4d2af33bc3ecb99aaa4",
+         x86_64_linux: "5f3e64cfbf02a53fdc23343f090ebc7713273ecd9c2a49fac3177fe7be9f2e8a"
 
   on_macos do
-    sha256 "a5b5f52134376e5bda35dffad77d55788305adf4253519e1818f6bc740a257fd"
-
     app "DB Browser for SQLCipher Nightly.app"
 
     zap trash: [
@@ -17,9 +19,6 @@ cask "db-browser-for-sqlcipher@nightly" do
     ]
   end
   on_linux do
-    sha256 arm64_linux:  "c33fc3fd32654d4695d787cfa65546987fcd02c3a862a4d2af33bc3ecb99aaa4",
-           x86_64_linux: "5f3e64cfbf02a53fdc23343f090ebc7713273ecd9c2a49fac3177fe7be9f2e8a"
-
     app_image "DB.Browser.for.SQLCipher-#{version}-#{arch}.AppImage",
               target: "DBBrowserForSQLCipherNightly.AppImage"
   end
