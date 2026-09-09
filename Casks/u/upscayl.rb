a007ed591ec1c835645109a34224f86823e28fb7
botantony <antonsm21@gmail.com>
2026-09-09 13:59:42 +0200

    upscayl: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/u/upscayl.rb
+++ Casks/u/upscayl.rb
@@ -2,10 +2,11 @@ cask "upscayl" do
   os = on_system_conditional macos: "mac.dmg", linux: "linux.AppImage"
 
   version "2.15.0"
+  sha256 arm:          "0e53c9ee8c1800cb3e2ce0f574e4e1a35a51945e19ff2b93f33928bbd7fd4c5a",
+         intel:        "0e53c9ee8c1800cb3e2ce0f574e4e1a35a51945e19ff2b93f33928bbd7fd4c5a",
+         x86_64_linux: "6459457e589bcb99e779ea442dce608bacda33916b7019a8862b7c6252aa8229"
 
   on_macos do
-    sha256 "0e53c9ee8c1800cb3e2ce0f574e4e1a35a51945e19ff2b93f33928bbd7fd4c5a"
-
     depends_on macos: :monterey
 
     app "Upscayl.app"
@@ -17,8 +18,6 @@ cask "upscayl" do
     ]
   end
   on_linux do
-    sha256 "6459457e589bcb99e779ea442dce608bacda33916b7019a8862b7c6252aa8229"
-
     depends_on arch: :x86_64
 
     app_image "upscayl-#{version}-linux.AppImage", target: "Upscayl.AppImage"
