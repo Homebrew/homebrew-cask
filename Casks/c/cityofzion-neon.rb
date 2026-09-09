a536d2f9f14c5afa760ba626fe77ac0508759b88
botantony <antonsm21@gmail.com>
2026-09-09 13:59:34 +0200

    cityofzion-neon: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/cityofzion-neon.rb
+++ Casks/c/cityofzion-neon.rb
@@ -3,11 +3,11 @@ cask "cityofzion-neon" do
   url_end = on_system_conditional macos: ".#{arch}.dmg", linux: ".AppImage"
 
   version "2.25.1"
+  sha256 arm:          "6b4b6008d2984484e2848d352616ef3263529ca5d0f0738ebf04300e6e6beb02",
+         intel:        "224141c65c163a6c8aa721a9e03064a7dbaeb63aa199e08d1c4230b53ac5f117",
+         x86_64_linux: "154668ad628996296ac24dadce210cad87df21431401170c4e0b093f5d5be5a3"
 
   on_macos do
-    sha256 arm:   "6b4b6008d2984484e2848d352616ef3263529ca5d0f0738ebf04300e6e6beb02",
-           intel: "224141c65c163a6c8aa721a9e03064a7dbaeb63aa199e08d1c4230b53ac5f117"
-
     app "Neon.app"
 
     zap trash: [
@@ -18,8 +18,6 @@ cask "cityofzion-neon" do
     ]
   end
   on_linux do
-    sha256 "154668ad628996296ac24dadce210cad87df21431401170c4e0b093f5d5be5a3"
-
     depends_on arch: :x86_64
 
     app_image "Neon.#{version}.AppImage", target: "Neon.AppImage"
