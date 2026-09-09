cd73423269f99ee3ffa11f6fce3618b590f5b19c
botantony <antonsm21@gmail.com>
2026-09-09 13:59:28 +0200

    appflowy: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -5,11 +5,11 @@ cask "appflowy" do
   url_end = on_system_conditional macos: "zip", linux: "AppImage"
 
   version "0.14.1"
+  sha256 arm:          "a7ce5180eb2086d4dfd3e3fa8cf89fb4508a71e5a48bf5ff8615e7c4ce669101",
+         intel:        "536f2edb4f304d775df8d4c53fd3d210ab8b447b038f4d3035a23802cc3fdcf5",
+         x86_64_linux: "b2581ce65dc9b5ac0eec556e5c4491f3a62060c09a2912f16db08584e98bc1fd"
 
   on_macos do
-    sha256 arm:   "a7ce5180eb2086d4dfd3e3fa8cf89fb4508a71e5a48bf5ff8615e7c4ce669101",
-           intel: "536f2edb4f304d775df8d4c53fd3d210ab8b447b038f4d3035a23802cc3fdcf5"
-
     depends_on macos: :monterey
 
     app "AppFlowy-#{arch}.app", target: "AppFlowy.app"
@@ -20,8 +20,6 @@ cask "appflowy" do
     ]
   end
   on_linux do
-    sha256 "b2581ce65dc9b5ac0eec556e5c4491f3a62060c09a2912f16db08584e98bc1fd"
-
     depends_on arch: :x86_64
 
     app_image "AppFlowy-#{version}-linux-#{arch}.AppImage", target: "AppFlowy.AppImage"
