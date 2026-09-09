4c3acb4b7449a72877328977014c72be11314780
botantony <antonsm21@gmail.com>
2026-09-09 13:59:40 +0200

    p2p-kiwi: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/p2p-kiwi.rb
+++ Casks/p/p2p-kiwi.rb
@@ -3,10 +3,12 @@ cask "p2p-kiwi" do
   url_end = on_system_conditional macos: "universal.dmg", linux: "#{arch}.AppImage"
 
   version "1.0.0"
+  sha256 arm:          "18d6082cdcf073e0842d82d9235882454ed42325868547da56700a4542703b6f",
+         intel:        "18d6082cdcf073e0842d82d9235882454ed42325868547da56700a4542703b6f",
+         arm64_linux:  "ee1db1cfd19c508b7c02dd4fd4c3334a9bbc8377e093c0283c79388871983bb9",
+         x86_64_linux: "7bf9b8e2b4e200cc567bdfa570a46e377c355090621125bc1ce1a50671f60386"
 
   on_macos do
-    sha256 "18d6082cdcf073e0842d82d9235882454ed42325868547da56700a4542703b6f"
-
     depends_on macos: :ventura
 
     app "p2p.kiwi.app"
@@ -18,9 +20,6 @@ cask "p2p-kiwi" do
     ]
   end
   on_linux do
-    sha256 arm64_linux:  "ee1db1cfd19c508b7c02dd4fd4c3334a9bbc8377e093c0283c79388871983bb9",
-           x86_64_linux: "7bf9b8e2b4e200cc567bdfa570a46e377c355090621125bc1ce1a50671f60386"
-
     app_image "p2p-kiwi_#{arch}.AppImage", target: "p2p.kiwi.AppImage"
   end
 
