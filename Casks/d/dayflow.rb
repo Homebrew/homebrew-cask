ed658d21f56670cd1308caffa654dfae78e9be6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 15:56:21 +0000

    dayflow 2.4.2

--- Casks/d/dayflow.rb
+++ Casks/d/dayflow.rb
@@ -1,6 +1,6 @@
 cask "dayflow" do
-  version "2.4.0"
-  sha256 "16183ec026b3f790a2b5d33d6b88ece27780c40acad9fc4fa6c0643751aaff60"
+  version "2.4.2"
+  sha256 "c4a24d47530a61587e8b8be0987ee683e586c5e82a81cd5e857da0f4cdd7f87d"
 
   url "https://github.com/JerryZLiu/Dayflow/releases/download/v#{version}/Dayflow.dmg"
   name "Dayflow"
