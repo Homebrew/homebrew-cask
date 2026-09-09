13b7edaceef24518298132f49ca5a93c28bdc238
botantony <antonsm21@gmail.com>
2026-09-09 13:59:33 +0200

    cables: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/cables.rb
+++ Casks/c/cables.rb
@@ -3,11 +3,11 @@ cask "cables" do
   url_end = on_system_conditional macos: "mac#{arch}.dmg", linux: "linux-x64.AppImage"
 
   version "0.11.1"
+  sha256 arm:          "a03b1051f00061a991fcc11b3771fd09eb890f5febd313b912000ead380f3e01",
+         intel:        "2b207148c5297275d4657d1e9cd25bca75860eba9f130af9b93b553774ce57e6",
+         x86_64_linux: "e42bb406228b0d34e14acc45b739e8c1169528e9bf469273473924018aa60acb"
 
   on_macos do
-    sha256 arm:   "a03b1051f00061a991fcc11b3771fd09eb890f5febd313b912000ead380f3e01",
-           intel: "2b207148c5297275d4657d1e9cd25bca75860eba9f130af9b93b553774ce57e6"
-
     depends_on macos: :monterey
 
     app "cables.app"
@@ -23,8 +23,6 @@ cask "cables" do
     ]
   end
   on_linux do
-    sha256 "e42bb406228b0d34e14acc45b739e8c1169528e9bf469273473924018aa60acb"
-
     depends_on arch: :x86_64
 
     app_image "cables-#{version}-linux-x64.AppImage", target: "Cables.AppImage"
