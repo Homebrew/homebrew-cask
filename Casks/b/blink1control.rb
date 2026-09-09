6477ba36ad7e56b5598dfbf05232a7e387dae014
botantony <antonsm21@gmail.com>
2026-09-09 13:59:31 +0200

    blink1control: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/blink1control.rb
+++ Casks/b/blink1control.rb
@@ -4,11 +4,11 @@ cask "blink1control" do
   url_end = on_system_conditional macos: "mac-#{arch}.dmg", linux: "linux-x86_64.AppImage"
 
   version "2.3.0"
+  sha256 arm:          "4b05ba63024c00c1226acdd48337fc0258d6cfb045d6ae720e588e11da78d13c",
+         intel:        "778dad2b5914e6e2cb792260e24fed3a490a3d762c6693033fa89472720ce737",
+         x86_64_linux: "147c220e339140ad6c6c3cf426aba56ded7dd1347fc94a42f4e71493398cffa5"
 
   on_macos do
-    sha256 arm:   "4b05ba63024c00c1226acdd48337fc0258d6cfb045d6ae720e588e11da78d13c",
-           intel: "778dad2b5914e6e2cb792260e24fed3a490a3d762c6693033fa89472720ce737"
-
     depends_on macos: :big_sur
 
     app "Blink1Control#{version.major}.app"
@@ -21,8 +21,6 @@ cask "blink1control" do
     ]
   end
   on_linux do
-    sha256 "147c220e339140ad6c6c3cf426aba56ded7dd1347fc94a42f4e71493398cffa5"
-
     depends_on arch: :x86_64
 
     app_image "Blink1Control#{version.major}-#{version}-linux-x86_64.AppImage",
