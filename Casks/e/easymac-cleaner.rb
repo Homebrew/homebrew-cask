8f53f6fe8f4716b87c1605accd8901e289ee1779
MartianCat <hello@martiancat.space>
2026-09-08 21:19:35 -0300

    Update Casks/e/easymac-cleaner.rb
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/e/easymac-cleaner.rb
+++ Casks/e/easymac-cleaner.rb
@@ -5,7 +5,7 @@ cask "easymac-cleaner" do
   url "https://updates.martiancat.space/cleaner/EasyMacCleaner-#{version}.zip"
   name "EasyMac Cleaner"
   desc "Cleaning, privacy, and system optimisation utility"
-  homepage "https://martiancat.space/"
+  homepage "https://martiancat.space/products/cleaner.html"
 
   livecheck do
     url "https://updates.martiancat.space/cleaner/appcast.xml"
