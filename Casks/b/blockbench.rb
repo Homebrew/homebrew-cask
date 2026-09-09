d822e4eec6aad3e6cb0eea59ee7d84717d1870e3
botantony <antonsm21@gmail.com>
2026-09-09 13:59:31 +0200

    blockbench: move `sha256` out of `on_{system}` blocks
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/b/blockbench.rb
+++ Casks/b/blockbench.rb
@@ -4,11 +4,11 @@ cask "blockbench" do
   url_arch = on_system_conditional macos: "#{arch}_"
 
   version "5.1.6"
+  sha256 arm:          "84d3f632d8c35c40174a05e0b52f035919722cd1b5175228751a09e550a58529",
+         intel:        "c9d1a31820f0bea0c9187c679c45d81ac62f54aa73b7b5823120d4d859cf71c1",
+         x86_64_linux: "c6dd92036f3c10495df53911a74e5b00a1d557ea13e506084177ef55a5cd7c0e"
 
   on_macos do
-    sha256 arm:   "84d3f632d8c35c40174a05e0b52f035919722cd1b5175228751a09e550a58529",
-           intel: "c9d1a31820f0bea0c9187c679c45d81ac62f54aa73b7b5823120d4d859cf71c1"
-
     depends_on macos: :monterey
 
     app "Blockbench.app"
@@ -21,8 +21,6 @@ cask "blockbench" do
     ]
   end
   on_linux do
-    sha256 "c6dd92036f3c10495df53911a74e5b00a1d557ea13e506084177ef55a5cd7c0e"
-
     depends_on arch: :x86_64
 
     app_image "Blockbench_#{version}.AppImage", target: "Blockbench.AppImage"
