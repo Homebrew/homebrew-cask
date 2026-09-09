b9dd25acfa172a42256ab4d62aefe2191652e524
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 18:48:32 +0000

    cloudflare-warp@beta 2026.8.1290.1

--- Casks/c/cloudflare-warp@beta.rb
+++ Casks/c/cloudflare-warp@beta.rb
@@ -1,6 +1,6 @@
 cask "cloudflare-warp@beta" do
-  version "2026.7.1210.1"
-  sha256 "44e16d2848bee801f759a915623b0b819a48aba834c1fe35be8724bacf13bda9"
+  version "2026.8.1290.1"
+  sha256 "12978bbc584409ccd3aca6c3c7de73dbdbcc0436db2956ef9c1a26a7356bb0b7"
 
   url "https://downloads.cloudflareclient.com/v1/download/macos/version/#{version}"
   name "Cloudflare WARP"
