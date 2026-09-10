a1367ad8f3b7e2b704fd74d47902cfe3212d38ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 02:09:45 +0000

    omniwm 0.6.9

--- Casks/o/omniwm.rb
+++ Casks/o/omniwm.rb
@@ -1,6 +1,6 @@
 cask "omniwm" do
-  version "0.6.8"
-  sha256 "0823963c8a5c3bde854f7fdd03cd8c25c4421a40bd6f74844f2c8f7c16a26765"
+  version "0.6.9"
+  sha256 "9c24ab174e11a1e21155a0640e0323beaa7e22a402e59b69694c8465b002b165"
 
   url "https://github.com/BarutSRB/OmniWM/releases/download/v#{version}/OmniWM-v#{version}.zip"
   name "OmniWM"
