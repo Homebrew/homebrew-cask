3e68df6fa307034e5bfc279c7ed553eb41422f58
botantony <antonsm21@gmail.com>
2026-09-10 13:49:27 +0200

    prowlarr: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/p/prowlarr.rb
+++ Casks/p/prowlarr.rb
@@ -20,7 +20,7 @@ cask "prowlarr" do
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   auto_updates true
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "Prowlarr.app"
 
