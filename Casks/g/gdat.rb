dad2cfc6cb7f700a819c53fcae15e92958f0fe3e
botantony <antonsm21@gmail.com>
2026-09-10 13:48:43 +0200

    gdat: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/g/gdat.rb
+++ Casks/g/gdat.rb
@@ -20,7 +20,7 @@ cask "gdat" do
 
   disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-  depends_on macos: :big_sur
+  depends_on :macos
   container nested: "macOS 64 bit/Genealogical DNA Analysis Tool.app.tar"
 
   app "Genealogical DNA Analysis Tool.app"
