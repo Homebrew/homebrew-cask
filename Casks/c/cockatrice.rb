17e10dc1d952cdf1d73065c1488290c72c3f7152
botantony <antonsm21@gmail.com>
2026-09-10 13:48:18 +0200

    cockatrice: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/c/cockatrice.rb
+++ Casks/c/cockatrice.rb
@@ -56,7 +56,7 @@ cask "cockatrice" do
   desc "Virtual tabletop for multiplayer card games"
   homepage "https://cockatrice.github.io/"
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "cockatrice.app"
   app "oracle.app"
