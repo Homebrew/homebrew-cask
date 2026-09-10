ede881ed0ee4cd43674ac49d9038f6361a096359
botantony <antonsm21@gmail.com>
2026-09-10 13:49:44 +0200

    supercollider: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/supercollider.rb
+++ Casks/s/supercollider.rb
@@ -12,7 +12,7 @@ cask "supercollider" do
     regex(/href=.*?SuperCollider[._-]v?(\d+(?:\.\d+)+)[._-]macOS(?:[._-]universal)?\.dmg/i)
   end
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "SuperCollider.app"
 
