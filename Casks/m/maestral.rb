6548620966eb34200648db8639e6e547744e9e7f
botantony <antonsm21@gmail.com>
2026-09-10 13:49:03 +0200

    maestral: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/m/maestral.rb
+++ Casks/m/maestral.rb
@@ -13,7 +13,7 @@ cask "maestral" do
   end
 
   auto_updates true
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "Maestral.app"
   binary "#{appdir}/Maestral.app/Contents/MacOS/maestral-cli", target: "maestral"
