f7e25bc23d8df50ab7588abdbb76061147251812
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 10:41:23 +0000

    singlebox 67.5.0

--- Casks/s/singlebox.rb
+++ Casks/s/singlebox.rb
@@ -1,6 +1,6 @@
 cask "singlebox" do
-  version "67.4.0"
-  sha256 "11c134f99adf5227d949c34b176592a0a665d4e78ed893ef45fd675a20cb86c3"
+  version "67.5.0"
+  sha256 "a50bd5d5c525813e26fc90163185b4e772f5b35df89cb97e7b4c5c55004678d8"
 
   url "https://cdn-2.webcatalog.io/singlebox2/Singlebox-#{version}-universal.dmg"
   name "Singlebox"
