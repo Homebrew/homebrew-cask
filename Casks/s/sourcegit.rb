cff154527cdd765a0a92910b411fecdc03e50429
botantony <antonsm21@gmail.com>
2026-09-10 13:49:41 +0200

    sourcegit: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/s/sourcegit.rb
+++ Casks/s/sourcegit.rb
@@ -15,7 +15,7 @@ cask "sourcegit" do
     strategy :github_latest
   end
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "SourceGit.app"
   binary "#{appdir}/SourceGit.app/Contents/MacOS/SourceGit", target: "sourcegit"
