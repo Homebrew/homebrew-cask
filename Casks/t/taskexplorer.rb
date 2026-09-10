527e919c26d1ef70c8be84ca4b3e418020008df6
botantony <antonsm21@gmail.com>
2026-09-10 13:49:45 +0200

    taskexplorer: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/t/taskexplorer.rb
+++ Casks/t/taskexplorer.rb
@@ -7,7 +7,7 @@ cask "taskexplorer" do
   desc "Tool to explore all the running tasks (processes)"
   homepage "https://objective-see.org/products/taskexplorer.html"
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "TaskExplorer.app"
 
