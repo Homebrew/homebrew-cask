1a5224ad305318770e64cb37e5d11bc4a120eb1c
botantony <antonsm21@gmail.com>
2026-09-10 13:49:18 +0200

    orcaslicer@nightly: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/o/orcaslicer@nightly.rb
+++ Casks/o/orcaslicer@nightly.rb
@@ -10,7 +10,7 @@ cask "orcaslicer@nightly" do
   homepage "https://github.com/OrcaSlicer/OrcaSlicer"
 
   conflicts_with cask: "orcaslicer"
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "OrcaSlicer.app"
 
