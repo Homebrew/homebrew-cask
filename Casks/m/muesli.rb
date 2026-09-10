88a420dfbd135190770d157d3ffd121929d05e6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 01:57:25 +0000

    muesli 0.8.4

--- Casks/m/muesli.rb
+++ Casks/m/muesli.rb
@@ -1,6 +1,6 @@
 cask "muesli" do
-  version "0.8.3"
-  sha256 "ac1e689e6fd7af5dbd3c7a6b02fd00f703e03ffa2191305e425fc97c500d4018"
+  version "0.8.4"
+  sha256 "79727313003ddb29289b6872a094a84ec9b2a011046a1344cea7e3598c4fc596"
 
   url "https://github.com/Muesli-HQ/muesli/releases/download/v#{version}/Muesli-#{version}.dmg"
   name "Muesli"
