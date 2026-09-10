54f5c6e6dac25259e3453e0ee410d990e7616514
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 07:43:41 +0000

    termius@beta 10.0.1

--- Casks/t/termius@beta.rb
+++ Casks/t/termius@beta.rb
@@ -1,7 +1,7 @@
 cask "termius@beta" do
   arch arm: "mac-beta-arm64", intel: "mac-beta"
 
-  version "10.0.0"
+  version "10.0.1"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
