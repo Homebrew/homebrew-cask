57250e62e1131da99be2de1054e33fbc6cbb12ff
botantony <antonsm21@gmail.com>
2026-09-10 13:48:58 +0200

    lapce: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/l/lapce.rb
+++ Casks/l/lapce.rb
@@ -7,7 +7,7 @@ cask "lapce" do
   desc "Open source code editor written in Rust"
   homepage "https://lapce.dev/"
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "Lapce.app"
   binary "#{appdir}/Lapce.app/Contents/MacOS/lapce"
