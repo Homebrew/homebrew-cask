70a9707dac1effa9182f805615ade86c7be48426
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 03:12:14 +0000

    token-monitor 0.55.0

--- Casks/t/token-monitor.rb
+++ Casks/t/token-monitor.rb
@@ -1,9 +1,9 @@
 cask "token-monitor" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.54.0"
-  sha256 arm:   "deaea5fe4695864d996c53addbd756d8d859b804410cbe33b1438b56c092b714",
-         intel: "583f4c7afd71214e3dcca7fb70e1626b77e26f043cecbb1b0105e81909ab0ead"
+  version "0.55.0"
+  sha256 arm:   "5526e22a8faa8ab29a8091bd187ee9359bafb3fe5822c6d18ebc6c6792a8b9d7",
+         intel: "92c27c756d691d6556bf9ca8eeecc3580c5cbc9ba4a90ab49426af3f92a699db"
 
   url "https://github.com/Javis603/token-monitor/releases/download/v#{version}/Token-Monitor-#{version}-#{arch}.dmg"
   name "Token Monitor"
