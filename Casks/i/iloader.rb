fe3f5cbed09163c8cda4f3491fe6fa87c9bb198c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 21:59:16 +0000

    iloader 2.3.3

--- Casks/i/iloader.rb
+++ Casks/i/iloader.rb
@@ -2,11 +2,11 @@ cask "iloader" do
   arch arm: "aarch64", intel: "amd64"
   os macos: "iloader-darwin-universal.dmg", linux: "iloader-linux-#{arch}.AppImage"
 
-  version "2.3.2"
-  sha256 arm:          "5569719659f1bf8dce2df5603a927ab1e3cce99e4363f478caf193179d3ca189",
-         intel:        "5569719659f1bf8dce2df5603a927ab1e3cce99e4363f478caf193179d3ca189",
-         arm64_linux:  "4ce31c41eca9f86c529023f19f1e7fd9b70292976f42760703819dd1c590da28",
-         x86_64_linux: "607b39849dfd04799118fcbb6256c74bb374651fd11d01436e99ea6d5b12611a"
+  version "2.3.3"
+  sha256 arm:          "9869447506cdbe990944f159f929b906cb71bb1d6d659efc52c8c9ca7675bcbd",
+         intel:        "9869447506cdbe990944f159f929b906cb71bb1d6d659efc52c8c9ca7675bcbd",
+         arm64_linux:  "84a3c95a0d17a56732d6c8202ac70207575cb8133c29ea95f8818e298b5b8e2a",
+         x86_64_linux: "a9e841259cfec05065dad31428dd1b27b6c3321f10d310cb0315082836b83b7e"
 
   on_macos do
     auto_updates true
