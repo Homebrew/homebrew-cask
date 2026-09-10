b1f671ccef7a17d930aa3a7593850dc175f9ceaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 15:37:57 +0000

    aside 1.0.910.1

--- Casks/a/aside.rb
+++ Casks/a/aside.rb
@@ -1,6 +1,6 @@
 cask "aside" do
-  version "1.0.825.1"
-  sha256 "91d3e6c0fc2256338a1c2f85ae0481b2ceb4a21c0f17105d4cdf29bbd535ba9b"
+  version "1.0.910.1"
+  sha256 "b71fc04ea5c89cd5e59f9ef50c15871e8a602bd27c3aee4d1df1a108677c8079"
 
   url "https://releases.aside.com/dev-updater/Aside-#{version}.dmg"
   name "Aside"
