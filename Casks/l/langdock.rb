d092eaa6d23cb240d80219cfa6a1c962c12f8a49
viegelinsch <viegelinsch@users.noreply.github.com>
2026-09-10 14:29:35 +0200

    fixed url livecheck

--- Casks/l/langdock.rb
+++ Casks/l/langdock.rb
@@ -2,12 +2,16 @@ cask "langdock" do
   version "1.0.5"
   sha256 "41f4a6267fdb582eea458de4a22218193e33adf0043e4816d2167d320e784f41"
 
-  url "https://desktop.langdock.com/global/stable/Langdock-1.0.5-universal.dmg"
+  url "https://desktop.langdock.com/global/stable/Langdock-#{version}-universal.dmg"
   name "Langdock"
   desc "Platform for AI Adoption"
   homepage "https://langdock.com/products/desktop"
 
-  depends_on :macos
+  livecheck do
+    skip "Upstream provides no machine-readable version listing"
+  end
+
+  depends_on macos: :sonoma
 
   app "Langdock.app"
 end
