db9b040b5f5ac8abd85259a681badfd7e6aec12a
Waldir Pimenta <waldyrious@gmail.com>
2026-09-10 21:24:23 +0100

    kirocrew: add Linux AppImage support

--- Casks/k/kirocrew.rb
+++ Casks/k/kirocrew.rb
@@ -1,8 +1,23 @@
 cask "kirocrew" do
-  version "0.5.0"
-  sha256 "95a70c62057b08827aa3e8d22ccaf72699bc15e6b0ad85894806aae28c74c8af"
+  arch arm: "aarch64", intel: "x86_64"
+  url_end = on_system_conditional macos: ".dmg", linux: "-#{arch}.AppImage"
 
-  url "https://download.crew.kiro.dev/desktop/stable/#{version}/KiroCrew.dmg"
+  version "0.5.0"
+  sha256 arm:          "95a70c62057b08827aa3e8d22ccaf72699bc15e6b0ad85894806aae28c74c8af",
+         intel:        "95a70c62057b08827aa3e8d22ccaf72699bc15e6b0ad85894806aae28c74c8af",
+         arm64_linux:  "78bf337c2273960d5f138c0024e6190f1dd866bdfd2fb49504d423a06c433872",
+         x86_64_linux: "df5b2e85a07cbabfac417676dd2d791670d721e3c3f69fd20c51b7bab2b97379"
+
+  on_macos do
+    depends_on macos: :monterey
+
+    app "KiroCrew.app"
+  end
+  on_linux do
+    app_image "KiroCrew-#{arch}.AppImage", target: "KiroCrew.AppImage"
+  end
+
+  url "https://download.crew.kiro.dev/desktop/stable/#{version}/KiroCrew#{url_end}"
   name "Kiro Crew"
   desc "Persistent AI development workspace with multi-agent support"
   homepage "https://kiro.dev/docs/crew/"
@@ -13,11 +28,10 @@ cask "kirocrew" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "KiroCrew.app"
 
   zap trash: [
+    "~/.cache/kirocrew-desktop-updater",
+    "~/.config/kirocrew-desktop",
     "~/.kirocrew.breadcrumb",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.amazon.kiro.crew.sfl*",
     "~/Library/Application Support/kirocrew-electron-mac",
