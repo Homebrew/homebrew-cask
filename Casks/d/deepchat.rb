8b139ec974a99fdf77bcd0906dcce12519efc211
botantony <antonsm21@gmail.com>
2026-09-10 13:48:26 +0200

    deepchat: `depends_on macos: :big_sur` -> `depends_on :macos`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/d/deepchat.rb
+++ Casks/d/deepchat.rb
@@ -19,7 +19,7 @@ cask "deepchat" do
   end
 
   auto_updates true
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "DeepChat.app"
   binary "#{appdir}/DeepChat.app/Contents/MacOS/DeepChat", target: "deepchat"
