b1da958e4ebb9cc042b0bbdc7508d4c3ba7b57a5
mingyinsun <mingyinsun@tencent.com>
2026-09-10 15:02:29 +0800

    tencent-yingyongbao 0.7.9,2112 (new cask)

---
+++ Casks/t/tencent-yingyongbao.rb
@@ -0,0 +1,19 @@
+cask "tencent-yingyongbao" do
+  version "0.7.9,2112"
+  sha256 "2dc719ec4c6207156513573336dc822b4bfe26ee63fc3491dfe1c9bba10ef788"
+
+  url "https://downmac.yyb.qq.com/channel/formal/raw/hJBBfZun3iTPqCMe/mac_yyb_#{version.csv.first}.2122.dmg?response-content-disposition=attachment%3Bfilename%3Dmacyyb_2102200000_8e8ca99bf44b44ff.dmg"
+  name "腾讯应用宝"
+  desc "Tencent application store"
+  homepage "https://sj.qq.com/download/macbrand"
+
+  livecheck do
+    skip "No reliable automated version detection available"
+  end
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: :monterey
+
+  app "YYBMacApp.app"
+end
