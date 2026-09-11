3265d5177fac0d6396344ae7fdcd547553d02bed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 19:20:48 +0000

    notepadexe 1.6.3

--- Casks/n/notepadexe.rb
+++ Casks/n/notepadexe.rb
@@ -1,6 +1,6 @@
 cask "notepadexe" do
-  version "1.6.2"
-  sha256 "24c0ef06023765ef6306feaa09edfae510b17af85d557935c42d0574ac8df859"
+  version "1.6.3"
+  sha256 "6d7b48a506a31ce506f60ac5ff91ad6c1a39b1024c734aad36dd69a2533797ef"
 
   url "https://github.com/notepadhq/notepadexe-public/releases/download/#{version}/Notepad.zip"
   name "Notepad.exe"
