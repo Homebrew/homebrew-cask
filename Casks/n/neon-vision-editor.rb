a20f4661ebb899efcdc29d6c2881b15fae0872aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 16:23:32 +0000

    neon-vision-editor 1.7.5

--- Casks/n/neon-vision-editor.rb
+++ Casks/n/neon-vision-editor.rb
@@ -1,6 +1,6 @@
 cask "neon-vision-editor" do
-  version "1.7.4"
-  sha256 "ce5be23c785f94b83af79f63125e90bcf8cf41b2249d2fca9ddd40949d852af6"
+  version "1.7.5"
+  sha256 "e958b449623be1ffb8ecbef464c3525d200d39b5072fb4962680eadde7b5b438"
 
   url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
   name "Neon Vision Editor"
