591a3bcfa3214092e70ab9e9863a9197b88ba582
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 15:42:06 +0000

    chatgpt 26.908.40401

--- Casks/c/chatgpt.rb
+++ Casks/c/chatgpt.rb
@@ -2,9 +2,9 @@ cask "chatgpt" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional intel: "-x64"
 
-  version "26.908.31748"
-  sha256 arm:   "81fe2b609c641ed1ba0d56ae8b985a9d5ee3fccc5d551d381616e3a5172204eb",
-         intel: "b1ccb58c1d362d3313412df05cb87df4b660fbf4949b1472dc284e17f0fd48d8"
+  version "26.908.40401"
+  sha256 arm:   "491670fc43c1f2c713961e18b6a0b5de19f114d41484ce2e264da1d9b7086fdf",
+         intel: "b036539549b704df34fe4313201e70e08564d9f19da691253b7ce7943d17cab5"
 
   url "https://persistent.oaistatic.com/codex-app-prod/ChatGPT-darwin-#{arch}-#{version}.zip"
   name "ChatGPT"
