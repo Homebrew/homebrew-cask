861b613b5746ee1b718989a2c7fbd0730b0340bb
Davy <95214375+thedavidweng@users.noreply.github.com>
2026-09-10 20:30:58 -0700

    workbuddy-cn 5.5.4.38151288-1ca4889a (new cask)

---
+++ Casks/w/workbuddy-cn.rb
@@ -0,0 +1,36 @@
+cask "workbuddy-cn" do
+  arch arm: "arm64", intel: "x64"
+
+  version "5.5.4.38151288-1ca4889a"
+  sha256 arm:   "f469b122d8095cfe3182bcd25bfcbfefbc75577c92fd4cdef877c9d5c84b88f3",
+         intel: "b59ca06e37fe30502b331912dc373c95c8f4f83e13faf6357d686060a70af71c"
+
+  url "https://download.codebuddy.cn/workbuddy/saas/darwin-#{arch}/WorkBuddy-darwin-#{arch}-#{version}.dmg"
+  name "WorkBuddy"
+  desc "AI agent for everyday office work"
+  homepage "https://www.workbuddy.cn/"
+
+  livecheck do
+    url "https://www.workbuddy.cn/v2/update?platform=workbuddy-darwin-arm64"
+    strategy :json do |json|
+      json["url"][/WorkBuddy-darwin-arm64-(.+)\.zip$/, 1]
+    end
+  end
+
+  depends_on macos: :big_sur
+
+  app "WorkBuddy.app"
+
+  uninstall quit: "com.tencent.workbuddy.mac"
+
+  zap trash: [
+    "~/.workbuddy",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tencent.workbuddy.mac.sfl*",
+    "~/Library/Application Support/com.tencent.workbuddy.mac",
+    "~/Library/Application Support/WorkBuddy",
+    "~/Library/Caches/com.tencent.workbuddy.mac",
+    "~/Library/HTTPStorages/com.tencent.workbuddy.mac",
+    "~/Library/Preferences/com.tencent.workbuddy.mac.plist",
+    "~/Library/WebKit/com.tencent.workbuddy.mac",
+  ]
+end
