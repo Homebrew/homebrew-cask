278d6616d92b8dd9a9ccab0d90432860d05cff9c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 01:16:53 +0000

    dbx 0.6.10

--- Casks/d/dbx.rb
+++ Casks/d/dbx.rb
@@ -2,11 +2,11 @@ cask "dbx" do
   arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "amd64")
   os macos: "dmg", linux: "AppImage"
 
-  version "0.6.9"
-  sha256 arm:          "280fff5cd03daa5b3595d08b8268003b1491304624ba355a4f9f5c0be7b2ed15",
-         intel:        "0b359771c68fc2959a528cf1ea8b9cd9d51a8adbd22538d585d7ababc71aa3a3",
-         arm64_linux:  "9937a226b01c11f7a49f5b67d0f6177d6265304217bf375f6cc1fbb32eae138d",
-         x86_64_linux: "f1877d255f6e5d78c9408f7ec064387fc1c600931a474d7602b45f317388a588"
+  version "0.6.10"
+  sha256 arm:          "5e1e85b0501b95146356a8fb573e530fa525dc68e62de1c355bee3b07240053b",
+         intel:        "5fdc73c34986d2f547d192644fe63c3056aa80fe25ccf0d0e2d0ec991255ef40",
+         arm64_linux:  "cd66a9e0c3813235dcc0e41dadb210b4f3302ba7c21da94c383e333863891975",
+         x86_64_linux: "69cbeb8dce877a8918193ee7019805491da6dfd239a44445824109dabc5056ad"
 
   on_macos do
     auto_updates true
