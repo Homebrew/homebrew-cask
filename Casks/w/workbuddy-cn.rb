78e5095928574dd18960e2db6b03843a95e6fc09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 16:50:50 +0000

    workbuddy-cn 5.5.6.38337834-5f969292

--- Casks/w/workbuddy-cn.rb
+++ Casks/w/workbuddy-cn.rb
@@ -1,9 +1,9 @@
 cask "workbuddy-cn" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.5.4.38151288-1ca4889a"
-  sha256 arm:   "f469b122d8095cfe3182bcd25bfcbfefbc75577c92fd4cdef877c9d5c84b88f3",
-         intel: "b59ca06e37fe30502b331912dc373c95c8f4f83e13faf6357d686060a70af71c"
+  version "5.5.6.38337834-5f969292"
+  sha256 arm:   "f54b701da51c079eab5097f58b8dfce9df38b43f2ac1d08d0a51efba481f6059",
+         intel: "207ef2904a029b6c0a0b7de68b28e038764c179488cb23c03b9a732741469158"
 
   url "https://download.codebuddy.cn/workbuddy/saas/darwin-#{arch}/WorkBuddy-darwin-#{arch}-#{version}.dmg"
   name "WorkBuddy"
