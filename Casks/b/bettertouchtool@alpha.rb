eb79a17b2bc0d1e3ee94d8a42a151c72837a3602
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 12:48:51 +0000

    bettertouchtool@alpha 6.809
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/b/bettertouchtool@alpha.rb
+++ Casks/b/bettertouchtool@alpha.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool@alpha" do
-  version "6.803,2026091001"
-  sha256 "54c2bd61bb30821ddd70b9c8e5019b3a3488ecb6acb7e2b3ad4bb0fa175e0076"
+  version "6.809,2026091103"
+  sha256 "b37437cb84636d2cc36a34d9da5adb7037c26caf9b76ecd14f72125dabfdb882"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
