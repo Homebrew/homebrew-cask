a52c0d50f5a73ef6264829cb0ecc77957fac6c12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 16:21:22 +0000

    motrix@beta 2.0.0-beta.36

--- Casks/m/motrix@beta.rb
+++ Casks/m/motrix@beta.rb
@@ -2,11 +2,11 @@ cask "motrix@beta" do
   arch arm: "-arm64", intel: on_system_conditional(macos: "-x64", linux: "-x86_64")
   os macos: "dmg", linux: "AppImage"
 
-  version "2.0.0-beta.35"
-  sha256 arm:          "5790e1d98c9ffb91b2e0d09fae24b940ad745fd5e83b4c06c001493768046c88",
-         intel:        "2dcfef9ca817e7dc1545f169a68408489cb79717da55987e6cf21735cbc3863e",
-         arm64_linux:  "d59e2ff0a615f38dbc7659aaf1df68a7a15eb384e5323ae71d73b50ee6802d0e",
-         x86_64_linux: "96519f2943efed95af5696d01a410ecdebbf1aa44b082f3e3e67225806da04b5"
+  version "2.0.0-beta.36"
+  sha256 arm:          "03943215e3868c221a3f6fad3f8bab01d9a94cac92751c1aa2b466e0041f7b31",
+         intel:        "aa3e1349dff6e51815f32f1b6e110930e5e4123a498185b6075d69827cabda61",
+         arm64_linux:  "d2a0134b2e2ebddd58b7612f378c41623ad216d861fbe91d588115f2aa5da85d",
+         x86_64_linux: "3d3687212d0640da1325fa623b844b13ea41b44cd7c886022eb881b9047c1e52"
 
   on_macos do
     depends_on macos: :ventura
