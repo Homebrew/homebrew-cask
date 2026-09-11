9ddc81ecdd0b8f1fcec6f29b225eec04e146747d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 18:53:15 +0000

    displaperture 3.0

--- Casks/d/displaperture.rb
+++ Casks/d/displaperture.rb
@@ -1,6 +1,6 @@
 cask "displaperture" do
-  version "2.3"
-  sha256 "abc53579993abb843cf0634ecf441a7a7277535ac5a440c6f9c4558c2a9aebf3"
+  version "3.0"
+  sha256 "aae036ad827760d949791989125fd1c7cbe1dbdc30e5726836aa63ccde78aadc"
 
   url "https://manytricks.com/download/_do_not_hotlink_/displaperture#{version.no_dots.ljust(3, "0")}.dmg"
   name "Displaperture"
