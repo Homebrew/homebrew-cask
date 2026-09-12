79843c186cb5e4f5568cb019ff5584f627a9e497
Sakutaro Shiina <companyemailetc@gmail.com>
2026-09-13 03:19:52 +0900

    poker-copilot: 8.23,10259

--- Casks/p/poker-copilot.rb
+++ Casks/p/poker-copilot.rb
@@ -1,15 +1,15 @@
 cask "poker-copilot" do
-  version "8.17,10148"
-  sha256 "ed9ab6a247ee28d93462daea040a52b5fbccd7fdc7d71ae46e3b26a4f01c511e"
+  version "8.23,10259"
+  sha256 "4d2ee12fb2f6be8875c05d5a9676abb81ccf4c2eb224d029f75bcc664ec203b7"
 
-  url "https://static.pokercopilot.com/pokercopilot_macos_#{version.csv.first.dots_to_underscores}_build_#{version.csv.second}.dmg"
+  url "https://static.pokercopilot.com/pokercopilot_macos_universal_#{version.csv.first.dots_to_underscores}_build_#{version.csv.second}.dmg"
   name "Poker Copilot"
   desc "Online poker HUD and tracking software"
   homepage "https://pokercopilot.com/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/pokercopilot_macos[._-]v?(\d+(?:[._]\d+)+)[._-]build[._-](\d+)\.dmg}i)
+    regex(/href=.*?pokercopilot_macos_universal[._-]v?(\d+(?:[._]\d+)+)[._-]build[._-](\d+)\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0].tr("_", ".")},#{match[1]}" }
     end
@@ -20,8 +20,4 @@ cask "poker-copilot" do
   app "Poker Copilot.app"
 
   zap trash: "~/Library/Application Support/com.barbarysoftware.pokercopilot"
-
-  caveats do
-    requires_rosetta
-  end
 end
