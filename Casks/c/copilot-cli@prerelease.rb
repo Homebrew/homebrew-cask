758500f4072096ec839640c788348ba3c95ba8d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-12 01:14:47 +0000

    copilot-cli@prerelease 1.0.84-5

--- Casks/c/copilot-cli@prerelease.rb
+++ Casks/c/copilot-cli@prerelease.rb
@@ -2,11 +2,11 @@ cask "copilot-cli@prerelease" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "1.0.84-4"
-  sha256 arm:          "6efa73ea716882145cee49575a60e87ba510cab1bc5ecf366e3f5d062b51384b",
-         intel:        "2f8233ab2a5fd6209b90f75a53cd6e31626fffb112d95fd05c7834934e269742",
-         arm64_linux:  "f4ecb3a36f5b8f2bff67032020c68a3cd3f671d6873c9215a0112d99df73bfaf",
-         x86_64_linux: "0505274eba180bb10385f38b557a71ab915da11fb5cdd62322ee6e44ffd8946c"
+  version "1.0.84-5"
+  sha256 arm:          "6e96758e6c76ea88203f5b04f8d5c571ec43f19192508e9951ac9da13180c45c",
+         intel:        "0f75aa5a93e7c9a1eaf769d53ff3149265ceee242603ab62d6f322fc010240ed",
+         arm64_linux:  "72fa21dde3b9bf4c9deaaeeb02746fed1eee8ca3c0d6a4de0c77a86e090ef21f",
+         x86_64_linux: "b4e32dbc7958ed07287d33953669bbdc0f0cd4ecc667040f5c229841618c7a09"
 
   on_macos do
     depends_on macos: :ventura
