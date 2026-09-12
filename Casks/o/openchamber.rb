877c6c58b811d744cc8db7df87b65c558cc40399
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-12 01:53:52 +0000

    openchamber 1.23.1

--- Casks/o/openchamber.rb
+++ Casks/o/openchamber.rb
@@ -3,11 +3,11 @@ cask "openchamber" do
   os macos: "mac", linux: "linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "1.23.0"
-  sha256 arm:          "4029f2344875fe1ab69d2cbcbd9f58cd17457c640d8c6da05f3e6668f5e12895",
-         intel:        "a11df642c4acb285f66985c3afa3a05e72a7e23d7f66afc626548d30523f6f62",
-         arm64_linux:  "74b6737a009a2050e6290379209fa44f1e42893b941d5967abe9cc4b02822efa",
-         x86_64_linux: "14222cde97a62888609edf604d6111229aa713fb623a9abe967fefd3f89647fc"
+  version "1.23.1"
+  sha256 arm:          "27f91ea748adf959d7881a22305b4dfb3d036fa7b636c309424ea395a912e7b0",
+         intel:        "9575027e2d6a5e1bd45d9a8ca503e40097db28c987969fa0157e7924afcdc4c1",
+         arm64_linux:  "c19a8d30c173eea787d546d8f8d38ea6938c5b557d759b8f42f20fa03acc07b4",
+         x86_64_linux: "22c57d99e95c0a6dd032ab3b37e84ffd808a04ce9f111808860caadd92f8aa87"
 
   on_macos do
     depends_on macos: :monterey
