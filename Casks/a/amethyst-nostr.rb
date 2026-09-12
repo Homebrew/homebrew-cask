ddfe5e0a3e7746978c6f21fe402e9b874563a8b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-12 15:32:11 +0000

    amethyst-nostr 1.15.2

--- Casks/a/amethyst-nostr.rb
+++ Casks/a/amethyst-nostr.rb
@@ -1,6 +1,6 @@
 cask "amethyst-nostr" do
-  version "1.15.1"
-  sha256 "9a23e43846072b2eb7e980b3348c06536780235cf5c8ab7e18afe32c6e3fa02d"
+  version "1.15.2"
+  sha256 "c02141beeb95fa2160f8c074a4a08bbf9851240da6dec66fa10a3ae41a3b5b1e"
 
   url "https://github.com/vitorpamplona/amethyst/releases/download/v#{version}/amethyst-desktop-#{version}-macos-arm64.dmg"
   name "Amethyst"
