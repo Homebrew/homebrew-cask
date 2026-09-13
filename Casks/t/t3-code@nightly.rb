6b84c27dabd0cc5445ca3c81be414928e7378dd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:20:11 +0000

    t3-code@nightly 0.0.41-nightly.20260913.1675

--- Casks/t/t3-code@nightly.rb
+++ Casks/t/t3-code@nightly.rb
@@ -2,10 +2,10 @@ cask "t3-code@nightly" do
   arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
   os macos: "dmg", linux: "AppImage"
 
-  version "0.0.41-nightly.20260913.1658"
-  sha256 arm:          "48c0580a31a61f3da07139af7709045ed4d07a33c09246e12c56aa90ac8640b9",
-         intel:        "5edfea077478f69f9d6404a389998c81cb993c2b61c62cbb116c1f9cc81c5ff8",
-         x86_64_linux: "d8db1d2d4015f7f807055dec28588e0d19f63feb182a49fa7bb3477bfcc58d14"
+  version "0.0.41-nightly.20260913.1675"
+  sha256 arm:          "077710132e2670176a500c784b21c34618f01774968518e33c747a7fb9529cc8",
+         intel:        "f0c8ace522bbd9063ca0a8d5095e23decc8876162f9ef48154ce542b87d6fe58",
+         x86_64_linux: "b6fb273d6189934f80cf8f036a32964ec7ce4bad3eb935f98d4c0a28203709fe"
 
   on_macos do
     auto_updates true
