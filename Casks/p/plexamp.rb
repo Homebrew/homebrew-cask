21fd88fdef00430223abe40f29e9267aa7aa33d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 16:15:50 +0000

    plexamp 4.50.11

--- Casks/p/plexamp.rb
+++ Casks/p/plexamp.rb
@@ -1,9 +1,9 @@
 cask "plexamp" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.50.10"
-  sha256 arm:   "87ac33732a03f46a6d10f64063e9bfac0e1a710d63c8f137d04a6dea70c5ae6e",
-         intel: "8691b3676fce796f8daffb2191b16bd63b6b43bfd5b126c8116bf9b03a45a620"
+  version "4.50.11"
+  sha256 arm:   "58d43cc20bd16927356c1bc44546b216f70ed589d2fac572298694864a2bd7c5",
+         intel: "01d6a3b2f6ef438ad07fe386b61ee10e6ae2499c4a4147090b7567c7dd822146"
 
   url "https://plexamp.plex.tv/desktop/Plexamp-#{version}-#{arch}.dmg"
   name "Plexamp"
