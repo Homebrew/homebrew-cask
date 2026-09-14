79556b41af020ca5b11243ed02591ec3eae92e6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 21:38:48 +0000

    bunch 1.4.19

--- Casks/b/bunch.rb
+++ Casks/b/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.4.17,180"
-  sha256 "8889757631a7a2fdfc9c81b0acf39459926fa1ce31a89822cb4ca788ca7370db"
+  version "1.4.19,188"
+  sha256 "8755ef0fe80d3aca8d39c8a0b300a23ba90682c7fa2eb5ade40f11ed5722063c"
 
   url "https://brettterpstra.com/updates/bunch/Bunch#{version.csv.first}#{version.csv.second}.dmg"
   name "Bunch"
