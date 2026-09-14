28f93e2e4577c83f452a11cfa5693cee0122daba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 10:31:08 +0000

    raycast 2.4.0.0

--- Casks/r/raycast.rb
+++ Casks/r/raycast.rb
@@ -39,8 +39,8 @@ cask "raycast" do
       end
     end
     on_tahoe :or_newer do
-      version "2.3.1.0"
-      sha256 "9af903aee95af55e5f844b7b47c77740cfc30fe50ce9e37bab2f7946584a0ec8"
+      version "2.4.0.0"
+      sha256 "1fb5e8f8ee53c7c7ee6f41608d485f3bc1a0fb7cd6084ae20a16485dc85e4e88"
 
       url "https://x.raycast-releases.com/download?platform=macos&architecture=arm64&version=#{version}"
 
