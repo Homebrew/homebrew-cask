68a4ef664872cf879cd9c653af0a3e20cb69403e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 18:40:02 +0000

    backblaze 10.0.3.1076

--- Casks/b/backblaze.rb
+++ Casks/b/backblaze.rb
@@ -1,6 +1,6 @@
 cask "backblaze" do
-  version "10.0.2.1048"
-  sha256 "21c7501391c3f28c13fd49c7e4eae24ea2780b708ef690cdc70464a88e141d57"
+  version "10.0.3.1076"
+  sha256 "baf57f5dac7814b80e32c9c6c35aef9661f4cb01a3484f6cbb31f60024580690"
 
   url "https://secure.backblaze.com/api/install_backblaze?file=bzinstall-mac-#{version}.dmg"
   name "Backblaze"
