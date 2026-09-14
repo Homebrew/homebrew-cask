d3d769bcf85d9b8b408d343aafce6bb8f61fd785
bevanjkay <email@bevankay.me>
2026-09-14 13:30:06 +1000

    shimo 6.0.1

--- Casks/s/shimo.rb
+++ Casks/s/shimo.rb
@@ -1,8 +1,8 @@
 cask "shimo" do
-  version "5.0.4,8890"
-  sha256 "0f624d29248c2cb3d77ede2cc8ce8e30cdddaa7358d444571dd2ec1932f6958c"
+  version "6.0.1,9076"
+  sha256 "f06e88f35513e02b328cbe43995b52e59af3f9020305f797d3e523c6b0587a62"
 
-  url "https://downloads.mailbutler.io/Shimo_#{version.csv.first}_#{version.csv.second}.zip"
+  url "https://s3.eu-central-1.amazonaws.com/shimo.jaeger.apps/Shimo_#{version.csv.first}_#{version.csv.second}.zip"
   name "Shimo"
   desc "VPN client for secure internet access and private browsing"
   homepage "https://www.shimovpn.com/"
@@ -13,7 +13,7 @@ cask "shimo" do
   end
 
   auto_updates true
-  depends_on :macos
+  depends_on macos: :ventura
 
   app "Shimo.app"
 
