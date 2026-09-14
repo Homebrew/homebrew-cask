d4e37f8178a5447c099c4c69d4bb18f8e4b5e67d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 18:50:36 +0000

    cursor 3.20.21

--- Casks/c/cursor.rb
+++ Casks/c/cursor.rb
@@ -3,11 +3,11 @@ cask "cursor" do
   os macos: "darwin", linux: "linux"
   url_end = on_system_conditional macos: "zip", linux: "AppImage"
 
-  version "3.20.17,0c32194e3fb5ffaced9fb36430b860ec301e1fc8"
-  sha256 arm:          "82d032b42addcb6af44746d74a90355f4c44184d2f4574b987fc71d558142a79",
-         intel:        "89099636426f88a12f0ff3f7735c5634eefd55ac0bc8f08a1d22540df3f78465",
-         arm64_linux:  "d4238ce5a8ddb4147d903203e73e38a2b571ec1bf2f50541ca01bf6a4d40a6c5",
-         x86_64_linux: "13e8089c85251a102f79905adffb91dd298dd5862652599be63457e28dc31218"
+  version "3.20.21,f09fca384ceca23f7bf21f9c23655b162641d747"
+  sha256 arm:          "be54b6add6d30bcd40235be1f5a9d9bc65e0977ca2e55bee8fe011af73048c49",
+         intel:        "66a52b5597caef100b093b24b716c674599965750d809abd21b89dcf5f691b8e",
+         arm64_linux:  "6b68999a0a3ba6dbf7cd7d094960597ecdbe135421283374b1ff15286f3c59b5",
+         x86_64_linux: "b9ec1e26de767323da056ad6b1b2fba3a4233213a1752de0ebf575afff063bc9"
 
   on_macos do
     url "https://downloads.cursor.com/production/#{version.csv.second}/#{os}/#{arch}/Cursor-darwin-#{arch}.#{url_end}"
