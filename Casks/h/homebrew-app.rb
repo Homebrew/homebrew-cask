b48f96a94bdc5a2a367aa008a74365fa0648a315
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 13:17:05 +0000

    homebrew-app 0.4.1

--- Casks/h/homebrew-app.rb
+++ Casks/h/homebrew-app.rb
@@ -1,6 +1,6 @@
 cask "homebrew-app" do
-  version "0.4.0"
-  sha256 "e5ddca524ad21481707363516837a344baeb00e9f43f61af7a20cb9e804666c5"
+  version "0.4.1"
+  sha256 "71f1d99e4db0c40fff102adc0309ea6f9c3c555e913c2afda3df8d0bb561ba8f"
 
   url "https://github.com/Homebrew/BrewUI/releases/download/v#{version}/Homebrew-#{version}.zip"
   name "Homebrew"
