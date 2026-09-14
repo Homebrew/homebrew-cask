fba2cf6ec65de3a545764a8b41c26bb45adc099d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 13:41:53 +0000

    psychopy 2026.2.4

--- Casks/p/psychopy.rb
+++ Casks/p/psychopy.rb
@@ -1,9 +1,9 @@
 cask "psychopy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2026.2.3"
-  sha256 arm:   "d715f35ea58ab3802f6bd8fa72ccc4969d84c051c65d80bce384dcdc4051fb6d",
-         intel: "0e0fe98bcac0910cbd8a96c4bda239a6865aa45b874c6e8c38d87b0b1019a642"
+  version "2026.2.4"
+  sha256 arm:   "1a99836dc552964693cb824e1d0d9e0e1fe39c3b4e202859181dc4a4c63f1487",
+         intel: "01c3a32db263a9927e94c147b2acaab6563c5d54e79233ebaae5e3ec76f28f2f"
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-macOS-#{arch}-3.11.dmg"
   name "PsychoPy"
