5b3e99e20f5a3134f232899e4300dfa13de14e48
Yosef Yona <8616405+nyouna@users.noreply.github.com>
2026-09-14 17:21:33 +0300

    bing-wallpaper 2.0.11.231

--- Casks/b/bing-wallpaper.rb
+++ Casks/b/bing-wallpaper.rb
@@ -1,15 +1,19 @@
 cask "bing-wallpaper" do
-  version "1.1.12,9f265e2b-e7fa-4a5a-a9ca-4b608ed11c0c"
-  sha256 "2db703f5c124afc9ae8b1c6a41da2258c56a8cf45d45cb6f7e4ace1594627cd3"
+  version "2.0.11.231,fd30d7b2-7ce9-4f68-b875-b3dde8061979"
+  sha256 "b1b3642a62489c1a20234c451e90111ef0e278d68e616c0a8a01d0adf560e4ea"
 
-  url "https://download.microsoft.com/download/#{version.csv.second}/Installer/#{version.csv.first}/var1/MW011/2/BingWallpaper.pkg"
+  url "https://download.microsoft.com/download/#{version.csv.second}/MW011/#{version.csv.first}/MicrosoftBingWallpaper_#{version.csv.first}.pkg"
   name "Bing Wallpaper"
+  name "Microsoft Bing Wallpaper"
   desc "Use the Bing daily image as your wallpaper"
   homepage "https://bingwallpaper.microsoft.com/"
 
   livecheck do
-    url "https://go.microsoft.com/fwlink/?linkid=2181295&installerType=PKG"
-    regex(%r{/([\h-]+)/Installer/(\d+(?:\.\d+)+)[^/]*/}i)
+    # The download endpoint only redirects to the current installer when the
+    # client reports macOS 14 or newer; otherwise it serves the legacy 1.x app.
+    url "https://www.bing.com/apps/api/wallpaper/downloadMacPackage?pc=mw011&brand=bing",
+        header: "Sec-CH-UA-Platform-Version: \"26.0.0\""
+    regex(%r{/([\h-]+)/MW011/v?(\d+(?:\.\d+)+)/MicrosoftBingWallpaper[._-]v?\d+(?:\.\d+)*\.pkg}i)
     strategy :header_match do |headers, regex|
       match = headers["location"]&.match(regex)
       next unless match
@@ -18,29 +22,42 @@ cask "bing-wallpaper" do
     end
   end
 
-  depends_on :macos
+  depends_on macos: :sonoma
 
-  pkg "BingWallpaper.pkg"
+  pkg "MicrosoftBingWallpaper_#{version.csv.first}.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "bing_search", # "Enable Bing search in your browser" (BingWallpaper-dse.pkg)
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
 
   uninstall launchctl: [
-              "com.microsoft.msbwapp",
-              "com.microsoft.msbwupdater",
+              "com.microsoft.bing.sunfish.updater",
+              "Microsoft Bing Wallpaper",
             ],
             quit:      [
-              "com.microsoft.autoupdate2",
+              "com.microsoft.bing.wallpaper",
               "com.microsoft.MicrosoftBingSearch",
-              "com.microsoft.msbwapp",
-              "com.microsoft.msbwdefaults",
             ],
-            pkgutil:   "com.microsoft.msbwpackage",
-            delete:    "/Applications/Microsoft Bing for Safari.app"
+            signal:    ["TERM", "com.microsoft.bing.wallpaper"],
+            pkgutil:   [
+              "com.microsoft.bing.sunfish.updater",
+              "com.microsoft.bing.wallpaper.installer",
+              "com.microsoft.bing.wallpaper.installer.dse",
+            ],
+            delete:    [
+              "/Applications/Microsoft Bing for Safari.app",
+              "/Library/Application Support/Microsoft/Sunfish/Updater/Clients/BingWallpaperApp.plist",
+            ]
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.msbwdefaults.sfl*",
-    "~/Library/Application Support/Microsoft/Bing Wallpaper",
+    "/Library/Application Support/Microsoft/BingWallpaper",
+    "/Library/Application Support/Microsoft/Sunfish",
+    "/Library/Logs/Microsoft/Sunfish",
+    "~/Library/Application Support/com.microsoft.bing.wallpaper",
+    "~/Library/Caches/com.microsoft.bing.wallpaper",
+    "~/Library/WebKit/com.microsoft.bing.wallpaper",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
