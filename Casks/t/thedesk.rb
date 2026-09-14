6b99ca98c2f84f749fcfd9d67e7557cc727cfaab
bevanjkay <email@bevankay.me>
2026-09-14 13:31:35 +1000

    thedesk 25.5.1

--- Casks/t/thedesk.rb
+++ Casks/t/thedesk.rb
@@ -1,8 +1,8 @@
 cask "thedesk" do
-  version "25.4.0"
-  sha256 "9bd7c57dc212eaa3ec28409d1a2409b72a1f7a2d1f10cd8b957be13e8f867951"
+  version "25.5.1"
+  sha256 "00dc2bf55b134473d8fd687721a369734b9fbb26cdfd3e911ef5e87fffbbf941"
 
-  url "https://github.com/cutls/thedesk-next/releases/download/v#{version}/TheDesk-#{version}-universal.dmg"
+  url "https://github.com/cutls/thedesk-next/releases/download/v#{version}/TheDesk-#{version}-arm64.dmg"
   name "TheDesk"
   desc "Mastodon/Misskey Client for PC"
   homepage "https://thedesk.top/"
@@ -12,7 +12,8 @@ cask "thedesk" do
     strategy :github_latest
   end
 
-  depends_on :macos
+  depends_on arch: :arm64
+  depends_on macos: :ventura
 
   app "TheDesk.app"
 
