616ae936aa7c5e3e7fbae716209b589a7fbeec59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 17:11:18 +0000

    tunarr 1.3.15

--- Casks/t/tunarr.rb
+++ Casks/t/tunarr.rb
@@ -1,9 +1,9 @@
 cask "tunarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.3.14"
-  sha256 arm:   "71993631fd67c7061d90bda1644b5d373c40d3e51910a028707deeffbfe00427",
-         intel: "98fbac88ac84de36389b87e5f0714624efdeadebaf83dc59763dbbe3e90bf696"
+  version "1.3.15"
+  sha256 arm:   "6f8e8ded3ff452bdee769d881673eddcbd9fe8ec43d9d33c8e5683a8b7872800",
+         intel: "f9f24011b562d4b328ca1ab8968041449165d725876fcf53705085b2cb11ab88"
 
   url "https://github.com/chrisbenincasa/tunarr/releases/download/v#{version}/Tunarr-v#{version}-macos-#{arch}.dmg"
   name "tunarr"
