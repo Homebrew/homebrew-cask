479f90044303262d2d37a8ba334706f67faf8486
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 11:04:58 +0000

    usenapp 1.28.1

--- Casks/u/usenapp.rb
+++ Casks/u/usenapp.rb
@@ -1,6 +1,6 @@
 cask "usenapp" do
-  version "1.28"
-  sha256 "af729bd66057ac0962a68537caca9afbb8b381c794d14b3ba1bdba2d64eebc15"
+  version "1.28.1"
+  sha256 "1dad13d22acba87a0eeedd687a1f066ca0ead94034b42e4f90e9f1ea0d85d4f9"
 
   url "https://www.usenapp.com/download/Usenapp-#{version}.dmg"
   name "Usenapp"
