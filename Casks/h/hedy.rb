836e766d482d509b601370dd37a57029e045558c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 22:06:42 +0000

    hedy 3.11.0

--- Casks/h/hedy.rb
+++ Casks/h/hedy.rb
@@ -1,6 +1,6 @@
 cask "hedy" do
-  version "3.10.1"
-  sha256 "65eda3b8623db1af0908a649d3b486b84fef58228e0889948180eafa9a6e529d"
+  version "3.11.0"
+  sha256 "e6b0d2b9dfc5095a48887aea2910558de1161a3bfa8bf6b8646ab4aa21200b8a"
 
   url "https://dl.hedy.ai/Hedy-MacOS-#{version}.dmg"
   name "Hedy AI"
