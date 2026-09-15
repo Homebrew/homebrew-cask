0bcb9b30b98545b6b1cd77f8532b6f4576f2c018
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 08:21:06 +0000

    shimo 6.0.3

--- Casks/s/shimo.rb
+++ Casks/s/shimo.rb
@@ -1,6 +1,6 @@
 cask "shimo" do
-  version "6.0.2,9091"
-  sha256 "22d85a263eb9285992fa5ddcc601cba4b6b248f8341d8792a20bc6be575a4376"
+  version "6.0.3,9095"
+  sha256 "5e26f6399cddc2bbd2e8ca12f6558173afa8ccfabc77c526e71b265c11fea102"
 
   url "https://s3.eu-central-1.amazonaws.com/shimo.jaeger.apps/Shimo_#{version.csv.first}_#{version.csv.second}.zip"
   name "Shimo"
