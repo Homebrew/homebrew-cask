9a666fae9f35eb3a6d5c013782a241b6fa879ad8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 14:26:09 +0000

    thunder 5.80.8.66675

--- Casks/t/thunder.rb
+++ Casks/t/thunder.rb
@@ -1,6 +1,6 @@
 cask "thunder" do
-  version "5.80.7.66659"
-  sha256 "2d5c41dad06f2070e0496a77754be25c3848e33c4e7ddd25ca61a04f8fb5bf9c"
+  version "5.80.8.66675"
+  sha256 "820b9023348d3923b0414cb63d9f5eb253a43e832112e1e38c9eaa66e6f9a32d"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg"
   name "Thunder"
