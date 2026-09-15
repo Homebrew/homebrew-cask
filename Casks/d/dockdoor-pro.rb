4ed66f9ac9394813c545dc10ee2f634b5ec91abd
Ethan <74191134+ejbills@users.noreply.github.com>
2026-09-15 10:04:27 -0700

    dockdoor-pro: skip beta channel in livecheck

--- Casks/d/dockdoor-pro.rb
+++ Casks/d/dockdoor-pro.rb
@@ -9,7 +9,9 @@ cask "dockdoor-pro" do
 
   livecheck do
     url "https://pro.dockdoor.net/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle do |items|
+      items.find { |item| item.channel.nil? }&.short_version
+    end
   end
 
   auto_updates true
