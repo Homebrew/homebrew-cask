f8c5d9ff1c25f591b178af38a14905cd4dc7216a
Rui Chen <rui@chenrui.dev>
2026-09-15 13:15:47 -0400

    notion: update ARM to 7.34.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/n/notion.rb
+++ Casks/n/notion.rb
@@ -2,11 +2,11 @@ cask "notion" do
   arch arm: "-arm64"
   livecheck_folder = on_arch_conditional arm: "arm64", intel: "latest"
 
-  sha256 arm:   "1c1d9834d565b382aa40568a99a608afc0222528ee7b166c02ce8cfd15aa906f",
+  sha256 arm:   "e5808d068d9e8b6bf15c9d08c2045c7cc9f8a406398e0c164c9622e39e7ea74e",
          intel: "44b6dd2b74cee6fa048b8d07297557d65a5d95b5cfcf4ccd1db28281411649a9"
 
   on_arm do
-    version "7.33.0"
+    version "7.34.0"
   end
   on_intel do
     version "7.34.0"
