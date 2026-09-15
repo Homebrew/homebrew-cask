ab3d8de32e8b291526988a04c02e07dd22924fe7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 08:24:24 +0000

    sonarqube-cli 1.8.0.5274

--- Casks/s/sonarqube-cli.rb
+++ Casks/s/sonarqube-cli.rb
@@ -2,10 +2,10 @@ cask "sonarqube-cli" do
   arch arm: "arm64", intel: "x86-64"
   os macos: "macos", linux: "linux"
 
-  version "1.7.0.4638"
-  sha256 arm:          "8a4611b2899dafeef2be6c2d820b3d2c004a02acd012bf7b0f1fd08ca9193659",
-         arm64_linux:  "f4a69cc7f9c1312567572f674e4343094e6325e1c142d05d3e491482d8979329",
-         x86_64_linux: "42b9dd631120b8776cd63daafd18db8e2d1229fdcde2a3ce6ef258580eb63a8c"
+  version "1.8.0.5274"
+  sha256 arm:          "ba4fb2ea8e98742db361162ec8454239d5d62c8cba8a6c41304cadb7f2151560",
+         arm64_linux:  "532e71e48b910c58741f9118cde845854c1e4a821ed0f50e1502f06cda3c9bb5",
+         x86_64_linux: "fbfd39b3798d7c11a1c5c81377351f82ba8ae6be65335243f0bb3c91e7a921d1"
 
   on_macos do
     depends_on arch: :arm64
