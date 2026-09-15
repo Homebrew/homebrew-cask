fe35e59928fb0f66417f0141d375e3159ba585f1
rakleed <19418601+rakleed@users.noreply.github.com>
2026-09-16 00:57:25 +0300

    telegram@beta 12.10 (new cask)

---
+++ Casks/t/telegram@beta.rb
@@ -0,0 +1,44 @@
+cask "telegram@beta" do
+  version "12.10,282985"
+  sha256 "b205080e9bdb9d42953abaef3340e4a1105658ae8d3038a04033056c514fd256"
+
+  url "https://mac-updates.telegram.org/beta/Telegram-#{version.csv.first}.#{version.csv.second}.app.zip"
+  name "Telegram for macOS"
+  desc "Messaging app with a focus on speed and security"
+  homepage "https://macos.telegram.org/"
+
+  # The sparkle feed uses incorrect pubDate values, so it's necessary to
+  # work with all of the items in the feed (not just the newest one).
+  livecheck do
+    url "https://mac-updates.telegram.org/beta/versions.xml"
+    strategy :sparkle do |items|
+      items.map(&:nice_version)
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "telegram"
+  depends_on :macos
+
+  app "Telegram.app"
+
+  uninstall quit: "ru.keepcoder.Telegram"
+
+  zap trash: [
+    "~/Library/Application Scripts/*.ru.keepcoder.Telegram",
+    "~/Library/Application Scripts/*.ru.keepcoder.Telegram.TelegramShare",
+    "~/Library/Application Scripts/ru.keepcoder.Telegram",
+    "~/Library/Application Scripts/ru.keepcoder.Telegram.TelegramShare",
+    "~/Library/Application Support/ru.keepcoder.Telegram",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/ru.keepcoder.Telegram",
+    "~/Library/Caches/ru.keepcoder.Telegram",
+    "~/Library/Containers/ru.keepcoder.Telegram",
+    "~/Library/Containers/ru.keepcoder.Telegram.TelegramShare",
+    "~/Library/Cookies/ru.keepcoder.Telegram.binarycookies",
+    "~/Library/Group Containers/*.ru.keepcoder.Telegram",
+    "~/Library/Group Containers/*.ru.keepcoder.Telegram.TelegramShare",
+    "~/Library/HTTPStorages/ru.keepcoder.Telegram",
+    "~/Library/Preferences/ru.keepcoder.Telegram.plist",
+    "~/Library/Saved Application State/ru.keepcoder.Telegram.savedState",
+  ]
+end
