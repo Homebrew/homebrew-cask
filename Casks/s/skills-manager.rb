ea4365eb4f32e9e7650b1d26514ced8c62817c6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 02:08:27 +0000

    skills-manager 1.39.0

--- Casks/s/skills-manager.rb
+++ Casks/s/skills-manager.rb
@@ -1,9 +1,9 @@
 cask "skills-manager" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.38.0"
-  sha256 arm:   "2b1aadc8427ba86af494533621a6ed107275c3b1891bc83ce2e6a5ab6ccec71a",
-         intel: "fd697f4d3934fc3989957d5ee3e1f58ae7f11749b8b0a88569275591ac5dcad0"
+  version "1.39.0"
+  sha256 arm:   "ece7f426039ad0889e482c53f453fd2decbddf37814b5117968694572cc7ff00",
+         intel: "148793aabd041fc732351b0bd56d08f9f9031a8431cecc7a248d803dec783f0f"
 
   url "https://github.com/xingkongliang/skills-manager/releases/download/v#{version}/skills-manager_#{version}_#{arch}.dmg"
   name "Skills Manager"
