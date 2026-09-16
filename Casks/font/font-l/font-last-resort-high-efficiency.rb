cd5866e2ca825c3ee11c930e2d1468c84d161d07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 16:03:56 +0000

    font-last-resort-high-efficiency 18.000

--- Casks/font/font-l/font-last-resort-high-efficiency.rb
+++ Casks/font/font-l/font-last-resort-high-efficiency.rb
@@ -1,6 +1,6 @@
 cask "font-last-resort-high-efficiency" do
-  version "17.000"
-  sha256 "3a936fe2379e9e164a8f9819095cbf53d9305a2d08532d9be5b496f4be45bcde"
+  version "18.000"
+  sha256 "cb7c2262cb86a33e7bdfabd0a51746e738b0dd10924126c4530c414c5fc7e4b6"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResortHE-Regular.ttf"
   name "Last Resort High-Efficiency"
