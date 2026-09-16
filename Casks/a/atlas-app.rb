8735eee91414514c9ddae61a51f93b89ef9d9b06
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 06:52:26 +0000

    atlas-app 0.3.2

--- Casks/a/atlas-app.rb
+++ Casks/a/atlas-app.rb
@@ -1,9 +1,9 @@
 cask "atlas-app" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.3.1"
-  sha256 arm:   "84247f492310c5977dacdb198edaf55f402229d274d2c896c44fbad11d7f176e",
-         intel: "77cb2fb8bf21884c254082cba4ea72742494081a1552b4ca4384274daf596a27"
+  version "0.3.2"
+  sha256 arm:   "17a32c40d2b891146b718727e6ea1d76f89c78a823d621cc1a8a4b5eb5bf4b73",
+         intel: "94108d7a9c16fa865b99bc97e2bc216f14fde2f065af037fbb387f38f421784d"
 
   url "https://github.com/pacifio/atlas/releases/download/alpha-#{version}/Atlas_#{version}_#{arch}.dmg"
   name "Atlas"
