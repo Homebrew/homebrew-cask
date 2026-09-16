335052aebcf83b49c40291b1c4bac865bc5a4ceb
Mohamad Yahia <xyzt70@gmail.com>
2026-09-16 20:09:51 +0000

    ghostex 9.8.5

--- Casks/g/ghostex.rb
+++ Casks/g/ghostex.rb
@@ -1,6 +1,6 @@
 cask "ghostex" do
-  version "9.7.0"
-  sha256 "bc361b5476e9dbceb5c858a21924fe7ec1934e423a023506e2d1a5a0f577db1c"
+  version "9.8.5"
+  sha256 "6ba03d97bc1b59de4c71f992f5f49cd8ff165b5e3589375dced714631a8d6e09"
 
   url "https://github.com/maddada/Ghostex/releases/download/v#{version}/ghostex-#{version}-arm64.dmg"
   name "Ghostex"
