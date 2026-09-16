47f1382163915cce7f5c7a16e93427c68e96881e
Filipe Guerra <alias.mac@gmail.com>
2026-09-16 10:00:42 -0500

    reolink: use electron_builder livecheck
    The Apple Silicon build is published at an unversioned URL, so this
    requires sha256 :no_check. Alternative to the previous commit; drop this
    commit to keep checksum verification.

--- Casks/r/reolink.rb
+++ Casks/r/reolink.rb
@@ -1,24 +1,15 @@
 cask "reolink" do
-  version "8.21.12,2026.07,230926448887f151e9d6d10f"
-  sha256 "a4b94c4fddcf4da25eee72bf421a1f790345c98b4675b1935ad1a37dab218103"
+  version "8.21.12"
+  sha256 :no_check
 
-  url "https://home-cdn.reolink.us/wp-content/uploads/#{version.csv.second.major}/#{version.csv.second.minor}/#{version.csv.third}.dmg"
+  url "https://home-cdn.reolink.us/wp-content/uploads/2026/07/230926448887f151e9d6d10f.dmg"
   name "Reolink Client"
   desc "Client for viewing and managing security cameras and NVRs"
   homepage "https://reolink.com/software-and-manual/"
 
   livecheck do
-    url "https://reolink.com/wp-json/reo-v2/download/?type=clients&lang=en"
-    regex(%r{/uploads/(\d+)/(\d+)/(\h+)\.dmg}i)
-    strategy :json do |json, regex|
-      item = json.dig("data", "downloads")&.find { |download| download["type"] == "mac_client_apple_silicon" }
-      next if item.blank?
-
-      match = item["url"]&.match(regex)
-      next if item["version"].blank? || match.blank?
-
-      "#{item["version"]},#{match[1]}.#{match[2]},#{match[3]}"
-    end
+    url "https://cdn.reolink.com/files/client/electron-release/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
