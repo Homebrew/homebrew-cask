de6e9639eae4dbb89ddd4fad1315dbe50ff8828c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 22:01:52 +0000

    grok-bot 0.55.0

--- Casks/g/grok-bot.rb
+++ Casks/g/grok-bot.rb
@@ -2,9 +2,9 @@ cask "grok-bot" do
   arch arm: "arm64", intel: "x64"
   arch_suffix = on_arch_conditional intel: "_x64"
 
-  version "0.53.0"
-  sha256 arm:   "dfcccb37a1bbb7fc426388929188c72e2b530b9245c47ee81b83c8a7beaf0444",
-         intel: "fc6b7e53ed91266a4276cdced220ae6f1216bcd2b160847ba2c149a68fe12818"
+  version "0.55.0"
+  sha256 arm:   "10ee58ba16351dc34490a9f135442c71501f4a5b6cf069f79d76c50e1631f483",
+         intel: "93ab15f80d6cf5df473df5d4d5b0d74a10e4619c9cdb250bfbc0363fd380420f"
 
   url "https://downloads.cursor.com/sand/stable/darwin-#{arch}/#{version}/Grok_Bot_#{version}#{arch_suffix}.dmg"
   name "Grok Bot"
