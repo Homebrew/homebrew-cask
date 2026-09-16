21513a768f3a8b89fc5efe303f898cda4097f044
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 13:54:38 +0000

    sessionwatcher 7.3.11

--- Casks/s/sessionwatcher.rb
+++ Casks/s/sessionwatcher.rb
@@ -1,6 +1,6 @@
 cask "sessionwatcher" do
-  version "7.3.1"
-  sha256 "b8248915b7aa337cbb6624a006dc5292a7da719fab1f31385a0568383901962b"
+  version "7.3.11"
+  sha256 "9011cf28a53137db7b5eb89cb698fafce8e28c312d06e2ebc3220303837b32ca"
 
   url "https://sessionwatcher-updates.vercel.app/downloads/SessionWatcher-#{version}.dmg"
   name "SessionWatcher"
