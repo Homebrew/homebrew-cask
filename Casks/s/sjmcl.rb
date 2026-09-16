8f66c79a3c45b8f5dbbe3dc517f591c358977706
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 13:57:36 +0000

    sjmcl 1.3.1

--- Casks/s/sjmcl.rb
+++ Casks/s/sjmcl.rb
@@ -3,11 +3,11 @@ cask "sjmcl" do
   os macos: "macos", linux: "linux"
   url_end = on_system_conditional macos: ".dmg", linux: "_portable"
 
-  version "1.3.0"
-  sha256 arm:          "8fe2c8b73954e39fcc7bcaaed3e5216f282382e49a914e8d4fd905c106b53d25",
-         intel:        "1a8a9a871f48111f4bda15c0cfd39d0413cdf4a604ab45e850a8504e2301fc8e",
-         arm64_linux:  "fc2dd163caa260d6873b3ac1674e61bb4f42643850d68194e1db29a3fe369243",
-         x86_64_linux: "cfbc8be093d5352935666955e82ba07fac5f3f094989e556826210c2ce9b2cbc"
+  version "1.3.1"
+  sha256 arm:          "815fff70148c26b113218af9fb88c010f80ea67dc1c506b43943dce835bcaefd",
+         intel:        "796f35e0f569c2583ec8bc6fe325775d05b16988695a43e92efc1cfa69f3dd9e",
+         arm64_linux:  "754f32cbb22ad0a5ba446e97fbc1b5c853c39827753ef37eaa722b805a5a4d72",
+         x86_64_linux: "714979b974df76a51e321135847cb2cecbb5798719fa6b5d0cd066094beaf5ee"
 
   on_macos do
     app "SJMCL.app"
