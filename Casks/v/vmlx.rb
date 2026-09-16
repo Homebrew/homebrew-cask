9cf833e00f5f288405a405ed3ba06597b064351e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 10:48:50 +0000

    vmlx 1.6.60

--- Casks/v/vmlx.rb
+++ Casks/v/vmlx.rb
@@ -1,6 +1,6 @@
 cask "vmlx" do
-  version "1.6.59"
-  sha256 "b19df798e246cdd963f5ba04b1e43361a81e9a29dcd46c7060f8eeea3c42be6d"
+  version "1.6.60"
+  sha256 "82d85ecb7dca3f2caac8cb7f6a734bd38976989a7686233f23e126d196b8b806"
 
   url "https://github.com/jjang-ai/mlxstudio/releases/download/v#{version}/vMLX-#{version}-sequoia-arm64.dmg"
   name "vMLX"
