0c3d464d317cfb8d6ffce2f1a9785490b645ade6
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-09-16 21:41:45 +0600

    signal: add Linux AppImage support

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,29 +1,43 @@
 cask "signal" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
+  os macos: "mac", linux: "linux"
+  separator = on_system_conditional macos: "-mac-#{arch}-", linux: "_"
+  url_end = on_system_conditional macos: ".zip", linux: "_#{arch}.AppImage"
 
   version "8.27.0"
-  sha256 arm:   "0ba4aeddb3042de54520aee97e01bb325acb67876d59f743ae77353506967d0c",
-         intel: "83ecc092ff6d4c040c33d6ff841e72007a0ce7babf75f193becfa055103996ad"
+  sha256 arm:          "0ba4aeddb3042de54520aee97e01bb325acb67876d59f743ae77353506967d0c",
+         intel:        "83ecc092ff6d4c040c33d6ff841e72007a0ce7babf75f193becfa055103996ad",
+         x86_64_linux: "c946f139cc7d7450503c65b29a09d3b4ccb0d2f8555678cad1731ed95cad0eed"
 
-  url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.zip"
+  on_macos do
+    depends_on macos: :monterey
+
+    app "Signal.app"
+
+    zap trash: [
+      "~/Library/Application Support/Signal",
+      "~/Library/Preferences/org.whispersystems.signal-desktop.helper.plist",
+      "~/Library/Preferences/org.whispersystems.signal-desktop.plist",
+      "~/Library/Saved Application State/org.whispersystems.signal-desktop.savedState",
+    ]
+  end
+  on_linux do
+    depends_on arch: :x86_64
+
+    app_image "signal-desktop.AppImage", target: "Signal.AppImage"
+
+    zap trash: "~/.config/Signal AppImage"
+  end
+
+  url "https://updates.signal.org/desktop/signal-desktop#{separator}#{version}#{url_end}"
   name "Signal"
   desc "Instant messaging application focusing on security"
   homepage "https://signal.org/"
 
   livecheck do
-    url "https://updates.signal.org/desktop/latest-mac.yml"
+    url "https://updates.signal.org/desktop/latest-#{os}.yml"
     strategy :electron_builder
   end
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "Signal.app"
-
-  zap trash: [
-    "~/Library/Application Support/Signal",
-    "~/Library/Preferences/org.whispersystems.signal-desktop.helper.plist",
-    "~/Library/Preferences/org.whispersystems.signal-desktop.plist",
-    "~/Library/Saved Application State/org.whispersystems.signal-desktop.savedState",
-  ]
 end
