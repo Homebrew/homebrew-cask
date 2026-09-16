d5c7531bf30b93f91b5b9bb9ba09f0971d8bbeeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 16:42:58 +0000

    sentry-cli 3.8.0

--- Casks/s/sentry-cli.rb
+++ Casks/s/sentry-cli.rb
@@ -2,11 +2,11 @@ cask "sentry-cli" do
   arch arm: on_system_conditional(macos: "arm64", linux: "aarch64"), intel: "x86_64"
   os macos: "Darwin", linux: "Linux"
 
-  version "3.7.0"
-  sha256 arm:          "c66564094fbe56ee3b359f7574541f858b8d1df0328a0a759da972fbf1886048",
-         intel:        "fcd74786b4d95c6b7531662607897aadd5ab5d64c5d0468a6f4bd97ad04bedb8",
-         arm64_linux:  "69cc0e951f663a332fd3bb3069e443cb3748abbfce497ae2caac6d5a9ec7ae65",
-         x86_64_linux: "cec71d46a7cc394c94b6e75f1601985c710d457376c546ef3975567b3671563b"
+  version "3.8.0"
+  sha256 arm:          "1dda212b0e168b9c4dc48d7d3aa24c1c37de9c6edf786e6ae661236e529969cd",
+         intel:        "279c795b15de7a76106d30b6611b7942b1a97323307327bafc18335f10384c4d",
+         arm64_linux:  "eaea24b5b47b61a96d9a2e353268ea41fd9a8c5b7979694330df614d4af672bf",
+         x86_64_linux: "13f8cb34ae01a6a272d7d7c22e277a105286615b4020de900ea95a8de47cdbb6"
 
   url "https://github.com/getsentry/sentry-cli/releases/download/#{version}/sentry-cli-#{os}-#{arch}"
   name "Sentry CLI"
