80e79db0b472e2994f2b71f7a9578e7ed8e6c131
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 01:25:56 +0000

    fathom 3.8.0

--- Casks/f/fathom.rb
+++ Casks/f/fathom.rb
@@ -1,9 +1,9 @@
 cask "fathom" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.7.1"
-  sha256 arm:   "1c61283236e09588c94ca9e5d4955e8a78d3832376a4fe11a46527e729f7430b",
-         intel: "4eaee9952c1d8c35603f42e09f0e3ca905e49391eda444e2931ede126132f983"
+  version "3.8.0"
+  sha256 arm:   "233e99c8f3889e846f31b86dafe19bbd6d782fecf0f01940528026f8f2676e54",
+         intel: "669f555579c100f288be7d462cadffb4f803eecbeed100c3a6d9e1954e44b069"
 
   url "https://electron-update.fathom.video/download/file/Fathom-darwin-#{arch}-#{version}.dmg"
   name "Fathom"
