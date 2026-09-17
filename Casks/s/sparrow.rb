c7dbc9e6009b8365497bd3d1d75e1a43c232fcde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 10:46:29 +0000

    sparrow 2.5.5

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.5.4"
-  sha256 arm:   "e8d8637a737480721bc820a1b96a79483fc4c73bc0095f6ce1cfb93637158173",
-         intel: "ab497521e5f7f2d525198df94ef26fdf3d4086ddaa611f578cac2c399a34cce2"
+  version "2.5.5"
+  sha256 arm:   "02be94466d92bef4b81b6b9c3efaf6ef4910f713330065309750e92679f711cd",
+         intel: "8db7d49112f73be162e2a22dd1494c095b760fca22e50223405a425deb4e7dcf"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg"
   name "Sparrow Bitcoin Wallet"
