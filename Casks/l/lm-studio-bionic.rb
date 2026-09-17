3a04c5786847fdee787dac703e0d122cb5b926e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 22:08:47 +0000

    lm-studio-bionic 1.1.4

--- Casks/l/lm-studio-bionic.rb
+++ Casks/l/lm-studio-bionic.rb
@@ -1,6 +1,6 @@
 cask "lm-studio-bionic" do
-  version "1.1.3,5"
-  sha256 "37d5bf2cf10ab4e83c27915767f5556c278beab098f5ac21faa66cd0ac62faf7"
+  version "1.1.4,3"
+  sha256 "482ec6afe840cbb6c4e2bdee5f99d03c04277fee7f77d3f291e95f9f42ed322e"
 
   url "https://bionic-installers.lmstudio.ai/darwin/arm64/#{version.tr(",", "-")}/Bionic-#{version.tr(",", "-")}-arm64.dmg"
   name "LM Studio Bionic"
