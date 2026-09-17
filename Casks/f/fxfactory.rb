874c2d9b1b5cbe290ec48f5a69ddae474a852636
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 16:16:12 +0000

    fxfactory 9.0.6

--- Casks/f/fxfactory.rb
+++ Casks/f/fxfactory.rb
@@ -1,5 +1,5 @@
 cask "fxfactory" do
-  version "9.0.5,8654"
+  version "9.0.6,8706"
   sha256 :no_check # required as upstream package is often updated in place
 
   url "https://files.fxfactory.com/fxfactory/FxFactory-#{version.csv.first}-#{version.csv.second}.zip"
