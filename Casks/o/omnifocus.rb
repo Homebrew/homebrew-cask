8645012d63f2e4f1b383dd1e7fa7e8967d51a35d
Wierzciu <wierzciu@gmail.com>
2026-09-17 12:32:00 +0200

    omnifocus 4.9

--- Casks/o/omnifocus.rb
+++ Casks/o/omnifocus.rb
@@ -23,11 +23,21 @@ cask "omnifocus" do
       skip "Legacy version"
     end
   end
-  on_sonoma :or_newer do
+  on_sonoma do
     version "4.8.13"
     sha256 "bc547b38e18d34409603666dc467ef3b83de615f728b53554003ac7d88c44ea7"
 
     url "https://downloads.omnigroup.com/software/macOS/14/OmniFocus-#{version}.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_sequoia :or_newer do
+    version "4.9"
+    sha256 "9f9c4eff8d577b4c6a883b68b5bb532988d80ccb996ce77b297e99b199cd58ee"
+
+    url "https://downloads.omnigroup.com/software/macOS/15/OmniFocus-#{version}.dmg"
 
     livecheck do
       url "https://www.omnigroup.com/download/latest/omnifocus/"
