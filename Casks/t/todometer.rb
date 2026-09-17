f7b68fcb73d5f2705d09974f45aa68865c5452f0
Patrick Linnane <patrick@linnane.io>
2026-09-17 08:05:49 -0700

    todometer: remove redundant macOS minimum
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/t/todometer.rb
+++ Casks/t/todometer.rb
@@ -21,7 +21,7 @@ cask "todometer" do
 
     disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
-    depends_on macos: :big_sur
+    depends_on macos: :any
 
     app "mac/todometer.app"
   end
