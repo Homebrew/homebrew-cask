b551a264a15c252e50a6aa71108c9dc204413ae7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 10:52:35 +0000

    terminal-browser 0.11.1

--- Casks/t/terminal-browser.rb
+++ Casks/t/terminal-browser.rb
@@ -2,11 +2,11 @@ cask "terminal-browser" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "0.8.1"
-  sha256 arm:          "3b0e034d557986ab9289aa8baefdca5c3eb66aa493e27110f4e82c05f224a738",
-         intel:        "51645cfdf0428b439434776b5c2c21e50ea1200ea618c742cb3f5d65717f79dd",
-         arm64_linux:  "7f90d97003eedd6ac62e61c82290ffd280f2f0e8f266911f70ab0fbac8a102fe",
-         x86_64_linux: "35e78089d1089dc4f492bbd7d2a03d57ed5543b4394b4fddf3be57c75b77747e"
+  version "0.11.1"
+  sha256 arm:          "9b21729e47bcc07e969913223705ce1ae5bcaa8e49094d6c9705c4cca8311d90",
+         intel:        "9454b1467402049e0d08d07a69eb76b67e94f3c4c60ca2b8aadf4158c193c0f4",
+         arm64_linux:  "ef34c68333c4352e5107d5bd6c5cf7fe840a05c9a48a37084b9fc65fc986385c",
+         x86_64_linux: "b08327655aa3190260cf34807294be7c7c6685aa2639a393055b7c649eeb3a4a"
 
   url "https://terminal-browser.sh/install/dl/stable/v#{version}/terminal-browser-#{os}-#{arch}.tar.gz"
   name "terminal-browser"
