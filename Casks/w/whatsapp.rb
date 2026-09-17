e125f84188fa540091186bf5c7dc40ad90c68ae5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 17:05:19 +0000

    whatsapp 26.37.22

--- Casks/w/whatsapp.rb
+++ Casks/w/whatsapp.rb
@@ -1,6 +1,6 @@
 cask "whatsapp" do
-  version "26.37.15"
-  sha256 "68d92165face17a16e4ea7f33fcfbb1cf5989c2cb09ee8687501b35d2310b549"
+  version "26.37.22"
+  sha256 "6f471e73bff882bb0b32271514b728f63dd74fb74f1abe90305f9fbb47e9e965"
 
   url "https://web.whatsapp.com/desktop/mac_native/release/?version=2.#{version}&extension=zip&configuration=Release&branch=master&is_buck=true"
   name "WhatsApp"
