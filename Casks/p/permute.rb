5ef51b97310c5984ce4e57a60736f83b69f75c2e
Patrick Linnane <patrick@linnane.io>
2026-09-17 08:05:48 -0700

    permute: remove redundant macOS minimum
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/p/permute.rb
+++ Casks/p/permute.rb
@@ -9,7 +9,7 @@ cask "permute" do
     version "3.14.8,4018"
     sha256 "eacd8883163f43d9b48ae42e0900096a0f7b323488aaf9276ce49dad017f6649"
 
-    depends_on macos: :big_sur
+    depends_on macos: :any
   end
 
   url "https://software.charliemonroe.net/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg"
