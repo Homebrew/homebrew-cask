7c33c1580ac4e065237abd1b9b7dbee6e9354faa
Christopher Addison <christophera@unity3d.com>
2026-09-18 09:08:07 -0700

    plasticscm-cloud-edition: rename to unity-version-control

--- Casks/u/unity-version-control.rb
+++ Casks/u/unity-version-control.rb
@@ -1,4 +1,4 @@
-cask "plasticscm-cloud-edition" do
+cask "unity-version-control" do
   arch arm: "arm64", intel: "x64"
   zip_arch = on_arch_conditional arm: "-arm"
 
@@ -7,9 +7,11 @@ cask "plasticscm-cloud-edition" do
          intel: "2807888445dd0d9bfc31112f4e1b8eac164a18996ff1686b341eb4576491415e"
 
   url "https://d26z97tczqnlef.cloudfront.net/releases/#{version}/plasticscm/osx/unity-vcs-#{version}-mac#{zip_arch}.pkg.zip"
-  name "PlasicSCM - a Cloud Edition"
-  desc "Install PlasticSCM locally and join a Cloud Edition subscription"
-  homepage "https://www.plasticscm.com/"
+  name "Unity Version Control"
+  name "Unity VCS"
+  name "UVCS"
+  desc "Install Unity Version Control locally and join a Cloud Edition subscription"
+  homepage "https://docs.unity.com/en-us/unity-version-control"
 
   livecheck do
     url "https://www.plasticscm.com/download"
