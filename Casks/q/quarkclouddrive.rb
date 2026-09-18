f9767706bf09726265795514be4fb19a6f9a3753
Samuel Huang <hi@sgh.ng>
2026-09-18 19:10:32 -0400

    quarkclouddrive 7.2.2.806

--- Casks/q/quarkclouddrive.rb
+++ Casks/q/quarkclouddrive.rb
@@ -1,38 +1,17 @@
 cask "quarkclouddrive" do
-  arch arm: "arm64", intel: "x64"
+  version "7.2.2.806"
+  sha256 "faeedba86b24085e9cc24d4105d37599b7b23f036e21152c5198c98b30d67c37"
 
-  on_arm do
-    version "7.2.0.792,3172746"
-    sha256 "529f2836c2dfbf95add095667e5a56187237aa3142f8c238fa12bd97471237c5"
-  end
-  on_intel do
-    version "7.2.0.792,3172748"
-    sha256 "3c532235a51034bc4bf9e8ea78370010169285bfec6c1cd1d133d990ae19e753"
-  end
-
-  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@product_guanwang/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_abi#{arch}_(Build#{version.csv.second}).dmg"
+  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@product_guanwang_macmini/QuarkCloudDriveMac_V#{version}_mac_pf30003_(zh-cn)_releasemini_(Build3004627).dmg"
   name "Quark Cloud Drive"
   name "夸克网盘"
   desc "Cloud storage and file management platform"
   homepage "https://pan.quark.cn/"
 
   livecheck do
-    url "https://pan.quark.cn/api/client_version?pr=ucpro&fr=pc",
-        referer: "https://pan.quark.cn/"
-    regex(%r{
-      /download/37213/quarkclouddrivemac/
-      pckk@product_guanwang/
-      QuarkCloudDriveMac[._-]v?(\d+(?:\.\d+)+)
-      [^/]*abi#{arch}[^(]*\(Build(\d+)\)\.dmg
-    }xi)
-    strategy :json do |json, regex|
-      data = json["data"]
-      next [] if data.blank?
-
-      [data["macDmgUrl"], data["macDmgForArmUrl"]].filter_map do |url|
-        match = url&.match(regex)
-        "#{match[1]},#{match[2]}" if match
-      end
+    url "https://pan.quark.cn/api/client_version"
+    strategy :json do |json|
+      json.dig("data", "origin_macDmgForArmUrl")&.[](/QuarkCloudDriveMac[._-]v?(\d+(?:\.\d+)+)[._-]mac/i, 1)
     end
   end
 
