fb1f574b46bd1e2f920dcd1c0652b318076f7259
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 13:53:49 +0000

    sxitch 1.5.2

--- Casks/s/sxitch.rb
+++ Casks/s/sxitch.rb
@@ -1,6 +1,6 @@
 cask "sxitch" do
-  version "1.5.1"
-  sha256 "45a4e1bf1fde85b728a7d87afcac4d09900f2f3aeac7919f8ae735bd515ce690"
+  version "1.5.2"
+  sha256 "e4fff1608b3e340bedab9660b3db14d4a1302402d59a4df13152cd3f655f80a5"
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip"
   name "Sxitch"
