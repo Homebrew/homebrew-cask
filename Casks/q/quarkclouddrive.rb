d5863a619e40a9494534b497acc169df93ce59b6
Samuel Huang <hi@sgh.ng>
2026-09-18 19:10:32 -0400

    quarkclouddrive 7.2.2.806

--- Casks/q/quarkclouddrive.rb
+++ Casks/q/quarkclouddrive.rb
@@ -1,44 +1,37 @@
 cask "quarkclouddrive" do
-  arch arm: "arm64", intel: "x64"
+  version "7.2.2.806,3004627"
+  sha256 "faeedba86b24085e9cc24d4105d37599b7b23f036e21152c5198c98b30d67c37"
 
-  on_arm do
-    version "7.2.0.792,3172746"
-    sha256 "529f2836c2dfbf95add095667e5a56187237aa3142f8c238fa12bd97471237c5"
-  end
-  on_intel do
-    version "7.2.0.792,3172748"
-    sha256 "3c532235a51034bc4bf9e8ea78370010169285bfec6c1cd1d133d990ae19e753"
-  end
-
-  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@product_guanwang/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_abi#{arch}_(Build#{version.csv.second}).dmg"
+  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@product_guanwang_macmini/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_releasemini_(Build#{version.csv.second}).dmg"
   name "Quark Cloud Drive"
   name "夸克网盘"
   desc "Cloud storage and file management platform"
   homepage "https://pan.quark.cn/"
 
   livecheck do
-    url "https://pan.quark.cn/api/client_version?pr=ucpro&fr=pc",
-        referer: "https://pan.quark.cn/"
-    regex(%r{
-      /download/37213/quarkclouddrivemac/
-      pckk@product_guanwang/
-      QuarkCloudDriveMac[._-]v?(\d+(?:\.\d+)+)
-      [^/]*abi#{arch}[^(]*\(Build(\d+)\)\.dmg
-    }xi)
-    strategy :json do |json, regex|
-      data = json["data"]
-      next [] if data.blank?
+    url "https://pan.quark.cn/api/client_version"
+    strategy :json do |json|
+      url = json.dig("data", "origin_macDmgForArmUrl") || json.dig("data", "macDmgForArmUrl")
+      match = url&.match(%r{QuarkCloudDriveMac[._-]v?(\d+(?:\.\d+)+)[^/]*\(Build(\d+)\)\.dmg}i)
+      next if match.blank?
 
-      [data["macDmgUrl"], data["macDmgForArmUrl"]].filter_map do |url|
-        match = url&.match(regex)
-        "#{match[1]},#{match[2]}" if match
-      end
+      "#{match[1]},#{match[2]}"
     end
   end
 
   depends_on :macos
 
-  app "QuarkCloudDrive.app"
+  installer script: {
+    executable: "QuarkCloudDrive.app/Contents/MacOS/QuarkCloudDrive",
+    args:       [
+      "--quark-install",
+      "--install-from=mini_install",
+      "--quark-ch=pckk@product_guanwang_macmini",
+    ],
+  }
+
+  uninstall quit:  "com.quark.clouddrive.desktop",
+            trash: "/Applications/QuarkCloudDrive.app"
 
   zap trash: [
     "~/Library/Application Support/QuarkCloudDrive",
