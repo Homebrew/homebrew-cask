044bab41360685578c951cce793e80f5c1cb1b6a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 01:10:54 +0000

    beekeeper-studio 6.1.2

--- Casks/b/beekeeper-studio.rb
+++ Casks/b/beekeeper-studio.rb
@@ -2,11 +2,11 @@ cask "beekeeper-studio" do
   arch arm: "-arm64"
   os macos: "dmg", linux: "AppImage"
 
-  version "6.1.1"
-  sha256 arm:          "65e2d165be7e0015e50d74f2f6815614304578f8e1ddd1a40a582203816126cf",
-         intel:        "5268ac9c8f149fb0c00a113ae02007274f8386ce91eb7446e4275c500ba8e60b",
-         arm64_linux:  "632bb5e99df2da4512541ded1e51882db692302912e3c4a91a4d7f4fe7ea3f23",
-         x86_64_linux: "634a2c3f2ee84075af10229f05953815e162a891bc84c15be994ed1d98426b3e"
+  version "6.1.2"
+  sha256 arm:          "2b3a0e9b45fee28d1d1fba4e10250cb127054d7b652d9358288c89bb9cdc02e5",
+         intel:        "c48bc99b2a882dc59c67fa5b3899fdc5c8b1465556eed8659045acc341881bb1",
+         arm64_linux:  "6abccaf98f45b84cb83a2ba2082099920e6648379406be121e42e4546b1e23c9",
+         x86_64_linux: "808b46a4dc6516bffeb35e1c0211312c37ef39170d64521f1f4f39c50c5009d2"
 
   on_macos do
     auto_updates true
