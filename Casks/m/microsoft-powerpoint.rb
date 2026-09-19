b5c6279fd1acafc7aaa8733e2cbffc589dc98087
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 01:55:31 +0000

    microsoft-powerpoint 16.113.26091740

--- Casks/m/microsoft-powerpoint.rb
+++ Casks/m/microsoft-powerpoint.rb
@@ -18,8 +18,8 @@ cask "microsoft-powerpoint" do
     end
   end
   on_sonoma :or_newer do
-    version "16.113.26091433"
-    sha256 "a53535cba997880e8003059211e1d826629a1f55d9f4e1deabc1e7d18c11f46f"
+    version "16.113.26091740"
+    sha256 "b17cd601dc6415abe77444302fb80cf41630278063df476cc9118ee8ca8a1f27"
 
     livecheck do
       url "https://go.microsoft.com/fwlink/p/?linkid=525136"
