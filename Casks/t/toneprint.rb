147f374717f63e613a17af3108919802c435c23e
Bevan Kay <email@bevankay.me>
2026-09-19 15:29:38 +1000

    toneprint 4.7.2

--- Casks/t/toneprint.rb
+++ Casks/t/toneprint.rb
@@ -1,30 +1,23 @@
 cask "toneprint" do
-  version "4.6.16,dBdeIScCJECmA6pTU4VfHA,bykSZtSgsUKVx3UABT5qSg,7+25e8c66a"
-  sha256 "d9bbfef769a14ccf2a024218efb7164420211c9d68f2630dd31ce4edbce2d106"
+  version "4.7.2,4580aa8802a3c5cccde223bc3eaeca51"
+  sha256 "ff9db220fb31272071959d98d7424d7ed76ee3501d6f0c6452b2a439733f9da9"
 
-  url "https://cdn.mediavalet.com/aunsw/musictribe/#{version.csv.second}/#{version.csv.third}/Original/Toneprint_#{version.csv.first}-public.#{version.csv.fourth}.dmg"
+  url "https://cdn-media.empowertribe.com/#{version.csv.second}/TonePrint_#{version.csv.first}.dmg"
   name "TonePrint"
   desc "Alter the character of your TonePrint pedal"
-  homepage "https://www.tcelectronic.com/tonePrints.html"
+  homepage "https://www.tcelectronic.com/en/toneprints"
 
   livecheck do
-    url "https://www.tcelectronic.com/.rest/musictribe/v1/downloadcenter/solr-dldatatable?brandName=tcelectronic&modelCode=P0CLC&iDisplayStart=0&iDisplayLength=15"
-    regex(%r{/musictribe/(\w+)/(\w+)/Original/TonePrint[._-]v?(\d+(?:\.\d+)+)[._-]public[._-](\d+\+\h+)\.dmg}i)
-    strategy :json do |json, regex|
-      json["aaData"]&.map do |item|
-        next if item["subGroupName"] != "Mac"
-
-        match = item.dig("url", "url")&.match(regex)
-        next if match.blank?
-
-        "#{match[3]},#{match[1]},#{match[2]},#{match[4]}"
-      end
+    url "https://www.tcelectronic.com/en/products/0709-aik", header: "RSC: 1"
+    regex(%r{/(\h+)/TonePrint[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
   end
 
   depends_on :macos
 
-  app "Toneprint.app"
+  app "TonePrint.app"
 
   zap trash: [
     "~/Library/Application Support/TonePrint",
