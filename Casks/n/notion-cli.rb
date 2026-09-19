b49a37b1fd5036c9ac37b66f94bc12eee302bbfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 13:26:38 +0000

    notion-cli 0.23.8

--- Casks/n/notion-cli.rb
+++ Casks/n/notion-cli.rb
@@ -2,11 +2,11 @@ cask "notion-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-musl"
 
-  version "0.23.7"
-  sha256 arm:          "2dd7fd7d64c2d137c7e7a7dfeacfc8eb47ea2c7f385205cc51ca9696df9d7370",
-         intel:        "695005751c719c7f8326b10fdfca0dcc3d46625595919f0301c31475047df2f3",
-         arm64_linux:  "33b44ad698fcf27c95e8f41111d0021dae04a9cd6b8940620641e6c74775c3bf",
-         x86_64_linux: "928d44db2a9597fb3e9dcce9c22679b21536c566d6e5bf1b8c3a24804cd21b84"
+  version "0.23.8"
+  sha256 arm:          "b6db1cb669c05299c2932db0e9d25c7aeafd9663c5937bb61b6408667c5752e7",
+         intel:        "1d5cec232cc48526940fdff49ab5aab287292febffb2c4071eb72e6a9dbae84b",
+         arm64_linux:  "8a31708c9881ca1d7e1ea49a69156bf17aa8aff940759fc4a4523e001ea7745e",
+         x86_64_linux: "f904a09883a70a30d00da8176909f3d3b70cb14d7731ea7d22289152193cae7e"
 
   url "https://ntn.dev/releases/v#{version}/ntn-#{arch}-#{os}.tar.gz"
   name "Notion CLI"
