2bff273f39dcfad8db64a63d9acedc700b37fb89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 16:48:18 +0000

    yaak 2026.8.0 (arm only)

--- Casks/y/yaak.rb
+++ Casks/y/yaak.rb
@@ -3,12 +3,17 @@ cask "yaak" do
   os macos: "Yaak", linux: "yaak"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "2026.7.1"
-  sha256 arm:          "4cb8760964a6ddf51a1d8ed9b3b32bdbb61c5c2c608fb2707f4d757c228a6dff",
+  sha256 arm:          "df110eed6c1b39dfb0fcefb4131b71f07135ec246b72b772ef8f8d31e20c8aed",
          intel:        "997e18f1e116f2048d4151735b2d2e4d814efbc2f2d3192cc600b01ea0a9ab70",
-         arm64_linux:  "d6810698c61d72511a2acd319a4d0a4339e02fc16f7a578881538af053785332",
+         arm64_linux:  "f2856f1d3eb374faa564e717ca69183bf1797ed471cb47bfe46147fd49ea3ac3",
          x86_64_linux: "6022c792246a60c50cb06ff61455deff0a7b6eeed876e9da844518a8116315e4"
 
+  on_arm do
+    version "2026.8.0"
+  end
+  on_intel do
+    version "2026.7.1"
+  end
   on_macos do
     auto_updates true
     depends_on macos: :ventura
