1b08ef81d36a5e2c7a8b4bf99da08d7d67ec9063
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 01:48:51 +0000

    openshot-video-editor@daily 4.0.0,16978-813fcca9-8f03cf89

--- Casks/o/openshot-video-editor@daily.rb
+++ Casks/o/openshot-video-editor@daily.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor@daily" do
-  version "4.0.0,16962-f20829fb-8f03cf89"
-  sha256 "597025bcf3f6dd015e6c50afece323a5453777298427ec0b5603b96ae786bda5"
+  version "4.0.0,16978-813fcca9-8f03cf89"
+  sha256 "b36d732c6b56425a0f9b7d121510379c1180defdf01a6847b8c57acf3c67af64"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-#{version.csv.third || "daily"}-#{version.csv.second}-x86_64.dmg"
   name "OpenShot Video Editor (Daily Build)"
