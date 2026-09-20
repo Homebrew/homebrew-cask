485fa5b9770d5851c840532845f2320bcbb3bbf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 03:43:46 +0000

    clash-mi 1.0.30.1604

--- Casks/c/clash-mi.rb
+++ Casks/c/clash-mi.rb
@@ -1,10 +1,10 @@
 cask "clash-mi" do
   url_end = on_system_conditional macos: "macos_universal.dmg", linux: "linux_amd64.AppImage"
 
-  version "1.0.29.1503"
-  sha256 arm:          "036dab099692d4f8824724d279719fb29ede6c6873a7711cf1d19243de76247f",
-         intel:        "036dab099692d4f8824724d279719fb29ede6c6873a7711cf1d19243de76247f",
-         x86_64_linux: "3853c7e26dcec8c42b53e4c3e321d093c73697a3f265cfadd2949e739835169d"
+  version "1.0.30.1604"
+  sha256 arm:          "4342af77ed58ff0e4e983fda4ea4061a94c1aa86b9274ac46b880530fd55278f",
+         intel:        "4342af77ed58ff0e4e983fda4ea4061a94c1aa86b9274ac46b880530fd55278f",
+         x86_64_linux: "23fca59d8aeaf0d136be6485e61f331a947f8cac329dec875b5413989340ef84"
 
   on_macos do
     depends_on macos: :monterey
