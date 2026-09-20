31040968632233499620a6162aae319294f0aa67
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 13:10:10 +0000

    keyty 1.7.0

--- Casks/k/keyty.rb
+++ Casks/k/keyty.rb
@@ -1,6 +1,6 @@
 cask "keyty" do
-  version "1.6.0"
-  sha256 "a8c65272cccc731f9799c4fa5b9f8dd078dde56fcfe48888d59de30394720d1b"
+  version "1.7.0"
+  sha256 "437acdd76ab6ba8e4ae9d5bfd2de0194342250f2c4446522c36d4c4b0b121b40"
 
   url "https://github.com/keytyapp/Keyty/releases/download/v#{version}/Keyty.dmg"
   name "Keyty"
