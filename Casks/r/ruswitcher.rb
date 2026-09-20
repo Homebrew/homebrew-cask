50f7b2ea6fc9f59af595cc71dbc1efa483b85075
Rash MacMiniM2 <xrashid@gmail.com>
2026-09-20 09:17:40 +0300

    ruswitcher 3.3.0 (new cask)

---
+++ Casks/r/ruswitcher.rb
@@ -0,0 +1,24 @@
+cask "ruswitcher" do
+  version "3.3.0"
+  sha256 "22e1ffa0c2021eb75c0d341d175ef7da310b5411e82456125d8f24fa59a02a19"
+
+  url "https://github.com/rashn/RuSwitcher/releases/download/v#{version}/RuSwitcher-#{version}.dmg"
+  name "RuSwitcher"
+  desc "Keyboard layout switcher that fixes text typed in the wrong layout"
+  homepage "https://github.com/rashn/RuSwitcher"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: :ventura
+
+  app "RuSwitcher.app"
+
+  zap trash: [
+    "~/Library/Logs/RuSwitcher",
+    "~/Library/Preferences/com.ruswitcher.app.plist",
+  ]
+end
