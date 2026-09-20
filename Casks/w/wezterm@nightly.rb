cc1070213c5d90599a6606869f012439c5be81e7
AltCode <altcode@vivaldi.net>
2026-09-20 18:01:40 +0200

    wezterm@nightly: reorder zsh/fish completion stanzas

--- Casks/w/wezterm@nightly.rb
+++ Casks/w/wezterm@nightly.rb
@@ -24,8 +24,8 @@ cask "wezterm@nightly" do
   end
 
   bash_completion "#{appdir}/WezTerm.app/Contents/Resources/shell-completion/bash", target: "wezterm"
-  fish_completion "#{appdir}/WezTerm.app/Contents/Resources/shell-completion/fish", target: "wezterm.fish"
   zsh_completion "#{appdir}/WezTerm.app/Contents/Resources/shell-completion/zsh", target: "_wezterm"
+  fish_completion "#{appdir}/WezTerm.app/Contents/Resources/shell-completion/fish", target: "wezterm.fish"
 
   zap trash: "~/Library/Saved Application State/com.github.wez.wezterm.savedState"
 end
