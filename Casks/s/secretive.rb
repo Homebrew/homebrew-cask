93a2638c7aeb64a5ac84726feafdd80ccb0bd4a9
mschfh <37435502+mschfh@users.noreply.github.com>
2026-09-21 01:49:35 -0500

    secretive: fix minimum macOS for v4.0.0

--- Casks/s/secretive.rb
+++ Casks/s/secretive.rb
@@ -13,7 +13,15 @@ cask "secretive" do
       skip "Legacy version"
     end
   end
-  on_sonoma :or_newer do
+  on_sonoma do
+    version "3.0.4"
+    sha256 "696d07812e4431075234a900a0136dbad3131a91086e535fc2b07d69a1d084ba"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_sequoia :or_newer do
     version "4.0.0"
     sha256 "877517e212938ffb7048bc7b6811e992194ba4b3eaa4dae36405b6303d1ac1fa"
 
