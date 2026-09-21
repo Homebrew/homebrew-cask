716d9f9b764cdb0309413d2c827c67db4ccbb3fd
Patrick Linnane <patrick@linnane.io>
2026-09-21 13:42:26 -0700

    ghostty: reorder completion stanzas
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/ghostty.rb
+++ Casks/g/ghostty.rb
@@ -20,8 +20,8 @@ cask "ghostty" do
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man1/ghostty.1"
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man5/ghostty.5"
   bash_completion "#{appdir}/Ghostty.app/Contents/Resources/bash-completion/completions/ghostty.bash"
-  fish_completion "#{appdir}/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish"
   zsh_completion "#{appdir}/Ghostty.app/Contents/Resources/zsh/site-functions/_ghostty"
+  fish_completion "#{appdir}/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish"
 
   zap trash: [
     "~/.cache/ghostty",
