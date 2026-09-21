5e4c224d0cc6e6614cccd330788ecc2698aec78e
Patrick Linnane <patrick@linnane.io>
2026-09-21 13:42:27 -0700

    ghostty@tip: reorder completion stanzas
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/ghostty@tip.rb
+++ Casks/g/ghostty@tip.rb
@@ -31,8 +31,8 @@ cask "ghostty@tip" do
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man1/ghostty.1"
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man5/ghostty.5"
   bash_completion "#{appdir}/Ghostty.app/Contents/Resources/bash-completion/completions/ghostty.bash"
-  fish_completion "#{appdir}/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish"
   zsh_completion "#{appdir}/Ghostty.app/Contents/Resources/zsh/site-functions/_ghostty"
+  fish_completion "#{appdir}/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish"
 
   uninstall quit: "com.mitchellh.ghostty"
 
