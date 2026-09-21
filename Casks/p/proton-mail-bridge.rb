b56f8a49505f7789ea87f29c46aaccdf9d3e5c04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 10:38:11 +0000

    proton-mail-bridge 3.27.0

--- Casks/p/proton-mail-bridge.rb
+++ Casks/p/proton-mail-bridge.rb
@@ -1,6 +1,6 @@
 cask "proton-mail-bridge" do
-  version "3.26.0"
-  sha256 "9f47cedf1e5a239c1c97083723a2b704677410719bc0c82f75e227fe89953884"
+  version "3.27.0"
+  sha256 "44152e90108ae4280aafec86604b7871697bd514633c2afb911038f9ab50fe3e"
 
   url "https://github.com/ProtonMail/proton-bridge/releases/download/v#{version}/Bridge-Installer.dmg"
   name "Proton Mail Bridge"
