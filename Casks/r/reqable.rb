2a4f6e81965814b2ef63870cd08612073e205ee7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-09-20 22:38:25 -0400

    reqable: add Linux AppImage

--- Casks/r/reqable.rb
+++ Casks/r/reqable.rb
@@ -1,20 +1,44 @@
 cask "reqable" do
   arch arm: "arm64", intel: "x86_64"
+  os macos: "macos", linux: "linux"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "3.2.23"
-  sha256 arm:   "894def95a02a53fc0fbdbf9ae2f5291ccc25ba1e8cda2fd87c31dc80fe34fe4c",
-         intel: "feb96a2ab938bb118119fec97666682f5b06d79195f6ffffef29fb5227ab2b9e"
+  sha256 arm:          "894def95a02a53fc0fbdbf9ae2f5291ccc25ba1e8cda2fd87c31dc80fe34fe4c",
+         intel:        "feb96a2ab938bb118119fec97666682f5b06d79195f6ffffef29fb5227ab2b9e",
+         x86_64_linux: "b7cdceb6fc2b7eb6e5b31b47076d32bfa6c69ff8e545cc493ae357fbfa58b469"
 
-  url "https://github.com/reqable/reqable-app/releases/download/#{version}/reqable-app-macos-#{arch}.dmg"
+  on_macos do
+    app "Reqable.app"
+
+    uninstall_postflight_steps do
+      delete_keychain_certificates "Reqable Proxy"
+    end
+
+    zap trash: [
+      "~/Library/Application Support/com.reqable.macosx",
+      "~/Library/Caches/Reqable",
+      "~/Library/Preferences/com.reqable.macosx.plist",
+    ]
+  end
+  on_linux do
+    depends_on arch: :x86_64
+
+    app_image "reqable-app-linux-#{arch}.AppImage", target: "Reqable.AppImage"
+
+    zap trash: "~/.local/share/com.reqable.linux"
+  end
+
+  url "https://github.com/reqable/reqable-app/releases/download/#{version}/reqable-app-#{os}-#{arch}.#{url_end}"
   name "Reqable"
   desc "Advanced API Debugging Proxy"
   homepage "https://reqable.com/"
 
-  # Not every GitHub release provides a file for macOS
+  # Not every GitHub release provides a file for macOS/Linux
   # so we check multiple recent releases instead of only the "latest" release.
   livecheck do
     url :url
-    regex(/^reqable[._-]app[._-]macos[._-]#{arch}\.dmg$/i)
+    regex(/^reqable[._-]app[._-]#{os}[._-]#{arch}\.#{url_end}$/i)
     strategy :github_releases do |json, regex|
       json.map do |release|
         next if release["draft"] || release["prerelease"]
@@ -30,17 +54,4 @@ cask "reqable" do
   end
 
   auto_updates true
-  depends_on :macos
-
-  app "Reqable.app"
-
-  uninstall_postflight_steps do
-    delete_keychain_certificates "Reqable Proxy"
-  end
-
-  zap trash: [
-    "~/Library/Application Support/com.reqable.macosx",
-    "~/Library/Caches/Reqable",
-    "~/Library/Preferences/com.reqable.macosx.plist",
-  ]
 end
