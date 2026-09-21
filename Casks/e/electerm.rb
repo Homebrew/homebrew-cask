7b36d75b3ff94f393fb30deb0957e3d5e2c9fbd5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-09-20 22:47:11 -0400

    electerm: add Linux AppImage

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,11 +1,35 @@
 cask "electerm" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: on_system_conditional(macos: "x64", linux: "x86_64")
+  os macos: "mac", linux: "linux"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
   version "5.5.15"
-  sha256 arm:   "98cc8278d0ea9918b46f80514fe8bdea5b2a5ac0fcae054e37e70e75f0c96bdc",
-         intel: "37e6f814196f5fbabe7c1984471c16ef2542e92f028e84b35d41ca1a01affe55"
+  sha256 arm:          "98cc8278d0ea9918b46f80514fe8bdea5b2a5ac0fcae054e37e70e75f0c96bdc",
+         intel:        "37e6f814196f5fbabe7c1984471c16ef2542e92f028e84b35d41ca1a01affe55",
+         arm64_linux:  "a4a3d4daecbea9fcca2f76e0833999934f26f45044264be22281de4653b83157",
+         x86_64_linux: "5a418e14db59811348dd7bf53920c6e9a30accd8134066f4113b395e8f1306e8"
 
-  url "https://mirror.electerm.org/https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
+  on_macos do
+    depends_on macos: :monterey
+
+    app "electerm.app"
+    binary "#{appdir}/electerm.app/Contents/MacOS/electerm"
+
+    zap trash: [
+      "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.electerm.electerm.sfl*",
+      "~/Library/Application Support/electerm",
+      "~/Library/Logs/electerm",
+      "~/Library/Preferences/org.electerm.electerm.plist",
+      "~/Library/Saved Application State/org.electerm.electerm.savedState",
+    ]
+  end
+  on_linux do
+    app_image "electerm-#{version}-linux-#{arch}.AppImage", target: "electerm.AppImage"
+
+    zap trash: "~/.config/electerm"
+  end
+
+  url "https://mirror.electerm.org/https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-#{os}-#{arch}.#{url_end}"
   name "electerm"
   desc "Terminal/ssh/sftp/telnet/serialport/RDP/VNC/Spice/ftp client"
   homepage "https://electerm.org/"
@@ -18,16 +42,4 @@ cask "electerm" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
-
-  app "electerm.app"
-  binary "#{appdir}/electerm.app/Contents/MacOS/electerm"
-
-  zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.electerm.electerm.sfl*",
-    "~/Library/Application Support/electerm",
-    "~/Library/Logs/electerm",
-    "~/Library/Preferences/org.electerm.electerm.plist",
-    "~/Library/Saved Application State/org.electerm.electerm.savedState",
-  ]
 end
