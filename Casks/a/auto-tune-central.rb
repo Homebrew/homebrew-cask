ebb9058bc111273af010bff90542eeff9e20bd71
小恶魔 <281437288@qq.com>
2026-09-22 13:09:11 +0800

    Fix cask style

--- Casks/a/auto-tune-central.rb
+++ Casks/a/auto-tune-central.rb
@@ -6,11 +6,13 @@ cask "auto-tune-central" do
   name "Auto-Tune Central"
   desc "Software download manager for Antares products"
   homepage "https://www.antarestech.com/"
-  depends_on :macos
+
   livecheck do
     url "https://www.antarestech.com/software-download"
     regex(/Auto_Tune_Central[._-]v?(\d+(?:\.\d+)+)[._-]universal/i)
   end
+
+  depends_on :macos
 
   app "Auto-Tune Central.app"
 end
