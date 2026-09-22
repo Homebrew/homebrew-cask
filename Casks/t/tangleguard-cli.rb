f26e738d79bc0bb74db70a6d6fccbccf598e459f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 20:52:30 +0000

    tangleguard-cli 0.15.2

--- Casks/t/tangleguard-cli.rb
+++ Casks/t/tangleguard-cli.rb
@@ -2,11 +2,11 @@ cask "tangleguard-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-gnu"
 
-  version "0.15.1"
-  sha256 arm:          "54c8c8bb13616e3e275545652a2e64e5126472ef3c99b14de14a0f44fe726273",
-         intel:        "db6068169ec15cd1ac2cf7bf445f9cf3e75f20274c5231cc498b0f86973d55d7",
-         arm64_linux:  "f3389dc86635b82aae0eb2ff52ff2bef1f8ff9475561a044361c256f7197da64",
-         x86_64_linux: "2583dfa5b5b734061890eac03cbff5216a16e6d7133604fc5dc6b526f19b437c"
+  version "0.15.2"
+  sha256 arm:          "0300a7d12cf49d4888e9cd4fc623be93e468bb2c5b5f2e33e8da79c340fc0ecd",
+         intel:        "e7f0afae4e8f946c42b9abf632d045c4ad6d5638e5ae684cc79149473089a3bb",
+         arm64_linux:  "feb71d47852335018910f6b206dab1dfb22cbf4e4ffa17d127411bb4744ba74d",
+         x86_64_linux: "ccb4081d63293af182f986f4893b49eb12815fbf412b3e07f44095788ed21088"
 
   on_macos do
     zap trash: "~/Library/Application Support/CrashReporter/tangleguard-cli*"
