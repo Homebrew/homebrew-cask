90f6ecd92702ba306a69691aa1117ab259834f94
Stephen Krol <krol17@gmail.com>
2026-09-22 16:47:26 +0200

    otel-viewer 0.1.1 (new cask)

---
+++ Casks/o/otel-viewer.rb
@@ -0,0 +1,17 @@
+cask "otel-viewer" do
+  version "0.1.1"
+  sha256 "77c759167af1fabac7cb913e6a6579c36d3ea1e135d54e475dc13c89a0f4dda0"
+
+  url "https://github.com/stivio00/otel-viewer/releases/download/v#{version}/otel-viewer_#{version}_aarch64.dmg"
+  name "otel-viewer"
+  desc "OTLP/gRPC collector with DuckDB storage, REST API and SQL console"
+  homepage "https://github.com/stivio00/otel-viewer"
+
+  depends_on arch: :arm
+
+  app "otel-viewer.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.otelviewer.desktop",
+  ]
+end
