e7c5c7f905e5b113fd75f7ac9fbfccf05b50157e
botantony <antonsm21@gmail.com>
2026-09-22 16:07:25 +0200

    readdle-spark: add `uninstall` stanza
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/r/readdle-spark.rb
+++ Casks/r/readdle-spark.rb
@@ -19,11 +19,19 @@ cask "readdle-spark" do
 
   app "Spark Desktop.app"
 
+  uninstall quit: "com.readdle.SparkDesktop"
+
   zap trash: [
+    "~/Library/Application Scripts/com.readdle.SparkDesktop.share",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.readdle.sparkdesktop.sfl*",
+    "~/Library/Application Support/com.readdle.SparkDesktop.helper",
     "~/Library/Application Support/Spark Desktop",
     "~/Library/Caches/com.readdle.SparkDesktop.helper",
     "~/Library/Caches/Spark Desktop",
+    "~/Library/Containers/com.readdle.SparkDesktop.share",
+    "~/Library/HTTPStorages/com.readdle.SparkDesktop*",
     "~/Library/Logs/Spark Desktop",
+    "~/Library/Preferences/com.readdle.SparkDesktop.helper.plist",
     "~/Library/Preferences/com.readdle.SparkDesktop.plist",
     "~/Library/Saved Application State/com.readdle.SparkDesktop.savedState",
   ]
