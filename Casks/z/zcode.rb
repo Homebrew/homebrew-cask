63624af546be0ec489a124418fc09697872cc7d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 11:04:41 +0000

    zcode 3.14.3

--- Casks/z/zcode.rb
+++ Casks/z/zcode.rb
@@ -1,9 +1,9 @@
 cask "zcode" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.14.1"
-  sha256 arm:   "f2ae7aba49681a65745657f9d95ddd8fd9dfa2db3e6e3bae91b3fecec68a99b6",
-         intel: "f65b6b6e97a4098bf214460bb05a5fc00107c678e8b9ee40b746fe9923001219"
+  version "3.14.3"
+  sha256 arm:   "32ef4120c8823bd6f793a1491669c62ca66d04c7a706ebbc736071a75a1d6802",
+         intel: "475a6dc3d43b0b158d631b397a3101717f5ecb9f4e9ea8a3a8b9fc8407de4ef4"
 
   url "https://cdn-zcode.z.ai/zcode/electron/releases/#{version}/macos-#{arch}/ZCode-#{version}-mac-#{arch}.dmg"
   name "ZCode"
