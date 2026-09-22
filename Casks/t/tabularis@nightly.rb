e5760b1503c55be5755b273ee68489de76cdb9cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 14:14:33 +0000

    tabularis@nightly 0.25.1-1

--- Casks/t/tabularis@nightly.rb
+++ Casks/t/tabularis@nightly.rb
@@ -2,10 +2,10 @@ cask "tabularis@nightly" do
   arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
   os macos: "dmg", linux: "AppImage"
 
-  version "0.24.1-6,20260922-72895f9"
-  sha256 arm:          "387ca33e53d3b8bf91933aa0d2a77bb43c0084d07fb88f21b5eaba8317973573",
-         intel:        "7af36fb87ab818cc5eccdd947db2a95597438b714be0b6b769e405f4515a9d61",
-         x86_64_linux: "9863cc54698f08b87e3251bf2f220607eedf9555d98bb4bdef901f8fd8776da4"
+  version "0.25.1-1,20260922-65baceb"
+  sha256 arm:          "e78e5d076b21c892cbc1f7a97fc5c0dcfed883d0e2aef697974573c51e9cdb6f",
+         intel:        "11506fa34b736ae734067a64727d6c91583ce26d0a82f8024f4bba24b050d86c",
+         x86_64_linux: "b8bbd6fa1c2d7fbfda9fff08264b44639992cad95b71850c03fd475d3750662b"
 
   on_macos do
     depends_on macos: :monterey
