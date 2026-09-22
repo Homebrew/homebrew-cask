ef34c09d60398a5cdada9dde1f4f9d1db9a8f2ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 22:34:13 +0000

    picview 5.1.2

--- Casks/p/picview.rb
+++ Casks/p/picview.rb
@@ -1,9 +1,9 @@
 cask "picview" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.1.1"
-  sha256 arm:   "8986be39e23360d4e53a6183fcc267f03f1bd142d1f37e4db1922236e6903141",
-         intel: "50b03bed805da9ab2ac19c614ae3ba76d070d757f55895f74250013a274bc31b"
+  version "5.1.2"
+  sha256 arm:   "ed7d7a59ad80d4edef6d617e360b4f9170643ce56ef354656b8773f4a3c41440",
+         intel: "284c31ac46927f3592fed69c0e634a9c6c90209bf7c59d26b0fb3c15f53176c0"
 
   url "https://github.com/Ruben2776/PicView/releases/download/#{version}/PicView-#{version}-macOS-#{arch}.dmg"
   name "PicView"
