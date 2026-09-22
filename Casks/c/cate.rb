f0f1eb03a109fd721c00024e68c649361196fa83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 18:51:55 +0000

    cate 2.0.3

--- Casks/c/cate.rb
+++ Casks/c/cate.rb
@@ -2,10 +2,10 @@ cask "cate" do
   arch arm: "-arm64"
   url_end = on_system_conditional macos: "#{arch}.dmg", linux: ".AppImage"
 
-  version "2.0.2"
-  sha256 arm:          "683fac19eb02cdecd5351ce126f09d1795750f4f17eb586931424bb0f5b1b327",
-         intel:        "4c5879b232fc991b43e3d1f7bd58c8046c3cffb1db861df7d31d82b475577809",
-         x86_64_linux: "6b13242256291a9702d725e71f5ee63540652b96c35561950eec6f0f57451996"
+  version "2.0.3"
+  sha256 arm:          "98d9c06d2bada74851d0fff8e85c7f61314df62f73349f17a46fff1ea162f26d",
+         intel:        "be2198eb35d3935e07c5a990795ef7f9f449287be79fc0dc97b0687d6de57c70",
+         x86_64_linux: "5614eb5d7207125023e6c283262998402e3eda22a212e60bbcdfd386fa6510bf"
 
   on_macos do
     depends_on macos: :monterey
