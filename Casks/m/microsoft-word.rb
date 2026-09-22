c0504a045f2c641148f8ee05a05e57d83e657ad5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 16:38:57 +0000

    microsoft-word 16.113.26092012

--- Casks/m/microsoft-word.rb
+++ Casks/m/microsoft-word.rb
@@ -18,8 +18,8 @@ cask "microsoft-word" do
     end
   end
   on_sonoma :or_newer do
-    version "16.113.26091740"
-    sha256 "04d354e2a56f4ebaa8f56b82cb1dd75309a2444415f9f0830cea5ba8f97f1e9a"
+    version "16.113.26092012"
+    sha256 "747876ba0c52d2b1b6cc0abdc62716cda6fd2a2a5d5cff0358e3fae5a8f269c5"
 
     livecheck do
       url "https://go.microsoft.com/fwlink/p/?linkid=525134"
