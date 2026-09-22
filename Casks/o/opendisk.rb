24a9d8d0f5d66f0ae3e51b943cb150da7a9d4753
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 22:29:53 +0000

    opendisk 1.2.1

--- Casks/o/opendisk.rb
+++ Casks/o/opendisk.rb
@@ -1,6 +1,6 @@
 cask "opendisk" do
-  version "1.2.0"
-  sha256 "feb0ede55c3f158fff90a6fc84c2986fb3be07b3ef0f4c118aa713a8638d50b9"
+  version "1.2.1"
+  sha256 "a3e255d3240180123e823981050587a17dbb4412d5e99f39041d272b025701e2"
 
   url "https://github.com/137137137/OpenDisk/releases/download/v#{version}/OpenDisk-#{version}.zip"
   name "OpenDisk"
@@ -13,7 +13,7 @@ cask "opendisk" do
   end
 
   auto_updates true
-  depends_on macos: :tahoe
+  depends_on macos: :sequoia
 
   app "OpenDisk.app"
 
