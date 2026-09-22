f9e35876d52ee5c41cb0ae9ccc581d71e71ece69
Tom Matthews <trcm@pm.me>
2026-09-22 15:10:54 +0100

    recaf: install Java for Finder launching

--- Casks/r/recaf.rb
+++ Casks/r/recaf.rb
@@ -27,6 +27,7 @@ cask "recaf" do
     end
   end
 
+  depends_on cask: "temurin@25"
   depends_on :macos
   container type: :naked
 
@@ -37,8 +38,4 @@ cask "recaf" do
     "~/Library/Application Support/Recaf",
     "~/Library/Preferences/Recaf",
   ]
-
-  caveats do
-    depends_on_java "22+"
-  end
 end
