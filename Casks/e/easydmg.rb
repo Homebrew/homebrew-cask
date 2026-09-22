55b80466016cbb676b9b00ee63e9bdf2c760c399
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 07:06:34 +0000

    easydmg 2.5.0

--- Casks/e/easydmg.rb
+++ Casks/e/easydmg.rb
@@ -1,6 +1,6 @@
 cask "easydmg" do
-  version "2.4.0"
-  sha256 "b8d8247ac9bae0632df2246d88bd5fba4b5498a197f0ca55a4d0470cb6eb4bf1"
+  version "2.5.0"
+  sha256 "c320cae3ea6cc6d90035a42220543710f8b42de54b8df55ab0b1d99110a4e68c"
 
   url "https://github.com/jeff-schumann/EasyDMG/releases/download/v#{version}/EasyDMG.dmg"
   name "EasyDMG"
