dadf9c7f9a219b84e71ea65ed6e531a6efa5c7e5
botantony <antonsm21@gmail.com>
2026-09-23 02:05:21 +0200

    blood-on-the-clocktower-online 3.57.3

--- Casks/b/blood-on-the-clocktower-online.rb
+++ Casks/b/blood-on-the-clocktower-online.rb
@@ -1,9 +1,9 @@
 cask "blood-on-the-clocktower-online" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.57.2"
-  sha256 arm:   "6ead034fe1a5cb464e160a24fa0a46e21ad8642c893b91f565ab5f3dee0eaa24",
-         intel: "e9dcf4aa35b080134f5657a9482c9e282f3205c5c01ac1ea5cc0cd45feb48bf1"
+  version "3.57.3"
+  sha256 arm:   "0382aba9bbd11761751ca494e9c153c0c1e31c236e11f656ab0bc59995f6018c",
+         intel: "9b1b5e89af34162ed904751c942e0041fb6dca5f54da2858db53759296401b6f"
 
   url "https://github.com/ThePandemoniumInstitute/botc-release/releases/download/v#{version}/Blood.on.the.Clocktower.Online_#{version}_#{arch}.dmg"
   name "Blood on the Clocktower Online"
