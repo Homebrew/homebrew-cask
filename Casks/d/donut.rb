529642d725bf18d8ee0fabb8425af1d6068603a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 15:51:26 +0000

    donut 0.31.2

--- Casks/d/donut.rb
+++ Casks/d/donut.rb
@@ -2,11 +2,11 @@ cask "donut" do
   arch arm: "aarch64", intel: on_system_conditional(macos: "x64", linux: "amd64")
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "0.31.1"
-  sha256 arm:          "e794007c23537ad2732768850c682cd0c2314b34810c5ded712b055733b9e8f1",
-         intel:        "0fe301f64863b8874629f58a2a60cdcd07a73616bb4a294d4dee1d1e7ea0ee0b",
-         arm64_linux:  "2433fe7e2de68ebd4af06090e683e80271e0889d8296a538c4ffdad6e077cbe4",
-         x86_64_linux: "0578da20299cad6751e20b642cbbbd6d5e1e7641c709757fd0f23c0da1e58e33"
+  version "0.31.2"
+  sha256 arm:          "50eee0f66ffdb720b888f2dd03913cf6c67d1969b6cdd0417f4fd5c93aae78b7",
+         intel:        "5a26ad024e0fc7899414df49e5df1904bcbda3ec24563b04a8df2c5f0ba5c1e2",
+         arm64_linux:  "2b4502757cce30e2907975851fd6e1317731b50f772b8da07fb3af852c5db727",
+         x86_64_linux: "29082fb8a9dbf41f95fae432b0aec0bfeaccbadcafeee57b26a87394cd48bbed"
 
   on_macos do
     app "Donut.app"
