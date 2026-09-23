0335879625dac54f3d9995f4d006556eb175352b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 21:41:24 +0000

    calendr 1.25.5

--- Casks/c/calendr.rb
+++ Casks/c/calendr.rb
@@ -1,6 +1,6 @@
 cask "calendr" do
-  version "1.25.4"
-  sha256 "fafcd0292ba69c51eecafb9a80049682a61a7351641846faa91487abb2f968e1"
+  version "1.25.5"
+  sha256 "2b8d6c43bac47b5f4f5830cbbfe2f1bb01d0571cabc21029ef94ddb1bf7d7bc0"
 
   url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.zip"
   name "Calendr"
