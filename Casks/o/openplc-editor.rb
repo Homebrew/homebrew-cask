a5e6f5722a39ec965d9b0e5e4532d72bd7644759
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 16:23:02 +0000

    openplc-editor 4.3.1

--- Casks/o/openplc-editor.rb
+++ Casks/o/openplc-editor.rb
@@ -1,9 +1,9 @@
 cask "openplc-editor" do
   arch arm: "-ARM"
 
-  version "4.3.0"
-  sha256 arm:   "b307ea2a98f5728f802f4d20be0e848e3cab3cff8c011d0556d60c645c24238a",
-         intel: "d9a60991f811832961a3522e58b445833892e0f7494ea6a2359cde9ceeb4d2ed"
+  version "4.3.1"
+  sha256 arm:   "85308d6c52126d02fb394b725aaccf9e68290a87548be076e57dfe373fd461f6",
+         intel: "0d87b4b5d09b44c1df4ce446995b92aa66ad62be13e50dba6eebc1831579a178"
 
   url "https://github.com/Autonomy-Logic/openplc-editor/releases/download/v#{version}/OpenPLC_Editor_#{version}#{arch}.dmg"
   name "OpenPLC Editor"
