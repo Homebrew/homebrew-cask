6053f1941d53d014eb6f4ea2b874b90a818f2064
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 08:03:18 +0000

    visual-studio-code@insiders 1.140.0-insider

--- Casks/v/visual-studio-code@insiders.rb
+++ Casks/v/visual-studio-code@insiders.rb
@@ -1,9 +1,9 @@
 cask "visual-studio-code@insiders" do
   arch arm: "-arm64"
 
-  version "1.139.0-insider,1790072138550,a3b0c130bdeeb42c8de4817bd4f5b00b051e8393"
-  sha256 arm:   "469af8fed0949da15d394a34581de6f319e5c14e88bd4e19c179c3c6da8a1a46",
-         intel: "d2eb625694fe0ce86d01aa15ce88370f4e0709d8b5823bda074401538347e89d"
+  version "1.140.0-insider,1790140111422,9cc2a44bf20ac6f4badef53bc9f33dfdb5803378"
+  sha256 arm:   "99e85d05b6d03a3c6fae131c4e30bb03a372bd8c83f93d7b6a185e72cea46944",
+         intel: "f5b874f07c827266713e6022e0a967335753253a12a42b467b7c8841c6076afc"
 
   url "https://vscode.download.prss.microsoft.com/dbazure/download/insider/#{version.csv.third}/VSCode-darwin#{arch}.zip"
   name "Microsoft Visual Studio Code Insiders"
