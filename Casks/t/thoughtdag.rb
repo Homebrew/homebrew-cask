70f9f08204ddeb2bf2cf2fa0074c067316f0e978
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 14:02:44 +0000

    thoughtdag 0.4.20

--- Casks/t/thoughtdag.rb
+++ Casks/t/thoughtdag.rb
@@ -1,9 +1,9 @@
 cask "thoughtdag" do
   arch arm: "-arm64"
 
-  version "0.4.18"
-  sha256 arm:   "e5268ed50d9c9e9f2188f97cbc545126722297404af5c8c1985f212e8d610939",
-         intel: "99618f808df6b1c49879308e173c3631817c758b0c576b1cf7f888a21cb360d4"
+  version "0.4.20"
+  sha256 arm:   "d9a3d0e88ac5db3f3415c884aaa5ff873b2fc907471d1cadd7010c050ca8b3a8",
+         intel: "f33348268b2895340e3d05fa1fdf068297babbe0d645c30acf6ed05430a18aba"
 
   url "https://github.com/chenxiachan/thoughtdag/releases/download/v#{version}/ThoughtDAG-#{version}#{arch}.dmg"
   name "ThoughtDAG"
