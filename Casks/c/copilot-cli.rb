4c07c3c5d8bd7b6f24226ec8429349712ab6545b
Devraj Mehta <devm33@github.com>
2026-09-22 21:34:54 -0400

    Refactor zap trash to use array syntax

--- Casks/c/copilot-cli.rb
+++ Casks/c/copilot-cli.rb
@@ -28,6 +28,8 @@ cask "copilot-cli" do
   binary "copilot"
   generate_completions_from_executable "copilot", "completion"
 
-  zap trash: "~/.copilot"
-  zap trash: "~/Library/Caches/copilot"
+  zap trash: [
+    "~/.copilot",
+    "~/Library/Caches/copilot",
+  ]
 end
