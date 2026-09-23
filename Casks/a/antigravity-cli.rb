e61db351f85a07a3ea625a56076a301fe3fd6eee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 06:51:30 +0000

    antigravity-cli 1.2.9

--- Casks/a/antigravity-cli.rb
+++ Casks/a/antigravity-cli.rb
@@ -5,11 +5,11 @@ cask "antigravity-cli" do
   os macos: "darwin", linux: "linux"
   file_os = on_system_conditional macos: "mac", linux: "linux"
 
-  version "1.2.8,4907747922280448"
-  sha256 arm:          "f77d57d99ca83e50a3a767d70621c99962ce4242e5470220f3adbf1c50844538",
-         intel:        "8ba276816890a91171a8b0861e4e9eaeb334f884743f525702095c04b4bbf446",
-         arm64_linux:  "85ea71929436711e4b0332026508ee4e7ddb05192f24f1774dfbed879255fc6e",
-         x86_64_linux: "244752206d1f65c01aff489628f1df51f1a3fddacaa8ed74984661ebb6d09136"
+  version "1.2.9,5905287731871744"
+  sha256 arm:          "2b2671c846f62cb1159817517e4a9fff3e9a2ffe01f9dd0b3e227298d88b46f6",
+         intel:        "84c284693bc59334066263a397f6da8a34490b2d8a71dea6d67edc9852ef20b6",
+         arm64_linux:  "8a63cf4c4f559e2ff91bd46fbdf015ca7937415805d0cff82015b9cb9dbbdfcd",
+         x86_64_linux: "d9850373f3df866011024a961fa9740cc4adaac060eebe9c70fbf263ac6b2624"
 
   on_macos do
     depends_on macos: :monterey
