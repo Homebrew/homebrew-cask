d4c4d7cb11d8ae2bee7ca486e1e813b50bc221c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:43:26 +0000

    wox 2.4.5

--- Casks/w/wox.rb
+++ Casks/w/wox.rb
@@ -1,9 +1,9 @@
 cask "wox" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2.4.4"
-  sha256 arm:   "dfa2a016fb2bdaa4a65f2ed3647924705cca10f5972e08847e7d8683a0916a2d",
-         intel: "d1bbd750160e5ac37cd4d993f4eefbc0836067da7e8ccad1ac6f3111ae652fb0"
+  version "2.4.5"
+  sha256 arm:   "6c03a238344aa789fecabd3164217240e5aa1ac9da40fadd01181f30d6bb8f25",
+         intel: "cd45839ff75848d08601f9bfbb84014606b729bc2e4e76d124af3743f4d5e74f"
 
   url "https://github.com/Wox-launcher/Wox/releases/download/v#{version}/wox-mac-#{arch}.dmg"
   name "Wox"
