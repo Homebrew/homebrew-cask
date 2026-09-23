d072b1c30ee1d0dcd014b70d5ec89fa8f8d622a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 19:31:21 +0000

    kando 3.0.0

--- Casks/k/kando.rb
+++ Casks/k/kando.rb
@@ -1,9 +1,9 @@
 cask "kando" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.1"
-  sha256 arm:   "69a8e7699366a490146646dd221dfa30c13b7e30308390f3987120c7ce8bc766",
-         intel: "39401a49a32377b4f73a082efe31a831bc9a927d2c6d1b7ad364494daf5c4c58"
+  version "3.0.0"
+  sha256 arm:   "4b6542e0e6191b742603f3577c599f137b127520b465289c522b23d1d31f71c0",
+         intel: "42d36215b259e7e928cfc24aa1a9793230867f78b93028c2ef375ff234572879"
 
   url "https://github.com/kando-menu/kando/releases/download/v#{version}/Kando-#{version}-#{arch}.dmg"
   name "Kando"
