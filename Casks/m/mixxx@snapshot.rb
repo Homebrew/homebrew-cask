dd6e80d5d22ae144f7de0e3dddd60207953eda59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 22:24:46 +0000

    mixxx@snapshot 2.7-alpha-404-g87ba13b2bb

--- Casks/m/mixxx@snapshot.rb
+++ Casks/m/mixxx@snapshot.rb
@@ -1,14 +1,14 @@
 cask "mixxx@snapshot" do
   arch arm: "arm", intel: "intel"
 
-  sha256 arm:   "860acce2324c18e55cdf911d760757a272a000f649177f8c48427a723a7f2460",
-         intel: "9e0ac7b3efa3c250ff7deb903f702f8b3c8f52b63080bac3e6f42e495c354d3d"
+  sha256 arm:   "0d9479f718a3c62b21b31c2d56133339080e9c7aa884c176c965ef12d588906e",
+         intel: "a9bd57dba129f455ea19ee768d219ac040895ca4bd9afbd0c3e7d94e09deb2f2"
 
   on_arm do
-    version "2.7-alpha-403-ga2ba5177d7"
+    version "2.7-alpha-404-g87ba13b2bb"
   end
   on_intel do
-    version "2.7-alpha-403-ga2ba5177d7"
+    version "2.7-alpha-404-g87ba13b2bb"
   end
 
   url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
