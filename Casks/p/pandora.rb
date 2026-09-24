07010ce4ee90e94aeb0d8fdd7040b5e50a26b984
heatwaveo8 <rheaton2015@outlook.com>
2026-09-24 14:52:25 -0700

    pandora 16.0.3

--- Casks/p/pandora.rb
+++ Casks/p/pandora.rb
@@ -1,29 +1,32 @@
 cask "pandora" do
-  version "15.0.3"
-  sha256 "1858152de9d5fe18c876093a95c39c92fe878fba633c3e54e27fcdeb70a06c15"
+  version "16.0.3"
+  sha256 "04412c09f13416b8ad66ca5123e48b9803cfb627f09892434af66a40ed3a41b8"
 
-  url "https://p-desktop-app.s3.amazonaws.com/releases/Pandora-#{version}.dmg"
+  url "https://s3-us-west-2.amazonaws.com/p-desktop-app/releases/Pandora-#{version}-universal.dmg"
   name "Pandora"
   desc "Desktop client for the Pandora web radio service"
   homepage "https://www.pandora.com/desktop"
 
   livecheck do
-    url "https://pdora.co/desktop_mac_download"
-    strategy :header_match
+    url "https://pandora-web.app.link/e/desktop_mac_download"
+    regex(/Pandora[._-](\d+(?:\.\d+)+)[._-]universal\.dmg/i)
+    strategy :page_match
   end
 
   depends_on :macos
 
   app "Pandora.app"
 
+  uninstall quit: "com.pandora.desktop"
+
   zap trash: [
+    "~/Caches/com.pandora.desktop",
+    "~/Caches/com.pandora.desktop.ShipIt",
+    "~/Caches/Pandora",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.pandora.desktop.sfl*",
     "~/Library/Application Support/Pandora",
     "~/Library/Logs/Pandora",
     "~/Library/Preferences/com.pandora.desktop.plist",
     "~/Library/Saved Application State/com.pandora.desktop.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
