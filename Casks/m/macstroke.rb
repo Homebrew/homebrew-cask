0bb4416b266eea9dfb1efe70254057259094be9e
MTJO <mtjo@qq.com>
2026-09-24 08:11:03 +0800

    macstroke 3.0.0
    The 3.x line is the same app rewritten in Swift: same bundle id
    (net.mtjo.MacStroke), same data locations, and 2.x users can upgrade in
    place through the existing Sparkle appcast. The release asset is a
    universal binary (arm64 + x86_64) and requires macOS 13 or later.
    
    Signed-off-by: MTJO <mtjo@qq.com>

--- Casks/m/macstroke.rb
+++ Casks/m/macstroke.rb
@@ -1,20 +1,27 @@
 cask "macstroke" do
-  version "2.0.5"
-  sha256 "113116367df18a2d3278d9540afdb0476f6b45dd6fbd09a565c9c038bb5e1a88"
+  version "3.0.0"
+  sha256 "f69ec4d8a77d8a7f351bfcea01317abb0dc516f9b1d87df0bb0ed8ff310bce94"
 
   url "https://github.com/mtjo/MacStroke/releases/download/#{version}/MacStroke.zip"
   name "MacStroke"
   desc "Configurable global mouse gestures"
   homepage "https://github.com/mtjo/MacStroke/"
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
-  depends_on :macos
+  auto_updates true
+  depends_on macos: :ventura
 
   app "MacStroke.app"
 
+  uninstall quit: "net.mtjo.MacStroke"
+
   zap trash: [
     "~/Library/Application Scripts/net.mtjo.MacStroke.FinderSyncExtension",
+    "~/Library/Application Support/MacStroke",
     "~/Library/Caches/MacStroke",
     "~/Library/Caches/net.mtjo.MacStroke",
     "~/Library/Containers/net.mtjo.MacStroke.FinderSyncExtension",
