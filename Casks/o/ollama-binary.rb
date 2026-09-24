844c542423b8b00abdb0de2c2316b4f9f3824a9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 07:51:18 +0000

    ollama-binary 0.34.4

--- Casks/o/ollama-binary.rb
+++ Casks/o/ollama-binary.rb
@@ -3,11 +3,11 @@ cask "ollama-binary" do
   os macos: "darwin", linux: "linux"
   url_end = on_system_conditional macos: ".tgz", linux: "-#{arch}.tar.zst"
 
-  version "0.34.3"
-  sha256 arm:          "2c45865f94bce0d4d1d2567603dd2fdacaf375585220a175aa4800105193d36e",
-         intel:        "2c45865f94bce0d4d1d2567603dd2fdacaf375585220a175aa4800105193d36e",
-         arm64_linux:  "cb1d3c178d48b302dbe42b4fb0ce25ef6282e02eac25496cfc07f5333e2264dd",
-         x86_64_linux: "e83a089fd0cd2f79ee2933cca2085846a2065f497adbc6467c402177c68423f9"
+  version "0.34.4"
+  sha256 arm:          "e9c8fddaab5f48f47f2c4ae3d23d0732f5182417125353faeed2188e34a22799",
+         intel:        "e9c8fddaab5f48f47f2c4ae3d23d0732f5182417125353faeed2188e34a22799",
+         arm64_linux:  "96f50a1192133028cf4e010d8c333f8af14b1505db6be7b2034c11487e7fd7e6",
+         x86_64_linux: "c238986e61d40c0cc5f4a9b9e40b9eea104350b77efa34741fc134e105cb9533"
 
   on_macos do
     conflicts_with cask: "ollama-app"
