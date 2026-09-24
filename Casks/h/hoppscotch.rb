e49375883fb6f16b63760d65084294235c60c8c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 13:36:17 +0000

    hoppscotch 26.8.2-0

--- Casks/h/hoppscotch.rb
+++ Casks/h/hoppscotch.rb
@@ -3,10 +3,10 @@ cask "hoppscotch" do
   os macos: "mac", linux: "linux"
   url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  version "26.8.1-0"
-  sha256 arm:          "0548faf630d803aa331ba1d5569e17bcd9bfd58b32b7e002678542286b3d4a00",
-         intel:        "f3db532d837147fb7bfa2afeeff6b8b053fe7ea1a5eb2f9e634db1ff89ff2566",
-         x86_64_linux: "75550be19ffbcf26cee11a8695d45e040f6a71153b23ce38bdb624dceac33169"
+  version "26.8.2-0"
+  sha256 arm:          "f307364a41e5becb3d93e6a007f24a7c5fed0073f12d569bea67885ec9447aab",
+         intel:        "82f2e91d8db32340a98fc6f1cf76d2a73f3a6ba37ca33d6b7bfb14d89d0033c4",
+         x86_64_linux: "a41b794bef1576a8a9991c0e190bab1e9660e6e1efd0c261d066eecc11434e38"
 
   on_macos do
     app "Hoppscotch.app"
