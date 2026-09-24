a54b5478534d05200c92e86c826a72bce7210cdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 14:38:43 +0000

    unity-windows-support-for-editor 6000.6.3f1

--- Casks/u/unity-windows-support-for-editor.rb
+++ Casks/u/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "6000.6.2f1,770e33f6875c"
-  sha256 "3d8ea0bec9446206a77488c512023c0db4090eb696366cce88cb118e24bd3fcd"
+  version "6000.6.3f1,45d8eee7de74"
+  sha256 "0ea0fcbd8dffc2ab9d0126bc356e8e18c96c75bf5fd91f5bfdf55fb7cf6e1c41"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg"
   name "Unity Windows (Mono) Build Support"
