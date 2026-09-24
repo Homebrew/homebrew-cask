5b812e1242da0b0cbcad61fb4c8e0942e90ce4e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 01:42:19 +0000

    moonfin 2.6.0

--- Casks/m/moonfin.rb
+++ Casks/m/moonfin.rb
@@ -1,6 +1,6 @@
 cask "moonfin" do
-  version "2.5.1"
-  sha256 "2105316137ba140fc41d0976281ef75bddac8b29ab3e55c7f6e4d6ce72542475"
+  version "2.6.0"
+  sha256 "354391a46f79458de9a77327f342672308bd755aba6c665af784c2461bd99be2"
 
   url "https://github.com/Moonfin-Client/Moonfin-Core/releases/download/#{version}/Moonfin_macOS_v#{version}.dmg"
   name "Moonfin"
