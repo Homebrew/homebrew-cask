7ff090fbd0f1c5a95f3d84431ba5106e8dd6bcaf
Sam Doran <github@samdoran.com>
2026-09-24 12:01:11 -0400

    Add livecheck using Sparkle

--- Casks/a/appzapper.rb
+++ Casks/a/appzapper.rb
@@ -1,11 +1,16 @@
 cask "appzapper" do
-  version "3000"
+  version "1.0"
   sha256 "eff16e4f7a9d8498cf10f6d29ce78a38b89e043b07f57848a250f61fee4a84a7"
 
-  url "https://appzapper.com/AppZapper-#{version}.zip"
-  name "AppZapper"
+  url "https://appzapper.com/3000-updates/AppZapper-3000-#{version}-30.zip"
+  name "AppZapper 3000"
   desc "Tool to uninstall unwanted applications and their support files"
   homepage "https://appzapper.com/"
+
+  livecheck do
+    url "https://appzapper.com/3000-updates/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
 
   depends_on macos: :sequoia
 
