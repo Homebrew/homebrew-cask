d8230dc581b7bb65f1b658c759f6c5117d743a36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 17:11:56 +0000

    semeru-jdk-open@8 8.0.504.5

--- Casks/s/semeru-jdk-open@8.rb
+++ Casks/s/semeru-jdk-open@8.rb
@@ -1,6 +1,6 @@
 cask "semeru-jdk-open@8" do
-  version "8.0.504.0"
-  sha256 "a5799c5d58e7105559abeab0976b2b1fd8155544c325ed98edd2ea4a7399865e"
+  version "8.0.504.5"
+  sha256 "ebb1ab095614399ff24ca89686187eef8193bf23748da7a6b77bf318a6895d1f"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version}/ibm-semeru-open-jdk_x64_mac_#{version}.pkg"
   name "IBM Semeru Runtime (JDK 8) Open Edition"
