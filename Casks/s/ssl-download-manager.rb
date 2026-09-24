5d28c0c3e4893e8205178dbcb68e0f1f5824f842
小恶魔 <281437288@qq.com>
2026-09-24 19:34:14 +0800

    Fix SSL Download Manager zap

--- Casks/s/ssl-download-manager.rb
+++ Casks/s/ssl-download-manager.rb
@@ -16,13 +16,14 @@ cask "ssl-download-manager" do
 
   app "SSL Download Manager.app"
 
-  zap trash: [
-    "~/Library/Application Support/CrashReporter/SSL Download Manager_*.plist",
-    "~/Library/Application Support/SSL Download Manager",
-    "~/Library/Caches/com.solidstatelogic.downloadmanager*",
-    "~/Library/HTTPStorages/com.solidstatelogic.downloadmanager",
-    "~/Library/Logs/SSL Download Manager",
-    "~/Library/Preferences/ByHost/com.solidstatelogic.downloadmanager.ShipIt.*.plist",
-    "~/Library/Preferences/com.solidstatelogic.downloadmanager.plist",
-  ]
+  zap launchctl: "com.solidstatelogic.analytics",
+      trash:     [
+        "~/Library/Application Support/CrashReporter/SSL Download Manager_*.plist",
+        "~/Library/Application Support/SSL Download Manager",
+        "~/Library/Caches/com.solidstatelogic.downloadmanager*",
+        "~/Library/HTTPStorages/com.solidstatelogic.downloadmanager",
+        "~/Library/Logs/SSL Download Manager",
+        "~/Library/Preferences/ByHost/com.solidstatelogic.downloadmanager.ShipIt.*.plist",
+        "~/Library/Preferences/com.solidstatelogic.downloadmanager.plist",
+      ]
 end
