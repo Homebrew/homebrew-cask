5022c85b9c0f05bb6338d69428b05c766375d2e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 21:48:50 +0000

    deviceshelf 1.9.58

--- Casks/d/deviceshelf.rb
+++ Casks/d/deviceshelf.rb
@@ -2,11 +2,11 @@ cask "deviceshelf" do
   arch arm: on_system_conditional(linux: "-arm64")
   os macos: "dmg", linux: "AppImage"
 
-  version "1.9.56"
-  sha256 arm:          "b460411f2b9f4ce12a7b608c9da34bbd3051ec96121a7ff33fd26a588b418d2f",
-         intel:        "b460411f2b9f4ce12a7b608c9da34bbd3051ec96121a7ff33fd26a588b418d2f",
-         arm64_linux:  "618b87a92630b85a5e1d31ac96fe744fbaabb63a218b7428c3b62aca178cccf6",
-         x86_64_linux: "e185f7a2b3d48c9f574020c9d9e4b9efb6559ef1cffa79e8279360c92cf94d01"
+  version "1.9.58"
+  sha256 arm:          "188e063d03e11ba702ee5b24905089f094d7988345d1afd08407c97f7daaa632",
+         intel:        "188e063d03e11ba702ee5b24905089f094d7988345d1afd08407c97f7daaa632",
+         arm64_linux:  "8c003f34421a8877c4887268a203090e0d2c6fe6da2298d983f792ef03ef2eca",
+         x86_64_linux: "b966bbb40e0e1a2d0fa102f83de4419b7f1f4e7d6bf5a99093dc3014be507e7c"
 
   on_macos do
     app "DeviceShelf.app"
