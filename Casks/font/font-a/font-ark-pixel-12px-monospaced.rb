83c6c0c46ffd0518b5365fd0e24d820f749a6b38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 03:54:48 +0000

    font-ark-pixel-12px-monospaced 2026.09.25

--- Casks/font/font-a/font-ark-pixel-12px-monospaced.rb
+++ Casks/font/font-a/font-ark-pixel-12px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-12px-monospaced" do
-  version "2026.09.01"
-  sha256 "b82530003762e41666aa34a38afbaa2b0787764e0e1ae12e35fabac3de008d4d"
+  version "2026.09.25"
+  sha256 "fb5cd818263e4c49360039f3b5eefe94b74ed966bfdcafdc5e032159cbe90b56"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 12px Monospaced"
@@ -9,9 +9,9 @@ cask "font-ark-pixel-12px-monospaced" do
   font "ark-pixel-12px-monospaced-ja.otf"
   font "ark-pixel-12px-monospaced-ko.otf"
   font "ark-pixel-12px-monospaced-latin.otf"
-  font "ark-pixel-12px-monospaced-zh_cn.otf"
+  font "ark-pixel-12px-monospaced-zh_hans.otf"
+  font "ark-pixel-12px-monospaced-zh_hant.otf"
   font "ark-pixel-12px-monospaced-zh_hk.otf"
-  font "ark-pixel-12px-monospaced-zh_tr.otf"
   font "ark-pixel-12px-monospaced-zh_tw.otf"
 
   # No zap stanza required
