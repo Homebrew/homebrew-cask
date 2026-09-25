42b248dc583070ef4bd2da64581487767fa9ad0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 12:54:55 +0000

    calyx 0.44.0

--- Casks/c/calyx.rb
+++ Casks/c/calyx.rb
@@ -1,6 +1,6 @@
 cask "calyx" do
-  version "0.43.0"
-  sha256 "d4aee9533386ec9003ec8d3fdfac77012fb8c982d326b73b5779f9d6eeba6f6d"
+  version "0.44.0"
+  sha256 "f8da392aac2202dbde1f937a0966c01aed684897b21db41d3a82ceeab9acabb6"
 
   url "https://github.com/yuuichieguchi/Calyx/releases/download/v#{version}/Calyx.zip"
   name "Calyx"
