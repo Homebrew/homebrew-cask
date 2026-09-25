e14c3349830a29f92ed5aca9d6ea6b9bf882ede0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 09:45:32 +0000

    cline-app 0.0.36

--- Casks/c/cline-app.rb
+++ Casks/c/cline-app.rb
@@ -1,6 +1,6 @@
 cask "cline-app" do
-  version "0.0.35"
-  sha256 "c010dd087c44cccf1f82bd2c5b3c3589279763e5f536b1530e6b82da91383013"
+  version "0.0.36"
+  sha256 "ae80e892baec702e705b025ef65c4884bf3e9597cf50b7395f5c00403a9903f0"
 
   url "https://github.com/cline/cline/releases/download/desktop-v#{version}/Cline_#{version}_universal.dmg"
   name "Cline"
