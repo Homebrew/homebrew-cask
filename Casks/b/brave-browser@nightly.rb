4daaefd64af70a400470253f6d97f830a0b6fd56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 21:39:53 +0000

    brave-browser@nightly 1.98.33.0 (arm only)

--- Casks/b/brave-browser@nightly.rb
+++ Casks/b/brave-browser@nightly.rb
@@ -2,11 +2,11 @@ cask "brave-browser@nightly" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  sha256 arm:   "37fa910ebbb0e40b126190423df649aba9e1991df431a17b81ede6762a8a159a",
+  sha256 arm:   "526dd78b59c5cdea7f21c5216906c16d71fde432654d7172625bf3a663541f09",
          intel: "c0c498110c04bf7013f5344e818f597a3f4c099add8da09cd3fedb6881d7c7f2"
 
   on_arm do
-    version "1.98.28.0"
+    version "1.98.33.0"
   end
   on_intel do
     version "1.98.28.0"
