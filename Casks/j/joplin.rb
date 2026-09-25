dee8d0d96602799cdfeccf596b53c7ab9c8b1bbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 22:08:59 +0000

    joplin 3.7.21

--- Casks/j/joplin.rb
+++ Casks/j/joplin.rb
@@ -2,10 +2,10 @@ cask "joplin" do
   arch arm: "-arm64"
   url_end = on_system_conditional macos: "#{arch}.DMG", linux: ".AppImage"
 
-  version "3.7.18"
-  sha256 arm:          "103c9bc884108bc31593060008005411a45c1b2b530eeffca4d5ad262306c6e6",
-         intel:        "73cd99d29fa0756078d70f799dbc440baabc0055c2a80874c88ba101e9af7d52",
-         x86_64_linux: "c7ed7eeb6985621b75f0d09088cd01efc9af7aa2cfa17649ed4a83a75b29aca5"
+  version "3.7.21"
+  sha256 arm:          "7e58db4346240de4cc7b7794d603be06b1a10b981bc5c22e0792080f28e856b7",
+         intel:        "bd4c0a610a6c884ff987b73cd0794c0144f3dfdbeaba7ebc5db8107ad7e40378",
+         x86_64_linux: "5d202dcaca31e14206826aa3f04125e7c218dbc3dc3d98479d811efbd6b856c1"
 
   on_macos do
     depends_on macos: :monterey
