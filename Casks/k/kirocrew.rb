6e8d0c4a6afd3b11784fba8490c5e2aa9c74b2d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 04:08:19 +0000

    kirocrew 0.7.1

--- Casks/k/kirocrew.rb
+++ Casks/k/kirocrew.rb
@@ -2,11 +2,11 @@ cask "kirocrew" do
   arch arm: "aarch64", intel: "x86_64"
   url_end = on_system_conditional macos: ".dmg", linux: "-#{arch}.AppImage"
 
-  version "0.7.0"
-  sha256 arm:          "df474085ddf9640282e4105ef62dee29e97e48d325c8453fb46e5bc3056c1786",
-         intel:        "df474085ddf9640282e4105ef62dee29e97e48d325c8453fb46e5bc3056c1786",
-         arm64_linux:  "a3a39c57cf4779e7118ed7929dee584add5c245e0fb3dc98ba8fbfa26a188c1e",
-         x86_64_linux: "71173fe642d49679ce5e12d297a66c4938dd6a273c6d710a25947e9ad43e77ab"
+  version "0.7.1"
+  sha256 arm:          "0ba1466ae954f4444458192371cb15947aa43f2671ff354e1c6ceed23af9443a",
+         intel:        "0ba1466ae954f4444458192371cb15947aa43f2671ff354e1c6ceed23af9443a",
+         arm64_linux:  "827c6f2628e9a709747610e53ed846c882699171e2ac129b865f0657059cccf8",
+         x86_64_linux: "009fa345323e1f69bd69f0f811d3a9fedb0b47dd215da6e7b9ed3a3b522d9779"
 
   on_macos do
     depends_on macos: :monterey
