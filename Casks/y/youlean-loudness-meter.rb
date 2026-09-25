0520627c6be380393649d035ff811e52856dbeeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 11:06:39 +0000

    youlean-loudness-meter 2.5.15

--- Casks/y/youlean-loudness-meter.rb
+++ Casks/y/youlean-loudness-meter.rb
@@ -1,6 +1,6 @@
 cask "youlean-loudness-meter" do
-  version "2.5.14,2025,11"
-  sha256 "24d6bf74e6f5690c5202746dea9fc6eb616c47e46a6860d01e1e5a4275b8bcf1"
+  version "2.5.15,2026,09"
+  sha256 "0497a988c390e12ac4a7d71fe12aa11847af1787d972d40fc2ae03c25131eeab"
 
   url "https://cdn.youlean.co/wp-content/uploads/#{version.csv.second}/#{version.csv.third}/Youlean-Loudness-Meter-#{version.major}-V#{version.csv.first}-macOS.dmg"
   name "Youlean Loudness Meter"
