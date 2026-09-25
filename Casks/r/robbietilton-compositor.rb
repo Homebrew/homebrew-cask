022ded1878d0489635c724abec5b568a5fa05cdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 22:44:01 +0000

    robbietilton-compositor 1.3.2

--- Casks/r/robbietilton-compositor.rb
+++ Casks/r/robbietilton-compositor.rb
@@ -1,6 +1,6 @@
 cask "robbietilton-compositor" do
-  version "1.3.1"
-  sha256 "c2ebae35d9e2f10c7e392010ff22a0d44fcabb0f49a77765f292e827781fab00"
+  version "1.3.2"
+  sha256 "54c4df9842b55e4b334482ee9bae8cf8be6e4fa7460906b12206e404691adf1d"
 
   url "https://github.com/robbietilton/Compositor/releases/download/v#{version}/Compositor.dmg"
   name "Compositor"
