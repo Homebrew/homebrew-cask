24fcbdc527b6bbd560786226c08c129450b11a61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 09:46:27 +0000

    coderabbit 0.8.1

--- Casks/c/coderabbit.rb
+++ Casks/c/coderabbit.rb
@@ -2,11 +2,11 @@ cask "coderabbit" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "0.8.0"
-  sha256 arm:          "748271195b85cc245613f2affabacd4189c721f3839fb14d3efceccffdb382f5",
-         intel:        "ccebc6335fd7fe0bbe2ad595aa57f684890551ff03114c8cc2403952d9c7ac52",
-         arm64_linux:  "570fd0569239371de6b7cb1004c760052c507d1fbb3189b692988d7f3804ee6d",
-         x86_64_linux: "e60aa408266c3e2350aae890b239ab8d1c12b3f607f4475e1c2ddd4529af1d71"
+  version "0.8.1"
+  sha256 arm:          "c5e22f56c9ab5568367da8bfb633e59c9553f348ad6fcc4cb034edffe0a42d7c",
+         intel:        "911ca4615ebed98109fd4724d16dae6d7a42bfdc1d94ec99632a129d0e3f8ca4",
+         arm64_linux:  "9b42bec99ba4ef842635ecf1d636f2ec285c01103bf0f7a918e5dbf891efb268",
+         x86_64_linux: "39844e339770a780e0606ec53986cb2694398a0a0ea5af420e0b513816326f40"
 
   url "https://cli.coderabbit.ai/releases/#{version}/coderabbit-#{os}-#{arch}.zip"
   name "CodeRabbit"
