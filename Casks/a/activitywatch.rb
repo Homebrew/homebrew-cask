0b6be93c3589fd85c1b7b6c5d46977b60dba8b14
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-09-25 22:08:06 -0400

    activitywatch: add Linux AppImage

--- Casks/a/activitywatch.rb
+++ Casks/a/activitywatch.rb
@@ -1,28 +1,46 @@
 cask "activitywatch" do
-  version "0.13.2"
-  sha256 "22f3bce0e169457902b2c8d2967701cde887171f737d281dd414a210bd3090ed"
+  os macos: "macos-x86_64.dmg", linux: "linux-x86_64.AppImage"
 
-  url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.dmg"
+  version "0.13.2"
+  sha256 arm:          "22f3bce0e169457902b2c8d2967701cde887171f737d281dd414a210bd3090ed",
+         intel:        "22f3bce0e169457902b2c8d2967701cde887171f737d281dd414a210bd3090ed",
+         x86_64_linux: "fda7a9bd13d5d5902210c6552bf5693d4b2a30fce5aed3fef30a21ea8f47fc55"
+
+  on_macos do
+    url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-#{os}"
+
+    app "ActivityWatch.app"
+
+    zap trash: [
+      "~/Library/Application Support/activitywatch",
+      "~/Library/Caches/activitywatch",
+      "~/Library/Logs/activitywatch",
+    ]
+
+    caveats do
+      requires_rosetta
+    end
+  end
+  on_linux do
+    url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-#{os}"
+
+    depends_on arch: :x86_64
+
+    app_image "activitywatch-#{os}", target: "ActivityWatch.AppImage"
+
+    zap trash: [
+      "~/.cache/activitywatch",
+      "~/.config/activitywatch",
+      "~/.local/share/activitywatch",
+    ]
+  end
+
   name "ActivityWatch"
   desc "Time tracker"
   homepage "https://activitywatch.net/"
 
   livecheck do
-    url "https://activitywatch.net/downloads/"
-    regex(/href=.*?activitywatch[._-]v?(\d+(?:\.\d+)+)-macos-x86_64\.dmg/i)
-  end
-
-  depends_on :macos
-
-  app "ActivityWatch.app"
-
-  zap trash: [
-    "~/Library/Application Support/activitywatch",
-    "~/Library/Caches/activitywatch",
-    "~/Library/Logs/activitywatch",
-  ]
-
-  caveats do
-    requires_rosetta
+    url :url
+    strategy :github_latest
   end
 end
