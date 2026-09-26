10c717f897949a8ca52a45faa473297f4be0ead5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 15:43:39 +0000

    dockfix 5.3.0-beta

--- Casks/d/dockfix.rb
+++ Casks/d/dockfix.rb
@@ -1,5 +1,5 @@
 cask "dockfix" do
-  version "5.2.0"
+  version "5.3.0-beta"
   sha256 :no_check
 
   url "https://www.dockfix.app/downloads/DockFix.dmg"
@@ -13,7 +13,7 @@ cask "dockfix" do
   end
 
   auto_updates true
-  depends_on macos: :sonoma
+  depends_on macos: :sequoia
 
   app "DockFix.app"
 
