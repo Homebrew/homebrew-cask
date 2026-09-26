e5dd194062b6f0661009eaf444365807407de087
Justin Krehel <39449589+krehel@users.noreply.github.com>
2026-09-25 22:04:21 -0400

    bambu-studio: add Linux AppImage

--- Casks/b/bambu-studio.rb
+++ Casks/b/bambu-studio.rb
@@ -1,8 +1,36 @@
 cask "bambu-studio" do
-  version "02.08.02.61,20260820225108"
-  sha256 "cf648a95858fb630e1353c4987038df60d6caab693f18411fb95fb809f2d6926"
+  os macos: "mac", linux: "ubuntu24.04"
+  name_start = on_system_conditional macos: "Bambu_Studio", linux: "BambuStudio"
+  url_end = on_system_conditional macos: "dmg", linux: "AppImage"
 
-  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.third || version.csv.first}/Bambu_Studio_mac-v#{version.csv.first}-#{version.csv.second}.dmg"
+  version "02.08.02.61,20260820225108"
+  sha256 arm:          "cf648a95858fb630e1353c4987038df60d6caab693f18411fb95fb809f2d6926",
+         intel:        "cf648a95858fb630e1353c4987038df60d6caab693f18411fb95fb809f2d6926",
+         x86_64_linux: "d501b103fac5424513ec0e8d6bc145fb30719de2c7d94d7320d723740c81a7fd"
+
+  on_macos do
+    app "BambuStudio.app"
+
+    zap trash: [
+      "/Library/Logs/DiagnosticsReports/BambuStudio*",
+      "~/Library/Application Support/BambuStudio",
+      "~/Library/Caches/com.bambulab.bambu-studio",
+      "~/Library/HTTPStorages/com.bambulab.bambu-studio.binarycookies",
+      "~/Library/Preferences/com.bambulab.bambu-studio.plist",
+      "~/Library/Saved Application State/com.bambulab.bambu-studio.savedState",
+      "~/Library/WebKit/com.bambulab.bambu-studio",
+    ]
+  end
+  on_linux do
+    depends_on arch: :x86_64
+
+    app_image "BambuStudio_ubuntu24.04-v#{version.csv.first}-#{version.csv.second}.AppImage",
+              target: "BambuStudio.AppImage"
+
+    zap trash: "~/.config/BambuStudio"
+  end
+
+  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.third || version.csv.first}/#{name_start}_#{os}-v#{version.csv.first}-#{version.csv.second}.#{url_end}"
   name "Bambu Studio"
   desc "3D model slicing software for 3D printers, maintained by Bambu Lab"
   homepage "https://bambulab.com/en/download/studio"
@@ -19,18 +47,4 @@ cask "bambu-studio" do
       end
     end
   end
-
-  depends_on :macos
-
-  app "BambuStudio.app"
-
-  zap trash: [
-    "/Library/Logs/DiagnosticsReports/BambuStudio*",
-    "~/Library/Application Support/BambuStudio",
-    "~/Library/Caches/com.bambulab.bambu-studio",
-    "~/Library/HTTPStorages/com.bambulab.bambu-studio.binarycookies",
-    "~/Library/Preferences/com.bambulab.bambu-studio.plist",
-    "~/Library/Saved Application State/com.bambulab.bambu-studio.savedState",
-    "~/Library/WebKit/com.bambulab.bambu-studio",
-  ]
 end
